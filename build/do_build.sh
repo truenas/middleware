f14fba890d2ccdffc3f05e4ae4350d154faf45d3
M. Warner Losh <imp@freebsd.org>
2010-09-08 02:13:06 +0000

    Add a very simple (and somewhat stupid) build script, slow but works.

---
+++ build/do_build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh -x
+
+# interface:
+# cd to top of tree
+# sh ./build/do_build.sh
+# magic happens :)
+#
+
+root=$(pwd)
+
+# Make sure we have FreeBSD dirs
+if [ ! -d FreeBSD ]; then
+    mkdir FreeBSD
+    mkdir FreeBSD/src
+    mkdir FreeBSD/ports
+fi
+
+# Make sure we have FreeBSD src, fetch using csup if not
+if [ ! -f FreeBSD/supfile -o -n "$force_update" ]; then
+    if [ -z "$FREEBSD_CVSUP_HOST" ]; then
+	echo "No sup host defined, please define FREEBSD_CVSUP_HOST and rerun"
+	exit 1
+    fi
+    echo "Checking out tree from ${FREEBSD_CVSUP_HOST}..."
+    cat <<EOF > FreeBSD/supfile
+*default host=${FREEBSD_CVSUP_HOST}
+*default base=${root}/FreeBSD/sup
+*default prefix=${root}/FreeBSD
+*default release=cvs
+*default delete use-rel-suffix
+
+src-all tag=RELENG_8_1_0_RELEASE
+ports-all date=2010.06.24.12.00.00
+
+EOF
+    csup -L 0 ${root}/FreeBSD/supfile
+fi
+
+# Make sure that all the patches are applied
+touch ${root}/FreeBSD/src-patches
+for i in $(cd ${root}/patches && echo freebsd-*); do
+    if ! grep $i  ${root}/FreeBSD/src-patches; then
+	echo "Applying patch $i..."
+	(cd FreeBSD/src && patch -p0 < ${root}/patches/$i)
+	echo $i >> ${root}/FreeBSD/src-patches
+    fi
+done
+
+# OK, now we can build
+cd FreeBSD/src
+args="-c ../../nanobsd/freenas64.conf"
+if [ `whoami` != "root" ]; then
+    echo "You must be root to run this"
+    exit 1
+fi
+sh tools/tools/nanobsd/nanobsd.sh $args
