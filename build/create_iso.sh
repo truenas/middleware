70511ed15913ab0c6c1ff3662109c523265d2e3c
M. Warner Losh <imp@freebsd.org>
2010-11-19 21:21:23 +0000

    More bulletproofing

--- build/create_iso.sh
+++ build/create_iso.sh
@@ -2,14 +2,6 @@
 
 # This script creates a bootable LiveCD iso from a nanobsd image
 
-root=$(pwd)
-: ${FREENAS_ARCH=$(uname -p)}
-export FREENAS_ARCH
-export NANO_OBJ=${root}/obj.${FREENAS_ARCH}
-: ${REVISION=`svnversion ${root}`}
-export NANO_NAME="FreeNAS-8r${REVISION}-${FREENAS_ARCH}"
-export NANO_IMGNAME="${NANO_NAME}.full"
-
 main()
 {
     # This script must be run as root
@@ -17,6 +9,21 @@ main()
         echo "This script must be run by root"
         exit
     fi
+
+    root=$(pwd)
+    : ${FREENAS_ARCH=$(uname -p)}
+    export FREENAS_ARCH
+    export NANO_OBJ=${root}/obj.${FREENAS_ARCH}
+    : ${REVISION=`svnversion ${root} | tr -d M`}
+    if [ ! -f ${NANO_OBJ}/"FreeNAS-8r${REVISION}-${FREENAS_ARCH}.full" ]; then
+	REVISION=${REVISION}M
+    fi
+    if [ ! -f ${NANO_OBJ}/"FreeNAS-8r${REVISION}-${FREENAS_ARCH}.full" ]; then
+	echo "Can't find image file for ${REVISION}, punting"
+	exit 1
+    fi
+    export NANO_NAME="FreeNAS-8r${REVISION}-${FREENAS_ARCH}"
+    export NANO_IMGNAME="${NANO_NAME}.full"
 
     # Paths that may need altering on the build system
     IMGFILE="${NANO_OBJ}/$NANO_IMGNAME"
