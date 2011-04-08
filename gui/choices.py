0067c9b7069d559c5c9b52462d84ff785d2b870c
William Grzybowski <william88@gmail.com>
2011-04-08 19:24:06 +0000

    change NICChoices to optionally show configured interfaces

--- gui/choices.py
+++ gui/choices.py
@@ -337,7 +337,7 @@ class whoChoices:
 ## Network|Interface Management
 class NICChoices:
     """Populate a list of NIC choices"""
-    def __init__(self, nolagg=False, novlan=False):
+    def __init__(self, nolagg=False, novlan=False, exclude_configured=True):
         pipe = popen("/sbin/ifconfig -l")
         self._NIClist = pipe.read().strip().split(' ')
         # Remove lo0 from choices
@@ -383,17 +383,18 @@ class NICChoices:
                     if interface[0] in self._NIClist:
                         self._NIClist.remove(interface[0])
 
-        try:
-            # Exclude any configured interfaces
-            c.execute("SELECT int_interface FROM network_interfaces "
-                      "WHERE int_ipv4address != '' OR int_dhcp != '0' "
-                      "OR int_ipv6auto != '0' OR int_ipv6address != ''")
-        except sqlite3.OperationalError:
-            pass
-        else:
-            for interface in c:
-                if interface[0] in self._NIClist:
-                    self._NIClist.remove(interface[0])
+        if exclude_configured:
+            try:
+                # Exclude any configured interfaces
+                c.execute("SELECT int_interface FROM network_interfaces "
+                          "WHERE int_ipv4address != '' OR int_dhcp != '0' "
+                          "OR int_ipv6auto != '0' OR int_ipv6address != ''")
+            except sqlite3.OperationalError:
+                pass
+            else:
+                for interface in c:
+                    if interface[0] in self._NIClist:
+                        self._NIClist.remove(interface[0])
 
         self.max_choices = len(self._NIClist)
 
