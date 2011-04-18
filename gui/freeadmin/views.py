26f4617b5ba5c8be0d946a6109de67a9c16031ec
William Grzybowski <william88@gmail.com>
2011-04-18 20:49:49 +0000

    Fixes #267

--- gui/freeadmin/views.py
+++ gui/freeadmin/views.py
@@ -25,7 +25,6 @@
 #
 # $FreeBSD$
 #####################################################################
-
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
