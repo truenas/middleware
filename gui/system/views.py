5a6545d5eccfe67eb06d3ba4104cb7d2e0f9f01c
William Grzybowski <william88@gmail.com>
2011-04-30 12:09:33 +0000

    workaround to show correct uptime local time

--- gui/system/views.py
+++ gui/system/views.py
@@ -51,7 +51,10 @@ def _system_info():
     uname2 = os.uname()[2]
     platform = os.popen("sysctl -n hw.model").read()
     date = os.popen('env -u TZ date').read()
+    tz = os.environ.pop('TZ', None)
     uptime = commands.getoutput("uptime | awk -F', load averages:' '{    print $1 }'")
+    if tz:
+        os.environ['TZ'] = tz
     loadavg = "%.2f, %.2f, %.2f" % os.getloadavg()
 
     try:
