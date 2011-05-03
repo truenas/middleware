04d15ffa69cd97c298f9393ac10b1261d2e1e8b5
William Grzybowski <william88@gmail.com>
2011-05-03 21:19:56 +0000

    prepare for use staticfiles of django 1.3

--- gui/settings.py
+++ gui/settings.py
@@ -89,9 +89,7 @@ STATIC_ROOT = os.path.join(HERE, "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE, "media"),
 )
 
 # List of finder classes that know how to find static files in
