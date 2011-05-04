1b5b9ce53bc9e52e13c275a9d189ec942ebd68db
William Grzybowski <william88@gmail.com>
2011-05-04 20:19:33 +0000

    profile for dojo 1.6; freenasdojo.js build

---
+++ gui/dojango/freenas.profile.js
@@ -0,0 +1,46 @@
+ dependencies ={
+
+   layers:  [
+       {
+       name: "freenasdojo.js",
+       dependencies: [
+           "dijit.Button",
+           "dojo.data.ItemFileReadStore",
+           "dojo.data.ItemFileWriteStore",
+           "dijit.Tree",
+           "dojo.dnd.Moveable",
+           "dojox.grid.DataGrid",
+           "dojox.data.JsonRestStore",
+           "dojo.NodeList-traverse",
+           "dojo.io.iframe",
+           "dojox.validate.regexp",
+           "dijit.layout.BorderContainer",
+           "dijit.layout.ContentPane",
+           "dijit.layout.TabContainer",
+           "dojox.form.FileInput",
+           "dojo._base.xhr",
+           "dojo.timing._base",
+           "dojox.form.CheckedMultiSelect",
+           "dijit.form.MultiSelect",
+           "dijit.ProgressBar",
+           "dijit.MenuBar",
+           "dijit.MenuBarItem",
+           "dijit.Dialog",
+           "dijit.form.Form",
+           "dijit.form.Select",
+           "dijit.form.ValidationTextBox",
+           "dijit.form.NumberTextBox",
+           "dijit.form.Textarea",
+           "dijit.form.TimeTextBox",
+           "dijit.form.ComboBox",
+           "dijit.form.FilteringSelect",
+       ]
+       }
+   ],
+
+   prefixes: [
+       [ "dijit", "../dijit" ],
+       [ "dojox", "../dojox" ],
+   ]
+
+ };
