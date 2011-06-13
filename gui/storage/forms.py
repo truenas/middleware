f2a2028e8b8e110e7017d6c489e89454448e416a
William Grzybowski <william88@gmail.com>
2011-06-13 18:38:17 +0000

    allow creation of a zpool with "."

--- gui/storage/forms.py
+++ gui/storage/forms.py
@@ -216,8 +216,8 @@ class VolumeWizardForm(forms.Form):
 
     def clean_volume_name(self):
         vname = self.cleaned_data['volume_name']
-        if not re.search(r'^[a-z][a-z0-9_-]*$', vname, re.I):
-            raise forms.ValidationError(_("The volume name must start with letters and may include numbers, \"-\", \"_\"."))
+        if not re.search(r'^[a-z][a-z0-9_-.]*$', vname, re.I):
+            raise forms.ValidationError(_("The volume name must start with letters and may include numbers, \"-\", \"_\" and \".\" ."))
         return vname
 
     def clean_group_type(self):
