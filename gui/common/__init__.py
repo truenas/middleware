0ee11f6e3af97f2b46e08bca626fd2d0ea7fefa8
William Grzybowski <william88@gmail.com>
2011-06-22 19:58:20 +0000

    fix humanize size to use IEC binary prefixes symbol [ty to delphij]

--- gui/common/__init__.py
+++ gui/common/__init__.py
@@ -42,10 +42,10 @@ def humanize_number_si(number):
 def humanize_size(number):
     number = int(number)
     humanize_si_map = (
-        ('TB', 1099511627776.0),
-        ('GB', 1073741824.0),
-        ('MB', 1048576.0),
-        ('KB', 1024.0),
+        ('TiB', 1099511627776.0),
+        ('GiB', 1073741824.0),
+        ('MiB', 1048576.0),
+        ('KiB', 1024.0),
         ('B', 1),
         )
     for suffix, factor in humanize_si_map:
