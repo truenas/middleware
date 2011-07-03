b41cdb62985c86dee1676a92cb71a50e2a03c17c
Xin Li <d@delphij.net>
2011-07-03 03:40:10 +0000

    Bump copyright year.

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-
-# Copyright (c) 2010 iXsystems, Inc.
+# Copyright (c) 2010-2011 iXsystems, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
