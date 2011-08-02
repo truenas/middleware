a71dfcc7af90120f30374f0ff6691e12332a07a4
William Grzybowski <william88@gmail.com>
2011-08-02 13:46:54 +0000

    add ftp user required for anonymous ftp login

---
+++ gui/account/migrations/0010_bsdusers_ftp.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+
+        if orm.bsdUsers.objects.filter(models.Q(bsdusr_username='ftp')|models.Q(bsdusr_uid=14)).count() == 0:
+            usr = orm.bsdUsers()
+            usr.bsdusr_uid = 14
+            usr.bsdusr_username = 'ftp'
+            usr.bsdusr_group = orm.bsdGroups.objects.get(bsdgrp_group='ftp')
+            usr.bsdusr_builtin = True
+            usr.save()
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+        orm.bsdUsers.objects.get(bsdusr_uid=14).delete()
+
+    models = {
+        'account.bsdgroupmembership': {
+            'Meta': {'object_name': 'bsdGroupMembership'},
+            'bsdgrpmember_group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['account.bsdGroups']"}),
+            'bsdgrpmember_user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['account.bsdUsers']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'account.bsdgroups': {
+            'Meta': {'object_name': 'bsdGroups'},
+            'bsdgrp_builtin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdgrp_gid': ('django.db.models.fields.IntegerField', [], {}),
+            'bsdgrp_group': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '120'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'account.bsdusers': {
+            'Meta': {'object_name': 'bsdUsers'},
+            'bsdusr_builtin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'bsdusr_full_name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'bsdusr_group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['account.bsdGroups']"}),
+            'bsdusr_home': ('django.db.models.fields.CharField', [], {'default': "'/nonexistent'", 'max_length': '120'}),
+            'bsdusr_shell': ('django.db.models.fields.CharField', [], {'default': "'/bin/csh'", 'max_length': '120'}),
+            'bsdusr_smbhash': ('django.db.models.fields.CharField', [], {'default': "'*'", 'max_length': '128', 'blank': 'True'}),
+            'bsdusr_uid': ('django.db.models.fields.IntegerField', [], {'unique': "'True'", 'max_length': '10'}),
+            'bsdusr_unixhash': ('django.db.models.fields.CharField', [], {'default': "'*'", 'max_length': '128', 'blank': 'True'}),
+            'bsdusr_username': ('django.db.models.fields.CharField', [], {'default': "u'User &'", 'unique': 'True', 'max_length': '16'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['account']
