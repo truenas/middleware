bc399a7f3e943a96aa777d73d9c7b04789be88bf
Garret Cooper <yanegomi@gmail.com>
2011-08-24 20:44:38 +0000

    Move the "is root" check to the top to avoid waiting 15 minutes for the failed check to pop up on the screen.
    Use consistent shell style, i.e.
    - Use hard tabs instead of 4 spaces.
    - Use $() instead of ``.
    
    And other whitespace cleanup..

--- build/do_build.sh
+++ build/do_build.sh
@@ -6,6 +6,11 @@
 # magic happens :)
 #
 
+if [ $(id -ru) -ne 0 ]; then
+	echo "You must be root to run this"
+	exit 1
+fi
+
 root=$(pwd)
 : ${FREENAS_ARCH=$(uname -p)}
 export FREENAS_ARCH
@@ -14,19 +19,18 @@ PREP_SOURCE=${PREP_SOURCE:-""}
 
 # Make sure we have FreeBSD dirs
 if [ ! -d FreeBSD ]; then
-    mkdir FreeBSD
-    mkdir FreeBSD/src
-    mkdir FreeBSD/ports
+	mkdir -p FreeBSD/src
+	mkdir -p FreeBSD/ports
 fi
 
 # Make sure we have FreeBSD src, fetch using csup if not
 if [ ! -f FreeBSD/supfile -o -n "$force_update" ]; then
-    if [ -z "$FREEBSD_CVSUP_HOST" ]; then
-        echo "No sup host defined, please define FREEBSD_CVSUP_HOST and rerun"
-        exit 1
-    fi
-    echo "Checking out tree from ${FREEBSD_CVSUP_HOST}..."
-    cat <<EOF > FreeBSD/supfile
+	if [ -z "$FREEBSD_CVSUP_HOST" ]; then
+		echo "No sup host defined, please define FREEBSD_CVSUP_HOST and rerun"
+		exit 1
+	fi
+	echo "Checking out tree from ${FREEBSD_CVSUP_HOST}..."
+	cat <<EOF > FreeBSD/supfile
 *default host=${FREEBSD_CVSUP_HOST}
 *default base=${root}/FreeBSD/sup
 *default prefix=${root}/FreeBSD
@@ -36,41 +40,37 @@ if [ ! -f FreeBSD/supfile -o -n "$force_update" ]; then
 src-all tag=RELENG_8_2
 ports-all date=2011.07.17.00.00.00
 EOF
-    csup -L 1 ${root}/FreeBSD/supfile
-# cvsup fixes any changes we make, it seems.  Repatch
-    rm -f ${root}/FreeBSD/src-patches
-    rm -f ${root}/FreeBSD/ports-patches
+	csup -L 1 ${root}/FreeBSD/supfile
+	# Force a repatch because csup pulls pristine sources.
+	rm -f ${root}/FreeBSD/src-patches
+	rm -f ${root}/FreeBSD/ports-patches
 fi
 
 # Make sure that all the patches are applied
 touch ${root}/FreeBSD/src-patches
 for i in $(cd ${root}/patches && echo freebsd-*.patch); do
-    if ! grep $i ${root}/FreeBSD/src-patches > /dev/null 2>&1; then
-        echo "Applying patch $i..."
-        (cd FreeBSD/src && patch -p0 < ${root}/patches/$i)
-        echo $i >> ${root}/FreeBSD/src-patches
-    fi
+	if ! grep -q $i ${root}/FreeBSD/src-patches; then
+		echo "Applying patch $i..."
+		(cd FreeBSD/src && patch -p0 < ${root}/patches/$i)
+		echo $i >> ${root}/FreeBSD/src-patches
+	fi
 done
 touch ${root}/FreeBSD/ports-patches
 for i in $(cd ${root}/patches && echo ports-*.patch); do
-    if ! grep $i ${root}/FreeBSD/ports-patches > /dev/null 2>&1; then
-        echo "Applying patch $i..."
-        (cd FreeBSD/ports && patch -p0 < ${root}/patches/$i)
-        echo $i >> ${root}/FreeBSD/ports-patches
-    fi
+	if ! grep -q $i ${root}/FreeBSD/ports-patches; then
+		echo "Applying patch $i..."
+		(cd FreeBSD/ports && patch -p0 < ${root}/patches/$i)
+		echo $i >> ${root}/FreeBSD/ports-patches
+	fi
 done
 
 if [ -n "${PREP_SOURCE}" ]; then
-    exit
+	exit
 fi
 
 # OK, now we can build
 cd FreeBSD/src
 args="-c ../../nanobsd/freenas-common"
-if [ `whoami` != "root" ]; then
-    echo "You must be root to run this"
-    exit 1
-fi
 if [ -d ${NANO_OBJ} ]; then
 	extra_args="-b"
 fi
@@ -80,12 +80,11 @@ for i in $*; do
 		extra_args="" ;;
 	*)	args="$args $i" ;;
 	esac
-	
 done
 echo tools/tools/nanobsd/nanobsd.sh $args $extra_args
 sh tools/tools/nanobsd/nanobsd.sh $args $extra_args
 if [ $? -eq 0 ]; then
-	REVISION=`svnversion ${root}`
+	REVISION=$(svnversion ${root})
 	NANO_NAME="FreeNAS-8r${REVISION}-${FREENAS_ARCH}"
 	xz -f ${NANO_OBJ}/_.disk.image
 	mv ${NANO_OBJ}/_.disk.image.xz ${NANO_OBJ}/${NANO_NAME}.xz
