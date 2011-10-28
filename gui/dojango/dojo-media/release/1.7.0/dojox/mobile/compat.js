f08b179c4d8c5c15ca05f7604fbb30ed276a43c5
William Grzybowski <william88@gmail.com>
2011-10-28 01:15:35 +0000

    add the first build of 1.7 and set correct settings to use it

---
+++ gui/dojango/dojo-media/release/1.7.0/dojox/mobile/compat.js
@@ -0,0 +1,12 @@
+//>>built
+define("dojox/mobile/compat", [
+	"dojo/_base/lang",
+	"dojo/_base/sniff"
+], function(lang, has){
+	var dm = lang.getObject("dojox.mobile", true);
+	if(!has("webkit")){
+		var s = "dojox/mobile/_compat"; // assign to a variable so as not to be picked up by the build tool
+		require([s]);
+	}
+	return dm;
+});
