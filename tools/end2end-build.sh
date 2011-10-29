c16936560722d35dfad16c2b7fcce020d60b4896
Garret Cooper <yanegomi@gmail.com>
2011-10-29 01:59:42 +0000

    Optimizations: don't create the disk image or copy out partitions on the first build.

--- tools/end2end-build.sh
+++ tools/end2end-build.sh
@@ -14,7 +14,7 @@ tmpdir=/dev/null
 # Values you can and should change.
 
 branch=trunk
-cvsup_host=cvsup1.freebsd.org
+cvsup_host=cvsup.ixsystems.com
 default_archs="amd64 i386"
 
 setup() {
@@ -89,7 +89,7 @@ for arch in $archs; do
 	# required for producing ports.
 	# XXX: this should really be done in the nanobsd files to only have to
 	# do this once, but it requires installing world twice.
-	if sudo sh -c "$BUILD && $BUILD && sh build/create_iso.sh"; then
+	if sudo sh -c "$BUILD -fi && $BUILD && sh build/create_iso.sh"; then
 		post_images
 	else
 		clean=false
