4b408f9afaad5b9ffd27cee9a0e0167a33c6b9ea
Garret Cooper <yanegomi@gmail.com>
2011-11-20 22:49:19 +0000

    -f was unnecessary before. -i is unnecessary now, pending the previous commit.

--- tools/end2end-build.sh
+++ tools/end2end-build.sh
@@ -117,7 +117,7 @@ for arch in $archs; do
 	# required for producing ports.
 	# XXX: this should really be done in the nanobsd files to only have to
 	# do this once, but it requires installing world twice.
-	if sudo sh -c "export FREENAS_ARCH=$arch; env $BUILD_PASS1_ENV $BUILD -- -fi && env $BUILD_PASS2_ENV $BUILD"; then
+	if sudo sh -c "export FREENAS_ARCH=$arch; env $BUILD_PASS1_ENV $BUILD && env $BUILD_PASS2_ENV $BUILD"; then
 		post_images
 	else
 		clean=false
