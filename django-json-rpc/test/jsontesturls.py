ebda571f3dfd8c513f47b03a5bed318793a61f85
John Hixson <john@ixsystems.com>
2012-02-04 07:17:53 +0000

    Removing this since we've got a port for it now.

--- django-json-rpc/test/jsontesturls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from jsonrpc.site import jsonrpc_site
-
-urlpatterns = patterns('', 
-  url(r'^json/browse/$', 'jsonrpc.views.browse', name='jsonrpc_browser'),
-  url(r'^json/$', jsonrpc_site.dispatch, name='jsonrpc_mountpoint'),
-  (r'^json/(?P<method>[a-zA-Z0-9.-_]+)$', jsonrpc_site.dispatch),
-)
