9ac011541185b6b941315c99b93576b932a17ec5
William Grzybowski <william88@gmail.com>
2012-02-22 12:27:43 +0000

    Delete dojo 1.7.1

--- gui/dojango/dojo-media/release/1.7.1/dojox/charting/widget/Chart2D.js
+++
@@ -1,7 +0,0 @@
-//>>built
-define("dojox/charting/widget/Chart2D", ["dojo/_base/kernel", "./Chart", "../Chart2D",
-	"../action2d/Highlight", "../action2d/Magnify", 
-	"../action2d/MoveSlice", "../action2d/Shake", "../action2d/Tooltip"], function(dojo, Chart) {
-	dojo.deprecated("dojox.charting.widget.Chart2D", "Use dojo.charting.widget.Chart instead and require all other components explicitly", "2.0");
-	return dojox.charting.widget.Chart2D = Chart;
-});
--- gui/dojango/dojo-media/release/1.7.1/dojox/mobile/app.js
+++
@@ -1,6 +0,0 @@
-//>>built
-define("dojox/mobile/app", [
-	"./app/_base"
-], function(appBase){
-	return appBase;
-});
