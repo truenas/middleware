2679bc9514e35b9646441c674c6a521061e7d404
Garret Cooper <yanegomi@gmail.com>
2012-03-25 20:57:40 +0000

    Note the return code

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -152,7 +152,7 @@ class notifier:
             retval = self.___system("(" + command + ") >/dev/null 2>&1")
         finally:
             libc.sigprocmask(signal.SIGQUIT, pomask, None)
-        log.debug("Executed: %s", command)
+        log.debug("Executed: %s; returned %d", command, retval)
         return retval
 
     def __pipeopen(self, command):
