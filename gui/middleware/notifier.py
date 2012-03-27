1a7813e90345280bc045d7ab2e5d41ac4ec07a08
Garret Cooper <yanegomi@gmail.com>
2012-03-27 02:07:11 +0000

    Shift the return value over by 8 so the semantics match system(3)'s semantics and are more intuitive, as opposed to the perl/python wait(2) semantics.

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -152,7 +152,8 @@ class notifier:
             retval = self.___system("(" + command + ") >/dev/null 2>&1")
         finally:
             libc.sigprocmask(signal.SIGQUIT, pomask, None)
-        log.debug("Executed: %s; returned %d", command, retval >> 8)
+        retval =>> 8
+        log.debug("Executed: %s; returned %d", command, retval)
         return retval
 
     def __pipeopen(self, command):
