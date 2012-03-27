1fd18c98712faa035eff893af701ebfa88908f2d
Garret Cooper <yanegomi@gmail.com>
2012-03-27 03:32:40 +0000

    Revert r10851. This doesn't fix the problem... Bug incoming soon

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -1798,7 +1798,6 @@ class notifier:
 
         self.__system("/bin/rm -rf %s" % vardir)
         self.__system("/bin/mkdir -p %s/.freenas" % path)
-        self.__system("/bin/chmod 0770 %s/.freenas" % path)
         self.__system("/usr/sbin/chown www:www %s/.freenas" % path)
         self.__system("/bin/ln -s %s/.freenas %s" % (path, vardir))
 
