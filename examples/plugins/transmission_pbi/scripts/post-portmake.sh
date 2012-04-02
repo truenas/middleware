4bd30400e6080a0312cf781a1879c2a6ff16a66c
William Grzybowski <william88@gmail.com>
2012-04-02 16:03:34 +0000

    Removing .py[oc] and share/doc should really be done in post-portmake.sh (I think)

--- examples/plugins/transmission_pbi/scripts/post-portmake.sh
+++ examples/plugins/transmission_pbi/scripts/post-portmake.sh
@@ -2,3 +2,8 @@
 # PBI building script
 # This will run after your port build is complete
 ##############################################################################
+
+transmission_pbi_path=/usr/pbi/transmission-$(uname -m)/
+
+find ${transmission_pbi_path}/lib -iname "*.py[co]" -delete
+rm -rf ${transmission_pbi_path}/share/doc
