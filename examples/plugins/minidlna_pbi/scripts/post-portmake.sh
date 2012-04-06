8271950c3e5c404edade4f4082ed7efc773c7db7
William Grzybowski <william88@gmail.com>
2012-04-06 22:36:26 +0000

    Some effort to try to reduce minidla plugin footprint

--- examples/plugins/minidlna_pbi/scripts/post-portmake.sh
+++ examples/plugins/minidlna_pbi/scripts/post-portmake.sh
@@ -2,3 +2,10 @@
 # PBI building script
 # This will run after your port build is complete
 ##############################################################################
+
+minidlna_pbi_path=/usr/pbi/minidlna-$(uname -m)/
+
+rm -rf ${minidlna_pbi_path}/share/doc
+rm -rf ${minidlna_pbi_path}/share/emacs
+rm -rf ${minidlna_pbi_path}/share/examples
+rm -rf ${minidlna_pbi_path}/share/gettext
