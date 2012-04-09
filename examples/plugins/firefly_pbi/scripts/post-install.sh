035d835390a95e171504167509304f482cc4fbab
William Grzybowski <william88@gmail.com>
2012-04-09 18:55:25 +0000

    pbi.conf for firefly to build django packages as well

--- examples/plugins/firefly_pbi/scripts/post-install.sh
+++ examples/plugins/firefly_pbi/scripts/post-install.sh
@@ -6,7 +6,4 @@ firefly_pbi_path=/usr/pbi/firefly-$(uname -m)/
 mkdir -p ${firefly_pbi_path}/mnt
 mkdir -p ${firefly_pbi_path}/www
 
-mv ${firefly_pbi_path}/edit.html ${firefly_pbi_path}/www/
-mv ${firefly_pbi_path}/mp_edit.html ${firefly_pbi_path}/www/
-
 pw user add daapd -d ${firefly_pbi_path}
