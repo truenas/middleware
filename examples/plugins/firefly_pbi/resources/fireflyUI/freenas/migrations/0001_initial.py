9fce0c1a3c164bc4046bbec9a7aadc84fc478fd2
William Grzybowski <william88@gmail.com>
2012-04-10 14:38:31 +0000

    Add scan/rescan options to firefly form

--- examples/plugins/firefly_pbi/resources/fireflyUI/freenas/migrations/0001_initial.py
+++ examples/plugins/firefly_pbi/resources/fireflyUI/freenas/migrations/0001_initial.py
@@ -18,6 +18,9 @@ class Migration(SchemaMigration):
             ('extensions', self.gf('django.db.models.fields.CharField')(default='.mp3,.m4a,.m4p,.ogg,.flac', max_length=500, blank=True)),
             ('mp3_dir', self.gf('django.db.models.fields.CharField')(max_length=500)),
             ('logfile', self.gf('django.db.models.fields.CharField')(default='/var/log/mt-daapd.log', max_length=500)),
+            ('rescan_interval', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('always_scan', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('scan_type', self.gf('django.db.models.fields.IntegerField')(default=2)),
             ('process_playlists', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('process_itunes', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('process_m3u', self.gf('django.db.models.fields.BooleanField')(default=True)),
@@ -32,6 +35,7 @@ class Migration(SchemaMigration):
         'freenas.firefly': {
             'Meta': {'object_name': 'Firefly'},
             'admin_pw': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'always_scan': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'enable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'extensions': ('django.db.models.fields.CharField', [], {'default': "'.mp3,.m4a,.m4p,.ogg,.flac'", 'max_length': '500', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -41,6 +45,8 @@ class Migration(SchemaMigration):
             'process_itunes': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'process_m3u': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'process_playlists': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'rescan_interval': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'scan_type': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
             'servername': ('django.db.models.fields.CharField', [], {'default': "'Firefly %v on %h'", 'max_length': '500', 'blank': 'True'})
         }
     }
--- examples/plugins/firefly_pbi/resources/fireflyUI/freenas/models.py
+++ examples/plugins/firefly_pbi/resources/fireflyUI/freenas/models.py
@@ -13,6 +13,15 @@ class Firefly(models.Model):
     extensions = models.CharField(max_length=500, default='.mp3,.m4a,.m4p,.ogg,.flac', blank=True)
     mp3_dir = models.CharField(max_length=500)
     logfile = models.CharField(max_length=500, default='/var/log/mt-daapd.log')
+    rescan_interval = models.IntegerField(default=0)
+    always_scan = models.BooleanField(default=False)
+    scan_type = models.IntegerField(default=2,
+        choices=(
+            (0, 'Normal'),
+            (1, 'Aggressive'),
+            (2, 'Painfully aggressive'),
+        ),
+        )
     process_playlists = models.BooleanField(default=True)
     process_itunes = models.BooleanField(default=True)
     process_m3u = models.BooleanField(default=True)
