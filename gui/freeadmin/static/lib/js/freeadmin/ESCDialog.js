b79c10901803123e98496d087b983eecc725413c
William Grzybowski <william88@gmail.com>
2012-04-16 11:50:53 +0000

    Pass ESC keypress to the webshell

--- gui/freeadmin/static/lib/js/freeadmin/ESCDialog.js
+++ gui/freeadmin/static/lib/js/freeadmin/ESCDialog.js
@@ -2,7 +2,12 @@ define(["dijit/Dialog","dojo/_base/declare"], function(Dialog, declare) {
 
     var MyDialog = declare("freeadmin.ESCDialog", [Dialog], {
         _onKey: function(evt) {
-            if(evt.charOrCode == dojo.keys.ESCAPE) return;
+            if(evt.charOrCode == dojo.keys.ESCAPE) {
+                if(_webshell) {
+                    _webshell.keypress(evt);
+                }
+                return;
+            }
             this.inherited(arguments);
         },
     });
