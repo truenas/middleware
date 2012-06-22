5e37279f706806031b282e1b98af73a65da06186
Xin Li <d@delphij.net>
2012-06-22 18:48:11 +0000

    Special handling for end_time = 00:00 case.  If the user specified the time that way, consider the all time beginning from begin_time, to next day's 00:00 as valid range.

--- gui/tools/autosnap.py
+++ gui/tools/autosnap.py
@@ -85,7 +85,9 @@ def snap_expired(snapinfo, snaptime):
     return snapinfo_expirationtime <= snaptime
 
 def isTimeBetween(time_to_test, begin_time, end_time):
-    if begin_time < end_time:
+    if end_time == time(0, 0):
+        return ((begin_time <= time_to_test) or (time_to_test == end_time))
+    elif begin_time < end_time:
         return ((begin_time <= time_to_test) and (time_to_test <= end_time))
     else:
         return ((begin_time <= time_to_test) or (time_to_test <= end_time))
