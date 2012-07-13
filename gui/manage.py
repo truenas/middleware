887fa7af496451d3366934b78841fe21fd4965e5
William Grzybowski <william88@gmail.com>
2012-07-13 19:44:52 +0000

    Use manage.py from 1.4 and remove hack for multi-threaded dev server

--- gui/manage.py
+++ gui/manage.py
@@ -28,32 +28,15 @@
 
 # this is a hack
 import sys
-sys.path.append('/usr/local/www')
+import os
 
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+HERE = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(HERE, ".."))
 
-"""
-TEMPORARY WORKAROUND FOR THREADED DEV SERVER
-REMOVE ME IN DJANGO 1.4
-"""
-import SocketServer
-import django.core.servers.basehttp
-django.core.servers.basehttp.WSGIServer = \
-    type('WSGIServer',
-         (SocketServer.ThreadingMixIn,
-          django.core.servers.basehttp.WSGIServer,
-          object),
-         {})
-"""
-TEMPORARY WORKAROUND FOR THREADED DEV SERVER
-REMOVE ME IN DJANGO 1.4
-"""
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freenasUI.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
