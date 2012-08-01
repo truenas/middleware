54c338b7a8ea2fc5ed1040b369e98f0789dc3583
William Grzybowski <william88@gmail.com>
2012-08-01 14:30:15 +0000

    Convert common.jail to use logging

--- gui/common/jail.py
+++ gui/common/jail.py
@@ -25,18 +25,18 @@
 #
 # $FreeBSD$
 #####################################################################
-import os
-import sys
-import syslog
+import logging
 
 from subprocess import Popen, PIPE
-from syslog import syslog, LOG_DEBUG
+
+log = logging.getLogger('common.jail')
+
 
 class jail_arg(object):
     def __init__(self, int, string, arg=False, argname=None):
         self.int = int
         self.string = string
-        self.arg = arg 
+        self.arg = arg
         self.argname = argname
 
     def __str__(self):
@@ -44,98 +44,142 @@ class jail_arg(object):
 
     def __lt__(self, other):
         return self.int < other
+
     def __le__(self, other):
         return self.int <= other
+
     def __eq__(self, other):
         return self.int == other
+
     def __ne__(self, other):
         return self.int != other
+
     def __gt__(self, other):
         return self.int > other
+
     def __ge__(self, other):
         return self.int >= other
+
     def __add__(self, other):
         return self.int + other
+
     def __sub__(self, other):
         return self.int - other
+
     def __mul__(self, other):
         return self.int * other
+
     def __floordiv__(self, other):
         return self.int // other
+
     def __mod__(self, other):
         return self.int % other
+
     def __divmod__(self, other):
         return (self.int // other, self.int % other)
+
     def __pow__(self, other):
         return self.int ** other
+
     def __lshift__(self, other):
         return self.int << other
+
     def __rshift__(self, other):
         return self.int >> other
+
     def __and__(self, other):
         return self.int & other
+
     def __xor__(self, other):
         return self.int ^ other
+
     def __or__(self, other):
         return self.int | other
+
     def __div__(self, other):
         return self.int / other
+
     def __truediv__(self, other):
         return self.int / other
 
     def __radd__(self, other):
         return self.int + other
+
     def __rsub__(self, other):
         return self.int - other
+
     def __rmul__(self, other):
         return self.int * other
+
     def __rdiv__(self, other):
         return self.int / other
+
     def __rtruediv__(self, other):
         return self.int // other
+
     def __rfloordiv__(self, other):
         return self.int // other
+
     def __rmod__(self, other):
         return self.int % other
+
     def __rdivmod__(self, other):
         return (self.int // other, self.int % other)
+
     def __rpow__(self, other):
         return self.int ** other
+
     def __rlshift__(self, other):
         return self.int << other
+
     def __rrshift__(self, other):
         return self.int << other
+
     def __rand__(self, other):
         return self.int & other
+
     def __rxor__(self, other):
         return self.int ^ other
+
     def __ror__(self, other):
         return self.int | other
 
     def __iadd__(self, other):
         return self.int + other
+
     def __isub__(self, other):
         return self.int - other
+
     def __imul__(self, other):
         return self.int * other
+
     def __idiv__(self, other):
         return self.int / other
+
     def __itruediv__(self, other):
         return self.int // other
+
     def __ifloordiv__(self, other):
         return self.int // other
+
     def __imod__(self, other):
         return self.int % other
+
     def __ipow__(self, other):
         return self.int ** other
+
     def __ilshift__(self, other):
         return self.int << other
+
     def __irshift__(self, other):
         return self.int >> other
+
     def __iand__(self, other):
         return self.int & other
+
     def __ixor__(self, other):
         return self.int ^ other
+
     def __ior__(self, other):
         return self.int | other
 
@@ -169,22 +213,23 @@ JLS_FLAGS = [
     JLS_FLAGS_JID
 ]
 
+
 class Jail_exception(Exception):
-    def __init__(self, msg = None):
-        syslog(LOG_DEBUG, "Jail_exception.__init__: enter")
+    def __init__(self, msg=None):
+        log.debug("Jail_exception.__init__: enter")
         if msg:
-            syslog(LOG_DEBUG, "Jail_exception.__init__: error = %s" % msg)
-        syslog(LOG_DEBUG, "Jail_exception.__init__: leave")
+            log.debug("Jail_exception.__init__: error = %s", msg)
+        log.debug("Jail_exception.__init__: leave")
 
 
 class Jail_pipe(object):
     def __init__(self, cmd, func=None, **kwargs):
-        syslog(LOG_DEBUG, "Jail_pipe.__init__: enter") 
-        syslog(LOG_DEBUG, "Jail_pipe.__init__: cmd = %s" % cmd) 
+        log.debug("Jail_pipe.__init__: enter")
+        log.debug("Jail_pipe.__init__: cmd = %s", cmd)
 
         self.error = None
-        self.__pipe = Popen(cmd, stdin = PIPE, stdout = PIPE,
-            stderr = PIPE, shell = True, close_fds = True)
+        self.__pipe = Popen(cmd, stdin=PIPE, stdout=PIPE,
+            stderr=PIPE, shell=True, close_fds=True)
 
         self.__stdin = self.__pipe.stdin
         self.__stdout = self.__pipe.stdout
@@ -192,7 +237,7 @@ class Jail_pipe(object):
 
         self.__out = ""
         if func is not None:
-            for line in self.__stdout: 
+            for line in self.__stdout:
                 line = line.strip()
                 self.__out += line
                 func(line, **kwargs)
@@ -201,13 +246,13 @@ class Jail_pipe(object):
             self.__out = self.__stdout.read().strip()
 
         self.__pipe.wait()
-        syslog(LOG_DEBUG, "Jail_pipe.__init__: out = %s" % self.__out)
+        log.debug("Jail_pipe.__init__: out = %s", self.__out)
 
         if self.__pipe.returncode != 0:
-            self.error = self.__out 
+            self.error = self.__out
 
         self.returncode = self.__pipe.returncode
-        syslog(LOG_DEBUG, "Jail_pipe.__init__: leave")
+        log.debug("Jail_pipe.__init__: leave")
 
     def __str__(self):
         return self.__out
@@ -217,15 +262,16 @@ class Jail_pipe(object):
         for line in lines:
             yield line
 
+
 class Jail_bait(object):
     def __init__(self, path, objflags, flags=JEXEC_FLAGS_NONE, **kwargs):
-        syslog(LOG_DEBUG, "Jail_bait.__init__: enter")
-        syslog(LOG_DEBUG, "Jail_bait.__init__: path = %s" % path)
-        syslog(LOG_DEBUG, "Jail_bait.__init__: flags = 0x%08x" % (flags + 0))
+        log.debug("Jail_bait.__init__: enter")
+        log.debug("Jail_bait.__init__: path = %s", path)
+        log.debug("Jail_bait.__init__: flags = 0x%08x", flags + 0)
 
         self.path = path
-        self.flags = flags 
-        self.args = "" 
+        self.flags = flags
+        self.args = ""
 
         if objflags is None:
             objflags = []
@@ -236,35 +282,35 @@ class Jail_bait(object):
                     kwargs.has_key(obj.argname) and kwargs[obj.argname] is not None:
                     self.args += " %s %s" % (obj, kwargs[obj.argname])
 
-                elif obj.arg == False: 
+                elif obj.arg == False:
                     self.args += " %s" % obj
 
-        syslog(LOG_DEBUG, "Jail_bait.__init__: args = %s" % self.args)
+        log.debug("Jail_bait.__init__: args = %s", self.args)
 
         self.pipe_func = None
         if kwargs.has_key("pipe_func") and kwargs["pipe_func"] is not None:
             self.pipe_func = kwargs["pipe_func"]
 
-        syslog(LOG_DEBUG, "Jail_bait.__init__: leave")
+        log.debug("Jail_bait.__init__: leave")
 
     def run(self):
-        syslog(LOG_DEBUG, "Jail_bait.run: enter")
+        log.debug("Jail_bait.run: enter")
 
         cmd = self.path
         if self.args is not None:
             cmd += " %s" % self.args
 
-        syslog(LOG_DEBUG, "Jail_bait.cmd = %s" % cmd)
+        log.debug("Jail_bait.cmd = %s", cmd)
         pobj = Jail_pipe(cmd, self.pipe_func)
         self.error = pobj.error
 
-        syslog(LOG_DEBUG, "Jail_bait.run: leave")
+        log.debug("Jail_bait.run: leave")
         return (pobj.returncode, str(pobj))
 
 
 class Jexec(Jail_bait):
     def __init__(self, flags=JEXEC_FLAGS_NONE, **kwargs):
-        syslog(LOG_DEBUG, "Jexec.__init__: enter")
+        log.debug("Jexec.__init__: enter")
 
         super(Jexec, self).__init__(JEXEC_PATH, JEXEC_FLAGS, flags, **kwargs)
 
@@ -276,7 +322,7 @@ class Jexec(Jail_bait):
         if kwargs.has_key("command") and kwargs["command"] is not None:
             self.args += " %s" % kwargs["command"]
 
-        syslog(LOG_DEBUG, "Jexec.__init__: leave")
+        log.debug("Jexec.__init__: leave")
 
 
 class JailObject(object):
@@ -284,7 +330,7 @@ class JailObject(object):
         self.jid = -1
         self.ip = None
         self.hostname = None
-        self.path = None 
+        self.path = None
 
         for key in kwargs:
             if key in ('jid', 'ip', 'hostname', 'path'):
@@ -296,7 +342,7 @@ class JailObject(object):
 
 class Jls(Jail_bait):
     def __init__(self, flags=JLS_FLAGS_NONE, **kwargs):
-        syslog(LOG_DEBUG, "Jls.__init__: enter")
+        log.debug("Jls.__init__: enter")
 
         super(Jls, self).__init__(JLS_PATH, JLS_FLAGS, flags, **kwargs)
         self.__jails = []
@@ -307,9 +353,9 @@ class Jls(Jail_bait):
             for i in range(0, 1024):
                 if kwargs.has_key("parameter%d" % i) and kwargs["parameter%d" % i] is not None:
                     self.args += " %s" % kwargs["parameter%d" % i]
-         
+
         self._load()
-        syslog(LOG_DEBUG, "Jls.__init__: leave")
+        log.debug("Jls.__init__: leave")
 
     def _load(self):
         out = super(Jls, self).run()
