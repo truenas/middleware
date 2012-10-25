ff6a4502775fbf93a4e61bec2d4a2e0b1562452c
John Hixson <john@ixsystems.com>
2012-10-25 15:46:01 +0000

    Revert prior revert with slipped in revert by un-reverting.

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -3458,7 +3458,7 @@ class notifier:
 
         p1 = Popen(["/usr/local/sbin/smartctl", "-i"] + args, stdout=PIPE)
         output = p1.communicate()[0]
-        search = re.search(r'^Serial Number:[ \t\s]+(?P<serial>.+)', output, re.I)
+        search = re.search(r'Serial Number:\s+(?P<serial>.+)', output, re.I)
         if search:
             serial = search.group("serial")
             self.__diskserial[devname] = serial
