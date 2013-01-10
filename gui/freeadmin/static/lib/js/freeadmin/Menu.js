2f82dd6d6c3377e21f135d5b6269da485fb88738
John Hixson <john@ixsystems.com>
2013-01-10 08:34:57 +0000

    initial nav, view and urls. This doesn't do anything.

--- gui/freeadmin/static/lib/js/freeadmin/Menu.js
+++ gui/freeadmin/static/lib/js/freeadmin/Menu.js
@@ -184,6 +184,41 @@ define([
         },
 
         openJails: function(onload, jail) {
+            var opened = false;
+            var p = registry.byId("content");
+
+            var c = p.getChildren();
+            for(var i=0; i<c.length; i++){
+                if(c[i].tab == 'jails'){
+                    p.selectChild(c[i]);
+                    opened = true;
+                    if(tab) {
+                        var tabnet = registry.byId("tab_jails");
+                        if(tabnet) {
+                            var c2 = tabnet.getChildren();
+                            for(var j=0; j<c2.length; j++){
+                                if(c2[j].domNode.getAttribute("tab") == tab)
+                                    tabnet.selectChild(c2[j]);
+                            }
+                        }
+                    }
+                }
+            }
+            if(opened != true) {
+                openurl = this.urlStorage;
+                if(tab) {
+                    openurl += '?tab='+tab;
+                }
+                var pane = new ContentPane({
+                    title: gettext('Jails'),
+                    closable: true,
+                    href:openurl,
+                });
+                pane.tab = 'jails';
+                p.addChild(pane);
+                p.selectChild(pane);
+                domClass.add(pane.domNode,["objrefresh", "data_jails"]);
+            }
         },
 
         openAccount: function(tab) {
--- gui/jails/urls.py
+++ gui/jails/urls.py
@@ -24,45 +24,11 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 #####################################################################
-from django.utils.translation import ugettext_lazy as _
 
-from . import models
-from freenasUI.middleware.notifier import notifier
-from freenasUI.freeadmin.tree import TreeNode
-from freenasUI.common.warden import Warden
+from django.conf.urls.defaults import patterns, url
 
-NAME = _('Jails')
-BLACKLIST = []
-ICON = u'ServicesIcon'
+urlpatterns = patterns('freenasUI.services.views',
+    url(r'^index/$', 'index', name="jails_home"),
+    url(r'enable/(?P<jail>\w+)/.*$', 'enable', name="jail_enable"),
+    )
 
-class JailsSettings(TreeNode):
-
-    gname = 'Settings'
-    name = _(u'Settings')
-    type = 'object'
-    icon = u"SettingsIcon"
-    skip = True
-
-    def __init__(self, *args, **kwargs):
-        super(JailsSettings, self).__init__(*args, **kwargs)
-
-
-class JailsManagement(TreeNode):
-
-    gname = 'management'
-    name = _(u'Management')
-    icon = u"SettingsIcon"
-    #skip = True
-    #order = -1
-
-    def __init__(self, *args, **kwargs):
-        super(JailsManagement, self).__init__(*args, **kwargs)
-        self.append_children([JailsSettings()])
-
-#        jails = Warden().list()
-#        for jail in jails:
-#            nav = TreeNode()
-#            nav.name = jail['host']
-#            nav.icon = u'SettingsIcon'
-#            nav._children = []
-#            self.append_child(nav)
