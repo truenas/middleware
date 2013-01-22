d7d4f0aad0e6170f8d3ebcb415e9553926758e65
William Grzybowski <william88@gmail.com>
2013-01-22 12:06:57 +0000

    Sort snapshots using bytes for used/refer instead of strng comparision
    ticket: #2013

--- gui/storage/views.py
+++ gui/storage/views.py
@@ -132,6 +132,11 @@ def snapshots_data(request):
         else:
             r = None
 
+    fieldmap = {
+        'used': 'usedb',
+        'refer': 'referb',
+    }
+
     for key in request.GET.keys():
         reg = re.search(r'sort\((?P<sign>.)(?P<field>.+?)\)', key)
         if reg:
@@ -140,6 +145,8 @@ def snapshots_data(request):
                 rev = True
             else:
                 rev = False
+            field = fieldmap.get(field, field)
+            log.error("filter really by: %s", field)
             if field in zfsnap_list[0]:
                 zfsnap_list.sort(key=lambda item: item[field], reverse=rev)
 
