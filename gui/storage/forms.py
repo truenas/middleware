99a1ef15c8d14fd37d3731b9cab74a6aa8128e3a
William Grzybowski <william88@gmail.com>
2013-01-24 16:09:16 +0000

    Fix issue of resetting recovery key while rekeying and recovery key on change passphrase
    Also warning about remove of recovery key on create passphrase

--- gui/storage/forms.py
+++ gui/storage/forms.py
@@ -1726,7 +1726,7 @@ class CreatePassphraseForm(forms.Form):
                 f.write(passphrase)
         else:
             passfile = None
-        notifier().geli_passphrase(volume, passfile)
+        notifier().geli_passphrase(volume, passfile, rmrecovery=True)
         if passfile is not None:
             os.unlink(passfile)
         volume.vol_encrypt = 2
