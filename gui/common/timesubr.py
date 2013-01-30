10258a3b1f63c243ff4aec80d3e248a4cf2deaa3
William Grzybowski <william88@gmail.com>
2013-01-30 13:41:09 +0000

    Fix inverted logic.

--- gui/common/timesubr.py
+++ gui/common/timesubr.py
@@ -6,4 +6,4 @@ def isTimeBetween(time_to_test, begin_time, end_time):
     elif begin_time < end_time:
         return ((begin_time <= time_to_test) and (time_to_test <= end_time))
     else:
-        return ((begin_time <= time_to_test) or (time_to_test <= end_time))
+        return ((begin_time >= time_to_test) or (time_to_test >= end_time))
