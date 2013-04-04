b181501e2da242ce1e3ad20276580efbfd0cfa7a
John Hixson <john@ixsystems.com>
2013-04-03 22:40:36 -0700

    Fix pbi-wrapper bug and add sipcalc network functions. pbi-wrapper had BINDIR defined incorrectly. Specifically add sources so that binary can be .pbiwrapper and can be installed into /usr/pbi. sipcalc network functions added so that ipv4 and ipv6 network functions can be utilized.

---
+++ gui/common/sipcalc.py
@@ -0,0 +1,304 @@
+#+
+# Copyright 2013 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# $FreeBSD$
+#####################################################################
+import string
+
+from freenasUI.common.pipesubr import pipeopen
+
+SIPCALC_PATH = "/usr/local/bin/sipcalc"
+
+class sipcalc_base_type(object):
+    def __init__(self, *args):    
+        self.sipcalc = SIPCALC_PATH
+
+        self.sipcalc_args = [self.sipcalc]
+        self.sipcalc_args += args
+
+        obj = None
+        p1 = pipeopen(string.join(self.sipcalc_args, ' '), allowfork=True)
+        self.sipcalc_out = p1.communicate()
+        if self.sipcalc_out:
+            self.sipcalc_out = self.sipcalc_out[0]
+            if self.sipcalc_out:
+                self.sipcalc_out = self.sipcalc_out.split('\n')
+
+    def is_ipv4(self):
+        res = False
+        if self.sipcalc_out[0].startswith("-[ipv4"): 
+            res = True
+        return res
+
+    def is_ipv6(self):
+        res = False
+        if self.sipcalc_out[0].startswith("-[ipv6"): 
+            res = True
+        return res
+
+class sipcalc_ipv4_type(sipcalc_base_type):
+    def __init__(self, *args):
+        super(sipcalc_ipv4_type, self).__init__(*args)
+
+        for line in self.sipcalc_out:
+            parts = line.split("-") 
+
+            i = 0
+            plen = len(parts)
+            while i < plen:
+                parts[i] = parts[i].strip()
+                i += 1
+
+            if parts[0] == "Host address":
+                self.host_address = parts[1]
+
+            elif parts[0] == "Host address (decimal)":
+                self.host_address_dec = int(parts[1])
+
+            elif parts[0] == "Host address (hex)":
+                self.host_address_hex = parts[1]
+
+            elif parts[0] == "Network address":
+                self.network_address  = parts[1]
+
+            elif parts[0] == "Network mask":
+                self.network_mask = parts[1]
+
+            elif parts[0] == "Network mask (bits)":
+                self.network_mask_bits = int(parts[1])
+
+            elif parts[0] == "Network mask (hex)":
+                self.network_mask_hex = parts[1]
+
+            elif parts[0] == "Broadcast address":
+                self.broadcast_address = parts[1]
+
+            elif parts[0] == "Cisco wildcard":
+                self.cisco_wildcard = parts[1]
+
+            elif parts[0] == "Addresses in network":
+                self.network_addresses = int(parts[1])
+
+            elif parts[0] == "Network range":
+                self.network_range = parts[1:]
+
+            elif parts[0] == "Usable range":
+                self.usable_range = parts[1:]
+
+    def in_network(self, addr):
+        res = False
+
+        st_addr = sipcalc_type(addr).host_address_dec
+        st_start = sipcalc_type(self.network_range[0]).host_address_dec
+        st_end = sipcalc_type(self.network_range[1]).host_address_dec
+
+        if st_addr >= st_start and st_addr <= st_end:
+            res = True 
+
+        return res
+
+    def get_next_addr(self, addr=None):
+        naddr = ""
+
+        if addr is not None:
+            addr = sipcalc_type(addr).host_address_dec
+        else:
+            addr = self.host_address_dec
+
+        addr += 1
+
+        oct1 = (addr >> 24) & 0xff
+        oct2 = (addr >> 16) & 0xff
+        oct3 = (addr >> 8) & 0xff
+        oct4 = (addr >> 0) & 0xff
+
+        naddr = "%d.%d.%d.%d" % (oct1, oct2, oct3, oct4)
+        return naddr
+
+class sipcalc_ipv6_type(sipcalc_base_type):
+    def __init__(self, *args):
+        super(sipcalc_ipv6_type, self).__init__(*args)
+
+        network_range = 0
+        for line in self.sipcalc_out:
+            parts = line.split("-") 
+
+            i = 0
+            plen = len(parts)
+            while i < plen:
+                parts[i] = parts[i].strip()
+                i += 1
+
+            if parts[0] == "Expanded Address":
+                self.expanded_address = parts[1]
+
+            elif parts[0] == "Compressed address":
+                self.compressed_address = parts[1]
+
+            elif parts[0] == "Subnet prefix (masked)":
+                self.subnet_prefix_masked = parts[1]
+
+            elif parts[0] == "Address ID (masked)":
+                self.address_id_masked = parts[1]
+
+            elif parts[0] == "Prefix address":
+                self.prefix_address = parts[1]
+
+            elif parts[0] == "Prefix length":
+                self.prefix_length = int(parts[1])
+
+            elif parts[0] == "Address type":
+                self.address_type = parts[1]
+
+            elif parts[0] == "Network range":
+                self.network_range = [None, None]
+                self.network_range[0] = parts[1]
+                network_range = 1
+
+            elif network_range == 1:
+                network_range = 0
+                self.network_range[1] = parts[0]
+
+    def to_binary(self, addr=None):
+        numbers = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+            '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'a': 10,
+            'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15 }
+   
+        if addr is not None:
+            addr = sipcalc_type(addr).expanded_address
+        else:
+            addr = self.expanded_address
+
+        bnum = ""
+        octets = addr.split(':')
+        for oct in octets:
+
+            # hex to decimal
+            dnum = 0
+            for b in oct:
+                b = b.lower()
+                dnum *= 16
+                dnum += numbers[b]
+
+            # decimal to binary
+            tbnum = ""
+            while True:
+                if dnum & 1:
+                    tbnum = "1%s" % tbnum
+                else:
+                    tbnum = "0%s" % tbnum
+
+                dnum /= 2
+                if dnum < 1:
+                    break
+
+            tbnum = tbnum.zfill(16)
+            bnum = "%s%s" % (bnum, tbnum)
+
+        return bnum
+
+    def to_decimal(self, addr=None):
+        numbers = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+            '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'a': 10,
+            'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15 }
+
+        if addr is not None:
+            addr = sipcalc_type(addr).expanded_address
+        else:
+            addr = self.expanded_address
+
+        si = 128
+        num = 0
+        octets = addr.split(':')
+        for oct in octets:
+            si -= 16
+
+            # hex to decimal
+            dnum = 0
+            for b in oct:
+                b = b.lower()
+                dnum *= 16
+                dnum += numbers[b]
+
+            num += dnum << si
+
+        return num
+   
+    def in_network(self, addr):
+        res = False
+
+        st_addr = sipcalc_type(addr)
+        st_start = sipcalc_type(self.network_range[0])
+
+        st_baddr = self.to_binary(st_addr.expanded_address)
+        st_bstart = self.to_binary(st_start.expanded_address)
+
+        if st_baddr[0:self.prefix_length] == st_bstart[0:self.prefix_length]:
+            res = True
+
+        return res
+
+    def get_next_addr(self, addr=None):
+        naddr = ""
+
+        if addr is not None:
+            addr = sipcalc_type(addr).expanded_address
+        else:
+            addr = self.expanded_address
+
+        daddr = self.to_decimal(addr)
+        daddr += 1
+
+        haddr = hex(daddr)
+        parts1 = haddr.split('x')
+        if len(parts1) > 1:
+            parts2 = parts1[1].split('L') 
+            hstr = parts2[0]
+
+            i = 0
+            hlen = len(hstr) 
+            while i < hlen:
+                naddr += hstr[i]
+                if (i + 1) % 4 == 0 and (i + 1) < hlen:
+                    naddr += ':'
+                i += 1 
+
+        if not naddr:
+            naddr = None
+
+        return naddr
+
+class sipcalc_type(sipcalc_base_type):
+    def __new__(cls, *args):    
+        obj = None
+        sbt = sipcalc_base_type(*args) 
+
+        if sbt.is_ipv4():
+            obj = sipcalc_ipv4_type(*args)
+
+        elif sbt.is_ipv6():
+            obj = sipcalc_ipv6_type(*args)
+
+        return obj
