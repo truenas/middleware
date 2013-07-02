e944d25f1211231aca800154cb964b1660b960d0
William Grzybowski <wg@FreeBSD.org>
2013-07-02 09:44:57 -0300

    Fix model object nodes to have its own gname

--- gui/freeadmin/navtree.py
+++ gui/freeadmin/navtree.py
@@ -391,6 +391,7 @@ class NavTree(object):
                             subopt.kwargs = {
                                 'oid': e.id,
                             }
+                            subopt.gname = e.id
                             try:
                                 subopt.name = unicode(e)
                             except:
