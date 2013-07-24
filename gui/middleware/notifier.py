39cfaa28215c54fbdbd134f1d2f8ad8ea2badf8f
William Grzybowski <wg@FreeBSD.org>
2013-07-24 08:52:48 -0300

    Create a sentinel file to indicate start of plugin install
    Merge:	releng/9.1.0
    (cherry picked from commit 39b3050f7d9cf323d21eaca101eb036c2a3577d9)

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -2399,6 +2399,8 @@ class notifier:
         from freenasUI.jails.models import JailsConfiguration
         ret = False
 
+        open('/tmp/.plugin_upload_install', 'w+').close()
+
         if not pjail:
             log.debug("install_pbi: pjail is NULL")
             return False
