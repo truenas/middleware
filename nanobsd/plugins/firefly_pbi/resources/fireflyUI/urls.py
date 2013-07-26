d4daf22a18ad9c764439620d1a5708691a0623ac
William Grzybowski <wg@FreeBSD.org>
2013-07-26 08:31:26 -0300

    Rename deprecated import for firefly

--- nanobsd/plugins/firefly_pbi/resources/fireflyUI/urls.py
+++ nanobsd/plugins/firefly_pbi/resources/fireflyUI/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
      url(r'^plugins/firefly/(?P<plugin_id>\d+)/', include('fireflyUI.freenas.urls')),
