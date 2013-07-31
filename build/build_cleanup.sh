50758707389b9f196656fde5bdc928259db66eeb
Alfred Perlstein <alfred@ixsystems.com>
2013-07-31 12:56:52 -0700

    Don't fail if nullfs unmount fails

--- build/build_cleanup.sh
+++ build/build_cleanup.sh
@@ -11,11 +11,13 @@ if [ -e os-base ] ; then
 fi
 
 # clean up devfs mounts
+set +e
 for mount_dir in os-base/amd64/_.w/dev os-base/amd64/jails/jail-i386/dev ; do
     if [ -e "$mount_dir" ] ; then
     	umount $mount_dir
     fi
 done
+set -e
 
 if [ -d pbi ] ; then
 	zfs destroy -r `realpath pbi | sed 's@^/@@'`
