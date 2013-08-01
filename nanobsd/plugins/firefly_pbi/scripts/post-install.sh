5f2de17083d0309f91d54b520e159d0745187d45
William Grzybowski <wg@FreeBSD.org>
2013-08-01 19:46:09 -0300

    Workaround to create /var/db/firefly and fix start
    Ticket:	#2472
    Merge:	releng/9.1.0
    (cherry picked from commit 55adead617bc05b9bd36c0cfb3dd803f34d4194e)

--- nanobsd/plugins/firefly_pbi/scripts/post-install.sh
+++ nanobsd/plugins/firefly_pbi/scripts/post-install.sh
@@ -5,6 +5,7 @@ firefly_pbi_path=/usr/pbi/firefly-$(uname -m)/
 
 mkdir -p ${firefly_pbi_path}/mnt
 mkdir -p ${firefly_pbi_path}/www
+mkdir -p ${firefly_pbi_path}/var/db/firefly
 
 pw user add daapd -d ${firefly_pbi_path}
 
