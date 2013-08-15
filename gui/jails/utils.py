7fc62a9780f6b8975ddabe9d04bd2a8baf891509
William Grzybowski <wg@FreeBSD.org>
2013-08-15 15:20:00 -0300

    Change error message about missing ZFS volume
    Ticket:	#2540

--- gui/jails/utils.py
+++ gui/jails/utils.py
@@ -232,7 +232,7 @@ def jail_auto_configure():
 
     volume = Volume.objects.filter(vol_fstype='ZFS')
     if not volume.exists():
-        raise MiddlewareError(_("You need to create a Volume to proceed!"))
+        raise MiddlewareError(_("You need to create a ZFS volume to proceed!"))
     volume = volume[0]
     basename = "%s/jails" % volume.vol_name
     name = basename
