44008f1c8d2276b301889646d82160434462f574
William Grzybowski <wg@FreeBSD.org>
2013-08-20 12:06:51 -0300

    Add CRUD API unit tests for Scrub

--- gui/api/tests/storage.py
+++ gui/api/tests/storage.py
@@ -1,4 +1,5 @@
 from .utils import APITestCase
+from freenasUI.storage import models
 
 
 class VolumeResourceTest(APITestCase):
@@ -47,3 +48,103 @@ class VolumeResourceTest(APITestCase):
             self.client.get(self.get_api_url(), format='json')
         )
 
+
+class ScrubResourceTest(APITestCase):
+
+    def setUp(self):
+        super(ScrubResourceTest, self).setUp()
+        self._vol = models.Volume.objects.create(
+            vol_name='tank',
+            vol_fstype='ZFS',
+        )
+        models.MountPoint.objects.create(
+            mp_path='/mnt/tank',
+            mp_volume=self._vol,
+        )
+
+    def test_get_list_unauthorzied(self):
+        self.assertHttpUnauthorized(
+            self.client.get(self.get_api_url(), format='json')
+        )
+
+    def test_Create(self):
+        resp = self.api_client.post(
+            self.get_api_url(),
+            format='json',
+            data={
+                'scrub_volume': self._vol.id,
+                'scrub_minute': '00',
+                'scrub_hour': '00',
+                'scrub_daymonth': '*',
+                'scrub_month': '*',
+                'scrub_dayweek': '7',
+            }
+        )
+        self.assertHttpCreated(resp)
+        self.assertValidJSON(resp.content)
+
+        data = self.deserialize(resp)
+        self.assertEqual(data, {
+            u'id': 1,
+            u'scrub_daymonth': u'*',
+            u'scrub_dayweek': u'7',
+            u'scrub_description': u'',
+            u'scrub_enabled': True,
+            u'scrub_hour': u'00',
+            u'scrub_minute': u'00',
+            u'scrub_month': u'*',
+            u'scrub_threshold': 35,
+            u'scrub_volume': u'tank',
+            u'scrub_volume_id': None,
+        })
+
+    def test_Retrieve(self):
+        obj = models.Scrub.objects.create(
+            scrub_volume=self._vol
+        )
+        resp = self.api_client.get(
+            self.get_api_url(),
+            format='json',
+        )
+        self.assertHttpOK(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(data, [{
+            u'id': obj.id,
+            u'scrub_daymonth': u'*',
+            u'scrub_dayweek': u'7',
+            u'scrub_description': u'',
+            u'scrub_enabled': True,
+            u'scrub_hour': u'00',
+            u'scrub_minute': u'00',
+            u'scrub_month': u'*',
+            u'scrub_threshold': 35,
+            u'scrub_volume': u'tank'
+        }])
+
+    def test_Update(self):
+        obj = models.Scrub.objects.create(
+            scrub_volume=self._vol,
+            scrub_enabled=True,
+        )
+        resp = self.api_client.put(
+            '%s%d/' % (self.get_api_url(), obj.id),
+            format='json',
+            data={
+                'scrub_enabled': False,
+                'scrub_volume': obj.id,
+            }
+        )
+        self.assertHttpAccepted(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(data['id'], obj.id)
+        self.assertEqual(data['scrub_enabled'], False)
+
+    def test_Delete(self):
+        obj = models.Scrub.objects.create(
+            scrub_volume=self._vol,
+        )
+        resp = self.api_client.delete(
+            '%s%d/' % (self.get_api_url(), obj.id),
+            format='json',
+        )
+        self.assertHttpAccepted(resp)
