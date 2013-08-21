642db35903d1443fe952236b1ccbc7701ad50e65
William Grzybowski <wg@FreeBSD.org>
2013-08-21 14:00:10 -0300

    Add CRUD API unit tests for Static Route

--- gui/api/tests/network.py
+++ gui/api/tests/network.py
@@ -101,3 +101,83 @@ class InterfacesResourceTest(APITestCase):
             format='json',
         )
         self.assertHttpAccepted(resp)
+
+
+class StaticRouteResourceTest(APITestCase):
+
+    def test_get_list_unauthorzied(self):
+        self.assertHttpUnauthorized(
+            self.client.get(self.get_api_url(), format='json')
+        )
+
+    def test_Create(self):
+        resp = self.api_client.post(
+            self.get_api_url(),
+            format='json',
+            data={
+                'sr_destination': '192.168.1.111/24',
+                'sr_gateway': '192.168.3.1',
+                'sr_description': 'test route',
+            }
+        )
+        self.assertHttpCreated(resp)
+        self.assertValidJSON(resp.content)
+
+        data = self.deserialize(resp)
+        self.assertEqual(data, {
+            u'id': 1,
+            u'sr_description': u'test route',
+            u'sr_destination': u'192.168.1.111/24',
+            u'sr_gateway': u'192.168.3.1'
+        })
+
+    def test_Retrieve(self):
+        obj = models.StaticRoute.objects.create(
+            sr_destination='192.168.1.111/24',
+            sr_gateway='192.168.3.1',
+            sr_description='test route',
+        )
+        resp = self.api_client.get(
+            self.get_api_url(),
+            format='json',
+        )
+        self.assertHttpOK(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(data, [{
+            u'id': obj.id,
+            u'sr_description': u'test route',
+            u'sr_destination': u'192.168.1.111/24',
+            u'sr_gateway': u'192.168.3.1'
+        }])
+
+    def test_Update(self):
+        obj = models.StaticRoute.objects.create(
+            sr_destination='192.168.1.111/24',
+            sr_gateway='192.168.3.1',
+            sr_description='test route',
+        )
+        resp = self.api_client.put(
+            '%s%d/' % (self.get_api_url(), obj.id),
+            format='json',
+            data={
+                'sr_destination': '192.168.1.112/24',
+                'sr_description': 'test route 2',
+            }
+        )
+        self.assertHttpAccepted(resp)
+        data = self.deserialize(resp)
+        self.assertEqual(data['id'], obj.id)
+        self.assertEqual(data['sr_destination'], '192.168.1.112/24')
+        self.assertEqual(data['sr_description'], 'test route 2')
+
+    def test_Delete(self):
+        obj = models.StaticRoute.objects.create(
+            sr_destination='192.168.1.111/24',
+            sr_gateway='192.168.3.1',
+            sr_description='test route',
+        )
+        resp = self.api_client.delete(
+            '%s%d/' % (self.get_api_url(), obj.id),
+            format='json',
+        )
+        self.assertHttpAccepted(resp)
