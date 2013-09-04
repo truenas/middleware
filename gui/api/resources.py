314238db8bf1de766f62ade85cdc5ae235539a44
William Grzybowski <wg@FreeBSD.org>
2013-09-04 14:44:07 -0300

    Set initial data for user change form and fix status code

--- gui/api/resources.py
+++ gui/api/resources.py
@@ -1690,8 +1690,13 @@ class AdminUserResource(DojoResource):
             format=request.META.get('CONTENT_TYPE', 'application/json'),
         )
         user = User.objects.all()[0]
+        data = user.__dict__
+        data.update(deserialized)
         form = UserChangeForm(
-            instance=user, data=deserialized, api_validation=True
+            instance=user,
+            data=data,
+            api_validation=True,
+            initial=user.__dict__,
         )
         if not form.is_valid():
             raise ImmediateHttpResponse(
@@ -1700,4 +1705,6 @@ class AdminUserResource(DojoResource):
         else:
             form.save()
 
-        return self.get_list(request, **kwargs)
+        response = self.get_list(request, **kwargs)
+        response.status_code = 202
+        return response
