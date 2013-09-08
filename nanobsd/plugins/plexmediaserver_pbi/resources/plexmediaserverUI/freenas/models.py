269d4dff380b3bc020edb2ce89d56dfed3b52a98
John Hixson <john@ixsystems.com>
2013-09-08 12:00:04 -0700

    Remove some leftovers that were left here from cut and paste

--- nanobsd/plugins/plexmediaserver_pbi/resources/plexmediaserverUI/freenas/models.py
+++ nanobsd/plugins/plexmediaserver_pbi/resources/plexmediaserverUI/freenas/models.py
@@ -3,18 +3,6 @@ import platform
 from django.db import models
 
 
-def conf_dir():
-    return '/usr/pbi/plexmediaserver-%s/etc/plexmediaserver/home' % (
-        platform.machine(),
-        )
-
-
-def download_dir():
-    return '/usr/pbi/plexmediaserver-%s/etc/plexmediaserver/home/Downloads' % (
-        platform.machine(),
-        )
-
-
 class PlexMediaServer(models.Model):
     """
     Django model describing every tunable setting for plexmediaserver
