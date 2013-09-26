ed0c0f68f3317b3807d58729b1cabaa41adf638a
Josh Paetzel <josh@tcbug.org>
2013-09-25 20:41:03 -0700

    Incremental work on the config file generator for netatalk3
    Fairly soon here I'm going to need to change the GUI as many
    options have come and gone.
    
    Also there's a ton of stuff for using netatalk3 with AD and
    LDAP that can all get hooked up.

--- nanobsd/Files/usr/local/libexec/nas/generate_afpd_conf.py
+++ nanobsd/Files/usr/local/libexec/nas/generate_afpd_conf.py
@@ -8,17 +8,41 @@ sys.path.extend([
 ])
 
 from freenasUI import settings
-
 from django.core.management import setup_environ
 setup_environ(settings)
 
-from freenasUI.services.models import AFP
+def main():
+    """Use the django ORM to generate a config file.  We'll build the
+    config file as a series of lines, and once that is done write it
+    out in one go"""
 
-afp = AFP.objects.order_by('id')[0]
-afpd_config = "/etc/afpd.conf"
+    afpd_config = "/etc/afpd.conf"
+    cf_contents = []
 
-guest_user = afp.afp_srv_guest_user
-allow_guest = afp.afp_srv_guest
-max_connections = afp.afp_srv_connections_limit
-server_name = afp.afp_srv_name
-print guest_user
+    from freenasUI.services.models import AFP
+    afp = AFP.objects.order_by('id')[0]
+
+    cf_contents.append("[Global]")
+    
+    if afp.afp_srv_guest:
+        cf_contents.append("\tuam list = uams_dhx.so uams_dhx2.so"
+                           " uams_guest.so")
+        cf_contents.append('\tguest account = "%s"' % afp.afp_srv_guest_user)
+    else:
+        cf_contents.append("\tuam list = uams_dhx.so uams_dhx2.so")
+
+    cf_contents.append("\tmax connections = %s" % afp.afp_srv_connections_limit)
+    cf_contents.append("\tmimic model = RackMac")
+    #server_name = afp.afp_srv_name
+    cf_contents.append("")
+
+    from freenasUI.sharing.models import AFP_Share
+    afp_share = AFP_Share.objects.all()
+    for share in afp_share:
+        cf_contents.append("[%s]" % share.afp_name)
+
+    for line in cf_contents:
+        print line
+
+if __name__ == "__main__":
+    main()
