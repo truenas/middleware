f3e51ca65afec53f12ec440bfaebbc5cbcf863a3
Jordan Hubbard <jkh@ixsystems.com>
2013-09-27 19:38:38 -0700

    Try to use the actual name of the targets in the link stage.

--- build/create_release_tarball.sh
+++ build/create_release_tarball.sh
@@ -25,8 +25,8 @@ for arch in x64 x86 ; do
 	echo $arch = $mapped_arch
 	mkdir $arch
 	for ext in img.xz GUI_Upgrade.txz iso ; do
-		ln "${TOP}/os-base/$mapped_arch/${IMG_PREFIX}-${arch}.${ext}" "${TOP}/release_stage/${arch}"
-		ln "${TOP}/os-base/$mapped_arch/${IMG_PREFIX}-${arch}.${ext}.sha256.txt" "${TOP}/release_stage/${arch}"
+		ln "${TOP}/os-base/$mapped_arch/${IMG_PREFIX}-*-${arch}.${ext}" "${TOP}/release_stage/${arch}"
+		ln "${TOP}/os-base/$mapped_arch/${IMG_PREFIX}-*-${arch}.${ext}.sha256.txt" "${TOP}/release_stage/${arch}"
 	done
 done
 tar -czvf $TARBALL -H release_stage
