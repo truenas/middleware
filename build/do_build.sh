25cff75c4820d42cf92c1db8ebae151f118ad191
Alfred Perlstein <alfred@ixsystems.com>
2013-10-07 17:46:39 -0700

    Pull in TrueNAS gui/build under nas_source.
    We will be modifying the build to pick files from
    this directory instead of using the top level build.
    
    Example would be 'build/files/install_sata_dom.sh',
    this file will now come from
    nas_source/truenas-components/build/files/install_sata_dom.sh.
    
    And so we shall do it for all the other TrueNAS specific
    files.

--- build/do_build.sh
+++ build/do_build.sh
@@ -36,10 +36,11 @@ ADDL_REPOS=""
 if is_truenas ; then
     GIT_DEEP=yes  # shallow checkouts cause too many problems right now.
     # Additional repos to checkout for build
-    ADDL_REPOS="$ADDL_REPOS ZFSD TRUENAS-FILES"
+    ADDL_REPOS="$ADDL_REPOS ZFSD TRUENAS-FILES TRUENAS-COMPONENTS"
 
     : ${GIT_ZFSD_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/git-repo/zfsd.git}
     : ${GIT_TRUENAS_FILES_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/git-repo/truenas-files.git}
+    : ${GIT_TRUENAS_COMPONENTS_REPO=git@gitserver:/git/repos/truenas-build/truenas.git}
     : ${GIT_PORTS_BRANCH=truenas-9.1-a}
     : ${GIT_PORTS_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/git-repo/ports.git}
 
