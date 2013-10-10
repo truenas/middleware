b8ed1144b12a4f8d0988e68a322381372ac8f5ac
William Grzybowski <wg@FreeBSD.org>
2013-10-10 08:59:22 -0300

    Add minimalistic hook framework
    Each app register itself in the appPool instance, using an AppHook class
    containing the appropriate hook methods.

---
+++ gui/freeadmin/apppool.py
@@ -0,0 +1,31 @@
+import logging
+
+log = logging.getLogger('freeadmin.apppool')
+
+
+class AppPool(object):
+
+    def __init__(self):
+        self._registered = {}
+
+    def __iter__(self):
+        for i in self._registered.values():
+            yield i
+
+    def register(self, hook):
+        self._registered[hook.name] = hook()
+
+    def _get_array(self, fname, *args, **kwargs):
+        arr = []
+        for i in self:
+            arr.extend(getattr(i, fname)(*args, **kwargs))
+        return arr
+
+    def get_base_css(self, request):
+        return self._get_array("base_css", request)
+
+    def get_base_js(self, request):
+        return self._get_array("base_js", request)
+
+
+appPool = AppPool()
