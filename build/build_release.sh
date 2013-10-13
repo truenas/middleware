2091d9b6ac7ddf6de22634a8a283edc142dff27f
Jordan Hubbard <jkh@ixsystems.com>
2013-10-12 22:32:22 -0700

    Remove extraneous files.

--- build/build_release.sh
+++ build/build_release.sh
@@ -9,7 +9,7 @@ set -e
 
 mydir=`dirname $0`
 
-rm -rf FreeBSD os-base release_stage
+rm -rf FreeBSD os-base release_stage ${NANO_LABEL}-${VERSION}-release.tar release.build.log
 
 sh $mydir/do_build.sh -a
 env FREENAS_ARCH=i386 sh $mydir/do_build.sh -a
