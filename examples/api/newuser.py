56b166c79d4d0c70ef6b78e9319d6205f4a2cbdd
William Grzybowski <wg@FreeBSD.org>
2013-10-18 10:11:49 -0300

    Fix new user API example

--- examples/api/newuser.py
+++ examples/api/newuser.py
@@ -2,7 +2,7 @@ import json
 import oauth2
 
 consumer = oauth2.Consumer(
-    key='myClient',
+    key='myclient',
     secret='886e8991d4ae9a010656921b1011f2c400348d1d643edbc807c429bb30168b8a55670f9f22cf68610c0a9e277dd69151c48c15c18aa2dccb8bf8b057ca1187c1',
 )
 client = oauth2.Client(consumer)
@@ -11,11 +11,11 @@ content = client.request(
     'http://freenas.mydomain/api/v1.0/account/bsdusers/',
     method='POST',
     headers={'Content-Type': 'application/json'},
-    data=json.dumps({
+    body=json.dumps({
         'bsdusr_uid': '1100',
         'bsdusr_username': 'myuser',
         'bsdusr_mode': '755',
-        'bsdusr_creategroup': 'on',
+        'bsdusr_creategroup': True,
         'bsdusr_password': '12345',
         'bsdusr_shell': '/usr/local/bin/bash',
         'bsdusr_full_name': 'Full Name',
