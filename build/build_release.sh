78d21da6d4e5ea4e62b6b6b040f7da737d776ae9
Josh Paetzel <josh@ixsystems.com>
2013-11-23 14:09:24 -0800

    Fix to build releases with thin images.
    The way nanobsd works, it first builds a thick image
    which is used to compile all of the ports needed.  This
    image has things like gcc, make a toolchain, etc.
    
    A side effect of building ports to place in the image is
    packages are created.  Subsequent builds using that build
    environment will use the packages and a "thin" image is
    created which does not have a toolchain.

--- build/build_release.sh
+++ build/build_release.sh
@@ -9,6 +9,8 @@ set -e
 
 mydir=`dirname $0`
 
+sh $mydir/do_build.sh -z
 sh $mydir/do_build.sh -a
+env FREENAS_ARCH=i386 sh $mydir/do_build.sh -z
 env FREENAS_ARCH=i386 sh $mydir/do_build.sh -a
 sh $mydir/create_release_tarball.sh
