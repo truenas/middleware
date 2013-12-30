4081db7f04a2eef7370aabcb79023c58b5980474
Jordan Hubbard <jkh@ixsystems.com>
2013-12-30 01:48:02 -0800

    Small changes to the way releases are packaged up.

--- build/build_release.sh
+++ build/build_release.sh
@@ -13,4 +13,4 @@ sh $mydir/do_build.sh -z
 sh $mydir/do_build.sh -a
 env FREENAS_ARCH=i386 sh $mydir/do_build.sh -z
 env FREENAS_ARCH=i386 sh $mydir/do_build.sh -a
-sh $mydir/create_release_tarball.sh
+sh $mydir/create_release_distribution.sh
