903bbfe90a58bb0c3782fa2a8722f548a762b832
William Grzybowski <wg@FreeBSD.org>
2014-02-13 16:16:40 -0200

    Change API of app pool hook allowing to call a single generic method
    This should reduce the amount of duplicated code.

--- gui/freeadmin/apppool.py
+++ gui/freeadmin/apppool.py
@@ -18,105 +18,113 @@ class AppPool(object):
     def get_app(self, name):
         return self._registered[name]
 
-    def _get_array(self, fname, *args, **kwargs):
+    def _get_array(self, fname, cname, *args, **kwargs):
         method = kwargs.pop('_method', list.extend)
         arr = []
         for i in self:
-            func = getattr(i, fname, None)
+            func = getattr(i, '%s_%s' % (fname, cname), None)
             if func and callable(func):
                 method(arr, func(*args, **kwargs))
+                continue
+
+            func = getattr(i, fname, None)
+            if func and callable(func):
+                if cname:
+                    method(arr, func(cname, *args, **kwargs))
+                else:
+                    method(arr, func(*args, **kwargs))
         return arr
 
     def get_base_css(self, request):
-        return self._get_array("base_css", request)
+        return self._get_array("base_css", None, request)
 
     def get_base_js(self, request):
-        return self._get_array("base_js", request)
+        return self._get_array("base_js", None, request)
 
     def get_top_menu(self, request):
-        arr = self._get_array("top_menu", request)
+        arr = self._get_array("top_menu", None, request)
         arr = sorted(arr, key=lambda x: x.get('weight'))
         return arr
 
     def get_system_info(self, request):
-        return self._get_array("system_info", request)
+        return self._get_array("system_info", None, request)
 
     def hook_app_index(self, name, request):
         return self._get_array(
-            'hook_app_index_%s' % name, request,
+            'hook_app_index', name, request,
             _method=list.append
         )
 
     def hook_app_tabs(self, name, request):
-        return self._get_array('hook_app_tabs_%s' % name, request)
+        return self._get_array('hook_app_tabs', name, request)
 
     def hook_class_new(self, name, bases, attrs):
         return self._get_array(
-            'hook_class_new_%s' % name, bases, attrs,
+            'hook_class_new', name, bases, attrs,
             _method=list.append
         )
 
     def hook_datagrid_actions(self, rname, admin, actions):
         return self._get_array(
-            'hook_datagrid_actions_%s' % rname, admin, actions,
+            'hook_datagrid_actions', rname, admin, actions,
             _method=list.append
         )
 
     def hook_datagrid_buttons(self, rname, admin):
         return self._get_array(
-            'hook_datagrid_buttons_%s' % rname, admin,
+            'hook_datagrid_buttons', rname, admin,
         )
 
     def hook_feature_disabled(self, name):
         return True in self._get_array(
-            'hook_feature_disabled_%s' % name,
+            'hook_feature_disabled', name,
             _method=list.append
         )
 
     def hook_form_buttons(self, fname, form, action, *args, **kwargs):
         return self._get_array(
-            'hook_form_buttons_%s' % fname, form, action, *args, **kwargs
+            'hook_form_buttons', fname, form, action, *args, **kwargs
         )
 
     def hook_form_delete(self, fname, form, request, events):
         return self._get_array(
-            'hook_form_delete_%s' % fname, form, request, events,
+            'hook_form_delete', fname, form, request, events,
             _method=list.append
         )
 
     def hook_form_done(self, fname, form, request, events):
         return self._get_array(
-            'hook_form_done_%s' % fname, form, request, events,
+            'hook_form_done', fname, form, request, events,
             _method=list.append
         )
 
     def hook_form_init(self, fname, form, *args, **kwargs):
         kwargs['_method'] = list.append
         return self._get_array(
-            'hook_form_init_%s' % fname, form, *args, **kwargs
+            'hook_form_init', fname, form, *args, **kwargs
         )
 
     def hook_model_new(self, name, bases, attrs):
         return self._get_array(
-            'hook_model_new_%s' % name, bases, attrs,
+            'hook_model_new', name, bases, attrs,
             _method=list.append
         )
 
     def hook_nav_init(self, app, tree_roots, nav, request):
         return self._get_array(
-            'hook_nav_init_%s' % app, tree_roots, nav, request,
+            'hook_nav_init', app, tree_roots, nav, request,
             _method=list.append
         )
 
     def hook_resource_bundle(self, rname, resource, bundle):
         return self._get_array(
-            'hook_resource_bundle_%s' % rname, resource, bundle,
+            'hook_resource_bundle', rname, resource, bundle,
             _method=list.append
         )
 
     def hook_tool_run(self, name):
         return self._get_array(
-            'hook_tool_run_%s' % name,
+            'hook_tool_run', name,
             _method=list.append
         )
 
