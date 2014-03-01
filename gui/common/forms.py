truenas/middleware

commit 9e2a5306f142d884da4294916c13705a124e848c

Add done method and hook for normal forms

gui/common/forms.py
@@ -156,3 +156,7 @@ class Form(AdvMixin, F):
             row_ender=u'</td></tr>',
             help_text_html=u'<br />%s',
             errors_on_separate_row=False)
+
+    def done(self, request, events):
+        fname = str(type(self).__name__)
+        appPool.hook_form_done(fname, self, request, events)

commit a777ef593036fe6dc63b9b1910054e1dce2e3bbd

Fix API breakage introduced in 9e2a5306f14
Now heading to the beach for carnival! :)

Ticket:	#4420

gui/common/forms.py
@@ -157,6 +157,6 @@ class Form(AdvMixin, F):
             help_text_html=u'<br />%s',
             errors_on_separate_row=False)
 
-    def done(self, request, events):
+    def done(self, request=None, events=None, **kwargs):
         fname = str(type(self).__name__)
         appPool.hook_form_done(fname, self, request, events)