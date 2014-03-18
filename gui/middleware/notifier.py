f08784f44290444d146cd0958cd65b5ba30a7dd8
William Grzybowski <wg@FreeBSD.org>
2014-03-18 14:29:29 -0300

    Show whether the snapshot is already in remote side
    Ticket:	#4553

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -1584,6 +1584,24 @@ class notifier:
                 return True
         return False
 
+    def repl_remote_snapshots(self, repl):
+        """
+        Get a list of snapshots in the remote side
+        """
+        if repl.repl_remote.ssh_remote_dedicateduser_enabled:
+            user = repl.repl_remote.ssh_remote_dedicateduser
+        else:
+            user = 'root'
+        proc = self._pipeopen('/usr/bin/ssh -i /data/ssh/replication -o ConnectTimeout=3 -p %s %s@%s "zfs list -Ht snapshot -o name"' % (
+            repl.repl_remote.ssh_remote_port,
+            user,
+            repl.repl_remote.ssh_remote_hostname,
+        ))
+        data = proc.communicate()[0]
+        if proc.returncode != 0:
+            return []
+        return data.strip('\n').split('\n')
+
     def destroy_zfs_dataset(self, path, recursive=False):
         retval = None
         if '@' in path:
