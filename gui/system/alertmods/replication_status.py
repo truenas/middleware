7618b08224c7f07b96b7fa57e02b038f51e060a0
William Grzybowski <wg@FreeBSD.org>
2014-03-19 09:57:17 -0300

    repl_lastresult is not a field in the ORM anymore
    Ticket:	#4553

--- gui/system/alertmods/replication_status.py
+++ gui/system/alertmods/replication_status.py
@@ -8,11 +8,11 @@ from freenasUI.storage.models import Replication
 class ReplicationStatusAlert(BaseAlert):
 
     def run(self):
-        qs = Replication.objects.filter(repl_enabled=True).exclude(
-            Q(repl_lastresult='Succeeded') | Q(repl_lastresult='')
-        )
+        qs = Replication.objects.filter(repl_enabled=True)
         alerts = []
         for repl in qs:
+            if repl.repl_lastresult in ('Succeeded', ''):
+                continue
             alerts.append(Alert(
                 Alert.CRIT,
                 _('Replication %s failed: %s') % (repl, repl.repl_lastresult),
