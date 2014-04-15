6cd345c7410367fc485cbacc28cc9be086c27097
Craig Rodrigues <rodrigc@ixsystems.com>
2014-04-15 13:17:22 -0700

    Add debug.

--- build/ports/build-ports.sh
+++ build/ports/build-ports.sh
@@ -6,5 +6,8 @@ cd "$(dirname "$0")/../.."
 TOP="$(pwd)"
 . build/nano_env
 
+# XX: Uncomment to debug
+#TRACE=-x
+
 mkdir -p ${NANO_OBJ}/poudriere/ports/distfiles
-poudriere -e ${NANO_OBJ}/poudriere/etc bulk -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
+poudriere ${TRACE} -e ${NANO_OBJ}/poudriere/etc bulk -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
