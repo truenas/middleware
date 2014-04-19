cb1d34379e4c90d03ae5b39322ccd881caf0a0a2
Josh Paetzel <josh@ixsystems.com>
2014-04-19 12:58:01 -0700

    I know python, I really do.

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -4144,7 +4144,7 @@ class notifier:
         This syncs the EncryptedDisk table with the current state
         of a volume
         """
-        from freenasUI.storage.models import Disk EncryptedDisk, Volume
+        from freenasUI.storage.models import Disk, EncryptedDisk, Volume
         if volume is not None:
             volumes = [volume]
         else:
