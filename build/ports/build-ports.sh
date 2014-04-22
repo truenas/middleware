367b2e623163cacbc95d070d3460e6e14da167ae
Craig Rodrigues <rodrigc@ixsystems.com>
2014-04-22 09:03:12 -0700

    Throttle poudriere to use 10 parallel builds.  This makes things a bit more usable when multiple people are compiling on the same build server. We can raise this limit later.

--- build/ports/build-ports.sh
+++ build/ports/build-ports.sh
@@ -20,5 +20,10 @@ trap cleanup EXIT
 
 mount -t nullfs ${GIT_FREEBSD_CHECKOUT_PATH} ${NANO_OBJ}/_.w/usr/src  || exit 1
 
+MAKE_JOBS=$(( 2 * $(sysctl -n kern.smp.cpus) + 1 ))
+if [ ${MAKE_JOBS} -gt 10 ]; then
+        MAKE_JOBS=10
+fi
+
 mkdir -p ${NANO_OBJ}/ports/distfiles
-poudriere ${TRACE} -e ${NANO_OBJ}/poudriere/etc bulk -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
+poudriere ${TRACE} -e ${NANO_OBJ}/poudriere/etc bulk -J ${MAKE_JOBS} -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
