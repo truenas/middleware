06d04e41d7ae8b904b94719ae35029466c857682
Craig Rodrigues <rodrigc@ixsystems.com>
2014-04-23 18:03:53 -0700

    Add logic to copy in FreeNAS-specific ports directories into the main ports jail tree. We cannot overlay these directories via nullfs, because poudriere uses the cpdup utility to clone the ports jail tree multiple times in order to parallelize the ports build.

--- build/ports/build-ports.sh
+++ build/ports/build-ports.sh
@@ -10,7 +10,13 @@ TOP="$(pwd)"
 
 cleanup()
 {
-	umountfs ${NANO_OBJ}/_.j/usr/src
+	echo "Cleaning up jail...."
+	sleep 3
+	umountfs ${NANO_OBJ}/_.j
+
+	for d in $EXTRA_PORT_DIRS; do
+		[ -d  "${GIT_PORTS_CHECKOUT_PATH}/${d}" ] && rm -fr "${GIT_PORTS_CHECKOUT_PATH}/${d}"
+	done
 }
 
 # XX: Uncomment to debug
@@ -18,7 +24,18 @@ cleanup()
 
 trap cleanup EXIT
 
-mount -t nullfs ${GIT_FREEBSD_CHECKOUT_PATH} ${NANO_OBJ}/_.j/usr/src  || exit 1
+EXTRA_PORT_DIRS="sysutils/zfsd misc/truenas-files misc/freenas-files"
+
+for d in $EXTRA_PORT_DIRS; do
+	mkdir -p "${GIT_PORTS_CHECKOUT_PATH}/${d}"
+done
+
+mount -t nullfs -o ro ${GIT_FREEBSD_CHECKOUT_PATH} ${NANO_OBJ}/_.j/usr/src  || exit 1
+#mount -t nullfs -o ro ${AVATAR_ROOT}/src ${NANO_OBJ}/_.j/usr/nas_source2 || exit 1
+cp -a ${AVATAR_ROOT}/src/ ${NANO_OBJ}/_.j/usr/nas_source2 
+cp -a ${AVATAR_ROOT}/nas_ports/sysutils/zfsd ${GIT_PORTS_CHECKOUT_PATH}/sysutils 
+cp -a ${AVATAR_ROOT}/nas_ports/misc/truenas-files ${GIT_PORTS_CHECKOUT_PATH}/misc 
+cp -a ${AVATAR_ROOT}/nas_ports/misc/freenas-files ${GIT_PORTS_CHECKOUT_PATH}/misc
 
 MAKE_JOBS=$(( 2 * $(sysctl -n kern.smp.cpus) + 1 ))
 if [ ${MAKE_JOBS} -gt 10 ]; then
@@ -26,4 +43,4 @@ if [ ${MAKE_JOBS} -gt 10 ]; then
 fi
 
 mkdir -p ${NANO_OBJ}/ports/distfiles
-poudriere ${TRACE} -e ${NANO_OBJ}/poudriere/etc bulk -J ${MAKE_JOBS} -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
+poudriere ${TRACE} -e ${NANO_OBJ}/poudriere/etc bulk -w -J ${MAKE_JOBS} -f ${NANO_OBJ}/poudriere/etc/ports.txt -j j -p p
