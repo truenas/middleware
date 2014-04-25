2cedc1b251173fd7f6e8f433e8431477ef860c83
Craig Rodrigues <rodrigc@ixsystems.com>
2014-04-25 08:24:37 -0700

    Pull out the logic for checking the build host environment from do_build.sh into a separate script.  Call this script in a few more places.

--- build/do_build.sh
+++ build/do_build.sh
@@ -45,35 +45,6 @@ EOF
 	exit 1
 }
 
-check_build_sanity()
-{
-    # The build will fail if we make directories too long due to
-    # using nullfs.  This is because nullfs can not handle long
-    # directory names for mounts.
-    # Catch this early so we don't spend a lot of time doing stuff
-    # just to get a build error.
-    local mypwd=`pwd`
-    local mypwdlen=`pwd | wc -c | awk '{print $1}'`  # use awk to cleanup wc output
-    local pwdmaxlen="45"
-    if [ $mypwdlen -ge $pwdmaxlen ] ; then
-        cat <<PWD_ERROR
-=================================================================
-FATAL:
-current path (pwd) too long ($mypwdlen) for nullfs mounts during
-build.
-=================================================================
-WHY:
-Building ports will very likely fail when doing nullfs.
-=================================================================
-TO FIX:
-please rename/move your build directory to a place with a shorter
-less than $pwdmaxlen characters)
-current pwd: '$mypwd'
-PWD_ERROR
-        exit 1
-    fi
-}
-
 show_build_targets()
 {
 	for _target in ${BUILD_TARGETS}
@@ -186,37 +157,6 @@ build_targets()
 	done
 }
 
-check_for_command_from_port()
-{
-   local COMMAND=$1
-   local PACKAGE=$2
-   local FOUND
-   local MSG
-
-   FOUND="$(command -v $COMMAND || echo '')"
-
-   if [ -z "$FOUND" ]; then
-       MSG="ERROR: $COMMAND not found."
-       if [ -n "$PACKAGE" ]; then
-           MSG="$MSG.\nERROR: Please run 'pkg install $PACKAGE' or install from ports."
-       fi
-       printf "\n$MSG\n\n"
-       exit 1
-   fi 
-}
-
-check_build_tools()
-{
-	check_for_command_from_port mkisofs sysutils/cdrtools
-	check_for_command_from_port git devel/git
-	check_for_command_from_port pxz archivers/pxz
-	check_for_command_from_port xz archivers/xz
-	check_for_command_from_port python lang/python
-	check_for_command_from_port python2 lang/python2
-	check_for_command_from_port VBoxManage emulators/virtualbox-ose
-	check_for_command_from_port poudriere ports-mgmt/poudriere-devel
-}
-
 main()
 {
 	parse_cmdline "$@"
@@ -234,13 +174,6 @@ main()
 	#
 	set -e
 	requires_root
-
-    #
-    # Do extra checks to make sure the build will succeed.
-    #
-    check_build_sanity
-
-	check_build_tools
 
 	#
 	# Expand targets to their full path in the file system
