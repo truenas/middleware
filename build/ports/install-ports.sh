fe6e20a6f8ef1d0ec78cca2b232157e0d84fb64c
Craig Rodrigues <rodrigc@ixsystems.com>
2014-04-28 18:19:40 -0700

    Fix paths.

--- build/ports/install-ports.sh
+++ build/ports/install-ports.sh
@@ -19,10 +19,10 @@ cleanup()
 trap cleanup EXIT
 
 mkdir -p ${NANO_OBJ}/_.w/usr/ports/packages
-mount -t nullfs ${NANO_OBJ}/.ports/packages/j-p ${NANO_OBJ}/_.w/usr/ports/packages  || exit 1
+mount -t nullfs ${NANO_OBJ}/ports/packages/j-p ${NANO_OBJ}/_.w/usr/ports/packages  || exit 1
 
-build/ports/install-ports-from-list.py --index ${NANO_OBJ}/.ports/packages/j-p/INDEX-9.bz2 \
-                                       --packages ${NANO_OBJ}/.ports/packages/j-p \
+build/ports/install-ports-from-list.py --index ${NANO_OBJ}/ports/packages/j-p/INDEX-9.bz2 \
+                                       --packages ${NANO_OBJ}/ports/packages/j-p \
                                        --chroot ${NANO_OBJ}/_.w \
                                        --ports ${NANO_OBJ}/poudriere/etc/ports.txt
 
