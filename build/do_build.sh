52b5d492b8f9ea9096a989fdc7703285862899bf
Craig Rodrigues <rodrigc@ixsystems.com>
2014-05-05 21:39:53 -0700

    Delete unused code.

--- build/do_build.sh
+++ build/do_build.sh
@@ -18,95 +18,17 @@ if [ ${MAKE_JOBS} -gt 10 ]; then
 fi
 export MAKE_JOBS
 
-# Available targets to build
-BUILD_TARGETS="os-base"
-
-# Targets to build (os-base, plugins/<plugin>).
-TARGETS=""
-
 # Trace flags
 TRACE=""
 
-# NanoBSD flags
-NANO_ARGS=""
-
-usage() {
-	cat <<EOF
-usage: ${0##*/} [-Jx] [-j make-jobs] [-- nanobsd-options]
-
--j make-jobs	- number of make jobs to run; defaults to ${MAKE_JOBS}.
--x		- enable sh -x debugging
-
-EOF
-	exit 1
-}
-
-parse_cmdline()
+main()
 {
-	while getopts 'afj:st:x' _optch
-	do
-		case "${_optch}" in
-		j)
-			echo ${OPTARG} | egrep -q '^[[:digit:]]+$' && [ ${OPTARG} -gt 0 ]
-			if [ $? -ne 0 ]; then
-				usage
-			fi
-			MAKE_JOBS=${OPTARG}
-			;;
-		x)
-			TRACE="-x"
-			;;
-		\?)
-			usage
-			;;
-		esac
-	done
-
-	shift $((${OPTIND} - 1))
-
-	NANO_ARGS="$@"
-	export NANO_ARGS
-}
-
-expand_targets()
-{
-	local _targets=""
-	for _target in ${TARGETS}
-	do
-		if [ -f "${NANO_CFG_BASE}/${_target}" ]
-		then
-			_targets="${_targets} ${NANO_CFG_BASE}/${_target}"
-		fi
-	done
-	TARGETS="${_targets}"
-
-	if [ -z "${TARGETS}" ]
-	then
-		error "Build targets -- ${TARGETS} -- don't exist"
-	fi
-}
-
-build_target()
-{
-	local _target="${1}"
-	local _args="${NANO_ARGS}"
 	local _nanobsd="${AVATAR_ROOT}/build/nanobsd/nanobsd.sh"
-	local _c
 
-	export AVATAR_COMPONENT=${_target##*/}
-
-	#
-	# XXX: chicken and egg problem. Not doing this will always cause plugins-base,
-	# etc to rebuild if os-base isn't already present, or the build to fail if
-	# os-base is built and plugins-base isn't, etc.
-	#
+	export AVATAR_COMPONENT=os-base
 	export NANO_OBJ=${AVATAR_ROOT}/${AVATAR_COMPONENT}/${NANO_ARCH}
 
-	local _required_logs="_.ik _.iw"
-
-	_args="${_args} -w -k"
-
-	local _cmd="${_nanobsd} -c ${_target} ${_args} -j ${MAKE_JOBS}"
+	local _cmd="${_nanobsd} -c os-base ${NANO_ARGS} -w -k -j ${MAKE_JOBS}"
 	echo ${_cmd}
 
 	if sh ${TRACE} ${_cmd}
@@ -118,61 +40,5 @@ build_target()
 	
 	return $?
 }
-
-build_targets()
-{
-	#
-	# For now do this iteratively. Eventually it would be nice to
-	# be able to background building each target, but currently
-	# that needs some more kung-fu. 
-	#
-	cd ${NANO_SRC}
-	for _target in ${TARGETS}
-	do
-		build_target "${_target}"
-	done
-}
-
-main()
-{
-	parse_cmdline "$@"
-
-	#
-	# Assume os-base if no targets are specified
-	#
-	if [ -z "${TARGETS}" ]
-	then
-		TARGETS="os-base"
-	fi
-
-	#
-	# You must be root to build FreeNAS
-	#
-	set -e
-	requires_root
-
-	#
-	# Expand targets to their full path in the file system
-	#
-	expand_targets
-
-	#
-	# HACK: chmod +x the script because:
-	# 1. It's not in FreeBSD proper, so it will always be touched.
-	# 2. The mode is 0644 by default, and using a pattern like ${SHELL}
-	#    in the Makefile snippet won't work with csh users because the
-	#    script uses /bin/sh constructs.
-	#
-	if [ -f "${NANO_SRC}/include/mk-osreldate.sh.orig" ]
-	then
-		chmod +x ${NANO_SRC}/include/mk-osreldate.sh
-	fi
-
-	#
-	# Now let's build the targets
-	#	
-	build_targets "$@"
-}
-
 
 main "$@"
