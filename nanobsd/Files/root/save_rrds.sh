72076a94678e92b00bf8b525f39cc06393fe3c38
John Hixson <john@ixsystems.com>
2014-05-12 23:53:04 -0700

    Only save rrds to tar file when not using the rrd dataset

--- nanobsd/Files/root/save_rrds.sh
+++ nanobsd/Files/root/save_rrds.sh
@@ -6,7 +6,70 @@
 # If the rrd directory does exist it will archive it to persistant
 # storage.
 
+. /etc/rc.freenas
+
 PERSIST_FILE="/data/rrd_dir.tar.bz2"
+
+get_system_pool()
+{
+	${FREENAS_SQLITE_CMD} ${FREENAS_CONFIG} "
+	SELECT
+		sys_pool
+	FROM
+		system_systemdataset
+	ORDER BY
+		-id
+	LIMIT 1
+	" | \
+	while read -r system_pool
+	do
+		echo "${system_pool}"
+	done
+}
+
+use_rrd_dataset()
+{
+	local use
+	local pool
+
+	if ! is_freenas
+	then
+		return 1
+	fi
+
+	pool="$(get_system_pool)"
+	if [ -z "${pool}" ]
+	then
+		return 1
+	fi
+
+	use="$(${FREENAS_SQLITE_CMD} ${FREENAS_CONFIG} "
+	SELECT
+		sys_rrd_usedataset
+	FROM
+		system_systemdataset
+	ORDER BY
+		-id
+	LIMIT 1
+	" | \
+	while read -r rrd_usedataset
+	do
+		if [ "${rrd_usedataset}" = "0" ]
+		then
+			echo "1"
+		else
+			echo "0"
+		fi
+	done
+	)"
+
+	return ${use}
+}
+
+if use_rrd_dataset
+then
+	exit 0
+fi
 
 cd /var/db
 if [ -d collectd ]
