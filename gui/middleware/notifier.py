761caab4a3acdc9fc6b024601df117eaf42f9292
William Grzybowski <wg@FreeBSD.org>
2014-05-28 12:27:21 -0300

    Add a comment about rsync notifier method

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -4988,6 +4988,11 @@ class notifier:
             return False
 
     def rsync_command(self, obj_or_id):
+        """
+        Helper method used in ix-crontab to generate the rsync command to
+        avoid code duplication
+        This should be removed once ix-crontab is rewritten in python
+        """
         from freenasUI.system.models import Rsync
         oid = int(obj_or_id)
         rsync = Rsync.objects.get(id=oid)
