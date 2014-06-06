4b0675687fa0154cca2034672c240a3bfceb9a1a
John Hixson <john@ixsystems.com>
2014-06-05 19:36:48 -0700

    Generate nsswitch.conf from python and using sssd

---
+++ nanobsd/Files/usr/local/libexec/nas/generate_nsswitch_conf.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python
+
+import os
+import string
+import sys
+
+sys.path.extend([
+    '/usr/local/www',
+    '/usr/local/www/freenasUI'
+])
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "freenasUI.settings"
+
+from django.db.models.loading import cache
+cache.get_apps()
+
+NSSWITCH_CONF_PATH = "/etc/nsswitch.conf"
+
+from freenasUI.common.system import (
+    activedirectory_enabled,
+    domaincontroller_enabled,
+    ldap_enabled,
+    nis_enabled,
+    nt4_enabled
+)
+
+def main():
+    nsswitch_conf = {
+        'group': ['files'],
+        'hosts': ['files', 'dns'],
+        'networks': ['files'],
+        'passwd': ['files'],
+        'shells': ['files'],
+        'services': ['files'],
+        'protocols': ['files'],
+        'rpc': ['files']
+    }
+
+    if activedirectory_enabled() and activedirectory_has_unix_extensions():
+        nsswitch_conf['passwd'].append('sss')
+        nsswitch_conf['group'].append('sss')
+    elif activedirectory_enabled() or \
+        domaincontroller_enabled() or nt4_enabled():
+        nsswitch_conf['passwd'].append('winbind')
+        nsswitch_conf['group'].append('winbind')
+
+    if ldap_enabled():
+        nsswitch_conf['passwd'].append('sss')
+        nsswitch_conf['group'].append('sss')
+
+    if nis_enabled():
+        nsswitch_conf['passwd'].append('nis')
+        nsswitch_conf['group'].append('nis')
+        nsswitch_conf['hosts'].append('nis')
+
+    try:
+        fd = os.open(NSSWITCH_CONF_PATH, os.O_WRONLY|os.O_CREAT|os.O_TRUNC, 0644)
+        for key in nsswitch_conf:
+            line = "%s: %s\n" % (
+                key.strip(),
+                string.join(map(lambda x: x.strip(), nsswitch_conf[key]), ' ')
+            )
+            os.write(fd, line)
+        os.close(fd)
+
+    except Exception as e:
+        print >> sys.stderr, "can't create %s: %s" % (NSSWITCH_CONF_PATH, e)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
