016a64ce2d6062b245686211d099fdaeeb2cb435
William Grzybowski <wg@FreeBSD.org>
2014-06-26 10:08:03 -0300

    Fix typo in knob for resource name

--- gui/jails/admin.py
+++ gui/jails/admin.py
@@ -256,7 +256,7 @@ class JailTemplateFAdmin(BaseFreeAdmin):
     icon_view = u"ServicesIcon"
 
     resource_mixin = JailTemplateResourceMixin
-    resourcename = 'jails/templates'
+    resource_name = 'jails/templates'
 
     def get_datagrid_columns(self):
         columns = []
