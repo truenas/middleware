331512ab5d5193342e024876413c28aa9cc13841
William Grzybowski <wg@FreeBSD.org>
2014-07-02 09:50:48 -0300

    Move NTP Servers from system nested tab to a button under General
    Ticket:	#5416

--- gui/system/nav.py
+++ gui/system/nav.py
@@ -1,7 +1,15 @@
 from freenasUI.freeadmin.tree import TreeNode
 from django.utils.translation import ugettext_lazy as _
 
-BLACKLIST = ['Email', 'Advanced', 'Settings', 'SSL', 'SystemDataset', 'Registration']
+BLACKLIST = [
+    'Advanced',
+    'Email',
+    'NTPServer',
+    'Settings',
+    'SSL',
+    'SystemDataset',
+    'Registration',
+]
 NAME = _('System')
 ICON = u'SystemIcon'
 ORDER = 1
@@ -37,13 +45,6 @@ class Info(TreeNode):
     name = _(u'System Information')
     icon = u"InfoIcon"
     type = 'opensystem'
-
-
-class NTPServerView(TreeNode):
-
-    gname = 'View'
-    type = 'opensystem'
-    append_to = 'system.NTPServer'
 
 
 class SSL(TreeNode):
