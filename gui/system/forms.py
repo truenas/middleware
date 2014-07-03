b95e5b0d47cc08292a4b8fd4df29ba4015b6262f
William Grzybowski <wg@FreeBSD.org>
2014-07-03 09:52:50 -0300

    Move tasks forms from system

--- gui/system/forms.py
+++ gui/system/forms.py
@@ -1,4 +1,4 @@
-#+
+# +
 # Copyright 2010 iXsystems, Inc.
 # All rights reserved
 #
@@ -25,11 +25,9 @@
 #
 #####################################################################
 
-import glob
 import json
 import logging
 import os
-import pwd
 import re
 import stat
 import subprocess
@@ -45,8 +43,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from dojango import forms
 from freenasUI import choices
-from freenasUI.common.forms import ModelForm, Form, mchoicefield
-from freenasUI.freeadmin.forms import CronMultiple
+from freenasUI.common.forms import ModelForm, Form
 from freenasUI.middleware.exceptions import MiddlewareError
 from freenasUI.middleware.notifier import notifier
 from freenasUI.storage.models import MountPoint, Volume
@@ -95,7 +92,7 @@ class FileWizard(SessionWizardView):
 
     def done(self, form_list, **kwargs):
         response = render_to_response('system/done.html', {
-            #'form_list': form_list,
+            # 'form_list': form_list,
             'retval': getattr(self, 'retval', None),
         })
         if not self.request.is_ajax():
@@ -613,110 +610,6 @@ class SSLForm(ModelForm):
         notifier().start_ssl("nginx")
 
 
-class SMARTTestForm(ModelForm):
-
-    class Meta:
-        fields = '__all__'
-        model = models.SMARTTest
-        widgets = {
-            'smarttest_hour': CronMultiple(
-                attrs={'numChoices': 24, 'label': _("hour")}
-            ),
-            'smarttest_daymonth': CronMultiple(
-                attrs={
-                    'numChoices': 31, 'start': 1, 'label': _("day of month"),
-                }
-            ),
-            'smarttest_dayweek': forms.CheckboxSelectMultiple(
-                choices=choices.WEEKDAYS_CHOICES
-            ),
-            'smarttest_month': forms.CheckboxSelectMultiple(
-                choices=choices.MONTHS_CHOICES
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        if 'instance' in kwargs:
-            ins = kwargs.get('instance')
-            if ins.smarttest_daymonth == "..":
-                ins.smarttest_daymonth = '*/1'
-            elif ',' in ins.smarttest_daymonth:
-                days = [int(day) for day in ins.smarttest_daymonth.split(',')]
-                gap = days[1] - days[0]
-                everyx = range(0, 32, gap)[1:]
-                if everyx == days:
-                    ins.smarttest_daymonth = '*/%d' % gap
-            if ins.smarttest_hour == "..":
-                ins.smarttest_hour = '*/1'
-            elif ',' in ins.smarttest_hour:
-                hours = [int(hour) for hour in ins.smarttest_hour.split(',')]
-                gap = hours[1] - hours[0]
-                everyx = range(0, 24, gap)
-                if everyx == hours:
-                    ins.smarttest_hour = '*/%d' % gap
-        super(SMARTTestForm, self).__init__(*args, **kwargs)
-        self.fields.keyOrder.remove('smarttest_disks')
-        self.fields.keyOrder.insert(0, 'smarttest_disks')
-        mchoicefield(self, 'smarttest_month', [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        ])
-        mchoicefield(self, 'smarttest_dayweek', [
-            1, 2, 3, 4, 5, 6, 7
-        ])
-
-    def save(self):
-        super(SMARTTestForm, self).save()
-        notifier().restart("smartd")
-
-    def clean_smarttest_hour(self):
-        h = self.cleaned_data.get("smarttest_hour")
-        if h.startswith('*/'):
-            each = int(h.split('*/')[1])
-            if each == 1:
-                return ".."
-        return h
-
-    def clean_smarttest_daymonth(self):
-        h = self.cleaned_data.get("smarttest_daymonth")
-        if h.startswith('*/'):
-            each = int(h.split('*/')[1])
-            if each == 1:
-                return ".."
-        return h
-
-    def clean_smarttest_month(self):
-        m = eval(self.cleaned_data.get("smarttest_month"))
-        m = ",".join(m)
-        return m
-
-    def clean_smarttest_dayweek(self):
-        w = eval(self.cleaned_data.get("smarttest_dayweek"))
-        w = ",".join(w)
-        return w
-
-    def clean(self):
-        disks = self.cleaned_data.get("smarttest_disks", [])
-        test = self.cleaned_data.get("smarttest_type")
-        used_disks = []
-        for disk in disks:
-            qs = models.SMARTTest.objects.filter(
-                smarttest_disks__in=[disk],
-                smarttest_type=test,
-            )
-            if self.instance.id:
-                qs = qs.exclude(id=self.instance.id)
-            if qs.count() > 0:
-                used_disks.append(disk.disk_name)
-        if used_disks:
-            self._errors['smarttest_disks'] = self.error_class([_(
-                "The following disks already have tests for this type: %s" % (
-                    ', '.join(used_disks),
-                )),
-            ])
-            self.cleaned_data.pop("smarttest_disks", None)
-        return self.cleaned_data
-
-
 class FirmwareTemporaryLocationForm(Form):
     mountpoint = forms.ChoiceField(
         label=_("Place to temporarily place firmware file"),
@@ -763,181 +656,6 @@ class FirmwareUploadForm(Form):
 class ConfigUploadForm(Form):
     config = FileField(label=_("New config to be installed"))
 
-
-class CronJobForm(ModelForm):
-
-    class Meta:
-        fields = '__all__'
-        model = models.CronJob
-        widgets = {
-            'cron_command': forms.widgets.TextInput(),
-            'cron_minute': CronMultiple(
-                attrs={'numChoices': 60, 'label': _("minute")}
-            ),
-            'cron_hour': CronMultiple(
-                attrs={'numChoices': 24, 'label': _("hour")}
-            ),
-            'cron_daymonth': CronMultiple(
-                attrs={
-                    'numChoices': 31, 'start': 1, 'label': _("day of month"),
-                }
-            ),
-            'cron_dayweek': forms.CheckboxSelectMultiple(
-                choices=choices.WEEKDAYS_CHOICES
-            ),
-            'cron_month': forms.CheckboxSelectMultiple(
-                choices=choices.MONTHS_CHOICES
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(CronJobForm, self).__init__(*args, **kwargs)
-        mchoicefield(self, 'cron_month', [
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        ])
-        mchoicefield(self, 'cron_dayweek', [
-                1, 2, 3, 4, 5, 6, 7
-        ])
-
-    def clean_cron_user(self):
-        user = self.cleaned_data.get("cron_user")
-        # See #1061 or FreeBSD PR 162976
-        if len(user) > 17:
-            raise forms.ValidationError(_(
-                "Usernames cannot exceed 17 characters for cronjobs"
-            ))
-        # Windows users can have spaces in their usernames
-        # http://www.freebsd.org/cgi/query-pr.cgi?pr=164808
-        if ' ' in user:
-            raise forms.ValidationError("Usernames cannot have spaces")
-        return user
-
-    def clean_cron_month(self):
-        m = self.data.getlist("cron_month")
-        if len(m) == 12:
-            return '*'
-        m = ",".join(m)
-        return m
-
-    def clean_cron_dayweek(self):
-        w = self.data.getlist('cron_dayweek')
-        if w == '*':
-            return w
-        if len(w) == 7:
-            return '*'
-        w = ",".join(w)
-        return w
-
-    def save(self):
-        super(CronJobForm, self).save()
-        notifier().restart("cron")
-
-
-class RsyncForm(ModelForm):
-
-    class Meta:
-        fields = '__all__'
-        model = models.Rsync
-        widgets = {
-            'rsync_minute': CronMultiple(
-                attrs={'numChoices': 60, 'label': _("minute")}
-            ),
-            'rsync_hour': CronMultiple(
-                attrs={'numChoices': 24, 'label': _("hour")}
-            ),
-            'rsync_daymonth': CronMultiple(
-                attrs={
-                    'numChoices': 31, 'start': 1, 'label': _("day of month"),
-                }
-            ),
-            'rsync_dayweek': forms.CheckboxSelectMultiple(
-                choices=choices.WEEKDAYS_CHOICES
-            ),
-            'rsync_month': forms.CheckboxSelectMultiple(
-                choices=choices.MONTHS_CHOICES
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(RsyncForm, self).__init__(*args, **kwargs)
-        mchoicefield(self, 'rsync_month', [
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        ])
-        mchoicefield(self, 'rsync_dayweek', [
-                1, 2, 3, 4, 5, 6, 7
-        ])
-        self.fields['rsync_mode'].widget.attrs['onChange'] = (
-            "rsyncModeToggle();"
-        )
-
-    def clean_rsync_user(self):
-        user = self.cleaned_data.get("rsync_user")
-        # See #1061 or FreeBSD PR 162976
-        if len(user) > 17:
-            raise forms.ValidationError(_(
-                "Usernames cannot exceed 17 characters for rsync tasks"
-            ))
-        # Windows users can have spaces in their usernames
-        # http://www.freebsd.org/cgi/query-pr.cgi?pr=164808
-        if ' ' in user:
-            raise forms.ValidationError(_("Usernames cannot have spaces"))
-        return user
-
-    def clean_rsync_remotemodule(self):
-        mode = self.cleaned_data.get("rsync_mode")
-        val = self.cleaned_data.get("rsync_remotemodule")
-        if mode == 'module' and not val:
-            raise forms.ValidationError(_("This field is required"))
-        return val
-
-    def clean_rsync_remotepath(self):
-        mode = self.cleaned_data.get("rsync_mode")
-        val = self.cleaned_data.get("rsync_remotepath")
-        if mode == 'ssh' and not val:
-            raise forms.ValidationError(_("This field is required"))
-        return val
-
-    def clean_rsync_month(self):
-        m = self.data.getlist("rsync_month")
-        if len(m) == 12:
-            return '*'
-        m = ",".join(m)
-        return m
-
-    def clean_rsync_dayweek(self):
-        w = self.data.getlist("rsync_dayweek")
-        if len(w) == 7:
-            return '*'
-        w = ",".join(w)
-        return w
-
-    def clean_rsync_extra(self):
-        extra = self.cleaned_data.get("rsync_extra")
-        if extra:
-            extra = extra.replace('\n', ' ')
-        return extra
-
-    def clean(self):
-        cdata = self.cleaned_data
-        mode = cdata.get("rsync_mode")
-        user = cdata.get("rsync_user")
-        if mode == 'ssh':
-            try:
-                home = pwd.getpwnam(user).pw_dir
-                search = os.path.join(home, ".ssh", "id_[edr]*.*")
-                if not glob.glob(search):
-                    raise ValueError
-            except (KeyError, ValueError, AttributeError, TypeError):
-                self._errors['rsync_user'] = self.error_class([
-                    _("In order to use rsync over SSH you need a user<br />"
-                      "with a public key (DSA/ECDSA/RSA) set up in home dir."),
-                ])
-                cdata.pop('rsync_user', None)
-        return cdata
-
-    def save(self):
-        super(RsyncForm, self).save()
-        notifier().restart("cron")
 
 """
 TODO: Move to a unittest .py file.
@@ -1040,33 +758,6 @@ class TunableForm(ModelForm):
             notifier().reload("sysctl")
 
 
-class InitShutdownForm(ModelForm):
-
-    class Meta:
-        fields = '__all__'
-        model = models.InitShutdown
-
-    def __init__(self, *args, **kwargs):
-        super(InitShutdownForm, self).__init__(*args, **kwargs)
-        self.fields['ini_type'].widget.attrs['onChange'] = (
-            "initshutdownModeToggle();"
-        )
-
-    def clean_ini_command(self):
-        _type = self.cleaned_data.get("ini_type")
-        val = self.cleaned_data.get("ini_command")
-        if _type == 'command' and not val:
-            raise forms.ValidationError(_("This field is required"))
-        return val
-
-    def clean_ini_script(self):
-        _type = self.cleaned_data.get("ini_type")
-        val = self.cleaned_data.get("ini_script")
-        if _type == 'script' and not val:
-            raise forms.ValidationError(_("This field is required"))
-        return val
-
-
 class RegistrationForm(ModelForm):
 
     class Meta:
@@ -1110,7 +801,6 @@ class SystemDatasetForm(ModelForm):
     class Meta:
         fields = '__all__'
         model = models.SystemDataset
-
 
     def __init__(self, *args, **kwargs):
         super(SystemDatasetForm, self).__init__(*args, **kwargs)
