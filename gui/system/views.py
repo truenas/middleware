45558f9cde62e2f92b49a577e515771e7f5de765
William Grzybowski <wg@FreeBSD.org>
2014-07-07 14:12:34 -0300

    Use StreamingHttpResponse to serve the config file
    Ticket:	#5385
    (cherry picked from commit 3f59e71f8561b128667f560a33b55da463018509)

--- gui/system/views.py
+++ gui/system/views.py
@@ -41,7 +41,11 @@ from django.contrib.auth import login, get_backends
 from django.core.servers.basehttp import FileWrapper
 from django.core.urlresolvers import reverse
 from django.db import transaction
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import (
+    HttpResponse,
+    HttpResponseRedirect,
+    StreamingHttpResponse,
+)
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext as _
@@ -176,7 +180,9 @@ def config_save(request):
     except:
         pass
 
-    response = HttpResponse(wrapper, content_type='application/octet-stream')
+    response = StreamingHttpResponse(
+        wrapper, content_type='application/octet-stream'
+    )
     response['Content-Length'] = os.path.getsize(filename)
     response['Content-Disposition'] = \
         'attachment; filename="%s-%s-%s.db"' % (
