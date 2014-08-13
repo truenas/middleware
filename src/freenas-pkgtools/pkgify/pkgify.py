62fcf66d56a0391bce9f9aea1f847ff75946fef7
Sean Fagan <sef@ixsystems.com>
2014-08-13 16:02:39 -0700

    Rewrite based on current auto-categorization.  And improve based on how create_package does the job.

--- src/freenas-pkgtools/pkgify/pkgify.py
+++ src/freenas-pkgtools/pkgify/pkgify.py
@@ -36,11 +36,15 @@ import hashlib
 import json
 import tarfile
 import StringIO
+import ConfigParser
 
 CAT_KEY = "category"
 TYPE_KEY = "type"
 TYPE_FILE = ["file", "link", "hlink"]
 TYPE_DIR = ["dir"]
+
+debug = 0
+verbose = False
 
 def ParseLine(line, root = None):
     elems = line.split(" ")
@@ -86,7 +90,12 @@ def ParseLine(line, root = None):
 def ChecksumFile(root, path):
     full_path = root + "/" + path
     if os.path.islink(full_path):
-        return "-"
+        # Symlinks have their leading / (if any) removed
+        link_target = os.readlink(full_path)
+        if link_target[0] == "/":
+            return hashlib.sha256(link_target[1:]).hexdigest()
+        else:
+            return hashlib.sha256(link_target).hexdigest()
     elif os.path.isfile(full_path):
         with open(full_path, "r") as f:
             retval = hashlib.sha256(f.read()).hexdigest()
@@ -98,8 +107,7 @@ def usage():
     print >> sys.stderr, "Usage: %s [-p pkg[,pkg...]] [-t file] [-N name] [-V version] [-O origin]" \
         "[-M maintainer] [-D description] [-a] [-o dir] [-u] root [metalog]" % sys.argv[0]
     print >> sys.stderr, "\t-t\ttemplate file"
-    print >> sys.stderr, "\t-p\tCategories/Packages to include (e.g., base, dev, kernel)"
-    print >> sys.stderr, "\t-a\tInclude all sub packages (e.g., base includes base:doc)"
+    print >> sys.stderr, "\t-p\tCategories/Packages to include (e.g., base, dev, kernel, crypto:ALL)"
     print >> sys.stderr, "\t-o\tOutput location"
     print >> sys.stderr, "\t-u\tInclude uncategorized entries"
     print >> sys.stderr, "\t-l\tList categories in metafile, and exit."
@@ -111,61 +119,216 @@ def usage():
     print >> sys.stderr, "\t-O origin\tPackage origin (e.g., system/os)"
     sys.exit(1)
 
-def LoadTemplate(m, path):
+def TemplateFiles(path):
     """
-    Given a path -- which may be a file or a directory -- load manifest fields
-    from it.
-    If it's a file, then we just treat it as a shlex
+    Load a ConfigParser file as a configuration file.
+    Look for a section labeled "Files", "exclude" and
+    "include" otions.  Return both in a dictionary; the
+    value is an array of files to include or exclude.
+    (The files may be paths, or shell-style globs.)
     """
-    import ConfigParser
-    conf = ConfigParser.ConfigParser()
-    isdir = False
 
-    scripts = None
-    if os.path.isfile(path):
-        conf.read(path)
-    elif os.path.isdir(path) and os.path.isfile(path + "/settings.cfg"):
-        conf.read(path + "/settings.cfg")
-        isdir = True
+    rv = {}
+    includes = []
+    excludes = []
+    if os.path.exists(path) == False:
+        return None
+    if os.path.isdir(path):
+        base_dir = path
+        cfg_file = path + "/config"
     else:
-        print >> sys.stderr, "Cannot handle %s (perhaps it doesn't exist)" % path
-        return m
+        base_dir = os.path.dirname(path)
+        cfg_file = path
 
-    # Look for "Settings"
-    for key, val in conf.items("Settings"):
-        # Don't over-ride anything set via CLI options
-        if key not in m or m[key] is None:
-            m[key] = val
+    cfp = ConfigParser.ConfigParser()
+    try:
+        cfp.read(cfg_file)
+    except:
+        return None
 
-    # Look for scripts
-    # Scripts can only be set via template file
-    if conf.has_section("Scripts"):
-        scripts = {}
-        for key, val in conf.items("Scripts"):
-            scripts[key] = val
-    elif isdir:
-        # If the path was a directory, then look for scripts in it
-        scriptnames = {
-            "pre-upgrade" : "+PRE_UPGRADE",
-            "upgrade" : "+UPGRADE",
-            "post-upgrade" : "+POST_UPGRADE",
-            "pre-install" : "+PRE_INSTALL",
-            "install" : "+INSTALL",
-            "post-install" : "+POST_INSTALL",
-            "pre-deinstall" : "+PRE_DEINSTALL",
-            "deinstall" : "+DEINSTALL",
-            "post-deinstall" : "+POST_DEINSTALL",
-            }
-        for key in scriptnames.keys():
-            if os.path.isfile(path + "/" + scriptnames[key]):
-                with open(path + "/" + scriptnames[key]) as f:
-                    scripts[key] = f.read()
-    if scripts is not None:
-        m["scripts"] = scripts
-    return m
+    if cfp.has_option("Files", "exclude"):
+        opt = cfp.get("Files", "exclude")
+        for f in opt.split():
+            excludes.append(f)
+
+    if cfp.has_option("Files", "include"):
+        opt = cfp.get("Files", "include")
+        for f in opt.split():
+            includes.append(f)
+
+    rv["include"] = includes
+    rv["exclude"] = excludes
+    return rv
+
+def LoadTemplate(path):
+    """
+    Load a ConfigParser file as a template.
+    The "Package" section has various defaults for the
+    PKGNG-like manifest; other sections have other values
+    of interest.
+    If path is a directory, then the configuration file
+    will be path + "/config".
+    For scripts, "file:" indicates a filename to read;
+    if path is a directory, it will be relative to that
+    directory; otherwise, it will be relative to the directory
+    contaning path.  (That is, "/tmp/pkg.cfg" will result in
+    "file:+INSTALL" looking for /tmp/+INSTALL.)
+    Returns a dictionary, which may be empty.  If path does
+    not exist, it raises an exception.
+    """
+    rv = {}
+    if os.path.exists(path) == False:
+	raise Exception("%s does not exist" % path)
+    if os.path.isdir(path):
+	base_dir = path
+	cfg_file = path + "/config"
+    else:
+	base_dir = os.path.dirname(path)
+	cfg_file = path
+
+    cfp = ConfigParser.ConfigParser()
+    try:
+	cfp.read(cfg_file)
+    except:
+	return rv
+
+    if cfp.has_section("Package"):
+	# Get the various manifest 
+	for key in ["name", "www", "arch", "maintainer",
+		"comment", "origin", "prefix", "licenslogic",
+		"licenses", "desc"]:
+	    if cfp.has_option("Package", key):
+		rv[key] = cfp.get("Package", key)
+
+    if cfp.has_section("Scripts"):
+	if "scripts" not in rv:
+	    rv["scripts"] = {}
+	for opt, val in cfp.items("Scripts"):
+	    if val.startswith("file:"):
+		# Skip over the file: part
+		fname = val[5:]
+		if fname.startswith("/") == False:
+		    fname = base_dir + "/" + fname
+		with open(fname, "r") as f:
+		    rv["scripts"][opt] = f.read()
+	    else:
+		rv["scripts"][opt] = val
+
+    return rv
     
+def CategoryIsWanted(file_categories, desired_categories):
+    """
+    Return a boolean indicating whether the file (based
+    on file_categories) is desired (based on desired_categories).
+    Both file_categories and desired_categories are arrays of
+    strings; the strings look like "name" or "name:subname".
+    Alternately, "ALL" means everything (including subcategories),
+    and "name:ALL" means all name:subname (including "name").
+    e.g.:  CategoryIsWanted("base:doc", ["base", "kernel"]) would
+    return False, while CategoryIsWanted("base:doc", ["base", "base:doc"])
+    would return True
+    """
+    # First, go through the categories for the file
+    for file_entry in file_categories:
+        if ":" in file_entry:
+            (cat, subcat) = file_entry.split(":")
+        else:
+            cat = file_entry
+            subcat = None
+        # Next, go through the desired categories
+        for desired_entry in desired_categories:
+            if ":" in desired_entry:
+                (desired_cat, desired_subcat) = desired_entry.split(":")
+            else:
+                desired_cat = desired_entry
+                desired_subcat = None
+
+            # If desired_cat is "ALL" and desired_subcat is None or ALL then we want it regardless
+            if desired_cat == "ALL":
+                if desired_subcat == "ALL" or desired_subcat is None:
+                    return True
+                if subcat is desired_subcat:
+                    return True
+
+            if cat == desired_cat:
+                # If it's the same category, check the subcategory
+                if subcat == desired_subcat:
+                    return True
+                if desired_subcat == "ALL":
+                    return True
+    return False
+
+def FilterFunc(path, include_list, exclude_list):
+    """
+    Return a boolean indicating whether the path in question
+    should be filtered out.  This is a bit tricky, unfortunately,
+    but we'll start out simple.
+    Returns True if it should be filtered out, False if not.
+    There are four cases to worry about:
+    1.  No include_list or exclude_list
+    2.  include_list but no exclude_list
+    3.  exclude_list but no include_list
+    4.  Both include_list and exclude_list
+    For (1), return value is always False (never filter out).
+    For (2), return value is False if it is in the list, True otherwise.
+    For (3), return value is True if it is in the list, False otherwise.
+    For (4), return value is True if it is in the exclude list,
+    False if it is in the include list, False if it is in neither.
+    """
+    # Set the default return value based on include and exclude lists.
+    if include_list is None and exclude_list is None:
+        retval = False
+    elif include_list is not None and exclude_list is None:
+        retval = True
+    elif include_list is None and exclude_list is not None:
+        retval = False
+    else:
+        retval = False
+
+    def matches(path, pattern):
+        import fnmatch
+
+        prefix = ""
+        if path.startswith("./"):
+            prefix = "."
+        if pattern.startswith("/"):
+            tmp = prefix + pattern
+        else:
+            tmp = pattern
+        # First, check to see if the name simply matches
+        if path == tmp:
+            if debug: print >> sys.stderr, "Match: %s" % path
+            return True
+        # Next, check to see if elem is a subset of it
+        if path.startswith(tmp) and \
+           path[len(tmp)] == "/":
+            if debug: print >> sys.stderr, "Match %s as child of %s" % (path, tmp)
+            return True
+        # Now to start using globbing.
+        # fnmatch is awful, but let's try just that
+        # (It's awful because it doesn't handle path boundaries.
+        # Thus, "/usr/*.cfg" matches both "/usr/foo.cfg" and
+        # "/usr/local/etc/django.cfg".)
+        if fnmatch.fnmatch(path, elem):
+            if debug: print >> sys.stderr, "Match: %s as glob match for %s" % (path, tmp)
+            return True
+        return False
+
+    # Include takes precedence over exclude, so we handle
+    # the exclude list first
+    if exclude_list is not None:
+        for elem in exclude_list:
+            if matches(path, elem) == True:
+                retval = True
+                break
+    if include_list is not None:
+        for elem in include_list:
+            if matches(path, elem) == True:
+                retval = False
+
+    return retval
+
 def main():
-    subpackages = False
     categories = []
     verbose = False
     debug = False
@@ -175,12 +338,14 @@ def main():
     list_cats = False
     pkg_dirs = []
     pkg_files = []
-    output_dir = "."
+    output_file = None
     uniq_cats = {}
     root_path = None
     metalog = None
     use_json = True
     template_file = None
+    include_list = None
+    exclude_list = None
 
     manifest = {}
     default_manifest_keys = {
@@ -208,10 +373,8 @@ def main():
             list_cats = True
         elif o == "-t":
             template_file = a
-        elif o == "-a":
-            subpackages = True
         elif o == "-o":
-            output_dir = a
+            output_file = a
         elif o == "-p":
             categories.extend(a.split(","))
         elif o == "-M":
@@ -236,25 +399,38 @@ def main():
             usage()
 
     if template_file is not None:
-        manifest = LoadTemplate(manifest, template_file)
+        tdict = LoadTemplate(template_file)
+        if tdict is not None:
+            for k in tdict.keys():
+                manifest[k] = tdict[k]
+        filters = TemplateFiles(template_file)
+        if filters is not None:
+            if debug > 1:  print >> sys.stderr, "Filter list = %s" % filters
+            if len(filters["include"]) > 0:
+                include_list = filters["include"]
+            if len(filters["exclude"]) > 0:
+                exclude_list = filters["exclude"]
 
     # Now we set any defaults that are left
     for key in default_manifest_keys.keys():
         if key not in manifest:
             manifest[key] = default_manifest_keys[key]
 
-    # And a couple of special case ones
-    if "name" not in manifest:
-        print >> sys.stderr, "Package does not have a name.  Not acceptable"
-        sys.exit(1)
-    if "version" not in manifest:
-        print >> sys.stderr, "Package %s does not have a version.  Not acceptable!" % manifest["name"]
-        sys.exit(1)
+    # And a couple of special case ones (unless "-l" was given)
+    if not list_cats:
+        if "name" not in manifest:
+            print >> sys.stderr, "Package does not have a name.  Not acceptable"
+            sys.exit(1)
+        if "version" not in manifest:
+            print >> sys.stderr, "Package %s does not have a version.  Not acceptable!" % manifest["name"]
+            sys.exit(1)
 
-    if "origin" not in manifest:
-        manifest["origin"] = "system/" + manifest["name"]
+        if "origin" not in manifest:
+            manifest["origin"] = "system/" + manifest["name"]
 
-        
+        if output_file is None:
+            output_file = "%s-%s.tgz" % (manifest["name"], manifest["version"])
+
     # If we have two arguments, check to see which is a directory
     if len(args) > 2 or len(args) == 0:
         usage()
@@ -315,17 +491,13 @@ def main():
         return 0
 
     for fname in system.keys():
+        if FilterFunc(fname, include_list, exclude_list) is True:
+            continue
         wantit = False
         parms = system[fname]
 #        print "parms = %s" % parms
         if CAT_KEY in parms:
-            for file_key in parms[CAT_KEY]:
-                for requested_key in categories:
-                    if ((requested_key == file_key)
-                        or
-                        (":" in file_key and file_key.startswith(requested_key) and subpackages)):
-                        if verbose or debug: print "%s%s" % (fname, "\t#%s, type = %s" % (file_key, parms[TYPE_KEY]) if (debug or verbose) else "")
-                        wantit = True
+            wantit = CategoryIsWanted(parms[CAT_KEY], categories)
         elif uncat:
             if verbose or debug: print "%s%s" % (fname, "\t# uncategorized, type = %s" % (parms[TYPE_KEY]) if (debug or verbose) else "")
             wantit = True
@@ -336,7 +508,7 @@ def main():
             elif parms[TYPE_KEY] in TYPE_DIR:
                 pkg_dirs.append(fname)
     if verbose or debug: print "%d files\n%d directories" % (len(pkg_files), len(pkg_dirs))
-    output_file = "%s/+MANIFEST" % output_dir
+
     # Collect the main keys first
     manifest["files"] = {}
     for (fname, hash) in pkg_files:
@@ -347,14 +519,9 @@ def main():
         manifest["directories"][dname] = "n"
     manifest_string = json.dumps(manifest, sort_keys=True, indent=4, separators=(',', ': '))
 
-    # N.B.
-    # This should be replaced with tarfile usage.
-    with open(output_file, "w") as f:
-        f.write(manifest_string)
-
-    tf = tarfile.open(output_dir + ".tgz", mode = "w:gz", format = tarfile.PAX_FORMAT)
+    tf = tarfile.open(output_file, mode = "w:gz", format = tarfile.PAX_FORMAT)
     if tf is None:
-        print >> sys.stderr, "Cannot create tar file %s" % (output_dir + ".tgz")
+        print >> sys.stderr, "Cannot create tar file %s" % output_file
         sys.exit(1)
 
     metaobj = tarfile.TarInfo(name="+MANIFEST")
