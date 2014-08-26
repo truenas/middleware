c81d18e93a9c429fdcf4053094a848220dc95b33
John Hixson <john@ixsystems.com>
2014-08-26 09:37:02 -0700

    Show proper columns for Certificates

--- gui/system/admin.py
+++ gui/system/admin.py
@@ -75,5 +75,48 @@ class CertificateAuthorityFAdmin(BaseFreeAdmin):
         return columns
 
 
+class CertificateFAdmin(BaseFreeAdmin):
+
+    create_modelform = "CertificateForm"
+    edit_modelform = "CertificateForm"
+    icon_object = u"SettingsIcon"
+    icon_model = u"SettingsIcon"
+    icon_add = u"SettingsIcon"
+    icon_view = u"SettingsIcon"
+
+    resource_mixin = CertificateAuthorityResourceMixin
+
+    def get_datagrid_columns(self):
+        columns = []
+
+        columns.append({
+            'name': 'cert_name',
+            'label': _('Name') 
+        }) 
+
+        columns.append({
+            'name': 'cert_issuer',
+            'label': _('Issuer') 
+        }) 
+
+        columns.append({
+            'name': 'cert_DN',
+            'label': _('Distinguished Name') 
+        }) 
+
+        columns.append({
+            'name': 'cert_from',
+            'label': _('From') 
+        }) 
+
+        columns.append({
+            'name': 'cert_until',
+            'label': _('Until') 
+        }) 
+
+        return columns
+
+
 site.register(models.Settings, SettingsFAdmin)
 site.register(models.CertificateAuthority, CertificateAuthorityFAdmin)
+site.register(models.Certificate, CertificateFAdmin)
