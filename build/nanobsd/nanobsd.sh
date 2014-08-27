870fc74a982cddd4027cb619dafd322ef6889545
William Grzybowski <wg@FreeBSD.org>
2014-08-27 11:02:07 -0700

    Sean says -f should do the job here

--- build/nanobsd/nanobsd.sh
+++ build/nanobsd/nanobsd.sh
@@ -188,7 +188,7 @@ if [ -f ${TOOLDIR}/usr/local/bin/create_package ]; then
 	seq_arg=""
     fi
     env PYTHONPATH="${TOOLDIR}/usr/local/lib" ${TOOLDIR}/usr/local/bin/create_manifest -P ${NANO_OBJ}/_.packages -o ${NANO_OBJ}/_.packages/FreeNAS-${SEQUENCE:-0} -R FreeNAS-${VERSION} ${seq_arg} -T ${TRAIN:-FreeNAS} freenas=${VERSION}-${REVISION:-0} freenasUI=${VERSION}-${REVISION:-0} freenas-pkg-tools=${VERSION}-${REVISION:-0}
-    ln -s FreeNAS-${SEQUENCE:-0} ${NANO_OBJ}/_.packages/FreeNAS-MANIFEST
+    ln -fs FreeNAS-${SEQUENCE:-0} ${NANO_OBJ}/_.packages/FreeNAS-MANIFEST
 else
     echo "What happened to the tools?!?!?!"
     false
