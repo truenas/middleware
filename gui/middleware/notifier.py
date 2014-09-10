b428bbe460691cf6e97209ab6ae7c50d850c105a
William Grzybowski <wg@FreeBSD.org>
2014-09-10 17:57:38 -0300

    Fix name of snapshot to check on remote side
    (cherry picked from commit 310703bc28ddbd9038d200a11826a39f6c1d2b96)

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -3581,11 +3581,10 @@ class notifier:
                 replication = None
                 if replications:
                     for repl, snaps in replications.iteritems():
+                        if fs != repl.repl_filesystem:
+                            break
                         remotename = '%s@%s' % (
-                            fs.replace(
-                                repl.repl_filesystem + '@',
-                                repl.repl_zfs + '@',
-                            ),
+                            repl.repl_zfs,
                             name,
                         )
                         if remotename in snaps:
