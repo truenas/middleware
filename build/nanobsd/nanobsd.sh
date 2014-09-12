3c29ec8f7271676c8f08e9c3a0aa8a63f383335b
William Grzybowski <wg@FreeBSD.org>
2014-09-12 11:06:24 -0300

    One more missing FreeNAS -> ${NANO_LABEL} to fix TN build

--- build/nanobsd/nanobsd.sh
+++ build/nanobsd/nanobsd.sh
@@ -188,7 +188,7 @@ if [ -f ${TOOLDIR}/usr/local/bin/create_package ]; then
 	seq_arg=""
     fi
     env PYTHONPATH="${TOOLDIR}/usr/local/lib" ${TOOLDIR}/usr/local/bin/create_manifest -P ${NANO_OBJ}/_.packages/Packages -o ${NANO_OBJ}/_.packages/FreeNAS-${SEQUENCE:-0} -R FreeNAS-${VERSION} ${seq_arg} -T ${TRAIN:-FreeNAS} base-os=${VERSION}-${REVISION:-0} ${NANO_LABEL}UI=${VERSION}-${REVISION:-0} freenas-pkg-tools=${VERSION}-${REVISION:-0}
-    ln -sf FreeNAS-${SEQUENCE:-0} ${NANO_OBJ}/_.packages/FreeNAS-MANIFEST
+    ln -sf FreeNAS-${SEQUENCE:-0} ${NANO_OBJ}/_.packages/${NANO_LABEL}-MANIFEST
 else
     echo "What happened to the tools?!?!?!"
     false
