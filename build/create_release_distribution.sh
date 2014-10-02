450f0fb4ad3a4dbd7c9158dcad5a64fbcd03cee1
Suraj Ravichandran <suraj@ixsystems.com>
2014-10-01 21:37:51 -0700

    Adding a JSON file with information about builds to the release script

--- build/create_release_distribution.sh
+++ build/create_release_distribution.sh
@@ -4,6 +4,29 @@
 # directory format.
 #
 
+# Creating a json generating function
+create_json()
+{
+  local filename=${1}
+  local archbit=${2}
+  local ftype=${3}
+  local hash=${4}
+  local url="http://download.freenas.org/nightlies/$(echo ${VERSION} | sed 's/ *-.*//')/$(echo ${VERSION} | sed -n -e 's/^.*-//p')/${BUILD_TIMESTAMP}/${archbit}/${NANO_NAME%-*}-${archbit}.${ftype}"
+  
+  cat<<-__EOF__>>${filename}.json
+{
+    "name": "${NANO_LABEL}",
+    "version": "${VERSION}",
+    "arch": "${archbit}",
+    "install_type": "${ftype}",
+    "hash": "${hash}",
+    "date": "${BUILD_TIMESTAMP}",
+    "url": "${url}"
+}
+__EOF__
+
+}
+
 cd "$(dirname "$0")/.."
 TOP="$(pwd)"
 
@@ -22,8 +45,10 @@ arch=x64
 mapped_arch=amd64
 mkdir -p ${TOP}/${OBJ}/${STAGEDIR}/${arch}
 for ext in GUI_Upgrade.txz iso; do
-	if [ -f ${TOP}/${OBJ}/os-base/$mapped_arch/${NANO_NAME%-*}-${arch}.${ext} ]; then
-		ln ${TOP}/${OBJ}/os-base/$mapped_arch/${NANO_NAME%-*}-${arch}.${ext} ${TOP}/${OBJ}/${STAGEDIR}/${arch}
-		ln ${TOP}/${OBJ}/os-base/$mapped_arch/${NANO_NAME%-*}-${arch}.${ext}.sha256.txt ${TOP}/${OBJ}/${STAGEDIR}/${arch}
+	tfile=${TOP}/${OBJ}/os-base/$mapped_arch/${NANO_NAME%-*}-${arch}.${ext}
+	if [ -f ${tfile} ]; then
+		ln ${tfile} ${TOP}/${OBJ}/${STAGEDIR}/${arch}
+		ln ${tfile}.sha256.txt ${TOP}/${OBJ}/${STAGEDIR}/${arch}
+		create_json ${TOP}/${OBJ}/${STAGEDIR}/${arch}/${NANO_NAME%-*}-${arch}.${ext} ${arch} ${ext} $(cat ${tfile}.sha256.txt | sed -n -e 's/^.*= //p') 
 	fi
 done
