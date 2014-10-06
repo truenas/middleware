ccf10efda1f00e5aa0a8f3ebb810011ae4279210
William Grzybowski <wg@FreeBSD.org>
2014-10-06 08:48:19 -0300

    Remove unused fields in the Upgrade model/form

--- gui/system/models.py
+++ gui/system/models.py
@@ -500,18 +500,9 @@ class SystemDataset(Model):
 
 
 class Upgrade(Model):
-    upd_train = models.CharField(
-        editable=False,
-        max_length=200,
-        default='stable',
-    )
     upd_autocheck = models.BooleanField(
         verbose_name=_('Check Automatically For Updates'),
         default=True,
-    )
-    upd_location = models.URLField(
-        verbose_name=_('Location'),
-        default='http://download.freenas.org/FreeNAS/',
     )
 
     class Meta:
