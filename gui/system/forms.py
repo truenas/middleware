ddd1b95c8494e460598d89ea5115e9d4abf44213
Josh Paetzel <josh@ixsystems.com>
2014-10-11 23:26:49 -0700

    Regenerate loader.conf.local when serial port address or speed is changed.
    Ticket: #6340
    (cherry picked from commit f30e1db3b19bcc0e9928d3f502ee353439e5a5eb)

--- gui/system/forms.py
+++ gui/system/forms.py
@@ -339,6 +339,8 @@ class AdvancedForm(ModelForm):
         self.instance._original_adv_serialconsole = (
             self.instance.adv_serialconsole
         )
+        self.instance._original_adv_serialspeed = self.instance.adv_serialspeed
+        self.instance._original_adv_serialport = self.instance.adv_seriaport
         self.instance._original_adv_consolescreensaver = (
             self.instance.adv_consolescreensaver
         )
@@ -361,6 +363,11 @@ class AdvancedForm(ModelForm):
         if self.instance._original_adv_serialconsole != self.instance.adv_serialconsole:
             notifier().start("ttys")
             notifier().start("ix-sercons")
+            if not loader_reloaded:
+                notifier().reload("loader")
+                loader_reloaded = True
+        elif (self.instance._original_adv_serialspeed != self.instance.adv_serialspeed or
+                self.instance._original_adv_serialport != self.instance.adv_seriaport):
             if not loader_reloaded:
                 notifier().reload("loader")
                 loader_reloaded = True
