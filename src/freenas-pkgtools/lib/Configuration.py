aa905a2bc27eb73bdf0289b5e7f97c6ff91b6813
Sean Fagan <sef@ixsystems.com>
2014-11-06 13:43:04 -0800

    Start putting in support for stricter https.

--- src/freenas-pkgtools/lib/Configuration.py
+++ src/freenas-pkgtools/lib/Configuration.py
@@ -6,6 +6,9 @@ import sys
 import tempfile
 import time
 import urllib2
+import httplib
+import socket
+import ssl
 
 from . import Avatar, UPDATE_SERVER
 import Exceptions
@@ -55,6 +58,86 @@ def TryOpenFile(path):
         return None
     else:
         return f
+
+# This code taken from
+# http://stackoverflow.com/questions/1087227/validate-ssl-certificates-with-python
+
+class InvalidCertificateException(httplib.HTTPException, urllib2.URLError):
+    def __init__(self, host, cert, reason):
+        httplib.HTTPException.__init__(self)
+        self.host = host
+        self.cert = cert
+        self.reason = reason
+
+    def __str__(self):
+        return ('Host %s returned an invalid certificate (%s) %s\n' %
+                (self.host, self.reason, self.cert))
+
+class CertValidatingHTTPSConnection(httplib.HTTPConnection):
+    default_port = httplib.HTTPS_PORT
+
+    def __init__(self, host, port=None, key_file=None, cert_file=None,
+                             ca_certs=None, strict=None, **kwargs):
+        httplib.HTTPConnection.__init__(self, host, port, strict, **kwargs)
+        self.key_file = key_file
+        self.cert_file = cert_file
+        self.ca_certs = ca_certs
+        if self.ca_certs:
+            self.cert_reqs = ssl.CERT_REQUIRED
+        else:
+            self.cert_reqs = ssl.CERT_NONE
+
+    def _GetValidHostsForCert(self, cert):
+        if 'subjectAltName' in cert:
+            return [x[1] for x in cert['subjectAltName']
+                         if x[0].lower() == 'dns']
+        else:
+            return [x[0][1] for x in cert['subject']
+                            if x[0][0].lower() == 'commonname']
+
+    def _ValidateCertificateHostname(self, cert, hostname):
+        hosts = self._GetValidHostsForCert(cert)
+        for host in hosts:
+            host_re = host.replace('.', '\.').replace('*', '[^.]*')
+            if re.search('^%s$' % (host_re,), hostname, re.I):
+                return True
+        return False
+
+    def connect(self):
+        sock = socket.create_connection((self.host, self.port))
+        self.sock = ssl.wrap_socket(sock, keyfile=self.key_file,
+                                          certfile=self.cert_file,
+                                          cert_reqs=self.cert_reqs,
+                                          ca_certs=self.ca_certs)
+        if self.cert_reqs & ssl.CERT_REQUIRED:
+            cert = self.sock.getpeercert()
+            hostname = self.host.split(':', 0)[0]
+            if not self._ValidateCertificateHostname(cert, hostname):
+                raise InvalidCertificateException(hostname, cert,
+                                                  'hostname mismatch')
+
+
+class VerifiedHTTPSHandler(urllib2.HTTPSHandler):
+    def __init__(self, **kwargs):
+        urllib2.AbstractHTTPHandler.__init__(self)
+        self._connection_args = kwargs
+
+    def https_open(self, req):
+        def http_class_wrapper(host, **kwargs):
+            full_kwargs = dict(self._connection_args)
+            full_kwargs.update(kwargs)
+            return CertValidatingHTTPSConnection(host, **full_kwargs)
+
+        try:
+            return self.do_open(http_class_wrapper, req)
+        except urllib2.URLError, e:
+            if type(e.reason) == ssl.SSLError and e.reason.args[0] == 1:
+                raise InvalidCertificateException(req.host, '',
+                                                  e.reason.args[1])
+            raise
+
+    https_request = urllib2.HTTPSHandler.do_request_
+
 
 class PackageDB:
 #    DB_NAME = "var/db/ix/freenas-db"
@@ -378,11 +461,13 @@ class Configuration(object):
         if temp_mani:
             current_version = temp_mani.Sequence()
         try:
+            handler = VerifiedHTTPSHandler(ca_certs = ROOT_CA_FILE)
+            opener = urlib2.build_opener(handler)
             req = urllib2.Request(url)
             req.add_header(AVATAR_VERSION, current_version)
             # Hack for debugging
             req.add_header("User-Agent", "%s=%s" % (AVATAR_VERSION, current_version))
-            furl = urllib2.urlopen(req, timeout=5)
+            furl = opener.open(req, timeout=5)
         except:
             log.warn("Unable to load %s", url)
             return None
