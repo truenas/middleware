ba47bdf03e2ef09fdf34d9dfac27df781ac81d09
Corey Vixie <corey.vixie@gmail.com>
2014-11-07 16:20:07 -0800

    Changed some minor structural variables to correct display of innermost content area.

--- src/gui/app/jsx/views/FreeNASWebApp.js
+++ src/gui/app/jsx/views/FreeNASWebApp.js
@@ -19,31 +19,27 @@ var FreeNASWebApp = React.createClass({
   render: function() {
     return (
       <div>
-      <div className="navBar">Notification Bar will live here!</div>
+      <div className="notificationBar">Notification Bar will live here!</div>
       <div className="leftMenu">
         <ul>
           <li><Link to="dashboard" className="ico-dashboard">Dashboard</Link></li>
           <li><Link to="accounts" className="ico-accounts ">Accounts</Link></li>
-          <li><Link to="tasks" className="ico-tasks ">Tasks</Link></li>          
+          <li><Link to="tasks" className="ico-tasks ">Tasks</Link></li>
           <li><Link to="network" className="ico-network ">Network</Link></li>
           <li><Link to="storage" className="ico-storage ">Storage</Link></li>
-          <li><Link to="sharing" className="ico-sharing ">Sharing</Link></li>                    
-          <li><Link to="services" className="ico-services">Services</Link></li>          
+          <li><Link to="sharing" className="ico-sharing ">Sharing</Link></li>
+          <li><Link to="services" className="ico-services">Services</Link></li>
           <li><Link to="system-tools" className="ico-system-tools ">System Tools</Link></li>
           <li><Link to="control-panel" className="ico-control-panel">Control Panel</Link></li>
           <li><Link to="power" className="ico-power ">Power</Link></li>
         </ul>
       </div>
-      <TWBS.Grid className="mainGrid">
+      <TWBS.Grid fluid className="mainGrid">
         {/* TODO: Add Modal mount div */}
         <TWBS.Row>
-          {/* Navigation side menu */}
-          <TWBS.Col xs={2} sm={2} md={2} lg={2} xl={2}>
-
-          </TWBS.Col>
-
           {/* Primary view */}
-          <TWBS.Col xs={8} sm={8} md={8} lg={8} xl={8}>
+          <TWBS.Col xs={9} sm={9} md={9} lg={9} xl={9}
+                    xsOffset={1} smOffset={1} mdOffset={1} lgOffset={1} xlOffset={1}>
             <h1>FreeNAS WebGUI</h1>
             { this.props.activeRouteHandler() }
           </TWBS.Col>
