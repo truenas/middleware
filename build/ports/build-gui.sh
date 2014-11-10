f2e19d66160adbef560b70af7a11b97e8bd28a6a
Jakub Klama <jakub@ixsystems.com>
2014-11-09 20:30:34 -0800

    Workaround for retarded npm packages ignoring $CC.

--- build/ports/build-gui.sh
+++ build/ports/build-gui.sh
@@ -8,18 +8,68 @@ umask 022
 . build/repos.sh
 . build/ports/ports_funcs.sh
 
+set -e
+
 NANO_GUI=${NANO_OBJ}/gui
 NANO_GUI_DEST=${NANO_OBJ}/gui-dest
 NANO_GUI_PLIST=${NANO_GUI_DEST}/gui-plist
 NANO_NPM_MODULES=${NANO_GUI}/node_modules
+CUSTOM_PLIST=${NANO_GUI}/custom-plist
 BOWER=${NANO_NPM_MODULES}/bower/bin/bower
 GRUNT=${NANO_NPM_MODULES}/grunt-cli/bin/grunt
 NPM=npm
 
-mkdir -p ${NANO_GUI} && cd ${NANO_GUI}
+create_plist()
+{
+	cd ${NANO_GUI_DEST}
+	find . -type f | sed 's/^\./\/usr\/local\/www\/gui/g'
+	cat ${CUSTOM_PLIST}
+}
+
+gplusplus_version()
+{
+	ls -1 /usr/local/bin/g++?? | head -1
+}
+
+apply_npm_quirks()
+{
+	if [ ! -L /usr/local/bin/g++ ]; then
+		ln -s `gplusplus_version` /usr/local/bin/g++
+		quirks="$quirks /usr/local/bin/g++"
+	fi
+
+	if [ ! -L /usr/local/bin/c++ ]; then
+		ln -s `gplusplus_version` /usr/local/bin/c++
+		quirks="$quirks /usr/local/bin/c++"
+	fi
+}
+
+remove_npm_quirks()
+{
+	if [ -n "${quirks}" ]; then
+		rm ${quirks}
+	fi
+}
+
+# Clean up the staging and deployment directories
+mkdir -p ${NANO_GUI} ${NANO_GUI_DEST}
+rm -rf ${NANO_GUI}/* ${NANO_GUI_DEST}/*
+
+# Copy over gui src to staging directory
+cd ${NANO_GUI}
 cp -a ${AVATAR_ROOT}/src/gui/ ${NANO_GUI}
+
+# Symlink g++ because npm modules tend to completely ignore CC
+apply_npm_quirks
+
+# Do the deployment
 ${NPM} install grunt grunt-cli bower
+${NPM} install
 ${BOWER} install --allow-root
 ${GRUNT} deploy --force --dir=${NANO_GUI_DEST}
-cd ${NANO_GUI_DEST} && find . -type f | sed 's/^\./\/usr\/local\/www\/gui/g' > ${NANO_GUI_PLIST}
-echo '/usr/local/etc/rc.d/gui' >> ${NANO_GUI_PLIST}
+
+# Remove g++ symlinks
+remove_npm_quirks
+
+# Create package plist
+create_plist > ${NANO_GUI_PLIST}
