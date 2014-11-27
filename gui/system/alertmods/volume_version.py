62148a7b55249852e48382b8d318c3b4a2be44b8
William Grzybowski <wg@FreeBSD.org>
2014-11-27 12:07:31 -0200

    Use a more descriptive message for upgrading volume
    While I'm here remove deprecated stuff.
    
    Ticket:	#6823

--- gui/system/alertmods/volume_version.py
+++ gui/system/alertmods/volume_version.py
@@ -1,8 +1,5 @@
-import subprocess
-
 from django.utils.translation import ugettext as _
 
-from freenasUI.middleware.notifier import notifier
 from freenasUI.storage.models import Volume
 from freenasUI.system.alert import alertPlugins, Alert, BaseAlert
 
@@ -11,17 +8,14 @@ class VolumeVersionAlert(BaseAlert):
 
     def run(self):
         alerts = []
-        _n = notifier()
         for vol in Volume.objects.filter(vol_fstype='ZFS'):
-            try:
-                version = _n.zpool_version(vol.vol_name)
-            except:
-                continue
-
-            if vol.is_upgraded != True:
+            if vol.is_upgraded is not True:
                 alerts.append(Alert(
-                    Alert.WARN,
-                    _('You need to upgrade the volume %s') % vol.vol_name,
+                    Alert.WARN, _(
+                        'New feature flags are available for volume %s. Refer '
+                        'to the "Upgrading a ZFS Pool" section of the User '
+                        'Guide for instructions.'
+                    ) % vol.vol_name,
                 ))
 
         return alerts
