8908d646294ad616daf3e91edbf565a7d2a6e40d
Jakub Klama <jakub@ixsystems.com>
2014-12-09 21:48:48 +0100

    Log all system commands executed.

--- src/dispatcher/src/lib/system.py
+++ src/dispatcher/src/lib/system.py
@@ -1,6 +1,10 @@
 __author__ = 'jceel'
 
+import logging
 from gevent import subprocess
+
+logger = logging.getLogger('system')
+
 
 class SubprocessException(Exception):
     def __init__(self, code, out, err):
@@ -8,11 +12,15 @@ class SubprocessException(Exception):
         self.out = out
         self.err = err
 
+
 def system(*args):
     proc = subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
     out, err = proc.communicate()
 
+    logger.debug("Running command: %s", ' '.join(args))
+
     if proc.returncode != 0:
+        logger.warning("Command %s failed, return code %d, stderr output: %s", ' '.join(args), proc.returncode, err)
         raise SubprocessException(proc.returncode, out, err)
 
     return out, err
