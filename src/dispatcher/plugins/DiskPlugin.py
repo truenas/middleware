21b7b31c630bff6cdb2091f825f77d8f70d666dc
Jakub Klama <jakub@ixsystems.com>
2014-12-09 21:42:25 +0100

    Implement disk management plugin using a bit of code from FreeNAS 9.

--- src/dispatcher/plugins/DiskPlugin.py
+++ src/dispatcher/plugins/DiskPlugin.py
@@ -26,28 +26,60 @@
 #####################################################################
 
 import os
+import re
 import errno
-from gevent.event import Event
-from lib import zfs
+import gevent
+from collections import defaultdict
+from cache import CacheStore
+from lib import zfs, geom
 from lib.system import system, SubprocessException
-from task import Provider, Task, TaskStatus, TaskException
-from dispatcher.rpc import accepts, returns, description
-from balancer import TaskState
+from task import Provider, Task, TaskStatus, TaskException, VerifyException, query
+from dispatcher.rpc import RpcException, accepts, returns, description
+
+
+diskinfo_cache = CacheStore()
+camcontrol_cache = CacheStore()
 
 
 class DiskProvider(Provider):
+    @query
+    @returns({'$ref': 'definitions/disk'})
     def query(self, filter=None, params=None):
-        pass
+        result = []
+        for disk in self.datastore.query('disks', *(filter or []), **(params or {})):
+            disk['online'] = self.is_online(disk['path'])
+            disk['label-path'] = ''
+            disk['uuid-path'] = ''
+            result.append(disk)
+
+        return result
+
+    def is_online(self, name):
+        return os.path.exists(name)
+
+    def get_config(self, name):
+        if not diskinfo_cache.exists(name):
+            raise RpcException(errno.ENOENT, "Disk {0} not found".format(name))
+
+        return diskinfo_cache.get(name)
 
 
+@accepts(
+    {'type': 'string'},
+    {'type': 'string'},
+    {'type': 'object'}
+)
 class DiskGPTFormatTask(Task):
-    def describe(self, disk, type, swapsize=2048, params=None):
+    def describe(self, disk, fstype, params=None):
         return "Formatting disk {0}".format(os.path.basename(disk))
 
-    def verify(self, disk, params=None):
-        pass
+    def verify(self, disk, fstype, params=None):
+        if fstype not in ['freebsd-zfs']:
+            raise VerifyException(errno.EINVAL, "Unsupported fstype {0}".format(fstype))
 
-    def run(self, disk, typename, params=None):
+        return [os.path.basename(disk)]
+
+    def run(self, disk, fstype, params=None):
         if params is None:
             params = {}
 
@@ -63,18 +95,321 @@ class DiskGPTFormatTask(Task):
 
         try:
             system('gpart', 'create', '-s', 'gpt', disk)
-
             if swapsize > 0:
                 system('gpart', 'add', '-a', str(blocksize), '-b', '128', '-s', swapsize, '-t', 'freebsd-swap', disk)
-                system('gpart', 'add', '-a', str(blocksize), '-t', typename)
+                system('gpart', 'add', '-a', str(blocksize), '-t', fstype, disk)
             else:
-                system('gpart', 'add', '-a', str(blocksize), '-b', '128', '-t', typename)
+                system('gpart', 'add', '-a', str(blocksize), '-b', '128', '-t', fstype, disk)
 
             system('gpart', 'bootcode', '-b', bootcode, disk)
         except SubprocessException, err:
-            raise TaskException(errno.EFAULT, 'Cannot format disk: {0}'.format(str(err)))
+            raise TaskException(errno.EFAULT, 'Cannot format disk: {0}'.format(err.err))
+
+        # Yield to allow firing mediachange event
+        gevent.sleep(0)
+
+
+class DiskEraseTask(Task):
+    def verify(self, disk, erase_data=False):
+        pass
+
+    def run(self, disk, erase_data=False):
+        pass
+
+    def get_status(self, disk):
+        pass
+
+
+@accepts({
+    'allOf': [
+        {'$ref': 'definitions/disk'},
+        {'not': {'required': ['name', 'serial', 'description', 'mediasize']}}
+    ]
+})
+class DiskConfigureTask(Task):
+    def verify(self, name, updated_fields):
+        return [os.path.basename(name)]
+
+    def run(self, name, updated_fields):
+        disk = self.datastore.query('disks', ('name', '=', name))
+
+
+class DiskDeleteTask(Task):
+    def verify(self, name):
+        pass
+
+    def run(self, name):
+        pass
+
+
+def generate_camcontrol_cache():
+    """
+    Parse camcontrol devlist -v output to gather
+    controller id, channel no and driver from a device
+    Returns:
+        dict(devname) = dict(drv, controller, channel)
+
+    Hacky workaround
+    It is known that at least some HPT controller have a bug in the
+    camcontrol devlist output with multiple controllers, all controllers
+    will be presented with the same driver with index 0
+    e.g. two hpt27xx0 instead of hpt27xx0 and hpt27xx1
+    What we do here is increase the controller id by its order of
+    appearance in the camcontrol output
+    """
+    hptctlr = defaultdict(int)
+
+    re_drv_cid = re.compile(r'.* on (?P<drv>.*?)(?P<cid>[0-9]+) bus', re.S | re.M)
+    re_tgt = re.compile(r'target (?P<tgt>[0-9]+) .*?lun (?P<lun>[0-9]+) .*\((?P<dv1>[a-z]+[0-9]+),(?P<dv2>[a-z]+[0-9]+)\)', re.S | re.M)
+    drv, cid, tgt, lun, dev, devtmp = (None, ) * 6
+
+    out, err = system('camcontrol', 'devlist', '-v')
+    for line in out.splitlines():
+        if not line.startswith('<'):
+            reg = re_drv_cid.search(line)
+            if not reg:
+                continue
+            drv = reg.group("drv")
+            if drv.startswith("hpt"):
+                cid = hptctlr[drv]
+                hptctlr[drv] += 1
+            else:
+                cid = reg.group("cid")
+        else:
+            reg = re_tgt.search(line)
+            if not reg:
+                continue
+            tgt = reg.group("tgt")
+            lun = reg.group("lun")
+            dev = reg.group("dv1")
+            devtmp = reg.group("dv2")
+            if dev.startswith("pass"):
+                dev = devtmp
+
+            camcontrol_cache.put(os.path.join('/dev', dev), {
+                'drv': drv,
+                'controller': int(cid),
+                'channel': int(tgt),
+                'lun': int(lun)
+            })
+
+
+def get_twcli(controller):
+    re_port = re.compile(r'^p(?P<port>\d+).*?\bu(?P<unit>\d+)\b', re.S | re.M)
+    output, err = system("/usr/local/sbin/tw_cli", "/c{0}".format(controller), "show")
+
+    units = {}
+    for port, unit in re_port.findall(output):
+        units[int(unit)] = int(port)
+
+    return units
+
+
+def device_to_identifier(doc, name, serial=None):
+    if serial:
+        return "serial:{0}".format(serial)
+
+    search = doc.xpath("//class[name = 'PART']/..//*[name = '{0}']//config[type = 'freebsd-zfs']/rawuuid".format(name))
+    if len(search) > 0:
+        return "uuid:{0}".format(search[0].text)
+
+    search = doc.xpath("//class[name = 'PART']/geom/..//*[name = '{0}']//config[type = 'freebsd-ufs']/rawuuid".format(name))
+    if len(search) > 0:
+        return "uuid:{0}".format(search[0].text)
+
+    search = doc.xpath("//class[name = 'LABEL']/geom[name = '{0}']/provider/name".format(name))
+    if len(search) > 0:
+        return "label:{0}".format(search[0].text)
+
+    search = doc.xpath("//class[name = 'DEV']/geom[name = '{0}']".format(name))
+    if len(search) > 0:
+        return "devicename:{0}".format(name)
+
+    return ''
+
+
+def serial_from_device(devname):
+    args = [devname]
+    info = camcontrol_cache.get(devname)
+    if info is not None:
+        if info.get("drv") == "rr274x_3x":
+            channel = info["channel"] + 1
+            if channel > 16:
+                channel -= 16
+            elif channel > 8:
+                channel -= 8
+            args = [
+                "/dev/%s" % info["drv"],
+                "-d",
+                "hpt,%d/%d" % (info["controller"] + 1, channel)
+            ]
+        elif info.get("drv").startswith("arcmsr"):
+            args = [
+                "/dev/%s%d" % (info["drv"], info["controller"]),
+                "-d",
+                "areca,%d" % (info["lun"] + 1 + (info["channel"] * 8), )
+            ]
+        elif info.get("drv").startswith("hpt"):
+            args = [
+                "/dev/%s" % info["drv"],
+                "-d",
+                "hpt,%d/%d" % (info["controller"] + 1, info["channel"] + 1)
+            ]
+        elif info.get("drv") == "ciss":
+            args = [
+                "/dev/%s%d" % (info["drv"], info["controller"]),
+                "-d",
+                "cciss,%d" % (info["channel"], )
+            ]
+        elif info.get("drv") == "twa":
+            twcli = get_twcli(info["controller"])
+            args = [
+                "/dev/%s%d" % (info["drv"], info["controller"]),
+                "-d",
+                "3ware,%d" % (twcli.get(info["channel"], -1), )
+            ]
+
+    output, err = system("/usr/local/sbin/smartctl", "-i", *args)
+    search = re.search(r'Serial Number:\s+(?P<serial>.+)', output, re.I)
+    if search:
+        serial = search.group("serial")
+        return serial
+
+    return None
+
+
+def generate_disk_cache(dispatcher, path):
+    confxml = geom.confxml()
+    name = os.path.basename(path)
+    gdisk = confxml.xpath("/mesh/class[name='DISK']/geom[name='{0}']".format(name))
+    gpart = confxml.xpath("/mesh/class[name='PART']/geom[name='{0}']".format(name))
+
+    if not gdisk:
+        return
+
+    gdisk = gdisk.pop()
+    gpart = gpart.pop() if gpart else None
+    provider = gdisk.find('provider')
+    partitions = []
+
+    if gpart:
+        for p in gpart.findall('provider'):
+            partitions.append({
+                'name': p.find("name").text,
+                'mediasize': int(p.find("mediasize").text),
+                'uuid': p.find("config/rawuuid").text,
+                'type': p.find("config/type").text,
+                'label': p.find("config/label").text if p.find("config/label") else None
+            })
+
+    serial = serial_from_device(path)
+    identifier = device_to_identifier(confxml, name, serial)
+    data_partitions = filter(lambda x: x['type'] == 'freebsd-zfs', partitions)
+    data_uuid = data_partitions[0].get('uuid') if len(data_partitions) > 0 else None
+
+    disk = {
+        'mediasize': int(provider.find("mediasize").text),
+        'sectorsize': int(provider.find("sectorsize").text),
+        'description': provider.find("config/descr").text,
+        'identifier': identifier,
+        'serial': serial,
+        'schema': gpart.find("config/scheme").text if gpart else None,
+        'controller': camcontrol_cache.get(name),
+        'partitions': partitions,
+        'data-partition-uuid': data_uuid,
+        'data-partition-path': os.path.join("/dev/gptid", data_uuid) if data_uuid else None
+    }
+
+    diskinfo_cache.put(path, disk)
+    ds_disk = dispatcher.datastore.get_one('disks', ('id', '=', identifier))
+
+    if ds_disk is None:
+
+        dispatcher.datastore.insert('disks', {
+            'id': identifier,
+            'path': path,
+            'serial': disk['serial'],
+            'data-partition-uuid': disk['data-partition-uuid']
+        })
+
+
+def purge_disk_cache(path):
+    diskinfo_cache.remove(path)
+
+
+def _depends():
+    return ['DevdPlugin']
 
 
 def _init(dispatcher):
+    def on_device_attached(args):
+        path = args['path']
+        if not re.match(r'^/dev/(da|ad|ada)[0-9]+$', path):
+            return
+
+        # Regenerate camcontrol and disk cache
+        generate_camcontrol_cache()
+        generate_disk_cache(dispatcher, path)
+
+        # Push higher tier event
+        disk = diskinfo_cache.get(path)
+        dispatcher.dispatch_event('disk.attached', {
+            'name': path,
+            'identifier': disk['identifier']
+        })
+
+    def on_device_detached(args):
+        path = args['path']
+        if not re.match(r'^/dev/(da|ad|ada)[0-9]+$', path):
+            return
+
+        purge_disk_cache(path)
+        disk = dispatcher.datastore.get_one('disks', ('path', '=', path))
+        dispatcher.dispatch_event('disk.detached', {
+            'name': path,
+            'serial': disk['serial']
+        })
+
+    def on_device_mediachange(args):
+        # Regenerate caches
+        generate_camcontrol_cache()
+        generate_disk_cache(dispatcher, args['path'])
+
+        dispatcher.dispatch_event('disk.updated', {
+            'name': args['path'],
+            'identifier': diskinfo_cache.get(args['path'])['identifier']
+        })
+
+    dispatcher.register_schema_definition('disk', {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'description': {'type': 'string'},
+            'serial': {'type': 'string'},
+            'mediasize': {'type': 'integer'},
+            'smart': {'type': 'boolean'},
+            'smart-options': {'type': 'string'},
+            'standby-mode': {
+                'type': 'string'
+            },
+            'acoustic-level': {
+                'type': 'string'
+            },
+            'apm-mode': {
+                'type': 'string'
+            }
+        }
+    })
+
+    dispatcher.require_collection('disks')
     dispatcher.register_provider('disk', DiskProvider)
-    dispatcher.register_task_handler('disk.format', DiskGPTFormatTask)
+    dispatcher.register_event_handler('system.device.attached', on_device_attached)
+    dispatcher.register_event_handler('system.device.detached', on_device_detached)
+    dispatcher.register_event_handler('system.device.mediachange', on_device_mediachange)
+    dispatcher.register_task_handler('disk.erase', DiskEraseTask)
+    dispatcher.register_task_handler('disk.format.gpt', DiskGPTFormatTask)
+    dispatcher.register_task_handler('disk.configure', DiskConfigureTask)
+    dispatcher.register_task_handler('disk.delete', DiskDeleteTask)
+
+    for i in dispatcher.rpc.call_sync('system.device.get_devices', 'disk'):
+        on_device_attached({'path': i['path']})
