82b7d64731605a5cefa9a5ac9c0bcaac91aeded2
Jordan Hubbard <jkh@ixsystems.com>
2014-12-24 00:11:01 -0800

    Make link correctly in light of -f behavior.

--- build/post-to-download.sh
+++ build/post-to-download.sh
@@ -32,5 +32,5 @@ fi
 ssh ${ID}@download.freenas.org rm -rf /tank/downloads/nightlies/$TRAIN/$DATE
 ssh ${ID}@download.freenas.org mkdir -p /tank/downloads/nightlies/$TRAIN/$DATE
 scp -pr $STAGE/$VERSION-$DATE/* ${ID}@download.freenas.org:/tank/downloads/nightlies/$TRAIN/$DATE/
-ssh ${ID}@download.freenas.org "(cd /tank/downloads; ln -fs nightlies/$TRAIN/$DATE nightly)"
+ssh ${ID}@download.freenas.org "(cd /tank/downloads; rm -f nightly; ln -fs nightlies/$TRAIN/$DATE nightly)"
 exit 0
