0c1a4ca0f5f6fb67a3d52296c5d5c2c296130c78
Xin Li <delphij@FreeBSD.org>
2015-01-02 18:19:52 -0800

    Welcome to 2015.
    (cherry picked from commit 777370ab90ab2dd8289f37af57081891e5def4aa)

--- docs/userguide/conf.py
+++ docs/userguide/conf.py
@@ -41,7 +41,7 @@ master_doc = 'freenas'
 
 # General information about the project.
 project = u'FreeNAS User Guide'
-copyright = u'2011-2014, iXsystems'
+copyright = u'2011-2015, iXsystems'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
