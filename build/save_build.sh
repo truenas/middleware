b5b3ff0a534b85392f424bbafcf7e4b6b817d67f
Josh Paetzel <josh@ixsystems.com>
2015-01-11 11:53:55 -0800

    Add the build timestamp to the name of the saved build directory.
    This change prevents SU saved builds from overwriting each other by
    giving the saved build environment directory a unique name.
    Along the way strip some unneeded stuff from the save.
    This reduces the size of the saved environment by 28%!
    (I haven't been so excited by 2.2GB since the 90's, when I had systems
    with 8GB hard drives.)
    
    (cherry picked from commit 2edcd8e8392f9cb6db3ee31b27431ada17b30280)

--- build/save_build.sh
+++ build/save_build.sh
@@ -22,7 +22,7 @@ fi
 
 : ${SAVED_BUILD_ENV_ROOT="/freenas/BSD/releng/${NANO_LABEL}/build_env/"}
 
-: ${SAVED_VERSION_NAME=${VERSION}}
+: ${SAVED_VERSION_NAME=${VERSION}-${BUILD_TIMESTAMP}}
 
 SAVED_BUILD_ENV_DESTDIR="${SAVED_BUILD_ENV_ROOT}/${SAVED_VERSION_NAME}"
 if [ -e "${SAVED_BUILD_ENV_DESTDIR}" ] ; then
@@ -32,7 +32,7 @@ fi
 set -x
 set -e
 mkdir -p ${SAVED_BUILD_ENV_DESTDIR}
-tar -C "$mydir/.." -cf - . | tar -C "${SAVED_BUILD_ENV_DESTDIR}" --no-same-permissions --no-same-owner -xf -
+tar -C "$mydir/.." -cf - --exclude "*.iso" --exclude "Packages/*.tgz" --exclude "*GUI_Upgrade.txz" . | tar -C "${SAVED_BUILD_ENV_DESTDIR}" --no-same-permissions --no-same-owner -xf -
 set +x
 echo "Build saved to '$SAVED_BUILD_ENV_DESTDIR'"
 exit 0
