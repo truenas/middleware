38d0ebe199bff0c9b419a60c006a828c48d23029
Kris Moore <kris@pcbsd.org>
2015-01-16 18:02:23 +0000

    Remove dynamic port generation of PC-BSD bits, they are in FreeNAS ports masters/2014q4 now

--- build/ports/fetch-ports-srcs.sh
+++ build/ports/fetch-ports-srcs.sh
@@ -11,31 +11,6 @@ TOP="$(pwd)"
 #     ports tree.
 #poudriere -e ${NANO_OBJ}/poudriere/etc ports -c -p p -m git
 
-# Download the PC-BSD ports tree
-if [ -d "${GIT_PCBSD_CHECKOUT_PATH}" ] ; then
-   # We have a tree, just update it
-   cd ${GIT_PCBSD_CHECKOUT_PATH} && git pull
-else
-  # Fresh checkout
-  git clone -b ${GIT_PCBSD_BRANCH} ${GIT_PCBSD_REPO} ${GIT_PCBSD_CHECKOUT_PATH}
-  if [ $? -ne 0 ] ; then
-     echo "Failed checking out PC-BSD sources from github..."
-     exit 1
-  fi
-fi
-
-# Figure out where to place PCBSD generated distfiles
-PCBSD_DISTFILES_CACHE="${NANO_OBJ}/ports/distfiles"
-if [ -n "$PORTS_DISTFILES_CACHE" -a -d "$PORTS_DISTFILES_CACHE" ] ; then
-   PCBSD_DISTFILES_CACHE="$PORTS_DISTFILES_CACHE"
-fi
-if [ ! -d "$PCBSD_DISTFILES_CACHE" ] ; then
-  mkdir -p ${PCBSD_DISTFILES_CACHE}
-fi
-
-# Merge the PC-BSD ports into the FreeNAS tree
-cd ${GIT_PCBSD_CHECKOUT_PATH} && ./mkports.sh ${GIT_PORTS_CHECKOUT_PATH} ${PCBSD_DISTFILES_CACHE}
-
 # Create the metadata which tells poudriere where the checked
 # out ports tree lives.  Use the checked out tree that the
 # FreeNAS build uses.
