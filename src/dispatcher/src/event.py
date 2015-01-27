7def358779d32574e25f8c297d78ad4804290dc6
Jakub Klama <jakub@ixsystems.com>
2015-01-27 19:49:10 +0100

    Fix field naming in event sources.

--- src/dispatcher/src/event.py
+++ src/dispatcher/src/event.py
@@ -1,18 +1,47 @@
-__author__ = 'jceel'
+#+
+# Copyright 2015 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################
+
 
 import logging
 
 
 class EventSource(object):
     def __init__(self, dispatcher):
-        self.__dispatcher = dispatcher
-        self.__logger = logging.getLogger(self.__class__.__name__)
+        self.dispatcher = dispatcher
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def enable(self, event):
+        pass
+
+    def disable(self, event):
+        pass
 
     def register_event_type(self, name):
         pass
 
     def emit_event(self, type, **kwargs):
-        self.__dispatcher.dispatch_event(type, kwargs)
-
-    def listen_for_event(self, type, handler):
-        pass
+        self.dispatcher.dispatch_event(type, kwargs)
