c7e85c63ddfe0ba90668028723eedcf0934270c7
William Grzybowski <wg@FreeBSD.org>
2015-01-30 14:17:46 -0200

    Add a timeout and handle connection exceptions
    Ticket:	#6148

--- gui/support/utils.py
+++ gui/support/utils.py
@@ -8,11 +8,17 @@ ADDRESS = 'support-proxy.ixsystems.com:8080'
 
 def new_ticket(data):
 
-    r = requests.post(
-        'http://%s/api/v1.0/ticket' % ADDRESS,
-        data=json.dumps(data),
-        headers={'Content-Type': 'application/json'},
-    )
+    try:
+        r = requests.post(
+            'http://%s/api/v1.0/ticket' % ADDRESS,
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'},
+            timeout=10,
+        )
+    except requests.ConnectionError, e:
+        return False, 'Connection failed: %s' % e
+    except requests.Timeout, e:
+        return False, 'Connection timed out: %s' % e
     if r.status_code != 200:
         log.debug('Support Ticket failed (%d): %s', r.status_code, r.text)
         return False, 'Ticket creation failed, try again later.'
