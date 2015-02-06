f81a1646f7e72a88876af915769da153b165aaa9
Corey Vixie <corey.vixie@gmail.com>
2015-02-06 12:30:36 -0800

    Fixed the ENV shim.

--- src/gui/app/ENV.js
+++ src/gui/app/ENV.js
@@ -1,4 +1,10 @@
-ENV = {
-  CLIENT: typeof window !== 'undefined',
-  SERVER: typeof window === 'undefined'
+// ENV
+// ---
+// Tiny helper shim for determining the current environment.
+
+"use strict";
+
+module.exports = {
+    CLIENT: typeof window !== "undefined"
+  , SERVER: typeof window === "undefined"
 };
