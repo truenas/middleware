4e12da21375c513de6ac4e6e71234454dd3018d7
Xin Li <delphij@FreeBSD.org>
2015-02-13 17:23:05 -0800

    Basic escrow daemon.
    Ticket: #6292
    (cherry picked from commit ddcf3e13400fde68083da44cb6cc7e1684520835)

---
+++ gui/failover/enc_helper.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+#-
+# Copyright (c) 2015 iXsystems, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+import logging
+import socket
+import subprocess
+import os
+import re
+import sys
+import tempfile
+import time
+sys.path.append('/usr/local/www')
+sys.path.append('/usr/local/www/freenasUI')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freenasUI.settings')
+
+# Make sure to load all modules
+from django.db.models.loading import cache
+cache.get_apps()
+
+from freenasUI.common.pipesubr import pipeopen
+from freenasUI.freeadmin.apppool import appPool
+from freenasUI.failover.models import Failover
+from freenasUI.storage.models import Volume
+
+log = logging.getLogger('tools.haenc')
+
+BUFSIZE = 256
+
+class LocalEscrowCtl:
+    def __init__(self):
+        server = "/tmp/escrowd.sock"
+        connected = False
+        retries = 5
+
+        # Start escrowd on demand
+        #
+        # Attempt to connect the server;
+        # if connection can not be established, startescrowd and
+        # retry.
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            sock.connect(server)
+            connected = True
+        except:
+            proc = subprocess.Popen(["/usr/local/sbin/escrowd"])
+            while retries > 0 and connected == False:
+                try:
+                    retries = retries - 1
+                    sock.connect(server)
+                    connected = True
+                except:
+                    time.sleep(1)
+
+        # TODO
+        if not connected:
+            print "FATAL: Can't connect to escrowd"
+            sys.exit(1)
+
+        data = sock.recv(BUFSIZE)
+        if data != "220 Ready, go ahead\n":
+            print "FATAL: server didn't send welcome message, exiting"
+            sys.exit(2)
+        self.sock = sock
+
+    # Set key on local escrow daemon.
+    def setkey(self, passphrase):
+        command = "SETKEY %s\n" % (passphrase)
+        self.sock.sendall(command)
+        data = self.sock.recv(BUFSIZE)
+        return (data == "250 setkey accepted.\n")
+        # Push the key to remote.
+
+    # Clear key on local escrow daemon.
+    def clear(self):
+        command = "CLEAR"
+        self.sock.sendall(command)
+        data = self.sock.recv(BUFSIZE)
+        return (data == "200 clear succeeded.\n")
+
+    # Shutdown local escrow daemon.
+    def shutdown(self):
+        command = "SHUTDOWN"
+        self.sock.sendall(command)
+        data = self.sock.recv(BUFSIZE)
+        return (data == "250 Shutting down.\n")
+
+    # Get key from local escrow daemon.  Returns None if not available.
+    def getkey(self):
+        command = "REVEAL"
+        self.sock.sendall(command)
+        data = self.sock.recv(BUFSIZE)
+        lines = data.split('\n')
+        if lines[0] == "404 No passphrase present":
+            return None
+        elif lines[0] == "200 Approved":
+            if len(lines) > 2:
+                data = lines[1]
+            else:
+                data = self.sock.recv(BUFSIZE)
+                data = data.split('\n')[0]
+            return data
+        else:
+            # Should never happen.
+            return None
+
+    # Get status of local escrow daemon.  True -- Have key; False -- No key.
+    def status(self):
+        command = "STATUS"
+        self.sock.sendall(command)
+        data = self.sock.recv(BUFSIZE)
+        return (data == "200 keyd\n")
+
+cmdset = set(['setkey', 'clear', 'synctopeer', 'syncfrompeer', 'shutdown', 'status', 'attachall'])
+
+# Parse command options
+if len(sys.argv) < 2 or not sys.argv[1] in cmdset or (sys.argv[1] == 'setkey' and len(sys.argv) != 3):
+    print """
+Usage: %s command [args...]
+
+Available commands are:
+  setkey [passphrase] Sets passphrase
+  clear               Clears passphrase
+  synctopeer          Transfer local passphrase to remote system
+  syncfrompeer        Transfer remote passphrase to local system
+  shutdown            Shuts down escrow daemon
+  status              Inquiry escrow daemon status
+  attachall           Attach volumes with the escrowed passphrase
+          """ % sys.argv[0]
+    sys.exit(1)
+
+escrowctl = LocalEscrowCtl()
+cmd = sys.argv[1]
+rv = "Unknown"
+if cmd == 'setkey':
+    rv = escrowctl.setkey(sys.argv[2])
+elif cmd == 'clear':
+    rv = escrowctl.clear()
+elif cmd == 'shutdown':
+    rv = escrowctl.shutdown()
+elif cmd == 'status':
+    rv = escrowctl.status()
+    if rv:
+        print "Escrow have the passphrase"
+    else:
+        print "Escrow running without passphrase"
+elif cmd == 'attachall':
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.file.write(escrowctl.getkey())
+        tmp.file.flush()
+        procs = []
+        failed = 0
+        for vol in Volume.objects.filter(vol_encrypt__exact=2):
+            failed = 0
+            keyfile = vol.get_geli_keyfile()
+            for ed in vol.encrypteddisk_set.all():
+                provider = ed.encrypted_provider
+                if not os.path.exists('/dev/%s.eli' % provider):
+                    proc = pipeopen("geli attach -j %s -k %s %s" % (
+                        tmp.name, keyfile, provider) , quiet=True)
+                    procs.append(proc)
+            for proc in procs:
+                msg = proc.communicate()[1]
+                if proc.returncode != 0:
+                    print ("Unable to attach GELI provider: %s" % (msg))
+                    log.warn("Unable to attach GELI provider: %s", (msg))
+                    failed += 1
+            importcmd = "zpool import -f -R /mnt %s" % (vol.vol_name)
+            proc = pipeopen(importcmd)
+            proc.communicate()
+        rv = (failed == 0)
+else:
+    peer = str(Failover.objects.all()[0].ipaddress)
+    sshcmd = "/usr/bin/ssh -o BatchMode=yes -o StrictHostKeyChecking=yes -o ConnectTimeout=7 %s" % (peer)
+    if cmd == 'synctopeer':
+        passphrase = escrowctl.getkey()
+        if passphrase:
+            sshcmd = "%s /usr/local/bin/python /usr/local/www/freenasUI/failover/enc_helper.py setkey %s" % (sshcmd, passphrase)
+        else:
+            print "ERROR: passphrase unavailable."
+    elif cmd == 'syncfrompeer':
+        sshcmd = "%s /usr/local/bin/python /usr/local/www/freenasUI/failover/enc_helper.py synctopeer" % (sshcmd)
+    sshcmd = pipeopen(sshcmd, quiet=True)
+    msg = sshcmd.communicate()[1]
+    rv = (sshcmd.returncode == 0)
+    if not rv:
+        print msg
+
+if rv:
+    print "Succeeded."
+else:
+    print "Failed."
