b6dd06272aae87f58b5e4340027a9db12b926196
Josh Paetzel <josh@ixsystems.com>
2015-02-17 10:01:53 -0800

    Fix reversed logic.
    It's unclear whether this was a thinko or a miscommunicationo.
    
    Ticket: #8067

--- gui/failover/migrations/0006_critical_carp.py
+++ gui/failover/migrations/0006_critical_carp.py
@@ -11,7 +11,7 @@ class Migration(DataMigration):
         # Note: Don't use "from appname.models import ModelName". 
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
-        for o in orm['failover.CARP'].objects.filter(carp_number__in=[1, 2]):
+        for o in orm['failover.CARP'].objects.all().exclude(carp_number__in=[1, 2]):
             o.carp_critical = True
             o.carp_group = 1
             o.save()
