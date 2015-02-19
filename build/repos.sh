57cfaa152affdf994a708674cb32957966738464
William Grzybowski <wg@FreeBSD.org>
2015-02-19 16:00:54 -0200

    Switch from keyserverlib to licenselib and make it opensource
    Ticket:	#8093

--- build/repos.sh
+++ build/repos.sh
@@ -11,14 +11,12 @@ SRCS_MANIFEST="${AVATAR_ROOT}/FreeBSD/repo-manifest"
 
 if is_truenas ; then
     # Additional repos to checkout for build
-    ADDL_REPOS="$ADDL_REPOS ZFSD TRUENAS_COMPONENTS KEYSERVER"
+    ADDL_REPOS="$ADDL_REPOS ZFSD TRUENAS_COMPONENTS"
 
     : ${GIT_ZFSD_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/git-repo/zfsd.git}
     : ${GIT_ZFSD_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/zfsd"}
     : ${GIT_TRUENAS_COMPONENTS_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/truenas.git}
     : ${GIT_TRUENAS_COMPONENTS_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/truenas-components"}
-    : ${GIT_KEYSERVER_REPO=git@gitserver.ixsystems.com:repos/projects/keyserver}
-    : ${GIT_KEYSERVER_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/keyserver"}
 
     export NAS_PORTS_DIRECT=1
 
@@ -27,6 +25,7 @@ fi
 if [ "${GIT_LOCATION}" = "EXTERNAL" ] ; then
     : ${GIT_FREEBSD_REPO=https://github.com/trueos/trueos}
     : ${GIT_PORTS_REPO=https://github.com/freenas/ports.git}
+    : ${GIT_LICENSELIB_REPO=https://github.com/freenas/licenselib.git}
 fi
 
 : ${GIT_FREEBSD_BRANCH=masters/releng/9.3}
@@ -37,4 +36,7 @@ fi
 : ${GIT_PORTS_REPO=git@gitserver.ixsystems.com:/git/repos/freenas-build/ports.git}
 : ${GIT_PORTS_CHECKOUT_PATH="${AVATAR_ROOT}/FreeBSD/ports"}
 
-: ${REPOS="FREEBSD PORTS ${ADDL_REPOS}"}
+: ${GIT_LICENSELIB_REPO=git@gitserver.ixsystems.com:repos/projects/licenselib}
+: ${GIT_LICENSELIB_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/licenselib"}
+
+: ${REPOS="FREEBSD PORTS LICENSELIB ${ADDL_REPOS}"}
