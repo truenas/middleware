ed2beb57a5ce5125c3938d973c3682c7410439e0
Ben Milman <bpmilman@gmail.com>
2015-02-26 13:50:40 -0800

    Determine TOC name based on software version.
    Ticket: #8225

--- docs/userguide/conf.py
+++ docs/userguide/conf.py
@@ -108,8 +108,11 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'FreeNAS User Guide 9.3 Table of Contents'
-
+if tags.has('truenas'):
+    html_title = 'TrueNAS User Guide 9.3 Table of Contents'
+else:
+    html_title = 'TrueNAS User Guide 9.3 Table of Contents'
+    
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
