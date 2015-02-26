b6da33b4d04d3805223b15375d9801844e02dab1
Jordan Hubbard <jkh@ixsystems.com>
2015-02-26 15:26:57 -0800

    Wave goodbye to zfsd repo.

--- build/repos.sh
+++ build/repos.sh
@@ -11,15 +11,12 @@ SRCS_MANIFEST="${AVATAR_ROOT}/FreeBSD/repo-manifest"
 
 if is_truenas ; then
     # Additional repos to checkout for build
-    ADDL_REPOS="$ADDL_REPOS ZFSD TRUENAS_COMPONENTS"
+    ADDL_REPOS="$ADDL_REPOS TRUENAS_COMPONENTS"
 
-    : ${GIT_ZFSD_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/git-repo/zfsd.git}
-    : ${GIT_ZFSD_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/zfsd"}
     : ${GIT_TRUENAS_COMPONENTS_REPO=git@gitserver.ixsystems.com:/git/repos/truenas-build/truenas.git}
     : ${GIT_TRUENAS_COMPONENTS_CHECKOUT_PATH="${AVATAR_ROOT}/nas_source/truenas-components"}
 
     export NAS_PORTS_DIRECT=1
-
 fi
 
 if [ "${GIT_LOCATION}" = "EXTERNAL" ] ; then
