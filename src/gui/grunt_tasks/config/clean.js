5a7cd39c839fb683e8d08bb170c088dafea351cb
Corey Vixie <corey.vixie@gmail.com>
2015-02-27 17:51:24 -0800

    Grunt clean task should wipe out the vanilla JS as well.

--- src/gui/grunt_tasks/config/clean.js
+++ src/gui/grunt_tasks/config/clean.js
@@ -5,6 +5,6 @@
 
 module.exports = function( grunt ) {
   this.deployment = [ "<%= dirTree.deployment %>" ];
-  this.build      = [ "<%= dirTree.build.root %>" ];
+  this.build      = [ "<%= dirTree.build.root %>", "<%= dirTree.build.ssrjs %>" ];
   this.pkgs       = [ "bower_components", "node_modules" ];
 };
