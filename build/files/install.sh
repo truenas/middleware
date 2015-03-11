9574e3347a7d11612a2f4ee2c0df42fd803ea03e
Jordan Hubbard <jkh@ixsystems.com>
2015-03-10 18:53:04 -0700

    Use compression by default on freenas boot pool, increasing space for boot environments and possibly doing less I/O to devices.
    Ticket: #8514

--- build/files/install.sh
+++ build/files/install.sh
@@ -327,6 +327,8 @@ partition_disk() {
 	    _mirror=""
 	fi
 	zpool create -f -o cachefile=/tmp/zpool.cache -o version=28 -O mountpoint=none -O atime=off -O canmount=off freenas-boot ${_mirror} ${_disksparts}
+	zpool set feature@lz4_compress=enabled freenas-boot
+	zfs set compress=lz4 freenas-boot
 	zfs create -o canmount=off freenas-boot/ROOT
 	zfs create -o mountpoint=legacy freenas-boot/ROOT/default
 	zfs create -o mountpoint=legacy freenas-boot/grub
