98142ba3535bc83d14889b26ab6b037107ba3f11
Xin Li <delphij@FreeBSD.org>
2015-03-12 19:42:17 -0700

    Hook passphrase form with Escrow daemon on TrueNAS when in HA mode.

--- gui/storage/forms.py
+++ gui/storage/forms.py
@@ -2531,6 +2531,13 @@ class UnlockPassphraseForm(Form):
     def done(self, volume):
         passphrase = self.cleaned_data.get("passphrase")
         key = self.cleaned_data.get("key")
+        if hasattr(_n, 'failover_status'):
+            if notifier().failover_status() == 'MASTER':
+                from freenasUI.failover.enc_helper import LocalEscrowCtl
+                escrowctl = LocalEscrowCtl()
+                escrowctl.setkey(passphrase)
+                os.system("/usr/local/bin/python /usr/local/libexec/truenas/carp-state-change-hook.py carp0 LINK_UP")
+                return
         if passphrase:
             passfile = tempfile.mktemp(dir='/tmp/')
             with open(passfile, 'w') as f:
