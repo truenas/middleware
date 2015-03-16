f8f651f7fbf22a228e232ca43d9d00e722377c0b
Corey Vixie <corey.vixie@gmail.com>
2015-03-15 18:50:15 -0700

    Events tab in Debug Tools now supports filtering events by key-value match, string, or regex.
    Ticket: #8545

--- src/gui/app/jsx/components/DebugTools/Events.jsx
+++ src/gui/app/jsx/components/DebugTools/Events.jsx
@@ -3,6 +3,7 @@
 
 "use strict";
 
+var _      = require("lodash");
 var React  = require("react");
 var TWBS   = require("react-bootstrap");
 var moment = require("moment");
@@ -11,12 +12,21 @@ var moment = require("moment");
 var MiddlewareClient = require("../../middleware/MiddlewareClient");
 var MiddlewareStore  = require("../../stores/MiddlewareStore");
 
+var defaultPredicate = {
+    "Object" : "{ \"args\": { \"args\": { \"percentage\": 100 } } }"
+  , "String" : "String to search for"
+  , "RegExp" : "[ \"pattern\", \"flags\" ]"
+};
+
 var Events = React.createClass({
 
     getInitialState: function() {
       return {
-          events     : MiddlewareStore.getEventLog()
-        , timeFormat : "absolute"
+          events           : MiddlewareStore.getEventLog()
+        , timeFormat       : "absolute"
+        , predicate        : defaultPredicate["Object"]
+        , predicateType    : "Object"
+        , appliedPredicate : null
       };
     }
 
@@ -50,6 +60,25 @@ var Events = React.createClass({
       this.setState({ timeFormat: "absolute" });
     }
 
+  , handlePredicateChange: function( event ) {
+      this.setState({
+          predicate        : event.target.value
+        , appliedPredicate : null
+      });
+    }
+
+  , applyPredicate: function( event ) {
+      this.setState({ appliedPredicate: this.state.predicate });
+    }
+
+  , switchPredicateType: function( predicateType ) {
+      this.setState({
+          appliedPredicate : null
+        , predicateType    : predicateType
+        , predicate        : defaultPredicate[ predicateType ]
+      });
+    }
+
   , createEventLog: function( event, index ) {
       var eventObj  = event.args;
       var timestamp = null;
@@ -75,10 +104,64 @@ var Events = React.createClass({
       );
     }
 
-  , render: function() {
-      var logContent = null;
+  , getPredicateHelp: function( predicateType ) {
+      switch( predicateType ) {
+        case "Object":
+          return ( <span>In "Object" mode, the "Filter Predicate" field uses <code>_.where()</code> from <a href="http://devdocs.io/lodash/index#where" target="_blank">lodash</a>, and will return matching entries that satisfy the object comparison. Remember, most <code>event</code> objects store their data in the following format: <code>{"{ args: { args: { /* data is here */ } } }"}</code></span> );
 
-      if ( this.state.events.length ) {
+        case "String":
+          return ( <span>In "String" mode, each event entry is converted by <code>JSON.stringify()</code>, into a string, and then the string entered in the "Filter Predicate" field is used as a substring match.</span> );
+
+        case "RegExp":
+          return ( <span>In "String" mode, each event entry is converted by <code>JSON.stringify()</code>, into a string, and then the array entered in the "Filter Predicate" field is used to construct a new <code>RegExp</code> that will test each string. The first value in the array should be your RegExp test string, and the second is (optionally) the flags (<code>g</code>, <code>i</code>, etc.) to use.</span> );
+
+      }
+    }
+
+  , render: function() {
+      var filteredEventLog = [];
+      var logContent       = null;
+
+      if ( this.state.appliedPredicate ) {
+        switch ( this.state.predicateType ) {
+          case "Object":
+            try {
+              filteredEventLog = _.where( this.state.events, JSON.parse( this.state.predicate ) );
+            }
+            catch ( error ) {
+              window.alert( "The entered text could not be parsed as an object", error );
+            }
+            break;
+
+          case "String":
+            try {
+              filteredEventLog = _.filter( this.state.events, function( eventData ) {
+                return JSON.stringify( eventData ).indexOf( this.state.predicate ) !== -1;
+              }.bind(this) );
+            }
+            catch ( error ) {
+              window.alert( error );
+            }
+            break;
+
+          case "RegExp":
+            try {
+              var reInput = JSON.parse( this.state.predicate );
+              var re = new RegExp( reInput[0], reInput[1] ? reInput[1] : "" );
+              filteredEventLog = _.filter( this.state.events, function( eventData ) {
+                return re.test( JSON.stringify( eventData ) );
+              });
+            }
+            catch ( error ) {
+              window.alert( error );
+            }
+            break;
+        }
+      }
+
+      if ( filteredEventLog.length ) {
+        logContent = filteredEventLog.map( this.createEventLog );
+      } else if ( this.state.events.length ) {
         logContent = this.state.events.map( this.createEventLog );
       } else {
         logContent = <h3 className="text-center">No log content</h3>;
@@ -89,7 +172,7 @@ var Events = React.createClass({
 
           <TWBS.Col xs={6} className="debug-column" >
 
-            <h5 className="debug-heading">System Event Log</h5>
+            <h5 className="debug-heading">FreeNAS Event Log</h5>
             <div className="debug-column-content">
               { logContent }
             </div>
@@ -98,23 +181,74 @@ var Events = React.createClass({
 
           <TWBS.Col xs={6} className="debug-column" >
 
-            <h5 className="debug-heading">Log Options</h5>
+            <h5 className="debug-heading">Options</h5>
             <div className="debug-column-content well well-sm">
-              <div>
-                <label style={{ marginRight: "15px" }}>Time Format</label>
-                <TWBS.ButtonGroup>
-                  <TWBS.Button
-                      active  = { this.state.timeFormat === "human" }
-                      onClick = { this.handleHumanDateSelect }>
-                    {"Relative Time"}
-                  </TWBS.Button>
-                  <TWBS.Button
-                      active  = { this.state.timeFormat === "absolute" }
-                      onClick = { this.handleAbsoluteDateSelect }>
-                    {"Absolute Date"}
-                  </TWBS.Button>
-                </TWBS.ButtonGroup>
-              </div>
+
+              <form className="form-horizontal">
+
+                <TWBS.Input
+                  type             = "text"
+                  value            = { this.state.predicate }
+                  onChange         = { this.handlePredicateChange }
+                  label            = "Filter Predicate"
+                  labelClassName   = "col-xs-2"
+                  wrapperClassName = "col-xs-10"
+                  buttonBefore = {
+                    <TWBS.DropdownButton
+                      bsStyle  = "default"
+                      title    = { this.state.predicateType }
+                      >
+                      <TWBS.MenuItem
+                        onClick  = { this.switchPredicateType.bind( null, "Object" ) }
+                      >
+                        Object
+                      </TWBS.MenuItem>
+                      <TWBS.MenuItem
+                        onClick  = { this.switchPredicateType.bind( null, "String" ) }
+                      >
+                        String
+                      </TWBS.MenuItem>
+                      <TWBS.MenuItem
+                        onClick  = { this.switchPredicateType.bind( null, "RegExp" ) }
+                      >
+                        RegExp
+                      </TWBS.MenuItem>
+                    </TWBS.DropdownButton>
+                  }
+                  buttonAfter = {
+                    <TWBS.Button
+                      bsStyle  = { this.state.appliedPredicate ? "success" : "primary" }
+                      onClick  = { this.applyPredicate }
+                      disabled = { !this.state.predicate.length || !!this.state.appliedPredicate }
+                      >
+                      { this.state.appliedPredicate ? "Filter Applied" : "Apply Filter" }
+                    </TWBS.Button>
+                  } />
+
+                <TWBS.Col xs={ 10 } xsOffset={ 2 }>
+                  <small>{ this.getPredicateHelp( this.state.predicateType ) }</small>
+                </TWBS.Col>
+
+                <div className="form-group">
+                  <label className="control-label col-xs-2">Time Format</label>
+                  <TWBS.Col xs={ 10 }>
+                    <TWBS.ButtonGroup>
+                      <TWBS.Button
+                          active  = { this.state.timeFormat === "human" }
+                          onClick = { this.handleHumanDateSelect }>
+                        {"Relative Time"}
+                      </TWBS.Button>
+                      <TWBS.Button
+                          active  = { this.state.timeFormat === "absolute" }
+                          onClick = { this.handleAbsoluteDateSelect }>
+                        {"Absolute Date"}
+                      </TWBS.Button>
+                    </TWBS.ButtonGroup>
+                  </TWBS.Col>
+                </div>
+
+              </form>
+
             </div>
 
           </TWBS.Col>
