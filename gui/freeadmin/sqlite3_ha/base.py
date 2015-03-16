32cd02395fa06f8d5295065dea754c122df3297f
William Grzybowski <wg@FreeBSD.org>
2015-03-16 08:33:30 -0300

    Proof of concept parsing the UPDATE query using sqlparse
    Ticket:	#8534

--- gui/freeadmin/sqlite3_ha/base.py
+++ gui/freeadmin/sqlite3_ha/base.py
@@ -9,6 +9,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.db.backends.sqlite3 import base as sqlite3base
+import sqlparse
 
 Database = sqlite3base.Database
 DatabaseError = sqlite3base.DatabaseError
@@ -34,6 +35,50 @@ class DatabaseWrapper(sqlite3base.DatabaseWrapper):
 class HASQLiteCursorWrapper(Database.Cursor):
 
     def execute(self, query, params=None):
+        parse = sqlparse.parse(query)
+        for p in parse:
+            if p.tokens[0].normalized not in ('DELETE', 'INSERT', 'UPDATE'):
+                continue
+
+            if p.tokens[0].normalized == 'UPDATE':
+
+                name = p.token_next(p.tokens[0]).get_name()
+                if name not in (
+                    'network_globalconfiguration',
+                ):
+                    continue
+
+                set_ = p.token_next_match(0, sqlparse.tokens.Keyword, 'SET')
+                if not set_:
+                    continue
+
+                next_ = p.token_next(set_)
+                if not next_:
+                    continue
+
+                if issubclass(next_.__class__, sqlparse.sql.IdentifierList):
+                    lookup = list(next_.get_sublists())
+                elif issubclass(next_.__class__, sqlparse.sql.Comparison):
+                    lookup = [next_]
+
+                for l in lookup:
+
+                    if l.get_name() not in ('gc_hosts'):
+                        continue
+
+                    # If it is a list we must also remove the comma around it
+                    prev_ = l.parent.token_prev(l)
+                    next_ = l.parent.token_next(l)
+                    if next_ and issubclass(
+                        next_.__class__, sqlparse.sql.Token
+                    ) and next_.value == ',':
+                        del l.parent.tokens[l.parent.token_index(next_)]
+                    elif prev_ and issubclass(
+                        prev_.__class__, sqlparse.sql.Token
+                    ) and prev_.value == ',':
+                        del l.parent.tokens[l.parent.token_index(prev_)]
+                    del l.parent.tokens[l.parent.token_index(l)]
+
         if params is None:
             return Database.Cursor.execute(self, query)
         query = self.convert_query(query)
@@ -44,4 +89,6 @@ class HASQLiteCursorWrapper(Database.Cursor):
         return Database.Cursor.executemany(self, query, param_list)
 
     def convert_query(self, query):
-        return sqlite3base.FORMAT_QMARK_REGEX.sub('?', query).replace('%%', '%')
+        return sqlite3base.FORMAT_QMARK_REGEX.sub('?', query).replace(
+            '%%', '%'
+        )
