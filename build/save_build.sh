1093cbeafe1f0b09605b2c05aa3b78a2fd7a60fd
Sean Eric Fagan <sef@ixsystems.com>
2015-03-16 12:34:39 -0700

    Save the -Update directory as part of save_build, but to a different location.
    Ticket: #8680
    Merge-FN93: Yes
    Merge-TN93: Yes

--- build/save_build.sh
+++ build/save_build.sh
@@ -21,6 +21,7 @@ if [ -z "$NANO_LABEL" ] ; then
 fi
 
 : ${SAVED_BUILD_ENV_ROOT="/freenas/BSD/releng/${NANO_LABEL}/build_env/"}
+: ${SAVED_UPDATE_ROOT="/freenas/BSD/releng/${NANO_LABEL}/updates"}
 
 : ${SAVED_VERSION_NAME=${VERSION}-${BUILD_TIMESTAMP}}
 
@@ -35,5 +36,22 @@ mkdir -p ${SAVED_BUILD_ENV_DESTDIR}
 tar -C "$mydir/.." -cf - --exclude "dev" --exclude "*.iso" --exclude "Packages/*.tgz" --exclude "*GUI_Upgrade.txz" . | tar -C "${SAVED_BUILD_ENV_DESTDIR}" --no-same-permissions --no-same-owner -xf -
 set +x
 echo "Build saved to '$SAVED_BUILD_ENV_DESTDIR'"
+
+if [ -h "${AVATAR_ROOT}/objs/LATEST" ]; then
+    upd=$(realpath "${AVATAR_ROOT}/objs/LATEST")
+    upd_name=$(basename "${upd}")
+    SAVED_UPDATE_DESTDIR="${SAVED_UPDATE_ROOT}/${upd_name}"
+
+    if [ -e "${SAVED_UPDATE_DESTDIR}/${upd_name}" ]; then
+	echo "${upd_name} already exists in update archive, doing nothing" 1>&2
+    else
+	set -x
+	mkdir -p "${SAVED_UPDATE_DESTDIR}"
+	tar -C "${AVATAR_ROOT}/objs/${upd_name}" -cf - . | tar -C "${SAVED_UPDATE_DESTDIR}" --no-same-permissions --no-same-owner -xf -
+	set +x
+	echo "Update saved to ${SAVED_UPDATE_DESTDIR}"
+    fi
+fi
+
 exit 0
 
