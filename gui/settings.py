4c29bc7eb523d7065128ad1fcb9ed14b2b8ffefd
William Grzybowski <wg@FreeBSD.org>
2015-03-17 14:58:57 -0300

    Revert "Be more thorough in logging commands"
    This reverts commit 2bbf100b40039b6a335cd57f16bf816dec1720da.
    There are side effects like breaking _system_nolog. We might come back
    to this sometime.
    
    (cherry picked from commit 875d6ca0f76c04f3c32b4d0ce2458950bf6e6cfa)

--- gui/settings.py
+++ gui/settings.py
@@ -228,10 +228,6 @@ LOGGING = {
         'simple': {
             'format': '[%(name)s:%(lineno)s] %(message)s'
         },
-        'journal': {
-            'format': ('[%(filename)s:%(line)s] sts=(%(status)d) '
-                       'cmd="%(message)s" err="%(error)s"')
-        },
     },
     'handlers': {
         'mail_admins': {
@@ -243,12 +239,7 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'freenasUI.freeadmin.handlers.SysLogHandler',
             'formatter': 'simple',
-        },
-        'journal': {
-            'level': 'DEBUG',
-            'class': 'freenasUI.freeadmin.handlers.SysLogHandler',
-            'formatter': 'journal',
-        },
+        }
     },
     'loggers': {
         'django.request': {
@@ -260,11 +251,6 @@ LOGGING = {
             'handlers': ['syslog'],
             'level': 'DEBUG',
             'propagate': True,
-        },
-        'journal': {
-            'handlers': ['journal'],
-            'level': 'DEBUG',
-            'propagate': False,
         },
     }
 }
