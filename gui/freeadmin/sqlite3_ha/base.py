5ad0d80d2de6ee8dd233188a34436da2f224b3cf
William Grzybowski <wg@FreeBSD.org>
2015-03-17 14:36:46 -0300

    Remove legacy comment
    Ticket:	#8534

--- gui/freeadmin/sqlite3_ha/base.py
+++ gui/freeadmin/sqlite3_ha/base.py
@@ -1,9 +1,3 @@
-"""
-SQLite3 backend for django.
-
-Works with either the pysqlite2 module or the sqlite3 module in the
-standard library.
-"""
 from __future__ import unicode_literals
 
 import logging
