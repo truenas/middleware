e753ba458e66b5bb6b536a1d8560ee2ca1a6d770
William Grzybowski <wg@FreeBSD.org>
2015-03-19 23:18:23 -0300

    Use the root password as a source for the failover secret
    This is only for migrations.
    
    Ticket:	#8534

--- gui/failover/migrations/0010_auto__add_field_failover_secret.py
+++ gui/failover/migrations/0010_auto__add_field_failover_secret.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import hashlib
 from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
@@ -20,6 +21,13 @@ class Migration(SchemaMigration):
         # Workaround sqlite3 reserved name bug #8184
         db.create_unique('system_failover', ['carp_id', 'volume_id'])
 
+        if not db.dry_run:
+            user = orm['account.bsdUsers'].objects.filter(bsdusr_uid=0)
+            if user.exists():
+                user = user[0]
+                orm['failover.Failover'].objects.all().update(
+                    secret=hashlib.sha256(str(user.bsdusr_unixhash)).hexdigest(),
+                )
 
     def backwards(self, orm):
         # Deleting field 'Failover.secret'
@@ -27,6 +35,39 @@ class Migration(SchemaMigration):
 
 
     models = {
+        u'account.bsdgroupmembership': {
+            'Meta': {'object_name': 'bsdGroupMembership'},
+            'bsdgrpmember_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['account.bsdGroups']"}),
+            'bsdgrpmember_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['account.bsdUsers']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'account.bsdgroups': {
+            'Meta': {'ordering': "['bsdgrp_builtin']", 'object_name': 'bsdGroups'},
+            'bsdgrp_builtin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdgrp_gid': ('django.db.models.fields.IntegerField', [], {}),
+            'bsdgrp_group': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '120'}),
+            'bsdgrp_sudo': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'account.bsdusers': {
+            'Meta': {'ordering': "['bsdusr_builtin']", 'object_name': 'bsdUsers'},
+            'bsdusr_builtin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'bsdusr_full_name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'bsdusr_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['account.bsdGroups']"}),
+            'bsdusr_home': ('freenasUI.freeadmin.models.fields.PathField', [], {'default': "'/nonexistent'", 'max_length': '255'}),
+            'bsdusr_locked': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_microsoft_account': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_password_disabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_shell': ('django.db.models.fields.CharField', [], {'default': "'/bin/csh'", 'max_length': '120'}),
+            'bsdusr_smbhash': ('django.db.models.fields.CharField', [], {'default': "'*'", 'max_length': '128', 'blank': 'True'}),
+            'bsdusr_sudo': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_sudo': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bsdusr_uid': ('django.db.models.fields.IntegerField', [], {}),
+            'bsdusr_unixhash': ('django.db.models.fields.CharField', [], {'default': "'*'", 'max_length': '128', 'blank': 'True'}),
+            'bsdusr_username': ('django.db.models.fields.CharField', [], {'default': "u'User &'", 'unique': 'True', 'max_length': '16'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
         u'failover.carp': {
             'Meta': {'object_name': 'CARP', 'db_table': "'network_carp'"},
             'carp_critical': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -73,4 +114,4 @@ class Migration(SchemaMigration):
         }
     }
 
-    complete_apps = ['failover']
+    complete_apps = ['account', 'failover']
