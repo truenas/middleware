53d15cf75b93b6614c71b19983cd5b95d6edbdd1
Corey Vixie <corey.vixie@gmail.com>
2015-03-19 18:17:11 -0700

    Removed dead widgets and unused references. Minor cleanup.

--- src/gui/app/jsx/views/Dashboard.jsx
+++ src/gui/app/jsx/views/Dashboard.jsx
@@ -5,30 +5,16 @@
 
 "use strict";
 
+var React = require("react");
 
-var React 		         = 	require("react");
-
-var Widget   	         = 	require("../components/Widget");
-var MemoryUtil         = 	require("../components/Widgets/MemoryUtil");
-var CpuUtil            =  require("../components/Widgets/CpuUtil");
-var SystemInfo         =  require("../components/Widgets/SystemInfo");
-var SystemLoad         =  require("../components/Widgets/SystemLoad");
-var NetworkUsage       =  require("../components/Widgets/NetworkUsage");
-var DiskUsage          =  require("../components/Widgets/DiskUsage");
-
-
-var DriveInfo 	       = 	require("../components/Widgets/DriveInfo");
-var DriveInfo2 	       = 	require("../components/Widgets/DriveInfo2");
-var ProcessesPie 	     = 	require("../components/Widgets/ProcessesPie");
-var SwapUsage 	       = 	require("../components/Widgets/SwapUsage");
-var NetworkChart 	     = 	require("../components/Widgets/NetworkChart");
-var PoolIOs 		       = 	require("../components/Widgets/PoolIOs");
-
-
+var MemoryUtil   = require("../components/Widgets/MemoryUtil");
+var CpuUtil      = require("../components/Widgets/CpuUtil");
+var SystemInfo   = require("../components/Widgets/SystemInfo");
+var SystemLoad   = require("../components/Widgets/SystemLoad");
+var NetworkUsage = require("../components/Widgets/NetworkUsage");
+var DiskUsage    = require("../components/Widgets/DiskUsage");
 
 var Dashboard = React.createClass({
-  componentDidMount: function() {
-  },
 
   render: function() {
     return (
@@ -57,23 +43,11 @@ var Dashboard = React.createClass({
             title = "Disk Usage"
             size  = "l-rect"
             graphType = "line" />
-
-
         </div>
       </main>
     );
   }
+
 });
 
 module.exports = Dashboard;
-
-
-//Dead Widgets
-//          <DriveInfo positionX="375" positionY="100" title="Drive Info 1" size="s-square" sn="WC-C4NFLDU8RP" />
-//          <DriveInfo2 positionX="555" positionY="100" title="Drive Info 2a" size="s-square" diskData={{sn:"WC-AWZ0927810", name:"da2", size:"6 TB"}} />
-//          <DriveInfo2 positionX="735" positionY="820" title="Drive Info 2b" size="s-square" diskData={{sn:"SSD-0543TG812", name:"da3", size:"1 TB", type:"ssd"}} />
-//          <ProcessesPie positionX="15" positionY="100" title="Processes Pie" size="l-square" />
-//          <SwapUsage positionX="375" positionY="280" title="Swap Usage" size="sl-rect" />
-//          <NetworkChart positionX="15" positionY="1000" title="Networ Chart" size="xl-rect" />
-//          <PoolIOs positionX="15" positionY="460" title="Pool IOs" size="xl-rect" />
-
