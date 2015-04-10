bbfe5562738f1f4d52da2190e70a159f1c543ddc
William Grzybowski <wg@FreeBSD.org>
2015-04-10 14:02:41 -0300

    Add smart module to the alert system
    Ticket:	#8953
    Merge-FN93:	yes
    Merge-TN93:	yes

---
+++ gui/system/alertmods/smart.py
@@ -0,0 +1,46 @@
+import cPickle as pickle
+import logging
+
+from freenasUI.system.alert import alertPlugins, Alert, BaseAlert
+from lockfile import LockFile, LockTimeout
+
+log = logging.getLogger('system.alertmods.smart')
+
+SMART_FILE = '/tmp/.smartalert'
+
+
+class SMARTAlert(BaseAlert):
+
+    interval = 5
+
+    def run(self):
+        alerts = []
+
+        lock = LockFile(SMART_FILE)
+
+        while not lock.i_am_locking():
+            try:
+                lock.acquire(timeout=5)
+            except LockTimeout:
+                return alerts
+
+        with open(SMART_FILE, 'rb') as f:
+            try:
+                data = pickle.loads(f.read())
+            except:
+                data = {}
+
+        msg = ''
+        for msgs in data.itervalues():
+            if not msgs:
+                continue
+            msg += '<br />\n'.join(msgs)
+
+        if msg:
+            alerts.append(Alert(Alert.CRIT, msg))
+
+        lock.release()
+
+        return alerts
+
+alertPlugins.register(SMARTAlert)
