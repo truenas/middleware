5dc5362962602b8fa6b2442030d2fd2cfb62d4b4
Jordan Hubbard <jkh@ixsystems.com>
2015-04-15 15:48:59 -0700

    Put 10 nightly builds into a more sane location on the download server.

--- build/post-to-download.sh
+++ build/post-to-download.sh
@@ -29,8 +29,10 @@ if [ ! -d ${STAGE}/$VERSION-$DATE ]; then
 	exit 2
 fi
 
-ssh ${ID}@download.freenas.org rm -rf /tank/downloads/nightlies/$TRAIN/$MILESTONE/$DATE
-ssh ${ID}@download.freenas.org mkdir -p /tank/downloads/nightlies/$TRAIN/$MILESTONE/$DATE
-scp -pr $STAGE/$VERSION-$DATE/* ${ID}@download.freenas.org:/tank/downloads/nightlies/$TRAIN/$MILESTONE/$DATE/
-ssh ${ID}@download.freenas.org "(cd /tank/downloads; rm -f nightly; ln -fs nightlies/$TRAIN/$MILESTONE/$DATE nightly)"
+BASEDIR=/tank/downloads/10/Nightlies
+TARGETDIR=${BASEDIR}/${MILESTONE}/${DATE}
+
+ssh ${ID}@download.freenas.org rm -rf ${TARGETDIR}
+ssh ${ID}@download.freenas.org mkdir -p ${TARGETDIR}
+scp -pr $STAGE/$VERSION-$DATE/* ${ID}@download.freenas.org:${TARGETDIR}/
 exit 0
