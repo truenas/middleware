f9416eb35a64b10aebd3502e54f8fc1343a23086
Josh Paetzel <josh@ixsystems.com>
2015-04-17 13:40:41 -0700

    Add a copyright notice.
    A couple minor flake fixes while I'm at it.
    
    (cherry picked from commit 15fbcf8d03217e1106e645825c9276c8bd94cec5)

--- src/freenas/usr/local/bin/test_ssh.py
+++ src/freenas/usr/local/bin/test_ssh.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+#-
+# Copyright (c) 2015 iXsystems, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
 import os
 import paramiko
 from time import sleep
@@ -15,6 +42,7 @@ cache.get_apps()
 
 from freenasUI.storage.models import Replication
 
+
 def check_ssh(ip, port, user, key_file, initial_wait=0, interval=0, retries=1):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys("/etc/ssh/ssh_known_hosts")
@@ -25,7 +53,7 @@ def check_ssh(ip, port, user, key_file, initial_wait=0, interval=0, retries=1):
         try:
             ssh.connect(ip, port, username=user, key_filename=key_file)
             return True
-        except (paramiko.BadHostKeyException, paramiko.AuthenticationException, 
+        except (paramiko.BadHostKeyException, paramiko.AuthenticationException,
                 paramiko.SSHException, socket.error) as e:
             print e
             sleep(interval)
