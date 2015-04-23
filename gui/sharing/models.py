d1af160e81215474712884bcdad49a6aaee09f63
William Grzybowski <wg@FreeBSD.org>
2015-04-23 13:15:45 -0300

    Handle default values for MultiSelectField
    Ticket:	#9355
    Merge-FN93:	yes
    Merge-TN93:	yes

--- gui/sharing/models.py
+++ gui/sharing/models.py
@@ -105,7 +105,7 @@ class CIFS_Share(Model):
         verbose_name=_('VFS Objects'),
         max_length=255,
         blank=True,
-        default='aio_pthread,streams_xattr',
+        default=['aio_pthread', 'streams_xattr'],
         choices=list(choices.CIFS_VFS_OBJECTS())
     )
     cifs_storage_task = models.ForeignKey(
