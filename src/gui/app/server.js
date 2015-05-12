dca386ae1c63563c926ee48cd1a8b6e6a6cea51b
Corey Vixie <corey.vixie@gmail.com>
2015-05-11 17:06:25 -0700

    Upgrade mach from 0.12 to 1.3.8.
    Ticket: #7910

--- src/gui/app/server.js
+++ src/gui/app/server.js
@@ -1,62 +1,64 @@
 // MACH SERVER
+// ES5
 
 "use strict";
 
-var fs   = require("fs");
-var path = require("path");
-var mach = require("mach");
-var when = require("when");
+var fs   = require( "fs" );
+var path = require( "path" );
+var mach = require( "mach" );
+var when = require( "when" );
 
-var React  = require("react");
-var Router = require("react-router");
+var React  = require( "react" );
+var Router = require( "react-router" );
 
-var routes = require("./ssrjs/routes");
+var routes = require( "./ssrjs/routes" );
 
 // Content
-var baseHTML = fs.readFileSync( __dirname + "/templates/mainlayout.html" ).toString();
+var baseHTML = fs.readFileSync( __dirname + "/templates/mainlayout.html" )
+                 .toString();
 var jsBundle = fs.readFileSync( __dirname + "/build/js/app.js" );
 
 var app = mach.stack();
 
 
 // Mach server helpers
-function renderApp( path ) {
+function renderApp ( path ) {
   var bodyRegex = /¡HTML!/;
   var dataRegex = /¡DATA!/;
 
   return new when.Promise( function ( resolve, reject ) {
     Router.run( routes, path, function ( Handler ) {
       var innerHTML = React.renderToString( React.createElement( Handler ) );
-      var output    = baseHTML.replace( bodyRegex, innerHTML )
-                              .replace( dataRegex, null );
+      var output = baseHTML.replace( bodyRegex, innerHTML )
+                           .replace( dataRegex, "{}" );
 
       if ( baseHTML && innerHTML && output ) {
         resolve( output );
       } else {
-        reject( "Handler for " + path + " did not return any HTML when rendered to string" );
+        reject( "Handler for " + path +
+                " did not return any HTML when rendered to string" );
       }
     });
   });
 }
 
 // Mach server config
-// TODO: production mode with different stack options:
-// app.use(mach.gzip);      // Gzip-encode responses
-// app.use(mach.logger);    // Log responses
+app.use( mach.gzip );
 app.use( mach.favicon );
 app.use( mach.file, { root: path.join( __dirname, "build" ) } );
-app.run( function ( req, res ) {
 
-  switch ( req.path ) {
-    case "/js/app.js":
-      return jsBundle;
+// Routes
+app.get( "/js/app.js"
+       , function ( request ) {
+           return jsBundle;
+         }
+       );
 
-    default:
-      return renderApp( req.path );
-  }
-
-});
-
+app.get( "*"
+       , function ( request ) {
+           return renderApp( request.path );
+         }
+       );
 
 // Start Mach server
 mach.serve( app, ( process.env.PORT || 3000 ) );
