4c91d41972f2686697c0a52672f850ba1e0510d9
William Grzybowski <wg@FreeBSD.org>
2015-05-12 13:34:36 -0300

    Split general and UI settings
    Ticket:	#9712

--- src/dispatcher/plugins/SystemInfoPlugin.py
+++ src/dispatcher/plugins/SystemInfoPlugin.py
@@ -47,8 +47,82 @@ ZONEINFO_DIR = "/usr/share/zoneinfo"
 @description("Provides informations about the running system")
 class SystemInfoProvider(Provider):
 
-    @returns(h.ref('system-settings'))
-    def get_system_config(self):
+    @returns(str)
+    def uname_full(self):
+        out, _ = system('uname', '-a')
+        return out
+
+    @returns(str)
+    def version(self):
+        with open('/etc/version') as fd:
+            return fd.read().strip()
+
+    def hardware(self):
+        return {
+            'cpu-model': get_sysctl("hw.model"),
+            'cpu-cores': get_sysctl("hw.ncpu"),
+            'memory-size': get_sysctl("hw.physmem")
+        }
+
+    def time(self):
+        return {
+            'system-time': datetime.now(tz=tz.tzlocal()),
+            'boot-time': datetime.fromtimestamp(
+                psutil.BOOT_TIME, tz=tz.tzlocal()
+            ).isoformat(),
+            'timezone': time.tzname[time.daylight],
+        }
+
+
+@description("Provides informations about general system settings")
+class SystemGeneralProvider(Provider):
+
+    @returns(h.ref('system-general'))
+    def get_config(self):
+
+        return {
+            'language': self.dispatcher.configstore.get(
+                'system.language',
+            ),
+            'timezone': self.dispatcher.configstore.get(
+                'system.timezone',
+            ),
+            'console-keymap': self.dispatcher.configstore.get(
+                'system.console.keymap',
+            ),
+        }
+
+    def keymaps(self):
+        if not os.path.exists(KEYMAPS_INDEX):
+            return []
+
+        rv = []
+        with open(KEYMAPS_INDEX, 'r') as f:
+            d = f.read()
+        fnd = re.findall(r'^(?P<name>[^#\s]+?)\.kbd:en:(?P<desc>.+)$', d, re.M)
+        for name, desc in fnd:
+            rv.append((name, desc))
+        return rv
+
+    def timezones(self):
+        result = []
+        for root, _, files in os.walk(ZONEINFO_DIR):
+            for f in files:
+                if f in (
+                    'zone.tab',
+                ):
+                    continue
+                result.append(os.path.join(root, f).replace(
+                    ZONEINFO_DIR + '/', '')
+                )
+        return result
+
+
+@description("Provides informations about UI system settings")
+class SystemUIProvider(Provider):
+
+    @returns(h.ref('system-ui'))
+    def get_config(self):
 
         protocol = []
         if self.dispatcher.configstore.get('service.nginx.http.enable'):
@@ -67,74 +141,53 @@ class SystemInfoProvider(Provider):
             'webui-https-port': self.dispatcher.configstore.get(
                 'service.nginx.https.port',
             ),
-            'language': self.dispatcher.configstore.get(
-                'system.language',
-            ),
-            'timezone': self.dispatcher.configstore.get(
-                'system.timezone',
-            ),
-            'console-keymap': self.dispatcher.configstore.get(
-                'system.console.keymap',
-            ),
         }
 
-    @returns(str)
-    def uname_full(self):
-        out, _ = system('uname', '-a')
-        return out
 
-    @returns(str)
-    def version(self):
-        with open('/etc/version') as fd:
-            return fd.read().strip()
-
-    def hardware(self):
-        return {
-            'cpu-model': get_sysctl("hw.model"),
-            'cpu-cores': get_sysctl("hw.ncpu"),
-            'memory-size': get_sysctl("hw.physmem")
-        }
-
-    def keymaps(self):
-        if not os.path.exists(KEYMAPS_INDEX):
-            return []
-
-        rv = []
-        with open(KEYMAPS_INDEX, 'r') as f:
-            d = f.read()
-        fnd = re.findall(r'^(?P<name>[^#\s]+?)\.kbd:en:(?P<desc>.+)$', d, re.M)
-        for name, desc in fnd:
-            rv.append((name, desc))
-        return rv
-
-    def time(self):
-        return {
-            'system-time': datetime.now(tz=tz.tzlocal()),
-            'boot-time': datetime.fromtimestamp(
-                psutil.BOOT_TIME, tz=tz.tzlocal()
-            ).isoformat(),
-            'timezone': time.tzname[time.daylight],
-        }
-
-    def timezones(self):
-        result = []
-        for root, _, files in os.walk(ZONEINFO_DIR):
-            for f in files:
-                if f in (
-                    'zone.tab',
-                ):
-                    continue
-                result.append(os.path.join(root, f).replace(
-                    ZONEINFO_DIR + '/', '')
-                )
-        return result
-
-
-@accepts(h.ref('system-settings'))
-class SystemConfigureTask(Task):
+@accepts(h.ref('system-general'))
+class SystemGeneralConfigureTask(Task):
 
     def describe(self):
-        return "System Settings Configure"
+        return "System General Settings Configure"
+
+    def verify(self, props):
+        return ['system']
+
+    def run(self, props):
+        self.dispatcher.configstore.set(
+            'system.language',
+            props.get('language'),
+        )
+        self.dispatcher.configstore.set(
+            'system.timezone',
+            props.get('timezone'),
+        )
+        self.dispatcher.configstore.set(
+            'system.console.keymap',
+            props.get('console-keymap'),
+        )
+
+        try:
+            self.dispatcher.call_sync(
+                'etcd.generation.generate_group', 'localtime'
+            )
+        except RpcException, e:
+            raise TaskException(
+                errno.ENXIO,
+                'Cannot reconfigure system: {0}'.format(str(e),)
+            )
+
+        self.dispatcher.dispatch_event('system.general.changed', {
+            'operation': 'update',
+            'ids': ['system.general'],
+        })
+
+
+@accepts(h.ref('system-ui'))
+class SystemUIConfigureTask(Task):
+
+    def describe(self):
+        return "System UI Settings Configure"
 
     def verify(self, props):
         return ['system']
@@ -160,23 +213,8 @@ class SystemConfigureTask(Task):
             'service.nginx.https.port',
             props.get('webui-https-port'),
         )
-        self.dispatcher.configstore.set(
-            'system.language',
-            props.get('language'),
-        )
-        self.dispatcher.configstore.set(
-            'system.timezone',
-            props.get('timezone'),
-        )
-        self.dispatcher.configstore.set(
-            'system.console.keymap',
-            props.get('console-keymap'),
-        )
 
         try:
-            self.dispatcher.call_sync(
-                'etcd.generation.generate_group', 'localtime'
-            )
             self.dispatcher.call_sync(
                 'etcd.generation.generate_group', 'nginx'
             )
@@ -184,12 +222,12 @@ class SystemConfigureTask(Task):
         except RpcException, e:
             raise TaskException(
                 errno.ENXIO,
-                'Cannot reconfigure system: {0}'.format(str(e),)
+                'Cannot reconfigure system UI: {0}'.format(str(e),)
             )
 
-        self.dispatcher.dispatch_event('system.info.changed', {
+        self.dispatcher.dispatch_event('system.ui.changed', {
             'operation': 'update',
-            'ids': ['system.info'],
+            'ids': ['system.ui'],
         })
 
 
@@ -228,7 +266,16 @@ class SystemHaltTask(Task):
 def _init(dispatcher):
 
     # Register schemas
-    dispatcher.register_schema_definition('system-settings', {
+    dispatcher.register_schema_definition('system-general', {
+        'type': 'object',
+        'properties': {
+            'language': {'type': 'string'},
+            'timezone': {'type': 'string'},
+            'console-keymap': {'type': 'string'},
+        },
+    })
+
+    dispatcher.register_schema_definition('system-ui', {
         'type': 'object',
         'properties': {
             'webui-protocol': {
@@ -244,16 +291,18 @@ def _init(dispatcher):
             },
             'webui-http-port': {'type': 'integer'},
             'webui-https-port': {'type': 'integer'},
-            'language': {'type': 'string'},
-            'timezone': {'type': 'string'},
-            'console-keymap': {'type': 'string'},
         },
     })
 
     # Register providers
+    dispatcher.register_provider("system.general", SystemGeneralProvider)
     dispatcher.register_provider("system.info", SystemInfoProvider)
+    dispatcher.register_provider("system.ui", SystemUIProvider)
 
     # Register task handlers
-    dispatcher.register_task_handler("system.configure", SystemConfigureTask)
+    dispatcher.register_task_handler("system.general.configure",
+                                     SystemGeneralConfigureTask)
+    dispatcher.register_task_handler("system.ui.configure",
+                                     SystemUIConfigureTask)
     dispatcher.register_task_handler("system.shutdown", SystemHaltTask)
     dispatcher.register_task_handler("system.reboot", SystemRebootTask)
