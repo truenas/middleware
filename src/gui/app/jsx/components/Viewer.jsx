b52db44dd77681e4e5b9437199f8ebdb30a3a1f5
Corey Vixie <corey.vixie@gmail.com>
2015-05-15 16:55:43 -0700

    ES6ified and removed lint errors from Viewer.
    Ticket: #9696

--- src/gui/app/jsx/components/Viewer.jsx
+++ src/gui/app/jsx/components/Viewer.jsx
@@ -1,4 +1,9 @@
-
+// VIEWER
+// ======
+// One of the primary display components in FreeNAS. The Viewer is capable of
+// ingesting data sets or collections of "like" things, and displaying them in
+// a variety of modes. It is similar in this way to a desktop client's browser
+// window, though not limited to just displaying files.
 
 "use strict";
 
@@ -15,25 +20,22 @@ import TableViewer from "./Viewer/TableViewer";
 
 
 // Main Viewer Wrapper Component
-var Viewer = React.createClass({
+const Viewer = React.createClass(
 
-    mixins: [ viewerCommon ]
+  { mixins: [ viewerCommon ]
 
-  , contextTypes: {
-      router: React.PropTypes.func
-    }
+  , contextTypes: { router: React.PropTypes.func }
 
-  , propTypes: {
-        defaultMode  : React.PropTypes.string
-      , allowedModes : React.PropTypes.array
-      , inputData    : React.PropTypes.array.isRequired
-      , viewData     : React.PropTypes.object.isRequired
-      , displayData  : React.PropTypes.object
-    }
+  , propTypes:
+      { defaultMode: React.PropTypes.string
+      , allowedModes: React.PropTypes.array
+      , inputData: React.PropTypes.array.isRequired
+      , viewData: React.PropTypes.object.isRequired
+      , displayData: React.PropTypes.object
+      }
 
 
   // REACT LIFECYCLE
-
   , getDefaultProps: function () {
       // Viewer allows all modes by default, except for heirarchical. This list
       // can be overwritten by passing allowedModes into your <Viewer />.
@@ -42,49 +44,56 @@ var Viewer = React.createClass({
       // "icon"   : Items as icons, with properties as modal
       // "table"  : Items as table rows, showing more data
       // TODO: "heir"   : Heirarchical view, shows relationships between items
-      return {
-        allowedModes: [ "detail", "icon", "table" ]
-      };
+      return { allowedModes: [ "detail", "icon", "table" ] };
     }
 
   , getInitialState: function () {
+      const VIEWDATA = this.props.viewData;
       // render will always use currentMode - in an uninitialized Viewer, the
       // mode will not have been set, and should therefore come from either a
       // passed in currentMode or defaultMode, falling back to getDefaultProps
-      var initialMode = ( this.props.currentMode || this.props.defaultMode || "detail" );
+      const INITIALMODE = ( this.props.currentMode ||
+                            this.props.defaultMode ||
+                            "detail"
+                          );
 
       // Generate an array of keys which TableViewer can use to quickly generate
       // its internal structure by looping through the returned data from the
       // middleware and creating cells. Also useful for getting human-friendly
       // names out of the translation key.
-      var defaultTableCols = [];
-      var selectedItem = this.context.router.getCurrentParams()[ this.props.viewData.routing["param"] ];
+      let defaultTableCols = [];
+      let currentParams = this.context.router.getCurrentParams();
+      let selectedItem = currentParams[ VIEWDATA.routing["param"] ];
 
-      _.filter( this.props.viewData.format.dataKeys, function( item, key, collection ) {
-        if ( item["defaultCol"] ) {
-          defaultTableCols.push( item["key"] );
-        }
-      });
+      _.filter( VIEWDATA.format.dataKeys
+              , function ( item, key, collection ) {
+                  if ( item["defaultCol"] ) {
+                    defaultTableCols.push( item["key"] );
+                  }
+                }
+              );
 
       if ( !_.isNumber( selectedItem ) && !_.isString( selectedItem ) ) {
         selectedItem = null;
       }
 
-      return {
-          currentMode    : this.changeViewerMode( initialMode )
-        , tableCols      : defaultTableCols
-        , enabledGroups  : this.props.viewData.display.defaultGroups.length ? this.props.viewData.display.defaultGroups : null
-        , enabledFilters : this.props.viewData.display.defaultFilters.length ? this.props.viewData.display.defaultFilters : null
-        , filteredData   : {
-              grouped   : false
-            , groups    : []
-            , remaining : {
-                entries: []
-              }
-          }
-        , searchString   : ""
-        , selectedItem   : selectedItem
-      };
+      return (
+        { currentMode: this.changeViewerMode( INITIALMODE )
+        , tableCols: defaultTableCols
+        , enabledGroups: VIEWDATA.display.defaultGroups.length
+                       ? VIEWDATA.display.defaultGroups
+                       : null
+        , enabledFilters: VIEWDATA.display.defaultFilters.length
+                        ? VIEWDATA.display.defaultFilters
+                        : null
+        , filteredData: { grouped: false
+                        , groups: []
+                        , remaining: { entries: [] }
+                        }
+        , searchString: ""
+        , selectedItem: selectedItem
+        }
+      );
     }
 
   , componentWillReceiveProps: function ( nextProps ) {
@@ -94,27 +103,29 @@ var Viewer = React.createClass({
 
   // VIEWER DATA HANDLING
 
-    // processDisplayData applys filters, searches, and then groups before handing
-    // the data to any of its sub-views. The structure is deliberately generic
-    // so that any sub-view may display the resulting data as it sees fit.
+    // processDisplayData applys filters, searches, and then groups before
+    // handing the data to any of its sub-views. The structure is deliberately
+    // generic so that any sub-view may display the resulting data as it
+    // sees fit.
   , processDisplayData: function ( options ) {
-      var displayParams = {
-          inputData      : this.props.inputData
-        , searchString   : this.state.searchString
-        , enabledGroups  : this.state.enabledGroups
-        , enabledFilters : this.state.enabledFilters
-      };
+      const VIEWDATA = this.props.viewData;
+
+      let displayParams =
+        { inputData: this.props.inputData
+        , searchString: this.state.searchString
+        , enabledGroups: this.state.enabledGroups
+        , enabledFilters: this.state.enabledFilters
+        };
 
       _.assign( displayParams, options );
 
       // Prevent function from modifying nextProps
-      var inputDataArray = _.cloneDeep( displayParams.inputData );
-      var filteredData = {
-          grouped   : false
-        , groups    : []
-        , remaining : {}
-        , rawList   : []
-      };
+      let inputDataArray = _.cloneDeep( displayParams.inputData );
+      let filteredData = { grouped: false
+                         , groups: []
+                         , remaining: {}
+                         , rawList: []
+                         };
 
 
       // Reduce the array by applying exclusion filters (defined in the view)
@@ -122,22 +133,33 @@ var Viewer = React.createClass({
       if ( displayParams.enabledFilters ) {
         displayParams.enabledFilters.map(
           function ( filter ) {
-            _.remove( inputDataArray, this.props.viewData.display.filterCriteria[ filter ].testProp );
-          }.bind(this)
+            _.remove( inputDataArray
+                    , VIEWDATA.display.filterCriteria[ filter ].testProp
+                    );
+          }.bind( this )
         );
       }
 
 
       // Reduce the array to only items which contain a substring match for the
       // searchString in either their primary or secondary keys
-      inputDataArray = _.filter( inputDataArray, function ( item ) {
-        // TODO: Are keys always strings? May want to rethink this
-        var searchableString = item[ this.props.viewData.format.primaryKey ] + item[ this.props.viewData.format.secondaryKey ] || "";
-        searchableString = searchableString.toLowerCase();
+      inputDataArray =
+        _.filter( inputDataArray
+                , function ( item ) {
+                    // TODO: Are keys always strings? May want to rethink this
+                    let searchString = displayParams.searchString.toLowerCase();
 
-        return ( searchableString.indexOf( displayParams.searchString.toLowerCase() ) !== -1 );
+                    let searchTarget = item[ VIEWDATA.format.primaryKey ] +
+                                       item[ VIEWDATA.format.secondaryKey ] ||
+                                       "";
 
-      }.bind(this) );
+                    return (
+                      _.includes( searchTarget.toLowerCase()
+                                , searchString
+                                )
+                    );
+                  }
+                );
 
       // At this point, inputDataArray is an ungrouped (but filtered) list of
       // items, useful for views like the table.
@@ -148,15 +170,16 @@ var Viewer = React.createClass({
       if ( displayParams.enabledGroups.length ) {
         displayParams.enabledGroups.map(
           function ( group ) {
-            var groupData  = this.props.viewData.display.filterCriteria[ group ];
-            var newEntries = _.remove( inputDataArray, groupData.testProp );
+            let groupData  = VIEWDATA.display.filterCriteria[ group ];
+            let newEntries = _.remove( inputDataArray, groupData.testProp );
 
-            filteredData.groups.push({
-                name    : groupData.name
-              , key     : group
-              , entries : newEntries
-            });
-          }.bind(this)
+            filteredData.groups.push(
+              { name: groupData.name
+              , key: group
+              , entries: newEntries
+              }
+            );
+          }
         );
 
         filteredData["grouped"] = true;
@@ -166,21 +189,24 @@ var Viewer = React.createClass({
 
 
       // All remaining items are put in the "remaining" property
-      filteredData["remaining"] = {
-          name    : filteredData["grouped"] ? this.props.viewData.display["remainingName"] : this.props.viewData.display["ungroupedName"]
-        , entries : inputDataArray
-      };
+      filteredData["remaining"] =
+        { name: filteredData["grouped"]
+              ? VIEWDATA.display["remainingName"]
+              : VIEWDATA.display["ungroupedName"]
+        , entries: inputDataArray
+        };
 
-      this.setState({
-          filteredData   : filteredData
-        , searchString   : displayParams.searchString
-        , enabledGroups  : displayParams.enabledGroups
-        , enabledFilters : displayParams.enabledFilters
-      });
+      this.setState(
+        { filteredData: filteredData
+        , searchString: displayParams.searchString
+        , enabledGroups: displayParams.enabledGroups
+        , enabledFilters: displayParams.enabledFilters
+        }
+      );
     }
 
   , handleItemSelect: function ( selectionValue, event ) {
-      var newSelection = null;
+      let newSelection = null;
 
       if ( !_.isNumber( selectionValue ) || !_.isString( selectionValue ) ) {
         newSelection = selectionValue;
@@ -194,10 +220,12 @@ var Viewer = React.createClass({
     }
 
   , changeViewerMode: function ( targetMode ) {
-      var newMode;
+      let newMode;
 
       // See if a disallowed mode has been requested
-      if ( this.props.allowedModes.indexOf( targetMode ) === -1 ) {
+      if ( _.includes( this.props.allowedModes, targetMode ) ) {
+        newMode = targetMode;
+      } else {
         if ( this.props.defaultMode ) {
           // Use the default mode, if provided
           newMode = this.props.defaultMode;
@@ -205,8 +233,6 @@ var Viewer = React.createClass({
           // If no default, use the first allowed mode in the list
           newMode = this.props.allowedModes[0];
         }
-      } else {
-        newMode = targetMode;
       }
 
       // When changing viewer modes, close any previously open items.
@@ -216,43 +242,50 @@ var Viewer = React.createClass({
       return newMode;
     }
 
-  , handleModeSelect: function( selectedKey ) {
-     this.setState({ currentMode: this.changeViewerMode( selectedKey ) });
-  }
+  , handleModeSelect: function ( selectedKey, foo, bar ) {
+      this.setState({ currentMode: this.changeViewerMode( selectedKey ) });
+    }
 
-  , changeTargetItem: function( params ) {
-      return _.find( this.props.inputData, function( item ) {
-          // Returns the first object from the input array whose selectionKey matches
-          // the current route's dynamic portion. For instance, /accounts/users/root
-          // with bsdusr_usrname as the selectionKey would match the first object
-          // in inputData whose username === "root"
-          return params[ this.props.viewData.routing["param"] ] === item[ this.props.viewData.format["selectionKey"] ];
-        }.bind(this)
-      );
+  , changeTargetItem: function ( params ) {
+      // Returns the first object from the input array whose selectionKey
+      // matches the current route's dynamic portion. For instance,
+      // "/accounts/users/root" with "bsdusr_usrname" as the selectionKey would
+      // match the first object in inputData whose username === "root"
+      return _.find( this.props.inputData
+                   , function ( item ) {
+                       return (
+                         params[ this.props.viewData.routing["param"] ] ===
+                         item[ this.props.viewData.format["selectionKey"] ]
+                       );
+                     }.bind( this )
+                   );
     }
 
 
   // VIEWER DISPLAY
   , createModeNav: function ( mode, index ) {
-      var modeIcons = {
-          "detail" : "th-list"
-        , "icon"   : "th"
-        , "table"  : "align-justify"
-        , "heir"   : "bell"
-      };
+      var modeIcons = { detail: "th-list"
+                      , icon: "th"
+                      , table: "align-justify"
+                      , heir: "bell"
+                      };
 
       return (
-        <TWBS.Button onClick = { function() { this.handleModeSelect( mode ); }.bind(this) }
-                     key     = { index }
-                     bsStyle = { ( mode === this.state.currentMode ) ? "info" : "default" }
-                     active  = { false } >
+        <TWBS.Button
+          onClick = { this.handleModeSelect.bind( this, mode ) }
+          key = { index }
+          bsStyle = { ( mode === this.state.currentMode )
+                    ? "info"
+                    : "default"
+                    }
+          active = { false } >
           <Icon glyph = { modeIcons[ mode ] } />
         </TWBS.Button>
       );
     }
 
   , createViewerContent: function () {
-      var ViewerContent = null;
+      let ViewerContent = null;
 
       switch ( this.state.currentMode ) {
         default:
@@ -274,13 +307,13 @@ var Viewer = React.createClass({
       }
 
       return <ViewerContent
-                viewData         = { this.props.viewData }
-                inputData        = { this.props.inputData }
-                tableCols        = { this.state.tableCols }
+                viewData = { this.props.viewData }
+                inputData = { this.props.inputData }
+                tableCols = { this.state.tableCols }
                 handleItemSelect = { this.handleItemSelect }
-                selectedItem     = { this.state.selectedItem }
-                searchString     = { this.state.searchString }
-                filteredData     = { this.state.filteredData } />;
+                selectedItem = { this.state.selectedItem }
+                searchString = { this.state.searchString }
+                filteredData = { this.state.filteredData } />;
     }
 
   , render: function () {
@@ -289,7 +322,9 @@ var Viewer = React.createClass({
       // Create navigation mode icons
       if ( this.props.allowedModes.length > 1 ) {
         viewerModeNav = (
-          <TWBS.ButtonGroup className="navbar-btn navbar-right" activeMode={ this.state.currentMode } >
+          <TWBS.ButtonGroup
+            className = "navbar-btn navbar-right"
+            activeMode = { this.state.currentMode } >
             { this.props.allowedModes.map( this.createModeNav ) }
           </TWBS.ButtonGroup>
         );
@@ -299,12 +334,12 @@ var Viewer = React.createClass({
         <div className="viewer">
           <TWBS.Navbar fluid className="viewer-nav">
             {/* Searchbox for Viewer (1) */}
-            <TWBS.Input type           = "text"
-                        placeholder    = "Search"
-                        value          = { this.state.searchString }
+            <TWBS.Input type = "text"
+                        placeholder = "Search"
+                        value = { this.state.searchString }
                         groupClassName = "navbar-form navbar-left"
-                        onChange       = { this.handleSearchChange }
-                        addonBefore    = { <Icon glyph ="search" /> } />
+                        onChange = { this.handleSearchChange }
+                        addonBefore = { <Icon glyph ="search" /> } />
 
             {/* Select view mode (3) */}
             { viewerModeNav }
@@ -318,4 +353,4 @@ var Viewer = React.createClass({
 
 });
 
-module.exports = Viewer;
+export default Viewer;
