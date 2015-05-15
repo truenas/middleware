888c75a5576d016a714f37921f6e1d4434c835c4
Corey Vixie <corey.vixie@gmail.com>
2015-05-14 18:55:53 -0700

    Corrected a formatting error in MCD ( " vs ` ), and abstracted out a few more of the verbose log functions.

--- src/gui/app/jsx/middleware/MiddlewareClientDebug.js
+++ src/gui/app/jsx/middleware/MiddlewareClientDebug.js
@@ -6,41 +6,43 @@
 
 "use strict";
 
+import _ from "lodash";
+
 import DebugLogger from "../common/DebugLogger";
 
 class MiddlewareClientDebug extends DebugLogger {
 
   constructor () {
-    super();
+    super( "MIDDLEWARE_CLIENT_DEBUG" );
   }
 
-  static logPack ( namespace, name, args, id ) {
+  logPack ( namespace, name, args, id ) {
     let validPack = true;
     let prefix = "BAD PACK: ";
 
     if ( !_.isString( namespace ) ) {
       validPack = false;
-      this.constructor.warn( "packing"
-                           , prefix + "Provided namespace was not a string for request " +
-                             "%c'${ id }'%c"
-                           , [ DEBUGCSS.idColor, DEBUGCSS.defaultStyle ]
+      this.warn( "packing"
+                           , prefix + `Provided namespace was not a string for request ` +
+                             `%c'${ id }'%c`
+                           , [ this.DEBUGCSS.uuid, this.DEBUGCSS.normal ]
       );
     }
     if ( !_.isString( name ) ) {
       validPack = false;
-      this.constructor.warn( "packing"
+      this.warn( "packing"
              , prefix +
-               "Provided name was not a string for request %c'${ id }'%c"
-             , [ DEBUGCSS.idColor, DEBUGCSS.defaultStyle ]
+               `Provided name was not a string for request %c'${ id }'%c`
+             , [ this.DEBUGCSS.uuid, this.DEBUGCSS.normal ]
              );
     }
     if ( typeof args === ( "null" || "undefined" ) ) {
       validPack = false;
-      this.constructor.warn( "packing"
+      this.warn( "packing"
              , prefix +
-               "Provided args value was null or undefined for request " +
-               "%c'${ id }'%c"
-             , [ DEBUGCSS.idColor, DEBUGCSS.defaultStyle ]
+               `Provided args value was null or undefined for request ` +
+               `%c'${ id }'%c`
+             , [ this.DEBUGCSS.uuid, this.DEBUGCSS.normal ]
              );
     }
     if ( !_.isString( id ) ) {
@@ -49,21 +51,21 @@ class MiddlewareClientDebug extends DebugLogger {
                    ? ":" + args
                    : "";
 
-      this.constructor.warn( "packing"
+      this.warn( "packing"
              , prefix +
-               "UUID %c'${ id }'%c for '${ namespace }'${ packArgs } had " +
-               "to be generated because none was provided%c"
-             , [ DEBUGCSS.idColor, DEBUGCSS.defaultStyle ]
+               `UUID %c'${ id }'%c for '${ namespace }'${ packArgs } had ` +
+               `to be generated because none was provided%c`
+             , [ this.DEBUGCSS.uuid, this.DEBUGCSS.normal ]
              );
 
     }
 
     if ( validPack ) {
-      this.constructor.info( "Packed request %c'${ id }'%c successfully."
-             , [ DEBUGCSS.idColor, DEBUGCSS.defaultStyle ]
+      this.info( `Packed request %c'${ id }'%c successfully.`
+             , [ this.DEBUGCSS.uuid, this.DEBUGCSS.normal ]
              );
     } else {
-      this.constructor.log(
+      this.log(
         [ "Dump of bad pack:"
         , { namespace: namespace
           , name: name
@@ -75,8 +77,70 @@ class MiddlewareClientDebug extends DebugLogger {
     }
   }
 
-  static logPythonTraceback ( requestID, args, originalRequest ) {
-    console.groupCollapsed( "%cRequest %c'" + requestID + "'%c caused a Python traceback", DEBUGCSS.errorColor, DEBUGCSS.idColor, DEBUGCSS.errorColor );
+  logNewSubscriptionMasks ( masks ) {
+    let logMasks = masks.length > 1
+                 ? _.clone( masks )
+                     .splice( masks.length - 1, 0, " and " )
+                     .join( ", " )
+                 : masks;
+
+    this.log( `Requested: Subscribe to %c'${ logMasks }'%c events`
+            , [ "args", "normal" ]
+            );
+  }
+
+  logSubscription ( subCount, mask ) {
+    if ( subCount > 0 ) {
+      this.info( `${ subCount } React components are currently ` +
+                 `subscribed to %c'${ mask }'%c events`
+               , [ "args", "normal" ]
+               );
+      this.log( `Increasing subscription count for %c'${ mask }'`, "args" );
+    } else {
+      this.info( `No React components are currently subscribed to ` +
+                 `%c'${ mask }'%c events`
+               , [ "args", "normal" ]
+               );
+      this.log( `Sending subscription request, and setting subscription ` +
+                `count for %c'${ mask }'%c to 1`
+              , [ "args", "normal" ]
+              );
+    }
+  }
+
+  logUnsubscribeMasks ( masks ) {
+    let logMasks = masks.length > 1
+                 ? _.clone( masks )
+                     .splice( masks.length - 1, 0, " and " )
+                     .join( ", " )
+                 : masks;
+
+    this.log( `Requested: Subscribe to %c'${ logMasks }'%c events`
+            , [ "args", "normal" ]
+            );
+  }
+
+  logUnsubscribe ( subCount, mask ) {
+    if ( subCount === 1 ) {
+      this.info( `Only one React component is currently subscribed to ` +
+                 `%c'${ mask }'%c events, so the subscription will be removed`
+               , [ "args", "normal" ]
+               );
+      this.log( `Sending unsubscribe request, and deleting subscription ` +
+                `count entry for %c'${ mask }'`
+              , "args"
+              );
+    } else {
+      this.info( `${ subCount } React components are currently subscribed ` +
+                 `to %c'${ mask }'%c events, and one will be unsubscribed`
+               , [ "args", "normal" ]
+               );
+      this.log( `Decreasing subscription count for %c'${ mask }'`, "args" );
+    }
+  }
+
+  logPythonTraceback ( requestID, args, originalRequest ) {
+    console.groupCollapsed( `%cRequest %c'${ requestID }'%c caused a Python traceback`, this.DEBUGCSS.error, this.DEBUGCSS.uuid, this.DEBUGCSS.error );
     if ( originalRequest ) {
       console.groupCollapsed( "Original request" );
       console.log( originalRequest );
@@ -85,12 +149,12 @@ class MiddlewareClientDebug extends DebugLogger {
     console.groupCollapsed( "Response data" );
     console.log( args );
     console.groupEnd();
-    console.log( "%c" + args.message , DEBUGCSS.codeStyle );
+    console.log( "%c" + args.message , this.DEBUGCSS.code );
     console.groupEnd();
   }
 
-  static logErrorWithCode ( requestID, args, originalRequest ) {
-    console.groupCollapsed( "%cERROR %s: Request %c'%s'%c returned: %s", DEBUGCSS.errorColor, args.code, DEBUGCSS.idColor, requestID, DEBUGCSS.errorColor, args.message );
+  logErrorWithCode ( requestID, args, originalRequest ) {
+    console.groupCollapsed( "%cERROR %s: Request %c'%s'%c returned: %s", this.DEBUGCSS.error, args.code, this.DEBUGCSS.uuid, requestID, this.DEBUGCSS.error, args.message );
     if ( originalRequest ) {
       console.groupCollapsed( "Original request" );
       console.log( originalRequest );
@@ -100,8 +164,8 @@ class MiddlewareClientDebug extends DebugLogger {
     console.groupEnd();
   }
 
-  static logErrorResponse ( requestID, args, originalRequest ) {
-    console.groupCollapsed( "%cERROR: Request %c'" + requestID + "'%c returned with an error status", DEBUGCSS.errorColor, DEBUGCSS.idColor, DEBUGCSS.errorColor );
+  logErrorResponse ( requestID, args, originalRequest ) {
+    console.groupCollapsed( `%cERROR: Request %c'${ requestID }'%c returned with an error status`, this.DEBUGCSS.error, this.DEBUGCSS.uuid, this.DEBUGCSS.error );
     if ( originalRequest ) {
       console.groupCollapsed( "Original request" );
       console.log( originalRequest );
@@ -113,4 +177,4 @@ class MiddlewareClientDebug extends DebugLogger {
 
 }
 
-export default new MiddlewareClientDebug( "MIDDLEWARE_CLIENT_DEBUG" );
+export default new MiddlewareClientDebug();
