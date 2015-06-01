41b030513bd1ce89b5392b8b9ad2349b678448af
Vaclav Navratil <vaclav@ixsystems.com>
2015-06-01 13:44:52 -0700

    JSCS clen up. Ticket: #9697

--- src/gui/app/jsx/components/Widget.jsx
+++ src/gui/app/jsx/components/Widget.jsx
@@ -11,19 +11,23 @@ var Widget = React.createClass({
   getInitialState: function () {
     return {  count              : 0
             , sizeArr            : [ "s", "m", "l" ]
-            , widgetContetnStyle : { width: this.props.dimensions[0], height: this.props.dimensions[1] - 16 }
+            , widgetContetnStyle : { width: this.props.dimensions[0]
+                                     , height: this.props.dimensions[1] - 16 }
     };
   }
 
   , changeSize: function () {
-    //console.log( "changeSize" );
+    // console.log( "changeSize" );
     var i = ( this.state.count < this.state.sizeArr.length ? this.state.count
                                                            : 0 );
-    //console.log( i );
+    // console.log( i );
     i++;
-    //console.log( i );
+    // console.log( i );
 
-    this.setState( {    size   : this.state.sizeArr[ i - 1 ] + this.state.size.substring( 1, this.state.size.length ), count  : i
+    this.setState( {    size   : this.state.sizeArr[ i - 1 ]
+                                 + this.state.size
+                                 .substring( 1, this.state.size.length )
+                        , count: i
                    } );
   }
 
@@ -36,7 +40,9 @@ var Widget = React.createClass({
     return (
       <div  ref = { this.props.refHolder }
             onMouseDown = { this.props.onMouseDownHolder }
-            className={"widget " + this.props.size + ( this.props.inMotion ? " in-motion" : "" ) }
+            className={"widget " + this.props.size + ( this.props.inMotion
+                                                        ? " in-motion"
+                                                        : "" ) }
             style= { widgetStyle }>
         <header>
           <span className="widgetTitle">
@@ -48,7 +54,8 @@ var Widget = React.createClass({
               onClick      = { this.changeSize } />
             </span>
         </header>
-        <div className="widget-content" style= { this.state.widgetContetnStyle }>
+        <div className="widget-content"
+             style= { this.state.widgetContetnStyle }>
           { this.props.children }
         </div>
       </div>
