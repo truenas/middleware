0b6ee70ca1cb11666fddda24d306633cffd50ce1
Alexander Motin <mav@FreeBSD.org>
2015-06-25 20:42:54 +0300

    Significantly rewrite ctl.conf generator.
    It should fix some old bugs and adapt it to the new iSCSI+FC block
    sharing world order.
    
    Ticket:	#9979
    (cherry picked from commit 95cceef815c1d44847e5d623b98794255251bccb)

--- src/freenas/usr/local/libexec/nas/generate_ctl_conf.py
+++ src/freenas/usr/local/libexec/nas/generate_ctl_conf.py
@@ -53,16 +53,44 @@ def addline(line, plaintextonly=False, shadowonly=False):
 
 
 def auth_group_config(auth_tag=None, auth_list=None, auth_type=None, initiator=None):
+    # First prepare all the lists, filtering out garpage.
+    if auth_list is None:
+        auth_list = []
+    inames = []
+    inets = []
+    if initiator:
+        if initiator.iscsi_target_initiator_initiators:
+            sep = "\n"
+            if "," in initiator.iscsi_target_initiator_initiators:
+                sep = ","
+            elif " " in initiator.iscsi_target_initiator_initiators:
+                sep = " "
+            inames = initiator.iscsi_target_initiator_initiators.strip('\n').split(sep)
+            inames = filter(lambda x:x != 'ALL', inames)
+        if initiator.iscsi_target_initiator_auth_network:
+            sep = "\n"
+            if "," in initiator.iscsi_target_initiator_auth_network:
+                sep = ","
+            elif " " in initiator.iscsi_target_initiator_auth_network:
+                sep = " "
+            inets = initiator.iscsi_target_initiator_auth_network.strip('\n').split(sep)
+            inets = filter(lambda x:x != 'ALL', inets)
+
+    # If nothing left after filtering, then we are done.
+    if not inames and not inets and not auth_list and (auth_type == 'None' or auth_type == 'auto'):
+        return False
+
+    # There are some real paremeters, so write the auth group.
     addline("auth-group ag%s {\n" % auth_tag)
+    for name in inames:
+        addline("""\tinitiator-name "%s"\n""" % name.lstrip())
+    for name in inets:
+        addline("""\tinitiator-portal "%s"\n""" % name.lstrip())
     # It is an error to mix CHAP and Mutual CHAP in the same auth group
     # But not in istgt, so we need to catch this and do something.
     # For now just skip over doing something that would cause ctld to bomb
-    rv = False
-    if auth_list is None:
-        auth_list = []
     for auth in auth_list:
         if auth.iscsi_target_auth_peeruser and auth_type != "CHAP":
-            rv = True
             auth_type = "Mutual"
             addline("\tchap-mutual %s \"%s\" %s \"%s\"\n" % (
                 auth.iscsi_target_auth_user,
@@ -72,41 +100,16 @@ def auth_group_config(auth_tag=None, auth_list=None, auth_type=None, initiator=N
             ), plaintextonly=True)
             addline("\tchap-mutual REDACTED REDACTED REDACTED REDACTED\n", shadowonly=True)
         elif auth_type != "Mutual":
-            rv = True
             auth_type = "CHAP"
             addline("\tchap %s \"%s\"\n" % (
                 auth.iscsi_target_auth_user,
                 auth.iscsi_target_auth_secret,
             ), plaintextonly=True)
             addline("\tchap REDACTED REDACTED\n", shadowonly=True)
-
-    if initiator:
-        if initiator.iscsi_target_initiator_initiators:
-            sep = "\n"
-            if "," in initiator.iscsi_target_initiator_initiators:
-                sep = ","
-            elif " " in initiator.iscsi_target_initiator_initiators:
-                sep = " "
-            for name in initiator.iscsi_target_initiator_initiators.strip('\n').split(sep):
-                if name == 'ALL':
-                    continue
-                rv = True
-                addline("""\tinitiator-name "%s"\n""" % name.lstrip())
-        if initiator.iscsi_target_initiator_auth_network:
-            sep = "\n"
-            if "," in initiator.iscsi_target_initiator_auth_network:
-                sep = ","
-            elif " " in initiator.iscsi_target_initiator_auth_network:
-                sep = " "
-            for name in initiator.iscsi_target_initiator_auth_network.strip('\n').split(sep):
-                if name == 'ALL':
-                    continue
-                rv = True
-                addline("""\tinitiator-portal "%s"\n""" % name.lstrip())
-        if rv and not auth_list and auth_type == 'None':
-            addline("\tauth-type \"none\"\n")
+    if not auth_list and (auth_type == 'None' or auth_type == 'auto'):
+        addline("\tauth-type \"none\"\n")
     addline("}\n\n")
-    return rv
+    return True
 
 
 def main():
@@ -118,6 +121,7 @@ def main():
     from freenasUI.services.models import iSCSITargetPortal
     from freenasUI.services.models import iSCSITargetPortalIP
     from freenasUI.services.models import iSCSITargetAuthCredential
+    from freenasUI.services.models import iSCSITargetExtent
     from freenasUI.services.models import iSCSITarget
     from freenasUI.storage.models import Disk
 
@@ -127,23 +131,23 @@ def main():
         for server in gconf.iscsi_isns_servers.split(' '):
             addline('isns-server %s\n\n' % server)
 
-    # We support multiple authentications for a single group
-    auths = defaultdict(list)
-    for auth in iSCSITargetAuthCredential.objects.order_by('iscsi_target_auth_tag'):
-        auths[auth.iscsi_target_auth_tag].append(auth)
-
-    for auth_tag, auth_list in auths.items():
-        auth_group_config(auth_tag, auth_list)
-
     # Generate the portal-group section
+    addline('portal-group default {\n}\n\n')
     for portal in iSCSITargetPortal.objects.all():
+        # Prepare auth group for the portal group
+        if portal.iscsi_target_portal_discoveryauthgroup:
+            auth_list = iSCSITargetAuthCredential.objects.filter(iscsi_target_auth_tag=portal.iscsi_target_portal_discoveryauthgroup)
+        else:
+            auth_list = []
+        agname = '4pg%d' % portal.iscsi_target_portal_tag
+        auth = auth_group_config(auth_tag=agname, auth_list=auth_list, auth_type=portal.iscsi_target_portal_discoveryauthmethod)
+
         addline("portal-group pg%s {\n" % portal.iscsi_target_portal_tag)
         addline("\tdiscovery-filter portal-name\n")
-        disc_authmethod = portal.iscsi_target_portal_discoveryauthmethod
-        if disc_authmethod == "None" or ((disc_authmethod == "Auto" or disc_authmethod == "auto") and portal.iscsi_target_portal_discoveryauthgroup is None):
-            addline("\tdiscovery-auth-group no-authentication\n")
+        if auth:
+            addline("\tdiscovery-auth-group ag%s\n" % agname)
         else:
-            addline("\tdiscovery-auth-group ag%s\n" % portal.iscsi_target_portal_discoveryauthgroup)
+            addline("\tdiscovery-auth-group no-authentication\n")
         listen = iSCSITargetPortalIP.objects.filter(iscsi_target_portalip_portal=portal)
         for obj in listen:
             if ':' in obj.iscsi_target_portalip_ip:
@@ -157,6 +161,78 @@ def main():
     poolthreshold = {}
     zpoollist = zfs.zpool_list()
 
+    # Generate the LUN section
+    for extent in iSCSITargetExtent.objects.all():
+        path = extent.iscsi_target_extent_path
+        poolname = None
+        lunthreshold = None
+        if extent.iscsi_target_extent_type == 'Disk':
+            disk = Disk.objects.filter(id=path).order_by('disk_enabled')
+            if not disk.exists():
+                continue
+            disk = disk[0]
+            if disk.disk_multipath_name:
+                path = "/dev/multipath/%s" % disk.disk_multipath_name
+            else:
+                path = "/dev/%s" % disk.identifier_to_device()
+        else:
+            if not path.startswith("/mnt"):
+                poolname = path.split('/', 2)[1]
+                if gconf.iscsi_pool_avail_threshold:
+                    if poolname in zpoollist:
+                        poolthreshold[poolname] = int(
+                            zpoollist.get(poolname).get('size') * (
+                                gconf.iscsi_pool_avail_threshold / 100.0
+                            )
+                        )
+                if extent.iscsi_target_extent_avail_threshold:
+                    zvolname = path.split('/', 1)[1]
+                    zfslist = zfs.zfs_list(path=zvolname, types=['volume'])
+                    if zfslist:
+                        lunthreshold = int(zfslist[zvolname].volsize * (extent.iscsi_target_extent_avail_threshold / 100.0))
+                path = "/dev/" + path
+            else:
+                if extent.iscsi_target_extent_avail_threshold and os.path.exists(path):
+                    try:
+                        stat = os.stat(path)
+                        lunthreshold = int(stat.st_size * (extent.iscsi_target_extent_avail_threshold / 100.0))
+                    except OSError:
+                        pass
+        addline("lun \"%s\" {\n" % extent.iscsi_target_extent_name)
+        size = extent.iscsi_target_extent_filesize
+        addline("\tpath %s\n" % path)
+        addline("\tblocksize %s\n" % extent.iscsi_target_extent_blocksize)
+        if extent.iscsi_target_extent_pblocksize:
+            addline("\toption pblocksize 0\n")
+        addline("\tserial %s\n" % (extent.iscsi_target_extent_serial, ))
+        padded_serial = extent.iscsi_target_extent_serial
+        if not extent.iscsi_target_extent_xen:
+            for i in xrange(31-len(extent.iscsi_target_extent_serial)):
+                padded_serial += " "
+        addline('\tdevice-id "iSCSI Disk      %s"\n' % padded_serial)
+        if size != "0":
+            if size.endswith('B'):
+                size = size.strip('B')
+            addline("\t\tsize %s\n" % size)
+        addline('\toption vendor "FreeBSD"\n')
+        addline('\toption product "iSCSI Disk"\n')
+        addline('\toption revision "0123"\n')
+        addline('\toption naa %s\n' % extent.iscsi_target_extent_naa)
+        if extent.iscsi_target_extent_insecure_tpc:
+            addline('\toption insecure_tpc on\n')
+            if lunthreshold:
+                addline('\toption avail-threshold %s\n' % lunthreshold)
+        if poolname is not None and poolname in poolthreshold:
+            addline('\toption pool-avail-threshold %s\n' % poolthreshold[poolname])
+        if extent.iscsi_target_extent_rpm == "Unknown":
+            addline('\toption rpm 0\n')
+        elif extent.iscsi_target_extent_rpm == "SSD":
+            addline('\toption rpm 1\n')
+        else:
+            addline('\toption rpm %s\n' % extent.iscsi_target_extent_rpm)
+        addline("}\n")
+        addline("\n")
+
     # Generate the target section
     target_basename = gconf.iscsi_basename
     for target in iSCSITarget.objects.all():
@@ -168,13 +244,8 @@ def main():
                 auth_list = iSCSITargetAuthCredential.objects.filter(iscsi_target_auth_tag=grp.iscsi_target_authgroup)
             else:
                 auth_list = []
-            agname = '4tg_%d_%d' % (
-                target.id,
-                grp.id,
-            )
-            _has_auth = auth_group_config(auth_tag=agname, auth_list=auth_list, auth_type=grp.iscsi_target_authtype, initiator=grp.iscsi_target_initiatorgroup)
-            has_auth |= _has_auth
-            if _has_auth:
+            agname = '4tg%d_%d' % (target.id, grp.id)
+            if auth_group_config(auth_tag=agname, auth_list=auth_list, auth_type=grp.iscsi_target_authtype, initiator=grp.iscsi_target_initiatorgroup):
                 authgroups[grp.id] = agname
         if (target.iscsi_target_name.startswith("iqn.") or
                 target.iscsi_target_name.startswith("eui.") or
@@ -187,22 +258,14 @@ def main():
         elif target.iscsi_target_name:
             addline("\talias \"%s\"\n" % target.iscsi_target_name)
 
-
         for fctt in target.fiberchanneltotarget_set.all():
             addline("\tport %s\n" % fctt.fc_port)
 
-        if target.iscsi_target_mode == 'fc':
-            addline("}\n\n")
-            continue
-
-        if not has_auth:
-            addline("\tauth-group no-authentication\n")
-
         for grp in target.iscsitargetgroups_set.all():
             agname = authgroups.get(grp.id) or None
-            addline("\tportal-group pg%d%s\n" % (
+            addline("\tportal-group pg%d %s\n" % (
                 grp.iscsi_target_portalgroup.iscsi_target_portal_tag,
-                ' ag' + agname if agname else '',
+                'ag' + agname if agname else 'no-authentication',
             ))
         used_lunids = [
             o.iscsi_lunid
@@ -215,87 +278,14 @@ def main():
             'null_first': 'iscsi_lunid IS NULL',
         }).order_by('null_first', 'iscsi_lunid'):
 
-            path = t2e.iscsi_extent.iscsi_target_extent_path
-            unmap = False
-            poolname = None
-            lunthreshold = None
-            if t2e.iscsi_extent.iscsi_target_extent_type == 'Disk':
-                disk = Disk.objects.filter(id=path).order_by('disk_enabled')
-                if not disk.exists():
-                    continue
-                disk = disk[0]
-                if disk.disk_multipath_name:
-                    path = "/dev/multipath/%s" % disk.disk_multipath_name
-                else:
-                    path = "/dev/%s" % disk.identifier_to_device()
-            else:
-                if not path.startswith("/mnt"):
-                    poolname = path.split('/', 2)[1]
-                    if gconf.iscsi_pool_avail_threshold:
-                        if poolname in zpoollist:
-                            poolthreshold[poolname] = int(
-                                zpoollist.get(poolname).get('size') * (
-                                    gconf.iscsi_pool_avail_threshold / 100.0
-                                )
-                            )
-                    if t2e.iscsi_extent.iscsi_target_extent_avail_threshold:
-                        zvolname = path.split('/', 1)[1]
-                        zfslist = zfs.zfs_list(path=zvolname, types=['volume'])
-                        if zfslist:
-                            lunthreshold = int(zfslist[zvolname].volsize * (t2e.iscsi_extent.iscsi_target_extent_avail_threshold / 100.0))
-                    path = "/dev/" + path
-                    unmap = True
-                else:
-                    if t2e.iscsi_extent.iscsi_target_extent_avail_threshold and os.path.exists(path):
-                        try:
-                            stat = os.stat(path)
-                            lunthreshold = int(stat.st_size * (t2e.iscsi_extent.iscsi_target_extent_avail_threshold / 100.0))
-                        except OSError:
-                            pass
-            if os.path.exists(path):
-                addline("\t\t\n")
-                if t2e.iscsi_lunid is None:
-                    while cur_lunid in used_lunids:
-                        cur_lunid += 1
-                    addline("\t\tlun %s {\n" % cur_lunid)
+            addline("\t\t\n")
+            if t2e.iscsi_lunid is None:
+                while cur_lunid in used_lunids:
                     cur_lunid += 1
-                else:
-                    addline("\t\tlun %s {\n" % t2e.iscsi_lunid)
-                size = t2e.iscsi_extent.iscsi_target_extent_filesize
-                if unmap:
-                    addline("\t\t\toption unmap on\n")
-                addline("\t\t\tpath %s\n" % path)
-                addline("\t\t\tblocksize %s\n" % t2e.iscsi_extent.iscsi_target_extent_blocksize)
-                if t2e.iscsi_extent.iscsi_target_extent_pblocksize:
-                    addline("\t\t\toption pblocksize 0\n")
-                addline("\t\t\tserial %s\n" % (t2e.iscsi_extent.iscsi_target_extent_serial, ))
-                padded_serial = t2e.iscsi_extent.iscsi_target_extent_serial
-                if not t2e.iscsi_extent.iscsi_target_extent_xen:
-                    for i in xrange(31-len(t2e.iscsi_extent.iscsi_target_extent_serial)):
-                        padded_serial += " "
-                addline('\t\t\tdevice-id "iSCSI Disk      %s"\n' % padded_serial)
-                if size != "0":
-                    if size.endswith('B'):
-                        size = size.strip('B')
-                    addline("\t\t\tsize %s\n" % size)
-                addline('\t\t\toption vendor "FreeBSD"\n')
-                addline('\t\t\toption product "iSCSI Disk"\n')
-                addline('\t\t\toption revision "0123"\n')
-                addline('\t\t\toption naa %s\n' % t2e.iscsi_extent.iscsi_target_extent_naa)
-                if t2e.iscsi_extent.iscsi_target_extent_insecure_tpc:
-                    addline('\t\t\toption insecure_tpc on\n')
-
-                if lunthreshold:
-                    addline('\t\t\toption avail-threshold %s\n' % lunthreshold)
-                if poolname is not None and poolname in poolthreshold:
-                    addline('\t\t\toption pool-avail-threshold %s\n' % poolthreshold[poolname])
-                if t2e.iscsi_extent.iscsi_target_extent_rpm == "Unknown":
-                    addline('\t\t\toption rpm 0\n')
-                elif t2e.iscsi_extent.iscsi_target_extent_rpm == "SSD":
-                    addline('\t\t\toption rpm 1\n')
-                else:
-                    addline('\t\t\toption rpm %s\n' % t2e.iscsi_extent.iscsi_target_extent_rpm)
-                addline("\t\t}\n")
+                addline("\tlun %s \"%s\"\n" % (cur_lunid, t2e.iscsi_extent.iscsi_target_extent_name))
+                cur_lunid += 1
+            else:
+                addline("\tlun %s \"%s\"\n" % (t2e.iscsi_lunid, t2e.iscsi_extent.iscsi_target_extent_name))
         addline("}\n\n")
 
     os.umask(077)
