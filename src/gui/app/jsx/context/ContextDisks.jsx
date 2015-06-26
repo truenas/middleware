0aa33f6f6f2e25ef14fdc4e0252aa811b1f89d68
Ben Milman <bpmilman@gmail.com>
2015-06-26 14:58:06 -0700

    Boilerplate in ContextDisks.
    Ticket: #10358

--- src/gui/app/jsx/context/ContextDisks.jsx
+++ src/gui/app/jsx/context/ContextDisks.jsx
@@ -6,16 +6,64 @@
 
 "use strict";
 
+const componentLongName = "ContextDisks";
+
 import React from "react";
 import _ from "lodash";
+
+import SS from "../stores/SchemaStore"
+import DS from "../stores/DisksStore";
+import DM from "../middleware/DisksMiddleware";
+import VS from "../stores/VolumeStore";
+import ZM from "../middleware/ZfsMiddleware";
 
 import DiskItemIcon from "../views/Storage/Disks/DiskItemIcon";
 
 const ContextDisks = React.createClass({
 
-  propTypes: { disks: React.PropTypes.object }
+  getInitialState () {
+    return ( { disks: DS.disksArray
+             , availableDisks: VS.availableDisks
+             , diskSchema: SS.getDef( "disk" )
+             }
+           );
+  }
 
-  , render: function () {
+  , componentDidMount () {
+    DS.addChangeListener( this.handleDisksChange );
+    DM.subscribe( componentLongName );
+    DM.requestDisksOverview();
+
+    VS.addChangeListener( this.handleVolumeChange );
+
+    ZM.subscribe( componentLongName );
+
+    ZM.requestVolumes();
+    ZM.requestAvailableDisks();
+  }
+
+  , componentWillUnmount () {
+    DS.removeChangeListener( this.handleDisksChange );
+    DM.unsubscribe( componentLongName );
+
+    VS.removeChangeListener( this.handleVolumeChange );
+
+    ZM.unsubscribe( componentLongName );
+  }
+
+  , handleDisksChange () {
+    this.setState( { disks: DS.disksArray
+                   , availableDisks: VS.availableDisks
+                   }
+                 );
+  }
+
+  , handleVolumeChange () {
+    this.setState( { availableDisks: VS.availableDisks } );
+  }
+
+  , render () {
+
     return (
       <h4>
         { "Available Disks" }
