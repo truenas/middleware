5841efd3e51bc1a29bdc1ac297d30b8f7de92c0f
John Hixson <john@ixsystems.com>
2015-06-30 00:43:28 -0700

    Allow rfc2307 and ldap backend to use SSL and TLS
    - This was necessary to get FreeNAS working with centrify LDAP proxy
    
    Ticket: #6855
    (cherry picked from commit 46fa7e03fe6c7ea790e70e021eaa609473abc5a3)

--- src/freenas/usr/local/libexec/nas/generate_ldap_conf.py
+++ src/freenas/usr/local/libexec/nas/generate_ldap_conf.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 
 import os
+import re
 import string
 import sys
 
@@ -25,6 +26,7 @@ from freenasUI.common.system import (
     ldap_enabled
 )
 from freenasUI.directoryservice import models
+from freenasUI.directoryservice.utils import get_idmap_object
 
 def ldap_conf_ldap(ldap_conf):
     try:
@@ -50,20 +52,46 @@ def ldap_conf_ldap(ldap_conf):
 
 def ldap_conf_activedirectory(ldap_conf):
     ad = FreeNAS_ActiveDirectory(flags=FLAGS_DBINIT)
-    
-    f = open(ldap_conf, "w")
-    f.write("URI %s://%s\n" % (
+
+    config = { }
+    config["URI"] = "%s://%s" % (
         "ldaps" if ad.ssl == "on" else "ldap",
         ad.domainname
-    ))
-    f.write("BASE %s\n" % ad.basedn)
+    )
+    config["BASE"] = ad.basedn
 
     if ad.ssl in ("start_tls", "on"):
         if ad.certfile: 
-            f.write("TLS_CACERT %s\n" % ad.certfile)
-        f.write("TLS_REQCERT allow\n")
+            config["TLS_CACERT"] = ad.certfile
+        config["TLS_REQCERT"] = "allow"
 
-    f.close()
+    #
+    # So what if the AD server is configured to use SSL or TLS,
+    # and the idmap backend is as well? WTF? whaddoyoudo?
+    #
+    ad = models.ActiveDirectory.objects.all()[0]
+    if ad.ad_idmap_backend in ("rfc2307", "ldap"):
+        idmap = get_idmap_object(ad.ds_type, ad.id, ad.ad_idmap_backend)
+        idmap_url = idmap.get_url()  
+        idmap_url = re.sub('^(ldaps?://)', '', idmap_url)
+
+        config["URI"] = "%s://%s" % (
+            "ldaps" if idmap.get_ssl() == "on" else "ldap",
+            idmap_url
+        )
+
+        if idmap.get_ssl() in ('start_tls', 'on'):
+            capath = get_certificateauthority_path(idmap.get_certificate())
+            if capath:
+                config["TLS_CACERT"] = capath
+            config["TLS_REQCERT"] = "allow"
+
+    keys = ["URI", "BASE", "TLS_CACERT", "TLS_REQCERT"]
+    with open(ldap_conf, "w") as f:
+        for key in keys:
+            if key in config:
+                f.write("%s %s\n" % (key, config[key]))
+        f.close()
     os.chmod(ldap_conf, 0644)
 
 def main():
