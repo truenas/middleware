d5bc5bcd7bb55922c178f3691a57d687ffa60d31
Suraj Ravichandran <suraj@ixsystems.com>
2015-07-06 09:55:41 -0700

    Add 'sessions.get_live_user_sessions' RPC.
    This is a helper method to make it easy for the gui to get just
    the "user" sessions that are currently live.
    
    Ticket: #7964

--- src/dispatcher/plugins/SessionPlugin.py
+++ src/dispatcher/plugins/SessionPlugin.py
@@ -27,14 +27,35 @@
 
 import time
 from task import Provider, query
-from dispatcher.rpc import description, pass_sender, returns
+from dispatcher.rpc import (description, pass_sender, returns, accepts,
+                            SchemaHelper as h)
 
 
 @description("Provides Information about the current loggedin Session")
 class SessionProvider(Provider):
     @query('session')
     def query(self, filter=None, params=None):
-        return self.datastore.query('sessions', *(filter or []), **(params or {}))
+        return self.datastore.query('sessions',
+                                    *(filter or []),
+                                    **(params or {}))
+
+    @accepts()
+    @returns(h.array(h.ref('sessions')))
+    @description("Returns the logged in and active user sessions" +
+                 "Does not include the service sessions in this.")
+    def get_live_user_sessions(self):
+        live_user_session_ids = []
+        for conn in self.dispatcher.ws_server.connections:
+            # The if check for 'uid' below is to seperate the actuall gui/cli
+            # users of the websocket connection from that of system services
+            # like etcd, statd and so on.
+            if hasattr(conn.user, 'uid'):
+                live_user_session_ids.append(conn.session_id)
+        return self.datastore.query('sessions',
+                                    ('id',
+                                     'in',
+                                     live_user_session_ids),
+                                    **({}))
 
     @description("Returns the loggedin user for the current session")
     @returns(str)
