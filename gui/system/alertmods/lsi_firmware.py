066e3770ddc80fdadbfc149fe62cd192c43816eb
Jakub Klama <jakub@ixsystems.com>
2015-07-20 19:22:47 +0200

    Catch OSEerror raised by new py-sysctl module when sysctl is not found.

--- gui/system/alertmods/lsi_firmware.py
+++ gui/system/alertmods/lsi_firmware.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import errno
 import sysctl
 
 from django.utils.translation import ugettext as _
@@ -11,35 +12,42 @@ class LSIFirmwareAlert(BaseAlert):
     def run(self):
         alerts = []
         mps = defaultdict(dict)
-        for k, v in sysctl.filter('dev.mps'):
-            mibs = k.split('.', 3)
-            if len(mibs) < 4:
-                continue
+        try:
+            for k, v in sysctl.filter('dev.mps'):
+                mibs = k.split('.', 3)
+                if len(mibs) < 4:
+                    continue
 
-            number, mib = mibs[2:4]
+                number, mib = mibs[2:4]
 
-            try:
-                major = int(v.split('.', 1)[0])
-                mps[number][mib] = major
-            except:
-                continue
+                try:
+                    major = int(v.split('.', 1)[0])
+                    mps[number][mib] = major
+                except:
+                    continue
 
-        for number, mibs in mps.items():
-            firmware = mibs.get('firmware_version')
-            driver = mibs.get('driver_version')
-            if firmware != driver:
-                alerts.append(Alert(
-                    Alert.WARN,
-                    _(
-                        'Firmware version %(fwversion)s does not match driver '
-                        'version %(drversion)s for /dev/mps%(mps)s'
-                    ) % {
-                        'fwversion': firmware,
-                        'drversion': driver,
-                        'mps': number,
-                    }
-                ))
+            for number, mibs in mps.items():
+                firmware = mibs.get('firmware_version')
+                driver = mibs.get('driver_version')
+                if firmware != driver:
+                    alerts.append(Alert(
+                        Alert.WARN,
+                        _(
+                            'Firmware version %(fwversion)s does not match driver '
+                            'version %(drversion)s for /dev/mps%(mps)s'
+                        ) % {
+                            'fwversion': firmware,
+                            'drversion': driver,
+                            'mps': number,
+                        }
+                    ))
 
-        return alerts
+            return alerts
+        except OSError, err:
+            if err.errno == errno.ENOENT:
+                # No hardware present
+                return []
+
+            raise
 
 alertPlugins.register(LSIFirmwareAlert)
