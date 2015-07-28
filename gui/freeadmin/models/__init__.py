45f544cf4cbc2119fc0ac50c3188f92533cfe4b0
William Grzybowski <wg@FreeBSD.org>
2015-07-28 09:22:31 -0300

    Add a very simple QuerySet that will fetch data using new middleware
    Ticket:	#10660

--- gui/freeadmin/models/__init__.py
+++ gui/freeadmin/models/__init__.py
@@ -92,3 +92,149 @@ class Model(models.Model):
             self._meta.app_label,
             self._meta.model_name,
             ), )
+
+
+FIELD2MIDDLEWARE = {
+    'bsdgroups': {
+        'bsdgrp_group': 'name',
+        'bsdgrp_builtin': 'builtin',
+        'bsdgrp_gid': 'id',
+        'bsdgrp_sudo': 'sudo',
+        'id': 'id',
+    },
+}
+
+MIDDLEWARE2FIELD = {
+    'bsdgroups': {
+        'name': 'bsdgrp_group',
+        'builtin': 'bsdgrp_builtin',
+        'id': ('id', 'bsdgrp_gid'),
+        'sudo': 'bsdgrp_sudo',
+    },
+}
+
+MIDDLEWARE_MODEL_METHODS = {
+    'bsdgroups': {
+        'query': 'groups.query',
+        'add': 'groups.create',
+        'delete': 'groups.delete',
+        'update': 'groups.update',
+    }
+}
+
+
+def get_middleware_methods(model):
+    methods = MIDDLEWARE_MODEL_METHODS.get(model._meta.model_name)
+    if methods is None:
+        raise NotImplementedError("RPC methods for '%s' not defined'" % (
+            model._meta.model_name,
+        ))
+    return methods
+
+
+class NewQuerySet(object):
+
+    def __init__(self, model, **kwargs):
+        self.model = model
+        self._result_cache = None
+        self._filters = []
+        self._f2m = FIELD2MIDDLEWARE.get(
+            self.model._meta.model_name
+        )
+        self._m2f = MIDDLEWARE2FIELD.get(
+            self.model._meta.model_name
+        )
+
+    def __iter__(self):
+        self._fetch_all()
+        return iter(self._result_cache)
+
+    def __getitem__(self, k):
+        """
+        Retrieves an item or slice from the set of results.
+        """
+        if not isinstance(k, (slice,) + six.integer_types):
+            raise TypeError
+
+        self._fetch_all()
+        if self._result_cache is not None:
+            return self._result_cache[k]
+
+    def __len__(self):
+        self._fetch_all()
+        return len(self._result_cache)
+
+    def _clone(self, klass=None, setup=False, **kwargs):
+        if klass is None:
+            klass = self.__class__
+        c = klass(model=self.model)
+        return c
+
+    def _fetch_all(self):
+        if self._result_cache is None:
+            self._result_cache = list(self.iterator())
+
+    def count(self):
+        self._fetch_all()
+        return len(self._result_cache)
+
+    def filter(self, *args, **kwargs):
+        return self._exclude_or_filter(False, *args, **kwargs)
+
+    def _exclude_or_filter(self, opposite, *args, **kwargs):
+        for key, val in kwargs.items():
+            _filter = 'exact'
+            if '__' in key:
+                key, _filter = key.split('__')
+
+            if key == 'pk':
+                key = 'id'
+
+            val = self.model._meta.get_field(key).to_python(val)
+
+            if self._f2m:
+                field = self._f2m.get(key)
+                if field is None:
+                    raise NotImplementedError("Field '%s' not mapped" % key)
+            else:
+                field = key
+
+            if _filter == 'exact':
+                self._filters.append(
+                    (field, '=' if not opposite else '!=', val)
+                )
+            else:
+                raise NotImplementedError("Filter '%s' not implemented" % _filter)
+
+        return self
+
+    def iterator(self):
+        from freenasUI.middleware.connector import connection as dispatcher
+        print self._filters
+        methods = get_middleware_methods(self.model)
+        method = methods.get('query')
+        if method is None:
+            raise NotImplementedError("RPC query method for '%s' not defined'" % (
+                self.model._meta.model_name,
+            ))
+
+        options = {}
+        if self._sort is not None:
+            options['sort'] = self._sort
+        if self._dir is not None:
+            options['dir'] = self._dir
+
+        for i in dispatcher.call_sync(method, self._filters, options):
+            data = {}
+            if self._m2f:
+                for key, val in i.items():
+                    field = self._m2f.get(key)
+                    if isinstance(field, tuple):
+                        for f in field:
+                            data[f] = val
+                    elif isinstance(field, str):
+                        data[field] = val
+                    elif field is None:
+                        continue
+
+            yield self.model(**data)
