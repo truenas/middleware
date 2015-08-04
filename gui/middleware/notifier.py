fb1564f8d1254c73c309afe526acdb40743c8512
Xin Li <delphij@FreeBSD.org>
2015-08-04 11:57:09 -0700

    Revert "Fix AFP not stopping properly"
    This reverts commit 636a5f8eb09af85c1ac233dd37263bb2f79b4282.
    
    According to Suraj this should be reverted.

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -869,8 +869,6 @@ class notifier:
 
     def _stop_afp(self):
         self._system("/usr/sbin/service netatalk forcestop")
-        self._system("/bin/pkill afpd")
-        self._system("/bin/pkill cnid_metad")
 
     def _restart_afp(self):
         self._stop_afp()
