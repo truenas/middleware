730f402a761cfb29559d0c8c93765a58539f311d
Josh Paetzel <josh@ixsystems.com>
2015-08-07 10:48:53 -0500

    Add alerts for 12 Gbps avago HBA firmware mismatches.
    Along the way chase the LSI -> Avago name changes.
    (I'll never forget you LSI *sniff*)
    
    Ticket:	#10908
    (cherry picked from commit 82e40af9ee76612988c623f66606b772f011935d)

---
+++ gui/system/alertmods/mpr_firmware.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+import sysctl
+
+from django.utils.translation import ugettext as _
+
+from freenasUI.system.alert import alertPlugins, Alert, BaseAlert
+
+
+class MPRFirmwareAlert(BaseAlert):
+
+    def run(self):
+        alerts = []
+        mpr = defaultdict(dict)
+        for o in sysctl.filter('dev.mpr'):
+            mibs = o.name.split('.', 3)
+            if len(mibs) < 4:
+                continue
+
+            number, mib = mibs[2:4]
+
+            try:
+                major = int(o.value.split('.', 1)[0])
+                mpr[number][mib] = major
+            except:
+                continue
+
+        for number, mibs in mpr.items():
+            firmware = mibs.get('firmware_version')
+            driver = mibs.get('driver_version')
+            if firmware != driver:
+                alerts.append(Alert(
+                    Alert.WARN,
+                    _(
+                        'Firmware version %(fwversion)s does not match driver '
+                        'version %(drversion)s for /dev/mpr%(mpr)s'
+                    ) % {
+                        'fwversion': firmware,
+                        'drversion': driver,
+                        'mpr': number,
+                    }
+                ))
+
+        return alerts
+
+alertPlugins.register(MPRFirmwareAlert)
