abab2c119fc204ae9b3f4e3255e4550f0e72d14e
dlavigne <dru@freebsd.org>
2015-08-25 12:46:25 -0400

    Test of new sphinx version on doc build server.

--- docs/userguide/conf.py
+++ docs/userguide/conf.py
@@ -25,7 +25,13 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.httpdomain']
+extensions = ['sphinxcontrib.httpdomain',
+	      'sphinx.ext.numfig'
+]
+
+# -- Options for automatic Figure numbering
+numfig = True
+numfig_secnum_depth = (2)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -94,7 +100,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
