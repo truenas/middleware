ab8055749fc174cf1eef593216a0e19a0ea6176a
Jakub Klama <jakub@ixsystems.com>
2015-09-18 18:18:58 +0200

    Make multipath status alert use dispatcher query.

--- gui/system/alertmods/multipaths_status.py
+++ gui/system/alertmods/multipaths_status.py
@@ -1,16 +1,21 @@
 from django.utils.translation import ugettext as _
 
 from freenasUI.middleware.notifier import notifier
+from freenasUI.middleware.connector import connection as dispatcher
 from freenasUI.system.alert import alertPlugins, Alert, BaseAlert
+from fnutils.query import wrap
 
 
 class MultipathAlert(BaseAlert):
 
     def run(self):
         not_optimal = []
-        for mp in notifier().multipath_all():
-            if mp.status != 'OPTIMAL':
-                not_optimal.append(mp.name)
+        for disk in wrap(dispatcher.call_sync('disks.query')):
+            if not disk.get('is_multipath'):
+                continue
+
+            if disk['multipath.status'] != 'OPTIMAL':
+                not_optimal.append(disk['path'])
 
         if not_optimal:
             return [
