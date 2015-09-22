e0067d5bca4e229b4d0fd4a70b34f6ad234d9824
William Grzybowski <wg@FreeBSD.org>
2015-09-22 11:44:36 -0300

    Its not job of the GUI to restart nfs
    Ticket:	#11609

--- gui/services/forms.py
+++ gui/services/forms.py
@@ -341,13 +341,6 @@ class NFSForm(ModelForm):
         obj = super(NFSForm, self).save(commit=False)
         obj.nfs_srv_bindip = self.cleaned_data.get('nfs_srv_bindip')
         obj.save()
-        started = notifier().restart("nfs")
-        if (
-            started is False
-            and
-            models.services.objects.get(srv_service='nfs').srv_enable
-        ):
-            raise ServiceFailed("nfs", _("The NFS service failed to reload."))
 
 
 class FTPForm(ModelForm):
