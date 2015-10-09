c0c4dbc20ae6616949865f213b809c04cce4af23
William Grzybowski <wg@FreeBSD.org>
2015-10-09 16:18:39 -0300

    Allow zvol to be deleted from API
    Ticket:	#11875
    (cherry picked from commit eaf959089ce46bdcd79185e6092777da8d4f2a36)
    (cherry picked from commit 7cdada8c2ffee37e9c39fe3d4d709d0a9cdd36e2)

--- gui/api/resources.py
+++ gui/api/resources.py
@@ -445,6 +445,14 @@ class VolumeResourceMixin(NestedMixin):
                 name="api_volume_zvols"
             ),
             url(
+                r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/zvols/"
+                "(?P<pk2>\w[\w/-]*)%s$" % (
+                    self._meta.resource_name, trailing_slash()
+                ),
+                self.wrap_view('zvols_detail'),
+                name="api_volume_zvols_detail"
+            ),
+            url(
                 r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/replace%s$" % (
                     self._meta.resource_name, trailing_slash()
                 ),
@@ -829,6 +837,13 @@ class VolumeResourceMixin(NestedMixin):
 
         child_resource = ZVolResource()
         return child_resource.dispatch_list(request, parent=obj)
+
+    def zvols_detail(self, request, **kwargs):
+        pk = kwargs.pop('pk2')
+        bundle, obj = self._get_parent(request, kwargs)
+
+        child_resource = ZVolResource()
+        return child_resource.dispatch_detail(request, pk=pk, parent=obj)
 
     def _get_children(self, bundle, vol, children, uid):
         rv = []
