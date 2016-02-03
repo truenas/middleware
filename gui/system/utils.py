3fc334d4184871ec9e8799e7ac18791aed5272b2
William Grzybowski <wg@FreeBSD.org>
2016-02-03 13:38:00 -0200

    Add API endpoint to generate debug
    Ticket:	#13017

--- gui/system/utils.py
+++ gui/system/utils.py
@@ -27,6 +27,7 @@ import json
 import logging
 import os
 import re
+import tarfile
 import time
 from datetime import timedelta
 from uuid import uuid4
@@ -457,3 +458,34 @@ def debug_run(direc):
     opts = ["/usr/local/bin/ixdiagnose", "-d", direc, "-s", "-F"]
     p1 = pipeopen(' '.join(opts), allowfork=True)
     p1.communicate()
+
+
+def debug_generate():
+    from freenasUI.common.locks import mntlock
+    from freenasUI.middleware.notifier import notifier
+    from freenasUI.network.models import GlobalConfiguration
+    _n = notifier()
+    gc = GlobalConfiguration.objects.all().order_by('-id')[0]
+    mntpt, direc, dump = debug_get_settings()
+
+    standby_debug = None
+    if not _n.is_freenas() and _n.failover_licensed():
+        s = _n.failover_rpc()
+        standby_debug = s.debug()
+
+    with mntlock(mntpt=mntpt):
+
+        debug_run(direc)
+
+        if standby_debug:
+            debug_file = '%s/debug.tar' % direc
+            _n.sync_file_recv(s, standby_debug, '%s/standby.txz' % direc)
+            if _n.failover_node() == 'B':
+                hostname_a = gc.gc_hostname
+                hostname_b = gc.gc_hostname_b
+            else:
+                hostname_a = gc.gc_hostname_b
+                hostname_b = gc.gc_hostname
+            with tarfile.open(debug_file, 'w') as tar:
+                tar.add('%s/standby.txz' % direc, '%s.txz' % hostname_b)
+                tar.add(dump, '%s.txz' % hostname_a)
