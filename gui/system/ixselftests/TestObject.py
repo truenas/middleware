c221143d14efb02581311403a4de509302a56d71
Sean Eric Fagan <sef@ixsystems.com>
2016-02-11 11:51:51 -0800

    Initial attempt to put self-tests into freenas.  This contains three tests (DNS, Replication, and ZFS), and some infrastructure to run them.  Also to generate alerts.  Which is questionable, but it seems to match other parts of the system.
    Not going into anything other than master just yet.
    
    Ticket:	#13138
    Merge-FN931:
    Merge-TN931:

---
+++ gui/system/ixselftests/TestObject.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import os, sys
+
+from . import (ALERT_FILE, TEST_PASS, TEST_WARNING, TEST_FAIL, TEST_CRITICAL)
+from . import (_module_dir, _module_name)
+from . import TestStatus
+
+class __handler(TestStatus):
+    """
+    Default handler for tests, when a real one isn't
+    specified.  This only prints to stdout, and will never
+    set (or remove) an alert.
+    """
+    def __init__(self, verbose=False, alert=False):
+        super(self.__class__, self).__init__(verbose, alert)
+        self._alert = False
+
+class TestObject(object):
+    def __init__(self, handler=None):
+        if handler is None:
+            self._handler = __handler(verbose = True)
+        else:
+            self._handler = handler
+
+    def Enabled(self):
+        return True
+
+    def Test(self):
+        return True
+
+    def Requires(self):
+        return []
+
+    
