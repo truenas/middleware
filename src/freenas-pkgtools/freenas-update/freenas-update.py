cdf54b2bfd72b2400f08013d758480d4c6178b8a
Suraj Ravichandran <suraj@ixsystems.com>
2016-03-01 19:22:22 -0800

    Make `freenas-update update/check` use a progress bar instead of scrolling `TryGetNetworkFile` prints.
    One can go back to the earlier versobe download prints via the `--verbose` flag i.e.:
    `freenas-update --verbose update/check`
    
    Ticket: #12731
    (cherry picked from commit 87f01c0d8b22dca1a8b49a75ed1025fcc5eb37e8)

--- src/freenas-pkgtools/freenas-update/freenas-update.py
+++ src/freenas-pkgtools/freenas-update/freenas-update.py
@@ -6,6 +6,11 @@ import logging.config
 import os
 import sys
 import tempfile
+sys.path.append("/usr/local/lib")
+import freenasOS.Configuration as Configuration
+import freenasOS.Manifest as Manifest
+import freenasOS.Update as Update
+
 
 def PrintDifferences(diffs):
     for type in diffs:
@@ -13,41 +18,174 @@ def PrintDifferences(diffs):
             pkg_diffs = diffs[type]
             for (pkg, op, old) in pkg_diffs:
                 if op == "delete":
-                    print >> sys.stderr, "Delete package %s" % pkg.Name()
+                    print >> sys.stderr, "Delete package {0}".format(pkg.Name())
                 elif op == "install":
-                    print >> sys.stderr, "Install package %s-%s" % (pkg.Name(), pkg.Version())
+                    print >> sys.stderr, "Install package {0}-{1}".format(
+                        pkg.Name(), pkg.Version()
+                    )
                 elif op == "upgrade":
-                    print >> sys.stderr, "Upgrade package %s %s->%s" % (pkg.Name(), old.Version(), pkg.Version())
+                    print >> sys.stderr, "Upgrade package {0} {1}->{2}".format(
+                        pkg.Name(), old.Version(), pkg.Version()
+                    )
                 else:
-                    print >> sys.stderr, "Unknown package operation %s for packge %s-%s" % (op, pkg.Name(), pkg.Version())
+                    print >> sys.stderr, "Unknown package operation {0} for packge {1}-{2}".format(
+                        op, pkg.Name(), pkg.Version()
+                    )
         elif type == "Restart":
             from freenasOS.Update import GetServiceDescription
             for svc in diffs[type]:
                 desc = GetServiceDescription(svc)
                 if desc:
-                    print "%s" % desc
+                    print desc
                 else:
-                    print "Unknown service restart %s?!" % svc
+                    print "Unknown service restart {0}?!".format(svc)
         elif type in ("Train", "Sequence"):
             # Train and Sequence are a single tuple, (old, new)
             old, new = diffs[type]
-            print >> sys.stderr, "%s %s -> %s" % (type, old, new)
+            print >> sys.stderr, "{0} {1} -> {2}".format(type, old, new)
         elif type == "Reboot":
             rr = diffs[type]
-            print >> sys.stderr, "Reboot is (conditionally) %srequired" % ("" if rr else "not ")
+            print >> sys.stderr, "Reboot is (conditionally) {0}required".format(
+                "" if rr else "not "
+            )
         else:
-            print >> sys.stderrr, "*** Unknown key %s (value %s)" % (type, str(diffs[type]))
+            print >> sys.stderr, "*** Unknown key {0} (value {1})".format(type, str(diffs[type]))
+
+
+class ProgressBar(object):
+    def __init__(self):
+        self.message = None
+        self.percentage = 0
+        self.write_stream = sys.stderr
+        self.write_stream.write('\n')
+
+    def draw(self):
+        progress_width = 40
+        filled_width = int(self.percentage * progress_width)
+        self.write_stream.write('\033[2K\033[A\033[2K\r')
+        self.write_stream.write('Status: {}\n'.format(self.message))
+        self.write_stream.write('Total Progress: [{}{}] {:.2%}'.format(
+            '#' * filled_width,
+            '_' * (progress_width - filled_width),
+            self.percentage))
+
+        self.write_stream.flush()
+
+    def update(self, percentage=None, message=None):
+        if percentage:
+            self.percentage = float(percentage / 100.0)
+
+        if message:
+            self.message = message
+
+        self.draw()
+
+    def finish(self):
+        self.percentage = 1
+        self.draw()
+        self.write_stream.write('\n')
+
+
+class UpdateHandler(object):
+    "A handler for Downloading and Applying Updates calls"
+
+    def __init__(self, update_progress=None):
+        self.progress = 0
+        self.details = ''
+        self.finished = False
+        self.error = False
+        self.indeterminate = False
+        self.reboot = False
+        self.pkgname = ''
+        self.pkgversion = ''
+        self.operation = ''
+        self.filesize = 0
+        self.numfilestotal = 0
+        self.numfilesdone = 0
+        self._baseprogress = 0
+        self.master_progress = 0
+        # Below is the function handle passed to this by the caller so that
+        # its status and progress can be updated accordingly
+        self.update_progress = update_progress
+
+    def check_handler(self, index, pkg, pkgList):
+        self.pkgname = pkg.Name()
+        self.pkgversion = pkg.Version()
+        self.operation = 'Downloading'
+        self.details = 'Downloading {0}'.format(self.pkgname)
+        stepprogress = int((1.0 / float(len(pkgList))) * 100)
+        self._baseprogress = index * stepprogress
+        self.progress = (index - 1) * stepprogress
+
+    def get_handler(self, method, filename, size=None, progress=None, download_rate=None):
+        if progress is not None:
+            self.progress = (progress * self._baseprogress) / 100
+            if self.progress == 0:
+                self.progress = 1
+            display_size = ' Size: {0}'.format(size) if size else ''
+            display_rate = ' Rate: {0} B/s'.format(download_rate) if download_rate else ''
+            self.details = 'Downloading: {0} Progress:{1}{2}{3}'.format(
+                self.pkgname, progress, display_size, display_rate
+                )
+
+        # Doing the drill below as there is a small window when
+        # step*progress logic does not catch up with the new value of step
+        if self.progress >= self.master_progress:
+            self.master_progress = self.progress
+        if self.update_progress is not None:
+            self.update_progress(self.master_progress, self.details)
+
+
+def download_update_call(train, download_dir, pkg_type, verbose=False):
+    if not verbose:
+        progress_bar = ProgressBar()
+        handler = UpdateHandler(progress_bar.update)
+        rv = Update.DownloadUpdate(
+            train,
+            download_dir,
+            get_handler=handler.get_handler,
+            check_handler=handler.check_handler,
+            pkg_type=pkg_type,
+        )
+        progress_bar.finish()
+    else:
+        rv = Update.DownloadUpdate(
+            train,
+            download_dir,
+            pkg_type=pkg_type,
+        )
+    return rv
+
+
+# Lets try to write a logging Filter to sift "TryGetNetworkFile" logs
+class StartsWithFilter(logging.Filter):
+    def __init__(self, params):
+        self.params = params
+
+    def filter(self, record):
+        if self.params:
+            allow = not any(record.msg.startswith(x) for x in self.params)
+        else:
+            allow = True
+        return allow
+
 
 def main():
     global log
 
-    logging.config.dictConfig({
+    log_config_dict = {
         'version': 1,
-        'disable_existing_loggers': True,
+        'disable_existing_loggers': False,
         'formatters': {
             'simple': {
                 'format': '[%(name)s:%(lineno)s] %(message)s',
             },
+        },
+        'filters': {
+            'cleandownload': {
+                '()': StartsWithFilter,
+                'params': ['TryGetNetworkFile', 'Searching']
+            }
         },
         'handlers': {
             'std': {
@@ -62,16 +200,8 @@ def main():
                 'level': 'DEBUG',
                     'propagate': True,
             },
-        },
-    })
-
-    log = logging.getLogger('freenas-update')
-
-    sys.path.append("/usr/local/lib")
-
-    import freenasOS.Configuration as Configuration
-    import freenasOS.Manifest as Manifest
-    import freenasOS.Update as Update
+        }
+    }
 
     def usage():
         print >> sys.stderr, """Usage: %s [-C cache_dir] [-d] [-T train] [--no-delta] [-v] <cmd>, where cmd is one of:
@@ -81,12 +211,13 @@ def main():
 
     try:
         short_opts = "C:dT:v"
-        long_opts = [ "cache=",
-                      "debug",
-                      "train=",
-                      "verbose",
-                      "no-delta"
-                      ]
+        long_opts = [
+            "cache=",
+            "debug",
+            "train=",
+            "verbose",
+            "no-delta"
+        ]
         opts, args = getopt.getopt(sys.argv[1:], short_opts, long_opts)
     except getopt.GetoptError as err:
         print str(err)
@@ -98,7 +229,7 @@ def main():
     cache_dir = None
     train = None
     pkg_type = None
-    
+
     for o, a in opts:
         if o in ("-v", "--verbose"):
             verbose = True
@@ -111,7 +242,13 @@ def main():
         elif o in ("--no-delta"):
             pkg_type = Update.PkgFileFullOnly
         else:
-            assert False, "unhandled option %s" % o
+            assert False, "unhandled option {0}".format(o)
+
+    if not verbose:
+        log_config_dict['handlers']['std']['filters'] = ['cleandownload']
+    logging.config.dictConfig(log_config_dict)
+
+    log = logging.getLogger('freenas-update')
 
     config = Configuration.Configuration()
     if train is None:
@@ -126,16 +263,16 @@ def main():
         # given a cache directory, we pass that in; otherwise,
         # we make a temporary directory and use that.  We
         # have to clean up afterwards in that case.
-        
+
         if cache_dir is None:
-            download_dir = tempfile.mkdtemp(prefix = "UpdateCheck-", dir = config.TemporaryDirectory())
+            download_dir = tempfile.mkdtemp(prefix="UpdateCheck-", dir=config.TemporaryDirectory())
             if download_dir is None:
                 print >> sys.stderr, "Unable to create temporary directory"
                 sys.exit(1)
         else:
             download_dir = cache_dir
 
-        rv = Update.DownloadUpdate(train, download_dir, pkg_type = pkg_type)
+        rv = download_update_call(train, download_dir, pkg_type, verbose)
         if rv is False:
             if verbose:
                 print "No updates available"
@@ -171,21 +308,24 @@ def main():
             if o in ("-R", "--reboot"):
                 force_reboot = True
             else:
-                assert False, "Unhandled option %s" % o
-        
+                assert False, "Unhandled option {0}".format(o)
+
         if cache_dir is None:
-            download_dir = tempfile.mkdtemp(prefix = "UpdateUpdate-", dir = config.TemporaryDirectory())
+            download_dir = tempfile.mkdtemp(
+                prefix="UpdateUpdate-",
+                dir=config.TemporaryDirectory()
+            )
             if download_dir is None:
                 print >> sys.stderr, "Unable to create temporary directory"
                 sys.exit(1)
-            rv = Update.DownloadUpdate(train, download_dir, pkg_type = pkg_type)
+            rv = download_update_call(train, download_dir, pkg_type, verbose)
             if rv is False:
                 if verbose or debug:
                     print >> sys.stderr, "DownloadUpdate returned False"
                 sys.exit(1)
         else:
             download_dir = cache_dir
-        
+
         diffs = Update.PendingUpdatesChanges(download_dir)
         if diffs is None or diffs == {}:
             if verbose:
@@ -194,12 +334,12 @@ def main():
             if verbose:
                 PrintDifferences(diffs)
             try:
-                rv = Update.ApplyUpdate(download_dir, force_reboot = force_reboot)
+                rv = Update.ApplyUpdate(download_dir, force_reboot=force_reboot)
             except BaseException as e:
-                print >> sys.stderr, "Unable to apply update: %s" % str(e)
+                print >> sys.stderr, "Unable to apply update: {0}".format(e)
                 sys.exit(1)
             if cache_dir is None:
-                Update.RemoveUpdate(download_dir)    
+                Update.RemoveUpdate(download_dir)
             if rv:
                 print >> sys.stderr, "System should be rebooted now"
             sys.exit(0)
@@ -208,4 +348,3 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
-
