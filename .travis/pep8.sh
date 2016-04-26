81fb0dd7b99419ad711c4ec1e155b8404481e114
William Grzybowski <wg@FreeBSD.org>
2016-04-26 11:29:47 -0300

    Use set -x and proper exit code

--- .travis/pep8.sh
+++ .travis/pep8.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 # Run pep8 on all .py files in all subfolders
 # We must ignore E402 module level import not at top of file
 # because of use case sys.path.append('..'); import <module>
@@ -19,7 +21,8 @@ num_errors_after=`cat $tmpafter | wc -l`
 echo $num_errors_after
 
 if [ $num_errors_after -gt $num_errors_before ]; then
-	echo "New PEP8 errors were introduced: $(diff -u $tmpbefore $tmpafter)"
-	return 1
+	echo "New PEP8 errors were introduced:"
+	diff -u $tmpbefore $tmpafter
+	exit 1
 fi
 
