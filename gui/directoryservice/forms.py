9fbec1bf168a8ea56c8a49ea87a31409648abb7c
Erin Clark <erin@ixsystems.com>
2016-05-11 14:17:03 -0700

    Revert "Show Active Directory password as dots to give visual indicator that it is saved"
    Apparently there is a security risk with this, thanks William for
    pointing this out.
    
    This reverts commit 8d60b2300e9991384a98eead02a194e0609c73db.

--- gui/directoryservice/forms.py
+++ gui/directoryservice/forms.py
@@ -345,7 +345,7 @@ class ActiveDirectoryForm(ModelForm):
 
         model = models.ActiveDirectory
         widgets = {
-            'ad_bindpw': forms.widgets.PasswordInput(render_value=True),
+            'ad_bindpw': forms.widgets.PasswordInput(render_value=False),
         }
 
     def __original_save(self):
