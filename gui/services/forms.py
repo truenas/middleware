truenas/middleware

commit 61042dcad370c37ec7d519dd6b4210b2c0750b43

Fix DynDNS password encryption/decryption setup.
Ticket: #15331

gui/services/forms.py
@@ -733,11 +733,7 @@ class DynamicDNSForm(ModelForm):
         return cdata
 
     def save(self):
-        obj = super(DynamicDNSForm, self).save(commit=False)
-        obj.ddns_password = notifier().pwenc_encrypt(
-            self.cleaned_data.get('ddns_password')
-        )
-        obj.save()
+        obj = super(DynamicDNSForm, self).save()
         started = notifier().restart("dynamicdns")
         if (
             started is False and

gui/services/models.py
@@ -1098,6 +1098,22 @@ class DynamicDNS(Model):
             "inadyn-mt.conf."),
     )
 
+    def __init__(self, *args, **kwargs):
+        super(DynamicDNS, self).__init__(*args, **kwargs)
+        if self.ddns_password:
+            try:
+                self.ddns_password = notifier().pwenc_decrypt(self.ddns_password)
+            except:
+                log.debug('Failed to decrypt DDNS password', exc_info=True)
+                self.ddns_password = ''
+        self._ddns_password_encrypted = False
+
+    def save(self, *args, **kwargs):
+        if self.ddns_password and not self._ddns_password_encrypted:
+            self.ddns_password = notifier().pwenc_encrypt(self.ddns_password)
+            self._ddns_password_encrypted = True
+        return super(DynamicDNS, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name = _("Dynamic DNS")
         verbose_name_plural = _("Dynamic DNS")

commit 6efca9cff8cb131c19ade404ac4d241edc04d159

Fix flake8 error introduced in 61042dca

gui/services/forms.py
@@ -261,7 +261,7 @@ class CIFSForm(ModelForm):
         alias = self.cleaned_data.get("cifs_srv_netbiosalias")
         if alias:
             try:
-                validate_netbios_names(netbios)
+                validate_netbios_names(alias)
             except Exception as e:
                 raise forms.ValidationError(_("NetBIOS Alias: %s" % e))
         return alias
@@ -742,6 +742,7 @@ class DynamicDNSForm(ModelForm):
             raise ServiceFailed(
                 "dynamicdns", _("The DynamicDNS service failed to reload.")
             )
+        return obj
 DynamicDNSForm.base_fields.keyOrder.remove('ddns_password2')
 DynamicDNSForm.base_fields.keyOrder.insert(5, 'ddns_password2')
 