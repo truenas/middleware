47331b9bb65614a859c488194800d9b7e7a9a8c4
Erin Clark <erin@ixsystems.com>
2016-05-26 16:32:58 -0700

    Combine ldap clean methods to fix SSL connection password checking
    Ticket: #15533
    Ticket: #15601
    (cherry picked from commit c30e7721c73df843172cc1de7b1f6421346aa082)

--- gui/directoryservice/forms.py
+++ gui/directoryservice/forms.py
@@ -673,41 +673,6 @@ class LDAPForm(ModelForm):
                 self.fields['ldap_netbiosname_b'],
             )
 
-    def clean_ldap_bindpw(self):
-        cdata = self.cleaned_data
-        if not cdata.get("ldap_bindpw"):
-            cdata["ldap_bindpw"] = self.instance.ldap_bindpw
-
-        binddn = cdata.get("ldap_binddn")
-        bindpw = cdata.get("ldap_bindpw")
-        basedn = cdata.get("ldap_basedn")
-        hostname = cdata.get("ldap_hostname")
-        ssl = cdata.get("ldap_ssl")
-        port = 389
-        if ssl == "on":
-            port = 636
-        if hostname:
-            parts = hostname.split(':')
-            hostname = parts[0]
-            if len(parts) > 1:
-                port = int(parts[1]) 
-        errors = []
-
-        certfile = None
-
-        if ssl in ('start_tls', 'on'):
-            certificate = cdata["ldap_certificate"]
-            certfile = get_certificateauthority_path(certificate)
-
-        ret = FreeNAS_LDAP.validate_credentials(
-            hostname, binddn=binddn, bindpw=bindpw, basedn=basedn,
-            port=port, certfile=certfile, ssl=ssl, errors=errors
-        )
-        if ret is False:
-            raise forms.ValidationError("%s." % errors[0])
-
-        return bindpw
-
     def check_for_samba_schema(self):
         self.clean_bindpw()
 
@@ -762,17 +727,64 @@ class LDAPForm(ModelForm):
 
     def clean(self):
         cdata = self.cleaned_data
-        ssl = cdata.get("ldap_ssl")
-        if ssl in ("off", None):
-            # self.check_for_samba_schema()
-            return cdata
+        if not cdata.get("ldap_bindpw"):
+            cdata["ldap_bindpw"] = self.instance.ldap_bindpw
 
-        certificate = cdata["ldap_certificate"]
-        if not certificate:
-            raise forms.ValidationError(
-                "SSL/TLS specified without certificate")
+        binddn = cdata.get("ldap_binddn")
+        bindpw = cdata.get("ldap_bindpw")
+        basedn = cdata.get("ldap_basedn")
+        hostname = cdata.get("ldap_hostname")
+        ssl = cdata.get("ldap_ssl")
+
+        certfile = None
+        if ssl in ('start_tls', 'on'):
+            certificate = cdata["ldap_certificate"]
+            if not certificate:
+                raise forms.ValidationError(
+                    "SSL/TLS specified without certificate")
+            else:
+                certfile = get_certificateauthority_path(certificate)
+
+        port = 389
+        if ssl == "on":
+            port = 636
+        if hostname:
+            parts = hostname.split(':')
+            hostname = parts[0]
+            if len(parts) > 1:
+                port = int(parts[1])
 
         # self.check_for_samba_schema()
+        try:
+            FreeNAS_LDAP.validate_credentials(
+                hostname,
+                binddn=binddn,
+                bindpw=bindpw,
+                basedn=basedn,
+                port=port,
+                certfile=certfile,
+                ssl=ssl
+            )
+        except LDAPError as e:
+            # LDAPError is dumb, it returns a list with one element for goodness knows what reason
+            e = e[0]
+            error = []
+            desc = e.get('desc')
+            info = e.get('info')
+            if desc:
+                error.append(desc)
+            if info:
+                error.append(info)
+
+            if error:
+                error = ', '.join(error)
+            else:
+                error = str(e)
+
+            raise forms.ValidationError("{0}".format(error))
+        except Exception as e:
+            raise forms.ValidationError("{0}".format(str(e)))
+
         return cdata
 
     def save(self):
