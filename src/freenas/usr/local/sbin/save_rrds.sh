truenas/middleware

commit 86d9a98f3371e516ed68ddf88f9c959f2a43c569

Due to some changes in TrueNAS shutdown we need to invert logic here.
Otherwise, if the system isn't using the system dataset for
storing RRDs it can try to copy off the current RRDs
during shutdown even if it's passive.  If the RRD files are
large this can take long enough to trip the watchdog shutdown
timer.

Ticket:	#15200

src/freenas/usr/local/sbin/save_rrds.sh
@@ -54,10 +54,10 @@ use_rrd_dataset()
 			echo "1"
 		else
 			local failover="$(/usr/local/bin/python /usr/local/www/freenasUI/middleware/notifier.py failover_status 2> /dev/null)"
-			if [ "x${failover}" = "xBACKUP" ]; then
-				echo "1"
-			else
+			if [ "x${failover}" = "xMASTER" ]; then
 				echo "0"
+			else
+				echo "1"
 			fi
 		fi
 	done

commit 4d54125fa8dd907a991998674c41a0f1cbc19c4b

Fix save_rrds.sh for FreeNAS
Regression in 86d9a98f3371e516ed68ddf88f9c959f2a43c569.

Ticket:	#15707

src/freenas/usr/local/sbin/save_rrds.sh
@@ -53,11 +53,15 @@ use_rrd_dataset()
 		then
 			echo "1"
 		else
-			local failover="$(/usr/local/bin/python /usr/local/www/freenasUI/middleware/notifier.py failover_status 2> /dev/null)"
-			if [ "x${failover}" = "xMASTER" ]; then
+			if is_freenas; then
 				echo "0"
 			else
-				echo "1"
+				local failover="$(/usr/local/bin/python /usr/local/www/freenasUI/middleware/notifier.py failover_status 2> /dev/null)"
+				if [ "x${failover}" = "xMASTER" ]; then
+					echo "0"
+				else
+					echo "1"
+				fi
 			fi
 		fi
 	done