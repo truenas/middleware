18d00df28ba13aec779945b2ecbbc85f15df49ad
Kris Moore <kris@pcbsd.org>
2016-06-02 15:23:53 -0400

    Add initial support for UEFI installs to FreeNAS
    This updates grub2-pcbsd with some needed kernel settings (Vt) and
    updates the sysutils/grub2-efi with a later version.
    
    Lastly it updates the installer to determine if we are UEFI booted
    or not, and DTRT if so.
    
    Ticket: #14414

--- src/freenas-installer/etc/install.sh
+++ src/freenas-installer/etc/install.sh
@@ -11,6 +11,8 @@ TERM=${TERM:-xterm}
 export TERM
 GRUB_TERMINAL_OUTPUT="console serial"
 export GRUB_TERMINAL_OUTPUT
+BOOTMODE=`kenv grub.platform`
+export BOOTMODE
 
 . /etc/avatar.conf
 
@@ -265,6 +267,7 @@ EOD
 install_grub() {
 	local _disk _disks
 	local _mnt
+	local _grub_args
 
 	_mnt="$1"
 	shift
@@ -282,7 +285,21 @@ install_grub() {
 	mkdir -p /tmp/bakup
 	mv ${_mnt}/conf/base/etc/local/grub.d/10_ktrueos.bak /tmp/bakup
 	for _disk in ${_disks}; do
-	    chroot ${_mnt} /usr/local/sbin/grub-install --modules='zfs part_gpt' /dev/${_disk}
+	    _grub_args=""
+	    if [ "$BOOTMODE" = "efi" ] ; then
+		# EFI Mode
+		sed -i '' 's|GRUB_TERMINAL_OUTPUT=console|GRUB_TERMINAL_OUTPUT=gfxterm|g' ${_mnt}/conf/base/etc/local/default/grub
+		glabel label efibsd /dev/${_disk}p1
+		mkdir -p ${_mnt}/boot/efi
+		mount -t msdosfs /dev/${_disk}p1 ${_mnt}/boot/efi
+		_grub_args="--efi-directory=/boot/efi --removable --target=x86_64-efi"
+	        echo "chroot ${_mnt} /usr/local/sbin/grub-install ${_grub_args} /dev/${_disk}"
+	        chroot ${_mnt} /usr/local/sbin/grub-install ${_grub_args} /dev/${_disk}
+		umount -f ${_mnt}/boot/efi
+	    else
+		# BIOS Mode
+	        chroot ${_mnt} /usr/local/sbin/grub-install --modules='zfs part_gpt' ${_grub_args} /dev/${_disk}
+	    fi
 	done
 	chroot ${_mnt} /usr/local/sbin/beadm activate default
 	chroot ${_mnt} /usr/local/sbin/grub-mkconfig -o /boot/grub/grub.cfg > /dev/null 2>&1
@@ -317,14 +334,29 @@ create_partitions() {
     if [ $# -eq 2 ]; then
 	_size="-s $2"
     fi
-    if gpart create -s GPT ${_disk}; then
-	if gpart add -t bios-boot -i 1 -s 512k ${_disk}; then
-	    if is_truenas; then
-		gpart add -t freebsd-swap -s 16g -i 3 ${_disk}
+    if gpart create -s GPT -f active ${_disk}; then
+	if [ "$BOOTMODE" = "efi" ] ; then
+	  # EFI Mode
+	  sysctl kern.geom.debugflags=16
+	  sysctl kern.geom.label.disk_ident.enable=0
+	  if gpart add -s 100m -t efi ${_disk}; then
+	    if ! newfs_msdos -F 16 /dev/${_disk}p1 ; then
+	      return 1
 	    fi
-	    if gpart add -t freebsd-zfs -i 2 -a 4k ${_size} ${_disk}; then
-		return 0
-	    fi
+	    gpart set -a lenovofix /dev/${_disk}
+	  fi
+	else
+	  # BIOS Mode
+          if ! gpart add -t bios-boot -i 1 -s 512k ${_disk}; then
+	    return 1
+	  fi
+	fi
+
+	if is_truenas; then
+	    gpart add -t freebsd-swap -s 16g -i 3 ${_disk}
+	fi
+	if gpart add -t freebsd-zfs -a 4k -i 2 ${_size} ${_disk}; then
+	    return 0
 	fi
     fi
 
@@ -335,7 +367,7 @@ get_minimum_size() {
     local _min=0
     local _disk
     local _size
-    # We use 1mbyte because the bios-boot is 512k,
+    # We use 1mbyte because the fat16 partition is 512k,
     # and there's some header space.
     # Now we use 8MBytes because gpart and some thumb drives
     # misbehave.
@@ -390,8 +422,10 @@ partition_disk() {
 	
 	_disksparts=$(for _disk in ${_disks}; do
 	    create_partitions ${_disk} ${_minsize} >&2
-	    # Make the disk active
-	    gpart set -a active ${_disk} >&2
+	    if [ "$BOOTMODE" != "efi" ] ; then
+	      # Make the disk active
+	      gpart set -a active ${_disk} >&2
+	    fi
 
 	    echo ${_disk}p2
 	done)
