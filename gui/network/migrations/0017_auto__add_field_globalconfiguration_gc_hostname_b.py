2a46b00b1ce6e863a9b558ebd94d9e78f5b9d2ab
William Grzybowski <wg@FreeBSD.org>
2016-06-20 12:25:45 -0300

    Use default value for node B hostname
    (cherry picked from commit f30566792e6ee73469fcc0e69e9a5f97ae645af7)

--- gui/network/migrations/0017_auto__add_field_globalconfiguration_gc_hostname_b.py
+++ gui/network/migrations/0017_auto__add_field_globalconfiguration_gc_hostname_b.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'GlobalConfiguration.gc_hostname_b'
         db.add_column(u'network_globalconfiguration', 'gc_hostname_b',
-                      self.gf('django.db.models.fields.CharField')(max_length=120, null=True, blank=True),
+                      self.gf('django.db.models.fields.CharField')(default='truenas-b', max_length=120, null=True, blank=True),
                       keep_default=False)
 
 
