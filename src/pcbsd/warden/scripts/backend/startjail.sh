dfa05b6b2d8148e22cb46d9d2f3821b4af99a587
neha <neha.rathod@calsoftinc.com>
2016-06-24 14:50:56 +0530

    In this commit, I have added the device "bpf*", needed for DHCP jails, to rule #4 and then applied the rule to jails so that /dev/adax disks are no longer reachable from inside a jail.
    (cherry picked from commit 66286792d61689e356453d6c9b1cd6a8ec07d048)

--- src/pcbsd/warden/scripts/backend/startjail.sh
+++ src/pcbsd/warden/scripts/backend/startjail.sh
@@ -518,7 +518,8 @@ if is_symlinked_mountpoint "${JAILDIR}/dev"; then
    warden_print "${JAILDIR}/dev has symlink as parent, not mounting"
 else
    mount -t devfs devfs "${JAILDIR}/dev"
-   #devfs -m "${JAILDIR}/dev" rule -s 4 applyset
+   devfs rule -s 4 add path 'bpf*' unhide
+   devfs -m "${JAILDIR}/dev" rule -s 4 applyset
 fi
 
 if [ "$LINUXJAIL" = "YES" ] ; then
