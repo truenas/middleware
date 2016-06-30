fd741e55f24cba7f0d965b8fccf29806f065b867
Deepika Dhiman <deepika.dhiman@calsoftinc.com>
2016-06-30 15:32:51 +0530

    Bug #16101: Reporting history gone after reboot In this commit, I have replaced "echo "0"" with "return 0" in file  save_rrds.sh, Now reporting data is not getting flush after reboot.
    (cherry picked from commit 6023bbff2e8bf4ba50469b2181938ebf80bd2a7b)

--- src/freenas/usr/local/sbin/save_rrds.sh
+++ src/freenas/usr/local/sbin/save_rrds.sh
@@ -54,11 +54,11 @@ use_rrd_dataset()
 			echo "1"
 		else
 			if is_freenas; then
-				echo "0"
+				return 0	
 			else
 				local failover="$(/usr/local/bin/python /usr/local/www/freenasUI/middleware/notifier.py failover_status 2> /dev/null)"
 				if [ "x${failover}" = "xMASTER" ]; then
-					echo "0"
+					return 0	
 				else
 					echo "1"
 				fi
