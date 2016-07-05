8bd50074e61d8ff2ddf055213f64c3b37b6dba9e
William Grzybowski <wg@FreeBSD.org>
2016-07-05 18:22:49 -0300

    Simplify service attributes

--- src/middlewared/middlewared/service.py
+++ src/middlewared/middlewared/service.py
@@ -1,23 +1,5 @@
 import re
 
-CONFIG_NAMES = ('namespace', )
-
-
-class Config(object):
-
-    def __init__(self, cls, meta):
-        namespace = cls.__name__
-        if namespace.endswith('Service'):
-            namespace = namespace[:-7]
-        self.namespace = self._camel_to_dotted(namespace)
-        if meta:
-            for key, val in meta.__dict__.items():
-                if key in CONFIG_NAMES:
-                    setattr(self, key, val)
-
-    def _camel_to_dotted(self, name):
-        return re.sub(r'([a-z])([A-Z])', '\\1.\\2', name).lower()
-
 
 class ServiceBase(type):
 
@@ -26,9 +8,20 @@ class ServiceBase(type):
         if name == 'Service' and bases == ():
             return super_new(cls, name, bases, attrs)
 
-        meta = attrs.pop('Meta', None)
+        config = attrs.pop('Config', None)
         klass = super_new(cls, name, bases, attrs)
-        klass._meta = Config(klass, meta)
+
+        namespace = cls.__name__
+        if namespace.endswith('Service'):
+            namespace = namespace[:-7]
+
+        config_attrs = {
+            'namespace': namespace,
+            'public': True,
+        }
+        config_atrrs.update(config.__dict__)
+
+        klass._config = type('Config', (), config_attrs)
         return klass
 
 
