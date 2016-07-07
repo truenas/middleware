a45650488c7c05afaebaf1bcebe437a6a26d25c4
William Grzybowski <wg@FreeBSD.org>
2016-07-07 19:03:18 -0300

    Dump certificate chain and check integrity (?) in cert extend

--- src/middlewared/middlewared/plugins/crypto.py
+++ src/middlewared/middlewared/plugins/crypto.py
@@ -1,8 +1,13 @@
 from middlewared.service import Service
+from OpenSSL import crypto
 
+import logging
 import os
+import re
 
 CERT_ROOT_PATH = '/etc/certificates'
+RE_CERTIFICATE = re.compile(r"(-{5}BEGIN[\s\w]+-{5}[^-]+-{5}END[\s\w]+-{5})+", re.M | re.S)
+logger = logging.getLogger('plugins.crypto')
 
 
 class CertificateService(Service):
@@ -27,4 +32,35 @@ class CertificateService(Service):
         cert['cert_csr_path'] = os.path.join(
             CERT_ROOT_PATH, '{0}.csr'.format(cert['cert_name'])
         )
+        cert['cert_chain_list'] = []
+        if cert['cert_chain']:
+            certs = RE_CERTIFICATE.findall(cert['cert_certificate'])
+        else:
+            certs = [cert['cert_certificate']]
+        try:
+            for c in certs:
+                # XXX Why load certificate if we are going to dump it right after?
+                # Maybe just to verify its integrity?
+                # Logic copied from freenasUI
+                cert_obj = crypto.load_certificate(crypto.FILETYPE_PEM, c)
+                cert['cert_chain_list'].append(
+                    crypto.dump_certificate(crypto.FILETYPE_PEM, cert_obj)
+                )
+        except:
+            logger.debug('Failed to load certificate {0}'.format(cert['cert_name']), exc_info=True)
+
+        try:
+            if cert['cert_privatekey']:
+                key_obj = crypto.load_privatekey(crypto.FILETYPE_PEM, cert['cert_privatekey'])
+                cert['cert_privatekey'] = crypto.dump_privatekey(crypto.FILETYPE_PEM, key_obj)
+        except:
+            logger.debug('Failed to load privatekey {0}'.format(cert['cert_name']), exc_info=True)
+
+        try:
+            if cert['cert_CSR']:
+                csr_obj = crypto.load_certificate_request(crypto.FILETYPE_PEM, cert['cert_CSR'])
+                cert['cert_CSR'] = crypto.dump_certificate_request(crypto.FILETYPE_PEM, csr_obj)
+        except:
+            logger.debug('Failed to load csr {0}'.format(cert['cert_name']), exc_info=True)
+
         return cert
