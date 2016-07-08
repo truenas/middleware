17859b82d725e90174eeaa44ae816bffc2b9863e
William Grzybowski <wg@FreeBSD.org>
2016-07-08 10:32:45 -0300

    Recursively get certificates chain and add CA service

--- src/middlewared/middlewared/plugins/crypto.py
+++ src/middlewared/middlewared/plugins/crypto.py
@@ -15,6 +15,7 @@ CERT_TYPE_INTERNAL = 0x10
 CERT_TYPE_CSR = 0x20
 
 CERT_ROOT_PATH = '/etc/certificates'
+CERT_CA_ROOT_PATH = '/etc/certificates/CA'
 RE_CERTIFICATE = re.compile(r"(-{5}BEGIN[\s\w]+-{5}[^-]+-{5}END[\s\w]+-{5})+", re.M | re.S)
 logger = logging.getLogger('plugins.crypto')
 
@@ -32,20 +33,47 @@ class CertificateService(Service):
 
         @private
         """
+        if cert['cert_type'] in (
+            CA_TYPE_EXISTING, CA_TYPE_INTERNAL, CA_TYPE_INTERMEDIATE
+        ):
+            root_path = CERT_CA_ROOT_PATH
+        else:
+            root_path = CERT_ROOT_PATH
+        cert['cert_root_path'] = root_path
         cert['cert_certificate_path'] = os.path.join(
-            CERT_ROOT_PATH, '{0}.crt'.format(cert['cert_name'])
+            root_path, '{0}.crt'.format(cert['cert_name'])
         )
         cert['cert_privatekey_path'] = os.path.join(
-            CERT_ROOT_PATH, '{0}.key'.format(cert['cert_name'])
+            root_path, '{0}.key'.format(cert['cert_name'])
         )
         cert['cert_csr_path'] = os.path.join(
-            CERT_ROOT_PATH, '{0}.csr'.format(cert['cert_name'])
+            root_path, '{0}.csr'.format(cert['cert_name'])
         )
+
+        def cert_issuer(cert):
+            issuer = None
+            if cert['cert_type'] in (CA_TYPE_EXISTING, CERT_TYPE_EXISTING):
+                issuer = "external"
+            elif cert['cert_type'] == CA_TYPE_INTERNAL:
+                issuer = "self-signed"
+            elif cert['cert_type'] in (CERT_TYPE_INTERNAL, CA_TYPE_INTERMEDIATE):
+                issuer = cert['cert_signedby']
+            elif cert['cert_type'] == CERT_TYPE_CSR:
+                issuer = "external - signature pending"
+            return issuer
+        cert['cert_issuer'] = cert_issuer(cert)
+
         cert['cert_chain_list'] = []
         if cert['cert_chain']:
             certs = RE_CERTIFICATE.findall(cert['cert_certificate'])
         else:
             certs = [cert['cert_certificate']]
+            signing_CA = cert['cert_issuer']
+            # Recursively get all internal/intermediate certificates
+            while signing_CA not in ["external", "self-signed", "external - signature pending"]:
+                certs.append(signing_CA['cert_certificate'])
+                signing_CA['cert_issuer'] = cert_issuer(signing_CA)
+                signing_CA = signing_CA['cert_issuer']
         try:
             for c in certs:
                 # XXX Why load certificate if we are going to dump it right after?
@@ -74,17 +102,6 @@ class CertificateService(Service):
 
         cert['cert_internal'] = 'NO' if cert['cert_type'] in (CA_TYPE_EXISTING, CERT_TYPE_EXISTING) else 'YES'
 
-        issuer = None
-        if cert['cert_type'] in (CA_TYPE_EXISTING, CERT_TYPE_EXISTING):
-            issuer = "external"
-        elif cert['cert_type'] == CA_TYPE_INTERNAL:
-            issuer = "self-signed"
-        elif cert['cert_type'] in (CERT_TYPE_INTERNAL, CA_TYPE_INTERMEDIATE):
-            issuer = cert['cert_signedby']
-        elif cert['cert_type'] == CERT_TYPE_CSR:
-            issuer = "external - signature pending"
-        cert['cert_issuer'] = issuer
-
         if cert['cert_type'] == CERT_TYPE_CSR:
             obj = csr_obj
             # date not applicable for CSR
@@ -108,3 +125,12 @@ class CertificateService(Service):
         ])
 
         return cert
+
+
+class CertificateauthorityService(Service):
+
+    def query(self, filters=None, options=None):
+        if options is None:
+            options = {}
+        options['extend'] = 'certificate.cert_extend'
+        return self.middleware.call('datastore.query', 'system.certificateauthority', filters, options)
