a6cee379e790e1bea769764f92522f51583b3232
William Grzybowski <wg@FreeBSD.org>
2016-07-13 17:13:05 -0300

    First pass at converting ldap.conf generation

--- src/freenas/usr/local/libexec/nas/generate_ldap_conf.py
+++ src/freenas/usr/local/libexec/nas/generate_ldap_conf.py
@@ -1,8 +1,9 @@
 #!/usr/local/bin/python
+from middlewared.client import Client
+from middlewared.client.utils import Struct
 
 import os
 import re
-import string
 import sys
 
 sys.path.extend([
@@ -17,23 +18,18 @@ cache.get_apps()
 
 from freenasUI.common.freenasldap import (
     FreeNAS_ActiveDirectory,
-    FreeNAS_LDAP,
     FLAGS_DBINIT
 )
 from freenasUI.common.ssl import get_certificateauthority_path
-from freenasUI.common.system import (
-    activedirectory_enabled,
-    ldap_enabled
-)
-from freenasUI.directoryservice import models
 from freenasUI.directoryservice.utils import get_idmap_object
 
-def ldap_conf_ldap(ldap_conf):
+
+def ldap_conf_ldap(client, ldap_conf):
     try:
-        ldap = models.LDAP.objects.all()[0]
+        ldap = Struct(client.call('datastore.query', 'directoryservice.ldap', None, {'get': True}))
     except:
         sys.exit(0)
-    
+
     f = open(ldap_conf, "w")
     f.write("URI %s://%s\n" % (
         "ldaps" if ldap.ldap_ssl == "on" else "ldap",
@@ -50,10 +46,11 @@ def ldap_conf_ldap(ldap_conf):
     f.close()
     os.chmod(ldap_conf, 0644)
 
-def ldap_conf_activedirectory(ldap_conf):
+
+def ldap_conf_activedirectory(client, ldap_conf):
     ad = FreeNAS_ActiveDirectory(flags=FLAGS_DBINIT)
 
-    config = { }
+    config = {}
     config["URI"] = "%s://%s" % (
         "ldaps" if ad.ssl == "on" else "ldap",
         ad.domainname
@@ -61,7 +58,7 @@ def ldap_conf_activedirectory(ldap_conf):
     config["BASE"] = ad.basedn
 
     if ad.ssl in ("start_tls", "on"):
-        if ad.certfile: 
+        if ad.certfile:
             config["TLS_CACERT"] = ad.certfile
         config["TLS_REQCERT"] = "allow"
 
@@ -69,10 +66,10 @@ def ldap_conf_activedirectory(ldap_conf):
     # So what if the AD server is configured to use SSL or TLS,
     # and the idmap backend is as well? WTF? whaddoyoudo?
     #
-    ad = models.ActiveDirectory.objects.all()[0]
+    ad = Struct(client.call('datastore.query', 'directoryservice.activedirectory', None, {'get': True}))
     if ad.ad_idmap_backend in ("rfc2307", "ldap"):
         idmap = get_idmap_object(ad.ds_type, ad.id, ad.ad_idmap_backend)
-        idmap_url = idmap.get_url()  
+        idmap_url = idmap.get_url()
         idmap_url = re.sub('^(ldaps?://)', '', idmap_url)
 
         config["URI"] = "%s://%s" % (
@@ -94,13 +91,15 @@ def ldap_conf_activedirectory(ldap_conf):
         f.close()
     os.chmod(ldap_conf, 0644)
 
+
 def main():
+    client = Client()
     ldap_conf = "/usr/local/etc/openldap/ldap.conf"
 
-    if ldap_enabled():
-        ldap_conf_ldap(ldap_conf)
-    elif activedirectory_enabled():
-        ldap_conf_activedirectory(ldap_conf)
+    if client.call('notifier.common', 'system', 'ldap_enabled'):
+        ldap_conf_ldap(client, ldap_conf)
+    elif client.call('notifier.common', 'system', 'activedirectory_enabled'):
+        ldap_conf_activedirectory(client, ldap_conf)
 
 if __name__ == '__main__':
     main()
