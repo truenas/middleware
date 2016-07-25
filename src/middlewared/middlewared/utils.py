ed63a8857cf0f6ce244a0e6f855f8865d9ac8c0b
William Grzybowski <wg@FreeBSD.org>
2016-07-25 09:38:42 -0300

    Serialize specific IPv4 and IPv6 fields as well

--- src/middlewared/middlewared/utils.py
+++ src/middlewared/middlewared/utils.py
@@ -1,12 +1,16 @@
 from django.db.models.fields.related import ForeignKey
-from freenasUI.contrib.IPAddressField import IPAddressField
+from freenasUI.contrib.IPAddressField import (
+    IPAddressField, IP4AddressField, IP6AddressField
+)
 
 
 def django_modelobj_serialize(middleware, obj, extend=None):
     data = {}
     for field in obj._meta.fields:
         value = getattr(obj, field.name)
-        if isinstance(field, IPAddressField):
+        if isinstance(field, (
+            IPAddressField, IP4AddressField, IP6AddressField
+        )):
             data[field.name] = str(value)
         elif isinstance(field, ForeignKey):
             data[field.name] = django_modelobj_serialize(value) if value is not None else value
