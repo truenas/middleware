971747e86abae3686834f238d04e751cf0bf8b8c
William Grzybowski <wg@FreeBSD.org>
2016-08-01 09:35:39 -0300

    Add auth service

---
+++ src/middlewared/middlewared/plugins/auth.py
@@ -0,0 +1,20 @@
+import crypt
+
+from middlewared.service import Service
+
+
+class AuthService(Service):
+
+    def login(self, username, password):
+        """Authenticate session using username and password.
+        Currently only root user is allowed.
+        """
+        if username != 'root':
+            return False
+        try:
+            user = self.middleware.call('datastore.query', 'account.bsdusers', [('bsdusr_username', '=', username)], {'get': True})
+        except IndexError:
+            return False
+        if user['bsdusr_unixhash'] in ('x', '*'):
+            return False
+        return crypt.crypt(password, user['bsdusr_unixhash']) == user['bsdusr_unixhash']
