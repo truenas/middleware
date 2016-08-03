4f447dbd369f701e949d8da5d209ab3fb07ccdfb
John Hixson <john@ixsystems.com>
2016-08-02 20:36:27 -0700

    Order samba vfs objects correctly
    Ticket: #16325

--- gui/sharing/models.py
+++ gui/sharing/models.py
@@ -117,7 +117,7 @@ class CIFS_Share(Model):
         verbose_name=_('VFS Objects'),
         max_length=255,
         blank=True,
-        default='aio_pthread,streams_xattr',
+        default='streams_xattr,aio_pthread',
         choices=list(choices.CIFS_VFS_OBJECTS())
     )
     cifs_storage_task = models.ForeignKey(
