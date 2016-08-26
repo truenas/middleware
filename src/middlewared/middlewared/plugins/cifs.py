822725406b0605b743286cda3cfc23b4c6ee21df
William Grzybowski <wg@FreeBSD.org>
2016-08-26 10:19:58 -0300

    feat(middlewared): mark method private and add docstring

--- src/middlewared/middlewared/plugins/cifs.py
+++ src/middlewared/middlewared/plugins/cifs.py
@@ -1,16 +1,15 @@
-from middlewared.service import Service
+from middlewared.service import Service, private
 
 
 class CIFSService(Service):
 
     def config(self):
+        """Returns CIFS configuration object."""
         return self.middleware.call('datastore.config', 'services.cifs', {'extend': 'cifs.cifs_extend'})
 
+    @private
     def cifs_extend(self, cifs):
-        """Extend cifs for netbios
-
-        @private
-        """
+        """Extend cifs for netbios."""
         if not self.middleware.call('notifier.is_freenas') and self.middleware.call('notifier.failover_node') == 'B':
             cifs['netbiosname'] = cifs['cifs_srv_netbiosname_b']
         else:
