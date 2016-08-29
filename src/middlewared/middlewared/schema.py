8a60173b0dce090ca38ab5801445802da4e27a66
William Grzybowski <wg@FreeBSD.org>
2016-08-29 11:43:29 -0300

    fix(middlewared): copy attrs to new function

--- src/middlewared/middlewared/schema.py
+++ src/middlewared/middlewared/schema.py
@@ -330,6 +330,13 @@ def accepts(*schema):
             return f(*args, **kwargs)
         nf.__name__ = f.__name__
         nf.__doc__ = f.__doc__
+        # Copy private attrs to new function so decorators can work on top of it
+        # e.g. _pass_app
+        for i in dir(f):
+            if i.startswith('__'):
+                continue
+            if i.startswith('_'):
+                setattr(nf, i, getattr(f, i))
         nf.accepts = list(schema)
 
         return nf
