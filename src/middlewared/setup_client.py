43e3b6b28e8732289919dd61d9b12820029b3a40
William Grzybowski <wg@FreeBSD.org>
2016-08-30 11:12:34 -0300

    feat(middlewared): add setup file to install the client only

---
+++ src/middlewared/setup_client.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+install_requires = [
+    'ws4py',
+]
+
+setup(
+    name='middlewared.client',
+    description='FreeNAS Middleware Daemon Client',
+    packages=[
+        'middlewared.client',
+    ],
+    package_data={},
+    include_package_data=True,
+    license='BSD',
+    platforms='any',
+    namespace_packages=[str('middlewared')],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'midclt = middlewared.client.client:main',
+        ],
+    },
+)
