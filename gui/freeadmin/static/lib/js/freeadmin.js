7747061ba9507c79071c4bc91f25fccb01faa492
neha <neha.rathod@calsoftinc.com>
2016-09-13 18:06:06 +0530

    In this commit, I have added a warning when user selects a new .system dataset.

--- gui/freeadmin/static/lib/js/freeadmin.js
+++ gui/freeadmin/static/lib/js/freeadmin.js
@@ -1416,6 +1416,19 @@ require([
 
     }
 
+    systemDatasetMigration = function() {
+        var dialog = new Dialog({
+            title: gettext("Warning!"),
+            id: "Warning_box_dialog",
+            content: domConstruct.create(
+                "p", {
+                    innerHTML: gettext(
+                        gettext("Warning: The action will result in migration of dataset.<br />Some services will be restarted.<br /><br />")
+                    )
+                }
+            )
+        });
+
     vcenter_https_enable_check = function () {
         vc_enable_https = registry.byId('id_vc_enable_https');
 
