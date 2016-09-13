932c413232845c02a4234e356d3c07198eec4001
William Grzybowski <wg@FreeBSD.org>
2016-09-13 13:59:37 -0300

    feat(gui): sketch for the cloud replication widget
    Ticket:	#14034

---
+++ gui/freeadmin/static/lib/js/freeadmin/CloudReplication.js
@@ -0,0 +1,159 @@
+define([
+  "dojo/_base/declare",
+  "dojo/dom-construct",
+  "dojo/dom-style",
+  "dojo/json",
+  "dojo/mouse",
+  "dojo/on",
+  "dojo/query",
+  "dojo/request/iframe",
+  "dojo/request/xhr",
+  "dijit/_Widget",
+  "dijit/_TemplatedMixin",
+  "dijit/Dialog",
+  "dijit/registry",
+  "dijit/TooltipDialog",
+  "dijit/form/Button",
+  "dijit/form/CheckBox",
+  "dijit/form/Form",
+  "dijit/form/Select",
+  "dijit/form/TextBox",
+  "dijit/form/SimpleTextarea",
+  "dijit/popup",
+  "dojox/uuid/generateRandomUuid",
+  "freeadmin/Progress",
+  "dojo/text!freeadmin/templates/cloudreplication.html",
+  ], function(
+  declare,
+  domConst,
+  domStyle,
+  json,
+  mouse,
+  on,
+  query,
+  iframe,
+  xhr,
+  _Widget,
+  _Templated,
+  Dialog,
+  registry,
+  TooltipDialog,
+  Button,
+  CheckBox,
+  Form,
+  Select,
+  TextBox,
+  SimpleTextarea,
+  popup,
+  generateRandomUuid,
+  Progress,
+  template) {
+
+    var CloudReplication = declare("freeadmin.CloudReplication", [ _Widget, _Templated ], {
+      errorMessage: "",
+      initial: "",
+      url: "",
+      templateString: template,
+      postCreate: function() {
+
+        var me = this, _form;
+        var submit, cancel;
+        var attachment_add;
+        var initial = {};
+
+        if(this.initial != '') {
+          initial = json.parse(this.initial);
+        }
+
+        if(!gettext) {
+          gettext = function(s) { return s; }
+        }
+
+        this._form = new Form({
+          enctype: "multipart/form-data",
+          method: "post"
+        }, this.dapForm);
+        this._form.startup();
+
+        if(this.errorMessage != '') {
+          this.dapErrorMessage.innerHTML = this.errorMessage;
+        } else {
+          domStyle.set(this.dapErrorMessageRow, "display", "none");
+        }
+
+        this.dapNameLabel.innerHTML = gettext('Name');
+
+        new TextBox({
+          name: "csrfmiddlewaretoken",
+          value: CSRFToken,
+          type: "hidden"
+        }, this.dapCSRF);
+
+/*
+        this._category = new Select({
+          name: "category",
+          options: options,
+        }, this.dapCategory);
+        if(initial.category) this._category.set('value', initial.category);
+        */
+
+        this._name = new TextBox({
+          name: "name",
+          value: initial.name
+        }, this.dapName);
+
+        this._submit = new Button({
+          label: gettext("Submit")
+        }, this.dapSubmit);
+
+        on(this._submit, 'click', function() {
+          me.submit();
+        });
+
+        this.inherited(arguments);
+
+      },
+      validate: function() {
+        var map;
+          map = {
+            1: this._username,
+            2: this._password,
+            2: this._category,
+            3: this._subject,
+            4: this._desc
+          };
+
+        for(var i in map) {
+          var field = map[i];
+          var value = field.get('value');
+          if(value == '') {
+            var tooltip = new TooltipDialog({
+              content: gettext('This field is required.'),
+              onMouseLeave: function() {
+                popup.close(tooltip);
+                tooltip.destroyRecursive();
+              }
+            });
+            popup.open({
+              popup: tooltip,
+              around: field.domNode,
+              orient: ["above", "after", "below-alt"]
+            });
+            field.focus();
+            return false;
+          }
+        }
+        return true;
+      },
+      submit: function() {
+
+        var me = this;
+
+        if(!this.validate()) return false;
+
+        this._submit.set('disabled', true);
+
+      }
+    });
+    return CloudReplication;
+});
