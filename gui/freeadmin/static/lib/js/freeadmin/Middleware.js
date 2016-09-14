e0e6498918c4a777b03154aeb46621f98ba35ad7
William Grzybowski <wg@FreeBSD.org>
2016-09-14 10:27:49 -0300

    feat(gui): add dojo middleware class
    Ticket:	#14034

---
+++ gui/freeadmin/static/lib/js/freeadmin/Middleware.js
@@ -0,0 +1,38 @@
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojox/timing",
+    "ddpjs/ddp"
+    ], function(
+    declare,
+    lang,
+    timing
+    ddp
+    ) {
+
+    var Middleware = declare("freeadmin.Middleware", [], {
+      token: "",
+      constructor: function(kwArgs) {
+        var me = this;
+        lang.mixin(this, kwArgs);
+        this._ddp = new ddp.default({
+          endpoint: 'ws://' + window.location.host + '/websocket',
+          SocketConstructor: WebSocket
+        });
+        this._ddp.on("connected", lang.hitch(me, me.onConnect));
+
+        var timer = new timing.Timer(1000*60*5);
+        timer.onTick = lang.hitch(me, me.authToken);
+        timer.start();
+      },
+      onConnect: function() {
+        this.authToken();
+      },
+      authToken: function() {
+        var mid = this._ddp.method("auth.token", [this.token]);
+        console.log(mid);
+      }
+    });
+    return Middleware;
+
+});
