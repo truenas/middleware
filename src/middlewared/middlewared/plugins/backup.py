881919ba83a13597a6ee4c5b57ec7d4ca476f201
William Grzybowski <wg@FreeBSD.org>
2016-09-19 09:02:23 -0300

    feat(middlewared): PoC to send snapshots
    Ticket:	#14034

--- src/middlewared/middlewared/plugins/backup.py
+++ src/middlewared/middlewared/plugins/backup.py
@@ -3,7 +3,9 @@ from middlewared.schema import accepts, Int
 from middlewared.service import CRUDService, Service, private
 
 import boto3
+import os
 import subprocess
+import sys
 
 
 class BackupService(CRUDService):
@@ -37,12 +39,33 @@ class BackupService(CRUDService):
         datasets = proc.communicate()[0].strip().split('\n')
         assert proc.returncode == 0
 
-        local_snapshots = defaultdict(dict)
+        local_snapshots = defaultdict(list)
         for snapshot in self.middleware.call('zfs.snapshot.query'):
-            local_snapshots[snapshot['dataset']][snapshot['name']] = snapshot
+            local_snapshots[snapshot['dataset']].append(snapshot)
 
+        for dataset, snapshots in local_snapshots.iteritems():
+            snapshots.sort(key=lambda x: x['properties']['createtxg']['rawvalue'])
 
+        send_snapshots = []
         # Send new snapshots to remote
+        for snapshot_to, snapshot_from in send_snapshots:
+            read_fd, write_fd = os.pipe()
+            if os.fork() == 0:
+                os.dup2(write_fd, 1)
+                try:
+                    for i in os.listdir('/dev/fd'):
+                        if i != '1':
+                            os.close(int(i))
+                except OSError:
+                    pass
+                os.execv('/sbin/zfs', ['/sbin/zfs', 'send', '-V', '-p'] + (['-i', snapshot_from] if snapshot_from else []) + [snapshot_to])
+
+            else:
+                os.close(write_fd)
+                while True:
+                    read = os.read(read_fd, 1024 * 1024)
+                    if read == b'':
+                        break
 
 
 class BackupS3Service(Service):
