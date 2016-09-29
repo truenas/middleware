133ceb80564362c2bea3e5535004959ff6867180
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-09-29 08:04:23 -0700

    Revert "In this commit, I have added the device "bpf*", needed for DHCP jails,"
    This reverts commit 66286792d61689e356453d6c9b1cd6a8ec07d048.

--- src/pcbsd/warden/scripts/backend/startjail.sh
+++ src/pcbsd/warden/scripts/backend/startjail.sh
@@ -518,8 +518,7 @@ if is_symlinked_mountpoint "${JAILDIR}/dev"; then
    warden_print "${JAILDIR}/dev has symlink as parent, not mounting"
 else
    mount -t devfs devfs "${JAILDIR}/dev"
-   devfs rule -s 4 add path 'bpf*' unhide
-   devfs -m "${JAILDIR}/dev" rule -s 4 applyset
+   #devfs -m "${JAILDIR}/dev" rule -s 4 applyset
 fi
 
 if [ "$LINUXJAIL" = "YES" ] ; then
