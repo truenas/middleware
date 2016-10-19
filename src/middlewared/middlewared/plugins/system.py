85b3875fd4d940d296d0c2021621bda884f42053
William Grzybowski <wg@FreeBSD.org>
2016-10-19 19:11:57 -0200

    feat(middlewared): add system.version
    Ticket:	#18371

--- src/middlewared/middlewared/plugins/system.py
+++ src/middlewared/middlewared/plugins/system.py
@@ -1,8 +1,19 @@
 from middlewared.schema import accepts
 from middlewared.service import Service
 
+import sys
+
+if '/usr/local/lib' not in sys.path:
+    sys.path.append('/usr/local/lib')
+
+from freenasOS import Configuration
+
 
 class SystemService(Service):
+
+    def __init__(self, *args, **kwargs):
+        super(SystemService, self).__init__(*args, **kwargs)
+        self.__version = None
 
     @accepts()
     def is_freenas(self):
@@ -12,3 +23,12 @@ class SystemService(Service):
         # This is a stub calling notifier until we have all infrastructure
         # to implement in middlewared
         return self.middleware.call('notifier.is_freenas')
+
+    @accepts()
+    def version(self):
+        if self.__version is None:
+            conf = Configuration.Configuration()
+            sys_mani = conf.SystemManifest()
+            if sys_mani:
+                self.__version = sys_mani.Version()
+        return self.__version
