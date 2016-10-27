18ca3f1308960dba97b3f772ddd7f93e5f1b5c86
William Grzybowski <wg@FreeBSD.org>
2016-10-27 14:47:13 -0200

    fix(gui): UI should not run alerts, we have alertd
    Ticket:	#18002
    (cherry picked from commit acb066972e6baab755965efbaa7cbcb1124e63ec)

--- gui/freeadmin/site.py
+++ gui/freeadmin/site.py
@@ -262,7 +262,7 @@ class FreeAdminSite(object):
         from freenasUI.system.models import Alert
         from freenasUI.system.alert import alert_node, alertPlugins
         dismisseds = [a.message_id for a in Alert.objects.filter(node=alert_node(), dismiss=True)]
-        alerts = alertPlugins.run()
+        alerts = alertPlugins.get_alerts()
         current = 'OK'
         for alert in alerts:
             # Skip dismissed alerts
