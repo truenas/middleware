ab6b08143ffe36e07f98b5547b414b0f105e47a0
Kris Moore <kris@ixsystems.com>
2016-10-28 15:41:02 -0400

    Merge our netatalk3 changes with newer port from upstream
    This updates to 3.1.10_2

--- nas_ports/net/netatalk3/files/patch-libatalk_vfs_extattr.c
+++ nas_ports/net/netatalk3/files/patch-libatalk_vfs_extattr.c
@@ -1,11 +1,11 @@
---- libatalk/vfs/extattr.c.orig	2014-06-03 01:40:44.000000000 -0400
-+++ libatalk/vfs/extattr.c	2014-06-07 14:09:11.000000000 -0400
-@@ -193,6 +193,8 @@ ssize_t sys_fgetxattr (int filedes, cons
+--- libatalk/vfs/extattr.c.orig	2016-06-22 08:57:37 UTC
++++ libatalk/vfs/extattr.c
+@@ -194,6 +194,8 @@ ssize_t sys_fgetxattr (int filedes, cons
      const char *attrname = ((s=strchr(name, '.')) == NULL) ? name : s + 1;
  
      if((retval=extattr_get_fd(filedes, attrnamespace, attrname, NULL, 0)) >= 0) {
-+	if (size == 0)
-+	    return retval;
++        if (size == 0)
++            return retval;
          if(retval > size) {
              errno = ERANGE;
              return -1;
