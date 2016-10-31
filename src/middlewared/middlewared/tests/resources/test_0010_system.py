9e65e2196e8449eb475217b5ae54dc3fe5a9846e
William Grzybowski <wg@FreeBSD.org>
2016-10-31 10:45:17 -0200

    feat(middlewared): add system tests
    Ticket:	#14034

---
+++ src/middlewared/middlewared/tests/resources/test_0010_system.py
@@ -0,0 +1,16 @@
+from base import RESTTestCase
+
+
+class SystemTestCase(RESTTestCase):
+
+    def test_040_version(self):
+        r = self.client.get('system/version')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, unicode)
+
+    def test_041_info(self):
+        r = self.client.get('system/info')
+        self.assertEqual(r.status_code, 200, msg=r.text)
+        data = r.json()
+        self.assertIsInstance(data, dict)
