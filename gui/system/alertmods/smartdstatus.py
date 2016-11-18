8ca8b32d58423d874c9d92aca56ff5064a9928d6
Josh Paetzel <josh@ixsystems.com>
2016-11-18 11:38:56 -0600

    Don't alert if smartd doesn't start and FreeNAS is running virtualized
    Ticket: #10646
    
    FreeNAS runs smartd by default, if smartd isn't running when
    it is configured to do so that is cause for an alert.
    
    However many hypervisors don't support SMART on their virtual disks.
    In those environments alerting that smartd isn't running when it
    is configured to be running isn't a huge deal.

--- gui/system/alertmods/smartdstatus.py
+++ gui/system/alertmods/smartdstatus.py
@@ -12,6 +12,14 @@ class SMARTDAlert(BaseAlert):
         alerts = []
 
         if (services.objects.get(srv_service='smartd').srv_enable):
+            # sysctl kern.vm_guest will return a hypervisor name, or the string "none" if FreeNAS is running on bare iron.
+            p0 = subprocess.Popen(["/sbin/sysctl",  "-n",  "kern.vm_guest"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            status = p0.communicate()[0].strip()
+            # This really isn't confused with python None
+            if status != "none":
+                # We got something other than "none", maybe "vmware", "xen", "vbox".  Regardless, smartd not running
+                # in these environments isn't a huge deal.  So we'll skip alerting.
+                return None
             if hasattr(notifier, 'failover_status') and notifier().failover_status() != 'MASTER':
                 return None
             p1 = subprocess.Popen(["/usr/sbin/service", "smartd", "status"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
