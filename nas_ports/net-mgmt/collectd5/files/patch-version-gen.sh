4a2bcf1bddfcc873222c1fc14e2d9210afbb6d12
John Hixson <john@ixsystems.com>
2016-12-05 23:38:25 -0500

    Update collectd version

--- nas_ports/net-mgmt/collectd5/files/patch-version-gen.sh
+++ nas_ports/net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,17 +1,17 @@
---- version-gen.sh.orig	2016-01-22 09:51:17 UTC
+--- version-gen.sh.orig	2016-09-11 08:10:25 UTC
 +++ version-gen.sh
-@@ -1,13 +1,3 @@
+@@ -1,13 +1,2 @@
  #!/bin/sh
- 
--DEFAULT_VERSION="5.5.1.git"
 -
--VERSION="`git describe 2> /dev/null | grep collectd | sed -e 's/^collectd-//'`"
+-DEFAULT_VERSION="5.6.0.git"
+-
+-if [ -d .git ]; then
+-	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | grep collectd | sed -e 's/^collectd-//' -e 's/-/./g'`"
+-fi
 -
 -if test -z "$VERSION"; then
 -	VERSION="$DEFAULT_VERSION"
 -fi
 -
--VERSION="`echo \"$VERSION\" | sed -e 's/-/./g'`"
--
 -printf "%s" "$VERSION"
-+echo -n "5.5.1.git"
++echo -n '5.6.0.git'
