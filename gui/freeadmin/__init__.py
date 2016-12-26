005bac8b7cbaa1306880fb5699f4f04d94bd36f7
William Grzybowski <wg@FreeBSD.org>
2016-12-26 15:58:46 -0200

    fix(gui): django importlib has been removed

--- gui/freeadmin/__init__.py
+++ gui/freeadmin/__init__.py
@@ -34,7 +34,7 @@ def autodiscover():
 
     import copy
     from django.conf import settings
-    from django.utils.importlib import import_module
+    from importlib import import_module
     from django.utils.module_loading import module_has_submodule
     from freenasUI.freeadmin.site import site
 
