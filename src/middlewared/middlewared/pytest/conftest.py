a0e1b02d0295ea82ecd428f6d082aa06b2e3ebd8
Marcelo Araujo <araujo@FreeBSD.org>
2016-12-29 17:48:05 +0800

    feat(middlewared/pytest): Convert the current middlewared/tests to pytest.
    Discussed with: william-gr

---
+++ src/middlewared/middlewared/pytest/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+import ConfigParser
+
+from client import Client
+
+
+class ConfigTarget(object):
+
+    def __init__(self):
+        self.config = ConfigParser.ConfigParser()
+        self.file_path = 'target.conf'
+        self.config_section = 'Target'
+        self.config.read(self.file_path)
+
+    def target_uri(self):
+        return self.config.get(self.config_section, 'uri')
+
+    def target_api(self):
+        return self.config.get(self.config_section, 'api')
+
+    def target_username(self):
+        return self.config.get(self.config_section, 'username')
+
+    def target_password(self):
+        return self.config.get(self.config_section, 'password')
+
+
+class Authentication(object):
+
+    def __init__(self):
+        conf = ConfigTarget()
+        self.connect = Client(conf.target_uri(), conf.target_api(),
+                              conf.target_username(), conf.target_password())
+
+
+@pytest.fixture
+def auth_prepare():
+    return Authentication()
