c3bc556180971315221ef310504cb92f6f9143d5
Kris Moore <kris@ixsystems.com>
2017-01-10 11:06:15 -0500

    Oops, fix the patch file included in open-vm-tools

--- nas_ports/emulators/open-vm-tools/files/patch-modules_freebsd_vmmemctl_os.c
+++ nas_ports/emulators/open-vm-tools/files/patch-modules_freebsd_vmmemctl_os.c
@@ -1,14 +1,11 @@
---- emulators/open-vm-tools/files/patch-modules_freebsd_vmmemctl_os.c	(nonexistent)
-+++ emulators/open-vm-tools/files/patch-modules_freebsd_vmmemctl_os.c	(working copy)
-@@ -0,0 +1,11 @@ 
-+--- modules/freebsd/vmmemctl/os.c.orig	2013-09-23 15:51:10 UTC
-++++ modules/freebsd/vmmemctl/os.c
-+@@ -824,7 +888,7 @@ vmmemctl_sysctl(SYSCTL_HANDLER_ARGS)
-+ static void
-+ vmmemctl_init_sysctl(void)
-+ {
-+-   oid =  sysctl_add_oid(NULL, SYSCTL_STATIC_CHILDREN(_vm), OID_AUTO,
-++   oid =  SYSCTL_ADD_OID(NULL, SYSCTL_STATIC_CHILDREN(_vm), OID_AUTO,
-+                          BALLOON_NAME, CTLTYPE_STRING | CTLFLAG_RD,
-+                          0, 0, vmmemctl_sysctl, "A",
-+                          BALLOON_NAME_VERBOSE);
+--- modules/freebsd/vmmemctl/os.c.orig	2013-09-23 15:51:10 UTC
++++ modules/freebsd/vmmemctl/os.c
+@@ -824,7 +888,7 @@ vmmemctl_sysctl(SYSCTL_HANDLER_ARGS)
+ static void
+ vmmemctl_init_sysctl(void)
+ {
+-   oid =  sysctl_add_oid(NULL, SYSCTL_STATIC_CHILDREN(_vm), OID_AUTO,
++   oid =  SYSCTL_ADD_OID(NULL, SYSCTL_STATIC_CHILDREN(_vm), OID_AUTO,
+                          BALLOON_NAME, CTLTYPE_STRING | CTLFLAG_RD,
+                          0, 0, vmmemctl_sysctl, "A",
+                          BALLOON_NAME_VERBOSE);
