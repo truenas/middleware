f1537ba8e0f2dbb34e638015681ac7b9c633d391
Marcelo Araujo <araujo@FreeBSD.org>
2017-01-26 15:12:33 +0800

    feat(middlewared): Decrease the timeout with the default value prior this change, I faced out timeout because of too many logs and debugs.
    Discussed with: william-gr

--- src/middlewared/middlewared/client/client.py
+++ src/middlewared/middlewared/client/client.py
@@ -141,7 +141,7 @@ class Client(object):
         self._calls.pop(call.id, None)
 
     def call(self, method, *params, **kwargs):
-        timeout = kwargs.pop('timeout', 60)
+        timeout = kwargs.pop('timeout', 30)
         c = Call(method, params)
         self._register_call(c)
         self._send({
