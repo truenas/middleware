4dbbe8de61635a3ffb39253182a9c79b069ea941
William Grzybowski <wg@FreeBSD.org>
2017-03-07 10:54:48 -0300

    fix(gui): close fds by default

--- gui/common/pipesubr.py
+++ gui/common/pipesubr.py
@@ -59,7 +59,7 @@ def unblock_sigchld():
 #
 # https://bugs.freenas.org/issues/15817
 #
-def pipeopen(command, important=True, logger=log, allowfork=False, quiet=True, close_fds=False):
+def pipeopen(command, important=True, logger=log, allowfork=False, quiet=True, close_fds=True):
     if not quiet:
         logger.log(
             logging.NOTICE if important else logging.DEBUG,
