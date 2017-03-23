ce6caefe7f8cfd94e293e7f9cc9804001e96636a
William Grzybowski <wg@FreeBSD.org>
2017-03-23 16:45:10 -0300

    fix(middlewared): unused imports, add accepts and comments in consul

--- src/middlewared/middlewared/plugins/consulkv.py
+++ src/middlewared/middlewared/plugins/consulkv.py
@@ -1,17 +1,24 @@
-from middlewared.schema import accepts, Dict, Str
-from middlewared.service import job, Service
+from middlewared.schema import accepts, Any, Str
+from middlewared.service import Service
 
 import consul
-import re
-import sys
+
 
 class ConsulService(Service):
 
+    @accepts(Str('key'), Any('value'))
     def set_kv(self, key, value):
+        """
+        Sets `key` with `value` in Consul KV.
+        """
         c = consul.Consul()
         return c.kv.put(key, value)
 
+    @accepts(Str('key'))
     def get_kv(self, key):
+        """
+        Gets value of `key` in Consul KV.
+        """
         c = consul.Consul()
         index = None
         index, data = c.kv.get(key, index=index)
@@ -19,4 +26,3 @@ class ConsulService(Service):
             return data['Value'].decode("utf-8")
         else:
             return ""
-
