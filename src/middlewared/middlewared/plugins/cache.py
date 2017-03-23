9ecff0775d581fdaa619222927d933a5ba78f493
William Grzybowski <wg@FreeBSD.org>
2017-03-23 10:45:18 -0300

    feat(middlewared): add accepts() to cache methods

--- src/middlewared/middlewared/plugins/cache.py
+++ src/middlewared/middlewared/plugins/cache.py
@@ -1,3 +1,4 @@
+from middlewared.schema import Any, Str, accepts
 from middlewared.service import Service
 
 
@@ -10,11 +11,14 @@ class CacheService(Service):
         super(CacheService, self).__init__(*args, **kwargs)
         self.__cache = {}
 
+    @accepts(Str('key'))
     def get(self, key):
         return self.__cache[key]
 
+    @accepts(Str('key'), Any('value'))
     def put(self, key, value):
         self.__cache[key] = value
 
+    @accepts(Str('key'))
     def pop(self, key):
         return self.__cache.pop(key, None)
