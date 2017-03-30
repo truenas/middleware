4323b54bca17b8358d5d59019aee2d5b4d9183fa
William Grzybowski <wg@FreeBSD.org>
2017-03-30 10:07:00 -0300

    fix(examples): move install.conf.example to examples folder

--- webui/src/app/pages/system/system.routing.ts
+++ webui/src/app/pages/system/system.routing.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdvancedComponent } from './advanced/';
 import { GeneralComponent } from './general/';
+import { UpdateComponent } from './update/';
 
 export const routes: Routes = [
   { path: 'general', component: GeneralComponent },
   { path: 'advanced', component: AdvancedComponent },
+  { path: 'update', component: UpdateComponent },
 ];
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
