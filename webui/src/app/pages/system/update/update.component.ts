15090b0eac2ebcab4a29c278a246b7b9d83bbc4f
William Grzybowski <wg@FreeBSD.org>
2017-03-30 16:31:10 -0300

    fix(webui): actually call update

--- webui/src/app/pages/system/update/update.component.ts
+++ webui/src/app/pages/system/update/update.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { RestService, WebSocketService } from '../../../services/';
 
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
@@ -10,15 +12,60 @@ import { RestService, WebSocketService } from '../../../services/';
 export class UpdateComponent implements OnInit {
 
   private packages: any[] = [];
+  private status: string;
+  private updating: boolean = false;
+  private updated: boolean = false;
+  private progress: Object = {};
+  private job: any = {};
+
+  private busy: Subscription;
 
   constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService) {
 
   }
 
   ngOnInit() {
-    this.ws.call('update.get_pending').subscribe((res) => {
-      this.packages = res;
+  }
+
+  check() {
+    this.busy = this.ws.call('update.check_available').subscribe((res) => {
+      this.status = res.status;
+      if(res.status == 'AVAILABLE') {
+        this.packages = [];
+        res.changes.forEach((item) => {
+          if(item.operation == 'upgrade') {
+            this.packages.push({
+              name: item.old.name + '-' + item.old.version + ' -> ' + item.new.name + '-' + item.new.version,
+            });
+          } else if (item.operation == 'install') {
+            this.packages.push({
+              name: item.new.name + '-' + item.new.version,
+            });
+          } else {
+            this.packages.push({
+              name: item.old.name + '-' + item.old.version,
+            });
+          }
+        });
+      }
     });
+  }
+
+  update() {
+    this.updating = true;
+    this.ws.job('update.update', []).subscribe(
+      (res) => {
+        this.job = res;
+        this.progress = res.progress;
+      },
+      () => {},
+      () => {
+        this.updating = false;
+        if(this.job.state == 'SUCCESS') {
+          this.updated = true;
+        }
+      }
+    );
   }
 
 }
