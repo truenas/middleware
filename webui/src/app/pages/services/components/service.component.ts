a8a3823b778ab3b63e56985c64b2c1fed5cd4b6e
Lola <lola.ly27@gmail.com>
2017-04-19 18:48:18 -0700

    fix(gui): imporve service screen
    Ticket: #23458

--- webui/src/app/pages/services/components/service.component.ts
+++ webui/src/app/pages/services/components/service.component.ts
@@ -6,21 +6,35 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'service',
+  styleUrls: ['./service.component.scss'],
   template: `
   <ba-card>
     <div [ngBusy]="busy" class="row">
-      <div class="col-md-1">
+      <div class="col-md-2">
         {{ status.label }}
       </div>
 
-      <div class="col-md-1">
+      <div class="col-md-1" [ngClass]="status.state == 'RUNNING' ? 'state-label-running' : 'state-label-stopped'">
+        {{status.state}}
+      </div>
+
+      <div class="col-md-2">
         <button class="btn btn-primary" (click)="toggle()">
+          <i [ngClass]="status.state == 'RUNNING' ? 'ion-stop' : 'ion-power'"></i>
           <span *ngIf="status.state != 'RUNNING'">Start</span>
           <span *ngIf="status.state == 'RUNNING'">Stop</span>
         </button>
       </div>
+
       <div class="col-md-2">
         <ba-checkbox [(ngModel)]="status.enable" (change)="enableToggle($event)" [label]="'Start on Boot'"></ba-checkbox>
+      </div>
+
+      <div class="col-md-1">
+        <button class="btn btn-primary">
+          <i class="ion-wrench"></i>
+          <span>Edit</span>
+        </button>
       </div>
     </div>
   </ba-card>
