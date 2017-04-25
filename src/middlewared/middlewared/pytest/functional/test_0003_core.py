e30f6f746bb7592a24305d8b90884af169276410
William Grzybowski <wg@FreeBSD.org>
2017-04-25 15:46:56 -0300

    feat(middlewared): add websocket to pytest
    While I'm here make things a bit shorter.

--- src/middlewared/middlewared/pytest/functional/test_0003_core.py
+++ src/middlewared/middlewared/pytest/functional/test_0003_core.py
@@ -1,26 +1,26 @@
-def test_get_services(auth_prepare):
-    services = auth_prepare.connect.get('core/get_services')
+def test_get_services(conn):
+    services = conn.rest.get('core/get_services')
 
     assert services.status_code == 200
     assert isinstance(services.json(), dict) is True
 
 
-def test_get_methods(auth_prepare):
-    methods = auth_prepare.connect.post('core/get_methods')
+def test_get_methods(conn):
+    methods = conn.rest.post('core/get_methods')
 
     assert methods.status_code == 200
     assert isinstance(methods.json(), dict) is True
 
 
-def test_get_jobs(auth_prepare):
-    jobs = auth_prepare.connect.get('core/get_jobs')
+def test_get_jobs(conn):
+    jobs = conn.rest.get('core/get_jobs')
 
     assert jobs.status_code == 200
     assert isinstance(jobs.json(), list) is True
 
 
-def test_ping(auth_prepare):
-    ping = auth_prepare.connect.get('core/ping')
+def test_ping(conn):
+    ping = conn.rest.get('core/ping')
 
     assert ping.status_code == 200
     assert ping.json() == 'pong'
