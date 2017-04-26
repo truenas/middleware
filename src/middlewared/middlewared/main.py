fa597eff7a6eb17bc81870ecb6737984878416a8
William Grzybowski <wg@FreeBSD.org>
2017-04-26 15:17:18 -0300

    fix(middlewared): remove unused modules because of last commit

--- src/middlewared/middlewared/main.py
+++ src/middlewared/middlewared/main.py
@@ -7,7 +7,6 @@ from .client.protocol import DDPProtocol
 from .job import Job, JobsQueue
 from .restful import RESTfulAPI
 from .service import CallException
-from .utils import Popen
 from collections import OrderedDict, defaultdict
 from daemon import DaemonContext
 from daemon.pidfile import TimeoutPIDLockFile
@@ -27,7 +26,6 @@ import os
 import psutil
 import setproctitle
 import signal
-import subprocess
 import sys
 import traceback
 import types
