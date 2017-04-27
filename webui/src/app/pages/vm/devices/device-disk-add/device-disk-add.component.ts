52aab9f316b805d24609f61b58eae27a5140ba72
Lola <lola.ly27@gmail.com>
2017-04-26 17:20:02 -0700

    Add disk component

--- webui/src/app/pages/vm/devices/device-disk-add/device-disk-add.component.ts
+++ webui/src/app/pages/vm/devices/device-disk-add/device-disk-add.component.ts
@@ -1,6 +1,6 @@
 import { ApplicationRef, Component, Injector, OnInit, ViewContainerRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute} from '@angular/router';
 
 import { DynamicFormControlModel, DynamicFormService, DynamicCheckboxModel, DynamicInputModel, DynamicSelectModel, DynamicRadioGroupModel } from '@ng2-dynamic-forms/core';
 import { GlobalState } from '../../../../global.state';
@@ -8,20 +8,35 @@ import { RestService, WebSocketService } from '../../../../services/';
 
 @Component({
   selector: 'app-device-disk-add',
-  template: `<entity-add [conf]="this"></entity-add>`
+  template: `<device-add [conf]="this"></device-add>`
 })
 export class DeviceDiskAddComponent {
 
   protected resource_name: string = 'vm/device';
-  protected route_success: string[] = ['vm', 'devices'];
+  protected pk: any;
+  protected route_success: string[] = ['vm', this.pk, 'devices'];
   protected formModel: DynamicFormControlModel[] = [
     new DynamicInputModel({
-        id: 'foo',
-        label: 'foo',
+      id: 'zvol',
+      label: 'ZVol',
+    }),
+    new DynamicSelectModel({
+      id: 'mode',
+      label: 'Mode',
+      options: [
+        { label: 'AHCI', value: "AHCI" },
+        { label: 'VirtIO', value: "VirtIO" },
+      ],
     }),
   ];
 
-  constructor(protected router: Router, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected _injector: Injector, protected _appRef: ApplicationRef, protected _state: GlobalState) {
+  afterInit() {
+    this.route.params.subscribe(params => {
+        this.pk = params['pk'];
+    });
+  }
+
+  constructor(protected router: Router, protected route: ActivatedRoute, protected rest: RestService, protected ws: WebSocketService, protected formService: DynamicFormService, protected _injector: Injector, protected _appRef: ApplicationRef, protected _state: GlobalState) {
 
   }
 
