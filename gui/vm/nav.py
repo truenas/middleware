7d2570643a1d7c781e131cbc3661eedb8d27701a
William Grzybowski <wg@FreeBSD.org>
2017-05-03 16:11:23 -0300

    feat(gui): remove VM from TrueNAS
    Ticket:	#23757
    (cherry picked from commit c3d748008ae3ad934983fc98fe2cbb27ac0b96ae)

--- gui/vm/nav.py
+++ gui/vm/nav.py
@@ -1,12 +1,19 @@
 from django.utils.translation import ugettext_lazy as _
 from freenasUI.freeadmin.tree import TreeNode
-from freenasUI.vm.utils import vm_enabled
+from freenasUI.middleware.notifier import notifier
 
 NAME = _('VMs')
 BLACKLIST = ['VM', 'Device']
 ICON = 'VMIcon'
 URL = 'vm_home'
 ORDER = 75
+
+
+def init(tree_roots, nav, request):
+
+    if not notifier().is_freenas():
+        tree_roots.unregister(nav)
+
 
 # TODO: For when we have more than just VM tab
 #class VMView(TreeNode):
