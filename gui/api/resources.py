ad4a34c0c531afc5de7ef7ad7967130f43161a48
William Grzybowski <wg@FreeBSD.org>
2017-05-04 13:21:07 -0300

    feat(api): endpoint for intermediate CA
    Ticket:	#23764

--- gui/api/resources.py
+++ gui/api/resources.py
@@ -95,6 +95,7 @@ from freenasUI.system.forms import (
     BootEnvAddForm,
     BootEnvRenameForm,
     CertificateAuthorityCreateInternalForm,
+    CertificateAuthorityCreateIntermediateForm,
     ManualUpdateTemporaryLocationForm,
     ManualUpdateUploadForm,
     ManualUpdateWizard,
@@ -2813,12 +2814,40 @@ class CertificateAuthorityResourceMixin(object):
     def prepend_urls(self):
         return [
             url(
+                r"^(?P<resource_name>%s)/intermediate%s$" % (
+                    self._meta.resource_name, trailing_slash()
+                ),
+                self.wrap_view('intermediate'),
+            ),
+            url(
                 r"^(?P<resource_name>%s)/internal%s$" % (
                     self._meta.resource_name, trailing_slash()
                 ),
                 self.wrap_view('internal'),
             ),
         ]
+
+    def intermediate(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        self.is_authenticated(request)
+
+        if request.body:
+            deserialized = self.deserialize(
+                request,
+                request.body,
+                format=request.META.get('CONTENT_TYPE', 'application/json'),
+            )
+        else:
+            deserialized = {}
+
+        form = CertificateAuthorityCreateIntermediateForm(data=deserialized)
+        if not form.is_valid():
+            raise ImmediateHttpResponse(
+                response=self.error_response(request, form.errors)
+            )
+        else:
+            form.save()
+        return HttpResponse('Certificated Authority created.', status=202)
 
     def internal(self, request, **kwargs):
         self.method_check(request, allowed=['post'])
