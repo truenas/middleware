5d37d1aca71119f54e8233ae483eb103a5a4d971
William Grzybowski <wg@FreeBSD.org>
2017-05-22 20:10:58 -0300

    feat(middlewared): react on DEVFS events
    Ticket:	#23523

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -1,4 +1,17 @@
+import os
+import re
+import sys
+import sysctl
+
 from middlewared.service import filterable, private, CRUDService
+
+# FIXME: temporary import of SmartAlert until alert is implemented
+# in middlewared
+if '/usr/local/www' not in sys.path:
+    sys.path.insert(0, '/usr/local/www')
+from freenasUI.services.utils import SmartAlert
+
+RE_ISDISK = re.compile(r'^(da|ada|vtbd|mfid|nvd)[0-9]+$')
 
 
 class DiskService(CRUDService):
@@ -18,3 +31,43 @@ class DiskService(CRUDService):
     def disk_extend(self, disk):
         disk.pop('enabled', None)
         return disk
+
+
+def _event_devfs(middleware, event_type, args):
+    data = args['data']
+    if data.get('subsystem') != 'CDEV':
+        return
+
+    if data['type'] == 'CREATE':
+        disks = middleware.threaded(lambda: sysctl.filter('kern.disks')[0].value.split())
+        # Device notified about is not a disk
+        if data['cdev'] not in disks:
+            return
+        # TODO: hack so every disk is not synced independently during boot
+        # This is a performance issue
+        if os.path.exists('/tmp/.sync_disk_done'):
+            middleware.call('notifier.sync_disk', data['cdev'])
+            try:
+                with SmartAlert() as sa:
+                    sa.device_delete(data['cdev'])
+            except Exception:
+                pass
+    elif data['type'] == 'DESTROY':
+        # Device notified about is not a disk
+        if not RE_ISDISK.match(data['cdev']):
+            return
+        # TODO: hack so every disk is not synced independently during boot
+        # This is a performance issue
+        if os.path.exists('/tmp/.sync_disk_done'):
+            middleware.call('notifier.sync_disks')
+            middleware.call('notifier.multipath_sync')
+            try:
+                with SmartAlert() as sa:
+                    sa.device_delete(data['cdev'])
+            except Exception:
+                pass
+
+
+def setup(middleware):
+    # Listen to DEVFS events so we can sync on disk attach/detach
+    middleware.event_subscribe('devd.devfs', _event_devfs)
