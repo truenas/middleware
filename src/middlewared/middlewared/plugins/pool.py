2c2bdcaa4da5b326cccd57c76620700af9c835d1
William Grzybowski <wg@FreeBSD.org>
2017-05-26 16:33:51 -0300

    feat(middlewared): add initial pool plugin

---
+++ src/middlewared/middlewared/plugins/pool.py
@@ -0,0 +1,49 @@
+import libzfs
+
+from bsd import geom
+
+from middlewared.schema import accepts, Int
+from middlewared.service import filterable, private, CRUDService
+
+
+class PoolService(CRUDService):
+
+    @filterable
+    def query(self, filters=None, options=None):
+        filters = filters or []
+        options = options or {}
+        options['extend'] = 'pool.pool_extend'
+        options['prefix'] = 'vol_'
+        return self.middleware.call('datastore.query', 'storage.volume', filters, options)
+
+    @private
+    def pool_extend(self, pool):
+        pool.pop('fstype', None)
+        return pool
+
+    @accepts(Int('id'))
+    def get_disks(self, oid):
+        """
+        Get all disks from a given pool `id`.
+        """
+        pool = self.query([('id', '=', oid)], {'get': True})
+        zfs = libzfs.ZFS()
+        zpool = zfs.get(pool['name'])
+
+        self.middleware.threaded(geom.scan)
+        labelclass = geom.class_by_name('LABEL')
+        for absdev in zpool.disks:
+            dev = absdev.replace('/dev/', '').replace('.eli', '')
+            find = labelclass.xml.findall(f".//provider[name='{dev}']/../consumer/provider")
+            name = None
+            if find:
+                name = geom.provider_by_id(find[0].get('ref')).geom.name
+            else:
+                g = geom.geom_by_name('DEV', dev)
+                if g:
+                    name = g.consumer.provider.geom.name
+
+            if name and geom.geom_by_name('DISK', name):
+                yield name
+            else:
+                self.logger.debug(f'Could not find disk for {dev}')
