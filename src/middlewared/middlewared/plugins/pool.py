a55fd60ddb1907fd837e96338428cab23d9fe55a
William Grzybowski <wg@FreeBSD.org>
2017-05-30 13:01:34 -0300

    feat(middlwared): add private zfs pool service

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -49,23 +49,4 @@ class PoolService(CRUDService):
         pool = self.query([('id', '=', oid)], {'get': True})
         if not pool['is_decrypted']:
             return []
-        zfs = libzfs.ZFS()
-        zpool = zfs.get(pool['name'])
-
-        self.middleware.threaded(geom.scan)
-        labelclass = geom.class_by_name('LABEL')
-        for absdev in zpool.disks:
-            dev = absdev.replace('/dev/', '').replace('.eli', '')
-            find = labelclass.xml.findall(f".//provider[name='{dev}']/../consumer/provider")
-            name = None
-            if find:
-                name = geom.provider_by_id(find[0].get('ref')).geom.name
-            else:
-                g = geom.geom_by_name('DEV', dev)
-                if g:
-                    name = g.consumer.provider.geom.name
-
-            if name and geom.geom_by_name('DISK', name):
-                yield name
-            else:
-                self.logger.debug(f'Could not find disk for {dev}')
+        return self.middleware.call('zfs.pool.get_disks', pool['name'])
---
+++ src/middlewared/middlewared/plugins/zfs.py
@@ -0,0 +1,36 @@
+from bsd import geom
+
+from middlewared.schema import Str, accepts
+from middlewared.service import Service
+
+import libzfs
+
+
+class ZFSPoolService(Service):
+
+    class Config:
+        namespace = 'zfs.pool'
+        private = True
+
+    @accepts(Str('pool'))
+    def get_disks(self, name):
+        zfs = libzfs.ZFS()
+        zpool = zfs.get(name)
+
+        self.middleware.threaded(geom.scan)
+        labelclass = geom.class_by_name('LABEL')
+        for absdev in zpool.disks:
+            dev = absdev.replace('/dev/', '').replace('.eli', '')
+            find = labelclass.xml.findall(f".//provider[name='{dev}']/../consumer/provider")
+            name = None
+            if find:
+                name = geom.provider_by_id(find[0].get('ref')).geom.name
+            else:
+                g = geom.geom_by_name('DEV', dev)
+                if g:
+                    name = g.consumer.provider.geom.name
+
+            if name and geom.geom_by_name('DISK', name):
+                yield name
+            else:
+                self.logger.debug(f'Could not find disk for {dev}')
