4505ea469cba47de90a6c7bcbff0bee021f5df4e
Marcelo Araujo <araujo@FreeBSD.org>
2017-05-31 16:09:45 +0800

    feat(container): Move field to a different position.

--- gui/vm/forms.py
+++ gui/vm/forms.py
@@ -29,7 +29,7 @@ class VMForm(ModelForm):
         super(VMForm, self).__init__(*args, **kwargs)
         self.fields['vm_type'].widget.attrs['onChange'] = ("vmTypeToggle();")
         key_order(self, 0, 'vm_type', instance=True)
-        key_order(self, 6, 'container_path', instance=True)
+        key_order(self, 5, 'container_path', instance=True)
 
     def get_cpu_flags(self):
         cpu_flags = {}
