e98ea2a65c30e69d6083685575e191f3dab340b8
William Grzybowski <wg@FreeBSD.org>
2017-06-08 10:16:40 -0300

    feat(middlewared): add filesystem service with `listdir` method

---
+++ src/middlewared/middlewared/plugins/filesystem.py
@@ -0,0 +1,55 @@
+from middlewared.schema import Str, accepts
+from middlewared.service import Service
+
+import errno
+import os
+
+
+class FilesystemService(Service):
+
+    @accepts(Str('path'))
+    def listdir(self, path):
+        """
+        Get the contents of a directory.
+
+        Each entry of the list consists of:
+          name(str): name of the file
+          path(str): absolute path of the entry
+          size(int): size of the entry
+          mode(int): file mode/permission
+          uid(int): user id of entry owner
+          gid(int): group id of entry onwer
+        """
+        if not os.path.exists(path):
+            return CallError(f'Directory {path} does not exist', errno.ENOENT)
+
+        if not os.path.isdir(path):
+            return CallError(f'Path {path} is not a directory', errno.ENOTDIR)
+
+        for entry in os.scandir(path):
+            if entry.is_dir():
+                etype = 'DIRECTORY'
+            elif entry.is_file():
+                etype = 'FILE'
+            elif entry.is_symlink():
+                etype = 'SYMLINK'
+            else:
+                etype = 'OTHER'
+
+            data = {
+                'name': entry.name,
+                'path': entry.path,
+                'realpath': os.path.realpath(entry.path) if etype == 'SYMLINK' else entry.path,
+                'type': etype,
+            }
+            try:
+                stat = entry.stat()
+                data.update({
+                    'size': stat.st_size,
+                    'mode': stat.st_mode,
+                    'uid': stat.st_uid,
+                    'gid': stat.st_gid,
+                })
+            except FileNotFoundError:
+                data.update({'size': None, 'mode': None, 'uid': None, 'gid': None})
+            yield data
