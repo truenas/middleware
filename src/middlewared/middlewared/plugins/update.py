9fc0e357c0ae35ba0ad993018a07f6a376988498
William Grzybowski <wg@FreeBSD.org>
2017-06-08 17:45:35 -0300

    feat(middlewared): add update.manual

--- src/middlewared/middlewared/plugins/update.py
+++ src/middlewared/middlewared/plugins/update.py
@@ -1,6 +1,7 @@
 from middlewared.schema import accepts, Bool, Dict, Str
-from middlewared.service import job, Service
+from middlewared.service import job, CallError, Service
 
+import errno
 import re
 import socket
 import sys
@@ -380,3 +381,18 @@ Changelog:
             except Exception:
                 self.logger.warn('Failed to send email about new update', exc_info=True)
         return True
+
+    @accepts(Str('path'))
+    @job(lock='updatemanual', process=True)
+    def manual(self, job, path):
+        """
+        Apply manual update of file `path`.
+        """
+        rv = self.middleware.call('notifier.validate_update', path)
+        if not rv:
+            raise CallError('Invalid update file', errno.EINVAL)
+        self.middleware.call('notifier.apply_update', path)
+        try:
+            self.middleware.call('notifier.destroy_upload_location')
+        except Exception:
+            self.logger.warn('Failed to destroy upload location', exc_info=True)
