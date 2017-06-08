c0d1db45954c13293e07c7756b07080539419b18
William Grzybowski <wg@FreeBSD.org>
2017-06-08 18:45:13 -0300

    feat(middlewared): add system.ready method
    Flag telling whether the system completed boot and is ready to use.

--- src/middlewared/middlewared/plugins/system.py
+++ src/middlewared/middlewared/plugins/system.py
@@ -16,6 +16,9 @@ if '/usr/local/www' not in sys.path:
     sys.path.append('/usr/local/www')
 from freenasUI.system.utils import debug_get_settings, debug_run
 
+# Flag telling whether the system completed boot and is ready to use
+SYSTEM_READY = False
+
 
 class SystemService(Service):
 
@@ -31,6 +34,13 @@ class SystemService(Service):
     @accepts()
     def version(self):
         return sw_version()
+
+    @accepts()
+    def ready(self):
+        """
+        Returns whether the system completed boot and is ready to use
+        """
+        return SYSTEM_READY
 
     @accepts()
     def info(self):
@@ -103,3 +113,17 @@ class SystemService(Service):
         mntpt, direc, dump = debug_get_settings()
         debug_run(direc)
         return dump
+
+
+def _event_system_ready(middleware, event_type, args):
+    """
+    Method called when system is ready, supposed to enable the flag
+    telling the system has completed boot.
+    """
+    global SYSTEM_READY
+    if args['id'] == 'ready':
+        SYSTEM_READY = True
+
+
+def setup(middleware):
+    middleware.event_subscribe('system', _event_system_ready)
