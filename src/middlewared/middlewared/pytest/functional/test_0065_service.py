c18571b0ed461341c69add9b21fb7700d6fab836
William Grzybowski <wg@FreeBSD.org>
2017-06-14 16:01:30 -0300

    feat(middlewared): add service tests

---
+++ src/middlewared/middlewared/pytest/functional/test_0065_service.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+services = [
+    'afp', 'cifs', 'nfs', 'snmp', 'tftp', 'webdav', 'lldp'
+]
+
+
+def test_service_query(conn):
+    req = conn.rest.get('service')
+
+    assert req.status_code == 200
+    assert isinstance(req.json(), list) is True
+
+
+def test_service_update(conn):
+    req = conn.rest.get('service')
+
+    assert req.status_code == 200
+    services = req.json()
+    assert isinstance(services, list) is True
+
+    for svc in services[:5]:
+        req = conn.rest.put(f'service/id/{svc["id"]}', data=[{'enable': svc['enable']}])
+        assert req.status_code == 200
+
+
+
+@pytest.mark.parametrize('svc', services)
+def test_service_start(conn, svc):
+
+    req = conn.rest.post('service/start', data=[svc])
+
+    assert req.status_code == 200
+    assert req.json() is True
+
+
+@pytest.mark.parametrize('svc', services)
+def test_service_stop(conn, svc):
+
+    req = conn.rest.post('service/stop', data=[svc])
+
+    assert req.status_code == 200
+    assert req.json() is False
