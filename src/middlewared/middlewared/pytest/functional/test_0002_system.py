0188366938c76a70c62ba04640ff0f7f079ff420
William Grzybowski <wg@FreeBSD.org>
2017-06-19 14:07:27 -0300

    feat(middlewared): add couple more system tests

--- src/middlewared/middlewared/pytest/functional/test_0002_system.py
+++ src/middlewared/middlewared/pytest/functional/test_0002_system.py
@@ -1,12 +1,22 @@
-def test_version(conn):
+def test_system_version(conn):
     version = conn.rest.get('system/version')
 
-    assert version.status_code == 200
+    assert version.status_code == 200, req.text
     assert isinstance(version.json(), str) is True
 
 
-def test_info(conn):
+def test_system_info(conn):
     info = conn.rest.get('system/info')
-
-    assert info.status_code == 200
+    assert info.status_code == 200, req.text
     assert isinstance(info.json(), dict) is True
+
+
+def test_system_ready(conn):
+    req = conn.rest.get('system/ready')
+    assert req.status_code == 200, req.text
+    assert isinstance(req.json(), bool) is True
+
+
+def test_system_is_freenas(conn):
+    r = conn.ws.call('system.is_freenas')
+    assert isinstance(r, bool) is True
