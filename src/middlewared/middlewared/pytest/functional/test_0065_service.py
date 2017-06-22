7c45ed43f170a92e6fa0280c8cd45f6e0c65c44b
William Grzybowski <wg@FreeBSD.org>
2017-06-22 12:56:04 -0300

    feat(middlewared): show req.text in case of status check failure

--- src/middlewared/middlewared/pytest/functional/test_0065_service.py
+++ src/middlewared/middlewared/pytest/functional/test_0065_service.py
@@ -9,21 +9,20 @@ services = [
 def test_service_query(conn):
     req = conn.rest.get('service')
 
-    assert req.status_code == 200
+    assert req.status_code == 200, req.text
     assert isinstance(req.json(), list) is True
 
 
 def test_service_update(conn):
     req = conn.rest.get('service')
 
-    assert req.status_code == 200
+    assert req.status_code == 200, req.text
     services = req.json()
     assert isinstance(services, list) is True
 
     for svc in services[:5]:
         req = conn.rest.put(f'service/id/{svc["id"]}', data=[{'enable': svc['enable']}])
-        assert req.status_code == 200
-
+        assert req.status_code == 200, req.text
 
 
 @pytest.mark.parametrize('svc', services)
@@ -31,7 +30,7 @@ def test_service_start(conn, svc):
 
     req = conn.rest.post('service/start', data=[svc])
 
-    assert req.status_code == 200
+    assert req.status_code == 200, req.text
     assert req.json() is True
 
 
@@ -40,5 +39,5 @@ def test_service_stop(conn, svc):
 
     req = conn.rest.post('service/stop', data=[svc])
 
-    assert req.status_code == 200
+    assert req.status_code == 200, req.text
     assert req.json() is False
