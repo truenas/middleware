74299defd9301b46e601fcecf876ca32945e8aaf
William Grzybowski <wg@FreeBSD.org>
2017-06-29 14:15:07 -0300

    feat(middlewared): add "update" prefix to update tests

--- src/middlewared/middlewared/pytest/functional/test_0080_update.py
+++ src/middlewared/middlewared/pytest/functional/test_0080_update.py
@@ -1,18 +1,18 @@
-def test_get_trains(conn):
+def test_update_get_trains(conn):
     req = conn.rest.get('update/get_trains')
 
     assert req.status_code == 200
     assert isinstance(req.json(), dict) is True
 
 
-def test_check_available(conn):
+def test_update_check_available(conn):
     req = conn.rest.post('update/check_available')
 
     assert req.status_code == 200
     assert isinstance(req.json(), dict) is True
 
 
-def test_get_pending(conn):
+def test_update_get_pending(conn):
     req = conn.rest.post('update/get_pending')
 
     assert req.status_code == 200
