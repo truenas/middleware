a213dd4f5b752fd2bc0b5f5ccad451f9d7890ad3
William Grzybowski <wg@FreeBSD.org>
2017-07-04 19:20:30 -0300

    feat(middlewared): first pass of support.new_ticket

--- src/middlewared/middlewared/plugins/support.py
+++ src/middlewared/middlewared/plugins/support.py
@@ -1,10 +1,12 @@
-from middlewared.schema import Str, accepts
-from middlewared.service import CallError, Service
+from middlewared.schema import Bool, Dict, Str, accepts
+from middlewared.service import CallError, Service, job
+from middlewared.utils import Popen
 
 import errno
 import json
 import requests
 import simplejson
+import subprocess
 
 ADDRESS = 'support-proxy.ixsystems.com'
 
@@ -45,3 +47,62 @@ class SupportService(Service):
             raise CallError(data['message'], errno.EINVAL)
 
         return data
+
+    @accepts(Dict(
+        'new_ticket',
+        Str('title', required=True),
+        Str('body', required=True),
+        Str('category', required=True),
+        Bool('attach_debug', default=False),
+        Str('username'),
+        Str('password'),
+        Str('type'),
+        Str('criticality'),
+        Str('environment'),
+        Str('phone'),
+        Str('name'),
+        Str('email'),
+    ))
+    @job()
+    async def new_ticket(self, job, data):
+
+        sw_name = 'freenas' if await self.middleware.call('system.is_freenas') else 'truenas'
+
+        if sw_name == 'freenas':
+            required_attrs = ('type', 'username', 'password')
+        else:
+            required_attrs = ('phone', 'name', 'email', 'criticality', 'environment')
+            data['serial'] = (await (await Popen(['/usr/local/sbin/dmidecode', '-s', 'system-serial-number'], stdout=subprocess.PIPE)).communicate())[0].decode().split('\n')[0].upper()
+            data['company'] = 'Unknown'
+
+        for i in required_attrs:
+            if i not in data:
+                raise CallError(f'{i} is required', errno.EINVAL)
+
+        data['version'] = (await self.middleware.call('system.version')).split('-', 1)[-1]
+        data['debug'] = data.pop('attach_debug')
+
+        try:
+            r = await self.middleware.threaded(lambda: requests.post(
+                'https://%s/%s/apii/v1.0/ticket' % (ADDRESS, sw_name),
+                data=json.dumps(data),
+                headers={'Content-Type': 'application/json'},
+                timeout=10,
+            ))
+            data = r.json()
+        except simplejson.JSONDecodeError as e:
+            self.logger.debug(f'Failed to decode ticket attachment response: {r.text}')
+            raise CallError('Invalid proxy server response', errno.EBADMSG)
+        except requests.ConnectionError as e:
+            raise CallError(f'Connection error {e}', errno.EBADF)
+        except requests.Timeout as e:
+            raise CallError('Connection time out', errno.ETIMEDOUT)
+
+        if r.status_code != 200:
+            self.logger.debug(f'Support Ticket failed ({r.status_code}): {r.text}', r.status_code, r.text)
+            raise CallError('Ticket creation failed, try again later.', errno.EINVAL)
+
+        if data['error']:
+            raise CallError(data['message'], errno.EINVAL)
+
+        return data.get('ticketnum')
