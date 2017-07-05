f6fa044b6177a0b497408c7139faabf71adee818
William Grzybowski <wg@FreeBSD.org>
2017-07-05 12:21:43 -0300

    feat(middlewared): make submitting a ticket actually work
    Sending debug still needs to be worked on.

--- src/middlewared/middlewared/plugins/support.py
+++ src/middlewared/middlewared/plugins/support.py
@@ -8,6 +8,14 @@ import requests
 import simplejson
 import subprocess
 
+# FIXME: Remove when we can generate debug from middleware
+if '/usr/local/www' not in sys.path:
+    sys.path.append('/usr/local/www')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freenasUI.settings')
+
+import django
+django.setup()
+
 ADDRESS = 'support-proxy.ixsystems.com'
 
 
@@ -56,7 +64,7 @@ class SupportService(Service):
         Bool('attach_debug', default=False),
         Str('username'),
         Str('password'),
-        Str('type'),
+        Str('type', enum=['BUG', 'FEATURE']),
         Str('criticality'),
         Str('environment'),
         Str('phone'),
@@ -65,6 +73,8 @@ class SupportService(Service):
     ))
     @job()
     async def new_ticket(self, job, data):
+
+        job.set_progress(1, 'Gathering data')
 
         sw_name = 'freenas' if await self.middleware.call('system.is_freenas') else 'truenas'
 
@@ -80,11 +90,18 @@ class SupportService(Service):
                 raise CallError(f'{i} is required', errno.EINVAL)
 
         data['version'] = (await self.middleware.call('system.version')).split('-', 1)[-1]
-        data['debug'] = data.pop('attach_debug')
+        data['user'] = data.pop('username')
+        debug = data.pop('attach_debug')
+
+        type_ = data.get('type')
+        if type_:
+            data['type'] = type_.lower()
+
+        job.set_progress(20, 'Submitting ticket')
 
         try:
             r = await self.middleware.threaded(lambda: requests.post(
-                'https://%s/%s/apii/v1.0/ticket' % (ADDRESS, sw_name),
+                'https://%s/%s/api/v1.0/ticket' % (ADDRESS, sw_name),
                 data=json.dumps(data),
                 headers={'Content-Type': 'application/json'},
                 timeout=10,
@@ -105,4 +122,31 @@ class SupportService(Service):
         if data['error']:
             raise CallError(data['message'], errno.EINVAL)
 
-        return data.get('ticketnum')
+        ticket = data.get('ticketnum')
+        if not ticket:
+            raise CallError('New ticket number was not informed', errno.EINVAL)
+        job.set_progress(100, f'Ticket created: {ticket}', extra={'ticket': ticket})
+
+        if debug:
+			# FIXME: generate debug from middleware
+            mntpt, direc, dump = await self.middleware.threaded(debug_get_settings)
+
+            job.set_progress(100, 'Generating debug file')
+            await self.middleware.threaded(debug_generate)
+
+            not_freenas = not (await self.middleware.call('system.is_freenas'))
+            if not_freenas:
+                not_freenas &= await self.middleware.call('notifier.failover_licensed')
+            if not_freenas:
+                debug_file = f'{direc}/debug.tar'
+                debug_name = 'debug-{}.tar'.format(time.strftime('%Y%m%d%H%M%S'))
+            else:
+                debug_file = dump
+                debug_name = 'debug-{}-{}.txz'.format(
+                    gc.gc_hostname,
+                    time.strftime('%Y%m%d%H%M%S'),
+                )
+
+            # Send it
+
+        return ticket
