8eedc331e67625df550df401eb68cc37c4d87a05
William Grzybowski <wg@FreeBSD.org>
2017-07-05 15:17:23 -0300

    feat(gui): use middleware to create a new support ticket

--- gui/support/utils.py
+++ gui/support/utils.py
@@ -1,11 +1,6 @@
 import datetime
-import json
 import logging
 import os
-import requests
-import simplejson
-
-from django.utils.translation import ugettext as _
 
 from licenselib.license import Features, License
 from freenasUI.common.system import get_sw_name
@@ -78,60 +73,3 @@ def vm_enabled():
     ):
         return True
     return False
-
-
-def new_ticket(data):
-
-    sw_name = get_sw_name().lower()
-    try:
-        r = requests.post(
-            'https://%s/%s/api/v1.0/ticket' % (ADDRESS, sw_name),
-            data=json.dumps(data),
-            headers={'Content-Type': 'application/json'},
-            timeout=10,
-        )
-        data = r.json()
-    except simplejson.JSONDecodeError as e:
-        log.debug("Failed to decode ticket attachment response: %s", r.text)
-        return False, r.text, None
-    except requests.ConnectionError as e:
-        return False, _('Connection failed: %s') % e, None
-    except requests.Timeout as e:
-        return False, _('Connection timed out: %s') % e, None
-    if r.status_code != 200:
-        log.debug('Support Ticket failed (%d): %s', r.status_code, r.text)
-        return False, _('Ticket creation failed, try again later.'), None
-
-    return (not data['error'], data['message'], data.get('ticketnum'))
-
-
-def ticket_attach(data, file_handler):
-
-    sw_name = get_sw_name().lower()
-    try:
-        r = requests.post(
-            'https://%s/%s/api/v1.0/ticket/attachment' % (ADDRESS, sw_name),
-            data=data,
-            timeout=10,
-            files={'file': (file_handler.name, file_handler.file)},
-        )
-        data = r.json()
-    except simplejson.JSONDecodeError as e:
-        log.debug("Failed to decode ticket attachment response: %s", r.text)
-        return False, r.text
-    except requests.ConnectionError as e:
-        return False, _('Connection failed: %s') % e
-    except requests.Timeout as e:
-        return False, _('Connection timed out: %s') % e
-
-    return (not data['error'], data['message'])
-
-
-if __name__ == '__main__':
-    print(new_ticket({
-        'user': 'william',
-        'password': '',
-        'title': 'API Test',
-        'body': 'Testing proxy',
-        'version': '9.3-RELEASE'
-    }))
