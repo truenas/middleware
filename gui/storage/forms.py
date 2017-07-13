09ab6f8231afee61003800c1df21f402072575bc
William Grzybowski <wg@FreeBSD.org>
2017-07-13 12:31:32 -0300

    fix(gui): unify some of ZVol create/update code
    Ticket:	#23848

--- gui/storage/forms.py
+++ gui/storage/forms.py
@@ -1529,7 +1529,6 @@ class ZFSDatasetEditForm(ZFSDatasetCommonForm):
 
         return True
 
-
 class CommonZVol(Form):
     zvol_comments = forms.CharField(max_length=120, label=_('Comments'), required=False)
     zvol_volsize = forms.CharField(
