9d6a5ebce7c694d3a18fa745493135bef097f47a
William Grzybowski <wg@FreeBSD.org>
2017-08-03 12:58:03 -0300

    feat(middlewared): add account plugin with user.query

---
+++ src/middlewared/middlewared/plugins/account.py
@@ -0,0 +1,26 @@
+from middlewared.service import CRUDService, filterable, private
+
+import os
+
+
+class UserService(CRUDService):
+
+    @filterable
+    async def query(self, filters=None, options=None):
+        options = options or {}
+        options['extend'] = 'user.user_extend'
+        options['prefix'] = 'bsdusr_'
+        return await self.middleware.call('datastore.query', 'account.bsdusers', filters, options)
+
+    @private
+    async def user_extend(self, user):
+        # Get authorized keys
+        keysfile = f'{user["home"]}/.ssh/authorized_keys'
+        user['sshpubkey'] = None
+        if os.path.exists(keysfile):
+            try:
+                with open(keysfile, 'r') as f:
+                    user['sshpubkey'] = f.read()
+            except Exception:
+                pass
+        return user
