6745bcb0a8d6f9efcfc423de272a287cbd011e25
William Grzybowski <wg@FreeBSD.org>
2017-08-08 11:47:23 -0300

    feat(middlewared): first pass at user update

--- src/middlewared/middlewared/plugins/account.py
+++ src/middlewared/middlewared/plugins/account.py
@@ -1,5 +1,7 @@
-from middlewared.service import CRUDService, filterable, private
+from middlewared.service import CallError, CRUDService, filterable, private
+from middlewared.utils import run
 
+import errno
 import os
 
 
@@ -24,6 +26,44 @@ class UserService(CRUDService):
             except Exception:
                 pass
         return user
+
+    async def do_update(self, id, data):
+
+        user = await self.middleware.call('datastore.query', 'account.bsdusers', [('id', '=', id)])
+        if not user:
+            raise CallError(f'User {id} does not exist', errno.ENOENT)
+
+        if 'sshpubkey' in data:
+            keysfile = f'{user["home"]}/.ssh/authorized_keys'
+            pubkey = data.pop('sshpubkey')
+            if pubkey is None:
+                if os.path.exists(keysfile):
+                    try:
+                        os.unlink(keysfile)
+                    except OSError:
+                        pass
+            else:
+                oldpubkey = ''
+                try:
+                    with open(keysfile, 'r') as f:
+                        oldpubkey = f.read()
+                except Exception:
+                    pass
+                pubkey = pubkey.strip() + '\n'
+                if pubkey != oldpubkey:
+                    sshpath = f'{user["home"]}/.ssh'
+                    saved_umask = os.umask(0o77)
+                    if not os.path.isdir(sshpath):
+                        os.makedirs(sshpath)
+                    if not os.path.isdir(sshpath):
+                        raise CallError(f'{sshpath} is not a directory')
+                    if pubkey == '' and os.path.exists(keysfile):
+                        os.unlink(keysfile)
+                    else:
+                        with open(keysfile, 'w') as f:
+                            f.write(pubkey)
+                        await run('chown', '-R', f'{user["username"]}:{user["group"]["bsdgrp_group"]}', sshpath, check=False)
+                    os.umask(saved_umask)
 
 
 class GroupService(CRUDService):
