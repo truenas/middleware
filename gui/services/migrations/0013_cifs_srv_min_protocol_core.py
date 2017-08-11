8a2df5c12f8868608c1f6c00e900af0a47644be5
John Hixson <john@ixsystems.com>
2017-08-11 10:54:47 -0700

    0012 -> 0013 : Lame.
    Ticket: #25101

--- gui/services/migrations/0013_cifs_srv_min_protocol_core.py
+++ gui/services/migrations/0013_cifs_srv_min_protocol_core.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('services', '0011_auto_20170729_1515'),
+        ('services', '0012_auto_20170810_1542'),
     ]
 
     operations = [
