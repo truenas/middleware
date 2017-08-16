69f391b2c5088e73f27c12cd103ddaa1af93aebd
William Grzybowski <wg@FreeBSD.org>
2017-08-16 14:29:57 -0300

    feat(gui): handle ValidationErrors from middleware in freeadmin code path
    Ticket:	#25565

--- src/middlewared/middlewared/client/client.py
+++ src/middlewared/middlewared/client/client.py
@@ -1,6 +1,6 @@
 from . import ejson as json
 from .protocol import DDPProtocol
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from threading import Event as TEvent, Lock, Thread
 from ws4py.client.threadedclient import WebSocketClient
 from ws4py.websocket import WebSocket
@@ -204,17 +204,21 @@ class ClientException(Exception):
         return self.error
 
 
+Error = namedtuple('Error', ['attribute', 'errmsg', 'errcode'])
+
+
 class ValidationErrors(ClientException):
     def __init__(self, errors):
-        self.errors = errors
+        self.errors = []
+        for e in errors:
+            self.errors.append(Error(e[0], e[1], e[2]))
 
     def __str__(self):
         msgs = []
         for e in self.errors:
-            attribute = e[0] or 'ALL'
-            errmsg = e[1]
-            errcode = errno.errorcode.get(e[2], 'EUNKNOWN')
-            msgs.append(f'[{errcode}] {attribute}: {errmsg}')
+            attribute = e.attribute or 'ALL'
+            errcode = errno.errorcode.get(e.errcode, 'EUNKNOWN')
+            msgs.append(f'[{errcode}] {e.attribute or "ALL"}: {e.errmsg}')
         return '\n'.join(msgs)
 
 
