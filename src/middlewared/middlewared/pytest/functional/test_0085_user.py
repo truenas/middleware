bbf89685952053f61b16fccd3d8d2e1d45cc7965
William Grzybowski <wg@FreeBSD.org>
2017-08-17 16:10:57 -0300

    feat(middlewared): CRUD tests for user
    Ticket:	#25565

--- src/middlewared/middlewared/pytest/functional/test_0085_user.py
+++ src/middlewared/middlewared/pytest/functional/test_0085_user.py
@@ -1,3 +1,11 @@
+import pytest
+
+
+@pytest.fixture(scope='module')
+def data():
+    return {}
+
+
 def test_user_query(conn):
     req = conn.rest.get('user')
 
@@ -5,12 +13,37 @@ def test_user_query(conn):
     assert isinstance(req.json(), list) is True
 
 
-def test_user_0100_create(conn):
+def test_user_0100_create(conn, data):
     req = conn.rest.post('user', data=[{
         'username': 'test555',
         'full_name': 'Test User',
         'password': '12345',
         'group': 1,
     }])
+
+    assert req.status_code == 200
+    assert isinstance(req.json(), int) is True
+    data['id'] = req.json()
+
+
+def test_user_0500_update(conn, data):
+
+    if 'id' not in data:
+        pytest.skip('No user id found')
+
+    req = conn.rest.put(f'user/id/{data["id"]}', data=[{
+        'full_name': 'Test User Update',
+    }])
+
+    assert req.status_code == 200
+    assert isinstance(req.json(), int) is True
+
+
+def test_user_0900_delete(conn, data):
+
+    if 'id' not in data:
+        pytest.skip('No user id found')
+
+    req = conn.rest.delete(f'user/id/{data["id"]}')
 
     assert req.status_code == 200
