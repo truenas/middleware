3abb7bc7ef50942a4838814728d56f05dcd06f9c
650elx <bprokop02@gmail.com>
2017-08-29 02:29:05 +0200

    More abstract Peer CRUDService. One to rule them all

--- src/middlewared/middlewared/plugins/peer.py
+++ src/middlewared/middlewared/plugins/peer.py
@@ -2,6 +2,50 @@ from middlewared.schema import accepts, Dict, Int, Ref, Str
 from middlewared.service import CRUDService, filterable
 
 
+
+class PeerService(CRUDService):
+
+    class Config:
+        namespace = "peer"
+
+    @filterable
+    async def query(self, filters=None, options=None):
+        return await self.middleware.call('datastore.query', 'peers.peer', filters, options)
+
+    @accepts(Dict(
+        'peer',
+        Str('peer_type', enum=['s3', 'ssh']),
+        additional_attrs=True,
+        register=True,
+    ))
+    async def do_create(self, peer):
+        peer_type = peer.get('peer_type')
+        return await self.middleware.call(
+            f'peer.{peer_type}.do_create',
+            peer,
+        )
+
+    @accepts(Int('id'), Ref('peer'))
+    async def do_update(self, id, peer):
+        peer_type = peer.get('peer_type')
+        if not peer_type:
+            peer_data = await self.query([('id', '=', id)], {'get': True})
+            peer_type = peer_data['peer_type']
+        return await self.middleware.call(
+            f'peer.{peer_type}.do_update',
+            id,
+            peer,
+        )
+
+    @accepts(Int('id'))
+    async def do_delete(self, id):
+        return await self.middleware.call(
+            'datastore.delete',
+            'peers.peer',
+            id,
+        )
+
+
 class SSHPeerService(CRUDService):
 
     class Config:
@@ -15,6 +59,7 @@ class SSHPeerService(CRUDService):
         'peer-ssh',
         Str('name'),
         Str('description'),
+        Str('peer_type'),
         Int('ssh_port'),
         Str('ssh_remote_hostname'),
         Str('ssh_remote_user'),
@@ -51,6 +96,7 @@ class S3PeerService(CRUDService):
         'peer-s3',
         Str('name'),
         Str('description'),
+        Str('peer_type'),
         Str('s3_access_key'),
         Str('s3_secret_key'),
         register=True,
