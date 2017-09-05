e61e83dfce1355d60d6404f774e3a7f1c0491324
William Grzybowski <wg@FreeBSD.org>
2017-09-05 09:00:05 -0300

    feat(middlewared/sysds): skeleton of system dataset setup
    Ticket:	#25764

--- src/middlewared/middlewared/plugins/sysdataset.py
+++ src/middlewared/middlewared/plugins/sysdataset.py
@@ -1,5 +1,9 @@
-from middlewared.schema import accepts
-from middlewared.service import ConfigService
+from middlewared.schema import accepts, Bool
+from middlewared.service import ConfigService, private
+
+import os
+
+SYSDATASET_PATH = '/var/db/system'
 
 
 class SystemDatasetService(ConfigService):
@@ -7,3 +11,78 @@ class SystemDatasetService(ConfigService):
     @accepts()
     async def config(self):
         return await self.middleware.call('datastore.config', 'system.systemdataset', {'prefix': 'sys_'})
+
+    @accepts(Bool('mount'))
+    @private
+    async def setup(self, mount=True):
+        if not await self.middleware.call('system.is_freenas'):
+            if await self.middleware.call('notifier.failover_status') == 'BACKUP':
+                try:
+                    os.unlink(SYSDATASET_PATH)
+                except OSError:
+                    pass
+                return
+
+        config = await self.config()
+        if not config['basename']:
+            if os.path.exists(SYSDATASET_PATH):
+                try:
+                    os.rmdir(SYSDATASET_PATH)
+                except Exception:
+                    self.logger.debug('Failed to remove system dataset dir', exc_info=True)
+            return config
+
+        if not config['is_decrypted']:
+            return
+
+        datasets = [config['basename']]
+        for sub in (
+            'cores', 'samba4', f'syslog-{config["uuid"]}',
+            f'rrd-{config["uuid"]}', f'configs-{config["uuid"]}',
+        ):
+            datasets.append(f'{config["basename"]}/{sub}')
+
+        createdds = False
+        for dataset in datasets:
+            # TODO: use zfs plugin
+            proc = await Popen([
+                'zfs', 'get', '-H', '-o', 'value', 'mountpoint', dataset,
+            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = await proc.communicate()
+            if proc.returncode == 0:
+                if stdout.decode().strip() != 'legacy':
+                    await run('zfs', 'set', 'mountpoint=legacy', dataset, check=False)
+                continue
+
+            #self.create_zfs_dataset(dataset, {"mountpoint": "legacy"})
+            createdds = True
+
+        if createdds:
+            self.middleware.call('service.restart', 'collectd')
+
+        if not os.path.isdir(SYSDATASET_PATH):
+            if os.path.exists(SYSDATASET_PATH):
+                os.unlink(SYSDATASET_PATH)
+            os.mkdir(SYSDATASET_PATH)
+
+        # TODO: use zfs plugin
+        proc = await Popen([
+            'zfs', 'get', '-H', '-o', 'value', 'aclmode', dataset,
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = await proc.communicate()
+        aclmode = stdout.decode().strip()
+        if aclmode and aclmode.lower() == 'restricted':
+            await run('zfs', 'set', 'aclmode=passthrough', basename, check=False)
+
+        if mount:
+            #self.system_dataset_mount(systemdataset.sys_pool, SYSDATASET_PATH)
+
+            corepath = f'{SYSDATASET_PATH}/cores'
+            if os.path.exists(corepath):
+                # FIXME: sysctl module not working
+                run('sysctl', f"kern.corefile='{corepath}/%N.core'")
+                os.chmod(corepath, 0o775)
+
+            #self.nfsv4link()
+
+        return systemdataset
