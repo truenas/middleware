f3672dcec010f68c121b66b106bc79005859932f
William Grzybowski <wg@FreeBSD.org>
2017-09-28 16:10:09 -0300

    feat(middlewared/mail): implement mail.update
    Ticket:	#26004

--- src/middlewared/middlewared/plugins/mail.py
+++ src/middlewared/middlewared/plugins/mail.py
@@ -1,5 +1,5 @@
 from middlewared.schema import Bool, Dict, Int, List, Str, accepts
-from middlewared.service import ConfigService
+from middlewared.service import ConfigService, ValidationErrors
 
 import os
 import sys
@@ -20,6 +20,35 @@ class MailService(ConfigService):
 
     class Config:
         datastore = 'system.email'
+        datastore_prefix = 'em_'
+
+    @accepts(Dict(
+        'mail',
+        Str('fromemail'),
+        Str('outgoingserver'),
+        Int('port'),
+        Str('security', enum=['PLAIN', 'SSL', 'TLS']),
+        Bool('smtp'),
+        Str('user'),
+        Str('pass'),
+    ))
+    async def do_update(self, data):
+        config = await self.config()
+
+        new = config.copy()
+        new.update(data)
+        new['security'] = new['security'].lower()  # Django Model compatibility
+
+        verrors = ValidationErrors()
+
+        if new['smtp'] and new['user'] == '':
+            verrors.add('user', 'This field is required when SMTP authentication is enabled')
+
+        if verrors:
+            raise verrors
+
+        await self.middleware.call('datastore.update', 'system.email', config['id'], new)
+        return config['id']
 
     @accepts(Dict(
         'mail-message',
