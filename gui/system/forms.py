59527fc3f70a9b9a6299d7182d035d97f9ad4e32
Xin Tan <mactanxin@gmail.com>
2017-10-02 17:04:54 -0700

    UPDATED: form with clean data and correct fields

--- gui/system/forms.py
+++ gui/system/forms.py
@@ -1179,13 +1179,17 @@ class AdvancedForm(ModelForm):
             events.append("refreshTree()")
 
     def clean(self):
-        syslogserver = self.adv_syslogserver
-        match = re.match("^[\w\.\-]+(\:\d+)?$", syslogserver)
-        if match is None:
-            self._errors['adv_syslogserver'] = self.error_class([_(
-                "Invalid syslog server format")
-            ])
-        return syslogserver
+        cdata = self.cleaned_data
+
+        # todo: make this and ix-syslogd support udp6
+        if cdata["adv_syslogserver"]:
+            syslogserver = cdata.get("adv_syslogserver")
+            match = re.match("^[\w\.\-]+(\:\d+)?$", syslogserver)
+            if match is None:
+                self._errors['adv_syslogserver'] = self.error_class([_(
+                    "Invalid syslog server format")
+                ])
+        return cdata
 
 class EmailForm(ModelForm):
     em_pass1 = forms.CharField(
