9aadd25f0e1438d3600ae977ca4ff2ad211e564e
themylogin <themylogin@gmail.com>
2017-10-11 23:18:57 +0300

    feat(gui): Properly display middleware validation errors for list items

--- gui/middleware/form.py
+++ gui/middleware/form.py
@@ -4,6 +4,7 @@ from freenasUI.middleware.client import client
 def handle_middleware_validation(form, excep):
     for err in excep.errors:
         field_name = form.middleware_attr_map.get(err.attribute)
+        error_message = err.errmsg
         if not field_name:
             field_name = err.attribute
             if form.middleware_attr_schema:
@@ -11,13 +12,20 @@ def handle_middleware_validation(form, excep):
                     field_name = field_name[len(form.middleware_attr_schema) + 1:]
             if form.middleware_attr_prefix:
                 field_name = f'{form.middleware_attr_prefix}{field_name}'
-        if field_name in form.fields:
-            form._errors[field_name] = form.error_class([err.errmsg])
+            if (field_name not in form.fields and
+                    len(field_name.split('.')) >= 3 and field_name.split('.')[-2].isdigit()):
+                list_field_name = '.'.join(field_name.split('.')[:-2])
+                if list_field_name in form.fields:
+                    list_index = int(field_name.split('.')[-2])
+                    field_name = list_field_name
+                    error_message = repr(form.cleaned_data[field_name][list_index]) + f": {error_message}"
+        if field_name not in form.fields:
+            field_name = '__all__'
+
+        if field_name not in form._errors:
+            form._errors[field_name] = form.error_class([error_message])
         else:
-            if '__all__' not in form._errors:
-                form._errors['__all__'] = form.error_class([err.errmsg])
-            else:
-                form._errors['__all__'] += [err.errmsg]
+            form._errors[field_name] += [error_message]
 
 
 class MiddlewareModelForm:
