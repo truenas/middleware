abb0ea7be8e09d4a66aac5413902a8a3064e96e0
Timur Bakeyev <timur@gnu.org>
2017-11-01 18:32:31 +0100

    Add REST API endpoint for KerberosPrincipal (#373)
    Ticket: #26330

--- gui/api/resources.py
+++ gui/api/resources.py
@@ -2939,6 +2939,13 @@ class KerberosKeytabResourceMixin(object):
         return bundle
 
 
+class KerberosPrincipalResourceMixin(object):
+
+    def dehydrate(self, bundle):
+        bundle = super(KerberosPrincipalResourceMixin, self).dehydrate(bundle)
+        return bundle
+
+
 class KerberosSettingsResourceMixin(object):
 
     def dehydrate(self, bundle):
