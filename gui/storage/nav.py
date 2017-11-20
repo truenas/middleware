a98c10f6a23c609ca0126782d660c800ac792a84
themylogin <themylogin@gmail.com>
2017-11-20 16:55:12 +0300

    Fix PEP8

--- gui/storage/nav.py
+++ gui/storage/nav.py
@@ -1,6 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
 from freenasUI.freeadmin.tree import TreeNode
-from freenasUI.middleware.notifier import notifier
 from . import models
 
 NAME = _('Storage')
