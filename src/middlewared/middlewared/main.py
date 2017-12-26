8a33725ca3141aae4b0e9bb9335b4b4edffcd94f
650elx <bprokop02@gmail.com>
2017-12-26 02:17:21 +0100

    Revert "Added: simple hack to allow periodic tasks to be jobs"
    This reverts commit 3be049e5b6a2130b23223efe582f38df1cb8b671.

--- src/middlewared/middlewared/main.py
+++ src/middlewared/middlewared/main.py
@@ -745,14 +745,7 @@ class Middleware(object):
         self.logger.trace("Calling periodic task %s::%s", service_name, task_name)
 
         try:
-            args = []
-            if hasattr(method, '_pass_app'):
-                args.append(app)
-
-            if getattr(method, '_job', None):
-                job = Job(self, task_name, method, args, method._job)
-                self.jobs.add(job)
-            elif asyncio.iscoroutinefunction(method):
+            if asyncio.iscoroutinefunction(method):
                 await method()
             else:
                 await self.run_in_thread(method)
