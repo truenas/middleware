d52ccc86cec63d709f9b86ae0d59916bf9a3e735
William Grzybowski <wg@FreeBSD.org>
2018-01-10 09:46:07 -0200

    fix(gui): race condition on django using middleware client
    (cherry picked from commit f2376226da7997043b6c6b41d26108907b55a6c1)

--- gui/middleware/client.py
+++ gui/middleware/client.py
@@ -1,14 +1,26 @@
 from middlewared.client import CallTimeout, Client, ClientException, ValidationErrors  # noqa
+import threading
 
 
 class Connection(object):
 
+    def __init__(self):
+        """
+        Original intent of that class was to use a single connection to
+        middleware for all django threads, however turned out a bit difficult
+        regarding management of that connection (e.g. reconnect) and due to priorities
+        that feature has been delayed.
+        As a stop-gap solution to keep the same API we are using local thread data to keep
+        track of the client object.
+        """
+        self.local = threading.local()
+
     def __enter__(self):
-        self.client = Client()
-        return self.client
+        self.local.client = Client()
+        return self.local.client
 
     def __exit__(self, typ, value, traceback):
-        self.client.close()
+        self.local.client.close()
         if typ is not None:
             raise
 
