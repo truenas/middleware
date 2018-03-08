ad72cc922e14a71388103eba753b1994323dff3a
ericbsd <ericturgeon.bsd@gmail.com>
2018-03-08 17:19:51 -0400

    Adding POST_TIMEOUT and PUT_TIMEOUT for timeout

--- tests/api1/create/jails.py
+++ tests/api1/create/jails.py
@@ -10,7 +10,7 @@ import os
 import xmlrunner
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import PUT, POST
+from functions import POST, PUT_TIMEOUT, POST_TIMEOUT
 from auto_config import results_xml
 
 try:
@@ -25,10 +25,9 @@ TestName = "create jails"
 class create_jails_test(unittest.TestCase):
 
     def test_01_Configuring_jails(self):
-        payload = {"jc_ipv4_network_start": JAILIP}
-        # ,
-        # "jc_path": "/mnt/tank/jails"}
-        assert PUT("/jails/configuration/", payload) == 200
+        payload = {"jc_ipv4_network_start": JAILIP,
+                   "jc_path": "/mnt/tank/jails"}
+        assert PUT_TIMEOUT("/jails/configuration/", payload, 60) == 200
 
     def test_02_Creating_jail_VNET_OFF(self):
         payload = {"jail_host": "testjail",
@@ -36,7 +35,7 @@ class create_jails_test(unittest.TestCase):
                    "jail_ipv4": JAILIP,
                    "jail_ipv4_netmask": JAILNETMASK,
                    "jail_vnet": True}
-        assert POST("/jails/jails/", payload) == 201
+        assert POST_TIMEOUT("/jails/jails/", payload, 1200) == 201
 
     def test_03_Mount_tank_share_into_jail(self):
         payload = {"destination": "/mnt",
@@ -47,13 +46,13 @@ class create_jails_test(unittest.TestCase):
         assert POST("/jails/mountpoints/", payload) == 201
 
     def test_04_Starting_jail(self):
-        assert POST("/jails/jails/1/start/", "") == 202
+        assert POST("/jails/jails/1/start/", None) == 202
 
     def test_05_Restarting_jail(self):
-        assert POST("/jails/jails/1/restart/", "") == 202
+        assert POST("/jails/jails/1/restart/", None) == 202
 
     def test_06_Stopping_jail(self):
-        assert POST("/jails/jails/1/stop/", "") == 202
+        assert POST("/jails/jails/1/stop/", None) == 202
 
 
 def run_test():
