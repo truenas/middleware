97e8456e8a63099d424827a7b813a2e4567b472d
William Grzybowski <wg@FreeBSD.org>
2018-03-19 09:46:58 -0300

    fix(ha): only force master if not MASTER yet
    Ticket:	#29781

--- gui/storage/forms.py
+++ gui/storage/forms.py
@@ -2870,7 +2870,8 @@ class UnlockPassphraseForm(Form):
                     c.call('failover.call_remote', 'failover.encryption_setkey', [passphrase])
             except:
                 log.warn('Failed to set key on standby node, is it down?', exc_info=True)
-            _notifier.failover_force_master()
+            if _notifier.failover_status() != 'MASTER':
+                _notifier.failover_force_master()
 
 
 class KeyForm(Form):
