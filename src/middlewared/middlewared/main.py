07caa8a1c0d75773c8862f9261e869aa1d3ff089
William Grzybowski <wg@FreeBSD.org>
2018-04-06 16:50:23 -0300

    fix(middlewared): do not spawn another thread on `call_sync`
    Ticket:	#31050

--- src/middlewared/middlewared/main.py
+++ src/middlewared/middlewared/main.py
@@ -887,7 +887,7 @@ class Middleware(object):
     def pipe(self):
         return Pipe(self)
 
-    async def _call(self, name, serviceobj, methodobj, params=None, app=None, pipes=None):
+    async def _call(self, name, serviceobj, methodobj, params=None, app=None, pipes=None, spawn_thread=True):
 
         args = []
         if hasattr(methodobj, '_pass_app'):
@@ -921,6 +921,11 @@ class Middleware(object):
 
             if asyncio.iscoroutinefunction(methodobj):
                 return await methodobj(*args)
+            elif not spawn_thread:
+                # If this method is already being called from a thread we dont need to spawn
+                # another one or we may run out of threads and deadlock.
+                # e.g. multiple concurrent calls to a threaded method which uses call_sync
+                return methodobj(*args)
             else:
                 tpool = None
                 if serviceobj._config.thread_pool:
@@ -990,7 +995,7 @@ class Middleware(object):
             raise RuntimeError('You cannot call_sync from main thread')
 
         serviceobj, methodobj = self._method_lookup(name)
-        fut = asyncio.run_coroutine_threadsafe(self._call(name, serviceobj, methodobj, params), self.__loop)
+        fut = asyncio.run_coroutine_threadsafe(self._call(name, serviceobj, methodobj, params, spawn_thread=False), self.__loop)
         event = threading.Event()
 
         def done(_):
