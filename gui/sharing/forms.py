f6fc98b659feefbf699dbdd28c96172c527c1700
Timur I. Bakeyev <timur@iXsystems.com>
2018-04-13 19:29:26 +0200

    * Remove `cifs_default_permissions` field from the `sharing_cifs_share`, making corresponding GUI element a trigger that if set launches `winacl`
    Ticket: #27881

--- gui/sharing/forms.py
+++ gui/sharing/forms.py
@@ -34,6 +34,7 @@ from django.utils.translation import ugettext_lazy as _
 from dojango import forms
 from freenasUI.common.forms import ModelForm
 from freenasUI.freeadmin.forms import SelectMultipleWidget
+from freenasUI.freeadmin.utils import key_order
 from freenasUI.middleware.exceptions import MiddlewareError
 from freenasUI.middleware.notifier import notifier
 from freenasUI.middleware.form import MiddlewareModelForm
@@ -50,6 +51,12 @@ log = logging.getLogger('sharing.forms')
 
 
 class CIFS_ShareForm(ModelForm):
+
+    cifs_default_permissions = forms.BooleanField(
+        label=_('Apply Default Permissions'),
+        help_text=_('Recursively set appropriate default Windows permissions on share'),
+        required=False
+    )
 
     def _get_storage_tasks(self, cifs_path=None, cifs_home=False):
         p = pipeopen("zfs list -H -o mountpoint,name")
@@ -86,6 +93,12 @@ class CIFS_ShareForm(ModelForm):
         else:
             self._original_cifs_vfsobjects = []
 
+        key_order(self, 4, 'cifs_default_permissions', instance=True)
+
+        self.fields['cifs_default_permissions'].widget.attrs['onChange'] = (
+            'javascript:toggleGeneric("id_cifs_guestok", '
+            '["id_cifs_guestonly"], true);')
+
         self.fields['cifs_guestok'].widget.attrs['onChange'] = (
             'javascript:toggleGeneric("id_cifs_guestok", '
             '["id_cifs_guestonly"], true);')
@@ -95,8 +108,6 @@ class CIFS_ShareForm(ModelForm):
                     'disabled'
         elif self.instance.cifs_guestok is False:
             self.fields['cifs_guestonly'].widget.attrs['disabled'] = 'disabled'
-        self.instance._original_cifs_default_permissions = \
-            self.instance.cifs_default_permissions
         self.fields['cifs_name'].required = False
         self.fields['cifs_home'].widget.attrs['onChange'] = (
             "cifs_storage_task_toggle();"
@@ -183,10 +194,8 @@ class CIFS_ShareForm(ModelForm):
             task_list = []
             if path:
                 task_list = self._get_storage_tasks(cifs_path=path)
-
             elif home:
                 task_list = self._get_storage_tasks(cifs_home=home)
-
             if task_list:
                 obj.cifs_storage_task = task_list[0]
 
@@ -198,9 +207,7 @@ class CIFS_ShareForm(ModelForm):
         if not services.objects.get(srv_service='cifs').srv_enable:
             events.append('ask_service("cifs")')
         super(CIFS_ShareForm, self).done(request, events)
-        if self.instance._original_cifs_default_permissions != \
-            self.instance.cifs_default_permissions and \
-            self.instance.cifs_default_permissions is True:
+        if self.cleaned_data.get('cifs_default_permissions') is True:
             try:
                 (owner, group) = notifier().mp_get_owner(self.instance.cifs_path)
             except:
