5cfe6c70f3bcd23aff70f0267de4f6dae09b70d2
ericbsd <ericturgeon.bsd@gmail.com>
2018-05-22 13:32:14 +0000

    Adding GET code to domaincontroler

--- tests/api2/domaincontroller.py
+++ tests/api2/domaincontroller.py
@@ -10,7 +10,7 @@ import os
 
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import PUT
+from functions import PUT, GET
 from config import *
 
 REALM = "samdom.local"
@@ -23,32 +23,58 @@ adpsswd_test_cfg = pytest.mark.skipif(all(["ADPASSWORD" in locals()
                                            ]) is False, reason=Reason)
 
 
-def test_01_Setting_Realm_Name():
+def test_01_setting_realm_name():
     payload = {"realm": REALM}
     results = PUT("/domaincontroller", payload)
     assert results.status_code == 200, results.text
 
 
-def test_02_Setting_Domain_name():
+def test_02_look_realm_name():
+    results = GET("/domaincontroller")
+    assert results.json()["realm"] == REALM, results.text
+
+
+def test_03_setting_domain_name():
     payload = {"domain": DOMAIN}
     results = PUT("/domaincontroller", payload)
     assert results.status_code == 200, results.text
 
 
-def test_03_Setting_DNS_forwarder():
+def test_04_look_domain_name():
+    results = GET("/domaincontroller")
+    assert results.json()["domain"] == DOMAIN, results.text
+
+
+def test_05_setting_dns_forwarder():
     payload = {"dns_forwarder": DNSFORWARDER}
     results = PUT("/domaincontroller", payload)
     assert results.status_code == 200, results.text
 
 
+def test_06_look_dns_forwarder():
+    results = GET("/domaincontroller")
+    assert results.json()["dns_forwarder"] == DNSFORWARDER, results.text
+
+
 @adpsswd_test_cfg
-def test_04_Setting_the_Admin_Password():
+def test_07_setting_the_admin_password():
     payload = {"passwd": ADPASSWORD}
     results = PUT("/domaincontroller", payload)
     assert results.status_code == 200, results.text
 
 
-def test_05_Setting_the_Forest_level():
+@adpsswd_test_cfg
+def test_08_look_the_admin_password():
+    results = GET("/domaincontroller")
+    assert results.json()["passwd"] == ADPASSWORD, results.text
+
+
+def test_09_setting_the_forest_level():
     payload = {"forest_level": FORESTLEVEL}
     results = PUT("/domaincontroller", payload)
     assert results.status_code == 200, results.text
+
+
+def test_10_look_the_forest_level():
+    results = GET("/domaincontroller")
+    assert results.json()["forest_level"] == FORESTLEVEL, results.text
