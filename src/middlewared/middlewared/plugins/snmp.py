417d8b1fd4a3af4daf72c675c5383c286fccfe47
Brandon Schneider <brandon@ixsystems.com>
2018-05-23 07:05:01 -0500

    Write API functional tests for snmp (#1259)
    - Add defaults to keys that need them
    Ticket: #33837

--- src/middlewared/middlewared/plugins/snmp.py
+++ src/middlewared/middlewared/plugins/snmp.py
@@ -6,8 +6,8 @@ from middlewared.service import SystemServiceService, ValidationErrors
 class SNMPService(SystemServiceService):
 
     class Config:
-        service = "snmp"
-        datastore_prefix = "snmp_"
+        service = 'snmp'
+        datastore_prefix = 'snmp_'
 
     @accepts(Dict(
         'snmp_update',
@@ -15,12 +15,13 @@ class SNMPService(SystemServiceService):
         Str('contact', validators=[Or(Email(), Match(r'^[-_a-zA-Z0-9\s]*$'))]),
         Bool('traps'),
         Bool('v3', default=False),
-        Str('community', validators=[Match(r'^[-_.a-zA-Z0-9\s]*$')]),
+        Str('community', validators=[Match(r'^[-_.a-zA-Z0-9\s]*$')],
+            default='public'),
         Str('v3_username'),
-        Str('v3_authtype', enum=['', 'MD5', 'SHA']),
+        Str('v3_authtype', enum=['', 'MD5', 'SHA'], default='SHA'),
         Str('v3_password'),
-        Str('v3_privproto', enum=[None, 'AES', 'DES']),
-        Str('v3_privpassphrase'),
+        Str('v3_privproto', enum=[None, 'AES', 'DES'], default=None),
+        Str('v3_privpassphrase', default=None),
         Int('loglevel', validators=[Range(min=0, max=7)]),
         Str('options'),
     ))
@@ -32,17 +33,17 @@ class SNMPService(SystemServiceService):
 
         verrors = ValidationErrors()
 
-        if not data["v3"] and not data["community"]:
-            verrors.add("snmp_update.community", "This field is required when SNMPv3 is disabled")
+        if not data['v3'] and not data['community']:
+            verrors.add('snmp_update.community', 'This field is required when SNMPv3 is disabled')
 
-        if data["v3_authtype"] and not data["v3_password"]:
-            verrors.add("snmp_update.v3_password", "This field is requires when SNMPv3 auth type is specified")
+        if data['v3_authtype'] and not data['v3_password']:
+            verrors.add('snmp_update.v3_password', 'This field is requires when SNMPv3 auth type is specified')
 
-        if data["v3_password"] and len(data["v3_password"]) < 8:
-            verrors.add("snmp_update.v3_password", "Password must contain at least 8 characters")
+        if data['v3_password'] and len(data['v3_password']) < 8:
+            verrors.add('snmp_update.v3_password', 'Password must contain at least 8 characters')
 
-        if data["v3_privproto"] and not data["v3_privpassphrase"]:
-            verrors.add("snmp_update.v3_privpassphrase", "This field is requires when SNMPv3 private protocol is specified")
+        if data['v3_privproto'] and not data['v3_privpassphrase']:
+            verrors.add('snmp_update.v3_privpassphrase', 'This field is requires when SNMPv3 private protocol is specified')
 
         if verrors:
             raise verrors
