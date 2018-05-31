b575e94211fdc97e6a6d65af3119196ec6465d55
ericbsd <ericturgeon.bsd@gmail.com>
2018-05-31 16:18:29 -0300

    Adding some update code and delete code for afp sharing

--- tests/api2/afp.py
+++ tests/api2/afp.py
@@ -83,6 +83,7 @@ def test_08_creating_a_afp_share_on_afp_path():
     results = POST("/sharing/afp", payload)
     assert results.status_code == 200, results.text
 
+
 # have to wait for the volume api
 # Mount share on OSX system and create a test file
 @mount_test_cfg
@@ -143,6 +144,25 @@ def test_15_unmount_afp_share():
     cmd = f"umount -f '{MOUNTPOINT}'"
     results = SSH_TEST(cmd, OSX_USERNAME, OSX_PASSWORD, OSX_HOST)
     assert results['result'] is True, results['output']
+
+
+# Update test
+def test_16_updating_the_apf_service():
+    payload = {"connections_limit": 10}
+    results = PUT("/afp", payload)
+    assert results.status_code == 200, results.text
+
+
+def test_18_update_afp_share():
+    afpid = GET(f'/sharing/afp?name={AFP_NAME}').json()[0]['id']
+    payload = {"home": True, "comment": "AFP Test"}
+    results = PUT(f"/sharing/afp/id/{afpid}", payload)
+    assert results.status_code == 200, results.text
+
+
+def test_19_checking_to_see_if_afp_service_is_enabled():
+    results = GET("/service?service=afp")
+    assert results.json()[0]["state"] == "RUNNING", results.text
 
 
 # Update tests
@@ -207,6 +227,12 @@ def test_22_removing_SMB_mountpoint():
     assert results['result'] is True, results['output']
 
 
+def test_28_delete_afp_share():
+    afpid = GET(f'/sharing/afp?name={AFP_NAME}').json()[0]['id']
+    results = DELETE(f"/sharing/afp/id/{afpid}")
+    assert results.status_code == 200, results.text
+
+
 def test_25_stopping_afp_service():
     payload = {"service": "afp", "service-control": {"onetime": True}}
     results = POST("/service/stop", payload)
@@ -227,6 +253,7 @@ def test_23_disable_afp_service_at_boot():
 def test_24_checking_afp_disable_at_boot():
     results = GET("/service?service=afp")
     assert results.json()[0]['enable'] == False, results.text
+
 
 def test_29_destroying_afp_dataset():
     results = DELETE(f"/pool/dataset/id/{urlDataset}/")
