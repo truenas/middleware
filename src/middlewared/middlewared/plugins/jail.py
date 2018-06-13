08fbdd2974bca3c4c2ca38369f2b8e93f832a9be
Brandon Schneider <brandon@ixsystems.com>
2018-06-13 14:41:37 -0500

    fix(plugins/jail): Fix default jail filtering (#1360)
    Ticket: #34846

--- src/middlewared/middlewared/plugins/jail.py
+++ src/middlewared/middlewared/plugins/jail.py
@@ -36,33 +36,39 @@ class JailService(CRUDService):
         jails = []
 
         if filters and len(filters) == 1 and list(
-                filters[0][:2]) == ['jail', '=']:
-            jail_identifier = filters[0].pop(2)
+                filters[0][:2]) == ['host_hostuuid', '=']:
+            jail_identifier = filters[0][2]
 
-        recursive = False if jail_identifier is not None else True
+        recursive = False if jail_identifier == 'default' else True
 
         try:
             jail_dicts = ioc.IOCage(
                 jail=jail_identifier).get('all', recursive=recursive)
-            for jail in jail_dicts:
-                jail = list(jail.values())[0]
-                if jail['dhcp'] == 'on':
-                    uuid = jail['host_hostuuid']
 
-                    if jail['state'] == 'up':
-                        interface = jail['interfaces'].split(',')[0].split(
-                            ':')[0]
-                        if interface == 'vnet0':
-                            # Inside jails they are epair0b
-                            interface = 'epair0b'
-                        ip4_cmd = ['jexec', f'ioc-{uuid}', 'ifconfig',
-                                   interface, 'inet']
-                        out = su.check_output(ip4_cmd)
-                        jail['ip4_addr'] = f'{interface}|' \
-                            f'{out.splitlines()[2].split()[1].decode()}'
-                    else:
-                        jail['ip4_address'] = 'DHCP (not running)'
-                jails.append(jail)
+            if jail_identifier == 'default':
+                jail_dicts['host_hostuuid'] = 'default'
+                jails.append(jail_dicts)
+
+            else:
+                for jail in jail_dicts:
+                    jail = list(jail.values())[0]
+                    if jail['dhcp'] == 'on':
+                        uuid = jail['host_hostuuid']
+
+                        if jail['state'] == 'up':
+                            interface = jail['interfaces'].split(',')[0].split(
+                                ':')[0]
+                            if interface == 'vnet0':
+                                # Inside jails they are epair0b
+                                interface = 'epair0b'
+                            ip4_cmd = ['jexec', f'ioc-{uuid}', 'ifconfig',
+                                       interface, 'inet']
+                            out = su.check_output(ip4_cmd)
+                            jail['ip4_addr'] = f'{interface}|' \
+                                f'{out.splitlines()[2].split()[1].decode()}'
+                        else:
+                            jail['ip4_address'] = 'DHCP (not running)'
+                    jails.append(jail)
         except BaseException:
             # Brandon is working on fixing this generic except, till then I
             # am not going to make the perfect the enemy of the good enough!
