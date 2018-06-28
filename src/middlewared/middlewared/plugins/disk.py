325fbda2f9eb38f6bef42b576daca6459e1b5b55
William Grzybowski <wg@FreeBSD.org>
2018-06-28 16:56:24 -0300

    feat(middlewared/disk): add private methods to format disk and get gptid
    Ticket:	#35749

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -1061,6 +1061,61 @@ class DiskService(CRUDService):
 
         await self.sync(dev)
 
+    @private
+    def format(self, disk, swapgb):
+
+        job = self.middleware.call_sync('disk.wipe', disk, 'QUICK')
+        job.wait_sync()
+        if job.error:
+            raise CallError(f'Failed to wipe disk {disk}: {job.error}')
+
+        # Calculate swap size.
+        swapsize = swapgb * 1024 * 1024 * 2
+        # Round up to nearest whole integral multiple of 128 and subtract by 34
+        # so next partition starts at mutiple of 128.
+        swapsize = (int((swapsize + 127) / 128)) * 128
+
+        geom.scan()
+        g = geom.geom_by_name('DISK', disk)
+        if g and g.provider.mediasize:
+            size = g.provider.mediasize
+            # The GPT header takes about 34KB + alignment, round it to 100
+            if size - 100 <= swapgb * 1024 * 1024:
+                raise CallError(f'Your disk size must be higher than {swapgb}GB')
+        else:
+            self.logger.error(f'Unable to determine size of {disk}')
+
+        commands = []
+        commands.append(('gpart', 'create', '-s', 'gpt', f'/dev/{disk}'))
+        if swapsize > 0:
+            commands.append(('gpart', 'add', '-a', '4k', '-b', '128', '-t', 'freebsd-swap', '-s', str(swapsize), disk))
+            commands.append(('gpart', 'add', '-a', '4k', '-t', 'freebsd-zfs', disk))
+        else:
+            commands.append(('gpart', 'add', '-a', '4k', '-b', '128', '-t', 'freebsd-zfs', disk))
+
+        # Install a dummy boot block so system gives meaningful message if booting
+        # from the wrong disk.
+        commands.append(('gpart', 'bootcode', '-b', '/boot/pmbr-datadisk', f'/dev/{disk}'))
+
+        for command in commands:
+            cp = subprocess.run(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
+            )
+            if cp.returncode != 0:
+                raise CallError(f'Unable to GPT format the disk "{disk}": {cp.stderr}')
+
+        # We might need to sync with reality (e.g. devname -> uuid)
+        self.middleware.call_sync('disk.sync', disk)
+
+    @private
+    def gptid_from_part_type(self, disk, part_type):
+        geom.scan()
+        g = geom.class_by_name('PART')
+        uuid = g.xml.find(f'.//geom[name="{disk}"]//config/[type="{part_type}"]/rawuuid')
+        if uuid is None:
+            raise ValueError(f'Partition type {part_type} not found on {disk}')
+        return f'gptid/{uuid.text}'
+
 
 def new_swap_name():
     """
