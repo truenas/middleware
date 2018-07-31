863ada563d1eeb3e8d1a05cc862e800ac7ff59f5
Andrew <walker.aj325@gmail.com>
2018-07-31 19:55:38 -0400

    flake8

--- gui/common/freenasldap.py
+++ gui/common/freenasldap.py
@@ -1158,7 +1158,7 @@ class FreeNAS_ActiveDirectory_Base(object):
 
         r = resolver.Resolver()
         r.lifetime = _fs().directoryservice.activedirectory.dns.lifetime
-        r.timeout = r.lifetime / 3 
+        r.timeout = r.lifetime / 3
 
         try:
 
