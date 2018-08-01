691e03355f62354df9f5b86e9db5ab60e2559f33
caleb <yocalebo@gmail.com>
2018-08-01 08:47:05 -0400

    use non-blocking middleware calls and style changes based on mav's feedback

--- src/middlewared/middlewared/plugins/unscheduled_reboot_alert.py
+++ src/middlewared/middlewared/plugins/unscheduled_reboot_alert.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 import os
 import textwrap
-import re
 
 from middlewared.service import Service
-from middlewared.client import Client
 
 SENTINEL_PATH = "/data/sentinels/unscheduled-reboot"
 MIDDLEWARE_STARTED_SENTINEL_PATH = "/tmp/.middleware-started"
@@ -16,10 +14,6 @@ WATCHDOG_ALERT_FILE = "/data/sentinels/.watchdog-alert"
 # This file is managed in TrueNAS HA code (.../sbin/fenced)
 # Ticket 39114
 FENCED_ALERT_FILE = "/data/sentinels/.fenced-alert"
-
-# This file only exists on TrueNAS HA systems
-# Ticket 39114
-TRUENAS_HA_FILE = "/tmp/.ha_mode"
 
 
 class UnscheduledRebootAlertService(Service):
@@ -36,24 +30,27 @@ async def setup(middleware):
 
         gc = await middleware.call('datastore.config', 'network.globalconfiguration')
         now = datetime.now().strftime("%c")
+        hostname = f"{gc['gc_hostname']}.{gc.['gc_domain']}"
 
-        Client() = c
-        if not c.call('notifier.is_freenas') and c.call('notifier.failover_licensed')
-            try:
-                with open(TRUENAS_HA_FILE, "rb") as controller:
-                    match = re.search(r"(.*):(.*)")
-                if match.group(2) == 'B':
-                    hostname = f"{gc['gc_hostname_b'].{gc['gc_domain']}"
-            except IOError:
-                pass
-        else:
-            hostname = f"{gc['gc_hostname']}.{gc.['gc_domain']}"
+        watchdog_file = False
+        fenced_file = False
+        if not await middleware.call('notifier.is_freenas') and await middleware.call('notifier.failover_licensed')
+            if os.path.exists(WATCHDOG_ALERT_FILE):
+                watchdog_file = True
+                watchdog_time = os.path.getmtime(WATCHDOG_ALERT_FILE)
+            if os.path.exists(FENCED_ALERT_FILE):
+                fenced_file = True
+                fenced_time = os.path.getmtime(FENCED_ALERT_FILE)
+
+            controller = await middleware.call('notifier.failover_node')
+            if controller == 'B':
+                hostname = f"{gc['gc_hostname_b']}.{gc.['gc_domain']}"
 
         # If the watchdog alert file exists,
         # then we can assume that carp-state-change-hook.py
         # panic'ed the box by design via a watchdog countdown.
         # Let's alert the end user why we did this
-        if os.path.exists(WATCHDOG_ALERT_FILE) and not os.path.exists(FENCED_ALERT_FILE):
+        if (watchdog_file and (not fenced_file or watchdog_time > fenced_time)):
             await middleware.call("mail.send", {
                 "subject": f"{hostname}: Failover event",
                 "text": textwrap.dedent(f"""\
@@ -66,7 +63,7 @@ async def setup(middleware):
         # If the fenced alert file exists,
         # then we can assume that fenced panic'ed the box by design.
         # Let's alert the end user why we did this
-        elif os.path.exists(FENCED_ALERT_FILE) and not os.path.exists(WATCHDOG_ALERT_FILE):
+        elif fenced_file:
             await middleware.call("mail.send", {
                 "subject": f"{hostname}: Failover event",
                 "text": textwrap.dedent(f"""\
@@ -74,34 +71,7 @@ async def setup(middleware):
                     The system was rebooted because persistent SCSI reservations were lost and/or cleared.
                     The operating system successfully came back online at {now}.
                 """),
-
             })
-
-        # If both files exist, then something weird happened.
-        # Get the modify time of the files, and send an email accordingly
-        elif os.path.exists(WATCHDOG_ALERT_FILE) and os.path.exists(FENCED_ALERT_FILE):
-            watchdog_time = os.path.getmtime(WATCHDOG_ALERT_FILE)
-            fenced_time = os.path.getmtime(FENCED_ALERT_FILE)
-            if (watchdog_time > fenced_time):
-                await middleware.call("mail.send", {
-                    "subject": f"{hostname}: Failover event",
-                    "text": textwrap.dedent(f"""\
-                        {hostname} had a failover event.
-                        The system was rebooted to ensure a proper failover occurred.
-                        The operating system successfully came back online at {now}.
-                    """),
-
-                })
-
-            else:
-                await middleware.call("mail.send", {
-                    "subject": f"{hostname}: Failover event",
-                    "text": textwrap.dedent(f"""\
-                        {hostname} had a failover event.
-                        The system was rebooted because persistent SCSI reservations were lost and/or cleared.
-                        The operating system successfully came back online at {now}.
-                    """),
-                })
 
         else:
             await middleware.call("mail.send", {
