9db565946629388581224ab9913eceffa9c891fd
Andrew <awalker@ixsystems.com>
2018-08-10 10:07:57 -0400

    fix whitespace

--- src/middlewared/middlewared/etc_files/krb5.keytab.py
+++ src/middlewared/middlewared/etc_files/krb5.keytab.py
@@ -2,7 +2,6 @@ import logging
 import os
 import base64
 
-from middlewared.utils import run
 from freenasUI.common.pipesubr import pipeopen
 
 logger = logging.getLogger(__name__)
@@ -10,12 +9,14 @@ kdir = "/etc/kerberos"
 keytabfile = "/etc/krb5.keytab"
 ktutil_cmd = "/usr/sbin/ktutil copy"
 
+
 async def ktutil_copy(temp_keytab):
     p = pipeopen(f'{ktutil_cmd} {temp_keytab} {keytabfile}')
     output = p.communicate()
     if output[1]:
         logger.debug(f'generate krb5.keytab failed with error: {output[1]}')
 
+        
 async def write_keytab(db_keytabname, db_keytabfile):
     temp_keytab = f'{kdir}/{db_keytabname}'
     if not os.path.exists(kdir):
@@ -27,6 +28,7 @@ async def write_keytab(db_keytabname, db_keytabfile):
 
     await ktutil_copy(temp_keytab)
 
+    
 async def render(service, middleware):
     keytabs = await middleware.call("datastore.query", "directoryservice.kerberoskeytab")
     if not keytabs:
@@ -37,4 +39,3 @@ async def render(service, middleware):
         db_keytabfile = base64.b64decode(keytab['keytab_file'])
         db_keytabname = keytab['keytab_name']
         await write_keytab(db_keytabname, db_keytabfile)
-
