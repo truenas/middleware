f74e41c23b545acdb383604eb93f4094b736466e
themylogin <themylogin@gmail.com>
2018-08-20 20:43:55 +0300

    Expose no_auth_required in core.get_methods

--- src/middlewared/middlewared/service.py
+++ src/middlewared/middlewared/service.py
@@ -448,6 +448,7 @@ class CoreService(Service):
                     'examples': examples,
                     'accepts': accepts,
                     'item_method': True if item_method else hasattr(method, '_item_method'),
+                    'no_auth_required': hasattr(method, '_no_auth_required'),
                     'filterable': hasattr(method, '_filterable'),
                     'require_websocket': hasattr(method, '_pass_app'),
                     'job': hasattr(method, '_job'),
