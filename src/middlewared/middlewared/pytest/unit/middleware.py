4bff57d39bdbd9befc0e91fdfa37c7f1adc80a25
William Grzybowski <wg@FreeBSD.org>
2018-08-30 15:22:39 -0300

    feat(pytest): first few tests for interfaces
    Ticket:	#35769

---
+++ src/middlewared/middlewared/pytest/unit/middleware.py
@@ -0,0 +1,17 @@
+from asynctest import Mock
+
+
+class Middleware(dict):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self['system.is_freenas'] = Mock(return_value=True)
+
+    async def _call(self, name, serviceobj, method, args):
+        return await method(*args)
+
+    async def call(self, name, *args):
+        return self[name](*args)
+
+    async def run_in_io_thread(self, method, *args, **kwargs):
+        return method(*args, **kwargs)
