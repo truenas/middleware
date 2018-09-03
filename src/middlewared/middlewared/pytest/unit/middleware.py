28e4abf6e5197537a1aac903d6015421e2434d97
William Grzybowski <wg@FreeBSD.org>
2018-09-03 14:47:16 -0300

    feat(pytest): a few more unit tests for interfaces
    While I'm here do some PEP8 fixes.
    
    Ticket:	#35769

--- src/middlewared/middlewared/pytest/unit/middleware.py
+++ src/middlewared/middlewared/pytest/unit/middleware.py
@@ -1,4 +1,5 @@
 from asynctest import Mock
+from middlewared.schema import Schemas, resolve_methods
 
 
 class Middleware(dict):
@@ -6,8 +7,11 @@ class Middleware(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self['system.is_freenas'] = Mock(return_value=True)
+        self.__schemas = Schemas()
 
     async def _call(self, name, serviceobj, method, args):
+        to_resolve = [getattr(serviceobj, attr) for attr in dir(serviceobj) if attr != 'query']
+        resolve_methods(self.__schemas, to_resolve)
         return await method(*args)
 
     async def call(self, name, *args):
--- src/middlewared/middlewared/pytest/unit/plugins/test_interfaces.py
+++ src/middlewared/middlewared/pytest/unit/plugins/test_interfaces.py
@@ -1,6 +1,6 @@
 import pytest
 
-from asynctest import ANY, Mock
+from asynctest import Mock
 
 from middlewared.service import ValidationErrors
 from middlewared.plugins.network import InterfacesService
@@ -11,6 +11,8 @@ INTERFACES = [
     {
         'id': 'em0',
         'name': 'em0',
+        'fake': False,
+        'type': 'PHYSICAL',
         'aliases': [],
         'options': '',
         'ipv4_dhcp': False,
@@ -22,6 +24,8 @@ INTERFACES = [
     {
         'id': 'em1',
         'name': 'em1',
+        'fake': False,
+        'type': 'PHYSICAL',
         'aliases': [],
         'options': '',
         'ipv4_dhcp': False,
@@ -32,13 +36,48 @@ INTERFACES = [
     },
 ]
 
+INTERFACES_WITH_VLAN = INTERFACES + [
+    {
+        'id': 'vlan5',
+        'name': 'vlan5',
+        'fake': False,
+        'type': 'VLAN',
+        'aliases': [],
+        'options': '',
+        'ipv4_dhcp': False,
+        'ipv6_auto': False,
+        'state': {
+            'cloned': True,
+        },
+        'vlan_tag': 5,
+        'vlan_parent_interface': 'em0',
+    },
+]
+
+
+INTERFACES_WITH_LAG = INTERFACES + [
+    {
+        'id': 'lagg0',
+        'name': 'lagg0',
+        'fake': False,
+        'type': 'LINK_AGGREGATION',
+        'aliases': [],
+        'options': '',
+        'ipv4_dhcp': False,
+        'ipv6_auto': False,
+        'state': {
+            'cloned': True,
+        },
+        'lag_ports': ['em0'],
+    },
+]
+
 
 @pytest.mark.asyncio
 async def test__interfaces_service__create_lagg_invalid_ports():
 
     m = Middleware()
     m['interfaces.query'] = Mock(return_value=INTERFACES)
-    verrors = Mock()
 
     with pytest.raises(ValidationErrors) as ve:
         await InterfacesService(m).create({
@@ -50,11 +89,40 @@ async def test__interfaces_service__create_lagg_invalid_ports():
 
 
 @pytest.mark.asyncio
+async def test__interfaces_service__create_lagg_invalid_ports_cloned():
+
+    m = Middleware()
+    m['interfaces.query'] = Mock(return_value=INTERFACES_WITH_VLAN)
+
+    with pytest.raises(ValidationErrors) as ve:
+        await InterfacesService(m).create({
+            'type': 'LINK_AGGREGATION',
+            'lag_protocol': 'LACP',
+            'lag_ports': ['em1', 'vlan5'],
+        })
+    assert 'interface_create.lag_ports.1' in ve.value
+
+
+@pytest.mark.asyncio
+async def test__interfaces_service__create_lagg_invalid_ports_used():
+
+    m = Middleware()
+    m['interfaces.query'] = Mock(return_value=INTERFACES_WITH_LAG)
+
+    with pytest.raises(ValidationErrors) as ve:
+        await InterfacesService(m).create({
+            'type': 'LINK_AGGREGATION',
+            'lag_protocol': 'LACP',
+            'lag_ports': ['em0'],
+        })
+    assert 'interface_create.lag_ports.0' in ve.value
+
+
+@pytest.mark.asyncio
 async def test__interfaces_service__create_lagg_invalid_name():
 
     m = Middleware()
     m['interfaces.query'] = Mock(return_value=INTERFACES)
-    verrors = Mock()
 
     with pytest.raises(ValidationErrors) as ve:
         await InterfacesService(m).create({
@@ -73,7 +141,6 @@ async def test__interfaces_service__create_lagg():
     m['interfaces.query'] = Mock(return_value=INTERFACES)
     m['datastore.query'] = Mock(return_value=[])
     m['datastore.insert'] = Mock(return_value=5)
-    verrors = Mock()
 
     await InterfacesService(m).create({
         'type': 'LINK_AGGREGATION',
@@ -87,7 +154,6 @@ async def test__interfaces_service__create_vlan_invalid_parent():
 
     m = Middleware()
     m['interfaces.query'] = Mock(return_value=INTERFACES)
-    verrors = Mock()
 
     with pytest.raises(ValidationErrors) as ve:
         await InterfacesService(m).create({
@@ -100,11 +166,25 @@ async def test__interfaces_service__create_vlan_invalid_parent():
 
 
 @pytest.mark.asyncio
+async def test__interfaces_service__create_vlan_invalid_parent_used():
+
+    m = Middleware()
+    m['interfaces.query'] = Mock(return_value=INTERFACES_WITH_LAG)
+
+    with pytest.raises(ValidationErrors) as ve:
+        await InterfacesService(m).create({
+            'type': 'VLAN',
+            'vlan_tag': 5,
+            'vlan_parent_interface': 'em0',
+        })
+    assert 'interface_create.vlan_parent_interface' in ve.value
+
+
+@pytest.mark.asyncio
 async def test__interfaces_service__create_vlan_invalid_name():
 
     m = Middleware()
     m['interfaces.query'] = Mock(return_value=INTERFACES)
-    verrors = Mock()
 
     with pytest.raises(ValidationErrors) as ve:
         await InterfacesService(m).create({
@@ -123,10 +203,53 @@ async def test__interfaces_service__create_vlan():
     m['interfaces.query'] = Mock(return_value=INTERFACES)
     m['datastore.query'] = Mock(return_value=[])
     m['datastore.insert'] = Mock(return_value=5)
-    verrors = Mock()
 
     await InterfacesService(m).create({
         'type': 'VLAN',
         'vlan_tag': 5,
         'vlan_parent_interface': 'em0',
     })
+
+
+@pytest.mark.asyncio
+async def test__interfaces_service__update_two_dhcp():
+
+    interfaces_with_one_dhcp = INTERFACES.copy()
+    interfaces_with_one_dhcp[0]['ipv4_dhcp'] = True
+
+    m = Middleware()
+    m['interfaces.query'] = Mock(return_value=interfaces_with_one_dhcp)
+
+    update_interface = interfaces_with_one_dhcp[1]
+
+    with pytest.raises(ValidationErrors) as ve:
+        await InterfacesService(m).update(
+            update_interface['id'], {
+                'ipv4_dhcp': True,
+            },
+        )
+    assert 'interface_update.ipv4_dhcp' in ve.value
+
+
+@pytest.mark.asyncio
+async def test__interfaces_service__update_two_same_network():
+
+    interfaces_one_network = INTERFACES.copy()
+    interfaces_one_network[0]['aliases'] = [
+        {'type': 'INET', 'address': '192.168.5.2', 'netmask': 24},
+    ]
+
+    m = Middleware()
+    m['interfaces.query'] = Mock(return_value=interfaces_one_network)
+    m['datastore.query'] = Mock(return_value=[])
+    m['datastore.insert'] = Mock(return_value=5)
+
+    update_interface = interfaces_one_network[1]
+
+    with pytest.raises(ValidationErrors) as ve:
+        await InterfacesService(m).update(
+            update_interface['id'], {
+                'aliases': ['192.168.5.3/24'],
+            },
+        )
+    assert 'interface_update.aliases.0' in ve.value
