11b0dc75daeccb669cdf2faa3481663bc6f250a4
Waqar Ahmed <waqarsonic1@gmail.com>
2018-09-03 15:21:33 +0500

    Bug fix for Menu link in Legacy UI
    This commit fixes a bug where context was being taken into account for a menu link under System ( View Enclosure ). The value of the tab should've been 'storage.ViewEnclosure', however as it was placed under the System tree, it's value was 'system.ViewEnclosure'. This commit does a context free comparision for children of settings tree.
    Ticket: #37990

--- gui/freeadmin/static/lib/js/freeadmin/Menu.js
+++ gui/freeadmin/static/lib/js/freeadmin/Menu.js
@@ -29,7 +29,7 @@ define([
                     if(tabnet) {
                         var c2 = tabnet.getChildren();
                         for(var j=0; j<c2.length; j++){
-                            if(c2[j].domNode.getAttribute("tab") == tab)
+                            if(c2[j].domNode.getAttribute("tab").split(".").slice(1).join(".") == tab.split(".").slice(1).join("."))
                                 tabnet.selectChild(c2[j]);
                         }
                     }
