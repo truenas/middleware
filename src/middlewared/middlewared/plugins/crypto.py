a20cc230b501262f65e77430d82c456d2986a7ad
Waqar Ahmed <waqarsonic1@gmail.com>
2018-09-11 01:34:26 +0500

    Potential bugs resolved

--- src/middlewared/middlewared/plugins/crypto.py
+++ src/middlewared/middlewared/plugins/crypto.py
@@ -1299,7 +1299,7 @@ class CertificateAuthorityService(CRUDService):
         Int('id')
     )
     async def do_delete(self, id):
-        ca = self._get_instance(id)
+        ca = await self._get_instance(id)
 
         response = await self.middleware.call(
             'datastore.delete',
