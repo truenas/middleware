d198c67e3bd8bf3fd3b8403946196a5b712bce0a
William Grzybowski <wg@FreeBSD.org>
2018-09-20 11:12:01 -0300

    fix(middlewared/vm): avoid exception if `path` does not exist
    While I'm here simplify logic/construct.
    
    Ticket:	#47415

--- src/middlewared/middlewared/plugins/vm.py
+++ src/middlewared/middlewared/plugins/vm.py
@@ -689,23 +689,30 @@ class VMService(CRUDService):
             dict: will return a dict with vm_id, vm_name, device_id and disk path.
         """
         vms_attached = []
-        if pool:
-            devices = await self.middleware.call('datastore.query', 'vm.device')
-            for device in devices:
-                if device['dtype'] == 'DISK' or device['dtype'] == 'RAW':
-                        disk = device['attributes'].get('path', None)
-                        if device['dtype'] == 'DISK':
-                            disk = disk.lstrip('/dev/zvol/').split('/')[0]
-                        elif device['dtype'] == 'RAW':
-                            disk = disk.lstrip('/mnt/').split('/')[0]
+        if not pool:
+            return vms_attached
+        devices = await self.middleware.call('datastore.query', 'vm.device')
+        for device in devices:
+            if device['dtype'] not in ('DISK', 'RAW'):
+                continue
+            disk = device['attributes'].get('path', None)
+            if not disk:
+                continue
+            if device['dtype'] == 'DISK':
+                disk = disk.lstrip('/dev/zvol/').split('/')[0]
+            elif device['dtype'] == 'RAW':
+                disk = disk.lstrip('/mnt/').split('/')[0]
 
-                        if disk == pool:
-                            status = await self.status(device['vm'].get('id'))
-                            vms_attached.append({'vm_id': device['vm'].get('id'), 'vm_name': device['vm'].get('name'),
-                                                 'device_id': device.get('id'), 'vm_disk': device['attributes'].get('path', None)})
-                            if stop:
-                                if status.get('state') == 'RUNNING':
-                                    await self.stop(device['vm'].get('id'))
+            if disk == pool:
+                status = await self.status(device['vm'].get('id'))
+                vms_attached.append({
+                    'vm_id': device['vm'].get('id'),
+                    'vm_name': device['vm'].get('name'),
+                    'device_id': device.get('id'),
+                    'vm_disk': device['attributes'].get('path', None)
+                })
+                if stop and status.get('state') == 'RUNNING':
+                    await self.stop(device['vm'].get('id'))
         return vms_attached
 
     @accepts(Int('id'))
