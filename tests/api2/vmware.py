58ee4487b8bc43c698c9cb180c2f3dff0796803a
ericbsd <ericturgeon.bsd@gmail.com>
2018-09-24 15:18:02 -0300

    Adding vmware API v2 testing

---
+++ tests/api2/vmware.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.6
+
+# Author: Eric Turgeon
+# License: BSD
+
+import pytest
+import sys
+import os
+apifolder = os.getcwd()
+sys.path.append(apifolder)
+from functions import GET, POST
+
+dataset = "tank/vmware"
+url_dataset = "tank%2Fvmware"
+
+vmw_credentials = pytest.mark.skipif(all(['VMWARE_HOST' in os.environ,
+                                          'VMWARE_USERNAME' in os.environ,
+                                          'VMWARE_PASSWORD' in os.environ]
+                                         ) is False, reason="No credentials"
+                                     )
+
+
+@pytest.fixture(scope='module')
+def data():
+    return {}
+
+
+def test_01_creating_dataset_for_vmware():
+    payload = {"name": dataset}
+    results = POST("/pool/dataset/", payload)
+    assert results.status_code == 200, results.text
+
+
+def test_02_get_vmware_query():
+    results = GET('/vmware/')
+    assert results.status_code == 200
+    assert isinstance(results.json(), list) is True
+
+
+@vmw_credentials
+def test_03_create_vmware(data):
+    payload = {
+        'hostname': os.environ['VMWARE_HOST'],
+        'username': os.environ['VMWARE_USERNAME'],
+        'password': os.environ['VMWARE_PASSWORD']
+    }
+    results = POST('/vmware/get_datastores/', payload)
+    assert results.status_code == 200, results.text
+    assert isinstance(results.json(), dict) is True, results.text
+    data['vmid'] = results.json()
