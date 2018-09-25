5c67c0545af008841dee714deab5e68935115d7d
William Grzybowski <wg@FreeBSD.org>
2018-09-25 11:58:26 -0300

    feat(asigra): create asigra auxiliary filesystems from base filesystem
    Ticket:	#48042

--- gui/services/migrations/0019_add_asigra_model.py
+++ gui/services/migrations/0019_add_asigra_model.py
@@ -3,7 +3,11 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import freenasUI.freeadmin.models.fields
+
+
+def add_asigra(apps, schema_editor):
+    Asigra = apps.get_model('services', 'asigra')
+    Asigra.objects.create()
 
 
 class Migration(migrations.Migration):
@@ -17,12 +21,11 @@ class Migration(migrations.Migration):
             name='Asigra',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('asigra_bindip', freenasUI.freeadmin.models.fields.MultiSelectField(blank=True, help_text='IP addresses to bind to. If none specified, all available interfaces that are up will be listened on.', max_length=250, null=True, verbose_name='Bind IP Addresses')),
-                ('asigra_path', freenasUI.freeadmin.models.fields.PathField(blank=True, max_length=255, null=True, verbose_name='Path')),
-                ('asigra_postgresql_path', freenasUI.freeadmin.models.fields.PathField(blank=True, max_length=255, null=True, verbose_name='PG Path')),
+                ('filesystem', models.CharField(blank=True, max_length=255, verbose_name='Base Filesystem')),
             ],
             options={
                 'abstract': False,
             },
         ),
+        migrations.RunPython(add_asigra, reverse_code=migrations.RunPython.noop)
     ]
