753177bf50500c5244989a6e0aed1c5d6f81ba45
William Grzybowski <wg@FreeBSD.org>
2018-09-25 11:56:45 -0300

    feat(asigra): create asigra auxiliary filesystems from base filesystem
    Ticket:	#48042

--- src/freenas/usr/local/lib/middlewared_truenas/plugins/asigra.py
+++ src/freenas/usr/local/lib/middlewared_truenas/plugins/asigra.py
@@ -1,5 +1,4 @@
 import glob
-import grp
 import os
 import psycopg2 as pg
 import pwd
@@ -7,18 +6,17 @@ import re
 import shutil
 import subprocess
 
-from psycopg2.extras import DictCursor
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-from middlewared.service import Service
+from middlewared.service import Service, private
 from subprocess import Popen
+
 
 class AsigraService(Service):
 
     class Config:
         service = "asigra"
         datastore_prefix = "asigra_"
-
 
     def __init__(self, middleware):
         super(AsigraService, self).__init__(middleware)
@@ -39,6 +37,29 @@ class AsigraService(Service):
             self.pg_user_uid = 5432
             self.pg_group_gid = 5432
 
+    @private
+    def setup_filesystems(self):
+        config = self.middleware.call_sync('datastore.config', 'services.asigra')
+
+        if not config['filesystem']:
+            return False
+
+        if not self.middleware.call_sync('zfs.dataset.query', [('id', '=', config['filesystem'])]):
+            return False
+
+        filesystems = ('files', 'database', 'upgrade')
+        for fs in filesystems:
+            fs = f'{config["filesystem"]}/{fs}'
+            if self.middleware.call_sync('zfs.dataset.query', [('id', '=', fs)]):
+                continue
+            proc = subprocess.Popen(
+                ['zfs', 'create', fs], stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            )
+            stdout = proc.communicate()[0]
+            if proc.returncode != 0:
+                self.logger.error('Failed to create %s: %s', fs, stdout.decode())
+                return False
+
     # Use etc plugin for this?
     def setup_postgresql(self):
         asigra_config = None
@@ -47,8 +68,8 @@ class AsigraService(Service):
         if not asigra_config:
             return False
 
-        asigra_postgresql_path = asigra_config['asigra_postgresql_path']
-        if not asigra_postgresql_path:
+        asigra_postgresql_path = f'/mnt/{asigra_config["filesystem"]}/database'
+        if not asigra_config["filesystem"] or not os.path.exists(asigra_postgresql_path):
             return False
 
         asigra_postgresql_conf = os.path.join(asigra_postgresql_path, "postgresql.conf")
@@ -59,7 +80,7 @@ class AsigraService(Service):
             os.mkdir(self.pg_home, mode=0o750)
             shutil.chown(self.pg_home, user=self.pg_user, group=self.pg_group)
 
-        s = os.stat(self.pg_home) 
+        s = os.stat(self.pg_home)
         if (s.st_uid != self.pg_user_uid) or (s.st_gid != self.pg_group_gid):
             shutil.chown(self.pg_home, user=self.pg_user, group=self.pg_group)
 
@@ -67,7 +88,7 @@ class AsigraService(Service):
             os.mkdir(asigra_postgresql_path, mode=0o750)
             shutil.chown(asigra_postgresql_path, user=self.pg_user, group=self.pg_group)
 
-        s = os.stat(asigra_postgresql_path) 
+        s = os.stat(asigra_postgresql_path)
         if (s.st_uid != self.pg_user_uid) or (s.st_gid != self.pg_group_gid):
             shutil.chown(asigra_postgresql_path, user=self.pg_user, group=self.pg_group)
 
@@ -96,13 +117,13 @@ class AsigraService(Service):
 
             if not os.path.exists(asigra_postgresql_conf):
                 self.logger.error("{} doesn't exist!".format(asigra_postgresql_conf))
-                return 
+                return
 
             # listen_address = '*'
             pg_conf_regex = re.compile('.*(#)\s{0,}?listen_addresses\s{0,}=\s{0,}([^\s]+)', re.M | re.S)
             pg_conf_buf = []
             rewrite = False
-   
+
             with open(asigra_postgresql_conf, "r") as f:
                 for line in f:
                     if pg_conf_regex.search(line):
@@ -215,7 +236,7 @@ class AsigraService(Service):
 
             except Exception as e:
                 self.logger.error("Can't init dssystem database: {}".format(e))
-                return False 
+                return False
 
         cur.close()
         con.close()
@@ -270,7 +291,7 @@ class AsigraService(Service):
 
         max = 0
         for f in files:
-            m  = re.match('.*/dssp([0-9]+).sql', f)
+            m = re.match('.*/dssp([0-9]+).sql', f)
             if not m or len(m.groups()) != 1:
                 continue
             if int(m.group(1)) > max:
@@ -279,8 +300,8 @@ class AsigraService(Service):
         db_number = self.get_db_number()
         if db_number < 0:
             db_number *= -1
-    
-        db_number += 1 
+
+        db_number += 1
         while max >= db_number:
             sql_patch = os.path.join(self.dssystem_db_path, "dssp{}.sql".format(db_number))
 
@@ -330,15 +351,15 @@ class AsigraService(Service):
                 self.logger.error('Failed to install dssystem package:\n{}'.format(output))
                 ret = False
 
-            if ret != False:
+            if ret is not False:
                 try:
                     os.chmod("/usr/local/etc/rc.d/dssystem", 0o555)
                     shutil.copyfile("/usr/local/etc/rc.d/dssystem", "/conf/base/etc/local/rc.d/dssystem")
                     os.chmod("/conf/base/etc/local/rc.d/dssystem", 0o555)
 
-                except Exception: 
+                except Exception:
                     self.logger.error("Failed to install dssystem rc.d script")
-                    ret = False 
+                    ret = False
 
         return ret
 
@@ -354,8 +375,8 @@ class AsigraService(Service):
         if not asigra_config:
             return False
 
-        asigra_path = asigra_config['asigra_path']
-        if not asigra_path:
+        asigra_path = f'/mnt/{asigra_config["filesystem"]}/files'
+        if not asigra_config["filesystem"] or not os.path.exists(asigra_path):
             return False
 
         self.logger.debug("Checking to see if Asigra is installed")
@@ -376,4 +397,4 @@ class AsigraService(Service):
             self.logger.debug("Updating database")
             self.asigra_database_update()
 
-        return True 
+        return True
