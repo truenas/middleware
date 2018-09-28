86586e1546b364c5d1204d105f8d3d33a5ce6dd5
bugclerk <40872210+bugclerk@users.noreply.github.com>
2018-09-28 15:16:53 +0300

    tkt-45904: Adjust B2 settings titles according to rclone documentation (#1822)
    Ticket: #45904

--- src/middlewared/middlewared/rclone/remote/b2.py
+++ src/middlewared/middlewared/rclone/remote/b2.py
@@ -1,3 +1,5 @@
+import textwrap
+
 from middlewared.rclone.base import BaseRcloneRemote
 from middlewared.schema import Str
 
@@ -13,6 +15,9 @@ class B2RcloneRemote(BaseRcloneRemote):
     rclone_type = "b2"
 
     credentials_schema = [
-        Str("account", title="Account ID or Application Key ID", required=True),
+        Str("account", title="Account ID or Application Key ID", description=textwrap.dedent("""\
+            Put your Account ID here and use your Master Application Key as Application Key or create separate
+            Application Key and use it with its own Application Key ID
+        """), required=True),
         Str("key", title="Application Key", required=True),
     ]
