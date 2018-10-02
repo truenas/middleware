4360a24717c354a6efbdb08443e450dbad312f61
William Grzybowski <wg@FreeBSD.org>
2018-10-02 12:32:16 -0300

    fix(migration): rename migration before merge

--- gui/system/migrations/0029_add_organizational_unit.py
+++ gui/system/migrations/0029_add_organizational_unit.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0026_keyvalue'),
+        ('system', '0028_cert_serials'),
     ]
 
     operations = [
