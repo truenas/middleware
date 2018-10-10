54f20f06e0215b88842b6de28418157adcea1fdc
Waqar Ahmed <waqarsonic1@gmail.com>
2018-10-10 05:53:13 +0500

    Minor improvements in nginx.conf and alert file updated to make use of list of addresses instead of one single address

--- src/middlewared/middlewared/alert/source/httpd_bindaddr.py
+++ src/middlewared/middlewared/alert/source/httpd_bindaddr.py
@@ -7,14 +7,20 @@ class HTTPDBindAddressAlertSource(AlertSource):
 
     async def check(self):
         settings = await self.middleware.call("datastore.query", "system.settings", None, {"get": True})
-        address = settings["stg_guiaddress"]
+        addresses = settings["stg_guiaddress"]
+        alerts = []
         with open("/usr/local/etc/nginx/nginx.conf") as f:
             # XXX: this is parse the file instead of slurping in the contents
             # (or in reality, just be moved somewhere else).
-            if (f.read().find("0.0.0.0") != -1 and
-                    address not in ("0.0.0.0", "")):
-                # XXX: IPv6
-                return Alert(
-                    "The WebGUI Address could not bind to %s; using wildcard",
-                    address,
-                )
+            data = f.read()
+            for address in addresses:
+                if (data.find("0.0.0.0") != -1 and
+                        address not in ("0.0.0.0", "")):
+                    # XXX: IPv6
+                    alerts.append(
+                        Alert(
+                            f"The WebGUI Address could not bind to {address}; using wildcard",
+                        )
+                    )
+
+        return alerts
