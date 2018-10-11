bb28d359d8acd02c6a14ffc54f716f35fc759224
William Grzybowski <wg@FreeBSD.org>
2018-10-11 13:35:20 -0300

    feat(api): use delete_form if available
    Ticket:	#35769

--- gui/api/utils.py
+++ gui/api/utils.py
@@ -53,6 +53,7 @@ from tastypie.authentication import (
 )
 from tastypie import fields, http
 from tastypie.authorization import Authorization
+from tastypie.bundle import Bundle
 from tastypie.exceptions import ImmediateHttpResponse, NotFound, UnsupportedFormat
 from tastypie.http import HttpUnauthorized, HttpNotFound
 from tastypie.paginator import Paginator
@@ -372,6 +373,45 @@ class DojoModelResource(ResourceMixin, ModelResource):
                 return self.create_response(request, updated_bundle)
         except (NotFound, MultipleObjectsReturned):
             return self.create_response(request, 'Object not found', response_class=HttpNotFound)
+
+    def delete_detail(self, request, **kwargs):
+        bundle = Bundle(request=request)
+        bundle.obj = self.obj_get(bundle=bundle, **self.remove_api_resource_names(kwargs))
+        if bundle.obj._meta.model._admin.delete_form:
+            deserialized = self.deserialize(
+                request,
+                request.body or '{}',
+                format=request.META.get('CONTENT_TYPE', 'application/json'),
+            )
+
+            Form = __import__(
+                f'{bundle.obj._meta.app_label}.forms',
+                globals(),
+                locals(),
+                [bundle.obj._meta.model._admin.delete_form],
+                0,
+            )
+            Form = getattr(Form, bundle.obj._meta.model._admin.delete_form)
+
+            form = Form(data=deserialized, instance=bundle.obj)
+            if not form.is_valid():
+                raise ImmediateHttpResponse(
+                    response=self.error_response(request, form.errors)
+                )
+        if bundle.obj._meta.model._admin.edit_modelform:
+            ModelForm = __import__(
+                f'{bundle.obj._meta.app_label}.forms',
+                globals(),
+                locals(),
+                [bundle.obj._meta.model._admin.edit_modelform],
+                0,
+            )
+            ModelForm = getattr(ModelForm, bundle.obj._meta.model._admin.edit_modelform)
+            mf = ModelForm(instance=bundle.obj)
+            mf.delete()
+            return http.HttpNoContent()
+        else:
+            return super().delete_detail(request, **kwargs)
 
     def save(self, bundle, skip_errors=False):
 
