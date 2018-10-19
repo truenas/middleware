a3662f0b2cc46fff52547d1515872c45ce6234ef
themylogin <themylogin@gmail.com>
2018-10-19 09:39:57 +0300

    New pool.snapshottask

--- src/middlewared/middlewared/plugins/snapshot.py
+++ src/middlewared/middlewared/plugins/snapshot.py
@@ -1,8 +1,7 @@
-from datetime import time
+from zettarepl.snapshot.task.naming_schema import validate_snapshot_naming_schema
 
-from middlewared.schema import accepts, Bool, Dict, Int, List, Patch, Str
+from middlewared.schema import accepts, Bool, Cron, Dict, Int, List, Patch, Str, Time
 from middlewared.service import CRUDService, private, ValidationErrors
-from middlewared.validators import Range, Time
 
 
 class PeriodicSnapshotTaskService(CRUDService):
@@ -10,66 +9,40 @@ class PeriodicSnapshotTaskService(CRUDService):
     class Config:
         datastore = 'storage.task'
         datastore_prefix = 'task_'
-        datastore_extend = 'pool.snapshottask.periodic_snapshot_extend'
+        datastore_extend = 'pool.snapshottask.extend'
         namespace = 'pool.snapshottask'
 
     @private
-    def periodic_snapshot_extend(self, data):
+    def extend(self, data):
+        Cron.convert_db_format_to_schedule(data)
         data['begin'] = str(data['begin'])
         data['end'] = str(data['end'])
-        data['ret_unit'] = data['ret_unit'].upper()
-        data['dow'] = [int(day) for day in data.pop('byweekday').split(',')]
-        data.pop('repeat_unit', None)
+
+        has_legacy_obstacles = bool(self._validate_legacy(data))
+        data['legacy_allowed'] = not has_legacy_obstacles
+
         return data
-
-    @private
-    async def common_validation(self, data, schema_name):
-        verrors = ValidationErrors()
-
-        if not data['dow']:
-            verrors.add(
-                f'{schema_name}.dow',
-                'At least one day must be chosen'
-            )
-
-        data['ret_unit'] = data['ret_unit'].lower()
-        data['begin'] = time(*[int(value) for value in data['begin'].split(':')])
-        data['end'] = time(*[int(value) for value in data['end'].split(':')])
-        data['byweekday'] = ','.join([str(day) for day in data.pop('dow')])
-
-        return data, verrors
 
     @accepts(
         Dict(
             'periodic_snapshot_create',
-            Bool('enabled', default=True),
-            Bool('recursive', default=False),
-            Int('interval', enum=[
-                5, 10, 15, 30, 60, 120, 180, 240,
-                360, 720, 1440, 10080, 20160, 40320
-            ], required=True),
-            Int('ret_count', required=True),
-            List('dow', items=[
-                Int('day', validators=[Range(min=1, max=7)])
-            ], required=True),
-            Str('begin', validators=[Time()], required=True),
-            Str('end', validators=[Time()], required=True),
             Str('filesystem', required=True),
-            Str('ret_unit', enum=[
-                'HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'
-            ], required=True),
+            Bool('recursive', default=False),
+            List('exclude', items=[Str('item', empty=False)], default=[]),
+            Int('ret_count', required=True),
+            Str('ret_unit', enum=['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], required=True),
+            Str('naming_schema', required=True),
+            Cron('schedule', required=True),
+            Time('begin', default='00:00'),
+            Time('end', default='23:59'),
+            Bool('enabled', default=True),
             register=True
         )
     )
     async def do_create(self, data):
+        verrors = ValidationErrors()
 
-        data, verrors = await self.common_validation(data, 'periodic_snapshot_create')
-
-        if data['filesystem'] not in (await self.middleware.call('pool.filesystem_choices')):
-            verrors.add(
-                'periodic_snapshot_create.filesystem',
-                'Invalid ZFS filesystem'
-            )
+        await self._validate(verrors, 'periodic_snapshot_create', data)
 
         if verrors:
             raise verrors
@@ -90,19 +63,25 @@ class PeriodicSnapshotTaskService(CRUDService):
         Patch('periodic_snapshot_create', 'periodic_snapshot_update', ('attr', {'update': True}))
     )
     async def do_update(self, id, data):
-
         old = await self._get_instance(id)
         new = old.copy()
         new.update(data)
 
-        new, verrors = await self.common_validation(new, 'periodic_snapshot_update')
+        verrors = ValidationErrors()
 
-        if old['filesystem'] != new['filesystem']:
-            if new['filesystem'] not in (await self.middleware.call('pool.filesystem_choices')):
-                verrors.add(
-                    'periodic_snapshot_update.filesystem',
-                    'Invalid ZFS filesystem'
-                )
+        await self._validate(verrors, 'periodic_snapshot_create', new)
+
+        legacy_replication_tasks = await self.middleware.call(
+            'datastore.query',
+            'storage.replication',
+            [
+                ['repl_tasks__id', '=', new['id']],
+                ['repl_transport', '=', 'LEGACY'],
+            ],
+            {'prefix': 'repl_'}
+        )
+        if legacy_replication_tasks:
+            verrors.add_child(f'periodic_snapshot_update', self._validate_legacy(new))
 
         if verrors:
             raise verrors
@@ -132,3 +111,51 @@ class PeriodicSnapshotTaskService(CRUDService):
         await self.middleware.call('service.restart', 'cron')
 
         return response
+
+    async def _validate(self, verrors, schema_name, data):
+        if data['filesystem'] not in (await self.middleware.call('pool.filesystem_choices')):
+            verrors.add(
+                f'{schema_name}.filesystem',
+                'Invalid ZFS filesystem'
+            )
+
+        if not data['recursive'] and data['exclude']:
+            verrors.add(
+                f'{schema_name}.exclude',
+                'Excluding filesystems has no sense for non-recursive periodic snapshot tasks'
+            )
+
+        for i, v in enumerate(data['exclude']):
+            if not v.startswith(f'{data["filesystem"]}/'):
+                verrors.add(
+                    f'{schema_name}.exclude.{i}',
+                    'Excluded dataset should be a child of selected filesystem'
+                )
+
+        try:
+            validate_snapshot_naming_schema(data['naming_schema'])
+        except ValueError as e:
+            verrors.add(
+                f'{schema_name}.naming_schema',
+                str(e)
+            )
+
+    def _validate_legacy(self, data):
+        verrors = ValidationErrors()
+
+        if data['exclude']:
+            verrors.add(
+                'exclude',
+                ('Excluding child datasets is not available because this snapshot task is being used in '
+                 'legacy replication task. Please upgrade your replication tasks to edit this field.'),
+            )
+
+        naming_schema = f'auto-%Y%m%d.%H%M%S-{data["ret_count"]}{data["ret_unit"].lower()[0]}'
+        if data['naming_schema'] != naming_schema:
+            verrors.add(
+                'naming_schema',
+                (f'Naming schema should be {naming_schema} because this snapshot task is being used in legacy '
+                 f'replication task. Please upgrade your replication tasks to edit this field.'),
+            )
+
+        return verrors
