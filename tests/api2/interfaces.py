75a95e9c26119e4eccafb4df9a14fe8ba2093dd9
ericbsd <ericturgeon.bsd@gmail.com>
2018-10-24 16:17:10 -0300

    adding test to set ipv4_dhcp to False

--- tests/api2/interfaces.py
+++ tests/api2/interfaces.py
@@ -10,7 +10,7 @@ import os
 apifolder = os.getcwd()
 sys.path.append(apifolder)
 from auto_config import interface, ip
-from functions import GET
+from functions import GET, PUT, POST, DELETE
 from config import *
 
 BRIDGENETMASKReason = "BRIDGENETMASK not in ixautomation.conf"
@@ -36,3 +36,16 @@ def test_03_get_interfaces_netmask():
     assert results.status_code == 200, results.text
     netmask = results.json()[0]['state']['aliases'][1]['netmask']
     assert netmask == int(BRIDGENETMASK), results.text
+
+
+def test_04_set_main_interfaces_ipv4_to_false():
+    payload = {'ipv4_dhcp': False}
+    results = PUT(f'/interfaces/id/{interface}', payload)
+    assert results.status_code == 200, results.text
+    assert results.json()['ipv4_dhcp'] is False, results.text
+
+
+def test_05_looking_main_interfaces_ipv4_dhcp_if_is_false():
+    results = GET(f'/interfaces/id/{interface}')
+    assert results.status_code == 200, results.text
+    assert results.json()['ipv4_dhcp'] is False, results.text
