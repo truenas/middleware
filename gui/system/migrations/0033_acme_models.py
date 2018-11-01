6cf25988299c5edf754279b67b9b002b03e7b3e0
Waqar Ahmed <waqarsonic1@gmail.com>
2018-11-01 14:05:12 +0500

    Update migration

--- gui/system/migrations/0033_acme_models.py
+++ gui/system/migrations/0033_acme_models.py
@@ -10,7 +10,7 @@ import freenasUI.freeadmin.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0031_adv_boot_scrub'),
+        ('system', '0032_remove_protocol_field'),
     ]
 
     operations = [
