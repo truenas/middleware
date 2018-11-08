34c3da29aa39afe4cdf211ebfceb6fa1a1e80c10
Waqar Ahmed <waqarsonic1@gmail.com>
2018-11-08 16:35:34 +0500

    minor alarm improvements

--- src/middlewared/middlewared/etc_files/local/netdata/alarms.py
+++ src/middlewared/middlewared/etc_files/local/netdata/alarms.py
@@ -2,7 +2,6 @@ async def render(service, middleware):
     netdata_config = await middleware.call('netdata.configuration.config')
     alarms = netdata_config['alarms']
     valid_alarms = await middleware.call('netdata.configuration.list_alarms')
-    print('\n\n\nin render')
     for alarm in valid_alarms:
         # These are valid alarms and their respective config files exist as well
         if alarm in alarms:
@@ -10,15 +9,15 @@ async def render(service, middleware):
                 content = file.readlines()
             # TODO: Execute this efficiently -- improve -- see different approaches
             with open(valid_alarms[alarm], 'w') as file:
-                print('\n\n\nwriiting file', valid_alarms[alarm])
                 check = False
                 for line in content:
-                    if 'alarm:' in line:
+                    if any(i in line for i in ('alarm:', 'template:')):
+                        # A new entity is starting
+
                         if line.split(':')[1].strip() == alarm:
                             check = True
                         elif check:
                             # This means to: wasn't present in the section of that alarm - let's write it
-                            # case - last alarm - this won't work - FIXME
                             file.write(f'      to: {"silent" if not alarms[alarm] else "sysadmin"}\n')
                             check = False
 
@@ -27,3 +26,7 @@ async def render(service, middleware):
                         check = False
                         continue
                     file.write(line)
+
+                if check:
+                    # For last alarm cases - is this ideal ?
+                    file.write(f'      to: {"silent" if not alarms[alarm] else "sysadmin"}\n')
