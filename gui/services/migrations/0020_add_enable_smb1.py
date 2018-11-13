1484dc90ce9e7a8c854d517cb7c1f06d7b1b232c
Andrew <awalker@ixsystems.com>
2018-11-13 14:02:05 -0500

    Add checkbox to enable SMB1 support
    (cherry picked from commit 70e6e541458e12c46bb947b68dffb7cfcf424955)

---
+++ gui/services/migrations/0020_add_enable_smb1.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0019_add_asigra_model'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='cifs',
+            name='cifs_srv_enable_smb1',
+            field=models.BooleanField(default=False,
+                help_text=(
+                    'Use this option to allow legacy SMB clients to connect to the '
+                    'server. Note that SMB1 is being deprecated and it is advised '
+                    'to upgrade clients to operating system versions that support '
+                    'modern versions of the SMB protocol.'
+                ),
+                verbose_name='Enable SMB1'
+        ),
+    ]
+
