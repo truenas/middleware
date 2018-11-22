afb2599681d987fbf832b307db0cc4eccef2c491
William Grzybowski <wg@FreeBSD.org>
2018-11-22 14:03:02 -0200

    fix(middlewared/mdns): do not use global variable
    Requested by @themylogin
    
    Ticket:	#59454

--- src/middlewared/middlewared/plugins/mdns.py
+++ src/middlewared/middlewared/plugins/mdns.py
@@ -16,10 +16,10 @@ from pybonjour import (
 from middlewared.service import Service, private
 
 
-MDNSD_MONITOR = None
-
-
 class mDNSDaemonMonitor(threading.Thread):
+
+    instance = None
+
     def __init__(self, middleware):
         super(mDNSDaemonMonitor, self).__init__(daemon=True)
         self.middleware = middleware
@@ -28,6 +28,11 @@ class mDNSDaemonMonitor(threading.Thread):
         self.mdnsd_piddir = "/var/run/"
         self.mdnsd_running = threading.Event()
         self.dns_sync = threading.Event()
+
+        if self.__class__.instance:
+            raise RuntimeError('Can only be instantiated a single time')
+        self.__class__.instance = self
+        self.start()
 
     def run(self):
         set_thread_name('mdnsd_monitor')
@@ -187,7 +192,7 @@ class DiscoverThread(mDNSThread):
         )
 
     def run(self):
-        if not MDNSD_MONITOR.mdnsd_running.wait(timeout=10):
+        if not mDNSDaemonMonitor.instance.mdnsd_running.wait(timeout=10):
             return
 
         sdRef = pybonjour.DNSServiceBrowse(
@@ -270,7 +275,7 @@ class ServicesThread(mDNSThread):
 
     def run(self):
         while True:
-            if not MDNSD_MONITOR.mdnsd_running.wait(timeout=10):
+            if not mDNSDaemonMonitor.instance.mdnsd_running.wait(timeout=10):
                 return
 
             try:
@@ -346,7 +351,7 @@ class ResolveThread(mDNSThread):
 
     def run(self):
         while True:
-            if not MDNSD_MONITOR.mdnsd_running.wait(timeout=10):
+            if not mDNSDaemonMonitor.instance.mdnsd_running.wait(timeout=10):
                 return
 
             try:
@@ -672,7 +677,7 @@ class mDNSAdvertiseService(Service):
             if self.initialized:
                 return
 
-        if not MDNSD_MONITOR.mdnsd_running.wait(timeout=10):
+        if not mDNSDaemonMonitor.instance.mdnsd_running.wait(timeout=10):
             return
 
         try:
@@ -717,12 +722,9 @@ class mDNSAdvertiseService(Service):
 
 
 async def dns_post_sync(middleware):
-    MDNSD_MONITOR.dns_sync.set()
+    mDNSDaemonMonitor.instance.dns_sync.set()
 
 
 def setup(middleware):
-    global MDNSD_MONITOR
-    MDNSD_MONITOR = mDNSDaemonMonitor(middleware)
-    MDNSD_MONITOR.start()
-
+    mDNSDaemonMonitor(middleware)
     middleware.register_hook('dns.post_sync', dns_post_sync)
