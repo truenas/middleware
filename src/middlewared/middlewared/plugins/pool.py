eaae26f3a9630ed104c953fabdd5da6191298f9d
themylogin <themylogin@gmail.com>
2018-11-24 11:36:01 +0300

    Fix pool.import_disk rsync stderr being lost
    (cherry picked from commit 25801806e50ce79003aa6c58f5d15186c96c2dff)

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -1783,7 +1783,7 @@ class PoolService(CRUDService):
                         dst_path
                     ]
                     rsync_proc = await Popen(
-                        line, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0, preexec_fn=os.setsid,
+                        line, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0, preexec_fn=os.setsid,
                     )
                     try:
                         progress_buffer = JobProgressBuffer(job)
