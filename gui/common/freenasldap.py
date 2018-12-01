39b5bf792016247afd6736d3272fe23fc32c0c6d
awalker <awalker@ixsystems.com>
2018-11-30 21:23:19 -0500

    remove extra line from freenasldap.py

--- gui/common/freenasldap.py
+++ gui/common/freenasldap.py
@@ -1904,7 +1904,6 @@ class FreeNAS_ActiveDirectory_Base(object):
                 log.debug("Correcting value in freenas-v1.db")
                 Client().call('datastore.update', 'services.cifs', '1', {'cifs_srv_workgroup': netbios_name})
 
-
         except Exception:
             netbios_name = None
 
