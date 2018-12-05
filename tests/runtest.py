d504aaa00dd2977ed989a69b3ba999ca8d97f5bf
ericbsd <ericturgeon.bsd@gmail.com>
2018-12-05 11:57:43 -0400

    remove vtnet0 condition for disk to suports ahci-hd

--- tests/runtest.py
+++ tests/runtest.py
@@ -81,9 +81,9 @@ if ('ip' not in locals() and
     print(error_msg)
     exit()
 
-if interface == "vtnet0":
-    disk = 'disk0 = "vtbd0"\ndisk1 = "vtbd1"\ndisk2 = "vtbd2"'
-elif api == "1.0":
+# if interface == "vtnet0":
+#     disk = 'disk0 = "vtbd0"\ndisk1 = "vtbd1"\ndisk2 = "vtbd2"'
+if api == "1.0":
     disk = 'disk0 = "da0"\ndisk1 = "da1"\ndisk2 = "da2"'
 else:
     disk = 'disk0 = "ada0"\ndisk1 = "ada1"\ndisk2 = "ada2"'
