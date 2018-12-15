0075b29e35c45765a0e734d77525f1fafb68a812
Ryan Moeller <ryan@freqlabs.com>
2018-12-15 22:09:21 +0000

    [middlewared] Give names to raw partition type identifers

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -34,6 +34,10 @@ RE_ISDISK = re.compile(r'^(da|ada|vtbd|mfid|nvd|pmem)[0-9]+$')
 RE_MPATH_NAME = re.compile(r'[a-z]+(\d+)')
 RE_SED_RDLOCK_EN = re.compile(r'(RLKEna = Y|ReadLockEnabled:\s*1)', re.M)
 RE_SED_WRLOCK_EN = re.compile(r'(WLKEna = Y|WriteLockEnabled:\s*1)', re.M)
+RAWTYPE = {
+    'freebsd-zfs':  '516e7cba-6ecf-11d6-8ff8-00022d09712b',
+    'freebsd-swap': '516e7cb5-6ecf-11d6-8ff8-00022d09712b',
+}
 
 
 class DiskService(CRUDService):
@@ -663,8 +667,7 @@ class DiskService(CRUDService):
             for g in klass.geoms:
                 for p in g.providers:
                     if p.name == name:
-                        # freebsd-zfs partition
-                        if p.config['rawtype'] == '516e7cba-6ecf-11d6-8ff8-00022d09712b':
+                        if p.config['rawtype'] == RAWTYPE['freebsd-zfs']:
                             return f'{{uuid}}{p.config["rawuuid"]}'
 
         g = geom.geom_by_name('LABEL', name)
@@ -1215,7 +1218,7 @@ class DiskService(CRUDService):
         for g in klass.geoms:
             for p in g.providers:
                 # if swap partition
-                if p.config['rawtype'] == '516e7cb5-6ecf-11d6-8ff8-00022d09712b':
+                if p.config['rawtype'] == RAWTYPE['freebsd-swap']:
                     if p.name not in used_partitions:
                         # Try to save a core dump from that.
                         # Only try savecore if the partition is not already in use
@@ -1279,7 +1282,7 @@ class DiskService(CRUDService):
             if not partgeom:
                 continue
             for p in partgeom.providers:
-                if p.config['rawtype'] == '516e7cb5-6ecf-11d6-8ff8-00022d09712b':
+                if p.config['rawtype'] == RAWTYPE['freebsd-swap']:
                     providers[p.id] = p
                     break
 
