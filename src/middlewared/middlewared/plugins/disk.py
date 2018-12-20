3b4fa8f81dd005d59f48755a78d4dd8dff172e81
Ryan Moeller <ryan@freqlabs.com>
2018-12-20 20:18:34 +0000

    [middlewared] Use default 'load' mirror balancing instead of 'prefer'
    `-b prefer` is not necessary, since the mirror is created after a
    dumpdev has been set. This means we can load balance for better
    swap performance now.
    
    Ticket: #60219

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -937,7 +937,7 @@ class DiskService(CRUDService):
                     if name is None:
                         # Which means maximum has been reached and we can stop
                         break
-                    await run('gmirror', 'create', '-b', 'prefer', name, part_a, part_b)
+                    await run('gmirror', 'create', name, part_a, part_b)
                 except Exception:
                     self.logger.warn(f'Failed to create gmirror {name}', exc_info=True)
                     continue
