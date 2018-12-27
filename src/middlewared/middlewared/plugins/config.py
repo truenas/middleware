43cbb66480f3c16cf129e42aa25c1260ee9a5297
William Grzybowski <wg@FreeBSD.org>
2018-12-27 10:57:59 -0200

    feat(middlewared/config): bring config upload logic from notifier

--- src/middlewared/middlewared/plugins/config.py
+++ src/middlewared/middlewared/plugins/config.py
@@ -1,10 +1,16 @@
+import asyncio
+import contextlib
 import os
 import shutil
+import sqlite3
 import tarfile
 import tempfile
 
 from middlewared.schema import Bool, Dict, accepts
-from middlewared.service import Service, job
+from middlewared.service import CallError, Service, job
+
+FREENAS_DATABASE = '/data/freenas-v1.db'
+NEED_UPDATE_SENTINEL = '/data/need-update'
 
 
 class ConfigService(Service):
@@ -25,13 +31,13 @@ class ConfigService(Service):
 
         if not options.get('secretseed'):
             bundle = False
-            filename = '/data/freenas-v1.db'
+            filename = FREENAS_DATABASE
         else:
             bundle = True
             filename = tempfile.mkstemp()[1]
             os.chmod(filename, 0o600)
             with tarfile.open(filename, 'w') as tar:
-                tar.add('/data/freenas-v1.db', arcname='freenas-v1.db')
+                tar.add(FREENAS_DATABASE, arcname='freenas-v1.db')
                 tar.add('/data/pwenc_secret', arcname='pwenc_secret')
 
         with open(filename, 'rb') as f:
@@ -60,8 +66,63 @@ class ConfigService(Service):
                     if nreads > 10240:
                         # FIXME: transfer to a file on disk
                         raise ValueError('File is bigger than 10MiB')
-        await self.middleware.run_in_thread(read_write)
-        rv = await self.middleware.call('notifier.config_upload', filename)
-        if not rv[0]:
-            raise ValueError(rv[1])
-        await self.middleware.call('system.reboot', {'delay': 10})
+        try:
+            await self.middleware.run_in_thread(read_write)
+            await self.middleware.run_in_thread(self.__upload, filename)
+        finally:
+            with contextlib.suppress(OSError):
+                os.unlink(filename)
+        asyncio.ensure_future(self.middleware.call('system.reboot', {'delay': 10}))
+
+    def __upload(self, config_file_name):
+        try:
+            """
+            First we try to open the file as a tar file.
+            We expect the tar file to contain at least the freenas-v1.db.
+            It can also contain the pwenc_secret file.
+            If we cannot open it as a tar, we try to proceed as it was the
+            raw database file.
+            """
+            try:
+                with tarfile.open(config_file_name) as tar:
+                    bundle = True
+                    tmpdir = tempfile.mkdtemp(dir='/var/tmp/firmware')
+                    tar.extractall(path=tmpdir)
+                    config_file_name = os.path.join(tmpdir, 'freenas-v1.db')
+            except tarfile.ReadError:
+                bundle = False
+            conn = sqlite3.connect(config_file_name)
+            try:
+                cur = conn.cursor()
+                cur.execute(
+                    "SELECT COUNT(*) FROM south_migrationhistory WHERE app_name != 'freeadmin'"
+                )
+                new_num = cur.fetchone()[0]
+                cur.close()
+            finally:
+                conn.close()
+            conn = sqlite3.connect(FREENAS_DATABASE)
+            try:
+                cur = conn.cursor()
+                cur.execute("SELECT COUNT(*) FROM south_migrationhistory WHERE app_name != 'freeadmin'")
+                num = cur.fetchone()[0]
+                cur.close()
+            finally:
+                conn.close()
+                if new_num > num:
+                    raise CallError(
+                        'Failed to upload config, version newer than the '
+                        'current installed.'
+                    )
+        except Exception:
+            os.unlink(config_file_name)
+            raise CallError('The uploaded file is not valid.')
+
+        shutil.move(config_file_name, '/data/uploaded.db')
+        if bundle:
+            secret = os.path.join(tmpdir, 'pwenc_secret')
+            if os.path.exists(secret):
+                shutil.move(secret, self.middleware.call_sync('pwenc.file_secret_path'))
+
+        # Now we must run the migrate operation in the case the db is older
+        open(NEED_UPDATE_SENTINEL, 'w+').close()
