f34ee1886f50cf06de35917fd1ef46d23750255a
Waqar Ahmed <waqarsonic1@gmail.com>
2019-01-03 20:43:45 +0500

    Update Netdata Service
    This commit makes sure that the changes reflected in netdata port are reflected in netdata service in middlewared as well. It also improves how we read/write alarm configurations.

--- src/middlewared/middlewared/etc_files/local/netdata/alarms.py
+++ src/middlewared/middlewared/etc_files/local/netdata/alarms.py
@@ -1,26 +1,38 @@
+import os
 import re
 
 
 async def render(service, middleware):
     def update_alarms(alarms):
-        for key, alarm in alarms.items():
-
-            # These are valid alarms and their respective config files exist as well
-            if alarm.get('path'):
-                with open(alarm['path'], 'r+') as f:
+        for path, alarm_dict in alarms.items():
+            if os.path.exists(path):
+                with open(path, 'r') as f:
                     content = f.read()
-                    f.seek(0)
-                    f.write(
-                        re.sub(
-                            fr'(alarm: {key}[\s\S]*?to: )(.*)',
-                            fr'\1{"silent" if not alarm["enabled"] else "sysadmin"}',
-                            content
-                        )
-                    )
-                    f.truncate()
-            else:
-                middleware.logger.debug(f'Could not find config file for {key} alarm')
 
-    alarms = await middleware.call('netdata.list_alarms')
+                alarm = None
+                for key, alarm in alarm_dict.items():
+                    content = re.sub(
+                        fr'(alarm: {key}[\s\S]*?to: )(.*)',
+                        fr'\1{"silent" if not alarm["enabled"] else "sysadmin"}',
+                        content
+                    )
+                # Better safe then sorry
+                if alarm:
+                    with open(alarm['write_path'], 'w') as f:
+                        f.write(content)
+
+            else:
+                middleware.logger.debug(
+                    f'Could not find config file {path} for {",".join(list(alarm_dict))} alarm(s)'
+                )
+
+    listed_alarms = await middleware.call('netdata.list_alarms')
+    alarms = {}
+    # Let's not unnecessarily open a single file again and again for reading/writing
+    for key, alarm in listed_alarms.items():
+        if not alarms.get(alarm.get('read_path')):
+            alarms[alarm.get('read_path')] = {}
+
+        alarms[alarm.get('read_path')].update({key: alarm})
 
     await middleware.run_in_thread(update_alarms, alarms)
