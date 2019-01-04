2ff22904261232dfb0e15e9bb7d612cb57bc58b6
Andrew <awalker@ixsystems.com>
2019-01-04 10:00:15 -0500

    Remove leftover debug from smb plugin

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -232,7 +232,6 @@ class SharingSMBService(CRUDService):
         data['id'] = await self.middleware.call(
             'datastore.insert', self._config.datastore, data,
             {'prefix': self._config.datastore_prefix})
-        self.logger.debug("We got past datastore insert")
         await self.extend(data)  # We should do this in the insert call ?
 
         await self.middleware.call('service.reload', 'cifs')
@@ -393,7 +392,7 @@ class SharingSMBService(CRUDService):
     @private
     async def generate_vuid(self, timemachine, vuid=""):
         try:
-            if timemachine:
+            if timemachine and vuid:
                 uuid.UUID(vuid, version=4)
         except ValueError:
             self.logger.debug(f"Time machine VUID string ({vuid}) is invalid. Regenerating.")
