15d9d64f0d6be472df6534e441751301d353c482
William Grzybowski <wg@FreeBSD.org>
2019-01-09 12:11:07 -0200

    fix(middlewared/disk): try to remove disks from graid
    Ticket:	#40560

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -1416,6 +1416,22 @@ class DiskService(CRUDService):
         """
         await self.swaps_remove_disks([dev])
 
+        # Its possible a disk was previously used by graid so we need to make sure to
+        # remove the disk from it (#40560)
+        gdisk = geom.class_by_name('DISK')
+        graid = geom.class_by_name('RAID')
+        if gdisk and graid:
+            prov = gdisk.xml.find(f'.//provider[name = "{dev}"]')
+            if prov is not None:
+                provid = prov.attrib.get('id')
+                graid = graid.xml.find(f'.//consumer/provider[@ref = "{provid}"]/../../name')
+                if graid is not None:
+                    cp = await run('graid', 'remove', graid.text, dev, check=False)
+                    if cp.returncode != 0:
+                        self.logger.debug(
+                            'Failed to remove %s from %s: %s', dev, graid.text, cp.stderr.decode()
+                        )
+
         # First do a quick wipe of every partition to clean things like zfs labels
         if mode == 'QUICK':
             await self.middleware.run_in_thread(geom.scan)
