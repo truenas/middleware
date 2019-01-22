089392eced27a66bbadb12d8d39d8d4dc96328d3
William Grzybowski <wg@FreeBSD.org>
2019-01-22 13:07:31 -0200

    feat(middlewared/update): reimplement destroy_upload_location from notifier

--- src/middlewared/middlewared/plugins/update.py
+++ src/middlewared/middlewared/plugins/update.py
@@ -623,3 +623,29 @@ Changelog:
 
         shutil.chown(UPLOAD_LOCATION, 'www', 'www')
         os.chmod(UPLOAD_LOCATION, 0o755)
+
+    @private
+    def destroy_upload_location(self):
+        geom.scan()
+        klass_label = geom.class_by_name('LABEL')
+        prov = klass_label.xml.find(
+            f'.//provider[name = "label/{UPLOAD_LABEL}"]/../consumer/provider'
+        )
+        if prov is None:
+            return
+        klass_md = geom.class_by_name('MD')
+        prov = klass_md.xml.find(f'.//provider[@id = "{prov.attrib["ref"]}"]/name')
+        if prov is None:
+            return
+
+        mddev = prov.text
+
+        subprocess.run(
+            ['umount', f'/dev/label/{UPLOAD_LABEL}'], capture_output=True, check=False,
+        )
+        cp = subprocess.run(
+            ['mdconfig', '-d', '-u', mddev],
+            text=True, capture_output=True, check=False,
+        )
+        if cp.returncode != 0:
+            raise CallError(f'Could not destroy memory device: {cp.stderr}')
