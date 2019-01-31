9223ddf5b881212702c2acb4d6b2eb28030992f5
Waqar Ahmed <waqarsonic1@gmail.com>
2019-01-31 18:08:36 +0500

    Remove ix-sudoers usages
    This commit removes all ix-sudoers usages effectively deprecating it and making use of the etc plugin instead.
    Ticket: #70345

--- src/middlewared/middlewared/plugins/service.py
+++ src/middlewared/middlewared/plugins/service.py
@@ -947,7 +947,7 @@ class ServiceService(CRUDService):
     async def _reload_user(self, **kwargs):
         await self.middleware.call("etc.generate", "user")
         await self._service("ix-aliases", "start", quiet=True, **kwargs)
-        await self._service("ix-sudoers", "start", quiet=True, **kwargs)
+        await self.middleware.call('etc.generate', 'sudoers')
         await self.reload("cifs", kwargs)
 
     async def _restart_system_datasets(self, **kwargs):
