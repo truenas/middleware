54b4880cfd18514cc99b3da862bf0e76fa1a662d
Waqar Ahmed <waqarsonic1@gmail.com>
2019-02-18 17:10:55 +0500

    Bug fix for alerts
    This commit makes sure we correctly set the title and args for Multipath alert.

--- src/middlewared/middlewared/alert/source/multipaths.py
+++ src/middlewared/middlewared/alert/source/multipaths.py
@@ -9,7 +9,7 @@ class MultipathsAlertSource(AlertSource):
 
     async def check(self):
         return [
-            Alert(title=f"Multipath {mp['name']} is not optimal",
+            Alert(title="Multipath %s is not optimal",
                   args=[mp['name']])
             for mp in await self.middleware.call("multipath.query")
             if mp['status'] != "OPTIMAL"
