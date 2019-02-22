b17ac9020c509388e5d1dc68cf62c2eca9f84edc
Andrew <awalker@ixsystems.com>
2019-02-22 16:56:07 -0500

    Add support for SMB Admin Group Add dropdown menu for admin group. When this group is selected: - Add the SID of the group to S-1-5-32-544 (BUILTIN\Administrators). - Remove other SIDs that may be present in foreign memberships of S-1-5-32-544. - On response WBC_ERR_WINBIND_NOT_AVAILABLE, defer "net groupmap addmem" until after samba_server start. This give following benefits: - Allows us a user-defined configuration option that we can use for setting intelligent permissions on Homes shares and recycle bin. - Allows users in standalone / LDAP environments to use Computer Management MMC Snap-In to administer FreeNAS server. - Allows users in AD environment to select a group that is not Domain Admins to do the same. - Allows users to select a group that can administer user quotas through smbcquotas (Linux/BSD) or File Explorer (Windows)

--- src/middlewared/middlewared/plugins/service.py
+++ src/middlewared/middlewared/plugins/service.py
@@ -944,6 +944,8 @@ class ServiceService(CRUDService):
         await self.middleware.call("etc.generate", "smb")
         await self.middleware.call("etc.generate", "smb_share")
         await self._service("samba_server", "start", quiet=True, **kwargs)
+        await self.middleware.call("smb.add_admin_group", "", True)
+        await self._service("ix-post-samba", "start", quiet=True, **kwargs)
 
     async def _stop_cifs(self, **kwargs):
         await self._service("samba_server", "stop", force=True, **kwargs)
