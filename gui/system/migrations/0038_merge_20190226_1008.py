53a2f02db9f1420e5a1b365da0d0378ae6e07eb9
Ryan Moeller <ryan@freqlabs.com>
2019-02-26 10:11:08 -0800

    [system/migrations] Merge migrations inception
    Ticket: #73917

---
+++ gui/system/migrations/0038_merge_20190226_1008.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0037_reporting'),
+        ('system', '0029_merge_20190226_0917'),
+    ]
+
+    operations = [
+    ]
