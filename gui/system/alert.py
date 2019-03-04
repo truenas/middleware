aca41535e76bb39e65426bf4e811c5e936521b40
yocalebo <yocalebo@gmail.com>
2019-03-04 17:10:00 -0500

    tkt-78636: return empty list on failure for alert system

--- gui/system/alert.py
+++ gui/system/alert.py
@@ -461,8 +461,14 @@ class AlertPlugins(metaclass=HookMetaclass):
 
         try:
             with open(self.ALERT_FILE, 'rb') as f:
-                return pickle.load(f)['alerts']
+                obj = pickle.load(f)['alerts']
+                if len(obj) > 0:
+                    return obj
+                else:
+                    return []
+                    log.debug('The alert state file has no data', exc_info=True)
         except Exception:
+            return []
             log.debug('Failed to load alert state file', exc_info=True)
 
 
