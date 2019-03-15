19c9387f99a7c98a068bd98b385f721ef95111a5
William Grzybowski <wg@FreeBSD.org>
2019-03-15 16:28:03 -0300

    feat(migration): set grub config for Container VMs
    Ticket:	#78843

--- gui/vm/migrations/0009_auto_20190315_1058.py
+++ gui/vm/migrations/0009_auto_20190315_1058.py
@@ -3,6 +3,27 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from pipes import quote
+
+
+def migrate_rancher_to_grub(apps, schemaeditor):
+
+    VM = apps.get_model('vm', 'vm')
+    Volume = apps.get_model('storage', 'volume')
+
+    # Getting the first volume is a compromise to ease the migration.
+    # It wont work if the user has multiple pools and VM does not live in first one.
+    volume = Volume.objects.order_by('id')[0]
+
+    for vm in VM.objects.filter(vm_type='Container Provider'):
+        password = 'docker'
+        for device in vm.device_set.filter(dtype='RAW'):
+            if 'rootpwd' in device.attributes:
+                password = quote(device.attributes.pop('rootpwd'))
+                device.save()
+                break
+        vm.grubconfig = f'/mnt/{volume.vol_name}/.bhyve_containers/configs/{vm.id}_{vm.name}/grub/grub.cfg'
+        vm.save()
 
 
 class Migration(migrations.Migration):
@@ -16,6 +37,9 @@ class Migration(migrations.Migration):
             model_name='vm',
             name='grubconfig',
             field=models.TextField(help_text='Grub.cfg file to be used on boot.', null=True, verbose_name='Grub Config'),
+        ),
+        migrations.RunPython(
+            migrate_rancher_to_grub, reverse_code=migrations.RunPython.noop,
         ),
         migrations.RemoveField(
             model_name='vm',
