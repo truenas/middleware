ebbbc027770e7ecd5bf04843d01d1460cd892ea6
themylogin <themylogin@gmail.com>
2019-03-25 20:17:58 +0100

    Use OAuth portal for rest of cloud sync oauth providers

--- src/middlewared/middlewared/rclone/remote/yandex.py
+++ src/middlewared/middlewared/rclone/remote/yandex.py
@@ -11,5 +11,9 @@ class WebDavRcloneRemote(BaseRcloneRemote):
     rclone_type = "yandex"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True
