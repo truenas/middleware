cbac76e6dbdf63fccb26f1e47ff9d3be25ef657b
themylogin <themylogin@gmail.com>
2019-03-25 23:31:05 +0100

    Use changes from Proactive Support to middlewared

--- src/freenas/usr/local/lib/middlewared_truenas/alert/source/ixalert.py
+++ src/freenas/usr/local/lib/middlewared_truenas/alert/source/ixalert.py
@@ -4,26 +4,25 @@
 # and may not be copied and/or distributed
 # without the express permission of iXsystems.
 
-from freenasUI.system.models import Support
-
-from middlewared.alert.base import Alert, AlertLevel, ThreadedAlertSource
+from middlewared.alert.base import Alert, AlertLevel, AlertSource
 
 
-class IxAlertSource(ThreadedAlertSource):
+class IxAlertSource(AlertSource):
     level = AlertLevel.WARNING
     title = 'The Proactive Support feature is not enabled.'
 
-    def check_sync(self):
-        available, support = Support.is_available()
-        if available and support.enabled is None:
+    async def check(self):
+        support = await self.middleware.call('support.config')
+        available = await self.middleware.call('support.is_available')
+        if available and support['enabled'] is None:
             return Alert('The Proactive Support feature is not enabled. Please see the System -> Proactive Support tab.')
-        elif support.is_enabled():
+        if support['enabled']:
             # This is for people who had ix alert enabled before Proactive Support
             # feature and have not filled all the new fields.
             unfilled = []
-            for field in Support._meta.fields:
-                if not getattr(support, field.name):
-                    unfilled.append(field)
+            for name, verbose_name in await self.middleware.call('support.fields'):
+                if not support[name]:
+                    unfilled.append(verbose_name)
             if unfilled:
                 return Alert('Please fill in these fields on the System -> Proactive Support tab: %s.',
-                             args=[', '.join([str(i.verbose_name) for i in unfilled])])
+                             args=[', '.join(unfilled)])
