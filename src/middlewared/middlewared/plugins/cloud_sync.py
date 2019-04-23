fbb3a1bc5482cf731042c757e7453f3c497b986c
themylogin <themylogin@gmail.com>
2019-04-23 20:14:25 +0200

    Do not perform IO operations in main loop

--- src/middlewared/middlewared/plugins/cloud_sync.py
+++ src/middlewared/middlewared/plugins/cloud_sync.py
@@ -116,10 +116,11 @@ def get_remote_path(provider, attributes):
 
 
 async def rclone(middleware, job, cloud_sync):
-    if not middleware.run_in_thread(os.path.exists, cloud_sync["path"]):
+    if not await middleware.run_in_thread(os.path.exists, cloud_sync["path"]):
         raise CallError(f"Directory {cloud_sync['path']!r} does not exist")
 
-    if middleware.run_in_thread(os.stat, cloud_sync["path"]).st_dev == middleware.run_in_thread(os.stat, "/mnt").st_dev:
+    if (await middleware.run_in_thread(os.stat, cloud_sync["path"]).st_dev ==
+            await middleware.run_in_thread(os.stat, "/mnt").st_dev):
         raise CallError(f"Directory {cloud_sync['path']!r} must reside within volume mount point")
 
     # Use a temporary file to store rclone file
