862c5e404645464183112e92e7cb0dac526daac4
themylogin <themylogin@gmail.com>
2019-04-29 20:42:27 +0200

    Allow simply running pytest unit/plugins/...py

--- src/middlewared/middlewared/pytest/conftest.py
+++ src/middlewared/middlewared/pytest/conftest.py
@@ -2,48 +2,50 @@ import configparser
 import os
 import pytest
 
-from .client import Client
-
 from middlewared.client import Client as WSClient
 
+try:
+    from .client import Client
+except ImportError:
+    pass
+else:
+    class ConfigTarget(object):
 
-class ConfigTarget(object):
+        def __init__(self):
+            self.config = configparser.ConfigParser()
+            self.file_path = 'target.conf'
+            self.config_section = 'Target'
+            self.config.read(self.file_path)
 
-    def __init__(self):
-        self.config = configparser.ConfigParser()
-        self.file_path = 'target.conf'
-        self.config_section = 'Target'
-        self.config.read(self.file_path)
+        def target_hostname(self):
+            return os.environ.get('TEST_HOSTNAME') or self.config.get(self.config_section, 'hostname')
 
-    def target_hostname(self):
-        return os.environ.get('TEST_HOSTNAME') or self.config.get(self.config_section, 'hostname')
+        def target_api(self):
+            return os.environ.get('TEST_API') or self.config.get(self.config_section, 'api')
 
-    def target_api(self):
-        return os.environ.get('TEST_API') or self.config.get(self.config_section, 'api')
+        def target_username(self):
+            return os.environ.get('TEST_USERNAME') or self.config.get(self.config_section, 'username')
 
-    def target_username(self):
-        return os.environ.get('TEST_USERNAME') or self.config.get(self.config_section, 'username')
-
-    def target_password(self):
-        return os.environ.get('TEST_PASSWORD') or self.config.get(self.config_section, 'password')
+        def target_password(self):
+            return os.environ.get('TEST_PASSWORD') or self.config.get(self.config_section, 'password')
 
 
-class Connection(object):
+    class Connection(object):
 
-    def __init__(self):
-        self.conf = ConfigTarget()
-        self.rest = Client(
-            f'http://{self.conf.target_hostname()}',
-            self.conf.target_api(),
-            self.conf.target_username(),
-            self.conf.target_password(),
-        )
-        self.ws = WSClient(f'ws://{self.conf.target_hostname()}/websocket')
-        self.ws.call('auth.login', self.conf.target_username(), self.conf.target_password())
+        def __init__(self):
+            self.conf = ConfigTarget()
+            self.rest = Client(
+                f'http://{self.conf.target_hostname()}',
+                self.conf.target_api(),
+                self.conf.target_username(),
+                self.conf.target_password(),
+            )
+            self.ws = WSClient(f'ws://{self.conf.target_hostname()}/websocket')
+            self.ws.call('auth.login', self.conf.target_username(), self.conf.target_password())
 
-connection = Connection()
+    connection = Connection()
 
-@pytest.fixture
-def conn():
-    global connection
-    return connection
+    @pytest.fixture
+    def conn():
+        global connection
+        return connection
