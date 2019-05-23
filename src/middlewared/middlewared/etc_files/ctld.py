truenas/middleware

commit 049b8e809ada2291e76cdcad922d4dc69a4f5dc4

fix(middlewared/etc): truncate ctl.conf

src/middlewared/middlewared/etc_files/ctld.py
@@ -384,12 +384,12 @@ def main(middleware):
         addline('}\n\n')
 
     # Write out the CTL config file
-    with open(os.open(ctl_config, os.O_CREAT | os.O_WRONLY, 0o600), 'w') as fh:
+    with open(os.open(ctl_config, os.O_CREAT | os.O_WRONLY | os.TRUNC, 0o600), 'w') as fh:
         for line in cf_contents:
             fh.write(line)
 
     # Write out the CTL config file with redacted CHAP passwords
-    with open(os.open(ctl_config_shadow, os.O_CREAT | os.O_WRONLY, 0o600), 'w') as fh:
+    with open(os.open(ctl_config_shadow, os.O_CREAT | os.O_WRONLY | os.TRUNC, 0o600), 'w') as fh:
         for line in cf_contents_shadow:
             fh.write(line)
 

commit caa51c9ba959826c2dbcc354a3552a326bcf2b3c

fix(middlewared/etc): regression in 049b8e80: O_TRUNC
Ticket:	NAS-101875

src/middlewared/middlewared/etc_files/ctld.py
@@ -384,12 +384,12 @@ def main(middleware):
         addline('}\n\n')
 
     # Write out the CTL config file
-    with open(os.open(ctl_config, os.O_CREAT | os.O_WRONLY | os.TRUNC, 0o600), 'w') as fh:
+    with open(os.open(ctl_config, os.O_CREAT | os.O_WRONLY | os.O_TRUNC, 0o600), 'w') as fh:
         for line in cf_contents:
             fh.write(line)
 
     # Write out the CTL config file with redacted CHAP passwords
-    with open(os.open(ctl_config_shadow, os.O_CREAT | os.O_WRONLY | os.TRUNC, 0o600), 'w') as fh:
+    with open(os.open(ctl_config_shadow, os.O_CREAT | os.O_WRONLY | os.O_TRUNC, 0o600), 'w') as fh:
         for line in cf_contents_shadow:
             fh.write(line)
 