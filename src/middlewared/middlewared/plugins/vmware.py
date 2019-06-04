034ec4978a8ff612ef19a8ba363d3bdb5db63544
themylogin <themylogin@gmail.com>
2019-06-04 23:07:33 +0200

    Fix VMWareService.get_datastores():195 - Unknown volume type "other"

--- src/middlewared/middlewared/plugins/vmware.py
+++ src/middlewared/middlewared/plugins/vmware.py
@@ -188,8 +188,8 @@ class VMWareService(CRUDService):
                         'remote_hostnames': host_mount_info.volume.remoteHostNames,
                         'username': host_mount_info.volume.userName,
                     }
-                elif host_mount_info.volume.type == 'OTHER':
-                    # Ignore OTHER type, it does not seem to be meaningful
+                elif host_mount_info.volume.type == 'other':
+                    # Ignore other type, it does not seem to be meaningful
                     pass
                 else:
                     self.logger.debug(f'Unknown volume type "{host_mount_info.volume.type}": {host_mount_info.volume}')
