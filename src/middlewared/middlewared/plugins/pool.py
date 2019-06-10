7e5274bea2ebb24418e972f7364fd3fcad2cbffc
themylogin <themylogin@gmail.com>
2019-06-10 22:57:51 +0200

    Improve pool.filesystem_choices performance

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -214,7 +214,8 @@ class PoolService(CRUDService):
                 [
                     ('name', 'rnin', '.system'),
                     ('pool', 'in', vol_names)
-                ]
+                ],
+                {'select': ['name', 'pool']}
             )
         ]
 
