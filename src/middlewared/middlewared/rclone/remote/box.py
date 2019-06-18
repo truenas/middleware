derzahla/zvio-middleware

Use OAuth portal for Box, Dropbox, Google Drive

src/middlewared/middlewared/rclone/remote/box.py
@@ -9,5 +9,8 @@ class BoxRcloneRemote(BaseRcloneRemote):
     rclone_type = "box"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
     ]
+    credentials_oauth = True

src/middlewared/middlewared/rclone/remote/dropbox.py
@@ -9,5 +9,8 @@ class DropboxRcloneRemote(BaseRcloneRemote):
     rclone_type = "dropbox"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
     ]
+    credentials_oauth = True

src/middlewared/middlewared/rclone/remote/google_drive.py
@@ -9,7 +9,10 @@ class GoogleDriveRcloneRemote(BaseRcloneRemote):
     rclone_type = "drive"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
         Str("team_drive", title="Team Drive ID (if connecting to Team Drive)"),
     ]
+    credentials_oauth = True
     refresh_credentials = True

truenas/middleware

src/middlewared/middlewared/rclone/remote/box.py
@@ -9,5 +9,8 @@ class BoxRcloneRemote(BaseRcloneRemote):
     rclone_type = "box"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
     ]
+    credentials_oauth = True

src/middlewared/middlewared/rclone/remote/dropbox.py
@@ -9,5 +9,8 @@ class DropboxRcloneRemote(BaseRcloneRemote):
     rclone_type = "dropbox"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
     ]
+    credentials_oauth = True

src/middlewared/middlewared/rclone/remote/google_drive.py
@@ -9,7 +9,10 @@ class GoogleDriveRcloneRemote(BaseRcloneRemote):
     rclone_type = "drive"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
         Str("team_drive", verbose="Team Drive ID (if connecting to Team Drive)"),
     ]
+    credentials_oauth = True
     refresh_credentials = True