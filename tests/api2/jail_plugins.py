9b590cfc02fbda49b218eed0c32e4df9df5d562d
ericbsd <ericturgeon.bsd@gmail.com>
2019-06-18 11:47:35 -0300

    Adding tests/jail_plugin.py for Jail plugins testing
    adding activate_jail_pool, verify_jail_pool and verify_list_plugins

---
+++ tests/api2/jail_plugins.py
@@ -0,0 +1,41 @@
+
+import pytest
+import sys
+import os
+apifolder = os.getcwd()
+sys.path.append(apifolder)
+from auto_config import pool_name
+from functions import GET, POST
+
+IOCAGE_POOL = pool_name
+JOB_ID = None
+RELEASE = None
+JAIL_NAME = 'jail1'
+
+not_freenas = GET("/system/is_freenas/").json() is False
+reason = "System is not FreeNAS skip Jails test"
+to_skip = pytest.mark.skipif(not_freenas, reason=reason)
+
+
+@to_skip
+def test_01_activate_jail_pool():
+    results = POST('/jail/activate/', IOCAGE_POOL)
+    assert results.status_code == 200, results.text
+    assert results.json() is True, results.text
+
+
+@to_skip
+def test_02_verify_jail_pool():
+    results = GET('/jail/get_activated_pool/')
+    assert results.status_code == 200, results.text
+    assert results.json() == IOCAGE_POOL, results.text
+
+
+@to_skip
+def test_03_verify_list_of_instaled_plugin():
+    payload = {
+        'resource': 'PLUGIN'
+    }
+    results = POST('/jail/list_resource/', payload)
+    assert results.status_code == 200, results.text
+    assert isinstance(results.json(), list), results.text
