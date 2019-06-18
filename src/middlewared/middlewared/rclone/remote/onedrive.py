derzahla/zvio-middleware

Use OAuth portal for rest of cloud sync oauth providers

src/middlewared/middlewared/rclone/remote/onedrive.py
@@ -9,10 +9,14 @@ class OneDriveRcloneRemote(BaseRcloneRemote):
     rclone_type = "onedrive"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
         Str("drive_type", title="Drive Account Type", enum=["PERSONAL", "BUSINESS", "DOCUMENT_LIBRARY"], required=True),
         Str("drive_id", title="Drive ID", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True
 
     def get_task_extra(self, task):
         return dict(

src/middlewared/middlewared/rclone/remote/pcloud.py
@@ -9,5 +9,9 @@ class PcloudRcloneRemote(BaseRcloneRemote):
     rclone_type = "pcloud"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True

src/middlewared/middlewared/rclone/remote/yandex.py
@@ -11,5 +11,9 @@ class WebDavRcloneRemote(BaseRcloneRemote):
     rclone_type = "yandex"
 
     credentials_schema = [
+        Str("client_id", title="OAuth Client ID", default=""),
+        Str("client_secret", title="OAuth Client Secret", default=""),
         Str("token", title="Access Token", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True

truenas/middleware

src/middlewared/middlewared/rclone/remote/onedrive.py
@@ -9,10 +9,14 @@ class OneDriveRcloneRemote(BaseRcloneRemote):
     rclone_type = "onedrive"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
         Str("drive_type", verbose="Drive Account Type", enum=["PERSONAL", "BUSINESS", "DOCUMENT_LIBRARY"], required=True),
         Str("drive_id", verbose="Drive ID", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True
 
     def get_task_extra(self, task):
         return dict(

src/middlewared/middlewared/rclone/remote/pcloud.py
@@ -9,5 +9,9 @@ class PcloudRcloneRemote(BaseRcloneRemote):
     rclone_type = "pcloud"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True

src/middlewared/middlewared/rclone/remote/yandex.py
@@ -9,5 +9,9 @@ class WebDavRcloneRemote(BaseRcloneRemote):
     rclone_type = "yandex"
 
     credentials_schema = [
+        Str("client_id", verbose="OAuth Client ID", default=""),
+        Str("client_secret", verbose="OAuth Client Secret", default=""),
         Str("token", verbose="Access Token", required=True),
     ]
+    credentials_oauth = True
+    refresh_credentials = True