89727af75473a035d28ebec77926d2c872d32447
ericbsd <ericturgeon.bsd@gmail.com>
2019-06-25 16:20:45 -0300

    adding verify_available_plugin and fix 'dict' object has no attribute 'json'

--- tests/api2/jail_plugins.py
+++ tests/api2/jail_plugins.py
@@ -2,7 +2,7 @@
 import pytest
 import sys
 import os
-import sleep
+from time import sleep
 apifolder = os.getcwd()
 sys.path.append(apifolder)
 from auto_config import pool_name
@@ -14,6 +14,39 @@ job_info = None
 not_freenas = GET("/system/is_freenas/").json() is False
 reason = "System is not FreeNAS skip Jails test"
 to_skip = pytest.mark.skipif(not_freenas, reason=reason)
+
+plugins_list = [
+    'asigra',
+    'backuppc',
+    'bacula-server',
+    'bru-server',
+    'clamav',
+    'couchpotato',
+    'deluge',
+    'emby',
+    'gitlab',
+    'irssi ',
+    'jenkins',
+    'jenkins-lts',
+    'madsonic',
+    'mineos',
+    'nextcloud',
+    'plexmediaserver',
+    'plexmediaserver-plexpass',
+    'qbittorrent',
+    'quasselcore',
+    'radarr',
+    'redmine',
+    'rslsync',
+    'sonarr',
+    'subsonic',
+    'syncthing',
+    'tarsnap',
+    'transmission',
+    'weechat',
+    'xmrig',
+    'zoneminder'
+]
 
 
 @to_skip
@@ -44,13 +77,12 @@ def test_03_get_list_of_instaled_plugin_job_id():
 
 @to_skip
 def test_04_verify_instaled_plugin_job_id_is_successfull():
-    global job_info
     while True:
-        info_status = GET(f'/core/get_jobs/?id={JOB_ID}').json()[0]
-        if info_status['state'] in ('RUNNING', 'WAITING'):
+        job_info = GET(f'/core/get_jobs/?id={JOB_ID}').json()[0]
+        if job_info['state'] in ('RUNNING', 'WAITING'):
             sleep(3)
         else:
-            assert info_status['state'] == 'SUCCESS', str(info_status)
+            assert job_info['state'] == 'SUCCESS', str(job_info)
             break
 
 
@@ -71,9 +103,18 @@ def test_05_get_list_of_available_plugins_job_id():
 def test_06_verify_list_of_available_plugins_job_id_is_successfull():
     global job_info
     while True:
-        info_status = GET(f'/core/get_jobs/?id={JOB_ID}').json()[0]
-        if info_status['state'] in ('RUNNING', 'WAITING'):
+        job_info = GET(f'/core/get_jobs/?id={JOB_ID}').json()[0]
+        if job_info['state'] in ('RUNNING', 'WAITING'):
             sleep(3)
         else:
-            assert info_status['state'] == 'SUCCESS', str(info_status)
+            assert job_info['state'] == 'SUCCESS', str(job_info)
             break
+
+
+@to_skip
+@pytest.mark.parametrize('plugin', plugins_list)
+def test_07_verify_available_plugin_(plugin):
+    for plugin_info in job_info:
+        if plugin in plugin_info:
+            assert isinstance(plugin_info, list), job_info.text
+            assert plugin in plugin_info, job_info.text
