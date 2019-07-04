b89533bededfb1ea87dedb4febc91ce7cafbd4a3
ericbsd <ericturgeon.bsd@gmail.com>
2019-07-04 11:16:18 -0300

    change get_list_of_instaled_plugin to use GET plugin
    change test 04 to verify plugin repos url is in official repositories
    change test 05 to use plugin/available

--- tests/api2/jail_plugins.py
+++ tests/api2/jail_plugins.py
@@ -14,6 +14,7 @@ job_results = None
 not_freenas = GET("/system/is_freenas/").json() is False
 reason = "System is not FreeNAS skip Jails test"
 to_skip = pytest.mark.skipif(not_freenas, reason=reason)
+plugin_repos = 'https://github.com/freenas/iocage-ix-plugins.git'
 
 plugins_list = [
     'asigra',
@@ -71,36 +72,24 @@ def test_02_verify_jail_pool():
 
 @to_skip
 def test_03_get_list_of_instaled_plugin_job_id():
-    global JOB_ID
-    payload = {
-        'resource': 'PLUGIN'
-    }
-    results = POST('/jail/list_resource/', payload)
+    results = GET('/plugin/')
     assert results.status_code == 200, results.text
-    assert isinstance(results.json(), int), results.text
-    JOB_ID = results.json()
+    assert isinstance(results.json(), list), results.text
 
 
 @to_skip
-def test_04_verify_instaled_plugin_job_id_is_successfull():
-    while True:
-        job_results = GET(f'/core/get_jobs/?id={JOB_ID}')
-        job_state = job_results.json()[0]['state']
-        if job_state in ('RUNNING', 'WAITING'):
-            sleep(3)
-        else:
-            assert job_state == 'SUCCESS', str(job_results)
-            break
+def test_04_verify_plugin_repos_is_in_official_repositories():
+    results = GET('/plugin/official_repositories/')
+    assert plugin_repos in results.json(), results.text
 
 
 @to_skip
 def test_05_get_list_of_available_plugins_job_id():
     global JOB_ID
     payload = {
-        'resource': 'PLUGIN',
-        "remote": True
+        "plugin_repository": plugin_repos
     }
-    results = POST('/jail/list_resource/', payload)
+    results = POST('/plugin/available/', payload)
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), int), results.text
     JOB_ID = results.json()
