1322ccbf7c5e60af7ce340fbd7a638efa8d04472
Waqar Ahmed <waqarsonic1@gmail.com>
2019-07-22 11:11:25 +0500

    Normalize size migration
    This commit adds a migration to normalize size attribute of vm raw devices which right now represents GB's and we normalize it to show bytes.

--- gui/vm/migrations/0010_normalize_size.py
+++ gui/vm/migrations/0010_normalize_size.py
@@ -1,13 +1,12 @@
-import math
-
 from django.db import migrations
 
 
 def convert_size(apps, schema_editor):
     device_model = apps.get_model('vm', 'Device')
     for device in device_model.objects.filter(dtype='RAW'):
-        if isinstance(device.attributes.get('size') or 0, str):
-            size = original_value = device.attributes['size']
+        size = device.attributes.get('size')
+        if not isinstance(size, int):
+            size = original_value = str(device.attributes.get('size') or 1)
             suffix = original_value[-1]
             if not suffix.isdigit():
                 size = original_value[:-1]
@@ -19,14 +18,23 @@ def convert_size(apps, schema_editor):
                 size = 1
 
             if suffix == 'T':
-                size *= 1024
+                size *= (1024 * 1024 * 1024 * 1024)
+            elif suffix == 'G' or suffix.isdigit():
+                # suffix would be a digit in cases where we only have say "14" saved in db
+                size *= (1024 * 1024 * 1024)
             elif suffix == 'M':
-                size = math.ceil(size / 1024)
+                size *= (1024 * 1024)
 
             # If we have a suffix other then T/G/B, we disregard it completely following how vm plugin does it
             # and consider size to be in GB now
-            device.attributes['size'] = size
-            device.save()
+        else:
+            # If it's an integer already, it's in GB
+            size = size or 1
+            size *= (1024 * 1024 * 1024)
+
+        # At this stage size has been converted to bytes
+        device.attributes['size'] = size
+        device.save()
 
 
 class Migration(migrations.Migration):
