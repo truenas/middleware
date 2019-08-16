b5bf2bb462f76d7ffe5e6ad6b4f4963b5b4b177a
William Grzybowski <william@grzy.org>
2019-08-16 15:13:26 -0300

    fix(gui): cleanup django settings

--- gui/settings.py
+++ gui/settings.py
@@ -35,17 +35,8 @@ sys.path.append('/usr/local/lib')
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
-#DEBUG = True
-#TASTYPIE_FULL_DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-LOGIN_REDIRECT_URL = '/legacy/'
-LOGIN_URL = '/legacy/account/login/'
-LOGOUT_URL = '/legacy/account/logout/'
 
-ADMINS = (
-)
-
-MANAGERS = ADMINS
+MANAGERS = ADMINS = ()
 
 DATABASE_PATH = os.environ.get('DATABASE_ROOT', '/data') + '/freenas-v1.db'
 
@@ -78,87 +69,11 @@ if os.path.exists(DATABASE_PATH) and os.environ.get('FREENAS_INSTALL', '').lower
     if mode != 0o640:
         os.chmod(DATABASE_PATH, 0o640)
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = None
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(HERE, 'media')
-MEDIA_URL = '/legacy/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(HERE, "static")
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/legacy/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(HERE, "fnstatic"),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [
-        os.path.join(HERE, 'templates'),
-    ],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.i18n',
-            'django.template.context_processors.media',
-            'django.template.context_processors.static',
-        ],
-        'builtins': [
-            'django.contrib.staticfiles.templatetags.staticfiles',
-            'django.templatetags.i18n',
-        ],
-    },
-}]
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    #'freenasUI.freeadmin.middleware.ProfileMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'freenasUI.freeadmin.middleware.LocaleMiddleware',
-    'freenasUI.freeadmin.middleware.CatchError',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'freenasUI.freeadmin.middleware.AuthTokenMiddleware',
-    'freenasUI.freeadmin.middleware.RequireLoginMiddleware',
-)
 
 ROOT_URLCONF = 'freenasUI.urls'
 
@@ -170,8 +85,6 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
 DIR_BLACKLIST = [
     '__pycache__',
-    'templates',
-    'fnstatic',
     'middleware',
     'contrib',
     'common',
@@ -206,22 +119,10 @@ BLACKLIST_NAV = (
     'freeadmin',
 )
 
-FORCE_SCRIPT_NAME = ''
-
-#TODO: Revisit me against cache posion attacks
-#      Maybe offer as an option in the GUI
-ALLOWED_HOSTS = ['*']
-
 AUTH_USER_MODEL = 'account.bsdUsers'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'freenasUI.middleware.auth.AuthTokenBackend',
 ]
-
-TASTYPIE_DEFAULT_FORMATS = ['json']
-
-FILE_UPLOAD_MAX_MEMORY_SIZE = 33554432
-FILE_UPLOAD_TEMP_DIR = "/var/tmp/firmware/"
 
 # Do not set up logging if its being imported from middlewared
 if 'MIDDLEWARED' in os.environ:
