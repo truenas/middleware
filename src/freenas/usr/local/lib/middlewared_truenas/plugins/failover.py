ac2c191782fa905dd7579be3f3d69bd193717661
William Grzybowski <william@grzy.org>
2019-08-26 15:48:41 -0300

    feat(middlewared/failover): mode LocalEscrewCtl to failover plugin

--- src/freenas/usr/local/lib/middlewared_truenas/plugins/failover.py
+++ src/freenas/usr/local/lib/middlewared_truenas/plugins/failover.py
@@ -39,10 +39,97 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freenasUI.settings')
 import django
 django.setup()
 from freenasUI.freeadmin.sqlite3_ha.base import Journal
-from freenasUI.failover.enc_helper import LocalEscrowCtl
 
+BUFSIZE = 256
 INTERNAL_IFACE_NF = '/tmp/.failover_internal_iface_not_found'
 SYNC_FILE = '/var/tmp/sync_failed'
+
+
+class LocalEscrowCtl:
+    def __init__(self):
+        server = "/tmp/escrowd.sock"
+        connected = False
+        retries = 5
+
+        # Start escrowd on demand
+        #
+        # Attempt to connect the server;
+        # if connection can not be established, startescrowd and
+        # retry.
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            sock.connect(server)
+            connected = True
+        except Exception:
+            subprocess.Popen(["/usr/sbin/escrowd"])
+            while retries > 0 and connected is False:
+                try:
+                    retries = retries - 1
+                    sock.connect(server)
+                    connected = True
+                except Exception:
+                    time.sleep(1)
+
+        # TODO
+        if not connected:
+            print("FATAL: Can't connect to escrowd")
+            sys.exit(1)
+
+        data = sock.recv(BUFSIZE).decode()
+        if data != "220 Ready, go ahead\n":
+            print("FATAL: server didn't send welcome message, exiting")
+            sys.exit(2)
+        self.sock = sock
+
+    # Set key on local escrow daemon.
+    def setkey(self, passphrase):
+        command = "SETKEY %s\n" % (passphrase)
+        self.sock.sendall(command.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+        return (data == "250 setkey accepted.\n")
+        # Push the key to remote.
+
+    # Clear key on local escrow daemon.
+    def clear(self):
+        command = "CLEAR"
+        self.sock.sendall(command.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+        succeeded = (data == "200 clear succeeded.\n")
+        open('/tmp/.failover_needop', 'w')
+        return (succeeded)
+
+    # Shutdown local escrow daemon.
+    def shutdown(self):
+        command = "SHUTDOWN"
+        self.sock.sendall(command.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+        return (data == "250 Shutting down.\n")
+
+    # Get key from local escrow daemon.  Returns None if not available.
+    def getkey(self):
+        command = "REVEAL"
+        self.sock.sendall(command.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+        lines = data.split('\n')
+        if lines[0] == "404 No passphrase present":
+            return None
+        elif lines[0] == "200 Approved":
+            if len(lines) > 2:
+                data = lines[1]
+            else:
+                data = self.sock.recv(BUFSIZE).decode()
+                data = data.split('\n')[0]
+            return data
+        else:
+            # Should never happen.
+            return None
+
+    # Get status of local escrow daemon.  True -- Have key; False -- No key.
+    def status(self):
+        command = "STATUS"
+        self.sock.sendall(command.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+        return (data == "200 keyd\n")
 
 
 class TruenasNodeSessionManagerCredentials(SessionManagerCredentials):
