e96980cc7ff8ea7648841978d5b06ba02f96c544
Michael Fayez <michaeleino@users.noreply.github.com>
2019-09-04 18:26:08 +0200

    Update telegram.py
    fix a missing comma

--- src/middlewared/middlewared/alert/service/telegram.py
+++ src/middlewared/middlewared/alert/service/telegram.py
@@ -20,7 +20,7 @@ class TelegramAlertService(ThreadedAlertService):
         url = self.attributes["base_url"] + {self.attributes['TELEGRAM_BOT_TOKEN']} + '/sendMessage'
         post_data = {"chat_id": {self.attributes['TELEGRAM_CHAT_ID']}, "text": self._format_alerts(alerts, gone_alerts, new_alerts), "parse_mode": "Markdown"}
         r = requests.post(
-            url, data=post_data
+            url, data=post_data,
             timeout=15,
         )
         r.raise_for_status()
