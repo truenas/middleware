f9b515ee57e382b8ff863f8d294494e4725cf586
Michael Fayez <michaeleino@users.noreply.github.com>
2019-09-04 19:09:39 +0200

    Update telegram.py
    fix line breaks

--- src/middlewared/middlewared/alert/service/telegram.py
+++ src/middlewared/middlewared/alert/service/telegram.py
@@ -15,12 +15,8 @@ class TelegramAlertService(ThreadedAlertService):
         strict=True,
     )
 
-    
     def send_sync(self, alerts, gone_alerts, new_alerts):
         url = self.attributes["base_url"] + {self.attributes['TELEGRAM_BOT_TOKEN']} + '/sendMessage'
         post_data = {"chat_id": {self.attributes['TELEGRAM_CHAT_ID']}, "text": self._format_alerts(alerts, gone_alerts, new_alerts), "parse_mode": "Markdown"}
-        r = requests.post(
-            url, data=post_data,
-            timeout=15,
-        )
+        r = requests.post(url, data=post_data, timeout=15)
         r.raise_for_status()
