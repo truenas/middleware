08d8bd71a362c70080e5c8c2c4cd0923ed9d0198
Andrew <awalker@ixsystems.com>
2019-09-19 09:51:34 -0400

    switch to present tense for `use_default_domain` comment
    Co-Authored-By: Aaron <32651598+a-stjohn@users.noreply.github.com>

--- src/middlewared/middlewared/plugins/activedirectory.py
+++ src/middlewared/middlewared/plugins/activedirectory.py
@@ -698,7 +698,7 @@ class ActiveDirectoryService(ConfigService):
 
         `verbose_logging` increase logging during the domain join process.
 
-        `use_default_domain` controls whether domain users and groups will have
+        `use_default_domain` controls whether domain users and groups have
         the pre-windows 2000 domain name prepended to the user account. When
         enabled, the user will appear as "administrator" rather than
         "EXAMPLE\administrator"
