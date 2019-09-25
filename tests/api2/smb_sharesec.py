2fe1f71ca33753ab7bfd8baea974f0149f50349a
ericbsd <ericturgeon.bsd@gmail.com>
2019-09-25 17:19:11 -0300

    addint test to verify POST smb/sharesec
     adding test to verify output from put smb/sharesec
     adding test to verify output from get smb/sharesec by id

--- tests/api2/smb_sharesec.py
+++ tests/api2/smb_sharesec.py
@@ -1,10 +1,11 @@
 
+import pytest
 import sys
 import os
 apifolder = os.getcwd()
 sys.path.append(apifolder)
 from functions import PUT, POST, GET, DELETE, SSH_TEST
-from auto_config import ip, pool_name, password, user
+from auto_config import pool_name, user, password, ip
 
 share_name = "my_sharesec"
 dataset = f"{pool_name}/smb-sharesec"
@@ -52,14 +53,16 @@ def test_05_get_sharesec_id_with_share_name():
     assert isinstance(results.json(), list), results.text
     assert isinstance(results.json()[0], dict), results.text
     sharesec_id = results.json()[0]['id']
+    print(results.text)
 
 
 def test_06_set_smb_sharesec_setacl():
+    global payload
     payload = {
         'share_name': share_name,
         'share_acl': [
             {
-                'ae_who_sid': 'S-1-5-32-544',
+                'ae_who_sid': 'S-1-5-32-545',
                 'ae_perm': 'FULL',
                 'ae_type': 'ALLOWED'
             }
@@ -69,7 +72,22 @@ def test_06_set_smb_sharesec_setacl():
     assert results.status_code == 200, results.text
 
 
-def test_07_set_smb_sharesec_update():
+def test_07_get_smb_sharesec_by_id():
+    global results
+    results = GET(f"/smb/sharesec/id/{sharesec_id}")
+    assert results.status_code == 200, results.text
+    assert isinstance(results.json(), dict), results.text
+    print(results.text)
+
+
+@pytest.mark.parametrize('ae', ['ae_who_sid', 'ae_perm', 'ae_type'])
+def test_08_verify_smb_sharesec_(ae):
+    ae_result = results.json()['share_acl'][0][ae]
+    assert ae_result == payload['share_acl'][0][ae], results.text
+
+
+def test_09_change_smb_sharesec_update():
+    global payload, results
     payload = {
         'share_acl': [
             {
@@ -84,13 +102,27 @@ def test_07_set_smb_sharesec_update():
     assert isinstance(results.json(), dict), results.text
 
 
-def test_08_get_smb_sharesec_by_id():
+@pytest.mark.parametrize('ae', ['ae_who_sid', 'ae_perm', 'ae_type'])
+def test_10_verify_output_from_put_smb_sharesec_(ae):
+    ae_result = results.json()['share_acl'][0][ae]
+    assert ae_result == payload['share_acl'][0][ae], results.text
+
+
+def test_11_get_smb_sharesec_by_id():
+    global results
     results = GET(f"/smb/sharesec/id/{sharesec_id}")
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
+    print(results.text)
 
 
-def test_09_get_smb_sharesec_getacl():
+@pytest.mark.parametrize('ae', ['ae_who_sid', 'ae_perm', 'ae_type'])
+def test_12_verify_get_smb_sharesec_(ae):
+    ae_result = results.json()['share_acl'][0][ae]
+    assert ae_result == payload['share_acl'][0][ae], results.text
+
+
+def test_13_get_smb_sharesec_getacl():
     payload = {
         "share_name": share_name,
         "options": {
@@ -102,27 +134,27 @@ def test_09_get_smb_sharesec_getacl():
     assert isinstance(results.json(), dict), results.text
 
 
-def test_10_get_smb_sharesec_by_id():
+def test_14_get_smb_sharesec_by_id():
     results = GET(f"/smb/sharesec/synchronize_acls/")
     assert results.status_code == 200, results.text
 
 
-def test_10_delete_share_acl():
+def test_15_delete_share_acl():
     results = DELETE(f"/smb/sharesec/id/{sharesec_id}")
     assert results.status_code == 200, results.text
 
 
-def test_11_starting_cifs_service():
+def test_16_starting_cifs_service():
     payload = {"service": "cifs", "service-control": {"onetime": True}}
     results = POST("/service/stop/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_12_delete_cifs_share():
+def test_17_delete_cifs_share():
     results = DELETE(f"/sharing/smb/id/{smb_id}")
     assert results.status_code == 200, results.text
 
 
-def test_13_destroying_smb_sharesec_dataset():
+def test_18_destroying_smb_sharesec_dataset():
     results = DELETE(f"/pool/dataset/id/{dataset_url}/")
     assert results.status_code == 200, results.text
