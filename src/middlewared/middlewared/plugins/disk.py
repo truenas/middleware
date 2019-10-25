6e4e03bfa67d1d4017d19a956bd2ac9aa426a07b
themylogin <themylogin@gmail.com>
2019-10-25 12:02:31 +0200

    Example of platform-dependent part

--- src/middlewared/middlewared/plugins/disk.py
+++ src/middlewared/middlewared/plugins/disk.py
@@ -639,46 +639,6 @@ class DiskService(CRUDService):
         self.geli_attach_single(devname, keypath, passphrase)
         return f'{devname}.eli'
 
-    @accepts(
-        List('devices', items=[Str('device')]),
-        Str('passphrase', null=True, default=None, private=True),
-    )
-    @job(pipes=['input'])
-    def decrypt(self, job, devices, passphrase=None):
-        """
-        Decrypt `devices` using uploaded encryption key
-        """
-        with tempfile.NamedTemporaryFile(dir='/tmp/') as f:
-            os.chmod(f.name, 0o600)
-            f.write(job.pipes.input.r.read())
-            f.flush()
-
-            if passphrase:
-                passf = tempfile.NamedTemporaryFile(mode='w+', dir='/tmp/')
-                os.chmod(passf.name, 0o600)
-                passf.write(passphrase)
-                passf.flush()
-                passphrase = passf.name
-
-            failed = []
-            for dev in devices:
-                try:
-                    self.middleware.call_sync(
-                        'disk.geli_attach_single',
-                        dev,
-                        f.name,
-                        passphrase,
-                    )
-                except Exception:
-                    failed.append(dev)
-
-            if passphrase:
-                passf.close()
-
-            if failed:
-                raise CallError(f'The following devices failed to attach: {", ".join(failed)}')
-        return True
-
     @private
     async def get_reserved(self):
         reserved = list(await self.middleware.call('boot.get_disks'))
