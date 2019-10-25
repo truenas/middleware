89439f942cafd0475edcef2ecfb198d2ca59b420
ericbsd <ericturgeon.bsd@gmail.com>
2019-10-25 16:19:02 -0300

    change disk testing to test all diks in a NAS
    remove defautl disk imports from disk testing

--- tests/api2/disk.py
+++ tests/api2/disk.py
@@ -3,104 +3,61 @@
 # Author: Eric Turgeon
 # License: BSD
 
+import pytest
 import sys
 import os
-
+import re
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from auto_config import disk1, disk2
 from functions import GET, POST, PUT
-
-disk0 = disk1[:-1] + '0'
 RunTest = True
 TestName = "get disk information"
 DISK_ID = None
+
+disk_list = list(POST('/device/get_info/', 'DISK').json().keys())
 
 
 def test_01_verifying_that_the_installer_created_all_disk():
     results = GET('/disk/')
     assert results.status_code == 200, results.text
-    assert len(results.json()) == 3
+    assert len(results.json()) == len(disk_list)
 
 
-def test_02_looking_for_disk0_was_created():
+@pytest.mark.parametrize('disk', disk_list)
+def test_02_looking_for_disk(disk):
     results = GET('/disk/')
     assert results.status_code == 200, results.text
-    assert results.json()[0]['name'] == disk0
+    assert results.json()[0]['name'] == disk
 
 
-def test_03_looking_disk0_subsystem():
-    results = GET('/disk?number=0')
+@pytest.mark.parametrize('disk', disk_list)
+def test_03_looking_subsystem(disk):
+    results = GET(f'/disk?name={disk}')
     assert results.status_code == 200, results.text
-    assert results.json()[0]['subsystem'] == disk0[:-1]
+    assert results.json()[0]['subsystem'] == disk[:-1]
 
 
-def test_04_looking_disk0_number():
-    results = GET('/disk?number=0')
+@pytest.mark.parametrize('disk', disk_list)
+def test_04_looking_number(disk):
+    results = GET(f'/disk?name={disk}')
     assert results.status_code == 200, results.text
-    assert results.json()[0]['number'] == 0
+    assert results.json()[0]['number'] == re.sub("\D", "", disk)
 
 
-def test_05_looking_disk0_identifier_and_serial_match():
-    results = GET('/disk?number=0')
+@pytest.mark.parametrize('disk', disk_list)
+def test_05_looking_disk0_identifier_and_serial_match(disk):
+    results = GET(f'/disk?name={disk}')
     assert results.status_code == 200, results.text
     identifier_serial = results.json()[0]['identifier'][8:]
     serial = results.json()[0]['serial']
     assert identifier_serial == serial
 
 
-def test_06_looking_for_disk1_was_created():
+def test_06_get_for_disk1_id():
     global DISK_ID
-    results = GET('/disk?number=1')
+    results = GET(f'/disk?name={disk_list[1]}')
     assert results.status_code == 200, results.text
     DISK_ID = results.json()[0]['identifier']
-    assert results.json()[0]['name'] == disk1, results.text
-
-
-def test_07_looking_disk1_subsystem():
-    results = GET('/disk?number=1')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['subsystem'] == disk1[:-1]
-
-
-def test_08_looking_disk1_number():
-    results = GET('/disk?number=1')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['number'] == 1
-
-
-def test_09_looking_disk1_identifier_and_serial_match():
-    results = GET('/disk?number=1')
-    assert results.status_code == 200, results.text
-    identifier_serial = results.json()[0]['identifier'][8:]
-    serial = results.json()[0]['serial']
-    assert identifier_serial == serial, results.text
-
-
-def test_10_looking_for_disk2_was_created():
-    results = GET('/disk?number=2')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['name'] == disk2
-
-
-def test_11_looking_disk2_subsystem():
-    results = GET('/disk?number=2')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['subsystem'] == disk2[:-1]
-
-
-def test_12_looking_disk2_number():
-    results = GET('/disk?number=2')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['number'] == 2
-
-
-def test_13_looking_disk2_identifier_and_serial_match():
-    results = GET('/disk?number=2')
-    assert results.status_code == 200, results.text
-    identifier_serial = results.json()[0]['identifier'][8:]
-    serial = results.json()[0]['serial']
-    assert identifier_serial == serial
 
 
 def test_14_update_disk_description():
