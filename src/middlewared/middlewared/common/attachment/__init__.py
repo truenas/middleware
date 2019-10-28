d9f83715bc86ceca9a89a2959b65962101e666d5
themylogin <themylogin@gmail.com>
2019-10-28 19:07:39 +0100

    Document FSAttachmentDelegate

--- src/middlewared/middlewared/common/attachment/__init__.py
+++ src/middlewared/middlewared/common/attachment/__init__.py
@@ -2,21 +2,52 @@ from middlewared.service import ServiceChangeMixin
 
 
 class FSAttachmentDelegate(ServiceChangeMixin):
+    """
+    Represents something (share, automatic task, etc.) that needs to be enabled or disabled when dataset
+    becomes available or unavailable (due to import/export, encryption/decryption, etc.)
+    """
+
+    # Unique identifier among all FSAttachmentDelegate classes
     name = NotImplementedError
+    # Human-readable name of item handled by this delegate (e.g. "NFS Share")
     title = NotImplementedError
+    # If is not None, corresponding service will be restarted after performing tasks on item
     service = None
 
     def __init__(self, middleware):
         self.middleware = middleware
 
     async def query(self, path, enabled):
+        """
+        Lists enabled/disabled items that depend on a dataset
+        :param path: mountpoint of the dataset (e.g. "/mnt/tank/work")
+        :param enabled: whether to list enabled or disabled items
+        :return: list of items of arbitrary type (will be passed to other methods of this class)
+        """
         raise NotImplementedError
 
     async def get_attachment_name(self, attachment):
+        """
+        Returns human-readable description of item (e.g. it's path). Will be combined with `cls.title`.
+        I.e. if you return here `/mnt/tank/work`, user will see: `NFS Share "/mnt/tank/work"`
+        :param attachment: one of the items returned by `query`
+        :return: string described above
+        """
         raise NotImplementedError
 
     async def delete(self, attachments):
+        """
+        Permanently delete said items
+        :param attachments: list of the items returned by `query`
+        :return: None
+        """
         raise NotImplementedError
 
     async def toggle(self, attachments, enabled):
+        """
+        Enable or disable said items
+        :param attachments: list of the items returned by `query`
+        :param enabled:
+        :return:
+        """
         raise NotImplementedError
