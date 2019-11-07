8eedcb3eda4993a2aeec4731334b18d76922d81d
yocalebo <yocalebo@gmail.com>
2019-11-07 11:23:54 -0500

    dont run nfs bind alerts on standby controller

--- src/middlewared/middlewared/alert/source/nfs_bindaddr.py
+++ src/middlewared/middlewared/alert/source/nfs_bindaddr.py
@@ -5,4 +5,6 @@ class NFSBindAlertSource(FilePresenceAlertSource):
     level = AlertLevel.WARNING
     title = "NFS Services Could Not Bind to Specific IP Addresses, Using 0.0.0.0"
 
+    run_on_backup_node = False
+
     path = "/tmp/.nfsbindip_notfound"
