ce0a25490f1feff58fc60aa8ed9579525e4315f9
caleb <yocalebo@gmail.comm>
2019-11-07 12:50:01 -0500

    use correct class

--- src/middlewared/middlewared/alert/source/nfs_bindaddr.py
+++ src/middlewared/middlewared/alert/source/nfs_bindaddr.py
@@ -1,4 +1,4 @@
-from middlewared.alert.base import AlertSource, AlertClass, AlertCategory, AlertLevel, FilePresenceAlertSource
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, FilePresenceAlertSource
 
 
 class NFSBindAlertClass(AlertClass):
@@ -7,8 +7,9 @@ class NFSBindAlertClass(AlertClass):
     title = "NFS Services Could Not Bind to Specific IP Addresses, Using 0.0.0.0"
     text = "NFS services could not bind to specific IP addresses, using 0.0.0.0."
 
-    run_on_backup_node = False
 
 class NFSBindAlertSource(FilePresenceAlertSource):
     path = "/tmp/.nfsbindip_notfound"
     klass = NFSBindAlertClass
+
+    run_on_backup_node = False
