79f7b7a43da760c5cf93409a7875686b76d69ea4
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-11-12 23:00:48 +0500

    Add KMIP service

--- src/middlewared/middlewared/plugins/kmip.py
+++ src/middlewared/middlewared/plugins/kmip.py
@@ -1,3 +1,6 @@
+from middlewared.service import accepts, Bool, ConfigService, Dict, Int, private, Str, ValidationErrors
+from middlewared.validators import Port
+
 import middlewared.sqlalchemy as sa
 
 
@@ -12,3 +15,64 @@ class KMIPModel(sa.Model):
     manage_sed_disks = sa.Column(sa.Boolean(), default=False)
     manage_zfs_keys = sa.Column(sa.Boolean(), default=False)
     enabled = sa.Column(sa.Boolean(), default=False)
+
+
+class KMIPService(ConfigService):
+
+    class Config:
+        datastore = 'system_kmip'
+        datastore_extend = 'kmip.kmip_extend'
+
+    @private
+    async def kmip_extend(self, data):
+        for k in filter(lambda v: data[v], ('certificate', 'certificate_authority')):
+            data[k] = data[k]['id']
+        return data
+
+    @accepts(
+        Dict(
+            'kmip_update',
+            Bool('enabled'),
+            Bool('manage_sed_disks'),
+            Bool('manage_zfs_keys'),
+            Int('certificate', null=True),
+            Int('certificate_authority', null=True),
+            Int('port', validators=[Port()]),
+            Str('server'),
+        )
+    )
+    async def do_update(self, data):
+        old = await self.config()
+        new = old.copy()
+        new.update(data)
+        verrors = ValidationErrors()
+
+        if not new['server']:
+            verrors.add('kmip_update.server', 'Please specify a valid hostname or an IPv4 address')
+
+        verrors.extend((await self.middleware.call(
+            'certificate.cert_services_validation', new['certificate'], 'kmip_update.certificate', False
+        )))
+
+        ca = await self.middleware.call('certificateauthority.query', [['id', '=', new['certificate_authority']]])
+        if ca and not verrors:
+            ca = ca[0]
+            if not await self.middleware.call(
+                'cryptokey.validate_cert_with_chain',
+                (await self.middleware.call('certificate._get_instance', new['certificate']))['certificate'],
+                [ca['certificate']]
+            ):
+                verrors.add(
+                    'kmip_update.certificate_authority',
+                    'Certificate chain could not be verified with specified certificate authority.'
+                )
+        elif not ca:
+            verrors.add('kmip_update.certificate_authority', 'Please specify a valid id.')
+
+        verrors.check()
+
+        await self.middleware.call(
+            'datastore.update', self._config.datastore, old['id'], new,
+        )
+
+        return await self.config()
