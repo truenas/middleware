a69a36e406ae74273212b4adf31e2667ed2d3303
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-11-26 22:13:10 +0500

    Add migration to disable interfaces capabilitie if asigra migration is pending

--- gui/network/migrations/0011_disable_offload_capabilities.py
+++ gui/network/migrations/0011_disable_offload_capabilities.py
@@ -1,6 +1,17 @@
 from django.db import migrations, models
 
 
+def disable_capabilities(apps, schema_editor):
+    asigra = apps.get_model('services', 'asigra').objects.all()
+    if asigra:
+        asigra = asigra[0]
+        if asigra.filesystem:
+            # Disable capabilities for all interfaces
+            for nic in apps.get_model('network', 'interfaces').objects.all():
+                nic.int_disable_offload_capabilities = True
+                nic.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -13,4 +24,7 @@ class Migration(migrations.Migration):
             name='int_disable_offload_capabilities',
             field=models.BooleanField(default=False, verbose_name='Disable offload capabilities'),
         ),
+        migrations.RunPython(
+            disable_capabilities,
+        )
     ]
