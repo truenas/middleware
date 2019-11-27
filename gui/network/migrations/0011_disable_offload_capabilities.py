7cbcba7d98c2226ad7ace0f3f92f426a2a8c3a90
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-11-27 19:46:12 +0500

    Correct migration for disabling capabilities

--- gui/network/migrations/0011_disable_offload_capabilities.py
+++ gui/network/migrations/0011_disable_offload_capabilities.py
@@ -1,22 +1,10 @@
 from django.db import migrations, models
 
 
-def disable_capabilities(apps, schema_editor):
-    asigra = apps.get_model('services', 'asigra').objects.all()
-    if asigra:
-        asigra = asigra[0]
-        if asigra.filesystem:
-            # Disable capabilities for all interfaces
-            for nic in apps.get_model('network', 'interfaces').objects.all():
-                nic.int_disable_offload_capabilities = True
-                nic.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ('network', '0010_auto_20181114_1129'),
-        ('services', '0037_remove_netdata'),
     ]
 
     operations = [
@@ -25,7 +13,4 @@ class Migration(migrations.Migration):
             name='int_disable_offload_capabilities',
             field=models.BooleanField(default=False, verbose_name='Disable offload capabilities'),
         ),
-        migrations.RunPython(
-            disable_capabilities,
-        )
     ]
