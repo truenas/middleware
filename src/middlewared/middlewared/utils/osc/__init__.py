37254750f87661176d8ae7a0e1558658182034e9
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-12-03 22:10:06 +0500

    Expose method to identify system platform

--- src/middlewared/middlewared/utils/osc/__init__.py
+++ src/middlewared/middlewared/utils/osc/__init__.py
@@ -1,8 +1,12 @@
 import platform
 
+platform_name = None
+
 if platform.system().lower() == "freebsd":
     from .freebsd import *  # noqa
+    platform_name = 'FREEBSD'
 
 
 if platform.system().lower() == "linux":
     from .linux import *  # noqa
+    platform_name = 'LINUX'
