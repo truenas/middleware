9395247e2db3a1bfdc3605a52266d86d0087043e
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-12-05 22:14:43 +0500

    Correctly retrieve disk identifier with gpt partition uuids

--- src/middlewared/middlewared/plugins/disk_/identify_freebsd.py
+++ src/middlewared/middlewared/plugins/disk_/identify_freebsd.py
@@ -34,11 +34,10 @@ class DiskService(Service, DiskIdentifyBase):
 
         klass = geom.class_by_name('PART')
         if klass:
-            for g in klass.geoms:
+            for g in filter(lambda v: v.name == name, klass.geoms):
                 for p in g.providers:
-                    if p.name == name:
-                        if p.config['rawtype'] == RAWTYPE['freebsd-zfs']:
-                            return f'{{uuid}}{p.config["rawuuid"]}'
+                    if p.config['rawtype'] == RAWTYPE['freebsd-zfs']:
+                        return f'{{uuid}}{p.config["rawuuid"]}'
 
         g = geom.geom_by_name('LABEL', name)
         if g:
