82af31854816b624a55b6c0751b542f3d24fc1cb
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-12-09 16:31:10 +0500

    Separate removing disk from graid from wiping disk

--- src/middlewared/middlewared/plugins/disk_/wipe_freebsd.py
+++ src/middlewared/middlewared/plugins/disk_/wipe_freebsd.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import platform
 import re
 import signal
 import subprocess
@@ -32,21 +33,9 @@ class DiskService(Service, WipeDiskBase):
     async def wipe(self, job, dev, mode, sync):
         await self.middleware.call('disk.swaps_remove_disks', [dev])
 
-        # Its possible a disk was previously used by graid so we need to make sure to
-        # remove the disk from it (#40560)
-        gdisk = geom.class_by_name('DISK')
-        graid = geom.class_by_name('RAID')
-        if gdisk and graid:
-            prov = gdisk.xml.find(f'.//provider[name = "{dev}"]')
-            if prov is not None:
-                provid = prov.attrib.get('id')
-                graid = graid.xml.find(f'.//consumer/provider[@ref = "{provid}"]/../../name')
-                if graid is not None:
-                    cp = await run('graid', 'remove', graid.text, dev, check=False)
-                    if cp.returncode != 0:
-                        self.logger.debug(
-                            'Failed to remove %s from %s: %s', dev, graid.text, cp.stderr.decode()
-                        )
+        # FIXME: Please implement removal from graid for linux
+        if platform.platform().lower() != 'linux':
+            await self.middleware.call('disk.remove_disk_from_graid', dev)
 
         # First do a quick wipe of every partition to clean things like zfs labels
         if mode == 'QUICK':
