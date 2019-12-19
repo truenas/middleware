743b6e6fdb2b27989a42239c82e909227480926d
themylogin <themylogin@gmail.com>
2019-12-19 10:44:36 +0100

    Add Dropbox chunk_size option and fix B2 chunk_size option

--- src/middlewared/middlewared/rclone/remote/dropbox.py
+++ src/middlewared/middlewared/rclone/remote/dropbox.py
@@ -1,5 +1,8 @@
+import textwrap
+
 from middlewared.rclone.base import BaseRcloneRemote
-from middlewared.schema import Str
+from middlewared.schema import Int, Str
+from middlewared.validators import Range
 
 
 class DropboxRcloneRemote(BaseRcloneRemote):
@@ -14,3 +17,15 @@ class DropboxRcloneRemote(BaseRcloneRemote):
         Str("token", title="Access Token", required=True, max_length=None),
     ]
     credentials_oauth = True
+
+    task_schema = [
+        Int("chunk_size", title="Upload chunk size (in megabytes)", description=textwrap.dedent("""\
+            Upload chunk size. Must fit in memory. Note that these chunks are buffered in memory and there might be a
+            maximum of «--transfers» chunks in progress at once. Dropbox Business accounts can have monthly data
+            transfer limits per team per month. By using larger chnuk sizes you will decrease the number of data
+            transfer calls used and you'll be able to transfer more data to your Dropbox Business account.
+        """), default=48, validators=[Range(min=5, max=149)]),
+    ]
+
+    async def get_task_extra(self, task):
+        return {"chunk_size": str(task["attributes"].get("chunk_size", 48)) + "M"}
