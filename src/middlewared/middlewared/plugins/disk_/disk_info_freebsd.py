d5cd36176feb8971e38f1e8549484436b11a135c
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-01-03 17:51:55 +0500

    Retrieve id of partitions

--- src/middlewared/middlewared/plugins/disk_/disk_info_freebsd.py
+++ src/middlewared/middlewared/plugins/disk_/disk_info_freebsd.py
@@ -33,7 +33,13 @@ class DiskService(Service, DiskInfoBase):
                     part_type = self.middleware.call_sync('disk.get_partition_uuid_from_name', part_type.text)
                 if not part_type:
                     part_type = 'UNKNOWN'
-                parts.append({'name': name.text, 'size': size, 'partition_type': part_type, 'disk': disk})
+                parts.append({
+                    'name': name.text,
+                    'size': size,
+                    'partition_type': part_type,
+                    'disk': disk,
+                    'id': p.get('id'),
+                })
 
         return parts
 
--- src/middlewared/middlewared/plugins/disk_/disk_info_linux.py
+++ src/middlewared/middlewared/plugins/disk_/disk_info_linux.py
@@ -30,6 +30,7 @@ class DiskService(Service, DiskInfoBase):
                 'size': p['partition_size'],
                 'partition_type': p['type'],
                 'disk': disk,
+                'id': f'{disk}{p["partition_number"]}',
             }
             for p in block_device.__getstate__()['partitions_data']['partitions']
         ]
