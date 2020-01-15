6908374b5df1028e0d5f6a9eacbdf8a796137e94
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-01-16 02:15:44 +0500

    Add migration to fix shell choice for existing netcli choice

---
+++ src/middlewared/middlewared/alembic/versions/12.0/2020-01-15_21-13_fix_shell_choices.py
@@ -0,0 +1,29 @@
+"""Fix user shell choices
+
+Revision ID: a7f13f81a210
+Revises: 133f2d9049d2
+Create Date: 2020-01-15 21:13:01.570666+00:00
+
+"""
+import json
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'a7f13f81a210'
+down_revision = '133f2d9049d2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+
+    conn.execute('UPDATE account_bsdUsers SET bsdusr_shell = ? WHERE bsdusr_shell = ?', (
+        '/etc/netcli.sh', '//etc/netcli.sh'
+    ))
+
+
+def downgrade():
+    pass
