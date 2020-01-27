1de051c67faf4bf5ebbc50263142ec5d93f918d3
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-01-27 18:46:25 +0500

    Add exception handling for zfs events

--- src/middlewared/middlewared/plugins/zfs_/events_linux.py
+++ src/middlewared/middlewared/plugins/zfs_/events_linux.py
@@ -13,16 +13,22 @@ class ZFSPoolService(Service):
         process_pool = True
 
     def events(self):
-        zevent_fd = os.open('/dev/zfs', os.O_RDWR)
-        try:
-            with libzfs.ZFS() as zfs:
-                while True:
-                    event = zfs.zpool_events_single(zevent_fd)
-                    self.middleware.send_event('zfs.pool.events', 'ADDED', id=event['class'], fields=event)
-        finally:
-            os.close(zevent_fd)
+        while True:
+            zevent_fd = None
+            try:
+                zevent_fd = os.open('/dev/zfs', os.O_RDWR)
+                with libzfs.ZFS() as zfs:
+                    while True:
+                        event = zfs.zpool_events_single(zevent_fd)
+                        self.middleware.send_event('zfs.pool.events', 'ADDED', id=event['class'], fields=event)
+            except Exception as e:
+                if self.middleware.call_sync('system.state') != 'SHUTTING_DOWN':
+                    self.middleware.logger.error('Failed to retrieve ZFS events: %s', str(e))
+            finally:
+                if zevent_fd is not None:
+                    os.close(zevent_fd)
 
 
 async def setup(middleware):
-    middleware.event_register('zfs.events', 'Retrieve realtime events from ZFS')
+    middleware.event_register('zfs.pool.events', 'Retrieve realtime events from ZFS')
     asyncio.ensure_future(middleware.call('zfs.pool.events'))
