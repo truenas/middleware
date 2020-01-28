5820287ce5097490add8beb28246c51ad490a12b
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-01-28 12:27:30 +0500

    Make hotplugging disks work in linux

---
+++ src/middlewared/middlewared/plugins/device_/udev_events_linux.py
@@ -0,0 +1,15 @@
+import pyudev
+
+from middlewared.utils import start_daemon_thread
+
+
+def udev_events(middleware):
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='block')
+    for device in iter(monitor.poll, None):
+        middleware.call_hook_sync(f'udev.{device.subsystem}', data={**dict(device), 'SYS_NAME': device.sys_name})
+
+
+def setup(middleware):
+    start_daemon_thread(target=udev_events, args=(middleware,))
