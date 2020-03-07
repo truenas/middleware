8a7a62d3fe657060cf99bcf99299ca5cc4d639a7
Andrew <awalker@ixsystems.com>
2020-03-06 22:24:46 -0500

    Revert jailed smbd (#4361)
    Encountered issues in testing this with more complex sharing environments.
    Reverting until feature more fully-baked in smbd.

--- src/middlewared/middlewared/plugins/smb_/registry.py
+++ src/middlewared/middlewared/plugins/smb_/registry.py
@@ -283,9 +283,6 @@ class SharingSMBService(Service):
             "ea support": "false",
         })
 
-        if not data['home'] and not data['path_suffix']:
-            conf["jail:enable"] = "true"
-
         if data['aapl_name_mangling']:
             data['vfsobjects'].append('catia')
             conf.update({
