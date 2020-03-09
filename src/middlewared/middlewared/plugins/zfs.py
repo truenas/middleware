b94645f572fa91bb2f57c595c51b3d66829e3426
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-03-09 12:50:32 +0500

    Allow creating special devices in zfs plugin

--- src/middlewared/middlewared/plugins/zfs.py
+++ src/middlewared/middlewared/plugins/zfs.py
@@ -96,7 +96,7 @@ class ZFSPoolService(CRUDService):
             List('vdevs', items=[
                 Dict(
                     'vdev',
-                    Str('root', enum=['DATA', 'CACHE', 'LOG', 'SPARE'], required=True),
+                    Str('root', enum=['DATA', 'CACHE', 'LOG', 'SPARE', 'SPECIAL'], required=True),
                     Str('type', enum=['RAIDZ1', 'RAIDZ2', 'RAIDZ3', 'MIRROR', 'STRIPE'], required=True),
                     List('devices', items=[Str('disk')], required=True),
                 ),
