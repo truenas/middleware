d699f8ae8d303d2bb44134cc429e5b7569ba5b71
Andrew <awalker@ixsystems.com>
2020-03-16 14:04:15 -0400

    Restart SMB if required after running smb_configure (#4408)
    - Refactor smb_configure etc plugin to smb plugin job
    - Ensure that smb server restarts after after configure is complete
    - Strip ACLs from samba directories if they exist.

--- src/middlewared/middlewared/plugins/smb_/groupmap.py
+++ src/middlewared/middlewared/plugins/smb_/groupmap.py
@@ -1,8 +1,9 @@
 from middlewared.service import Service, private
 from middlewared.service_exception import CallError
 from middlewared.utils import run
-from middlewared.plugins.smb import SMBCmd, SMBBuiltin
+from middlewared.plugins.smb import SMBCmd, SMBBuiltin, SMBPath
 
+import os
 import re
 
 RE_NETGROUPMAP = re.compile(r"^(?P<ntgroup>.+) \((?P<SID>S-[0-9\-]+)\) -> (?P<unixgroup>.+)$")
@@ -94,3 +95,44 @@ class SMBService(Service):
 
         if gm_delete.returncode != 0:
             self.logger.debug(f'Failed to delete groupmap for [{target}]: ({gm_delete.stderr.decode()})')
+
+    @private
+    async def synchronize_group_mappings(self):
+        if await self.middleware.call('ldap.get_state') != "DISABLED":
+            return
+
+        groupmap = await self.groupmap_list()
+        must_remove_cache = False
+        if groupmap:
+            sids_fixed = await self.middleware.call('smb.fixsid', groupmap)
+            if not sids_fixed:
+                groupmap = []
+
+        for b in SMBBuiltin:
+            entry = list(filter(lambda x: b.value[0] == x['unixgroup'], groupmap))
+            if b.name == 'ADMINISTRATORS':
+                if len(await self.middleware.call('group.query', [('gid', '=', 544)])) > 1:
+                    # Creating an SMB administrators mapping for a duplicate ID is potentially a security issue.
+                    self.logger.warn("Multiple groups have GID 544, switching allocation method for "
+                                     "SMB Administrators [S-1-5-32-544] to internal winbind method.")
+                    continue
+
+            if not entry:
+                stale_entry = list(filter(lambda x: b.name.lower().capitalize() == x['ntgroup'], groupmap))
+                if stale_entry:
+                    must_remove_cache = True
+                    await self.groupmap_delete(b.name.lower().capitalize())
+
+                await self.groupmap_add(b.value[0])
+
+        groups = await self.middleware.call('group.query', [('builtin', '=', False), ('smb', '=', True)])
+        for g in groups:
+            if not any(filter(lambda x: g['group'].upper() == x['ntgroup'].upper(), groupmap)):
+                await self.smb.groupmap_add(g['group'])
+
+        if must_remove_cache:
+            if os.path.exists(f'{SMBPath.STATEDIR.platform()}/winbindd_cache.tdb'):
+                os.remove(f'{SMBPath.STATEDIR.platform()}/winbindd_cache.tdb')
+            flush = await run([SMBCmd.NET.value, 'cache', 'flush'], check=False)
+            if flush.returncode != 0:
+                self.logger.debug('Attempt to flush cache failed: %s', flush.stderr.decode().strip())
