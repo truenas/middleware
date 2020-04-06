f05db98d20f0f321e4f0a0998d50ed43fe2dbe03
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-04-06 20:10:36 +0500

    Add truecommand basic service

--- src/middlewared/middlewared/plugins/truecommand.py
+++ src/middlewared/middlewared/plugins/truecommand.py
@@ -1,6 +1,7 @@
-from middlewared.schema import Bool, Str
-from middlewared.service import ConfigService
+from middlewared.schema import Bool, Dict, Str
+from middlewared.service import accepts, CallError, private, ConfigService, ValidationErrors
 import middlewared.sqlalchemy as sa
+from middlewared.validators import Range
 
 
 class TrueCommandModel(sa.Model):
@@ -16,3 +17,40 @@ class TrueCommandModel(sa.Model):
     endpoint = sa.Column(sa.String(255), default=None, nullable=True)
     remote_address = sa.Column(sa.String(255), default=None, nullable=True)
     enabled = sa.Column(sa.Boolean(), default=False)
+
+
+class TrueCommandService(ConfigService):
+
+    POLLING_GAP_MINUTES = 5
+
+    class Config:
+        service = 'truecommand'
+        datastore = 'services.truecommand'
+        datastore_extend = 'truecommand.tc_extend'
+
+    @private
+    async def tc_extend(self, config):
+        for key in ('wg_public_key', 'wg_private_key', 'tc_public_key', 'endpoint'):
+            config.pop(key)
+        return config
+
+    @accepts(
+        Dict(
+            'truecommand_update_config',
+            Bool('enabled'),
+            Str('api_key', null=True, validators=[Range(min=16, max=16)]),
+        )
+    )
+    async def do_update(self, data):
+        old = await self.config()
+        new = old.copy()
+        new.update(data)
+
+        await self.middleware.call(
+            'datastore.update',
+            self._config.datastore,
+            old['id'],
+            new
+        )
+
+        return await self.config()
