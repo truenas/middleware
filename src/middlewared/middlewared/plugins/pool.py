afffd16af412627135792a63e523113d31400401
William Grzybowski <william@grzy.org>
2020-04-16 11:45:59 -0300

    fix(middlewared/pool): invalid encoding for Popen

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -338,7 +338,7 @@ class PoolService(CRUDService):
         name = (await self.get_instance(oid))['name']
         proc = await Popen(
             f'zpool get -H -o value version {name}',
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8', shell=True
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
         )
         res, err = await proc.communicate()
         if proc.returncode != 0:
@@ -351,7 +351,7 @@ class PoolService(CRUDService):
             if res == '-':
                 proc = await Popen(
                     f"zpool get -H -o property,value all {name}",
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8', shell=True
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
                 )
                 data = (await proc.communicate())[0].decode('utf8').strip('\n')
                 for line in [l for l in data.split('\n') if l.startswith('feature') and '\t' in l]:
