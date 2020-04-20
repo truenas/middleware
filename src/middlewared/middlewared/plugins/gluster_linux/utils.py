964a4ff263ba99e659a29d712ad78bad286725a4
yocalebo <yocalebo@gmail.com>
2020-04-20 13:16:58 -0700

    make validate_gluster_job method common between the plugins

---
+++ src/middlewared/middlewared/plugins/gluster_linux/utils.py
@@ -0,0 +1,36 @@
+def validate_gluster_jobs(self, verrors, job):
+    """
+    Gluster CLI operations need to run synchronously.
+    """
+
+    peer_job = self.middleware.call_sync(
+        'core.get_jobs', [
+            ['method', '^', 'gluster.peer'],
+            ['state', 'in', ['RUNNING']],
+            ['id', '!=', job.id]
+        ]
+    )
+
+    if peer_job:
+        verrors.add(
+            'validate_gluster_jobs',
+            'There is an ongoing gluster peer operation. '
+            'Please wait for it to complete and then try again.'
+        )
+        raise verrors
+
+    volume_job = self.middleware.call_sync(
+        'core.get_jobs', [
+            ['method', '^', 'gluster.volume'],
+            ['state', 'in', ['RUNNING']],
+            ['id', '!=', job.id]
+        ]
+    )
+
+    if volume_job:
+        verrors.add(
+            'validate_gluster_jobs',
+            'There is an ongoing gluster volume operation. '
+            'Please wait for it to complete and then try again.'
+        )
+        raise verrors
