derzahla/zvio-middleware

Reconfigure logging in worker processes

src/middlewared/middlewared/plugins/zettarepl.py
@@ -40,7 +40,7 @@ from zettarepl.utils.logging import (
 from zettarepl.zettarepl import Zettarepl
 
 from middlewared.client import Client, ClientException, ejson
-from middlewared.logger import setup_logging
+from middlewared.logger import setup_logging, reconfigure_logging
 from middlewared.service import CallError, periodic, Service
 from middlewared.utils import start_daemon_thread
 from middlewared.utils.string import make_sentence
@@ -145,6 +145,9 @@ class ZettareplProcess:
         for handler in logging.getLogger("zettarepl").handlers:
             handler.addFilter(LongStringsFilter())
             handler.addFilter(ReplicationTaskLoggingLevelFilter(default_level))
+
+        c = Client('ws+unix:///var/run/middlewared-internal.sock', py_exceptions=True)
+        c.subscribe('core.reconfigure_logging', lambda *args, **kwargs: reconfigure_logging('zettarepl_file'))
 
         definition = Definition.from_data(self.definition, raise_on_error=False)
         self.observer_queue.put(DefinitionErrors(definition.errors))

truenas/middleware

(cherry picked from commit 164aa93013ff4ec7f071f2f359ac310bb8b0b8fd)

src/middlewared/middlewared/plugins/zettarepl.py
@@ -40,7 +40,7 @@ from zettarepl.utils.logging import (
 from zettarepl.zettarepl import Zettarepl
 
 from middlewared.client import Client, ClientException
-from middlewared.logger import setup_logging
+from middlewared.logger import reconfigure_logging, setup_logging
 from middlewared.service import CallError, Service
 from middlewared.utils import start_daemon_thread
 import middlewared.utils.osc as osc
@@ -145,6 +145,9 @@ class ZettareplProcess:
         for handler in logging.getLogger("zettarepl").handlers:
             handler.addFilter(LongStringsFilter())
             handler.addFilter(ReplicationTaskLoggingLevelFilter(default_level))
+
+        c = Client('ws+unix:///var/run/middlewared-internal.sock', py_exceptions=True)
+        c.subscribe('core.reconfigure_logging', lambda *args, **kwargs: reconfigure_logging('zettarepl_file'))
 
         definition = Definition.from_data(self.definition, raise_on_error=False)
         self.observer_queue.put(DefinitionErrors(definition.errors))