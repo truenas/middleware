aea4f49073a154f339bae287fe7621b99ba4171a
ericbsd <ericturgeon.bsd@gmail.com>
2020-05-13 10:22:56 -0300

    remove import not in use from alertclasses API test

--- tests/api2/alertclasses.py
+++ tests/api2/alertclasses.py
@@ -2,11 +2,9 @@
 
 import os
 import sys
-from time import sleep
 apifolder = os.getcwd()
 sys.path.append(apifolder)
 from functions import GET, PUT
-from auto_config import ip, password, user, pool_name
 
 
 def test_01_get_alertclasses():
@@ -30,7 +28,7 @@ def test_02_add_classes_to_alertclasses():
     assert isinstance(results.json(), dict), results.text
 
 
-def test_03_get_the_new_alertclasses():
+def test_03_verify_the_new_alertclasses():
     results = GET("/alertclasses/")
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
