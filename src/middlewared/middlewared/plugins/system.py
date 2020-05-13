43b45a1568710962857e2784e91f75303ed12ae4
Luís Bastião Silva <luis.kop@gmail.com>
2020-05-13 21:13:09 +0100

    NAS-105931: removed not needed space.

--- src/middlewared/middlewared/plugins/system.py
+++ src/middlewared/middlewared/plugins/system.py
@@ -7,7 +7,6 @@ from middlewared.schema import accepts, Bool, Dict, Int, IPAddr, List, Str
 from middlewared.service import CallError, ConfigService, no_auth_required, job, private, Service, ValidationErrors
 import middlewared.sqlalchemy as sa
 from middlewared.utils import Popen, run, start_daemon_thread, sw_buildtime, sw_version, osc
-
 from middlewared.validators import Range
 
 import ntplib
