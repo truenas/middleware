e198b896df27050990b56b099a2fe73edca28144
ericbsd <ericturgeon.bsd@gmail.com>
2020-06-12 16:53:19 -0300

    adding a test to get the active bootenv for the next test

--- tests/api2/bootenv.py
+++ tests/api2/bootenv.py
@@ -12,73 +12,79 @@ sys.path.append(apifolder)
 from functions import POST, DELETE, GET, PUT
 
 
-def test_01_creating_a_new_boot_environment():
-    payload = {"name": "bootenv01", "source": "default"}
+def test_01_get_the_activated_bootenv():
+    global active_be_id
+    results = GET('/bootenv/?activated=True')
+    assert results.status_code == 200, results.text
+    active_be_id = results.json()[0]['id']
+
+
+def test_02_creating_a_new_boot_environment_from_the_active_boot_environment():
+    payload = {"name": "bootenv01", "source": active_be_id}
     results = POST("/bootenv/", payload)
     assert results.status_code == 200, results.text
     sleep(1)
 
 
-def test_02_look_new_bootenv_is_created():
+def test_03_look_new_bootenv_is_created():
     assert len(GET('/bootenv?name=bootenv01').json()) == 1
 
 
-def test_03_activate_bootenv01():
-    payload = None
-    results = POST("/bootenv/id/bootenv01/activate/", payload)
+def test_04_activate_bootenv01():
+    results = POST("/bootenv/id/bootenv01/activate/", None)
     assert results.status_code == 200, results.text
 
 
 # Update tests
-def test_04_cloning_a_new_boot_environment():
+def test_05_cloning_a_new_boot_environment():
     payload = {"name": "bootenv02", "source": "bootenv01"}
     results = POST("/bootenv/", payload)
     assert results.status_code == 200, results.text
     sleep(1)
 
 
-def test_05_activate_bootenv02():
+def test_06_activate_bootenv02():
     payload = None
     results = POST("/bootenv/id/bootenv02/activate/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_06_change_boot_environment_name():
+def test_07_change_boot_environment_name():
     payload = {"name": "bootenv03"}
     results = PUT("/bootenv/id/bootenv01/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_07_set_keep_attribute_true():
+def test_08_set_keep_attribute_true():
     payload = {"keep": True}
     results = POST("/bootenv/id/bootenv03/set_attribute/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_08_activate_bootenv03():
+def test_09_activate_bootenv03():
     payload = None
     results = POST("/bootenv/id/bootenv03/activate/", payload)
     assert results.status_code == 200, results.text
 
 
 # Delete tests
-def test_09_removing_a_boot_environment_02():
+def test_10_removing_a_boot_environment_02():
     results = DELETE("/bootenv/id/bootenv02/")
     assert results.status_code == 200, results.text
 
 
-def test_10_set_keep_attribute_true():
+def test_11_set_keep_attribute_true():
     payload = {"keep": False}
     results = POST("/bootenv/id/bootenv03/set_attribute/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_11_activate_default():
+def test_12_activate_default():
     payload = None
-    results = POST("/bootenv/id/default/activate/", payload)
+    results = POST(f"/bootenv/id/{active_be_id}/activate/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_12_removing_a_boot_environment_03():
+def test_13_removing_a_boot_environment_03():
     results = DELETE("/bootenv/id/bootenv03/")
     assert results.status_code == 200, results.text
