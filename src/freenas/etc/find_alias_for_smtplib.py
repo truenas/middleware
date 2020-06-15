128b013112bc1af73eaa81e3d58a9475a2b21234
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-06-15 23:15:20 +0500

    Make sendmail work for SCALE

--- src/freenas/etc/find_alias_for_smtplib.py
+++ src/freenas/etc/find_alias_for_smtplib.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python
-
 import argparse
 import email
 import email.parser
--- src/freenas/etc/find_alias_for_smtplib.sh
+++ src/freenas/etc/find_alias_for_smtplib.sh
@@ -2,4 +2,4 @@
 # We need to start the python script without stdout/stderr or it will
 # segfault if the mail script chain is started from a process without tty.
 # See #27994
-exec /usr/local/bin/python /etc/find_alias_for_smtplib.py "$@" > /dev/null 2>&1
+exec `which python3` /etc/find_alias_for_smtplib.py "$@" > /dev/null 2>&1
