b950b3c9b433d8508cf6702e8499c959798a27ea
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-06-19 21:14:05 +0500

    Add rsyncmod_enabled field

--- src/middlewared/middlewared/plugins/rsync.py
+++ src/middlewared/middlewared/plugins/rsync.py
@@ -133,6 +133,7 @@ class RsyncModModel(sa.Model):
     rsyncmod_hostsallow = sa.Column(sa.Text())
     rsyncmod_hostsdeny = sa.Column(sa.Text())
     rsyncmod_auxiliary = sa.Column(sa.Text())
+    rsyncmod_enabled = sa.Column(sa.Boolean())
 
 
 class RsyncModService(CRUDService):
@@ -175,6 +176,7 @@ class RsyncModService(CRUDService):
 
     @accepts(Dict(
         'rsyncmod_create',
+        Bool('enabled', default=True),
         Str('name', validators=[Match(r'[^/\]]')]),
         Str('comment'),
         Str('path', required=True, max_length=RSYNC_PATH_LIMIT),
