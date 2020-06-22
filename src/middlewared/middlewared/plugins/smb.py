c15f597290f6b0646ac3ece5d28f1693b23fbc5d
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-06-22 05:32:36 +0500

    Make sure we don't toggle locked attachments on pool import

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -733,7 +733,6 @@ class SharingSMBService(SharingService):
             await self.apply_presets(new)
 
         await self.compress(new)
-        new.pop(self.locked_field)
         await self.middleware.call(
             'datastore.update', self._config.datastore, id, new,
             {'prefix': self._config.datastore_prefix})
@@ -919,6 +918,7 @@ class SharingSMBService(SharingService):
     async def compress(self, data):
         data['hostsallow'] = ' '.join(data['hostsallow'])
         data['hostsdeny'] = ' '.join(data['hostsdeny'])
+        data.pop(self.locked_field, None)
 
         return data
 
