938098ccc6204759f85c895527806695855a5676
ericbsd <ericturgeon.bsd@gmail.com>
2020-06-22 14:12:12 -0300

    adding pytest dependency suports for test that depend on user test

--- tests/api2/test_011_user.py
+++ tests/api2/test_011_user.py
@@ -4,8 +4,10 @@
 # License: BSD
 # Location for tests into REST API of FreeNAS
 
+import pytest
 import sys
 import os
+from pytest_dependency import depends
 apifolder = os.getcwd()
 sys.path.append(apifolder)
 from functions import POST, GET, DELETE, PUT
@@ -19,6 +21,7 @@ group = 'root' if scale else 'wheel'
 group_id = GET(f'/group/?group={group}').json()[0]['id']
 
 
+@pytest.mark.dependency(name="user_01")
 def test_01_get_next_uid():
     results = GET('/user/get_next_uid/')
     assert results.status_code == 200, results.text
@@ -26,7 +29,9 @@ def test_01_get_next_uid():
     next_uid = results.json()
 
 
-def test_02_creating_user_testuser():
+@pytest.mark.dependency(name="user_02")
+def test_02_creating_user_testuser(request):
+    depends(request, ["user_01"])
     global user_id
     payload = {
         "username": "testuser",
@@ -41,32 +46,39 @@ def test_02_creating_user_testuser():
     user_id = results.json()
 
 
-def test_03_look_user_is_created():
+def test_03_look_user_is_created(request):
+    depends(request, ["user_02", "user_01"])
     assert len(GET('/user?username=testuser').json()) == 1
 
 
-def test_04_get_user_info():
+def test_04_get_user_info(request):
+    depends(request, ["user_02", "user_01"])
     global userinfo
     userinfo = GET(f'/user/id/{user_id}').json()
 
 
-def test_05_look_user_name():
+def test_05_look_user_name(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["username"] == "testuser"
 
 
-def test_06_look_user_full_name():
+def test_06_look_user_full_name(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["full_name"] == "Test User"
 
 
-def test_07_look_user_uid():
+def test_07_look_user_uid(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["uid"] == next_uid
 
 
-def test_08_look_user_shell():
+def test_08_look_user_shell(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["shell"] == shell
 
 
-def test_09_add_employee_id_and_team_special_attributes():
+def test_09_add_employee_id_and_team_special_attributes(request):
+    depends(request, ["user_02", "user_01"])
     payload = {
         'key': 'Employee ID',
         'value': 'TU1234',
@@ -77,18 +89,21 @@ def test_09_add_employee_id_and_team_special_attributes():
     assert results.status_code == 200, results.text
 
 
-def test_10_get_new_next_uid():
+def test_10_get_new_next_uid(request):
+    depends(request, ["user_02", "user_01"])
     results = GET('/user/get_next_uid/')
     assert results.status_code == 200, results.text
     global new_next_uid
     new_next_uid = results.json()
 
 
-def test_11_next_and_new_next_uid_not_equal():
+def test_11_next_and_new_next_uid_not_equal(request):
+    depends(request, ["user_02", "user_01"])
     assert new_next_uid != next_uid
 
 
-def test_12_setting_user_groups():
+def test_12_setting_user_groups(request):
+    depends(request, ["user_02", "user_01"])
     payload = {'groups': [group_id]}
     GET('/user?username=testuser').json()[0]['id']
     results = PUT(f"/user/id/{user_id}/", payload)
@@ -97,7 +112,8 @@ def test_12_setting_user_groups():
 
 # Update tests
 # Update the testuser
-def test_13_updating_user_testuser_info():
+def test_13_updating_user_testuser_info(request):
+    depends(request, ["user_02", "user_01"])
     payload = {"full_name": "Test Renamed",
                "password": "testing123",
                "uid": new_next_uid}
@@ -105,57 +121,69 @@ def test_13_updating_user_testuser_info():
     assert results.status_code == 200, results.text
 
 
-def test_14_get_user_new_info():
+def test_14_get_user_new_info(request):
+    depends(request, ["user_02", "user_01"])
     global userinfo
     userinfo = GET('/user?username=testuser').json()[0]
 
 
-def test_15_look_user_full_name():
+def test_15_look_user_full_name(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["full_name"] == "Test Renamed"
 
 
-def test_16_look_user_new_uid():
+def test_16_look_user_new_uid(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["uid"] == new_next_uid
 
 
-def test_17_look_user_groups():
+def test_17_look_user_groups(request):
+    depends(request, ["user_02", "user_01"])
     assert userinfo["groups"] == [group_id]
 
 
-def test_18_remove_old_team_special_atribute():
+def test_18_remove_old_team_special_atribute(request):
+    depends(request, ["user_02", "user_01"])
     payload = 'Team'
     results = POST(f"/user/id/{user_id}/pop_attribute/", payload)
     assert results.status_code == 200, results.text
 
 
-def test_19_add_new_team_to_special_atribute():
+def test_19_add_new_team_to_special_atribute(request):
+    depends(request, ["user_02", "user_01"])
     payload = {'key': 'Team', 'value': 'QA'}
     results = POST(f"/user/id/{user_id}/set_attribute/", payload)
     assert results.status_code == 200, results.text
 
 
 # Delete the testuser
-def test_20_deleting_user_testuser():
+def test_20_deleting_user_testuser(request):
+    depends(request, ["user_02", "user_01"])
     results = DELETE(f"/user/id/{user_id}/", {"delete_group": True})
     assert results.status_code == 200, results.text
 
 
-def test_21_look_user_is_delete():
+def test_21_look_user_is_delete(request):
+    depends(request, ["user_02", "user_01"])
     assert len(GET('/user?username=testuser').json()) == 0
 
 
-def test_22_has_root_password():
+def test_22_has_root_password(request):
+    depends(request, ["user_02", "user_01"])
     assert GET('/user/has_root_password/', anonymous=True).json() is True
 
 
-def test_23_get_next_uid_for_shareuser():
+def test_23_get_next_uid_for_shareuser(request):
+    depends(request, ["user_02", "user_01"])
     results = GET('/user/get_next_uid/')
     assert results.status_code == 200, results.text
     global next_uid
     next_uid = results.json()
 
 
-def test_24_creating_shareuser_to_test_sharing():
+@pytest.mark.dependency(name="user_24")
+def test_24_creating_shareuser_to_test_sharing(request):
+    depends(request, ["user_02", "user_01"])
     payload = {
         "username": "shareuser",
         "full_name": "Share User",
