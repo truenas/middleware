09b9c248a57ccf320dd7d5b6b44f4f1ad2f4b8bc
Andrew Walker <awalker@ixsystems.com>
2020-06-26 07:30:04 -0700

    Store domain secrets in config file
    Add ability to backup and restore secrets.tdb from config file.
    Entries are indexed by netbios name so that we can have separate
    domain secrets on separate store controllers. During smb.configure
    automatically check the secrets.tdb for our requried entries and
    restore them if needed. smb.configure will be called on boot and
    change of system dataset location.

--- src/middlewared/middlewared/plugins/directoryservices.py
+++ src/middlewared/middlewared/plugins/directoryservices.py
@@ -1,6 +1,13 @@
 import enum
+import json
+import os
+import tdb
+
+from base64 import b64encode, b64decode
 from middlewared.schema import accepts
 from middlewared.service import Service, private
+from middlewared.plugins.smb import SMBCmd, SMBPath
+from middlewared.utils import run
 from samba.dcerpc.messaging import MSG_WINBIND_OFFLINE, MSG_WINBIND_ONLINE
 
 
@@ -35,6 +42,65 @@ class NSS_Info(enum.Enum):
     SFU20 = ('SFU20', [DSType.AD])
     RFC2307 = ('RFC2307', [DSType.AD, DSType.LDAP])
     RFC2307BIS = ('RFC2307BIS', [DSType.LDAP])
+
+
+class DirectorySecrets(object):
+    def __init__(self, **kwargs):
+        super(DirectorySecrets, self).__init__()
+        self.flags = kwargs.get('flags', 0)
+        self.logger = kwargs.get('logger')
+        self.ha_mode = kwargs.get('ha_mode')
+        self.tdb = None
+        self.is_open = False
+
+    def open_tdb(self):
+        if self.ha_mode == "LEGACY":
+            secret_path = f'{SMBPath.LEGACYPRIVATE.platform()}/secrets.tdb'
+        else:
+            secret_path = f'{SMBPath.PRIVATEDIR.platform()}/secrets.tdb'
+
+        if os.path.isfile(secret_path):
+            self.tdb = tdb.open(secret_path, self.flags)
+        else:
+            self.tdb = tdb.Tdb(secret_path, 0, tdb.DEFAULT, os.O_CREAT | os.O_RDWR)
+        self.is_open = True
+
+    def has_domain(self, domain):
+        return True if self.tdb.get(f"SECRETS/MACHINE_PASSWORD/{domain.upper()}".encode()) else False
+
+    def dump(self):
+        ret = {}
+        self.tdb.read_lock_all()
+        for entry in self.tdb:
+            ret.update({entry.decode(): (b64encode(self.tdb.get(entry))).decode()})
+            self.logger.debug("entry: %s", ret[entry.decode()])
+
+        self.tdb.read_unlock_all()
+        return ret
+
+    def restore(self, secrets):
+        self.tdb.transaction_start()
+        for k, v in secrets.items():
+            tdb_key = k.encode()
+            tdb_data = b64decode(v)
+            try:
+                self.tdb.store(tdb_key, tdb_data)
+            except Exception:
+                self.logger.warning("Failed to store tdb data. "
+                                    "Cancelling transaction.",
+                                    exc_info=True)
+                self.tdb.transaction_cancel()
+                return
+
+        self.tdb.transaction_commit()
+
+    def __enter__(self):
+        self.open_tdb()
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        if self.is_open:
+            self.tdb.close()
 
 
 class DirectoryServices(Service):
@@ -109,6 +175,126 @@ class DirectoryServices(Service):
                 ret.append(x.value[0])
 
         return ret
+
+    @private
+    def backup_secrets(self):
+        ha_mode = self.middleware.call_sync('smb.get_smb_ha_mode')
+        netbios_name = self.middleware.call_sync('smb.config')['netbiosname_local']
+        db = self.middleware.call_sync('datastore.query',
+                                       'services.cifs', [],
+                                       {'prefix': 'cifs_srv_', 'get': True})
+
+        if db['secrets'] is None:
+            db_secrets = {}
+        else:
+            db_secrets = json.loads(db['secrets'])
+
+        with DirectorySecrets(logger=self.logger, ha_mode=ha_mode) as s:
+            secrets = s.dump()
+
+        if not secrets:
+            self.logger.warning("Unable to parse secrets")
+            return
+
+        db_secrets.update({netbios_name: secrets})
+        self.middleware.call_sync('datastore.update',
+                                  'services.cifs', 1,
+                                  {'secrets': str(json.dumps(db_secrets))},
+                                  {'prefix': 'cifs_srv_'})
+
+    @private
+    def restore_secrets(self, netbios_name=None):
+        ha_mode = self.middleware.call_sync('smb.get_smb_ha_mode')
+
+        if netbios_name is None:
+            netbios_name = self.middleware.call_sync('smb.config')['netbiosname_local']
+
+        db = self.middleware.call_sync('datastore.query',
+                                       'services.cifs', [],
+                                       {'prefix': 'cifs_srv_', 'get': True})
+
+        db_secrets = json.loads(db['secrets'])
+        server_secrets = db_secrets.get(netbios_name)
+        if server_secrets is None:
+            self.logger.warning("Unable to find stored secrets for [%]. "
+                                "Directory service functionality may be impacted.",
+                                netbios_name)
+            return False
+
+        with DirectorySecrets(logger=self.logger, ha_mode=ha_mode) as s:
+            try:
+                s.restore(server_secrets)
+            except Exception:
+                self.logger.warning("Failed to restore secrets for [%s]. "
+                                    "Directory service functionality may be impacted.",
+                                    netbios_name, exc_info=True)
+                return False
+
+        return True
+
+    @private
+    def secrets_has_domain(self, domain):
+        ha_mode = self.middleware.call_sync('smb.get_smb_ha_mode')
+        with DirectorySecrets(logger=self.logger, ha_mode=ha_mode) as s:
+            rv = s.has_domain(domain)
+
+        return rv
+
+    @private
+    def available_secrets(self):
+        db = self.middleware.call_sync('datastore.query',
+                                       'services.cifs', [],
+                                       {'prefix': 'cifs_srv_', 'get': True})
+
+        db_secrets = json.loads(db['secrets'])
+        return list(db_secrets.keys())
+
+    @private
+    async def initialize(self):
+        """
+        Ensure that secrets.tdb at a minimum exists. If it doesn't exist, try to restore
+        from a backup stored in our config file. If this fails, try to use what
+        auth info we have to recover the information. If we are in an LDAP
+        environment with a samba schema in use, we just need to write the password into
+        secrets.tdb.
+        """
+        ldap_conf = await self.middleware.call("ldap.config")
+        ldap_enabled = ldap_conf['enable']
+        ad_enabled = (await self.middleware.call("activedirectory.config"))['enable']
+        workgroup = (await self.middleware.call("smb.config"))["workgroup"]
+
+        if not ldap_enabled and not ad_enabled:
+            return
+
+        has_secrets = await self.middleware.call("directoryservices.secrets_has_domain", workgroup)
+
+        if ad_enabled and not has_secrets:
+            kerberos_method = await self.middleware.call("smb.getparm", "kerberos method", "GLOBAL")
+            self.logger.warning("Domain secrets database does not exist. "
+                                "Attempting to restore.")
+            ok = await self.middleware.call("directoryservices.restore_secrets")
+            if not ok:
+                self.logger.warning("Failed to restore domain secrets database. "
+                                    "Re-joining AD domain may be required.")
+
+                if kerberos_method != "secrets and keytab":
+                    self.logger.warning("Restoration of secrets database failed. "
+                                        "Attempting to automatically re-join AD domain.")
+                    try:
+                        await self.middleware.call("activedirectory.start")
+                    except Exception:
+                        self.logger.warning("Failed to re-join active directory domain.",
+                                            exc_info=True)
+
+        elif ldap_enabled and not has_secrets and ldap_conf["has_samba_schema"]:
+            self.logger.warning("LDAP SMB secrets database does not exist. "
+                                "attempting to restore secrets from configuration file.")
+            self.middleware.call("smb.store_ldap_admin_password")
+
+        gencache_flush = await run([SMBCmd.NET.value, 'cache', 'flush'], check=False)
+        if gencache_flush.returncode != 0:
+            self.logger.warning("Failed to clear the SMB gencache after re-initializing "
+                                "directory services: [%s]", gencache_flush.stderr.decode())
 
 
 def setup(middleware):
