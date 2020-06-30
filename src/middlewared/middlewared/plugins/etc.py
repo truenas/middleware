newlysoft/freenas

Fix checkpoint usage for normal etc generation

src/middlewared/middlewared/plugins/etc.py
@@ -292,7 +292,7 @@ class EtcService(Service):
             'py': PyRenderer(self),
         }
 
-    async def generate(self, name, checkpoint='default'):
+    async def generate(self, name, checkpoint=None):
         group = self.GROUPS.get(name)
         if group is None:
             raise ValueError('{0} group not found'.format(name))
@@ -303,13 +303,14 @@ class EtcService(Service):
             if renderer is None:
                 raise ValueError(f'Unknown type: {entry["type"]}')
 
-            checkpoint_system = f'checkpoint_{osc.SYSTEM.lower()}'
-            if checkpoint_system in entry:
-                entry_checkpoint = entry[checkpoint_system]
-            else:
-                entry_checkpoint = entry.get('checkpoint', 'default')
-            if entry_checkpoint != checkpoint:
-                continue
+            if checkpoint:
+                checkpoint_system = f'checkpoint_{osc.SYSTEM.lower()}'
+                if checkpoint_system in entry:
+                    entry_checkpoint = entry[checkpoint_system]
+                else:
+                    entry_checkpoint = entry.get('checkpoint', 'default')
+                if entry_checkpoint != checkpoint:
+                    continue
 
             if 'platform' in entry and entry['platform'].upper() != osc.SYSTEM:
                 continue

truenas/middleware

src/middlewared/middlewared/plugins/etc.py
@@ -294,7 +294,7 @@ class EtcService(Service):
             'py': PyRenderer(self),
         }
 
-    async def generate(self, name, checkpoint='default'):
+    async def generate(self, name, checkpoint=None):
         group = self.GROUPS.get(name)
         if group is None:
             raise ValueError('{0} group not found'.format(name))
@@ -307,6 +307,15 @@ class EtcService(Service):
 
                 if 'platform' in entry and entry['platform'].upper() != osc.SYSTEM:
                     continue
+
+                if checkpoint:
+                    checkpoint_system = f'checkpoint_{osc.SYSTEM.lower()}'
+                    if checkpoint_system in entry:
+                        entry_checkpoint = entry[checkpoint_system]
+                    else:
+                        entry_checkpoint = entry.get('checkpoint', 'initial')
+                    if entry_checkpoint != checkpoint:
+                        continue
 
                 path = os.path.join(self.files_dir, entry.get('local_path') or entry['path'])
                 entry_path = entry['path']