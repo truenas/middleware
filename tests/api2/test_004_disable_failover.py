9a66f1b778733a7ed260d7c5e093f495085bb755
ericbsd <ericturgeon.bsd@gmail.com>
2020-07-07 16:36:39 -0300

    Replace test_004_disable_failover POST by PUT

--- tests/api2/test_004_disable_failover.py
+++ tests/api2/test_004_disable_failover.py
@@ -5,7 +5,7 @@ import sys
 import os
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import POST
+from functions import PUT
 from auto_config import ha
 
 pytestmark = pytest.mark.skipif(not ha, reason="Skipping test for Core and Scale")
@@ -15,5 +15,5 @@ def test_01_disable_failover():
     payload = {
         "disabled": True
     }
-    results = POST('/failover/', payload, controller_a=ha)
+    results = PUT('/failover/', payload, controller_a=ha)
     assert results.status_code == 200, results.text
