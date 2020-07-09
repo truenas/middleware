91048756b4c32fe2201911d036ca5675e4cf3ffb
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-07-09 21:15:22 +0500

    Add migration for cpu_host_passthrough

---
+++ src/middlewared/middlewared/alembic/versions/12.0/2020-07-09_16-11_cpu_passthrough.py
@@ -0,0 +1,21 @@
+"""
+CPU Host-Passthrough field
+
+Revision ID: 3ecb7147c137
+Revises: 71a8d1e504a7
+Create Date: 2020-07-09 16:11:26.058680+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '3ecb7147c137'
+down_revision = '71a8d1e504a7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('vm_vm', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('cpu_host_passthrough', sa.Boolean(), default=False))
