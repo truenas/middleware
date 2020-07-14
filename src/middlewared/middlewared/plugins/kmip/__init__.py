2fc35aceaf2338a0d9d2d893b7ade161a6ad24e8
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-07-14 22:35:18 +0500

    Use system ready event instead of interface post sync hook to initialise kmip keys

--- src/middlewared/middlewared/plugins/kmip/__init__.py
+++ src/middlewared/middlewared/plugins/kmip/__init__.py
@@ -1,14 +1,14 @@
-async def interface_post_sync(middleware):
-    if not await middleware.call('system.ready'):
-        await initialize_kmip_keys(middleware)
-
-
 async def initialize_kmip_keys(middleware):
     if (await middleware.call('kmip.config'))['enabled']:
         await middleware.call('kmip.initialize_keys')
 
 
+async def __event_system_ready(middleware, event_type, args):
+    if args['id'] == 'ready':
+        await initialize_kmip_keys(middleware)
+
+
 async def setup(middleware):
-    middleware.register_hook('interface.post_sync', interface_post_sync)
+    middleware.event_subscribe('system', __event_system_ready)
     if await middleware.call('system.ready'):
         await initialize_kmip_keys(middleware)
