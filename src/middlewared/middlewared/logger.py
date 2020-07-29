0c7ac89c86cbb31385a471837883f0c9116aeeef
William Grzybowski <william@grzy.org>
2020-07-29 14:30:54 -0300

    Check stderr too
    Ticket:	#

--- src/middlewared/middlewared/logger.py
+++ src/middlewared/middlewared/logger.py
@@ -327,7 +327,7 @@ def reconfigure_logging():
         # We want to reassign stdout/stderr if its not the default one or closed
         # which will happen on log file rotation.
         try:
-            if sys.stdout.fileno() != 1:
+            if sys.stdout.fileno() != 1 or sys.stderr.fileno():
                 raise ValueError()
         except ValueError:
             # ValueError can be raise if file handler is closed
