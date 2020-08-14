32b6394d7d0bb608c3351e29c833aff6e9a976e5
themylogin <themylogin@gmail.com>
2020-08-14 18:16:18 +0200

    Update disk zfs guid on zfs config sync

--- src/middlewared/middlewared/plugins/disk_/zfs_guid.py
+++ src/middlewared/middlewared/plugins/disk_/zfs_guid.py
@@ -4,6 +4,7 @@ import bidict
 
 from middlewared.service import private, Service
 from middlewared.service_exception import MatchNotFound
+from middlewared.utils import osc
 
 logger = logging.getLogger(__name__)
 
@@ -46,10 +47,37 @@ class DiskService(Service):
                     )
 
 
+async def devd_zfs_hook(middleware, data):
+    if data.get("type") in (
+        "misc.fs.zfs.config_sync",
+    ):
+        await middleware.call(
+            "disk.sync_zfs_guid",
+            await middleware.call("pool.query", [["name", "=", data["pool_name"]]], {"get": True}),
+        )
+
+
+async def zfs_events_hook(middleware, data):
+    event_id = data["class"]
+
+    if event_id in [
+        "sysevent.fs.zfs.config_sync",
+    ]:
+        await middleware.call(
+            "disk.sync_zfs_guid",
+            await middleware.call("pool.query", [["name", "=", data["pool"]]], {"get": True}),
+        )
+
+
 async def hook(middleware, pool):
     await middleware.call("disk.sync_zfs_guid", pool)
 
 
 async def setup(middleware):
+    if osc.IS_FREEBSD:
+        middleware.register_hook("devd.zfs", devd_zfs_hook)
+    else:
+        middleware.register_hook("zfs.pool.events", zfs_events_hook)
+
     middleware.register_hook("pool.post_create_or_update", hook)
     middleware.register_hook("pool.post_import", hook)
