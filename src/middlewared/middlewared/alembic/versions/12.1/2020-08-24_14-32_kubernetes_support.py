8163ea15852f30d5f57b17a696201572b5b35e32
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-24 14:44:43 +0500

    Add basic k8s plugin

---
+++ src/middlewared/middlewared/alembic/versions/12.1/2020-08-24_14-32_kubernetes_support.py
@@ -0,0 +1,35 @@
+"""
+Migration for ix-applications support
+
+Revision ID: f8b573192e43
+Revises: 6dfba265232e
+Create Date: 2020-08-24 14:32:34.620255+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'f8b573192e43'
+down_revision = '6dfba265232e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'services_kubernetes',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('pool', sa.String(length=255), nullable=True),
+        sa.Column('cluster_cidr', sa.String(length=128), nullable=False),
+        sa.Column('service_cidr', sa.String(length=128), nullable=False),
+        sa.Column('cluster_dns_ip', sa.String(length=128), nullable=False),
+        sa.Column('route_configuration', sa.TEXT(), nullable=True),
+        sa.Column('node_ip', sa.String(length=128), nullable=False),
+        sa.Column('multus_config', sa.TEXT(), nullable=True),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_services_kubernetes')),
+    )
+
+
+def downgrade():
+    pass
---
+++ src/middlewared/middlewared/plugins/kubernetes_linux/update.py
@@ -0,0 +1,71 @@
+import ipaddress
+import os
+
+import middlewared.sqlalchemy as sa
+
+from middlewared.schema import Dict, Int, IPAddr, Str
+from middlewared.service import accepts, SystemServiceService, private, ValidationErrors
+from middlewared.validators import Range
+
+
+class KubernetesModel(sa.Model):
+    __tablename__ = 'services_kubernetes'
+
+    id = sa.Column(sa.Integer(), primary_key=True)
+    pool = sa.Column(sa.String(255), default=None, nullable=True)
+    cluster_cidr = sa.Column(sa.String(128), default='172.16.0.0/16')
+    service_cidr = sa.Column(sa.String(128), default='172.17.0.0/16')
+    cluster_dns_ip = sa.Column(sa.String(128), default='172.17.0.10')
+    route_configuration = sa.Column(sa.JSON(type=dict))
+    node_ip = sa.Column(sa.String(128), default='0.0.0.0')
+    multus_config = sa.Column(sa.JSON(type=dict))
+
+
+class KubernetesService(SystemServiceService):
+
+    class Config:
+        datastore = 'services.kubernetes'
+        datastore_extend = 'kubernetes.k8s_extend'
+        service_verb = 'restart'
+        service_verb_sync = False
+
+    @private
+    async def k8s_extend(self, data):
+        data['dataset'] = os.path.join(data['pool'], 'ix-applications') if data['pool'] else None
+        return data
+
+    @private
+    async def validate_data(self, data, schema):
+        verrors = ValidationErrors()
+
+        if not await self.middleware.call('pool.query', [['name', '=', data['pool']]]):
+            verrors.add(f'{schema}.pool', 'Please provide a valid pool configured in the system.')
+
+        if ipaddress.ip_address(data['cluster_dns_ip']) not in ipaddress.ip_network(data['service_cidr']):
+            verrors.add(f'{schema}.cluster_dns_ip', 'Must be in range of "service_cidr".')
+
+        verrors.check()
+
+    @accepts(
+        Dict(
+            'kubernetes_update',
+            Str('pool', empty=False),
+            Int('netmask', validators=[Range(min=0, max=128)]),
+            IPAddr('cluster_cidr', cidr=True),
+            IPAddr('service_cidr', cidr=True),
+            IPAddr('cluster_dns_ip'),
+            update=True,
+        )
+    )
+    async def do_update(self, data):
+        old_config = await self.config()
+        old_config.pop('dataset')
+        config = old_config.copy()
+        config.update(data)
+
+        await self.validate_data(data, 'kubernetes_update')
+
+        if len(set(old_config.items()) ^ set(config.items())) > 0:
+            await self._update_service(old_config, config)
+
+        return await self.config()
