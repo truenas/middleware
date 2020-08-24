cfc5d6fcd29929d893da8dfede88ee17349ab697
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-24 20:53:32 +0500

    Disable docker default bridge

--- src/middlewared/middlewared/etc_files/docker.py
+++ src/middlewared/middlewared/etc_files/docker.py
@@ -8,10 +8,13 @@ def render(service, middleware):
         return
 
     # TODO: Add GPU support
+    # TODO: Test proxy support
 
     os.makedirs('/etc/docker', exist_ok=True)
     with open('/etc/docker/daemon.json', 'w') as f:
         f.write(json.dumps({
             'data-root': os.path.join(config['dataset'], 'docker'),
             'exec-opts': ['native.cgroupdriver=cgroupfs'],
+            'iptables': False,
+            'bridge': 'none',
         }))
