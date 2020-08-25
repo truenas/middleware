63f971c74218f8d4d5cb5e41187058fc8715a7f2
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-25 21:29:40 +0500

    Always assume we have one node named ix-truenas

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/nodes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/nodes.py
@@ -1,23 +1,23 @@
 from middlewared.service import CallError
 
+from .utils import NODE_NAME
 
-async def get_node_from_name(api_client, node_name, node_object):
+
+async def get_node(api_client, node_object=None):
     if node_object:
         return node_object
-    if not node_name:
-        raise CallError(f'No node name / node object specified.')
-    node_object = await api_client.list_node_with_http_info(field_selector=f'metadata.name={node_name}')
-    if not node_object:
-        raise CallError(f'Unable to find "{node_name}" node.')
+    node_object = await api_client.list_node_with_http_info(field_selector=f'metadata.name={NODE_NAME}')
+    if not node_object[0].items:
+        raise CallError(f'Unable to find "{NODE_NAME}" node.')
     else:
         return node_object[0].items[0]
 
 
-async def add_taint(api_client, taint_dict, node_name=None, node_object=None):
+async def add_taint(api_client, taint_dict, node_object=None):
     for k in ('key', 'effect'):
         assert k in taint_dict
 
-    node_object = await get_node_from_name(api_client, node_name, node_object)
+    node_object = await get_node(api_client, node_object)
 
     existing_taints = [t.to_dict() for t in (node_object.spec.taints or [])]
     await api_client.patch_node(
@@ -25,8 +25,8 @@ async def add_taint(api_client, taint_dict, node_name=None, node_object=None):
     )
 
 
-async def remove_taint(api_client, taint_key, node_name=None, node_object=None):
-    node_object = await get_node_from_name(api_client, node_name, node_object)
+async def remove_taint(api_client, taint_key, node_object=None):
+    node_object = await get_node(api_client, node_object)
     taints = node_object.spec.taints or []
 
     for index, taint in enumerate(taints):
