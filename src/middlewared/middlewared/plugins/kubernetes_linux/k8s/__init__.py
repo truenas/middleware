39d4140859f80acfbffd4cf8303b8d809ea2bc00
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-25 16:29:36 +0500

    Add method to retrieve tokens from a service account

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
@@ -1,4 +1,5 @@
+from . import service_accounts
 from .api_client import api_client
 
 
-__all__ = ['api_client']
+__all__ = ['api_client', 'service_accounts']
--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/api_client.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/api_client.py
@@ -1,4 +1,4 @@
-from kubernetes_asyncio import client, config
+from kubernetes_asyncio import config
 from kubernetes_asyncio.client.api_client import ApiClient
 
 from .utils import KUBECONFIG_FILE
