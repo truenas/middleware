416975abac52f4a015ee6897bf6906d29fdc21e1
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-25 17:16:54 +0500

    Add method to add a taint to a node

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
@@ -1,5 +1,5 @@
-from . import service_accounts
+from . import nodes, service_accounts
 from .api_client import api_client
 
 
-__all__ = ['api_client', 'service_accounts']
+__all__ = ['api_client', 'nodes', 'service_accounts']
--- src/middlewared/middlewared/plugins/kubernetes_linux/nodes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/nodes.py
@@ -1,7 +1,8 @@
 from kubernetes_asyncio import client
-from middlewared.service import CRUDService, filterable
+from middlewared.schema import Dict, List, Str
+from middlewared.service import accepts, CRUDService, filterable
 
-from .k8s import api_client
+from .k8s import api_client, nodes
 
 
 class KubernetesNodeService(CRUDService):
@@ -14,3 +15,22 @@ class KubernetesNodeService(CRUDService):
         async with (await api_client()) as api:
             v1 = client.CoreV1Api(api)
             return [n.to_dict() for n in await v1.list_node_with_http_info()]
+
+    @accepts(
+        Str('node_name'),
+        List(
+            'add_taint',
+            items=[Dict(
+                'taints',
+                Str('key', required=True, empty=False),
+                Str('value', null=True, default=None),
+                Str('effect', required=True, empty=False, enum=['NoSchedule', 'NoExecutable'])
+            )],
+            default=[],
+        )
+    )
+    async def add_taints(self, node_name, taints):
+        async with (await api_client(())) as api:
+            v1 = client.CoreV1Api(api)
+            for taint in taints:
+                await nodes.add_taint(v1, taint, node_name=node_name)
