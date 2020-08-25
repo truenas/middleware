c2522a48fd6528ee2fe815573aebb7dd8be18485
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-25 21:08:31 +0500

    Add method to remove a taint from a node

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/nodes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/nodes.py
@@ -1,24 +1,40 @@
-from kubernetes_asyncio.client import V1Taint
-
 from middlewared.service import CallError
 
 
-async def get_node_from_name(client, node_name):
+async def get_node_from_name(api_client, node_name, node_object):
+    if node_object:
+        return node_object
     if not node_name:
         raise CallError(f'No node name / node object specified.')
-    node_object = await client.list_node_with_http_info(field_selector=f'metadata.name={node_name}')
+    node_object = await api_client.list_node_with_http_info(field_selector=f'metadata.name={node_name}')
     if not node_object:
         raise CallError(f'Unable to find "{node_name}" node.')
     else:
-        return node_object[0]
+        return node_object[0].items[0]
 
 
-async def add_taint(client, taint_dict, node_name=None, node_object=None):
+async def add_taint(api_client, taint_dict, node_name=None, node_object=None):
     for k in ('key', 'effect'):
         assert k in taint_dict
 
-    if not node_object:
-        node_object = get_node_from_name(client, node_name)
+    node_object = await get_node_from_name(api_client, node_name, node_object)
 
-    node_object.spec.taints.append(V1Taint(**taint_dict))
-    client.patch_node(name=node_object.metadata.name, body=node_object)
+    existing_taints = [t.to_dict() for t in (node_object.spec.taints or [])]
+    await api_client.patch_node(
+        name=node_object.metadata.name, body={'spec': {'taints': existing_taints + [taint_dict]}}
+    )
+
+
+async def remove_taint(api_client, taint_key, node_name=None, node_object=None):
+    node_object = await get_node_from_name(api_client, node_name, node_object)
+    taints = node_object.spec.taints or []
+
+    for index, taint in enumerate(taints):
+        if taint.key == taint_key:
+            found_index = index
+            break
+    else:
+        raise CallError(f'Unable to find taint with "{taint_key}" key')
+
+    taints.pop(found_index)
+    await api_client.patch_node(name=node_object.metadata.name, body={'spec': {'taints': taints}})
