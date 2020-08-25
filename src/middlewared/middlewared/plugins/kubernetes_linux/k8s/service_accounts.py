66af8be4f7dcdd9b9d078d072d2d3d1ccb699175
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-25 22:29:17 +0500

    Retrieve ca for a service account as well with token

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/service_accounts.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/service_accounts.py
@@ -1,12 +1,13 @@
 from base64 import b64decode
 
 
-async def get_service_account_tokens(api_client, service_account):
-    return [
-        {
-            'secret_name': secret.name,
-            'token': b64decode(await api_client.list_secret_for_all_namespaces(
-                field_selector=f'metadata.name={secret.name}'
-            ).items[0].data['token']).decode()
-        } for secret in service_account.secrets
-    ]
+async def get_service_account_tokens_cas(api_client, service_account):
+    details = []
+    for secret in service_account.secrets:
+        s_obj = await api_client.list_secret_for_all_namespaces(field_selector=f'metadata.name={secret.name}').items[0]
+        details.append({
+            'secret_name': s_obj.metadata.name,
+            'token': b64decode(s_obj.data['token']).decode(),
+            'ca': s_obj.data.get('ca.crt'),
+        })
+    return details
