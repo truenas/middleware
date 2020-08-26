92dca4eed84911415e03ddd8d042a1fe1b2223be
themylogin <themylogin@gmail.com>
2020-08-26 13:17:33 +0200

    Fix disk process_datastore_event
    (cherry picked from commit 001cedc500d8cadf1e83bc0a97fc25bc76995041)

--- src/middlewared/middlewared/plugins/disk_/sync.py
+++ src/middlewared/middlewared/plugins/disk_/sync.py
@@ -226,7 +226,7 @@ class DiskService(Service, ServiceChangeMixin):
 
     @private
     async def process_datastore_event(self, type, kwargs):
-        if type == "CHANGED":
+        if type == "CHANGED" and "fields" in kwargs:
             if kwargs["fields"]["expiretime"] is not None:
                 if kwargs["fields"]["identifier"] not in self.expired_disks:
                     self.expired_disks.add(kwargs["fields"]["identifier"])
