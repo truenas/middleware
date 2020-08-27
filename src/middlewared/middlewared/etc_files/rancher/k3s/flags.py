73172891d75f6569dfe83d2ca6a1fb12f9cb726f
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-27 19:11:30 +0500

    Make sure netmask is respected as described in https://github.com/rancher/k3s/issues/697\#issuecomment-576463182

--- src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
+++ src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
@@ -1,14 +1,17 @@
+import ipaddress
 import os
 import yaml
 
 
 def render(service, middleware):
     config = middleware.call_sync('kubernetes.config')
+    kube_controller_args = f'node-cidr-mask-size={ipaddress.ip_network(config["cluster_cidr"]).prefixlen}'
     os.makedirs('/etc/rancher/k3s', exist_ok=True)
-    with open('/etc/rancher/k3s/flags.conf', 'w') as f:
+    with open('/etc/rancher/k3s/flags.yaml', 'w') as f:
         f.write(yaml.dump({
             'cluster-cidr': config['cluster_cidr'],
             'service-cidr': config['service_cidr'],
             'cluster-dns': config['cluster_dns_ip'],
             'data-dir': os.path.join('/mnt', config['dataset'], 'k3s'),
+            'kube-controller-manager-arg': kube_controller_args,
         }))
