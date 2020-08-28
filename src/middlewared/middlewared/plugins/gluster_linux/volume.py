1e8def1a8cc6155e05c98c5a2e9fa64aa282db54
caleb <yocalebo@gmail.com>
2020-08-28 13:18:04 -0400

    fix tracebacks in gluster plugins

--- src/middlewared/middlewared/plugins/gluster_linux/volume.py
+++ src/middlewared/middlewared/plugins/gluster_linux/volume.py
@@ -21,9 +21,14 @@ class GlusterVolumeService(CRUDService):
         try:
             result = method(*args, **kwargs)
         except GlusterCmdException as e:
+            # the gluster cli utility will return stderr
+            # to stdout and vice versa on certain failures.
+            # account for this and decode appropriately
             rc, out, err = e.args[0]
             err = err if err else out
-            raise CallError(f'{err.decode().strip()}')
+            if isinstance(err, bytes):
+                err = err.decode()
+            raise CallError(f'{err.strip()}')
 
         if isinstance(result, bytes):
             return result.decode().strip()
