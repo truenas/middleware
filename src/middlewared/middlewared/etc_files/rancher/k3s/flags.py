bf0dbda65f0e2c18f1ce7aa73dd82d14824c0a8b
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-09-01 16:09:14 +0500

    Add support for specifying node ip address

--- src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
+++ src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
@@ -1,17 +1,26 @@
+import contextlib
 import ipaddress
 import os
 import yaml
 
 
+FLAGS_PATH = '/etc/rancher/k3s/flags.yaml'
+
+
 def render(service, middleware):
     config = middleware.call_sync('kubernetes.config')
+    if not config['pool']:
+        with contextlib.suppress(OSError):
+            os.unlink(FLAGS_PATH)
+
     kube_controller_args = f'node-cidr-mask-size={ipaddress.ip_network(config["cluster_cidr"]).prefixlen}'
     os.makedirs('/etc/rancher/k3s', exist_ok=True)
-    with open('/etc/rancher/k3s/flags.yaml', 'w') as f:
+    with open(FLAGS_PATH, 'w') as f:
         f.write(yaml.dump({
             'cluster-cidr': config['cluster_cidr'],
             'service-cidr': config['service_cidr'],
             'cluster-dns': config['cluster_dns_ip'],
             'data-dir': os.path.join('/mnt', config['dataset'], 'k3s'),
             'kube-controller-manager-arg': kube_controller_args,
+            'node-ip': config['node_ip'],
         }))
