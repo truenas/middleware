truenas/middleware

commit 8c852eebcd28ae808931eecea5d0cf2f44c7fc39

add vrrp_config attribute

src/middlewared/middlewared/plugins/interface/configure.py
@@ -100,6 +100,8 @@ class InterfaceService(Service):
                         if cc.vhid == carp_vhid:
                             advskew = cc.advskew
                         break
+            else:
+                vrrp_vip = data.get('int_vip', None)
 
             addrs_database.add(self.alias_to_addr(vip_data))
 
@@ -166,6 +168,9 @@ class InterfaceService(Service):
 
                 # FIXME: change py-netif to accept str() key
                 iface.carp_config = [netif.CarpConfig(carp_vhid, advskew=advskew, key=carp_pass.encode())]
+        else:
+            if vrrp_vip:
+                iface.vrrp_config = self.middleware.call_sync('interfaces.vrrp_config', name)
 
         # Add addresses in database and not configured
         for addr in (addrs_database - addrs_configured):

src/middlewared/middlewared/plugins/interface/netif_linux/interface.py
@@ -8,6 +8,7 @@ from .bits import InterfaceFlags, InterfaceLinkState
 from .lagg import LaggMixin
 from .utils import bitmask_to_set, run
 from .vlan import VlanMixin
+from .vrrp import VrrpMixin
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +19,7 @@ CLONED_PREFIXES = [
 ]
 
 
-class Interface(AddressMixin, BridgeMixin, LaggMixin, VlanMixin):
+class Interface(AddressMixin, BridgeMixin, LaggMixin, VlanMixin, VrrpMixin):
     def __init__(self, name):
         self.name = name
 
@@ -91,7 +92,7 @@ class Interface(AddressMixin, BridgeMixin, LaggMixin, VlanMixin):
         except IndexError:
             return None
 
-    def __getstate__(self, address_stats=False, media=False):
+    def __getstate__(self, address_stats=False, media=False, vrrp_config=None):
         state = {
             'name': self.name,
             'orig_name': self.orig_name,
@@ -110,7 +111,7 @@ class Interface(AddressMixin, BridgeMixin, LaggMixin, VlanMixin):
             'media_options': None,
             'link_address': self.link_address.address.address if self.link_address is not None else '',
             'aliases': [i.__getstate__(stats=address_stats) for i in self.addresses],
-            'vrrp_config': None,
+            'vrrp_config': vrrp_config,
         }
 
         if media:

src/middlewared/middlewared/plugins/interface/netif_linux/vrrp.py
@@ -1,62 +1,28 @@
 # -*- coding=utf-8 -*-
-import logging
-
-from middlewared.services import Service
-from middlewared.plugins.interface.netif import netif
-
-logger = logging.getLogger(__name__)
-
-__all__ = ["VrrpConfig", "VrrpMixin"]
+__all__ = ['VrrpMixin']
 
 
-class VrrpConfig():
+class VrrpMixin:
 
-    def __init__(self, addr=None, state=None):
-        self.addr = addr
-        self.state = state
-
-    def __getstate__(self):
-
-        return {
-            'address': self.addr,
-            'state': self.state,
-        }
-
-
-class VrrpMixin(Service):
+    def __init__(self):
+        self.data = None
 
     @property
-    def vrrp_config(self, ifname):
+    def vrrp_config(self):
 
-        # query db for configured settings
-        info = self.middleware.call_sync(
-            'datastore.query',
-            'network.interfaces',
-            [('int_interface', '=', ifname)],
-        )
-        configured_vips = [i['int_vip'] for i in info]
-
-        # get current addresses on interface
-        iface = netif.get_interface(ifname)
-        iface_addrs = [
-            str(i.address) for i in iface.addresses if i.af == netif.AddressFamily.INET
-        ]
-
-        # check if the configured VIP is on the interface
-        for i in configured_vips:
-            if i in iface_addrs:
-                yield VrrpConfig(addr=i, state='MASTER')
-
-            # VIP is not on the interface which means it's
-            # considered in the BACKUP state
-            yield VrrpConfig(addr=i, state='BACKUP')
+        return self.data
 
     @vrrp_config.setter
-    def vrrp_config(self, addr):
+    def vrrp_config(self, data):
+        """
+        Keepalived (VRRP daemon) on SCALE actually adds/deletes
+        the VIP from the interface as needed depending on whether
+        or not the controller is in the MASTER or BACKUP state.
 
+        So this function simply gets set to whatever is sent to it
+        in the `interface/configure.py` plugin. This mixin is used
+        so that interfaces on SCALE can have a "vrrp_config" property
+        to match as close as possible to how freeBSD and CARP works.
         """
-        Not used/needed at the moment since keepalived assigns
-        the ip to the interface on service startup depending
-        on the election process.
-        """
-        pass
+
+        self.data = data

src/middlewared/middlewared/plugins/interface/vrrp_linux.py
@@ -1,0 +1,68 @@
+# -*- coding=utf8 -*-
+import json
+import subprocess
+
+from middlewared.service import Service
+
+
+class VrrpService(Service):
+
+    class Config:
+        private = True
+        namespace_aliases = 'interfaces'
+
+    def vrrp_config(self, ifname):
+
+        # query db for configured settings
+        info = self.middleware.call_sync(
+            'datastore.query',
+            'network.interfaces',
+            [('int_interface', '=', ifname)],
+        )
+        configured_vips = [
+            i['int_vip'] for i in info if i['int_vip']
+        ]
+
+        # if there are no VIPs then there is no reason to continue
+        if not configured_vips:
+            self.logger.warning(
+                f'No VIPs have been configured for {ifname}.'
+            )
+            return
+
+        # get current addresses on `ifname` in json form
+        data = subprocess.run(
+            ['ip', '-j', 'addr', 'show', ifname],
+            stdout=subprocess.PIPE,
+        )
+        if data.stdout:
+            try:
+                data = json.loads(data.stdout.decode())
+            except Exception:
+                self.logger.error(
+                    f'Unable to parse address information for {ifname}.',
+                    exc_info=True
+                )
+                return
+
+        # now get the current IP addresses on the interface
+        iface_addrs = [
+            i['local'] for i in data[0]['addr_info'] if i['family'] == 'inet'
+        ]
+
+        addrs = []
+        addr = {
+            'address': None,
+            'state': None,
+        }
+        # check if the configured VIP is on the interface
+        for i in configured_vips:
+            if i in iface_addrs:
+                addr['address'] = i
+                addr['state'] = 'MASTER'
+            else:
+                addr['state'] = 'BACKUP'
+
+            addrs.append(addr)
+
+        return addrs

src/middlewared/middlewared/plugins/network.py
@@ -398,6 +398,10 @@ class InterfaceService(CRUDService):
             iface_extend_kwargs = {}
             if osc.IS_LINUX:
                 iface_extend_kwargs = dict(media=True)
+
+                if licensed:
+                    vrrp_config = self.middleware.call_sync('interfaces.vrrp_config', name)
+                    iface_extend_kwargs.update(dict(vrrp_config=vrrp_config))
             try:
                 data[name] = self.iface_extend(iface.__getstate__(**iface_extend_kwargs), configs, licensed)
             except OSError:

src/middlewared/middlewared/plugins/service_/services/keepalived.py
@@ -6,8 +6,15 @@ class KeepalivedService(SimpleService):
     name = 'keepalived'
     systemd_unit = 'keepalived'
     reloadable = True
+    restartable = True
 
     etc = ['keepalived']
+
+    async def restart(self):
+        # NOTE: this causes all interfaces on the node
+        # to send an advertisement with priority 0
+        # which means transition from MASTER to BACKUP
+        await self._systemd_unit('keepalived', 'restart')
 
     async def reload(self):
         await self._systemd_unit('keepalived', 'reload')

commit 3eb599f1d7a3b922ad84818ac863e543c77c835e

Fix vrrp regression introduced in https://github.com/freenas/freenas/commit/8c852eebcd28ae808931eecea5d0cf2f44c7fc39\#diff-5102a05e284f3aaf93d6b9d63063ee1d

src/middlewared/middlewared/plugins/interface/configure.py
@@ -83,6 +83,7 @@ class InterfaceService(Service):
         # if there is a VIP, and this is a linux system, then
         # we dont need a "VRID" (VHID equivalent) because we use
         # unicast VRRP advertisements
+        vrrp_vip = data.get('int_vip', None)
         if data['int_vip']:
             vip_data = {
                 'address': data['int_vip'],
@@ -101,8 +102,6 @@ class InterfaceService(Service):
                         if cc.vhid == carp_vhid:
                             advskew = cc.advskew
                         break
-            else:
-                vrrp_vip = data.get('int_vip', None)
 
             addrs_database.add(self.alias_to_addr(vip_data))
 