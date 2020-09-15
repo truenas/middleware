e4b414b81182a5fec4540d06556ec424b0391199
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-09-15 19:34:22 +0500

    Add catalog location to catalog query output

--- src/middlewared/middlewared/plugins/catalogs_linux/update.py
+++ src/middlewared/middlewared/plugins/catalogs_linux/update.py
@@ -1,5 +1,11 @@
+import os
+
+from copy import deepcopy
+
 from middlewared.service import CRUDService, filterable
 from middlewared.utils import filter_list
+
+from .utils import convert_repository_to_path
 
 
 CATALOGS = [
@@ -14,4 +20,13 @@ class CatalogService(CRUDService):
 
     @filterable
     async def query(self, filters=None, options=None):
+        catalogs = deepcopy(CATALOGS)
+        k8s_dataset = (await self.middleware.call('kubernetes.config'))['dataset']
+        catalogs_dir = os.path.join(k8s_dataset, 'catalogs') if k8s_dataset else '/tmp/ix-applications/catalogs'
+        for catalog in catalogs:
+            catalog.update({
+                'location': os.path.join(catalogs_dir, convert_repository_to_path(catalog['repository'])),
+                'id': catalog['label'],
+            })
+
         return filter_list(CATALOGS, filters, options)
