fefe2384365b1a9c09f4ec1c1f8f41b54e44aa39
themylogin <themylogin@gmail.com>
2020-09-16 16:19:23 +0200

    Fix legacy replication

--- gui/tools/replication_adapter.py
+++ gui/tools/replication_adapter.py
@@ -10,7 +10,7 @@ def query_model(path, filter_key):
         url = c.call("system.general.local_url")
         token = c.call("auth.generate_token")
 
-    r = requests.get(f"{url}/api/v1.0/{path}/", headers={"Authorization": f"Token {token}"}, verify=False)
+    r = requests.get(f"{url}/api/v1.0/{path}/?limit=10000", headers={"Authorization": f"Token {token}"}, verify=False)
     r.raise_for_status()
 
     return [SimpleNamespace(**d) for d in r.json() if d[filter_key]]
