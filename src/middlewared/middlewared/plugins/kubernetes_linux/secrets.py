abf0630575feb26d77553212e29e23a4c6d6a657
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-09-29 23:07:05 +0500

    Add service to retrieve k8s secrets

---
+++ src/middlewared/middlewared/plugins/kubernetes_linux/secrets.py
@@ -0,0 +1,66 @@
+from kubernetes_asyncio import client
+
+from middlewared.schema import Dict, Ref, Str
+from middlewared.service import accepts, CallError, CRUDService, filterable
+from middlewared.utils import filter_list
+
+from .k8s import api_client
+
+
+class KubernetesSecretService(CRUDService):
+
+    class Config:
+        namespace = 'k8s.secret'
+        private = True
+
+    @filterable
+    async def query(self, filters=None, options=None):
+        async with api_client() as (api, context):
+            return filter_list(
+                [d.to_dict() for d in (await context['core_api'].list_secret_for_all_namespaces()).items],
+                filters, options
+            )
+
+    @accepts(
+        Dict(
+            'secret_create',
+            Str('namespace', required=True),
+            Dict('body', additional_attrs=True, required=True),
+            register=True
+        )
+    )
+    async def do_create(self, data):
+        async with api_client() as (api, context):
+            try:
+                await context['core_api'].create_namespaced_secret(
+                    namespace=data['namespace'], body=data['body']
+                )
+            except client.exceptions.ApiException as e:
+                raise CallError(f'Unable to create secret: {e}')
+
+    @accepts(
+        Str('name'),
+        Ref('secret_create'),
+    )
+    async def do_update(self, name, data):
+        async with api_client() as (api, context):
+            try:
+                await context['core_api'].patch_namespaced_secret(
+                    name, namespace=data['namespace'], body=data['body']
+                )
+            except client.exceptions.ApiException as e:
+                raise CallError(f'Unable to patch {name} secret: {e}')
+
+    @accepts(
+        Str('name'),
+        Dict(
+            'secret_delete_options',
+            Str('namespace', required=True),
+        )
+    )
+    async def do_delete(self, name, options):
+        async with api_client() as (api, context):
+            try:
+                await context['core_api'].delete_namespaced_secret(name, options['namespace'])
+            except client.exceptions.ApiException as e:
+                raise CallError(f'Unable to delete secret: {e}')
