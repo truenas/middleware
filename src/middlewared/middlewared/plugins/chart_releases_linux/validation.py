4cb0bbee69f1d86e6d0634cd42973385e38f7c8e
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-10-09 05:04:17 +0500

    Validate persistentVolumeClaims to make sure they have unique names

--- src/middlewared/middlewared/plugins/chart_releases_linux/validation.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/validation.py
@@ -22,11 +22,10 @@ class ChartReleaseService(Service):
         for k in RESERVED_NAMES:
             new_values.pop(k[0], None)
 
+        schema_name = f'chart_release_{"update" if update else "create"}'
         attrs = list(itertools.chain.from_iterable(get_schema(q) for q in item_version_details['questions']))
         dict_obj = update_conditional_validation(
-            Dict(f'chart_release_{"update" if update else "create"}', *attrs, update=update), {
-                'schema': {'attrs': item_version_details['questions']}
-            }
+            Dict(schema_name, *attrs, update=update), {'schema': {'attrs': item_version_details['questions']}}
         )
 
         verrors = validate_attributes(
@@ -37,35 +36,45 @@ class ChartReleaseService(Service):
         verrors.check()
 
         # If schema is okay, we see if we have question specific validation to be performed
-        for question in item_version_details['questions']:
-            await self.validate_question(verrors, question)
         questions = item_version_details['questions']
         for key in new_values:
-            await self.validate_question(verrors, new_values[key], questions[key], dict_obj.attrs[key])
+            await self.validate_question(verrors, new_values[key], questions[key], dict_obj.attrs[key], schema_name)
 
         verrors.check()
 
         return dict_obj
 
     @private
-    async def validate_question(self, verrors, value, question, var_attr):
+    async def validate_question(self, verrors, value, question, var_attr, schema_name):
         schema = question['schema']
+        schema_name = f'{schema_name}.{question["variable"]}'
 
         # TODO: Add nested support for subquestions
         if schema['type'] == 'dict':
             dict_attrs = {v['variable']: v for v in schema['attrs']}
             for k in filter(lambda k: k in dict_attrs, value):
-                await self.validate_question(verrors, value[k], dict_attrs[k], var_attr.attrs[k])
+                await self.validate_question(
+                    verrors, value[k], dict_attrs[k], var_attr.attrs[k], f'{schema_name}.{k}',
+                )
 
         elif schema['type'] == 'list':
             for index, item in enumerate(value):
                 item_index, attr = get_list_item_from_value(item, var_attr)
                 if attr:
-                    await self.validate_question(verrors, item, schema['items'][item_index], attr)
+                    await self.validate_question(
+                        verrors, item, schema['items'][item_index], attr, f'{schema_name}.{index}'
+                    )
 
         for validator_def in filter(lambda k: k in validation_mapping, schema.get('$ref', [])):
-            verrors.extend(await self.middleware.call(
-                f'chart.release.validate_{validation_mapping[validator_def]}', verrors, value, question)
+            await self.middleware.call(
+                f'chart.release.validate_{validation_mapping[validator_def]}', verrors, value, question, schema_name,
             )
 
         return verrors
+
+    @private
+    async def validate_persistent_volume_claims(self, verrors, value, question, schema_name):
+        used_names = set()
+        for index, pvc in enumerate(value):
+            if pvc['name'] in used_names:
+                verrors.add(f'{schema_name}.{index}.name', 'Names for PVC\'s must be unique')
