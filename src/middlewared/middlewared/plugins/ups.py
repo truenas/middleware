5385a9be9f223755db17bcbed6dfaee69542e6bf
caleb <yocalebo@gmail.com>
2020-11-03 14:58:43 -0500

    shutdown passive node first when UPS triggers shutdown

--- src/middlewared/middlewared/plugins/ups.py
+++ src/middlewared/middlewared/plugins/ups.py
@@ -290,8 +290,21 @@ class UPSService(SystemServiceService):
                     f'{config["identifier"]} is ONLINE (OL).'
                 )
             else:
+                # if we shutdown the active controller while the passive is still online
+                # then we're just going to cause a failover event. Shut the passive down
+                # first and then shut the active controller down
+                if await self.middleware.call('failover.licensed'):
+                    if await self.middleware.call('failover.status') == 'MASTER':
+                        syslog.syslog(syslog.LOG_NOTICE, 'upssched-cmd "issuing shutdown" for passive controller')
+                        try:
+                            await self.middleware.call('failover.call_remote', 'ups.upssched_event', 'shutdown')
+                        except Exception as e:
+                            syslog.syslog(syslog.LOG_ERROR, f'failed shutting down passive controller with error {e}')
+                            pass
+
                 syslog.syslog(syslog.LOG_NOTICE, 'upssched-cmd "issuing shutdown"')
                 await run('upsmon', '-c', 'fsd', check=False)
+
         elif 'notify' in notify_type.lower():
             # notify_type is expected to be of the following format
             # NOTIFY-EVENT i.e NOTIFY-LOWBATT
