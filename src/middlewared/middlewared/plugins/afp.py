manetherenio/freenas

Bind services to 0.0.0.0 when removing IP address from interface configuration

src/middlewared/middlewared/plugins/afp.py
@@ -4,6 +4,7 @@ import uuid
 
 from middlewared.async_validators import check_path_resides_within_volume
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import ServiceListenMultipleDelegate
 from middlewared.schema import (accepts, Bool, Dict, Dir, Int, List, Str,
                                 Patch, UnixPerm)
 from middlewared.validators import IpAddress, Range
@@ -390,5 +391,9 @@ class AFPFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        ServiceListenMultipleDelegate(middleware, 'afp', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', AFPFSAttachmentDelegate(middleware))
     middleware.register_hook('pool.post_import', pool_post_import, sync=True)

src/middlewared/middlewared/plugins/nfs.py
@@ -5,6 +5,7 @@ import os
 import socket
 
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import ServiceListenMultipleDelegate
 from middlewared.schema import accepts, Bool, Dict, Dir, Int, IPAddr, List, Patch, Str
 from middlewared.async_validators import check_path_resides_within_volume
 from middlewared.validators import Match, Range
@@ -603,6 +604,10 @@ class NFSFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        ServiceListenMultipleDelegate(middleware, 'nfs', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', NFSFSAttachmentDelegate(middleware))
 
     middleware.register_hook('interface.post_sync', interface_post_sync)

src/middlewared/middlewared/plugins/s3.py
@@ -1,4 +1,5 @@
 from middlewared.async_validators import check_path_resides_within_volume
+from middlewared.common.listen import ServiceListenSingleDelegate
 from middlewared.schema import accepts, Bool, Dict, Int, Str
 from middlewared.validators import Match, Range
 from middlewared.service import SystemServiceService, ValidationErrors, private
@@ -136,3 +137,10 @@ class S3Service(SystemServiceService):
             )
 
         return await self.config()
+
+
+async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        ServiceListenSingleDelegate(middleware, 's3', 'bindip'),
+    )

src/middlewared/middlewared/plugins/smb.py
@@ -1,4 +1,5 @@
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import ServiceListenMultipleDelegate
 from middlewared.schema import Bool, Dict, IPAddr, List, Str, Int, Patch
 from middlewared.service import accepts, job, private, SharingService, SystemServiceService, ValidationErrors
 from middlewared.service_exception import CallError
@@ -1315,5 +1316,9 @@ class SMBFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        ServiceListenMultipleDelegate(middleware, 'smb', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', SMBFSAttachmentDelegate(middleware))
     middleware.register_hook('pool.post_import', pool_post_import, sync=True)

src/middlewared/middlewared/plugins/vpn.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import tempfile
 
+from middlewared.common.listen import ServiceListenSingleDelegate
 from middlewared.service import CallError, SystemServiceService, private
 from middlewared.schema import accepts, Bool, Dict, Int, IPAddr, Str, ValidationErrors
 import middlewared.sqlalchemy as sa
@@ -640,7 +641,11 @@ async def _event_system(middleware, event_type, args):
             await middleware.call('service.start', srv['service'])
 
 
-def setup(middleware):
+async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        ServiceListenSingleDelegate(middleware, 'openvpn.server', 'server'),
+    )
     middleware.event_subscribe('system', _event_system)
     if not os.path.exists('/usr/local/etc/rc.d/openvpn'):
         return

truenas/middleware

src/middlewared/middlewared/plugins/afp.py
@@ -4,6 +4,7 @@ import uuid
 
 from middlewared.async_validators import check_path_resides_within_volume
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import SystemServiceListenMultipleDelegate
 from middlewared.schema import (accepts, Bool, Dict, Dir, Int, List, Str,
                                 Patch, UnixPerm)
 from middlewared.validators import IpAddress, Range
@@ -390,5 +391,9 @@ class AFPFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        SystemServiceListenMultipleDelegate(middleware, 'afp', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', AFPFSAttachmentDelegate(middleware))
     middleware.register_hook('pool.post_import', pool_post_import, sync=True)

src/middlewared/middlewared/plugins/nfs.py
@@ -5,6 +5,7 @@ import os
 import socket
 
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import SystemServiceListenMultipleDelegate
 from middlewared.schema import accepts, Bool, Dict, Dir, Int, IPAddr, List, Patch, Str
 from middlewared.async_validators import check_path_resides_within_volume
 from middlewared.validators import Match, Range
@@ -603,6 +604,10 @@ class NFSFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        SystemServiceListenMultipleDelegate(middleware, 'nfs', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', NFSFSAttachmentDelegate(middleware))
 
     middleware.register_hook('interface.post_sync', interface_post_sync)

src/middlewared/middlewared/plugins/s3.py
@@ -1,4 +1,5 @@
 from middlewared.async_validators import check_path_resides_within_volume
+from middlewared.common.listen import SystemServiceListenSingleDelegate
 from middlewared.schema import accepts, Bool, Dict, Int, Str
 from middlewared.validators import Match, Range
 from middlewared.service import SystemServiceService, ValidationErrors, private
@@ -136,3 +137,10 @@ class S3Service(SystemServiceService):
             )
 
         return await self.config()
+
+
+async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        SystemServiceListenSingleDelegate(middleware, 's3', 'bindip'),
+    )

src/middlewared/middlewared/plugins/smb.py
@@ -1,4 +1,5 @@
 from middlewared.common.attachment import LockableFSAttachmentDelegate
+from middlewared.common.listen import SystemServiceListenMultipleDelegate
 from middlewared.schema import Bool, Dict, IPAddr, List, Str, Int, Patch
 from middlewared.service import accepts, job, private, SharingService, SystemServiceService, ValidationErrors
 from middlewared.service_exception import CallError
@@ -1315,5 +1316,9 @@ class SMBFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
 
 async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        SystemServiceListenMultipleDelegate(middleware, 'smb', 'bindip'),
+    )
     await middleware.call('pool.dataset.register_attachment_delegate', SMBFSAttachmentDelegate(middleware))
     middleware.register_hook('pool.post_import', pool_post_import, sync=True)

src/middlewared/middlewared/plugins/vpn.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import tempfile
 
+from middlewared.common.listen import SystemServiceListenSingleDelegate
 from middlewared.service import CallError, SystemServiceService, private
 from middlewared.schema import accepts, Bool, Dict, Int, IPAddr, Str, ValidationErrors
 import middlewared.sqlalchemy as sa
@@ -640,7 +641,11 @@ async def _event_system(middleware, event_type, args):
             await middleware.call('service.start', srv['service'])
 
 
-def setup(middleware):
+async def setup(middleware):
+    await middleware.call(
+        'interface.register_listen_delegate',
+        SystemServiceListenSingleDelegate(middleware, 'openvpn.server', 'server'),
+    )
     middleware.event_subscribe('system', _event_system)
     if not os.path.exists('/usr/local/etc/rc.d/openvpn'):
         return