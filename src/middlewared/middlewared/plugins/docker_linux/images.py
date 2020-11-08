380ed6f12c4a62be1317615f8e103cae41c47ba3
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-09 00:15:15 +0500

    Update image cache after pulling/deleting docker image

--- src/middlewared/middlewared/plugins/docker_linux/images.py
+++ src/middlewared/middlewared/plugins/docker_linux/images.py
@@ -1,11 +1,12 @@
 import aiodocker
+import asyncio
 import errno
 import os
 
 from datetime import datetime
 
 from middlewared.schema import Bool, Dict, Str
-from middlewared.service import accepts, CallError, filterable, private, CRUDService
+from middlewared.service import accepts, CallError, filterable, job, private, CRUDService
 from middlewared.utils import filter_list
 
 
@@ -50,7 +51,8 @@ class DockerImagesService(CRUDService):
             Str('tag', default=None, null=True),
         )
     )
-    async def pull(self, data):
+    @job()
+    async def pull(self, job, data):
         """
         `from_image` is the name of the image to pull. Format for the name is "registry/repo/image" where
         registry may be omitted and it will default to docker registry in this case.
@@ -61,6 +63,7 @@ class DockerImagesService(CRUDService):
         `docker_authentication` should be specified if image to be retrieved is under a private repository.
         """
         await self.docker_checks()
+        # TODO: Have job progress report downloading progress
         async with aiodocker.Docker() as docker:
             try:
                 response = await docker.images.pull(
@@ -68,6 +71,10 @@ class DockerImagesService(CRUDService):
                 )
             except aiodocker.DockerError as e:
                 raise CallError(f'Failed to pull image: {e.message}')
+
+        asyncio.ensure_future(
+            self.middleware.call('docker.images.query', [['repo_tags', 'rin', f'{data["from_image"]}:{data["tag"]}']])
+        )
         return response
 
     @accepts(
@@ -82,8 +89,12 @@ class DockerImagesService(CRUDService):
         `options.force` should be used to force delete an image even if it's in use by a stopped container.
         """
         await self.docker_checks()
+        image = await self.get_instance(id)
         async with aiodocker.Docker() as docker:
             await docker.images.delete(name=id, force=options['force'])
+
+        for tag in image['repo_tags']:
+            await self.middleware.call('docker.images.remove_tag_from_cache', tag)
 
     @private
     async def load_images_from_file(self, path):
