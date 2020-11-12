303888c90f4423a9324761ba53518636b7059356
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-12 19:17:42 +0500

    Make backing up chart releases a job

--- src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 from middlewared.schema import Str
-from middlewared.service import accepts, CallError, Service
+from middlewared.service import accepts, CallError, job, Service
 
 from .utils import BACKUP_NAME_PREFIX
 
@@ -13,7 +13,8 @@ class KubernetesService(Service):
     @accepts(
         Str('backup_name', null=True, default=None)
     )
-    def backup_chart_releases(self, backup_name):
+    @job(lock='chart_releases_backup')
+    def backup_chart_releases(self, job, backup_name):
         self.middleware.call_sync('kubernetes.validate_k8s_setup')
         # TODO: Add validation for backup name to ensure it's not already taken
         name = BACKUP_NAME_PREFIX + (backup_name or datetime.utcnow().strftime('%F_%T'))
@@ -26,8 +27,13 @@ class KubernetesService(Service):
         backup_dir = os.path.join(backup_base_dir, name)
         os.makedirs(backup_dir)
 
+        job.set_progress(10, 'Basic validation complete')
         chart_releases = self.middleware.call_sync('chart.release.query')
-        for chart_release in chart_releases:
+        len_chart_releases = len(chart_releases)
+        for index, chart_release in enumerate(chart_releases):
+            job.set_progress(
+                10 + ((index + 1) / len_chart_releases) * len_chart_releases, f'Backing up {chart_release["name"]}'
+            )
             chart_release_backup_path = os.path.join(backup_dir, chart_release['name'])
             os.makedirs(chart_release_backup_path)
             with open(os.path.join(chart_release_backup_path, 'namespace.yaml'), 'w') as f:
@@ -45,6 +51,10 @@ class KubernetesService(Service):
                 with open(os.path.join(secrets_dir, secret['metadata']['name']), 'w') as f:
                     f.write(self.middleware.call_sync('k8s.secret.export_to_yaml_internal', secret))
 
+        job.set_progress(95, 'Taking snapshot of ix-applications')
+
         self.middleware.call_sync(
             'zfs.snapshot.create', {'dataset': k8s_config['dataset'], 'name': name, 'recursive': True}
         )
+
+        job.set_progress(100, f'Backup {name!r} complete')
