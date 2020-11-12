419dbc3493e9b8aa72093d7222278ba3cd8a940c
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-12 22:26:05 +0500

    Add ability to restore backed up chart releases

---
+++ src/middlewared/middlewared/plugins/kubernetes_linux/restore.py
@@ -0,0 +1,104 @@
+import errno
+import os
+import yaml
+
+from middlewared.schema import Str
+from middlewared.service import accepts, CallError, job, Service
+
+
+class KubernetesService(Service):
+
+    @accepts(Str('backup_name'))
+    @job(lock='kubernetes_restore_backup')
+    def restore_backup(self, job, backup_name):
+        self.middleware.call_sync('kubernetes.validate_k8s_setup')
+        if backup_name not in self.middleware.call_sync('kubernetes.list_backups'):
+            raise CallError(f'Backup {backup_name!r} does not exist', errno=errno.ENOENT)
+
+        job.set_progress(5, 'Basic validation complete')
+
+        k8s_config = self.middleware.call_sync('kubernetes.config')
+        self.middleware.call_sync(
+            'zfs.snapshot.rollback', f'{k8s_config["dataset"]}@{backup_name}', {
+                'force': True,
+                'recursive': True,
+                'recursive_clones': True,
+            }
+        )
+
+        k3s_ds = os.path.join(k8s_config['dataset'], 'k3s')
+        self.middleware.call_sync('service.stop', 'kubernetes')
+
+        if os.path.exists('/etc/rancher/node/password'):
+            os.unlink('/etc/rancher/node/password')
+
+        self.middleware.call_sync('zfs.dataset.delete', k3s_ds, {'force': True, 'recursive': False})
+        self.middleware.call_sync('pool.dataset.create', {'name': k3s_ds, 'type': 'FILESYSTEM'})
+
+        self.middleware.call_sync('service.start', 'kubernetes')
+
+        job.set_progress(30, 'Kubernetes cluster re-initialized')
+
+        backup_dir = os.path.join('/mnt', k8s_config['dataset'], 'backup', backup_name)
+        releases_datasets = {}
+        for ds in self.middleware.call_sync(
+            'pool.dataset.query', [['id', '^', f'{os.path.join(k8s_config["dataset"])}/releases/']]
+        ):
+            name = ds['id'].split('/', 3)[-1].split('/', 1)[0]
+            if name not in releases_datasets:
+                releases_datasets[name]: ds
+
+        releases = os.listdir(backup_dir)
+        len_releases = len(releases)
+        restored_chart_releases = []
+
+        for index, release_name in enumerate(releases):
+            job.set_progress(
+                30 + ((index + 1) / len_releases) * 60,
+                f'Restoring helm configuration for {release_name!r} chart release'
+            )
+
+            if release_name not in releases_datasets:
+                self.logger.error(
+                    'Skipping backup of %r chart release due to missing chart release dataset', release_name
+                )
+                continue
+
+            r_backup_dir = os.path.join(backup_dir, release_name)
+            if any(
+                not os.path.exists(os.path.join(r_backup_dir, f)) for f in ('namespace.yaml', 'secrets')
+            ) or not os.listdir(os.path.join(r_backup_dir, 'secrets')):
+                self.logger.error(
+                    'Skipping backup of %r chart release due to missing configuration files', release_name
+                )
+                continue
+
+            # First we will restore namespace and then the secrets
+            with open(os.path.join(r_backup_dir, 'namespace.yaml'), 'r') as f:
+                namespace_body = yaml.load(f.read(), Loader=yaml.FullLoader)
+                self.middleware.call_sync('k8s.namespace.create', {'body': namespace_body})
+
+            secrets_dir = os.path.join(r_backup_dir, 'secrets')
+            for secret in sorted(os.listdir(secrets_dir)):
+                with open(os.path.join(secrets_dir, secret)):
+                    self.middleware.call_sync(
+                        'k8s.secret.create', {
+                            'namespace': namespace_body['metadata']['name'],
+                            'body': yaml.load(f.read(), Loader=yaml.FullLoader),
+                        }
+                    )
+
+            restored_chart_releases.append(release_name)
+
+        # Now helm will recognise the releases as valid, however we don't have any actual k8s deployed resource
+        # That will be adjusted with updating chart releases with their existing values and helm will see that
+        # k8s resources don't exist and will create them for us
+        job.set_progress(95, 'Creating kubernetes resources')
+        update_jobs = []
+        for chart_release in restored_chart_releases:
+            update_jobs.append(self.middleware.call_sync('chart.release.update', chart_release, {'values': {}}))
+
+        for update_job in update_jobs:
+            update_job.wait_sync()
+
+        job.set_progress(100, f'Restore of {backup_name!r} backup complete')
