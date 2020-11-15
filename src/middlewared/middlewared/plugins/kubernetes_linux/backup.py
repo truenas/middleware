a855186ec161278b228f6bd627cf69f9b7d4caab
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-15 15:12:42 +0500

    Retrieve backup creation date

--- src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
@@ -95,6 +95,9 @@ class KubernetesService(Service):
             for release in filter(lambda r: r in releases_datasets, os.listdir(backup_path)):
                 backup_data['releases'].append(release)
 
+            snap = self.middleware.call_sync('zfs.snapshot.get_instance', snapshot['name'])
+            backup_data['created_on'] = snap['properties']['creation']['parsed']
+
             backups[backup_name] = backup_data
 
         return backups
