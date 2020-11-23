1474961c72d83d4b08d90860d8cd275113713901
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-11-23 23:40:47 +0500

    Mark vnet / openvpn- interfaces as internal interfaces

--- src/middlewared/middlewared/plugins/interface/info_linux.py
+++ src/middlewared/middlewared/plugins/interface/info_linux.py
@@ -9,4 +9,4 @@ class InterfaceService(Service, InterfaceInfoBase):
         namespace_alias = 'interfaces'
 
     async def internal_interfaces(self):
-        return ['lo', 'tun', 'tap', 'bond', 'docker', 'veth', 'kube-bridge', 'kube-dummy-if']
+        return ['lo', 'tun', 'tap', 'bond', 'docker', 'veth', 'kube-bridge', 'kube-dummy-if', 'vnet', 'openvpn']
