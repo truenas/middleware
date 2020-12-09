74e61eeb558ab0068616e4538928f37438bf3170
themylogin <themylogin@gmail.com>
2020-12-09 14:16:21 +0100

    Ignore syslog-ng cores on FreeBSD and remove old core files automatically

--- src/middlewared/middlewared/alert/source/cores.py
+++ src/middlewared/middlewared/alert/source/cores.py
@@ -1,22 +1,45 @@
 import os
+import time
 
 from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
+from middlewared.utils.osc import IS_FREEBSD
 
 
 class CoreFilesArePresentAlertClass(AlertClass):
     category = AlertCategory.SYSTEM
     level = AlertLevel.WARNING
     title = "Core Files Found in System Database"
-    text = ("System core files were found in /var/db/system/cores: %s. Please create a ticket at https://jira.ixsystems.com/ "
-            "and remove these files.")
+    text = ("System core files were found in /var/db/system/cores. Please create a ticket at "
+            "https://jira.ixsystems.com/ and remove these files.")
 
 
 class CoreFilesArePresentAlertSource(ThreadedAlertSource):
     def check_sync(self):
+        cores = "/var/db/system/cores"
         try:
-            files = sorted(os.listdir("/var/db/system/cores"))
+            listdir = os.listdir(cores)
         except Exception:
-            files = []
+            return
 
-        if files:
-            return Alert(CoreFilesArePresentAlertClass, ", ".join(files))
+        has_core_file = False
+        for file in listdir:
+            if not file.endswith(".core"):
+                continue
+
+            path = os.path.join(cores, file)
+            if not os.path.isfile(path):
+                continue
+
+            unlink = False
+            if IS_FREEBSD and file == "syslog-ng.core":
+                unlink = True
+            elif os.stat(path).st_mtime < time.time() - 86400 * 5:
+                unlink = True
+            else:
+                has_core_file = True
+
+            if unlink:
+                os.unlink(path)
+
+        if has_core_file:
+            return Alert(CoreFilesArePresentAlertClass)
