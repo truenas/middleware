c6460e35bcd4cbe91f36664c69698b8db66d1c50
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-12-16 01:09:26 +0500

    Update migration revision ids

--- src/middlewared/middlewared/alembic/versions/12.1/2020-11-23_04-15_gpu_isolation.py
+++ src/middlewared/middlewared/alembic/versions/12.1/2020-11-23_04-15_gpu_isolation.py
@@ -2,7 +2,7 @@
 GPU isolation property
 
 Revision ID: 72fc294965d1
-Revises: ce614715260a
+Revises: 3d611f8cc676
 Create Date: 2020-11-23 04:15:42.818433+00:00
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 revision = '72fc294965d1'
-down_revision = 'ce614715260a'
+down_revision = '3d611f8cc676'
 branch_labels = None
 depends_on = None
 
