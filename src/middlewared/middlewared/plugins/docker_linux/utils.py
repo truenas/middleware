63b1962005092d35c6d19f014cc7a9a5d856b163
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-01-07 23:19:56 +0500

    Retrieve docker image repo digests

--- src/middlewared/middlewared/plugins/docker_linux/utils.py
+++ src/middlewared/middlewared/plugins/docker_linux/utils.py
@@ -1,3 +1,4 @@
+DOCKER_CONTENT_DIGEST_HEADER = 'Docker-Content-Digest'
 DEFAULT_DOCKER_IMAGES_LIST_PATH = '/usr/local/share/docker_images/image-list.txt'
 DEFAULT_DOCKER_REGISTRY = 'registry-1.docker.io'
 DEFAULT_DOCKER_REPO = 'library'
