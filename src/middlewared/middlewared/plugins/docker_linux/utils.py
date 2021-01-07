a8001ddd5dbb01f2b4629e809f7ec799c03e112a
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-01-07 23:38:52 +0500

    Add ability to make head requests in docker client implementation

--- src/middlewared/middlewared/plugins/docker_linux/utils.py
+++ src/middlewared/middlewared/plugins/docker_linux/utils.py
@@ -1,5 +1,5 @@
-DOCKER_CONTENT_DIGEST_HEADER = 'Docker-Content-Digest'
 DEFAULT_DOCKER_IMAGES_LIST_PATH = '/usr/local/share/docker_images/image-list.txt'
 DEFAULT_DOCKER_REGISTRY = 'registry-1.docker.io'
 DEFAULT_DOCKER_REPO = 'library'
 DEFAULT_DOCKER_TAG = 'latest'
+DOCKER_CONTENT_DIGEST_HEADER = 'Docker-Content-Digest'
