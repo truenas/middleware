0b6fb3b827dc76f1a394dfbf5356385204ad3f2e
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-01-08 19:39:47 +0500

    Get more information for deployed resources in k8s cluster in debug

--- src/freenas/usr/local/libexec/freenas-debug/kubernetes_linux/kubernetes.sh
+++ src/freenas/usr/local/libexec/freenas-debug/kubernetes_linux/kubernetes.sh
@@ -12,8 +12,40 @@ kubernetes_func()
 
 	k8s_running="$(midclt call service.started kubernetes)"
 	if [ "$k8s_running" = "True" ]; then
-		section_header "k3s kubectl cluster-info dump"
-		k3s kubectl cluster-info dump
+		section_header "k3s kubectl describe nodes"
+		k3s kubectl describe nodes
+		section_footer
+
+		section_header "k3s kubectl get pods,svc,daemonsets,deployments,statefulset,sc,pvc,ns,job --all-namespaces -o wide"
+		k3s kubectl get pods,svc,daemonsets,deployments,statefulset,sc,pvc,ns,job --all-namespaces -o wide
+		section_footer
+
+		section_header "k3s kubectl describe deployments --all-namespaces"
+		k3s kubectl describe deployments --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe pods --all-namespaces"
+		k3s kubectl describe pods --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe services --all-namespaces"
+		k3s kubectl describe services --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe statefulset --all-namespaces"
+		k3s kubectl describe statefulset --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe job --all-namespaces"
+		k3s kubectl describe job --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe cronjob --all-namespaces"
+		k3s kubectl describe cronjob --all-namespaces
+		section_footer
+
+		section_header "k3s kubectl describe daemonsets --all-namespaces"
+		k3s kubectl describe daemonsets --all-namespaces
 		section_footer
 
 		section_header "docker ps -a"
@@ -22,6 +54,10 @@ kubernetes_func()
 
 		section_header "docker images -a"
 		docker images -a
+		section_footer
+
+		section_header "k3s kubectl cluster-info dump"
+		k3s kubectl cluster-info dump
 		section_footer
 	fi
 }
