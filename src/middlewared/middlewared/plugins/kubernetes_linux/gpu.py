b48d760770436b13d189cf69da479eee3942c69f
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-01-22 20:56:49 +0500

    Add support for INTEL GPU in k8s

--- src/middlewared/middlewared/plugins/kubernetes_linux/gpu.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/gpu.py
@@ -30,8 +30,51 @@ GPU_CONFIG = {
                 }
             }
         }
+    },
+    'INTEL': {
+        'apiVersion': 'apps/v1',
+        'kind': 'DaemonSet',
+        'metadata': {'name': 'intel-gpu-plugin', 'labels': {'app': 'intel-gpu-plugin'}, 'namespace': 'kube-system'},
+        'spec': {
+            'selector': {'matchLabels': {'app': 'intel-gpu-plugin'}},
+            'template': {
+                'metadata': {'labels': {'app': 'intel-gpu-plugin'}},
+                'spec': {
+                    'initContainers': [{
+                        'name': 'intel-gpu-initcontainer',
+                        'image': 'intel/intel-gpu-initcontainer:devel',
+                        'imagePullPolicy': 'IfNotPresent', 'securityContext': {'readOnlyRootFilesystem': True},
+                        'volumeMounts': [{
+                            'mountPath': '/etc/kubernetes/node-feature-discovery/source.d/', 'name': 'nfd-source-hooks'
+                        }]
+                    }],
+                    'containers': [{
+                        'name': 'intel-gpu-plugin',
+                        'env': [{'name': 'NODE_NAME', 'valueFrom': {'fieldRef': {'fieldPath': 'spec.nodeName'}}}],
+                        'image': 'intel/intel-gpu-plugin:devel', 'imagePullPolicy': 'IfNotPresent',
+                        'securityContext': {'readOnlyRootFilesystem': True},
+                        'volumeMounts': [
+                            {'name': 'devfs', 'mountPath': '/dev/dri', 'readOnly': True},
+                            {'name': 'sysfs', 'mountPath': '/sys/class/drm', 'readOnly': True},
+                            {'name': 'kubeletsockets', 'mountPath': '/var/lib/kubelet/device-plugins'}
+                        ]
+                    }],
+                    'volumes': [
+                        {'name': 'devfs', 'hostPath': {'path': '/dev/dri'}},
+                        {'name': 'sysfs', 'hostPath': {'path': '/sys/class/drm'}},
+                        {'name': 'kubeletsockets', 'hostPath': {'path': '/var/lib/kubelet/device-plugins'}},
+                        {
+                            'name': 'nfd-source-hooks', 'hostPath': {
+                                'path': '/etc/kubernetes/node-feature-discovery/source.d/',
+                                'type': 'DirectoryOrCreate'
+                            }
+                        }
+                    ],
+                    'nodeSelector': {'kubernetes.io/arch': 'amd64'}
+                }
+            }
+        }
     }
-
 }
 
 
@@ -62,8 +105,8 @@ class KubernetesGPUService(Service):
             f'{d["metadata"]["namespace"]}_{d["metadata"]["name"]}': d
             for d in await self.middleware.call('k8s.daemonset.query')
         }
-        # we only support nvidia for now
-        supported_gpus = {'NVIDIA'}
+
+        supported_gpus = {'NVIDIA', 'INTEL'}
         found_gpus = supported_gpus.intersection(set([gpu['vendor'] for gpu in gpus]))
         if found_gpus:
             to_remove = to_remove - found_gpus
