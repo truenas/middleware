4f8b788b6d31cf6e7da416870a00ff8e40f163e6
caleb <yocalebo@gmail.com>
2021-01-27 08:27:10 -0500

    make gluster.peer use new wrapper and convert to async

--- src/middlewared/middlewared/plugins/gluster_linux/peer.py
+++ src/middlewared/middlewared/plugins/gluster_linux/peer.py
@@ -1,26 +1,32 @@
-from glustercli.cli import peer
-from middlewared.async_validators import resolve_hostname
-from middlewared.schema import Dict, Str
-from middlewared.service import (accepts, private, job,
-                                 CallError, Service,
-                                 ValidationErrors)
-from .utils import GlusterConfig, run_method
-
-import subprocess
 import xml.etree.ElementTree as ET
+from glustercli.cli import peer
+
+from middlewared.utils import filter_list, run
+from middlewared.schema import Dict, Str, Bool
+from middlewared.service import (accepts, private, job, filterable,
+                                 CallError, CRUDService)
+from .utils import GlusterConfig
 
 
 GLUSTER_JOB_LOCK = GlusterConfig.CLI_LOCK.value
 
 
-class GlusterPeerService(Service):
+class GlusterPeerService(CRUDService):
 
     class Config:
         namespace = 'gluster.peer'
 
-    @private
-    def _parse_peer(self, p):
+    @filterable
+    async def query(self, filters=None, options=None):
+        peers = []
+        if await self.middleware.call('service.started', 'glusterd'):
+            peers = await self.middleware.call('gluster.peer.status')
+            peers = list(map(lambda i: dict(i, id=i['uuid']), peers))
 
+        return filter_list(peers, filters, options)
+
+    @private
+    async def _parse_peer(self, p):
         data = {
             'uuid': p.find('uuid').text,
             'hostname': p.find('hostname').text,
@@ -35,12 +41,11 @@ class GlusterPeerService(Service):
         return data
 
     @private
-    def _parse_peer_status_xml(self, data):
-
+    async def _parse_peer_status_xml(self, data):
         peers = []
         for _ in data.findall('peerStatus/peer'):
             try:
-                peers.append(self._parse_peer(_))
+                peers.append(await self._parse_peer(_))
             except Exception as e:
                 raise CallError(
                     f'Failed parsing peer information with error: {e}'
@@ -48,148 +53,90 @@ class GlusterPeerService(Service):
 
         return peers
 
-    @private
-    async def resolve_host_or_ip(self, hostname, verrors):
-
-        args = (self.middleware, verrors, 'resolve_host_or_ip', hostname)
-        return await resolve_hostname(*args)
-
-    @private
-    def common_validation(self, hostname=None):
-
-        verrors = ValidationErrors()
-
-        if hostname:
-            self.middleware.call_sync(
-                'gluster.peer.resolve_host_or_ip', hostname, verrors)
-
-        verrors.check()
-
-    @accepts(
-        Dict(
-            'probe_peer_create',
-            Str('hostname', required=True, max_length=253)
-        )
-    )
+    @accepts(Dict(
+        'peer_create',
+        Str('hostname', required=True, max_length=253)
+    ))
     @job(lock=GLUSTER_JOB_LOCK)
-    def create(self, job, data):
+    async def do_create(self, job, data):
         """
         Add peer to the Trusted Storage Pool.
 
-        `hostname` can be an IP(v4/v6) address or DNS name.
+        `hostname` String representing an IP(v4/v6) address or DNS name
         """
 
-        hostname = data.get('hostname')
+        await self.middleware.call('gluster.method.run', peer.attach, {'args': (data['hostname'],)})
+        return await self.middleware.call('gluster.peer.query', [('hostname', '=', data['hostname'])])
 
-        self.common_validation(hostname=hostname)
-
-        return run_method(peer.attach, hostname)
-
-    @accepts(
-        Dict(
-            'probe_peer_delete',
-            Str('hostname', required=True, max_length=253)
-        )
-    )
+    @accepts(Str('id'))
     @job(lock=GLUSTER_JOB_LOCK)
-    def delete(self, job, data):
+    async def do_delete(self, job, id):
         """
-        Remove peer of `hostname` from the Trusted Storage Pool.
-        """
+        Remove peer of `id` from the Trusted Storage Pool.
 
-        hostname = data.get('hostname')
-
-        self.common_validation(hostname=hostname)
-
-        return run_method(peer.detach, hostname)
-
-    @accepts()
-    def status(self):
-        """
-        List the status of peers in the Trusted Storage Pool
-        excluding localhost.
+        `id` String representing the uuid of the peer
         """
 
-        return run_method(peer.status)
+        await self.middleware.call(
+            'gluster.method.run', peer.detach, {'args': ((await self.get_instance(id))['hostname'],)})
 
-    @accepts()
-    def pool(self):
+    @accepts(Dict(
+        'peer_status',
+        Bool('localhost', default=True),
+    ))
+    async def status(self, data):
         """
-        List the status of peers in the Trusted Storage Pool
-        including localhost.
+        List the status of peers in the Trusted Storage Pool.
+
+        `localhost` Boolean if True, include localhost else exclude localhost
         """
 
-        final = None
+        peers = await self.middleware.call('gluster.method.run', peer.status)
+        if not data['localhost']:
+            return peers
 
-        # get the local viewpoint of the remote peers in the TSP
-        if local_view := run_method(peer.status):
-            remote_node = None
-            # need to pull out a remote peer (that's connected)
-            for i in local_view:
-                if i['connected'] == 'Connected' and i['hostname'] != 'localhost':
-                    remote_node = i['hostname']
-                    break
+        try:
+            remote_node = next(i['hostname'] for i in peers if i['connected'] == 'Connected')
+        except StopIteration:
+            raise CallError('All remote peers are disconnected.')
 
-            if remote_node is None:
-                raise CallError('All remote peers are disconnected.')
-
-            # now we need to run the same command as `run_method(peer.status)`
-            # but specifying a remote peer to get the "remote_local_view"
-            command = [
-                'gluster',
-                f'--remote-host={remote_node}',
-                'peer', 'status', '--xml'
-            ]
-            cp = subprocess.run(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
+        # this is the same as running `gluster.method.run, peer.status` but
+        # running it on the `remote_node` so that we can get 2 different
+        # "views" of the peers in the TSP.
+        command = ['gluster', f'--remote-host={remote_node}', 'peer', 'status', '--xml']
+        cp = await run(command, check=False)
+        if cp.returncode:
+            # the gluster cli utility will return stderr
+            # to stdout and vice versa on certain failures.
+            # account for this and decode appropriately
+            err = cp.stderr if cp.stderr else cp.stdout
+            if isinstance(err, bytes):
+                err = err.decode()
+            raise CallError(
+                f'Failed running remote peer status with error: {err.strip()}'
             )
-            if cp.returncode:
-                # the gluster cli utility will return stderr
-                # to stdout and vice versa on certain failures.
-                # account for this and decode appropriately
-                err = cp.stderr if cp.stderr else cp.stdout
-                if isinstance(err, bytes):
-                    err = err.decode()
-                raise CallError(
-                    f'Failed running remote peer status with error: {err.strip()}'
-                )
 
-            # build our data structure by parsing the xml
-            remote_local_view = ET.fromstring(cp.stdout)
-            remote_local_view = self._parse_peer_status_xml(remote_local_view)
+        # build our data structure by parsing the xml
+        remote_local_view = ET.fromstring(cp.stdout.decode())
+        remote_local_view = await self._parse_peer_status_xml(remote_local_view)
 
-            # now we compare the 2 "viewpoints" and deduce which IP address
-            # is our own
-            final = local_view.copy()
+        # this should only ever produce 1 entry
+        our_ip = [i for i in remote_local_view if i not in peers]
+        if len(our_ip) != 1:
+            raise CallError(
+                f'Remote peer: {remote_node} sees these peers: {remote_local_view}'
+                f'The local peer sees these peers: {peers}.'
+                'The local and remote peers should be the same quantity.'
+            )
 
-            # this should only ever produce 1 entry
-            our_ip = [i for i in remote_local_view if i not in local_view]
-            if len(our_ip) != 1:
-                raise CallError(
-                    f'Remote peer: {remote_node} sees these peers: '
-                    f'{remote_local_view}'
-                    f'The local peer sees these peers: {local_view}.'
-                    'The local and remote peers should be the same quantity.'
-                )
+        peers.append(our_ip[0])
 
-            final.append(our_ip[0])
-
-        return list(final)
+        return peers
 
     @accepts()
     async def ips_available(self):
         """
-        List of IPv4/v6 addresses available that can be used
-        as the `peer_name` when creating a gluster volume.
-
-        NOTE:
-            This will only return statically assigned IPs.
-            If this is an HA system, this will only return
-            the VIP addresses that have been configured on
-            the system.
+        Return list of VIP(v4/v6) addresses available on the system
         """
 
         return [
