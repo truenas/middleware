941976e995fe783f72b84208a1d28f56eb86bb73
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-01-31 19:22:15 +0500

    Raise error if storage path for minio is not set

--- src/middlewared/middlewared/plugins/service_/services/s3.py
+++ src/middlewared/middlewared/plugins/service_/services/s3.py
@@ -1,3 +1,5 @@
+from middlewared.service import CallError
+
 from .base import SimpleService
 
 
@@ -10,3 +12,7 @@ class S3Service(SimpleService):
     freebsd_pidfile = "/var/run/minio.pid"
 
     systemd_unit = "minio"
+
+    async def before_start(self):
+        if not (await self.middleware.call('s3.config'))['storage_path']:
+            raise CallError('Storage path must bet set to start S3 service')
