1804214cddc269f41bbf98ca18d954bc712accc0
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-01 20:17:22 +0500

    Add branch to path when converting git repo to path

--- src/middlewared/middlewared/plugins/catalogs_linux/utils.py
+++ src/middlewared/middlewared/plugins/catalogs_linux/utils.py
@@ -15,8 +15,8 @@ GIT_LOCK = defaultdict(threading.Lock)
 logger = logging.getLogger('catalog_utils')
 
 
-def convert_repository_to_path(git_repository_uri):
-    return git_repository_uri.split('://', 1)[-1].replace('/', '_').replace('.', '_')
+def convert_repository_to_path(git_repository_uri, branch):
+    return git_repository_uri.split('://', 1)[-1].replace('/', '_').replace('.', '_') + f'_{branch}'
 
 
 def clone_repository(repository_uri, destination, depth):
@@ -32,7 +32,7 @@ def get_repo(destination):
 def pull_clone_repository(repository_uri, parent_dir, branch, depth=None, raise_exception=False):
     with GIT_LOCK[repository_uri]:
         os.makedirs(parent_dir, exist_ok=True)
-        destination = os.path.join(parent_dir, convert_repository_to_path(repository_uri))
+        destination = os.path.join(parent_dir, convert_repository_to_path(repository_uri, branch))
         repo = get_repo(destination)
         clone_repo = not bool(repo)
         if repo:
