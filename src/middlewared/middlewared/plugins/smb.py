e30dd0f3d2b338567453e86d3bff51fa325fcd92
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-01 23:16:57 +0500

    Bug fix for handling smb shares without a path (#6328)

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -1403,6 +1403,9 @@ class SMBFSAttachmentDelegate(LockableFSAttachmentDelegate):
         await reg_sync.wait()
         await self.middleware.call('service.reload', 'mdns')
 
+    async def is_child_of_path(self, resource, path):
+        return await super().is_child_of_path(resource, path) if resource.get(self.path_field) else False
+
 
 async def setup(middleware):
     await middleware.call(
