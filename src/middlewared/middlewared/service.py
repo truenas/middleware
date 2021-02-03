b83c956692493d23b14d232ceb6e943f907ef139
Andrew Walker <awalker@ixsystems.com>
2021-02-03 08:52:51 -0500

    Fix potential division-by-zero error
    Ensure that we don't call core.bulk with empty list of params from
    restart_services_after_unlock(). Also return early in core.bulk
    if no params are passed in.

--- src/middlewared/middlewared/service.py
+++ src/middlewared/middlewared/service.py
@@ -1318,6 +1318,9 @@ class CoreService(Service):
         exception
         """
         statuses = []
+        if not params:
+            return statuses
+
         progress_step = 100 / len(params)
         current_progress = 0
 
