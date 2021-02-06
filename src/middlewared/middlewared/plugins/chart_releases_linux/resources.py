c5db3017e375307ec52668b57e7d8887b15fd255
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-06 20:43:24 +0500

    Retrieve pods with container separately

--- src/middlewared/middlewared/plugins/chart_releases_linux/resources.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/resources.py
@@ -1,5 +1,5 @@
 from middlewared.schema import Str
-from middlewared.service import accepts, Service
+from middlewared.service import accepts, private, Service
 
 
 class ChartReleaseService(Service):
@@ -7,14 +7,8 @@ class ChartReleaseService(Service):
     class Config:
         namespace = 'chart.release'
 
-    @accepts(Str('release_name'))
-    async def pod_console_choices(self, release_name):
-        """
-        Returns choices for console access to a chart release.
-
-        Output is a dictionary with names of pods as keys and containing names of containers which the pod
-        comprises of.
-        """
+    @private
+    async def retrieve_pod_with_containers(self, release_name):
         await self.middleware.call('kubernetes.validate_k8s_setup')
         release = await self.middleware.call(
             'chart.release.query', [['id', '=', release_name]], {'get': True, 'extra': {'retrieve_resources': True}}
@@ -26,6 +20,16 @@ class ChartReleaseService(Service):
                 choices[pod['metadata']['name']].append(container['name'])
 
         return choices
+
+    @accepts(Str('release_name'))
+    async def pod_console_choices(self, release_name):
+        """
+        Returns choices for console access to a chart release.
+
+        Output is a dictionary with names of pods as keys and containing names of containers which the pod
+        comprises of.
+        """
+        return await self.retrieve_pod_with_containers(release_name)
 
     @accepts()
     async def nic_choices(self):
