739fda27ecdde6145307c6c57dc9c5e7675cf6d9
themylogin <themylogin@gmail.com>
2021-02-09 23:03:00 +0100

    TFTP for SCALE

--- src/middlewared/middlewared/plugins/service_/services/tftp.py
+++ src/middlewared/middlewared/plugins/service_/services/tftp.py
@@ -1,10 +1,12 @@
+from middlewared.utils.osc import IS_FREEBSD
+
 from .base import SimpleService
 
 
 class TFTPService(SimpleService):
     name = "tftp"
 
-    etc = ["inetd"]
+    etc = ["inetd"] if IS_FREEBSD else ["tftp"]
 
     freebsd_rc = "inetd"
     freebsd_pidfile = "/var/run/inetd.pid"
