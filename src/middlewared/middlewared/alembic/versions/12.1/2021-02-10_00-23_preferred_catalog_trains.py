7b1de2e8525c71956961e04257dadfd2b1d90136
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-09 23:08:11 +0500

    Add preferred train attribute for catalogs

---
+++ src/middlewared/middlewared/alembic/versions/12.1/2021-02-10_00-23_preferred_catalog_trains.py
@@ -0,0 +1,27 @@
+"""
+Add Preferred Train option for catalogs
+
+Revision ID: daa17e858eaf
+Revises: c747d1692290
+Create Date: 2021-02-10 00:23:15.609666+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'daa17e858eaf'
+down_revision = 'c747d1692290'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('services_catalog', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('preferred_trains', sa.TEXT(), nullable=True))
+
+    op.execute("UPDATE services_catalog SET preferred_trains = '[\"charts\"]'")
+
+
+def downgrade():
+    pass
