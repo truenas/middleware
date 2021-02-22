ca5b2f2f3c8c4746d2b57ef892dfb378aef96081
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-22 13:08:17 +0500

    Remove minio certs directory when certificates are not configured
    (cherry picked from commit b9e9a1c47f4ae6e7d6a6014c3659503ae92aa7a1)

--- src/middlewared/middlewared/etc_files/local/minio/certificates.py
+++ src/middlewared/middlewared/etc_files/local/minio/certificates.py
@@ -1,20 +1,23 @@
 import grp
 import os
 import pwd
+import shutil
 
 
 def render(service, middleware):
     s3 = middleware.call_sync('s3.config')
+    minio_path = '/usr/local/etc/minio'
 
     cert = s3.get('certificate')
     if not cert:
+        # We do this so that minio does not pick up certs in this directory and sets itself in https mode even
+        # though configuration does not has any certificate in db set and these are old leftovers.
+        shutil.rmtree(minio_path, ignore_errors=True)
         return
     else:
         middleware.call_sync('certificate.cert_services_validation', cert, 's3.certificate')
 
         cert = middleware.call_sync('certificate._get_instance', cert)
-
-        minio_path = "/usr/local/etc/minio"
 
         minio_certpath = os.path.join(minio_path, "certs")
         minio_CApath = os.path.join(minio_certpath, "CAs")
