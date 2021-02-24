5dd594d50e6911c1211c052506c157ba7be4d816
themylogin <themylogin@gmail.com>
2021-02-24 11:01:23 +0100

    pwenc.decrypt does not raise exceptions
    (cherry picked from commit 289601cae4c8a2a42a0b2fa8963fdb12325cc6d0)

--- src/middlewared/middlewared/plugins/pwenc.py
+++ src/middlewared/middlewared/plugins/pwenc.py
@@ -52,10 +52,8 @@ class PWEncService(Service):
         except IndexError:
             self.middleware.call_sync('datastore.insert', 'system.settings', {})
             settings = self.middleware.call_sync('datastore.config', 'system.settings')
-        try:
-            return self.decrypt(settings['stg_pwenc_check']) == PWENC_CHECK
-        except (IOError, ValueError):
-            return False
+
+        return self.decrypt(settings['stg_pwenc_check']) == PWENC_CHECK
 
     @classmethod
     def get_secret(cls):
