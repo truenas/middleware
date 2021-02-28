83a1f6b29aff97e56893f1cd05eefcf2f3bc410d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-28 23:52:55 +0500

    Mark cert attachment register methods as private

--- src/middlewared/middlewared/plugins/crypto_/attachments.py
+++ src/middlewared/middlewared/plugins/crypto_/attachments.py
@@ -1,4 +1,4 @@
-from middlewared.service import Service
+from middlewared.service import private, Service
 
 
 class CertificateService(Service):
@@ -10,12 +10,15 @@ class CertificateService(Service):
         super(CertificateService, self).__init__(*args, **kwargs)
         self.delegates = []
 
+    @private
     async def register_attachment_delegate(self, delegate):
         self.delegates.append(delegate)
 
+    @private
     async def in_use_attachments(self, cert_id):
-        return [delegate for delegate in self.delegates if delegate.state(cert_id)]
+        return [delegate for delegate in self.delegates if await delegate.state(cert_id)]
 
+    @private
     async def redeploy_cert_attachments(self, cert_id):
         for delegate in await self.in_use_attachments(cert_id):
             await delegate.redeploy(cert_id)
--- src/middlewared/middlewared/plugins/idmap_/cert_attachments.py
+++ src/middlewared/middlewared/plugins/idmap_/cert_attachments.py
@@ -6,6 +6,9 @@ class IdmapCertificateAttachmentDelegate(CertificateCRUDServiceAttachmentDelegat
     CERT_FILTER_KEY = 'certificate.id'
     NAMESPACE = 'idmap'
 
+    async def redeploy(self, cert_id):
+        pass
+
 
 async def setup(middleware):
     await middleware.call('certificate.register_attachment_delegate', IdmapCertificateAttachmentDelegate(middleware))
