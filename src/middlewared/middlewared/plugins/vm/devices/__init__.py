7337fedb8092f92fba0a67704535f0565b905fbf
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-02 20:31:58 +0500

    Use DISPLAY as name for vnc device

--- src/middlewared/middlewared/plugins/vm/devices/__init__.py
+++ src/middlewared/middlewared/plugins/vm/devices/__init__.py
@@ -2,6 +2,6 @@ from .cdrom import CDROM
 from .nic import NIC
 from .pci import PCI
 from .storage_devices import DISK, RAW
-from .display import RemoteDisplay
+from .display import DISPLAY
 
-__all__ = ['CDROM', 'DISK', 'NIC', 'PCI', 'RAW', 'RemoteDisplay']
+__all__ = ['CDROM', 'DISK', 'NIC', 'PCI', 'RAW', 'DISPLAY']
