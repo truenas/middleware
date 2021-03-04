557680c447076b288673cb35739c74b4f8bdbeca
themylogin <themylogin@gmail.com>
2021-03-04 15:13:28 +0100

    Whitespaces in path names are not supported by exports file format

--- src/middlewared/middlewared/plugins/nfs.py
+++ src/middlewared/middlewared/plugins/nfs.py
@@ -431,6 +431,12 @@ class SharingNFSService(SharingService):
 
         dev = None
         for i, path in enumerate(data["paths"]):
+            if " " in path:
+                verrors.add(
+                    f'{schema_name}.paths.{i}',
+                    'Paths for a NFS share should not contain whitespaces'
+                )
+
             stat = os.stat(path)
             if dev is None:
                 dev = stat.st_dev
