e031e72420102ee433045d1eeae44e7efa990446
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2021-03-05 15:14:08 -0500

    NAS-109458 / 21.04 / try smartctl SAS to SATA translation in debug (#6444)
    * try smartctl SAS to SATA translation in debug
    
    * also log a message that translation was used
    
    * fix typo in comment
    
    * msg -> $msg
    
    * properly handle nvme disks and only run smartctl on sd/vd/da devices
    
    * use timeout when trying translation and log accordingly
    
    * use nvmecontrol nsid on freeBSD

--- src/freenas/usr/local/libexec/freenas-debug/smart/smart.sh
+++ src/freenas/usr/local/libexec/freenas-debug/smart/smart.sh
@@ -81,21 +81,77 @@ smart_func()
 	ORDER BY +id"
 	section_footer
 
-	section_header "smartctl -a"
-	if [ -f /tmp/smart.out ]; then
-		rm -f /tmp/smart.out
-	fi
+	section_header "smartctl output"
+	rm -f /tmp/smart.out 2>/dev/null
 
 	if is_linux; then
-		disks=$(lsblk -ndo name | grep -v '^sr')
+		# from /proc/devices
+		# only care about sd/vd/nvme major numbers
+		# sd* = 8 - 135
+		# vd* (virtio) = 254
+		# nvmeXnY* = 259
+		include="8,65,66,67,68,69,70,71,128,129,130,131,132,133,134,135,254,259"
+		disks=$(lsblk -ndo name -I $include)
 	else
 		disks=$(sysctl -n kern.disks)
 	fi
 
-	for i in $disks
-	do
-    		echo /dev/$i >> /tmp/smart.out
-		smartctl -a /dev/$i >> /tmp/smart.out
+	# SAS to SATA interposers could be involed. Unfortunately,
+	# there is no "easy" way of identifying that there is
+	# one involved without doing some extravagant reading of
+	# specific VPD pages from the device itself. Even doing
+	# that is fraught with errors because the interposer creates
+	# those pages, sometimes not creating them at all.
+	# So, instead, we'll try to tell smartctl to do the translation
+	# and if it fails (which it will on proper SAS devices) then
+	# we'll try to run it without translation
+	for i in $disks; do
+		case "$i" in
+		    nvd*)
+			ns=$(nvmecontrol nsid $i | awk '{print $1}')
+			output=$(smartctl -a /dev/$ns)
+		        msg="(NVME DEVICE DETECTED)"
+		        # double-quotes are important here to
+		        # maintain original formatting
+		        echo "/dev/$ns $msg" >> /tmp/smart.out
+		        echo "$output" >> /tmp/smart.out
+		        echo "" >> /tmp/smart.out
+		    ;;
+		    da*|sd*|vd*)
+			# try with translation first
+			msg="(USING TRANSLATION)"
+			output=$(timeout 3 smartctl -a -d sat /dev/$i)
+			rc=$?
+			if [ $rc -ne 0 ]; then
+			    if [ $rc -eq 124 ]; then
+				# timeout returns 124 rc when
+				# the called command is terminated
+				output="TIMEOUT"
+				msg="(DEVICE TOOK TOO LONG TO RESPOND)"
+			    else
+			        # oops try without translation
+			        output=$(smartctl -a /dev/$i)
+			        msg="(NOT USING TRANSLATION)"
+			    fi
+			fi
+			# double-quotes are important here to
+			# maintain original formatting
+			echo "/dev/$i $msg" >> /tmp/smart.out
+			echo "$output" >> /tmp/smart.out
+			echo "" >> /tmp/smart.out
+		    ;;
+	            nvme*)
+			# linux returns nvmeXnsY devices by
+			# default so catch it here.
+			output=$(smartctl -a /dev/$i)
+			msg="(NVME DEVICE DETECTED)"
+			# double-quotes are important here to
+			# maintain original formatting
+			echo "/dev/$i $msg" >> /tmp/smart.out
+			echo "$output" >> /tmp/smart.out
+			echo "" >> /tmp/smart.out
+		    ;;
+		esac
 	done
 	cat /tmp/smart.out
 	${FREENAS_DEBUG_MODULEDIR}/smart/smart.nawk < /tmp/smart.out
