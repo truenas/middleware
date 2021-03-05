d5b8dc0ea446c7841f6381b369ef8342f763a54a
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-05 17:55:55 +0500

    Delete snapshot from target destination as well

--- src/middlewared/middlewared/plugins/kubernetes_linux/update.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/update.py
@@ -237,26 +237,29 @@ class KubernetesService(ConfigService):
             }
         )
 
-        old_ds = applications_ds_name(old_pool)
-        new_ds = applications_ds_name(new_pool)
-        migrate_job = await self.middleware.call(
-            'replication.run_onetime', {
-                'direction': 'PUSH',
-                'transport': 'LOCAL',
-                'source_datasets': [old_ds],
-                'target_dataset': new_ds,
-                'recursive': True,
-                'also_include_naming_schema': [MIGRATION_NAMING_SCHEMA],
-                'retention_policy': 'NONE',
-                'replicate': True
-            }
-        )
-        await migrate_job.wait()
         try:
+            old_ds = applications_ds_name(old_pool)
+            new_ds = applications_ds_name(new_pool)
+            migrate_job = await self.middleware.call(
+                'replication.run_onetime', {
+                    'direction': 'PUSH',
+                    'transport': 'LOCAL',
+                    'source_datasets': [old_ds],
+                    'target_dataset': new_ds,
+                    'recursive': True,
+                    'also_include_naming_schema': [MIGRATION_NAMING_SCHEMA],
+                    'retention_policy': 'NONE',
+                    'replicate': True
+                }
+            )
+            await migrate_job.wait()
             if migrate_job.error:
                 raise CallError(f'Failed to migrate {old_ds} to {new_ds}: {migrate_job.error}')
         finally:
             await self.middleware.call('zfs.snapshot.delete', snap_details['id'], {'recursive': True})
+            snap_name = f'{applications_ds_name(new_pool)}@{snap_details["snapshot_name"]}'
+            if await self.middleware.call('zfs.snapshot.query', [['id', '=', snap_name]]):
+                await self.middleware.call('zfs.snapshot.delete', snap_name, {'recursive': True})
 
     @accepts()
     async def bindip_choices(self):
