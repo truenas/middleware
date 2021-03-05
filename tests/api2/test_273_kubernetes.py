8a433ec0864ccb45757abc22d4dce84f051cdaf3
Eric Turgeon <4249848+ericbsd@users.noreply.github.com>
2021-03-05 10:42:29 -0400

    DOCS-1843 / 21.04 / Changed the setup kubernetes test to use the system static ip. (#6547)
    * changed the setup kubernetes test to use the system static ip.
    
    Added assert to test 01 to make sure that the system static ip is present.
    
    * changed back wait_on_job to 300 in kubernetes test 02
    
    * changed test 01 to look for the ip in the dict keys in kubernetes test

--- tests/api2/test_273_kubernetes.py
+++ tests/api2/test_273_kubernetes.py
@@ -19,7 +19,8 @@ def test_01_get_kubernetes_bindip_choices():
     results = GET('/kubernetes/bindip_choices/')
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
-    assert results.json()['0.0.0.0'], results.text
+    assert '0.0.0.0' in results.json(), results.text
+    assert ip in results.json(), results.text
 
 
 @pytest.mark.dependency(name='setup_kubernetes')
@@ -31,7 +32,7 @@ def test_02_setup_kubernetes(request):
         'pool': pool_name,
         'route_v4_interface': interface,
         'route_v4_gateway': gateway,
-        'node_ip': '0.0.0.0'
+        'node_ip': ip
     }
     results = PUT('/kubernetes/', payload)
     assert results.status_code == 200, results.text
