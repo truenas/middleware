14818166c3d792d0bea6dce4a786a7b86979ffc3
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2021-03-09 08:18:39 -0500

    NAS-109638 / 21.04 / call fenced on zpool events (#6522)
    * add signal()/run_info() as well as fix stop() in fenced on scale
    
    * call fenced on zpool events
    
    * address William's review
    
    * fix build

--- src/freenas/usr/local/lib/middlewared_truenas/plugins/failover_/fenced_linux.py
+++ src/freenas/usr/local/lib/middlewared_truenas/plugins/failover_/fenced_linux.py
@@ -1,35 +1,43 @@
 import subprocess
+import psutil
+import contextlib
+import os
+import signal
 
-from middlewared.service import Service
+from middlewared.service import Service, CallError
+from fenced.fence import ExitCode as FencedExitCodes
 
 
-class FencedForceService(Service):
+PID_FILE = '/tmp/.fenced-pid'
+IS_ALIVE_SIGNAL = 0
+
+
+class FencedService(Service):
 
     class Config:
         private = True
         namespace = 'failover.fenced'
 
-    def start(self, force=False):
-
+    def start(self, force=False, use_zpools=False):
         # get the boot disks so fenced doesn't try to
         # place reservations on the boot drives
-        boot_disks = ""
         try:
-            boot_disks = ",".join(self.middleware.call_sync('boot.get_disks'))
+            boot_disks = ','.join(self.middleware.call_sync('boot.get_disks'))
         except Exception:
-            self.middleware.logger.warning(
-                'Failed to get boot disks for fenced', exc_info=True
-            )
+            self.logger.warning('Failed to get boot disks', exc_info=True)
             # just because we can't grab the boot disks from middleware
             # doesn't mean we should fail to start fenced since it
             # (ultimately) prevents data corruption on HA systems
-            pass
+            boot_disks = ''
 
-        # build the shell command include the "force" option
-        # if requested
-        cmd = ['fenced', '-ed', boot_disks]
+        # build the shell command
+        cmd = ['fenced']
+        if boot_disks:
+            cmd.extend(['-ed', boot_disks])
         if force:
             cmd.append('-f')
+        if use_zpools:
+            cmd.append('-uz')
 
         proc = subprocess.Popen(
             cmd,
@@ -40,6 +48,85 @@ class FencedForceService(Service):
 
         return proc.returncode
 
-    def stop(self):
+    def stop(self, banhammer=True):
+        if banhammer:
+            # dont care if it's running or not just
+            # SIGKILL anything that has "fenced" in
+            # the process name
+            subprocess.run(['pkill', '-9', '-f', 'fenced'])
+        else:
+            res = self.middleware.call_sync('failover.fenced.run_info')
+            if res['running'] and res['pid']:
+                os.kill(res['pid'], signal.SIGKILL)
 
-        subprocess.run(['pkill', '-9', '-f', 'fenced'])
+    def run_info(self):
+        res = {'running': False, 'pid': ''}
+        with contextlib.suppress(Exception):
+            with open(PID_FILE, 'rb') as f:
+                res['pid'] = int(f.read().decode())
+
+        check_running_procs = False
+        if res['pid']:
+            try:
+                os.kill(res['pid'], IS_ALIVE_SIGNAL)
+            except OSError:
+                check_running_procs = True
+            else:
+                res['running'] = True
+        else:
+            check_running_procs = True
+
+        if check_running_procs:
+            # either 1. no pid in file or 2. pid in file is wrong/stale
+            _iter = psutil.process_iter
+            proc = 'fenced'
+            res['pid'] = next((p.pid for p in _iter() if p.name() == proc), '')
+            res['running'] = bool(res['pid'])
+
+        return res
+
+    def signal(self, options):
+        res = self.middleware.call_sync('failover.fenced.run_info')
+        if res['running'] and res['pid']:
+            try:
+                if options.get('reload', False):
+                    os.kill(res['pid'], signal.SIGHUP)
+                if options.get('log_info', False):
+                    os.kill(res['pid'], signal.SIGUSR1)
+            except OSError as e:
+                raise CallError(f'Failed to signal fenced: {e}')
+
+
+async def hook_pool_event(middleware, *args, **kwargs):
+    # only run this on SCALE Enterprise
+    if await middleware.call('system.product_type') != 'SCALE_ENTERPRISE':
+        return
+
+    # HA licensed systems call fenced on their own
+    if await middleware.call('failover.licensed'):
+        return
+
+    # only run this on the m/x series platform since the other
+    # platforms are either non-supported or end of life
+    if (await middleware.call('failover.ha_mode'))[0] not in ('ECHOWARP', 'PUMA'):
+        return
+
+    if (await middleware.call('failover.fenced.run_info'))['running']:
+        try:
+            await middleware.call('failover.fenced.signal', {'reload': True})
+        except CallError as e:
+            middleware.logger.error('Failed to reload fenced: %r', e)
+    else:
+        force = True
+        use_zpools = True
+        rc = await middleware.call('failover.fenced.start', force, use_zpools)
+        if rc:
+            for i in FencedExitCodes:
+                if rc == i.value:
+                    middleware.logger.error('Failed to start fenced: %s', i.name)
+                    break
+
+
+async def setup(middleware):
+    middleware.register_hook('pool.post_create_or_update', hook_pool_event)
+    middleware.register_hook('pool.post_import', hook_pool_event)
