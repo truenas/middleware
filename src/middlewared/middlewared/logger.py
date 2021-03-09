truenas/middleware

commit 14b03ab027b9ea4a68ca2748817c1e330071365a

Reconfigure zettarepl_file logger on system dataset reconfiguration
(cherry picked from commit d3d5d19e928103ccfaea1be50025e007c185c72a)

src/middlewared/middlewared/logger.py
@@ -347,15 +347,16 @@ def reconfigure_logging():
 
         stream = handler.stream
         handler.stream = handler._open()
-        # We want to reassign stdout/stderr if its not the default one or closed
-        # which will happen on log file rotation.
-        try:
-            if sys.stdout.fileno() != 1 or sys.stderr.fileno() != 2:
-                raise ValueError()
-        except ValueError:
-            # ValueError can be raise if file handler is closed
-            sys.stdout = handler.stream
-            sys.stderr = handler.stream
+        if handler_name == 'file':
+            # We want to reassign stdout/stderr if its not the default one or closed
+            # which will happen on log file rotation.
+            try:
+                if sys.stdout.fileno() != 1 or sys.stderr.fileno() != 2:
+                    raise ValueError()
+            except ValueError:
+                # ValueError can be raise if file handler is closed
+                sys.stdout = handler.stream
+                sys.stderr = handler.stream
         try:
             stream.close()
         except Exception:

src/middlewared/middlewared/service.py
@@ -1211,8 +1211,8 @@ class CoreService(Service):
         of the new location
         """
         reconfigure_logging()
+        reconfigure_logging('zettarepl_file')
         self.__kill_multiprocessing()
-
         self.middleware.send_event('core.reconfigure_logging', 'CHANGED')
 
     @private

commit ebd25e43f912da522a2c4419d4c244669cee3b81

Fix reconfigure_logging bug introduced by incorrect backport in 14b03ab027b9ea4a68ca2748817c1e330071365a

src/middlewared/middlewared/logger.py
@@ -329,12 +329,12 @@ def setup_logging(name, debug_level, log_handler):
     _logger = Logger(name, debug_level)
     _logger.getLogger()
 
-    if 'file' in log_handler:
+    if log_handler == 'file':
         _logger.configure_logging('file')
         stream = _logger.stream()
         if stream is not None:
             sys.stdout = sys.stderr = stream
-    elif 'console' in log_handler:
+    elif log_handler == 'console':
         _logger.configure_logging('console')
     else:
         _logger.configure_logging('file')
@@ -347,14 +347,14 @@ def reconfigure_logging():
 
         stream = handler.stream
         handler.stream = handler._open()
-        if handler_name == 'file':
+        if name == 'file':
             # We want to reassign stdout/stderr if its not the default one or closed
             # which will happen on log file rotation.
             try:
                 if sys.stdout.fileno() != 1 or sys.stderr.fileno() != 2:
                     raise ValueError()
             except ValueError:
-                # ValueError can be raise if file handler is closed
+                # ValueError can be raised if file handler is closed
                 sys.stdout = handler.stream
                 sys.stderr = handler.stream
         try: