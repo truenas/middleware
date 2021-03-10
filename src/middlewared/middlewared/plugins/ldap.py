789910516cfa76c4a3b0c08b23249e84fbff5ce7
bugclerk <40872210+bugclerk@users.noreply.github.com>
2021-03-10 14:49:04 +0100

    Explicitly set tdbsam as passdb backend when stopping ldap (#6595)
    Original idea behind only doing this when the samba schema was enabled
    for the LDAP service overlooked the possibilty that the end-user might
    enable the samba schema, disable it, then stop the LDAP service. This
    was root cause for an API test failure related to SMB DOS modes.
    
    (cherry picked from commit 39eb926516d7f739c344c91d1b61026704341dd8)
    
    Co-authored-by: Andrew Walker <awalker@ixsystems.com>

--- src/middlewared/middlewared/plugins/ldap.py
+++ src/middlewared/middlewared/plugins/ldap.py
@@ -988,7 +988,7 @@ class LDAPService(ConfigService):
             await self.middleware.call('service.restart', 'cifs')
             await self.middleware.call('smb.synchronize_passdb')
             await self.middleware.call('smb.synchronize_group_mappings')
-            await self.middleware.call('smb.set_passdb_backend', 'tdbsam')
+        await self.middleware.call('smb.set_passdb_backend', 'tdbsam')
         await self.middleware.call('cache.pop', 'LDAP_cache')
         await self.middleware.call('service.stop', 'dscache')
         await self.nslcd_cmd('stop')
