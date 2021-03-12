39a0233d490ae0355903100a4611af550f294651
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-12 15:03:13 +0500

    Add ability to apply a yaml file to k8s cluster

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s/__init__.py
@@ -1,5 +1,5 @@
-from . import nodes, service_accounts
+from . import cluster, exceptions, nodes, service_accounts
 from .api_client import api_client
 
 
-__all__ = ['api_client', 'nodes', 'service_accounts']
+__all__ = ['api_client', 'cluster', 'exceptions', 'nodes', 'service_accounts']
