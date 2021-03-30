fbb58abf48355881507c16c709e5d235d32af9fb
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-30 19:37:56 +0500

    Update initramfs-update endpoint to use truenas-initrd script

--- src/middlewared/middlewared/etc_files/initramfs_update.py
+++ src/middlewared/middlewared/etc_files/initramfs_update.py
@@ -1,26 +1,8 @@
-import json
-import subprocess
-
-from middlewared.service import CallError
+from middlewared.utils import run
 
 
-INITRAMFS_CONF_PATH = '/boot/initramfs_config.json'
-
-
-def initramfs_config(middleware):
-    pci_ids = middleware.call_sync('device.get_to_isolate_pci_ids')
-    return {
-        'pci_ids': pci_ids,
-    }
-
-
-def render(service, middleware):
-    wanted_config = initramfs_config(middleware)
-
-    cp = subprocess.Popen(['update-initramfs', '-u', '-k', 'all'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stderr = cp.communicate()[1]
-    if cp.returncode:
-        raise CallError(f'Failed to update initramfs: {stderr.decode()}')
-
-    with open(INITRAMFS_CONF_PATH, 'w') as f:
-        f.write(json.dumps(wanted_config))
+async def render(service, middleware):
+    await run(
+        '/usr/local/bin/truenas-initrd.py', (await middleware.call('boot.pool_name')), '/', '1',
+        encoding='utf8', errors='ignore'
+    )
