c1ac089d32dc288afb60b23291425697c748e369
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-30 17:30:31 +0500

    Separate retrieving gpu details implementation

--- src/middlewared/middlewared/plugins/device_/device_info_linux.py
+++ src/middlewared/middlewared/plugins/device_/device_info_linux.py
@@ -9,10 +9,10 @@ import libsgio
 
 from .device_info_base import DeviceInfoBase
 from middlewared.service import CallError, private, Service
+from middlewared.utils.gpu import get_gpus
 
 RE_DISK_SERIAL = re.compile(r'Unit serial number:\s*(.*)')
 RE_NVME_PRIVATE_NAMESPACE = re.compile(r'nvme[0-9]+c')
-RE_PCI_ADDR = re.compile(r'(?P<domain>.*):(?P<bus>.*):(?P<slot>.*)\.')
 RE_SERIAL = re.compile(r'state.*=\s*(\w*).*io (.*)-(\w*)\n.*', re.S | re.A)
 RE_UART_TYPE = re.compile(r'is a\s*(\w+)')
 
@@ -268,45 +268,10 @@ class DeviceService(Service, DeviceInfoBase):
         return topology
 
     def get_gpus(self):
-        cp = subprocess.Popen(['lspci', '-D'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = cp.communicate()
-        if cp.returncode:
-            raise CallError(f'Unable to list available gpus: {stderr.decode()}')
-
+        gpus = get_gpus()
         to_isolate_gpus = self.middleware.call_sync('system.advanced.config')['isolated_gpu_pci_ids']
-        gpus = []
-        gpu_slots = [line.strip() for line in stdout.decode().splitlines() if 'VGA compatible controller' in line]
-        for gpu_line in gpu_slots:
-            addr = gpu_line.split()[0]
-            addr_re = RE_PCI_ADDR.match(addr)
-
-            gpu_dev = pyudev.Devices.from_name(pyudev.Context(), 'pci', addr)
-            # Let's normalise vendor for consistency
-            vendor = None
-            vendor_id_from_db = gpu_dev.get('ID_VENDOR_FROM_DATABASE', '').lower()
-            if 'nvidia' in vendor_id_from_db:
-                vendor = 'NVIDIA'
-            elif 'intel' in vendor_id_from_db:
-                vendor = 'INTEL'
-
-            gpus.append({
-                'addr': {
-                    'pci_slot': addr,
-                    **{k: addr_re.group(k) for k in ('domain', 'bus', 'slot')},
-                },
-                'description': gpu_line.split('VGA compatible controller:')[-1].split('(rev')[0].strip(),
-                'devices': [
-                    {
-                        'pci_id': child['PCI_ID'],
-                        'pci_slot': child['PCI_SLOT_NAME'],
-
-                    }
-                    for child in gpu_dev.parent.children if 'PCI_SLOT_NAME' in child and 'PCI_ID' in child
-                ],
-                'vendor': vendor,
-                'available_to_host': addr not in to_isolate_gpus,
-            })
-
+        for gpu in gpus:
+            gpu['available_to_host'] = gpu['addr']['pci_slot'] not in to_isolate_gpus
         return gpus
 
     @private
---
+++ src/middlewared/middlewared/utils/gpu.py
@@ -0,0 +1,49 @@
+import pyudev
+import re
+import subprocess
+
+from middlewared.service import CallError
+
+
+RE_PCI_ADDR = re.compile(r'(?P<domain>.*):(?P<bus>.*):(?P<slot>.*)\.')
+
+
+def get_gpus():
+    cp = subprocess.Popen(['lspci', '-D'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = cp.communicate()
+    if cp.returncode:
+        raise CallError(f'Unable to list available gpus: {stderr.decode()}')
+
+    gpus = []
+    gpu_slots = [line.strip() for line in stdout.decode().splitlines() if 'VGA compatible controller' in line]
+    for gpu_line in gpu_slots:
+        addr = gpu_line.split()[0]
+        addr_re = RE_PCI_ADDR.match(addr)
+
+        gpu_dev = pyudev.Devices.from_name(pyudev.Context(), 'pci', addr)
+        # Let's normalise vendor for consistency
+        vendor = None
+        vendor_id_from_db = gpu_dev.get('ID_VENDOR_FROM_DATABASE', '').lower()
+        if 'nvidia' in vendor_id_from_db:
+            vendor = 'NVIDIA'
+        elif 'intel' in vendor_id_from_db:
+            vendor = 'INTEL'
+
+        gpus.append({
+            'addr': {
+                'pci_slot': addr,
+                **{k: addr_re.group(k) for k in ('domain', 'bus', 'slot')},
+            },
+            'description': gpu_line.split('VGA compatible controller:')[-1].split('(rev')[0].strip(),
+            'devices': [
+                {
+                    'pci_id': child['PCI_ID'],
+                    'pci_slot': child['PCI_SLOT_NAME'],
+
+                }
+                for child in gpu_dev.parent.children if 'PCI_SLOT_NAME' in child and 'PCI_ID' in child
+            ],
+            'vendor': vendor,
+        })
+
+    return gpus
