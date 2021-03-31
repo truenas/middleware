dd3e5b094620a0baf75dcc3d9e652b0115de78a6
themylogin <themylogin@gmail.com>
2021-03-31 11:49:15 +0200

    Use --no-nvram when installing EFI grub on non-EFI systems

--- src/middlewared/middlewared/plugins/boot_/boot_loader_linux.py
+++ src/middlewared/middlewared/plugins/boot_/boot_loader_linux.py
@@ -22,10 +22,13 @@ class BootService(Service, BootLoaderBase):
             efi_dir = os.path.join(tmpdirname, 'efi')
             os.makedirs(efi_dir)
             await run('mount', '-t', 'vfat', f'/dev/{partition}', efi_dir)
-            await run(
+            grub_cmd = [
                 'grub-install', '--target=x86_64-efi', f'--efi-directory={efi_dir}',
                 '--bootloader-id=debian', '--recheck', '--no-floppy',
-            )
+            ]
+            if not os.path.exists('/sys/firmware/efi'):
+                grub_cmd.append('--no-nvram')
+            await run(*grub_cmd)
             mounted_efi_dir = os.path.join(efi_dir, 'EFI')
             os.makedirs(os.path.join(mounted_efi_dir, 'boot'), exist_ok=True)
             shutil.copy(
