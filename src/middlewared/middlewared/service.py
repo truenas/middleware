62aa706702bb80d54f0a834d4efe07745c3ba4de
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-22 22:52:14 +0500

    Add explicit get method on ui_restart endpoint
    (cherry picked from commit 0dc94f20efc3a8cff2e0b83b87f7ad9d12d423c5)

--- src/middlewared/middlewared/service.py
+++ src/middlewared/middlewared/service.py
@@ -175,7 +175,7 @@ def pass_app(rest=False):
     return wrapper
 
 
-def rest_api_metadata(explicit_methods=None):
+def rest_api_metadata(extra_methods=None):
     """
     Allow having endpoints specify explicit rest methods.
 
@@ -186,7 +186,7 @@ def rest_api_metadata(explicit_methods=None):
     """
     def wrapper(fn):
         fn._rest_api_metadata = {
-            'explicit_methods': explicit_methods,
+            'extra_methods': extra_methods,
         }
         return fn
     return wrapper
@@ -986,7 +986,7 @@ class CoreService(Service):
                     'no_auth_required': hasattr(method, '_no_auth_required'),
                     'filterable': hasattr(method, '_filterable'),
                     'pass_application': hasattr(method, '_pass_app'),
-                    'explicit_methods': method._rest_api_metadata['explicit_methods'] if hasattr(
+                    'extra_methods': method._rest_api_metadata['extra_methods'] if hasattr(
                         method, '_rest_api_metadata') else None,
                     'require_websocket': hasattr(method, '_pass_app') and not method._pass_app['rest'],
                     'job': hasattr(method, '_job'),
