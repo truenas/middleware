eb3574b5f38ed98a6e1d72de92ebba9925563939
themylogin <themylogin@gmail.com>
2021-04-22 12:20:36 +0200

    Remove LegacyVMWareLoginFailedAlertSource

--- src/middlewared/middlewared/alert/source/vmware_login.py
+++ src/middlewared/middlewared/alert/source/vmware_login.py
@@ -1,10 +1,4 @@
-import pickle as pickle
-
-from lockfile import LockFile
-
 from middlewared.alert.base import AlertClass, OneShotAlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
-
-VMWARELOGIN_FAILS = "/var/tmp/.vmwarelogin_fails"
 
 
 class VMWareLoginFailedAlertClass(AlertClass, OneShotAlertClass):
@@ -23,28 +17,3 @@ class VMWareLoginFailedAlertClass(AlertClass, OneShotAlertClass):
             lambda alert: alert.args["hostname"] != hostname,
             alerts
         ))
-
-
-class LegacyVMWareLoginFailedAlertSource(ThreadedAlertSource):
-    def check_sync(self):
-        try:
-            with LockFile(VMWARELOGIN_FAILS):
-                with open(VMWARELOGIN_FAILS, "rb") as f:
-                    fails = pickle.load(f)
-        except Exception:
-            return
-
-        alerts = []
-        for oid, errmsg in list(fails.items()):
-            try:
-                vmware = self.middleware.call_sync("datastore.query", "storage.vmwareplugin", [["id", "=", oid]],
-                                                   {"get": True})
-            except IndexError:
-                continue
-
-            alerts.append(Alert(VMWareLoginFailedAlertClass, {
-                "hostname": vmware["hostname"],
-                "error": errmsg,
-            }))
-
-        return alerts
