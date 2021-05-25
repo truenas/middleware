3a186b62fe9f2b980fc4fa85ac135756c4d5bd71
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-05-25 21:39:27 +0500

    Add migration to remove disk_acousticlevel field

---
+++ src/middlewared/middlewared/alembic/versions/21.MM/2021-05-25_21-32_remove_aam.py
@@ -0,0 +1,25 @@
+"""
+Remove AAM from disks
+
+Revision ID: bd637e18fb0b
+Revises: 45724786402e
+Create Date: 2021-05-25 21:32:28.702007+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'bd637e18fb0b'
+down_revision = '45724786402e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('storage_disk', schema=None) as batch_op:
+        batch_op.drop_column('disk_acousticlevel')
+
+
+def downgrade():
+    pass
