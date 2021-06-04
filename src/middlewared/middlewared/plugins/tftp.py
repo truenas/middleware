b6aae3cc5593cebcfc43432ba1d82065cc5daf0d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-06-04 21:58:43 +0500

    Document tftp plugin return types

--- src/middlewared/middlewared/plugins/tftp.py
+++ src/middlewared/middlewared/plugins/tftp.py
@@ -1,5 +1,5 @@
 from middlewared.async_validators import check_path_resides_within_volume
-from middlewared.schema import accepts, Bool, Dict, Dir, Int, Str
+from middlewared.schema import accepts, Bool, Dict, Dir, Int, Patch, Str
 from middlewared.validators import IpAddress
 from middlewared.service import SystemServiceService, ValidationErrors
 import middlewared.sqlalchemy as sa
@@ -26,16 +26,23 @@ class TFTPService(SystemServiceService):
         datastore_prefix = "tftp_"
         cli_namespace = "service.tftp"
 
-    @accepts(Dict(
-        'tftp_update',
-        Bool('newfiles'),
-        Dir('directory'),
-        Str('host', validators=[IpAddress()]),
-        Int('port', validators=[Port()]),
-        Str('options'),
-        Str('umask', validators=[Match(r"^[0-7]{3}$")]),
-        Str('username'),
-        update=True
+    CONFIG_ENTRY = Dict(
+        'tftp_entry',
+        Bool('newfiles', required=True),
+        Str('directory', required=True),
+        Str('host', validators=[IpAddress()], required=True),
+        Int('port', validators=[Port()], required=True),
+        Str('options', required=True),
+        Str('umask', required=True, validators=[Match(r'^[0-7]{3}$')]),
+        Str('username', required=True),
+        Int('id', required=True),
+    )
+
+    @accepts(Patch(
+        'tftp_entry', 'tftp_update',
+        ('rm', {'name': 'id'}),
+        ('replace', Dir('directory')),
+        ('attr', {'update': True}),
     ))
     async def do_update(self, data):
         """
