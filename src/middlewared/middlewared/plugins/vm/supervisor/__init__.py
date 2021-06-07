d0a15d978dc6aadf05b44a00d3efdee3f9d33944
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-06-08 00:02:57 +0500

    Clean supervisor_freebsd reference

--- src/middlewared/middlewared/plugins/vm/supervisor/__init__.py
+++ src/middlewared/middlewared/plugins/vm/supervisor/__init__.py
@@ -1,8 +1,3 @@
-from middlewared.utils import osc
-
-if osc.IS_LINUX:
-    from .supervisor_linux import VMSupervisor # noqa
-else:
-    from .supervisor_freebsd import VMSupervisor # noqa
+from .supervisor_linux import VMSupervisor # noqa
 
 __all__ = ['VMSupervisor']
