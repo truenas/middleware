b940b9bd82ef2b7fcc998cc0e21c20e1d3f7c7ab
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-06-10 12:08:49 +0500

    Add migration for configure_gpus field

---
+++ src/middlewared/middlewared/alembic/versions/21.MM/2021-06-10_11-30_configure_gpus_k8s.py
@@ -0,0 +1,25 @@
+"""
+Add field to configure gpus for k8s
+
+Revision ID: 737912c7a9c1
+Revises: bd637e18fb0b
+Create Date: 2021-06-10 11:30:28.702007+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '737912c7a9c1'
+down_revision = 'bd637e18fb0b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('services_kubernetes', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('configure_gpus', sa.Boolean(), default=False, nullable=False))
+
+
+def downgrade():
+    pass
