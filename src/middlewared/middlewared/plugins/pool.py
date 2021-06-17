02f313b2f842aef0876dddf9e5f78e93c78798b4
Andrew Walker <awalker@ixsystems.com>
2021-06-17 14:41:05 -0400

    Fix setting default SMB ACL on dataset creation
    check for acltype was broken.
    
    (cherry picked from commit c837b685d317ac37e3feb5e6d5a90190ddb394ff)

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -3140,7 +3140,7 @@ class PoolDatasetService(CRUDService):
 
         created_ds = await self.get_instance(data['id'])
 
-        if data['type'] == 'FILESYSTEM' and data['share_type'] == 'SMB' and created_ds['acltype'] == "NFSV4":
+        if data['type'] == 'FILESYSTEM' and data['share_type'] == 'SMB' and created_ds['acltype']['value'] == "NFSV4":
             await self.middleware.call('pool.dataset.permission', data['id'], {'mode': None})
 
         return created_ds
