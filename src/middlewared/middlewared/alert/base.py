b3852e97e21a966eb9eb3ab804020e15b3fd52a3
caleb <yocalebo@gmail.com>
2021-06-25 09:42:07 -0400

    flake8 fixes

--- src/middlewared/middlewared/alert/base.py
+++ src/middlewared/middlewared/alert/base.py
@@ -249,7 +249,7 @@ class AlertService:
 
     async def _format_alerts(self, alerts, gone_alerts, new_alerts):
         product_name = await self.middleware.call("system.product_name")
-        hostname = await self.middleware.call('system.hostname')
+        hostname = await self.middleware.call("system.hostname")
         if await self.middleware.call("system.is_enterprise"):
             node_map = await self.middleware.call("alert.node_map")
         else:
@@ -272,7 +272,7 @@ class ThreadedAlertService(AlertService):
 
     def _format_alerts(self, alerts, gone_alerts, new_alerts):
         product_name = self.middleware.call_sync("system.product_name")
-        hostname = await self.middleware.call('system.hostname')
+        hostname = self.middleware.call_sync("system.hostname")
         if self.middleware.call_sync("system.is_enterprise"):
             node_map = self.middleware.call_sync("alert.node_map")
         else:
