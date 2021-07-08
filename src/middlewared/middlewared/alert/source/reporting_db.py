930ec678e8366de24fe72f035ca429edd6347dc5
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2021-07-08 18:55:15 -0400

    raise collectd partition partition threshold to 1.5GB (#7124)
    (cherry picked from commit b468629186c15fc1761b11e2faba2d3a7ca33d39)

--- src/middlewared/middlewared/alert/source/reporting_db.py
+++ src/middlewared/middlewared/alert/source/reporting_db.py
@@ -1,26 +1,32 @@
-import humanfriendly
-import psutil
+import shutil
+from humanfriendly import format_size
 
-from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource, UnavailableException
+from middlewared.alert.base import (AlertClass, AlertCategory, AlertLevel,
+                                    Alert, ThreadedAlertSource, UnavailableException)
 
 
 class ReportingDbAlertClass(AlertClass):
     category = AlertCategory.REPORTING
     level = AlertLevel.WARNING
-    title = 'Reporting Database Size Is Larger than 1 GiB'
-    text = 'Reporting database size (%s) is larger than 1 GiB.'
+    title = 'Reporting Database Size Exceeds Threshold'
+    text = 'Reporting database used size %(used)s is larger than %(threshold)s.'
 
 
 class ReportingDbAlertSource(ThreadedAlertSource):
     def check_sync(self):
-        rrd_size_alert_threshold = 1073741824
+        rrd_size_alert_threshold = 1610611911  # bytes
 
         try:
-            used = psutil.disk_usage('/var/db/collectd/rrd').used
+            used = shutil.disk_usage('/var/db/collectd/rrd').used
         except FileNotFoundError:
             raise UnavailableException()
 
         if used > rrd_size_alert_threshold:
-            return Alert(ReportingDbAlertClass,
-                         args=humanfriendly.format_size(used),
-                         key=None)
+            # zfs list reports in kibi/mebi/gibi(bytes) but
+            # format_size() calculates in kilo/mega/giga by default
+            # so the report that we send the user needs to match
+            # up with what zfs list reports as to not confuse anyone
+            used = format_size(used, binary=True)
+            threshold = format_size(rrd_size_alert_threshold, binary=True)
+
+            return Alert(ReportingDbAlertClass, {'used': used, 'threshold': threshold})
