a97229e3fa5d8129cd4b82b288479b84e457a657
bugclerk <40872210+bugclerk@users.noreply.github.com>
2021-07-13 20:16:30 -0300

    NAS-111390 / 12.0 / Enclosure Management fix for R20A. (#7145)
    System has weird drive mapping top to bottom then left to right naturally
    Needs correspondending UI change to map drives Left to Right and top to
    bottom as is standard(same as r20)
    
    (cherry picked from commit 58f59d619fa5b9cc225ab5619214f460e8418ade)
    
    Co-authored-by: Nick 'darkfiberiru' Wolff <darkfiberiru@gmail.com>

--- src/freenas/usr/local/lib/middlewared_truenas/plugins/enclosure_/map.py
+++ src/freenas/usr/local/lib/middlewared_truenas/plugins/enclosure_/map.py
@@ -96,7 +96,7 @@ MAPPINGS = [
             MappingSlot(0, 15, False),
         ]),
     ]),
-    ProductMapping(re.compile(r"TRUENAS-R20A?$"), [
+    ProductMapping(re.compile(r"TRUENAS-R20$"), [
         VersionMapping(re.compile(".*"), [
             MappingSlot(2, 0, False),
             MappingSlot(2, 1, False),
@@ -110,6 +110,24 @@ MAPPINGS = [
             MappingSlot(2, 9, False),
             MappingSlot(2, 10, False),
             MappingSlot(2, 11, False),
+            MappingSlot(0, 0, False),
+            MappingSlot(0, 1, False),
+        ]),
+    ]),
+    ProductMapping(re.compile(r"TRUENAS-R20A$"), [
+        VersionMapping(re.compile(".*"), [
+            MappingSlot(2, 2, False),
+            MappingSlot(2, 5, False),
+            MappingSlot(2, 8, False),
+            MappingSlot(2, 11, False),
+            MappingSlot(2, 1, False),
+            MappingSlot(2, 4, False),
+            MappingSlot(2, 7, False),
+            MappingSlot(2, 10, False),
+            MappingSlot(2, 0, False),
+            MappingSlot(2, 3, False),
+            MappingSlot(2, 6, False),
+            MappingSlot(2, 9, False),
             MappingSlot(0, 0, False),
             MappingSlot(0, 1, False),
         ]),
