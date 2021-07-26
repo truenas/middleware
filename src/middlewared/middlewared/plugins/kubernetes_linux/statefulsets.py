b51c3847936e54a96d7e60db02ebd67a0fb4cb3d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-07-26 16:01:32 +0500

    Optionally retrieve each resource(s) individual events

--- src/middlewared/middlewared/plugins/kubernetes_linux/statefulsets.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/statefulsets.py
@@ -26,11 +26,13 @@ class KubernetesStatefulsetService(CRUDService):
                 func = functools.partial(context['apps_api'].list_stateful_set_for_all_namespaces)
 
             stateful_sets = [d.to_dict() for d in (await func()).items]
-            events = await self.middleware.call(
-                'kubernetes.get_events_of_resource_type', 'StatefulSet', [s['metadata']['uid'] for s in stateful_sets]
-            )
-            for stateful_set in stateful_sets:
-                stateful_set['events'] = events[stateful_set['metadata']['uid']]
+            if options['extra'].get('events'):
+                events = await self.middleware.call(
+                    'kubernetes.get_events_of_resource_type', 'StatefulSet',
+                    [s['metadata']['uid'] for s in stateful_sets]
+                )
+                for stateful_set in stateful_sets:
+                    stateful_set['events'] = events[stateful_set['metadata']['uid']]
 
         return filter_list(stateful_sets, filters, options)
 
