20d85f23e0b426a2df045d46dd2c12e4f860d72b
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-07-26 16:32:51 +0500

    Do not query chart releases on k8s events as that's an expensive call

--- src/middlewared/middlewared/plugins/chart_releases_linux/events.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/events.py
@@ -59,20 +59,21 @@ class ChartReleaseService(Service):
     async def handle_k8s_event(self, k8s_event):
         name = get_chart_release_from_namespace(k8s_event['involved_object']['namespace'])
         async with LOCKS[name]:
-            chart_release = await self.middleware.call(
-                'chart.release.query', [['id', '=', name]], {'extra': {'history': True}}
-            )
-            if not chart_release:
+            if name not in self.CHART_RELEASES:
                 # It's possible the chart release got deleted
                 return
-            else:
-                chart_release = chart_release[0]
 
-            if chart_release['status'] != self.CHART_RELEASES.get(name, {}).get('status'):
+            status = await self.middleware.call('chart.release.pod_status', name)
+            if status['status'] != self.CHART_RELEASES[name]['status']:
                 # raise event
-                self.middleware.send_event('chart.release.query', 'CHANGED', id=name, fields=chart_release)
-
-            ChartReleaseService.CHART_RELEASES[name] = chart_release
+                ChartReleaseService.CHART_RELEASES[name].update({
+                    'status': status['status'],
+                    'pod_status': {
+                        'desired': status['desired'],
+                        'available': status['available'],
+                    }
+                })
+                self.middleware.send_event('chart.release.query', 'CHANGED', id=name, fields=self.CHART_RELEASES[name])
 
 
 async def chart_release_event(middleware, event_type, args):
