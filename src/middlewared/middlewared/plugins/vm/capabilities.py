6eccc15b9b49f7a0e63cb9d27b5b9076a0c2b610
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-03 03:19:34 +0500

    Add endpoint to retrieve guest arch/machine type choices

--- src/middlewared/middlewared/plugins/vm/capabilities.py
+++ src/middlewared/middlewared/plugins/vm/capabilities.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from xml.etree import ElementTree as etree
 
-from middlewared.schema import accepts, Dict, List, returns
+from middlewared.schema import accepts, Dict, returns
 from middlewared.service import private, Service
 
 from .connection import LibvirtConnectionMixin
@@ -26,3 +26,22 @@ class VMService(Service, LibvirtConnectionMixin):
                 supported_archs[arch_name].append(machine_type.text)
 
         self.CAPABILITIES = supported_archs
+
+    @accepts()
+    @returns(Dict(
+        additional_attrs=True,
+        example={
+            'x86_64': ['pc-i440fx-5.2', 'pc-q35-5.2', 'pc-i440fx-2.7'],
+            'i686': ['pc-i440fx-3.0', 'xenfv'],
+        }
+    ))
+    async def guest_architecture_and_machine_choices(self):
+        """
+        Retrieve choices for supported guest architecture types and machine choices.
+
+        Keys in the response would be supported guest architecture(s) on the host and their respective values would
+        be supported machine type(s) for the specific architecture on the host.
+        """
+        if not self.CAPABILITIES:
+            await self.middleware.call('vm.update_capabilities_cache')
+        return self.CAPABILITIES
