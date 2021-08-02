46f46b68ffa8d2d32bb7945d6583c6f5f159f94c
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-02 13:16:48 +0500

    Add migration to make configuring video device optional

---
+++ src/middlewared/middlewared/alembic/versions/21.MM/2021-08-02_13-13_vm_video_device.py
@@ -0,0 +1,24 @@
+"""
+VM Video Device
+
+Revision ID: 4686771af68a
+Revises: 1857e74d5f11
+Create Date: 2021-02-08 13:13:42.818433+00:00
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '4686771af68a'
+down_revision = '1857e74d5f11'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('vm_vm', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('ensure_display_device', sa.Boolean(), default=True, nullable=False))
+
+
+def downgrade():
+    pass
