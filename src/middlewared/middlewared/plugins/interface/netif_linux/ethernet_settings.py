3bacd61ab0c78f15c323aa280bf34bdb18479736
caleb <yocalebo@gmail.com>
2021-08-03 08:13:20 -0400

    ability to enable/disable capabilities on SCALE

--- src/middlewared/middlewared/plugins/interface/netif_linux/ethernet_settings.py
+++ src/middlewared/middlewared/plugins/interface/netif_linux/ethernet_settings.py
@@ -18,24 +18,64 @@ class EthernetHardwareSettings:
         result = {'enabled': [], 'disabled': [], 'supported': []}
         try:
             for i in self._eth.get_features(self._name):
-                for feature, value in i.items():
-                    if value.enable:
-                        result['enabled'].append(feature)
+                for name, feature in i.items():
+                    if not name.strip() or not feature.available:
+                        # testing shows that there are features
+                        # without a name so make sure we ignore
+                        # those as well as ignore the feature if
+                        # it's not "available" to be changed
+                        continue
+
+                    if feature.enable:
+                        result['enabled'].append(name)
                     else:
-                        result['disabled'].append(feature)
-                    result['supported'].append(feature)
+                        result['disabled'].append(name)
+                    result['supported'].append(name)
         except Exception:
             logger.error('Failed to get capabilities for %s', self._name, exc_info=True)
 
         return result
 
+    def __set_features__(self, action, capabilities):
+        features = []
+        for cap in capabilities:
+            if action == 'enable' and cap in self.disabled_capabilities:
+                # means the feature(s) being requested to be enabled is currently disabled
+                features.append(cap)
+            elif action == 'disable' and cap in self.enabled_capabilities:
+                # means the feature(s) being requested to be disabled is currently enabled
+                features.append(cap)
+
+        if features:
+            changed_features = self._eth.get_features(self._name)
+            set_features = False
+            for feature in features:
+                try:
+                    changed_features.features[feature].enable = True if action == 'enable' else False
+                    set_features = True
+                except KeyError:
+                    logger.error('Feature "%s" not found on interface "%s"', feature, self._name)
+                    continue
+
+            if set_features:
+                # actually send the request to the kernel to enable/disable the feature(s)
+                self._eth.set_features(self._name, changed_features)
+
     @property
     def enabled_capabilities(self):
         return self._caps['enabled']
 
+    @enabled_capabilities.setter
+    def enabled_capabilities(self, capabilities):
+        self.__set_features__('enable', capabilities)
+
     @property
-    def disabled_capabilties(self):
+    def disabled_capabilities(self):
         return self._caps['disabled']
+
+    @disabled_capabilities.setter
+    def disabled_capabilities(self, capabilities):
+        self.__set_features__('disable', capabilities)
 
     @property
     def supported_capabilities(self):
