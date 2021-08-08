ed7ee8a1ff277f3ca74ea7c0b915259d938c0e0d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-08 17:09:45 +0500

    Stop containerd explicitly after stopping docker
    This commit adds changes to make sure we stop containerd which is not stopped when docker is stopped.

--- src/middlewared/middlewared/plugins/service_/services/docker.py
+++ src/middlewared/middlewared/plugins/service_/services/docker.py
@@ -10,3 +10,7 @@ class DockerService(SimpleService):
         # https://jira.ixsystems.com/browse/NAS-108883 we see that docker did not start right away
         # and takes 17 seconds to initialize.
         await self._unit_action('Start', timeout=30)
+
+    async def _stop_linux(self):
+        await super()._stop_linux()
+        await self._systemd_unit('containerd', 'stop')
