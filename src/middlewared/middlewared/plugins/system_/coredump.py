882296bca581530fb727ce6f9d5f3f8dea6d42f1
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-12 23:51:11 +0500

    Do not report coredumps generated by apps in containers
    This commit adds changes to not report coredumps generated by any specific app in a container as users are free to run whatever they would like to in containers and we don't officially support all the apps themselves.

--- src/middlewared/middlewared/plugins/system_/coredump.py
+++ src/middlewared/middlewared/plugins/system_/coredump.py
@@ -1,10 +1,13 @@
 # -*- coding=utf-8 -*-
 import logging
+import re
 
 from middlewared.service import private, Service
 from middlewared.utils import run
 
 logger = logging.getLogger(__name__)
+
+SKIP_REGEX = re.compile(r'Unit:\s+containerd.service')
 
 
 class SystemService(Service):
@@ -21,6 +24,15 @@ class SystemService(Service):
         for line in lines:
             exe = line[exe_pos:]
             time, pid, uid, gid, sig, corefile = line[:exe_pos].rsplit(maxsplit=5)
+            cp = await run("coredumpctl", "info", pid, check=False, encoding="utf-8", errors="ignore")
+            if cp.returncode:
+                self.logger.debug("Unable to retrieve coredump information of %r process", pid)
+                continue
+
+            if SKIP_REGEX.findall(cp.stdout):
+                # We don't want to raise an alert to user about a container which died for some reason
+                continue
+
             coredumps.append({
                 "time": time,
                 "pid": int(pid),
