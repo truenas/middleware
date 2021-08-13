truenas/middleware

commit acef89b7413859a9bbe70226139756be3551b784

Only have VMware periodic snapshot task job appear in task manager if we have VMware storage set up

src/middlewared/middlewared/plugins/vmware.py
@@ -393,15 +393,7 @@ class VMWareService(CRUDService):
         return self.middleware.call_sync("vmware.query", [f])
 
     @private
-    def snapshot_begin(self, dataset, recursive):
-        # If there's a VMWare Plugin object for this filesystem
-        # snapshot the VMs before taking the ZFS snapshot.
-        # Once we've taken the ZFS snapshot we're going to log back in
-        # to VMWare and destroy all the VMWare snapshots we created.
-        # We do this because having VMWare snapshots in existence impacts
-        # the performance of your VMs.
-        qs = self._dataset_get_vms(dataset, recursive)
-
+    def snapshot_proceed(self, dataset, qs):
         # Generate a unique snapshot name that won't collide with anything that exists on the VMWare side.
         vmsnapname = str(uuid.uuid4())
 
@@ -543,13 +535,29 @@ class VMWareService(CRUDService):
             connect.Disconnect(si)
 
     @private
-    @job()
-    def periodic_snapshot_task_begin(self, job, task_id):
+    def periodic_snapshot_task_begin(self, task_id):
         task = self.middleware.call_sync("pool.snapshottask.query",
                                          [["id", "=", task_id]],
                                          {"get": True})
 
-        return self.snapshot_begin(task["dataset"], task["recursive"])
+        # If there's a VMWare Plugin object for this filesystem
+        # snapshot the VMs before taking the ZFS snapshot.
+        # Once we've taken the ZFS snapshot we're going to log back in
+        # to VMWare and destroy all the VMWare snapshots we created.
+        # We do this because having VMWare snapshots in existence impacts
+        # the performance of your VMs.
+        qs = self._dataset_get_vms(task["dataset"], task["recursive"])
+        if qs:
+            return {
+                "dataset": task["dataset"],
+                "qs": qs,
+            }
+
+    @private
+    @accepts(Any("context", private=True))
+    @job()
+    def periodic_snapshot_task_proceed(self, job, context):
+        return self.snapshot_proceed(context["dataset"], context["qs"])
 
     @private
     @accepts(Any("context", private=True))

src/middlewared/middlewared/plugins/zettarepl.py
@@ -179,7 +179,9 @@ class ZettareplProcess:
 
                 if isinstance(message, PeriodicSnapshotTaskStart):
                     with Client() as c:
-                        context = c.call("vmware.periodic_snapshot_task_begin", task_id, job=True)
+                        context = None
+                        if begin_context := c.call("vmware.periodic_snapshot_task_begin", task_id):
+                            context = c.call("vmware.periodic_snapshot_task_proceed", begin_context, job=True)
 
                     self.vmware_contexts[task_id] = context
 

commit 4730b3ff68398fc3ee74c4ba1b5900996bcd91f7

Fix taking vmware-aware manual snapshots
This was broken by acef89b7413859a9bbe70226139756be3551b784

src/middlewared/middlewared/plugins/vmware.py
@@ -393,6 +393,11 @@ class VMWareService(CRUDService):
         return self.middleware.call_sync("vmware.query", [f])
 
     @private
+    def snapshot_begin(self, dataset, recursive):
+        qs = self._dataset_get_vms(dataset, recursive)
+        return self.snapshot_proceed(dataset, qs)
+
+    @private
     def snapshot_proceed(self, dataset, qs):
         # Generate a unique snapshot name that won't collide with anything that exists on the VMWare side.
         vmsnapname = str(uuid.uuid4())