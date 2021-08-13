8854a89b90c71be2c7bce2268a9ed7800ec221c9
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-13 07:29:10 +0500

    Move coredump filtering logic to cores alert

--- src/middlewared/middlewared/alert/source/cores.py
+++ src/middlewared/middlewared/alert/source/cores.py
@@ -1,6 +1,12 @@
+import re
+
 from datetime import timedelta
 
 from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, AlertSource, Alert, IntervalSchedule
+from middlewared.utils import run
+
+
+SKIP_REGEX = re.compile(r'Unit:\s+containerd.service')
 
 
 class CoreFilesArePresentAlertClass(AlertClass):
@@ -16,18 +22,37 @@ class CoreFilesArePresentAlertClass(AlertClass):
 
 
 class CoreFilesArePresentAlertSource(AlertSource):
+    dumps = {}
     schedule = IntervalSchedule(timedelta(minutes=5))
 
     products = ("SCALE",)
 
     async def check(self):
         corefiles = []
-        for coredump in await self.middleware.call("system.coredumps"):
-            if coredump["corefile"] == "present":
-                if coredump["exe"] == "/usr/sbin/syslog-ng":
+        for coredump in filter(lambda c: c["corefile"] == "present", await self.middleware.call("system.coredumps")):
+            if coredump["exe"] == "/usr/sbin/syslog-ng":
+                continue
+
+            if coredump["pid"] not in self.dumps:
+                cp = await run(
+                    "coredumpctl", "info", str(coredump["pid"]), check=False, encoding="utf-8", errors="ignore"
+                )
+                if cp.returncode:
+                    self.middleware.logger.debug(
+                        "Unable to retrieve coredump information of %r process", coredump["pid"]
+                    )
                     continue
 
-                corefiles.append(f"{coredump['exe']} ({coredump['time']})")
+                self.dumps[coredump["pid"]] = {
+                    **coredump,
+                    "happened_in_container": bool(SKIP_REGEX.findall(cp.stdout))
+                }
+
+            if self.dumps[coredump["pid"]]["happened_in_container"]:
+                # We don't want to raise an alert to user about a container which died for some reason
+                continue
+
+            corefiles.append(f"{coredump['exe']} ({coredump['time']})")
 
         if corefiles:
             return Alert(CoreFilesArePresentAlertClass, {"corefiles": ', '.join(corefiles)})
