0f09ac66841c08abe0b479861806039161ca496d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-17 23:58:56 +0500

    Add endpoint to retrieve serial port choices with serial port names in freebsd
    (cherry picked from commit d12d000bd5056b09f38ee109e7c7b9074e2d5558)

--- src/middlewared/middlewared/utils/osc/freebsd/system.py
+++ src/middlewared/middlewared/utils/osc/freebsd/system.py
@@ -1,5 +1,7 @@
 # -*- coding=utf-8 -*-
 import logging
+import re
+import subprocess
 import sysctl
 
 logger = logging.getLogger(__name__)
@@ -7,5 +9,22 @@ logger = logging.getLogger(__name__)
 __all__ = ['get_cpu_model']
 
 
+RE_PORT = re.compile(r'([0-9a-fA-Fx]+).*\((uart[0-9])+\)')
+
+
 def get_cpu_model():
     return sysctl.filter('hw.model')[0].value
+
+
+def serial_port_choices():
+    cp = subprocess.Popen(
+        "/usr/sbin/devinfo -u | grep -A 99999 '^I/O ports:' | grep -E '*([0-9a-fA-Fx]+).*\\(uart[0-9]+\\)'",
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    stdout, stderr = cp.communicate()
+    return [
+        {
+            'name': e[1],
+            'start': e[0],
+        } for e in RE_PORT.findall(stdout.decode(errors='ignore'))
+    ]
