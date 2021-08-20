41c98342844f1146c97ae1ae25b3204c03c7deb6
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-20 15:47:05 +0500

    Add check_password endpoint
    (cherry picked from commit 999aa2dafbb518ec0e42f0f4db8ac28e7783c437)

--- src/middlewared/middlewared/plugins/auth.py
+++ src/middlewared/middlewared/plugins/auth.py
@@ -260,6 +260,14 @@ class AuthService(Service):
     @accepts(Str('username'), Str('password'))
     async def check_user(self, username, password):
         """
+        Verify username and password (this will only validate root user's password and
+        would return `false` for any other user)
+        """
+        return False if username != 'root' else await self.check_password(username, password)
+
+    @accepts(Str('username'), Str('password'))
+    async def check_password(self, username, password):
+        """
         Verify username and password
         """
         try:
