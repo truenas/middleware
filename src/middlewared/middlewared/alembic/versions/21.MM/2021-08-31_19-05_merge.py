c8dc3f38ed67d5fdab605798b926a81f09e39f48
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-08-31 19:06:50 +0500

    Add merge migration

---
+++ src/middlewared/middlewared/alembic/versions/21.MM/2021-08-31_19-05_merge.py
@@ -0,0 +1,20 @@
+"""
+Merge
+
+Revision ID: b55d888749aa
+Revises: 8e991f9adcdf, 45d6f6f07b0f
+Create Date: 2021-08-31 19:05:57.583858+00:00
+
+"""
+revision = 'b55d888749aa'
+down_revision = ('8e991f9adcdf', '45d6f6f07b0f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
