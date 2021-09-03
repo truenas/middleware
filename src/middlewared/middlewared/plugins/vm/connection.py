0dbabf2ccda1623e9a6cec3bde52932016c98089
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-09-03 18:49:17 +0500

    Properly check if system supports virtualization

--- src/middlewared/middlewared/plugins/vm/connection.py
+++ src/middlewared/middlewared/plugins/vm/connection.py
@@ -27,22 +27,32 @@ class LibvirtConnectionMixin:
         else:
             LibvirtConnectionMixin.LIBVIRT_CONNECTION = None
 
-    def _is_connection_alive(self):
+    def _is_kvm_supported(self):
+        # We check if /dev/kvm exists to ensure that kvm can be consumed on this machine.
+        # Libvirt will still start even if kvm cannot be used on the machine which would falsely
+        # give the impression that virtualization can be used. We have checks in place to check if system
+        # supports virtualization but if we incorporate that check in all of the vm exposed methods which
+        # consume libvirt, it would be an expensive call as we figure that out by making a subprocess call
+        return os.path.exists('/dev/kvm')
+
+    def _is_libvirt_connection_alive(self):
         with contextlib.suppress(libvirt.libvirtError):
             # We see isAlive call failed for a user in NAS-109072, it would be better
             # if we handle this to ensure that system recognises libvirt  connection
             # is no longer active and a new one should be initiated.
-            #
-            # We check if /dev/kvm exists to ensure that kvm can be consumed on this machine.
-            # Libvirt will still start even if kvm cannot be used on the machine which would falsely
-            # give the impression that virtualization can be used. We have checks in place to check if system
-            # supports virtualization but if we incorporate that check in all of the vm exposed methods which
-            # consume libvirt, it would be an expensive call as we figure that out by making a subprocess call
-            return self.LIBVIRT_CONNECTION and self.LIBVIRT_CONNECTION.isAlive() and os.path.exists('/dev/kvm')
+            return self.LIBVIRT_CONNECTION and self.LIBVIRT_CONNECTION.isAlive()
         return False
 
+    def _is_connection_alive(self):
+        return self._is_kvm_supported() and self._is_libvirt_connection_alive()
+
+    def _system_supports_virtualization(self):
+        if not self._is_kvm_supported():
+            raise CallError('This system does not support virtualization.')
+
     def _check_connection_alive(self):
-        if not self._is_connection_alive():
+        self._system_supports_virtualization()
+        if not self._is_libvirt_connection_alive():
             raise CallError('Failed to connect to libvirt')
 
     def _check_setup_connection(self):
