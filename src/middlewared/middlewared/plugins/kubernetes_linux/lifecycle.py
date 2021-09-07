40fa61df0a5dcaad3623d0df24f9a456b54d673f
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-09-07 22:19:38 +0500

    Retrieve datasets when validating k8s setup in optimized fashion

--- src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
@@ -144,9 +144,12 @@ class KubernetesService(Service):
         required_datasets = set(config['dataset']) | set(
             os.path.join(config['dataset'], ds) for ds in ('k3s', 'docker', 'releases')
         )
-        diff = {
-            d['id'] for d in await self.middleware.call('zfs.dataset.query', [['id', 'in', list(k8s_datasets)]])
-        } ^ k8s_datasets
+        existing_datasets = {
+            d['id']: d for d in await self.middleware.call(
+                'zfs.dataset.query', [['id', 'in', list(k8s_datasets)]], {'extra': {'properties': []}}
+            )
+        }
+        diff = set(existing_datasets) ^ k8s_datasets
         fatal_diff = diff.intersection(required_datasets)
         if fatal_diff:
             raise CallError(f'Missing "{", ".join(fatal_diff)}" dataset(s) required for starting kubernetes.')
