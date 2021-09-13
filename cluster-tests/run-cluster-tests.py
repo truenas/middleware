5f7bca7bd3dc5ab6cf04406479713bc42d58b54b
caleb <yocalebo@gmail.com>
2021-09-13 10:41:57 -0400

    call pytest instead of pytest-3

--- cluster-tests/run-cluster-tests.py
+++ cluster-tests/run-cluster-tests.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 import pathlib
 import subprocess
 
@@ -55,7 +54,7 @@ def setup_api_results_dir():
 
 
 def setup_pytest_command(args, results_path):
-    cmd = [f'pytest-{sys.version_info.major}', '-v', '-rfesp', f'--junit-xml={results_path}']
+    cmd = ['pytest', '-v', '-rfesp', f'--junit-xml={results_path}']
 
     # pytest is clever enough to search the "tests" subdirectory
     # and look at the argument that is passed and figure out if
