c4307231f0f8816ada0f02d3dff0971398d80c93
caleb <yocalebo@gmail.com>
2021-09-14 16:19:19 -0400

    add nfsd.scope and nfsd.owner_major on Core HA
    (cherry picked from commit d6a73a7f3272d5f219032219b94751fdf83e991a)

--- src/middlewared/middlewared/plugins/nfs_/v4_freebsd.py
+++ src/middlewared/middlewared/plugins/nfs_/v4_freebsd.py
@@ -1,7 +1,7 @@
 import subprocess
+import uuid
 
 import sysctl
-
 from middlewared.service import private, Service
 
 
@@ -23,6 +23,31 @@ class NFSService(Service):
             subprocess.run(["service", "gssd", "forcestop"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         if config["v4"]:
+            if self.middleware.call_sync("failover.licensed"):
+                # on HA systems, when using NFSv4, starting with (we believe) ESXi 7+,
+                # the ESXi NFS client expects that the nfs server scope or owner major
+                # string be the same between the controllers. If they mismatch, on failover
+                # the ESXi hypervisors go into a state that, apparently, requires them to be
+                # rebooted. This defeats the entire purpose of the "stateful"-ness of NFSv4.
+                # To prevent this scenario from occurring, we must set these values to a
+                # globally unique value between the controllers.
+                # NOTE: If this is changed while nfsd is started, then problems will occur
+                # so it's important that if you're reading this and decide to change it that
+                # you restart the nfsd service and then have your clients unmount and remount.
+                owner_major = config["v4_owner_major"]
+                if not owner_major:
+                    owner_major = str(uuid.uuid4())
+                    # write this to the database since it shouldn't change after it's been set
+                    # and has to be shared between controllers on HA systems
+                    self.middleware.call_sync(
+                        "datastore.update", "services.nfs", config["id"],
+                        {"v4_owner_major": owner_major},
+                        {"prefix": self._config.datastore_prefix},
+                    )
+
+                sysctl.filter("vfs.nfsd.scope")[0].value = owner_major
+                sysctl.filter("vfs.nfsd.owner_major")[0].value = owner_major
+
             sysctl.filter("vfs.nfsd.server_max_nfsvers")[0].value = 4
             if config["v4_v3owner"]:
                 # Per RFC7530, sending NFSv3 style UID/GIDs across the wire is now allowed
