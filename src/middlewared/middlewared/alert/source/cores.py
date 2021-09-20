b32fa81cebe1b4ae93214bc8509ce2e6517166f5
caleb <yocalebo@gmail.com>
2021-09-20 14:06:31 -0400

    rewrite coredump code on SCALE

--- src/middlewared/middlewared/alert/source/cores.py
+++ src/middlewared/middlewared/alert/source/cores.py
@@ -1,57 +1,35 @@
-import re
-
-from datetime import timedelta
-
-from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, AlertSource, Alert, IntervalSchedule
-from middlewared.utils import run
-
-
-SKIP_REGEX = re.compile(r'Unit:\s+containerd.service')
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, AlertSource, Alert
 
 
 class CoreFilesArePresentAlertClass(AlertClass):
     category = AlertCategory.SYSTEM
     level = AlertLevel.WARNING
-    title = "Core Files Found in System Database"
+    title = "Core Files Found in System Dataset"
     text = ("Core files for the following executables were found: %(corefiles)s. Please create a ticket at "
             "https://jira.ixsystems.com/ and attach the relevant core files along with a system debug. "
             "Once the core files have been archived and attached to the ticket, they may be removed "
             "by running the following command in shell: 'rm /var/db/system/cores/*'.")
 
-    products = ("CORE", "SCALE")
+    products = ("SCALE",)
 
 
 class CoreFilesArePresentAlertSource(AlertSource):
-    dumps = {}
-    dump_error_logged = []
-    schedule = IntervalSchedule(timedelta(hours=6))
-
     products = ("SCALE",)
+    ignore = ("syslog-ng.service", "containerd.service")
 
     async def check(self):
         corefiles = []
         for coredump in filter(lambda c: c["corefile"] == "present", await self.middleware.call("system.coredumps")):
-            if coredump["exe"] == "/usr/sbin/syslog-ng" or coredump["pid"] in self.dump_error_logged:
-                continue
+            if coredump["unit"] in self.ignore:
+                # Unit: "syslog-ng.service" has been core dumping for, literally, years
+                # on freeBSD and now also on linux. The fix is non-trivial and it seems
+                # to be very specific to how we implemented our system dataset. Anyways,
+                # the crash isn't harmful so we ignore it.
 
-            if coredump["pid"] not in self.dumps:
-                cp = await run(
-                    "coredumpctl", "info", str(coredump["pid"]), check=False, encoding="utf-8", errors="ignore"
-                )
-                if cp.returncode:
-                    self.middleware.logger.debug(
-                        "Unable to retrieve coredump information of %r process", coredump["pid"]
-                    )
-                    self.dump_error_logged.append(coredump["pid"])
-                    continue
-
-                self.dumps[coredump["pid"]] = {
-                    **coredump,
-                    "happened_in_container": bool(SKIP_REGEX.findall(cp.stdout))
-                }
-
-            if self.dumps[coredump["pid"]]["happened_in_container"]:
-                # We don't want to raise an alert to user about a container which died for some reason
+                # Unit: "containerd.service" is related to k3s.
+                # users are free to run whatever they would like to in containers
+                # and we don't officially support all the apps themselves so we
+                # ignore those core dumps
                 continue
 
             corefiles.append(f"{coredump['exe']} ({coredump['time']})")
--- src/middlewared/middlewared/plugins/system_/coredump.py
+++ src/middlewared/middlewared/plugins/system_/coredump.py
@@ -1,33 +1,30 @@
-# -*- coding=utf-8 -*-
-import logging
+from os.path import exists
+from systemd import journal
 
 from middlewared.service import private, Service
-from middlewared.utils import run
-
-logger = logging.getLogger(__name__)
 
 
 class SystemService(Service):
-    @private
-    async def coredumps(self):
-        coredumps = []
-        coredumpctl = await run("coredumpctl", "list", "--no-pager", encoding="utf-8", errors="ignore", check=False)
-        if coredumpctl.returncode != 0:
-            return []
 
-        lines = coredumpctl.stdout.splitlines()
-        header = lines.pop(0)
-        exe_pos = header.find("EXE")
-        for line in lines:
-            exe = line[exe_pos:]
-            time, pid, uid, gid, sig, corefile = line[:exe_pos].rsplit(maxsplit=5)
-            coredumps.append({
-                "time": time,
-                "pid": int(pid),
-                "uid": int(uid),
-                "gid": int(gid),
-                "sig": int(sig),
-                "corefile": corefile,
-                "exe": exe,
-            })
+    @private
+    def coredumps(self):
+        coredumps = []
+
+        try:
+            with journal.Reader() as reader:
+                reader.add_match(CODE_FUNC='submit_coredump')
+                for core in reader:
+                    coredumps.append({
+                        'time': core['COREDUMP_TIMESTAMP'].strftime('%c'),
+                        'pid': core['COREDUMP_PID'],
+                        'uid': core['COREDUMP_UID'],
+                        'gid': core['COREDUMP_GID'],
+                        'unit': core['COREDUMP_UNIT'],
+                        'sig': core['COREDUMP_SIGNAL'],
+                        'corefile': 'present' if exists(core['COREDUMP_FILENAME']) else 'missing',
+                        'exe': core['COREDUMP_EXE'],
+                    })
+        except Exception:
+            self.logger.warning('Failed to obtain coredump information', exc_info=True)
+
         return coredumps
