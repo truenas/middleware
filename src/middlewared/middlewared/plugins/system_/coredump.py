f4d56cd08110495e933d4055431c8e6d4cb95522
caleb <yocalebo@gmail.com>
2021-09-20 14:26:42 -0400

    address review

--- src/middlewared/middlewared/plugins/system_/coredump.py
+++ src/middlewared/middlewared/plugins/system_/coredump.py
@@ -14,16 +14,20 @@ class SystemService(Service):
             with journal.Reader() as reader:
                 reader.add_match(CODE_FUNC='submit_coredump')
                 for core in reader:
-                    coredumps.append({
+                    coredump = {
                         'time': core['COREDUMP_TIMESTAMP'].strftime('%c'),
                         'pid': core['COREDUMP_PID'],
                         'uid': core['COREDUMP_UID'],
                         'gid': core['COREDUMP_GID'],
                         'unit': core['COREDUMP_UNIT'],
                         'sig': core['COREDUMP_SIGNAL'],
-                        'corefile': 'present' if exists(core['COREDUMP_FILENAME']) else 'missing',
                         'exe': core['COREDUMP_EXE'],
-                    })
+                    }
+                    if isinstance(core['COREDUMP_FILENAME'], str):
+                        coredump['corefile'] = 'present' if exists(core['COREDUMP_FILENAME']) else 'missing'
+                    else:
+                        coredump['corefile'] = 'none'
+                    coredumps.append(coredump)
         except Exception:
             self.logger.warning('Failed to obtain coredump information', exc_info=True)
 
