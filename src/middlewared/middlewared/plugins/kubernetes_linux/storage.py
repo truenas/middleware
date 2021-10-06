93bf94602a803839d9ad3ae04a47195932bd5268
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-10-06 22:36:03 +0500

    Add metadata to resources to see if they consume locked host path

--- src/middlewared/middlewared/plugins/kubernetes_linux/storage.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/storage.py
@@ -1,4 +1,5 @@
 from middlewared.service import Service
+from middlewared.utils.path import is_child
 
 
 class KubernetesStorageService(Service):
@@ -9,6 +10,7 @@ class KubernetesStorageService(Service):
 
     async def get_resources_consuming_host_path(self):
         resources = {'deployment': [], 'statefulset': []}
+        locked_datasets = await self.middleware.call('zfs.dataset.locked_datasets')
         for r_type in resources:
             for resource in await self.middleware.call(
                 f'k8s.{r_type}.query', [['spec.template.spec.volumes', '!=', []]]
@@ -18,6 +20,12 @@ class KubernetesStorageService(Service):
                     if (v.get('host_path') or {}).get('path')
                 ]
                 if host_paths:
-                    resource['host_paths'] = host_paths
+                    resource.update({
+                        'host_paths': host_paths,
+                        'consumes_locked_paths': any(
+                            any(is_child(p, d['mountpoint']) for d in locked_datasets if d['mountpoint'])
+                            for p in host_paths
+                        ),
+                    })
                     resources[r_type].append(resource)
         return resources
