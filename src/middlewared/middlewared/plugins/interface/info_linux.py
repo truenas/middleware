a2756f1f37f615e99ea6144e802a486c2c9f2291
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2021-10-12 16:02:45 -0400

    required commits for RC.1 (#7668)
    * prevent "wg" from being modified
    
    * fix ctdb.utils.resolve_hostnames
    
    * fix nfs alias parameter

--- src/middlewared/middlewared/plugins/interface/info_linux.py
+++ src/middlewared/middlewared/plugins/interface/info_linux.py
@@ -9,4 +9,4 @@ class InterfaceService(Service, InterfaceInfoBase):
         namespace_alias = 'interfaces'
 
     async def internal_interfaces(self):
-        return ['lo', 'tun', 'tap', 'docker', 'veth', 'kube-bridge', 'kube-dummy-if', 'vnet', 'openvpn']
+        return ['wg', 'lo', 'tun', 'tap', 'docker', 'veth', 'kube-bridge', 'kube-dummy-if', 'vnet', 'openvpn']
