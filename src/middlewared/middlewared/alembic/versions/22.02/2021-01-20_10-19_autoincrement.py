b84a6425ea4c1a3ae12f9a5e294ed508e3b427fa
themylogin <themylogin@gmail.com>
2021-10-12 21:48:38 +0200

    More robust database schema rewrite
    (cherry picked from commit de7ce042835933e6bcf90f8a3315bdd0b6174baa)

--- src/middlewared/middlewared/alembic/versions/22.02/2021-01-20_10-19_autoincrement.py
+++ src/middlewared/middlewared/alembic/versions/22.02/2021-01-20_10-19_autoincrement.py
@@ -19,12 +19,15 @@ depends_on = None
 
 def upgrade():
     conn = op.get_bind()
+    op.execute("""
+        UPDATE directoryservice_idmap_domain SET idmap_domain_certificate_id = NULL WHERE idmap_domain_certificate_id = ''
+    """)  # NAS-111944
     conn.execute("PRAGMA legacy_alter_table = TRUE")
     for name, sql in conn.execute("SELECT name, sql FROM sqlite_master WHERE type = 'table'").fetchall():
-        if m := re.match(r'CREATE TABLE "(.+)" \(\s*"?id"? integer (NOT NULL |)PRIMARY KEY,', sql, flags=re.IGNORECASE):
+        if m := re.match(r'CREATE TABLE "(.+)" \((\s*|.+\s)"?id"? integer (NOT NULL |)PRIMARY KEY[,)]', sql, flags=re.IGNORECASE):
             table_name = m.group(1)
             new_sql = m.group(0).replace('PRIMARY KEY', 'PRIMARY KEY AUTOINCREMENT') + sql[len(m.group(0)):]
-        elif m := re.match(r'(CREATE TABLE "?(.+) \((\s*)"?id"? integer( NOT NULL|),)(.+)'
+        elif m := re.match(r'(CREATE TABLE "?(.+) \((\s*|.+\s)"?id"? integer( NOT NULL|),)(.+)'
                            r'\n\s(CONSTRAINT ([a-z_]+) |)PRIMARY KEY \(id\),?',
                            sql, flags=re.IGNORECASE | re.DOTALL):
             table_name = m.group(2).rstrip('"')
