f43d26126ac26391f8dad12177e525aee8cca67c
bugclerk <40872210+bugclerk@users.noreply.github.com>
2021-10-13 00:02:56 +0200

    NAS-112328 / 22.02-RC.1 / set nfsd.scope and nfsd.owner_major on Core HA (by yocalebo) (by bugclerk) (#7666)
    * add nfsd.scope and nfsd.owner_major on Core HA
    
    (cherry picked from commit d6a73a7f3272d5f219032219b94751fdf83e991a)
    (cherry picked from commit dbf51ff512d701b02ded9fd912b4897010470ac6)
    
    * add comment about setting 2 sysctls to same value
    
    (cherry picked from commit 3fdd1c0c70282e4c8ca6838a1f9f87c544c8062a)
    (cherry picked from commit 8548425387c8b6b73c5e9e633fbbaa69e090147f)
    
    * Merge migrations
    
    (cherry picked from commit fa47937de0acd0fd720517e02fa92a65e20fb15f)
    
    * Rename migration file to keep convenient files alphabetical order
    
    (cherry picked from commit ba20cc63a7c432371b5bf22bf6e7acdcf1a5d024)
    (cherry picked from commit 91023857879852bc42431633fc6f3011997b46ff)
    
    * 21.MM -> 22.02
    
    (cherry picked from commit 5301a7ec267d76b71b27bf88995199af1579908a)
    
    Co-authored-by: caleb <yocalebo@gmail.com>
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2021-10-12_19-08_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 2076c53a1a28
+Revises: df19c1a8d4ae, fee786dfe121
+Create Date: 2021-10-12 19:08:13.314329+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2076c53a1a28'
+down_revision = ('df19c1a8d4ae', 'fee786dfe121')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
