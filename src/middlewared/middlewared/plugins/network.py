6e943abd19b8d93b36439de28ee7ddce3ed1068f
bugclerk <40872210+bugclerk@users.noreply.github.com>
2021-10-14 19:14:01 +0200

    NAS-112817 / 12.0 / prevent "wg" from being modified (#7692)
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/network.py
+++ src/middlewared/middlewared/plugins/network.py
@@ -1849,7 +1849,7 @@ class InterfaceService(CRUDService):
 
         self.logger.info('Interfaces in database: {}'.format(', '.join(interfaces) or 'NONE'))
 
-        internal_interfaces = ['lo', 'pflog', 'pfsync', 'tun', 'tap', 'epair']
+        internal_interfaces = ['wg', 'lo', 'pflog', 'pfsync', 'tun', 'tap', 'epair']
         if not await self.middleware.call('system.is_freenas'):
             internal_interfaces.extend(await self.middleware.call('failover.internal_interfaces') or [])
         internal_interfaces = tuple(internal_interfaces)
