6735a6c57cef2c3285cf22d2eb52c3b0e2334a83
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-10-14 15:47:52 +0500

    Auto-renew system generated cert if it has expired or is about to expire

--- src/middlewared/middlewared/plugins/crypto_/renew_certs.py
+++ src/middlewared/middlewared/plugins/crypto_/renew_certs.py
@@ -9,20 +9,41 @@ class CertificateService(Service):
     @private
     @job(lock='acme_cert_renewal')
     def renew_certs(self, job):
-        certs = self.middleware.call_sync(
-            'certificate.query',
-            [['acme', '!=', None]]
-        )
+        system_cert = self.middleware.call_sync('system.general.config')['ui_certificate']
+        if system_cert and all(
+            system_cert[k] == v for k, v in (
+                ('organization', 'iXsystems'),
+                ('san', ['DNS:localhost']),
+                ('signedby', None),
+                ('cert_type_existing', True),
+            )
+        ):
+            filters = [(
+                'OR', (('acme', '!=', None), ('id', '=', system_cert['id']))
+            )]
+        else:
+            filters = [('acme', '!=', None)]
+        certs = self.middleware.call_sync('certificate.query', filters)
 
         progress = 0
         for cert in certs:
             progress += (100 / len(certs))
 
-            if (
+            if not (
                 datetime.datetime.strptime(cert['until'], '%a %b %d %H:%M:%S %Y') - datetime.datetime.utcnow()
-            ).days < cert['renew_days']:
-                # renew cert
-                self.logger.debug(f'Renewing certificate {cert["name"]}')
+            ).days < cert.get('renew_days', 5):
+                continue
+
+            # renew cert
+            self.logger.debug(f'Renewing certificate {cert["name"]}')
+            if not cert.get('acme'):
+                cert_str, key = self.middleware.call_sync('cryptokey.generate_self_signed_certificate')
+                cert_payload = {
+                    'certificate': cert_str,
+                    'privatekey': key,
+                }
+
+            else:
                 final_order = self.middleware.call_sync(
                     'acme.issue_certificate',
                     job, progress / 4, {
@@ -32,22 +53,23 @@ class CertificateService(Service):
                     },
                     cert
                 )
+                cert_payload = {
+                    'certificate': final_order.fullchain_pem,
+                    'acme_uri': final_order.uri,
+                }
 
-                self.middleware.call_sync(
-                    'datastore.update',
-                    'system.certificate',
-                    cert['id'],
-                    {
-                        'certificate': final_order.fullchain_pem,
-                        'acme_uri': final_order.uri
-                    },
-                    {'prefix': 'cert_'}
+            self.middleware.call_sync(
+                'datastore.update',
+                'system.certificate',
+                cert['id'],
+                cert_payload,
+                {'prefix': 'cert_'}
+            )
+            try:
+                self.middleware.call_sync('certificate.redeploy_cert_attachments', cert['id'])
+            except Exception:
+                self.logger.error(
+                    'Failed to reload services dependent on %r certificate', cert['name'], exc_info=True
                 )
-                try:
-                    self.middleware.call_sync('certificate.redeploy_cert_attachments', cert['id'])
-                except Exception:
-                    self.logger.error(
-                        'Failed to reload services dependent on %r certificate', cert['name'], exc_info=True
-                    )
 
             job.set_progress(progress)
