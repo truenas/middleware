ef47ad742193640957dbae7d82de32921363fd10
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-10-20 16:17:03 +0500

    Clean freebsd usages in syslog generation script

--- src/middlewared/middlewared/etc_files/syslogd.py
+++ src/middlewared/middlewared/etc_files/syslogd.py
@@ -6,20 +6,13 @@ import shutil
 import subprocess
 import textwrap
 
-from middlewared.utils import osc
 
 logger = logging.getLogger(__name__)
 
-if osc.IS_FREEBSD:
-    SYSLOG_NG_CONF_ORIG = "/conf/base/etc/local/syslog-ng.conf.freenas"
-    SYSLOG_NG_CONF = "/etc/local/syslog-ng.conf"
-    LOG_FILTER_PREFIX = ""
-    LOG_SOURCE = "src"
-else:
-    SYSLOG_NG_CONF_ORIG = "/conf/base/etc/syslog-ng/syslog-ng.conf"
-    SYSLOG_NG_CONF = "/etc/syslog-ng/syslog-ng.conf"
-    LOG_FILTER_PREFIX = "f_freebsd_"
-    LOG_SOURCE = "s_src"
+SYSLOG_NG_CONF_ORIG = "/conf/base/etc/syslog-ng/syslog-ng.conf"
+SYSLOG_NG_CONF = "/etc/syslog-ng/syslog-ng.conf"
+LOG_FILTER_PREFIX = "f_freebsd_"
+LOG_SOURCE = "s_src"
 
 
 def generate_syslog_remote_destination(middleware, advanced_config):
@@ -88,9 +81,7 @@ def generate_syslog_remote_destination(middleware, advanced_config):
 
 
 def generate_syslog_conf(middleware):
-    shutil.copy(SYSLOG_NG_CONF_ORIG, SYSLOG_NG_CONF)
-
-    with open(SYSLOG_NG_CONF) as f:
+    with open(SYSLOG_NG_CONF_ORIG) as f:
         syslog_conf = f.read()
 
     advanced_config = middleware.call_sync("system.advanced.config")
@@ -182,28 +173,17 @@ def generate_ha_syslog(middleware):
     with open(SYSLOG_NG_CONF, "w") as f:
         f.write(syslog_conf)
 
-    if osc.IS_FREEBSD:
-        # Be sure and copy fresh file since we're appending
-        shutil.copy("/conf/base/etc/newsyslog.conf.template", "/etc/newsyslog.conf")
-
-        with open("/etc/newsyslog.conf") as f:
-            newsyslog_conf = f.read()
-
-        newsyslog_conf += f"{controller_file}               640  10   200 @0101T JC\n"
-        with open("/etc/newsyslog.conf", "w") as f:
-            f.write(newsyslog_conf)
-    else:
-        with open("/etc/logrotate.d/truenas-ha", "w") as f:
-            for file in [controller_file]:
-                f.write(textwrap.dedent(f"""\
-                    {file} {{
-                        daily
-                        missingok
-                        rotate 10
-                        notifempty
-                        create 640 root adm
-                    }}
-                """))
+    with open("/etc/logrotate.d/truenas-ha", "w") as f:
+        for file in [controller_file]:
+            f.write(textwrap.dedent(f"""\
+                {file} {{
+                    daily
+                    missingok
+                    rotate 10
+                    notifempty
+                    create 640 root adm
+                }}
+            """))
 
 
 def use_syslog_dataset(middleware):
@@ -288,11 +268,10 @@ def configure_syslog(middleware):
 
 
 def reconfigure_logging(middleware):
-    if osc.IS_LINUX:
-        p = subprocess.run(["systemctl", "restart", "systemd-journald"], stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT, encoding="utf-8", errors="ignore")
-        if p.returncode != 0:
-            logger.warning("Unable to restart systemd-journald: %s", p.stdout)
+    p = subprocess.run(["systemctl", "restart", "systemd-journald"], stdout=subprocess.PIPE,
+                       stderr=subprocess.STDOUT, encoding="utf-8", errors="ignore")
+    if p.returncode != 0:
+        logger.warning("Unable to restart systemd-journald: %s", p.stdout)
 
     middleware.call_sync("core.reconfigure_logging")
 
