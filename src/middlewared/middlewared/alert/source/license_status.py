truenas/middleware

commit 76a41bfa1c5c0f36d9fac606a694f75290f59ebb

NAS-111143 / 21.08 / reduce system.info calls (#7073)
* dont call system.info in hactl

* use system.version in nginx.conf.mako

* use system.dmidecode_info in alert plugin

* use system.cpu_info in snmpd.conf.mako

* use system.build_time in motd.mako

* use socket.gethostname() in auth plugin

* call system.dmidecode_info in m50_plx

* call system.dmidecode_info in r50_nvme

* add private system.license method and call it in system.py and failover.py

* call system.license in iscsi_global

* call system.version in mail.py

* call system.cpu_info in events.py

* call system.license in support.py

* flake8 fixes and call system.birthday in support.py

* flake8 fix and call system.version and socket.gethostname() in portal.py

* call system.license and system.dmidecode_info in truenas.py

* flake8 fix and call system.version in update.py

* flake8 fix and call system.version and system.{cpu_info/mem_info} in usage.py

* call system.cpu_info in test_events_amd_cpu unittest

* flake8 fixes and dont call system.info in license_status.py

* call system.dmidecode_info in map.py

* call system.dmidecode_info in enclosure.py

src/middlewared/middlewared/alert/source/license_status.py
@@ -1,9 +1,3 @@
-# Copyright (c) 2020 iXsystems, Inc.
-# All rights reserved.
-# This file is a part of TrueNAS
-# and may not be copied and/or distributed
-# without the express permission of iXsystems.
-
 from collections import defaultdict
 from datetime import date, timedelta
 import textwrap
@@ -44,29 +38,30 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
     run_on_backup_node = False
 
     def check_sync(self):
-        license = self.middleware.call_sync('system.info')['license']
         alerts = []
-        if license is None:
+
+        local_license = self.middleware.call_sync('system.license')
+        if local_license is None:
             return Alert(LicenseAlertClass, "Your TrueNAS has no license, contact support.")
 
-        serial = self.middleware.call_sync('system.dmidecode_info')['system-serial-number']
-
-        if license['system_serial'] != serial and license['system_serial_ha'] != serial:
+        # check if this node's system serial matches the serial in the license
+        local_serial = self.middleware.call_sync('system.dmidecode_info')['system-serial-number']
+        if local_serial not in (local_license['system_serial'], local_license['system_serial_ha']):
             alerts.append(Alert(LicenseAlertClass, 'System serial does not match license.'))
 
-        standby_info = None
+        standby_license = standby_serial = None
         try:
-            if self.middleware.call_sync('failover.licensed'):
-                standby_info = self.middleware.call_sync('failover.call_remote', 'system.info')
+            if local_license['system_serial_ha']:
+                standby_license = self.middleware.call_sync('failover.call_remote', 'system.license')
+                standby_serial = self.middleware.call_sync(
+                    'failover.call_remote', 'system.dmidecode_info')['system-serial-number']
         except Exception:
             pass
 
-        if (
-            standby_info and standby_info.get('license') and
-            standby_info['system_serial'] != standby_info['license']['system_serial'] and
-            standby_info['system_serial'] != standby_info['license']['system_serial_ha']
-        ):
-            alerts.append(Alert(LicenseAlertClass, 'System serial of standby node does not match license.',))
+        if standby_license and standby_serial is not None:
+            # check if the remote node's system serial matches the serial in the license
+            if standby_serial not in (standby_license['system_serial'], standby_license['system_serial_ha']):
+                alerts.append(Alert(LicenseAlertClass, 'System serial of standby node does not match license.',))
 
         chassis_hardware = self.middleware.call_sync('truenas.get_chassis_hardware')
         hardware = chassis_hardware.replace('TRUENAS-', '').split('-')
@@ -169,16 +164,16 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
 
                 if days == 0:
                     alert_klass = LicenseHasExpiredAlertClass
-                    alert_text = textwrap.dedent(f"""\
+                    alert_text = textwrap.dedent("""\
                         SUPPORT CONTRACT EXPIRATION. To reactivate and continue to receive technical support and
                         assistance, contact iXsystems @ telephone: 1-855-473-7449
                     """)
                     subject = "Your TrueNAS support contract has expired"
-                    opening = textwrap.dedent(f"""\
+                    opening = textwrap.dedent("""\
                         As of today, your support contract has ended. You will no longer be eligible for technical
                         support and assistance for your TrueNAS system.
                     """)
-                    encouraging = textwrap.dedent(f"""\
+                    encouraging = textwrap.dedent("""\
                         It is still not too late to renew your contract but you must do so as soon as possible by
                         contacting your authorized TrueNAS Reseller or iXsystems (sales@iXsystems.com) today to avoid
                         additional costs and lapsed-contract fees.
@@ -195,7 +190,7 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
                             This is the final reminder regarding the impending expiration of your TrueNAS
                             {contract_type} support contract. As of today, it is set to expire in 2 weeks.
                         """)
-                        encouraging = textwrap.dedent(f"""\
+                        encouraging = textwrap.dedent("""\
                             We encourage you to urgently contact your authorized TrueNAS Reseller or iXsystems
                             (sales@iXsystems.com) directly to renew your contract before expiration so that you continue
                             to enjoy the peace of mind and benefits that come with our support contracts.
@@ -208,7 +203,7 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
                             available to you as an active support contract customer at:
                             https://www.ixsystems.com/support/ and click on the “TrueNAS Arrays” tab.
                         """)
-                        encouraging = textwrap.dedent(f"""\
+                        encouraging = textwrap.dedent("""\
                             We encourage you to contact your authorized TrueNAS Reseller or iXsystems directly
                             (sales@iXsystems.com) to renew your contract before expiration. Doing so ensures that
                             you continue to enjoy the peace of mind and benefits that come with support coverage.

src/freenas/usr/local/sbin/hactl
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-# -
-# Copyright (c) 2020 iXsystems, Inc.
-# All rights reserved.
-# This file is a part of TrueNAS
-# and may not be copied and/or distributed
-# without the express permission of iXsystems.
-
 import argparse
 import os
 import sys
@@ -39,13 +32,15 @@ def main(command, quiet):
             print("Node status: Standby (Active node unreachable)")
 
         if fo_exists:
-            print(f"Node serial: {client.call('system.info')['system_serial']}{' (ACTIVE)' if ret == 'MASTER' else ''}")
+            serial = client.call('system.dmidecode_info')['system-serial-number']
+            serial += f"{' (ACTIVE)' if ret == 'MASTER' else ''}"
+            print(f"Node serial: {serial}")
             if ret != "UNKNOWN":
                 try:
-                    other_node_serial = client.call('failover.call_remote', 'system.info')['system_serial']
+                    other_serial = client.call('failover.call_remote', 'system.dmidecode_info')['system-serial-number']
                 except Exception as e:
-                    other_node_serial = f'ERROR: {e}'
-                print(f"Other node serial: {other_node_serial}{' (ACTIVE)' if ret == 'BACKUP' else ''}")
+                    other_serial = f'ERROR: {e}'
+                print(f"Other node serial: {other_serial}{' (ACTIVE)' if ret == 'BACKUP' else ''}")
 
             ret = client.call('failover.disabled_reasons')
             if 'NO_CRITICAL_INTERFACES' in ret:

src/middlewared/middlewared/etc_files/local/nginx/nginx.conf.mako
@@ -71,7 +71,7 @@
         ssl_configuration = True
         middleware.call_sync('alert.oneshot_delete', 'WebUiCertificateSetupFailed', None)
 
-    system_version = middleware.call_sync('system.info')['version']
+    system_version = middleware.call_sync('system.version')
 
     if not any(i in general_settings['ui_httpsprotocols'] for i in ('TLSv1', 'TLSv1.1')):
         disabled_ciphers = ':!SHA1:!SHA256:!SHA384'

src/middlewared/middlewared/etc_files/local/snmpd.conf.mako
@@ -13,7 +13,7 @@
         uname = os.uname()
 
         hw_machine = uname.machine
-        hw_model = middleware.call_sync("system.info")["model"]
+        hw_model = middleware.call_sync("system.cpu_info")["cpu_model"]
         kern_ostype = uname.sysname
         kern_osrelease = uname.release
         kern_osrevision = uname.version

src/middlewared/middlewared/etc_files/motd.mako
@@ -1,5 +1,5 @@
 <%
-	buildtime = middleware.call_sync('system.info')['buildtime']
+	buildtime = middleware.call_sync('system.build_time')
 	motd = middleware.call_sync('system.advanced.config')['motd']
 %>\
 % if IS_FREEBSD:

src/middlewared/middlewared/plugins/alert.py
@@ -556,7 +556,7 @@ class AlertService(Service):
                             support = await self.middleware.call("support.config")
                             msg = [f"* {alert.formatted}" for alert in new_hardware_alerts]
 
-                            serial = (await self.middleware.call("system.info"))["system_serial"]
+                            serial = (await self.middleware.call("system.dmidecode_info"))["system-serial-number"]
 
                             for name, verbose_name in await self.middleware.call("support.fields"):
                                 value = support[name]

src/middlewared/middlewared/plugins/auth.py
@@ -523,8 +523,7 @@ class TwoFactorAuthService(ConfigService):
         return pyotp.totp.TOTP(
             config['secret'], interval=config['interval'], digits=config['otp_digits']
         ).provisioning_uri(
-            f'{(await self.middleware.call("system.info"))["hostname"]}@'
-            f'{await self.middleware.call("system.product_name")}',
+            f'{socket.gethostname()}@{await self.middleware.call("system.product_name")}',
             'iXsystems'
         )
 

src/middlewared/middlewared/plugins/enclosure.py
@@ -344,32 +344,34 @@ class EnclosureService(CRUDService):
                     element.device_slot_set("clear")
 
     def __get_enclosures(self):
-        return Enclosures(self.middleware.call_sync("enclosure.get_ses_enclosures"),
-                          self.middleware.call_sync("system.info"))
+        return Enclosures(
+            self.middleware.call_sync("enclosure.get_ses_enclosures"),
+            self.middleware.call_sync("system.dmidecode_info")["system-product-name"]
+        )
 
 
 class Enclosures(object):
 
-    def __init__(self, stat, system_info):
+    def __init__(self, stat, product_name):
         blacklist = [
             "VirtualSES",
         ]
-        if (
-            system_info["system_product"] and
-            system_info["system_product"].startswith("TRUENAS-") and
-            "-MINI-" not in system_info["system_product"] and
-            system_info["system_product"] not in ["TRUENAS-R20", "TRUENAS-R20A"]
-        ):
-            blacklist.append("AHCI SGPIO Enclosure 2.00")
+        if product_name:
+            if (
+                product_name.startswith("TRUENAS-") and
+                "-MINI-" not in product_name and
+                product_name not in ["TRUENAS-R20", "TRUENAS-R20A"]
+            ):
+                blacklist.append("AHCI SGPIO Enclosure 2.00")
 
         self.__enclosures = []
         enclosures_tail = []
         for num, data in stat.items():
-            enclosure = Enclosure(num, data, stat, system_info)
+            enclosure = Enclosure(num, data, stat, product_name)
             if any(s in enclosure.encname for s in blacklist):
                 continue
             if (
-                system_info["system_product"] in ["TRUENAS-R20", "TRUENAS-R20A"] and
+                product_name and product_name in ["TRUENAS-R20", "TRUENAS-R20A"] and
                 enclosure.encname == "AHCI SGPIO Enclosure 2.00"
             ):
                 if enclosure.model.endswith("Drawer #2"):
@@ -410,10 +412,10 @@ class Enclosures(object):
 
 class Enclosure(object):
 
-    def __init__(self, num, data, stat, system_info):
+    def __init__(self, num, data, stat, product_name):
         self.num = num
         self.stat = stat
-        self.system_info = system_info
+        self.product_name = product_name
         self.devname, data = data
         self.encname = ""
         self.encid = ""
@@ -494,7 +496,7 @@ class Enclosure(object):
             self.model = "M Series"
             self.controller = True
         elif R_SERIES_REGEX.match(self.encname) or R20A_REGEX.match(self.encname):
-            self.model = self.system_info["system_product"].replace("TRUENAS-", "")
+            self.model = self.product_name.replace("TRUENAS-", "")
             self.controller = True
             if self.model in ["R20", "R20A"]:
                 self.model = f"{self.model}, Drawer #1"
@@ -502,11 +504,11 @@ class Enclosure(object):
                 index = [v for v in self.stat.values() if "ECStream FS2" in v].index(data)
                 self.model = f"{self.model}, Drawer #{index + 1}"
         elif (
-            self.system_info["system_product"] in ["TRUENAS-R20", "TRUENAS-R20A"] and
+            self.product_name in ["TRUENAS-R20", "TRUENAS-R20A"] and
             self.encname == "AHCI SGPIO Enclosure 2.00" and
             len(data.splitlines()) == 6
         ):
-            self.model = f"{self.system_info['system_product'].replace('TRUENAS-', '')}, Drawer #2"
+            self.model = f"{self.product_name.replace('TRUENAS-', '')}, Drawer #2"
             self.controller = True
         elif m := R50_REGEX.match(self.encname):
             self.model = f"R50, Drawer #{m.group(1)}"

commit 120813434c4f2abfffad379879d149e4079e03c7

Fix bug introduced in 76a41bfa1c5c0f36d9fac606a694f75290f59ebb

src/middlewared/middlewared/alert/source/license_status.py
@@ -78,35 +78,35 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
             alerts.append(Alert(LicenseAlertClass, 'You are not running TrueNAS on supported hardware.'))
         else:
             if hardware[0] == 'M':
-                if not license['model'].startswith('M'):
+                if not local_license['model'].startswith('M'):
                     alerts.append(Alert(
                         LicenseAlertClass,
                         (
                             'Your license was issued for model "%s" but it was '
                             ' detected as M series.'
-                        ) % license['model']
+                        ) % local_license['model']
                     ))
             elif hardware[0] == 'X':
-                if not license['model'].startswith('X'):
+                if not local_license['model'].startswith('X'):
                     alerts.append(Alert(
                         LicenseAlertClass,
                         (
                             'Your license was issued for model "%s" but it was '
                             ' detected as X series.'
-                        ) % license['model']
+                        ) % local_license['model']
                     ))
             elif hardware[0] == 'Z':
-                if not license['model'].startswith('Z'):
+                if not local_license['model'].startswith('Z'):
                     alerts.append(Alert(
                         LicenseAlertClass,
                         (
                             'Your license was issued for model "%s" but it was '
                             ' detected as Z series.'
-                        ) % license['model']
+                        ) % local_license['model']
                     ))
             else:
                 if hardware[0] in HW_MODELS:
-                    if hardware[0] != license['model']:
+                    if hardware[0] != local_license['model']:
                         alerts.append(Alert(
                             LicenseAlertClass,
                             (
@@ -114,7 +114,7 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
                                 'but it was detected as "%(model)s".'
                             ) % {
                                 'model': hardware[0],
-                                'license': license['model'],
+                                'license': local_license['model'],
                             }
                         ))
 
@@ -130,8 +130,8 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
 
             enc_nums[enc['model']] += 1
 
-        if license['addhw']:
-            for quantity, code in license['addhw']:
+        if local_license['addhw']:
+            for quantity, code in local_license['addhw']:
                 if code not in LICENSE_ADDHW_MAPPING:
                     self.middleware.logger.warning('Unknown additional hardware code %d', code)
                     continue
@@ -162,12 +162,13 @@ class LicenseStatusAlertSource(ThreadedAlertSource):
             return alerts
 
         for days in [0, 14, 30, 90, 180]:
-            if license['contract_end'] <= date.today() + timedelta(days=days):
-                serial_numbers = ", ".join(list(filter(None, [license['system_serial'], license['system_serial_ha']])))
-                contract_start = license['contract_start'].strftime("%B %-d, %Y")
-                contract_expiration = license['contract_end'].strftime("%B %-d, %Y")
-                contract_type = license['contract_type'].lower()
-                customer_name = license['customer_name']
+            if local_license['contract_end'] <= date.today() + timedelta(days=days):
+                serial_numbers = ", ".join(list(filter(None, [local_license['system_serial'],
+                                                              local_license['system_serial_ha']])))
+                contract_start = local_license['contract_start'].strftime("%B %-d, %Y")
+                contract_expiration = local_license['contract_end'].strftime("%B %-d, %Y")
+                contract_type = local_license['contract_type'].lower()
+                customer_name = local_license['customer_name']
 
                 if days == 0:
                     alert_klass = LicenseHasExpiredAlertClass