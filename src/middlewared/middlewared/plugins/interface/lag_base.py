843e7ce44a8422a000f49d6b11059b30d0b23e13
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2021-10-25 11:47:52 -0400

    remove unused code and clean-up lag_linux.py (#7755)

--- src/middlewared/middlewared/plugins/interface/lag_base.py
+++
@@ -1,7 +0,0 @@
-from middlewared.service import private, ServicePartBase
-
-
-class InterfaceLagBase(ServicePartBase):
-    @private
-    async def lag_supported_protocols(self):
-        return ['LACP', 'FAILOVER', 'LOADBALANCE']
