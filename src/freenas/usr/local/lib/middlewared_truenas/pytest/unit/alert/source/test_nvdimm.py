12900a080f27101f08e35874d2b60ddab1df2d7b
themylogin <themylogin@gmail.com>
2021-10-29 18:57:07 +0200

    Alert when ARM_INFO is not set for NVDIMM with specification 2.2 or later

--- src/freenas/usr/local/lib/middlewared_truenas/pytest/unit/alert/source/test_nvdimm.py
+++ src/freenas/usr/local/lib/middlewared_truenas/pytest/unit/alert/source/test_nvdimm.py
@@ -29,6 +29,7 @@ def test__produce_nvdimm_alerts__ok():
             ES Current Temperature: 23 C
             Total Runtime: 65535
         """),
+        "21",
     )
 
     assert alerts == []
@@ -54,6 +55,7 @@ def test__produce_nvdimm_alerts__PERSISTENCY_RESTORED():
             ES Current Temperature: 23 C
             Total Runtime: 65535
         """),
+        "21",
     )
 
     assert alerts == []
@@ -79,15 +81,43 @@ def test__produce_nvdimm_alerts__everything_is_broken():
             ES Current Temperature: 0 C
             Total Runtime: 65535
         """),
+        "21",
     )
 
     assert len(alerts) == 6
-    assert alerts[0].title % alerts[0].args == "NVDIMM 0 Critical Health Info is 0x1"
-    assert alerts[1].title % alerts[1].args == \
-        "NVDIMM 0 Module Health is 0x900: NOT ENOUGH ENERGY FOR CSAVE, NO ES PRESENT"
-    assert alerts[2].title % alerts[2].args == "NVDIMM 0 Error Threshold Status is 0x3"
-    assert alerts[3].title % alerts[3].args == "NVDIMM 0 Warning Threshold Status is 0x4"
-    assert alerts[4].title % alerts[4].args == "NVDIMM 0 NVM Lifetime is 15%"
-    assert alerts[4].level.name == "WARNING"
-    assert alerts[5].title % alerts[5].args == "NVDIMM 0 ES Lifetime is 5%"
-    assert alerts[5].level.name == "CRITICAL"
+    assert alerts[0].klass.text % alerts[0].args == "NVDIMM 0 Critical Health Info is 0x1."
+    assert alerts[1].klass.text % alerts[1].args == \
+        "NVDIMM 0 Module Health is 0x900: NOT ENOUGH ENERGY FOR CSAVE, NO ES PRESENT."
+    assert alerts[2].klass.text % alerts[2].args == "NVDIMM 0 Error Threshold Status is 0x3."
+    assert alerts[3].klass.text % alerts[3].args == "NVDIMM 0 Warning Threshold Status is 0x4."
+    assert alerts[4].klass.text % alerts[4].args == "NVDIMM 0 NVM Lifetime is 15%."
+    assert alerts[4].klass.level.name == "WARNING"
+    assert alerts[5].klass.text % alerts[5].args == "NVDIMM 0 ES Lifetime is 5%."
+    assert alerts[5].klass.level.name == "CRITICAL"
+
+
+def test__produce_nvdimm_alerts__ARM_INFO():
+    alerts = produce_nvdimm_alerts(
+        0,
+        textwrap.dedent("""\
+            Critical Health Info: 0x0
+        """),
+        textwrap.dedent("""\
+            Module Health: 0x0
+            Module Current Temperature: 34 C
+            Error Threshold Status: 0x0
+            Warning Threshold Status: 0x0
+            NVM Lifetime: 100%
+            Count of DRAM Uncorrectable ECC Errors: 0
+            Count of DRAM Correctable ECC Error Above Threshold Events: 0
+        """),
+        textwrap.dedent("""\
+            ES Lifetime Percentage: 99%
+            ES Current Temperature: 23 C
+            Total Runtime: 65535
+        """),
+        "22",
+    )
+
+    assert len(alerts) == 1
+    assert alerts[0].klass.text % alerts[0].args == "NVDIMM 0 ARM_INFO is not set."
