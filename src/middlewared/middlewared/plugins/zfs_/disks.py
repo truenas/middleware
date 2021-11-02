ff83800e4adce500bad484720006866e5a2b2bbe
caleb <yocalebo@gmail.com>
2021-11-02 14:10:25 -0400

    make zfs.pool.get_disks match 12

--- src/middlewared/middlewared/plugins/zfs_/disks.py
+++ src/middlewared/middlewared/plugins/zfs_/disks.py
@@ -1,3 +1,4 @@
+from libzfs import ZFS
 from middlewared.service import Service
 
 
@@ -9,24 +10,25 @@ class ZFSPoolService(Service):
         process_pool = True
 
     def get_disks(self, name):
-        pool_disks = []
-        label_xml = self.middleware.call_sync('geom.get_class_xml', 'LABEL')
-        dev_xml = self.middleware.call_sync('geom.get_class_xml', 'DEV')
-        disks = self.middleware.call_sync('geom.get_disks')
-        for dev in self.middleware.call_sync('zfs.pool.get_devices', name):
-            dev = dev.replace('.eli', '')
-            found = label_xml.find(f'.//provider[name="{dev}"]/../consumer/provider')
-            name = None
-            if found:
-                name = dev
-            else:
-                g = dev_xml.find(f'./geom[name="{dev}"]')
-                if g:
-                    name = g.find('name').text
+        try:
+            with ZFS() as zfs:
+                disks = [i.replace('/dev/', '').replace('.eli', '') for i in zfs.get(name).disks]
+        except Exception:
+            self.logger.error('Failed to retrieve disks for %r', name, exc_info=True)
+            return []
 
-            if name and (name.startswith('multipath/') or name in disks):
-                pool_disks.append(name)
+        pool_disks = []
+        info = self.middleware.call_sync('disk.label_to_dev_and_disk')
+        for disk in disks:
+            found_disk = None
+            found_label = info['label_to_dev'].get(disk)
+            if found_label:
+                found_disk = info['dev_to_disk'].get(found_label)
             else:
-                self.logger.debug('Disk %r not found in zpool %r', dev, name)
+                # maybe the disk for thi zpool doesn't have a label (freenas-boot/boot-pool)
+                # we still need to try and find the raw disk
+                found_disk = info['dev_to_disk'].get(disk)
+
+            pool_disks.append(found_disk) if found_disk else None
 
         return pool_disks
