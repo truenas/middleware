61186efa698ce77fc0aac44e6e13546350cb6b12
themylogin <themylogin@gmail.com>
2021-11-15 16:45:03 +0100

    Fix setup_test.py in non-virtualenv

--- src/middlewared/middlewared/__init__.py
+++ src/middlewared/middlewared/__init__.py
@@ -1,1 +1,9 @@
 from .client import Client  # NOQA
+
+# Fool setuptools to prevent
+#     error: Namespace package problem: middlewared is a namespace package, but its
+#     __init__.py does not call declare_namespace()! Please fix it.
+#     (See the setuptools manual under "Namespace Packages" for details.)
+# when running setup_test.py
+# (it checks for presence of declare_namespace in __init__.py so the above paragraph
+# alone does the job)
