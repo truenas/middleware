3456e26afe27366f511b69595f7af4c896ead2cd
ericbsd <ericturgeon.bsd@gmail.com>
2021-11-17 16:59:13 -0400

    Exclude noip test if config is missing in test_180_dyndns.py
    Exclude custom provider test if config is missing in test_180_dyndns.py

--- tests/api2/test_180_dyndns.py
+++ tests/api2/test_180_dyndns.py
@@ -8,21 +8,17 @@ from time import sleep
 from pytest_dependency import depends
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from config import *
 from auto_config import ip, user, password, dev_test
 from functions import GET, POST, PUT, SSH_TEST
 # comment pytestmark for development testing with --dev-test
 pytestmark = pytest.mark.skipif(dev_test, reason='Skip for testing')
 
-Reason = 'NOIPUSERNAME, NOIPPASSWORD and NOIPHOST' \
-    ' are missing in ixautomation.conf'
+try:
+    from config import NOIPUSERNAME, NOIPPASSWORD, NOIPHOST
+    noip_test_cfg = True
+except ImportError:
+    noip_test_cfg = False
 
-noip_test_cfg = pytest.mark.skipif(all(['NOIPUSERNAME' in locals(),
-                                        'NOIPPASSWORD' in locals(),
-                                        'NOIPHOST' in locals()
-                                        ]) is False, reason=Reason)
-custom_test_cfg = pytest.mark.skipif(noip_test_cfg is True,
-                                     reason='no-ip test has ran instead')
 global test
 test = ''
 
@@ -35,34 +31,34 @@ def test_01_get_dyndns_provider_choices():
 
 
 def test_02_Updating_Settings_with_a_not_supported_provider_expect_422():
-    global test
     results = PUT('/dyndns/', {
         'provider': 'ixsystems.com'})
     assert results.status_code == 422, results.text
 
 
-@noip_test_cfg
-def test_03_Updating_Settings_for_NO_IP():
-    global test
-    results = PUT('/dyndns/', {
-        'username': NOIPUSERNAME,
-        'password': NOIPPASSWORD,
-        'provider': 'default@no-ip.com',
-        'domain': NOIPHOST})
-    assert results.status_code == 200, results.text
-    test = 'NOIP'
+# Exclude noip test if config is missing
+if noip_test_cfg:
+    def test_03_Updating_Settings_for_NO_IP():
+        global test
+        results = PUT('/dyndns/', {
+            'username': NOIPUSERNAME,
+            'password': NOIPPASSWORD,
+            'provider': 'default@no-ip.com',
+            'domain': NOIPHOST})
+        assert results.status_code == 200, results.text
+        test = 'NOIP'
 
-
-@custom_test_cfg
-def test_04_Updating_Settings_for_Custom_Provider():
-    global test
-    results = PUT('/dyndns/', {
-        'username': 'foo',
-        'password': 'abcd1234',
-        'provider': 'default@dyndns.org',
-        'domain': ['foobar']})
-    assert results.status_code == 200, results.text
-    test = 'CUSTOM'
+# Exclude custom provider test if config is missing
+if not noip_test_cfg:
+    def test_04_Updating_Settings_for_Custom_Provider():
+        global test
+        results = PUT('/dyndns/', {
+            'username': 'foo',
+            'password': 'abcd1234',
+            'provider': 'default@dyndns.org',
+            'domain': ['foobar']})
+        assert results.status_code == 200, results.text
+        test = 'CUSTOM'
 
 
 def test_05_Check_that_API_reports_dyndns_service():
