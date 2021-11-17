69cd514b4aa3da67937ccab8e720543b9cb37ce1
themylogin <themylogin@gmail.com>
2021-11-17 17:35:16 +0100

    Allow larger reporting databases
    (cherry picked from commit d4bc0e4815f75054a2ad465d550c06a163d5fa53)

--- src/middlewared/middlewared/alert/source/reporting_db.py
+++ src/middlewared/middlewared/alert/source/reporting_db.py
@@ -14,19 +14,19 @@ class ReportingDbAlertClass(AlertClass):
 
 class ReportingDbAlertSource(ThreadedAlertSource):
     def check_sync(self):
-        rrd_size_alert_threshold = 1610611911  # bytes
-
         try:
             used = shutil.disk_usage('/var/db/collectd/rrd').used
         except FileNotFoundError:
             raise UnavailableException()
 
-        if used > rrd_size_alert_threshold:
+        threshold = 1073741824 + len(self.middleware.call_sync('disk.query')) * 1024 * 1024
+
+        if used > threshold:
             # zfs list reports in kibi/mebi/gibi(bytes) but
             # format_size() calculates in kilo/mega/giga by default
             # so the report that we send the user needs to match
             # up with what zfs list reports as to not confuse anyone
             used = format_size(used, binary=True)
-            threshold = format_size(rrd_size_alert_threshold, binary=True)
+            threshold = format_size(threshold, binary=True)
 
             return Alert(ReportingDbAlertClass, {'used': used, 'threshold': threshold}, key=None)
