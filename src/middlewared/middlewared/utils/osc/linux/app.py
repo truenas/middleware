4307ad33defd877b204d103ca6b4b71d25f987ce
themylogin <themylogin@gmail.com>
2021-11-24 11:44:10 +0100

    Fix system.is_stable

--- src/middlewared/middlewared/utils/osc/linux/app.py
+++ src/middlewared/middlewared/utils/osc/linux/app.py
@@ -1,5 +1,4 @@
 # -*- coding=utf-8 -*-
-import re
 import json
 import logging
 
@@ -7,15 +6,13 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['get_app_version']
 
-RE_UNSTABLE = re.compile('.+-[0-9]{12}$')
-
 
 def get_app_version():
     with open('/data/manifest.json') as f:
         manifest = json.load(f)
 
     return {
-        'stable': not RE_UNSTABLE.match(manifest['version']),
+        'stable': 'MASTER' not in manifest['version'],
         'version': manifest['version'],
         'fullname': f"TrueNAS-SCALE-{manifest['version']}",
         'buildtime': manifest['buildtime'],
