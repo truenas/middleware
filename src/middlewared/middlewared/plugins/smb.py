395685e7d23fff72588d5e3a947f9885bce91842
Andrew Walker <awalker@ixsystems.com>
2021-12-13 08:26:46 -0500

    Allow enabling multichannel SMB through API

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -641,11 +641,6 @@ class SMBService(TDBWrapConfigService):
         except ValidationErrors as errs:
             verrors.add_child('smb_update.smb_options', errs)
 
-        if new.get('multichannel', False) is True:
-            verrors.add(
-                'smb_update.multichannel',
-                'Multichannel SMB is not supported at this time'
-            )
         if new.get('unixcharset') and new['unixcharset'] not in await self.unixcharset_choices():
             verrors.add(
                 'smb_update.unixcharset',
