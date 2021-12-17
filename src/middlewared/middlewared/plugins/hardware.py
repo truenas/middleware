c677824c19c10528b781cfd88f7c3bc055cca0b4
themylogin <themylogin@gmail.com>
2021-12-17 13:10:13 +0100

    We don't have @returns in 13

--- src/middlewared/middlewared/plugins/hardware.py
+++ src/middlewared/middlewared/plugins/hardware.py
@@ -1,5 +1,5 @@
 from middlewared.service import Service, private, periodic
-from middlewared.schema import accepts, returns, Dict, List, Str
+from middlewared.schema import accepts
 
 
 class HardwareEventsService(Service):
@@ -8,21 +8,14 @@ class HardwareEventsService(Service):
         namespace = 'hardware.events'
 
     @accepts()
-    @returns(List('mca_events', items=[Str('mca_event')]))
     async def mca(self):
         return (await self.middleware.call('hardware.report'))['MCA_EVENTS']
 
     @accepts()
-    @returns(List('apei_events', items=[Dict('apei_event', additional_attrs=True)]))
     async def apei(self):
         return (await self.middleware.call('hardware.report'))['APEI_EVENTS']
 
     @accepts()
-    @returns(Dict(
-        'events',
-        List('mca_events', items=[Str('mca_event')]),
-        List('mca_events', items=[Dict('apei_event', additional_attrs=True)])
-    ))
     async def report(self):
         return await self.middleware.call('hardware.report')
 
