daf68a659d024ca40e8d15ba9fc8f100fc0bde6c
ericbsd <ericturgeon.bsd@gmail.com>
2021-12-22 15:30:06 -0400

    Only run fenced test if on --ha

--- tests/api2/test_006_fenced.py
+++ tests/api2/test_006_fenced.py
@@ -1,18 +1,16 @@
-import pytest
 import sys
 from os import getcwd, environ
 apifolder = getcwd()
 sys.path.append(apifolder)
 
 from functions import make_ws_request
-from auto_config import ha, dev_test
+from auto_config import ha
 
-pytestmark = pytest.mark.skipif(not ha or dev_test, reason='Only applicable to HA')
-IP = environ.get('controller1_ip')
-assert IP, 'Need controller 1 IP before this will work'
+if ha:
+    IP = environ.get('controller1_ip')
+    assert IP, 'Need controller 1 IP before this will work'
 
-
-def test_01_verify_fenced_is_running(request):
-    payload = {'msg': 'method', 'method': 'failover.fenced.run_info', 'params': []}
-    res = make_ws_request(IP, payload)
-    assert res['result']['running'], res
+    def test_01_verify_fenced_is_running(request):
+        payload = {'msg': 'method', 'method': 'failover.fenced.run_info', 'params': []}
+        res = make_ws_request(IP, payload)
+        assert res['result']['running'], res
