61c2bfa661a6604f80069adba85931f195e36a24
themylogin <themylogin@gmail.com>
2021-12-23 18:26:01 +0100

    Install sphinx-prompt when building doc

--- docs/install.sh
+++ docs/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 apt update
-apt install -y python3-pip python3-sphinx-rtd-theme
+apt install -y python3-pip python3-sphinx-prompt python3-sphinx-rtd-theme
 pip3 install Sphinx-Substitution-Extensions
