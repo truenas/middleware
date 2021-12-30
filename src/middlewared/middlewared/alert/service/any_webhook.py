720e2884fb5e759c194f8718f9b8a8b86bf68e54
Zorglube <630192+zorglube@users.noreply.github.com>
2021-12-30 18:50:39 +0100

    simplify any_webhook

--- src/middlewared/middlewared/alert/service/any_webhook.py
+++ src/middlewared/middlewared/alert/service/any_webhook.py
@@ -15,48 +15,26 @@ class AnyWebhookAlerteService(ThreadedAlertService):
     schema = Dict(
         "AnyWebhook_attributes",
         Str("url", requierd=True, empty=False),
-        Str("user_key", requierd=True, empty=False),
-        Str("user_val", requierd=True, empty=False),
-        Str("pass_key", requierd=True, empty=False),
-        Str("pass_value", requierd=True, empty=False),
-        Str("message_key", requierd=True, empty=False),
-        Str("raw_key", requierd=False, empty=True),
-        Str("raw_data", requierd=False, empty=True),
-        Bool("get_post", required=False),
-        Bool("json_body", requierd=False)
+        Str("body_template", requierd=True, empty=False),
+        Bool("get_or_post", required=False)
     )
 
     def send_sync(self, alerts, gone_alerts, new_alerts):
-        alertMsg = self._format_alerts(alerts, gone_alerts, new_alerts)
-        getPost = self.attributes["get_post"]
-        jsonBody = self.attributes["json_body"]
+        url = self.attributes["url"]
+        msg = self._format_alerts(alerts, gone_alerts, new_alerts)
+        getorpost = self.attributes["get_post"]
+        body = self.attributes["body_template"].replace("%msg", msg)
 
-        if getPost == False:
+        if getorpost == False:
             r = requests.get(
-                url=self.attributes["url"],
-                params={
-                    self.attributes["user_key"]: self.attributes["user_val"],
-                    self.attributes["pass_key"]: self.attributes["pass_key"],
-                    self.attributes["message_key"]: alertMsg,
-                    self.attributes["raw_key"]: self.attributes["raw_data"]
-                },
-                timeout=INTERNET_TIMEOUT
+                url = f"{url}?{body}",
+                timeout = INTERNET_TIMEOUT
             )
         else:
-            if jsonBody == False:
-                datas = self.attributes["user_key"] + "=" + self.attributes["user_val"] + "&" + self.attributes["pass_key"] + "=" + self.attributes["pass_key"] + "&" + self.attributes["message_key"] + "=" + alertMsg + "&" + self.attributes["raw_key"] + "=" + self.attributes["raw_data"]
-            else:
-                datas = json.dumps({
-                    self.attributes["user_key"]: self.attributes["user_val"],
-                    self.attributes["pass_key"]: self.attributes["pass_key"],
-                    self.attributes["message_key"]: alertMsg,
-                    self.attributes["raw_key"]: self.attributes["raw_data"]
-                })
-
             r = requests.post(
-                url=self.attributes["url"],
-                data=datas,
-                timeout=INTERNET_TIMEOUT
+                url = self.attributes["url"],
+                data = body,
+                timeout = INTERNET_TIMEOUT
             )
         
         r.raise_for_status()
