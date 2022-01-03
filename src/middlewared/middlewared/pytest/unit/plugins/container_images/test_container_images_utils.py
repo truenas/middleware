b475ed83cec44b44fe8158b7f7de74fcadf2a077
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-01-03 20:30:47 +0500

    Add a boolean flag to indicate reference is a digest

--- src/middlewared/middlewared/pytest/unit/plugins/container_images/test_container_images_utils.py
+++ src/middlewared/middlewared/pytest/unit/plugins/container_images/test_container_images_utils.py
@@ -10,6 +10,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'latest',
         'registry': 'registry-1.docker.io',
         'complete_tag': 'registry-1.docker.io/library/redis:latest',
+        'reference_is_digest': False,
     }),
     ('redis:12.1', {
         'reference': 'redis:12.1',
@@ -17,6 +18,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': '12.1',
         'registry': 'registry-1.docker.io',
         'complete_tag': 'registry-1.docker.io/library/redis:12.1',
+        'reference_is_digest': False,
     }),
     ('redis:12.1', {
         'reference': 'redis:12.1',
@@ -24,6 +26,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': '12.1',
         'registry': 'registry-1.docker.io',
         'complete_tag': 'registry-1.docker.io/library/redis:12.1',
+        'reference_is_digest': False,
     }),
     ('redis@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546', {
         'reference': 'redis@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
@@ -31,6 +34,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
         'registry': 'registry-1.docker.io',
         'complete_tag': 'registry-1.docker.io/library/redis@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
+        'reference_is_digest': True,
     }),
     ('redis:12.1@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546', {
         'reference': 'redis:12.1@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
@@ -38,6 +42,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
         'registry': 'registry-1.docker.io',
         'complete_tag': 'registry-1.docker.io/library/redis@sha256:54ee15a0b0d2c661d46b9bfbf55b181f9a4e7ddf8bf693eec5703dac2c0f5546',
+        'reference_is_digest': True,
     }),
     ('ghcr.io/k8s-at-home/transmission', {
         'reference': 'ghcr.io/k8s-at-home/transmission',
@@ -45,6 +50,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'latest',
         'registry': 'ghcr.io',
         'complete_tag': 'ghcr.io/k8s-at-home/transmission:latest',
+        'reference_is_digest': False,
     }),
     ('ghcr.io/k8s-at-home/transmission:v3.00', {
         'reference': 'ghcr.io/k8s-at-home/transmission:v3.00',
@@ -52,6 +58,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'v3.00',
         'registry': 'ghcr.io',
         'complete_tag': 'ghcr.io/k8s-at-home/transmission:v3.00',
+        'reference_is_digest': False,
     }),
     ('ghcr.io/k8s-at-home/transmission:v3.00@sha256:355f4036c53c782df1957de0e16c63f4298f5b596ae5e621fea8f9ef02dd09e6', {
         'reference': 'ghcr.io/k8s-at-home/transmission:v3.00@sha256:355f4036c53c782df1957de0e16c63f4298f5b596ae5e621fea8f9ef02dd09e6',
@@ -59,6 +66,7 @@ from middlewared.plugins.docker_linux.utils import normalize_reference
         'tag': 'sha256:355f4036c53c782df1957de0e16c63f4298f5b596ae5e621fea8f9ef02dd09e6',
         'registry': 'ghcr.io',
         'complete_tag': 'ghcr.io/k8s-at-home/transmission@sha256:355f4036c53c782df1957de0e16c63f4298f5b596ae5e621fea8f9ef02dd09e6',
+        'reference_is_digest': True,
     })
 ])
 def test_normalize_reference(reference, expected_results):
