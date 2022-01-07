1b36d8ec90db121cc8b9c67cf535e3a1c1a07b08
themylogin <themylogin@gmail.com>
2022-01-07 21:15:17 +0100

    Move disk_bus_lunid migration to 13.0

--- src/middlewared/middlewared/alembic/versions/22.02/2022-01-07_16-55_merge.py
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-01-07_16-55_merge.py
@@ -1,7 +1,7 @@
 """Merge
 
 Revision ID: b140ab0d3066
-Revises: 9e5e27934a42, 7132a60093ce
+Revises: 674afdc2a00b, 7132a60093ce
 Create Date: 2022-01-05 16:55:42.969495+00:00
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'b140ab0d3066'
-down_revision = ('9e5e27934a42', '7132a60093ce')
+down_revision = ('674afdc2a00b', '7132a60093ce')
 branch_labels = None
 depends_on = None
 
