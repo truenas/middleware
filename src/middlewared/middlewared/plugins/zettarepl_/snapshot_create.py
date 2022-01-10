e047587e59338a8847f0c3a765b95100167446ba
themylogin <themylogin@gmail.com>
2022-01-10 13:54:50 +0100

    zettarepl.create_recursive_snapshot_with_exclude

---
+++ src/middlewared/middlewared/plugins/zettarepl_/snapshot_create.py
@@ -0,0 +1,10 @@
+from middlewared.service import Service
+
+from zettarepl.snapshot.create import create_snapshot
+from zettarepl.snapshot.snapshot import Snapshot
+from zettarepl.transport.local import LocalShell
+
+
+class ZettareplService(Service):
+    def create_recursive_snapshot_with_exclude(self, dataset, snapshot, exclude):
+        create_snapshot(LocalShell(), Snapshot(dataset, snapshot), True, exclude, {})
