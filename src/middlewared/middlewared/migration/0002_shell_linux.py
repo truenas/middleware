8c9908ed1e4cf1b62f391e813cdaaa61af11214b
themylogin <themylogin@gmail.com>
2022-01-13 00:21:44 +0100

    Ensure valid root shell when migrating from FreeBSD

--- src/middlewared/middlewared/migration/0002_shell_linux.py
+++ src/middlewared/middlewared/migration/0002_shell_linux.py
@@ -5,23 +5,37 @@ import os
 logger = logging.getLogger(__name__)
 
 
+def is_valid_shell(shell):
+    return os.path.exists(shell) and os.access(shell, os.X_OK)
+
+
 def migrate(middleware):
     updated = False
     for user in middleware.call_sync("datastore.query", "account.bsdusers", [], {"prefix": "bsdusr_"}):
-        if user["shell"].startswith("/usr/local/") and not os.path.exists(user["shell"]):
-            new_shell = user["shell"].replace("/usr/local/", "/usr/")
-            if os.path.exists(new_shell) and os.access(new_shell, os.X_OK):
-                logger.info("Updating user %r shell from %r to %r", user["username"], user["shell"], new_shell)
-                middleware.call_sync(
-                    "datastore.update",
-                    "account.bsdusers",
-                    user["id"],
-                    {
-                        "shell": new_shell,
-                    },
-                    {"prefix": "bsdusr_"},
-                )
-                updated = True
+        if not user["shell"]:
+            continue
+
+        if is_valid_shell(user["shell"]):
+            continue
+
+        new_shell = user["shell"].replace("/usr/local/", "/usr/")
+        if not is_valid_shell(new_shell):
+            if user["username"] == "root":
+                new_shell = "/usr/bin/zsh"
+            else:
+                continue
+
+        logger.info("Updating user %r shell from %r to %r", user["username"], user["shell"], new_shell)
+        middleware.call_sync(
+            "datastore.update",
+            "account.bsdusers",
+            user["id"],
+            {
+                "shell": new_shell,
+            },
+            {"prefix": "bsdusr_"},
+        )
+        updated = True
 
     if updated:
         middleware.call_sync("service.reload", "user")
