b031fef159275fd42af6d282efb44d8116912cbf
caleb <yocalebo@gmail.com>
2022-01-21 09:24:19 -0500

    address reviews

--- tests/api2/test_pool_dataset_track_processes.py
+++ tests/api2/test_pool_dataset_track_processes.py
@@ -1,25 +1,37 @@
 import sys
 import os
+import time
 sys.path.append(os.getcwd())
 
-from functions import POST, make_ws_request
-from auto_config import pool_name, ip
+from functions import make_ws_request
+from auto_config import ip
 from middlewared.test.integration.utils import ssh
+from middlewared.test.integration.assets.pool import dataset
 
-TEST_DATASET = f'{pool_name}/testing_processes'
-
-
-def test_01_create_dataset():
-    results = POST('/pool/dataset', {'name': TEST_DATASET})
-    assert results.status_code == 200, results.text
+TEST_DATASET = 'testing_processes'
 
 
-def test_02_open_path_and_check_proc():
-    path = f'/mnt/{TEST_DATASET}'
-    test_file = f'{path}/test_file'
-    open_pid = ssh(f'python -c "import time; f = open(\"{test_file}\", \"w+\"); time.sleep(10)" & echo $!')
-    assert open_pid.isdigit()
+def test__open_path_and_check_proc():
+    with dataset(TEST_DATASET) as ds:
+        opened = False
+        try:
+            path = f'/mnt/{ds}'
+            test_file = f'{path}/test_file'
+            cmdline = f'python -c "import time; f = open(\"{test_file}\", \"w+\"); time.sleep(10)" & echo $!'
+            open_pid = ssh(cmdline)
+            assert open_pid.isdigit(), open_pid
+            opened = True
 
-    payload = {'msg': 'method', 'method': 'pool.dataset.processes_using_paths', 'params': list(path)}
-    res = make_ws_request(ip, payload)
-    assert int(res[0]['pid']) == int(open_pid), res
+            # spinning up python interpreter could take some time on busy system so sleep
+            # for a couple seconds to give it time
+            time.sleep(2)
+
+            # have to use websocket since the method being called is private
+            payload = {'msg': 'method', 'method': 'pool.dataset.processes_using_paths', 'params': list(path)}
+            res = make_ws_request(ip, payload)
+            assert len(res) == 1, res
+            assert int(res[0]['pid']) == int(open_pid), res
+            assert res[0]['cmdline'] == cmdline, res
+        finally:
+            if opened:
+                ssh(f'kill -9 {open_pid}')
