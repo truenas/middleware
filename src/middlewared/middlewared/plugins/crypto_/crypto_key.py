2839636008c2d36921a42923caad4dd11fcb714a
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-01-24 21:40:29 +0500

    Move generating cert to separate util

--- src/middlewared/middlewared/plugins/crypto_/crypto_key.py
+++ src/middlewared/middlewared/plugins/crypto_/crypto_key.py
@@ -7,6 +7,7 @@ from middlewared.service import Service
 from middlewared.validators import Email
 
 from .extensions_utils import add_extensions
+from .generate_certs import generate_certificate
 from .generate_utils import generate_builder, normalize_san
 from .load_utils import load_certificate, load_certificate_request, load_private_key
 from .key_utils import generate_private_key, retrieve_signing_algorithm, export_private_key_object
@@ -78,43 +79,7 @@ class CryptoKeyService(Service):
         )
     )
     def generate_certificate(self, data):
-        key = generate_private_key({
-            'type': data.get('key_type') or 'RSA',
-            'curve': data.get('ec_curve') or EC_CURVE_DEFAULT,
-            'key_length': data.get('key_length') or 2048
-        })
-
-        if data.get('ca_privatekey'):
-            ca_key = load_private_key(data['ca_privatekey'])
-        else:
-            ca_key = None
-
-        san_list = normalize_san(data.get('san'))
-
-        builder_data = {
-            'crypto_subject_name': {
-                k: data.get(v) for k, v in CERT_BACKEND_MAPPINGS.items()
-            },
-            'san': san_list,
-            'serial': data.get('serial'),
-            'lifetime': data.get('lifetime')
-        }
-        if data.get('ca_certificate'):
-            ca_data = load_certificate(data['ca_certificate'])
-            builder_data['crypto_issuer_name'] = {
-                k: ca_data.get(v) for k, v in CERT_BACKEND_MAPPINGS.items()
-            }
-            issuer = x509.load_pem_x509_certificate(data['ca_certificate'].encode(), default_backend())
-        else:
-            issuer = None
-
-        cert = add_extensions(generate_builder(builder_data), data.get('cert_extensions'), key, issuer)
-
-        cert = cert.sign(
-            ca_key or key, retrieve_signing_algorithm(data, ca_key or key), default_backend()
-        )
-
-        return cert.public_bytes(serialization.Encoding.PEM).decode(), export_private_key_object(key)
+        return generate_certificate(data)
 
     @accepts(
         Ref('certificate_cert_info')
