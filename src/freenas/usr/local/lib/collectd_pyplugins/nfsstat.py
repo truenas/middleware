3cf551a77df7350c7197ae5aeaad644302c30aca
Andrew Walker <awalker@ixsystems.com>
2022-01-28 17:58:58 -0500

    Convert collectd plugin to read nfs stats

--- src/freenas/usr/local/lib/collectd_pyplugins/nfsstat.py
+++ src/freenas/usr/local/lib/collectd_pyplugins/nfsstat.py
@@ -1,28 +1,10 @@
 import traceback
 
 import collectd
-import dbus
 
 READ_INTERVAL = 10.0
 
 collectd.info('Loading "nfsstat" python plugin')
-
-bus = dbus.SystemBus()
-
-
-class Stats:
-    def __init__(self):
-        self.read_bytes = 0
-        self.read_ops = 0
-        self.write_bytes = 0
-        self.write_ops = 0
-
-    def add(self, stats):
-        if stats[1] == "OK":
-            self.read_bytes += stats[3][1]
-            self.read_ops += stats[3][2]
-            self.write_bytes += stats[4][1]
-            self.write_ops += stats[4][2]
 
 
 class NFSStat(object):
@@ -34,53 +16,34 @@ class NFSStat(object):
 
     def read(self):
         try:
-            proxy = bus.get_object("org.ganesha.nfsd", "/org/ganesha/nfsd/ExportMgr")
-            exportmgr = dbus.Interface(proxy, dbus_interface="org.ganesha.nfsd.exportmgr")
-            exportstats = dbus.Interface(proxy, dbus_interface="org.ganesha.nfsd.exportstats")
-        except Exception:
-            return
-
-        try:
             stats = Stats()
-            for export in exportmgr.ShowExports()[1]:
-                export_id, path, v3, v40, v41, v42 = export[:6]
+            read_bytes = 0
+            write_bytes = 0
+            read_ops = 0
+            write_ops = 0
+            with open("/proc/net/rpc/nfsd", "r") as f:
+                for line in f:
+                    if line.startswith('io'):
+                        parsed = line.split()
+                        read_bytes = parsed[1]
+                        write_bytes = parsed[2]
+                        continue
 
-                if v3:
-                    try:
-                        stats.add(exportstats.GetNFSv3IO(export_id))
-                    except Exception:
-                        if "v3" not in self.errors:
-                            collectd.error(traceback.format_exc())
-                            self.errors.add("v3")
+                    if line.startswith('proc3')
+                        parsed = line.split()
+                        read_ops = int(parsed[6])
+                        write_ops = int(parsed[7])
 
-                if v40:
-                    try:
-                        stats.add(exportstats.GetNFSv40IO(export_id))
-                    except Exception:
-                        if "v40" not in self.errors:
-                            collectd.error(traceback.format_exc())
-                            self.errors.add("v40")
+                    if line.startswith('proc4ops'):
+                        parsed = line.split()
+                        read_ops += int(parsed[27])
+                        write_ops += int(parsed[40])
+                        continue
 
-                if v41:
-                    try:
-                        stats.add(exportstats.GetNFSv41IO(export_id))
-                    except Exception:
-                        if "v41" not in self.errors:
-                            collectd.error(traceback.format_exc())
-                            self.errors.add("v41")
-
-                if v42:
-                    try:
-                        stats.add(exportstats.GetNFSv42IO(export_id))
-                    except Exception:
-                        if "v42" not in self.errors:
-                            collectd.error(traceback.format_exc())
-                            self.errors.add("v42")
-
-            self.dispatch_value('read', stats.read_ops)
-            self.dispatch_value('write', stats.write_ops)
-            self.dispatch_value('read_bytes', stats.read_bytes)
-            self.dispatch_value('write_bytes', stats.write_bytes)
+            self.dispatch_value('read', read_ops)
+            self.dispatch_value('write', write_ops)
+            self.dispatch_value('read_bytes', read_bytes)
+            self.dispatch_value('write_bytes', write_bytes)
         except Exception:
             collectd.error(traceback.format_exc())
 
