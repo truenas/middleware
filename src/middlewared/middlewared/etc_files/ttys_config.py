a1f36262cc0d7c3f151b45ddffc24b73acf3af93
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-02-06 23:40:45 +0500

    Remove freebsd specific bits from ttys config
    (cherry picked from commit 16c7bb90ed964fbce2d528175f269c7f819a60af)

--- src/middlewared/middlewared/etc_files/ttys_config.py
+++ src/middlewared/middlewared/etc_files/ttys_config.py
@@ -1,19 +1,11 @@
-import os
-import signal
-import subprocess
-
-from middlewared.utils import osc
+from middlewared.utils import run
 
 
-def render(service, middleware):
-    if osc.IS_LINUX:
-        for command in [
-            ['systemctl', 'restart', 'getty@tty1.service'],
-            ['systemctl', 'restart', 'serial-getty@*.service'],
-        ]:
-            proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = proc.communicate()
-            if proc.returncode:
-                middleware.logger.debug('Failed to execute "%s": %s', ' '.join(command), stderr.decode())
-    else:
-        os.kill(1, signal.SIGHUP)
+async def render(service, middleware):
+    for command in [
+        ['systemctl', 'restart', 'getty@tty1.service'],
+        ['systemctl', 'restart', 'serial-getty@*.service'],
+    ]:
+        cp = await run(command, check=False)
+        if cp.returncode:
+            middleware.logger.debug('Failed to execute "%s": %s', ' '.join(command), cp.stderr.decode())
