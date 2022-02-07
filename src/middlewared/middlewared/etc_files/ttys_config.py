4931aaaf84b6e0a49f2cd5707e9a43072c3a60d5
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-02-07 21:02:30 +0500

    Properly enable/disable/start/stop only set serial port
    (cherry picked from commit b0c3880ce1edebcb9d3f142e1bc9f067077b2431)

--- src/middlewared/middlewared/etc_files/ttys_config.py
+++ src/middlewared/middlewared/etc_files/ttys_config.py
@@ -2,10 +2,11 @@ from middlewared.utils import run
 
 
 async def render(service, middleware):
-    serial_action = 'restart' if (await middleware.call('system.advanced.config'))['serialconsole'] else 'stop'
+    config = await middleware.call('system.advanced.config')
+    serial_action = 'restart' if config['serialconsole'] else 'stop'
     for command in [
         ['systemctl', 'restart', 'getty@tty1.service'],
-        ['systemctl', serial_action, 'serial-getty@*.service'],
+        ['systemctl', serial_action, f'serial-getty@{config["serialport"]}.service'],
     ]:
         cp = await run(command, check=False)
         if cp.returncode:
