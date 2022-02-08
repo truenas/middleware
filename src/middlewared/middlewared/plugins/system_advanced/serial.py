37a39fc911ff3f6cb7b10e06bdfa73102ed7fb74
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-02-09 02:49:12 +0500

    Move serial configuration logic to seperate service part
    (cherry picked from commit 6ee26d9a7eefb734a4ca66882a0e509fc8829c1e)

--- src/middlewared/middlewared/plugins/system_advanced/serial.py
+++ src/middlewared/middlewared/plugins/system_advanced/serial.py
@@ -1,5 +1,6 @@
 from middlewared.schema import accepts, Dict, returns
-from middlewared.service import Service
+from middlewared.service import private, Service
+from middlewared.utils import run
 
 
 class SystemAdvancedService(Service):
@@ -22,4 +23,37 @@ class SystemAdvancedService(Service):
 
         return ports
 
+    @private
+    async def configure_tty(self, old, new, generate_grub=False):
+        restart_ttys = any(old[k] != new[k] for k in ('serialconsole', 'serialspeed', 'serialport'))
 
+        if old['serialconsole'] != new['serialconsole']:
+            if old['serialport'] == new['serialport']:
+                action = 'enable' if new['serialconsole'] else 'disable'
+                cp = await run(
+                    ['systemctl', action, f'serial-getty@{old["serialport"]}.service'], check=False
+                )
+                if cp.returncode:
+                    self.logger.error('Failed to %r serialconsole: %r', action, cp.stderr.decode())
+
+        if old['serialport'] != new['serialport']:
+            for command in [
+               ['systemctl', 'disable', f'serial-getty@{old["serialport"]}.service'],
+               ['systemctl', 'stop', f'serial-getty@{old["serialport"]}.service'],
+            ] + (
+                [['systemctl', 'enable', f'serial-getty@{new["serialport"]}.service']] if new['serialconsole'] else []
+            ):
+                cp = await run(command, check=False)
+                if cp.returncode:
+                    self.logger.error(
+                        'Failed to %r %r serialport service: %r', command[1], command[2], cp.stderr.decode()
+                    )
+
+        if restart_ttys or new['consolemenu'] != new['consolemenu']:
+            await self.middleware.call('service.start', 'ttys')
+
+        if new['consolemenu'] != new['consolemenu']:
+            await self.middleware.call('service.start', 'tty')
+
+        if generate_grub or restart_ttys:
+            await self.middleware.call('etc.generate', 'grub')
