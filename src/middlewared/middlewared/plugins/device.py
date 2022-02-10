0b90629cc39bdd60b5dbcfede840fbae728d7115
caleb <yocalebo@gmail.com>
2022-02-10 12:11:48 -0500

    move device_info_freebsd.py to device.py

--- src/middlewared/middlewared/plugins/device.py
+++ src/middlewared/middlewared/plugins/device.py
@@ -1,5 +1,7 @@
 from middlewared.schema import accepts, Str
-from middlewared.service import Service
+from middlewared.service import Service, private
+from middlewared.common.camcontrol import camcontrol_list
+from bsd.devinfo import DevInfo
 
 
 class DeviceService(Service):
@@ -10,3 +12,37 @@ class DeviceService(Service):
         Get info for SERIAL/DISK device types.
         """
         return await self.middleware.call(f'device.get_{_type.lower()}s')
+
+    @private
+    async def get_disks(self):
+        return {
+            k: v for k, v in (await self.middleware.call('geom.cache.get_disks')).items()
+            if not k.startswith('multipath/')
+        }
+
+    @private
+    async def get_disk(self, name):
+        return (await self.middleware.call('geom.cache.get_disks')).get(name)
+
+    @private
+    async def get_serials(self):
+        ports = []
+        for devices in DevInfo().resource_managers['I/O ports'].values():
+            for dev in devices:
+                if not dev.name.startswith('uart'):
+                    continue
+                port = self.serial_port_default.copy()
+                port.update({
+                    'name': dev.name,
+                    'description': dev.desc,
+                    'drivername': dev.drivername,
+                    'location': dev.location,
+                    'start': hex(dev.start),
+                    'size': dev.size
+                })
+                ports.append(port)
+        return ports
+
+    @private
+    async def get_storage_devices_topology(self):
+        return await camcontrol_list()
