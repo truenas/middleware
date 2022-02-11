05a607a8faa0fe2deebf7048864918d1d984e77e
Andrew Walker <awalker@ixsystems.com>
2022-02-11 15:09:17 -0500

    Increase default number of NFS servers from 4 to 16
    (cherry picked from commit aa6a9be70314b7d92ac5ef597509361910d8c380)

---
+++ src/middlewared/middlewared/alembic/versions/13.0/2022-02-11_19-54_increase_nfs_servers.py
@@ -0,0 +1,22 @@
+"""increase minimum number of NFS servers
+
+Revision ID: 99aef90c4cd6
+Revises: cd7569a7b973
+Create Date: 2022-02-11 19:54:32.149486+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99aef90c4cd6'
+down_revision = 'cd7569a7b973'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute("UPDATE services_nfs SET nfs_srv_servers = 16 WHERE nfs_srv_servers = 4")
+
