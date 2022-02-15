4d40bfa348c241872480a57641d8eefe5ad73d51
caleb <yocalebo@gmail.com>
2022-02-15 10:48:28 -0500

    remove copyright

--- src/middlewared/middlewared/alert/source/failover.py
+++ src/middlewared/middlewared/alert/source/failover.py
@@ -1,9 +1,3 @@
-# Copyright (c) 2020 iXsystems, Inc.
-# All rights reserved.
-# This file is a part of TrueNAS
-# and may not be copied and/or distributed
-# without the express permission of iXsystems.
-
 import errno
 import os
 try:
