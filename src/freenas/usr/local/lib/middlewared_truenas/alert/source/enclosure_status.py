6e6426f5ab8df7592316f0cb026c089db640f725
caleb <yocalebo@gmail.com>
2022-02-16 12:13:01 -0500

    fix enclosure_status alert

--- src/freenas/usr/local/lib/middlewared_truenas/alert/source/enclosure_status.py
+++ src/freenas/usr/local/lib/middlewared_truenas/alert/source/enclosure_status.py
@@ -5,7 +5,7 @@ class EnclosureUnhealthyAlertClass(AlertClass):
     category = AlertCategory.HARDWARE
     level = AlertLevel.CRITICAL
     title = "Enclosure Status Is Not Healthy"
-    text = "Enclosure %d (%s): %s is %s (%s)."
+    text = "Enclosure /dev/ses%d (%s): %s at slot %s (in hex %s) is reporting %s."
 
     products = ("ENTERPRISE",)
 
@@ -14,7 +14,7 @@ class EnclosureHealthyAlertClass(AlertClass):
     category = AlertCategory.HARDWARE
     level = AlertLevel.INFO
     title = "Enclosure Status Is Healthy"
-    text = "Enclosure %d (%s): is healthy."
+    text = "Enclosure /dev/ses%d (%s): is healthy."
 
     products = ("ENTERPRISE",)
 
@@ -23,50 +23,37 @@ class EnclosureStatusAlertSource(AlertSource):
     products = ("ENTERPRISE",)
     failover_related = True
     run_on_backup_node = False
+    bad = ('Critical', 'Noncritical', 'Unknown', 'Unrecoverable')
 
     async def check(self):
         alerts = []
 
-        for num, enc in enumerate(await self.middleware.call('enclosure.query')):
-            healthy = True
-            for ele in sum([e['elements'] for e in enc['elements']], []):
-                if ele['status'] in ['Critical', 'Noncritical', 'Unrecoverable']:
-                    pass
-                elif ele['status'] == 'Not Installed' and ele['name'] in ['Power Supply']:
-                    pass
-                else:
-                    continue
+        for enc in await self.middleware.call('enclosure.query'):
+            if enc.get('status') == 'OK':
+                # m/x/z series devices return an overall status for the enclosure.
+                # In the situations where it's OK, just move on.
+                alerts.append(Alert(EnclosureHealthyAlertClass, args=[enc['number'], enc['name']]))
+            else:
+                for element_type, element_values in enc['elements'].items():
+                    for slot, value in element_values.items():
+                        if value['status'] in self.bad:
+                            if enc['name'] == 'ECStream 3U16+4R-4X6G.3 d10c' and value['descriptor'] == '1.8V Sensor':
+                                # The 1.8V sensor is bugged on the echostream enclosure (Z-series).
+                                # The management chip loses it's mind and claims undervoltage, but
+                                # scoping this confirms the voltage is fine.
+                                # Ignore alerts from this element. Redmine # 10077
+                                continue
 
-                # Enclosure element is CRITICAL in single head, ignore this for now
-                # See #11918
-                if ele['name'] == 'Enclosure':
-                    continue
-
-                # The 1.8V sensor is bugged on the echostream enclosure.  The
-                # management chip loses it's mind and claims undervoltage, but
-                # scoping this confirms the voltage is fine.
-                # Ignore alerts from this element.
-                # #10077
-                if enc['name'] == 'ECStream 3U16+4R-4X6G.3 d10c':
-                    if ele['descriptor'] == '1.8V Sensor':
-                        continue
-
-                healthy = False
-                alerts.append(Alert(
-                    EnclosureUnhealthyAlertClass,
-                    args=[
-                        num,
-                        enc['name'],
-                        f"{ele['name']} {hex(ele['slot'])} {ele['descriptor']}",
-                        ele['status'],
-                        ele['value_raw']
-                    ],
-                ))
-
-            if healthy:
-                alerts.append(Alert(
-                    EnclosureHealthyAlertClass,
-                    args=[num, enc['name']],
-                ))
+                            # getting here means that we came across an element that isn't reporting
+                            # a status we expect AND the overall enclosure status isn't "OK"
+                            # (or isn't reported at all)
+                            alerts.append(Alert(EnclosureUnhealthyAlertClass, args=[
+                                enc['number'],
+                                enc['name'],
+                                value['descriptor'],
+                                slot,
+                                hex(slot),
+                                value['value']
+                            ]))
 
         return alerts
