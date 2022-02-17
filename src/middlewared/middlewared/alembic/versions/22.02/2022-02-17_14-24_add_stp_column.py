bc96109985c8a97369a2dcc971c2a93664ef6469
caleb <yocalebo@gmail.com>
2022-02-17 09:29:58 -0500

    add stp column migration

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-02-17_14-24_add_stp_column.py
@@ -0,0 +1,26 @@
+"""add stp column to bridge table
+
+Revision ID: 7a143979d99b
+Revises: cda35deeed4f
+Create Date: 2022-02-17 14:24:38.186590+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a143979d99b'
+down_revision = 'cda35deeed4f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('network_bridge', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('stp', sa.Boolean(), nullable=False))
+
+
+def downgrade():
+    with op.batch_alter_table('network_bridge', schema=None) as batch_op:
+        batch_op.drop_column('stp')
