9f5303441cf1018594723e9b07ff76e6fddca518
themylogin <themylogin@gmail.com>
2022-02-28 17:43:36 +0100

    Fix `pool.dataset.processes_using_paths` for zvols

--- tests/api2/test_pool_dataset_track_processes.py
+++ tests/api2/test_pool_dataset_track_processes.py
@@ -3,6 +3,8 @@ import os
 import time
 sys.path.append(os.getcwd())
 
+import pytest
+
 from functions import make_ws_request
 from auto_config import ip
 from middlewared.test.integration.utils import ssh
@@ -11,12 +13,17 @@ from middlewared.test.integration.assets.pool import dataset
 TEST_DATASET = 'testing_processes'
 
 
-def test__open_path_and_check_proc():
-    with dataset(TEST_DATASET) as ds:
+@pytest.mark.parametrize("data,file_open_path,arg_path", [
+    (None, lambda ds: f'/mnt/{ds}/test_file', lambda ds, ssh: f'/mnt/{ds}'),
+    ({'type': 'VOLUME', 'volsize': 1024 * 1024 * 100}, lambda ds: f'/dev/zvol/{ds}', lambda ds, ssh: f'/dev/zvol/{ds}'),
+    ({'type': 'VOLUME', 'volsize': 1024 * 1024 * 100}, lambda ds: f'/dev/zvol/{ds}',
+     lambda ds, ssh: ssh(f'readlink -f /dev/zvol/{ds}').strip()),
+])
+def test__open_path_and_check_proc(data, file_open_path, arg_path):
+    with dataset(TEST_DATASET, data) as ds:
         opened = False
         try:
-            path = f'/mnt/{ds}'
-            test_file = f'{path}/test_file'
+            test_file = file_open_path(ds)
             open_pid = ssh(f"""python -c 'import time; f = open("{test_file}", "w+"); time.sleep(10)' > /dev/null 2>&1 & echo $!""")
             open_pid = open_pid.strip()
             assert open_pid.isdigit(), f'{open_pid!r} is not a digit'
@@ -30,7 +37,7 @@ def test__open_path_and_check_proc():
             cmdline = f"""python -c import time; f = open(\"{test_file}\", \"w+\"); time.sleep(10)"""
 
             # have to use websocket since the method being called is private
-            payload = {'msg': 'method', 'method': 'pool.dataset.processes_using_paths', 'params': [[path]]}
+            payload = {'msg': 'method', 'method': 'pool.dataset.processes_using_paths', 'params': [[arg_path(ds, ssh)]]}
             res = make_ws_request(ip, payload)
             assert len(res['result']) == 1, f'Length of the result should only be 1 but is {len(res["result"])!r}'
 
