c108d2bc1442f009849768fcf5a788fc192f2f3a
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-03-03 10:53:57 +0500

    Add migration for adding vm uuid

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-03-03_10-01_vm_uuid.py
@@ -0,0 +1,31 @@
+"""
+Add VM uuid field
+
+Revision ID: 2ed09f3b17b7
+Revises: 7a143979d99b
+Create Date: 2022-03-03 10:01:38.186590+00:00
+
+"""
+import uuid
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '2ed09f3b17b7'
+down_revision = '7a143979d99b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    with op.batch_alter_table('vm_vm', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('uuid', sa.Boolean(), server_default='1', nullable=False))
+
+    for vm in map(dict, conn.execute("SELECT * FROM vm_vm").fetchall()):
+        conn.execute("UPDATE vm_vm SET uuid = ? WHERE id = ?", (str(uuid.uuid4()), vm['id']))
+
+
+def downgrade():
+    pass
