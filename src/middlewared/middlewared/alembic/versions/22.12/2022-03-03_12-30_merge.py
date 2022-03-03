8c044670f6f20a1a903d00743719a0d2c957a5e6
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-03-03 12:38:40 +0500

    Add merge migration

---
+++ src/middlewared/middlewared/alembic/versions/22.12/2022-03-03_12-30_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 77832a4aeca8
+Revises: a2ae33484fed, c09a16ca3399
+Create Date: 2020-10-23 14:36:28.587671+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '77832a4aeca8'
+down_revision = ('a2ae33484fed', '2ed09f3b17b7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
