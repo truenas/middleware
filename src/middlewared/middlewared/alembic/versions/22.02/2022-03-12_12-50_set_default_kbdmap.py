66f7d9f22d0377b38434323c2b3c52245a62a3a9
themylogin <themylogin@gmail.com>
2022-03-12 14:08:36 +0100

    Set default kbdmap
    (cherry picked from commit af5e703cbc53b35ad422fdc41153fa0f142d9307)

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-03-12_12-50_set_default_kbdmap.py
@@ -0,0 +1,24 @@
+"""Set default kbdmap
+
+Revision ID: 8c9ad60244de
+Revises: b3c5a5321aef
+Create Date: 2022-03-12 12:50:48.732801+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c9ad60244de'
+down_revision = 'b3c5a5321aef'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("UPDATE system_settings SET stg_kbdmap = 'us' WHERE stg_kbdmap = ''")
+
+
+def downgrade():
+    pass
