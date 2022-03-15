f2e5380c499f59811e26ee172c73bd1161141d1f
caleb <yocalebo@gmail.com>
2022-03-15 10:30:15 -0400

    fix regression in zfs.pool.get_disks

--- src/middlewared/middlewared/plugins/zfs_/disks.py
+++ src/middlewared/middlewared/plugins/zfs_/disks.py
@@ -11,23 +11,23 @@ class ZFSPoolService(Service):
         process_pool = True
 
     def get_disks(self, name):
-        disks = self.middleware.call_sync('zfs.pool.get_devices', name)
-        mapping = {}
-        for dev in filter(
-            lambda d: not d.sys_name.startswith('sr') and d.get('DEVTYPE') in ('disk', 'partition'),
-            pyudev.Context().list_devices(subsystem='block')
-        ):
-            if dev['DEVTYPE'] == 'disk':
-                mapping[dev.sys_name] = dev.sys_name
+        sys_devices = {}
+        for dev in pyudev.Context().list_devices(subsystem='block'):
+            if dev.sys_name.startswith('sr') or dev.properties['DEVTYPE'] not in ('disk', 'partition'):
+                continue
 
-            for link in (dev.get('DEVLINKS') or '').split():
-                mapping[link[len('/dev/'):]] = dev.sys_name
+            # this is "sda/sda1/sda2/sda3" etc
+            sys_devices[dev.sys_name] = dev.sys_name
 
-        pool_disks = []
-        for dev in disks:
-            # dev can be partition name ( sdb1/sda2 etc ) or raw uuid ( disk/by-partuuid/uuid )
-            if dev in mapping:
-                pool_disks.append(mapping[dev])
-            else:
-                self.logger.debug(f'Could not find disk for {dev}')
-        return pool_disks
+            # this is the various "disk/by-partuuid" or "disk/by-label" or "disk/by-id" etc
+            for link in (dev.properties['DEVLINKS'] or '').split():
+                sys_devices[link.removeprefix('/dev/')] = dev.sys_name
+
+        mapping = {name: set()}
+        for disk in self.middleware.call_sync('zfs.pool.get_devices', name):
+            try:
+                mapping[name].add(sys_devices[disk])
+            except KeyError:
+                continue
+
+        return list(mapping[name])
