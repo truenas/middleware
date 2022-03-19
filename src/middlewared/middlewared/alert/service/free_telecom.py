36040b390fff993a2342dd649e232c122e0e305c
Zorglube <630192+zorglube@users.noreply.github.com>
2022-03-19 03:59:12 +0100

    Fixed typo into FreeTelecomAlertService

--- src/middlewared/middlewared/alert/service/free_telecom.py
+++ src/middlewared/middlewared/alert/service/free_telecom.py
@@ -5,18 +5,18 @@ from middlewared.alert.base import ThreadedAlertService
 from middlewared.schema import Dict, Str
 from middlewared.utils.network import INTERNET_TIMEOUT
 
-class FreeTelecomAlerteService(ThreadedAlertService):
+class FreeTelecomAlertService(ThreadedAlertService):
     title = "FreeTelecom"
 
     schema = Dict(
         "FreeTelecom_attributes",
-        Str("url", requierd=True, empty=False, default="https://smsapi.free-mobile.fr/sendmsg"),
+        Str("url", requierd=False, empty=False, default="https://smsapi.free-mobile.fr/sendmsg"),       
         Str("user", requierd=True, empty=False),
         Str("pass", requierd=True, empty=False)
     )
 
     def send_sync(self, alerts, gone_alerts, new_alerts):
-        
+
         r = requests.post(
             url = self.attributes["url"],
             data = json.dumps({
@@ -27,5 +27,5 @@ class FreeTelecomAlerteService(ThreadedAlertService):
             headers={"Content-type": "application/json"},
             timeout = INTERNET_TIMEOUT
         )
-        
+
         r.raise_for_status()
