e1f9304d02b04fb8ba26af6caa701345cad7e881
caleb <yocalebo@gmail.com>
2022-03-21 15:36:40 -0400

    fix zpool_cachefile.py after QA

--- src/middlewared/middlewared/plugins/failover_/zpool_cachefile.py
+++ src/middlewared/middlewared/plugins/failover_/zpool_cachefile.py
@@ -18,15 +18,12 @@ class FailoverZpoolCacheFileService(Service):
         saved = Path(ZPOOL_CACHE_FILE_SAVED)
         default = Path(ZPOOL_CACHE_FILE)
         overwrite = Path(ZPOOL_CACHE_FILE_OVERWRITE)
-        saved_stat = default_stat = overwrite_stat = None
-        for i in [[saved, saved_stat], [default, default_stat], [overwrite, overwrite_stat]]:
-            try:
-                i[1] = i[0].stat()
-            except FileNotFoundError:
-                continue
+        se = saved.exists()
+        de = default.exists()
+        oe = overwrite.exists()
 
         if event == 'MASTER':
-            if (saved_stat and default_stat) and (saved_stat.mtime < default_stat.mtime):
+            if (se and de) or (se and not de):
                 # we're becoming master which means on backup
                 # event we modify the save cache file first and
                 # if the pool is successfully exported then the
@@ -35,18 +32,16 @@ class FailoverZpoolCacheFileService(Service):
                 # itself and not us. That behavior is counter
                 # intuitive to what we're trying to do so that's
                 # why we save the cachefile before we export
-                saved.rename(default.as_posix())
-            elif saved_stat and not default_stat:
-                saved.rename(default.as_posix())
-        elif event == 'BACKUP' and default_stat:
+                saved.rename(default)
+        elif event == 'BACKUP' and de:
             # means we're becoming backup so we need to save
             # the zpool cachefile before we export the zpools
             saved.write_bytes(default.read_bytes())
-        elif event == 'SYNC' and overwrite_stat:
+        elif event == 'SYNC' and oe:
             # a zpool was created/updated on the active controller
             # and the newly created zpool cachefile was sent to this
             # controller so we need to overwrite
-            overwrite.rename(default.as_posix())
+            overwrite.rename(default)
 
         default.touch(exist_ok=True)
         if not event == 'BACKUP':
