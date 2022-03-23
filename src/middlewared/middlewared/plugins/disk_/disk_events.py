08f59e5936f2dbeb4d04b8263bcfa5c25f08fd55
caleb <yocalebo@gmail.com>
2022-03-23 09:56:01 -0400

    fix a few issues found during QA

--- src/middlewared/middlewared/plugins/disk_/disk_events.py
+++ src/middlewared/middlewared/plugins/disk_/disk_events.py
@@ -2,16 +2,20 @@ import asyncio
 
 DISK = ('da', 'ada', 'vtbd', 'mfid', 'nvd', 'pmem')
 SHELF = ('ses',)
+TYPES = ('CREATE', 'DESTROY')
 PREVIOUS = {'method': '', 'task': None}
 MAX_WAIT_TIME = 60
-SETTLE_TIME = 2
+SETTLE_TIME = 5
 LAST_EVENT_TIME = None
 
 
 async def reset_cache(middleware, *args):
     await middleware.call('geom.cache.invalidate')
     await (await middleware.call('disk.sync_all')).wait()
-    await middleware.call('sed_unlock_all')
+    await middleware.call('disk.sed_unlock_all')
+    global PREVIOUS
+    PREVIOUS['method'] = ''
+    PREVIOUS['task'] = None
 
 
 async def added_disk(middleware, disk_name):
@@ -20,6 +24,9 @@ async def added_disk(middleware, disk_name):
     await middleware.call('disk.sed_unlock', disk_name)
     await middleware.call('disk.multipath_sync')
     await middleware.call('alert.oneshot_delete', 'SMART', disk_name)
+    global PREVIOUS
+    PREVIOUS['method'] = ''
+    PREVIOUS['task'] = None
 
 
 async def remove_disk(middleware, disk_name):
@@ -30,10 +37,13 @@ async def remove_disk(middleware, disk_name):
     # If a disk dies we need to reconfigure swaps so we are not left
     # with a single disk mirror swap, which may be a point of failure.
     asyncio.ensure_future(middleware.call('disk.swaps_configure'))
+    global PREVIOUS
+    PREVIOUS['method'] = ''
+    PREVIOUS['task'] = None
 
 
 async def devd_devfs_hook(middleware, data):
-    if data.get('subsystem') != 'CDEV' or data['type'] not in ('CREATE', 'DESTROY'):
+    if data.get('subsystem') != 'CDEV' or data['type'] not in TYPES:
         return
     elif not data['cdev'].startswith(DISK + SHELF):
         return
