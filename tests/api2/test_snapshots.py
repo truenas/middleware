da58bc651c2f8c88875fe334afc3939d07e11558
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-03-24 20:13:15 +0500

    Add tests for testing mix/max txg together when querying snapshots

--- tests/api2/test_snapshots.py
+++ tests/api2/test_snapshots.py
@@ -13,10 +13,12 @@ reason = 'Skip for testing'
 pytestmark = pytest.mark.skipif(dev_test, reason=reason)
 
 
-def common_min_max_txg_snapshot_test(min_txg_test):
+def common_min_max_txg_snapshot_test(test_min_txg=False, test_max_txg=False):
+    assert all(i is False for i in (test_min_txg, test_max_txg)) is False
+
     with dataset('test') as test_dataset:
         created_snaps = []
-        total_snaps = 10
+        total_snaps = 20
         for i in range(total_snaps):
             created_snaps.append(int(call(
                 'zfs.snapshot.create', {'dataset': test_dataset, 'name': f'snap_{i}'}
@@ -24,17 +26,28 @@ def common_min_max_txg_snapshot_test(min_txg_test):
 
         assert call('zfs.snapshot.query', [['dataset', '=', test_dataset]], {'count': True}) == len(created_snaps)
 
-        for i in (2, 3, 4):
-            split = int(total_snaps / i)
-            new_list = created_snaps[split:] if min_txg_test else created_snaps[:split]
-            assert call('zfs.snapshot.query', [['dataset', '=', test_dataset]], {'count': True, 'extra': {
-                'min_txg' if min_txg_test else 'max_txg': new_list[0 if min_txg_test else -1],
-            }}) == len(new_list)
+        for i in range(int(total_snaps / 2) - 1):
+            new_list = created_snaps
+            extra_args = {}
+            if test_min_txg:
+                new_list = created_snaps[i:]
+                extra_args['min_txg'] = new_list[0]
+            if test_max_txg:
+                new_list = new_list[:int(len(new_list) / 2)]
+                extra_args['max_txg'] = new_list[-1]
+
+            assert call(
+                'zfs.snapshot.query', [['dataset', '=', test_dataset]], {'count': True, 'extra': extra_args}
+            ) == len(new_list)
 
 
 def test_min_txg_snapshot_query():
-    common_min_max_txg_snapshot_test(True)
+    common_min_max_txg_snapshot_test(True, False)
 
 
 def test_max_txg_snapshot_query():
-    common_min_max_txg_snapshot_test(False)
+    common_min_max_txg_snapshot_test(False, True)
+
+
+def test_min_max_txg_snapshot_query():
+    common_min_max_txg_snapshot_test(True, True)
