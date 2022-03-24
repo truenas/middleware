0246b934f1fa75ebbdcbc7e60c05affd36bc523d
pkgdemon <pkgdemonteam@gmail.com>
2022-03-24 13:26:17 -0400

    Revert "Fix import time so sleep can work"
    This reverts commit ec9ca6f69cf8341539fbcb59402eb7997719239e.

--- cluster-tests/init_cluster.py
+++ cluster-tests/init_cluster.py
@@ -1,6 +1,5 @@
 import sys
-import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed import time
 
 from utils import make_request, make_ws_request, wait_on_job
 from config import CLUSTER_INFO, DATASET_HIERARCHY
