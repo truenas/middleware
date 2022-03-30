93d89ec958b2cb57969adc804a22785908512f6f
caleb <yocalebo@gmail.com>
2022-03-30 16:27:11 -0400

    fix all the things after another round of QA

--- src/middlewared/middlewared/plugins/disk_/disk_events.py
+++ src/middlewared/middlewared/plugins/disk_/disk_events.py
@@ -1,12 +1,13 @@
 import asyncio
+import collections
 import re
 
 
 DISKS = ('da', 'ada', 'vtbd', 'mfid', 'nvd', 'pmem')
 SHELF = ('ses',)
 TYPES = ('CREATE', 'DESTROY')
-PREVIOUS = {'method': '', 'task': None}
-MAX_WAIT_TIME = 60
+PREV_TASK = collections.deque(maxlen=1)
+MAX_WAIT_TIME = 15
 SETTLE_TIME = 5
 HAS_PARTITION = re.compile(rf'^({"|".join(DISKS)})[0-9]+p[0-9]+.*$')
 
@@ -15,11 +16,7 @@ async def reset_cache(middleware, *args):
     await middleware.call('geom.cache.invalidate')
     await (await middleware.call('disk.sync_all')).wait()
     await middleware.call('disk.sed_unlock_all')
-
-    global PREVIOUS
-    PREVIOUS['method'] = 'reset_cache'
-    PREVIOUS['task'] = None
-    PREVIOUS['task_time'] = asyncio.get_event_loop().time()
+    PREV_TASK.clear()
 
 
 async def added_disk(middleware, disk_name):
@@ -28,11 +25,7 @@ async def added_disk(middleware, disk_name):
     await middleware.call('disk.sed_unlock', disk_name)
     await middleware.call('disk.multipath_sync')
     await middleware.call('alert.oneshot_delete', 'SMART', disk_name)
-
-    global PREVIOUS
-    PREVIOUS['method'] = 'added_disk'
-    PREVIOUS['task'] = None
-    PREVIOUS['task_time'] = asyncio.get_event_loop().time()
+    PREV_TASK.clear()
 
 
 async def remove_disk(middleware, disk_name):
@@ -43,11 +36,7 @@ async def remove_disk(middleware, disk_name):
     # If a disk dies we need to reconfigure swaps so we are not left
     # with a single disk mirror swap, which may be a point of failure.
     asyncio.ensure_future(middleware.call('disk.swaps_configure'))
-
-    global PREVIOUS
-    PREVIOUS['method'] = 'remove_disk'
-    PREVIOUS['task'] = None
-    PREVIOUS['task_time'] = asyncio.get_event_loop().time()
+    PREV_TASK.clear()
 
 
 async def devd_devfs_hook(middleware, data):
@@ -56,22 +45,25 @@ async def devd_devfs_hook(middleware, data):
     elif not data['cdev'].startswith(DISKS + SHELF):
         return
     elif data['type'] == 'CREATE' and data['cdev'].startswith(DISKS) and HAS_PARTITION.match(data['cdev']):
-        # Means we received an event for something like "da1p1" which means
-        # we have (or will) receive an event for the raw disk (i.e. "da1")
-        # so we ignore this event
+        # Means we received an event for a disk with a partition already on it (i.e. da1p1).
+        # This means we have (or will) receive an event for the raw disk (i.e. "da1")
+        # so we ignore this event.
         return
 
-    global PREVIOUS
-    if not PREVIOUS['task']:
+    now = asyncio.get_event_loop().time()
+    task = asyncio.get_event_loop().call_later
+
+    if not PREV_TASK:
         if data['cdev'].startswith(DISKS):
             method = added_disk if data['type'] == 'CREATE' else remove_disk
         else:
             method = reset_cache
 
-        PREVIOUS['task'] = asyncio.get_event_loop().call_later(
-            SETTLE_TIME, lambda: asyncio.ensure_future(method(middleware, data['cdev']))
-        )
-    elif PREVIOUS['method'] != 'reset_cache':
+        PREV_TASK.append({
+            'task': task(SETTLE_TIME, lambda: asyncio.ensure_future(method(middleware, data['cdev']))),
+            'method': method.__qualname__,
+        })
+    elif PREV_TASK[-1]['method'] != 'reset_cache':
         # we have a previously scheduled task and the event we received came
         # in within SETTLE_TIME AND the previous task method to be run was
         # not "reset_cache" so we assume that we're receiving a "burst" of
@@ -79,20 +71,30 @@ async def devd_devfs_hook(middleware, data):
         # a failure event that causes an undefined pattern of behavior. In
         # either of these scenarios, we need to reset the entirety of the
         # disk cache to play it safe.
-        PREVIOUS['task'].cancel()
-        PREVIOUS['task'] = asyncio.get_event_loop().call_later(
-            SETTLE_TIME, lambda: asyncio.ensure_future(reset_cache(middleware))
+        PREV_TASK[-1]['task'].cancel()
+        PREV_TASK.append({
+            'task': task(SETTLE_TIME, lambda: asyncio.ensure_future(reset_cache(middleware, data['cdev']))),
+            'method': 'reset_cache',
+        })
+    elif 'backoff' not in PREV_TASK[-1]:
+        # This means we're continuing to receive a stream of events but we've
+        # already created a task to wipe the entirety of the cache so we need
+        # to cancel the currently pending task and create a new one to run after
+        # MAX_WAIT_TIME to give the system "time to settle down". This shouldn't
+        # occur but the logic needs to be here to cover edge-case scenarios.
+        PREV_TASK[-1]['task'].cancel()
+        PREV_TASK.append({
+            'task': task(MAX_WAIT_TIME, lambda: asyncio.ensure_future(reset_cache(middleware, data['cdev']))),
+            'method': 'reset_cache',
+            'backoff': True,
+        })
+    elif (PREV_TASK[-1]['task'].when() - now) <= 0:
+        # We have continually received a stream of events for at least
+        # MAX_WAIT_TIME which means something is misbehaving badly.
+        # This is an edge-case so at least log a warning.
+        middleware.logger.warning(
+            f'Continually received disk events for {MAX_WAIT_TIME} seconds. Disk cache was reset during this time.'
         )
-    elif (asyncio.get_event_loop().time() - PREVIOUS['task_time']) >= MAX_WAIT_TIME:
-        # we have continually received a stream of events for at least
-        # MAX_WAIT_TIME which means something is misbehaving badly. Log
-        # a warning and run the method directly
-        PREVIOUS['task'].cancel()
-
-        err = f'Waited at least {MAX_WAIT_TIME}seconds but have continually received devd events.'
-        err += ' Resetting disk cache.'
-        middleware.logger.warning(err)
-        await reset_cache(middleware)
 
 
 def setup(middleware):
