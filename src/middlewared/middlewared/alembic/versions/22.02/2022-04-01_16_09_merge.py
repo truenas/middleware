bc1f44c398333afbc03e27cdcac5c7f83189057c
caleb <yocalebo@gmail.com>
2022-04-01 12:16:21 -0400

    add merge migration

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-04-01_16_09_merge.py
@@ -0,0 +1,21 @@
+"""merge
+
+Revision ID: 19eb67dcdee2
+Revises: 6980b63de512, 88bfe11b5be5
+Create Date: 2022-04-01 16:09:55.446715+00:00
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '19eb67dcdee2'
+down_revision = ('6980b63de512', '88bfe11b5be5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
