6e33a58982e43725d8b70286c73c215604edc5bf
caleb <yocalebo@gmail.com>
2022-04-04 10:46:49 -0400

    remove unreachable code

--- src/middlewared/middlewared/etc_files/sysctl_config.py
+++ src/middlewared/middlewared/etc_files/sysctl_config.py
@@ -4,24 +4,10 @@ from middlewared.utils import run
 async def sysctl_configuration(middleware):
     sysdefs = await middleware.call('tunable.get_system_defaults')
     for tun in filter(lambda x: x in sysdefs, (await middleware.call('tunable.query', [['type', '=', 'SYSCTL']]))):
-        value_default = sysdefs[tun['var']]
-        if tun['enabled']:
-            if not value_default:
-                cp = await run(['sysctl', tun['var']], check=False, encoding='utf8')
-                if cp.returncode:
-                    middleware.logger.error(
-                        'Failed to get default value of %r : %s', tun['var'], cp.stderr.strip()
-                    )
-                else:
-                    value_default = sysdefs[tun['var']] = cp.stdout.split('=')[-1].strip()
-                    await middleware.call('tunable.set_default_value', tun['var'], value_default)
+        if tun['enabled'] and (sysdefs[tun['var']] != tun['value']):
+            # tunable is enabled and the variable's value set by the end-user doesn't match what the system's
+            # default value is so change it
             cp = await run(['sysctl', f'{tun["var"]}={tun["value"]}'], check=False, encoding='utf8')
-            if cp.returncode:
-                middleware.logger.error(
-                    'Failed to set sysctl %r -> %r : %s', tun['var'], tun['value'], cp.stderr.strip()
-                )
-        elif value_default:
-            cp = await run(['sysctl', f'{tun["var"]}={value_default}'], check=False, encoding='utf8')
             if cp.returncode:
                 middleware.logger.error(
                     'Failed to set sysctl %r -> %r : %s', tun['var'], tun['value'], cp.stderr.strip()
