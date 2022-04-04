fbe773b56f65720e2ed112b2ecb8ab9753e3baeb
ericbsd <ericturgeon.bsd@gmail.com>
2022-04-04 16:31:04 -0300

    Fixed test_pool_export.py with disabling failover
    On HA this test will fail with the error below if failover is enable:
    [ENOTSUP] Disable failover before exporting last pool on system.

--- tests/api2/test_pool_export.py
+++ tests/api2/test_pool_export.py
@@ -9,13 +9,23 @@ import os
 import sys
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from auto_config import pool_name, dev_test
+from functions import PUT
+from auto_config import pool_name, ha, dev_test
 # comment pytestmark for development testing with --dev-test
 pytestmark = pytest.mark.skipif(dev_test, reason='Skip for testing')
 
 
 def test_systemdataset_migrate_error(request):
     depends(request, ["pool_04"], scope="session")
+    """
+    On HA this test will fail with the error below if failover is enable:
+    [ENOTSUP] Disable failover before exporting last pool on system.
+    """
+
+    # Disable Failover
+    if ha is True:
+        results = PUT('/failover/', {"disabled": True, "master": True})
+        assert results.status_code == 200, results.text
 
     pool = call("pool.query", [["name", "=", pool_name]], {"get": True})
 
@@ -32,3 +42,8 @@ def test_systemdataset_migrate_error(request):
         assert e.value.error == (
             "[EFAULT] This pool contains system dataset, but its reconfiguration failed: [EFAULT] Test error"
         )
+
+    # Enable back Failover.
+    if ha is True:
+        results = PUT('/failover/', {"disabled": False, "master": True})
+        assert results.status_code == 200, results.text
