6f3876800c59a942a2d6856d5855e1ed00aa866a
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-04-14 16:48:50 +0500

    Add certificate profiles for https certs
    This commit takes changes from https://github.com/truenas/middleware/pull/8317 addressing outdated reviews.

--- src/middlewared/middlewared/plugins/crypto_/cert_profiles.py
+++ src/middlewared/middlewared/plugins/crypto_/cert_profiles.py
@@ -7,6 +7,85 @@ from .utils import DEFAULT_LIFETIME_DAYS
 
 
 CERTIFICATE_PROFILES = {
+    # Options / EKUs reference rfc5246
+    'HTTPS RSA Certificate': {
+        'cert_extensions': {
+            'BasicConstraints': {
+                'enabled': True,
+                'ca': False,
+                'extension_critical': True
+            },
+            'AuthorityKeyIdentifier': {
+                'enabled': True,
+                'authority_cert_issuer': True,
+                'extension_critical': False
+            },
+            # These days, most TLS certs want "ClientAuth".
+            # LetsEncrypt appears to want this extension to issue.
+            # https://community.letsencrypt.org/t/extendedkeyusage-tls-client-
+            # authentication-in-tls-server-certificates/59140/7
+            'ExtendedKeyUsage': {
+                'enabled': True,
+                'extension_critical': True,
+                'usages': [
+                    'SERVER_AUTH',
+                    'CLIENT_AUTH',
+                ]
+            },
+            # RSA certs need "digitalSignature" for DHE,
+            # and "keyEncipherment" for nonDHE
+            # Include "keyAgreement" for compatibility (DH_DSS / DH_RSA)
+            # See rfc5246
+            'KeyUsage': {
+                'enabled': True,
+                'extension_critical': True,
+                'digital_signature': True,
+                'key_encipherment': True,
+                'key_agreement': True,
+            }
+        },
+        'key_length': 2048,
+        'key_type': 'RSA',
+        'lifetime': DEFAULT_LIFETIME_DAYS,
+        'digest_algorithm': 'SHA256'
+    },
+    'HTTPS ECC Certificate': {
+        'cert_extensions': {
+            'BasicConstraints': {
+                'enabled': True,
+                'ca': False,
+                'extension_critical': True
+            },
+            'AuthorityKeyIdentifier': {
+                'enabled': True,
+                'authority_cert_issuer': True,
+                'extension_critical': False
+            },
+            # These days, most TLS certs want "ClientAuth".
+            # LetsEncrypt appears to want this extension to issue.
+            # https://community.letsencrypt.org/t/extendedkeyusage-tls-client-
+            # authentication-in-tls-server-certificates/59140/7
+            'ExtendedKeyUsage': {
+                'enabled': True,
+                'extension_critical': True,
+                'usages': [
+                    'SERVER_AUTH',
+                    'CLIENT_AUTH',
+                ]
+            },
+            # keyAgreement is not generally required for EC certs.
+            # See Google, cloudflare certs
+            'KeyUsage': {
+                'enabled': True,
+                'extension_critical': True,
+                'digital_signature': True,
+            }
+        },
+        'ec_curve': 'SECP384R1',
+        'key_type': 'EC',
+        'lifetime': DEFAULT_LIFETIME_DAYS,
+        'digest_algorithm': 'SHA256'
+    },
     'Openvpn Server Certificate': {
         'cert_extensions': {
             'BasicConstraints': {
