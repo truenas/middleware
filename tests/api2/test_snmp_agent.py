37e3abc76bc6a49b3ddf3743df442a000db3c234
themylogin <themylogin@gmail.com>
2022-04-18 22:00:54 +0200

    Retrieve SNMP MIB to test runner
    (cherry picked from commit 80b4cce4f13c25ec5efb1decbff83046ae887ec6)

--- tests/api2/test_snmp_agent.py
+++ tests/api2/test_snmp_agent.py
@@ -1,6 +1,8 @@
+import base64
 import os
 import re
 import subprocess
+import tempfile
 
 import pytest
 
@@ -14,14 +16,20 @@ def snmpd_running():
 
 
 def test_freenas_mib(snmpd_running):
-    snmp = subprocess.run(
-        f"snmpwalk -v2c -c public -m /usr/local/share/snmp/mibs/FREENAS-MIB.txt {os.environ['MIDDLEWARE_TEST_IP']} "
-        "1.3.6.1.4.1.50536",
-        shell=True,
-        check=True,
-        capture_output=True,
-        text=True,
-    )
+    with tempfile.NamedTemporaryFile(suffix=".txt") as f:
+        f.write(base64.b64decode(
+            call("filesystem.file_get_contents", "/usr/local/share/snmp/mibs/FREENAS-MIB.txt").encode("ascii")
+        ))
+        f.flush()
+
+        snmp = subprocess.run(
+            f"snmpwalk -v2c -c public -m {f.name} {os.environ['MIDDLEWARE_TEST_IP']} "
+            "1.3.6.1.4.1.50536",
+            shell=True,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
 
     assert "FREENAS-MIB::zpoolDescr.1 = STRING: boot-pool\n" in snmp.stdout
     assert "FREENAS-MIB::datasetDescr.1 = STRING: boot-pool/.system\n" in snmp.stdout
