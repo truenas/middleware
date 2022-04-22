2a70bbcc58084a770db66489276d97e31b6f2879
themylogin <themylogin@gmail.com>
2022-04-22 12:12:02 +0200

    Fix `reporting.iostat` tests

--- src/middlewared/middlewared/pytest/unit/plugins/reporting/test_iostat.py
+++ src/middlewared/middlewared/pytest/unit/plugins/reporting/test_iostat.py
@@ -1,6 +1,6 @@
 import pytest
 
-from middlewared.plugins.reporting.iostat import get_device_disk
+from middlewared.plugins.reporting.iostat import DiskStats
 
 
 @pytest.mark.parametrize("device,disk", [
@@ -10,12 +10,12 @@ from middlewared.plugins.reporting.iostat import get_device_disk
     ("sda11", "sda"),
     ("sdab1", "sdab"),
     ("sdab11", "sdab"),
-    ("nvme0c0n1", "nvme0n1"),
-    ("nvme0c11n1", "nvme0n1"),
-    ("nvme0n1p1", "nvme0n1"),
-    ("nvme0n1p11", "nvme0n1"),
+    ("nvme0c0n1", None),
+    ("nvme0c11n1", None),
+    ("nvme0n1p1", None),
+    ("nvme0n1p11", None),
     ("nvme0n1", "nvme0n1"),
     ("nvme0n11", "nvme0n11"),
 ])
-def test__get_device_disk(device, disk):
-    assert get_device_disk(device) == disk
+def test__get_disk(device, disk):
+    assert DiskStats(None, None).get_disk(device) == disk
