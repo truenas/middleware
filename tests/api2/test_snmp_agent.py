2ec781b107edd00d94e2f75c81f2b173f22b9c63
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-04-26 17:26:09 +0200

    NAS-115176 / 22.02.2 / snmp-agent integration test (by themylogin) (#8776)
    * snmp-agent integration test
    
    (cherry picked from commit 28194344fdf1e988fa1c3f2d971006c7401ea221)
    
    * Retrieve SNMP MIB to test runner
    
    (cherry picked from commit 80b4cce4f13c25ec5efb1decbff83046ae887ec6)
    
    * Fix snmp-agent test
    
    (cherry picked from commit c669536fe4459a3ffff0b548bfc943824cf0c57b)
    
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ tests/api2/test_snmp_agent.py
@@ -0,0 +1,36 @@
+import base64
+import os
+import re
+import subprocess
+import tempfile
+
+import pytest
+
+from middlewared.test.integration.utils import call
+
+
+@pytest.fixture()
+def snmpd_running():
+    call("service.start", "snmp")
+    yield
+
+
+def test_freenas_mib(snmpd_running):
+    with tempfile.NamedTemporaryFile(suffix=".txt") as f:
+        f.write(base64.b64decode(
+            call("filesystem.file_get_contents", "/usr/local/share/snmp/mibs/FREENAS-MIB.txt").encode("ascii")
+        ))
+        f.flush()
+
+        snmp = subprocess.run(
+            f"snmpwalk -v2c -c public -m {f.name} {os.environ['MIDDLEWARE_TEST_IP']} "
+            "1.3.6.1.4.1.50536",
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
+        assert snmp.returncode == 0, snmp.stderr
+
+    assert "FREENAS-MIB::zpoolDescr.1 = STRING: boot-pool\n" in snmp.stdout
+    assert re.search(r"^FREENAS-MIB::datasetDescr\.1 = STRING: boot-pool/.+\n", snmp.stdout, re.MULTILINE), snmp.stdout
+    assert re.search(r"^FREENAS-MIB::zfsArcSize\.0 = Gauge32: ([1-9][0-9]+)\n", snmp.stdout, re.MULTILINE), snmp.stdout
