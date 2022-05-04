258d888a550f5adebfea44fa47b5c3ac9a7e5ca0
caleb <yocalebo@gmail.com>
2022-05-04 12:57:35 -0400

    fix WS-Discovery service not being detected as running

--- src/middlewared/middlewared/plugins/service_/services/wsd.py
+++ src/middlewared/middlewared/plugins/service_/services/wsd.py
@@ -1,11 +1,13 @@
-from .base import SimpleService
+from .base import SimpleService, ServiceState
 
 
 class WSDService(SimpleService):
     name = "wsdd"
-
     etc = ["wsd"]
-
     freebsd_rc = "wsdd"
 
-    systemd_unit = "wsdd"
+    async def _get_state_freebsd(self):
+        stdout = (await self._freebsd_service("wsdd", "status")).stdout
+        running = 'wsdd running as pid' in stdout
+        pid = [] if not running else stdout.split('wsdd running as pid')[1].strip()
+        return ServiceState(running, pid)
