2da1c9db90c730d939af87f5e8fb2de6bf389155
caleb <yocalebo@gmail.com>
2022-05-04 13:46:04 -0400

    better fix for wsdd service status

--- src/middlewared/middlewared/plugins/service_/services/wsd.py
+++ src/middlewared/middlewared/plugins/service_/services/wsd.py
@@ -1,13 +1,8 @@
-from .base import SimpleService, ServiceState
+from .base import SimpleService
 
 
 class WSDService(SimpleService):
     name = "wsdd"
     etc = ["wsd"]
     freebsd_rc = "wsdd"
-
-    async def _get_state_freebsd(self):
-        stdout = (await self._freebsd_service("wsdd", "status")).stdout
-        running = 'wsdd running as pid' in stdout
-        pid = [] if not running else stdout.split('wsdd running as pid')[1].strip()
-        return ServiceState(running, pid)
+    freebsd_proc_arguments_match = True
