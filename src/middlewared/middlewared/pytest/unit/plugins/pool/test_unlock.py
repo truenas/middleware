5e9c52f6f554232c2ba03255b393ff82833676e9
themylogin <themylogin@gmail.com>
2022-05-04 17:41:19 +0200

    Allow to unlock all the children of a dataset with the same key or passphrase

---
+++ src/middlewared/middlewared/pytest/unit/plugins/pool/test_unlock.py
@@ -0,0 +1,26 @@
+from unittest.mock import Mock
+
+import pytest
+
+from middlewared.plugins.pool import PoolDatasetService
+
+
+@pytest.mark.parametrize("request_datasets,keys_supplied,queried_datasets,result", [
+    (
+        [{"name": "tank/test", "recursive": True}, {"name": "tank/test/child", "recursive": True},
+         {"name": "tank/test/child/grandchild", "recursive": False}],
+        {"tank/test": "test-key", "tank/test/child": "child-key", "tank/test/child/grandchild": "grandchild-key"},
+        ["tank/test", "tank/test/another-child", "tank/test/child", "tank/test/child/grandchild",
+         "tank/test/child/grandchild/grandgrandchild"],
+        {
+            "tank/test": "test-key",
+            "tank/test/another-child": "test-key",
+            "tank/test/child": "child-key",
+            "tank/test/child/grandchild": "grandchild-key",
+            "tank/test/child/grandchild/grandgrandchild": "child-key",
+        }
+    )
+])
+def test_assign_supplied_recursive_keys(request_datasets, keys_supplied, queried_datasets, result):
+    PoolDatasetService(Mock())._assign_supplied_recursive_keys(request_datasets, keys_supplied, queried_datasets)
+    assert keys_supplied == result
