d43ea21d6db32af8d2f741180dc1734e08ec5c65
caleb <yocalebo@gmail.com>
2022-05-04 10:44:30 -0400

    catch up DNSService to SCALE

--- src/middlewared/middlewared/plugins/network.py
+++ src/middlewared/middlewared/plugins/network.py
@@ -1,6 +1,6 @@
 from middlewared.service import (CallError, ConfigService, CRUDService, Service,
                                  filterable, pass_app, private)
-from middlewared.utils import Popen, filter_list, run
+from middlewared.utils import filter_list, run
 from middlewared.schema import (Bool, Dict, Int, IPAddr, List, Patch, Ref, Str,
                                 ValidationErrors, accepts)
 import middlewared.sqlalchemy as sa
@@ -2363,26 +2363,35 @@ class StaticRouteService(CRUDService):
 class DNSService(Service):
 
     @filterable
-    async def query(self, filters, options):
+    def query(self, filters, options):
         """
         Query Name Servers with `query-filters` and `query-options`.
         """
-        data = []
-        resolvconf = (await run('resolvconf', '-l')).stdout.decode()
-        for nameserver in RE_NAMESERVER.findall(resolvconf):
-            data.append({'nameserver': nameserver})
-        return filter_list(data, filters, options)
+        ips = set()
+        with contextlib.suppress(Exception):
+            with open('/etc/resolv.conf') as f:
+                for line in f:
+                    if line.startswith('nameserver'):
+                        ip = line[len('nameserver'):].strip()
+                        try:
+                            IPAddr().validate(ip)  # make sure it's a valid IP (better safe than sorry)
+                            ips.add(ip)
+                        except ValidationErrors:
+                            self.logger.warning('IP %r in resolv.conf does not seem to be valid', ip)
+                            continue
+
+        return filter_list([{'nameserver': i} for i in ips], filters, options)
 
     @private
-    async def sync(self):
+    def sync(self):
+        domain = ''
         domains = []
         nameservers = []
-
-        gc = await self.middleware.call('datastore.query', 'network.globalconfiguration', [], {'get': True})
+        gc = self.middleware.call_sync('datastore.query', 'network.globalconfiguration')[0]
         if gc['gc_domain']:
-            domains.append(gc['gc_domain'])
+            domain = gc['gc_domain']
         if gc['gc_domains']:
-            domains += gc['gc_domains'].split()
+            domains = gc['gc_domains'].split()
         if gc['gc_nameserver1']:
             nameservers.append(gc['gc_nameserver1'])
         if gc['gc_nameserver2']:
@@ -2391,19 +2400,53 @@ class DNSService(Service):
             nameservers.append(gc['gc_nameserver3'])
 
         resolvconf = ''
+        if domain:
+            resolvconf += 'domain {}\n'.format(domain)
         if domains:
             resolvconf += 'search {}\n'.format(' '.join(domains))
-        for ns in nameservers:
-            resolvconf += 'nameserver {}\n'.format(ns)
 
-        proc = await Popen([
-            '/sbin/resolvconf', '-a', 'lo0'
-        ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        data = await proc.communicate(input=resolvconf.encode())
-        if proc.returncode != 0:
-            self.logger.warn(f'Failed to run resolvconf: {data[1].decode()}')
+        resolvconf += self.configure_nameservers(nameservers)
 
-        await self.middleware.call_hook('dns.post_sync')
+        try:
+            with open('/etc/resolv.conf', 'w') as f:
+                f.write(resolvconf)
+        except Exception:
+            self.logger.error('Failed to write /etc/resolv.conf', exc_info=True)
+
+    @private
+    def configure_nameservers(self, nameservers):
+        result = ''
+        if nameservers:
+            # means nameservers are configured explicitly so add them
+            for i in nameservers:
+                result += f'nameserver {i}\n'
+        else:
+            # means there aren't any nameservers configured so let's
+            # check to see if dhcp is running on any of the interfaces
+            # and if there are, then check dhclient leases file for
+            # nameservers that were handed to us via dhcp
+            interfaces = self.middleware.call_sync('datastore.query', 'network.interfaces')
+            if interfaces:
+                interfaces = [i['int_interface'] for i in interfaces if i['int_dhcp']]
+            else:
+                ignore = self.middleware.call_sync('interface.internal_interfaces')
+                ignore.extend(self.middleware.call_sync('failover.internal_interfaces'))
+                ignore = tuple(ignore)
+                interfaces = list(filter(lambda x: not x.startswith(ignore), netif.list_interfaces().keys()))
+
+            dns_from_dhcp = set()
+            for iface in interfaces:
+                dhclient_running, dhclient_pid = self.middleware.call_sync('interface.dhclient_status', iface)
+                if dhclient_running:
+                    leases = self.middleware.call_sync('interface.dhclient_leases', iface)
+                    for dns_srvs in re.findall(r'option domain-name-servers (.+)', leases or ''):
+                        for dns in dns_srvs.split(';')[0].split(','):
+                            dns_from_dhcp.add(f'nameserver {dns.strip()}\n')
+
+            for dns in dns_from_dhcp:
+                result += dns
+
+        return result
 
 
 class NetworkGeneralService(Service):
