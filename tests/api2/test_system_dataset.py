7e72c271181fd6a41339e0157887ad772d0630d2
themylogin <themylogin@gmail.com>
2022-05-07 08:17:34 +0200

    Allow moving system dataset to a passphrase-encrypted pool (system dataset will be unencrypted)

--- tests/api2/test_system_dataset.py
+++ tests/api2/test_system_dataset.py
@@ -1,5 +1,7 @@
 import pytest
 from pytest_dependency import depends
+
+from middlewared.test.integration.assets.pool import another_pool
 from middlewared.test.integration.utils import call, mock, pool, ssh
 
 from auto_config import dev_test
@@ -26,6 +28,7 @@ def write_to_log(string):
 
 def test_system_dataset_migrate(request):
     depends(request, ["pool_04"], scope="session")
+
     config = call("systemdataset.config")
     assert config["pool"] == pool
     assert config["syslog"]
@@ -41,3 +44,24 @@ def test_system_dataset_migrate(request):
     call("systemdataset.update", {"pool": pool}, job=True)
     assert "test_system_dataset_migrate step 1" in read_log()
     assert "test_system_dataset_migrate step 2" in read_log()
+
+
+@pytest.mark.parametrize("lock", [False, True])
+def test_migrate_to_a_pool_with_passphrase_encrypted_root_dataset(request, lock):
+    depends(request, ["pool_04"], scope="session")
+
+    config = call("systemdataset.config")
+    assert config["pool"] == pool
+
+    with another_pool({"encryption": True, "encryption_options": {"passphrase": "passphrase"}}):
+        if lock:
+            call("pool.dataset.lock", "test", job=True)
+
+        assert "test" in call("systemdataset.pool_choices")
+
+        call("systemdataset.update", {"pool": "test"}, job=True)
+
+        ds = call("zfs.dataset.get_instance", "test/.system")
+        assert ds["properties"]["encryption"]["value"] == "off"
+
+        call("systemdataset.update", {"pool": pool}, job=True)
