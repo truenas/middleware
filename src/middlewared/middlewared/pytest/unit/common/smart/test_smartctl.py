61e2b674ea9e93ef063b3cecf8a774e0500e3c4d
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-05-16 16:45:31 +0200

    NAS-116063 / 22.02.2 / optimize system.is_enterprise_ix_hardware calls in smartctl (by yocalebo) (by bugclerk) (#8958)
    * add SMARTCTX namedtuple
    
    (cherry picked from commit 5fdd2b23c845d672082cfe9d4814c192f66580e5)
    
    * use SMARTCTX in etc_files/smartd.py
    
    (cherry picked from commit 0617bc0e00549925aa3a153092f37d6a9d408396)
    
    * use SMARTCTX in disk_/smartctl.py
    
    (cherry picked from commit 2fa99de8ee246671753751bdfca92cff300c242d)
    
    * use SMARTCTX in unit tests
    
    (cherry picked from commit a99d73867dfa3443e9b68f5ed766dd5c5cc577d9)
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
+++ src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
@@ -3,20 +3,19 @@ import subprocess
 from mock import Mock, patch
 import pytest
 
-from middlewared.common.smart.smartctl import get_smartctl_args
-from middlewared.pytest.unit.middleware import Middleware
+from middlewared.common.smart.smartctl import get_smartctl_args, SMARTCTX
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__disk_nonexistent():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-        assert await get_smartctl_args(None, {}, "ada0", "") is None
+    context = SMARTCTX(devices={}, enterprise_hardware=False)
+    assert await get_smartctl_args(context, "ada0", "") is None
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__nvme():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", True):
-        assert await get_smartctl_args(Middleware(), {}, "nvme0n1", "") == ["/dev/nvme0n1", "-d", "nvme"]
+    context = SMARTCTX(devices={}, enterprise_hardware=False)
+    assert await get_smartctl_args(context, "nvme0n1", "") == ["/dev/nvme0n1", "-d", "nvme"]
 
 
 @pytest.mark.parametrize("enclosure,dev", [
@@ -25,125 +24,157 @@ async def test__get_smartctl_args__nvme():
 ])
 @pytest.mark.asyncio
 async def test__get_smartctl_args__arcmsr(enclosure, dev):
-    async def annotate_devices_with_areca_dev_id(devices):
-        for v in devices.values():
-            v["areca_dev_id"] = enclosure
-
-    with patch("middlewared.common.smart.smartctl.annotate_devices_with_areca_dev_id",
-               annotate_devices_with_areca_dev_id):
-        with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-            assert await get_smartctl_args(None, {"ada0": {
+    context = SMARTCTX(
+        devices={
+            "ada0": {
                 "driver": "arcmsrX",
                 "controller_id": 1000,
                 "bus": 0,
                 "channel_no": 100,
                 "lun_id": 10,
-            }}, "ada0", "") == ["/dev/arcmsr1000", "-d", dev]
+            }
+        },
+        enterprise_hardware=False,
+    )
+
+    async def annotate_devices_with_areca_dev_id(devices):
+        for v in devices.values():
+            v["areca_dev_id"] = enclosure
+
+    func_str = "middlewared.common.smart.smartctl.annotate_devices_with_areca_dev_id"
+    with patch(func_str, annotate_devices_with_areca_dev_id):
+        assert await get_smartctl_args(context, "ada0", "") == ["/dev/arcmsr1000", "-d", dev]
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__rr274x_3x():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-        assert await get_smartctl_args(None, {"ada0": {
-            "driver": "rr274x_3x",
-            "controller_id": 1,
-            "bus": 0,
-            "channel_no": 2,
-            "lun_id": 10,
-        }}, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
+    context = SMARTCTX(
+        devices={
+            "ada0": {
+                "driver": "rr274x_3x",
+                "controller_id": 1,
+                "bus": 0,
+                "channel_no": 2,
+                "lun_id": 10,
+            }
+        },
+        enterprise_hardware=False,
+    )
+    assert await get_smartctl_args(context, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__rr274x_3x__1():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-        assert await get_smartctl_args(None, {"ada0": {
-            "driver": "rr274x_3x",
-            "controller_id": 1,
-            "bus": 0,
-            "channel_no": 18,
-            "lun_id": 10,
-        }}, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
+    context = SMARTCTX(
+        devices={
+            "ada0": {
+                "driver": "rr274x_3x",
+                "controller_id": 1,
+                "bus": 0,
+                "channel_no": 18,
+                "lun_id": 10,
+            },
+        },
+        enterprise_hardware=False,
+    )
+    assert await get_smartctl_args(context, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__rr274x_3x__2():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-        assert await get_smartctl_args(None, {"ada0": {
-            "driver": "rr274x_3x",
-            "controller_id": 1,
-            "bus": 0,
-            "channel_no": 10,
-            "lun_id": 10,
-        }}, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
+    context = SMARTCTX(
+        devices={
+            "ada0": {
+                "driver": "rr274x_3x",
+                "controller_id": 1,
+                "bus": 0,
+                "channel_no": 10,
+                "lun_id": 10,
+            },
+        },
+        enterprise_hardware=False,
+    )
+    assert await get_smartctl_args(context, "ada0", "") == ["/dev/rr274x_3x", "-d", "hpt,2/3"]
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__hpt():
-    with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-        assert await get_smartctl_args(None, {"ada0": {
-            "driver": "hptX",
-            "controller_id": 1,
-            "bus": 0,
-            "channel_no": 2,
-            "lun_id": 10,
-        }}, "ada0", "") == ["/dev/hptX", "-d", "hpt,2/3"]
+    context = SMARTCTX(
+        devices={
+            "ada0": {
+                "driver": "hptx",
+                "controller_id": 1,
+                "bus": 0,
+                "channel_no": 2,
+                "lun_id": 10,
+            },
+        },
+        enterprise_hardware=False,
+    )
+    assert await get_smartctl_args(context, "ada0", "") == ["/dev/hptX", "-d", "hpt,2/3"]
 
 
 @pytest.mark.asyncio
 async def test__get_smartctl_args__twa():
-    m = Middleware()
-    m['system.is_enterprise_ix_hardware'] = Mock(return_value=False)
-    with patch("middlewared.common.smart.smartctl.run") as run:
-        run.return_value = Mock(stdout="p28 u1\np29 u2")
-
-        with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-            assert await get_smartctl_args(m, {"ada0": {
+    context = SMARTCTX(
+        devices={
+            "ada0": {
                 "driver": "twaX",
                 "controller_id": 1,
                 "bus": 0,
                 "channel_no": 2,
                 "lun_id": 10,
-            }}, "ada0", "") == ["/dev/twaX1", "-d", "3ware,29"]
+            },
+        },
+        enterprise_hardware=False,
+    )
+    with patch("middlewared.common.smart.smartctl.run") as run:
+        run.return_value = Mock(stdout="p28 u1\np29 u2")
 
-            run.assert_called_once_with(
-                ["/usr/local/sbin/tw_cli", "/c1", "show"],
-                encoding="utf8",
-            )
+        assert await get_smartctl_args(context, "ada0", "") == ["/dev/twaX1", "-d", "3ware,29"]
+
+        run.assert_called_once_with(["/usr/local/sbin/tw_cli", "/c1", "show"], encoding="utf8")
 
 
 @pytest.mark.asyncio
 async def test_get_disk__unknown_usb_bridge():
-    m = Middleware()
-    m['system.is_enterprise_ix_hardware'] = Mock(return_value=False)
-    with patch("middlewared.common.smart.smartctl.run") as run:
-        run.return_value = Mock(stdout="/dev/da0: Unknown USB bridge [0x0930:0x6544 (0x100)]\n"
-                                       "Please specify device type with the -d option.")
-
-        with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-            assert await get_smartctl_args(m, {"ada0": {
+    context = SMARTCTX(
+        devices={
+            "ada0": {
                 "driver": "ata",
                 "controller_id": 1,
                 "bus": 0,
                 "channel_no": 2,
                 "lun_id": 10,
-            }}, "ada0", "") == ["/dev/ada0", "-d", "sat"]
+            },
+        },
+        enterprise_hardware=False,
+    )
+    stdout = "/dev/da0: Unknown USB bridge [0x0930:0x6544 (0x100)]\nPlease specify device type with the -d option."
+    with patch("middlewared.common.smart.smartctl.run") as run:
+        run.return_value = Mock(stdout=stdout)
+        assert await get_smartctl_args(context, "ada0", "") == ["/dev/ada0", "-d", "sat"]
 
-        run.assert_called_once_with(["smartctl", "/dev/ada0", "-i"], stderr=subprocess.STDOUT, check=False,
-                                    encoding="utf8", errors="ignore")
+    run.assert_called_once_with(
+        ["smartctl", "/dev/ada0", "-i"], stderr=subprocess.STDOUT, check=False, encoding="utf8", errors="ignore"
+    )
 
 
 @pytest.mark.asyncio
 async def test_get_disk__generic():
-    m = Middleware()
-    m['system.is_enterprise_ix_hardware'] = Mock(return_value=False)
-    with patch("middlewared.common.smart.smartctl.run") as run:
-        run.return_value = Mock(stdout="Everything is OK")
-
-        with patch("middlewared.common.smart.smartctl.osc.IS_LINUX", False):
-            assert await get_smartctl_args(m, {"ada0": {
+    context = SMARTCTX(
+        devices={
+            "ada0": {
                 "driver": "ata",
                 "controller_id": 1,
                 "bus": 0,
                 "channel_no": 2,
                 "lun_id": 10,
-            }}, "ada0", "") == ["/dev/ada0"]
+            },
+        },
+        enterprise_hardware=False,
+    )
+    with patch("middlewared.common.smart.smartctl.run") as run:
+        run.return_value = Mock(stdout="Everything is OK")
+
+        assert await get_smartctl_args(context, "ada0", "") == ["/dev/ada0"]
