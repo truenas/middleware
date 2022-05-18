2eda5290fd1079b3603f06b97a0a16b24f159716
caleb <yocalebo@gmail.com>
2022-05-18 10:22:17 -0400

    clean up and use pyudev for enclosure listing

--- src/middlewared/middlewared/plugins/enclosure_/ses_enclosure_linux.py
+++ src/middlewared/middlewared/plugins/enclosure_/ses_enclosure_linux.py
@@ -1,45 +1,36 @@
-# Copyright (c) 2019 iXsystems, Inc.
-# All rights reserved.
-# This file is a part of TrueNAS
-# and may not be copied and/or distributed
-# without the express permission of iXsystems.
-import os
-import subprocess
+from subprocess import run, PIPE
+
+from pyudev import Context
 
 from middlewared.service import private, Service
 
 
 class EnclosureService(Service):
+
     @private
     def list_ses_enclosures(self):
-        try:
-            return sorted([
-                os.path.join("/dev/bsg", enc)
-                for enc in os.listdir("/sys/class/enclosure")
-            ])
-        except FileNotFoundError:
-            return []
+        ctx = Context()
+        return [f'/dev/bsg/{i.sys_name}' for i in ctx.list_devices(subsystem='enclosure')]
 
     @private
     def get_ses_enclosures(self):
         output = {}
+        opts = {'encoding': 'utf-8', 'errors': 'ignore', 'stdout': PIPE, 'stderr': PIPE}
         for i, name in enumerate(self.list_ses_enclosures()):
-            p = subprocess.run(["sg_ses", "--page=cf", name], encoding="utf-8", errors="ignore",
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = run(["sg_ses", "--page=cf", name], **opts)
             if p.returncode != 0:
-                self.middleware.logger.debug("Error querying enclosure configuration page %r: %s", name, p.stderr)
+                self.logger.warning("Error querying enclosure configuration page %r: %s", name, p.stderr)
                 continue
             else:
                 cf = p.stdout
 
-            p = subprocess.run(["sg_ses", "-i", "--page=es", name], encoding="utf-8", errors="ignore",
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = run(["sg_ses", "-i", "--page=es", name], **opts)
             if p.returncode != 0:
-                self.middleware.logger.debug("Error querying enclosure status page %r: %s", name, p.stderr)
+                self.logger.debug("Error querying enclosure status page %r: %s", name, p.stderr)
                 continue
             else:
                 es = p.stdout
 
-            output[i] = (os.path.relpath(name, "/dev"), (cf, es))
+            output[i] = (name.removeprefix('/dev/'), (cf, es))
 
         return output
