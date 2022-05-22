05a3c37ffac0c776bdf17a12cb56d7e3bbe4c192
themylogin <themylogin@gmail.com>
2022-05-22 23:04:41 +0200

    `pool.is_upgraded` minor performance improvements and tests (#9009)

---
+++ tests/api2/test_pool_is_upgraded.py
@@ -0,0 +1,33 @@
+import pytest
+
+from middlewared.test.integration.assets.pool import another_pool, pool
+from middlewared.test.integration.utils import call, ssh
+
+
+@pytest.fixture(scope="module")
+def outdated_pool():
+    with another_pool() as pool:
+        device = pool["topology"]["data"][0]["path"]
+        ssh(f"zpool export {pool['name']}")
+        ssh(f"zpool create test -o altroot=/mnt -o feature@sha512=disabled -f {device}")
+        yield pool
+
+
+def test_is_upgraded():
+    pool_id = call("pool.query", [["name", "=", pool]])[0]["id"]
+    assert call("pool.is_upgraded", pool_id)
+
+
+def test_is_outdated(outdated_pool):
+    assert call("pool.is_upgraded", outdated_pool["id"]) is False
+
+
+def test_is_outdated_in_list(outdated_pool):
+    pool = call("pool.query", [["id", "=", outdated_pool["id"]]], {"extra": {"is_upgraded": True}})[0]
+    assert pool["is_upgraded"] is False
+
+
+def test_is_outdated_alert(outdated_pool):
+    alerts = call("alert.run_source", "VolumeVersion")
+    assert len(alerts) == 1
+    assert alerts[0]["args"] == outdated_pool["name"]
