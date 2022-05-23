cfe64204031ab081ada2bda8f7797ba12f38e64b
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-05-23 18:54:32 +0500

    Add migration for defining ssl version of kmip server
    (cherry picked from commit 5af57fc2d94f22c145e82959b499d15b0d15e907)

---
+++ src/middlewared/middlewared/alembic/versions/22.02/2022-05-23_18_22_kmip_ssl_version.py
@@ -0,0 +1,28 @@
+"""
+Add KMIP ssl version field
+
+Revision ID: 8b6d0edc6a38
+Revises: 19eb67dcdee2
+Create Date: 2022-05-23 18:22:38.186590+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '8b6d0edc6a38'
+down_revision = '19eb67dcdee2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    with op.batch_alter_table('system_kmip', schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column('ssl_version', sa.String(length=128), nullable=False, server_default='PROTOCOL_TLSv1_2')
+        )
+
+
+def downgrade():
+    pass
