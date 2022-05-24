aa5dd424dc1698b738e5dbef0109463888389a62
Andrew Walker <awalker@ixsystems.com>
2022-05-24 10:12:30 -0700

    include file type in filesystem.stat output
    This is a convenience feature to show whether the target is
    directory, symlink, etc with appropriate call to lstat() /
    stat() depending on circumstances. Also include in stat() output
    whether the path is a mountpoint. Also include whether path is
    in the ZFS ctldir.

---
+++ tests/utils.py
@@ -0,0 +1,31 @@
+import contextlib
+import urllib.parse
+from functions import POST, SSH_TEST, DELETE, wait_on_job
+from time import sleep
+
+@contextlib.contextmanager
+def create_dataset(dataset, options=None, acl=None, mode=None):
+    assert "/" not in name
+    perm_job = None
+
+    result = POST("/pool/dataset/", {"name": dataset, **(options or {})})
+    assert result.status_code == 200, result.text
+
+    if mode is not None:
+        perm_job = POST("/filesystem/setperm/", {'path': f"/mnt/{dataset}", "mode": mode})
+    elif acl is not None:
+        perm_job = POST("/filesystem/setacl/", {'path': f"/mnt/{dataset}", "dacl": acl})
+
+    if perm_job:
+        assert perm_job.status_code == 200, result.text
+        job_status = wait_on_job(perm_job.json(), 180)
+        assert job_status["state"] == "SUCCESS", str(job_status["results"])
+
+    try:
+        yield dataset
+    finally:
+        # dataset may be busy
+        sleep(5)
+        result = DELETE(f"/pool/dataset/id/{urllib.parse.quote(dataset, '')}/",
+                        {'recursive': True})
+        assert result.status_code == 200, result.text
