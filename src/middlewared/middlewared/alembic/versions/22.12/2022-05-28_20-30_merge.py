194cadc9ff73039252f73731bf8ccf4029d6007f
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-05-28 22:34:55 +0500

    Add merge migration

---
+++ src/middlewared/middlewared/alembic/versions/22.12/2022-05-28_20-30_merge.py
@@ -0,0 +1,22 @@
+"""
+Merge Migration
+
+Revision ID: a719566695c3
+Revises: 59fbc9897ec3, 8b6d0edc6a38
+Create Date: 2022-05-28 20:30:01.759559+00:00
+
+"""
+
+revision = 'a719566695c3'
+down_revision = ('59fbc9897ec3', '8b6d0edc6a38')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
