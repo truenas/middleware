15e199fa7b8cf68d7c4cc22949be07ed525dbc6b
themylogin <themylogin@gmail.com>
2022-06-03 18:40:31 +0200

    Fix `rmtree_one_filesystem` error reporting
    (cherry picked from commit ad87370ac7d2dbcfa86147572d338ab33bfd5984)

--- src/middlewared/middlewared/utils/shutil.py
+++ src/middlewared/middlewared/utils/shutil.py
@@ -5,5 +5,8 @@ __all__ = ["rmtree_one_filesystem"]
 
 
 def rmtree_one_filesystem(path):
-    subprocess.run(['rm', '--one-file-system', '-rf', path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                   encoding="utf-8", errors="ignore", check=True)
+    try:
+        subprocess.run(['rm', '--one-file-system', '-rf', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       encoding="utf-8", errors="ignore", check=True)
+    except subprocess.CalledProcessError as e:
+        raise OSError(e.stderr)
