0ed5d8e036f4dbfa7ab6277e8928339bf0507f8c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-06-19 19:46:25 +0500

    Add nested execution test

--- tests/api2/test_can_access_as_user.py
+++ tests/api2/test_can_access_as_user.py
@@ -1,7 +1,7 @@
 import contextlib
 import pytest
 
-from middlewared.test.integration.assets.pool import dataset
+from middlewared.test.integration.assets.pool import dataset, pool
 from middlewared.test.integration.utils import call, ssh
 from auto_config import dev_test
 # comment pytestmark for development testing with --dev-test
@@ -19,14 +19,32 @@ pytestmark = pytest.mark.skipif(dev_test, reason=reason)
 
 @contextlib.contextmanager
 def file(name, user, group, permissions):
-    with dataset('test_perms', pool='evo') as test_dataset:
+    with dataset('test_perms', pool=pool) as test_dataset:
         path = os.path.join('/mnt', test_dataset, name)
-        ssh(f'install -o {user} -g {group} -m {permissions} /dev/null {path}')
+        with file_at_path(path, user, group, permissions):
+            yield path
+
+
+@contextlib.contextmanager
+def file_at_path(path, user, group, permissions):
+    ssh(f'install -o {user} -g {group} -m {permissions} /dev/null {path}')
+    try:
+        yield path
+    finally:
+        ssh(f'rm -f {path}')
+
+
+@contextlib.contextmanager
+def directory(name, user, group, permissions):
+    with dataset('test_perms', pool=pool) as test_dataset:
+        path = os.path.join('/mnt', test_dataset, name)
+        ssh(f'mkdir -p -m {permissions} {path}')
+        ssh(f'chown -R {user}:{group} {path}')
 
         try:
             yield path
         finally:
-            ssh(f'rm -f {path}')
+            ssh(f'rm -rf {path}')
 
 
 def test_non_authorized_user_access():
@@ -61,3 +79,10 @@ def test_execute_access():
         with file('test', user, group, '100') as file_path:
             for perm_check, value in (('read', False), ('write', False), ('execute', True)):
                 assert call('filesystem.can_access_as_user', user, file_path, {perm_check: True}) is value
+
+
+def test_nested_perm_execute_check():
+    with directory('test_dir', 'root', 'root', '700') as dir_path:
+        file_path = os.path.join(dir_path, 'testfile')
+        with file_at_path(file_path, 'root', 'root', '777'):
+            assert call('filesystem.can_access_as_user', 'apps', file_path, {'execute': True}) is False
