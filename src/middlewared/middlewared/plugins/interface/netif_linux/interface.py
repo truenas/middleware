1dc407db83403437aac70782f926166af7d366d0
caleb <yocalebo@gmail.com>
2022-06-23 08:55:32 -0400

    down iface before changing mac address

--- src/middlewared/middlewared/plugins/interface/netif_linux/interface.py
+++ src/middlewared/middlewared/plugins/interface/netif_linux/interface.py
@@ -85,9 +85,17 @@ class Interface(AddressMixin, BridgeMixin, LaggMixin, VlanMixin, VrrpMixin):
     def link_address(self, value):
         with NDB(log='off') as ndb:
             with ndb.interfaces[self.orig_name] as dev:
-                dev['address'] = value
+                upit = False
                 if dev['state'] == 'up':
+                    # gotta take down interface first
                     dev['state'] = 'down'
+                    upit = True
+
+                dev['address'] = value  # change mac
+
+                if upit:
+                    # only bring it back up if the iface
+                    # was up to begin with
                     dev['state'] = 'up'
 
         # NDB() synchronizes state but the instantiation
