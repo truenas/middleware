05e59802e5c89cd011aa715d41a819198fa9a95c
Andrew <awalker@ixsystems.com>
2022-06-27 07:07:18 -0700

    Ensure that SMB service is properly initialized before starting (#7800)
    When starting SMB service check that required steps to properly
    initialize it have been completed. This should happen during
    system dataset setup on boot, but historically system dataset
    setup can be unreliable due to varying user configurations and
    the complexity of steps involved. If SMB hasn't been configured
    at time of SMB service manual start, then configure it. This
    should at least make the situation at least somewhat more user-
    servicable as they will be able to toggle the SMB service and
    get a working configuration.
    
    When unlocking dataset or importing pool, verify that SMB service has
    been properly initialized before calling hooks. Uninitialized SMB
    service should be no-op here.

--- src/middlewared/middlewared/plugins/service_/services/idmap.py
+++ src/middlewared/middlewared/plugins/service_/services/idmap.py
@@ -8,8 +8,30 @@ class IdmapService(SimpleService):
 
     systemd_unit = "winbind"
 
+    async def healthy(self):
+        is_clustered = await self.middleware.call("smb.getparm", "clustering", "global")
+        if is_clustered:
+            cluster_healthy = await self.middleware.call("ctdb.general.healthy")
+            if not cluster_healthy:
+                self.middleware.logger.warning("Cluster is unhealthy. Refusing to start SMB service.")
+                return False
+
+        return await self.middleware.call("smb.configure_wait")
+
+    async def start(self):
+        if not await self.healthy():
+            return
+
+        await self._systemd_unit("winbind", "start")
+
     async def restart(self):
+        if not await self.healthy():
+            return
+
         return await self._systemd_unit("winbind", "restart")
 
     async def reload(self):
+        if not await self.healthy():
+            return
+
         return await self._systemd_unit("winbind", "reload")
