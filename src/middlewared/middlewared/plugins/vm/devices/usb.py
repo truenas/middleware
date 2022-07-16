61a28e7923ab49fe9344430d4f89535448c6d10c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-07-16 05:54:45 +0500

    Add logic to see if usb device is available

--- src/middlewared/middlewared/plugins/vm/devices/usb.py
+++ src/middlewared/middlewared/plugins/vm/devices/usb.py
@@ -12,5 +12,23 @@ class USB(Device):
         Str('device', required=True, empty=False),
     )
 
+    @property
     def usb_device(self):
         return self.data['attributes']['device']
+
+    def identity(self):
+        return self.usb_device
+
+    def get_vms_using_device(self):
+        devs = self.middleware.call_sync(
+            'vm.device.query', [['attributes.device', '=', self.usb_device], ['dtype', '=', 'USB']]
+        )
+        return self.middleware.call_sync('vm.query', [['id', 'in', [dev['vm'] for dev in devs]]])
+
+    def get_details(self):
+        return self.middleware.call_sync('vm.device.usb_passthrough_device', self.usb_device)
+
+    def is_available(self):
+        return self.get_details()['available']
+
+
