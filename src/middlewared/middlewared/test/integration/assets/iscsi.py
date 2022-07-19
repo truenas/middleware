4a9008be963bdfaf73944189cee25cad74fa0a72
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-07-19 09:59:24 +0500

    Added retries to handle running iscsi test on freebsd

--- src/middlewared/middlewared/test/integration/assets/iscsi.py
+++ src/middlewared/middlewared/test/integration/assets/iscsi.py
@@ -1,5 +1,6 @@
 import contextlib
 import json
+import time
 
 from middlewared.utils import osc
 
@@ -38,12 +39,22 @@ def iscsi_client_freebsd():
 
 def target_login_impl_freebsd(portal_ip, target_name):
     run_on_runner(['iscsictl', '-A', '-p', portal_ip, '-t', target_name])
-    cp = run_on_runner(['iscsictl', '-L', '--libxo', 'json'])
-    connected_clients = json.loads(cp.stdout.decode(errors='ignore'))
-    assert any(
-        session.get('state') == 'Connected' for session in connected_clients.get('iscsictl', {}).get('session', [])
-        if session.get('name') == target_name
-    ) is True, cp.stdout
+    retries = 5
+    connected = False
+    connected_clients = None
+    # Unfortunately iscsictl can take some time to show the client as actually connected so adding a few retries here
+    # to handle that case
+    while retries > 0 and not connected:
+        time.sleep(3)
+        cp = run_on_runner(['iscsictl', '-L', '--libxo', 'json'])
+        connected_clients = json.loads(cp.stdout)
+        connected = any(
+            session.get('state') == 'Connected' for session in connected_clients.get('iscsictl', {}).get('session', [])
+            if session.get('name') == target_name
+        )
+        retries -= 1
+
+    assert connected is True, connected_clients
 
 
 def target_login_test_freebsd(portal_ip, target_name):
@@ -52,6 +63,7 @@ def target_login_test_freebsd(portal_ip, target_name):
             target_login_impl_freebsd(portal_ip, target_name)
         except AssertionError:
             return False
+        
         else:
             run_on_runner(['iscsictl', '-R', '-t', target_name])
             return True
