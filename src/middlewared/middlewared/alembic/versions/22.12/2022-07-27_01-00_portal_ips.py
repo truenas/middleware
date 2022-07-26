178e88b376197d4133578f7dfd28ffff8e83ff2a
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-07-27 02:38:21 +0500

    Add migration to move port from portal table to global iscsi config

---
+++ src/middlewared/middlewared/alembic/versions/22.12/2022-07-27_01-00_portal_ips.py
@@ -0,0 +1,46 @@
+"""
+Migrate iSCSI Portal IPs
+
+Revision ID: 14899f89b885
+Revises: 3ac384af617f
+Create Date: 2022-07-27 01:00:58.755371+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '14899f89b885'
+down_revision = '3ac384af617f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+
+    with op.batch_alter_table('services_iscsitargetglobalconfiguration', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('iscsi_listen_port', sa.INTEGER(), nullable=False))
+
+    listen_port = None
+    all_ports = set()
+    for row in map(dict, conn.execute("SELECT * FROM services_iscsitargetportalip").fetchall()):
+        all_ports.add(row['iscsi_target_portalip_port'])
+        if row['iscsi_target_portalip_ip'] == '0.0.0.0':
+            listen_port = row['iscsi_target_portalip_port']
+            break
+    else:
+        if len(all_ports) == 1:
+            listen_port = all_ports.pop()
+
+    if listen_port is None:
+        listen_port = 3260
+
+    conn.execute("UPDATE services_iscsitargetglobalconfiguration SET iscsi_listen_port = ?", listen_port)
+
+    with op.batch_alter_table('services_iscsitargetportalip', schema=None) as batch_op:
+        batch_op.drop_column('iscsi_target_portalip_port')
+
+
+def downgrade():
+    pass
