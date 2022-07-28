6eefb880be4815dc14d2a7ce07a1ec13eb1ef8d5
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-07-28 12:53:32 +0200

    Fix kubernetes token issue (#9507)

--- src/middlewared/middlewared/plugins/service_/services/kubernetes.py
+++ src/middlewared/middlewared/plugins/service_/services/kubernetes.py
@@ -1,10 +1,15 @@
-import os
 import asyncio
+import contextlib
+import os
+import re
 
 from middlewared.service import CallError
 from middlewared.utils import run
 
 from .base import SimpleService
+
+
+RE_TOKEN_FORMAT = re.compile(r'^K10.+:.+:.*')
 
 
 class KubernetesService(SimpleService):
@@ -58,8 +63,25 @@ class KubernetesService(SimpleService):
         await self.middleware.call('service.start', 'docker')
         await self._systemd_unit('cni-dhcp', 'start')
 
-    async def _start_linux(self):
-        await super()._start_linux()
+        # It is possible server/token got misconfigured somehow ( we have had reports that on unclean shutdown the file
+        # is empty or malformed, in this case we should remove the token as it's generated automatically by k3s again
+        # and it's not useful to us as well per se as it's used for multi-node cluster )
+
+        await self.middleware.run_in_thread(
+            self.check_server_token, (await self.middleware.call('kubernetes.config'))['dataset']
+        )
+
+    def check_server_token(self, k8s_dataset):
+        token_path = os.path.join('/mnt', k8s_dataset, 'k3s/server/token')
+        with contextlib.suppress(FileNotFoundError):
+            with open(token_path, 'r') as f:
+                token = f.read().strip()
+
+            if not RE_TOKEN_FORMAT.findall(token):
+                os.unlink(token_path)
+
+    async def start(self):
+        await super().start()
         timeout = 40
         # First time when k8s is started, it takes a bit more time to initialise itself properly
         # and we need to have sleep here so that after start is called post_start is not dismissed
