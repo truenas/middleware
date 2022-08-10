309764f9d739479c21a34523df5faa63b6593c7a
caleb <yocalebo@gmail.com>
2022-08-10 08:29:08 -0400

    missing commas

--- src/middlewared/middlewared/plugins/pool_/dataset_details.py
+++ src/middlewared/middlewared/plugins/pool_/dataset_details.py
@@ -38,10 +38,10 @@ class PoolDatasetService(Service):
                         'source': 'LOCAL',
                         'parsed': '10.231.1.155'
                     },
-                    'quota_warning': {'value': '80', 'rawvalue': '80', 'source': 'LOCAL', 'parsed': '80'}
-                    'quota_critical': {'value': '95', 'rawvalue': '95', 'source': 'LOCAL', 'parsed': '95'}
-                    'refquota_warning': {'value': '80', 'rawvalue': '80', 'source': 'LOCAL', 'parsed': '80'}
-                    'refquota_critical': {'value': '95', 'rawvalue': '95', 'source': 'LOCAL', 'parsed': '95'}
+                    'quota_warning': {'value': '80', 'rawvalue': '80', 'source': 'LOCAL', 'parsed': '80'},
+                    'quota_critical': {'value': '95', 'rawvalue': '95', 'source': 'LOCAL', 'parsed': '95'},
+                    'refquota_warning': {'value': '80', 'rawvalue': '80', 'source': 'LOCAL', 'parsed': '80'},
+                    'refquota_critical': {'value': '95', 'rawvalue': '95', 'source': 'LOCAL', 'parsed': '95'},
                     'reservation': {
                         'parsed': None, 'rawvalue': '0', 'value': None, 'source': 'DEFAULT', 'source_info': None
                     },
