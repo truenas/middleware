dc3cd14ebed5c448611c4c361b295c374c457e96
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-08-16 20:35:01 +0500

    Add initial implementation to determine if a vm is affected by a snapshot

--- src/middlewared/middlewared/plugins/vm/attachments.py
+++ src/middlewared/middlewared/plugins/vm/attachments.py
@@ -1,9 +1,50 @@
 import asyncio
+import collections
 import os.path
 
 from middlewared.common.attachment import FSAttachmentDelegate
 from middlewared.plugins.zfs_.utils import zvol_path_to_name
+from middlewared.service import private, Service
 from middlewared.utils.path import is_child
+
+from .utils import ACTIVE_STATES
+
+
+class VMService(Service):
+
+    @private
+    async def query_snapshot_begin(self, dataset, recursive):
+        vms = collections.defaultdict(list)
+        to_ignore_vms = await self.get_vms_to_ignore_for_querying_attachments(True)
+        for device in await self.middleware.call(
+            'vm.device.query', [
+                ['dtype', 'in', ('DISK', 'RAW', 'CDROM')],
+                ['vm', 'nin', to_ignore_vms],
+            ]
+        ):
+            path = device['attributes'].get('path')
+            if not path:
+                continue
+            elif path.startswith('/dev/zvol'):
+                path = os.path.join('/mnt', zvol_path_to_name(path))
+
+            dataset_path = os.path.join('/mnt', dataset)
+            recursive_search = recursive or device['dtype'] != 'DISK'
+            if recursive_search:
+                if is_child(path, dataset_path):
+                    vms[device['vm']].append(device)
+            elif dataset_path == path:
+                vms[device['vm']].append(device)
+
+        return vms
+
+    @private
+    async def get_vms_to_ignore_for_querying_attachments(self, enabled):
+        return {
+            vm['id']: vm for vm in await self.middleware.call(
+                'vm.query', [('status.state', 'nin' if enabled else 'in', ACTIVE_STATES)]
+            )
+        }
 
 
 class VMFSAttachmentDelegate(FSAttachmentDelegate):
@@ -12,11 +53,7 @@ class VMFSAttachmentDelegate(FSAttachmentDelegate):
 
     async def query(self, path, enabled, options=None):
         vms_attached = []
-        ignored_vms = {
-            vm['id']: vm for vm in await self.middleware.call(
-                'vm.query', [('status.state', '!=' if enabled else '=', 'RUNNING')]
-            )
-        }
+        ignored_vms = await self.middleware.call('vm.get_vms_to_ignore_for_querying_attachments', enabled)
         for device in await self.middleware.call('datastore.query', 'vm.device'):
             if (device['dtype'] not in ('DISK', 'RAW', 'CDROM')) or device['vm']['id'] in ignored_vms:
                 continue
