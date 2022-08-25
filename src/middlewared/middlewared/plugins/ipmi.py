2550ad754d4c5889069692a6ab33aa6dcc2ae896
caleb <yocalebo@gmail.com>
2022-08-25 07:46:44 -0400

    start openipmi only when IPMI dev exists

--- src/middlewared/middlewared/plugins/ipmi.py
+++ src/middlewared/middlewared/plugins/ipmi.py
@@ -203,3 +203,12 @@ class IPMIService(CRUDService):
     def clear_sel(self):
         """Clear IPMI system event log."""
         run(['ipmitool', 'sel', 'clear'], stdout=DEVNULL, stderr=DEVNULL)
+
+
+async def setup(middleware):
+    if await middleware.call('system.ready') and (await middleware.call('system.dmidecode_info'))['has-ipmi']:
+        # systemd generates a unit file that doesn't honor presets so when it's started on a system without a
+        # BMC device, it always reports as a failure which is expected since no IPMI device exists. Instead
+        # we check to see if dmidecode reports an ipmi device via type "38" of the SMBIOS spec. It's not
+        # fool-proof but it's the best we got atm.
+        await middleware.call('service.start', 'openipmi')
