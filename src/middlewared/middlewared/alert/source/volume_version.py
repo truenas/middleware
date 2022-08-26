44e483ecf87614ddf3138196453bf5e431e20e28
caleb <yocalebo@gmail.com>
2022-08-26 11:24:29 -0400

    fix many issues with volume_version alert

--- src/middlewared/middlewared/alert/source/volume_version.py
+++ src/middlewared/middlewared/alert/source/volume_version.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, AlertSource
 from middlewared.alert.schedule import IntervalSchedule
 
 
@@ -9,22 +9,23 @@ class VolumeVersionAlertClass(AlertClass):
     level = AlertLevel.WARNING
     title = "New Feature Flags Are Available for Pool"
     text = (
-        "New ZFS version or feature flags are available for pool %s. Upgrading pools is a one-time process that can "
+        "New ZFS version or feature flags are available for pool(s) %s. Upgrading pools is a one-time process that can "
         "prevent rolling the system back to an earlier TrueNAS version. It is recommended to read the TrueNAS release "
         "notes and confirm you need the new ZFS feature flags before upgrading a pool."
     )
 
 
-class VolumeVersionAlertSource(ThreadedAlertSource):
+class VolumeVersionAlertSource(AlertSource):
     schedule = IntervalSchedule(timedelta(minutes=5))
+    run_on_backup_node = False
 
-    def check_sync(self):
-        alerts = []
-        for pool in self.middleware.call_sync("pool.query"):
-            if not self.middleware.call_sync('pool.is_upgraded', pool["id"]):
-                alerts.append(Alert(
-                    VolumeVersionAlertClass,
-                    pool["name"],
-                ))
+    async def check(self):
+        pools_needing_upgrade = []
+        for pool in await self.middleware.call("pool.query"):
+            if not await self.middleware.call("pool.is_upgraded", pool["id"]):
+                pools_needing_upgrade.append(pool["name"])
+
+        if pools_needing_upgrade:
+            return Alert(VolumeVersionAlertClass, ', '.join(pools_needing_upgrade))
 
         return alerts
