c324d6935b9669cff18a1bca6c6b8767cdf5e3b7
caleb <yocalebo@gmail.com>
2022-08-29 13:17:22 -0400

    make the functional tests apply to linux

--- src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
+++ src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
@@ -9,7 +9,7 @@ from middlewared.common.smart.smartctl import get_smartctl_args, SMARTCTX
 @pytest.mark.asyncio
 async def test__get_smartctl_args__disk_nonexistent():
     context = SMARTCTX(devices={}, enterprise_hardware=False)
-    assert await get_smartctl_args(context, "ada0", "") is None
+    assert await get_smartctl_args(context, "sda", "") is None
 
 
 @pytest.mark.asyncio
@@ -22,23 +22,23 @@ async def test__get_smartctl_args__nvme():
 async def test_get_disk__unknown_usb_bridge():
     context = SMARTCTX(
         devices={
-            "ada0": {
-                "driver": "ata",
-                "controller_id": 1,
+            "sda": {
+                "driver": "sd",
+                "controller_id": 17,
                 "bus": 0,
-                "channel_no": 2,
-                "lun_id": 10,
+                "channel_no": 0,
+                "lun_id": 0,
             },
         },
         enterprise_hardware=False,
     )
-    stdout = "/dev/da0: Unknown USB bridge [0x0930:0x6544 (0x100)]\nPlease specify device type with the -d option."
+    stdout = "/dev/sda: Unknown USB bridge [0x0930:0x6544 (0x100)]\nPlease specify device type with the -d option."
     with patch("middlewared.common.smart.smartctl.run") as run:
         run.return_value = Mock(stdout=stdout)
-        assert await get_smartctl_args(context, "ada0", "") == ["/dev/ada0", "-d", "sat"]
+        assert await get_smartctl_args(context, "sda", "") == ["/dev/sda", "-d", "sat"]
 
     run.assert_called_once_with(
-        ["smartctl", "/dev/ada0", "-i"], stderr=subprocess.STDOUT, check=False, encoding="utf8", errors="ignore"
+        ["smartctl", "/dev/sda", "-i"], stderr=subprocess.STDOUT, check=False, encoding="utf8", errors="ignore"
     )
 
 
@@ -46,12 +46,12 @@ async def test_get_disk__unknown_usb_bridge():
 async def test_get_disk__generic():
     context = SMARTCTX(
         devices={
-            "ada0": {
-                "driver": "ata",
-                "controller_id": 1,
+            "sda": {
+                "driver": "sd",
+                "controller_id": 17,
                 "bus": 0,
-                "channel_no": 2,
-                "lun_id": 10,
+                "channel_no": 0,
+                "lun_id": 0,
             },
         },
         enterprise_hardware=False,
@@ -59,4 +59,4 @@ async def test_get_disk__generic():
     with patch("middlewared.common.smart.smartctl.run") as run:
         run.return_value = Mock(stdout="Everything is OK")
 
-        assert await get_smartctl_args(context, "ada0", "") == ["/dev/ada0"]
+        assert await get_smartctl_args(context, "sda", "") == ["/dev/sda"]
