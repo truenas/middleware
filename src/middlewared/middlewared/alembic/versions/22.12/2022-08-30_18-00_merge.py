40d58c1739b655572e514011e459e6a3fcb83bc5
M. Rehan <mrehanlm93@gmail.com>
2022-08-31 19:20:20 +0500

    Merge migration

---
+++ src/middlewared/middlewared/alembic/versions/22.12/2022-08-30_18-00_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: eba33d756a77
+Revises: daaf691ed483, 98b2cfaa0e5a
+Create Date: 2022-08-30 18:00:11.301224+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eba33d756a77'
+down_revision = ('daaf691ed483', '98b2cfaa0e5a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
