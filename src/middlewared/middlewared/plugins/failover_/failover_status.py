3e258790f20c312fc61d167954066d082b1b068e
caleb <yocalebo@gmail.com>
2022-09-01 13:47:15 -0400

    zfs.pool.query_imported_fast in failover.status

--- src/middlewared/middlewared/plugins/failover_/failover_status.py
+++ src/middlewared/middlewared/plugins/failover_/failover_status.py
@@ -14,34 +14,21 @@ class DetectFailoverStatusService(Service):
         if not any(filter(lambda x: x['state']['vrrp_config'], interfaces)):
             return 'SINGLE'
 
-        # Check if we have at least 1 pool configured and imported
-        pools = await self.middleware.call('pool.query')
-        if not pools:
+        pools = await self.middleware.call('zfs.pool.query_imported_fast')
+        if len(pools) <= 1:
+            # Check if we have at least 1 pool configured and imported (excluding boot pool)
             return 'SINGLE'
-
-        # Check if we have any VRRP MASTER interfaces
-        if (await self.middleware.call('failover.vip.get_states', interfaces))[0]:
-
-            # If we have VRRP MASTER interfaces, ensure none of the zpools
-            # are offline
-            if any(filter(lambda x: x.get('status') != 'OFFLINE', pools)):
-                return 'MASTER'
-
-            # need to check to check 2 things if we get to this point
-            #
-            #   1. check to make sure there isn't an ongoing failover event
-            #
-            #   2. check to make sure that the last failover event didn't
-            #       fail
-            #
+        elif (await self.middleware.call('failover.vip.get_states', interfaces))[0]:
+            # Means we have VRRP MASTER interfaces and we have pool(s) imported
+            return 'MASTER'
+        else:
             failover_events = await self.middleware.call(
-                'core.get_jobs',
-                [('method', '=', 'failover.event.vrrp_master')],
-                {'order_by': ['-id']},
+                'core.get_jobs', [('method', '=', 'failover.event.vrrp_master')], {'order_by': ['-id']}
             )
-
             for i in failover_events:
                 if i['state'] == 'RUNNING':
+                    # we're currently becoming master node
                     return i['progress']['description']
                 elif i['progress']['description'] == 'ERROR':
+                    # last failover failed
                     return i['progress']['description']
