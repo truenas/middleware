58f816003a62901c59920eb2eb2bc03179cf2b72
caleb <yocalebo@gmail.com>
2022-09-02 08:09:41 -0400

    add fenced_running and improve status.get_local

--- src/middlewared/middlewared/plugins/failover_/failover_status.py
+++ src/middlewared/middlewared/plugins/failover_/failover_status.py
@@ -8,27 +8,63 @@ class DetectFailoverStatusService(Service):
         namespace = 'failover.status'
 
     async def get_local(self, app):
-
-        # Check if we have at least 1 interface with VRRP config
-        interfaces = await self.middleware.call('interface.query')
-        if not any(filter(lambda x: x['state']['vrrp_config'], interfaces)):
+        licensed = await self.middleware.call('failover.licensed')
+        if not licensed:
+            # no HA license so nothing else matters
             return 'SINGLE'
 
-        pools = await self.middleware.call('zfs.pool.query_imported_fast')
-        if len(pools) <= 1:
-            # Check if we have at least 1 pool configured and imported (excluding boot pool)
-            return 'SINGLE'
-        elif (await self.middleware.call('failover.vip.get_states', interfaces))[0]:
-            # Means we have VRRP MASTER interfaces and we have pool(s) imported
+        fenced_running = (await self.middleware.call('failover.fenced.run_info'))['running']
+        only_boot_pool = len((await self.middleware.call('zfs.pool.query_imported_fast'))) <= 1
+        if not fenced_running:
+            if only_boot_pool:
+                # we only have boot pool, fenced is not running, and we're licensed
+                # safe to assume we're the backup node
+                return 'BACKUP'
+            else:
+                # we have at least 1 zpool imported, but fenced is not running and we're licensed
+                # ...that's not good but it's safe to return MASTER. failover.disabled.reasons
+                # will return NO_FENCED will cause alerts and warnings and emails to be sent to
+                # end-user
+                return 'MASTER'
+        elif not only_boot_pool:
+            # we have at least 1 zpool imported, fenced is running, and we're licensed
+            # safe to assume we're the master node
             return 'MASTER'
-        else:
-            failover_events = await self.middleware.call(
-                'core.get_jobs', [('method', '=', 'failover.event.vrrp_master')], {'order_by': ['-id']}
-            )
-            for i in failover_events:
-                if i['state'] == 'RUNNING':
-                    # we're currently becoming master node
-                    return i['progress']['description']
-                elif i['progress']['description'] == 'ERROR':
-                    # last failover failed
-                    return i['progress']['description']
+
+        master_ifaces = backup_ifaces = 0
+        interfaces = await self.middleware.call('interface.query')
+        for iface in filter(lambda x: x['state']['vrrp_config'], interfaces):
+            for ip in iface['state']['vrrp_config']:
+                if ip['state'] == 'MASTER':
+                    master_ifaces += 1
+                else:
+                    backup_ifaces += 1
+
+        if master_ifaces and not backup_ifaces:
+            # all interfaces that are configured for HA are master, safe to assume
+            # this _should_ be the master node. we shouldn't get here on a
+            # healhy HA system because we should be able to determine the status
+            # based on zpools and fenced running
+            return 'MASTER'
+        elif backup_ifaces and not master_ifaces:
+            # all interfaces that are configured for HA are backup, safe to assume
+            # this _should_ be the backup node. we shouldn't get here on a
+            # healhy HA system because we should be able to determine the status
+            # based on zpools and fenced running
+            return 'BACKUP'
+
+        # last ditch effort to determine the status of this node. if there are
+        # no failover events occurring locally and we make it this far, the caller
+        # of this method will check the remote system which is slow...but have no
+        # option at that point. Note: we shouldn't get here ideally because calling
+        # core.get_jobs is not known for being "performant" especially as more jobs
+        # accumulate as uptime increases
+        filters = [('method', '=', 'failover.event.vrrp_master')]
+        options = {'order_by': ['-id']}
+        for i in await self.middleware.call('core.get_jobs', filters, options):
+            if i['state'] == 'RUNNING':
+                # we're currently becoming master node
+                return i['progress']['description']
+            elif i['progress']['description'] == 'ERROR':
+                # last failover failed
+                return i['progress']['description']
