195dcb67f6e69d8ae9713a4fb3335a4aff91dceb
caleb <yocalebo@gmail.com>
2022-09-09 11:33:27 -0400

    add generate_token function

--- src/middlewared/middlewared/utils/crypto.py
+++ src/middlewared/middlewared/utils/crypto.py
@@ -13,3 +13,15 @@ def generate_string(string_size=8, punctuation_chars=False):
         initial_string += punctuation
 
     return ''.join(secrets.choice(initial_string) for i in range(string_size))
+
+
+def generate_token(size, url_safe=False):
+    """
+    Generate a cryptographically secure token of `size` in bytes returned in hex format.
+
+    `url_safe` when True, returns the token using url safe characters only.
+    """
+    if url_safe:
+        return secrets.token_urlsafe(size)
+    else:
+        return secrets.token_hex(size)
