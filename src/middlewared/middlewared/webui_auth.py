5e6b30b5dc873c8741df47f5ec5c6b445d78e78e
caleb <yocalebo@gmail.com>
2022-09-12 09:54:47 -0400

    fix crash in webui_auth::addr_in_allowlist

--- src/middlewared/middlewared/webui_auth.py
+++ src/middlewared/middlewared/webui_auth.py
@@ -4,13 +4,27 @@ from aiohttp import web
 
 
 def addr_in_allowlist(remote_addr, allowlist):
-    remote_addr = ip_address(remote_addr)
-    for allowed in allowlist:
-        allowed = ip_network(allowed)
-        if remote_addr == allowed or remote_addr in allowed:
-            return True
+    valid = False
+    try:
+        remote_addr = ip_address(remote_addr)
+    except Exception:
+        # invalid/malformed IP so play it safe and
+        # return False
+        valid = False
+    else:
+        for allowed in allowlist:
+            try:
+                allowed = ip_network(allowed)
+            except Exception:
+                # invalid/malformed network so play it safe
+                valid = False
+                break
+            else:
+                if remote_addr == allowed or remote_addr in allowed:
+                    valid = True
+                    break
 
-    return False
+    return valid
 
 
 class WebUIAuth(object):
