a7cb18490b29530c71c7c59b07890e6937454435
caleb <yocalebo@gmail.com>
2022-09-14 08:36:06 -0400

    fix failover.status.get_local

--- src/middlewared/middlewared/plugins/failover_/status.py
+++ src/middlewared/middlewared/plugins/failover_/status.py
@@ -13,6 +13,34 @@ class DetectFailoverStatusService(Service):
             # no HA license so nothing else matters
             return 'SINGLE'
 
+        master_ifaces = backup_ifaces = vips_configured = 0
+        interfaces = await self.middleware.call('interface.query')
+        for iface in filter(lambda x: x['state']['vrrp_config'], interfaces):
+            vips_configured += len(iface.get('failover_virtual_aliases', []))
+            if iface['state']['vrrp_config']:
+                for ip in iface['state']['vrrp_config']:
+                    if ip['state'] == 'MASTER':
+                        master_ifaces += 1
+                    else:
+                        backup_ifaces += 1
+
+        if not vips_configured:
+            # We have a license but we don't have a single interface that has been
+            # configured with a VIP. It's safe to assume that this is a system that
+            # has just been licensed for HA. To allow the user to login to the system
+            # for initial HA configuration, we need to return SINGLE.
+            # `failover.disabled.reasons` will reports lots of issues on why HA isn't
+            # "healthy" in this scenario
+            return 'SINGLE'
+        elif master_ifaces and not backup_ifaces:
+            # all interfaces that are configured for HA are master, safe to assume
+            # this _should_ be the master node
+            return 'MASTER'
+        elif backup_ifaces and not master_ifaces:
+            # all interfaces that are configured for HA are backup, safe to assume
+            # this _should_ be the backup node
+            return 'BACKUP'
+
         fenced_running = (await self.middleware.call('failover.fenced.run_info'))['running']
         only_boot_pool = len((await self.middleware.call('zfs.pool.query_imported_fast'))) <= 1
         if not fenced_running:
@@ -30,28 +58,6 @@ class DetectFailoverStatusService(Service):
             # we have at least 1 zpool imported, fenced is running, and we're licensed
             # safe to assume we're the master node
             return 'MASTER'
-
-        master_ifaces = backup_ifaces = 0
-        interfaces = await self.middleware.call('interface.query')
-        for iface in filter(lambda x: x['state']['vrrp_config'], interfaces):
-            for ip in iface['state']['vrrp_config']:
-                if ip['state'] == 'MASTER':
-                    master_ifaces += 1
-                else:
-                    backup_ifaces += 1
-
-        if master_ifaces and not backup_ifaces:
-            # all interfaces that are configured for HA are master, safe to assume
-            # this _should_ be the master node. we shouldn't get here on a
-            # healhy HA system because we should be able to determine the status
-            # based on zpools and fenced running
-            return 'MASTER'
-        elif backup_ifaces and not master_ifaces:
-            # all interfaces that are configured for HA are backup, safe to assume
-            # this _should_ be the backup node. we shouldn't get here on a
-            # healhy HA system because we should be able to determine the status
-            # based on zpools and fenced running
-            return 'BACKUP'
 
         # last ditch effort to determine the status of this node. if there are
         # no failover events occurring locally and we make it this far, the caller
