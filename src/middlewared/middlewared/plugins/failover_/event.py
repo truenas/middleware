70c04a4236fb31e027f935dad5574255a10e5e70
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-09-14 20:41:39 +0200

    NAS-118178 / 22.12 / fix typo in failover_/event.py (by yocalebo) (#9854)
    * fix typo in failover_/event.py
    
    (cherry picked from commit 6869e1c2d517a35664e57248a6be62c9a87a7668)
    
    * more typo fixes
    
    (cherry picked from commit dc52781970b2ab6f6bd637948795b3440ca64d2d)
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/failover_/event.py
+++ src/middlewared/middlewared/plugins/failover_/event.py
@@ -428,8 +428,8 @@ class FailoverEventsService(Service):
         if len(failed) == len(fobj['volumes']):
             for i in failed:
                 logger.error(
-                    'Failed to import volume with name "%s" with guid "%s" '
-                    'with error "%s"', failed['name'], failed['guid'], failed['error'],
+                    'Failed to import volume with name %r with guid %r with error:\n %r',
+                    i['name'], i['guid'], i['error'],
                 )
 
             logger.error('All volumes failed to import!')
@@ -440,8 +440,8 @@ class FailoverEventsService(Service):
         elif len(failed):
             for i in failed:
                 logger.error(
-                    'Failed to import volume with name "%s" with guid "%s" '
-                    'with error "%s"', failed['name'], failed['guid'], failed['error'],
+                    'Failed to import volume with name %r with guid %r with error:\n %r',
+                    i['name'], i['guid'], i['error'],
                 )
                 logger.error(
                     'However, other zpools imported so the failover process continued.'
