dab8891712400bc4ef562cb198b8e5cccdd64b30
themylogin <themylogin@gmail.com>
2022-09-20 14:30:29 +0200

    NAS-115586 / 22.12 / Enable `pool.replace_disk` tests (#8694)
    * Enable `pool.replace_disk` tests
    
    * Skip `test_pool_replace_disk` for HA

--- tests/api2/test_pool_replace_disk.py
+++ tests/api2/test_pool_replace_disk.py
@@ -3,8 +3,11 @@ import pytest
 from middlewared.test.integration.assets.pool import another_pool_topologies, another_pool
 from middlewared.test.integration.utils import call
 
-from auto_config import dev_test
-pytestmark = pytest.mark.skipif(dev_test, reason='Skipping for test development testing')
+from auto_config import dev_test, ha
+pytestmark = [
+    pytest.mark.skipif(dev_test, reason='Skipping for test development testing'),
+    pytest.mark.skipif(ha, reason='Skipping for HA testing'),
+]
 
 
 def disks(topology):
@@ -14,7 +17,6 @@ def disks(topology):
 
 @pytest.mark.parametrize("topology", another_pool_topologies[1:])
 @pytest.mark.parametrize("i", list(range(0, max(topology[0] for topology in another_pool_topologies))))
-@pytest.mark.skip(reason="Global crisis, 8 GB virtual HDDs are temporary out of stock")
 def test_pool_replace_disk(topology, i):
     count = topology[0]
     if i >= count:
