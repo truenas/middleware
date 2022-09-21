48dacfcca5b773c3cfd8f7f46524aaa121b02cb1
caleb <yocalebo@gmail.com>
2022-09-21 13:44:30 -0400

    clean up dead-code

--- src/freenas/usr/local/libexec/freenas-debug/system/system.sh
+++ src/freenas/usr/local/libexec/freenas-debug/system/system.sh
@@ -48,13 +48,8 @@ system_func()
 	ntpq -pwn
 	section_footer
 
-	if is_linux; then
-		section_header "ps -auxwwf"
-		ps -auxwwf
-	else
-		section_header "ps -auxwwd"
-		ps -auxwwd
-	fi
+	section_header "ps -auxwwf"
+	ps -auxwwf
 	section_footer
 
 	section_header "mount"
@@ -65,69 +60,31 @@ system_func()
 	df -T -h
 	section_footer
 
-	if is_linux; then
-		section_header "swapon -s"
-		swapon -s
-		section_footer
+	section_header "swapon -s"
+	swapon -s
+	section_footer
 
-		section_header "lsmod"
-		lsmod
-		section_footer
+	section_header "lsmod"
+	lsmod
+	section_footer
 
-		section_header "dmesg -Tx"
-		dmesg -Tx
-		section_footer
+	section_header "dmesg -Tx"
+	dmesg -Tx
+	section_footer
 
-		section_header "vmstat"
-		vmstat
-		section_footer
+	section_header "vmstat"
+	vmstat
+	section_footer
 
-		section_header "top -SHbi -d1 -n2"
-		top -SHbi -d1 -n2
-		section_footer
-	else
-		section_header "swapinfo -h"
-		swapinfo -h
-		section_footer
+	section_header "top -SHbi -d1 -n2"
+	top -SHbi -d1 -n2
+	section_footer
 
-		section_header "kldstat"
-		kldstat
-		section_footer
-
-		section_header "dmesg -Tx"
-		dmesg -a
-		section_footer
-
-		section_header "vmstat -ia"
-		vmstat -ia
-		section_footer
-
-		section_header "top -SHIwz -d 2"
-		top -SHIwz -d 2
-		section_footer
-
-		section_header "procstat -akk"
-		procstat -akk
-		section_footer
-
-		section_header "vmstat -m"
-		vmstat -m
-		section_footer
-
-		section_header "vmstat -z"
-		vmstat -z
-		section_footer
-
-		section_header "beadm list"
-		beadm list
-		section_footer
-	fi
-
-	section_header "Alert System"
+	section_header "Current Alerts (midclt call alert.list)"
 	midclt call alert.list | jq .
 	section_footer
 
-	section_header "Core files"
+	section_header "Core files (midclt call system.coredumps)"
 	for row in $(midclt call "system.coredumps" | jq -r ".[] | @base64"); do
 		_jq() {
 			echo ${row} | base64 --decode | jq -r ${1}
@@ -142,7 +99,7 @@ system_func()
 	done
 	section_footer
 
-	section_header "Dump configuration"
+	section_header "Dump configuration (midclt call system.general.config and system.advanced.config)"
 	midclt call system.general.config | jq 'del(.ui_certificate.privatekey?, .ui_certificate.signedby.privatekey?, .ui_certificate.issuer.privatekey?)'
 	midclt call system.advanced.config | jq 'del(.sed_user, .sed_passwd)'
 	section_footer
@@ -208,7 +165,7 @@ system_func()
 	sc /data/update.failed
 	section_footer
 
-	section_header "Truecommand connection"
+	section_header "Truecommand connection (midclt call truecommand.connected)"
 	midclt call truecommand.connected | jq 'del(.truecommand_ip,.truecommand_url)'
 	section_footer
 }	
