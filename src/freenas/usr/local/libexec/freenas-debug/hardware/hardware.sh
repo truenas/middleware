38e1424bae6391277887d91644dfd5aef60aeb9b
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2022-09-21 14:26:55 -0400

    NAS-118216 / 22.12 / Clean up system and hardware section in debug and add enclosures (#9887)
    * clean up dead-code
    
    * dont be complicated
    
    * more clean-up
    
    * add enclosure.query to debug information

--- src/freenas/usr/local/libexec/freenas-debug/hardware/hardware.sh
+++ src/freenas/usr/local/libexec/freenas-debug/hardware/hardware.sh
@@ -1,51 +1,16 @@
 #!/bin/sh
-#+
-# Copyright 2011 iXsystems, Inc.
-# All rights reserved
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted providing that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-#####################################################################
-
-
 hardware_opt() { echo h; }
 hardware_help() { echo "Dump Hardware Configuration"; }
 hardware_directory() { echo "Hardware"; }
 hardware_func()
 {
 	section_header "CPU and Memory information"
-
 	echo "CPU architecture: $(uname -m)"
-
 	echo "CPU model: $(lscpu | grep 'Model name' | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//')"
-
 	echo "Number of active CPUs: $(grep -c 'model name' /proc/cpuinfo)"
-
 	echo "Number of CPUs online: $(lscpu -p=online | grep -v "^#" | grep -c "Y")"
-
 	echo "Current average CPU frequency: $(lscpu | grep 'CPU MHz' | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//') MHz"
-
 	echo "Physical Memory: $(getconf -a | grep PAGES | awk 'BEGIN {total = 1} {if (NR == 1 || NR == 3) total *=$NF} END {print total / 1024 / 1024 / 1024" GiB"}')"
-
 	section_footer
 
 	section_header "lspci -vvvD"
@@ -70,5 +35,9 @@ hardware_func()
 
 	section_header "sensors -j"
 	sensors -j
+	section_footer
+
+	section_header "Enclosures (midclt call enclosure.query)"
+	midclt call enclosure.query |jq .
 	section_footer
 }
--- src/freenas/usr/local/libexec/freenas-debug/system/system.sh
+++ src/freenas/usr/local/libexec/freenas-debug/system/system.sh
@@ -48,13 +48,8 @@ system_func()
 	ntpq -pwn
 	section_footer
 
-	if is_linux; then
-		section_header "ps -auxwwf"
-		ps -auxwwf
-	else
-		section_header "ps -auxwwd"
-		ps -auxwwd
-	fi
+	section_header "ps -auxwwf"
+	ps -auxwwf
 	section_footer
 
 	section_header "mount"
@@ -65,69 +60,31 @@ system_func()
 	df -T -h
 	section_footer
 
-	if is_linux; then
-		section_header "swapon -s"
-		swapon -s
-		section_footer
+	section_header "swapon -s"
+	swapon -s
+	section_footer
 
-		section_header "lsmod"
-		lsmod
-		section_footer
+	section_header "lsmod"
+	lsmod
+	section_footer
 
-		section_header "dmesg -Tx"
-		dmesg -Tx
-		section_footer
+	section_header "dmesg -Tx"
+	dmesg -Tx
+	section_footer
 
-		section_header "vmstat"
-		vmstat
-		section_footer
+	section_header "vmstat"
+	vmstat
+	section_footer
 
-		section_header "top -SHbi -d1 -n2"
-		top -SHbi -d1 -n2
-		section_footer
-	else
-		section_header "swapinfo -h"
-		swapinfo -h
-		section_footer
+	section_header "top -SHbi -d1 -n2"
+	top -SHbi -d1 -n2
+	section_footer
 
-		section_header "kldstat"
-		kldstat
-		section_footer
-
-		section_header "dmesg -Tx"
-		dmesg -a
-		section_footer
-
-		section_header "vmstat -ia"
-		vmstat -ia
-		section_footer
-
-		section_header "top -SHIwz -d 2"
-		top -SHIwz -d 2
-		section_footer
-
-		section_header "procstat -akk"
-		procstat -akk
-		section_footer
-
-		section_header "vmstat -m"
-		vmstat -m
-		section_footer
-
-		section_header "vmstat -z"
-		vmstat -z
-		section_footer
-
-		section_header "beadm list"
-		beadm list
-		section_footer
-	fi
-
-	section_header "Alert System"
+	section_header "Current Alerts (midclt call alert.list)"
 	midclt call alert.list | jq .
 	section_footer
 
-	section_header "Core files"
+	section_header "Core files (midclt call system.coredumps)"
 	for row in $(midclt call "system.coredumps" | jq -r ".[] | @base64"); do
 		_jq() {
 			echo ${row} | base64 --decode | jq -r ${1}
@@ -142,7 +99,7 @@ system_func()
 	done
 	section_footer
 
-	section_header "Dump configuration"
+	section_header "Dump configuration (midclt call system.general.config and system.advanced.config)"
 	midclt call system.general.config | jq 'del(.ui_certificate.privatekey?, .ui_certificate.signedby.privatekey?, .ui_certificate.issuer.privatekey?)'
 	midclt call system.advanced.config | jq 'del(.sed_user, .sed_passwd)'
 	section_footer
@@ -164,15 +121,8 @@ system_func()
 	section_footer
 
 	if [ -f /data/license ]; then
-		section_header "License"
-		cat /data/license
-		echo 'checksum'
-		md5 /data/license
-		echo 'Illuminated License'
-		python -c \
-			'from licenselib.license import License; import sys,pprint;\
-			a=License.load(sys.argv[1]); pprint.pprint (a, width=43)' \
-			`cat /data/license`
+		section_header "License Information (midclt call system.license)"
+		midclt call system.license | jq .
 		section_footer
 	fi
 
@@ -208,7 +158,7 @@ system_func()
 	sc /data/update.failed
 	section_footer
 
-	section_header "Truecommand connection"
+	section_header "Truecommand connection (midclt call truecommand.connected)"
 	midclt call truecommand.connected | jq 'del(.truecommand_ip,.truecommand_url)'
 	section_footer
 }	
