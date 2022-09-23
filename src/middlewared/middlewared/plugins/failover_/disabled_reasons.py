a2c4f0c95c81272e3b3ffe33df12f137de45eb7b
caleb <yocalebo@gmail.com>
2022-09-23 10:01:19 -0400

    add {NO/REM}_JOURNAL_SYNC reasons

--- src/middlewared/middlewared/plugins/failover_/disabled_reasons.py
+++ src/middlewared/middlewared/plugins/failover_/disabled_reasons.py
@@ -1,6 +1,8 @@
 from middlewared.schema import accepts, returns, List, Str
 from middlewared.service import Service, throttle, pass_app, no_auth_required, private
 from middlewared.plugins.failover_.utils import throttle_condition
+from middlewared.plugins.failover_.journal import JOURNAL_THREAD
+from middlewared.service import CallError
 
 
 class FailoverDisabledReasonsService(Service):
@@ -31,6 +33,8 @@ class FailoverDisabledReasonsService(Service):
         NO_CRITICAL_INTERFACES - No network interfaces are marked critical for failover.
         NO_FENCED - Zpools are imported but fenced isn't running.
         REM_FAILOVER_ONGOING - Other node is currently processing a failover event.
+        NO_JOURNAL_SYNC - Thread responsible for syncing db transactions not running on this node.
+        REM_NO_JOURNAL_SYNC - Thread responsible for syncing db transactions not running on other node.
         """
         reasons = self.middleware.call_sync('failover.disabled.get_reasons', app)
         if reasons != FailoverDisabledReasonsService.LAST_DISABLED_REASONS:
@@ -42,10 +46,17 @@ class FailoverDisabledReasonsService(Service):
         return list(reasons)
 
     @private
+    def journal_thread_running(self, app):
+        return JOURNAL_THREAD is not None and JOURNAL_THREAD.is_alive()
+
+    @private
     def get_local_reasons(self, app, ifaces, reasons):
         """This method checks the local node to try and determine its failover status."""
         if self.middleware.call_sync('failover.config')['disabled']:
             reasons.add('NO_FAILOVER')
+
+        if not self.journal_thread_running(self, app):
+            reasons.add('NO_JOURNAL_SYNC')
 
         crit_iface = vip = master = False
         for iface in ifaces:
@@ -104,6 +115,19 @@ class FailoverDisabledReasonsService(Service):
             if rv := self.middleware.call_sync('failover.call_remote', 'core.get_jobs', args):
                 if rv[0]['state'] == 'RUNNING':
                     reasons.add('REM_FAILOVER_ONGOING')
+
+            try:
+                rv = self.middleware.call_sync('failover.call_remote', 'failover.disabled.journal_thread_running', app)
+                if not rv:
+                    reasons.add('REM_NO_JOURNAL_SYNC')
+            except CallError as e:
+                if e.errno == CallError.ENOMETHOD:
+                    # the other node is running an old version so it'll fail as expected
+                    # just ignore this error since the other node will eventually be updated
+                    # to the same version as the current node
+                    pass
+                else:
+                    raise
 
             local = self.middleware.call_sync('failover.vip.get_states', ifaces)
             remote = self.middleware.call_sync('failover.call_remote', 'failover.vip.get_states')
