6699a37c0fdb9aae96ab4135e9b05eaa8dfe8d35
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2022-09-26 08:53:05 -0400

    NAS-118285 / 22.12 / add more reasons to failover.disabled.reasons (#9904)
    * split local checks from remote checks
    
    * add REM_FAILOVER_ONGOING reason
    
    * add {NO/REM}_JOURNAL_SYNC reasons
    
    * add new reasons to hactl
    
    * fix cosmetic bug in hactl while im here

--- src/middlewared/middlewared/plugins/failover_/disabled_reasons.py
+++ src/middlewared/middlewared/plugins/failover_/disabled_reasons.py
@@ -1,6 +1,8 @@
 from middlewared.schema import accepts, returns, List, Str
 from middlewared.service import Service, throttle, pass_app, no_auth_required, private
 from middlewared.plugins.failover_.utils import throttle_condition
+from middlewared.plugins.failover_.journal import JOURNAL_THREAD
+from middlewared.service import CallError
 
 
 class FailoverDisabledReasonsService(Service):
@@ -30,6 +32,9 @@ class FailoverDisabledReasonsService(Service):
         MISMATCH_DISKS - The storage controllers do not have the same quantity of disks.
         NO_CRITICAL_INTERFACES - No network interfaces are marked critical for failover.
         NO_FENCED - Zpools are imported but fenced isn't running.
+        REM_FAILOVER_ONGOING - Other node is currently processing a failover event.
+        NO_JOURNAL_SYNC - Thread responsible for syncing db transactions not running on this node.
+        REM_NO_JOURNAL_SYNC - Thread responsible for syncing db transactions not running on other node.
         """
         reasons = self.middleware.call_sync('failover.disabled.get_reasons', app)
         if reasons != FailoverDisabledReasonsService.LAST_DISABLED_REASONS:
@@ -41,13 +46,18 @@ class FailoverDisabledReasonsService(Service):
         return list(reasons)
 
     @private
-    def get_reasons(self, app):
-        reasons = set()
+    def journal_thread_running(self, app):
+        return JOURNAL_THREAD is not None and JOURNAL_THREAD.is_alive()
 
+    @private
+    def get_local_reasons(self, app, ifaces, reasons):
+        """This method checks the local node to try and determine its failover status."""
         if self.middleware.call_sync('failover.config')['disabled']:
             reasons.add('NO_FAILOVER')
 
-        ifaces = self.middleware.call_sync('interface.query')
+        if not self.journal_thread_running(self, app):
+            reasons.add('NO_JOURNAL_SYNC')
+
         crit_iface = vip = master = False
         for iface in ifaces:
             if iface['failover_critical']:
@@ -78,24 +88,46 @@ class FailoverDisabledReasonsService(Service):
                 # we've got interfaces marked as master but we have no zpool(s) imported
                 reasons.add('NO_VOLUME')
 
+    @private
+    def get_remote_reasons(self, app, ifaces, reasons):
+        """This method checks the remote node to try and determine its failover status."""
         try:
             assert self.middleware.call_sync('failover.remote_connected')
-
-            # if the remote node panic's (this happens on failover event if we cant export the
-            # zpool in 4 seconds on freeBSD systems (linux reboots silently by design)
-            # then the p2p interface stays "UP" and the websocket remains open.
-            # At this point, we have to wait for the TCP timeout (60 seconds default).
-            # This means the assert line up above will return `True`.
-            # However, any `call_remote` method will hang because the websocket is still
-            # open but hasn't closed due to the default TCP timeout window. This can be painful
-            # on failover events because it delays the process of restarting services in a timely
-            # manner. To work around this, we place a `timeout` of 5 seconds on the system.ready
-            # call. This essentially bypasses the TCP timeout window.
             if not self.middleware.call_sync('failover.call_remote', 'system.ready', [], {'timeout': 5}):
+                # if the remote node panic's (this happens on failover event if we cant export the
+                # zpool in 4 seconds (linux reboots silently by design) then the p2p interface stays
+                # "UP" and the websocket remains open. At this point, we have to wait for the TCP
+                # timeout (60 seconds default). This means the assert line up above will return `True`.
+                # However, any `call_remote` method will hang because the websocket is still
+                # open but hasn't closed due to the default TCP timeout window. This can be painful
+                # on failover events because it delays the process of restarting services in a timely
+                # manner. To work around this, we place a `timeout` of 5 seconds on the system.ready
+                # call. This essentially bypasses the TCP timeout window.
                 reasons.add('NO_SYSTEM_READY')
 
             if not self.middleware.call_sync('failover.call_remote', 'failover.licensed'):
                 reasons.add('NO_LICENSE')
+
+            args = [
+                [["method", "in", ["failover.events.vrrp_master", "failover.events.vrrp_backup"]]],
+                {"order_by": ["-id"]}
+            ]
+            if rv := self.middleware.call_sync('failover.call_remote', 'core.get_jobs', args):
+                if rv[0]['state'] == 'RUNNING':
+                    reasons.add('REM_FAILOVER_ONGOING')
+
+            try:
+                rv = self.middleware.call_sync('failover.call_remote', 'failover.disabled.journal_thread_running', app)
+                if not rv:
+                    reasons.add('REM_NO_JOURNAL_SYNC')
+            except CallError as e:
+                if e.errno == CallError.ENOMETHOD:
+                    # the other node is running an old version so it'll fail as expected
+                    # just ignore this error since the other node will eventually be updated
+                    # to the same version as the current node
+                    pass
+                else:
+                    raise
 
             local = self.middleware.call_sync('failover.vip.get_states', ifaces)
             remote = self.middleware.call_sync('failover.call_remote', 'failover.vip.get_states')
@@ -107,6 +139,13 @@ class FailoverDisabledReasonsService(Service):
                 reasons.add('MISMATCH_DISKS')
         except Exception:
             reasons.add('NO_PONG')
+
+    @private
+    def get_reasons(self, app):
+        reasons = set()
+        ifaces = self.middleware.call_sync('interface.query')
+        self.get_local_reasons(app, ifaces, reasons)
+        self.get_remote_reasons(app, ifaces, reasons)
 
         return reasons
 
