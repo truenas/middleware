db9b27fa170f3bed23e5d1b5d867476ec0532da1
ericbsd <ericturgeon.bsd@gmail.com>
2022-09-27 14:15:33 -0300

    Adding logs collection verification with system dataset changes
    I also changed the encrypted system dataset to work with the current behavior

--- tests/api2/test_007_systemdataset.py
+++ tests/api2/test_007_systemdataset.py
@@ -3,12 +3,11 @@
 import pytest
 import sys
 import os
-from time import sleep
 from pytest_dependency import depends
 apifolder = os.getcwd()
 sys.path.append(apifolder)
-from functions import POST, GET, PUT, wait_on_job
-from auto_config import ha, dev_test, hostname
+from functions import POST, GET, PUT, SSH_TEST, wait_on_job
+from auto_config import ha, dev_test, hostname, user, password
 # comment pytestmark for development testing with --dev-test
 pytestmark = pytest.mark.skipif(dev_test, reason='Skipping for test development testing')
 
@@ -30,8 +29,19 @@ SECOND_POOL_DISK = [POOL_DISKS[2]]
 SERVICES = []
 
 
+if ha and "virtual_ip" in os.environ:
+    ip = os.environ["virtual_ip"]
+else:
+    from auto_config import ip
+
+
 @pytest.fixture(scope='module')
 def pool_data():
+    return {}
+
+
+@pytest.fixture(scope='module')
+def logs_data():
     return {}
 
 
@@ -43,7 +53,24 @@ def test_01_verify_system_dataset_is_set_to_boot_pool():
     assert results.json()['basename'] == 'boot-pool/.system', results.text
 
 
-def test_02_verify_the_first_pool_created_with_encrypted_root_dataset_system_dataset_stays_on_the_boot_pool(request, pool_data):
+def test_02_get_initial_logs(logs_data):
+    cmd = "cat /var/log/middlewared.log"
+    middlewared_log = SSH_TEST(cmd, user, password, ip)
+    assert middlewared_log['result'] is True, str(middlewared_log)
+    logs_data['middleware_log_1'] = middlewared_log['output'].splitlines()[-1]
+
+    cmd = "journalctl --no-page"
+    journald_log = SSH_TEST(cmd, user, password, ip)
+    assert journald_log['result'] is True, str(journald_log)
+    logs_data['journald_log_1'] = journald_log['output'].splitlines()[-1]
+
+    cmd = "cat /var/log/syslog"
+    syslog = SSH_TEST(cmd, user, password, ip)
+    assert syslog['result'] is True, str(syslog)
+    logs_data['syslog_1'] = syslog['output'].splitlines()[-1]
+
+
+def test_03_verify_the_first_pool_created_with_encrypted_root_dataset_become_the_system_dataset(request, pool_data):
     payload = {
         'name': 'encrypted',
         'encryption': True,
@@ -67,25 +94,54 @@ def test_02_verify_the_first_pool_created_with_encrypted_root_dataset_system_dat
     results = GET("/systemdataset/")
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
-    assert results.json()['pool'] == 'boot-pool', results.text
-    assert results.json()['basename'] == 'boot-pool/.system', results.text
+    assert results.json()['pool'] == 'encrypted', results.text
+    assert results.json()['basename'] == 'encrypted/.system', results.text
 
 
-def test_03_verify_the_system_dataset_cant_move_to_a_pool_with_encrypted_root_dataset(request):
+def test_04_verify_the_system_dataset_can_move_to_a_pool_encrypted_root_dataset(request):
+    results = PUT("/systemdataset/", {'pool': 'boot-pool'})
+    assert results.status_code == 200, results.text
+    assert isinstance(results.json(), int), results.text
+    job_status = wait_on_job(results.json(), 120)
+    assert job_status['state'] == 'SUCCESS', str(job_status['results'])
+
     results = PUT("/systemdataset/", {'pool': 'encrypted'})
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), int), results.text
     job_status = wait_on_job(results.json(), 120)
-    assert job_status['state'] == 'FAILED', str(job_status['results'])
+    assert job_status['state'] == 'SUCCESS', str(job_status['results'])
 
     results = GET("/systemdataset/")
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
-    assert results.json()['pool'] == 'boot-pool', results.text
-    assert results.json()['basename'] == 'boot-pool/.system', results.text
+    assert results.json()['pool'] == 'encrypted', results.text
+    assert results.json()['basename'] == 'encrypted/.system', results.text
 
 
-def test_04_delete_the_encrypted_pool_and_verify_the_system_dataset(request, pool_data):
+def test_05_verify_logs_collection_still_work_after_enrcripted_dataset_is_moved_to_system_dataset(logs_data):
+    cmd = "cat /var/log/middlewared.log"
+    middlewared_log = SSH_TEST(cmd, user, password, ip)
+    assert middlewared_log['result'] is True, str(middlewared_log)
+    logs_data['middleware_log_2'] = middlewared_log['output'].splitlines()[-1]
+    assert logs_data['middleware_log_1'] in middlewared_log['output'], str(middlewared_log['output'])
+    assert logs_data['middleware_log_1'] != logs_data['middleware_log_2']
+
+    cmd = "journalctl --no-page"
+    journald_log = SSH_TEST(cmd, user, password, ip)
+    assert journald_log['result'] is True, str(journald_log)
+    logs_data['journald_log_2'] = journald_log['output'].splitlines()[-1]
+    assert logs_data['journald_log_1'] in journald_log['output'], str(journald_log['output'])
+    assert logs_data['journald_log_1'] != logs_data['journald_log_2']
+
+    cmd = "cat /var/log/syslog"
+    syslog = SSH_TEST(cmd, user, password, ip)
+    assert syslog['result'] is True, str(syslog)
+    logs_data['syslog_2'] = syslog['output'].splitlines()[-1]
+    assert logs_data['syslog_1'] in syslog['output'], str(syslog['output'])
+    assert logs_data['syslog_1'] != logs_data['syslog_2']
+
+
+def test_06_delete_the_encrypted_pool_and_verify_the_system_dataset(request, pool_data):
     payload = {
         'cascade': True,
         'restart_services': True,
@@ -104,8 +160,31 @@ def test_04_delete_the_encrypted_pool_and_verify_the_system_dataset(request, poo
     assert results.json()['basename'] == 'boot-pool/.system', results.text
 
 
+def test_07_verify_logs_collection_still_work_after_enrcripted_dataset_is_deleted_still_work(logs_data):
+    cmd = "cat /var/log/middlewared.log"
+    middlewared_log = SSH_TEST(cmd, user, password, ip)
+    assert middlewared_log['result'] is True, str(middlewared_log)
+    logs_data['middleware_log_3'] = middlewared_log['output'].splitlines()[-1]
+    assert logs_data['middleware_log_2'] in middlewared_log['output'], str(middlewared_log['output'])
+    assert logs_data['middleware_log_2'] != logs_data['middleware_log_3']
+
+    cmd = "journalctl --no-page"
+    journald_log = SSH_TEST(cmd, user, password, ip)
+    assert journald_log['result'] is True, str(journald_log)
+    logs_data['journald_log_3'] = journald_log['output'].splitlines()[-1]
+    assert logs_data['journald_log_2'] in journald_log['output'], str(journald_log['output'])
+    assert logs_data['journald_log_2'] != logs_data['journald_log_3']
+
+    cmd = "cat /var/log/syslog"
+    syslog = SSH_TEST(cmd, user, password, ip)
+    assert syslog['result'] is True, str(syslog)
+    logs_data['syslog_3'] = syslog['output'].splitlines()[-1]
+    assert logs_data['syslog_2'] in syslog['output'], str(syslog['output'])
+    assert logs_data['syslog_2'] != logs_data['syslog_3']
+
+
 @pytest.mark.dependency(name="first_pool")
-def test_05_creating_a_first_pool_and_verify_system_dataset_move_to_the_new_pool(request, pool_data):
+def test_08_creating_a_first_pool_and_verify_system_dataset_move_to_the_new_pool(request, pool_data):
     payload = {
         "name": 'first_pool',
         "encryption": False,
@@ -131,7 +210,7 @@ def test_05_creating_a_first_pool_and_verify_system_dataset_move_to_the_new_pool
 
 
 @pytest.mark.dependency(name="second_pool")
-def test_06_creating_a_second_pool_and_verify_system_dataset_does_not_move_to_the_new_pool(request, pool_data):
+def test_09_creating_a_second_pool_and_verify_system_dataset_does_not_move_to_the_new_pool(request, pool_data):
     depends(request, ["first_pool"])
     payload = {
         "name": 'second_pool',
@@ -157,7 +236,7 @@ def test_06_creating_a_second_pool_and_verify_system_dataset_does_not_move_to_th
     assert results.json()['basename'] == 'first_pool/.system', results.text
 
 
-def test_07_verify_changing_a_system_dataset_is_impossible_while_AD_is_running(request):
+def test_10_verify_changing_a_system_dataset_is_impossible_while_AD_is_running(request):
     depends(request, ["second_pool"])
 
     results = GET("/network/configuration/")
@@ -214,7 +293,24 @@ def test_07_verify_changing_a_system_dataset_is_impossible_while_AD_is_running(r
     assert results.status_code == 200, results.text
 
 
-def test_08_a_system_dataset_can_be_moved_to_another_root_dataset(request):
+def test_12_get_logs_before_moving_the_system_dataset_to_the_second_pool(logs_data):
+    cmd = "cat /var/log/middlewared.log"
+    middlewared_log = SSH_TEST(cmd, user, password, ip)
+    assert middlewared_log['result'] is True, str(middlewared_log)
+    logs_data['middleware_log_4'] = middlewared_log['output'].splitlines()[-1]
+
+    cmd = "journalctl --no-page"
+    journald_log = SSH_TEST(cmd, user, password, ip)
+    assert journald_log['result'] is True, str(journald_log)
+    logs_data['journald_log_4'] = journald_log['output'].splitlines()[-1]
+
+    cmd = "cat /var/log/syslog"
+    syslog = SSH_TEST(cmd, user, password, ip)
+    assert syslog['result'] is True, str(syslog)
+    logs_data['syslog_4'] = syslog['output'].splitlines()[-1]
+
+
+def test_11_a_system_dataset_can_be_moved_to_another_root_dataset(request):
     depends(request, ["second_pool"])
     results = PUT("/systemdataset/", {'pool': 'second_pool'})
     assert results.status_code == 200, results.text
@@ -229,7 +325,30 @@ def test_08_a_system_dataset_can_be_moved_to_another_root_dataset(request):
     assert results.json()['basename'] == 'second_pool/.system', results.text
 
 
-def test_09_system_dataset_can_be_moved_to_another_pool_successfully_when_all_services_running(request):
+def test_12_verify_logs_collection_still_work_after_moving_the_system_dataset_to_the_second_pool(logs_data):
+    cmd = "cat /var/log/middlewared.log"
+    middlewared_log = SSH_TEST(cmd, user, password, ip)
+    assert middlewared_log['result'] is True, str(middlewared_log)
+    logs_data['middleware_log_5'] = middlewared_log['output'].splitlines()[-1]
+    assert logs_data['middleware_log_4'] in middlewared_log['output'], str(middlewared_log['output'])
+    assert logs_data['middleware_log_4'] != logs_data['middleware_log_5']
+
+    cmd = "journalctl --no-page"
+    journald_log = SSH_TEST(cmd, user, password, ip)
+    assert journald_log['result'] is True, str(journald_log)
+    logs_data['journald_log_5'] = journald_log['output'].splitlines()[-1]
+    assert logs_data['journald_log_4'] in journald_log['output'], str(journald_log['output'])
+    assert logs_data['journald_log_4'] != logs_data['journald_log_5']
+
+    cmd = "cat /var/log/syslog"
+    syslog = SSH_TEST(cmd, user, password, ip)
+    assert syslog['result'] is True, str(syslog)
+    logs_data['syslog_5'] = syslog['output'].splitlines()[-1]
+    assert logs_data['syslog_4'] in syslog['output'], str(syslog['output'])
+    assert logs_data['syslog_4'] != logs_data['syslog_5']
+
+
+def test_13_system_dataset_can_be_moved_to_another_pool_successfully_when_all_services_running(request):
     depends(request, ["second_pool"])
     services = {i['service']: i for i in GET('/service').json()}
     services_list = list(services.keys())
@@ -256,7 +375,7 @@ def test_09_system_dataset_can_be_moved_to_another_pool_successfully_when_all_se
             assert results.status_code == 200, results.text
 
 
-def test_10_delete_the_second_pool_and_verify_the_system_dataset(request, pool_data):
+def test_14_delete_the_second_pool_and_verify_the_system_dataset(request, pool_data):
     payload = {
         'cascade': True,
         'restart_services': True,
@@ -275,7 +394,7 @@ def test_10_delete_the_second_pool_and_verify_the_system_dataset(request, pool_d
     assert results.json()['basename'] == 'first_pool/.system', results.text
 
 
-def test_10_delete_the_firs_pool_and_verify_the_system_dataset_moved_to_the_boot_pool(request, pool_data):
+def test_15_delete_the_firs_pool_and_verify_the_system_dataset_moved_to_the_boot_pool(request, pool_data):
     payload = {
         'cascade': True,
         'restart_services': True,
