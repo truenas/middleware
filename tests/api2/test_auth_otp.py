76f8b92ebe219d0dcc2ad8d374f881683d637f95
themylogin <themylogin@gmail.com>
2022-09-28 09:54:38 +0300

    Fix /_upload test

--- tests/api2/test_auth_otp.py
+++ tests/api2/test_auth_otp.py
@@ -1,3 +1,6 @@
+import io
+import json
+
 import pytest
 
 from middlewared.test.integration.utils import call, session, ssh, url
@@ -22,7 +25,18 @@ def test_otp_http_basic_auth(otp_enabled):
 
 def test_otp_http_basic_auth_upload(otp_enabled):
     with session() as s:
-        r = s.get(f"{url()}/_upload/")
+        r = s.post(
+            f"{url()}/_upload/",
+            data={
+                "data": json.dumps({
+                    "method": "filesystem.put",
+                    "params": ["/tmp/upload"],
+                })
+            },
+            files={
+                "file": io.BytesIO(b"test"),
+            },
+        )
         assert r.status_code == 401
         assert r.text == "HTTP Basic Auth is unavailable when OTP is enabled"
 
