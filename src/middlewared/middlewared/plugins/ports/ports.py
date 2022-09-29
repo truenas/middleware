7959361f4ec8d5b7f7ad3560b2d6293a5753500f
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-09-29 22:37:53 +0500

    Add endpoint to retrieve all consumed ports by port delegates

--- src/middlewared/middlewared/plugins/ports/ports.py
+++ src/middlewared/middlewared/plugins/ports/ports.py
@@ -1,10 +1,18 @@
-from middlewared.schema import accepts
 from middlewared.service import Service
 
 
 class PortService(Service):
 
+    DELEGATES = []
+
     class Config:
         private = True
 
+    async def register_attachment_delegate(self, delegate):
+        self.DELEGATES.append(delegate)
 
+    async def get_used_ports(self):
+        ports = []
+        for delegate in self.DELEGATES:
+            ports.extend(await delegate.get_ports())
+        return ports
