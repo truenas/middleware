b41efb324a6b9ce4d251fdbd49418fcb5fa0f106
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-09-29 22:27:55 +0500

    Add a service port delegate for system services

--- src/middlewared/middlewared/common/ports/__init__.py
+++ src/middlewared/middlewared/common/ports/__init__.py
@@ -1,4 +1,25 @@
 class PortDelegate:
 
-    def get_ports(self):
+    def __init__(self, middleware):
+        self.middleware = middleware
+        self.logger = middleware.logger
+
+    async def get_ports(self):
         raise NotImplementedError
+
+
+class ServicePortDelegate(PortDelegate):
+    # service object
+    service_class = NotImplementedError
+    port_fields = NotImplementedError
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.port_field = self.service_class.path_field
+        self.datastore_model = self.service_class._config.datastore
+        self.datastore_prefix = self.service_class._config.datastore_prefix
+        self.namespace = self.service_class._config.namespace
+
+    async def get_ports(self):
+        config = await self.middleware.call(f'{self.namespace}.config')
+        return [config[k] for k in self.port_fields]
