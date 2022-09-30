f61d20be7f95d66a19493ba329f419d5d5138814
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-09-30 12:55:35 +0500

    Add service port delegate for kubernetes/webui/webdav and reporting

--- src/middlewared/middlewared/common/ports/__init__.py
+++ src/middlewared/middlewared/common/ports/__init__.py
@@ -9,6 +9,9 @@ class PortDelegate:
     def __init__(self, middleware):
         self.middleware = middleware
         self.logger = middleware.logger
+        for k in ('name', 'title'):
+            if not getattr(self, k):
+                raise ValueError(f'{k!r} must be specified for port delegate')
 
     async def get_ports(self):
         raise NotImplementedError
@@ -19,13 +22,13 @@ class ServicePortDelegate(PortDelegate):
     port_fields = NotImplementedError
     service = NotImplementedError
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    async def basic_checks(self):
         if self.port_fields is NotImplementedError:
             raise ValueError('Port fields must be set for Service port delegate')
         elif not isinstance(self.port_fields, Iterable):
             raise ValueError('Port fields must be an iterable')
 
     async def get_ports(self):
+        await self.basic_checks()
         config = await self.middleware.call(f'{self.service}.config')
         return [config[k] for k in filter(lambda k: config.get(k), self.port_fields)]
---
+++ src/middlewared/middlewared/plugins/webdav_/attachments.py
@@ -0,0 +1,13 @@
+from middlewared.common.ports import ServicePortDelegate
+
+
+class WebdavServicePortDelegate(ServicePortDelegate):
+
+    name = 'webdav'
+    port_fields = ['tcpport', 'tcpportssl']
+    service = 'webdav'
+    title = 'Webdav Service'
+
+
+async def setup(middleware):
+    await middleware.call('port.register_attachment_delegate', WebdavServicePortDelegate(middleware))
