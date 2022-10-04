a600fecd326f6ebf526ee823bd1c7928f7b52eca
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-04 19:49:10 +0500

    Add service port delegate for smb/snmp and gluster plugins

---
+++ src/middlewared/middlewared/plugins/gluster_linux/attachments.py
@@ -0,0 +1,15 @@
+from middlewared.common.ports import ServicePortDelegate
+
+
+class GlusterServicePortDelegate(ServicePortDelegate):
+
+    name = 'gluster'
+    namespace = 'gluster.fuse'
+    title = 'Gluster Service'
+
+    async def get_ports(self):
+        return [24007, 24008]
+
+
+async def setup(middleware):
+    await middleware.call('port.register_attachment_delegate', GlusterServicePortDelegate(middleware))
---
+++ src/middlewared/middlewared/plugins/smb_/attachments.py
@@ -0,0 +1,15 @@
+from middlewared.common.ports import ServicePortDelegate
+
+
+class SMBServicePortDelegate(ServicePortDelegate):
+
+    name = 'smb'
+    namespace = 'smb'
+    title = 'SMB Service'
+
+    async def get_ports(self):
+        return [137, 138, 139, 445]
+
+
+async def setup(middleware):
+    await middleware.call('port.register_attachment_delegate', SMBServicePortDelegate(middleware))
