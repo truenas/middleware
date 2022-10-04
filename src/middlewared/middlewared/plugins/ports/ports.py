80bff926b0368e914a7dee3f799e268665bb710d
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-04 20:01:50 +0500

    Remove redundant used ports method

--- src/middlewared/middlewared/plugins/ports/ports.py
+++ src/middlewared/middlewared/plugins/ports/ports.py
@@ -16,14 +16,7 @@ class PortService(Service):
     async def register_attachment_delegate(self, delegate):
         self.DELEGATES[delegate.namespace] = delegate
 
-    async def get_used_ports(self):
-        ports = []
-        for delegate in self.DELEGATES:
-            ports.extend(await delegate.get_ports())
-        return ports
-
     async def get_in_use(self):
-        # TODO: Remove either this or the above one probably
         ports = copy.deepcopy(self.SYSTEM_USED_PORTS)
         for delegate in self.DELEGATES.values():
             used_ports = await delegate.get_ports()
