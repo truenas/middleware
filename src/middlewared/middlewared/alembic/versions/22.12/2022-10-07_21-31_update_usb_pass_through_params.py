a3d22091940199d10bf013190842f13e19d9ab3d
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-09 13:14:16 +0500

    Add migration for allowing product/vendor based usb passthrough

---
+++ src/middlewared/middlewared/alembic/versions/22.12/2022-10-07_21-31_update_usb_pass_through_params.py
@@ -0,0 +1,32 @@
+"""update usb pass through params
+
+Revision ID: d388b0e9a50d
+Revises: 7a74a8933a30
+Create Date: 2022-10-07 21:31:21.541782+00:00
+
+"""
+from alembic import op
+import json
+
+revision = 'd388b0e9a50d'
+down_revision = '7a74a8933a30'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    devices = {
+        row['id']: json.loads(row['attributes'])
+        for row in map(dict, conn.execute("SELECT * FROM vm_device WHERE dtype = 'USB'").fetchall())
+    }
+
+    for device_id, device_attrs in devices.items():
+        device_attrs['usb'] = None
+        conn.execute('UPDATE vm_device SET attributes = ? WHERE id = ?', (
+            json.dumps(device_attrs), device_id
+        ))
+
+
+def downgrade():
+    pass
