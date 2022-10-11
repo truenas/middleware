e4b412abfa563793c8045cc89eed40cc2f9e436c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-11 14:03:32 +0500

    Restore old configuration after performing valid test

--- tests/api2/test_port_delegates.py
+++ tests/api2/test_port_delegates.py
@@ -10,10 +10,10 @@ from middlewared.test.integration.utils import call, ValidationErrors
 
 
 PAYLOAD = (
-    ('s3.update', ['bindport', 'console_bindport'], {'access_key': '12345678', 'secret_key': '123456789'}),
-    ('ftp.update', ['port'], {}),
-    ('webdav.update', ['tcpport', 'tcpportssl'], {}),
-    ('rsyncd.update', ['port'], {}),
+    ('s3.config', 's3.update', ['bindport', 'console_bindport'], {'access_key': '12345678', 'secret_key': '123456789'}),
+    ('ftp.config', 'ftp.update', ['port'], {}),
+    ('webdav.config', 'webdav.update', ['tcpport', 'tcpportssl'], {}),
+    ('rsyncd.config', 'rsyncd.update', ['port'], {}),
 )
 
 
@@ -22,7 +22,7 @@ def test_port_delegate_validation_with_invalid_ports():
     for entry in call('port.get_in_use'):
         in_use_ports.extend(filter(lambda i: i > 1024, entry['ports']))
 
-    for method, keys, payload in PAYLOAD:
+    for config_method, method, keys, payload in PAYLOAD:
         validation_error = None
         for key in keys:
             payload[key] = in_use_ports[random.randint(0, len(in_use_ports) - 1)]
@@ -42,13 +42,18 @@ def test_port_delegate_validation_with_valid_ports():
     for entry in call('port.get_in_use'):
         in_use_ports.extend(entry['ports'])
 
-    for method, keys, payload in PAYLOAD:
+    for config_method, method, keys, payload in PAYLOAD:
         validation_error = None
+        old_config = call(config_method)
+        to_restore_config = {}
         for key in keys:
-            payload[key] = next(i for i in range(random.randint(1025, 20000), 60000))
+            payload[key] = next(i for i in range(random.randint(1025, 50000), 60000) if i not in in_use_ports)
+            to_restore_config[key] = old_config[key]
         try:
             call(method, payload, client_args={'py_exceptions': False})
         except ValidationErrors as ve:
             validation_error = ve
+        else:
+            call(method, to_restore_config)
 
         assert validation_error is None, f'No validation exception expected: {validation_error}'
