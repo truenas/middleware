c586b9d2ffd063e9a94993f48511138536aca6c3
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-11 13:50:22 +0500

    Add initial port delegate validation tests

---
+++ tests/api2/test_port_delegates.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import sys
+
+apifolder = os.getcwd()
+sys.path.append(apifolder)
+from middlewared.test.integration.utils import call, ValidationErrors
+
+
+def test_port_delegate_validation_with_invalid_ports():
+    in_use_ports = []
+    for entry in call('port.get_in_use'):
+        in_use_ports.extend(filter(lambda i: i > 1024, entry['ports']))
+
+    for method, keys, payload in (
+        ('s3.update', ['bindport', 'console_bindport'], {'access_key': '12345678', 'secret_key': '123456789'}),
+        ('ftp.update', ['port'], {}),
+        ('webdav.update', ['tcpport', 'tcpportssl'], {}),
+        ('rsyncd.update', ['port'], {}),
+    ):
+        validation_error = None
+        for key in keys:
+            payload[key] = in_use_ports[random.randint(0, len(in_use_ports) - 1)]
+        try:
+            call(method, payload, client_args={'py_exceptions': False})
+        except ValidationErrors as ve:
+            validation_error = ve
+
+        assert validation_error is not None, 'Port validation exception expected'
+        assert any(
+            'The port is being used by' in error.errmsg for error in validation_error.errors
+        ) is True, validation_error
