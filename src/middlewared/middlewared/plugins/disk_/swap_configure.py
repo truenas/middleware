4edee8fecc1f1c05ab8e69f0a469ff724bbfa57b
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-11 23:10:52 +0500

    Do not configure swap for ha capable devices

--- src/middlewared/middlewared/plugins/disk_/swap_configure.py
+++ src/middlewared/middlewared/plugins/disk_/swap_configure.py
@@ -23,7 +23,10 @@ class DiskService(Service):
         swap_redundancy = await self.middleware.call('disk.swap_redundancy')
         used_partitions_in_mirror = set()
         create_swap_devices = {}
-        disks = await self.middleware.call('pool.get_disks')
+        if await self.middleware.call('system.is_ha_capable'):
+            disks = []
+        else:
+            disks = await self.middleware.call('pool.get_disks')
         disks.extend(await self.middleware.call('boot.get_disks'))
         existing_swap_devices = {'mirrors': [], 'partitions': []}
         mirrors = await self.middleware.call('disk.get_swap_mirrors')
