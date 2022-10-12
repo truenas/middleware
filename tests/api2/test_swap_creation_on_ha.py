4ab895a758ce1b4c6fb51892cee4805581653dda
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-12 18:50:08 +0500

    Add test to ensure pool disk is not being used for swap in HA

---
+++ tests/api2/test_swap_creation_on_ha.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+apifolder = os.getcwd()
+sys.path.append(apifolder)
+from middlewared.test.integration.utils import another_pool, call
+from auto_config import ha
+
+
+if ha:
+    def test_swap_creation_on_ha(request):
+        with another_pool():
+            swap_disks = [
+                provider['disk']
+                for swap_mirror in call('disk.get_swap_mirrors') for provider in swap_mirror['providers']
+            ]
+            pool_disks = call('pool.get_disks')
+            assert all(disk not in swap_disks for disk in pool_disks) is True, 'Pool disks ' \
+                                                                               f'({", ".join(pool_disks)!r}) should ' \
+                                                                               'not be used for swap ( swap disks: ' \
+                                                                               f'{", ".join(swap_disks)} )'
