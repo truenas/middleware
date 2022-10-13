3b8d723776b27281d58a6785cc5ab7aedf8b4ff5
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-13 15:24:37 +0500

    Use pytest.mark.parametrize for testing different port delegate validations

--- tests/api2/test_port_delegates.py
+++ tests/api2/test_port_delegates.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import pytest
 import sys
 
 apifolder = os.getcwd()
@@ -18,50 +19,50 @@ PAYLOAD = (
 )
 
 
-def test_port_delegate_validation_with_invalid_ports():
+@pytest.mark.parametrize('config_method,method,keys,payload', PAYLOAD)
+def test_port_delegate_validation_with_invalid_ports(config_method, method, keys, payload):
     in_use_ports = []
     for entry in call('port.get_in_use'):
         in_use_ports.extend(filter(lambda i: i > 1024, entry['ports']))
 
     assert in_use_ports != [], 'No in use ports retrieved'
 
-    for config_method, method, keys, payload in PAYLOAD:
-        validation_error = None
-        for index, key in enumerate(keys):
-            payload[key] = in_use_ports[index] if len(in_use_ports) > index else in_use_ports[0]
-        try:
-            call(method, payload)
-        except ValidationErrors as ve:
-            validation_error = ve
+    validation_error = None
+    for index, key in enumerate(keys):
+        payload[key] = in_use_ports[index] if len(in_use_ports) > index else in_use_ports[0]
+    try:
+        call(method, payload)
+    except ValidationErrors as ve:
+        validation_error = ve
 
-        assert validation_error is not None, 'Port validation exception expected'
-        assert any(
-            'The port is being used by' in error.errmsg for error in validation_error.errors
-        ) is True, validation_error
+    assert validation_error is not None, 'Port validation exception expected'
+    assert any(
+        'The port is being used by' in error.errmsg for error in validation_error.errors
+    ) is True, validation_error
 
 
-def test_port_delegate_validation_with_valid_ports():
+@pytest.mark.parametrize('config_method,method,keys,payload', PAYLOAD)
+def test_port_delegate_validation_with_valid_ports(config_method, method, keys, payload):
     in_use_ports = []
     for entry in call('port.get_in_use'):
         in_use_ports.extend(entry['ports'])
 
     assert in_use_ports != [], 'No in use ports retrieved'
 
-    for config_method, method, keys, payload in PAYLOAD:
-        validation_error = None
-        old_config = call(config_method)
-        to_restore_config = {}
-        used_ports = []
-        for key in keys:
-            port = next(i for i in range(20000, 60000) if i not in in_use_ports and i not in used_ports)
-            payload[key] = port
-            used_ports.append(port)
-            to_restore_config[key] = old_config[key]
-        try:
-            call(method, payload)
-        except ValidationErrors as ve:
-            validation_error = ve
-        else:
-            call(method, to_restore_config)
+    validation_error = None
+    old_config = call(config_method)
+    to_restore_config = {}
+    used_ports = []
+    for key in keys:
+        port = next(i for i in range(20000, 60000) if i not in in_use_ports and i not in used_ports)
+        payload[key] = port
+        used_ports.append(port)
+        to_restore_config[key] = old_config[key]
+    try:
+        call(method, payload)
+    except ValidationErrors as ve:
+        validation_error = ve
+    else:
+        call(method, to_restore_config)
 
-        assert validation_error is None, f'No validation exception expected: {validation_error}'
+    assert validation_error is None, f'No validation exception expected: {validation_error}'
