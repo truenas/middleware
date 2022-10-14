60a2c6ad5d21ad14472e927674a86c453d2e9510
themylogin <themylogin@gmail.com>
2022-10-14 10:33:03 +0200

    Fix VMware snapshot delete test

--- src/middlewared/middlewared/plugins/vmware_/snapshot_delete.py
+++ src/middlewared/middlewared/plugins/vmware_/snapshot_delete.py
@@ -39,27 +39,29 @@ class VMWareService(Service):
             "datastore.query",
             "storage.vmwarependingsnapshotdelete",
         ):
+            deleted = False
             try:
                 si = await self.middleware.call("vmware.connect", pending_snapshot_delete["vmware"])
                 await self.middleware.call("vmware.delete_vmware_login_failed_alert", pending_snapshot_delete["vmware"])
             except Exception as e:
                 await self.middleware.call("vmware.alert_vmware_login_failed", pending_snapshot_delete["vmware"], e)
-                continue
+            else:
+                try:
+                    for vm in await self.middleware.call("vmware.find_vms_by_uuid", si,
+                                                         pending_snapshot_delete["vm_uuid"]):
+                        try:
+                            await self.middleware.call(
+                                "vmware.delete_snapshot",
+                                vm,
+                                pending_snapshot_delete["snapshot_name"],
+                            )
+                            deleted = True
+                        except Exception:
+                            pass
+                except Exception:
+                    pass
 
-            deleted = False
-            try:
-                for vm in await self.middleware.call("vmware.find_vms_by_uuid", si, pending_snapshot_delete["vm_uuid"]):
-                    try:
-                        await self.middleware.call(
-                            "vmware.delete_snapshot",
-                            vm,
-                            pending_snapshot_delete["snapshot_name"],
-                        )
-                        deleted = True
-                    except Exception:
-                        pass
-            except Exception:
-                pass
+                await self.middleware.call("vmware.disconnect", si)
 
             if deleted or datetime.utcnow() - pending_snapshot_delete["datetime"] > PENDING_SNAPSHOT_DELETE_LIFETIME:
                 await self.middleware.call(
@@ -67,5 +69,3 @@ class VMWareService(Service):
                     "storage.vmwarependingsnapshotdelete",
                     pending_snapshot_delete["id"],
                 )
-
-            await self.middleware.call("vmware.disconnect", si)
