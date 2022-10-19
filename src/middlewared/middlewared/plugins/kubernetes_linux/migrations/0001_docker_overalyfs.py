f7d4b38c44dc112dac786a38ed3194abd14d9fb3
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-19 20:57:58 +0500

    Safely retrieve docker dataset

--- src/middlewared/middlewared/plugins/kubernetes_linux/migrations/0001_docker_overalyfs.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/migrations/0001_docker_overalyfs.py
@@ -11,7 +11,7 @@ def migrate(middleware):
     k8s_config = middleware.call_sync('kubernetes.config')
     docker_ds = os.path.join(k8s_config['dataset'], 'docker')
     if middleware.call_sync(
-        'zfs.dataset.get_instance', docker_ds, {
+        'zfs.dataset.query', [['id', '=', docker_ds]], {
             'extra': {'retrieve_children': False, 'retrieve_properties': False}
         }
     ):
