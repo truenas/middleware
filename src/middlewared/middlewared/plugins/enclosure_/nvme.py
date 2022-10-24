4b16d7456fa6174074f1f19049581b4a4824f18b
themylogin <themylogin@gmail.com>
2022-10-24 16:16:05 +0200

    Map fake NVMe enclosures to actual block devices so that `disk.query` can match enclosures

--- src/middlewared/middlewared/plugins/enclosure_/nvme.py
+++ src/middlewared/middlewared/plugins/enclosure_/nvme.py
@@ -7,7 +7,6 @@ from middlewared.service import Service, private
 
 
 class EnclosureService(Service):
-    RE_NVME = re.compile(r"^nvme[0-9]+$")
     RE_SLOT = re.compile(r"^0-([0-9]+)$")
 
     @private
@@ -75,10 +74,10 @@ class EnclosureService(Service):
                 pass
             else:
                 for child in physical_node.children:
-                    if not self.RE_NVME.fullmatch(child.sys_name):
+                    if child.properties.get("SUBSYSTEM") != "block":
                         continue
 
-                    if (slot := addresses_to_slots.get(child.parent.sys_name.split(".")[0])) is None:
+                    if (slot := addresses_to_slots.get(child.parent.parent.sys_name.split(".")[0])) is None:
                         continue
 
                     if not (m := re.match(self.RE_SLOT, slot)):
