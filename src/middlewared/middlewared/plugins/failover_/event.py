ef854a0cee076c6952f1c56464811d246b8cf308
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-10-28 17:06:47 +0500

    Do not wait for initialization of vms

--- src/middlewared/middlewared/plugins/failover_/event.py
+++ src/middlewared/middlewared/plugins/failover_/event.py
@@ -526,12 +526,15 @@ class FailoverEventsService(Service):
         return self.FAILOVER_RESULT
 
     async def start_apps_vms(self):
+        async def start_vms():
+            await self.middleware.call('vm.initialize_vms')
+            await self.middleware.call('vm.start_on_boot')
+
         if await self.middleware.call('vm.license_active'):
             # start any VMs (this will log errors if the vm(s) fail to start)
             # Initialize VMs first to make sure system has relevant
             # objects for each VM initialized
-            await self.middleware.call('vm.initialize_vms')
-            asyncio.ensure_future(self.middleware.call('vm.start_on_boot'))
+            asyncio.ensure_future(start_vms())
 
         if await self.middleware.call('kubernetes.license_active') and (
             await self.middleware.call('kubernetes.config')
