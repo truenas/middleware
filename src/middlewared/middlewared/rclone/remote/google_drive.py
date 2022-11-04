41565c8606ff5a452644349424a566bc4ba84bd6
themylogin <themylogin@gmail.com>
2022-11-04 10:33:31 +0100

    Google Drive `acknowledge_abuse`

--- src/middlewared/middlewared/rclone/remote/google_drive.py
+++ src/middlewared/middlewared/rclone/remote/google_drive.py
@@ -1,5 +1,7 @@
+import textwrap
+
 from middlewared.rclone.base import BaseRcloneRemote
-from middlewared.schema import Str
+from middlewared.schema import Bool, Str
 
 
 class GoogleDriveRcloneRemote(BaseRcloneRemote):
@@ -18,6 +20,16 @@ class GoogleDriveRcloneRemote(BaseRcloneRemote):
     ]
     credentials_oauth = True
     refresh_credentials = ["token"]
+
+    task_schema = [
+        Bool("acknowledge_abuse",
+             title="Allow files which return cannotDownloadAbusiveFile to be downloaded.",
+             description=textwrap.dedent("""\
+                If downloading a file returns the error "This file has been identified as malware or spam and cannot be
+                downloaded" with the error code "cannotDownloadAbusiveFile" then enable this flag to indicate you
+                acknowledge the risks of downloading the file and TrueNAS will download it anyway.
+        """), default=False),
+    ]
 
     async def get_credentials_extra(self, credentials):
         if credentials["attributes"].get("team_drive"):
