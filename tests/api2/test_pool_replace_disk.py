b34708b7fc21b133f4a8051993a0ce92d089efdc
themylogin <themylogin@gmail.com>
2022-11-04 12:49:37 +0100

    Shrink swap when formatting a disk in `pool.replace` to fit a data partiton of required size

--- tests/api2/test_pool_replace_disk.py
+++ tests/api2/test_pool_replace_disk.py
@@ -1,7 +1,7 @@
 import pytest
 
 from time import sleep
-from middlewared.test.integration.assets.pool import another_pool_topologies, another_pool
+from middlewared.test.integration.assets.pool import mirror_topology, another_pool_topologies, another_pool
 from middlewared.test.integration.utils import call
 
 from auto_config import dev_test, ha
@@ -39,11 +39,10 @@ def test_pool_replace_disk(topology, i):
             "force": True,
         }, job=True)
 
-        # Sometimes the VM is to slow so look 5 times with 1 second in between
+        # Sometimes the VM is slow so look 5 times with 1 second in between
         for _ in range(5):
             pool = call("pool.get_instance", pool["id"])
             if len(disks(pool["topology"])) == count:
-                assert len(disks(pool["topology"])) == count
                 break
             sleep(1)
 
@@ -52,3 +51,27 @@ def test_pool_replace_disk(topology, i):
 
         assert call("disk.get_instance", new_disk["identifier"], {"extra": {"pools": True}})["pool"] == pool["name"]
         assert call("disk.get_instance", to_replace_disk["identifier"], {"extra": {"pools": True}})["pool"] is None
+
+
+def test_pool_replace_disk_with_larger_swap():
+    unused = call("disk.get_unused")
+    if len(unused) < 3:
+        raise RuntimeError(f"At least 3 unused disks required to run this test")
+
+    test_disks = unused[:3]
+
+    sizes = {disk["name"]: disk["size"] for disk in test_disks}
+    assert len(set(sizes.values())) == 1, sizes
+
+    with another_pool(topology=mirror_topology) as pool:
+        to_replace_vdev = disks(pool["topology"])[0]
+
+        call("system.advanced.update", {"swapondrive": 4})
+        try:
+            call("pool.replace", pool["id"], {
+                "label": to_replace_vdev["guid"],
+                "disk": test_disks[2]["identifier"],
+                "force": True,
+            }, job=True)
+        finally:
+            call("system.advanced.update", {"swapondrive": 2})
