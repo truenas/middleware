f084bd99a3c698019cd0dab0d02369016c5da4d1
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-09 16:10:23 +0500

    Add crud methods for core api

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/core_api.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/core_api.py
@@ -1,4 +1,5 @@
 from .client import K8sClientBase
+from .utils import UPDATE_HEADERS
 
 
 class CoreAPI(K8sClientBase):
@@ -6,5 +7,23 @@ class CoreAPI(K8sClientBase):
     NAMESPACE = '/api/v1/namespaces'
 
     @classmethod
-    async def query(cls):
-        pass
+    async def query(cls, *args, **kwargs):
+        return await cls.call(cls.uri(parameters=kwargs), mode='get')
+
+    @classmethod
+    async def create(cls, data, **kwargs):
+        return await cls.call(cls.uri(
+            namespace=kwargs.pop('namespace', None), parameters=kwargs,
+        ), body=data, mode='post')
+
+    @classmethod
+    async def update(cls, name, data, **kwargs):
+        return await cls.call(cls.uri(
+            namespace=kwargs.pop('namespace', None), parameters=kwargs, object_name=name,
+        ), body=data, mode='patch', headers=UPDATE_HEADERS)
+
+    @classmethod
+    async def delete(cls, name, **kwargs):
+        return await cls.call(cls.uri(
+            object_name=name, namespace=kwargs.pop('namespace', None), parameters=kwargs,
+        ), mode='delete')
--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/utils.py
@@ -1,1 +1,6 @@
 from middlewared.plugins.kubernetes_linux.utils import KUBECONFIG_FILE  # noqa
+
+
+UPDATE_HEADERS = {
+    'Content-Type': 'application/merge-patch+json',
+}
