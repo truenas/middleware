5493fb75aa4ee1e7e3249b39dd2f042522bca201
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-09 16:23:48 +0500

    Complete coreapi implementation

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/core_api.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/core_api.py
@@ -8,7 +8,7 @@ class CoreAPI(K8sClientBase):
 
     @classmethod
     async def query(cls, *args, **kwargs):
-        return await cls.call(cls.uri(parameters=kwargs), mode='get')
+        return await cls.call(cls.uri(namespace=kwargs.pop('namespace', None), parameters=kwargs), mode='get')
 
     @classmethod
     async def create(cls, data, **kwargs):
@@ -27,3 +27,61 @@ class CoreAPI(K8sClientBase):
         return await cls.call(cls.uri(
             object_name=name, namespace=kwargs.pop('namespace', None), parameters=kwargs,
         ), mode='delete')
+
+
+class Namespace(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/namespaces'
+
+
+class Node(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/nodes'
+
+
+class Service(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/services'
+    OBJECT_TYPE = 'services'
+
+
+class Pod(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/pods'
+    OBJECT_TYPE = 'pods'
+
+
+class Event(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/events'
+    OBJECT_TYPE = 'events'
+
+
+class Secret(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/secrets'
+    OBJECT_TYPE = 'secrets'
+
+
+class PersistentVolume(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/persistentvolumes'
+    OBJECT_TYPE = 'persistentvolumes'
+
+
+class PersistentVolumeClaim(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/persistentvolumeclaims'
+    OBJECT_TYPE = 'persistentvolumeclaims'
+
+
+class Configmap(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/configmaps'
+    OBJECT_TYPE = 'configmaps'
+
+
+class ServicesAccount(CoreAPI):
+
+    OBJECT_ENDPOINT = '/api/v1/serviceaccounts'
+    OBJECT_TYPE = 'serviceaccounts'
