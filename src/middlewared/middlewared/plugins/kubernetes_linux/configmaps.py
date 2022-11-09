0bdcc1829ea98364179b9e6708254d0946bcc1fc
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-09 20:15:51 +0500

    Use newer configmap api for configmap usages

--- src/middlewared/middlewared/plugins/kubernetes_linux/configmaps.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/configmaps.py
@@ -1,7 +1,7 @@
 from middlewared.service import CRUDService, filterable
 from middlewared.utils import filter_list
 
-from .k8s import api_client
+from .k8s_new import Configmap
 
 
 class KubernetesSecretService(CRUDService):
@@ -15,8 +15,4 @@ class KubernetesSecretService(CRUDService):
         options = options or {}
         label_selector = options.get('extra', {}).get('label_selector')
         kwargs = {k: v for k, v in [('label_selector', label_selector)] if v}
-        async with api_client() as (api, context):
-            return filter_list(
-                [d.to_dict() for d in (await context['core_api'].list_config_map_for_all_namespaces(**kwargs)).items],
-                filters, options
-            )
+        return filter_list((await Configmap.query(**kwargs))['items'], filters, options)
