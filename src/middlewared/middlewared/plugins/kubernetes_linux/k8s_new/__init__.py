8e7110afb57b327078247bf84582cddae07bb1f3
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 20:12:04 +0500

    Add class for retrieving custom objects in k8s api

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/__init__.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/__init__.py
@@ -1,5 +1,6 @@
 from .app_api import * # noqa
 from .batch_api import * # noqa
 from .core_api import * # noqa
+from .custom_object_api import * # noqa
 from .exceptions import * # noqa
 from .storage_api import * # noqa
