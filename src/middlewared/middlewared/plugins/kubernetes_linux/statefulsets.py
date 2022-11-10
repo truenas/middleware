a033bb957d33e5b19376c7d08c52eaa5b56c3c89
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 10:48:36 +0500

    Update statefulset class to use newer k8s base resource

--- src/middlewared/middlewared/plugins/kubernetes_linux/statefulsets.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/statefulsets.py
@@ -1,15 +1,11 @@
-from middlewared.service import CRUDService, filterable
-from middlewared.utils import filter_list
-
+from .k8s_base_resources import KubernetesBaseResource
 from .k8s_new import StatefulSet
 
 
-class KubernetesStatefulsetService(CRUDService):
+class KubernetesStatefulsetService(KubernetesBaseResource):
+
+    KUBERNETES_RESOURCE = StatefulSet
 
     class Config:
         namespace = 'k8s.statefulset'
         private = True
-
-    @filterable
-    async def query(self, filters, options):
-        return filter_list((await StatefulSet.query())['items'], filters, options)
