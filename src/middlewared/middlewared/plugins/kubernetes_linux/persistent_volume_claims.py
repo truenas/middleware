889c5bff12794d8307377df751df95f103e5505c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 10:45:24 +0500

    Update pvc class to use newer k8s base resource

--- src/middlewared/middlewared/plugins/kubernetes_linux/persistent_volume_claims.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/persistent_volume_claims.py
@@ -1,28 +1,12 @@
-from middlewared.schema import accepts, Dict, Str
-from middlewared.service import CRUDService, filterable
-from middlewared.utils import filter_list
-
+from .k8s_base_resources import KubernetesBaseResource
 from .k8s_new import PersistentVolumeClaim
 
 
-class KubernetesPersistentVolumeClaimService(CRUDService):
+class KubernetesPersistentVolumeClaimService(KubernetesBaseResource):
+
+    KUBERNETES_RESOURCE = PersistentVolumeClaim
 
     class Config:
         namespace = 'k8s.pvc'
         namespace_alias = 'k8s.persistent_volume_claim'
         private = True
-
-    @filterable
-    async def query(self, filters, options):
-        return filter_list((await PersistentVolumeClaim.query())['items'], filters, options)
-
-    @accepts(
-        Str('pvc_name'),
-        Dict(
-            'pvc_delete',
-            Str('namespace', required=True),
-        )
-    )
-    async def do_delete(self, pvc_name, options):
-        await PersistentVolumeClaim.delete(pvc_name, **options)
-        return True
