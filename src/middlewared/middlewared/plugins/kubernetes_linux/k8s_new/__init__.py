f0109fccecbd38ea51b5529073fe8398fc227085
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-11 04:53:12 +0500

    Add extensions api

--- src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/__init__.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/__init__.py
@@ -3,5 +3,6 @@ from .batch_api import * # noqa
 from .core_api import * # noqa
 from .custom_object_api import * # noqa
 from .exceptions import * # noqa
+from .extensions_api import * # noqa
 from .openebs import * # noqa
 from .storage_api import * # noqa
