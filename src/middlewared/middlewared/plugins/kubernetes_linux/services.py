f0b3d7f35434712b3a40fd4aa6cf9bdf03bcb6dc
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 10:47:28 +0500

    Update secrets/services classes to use newer k8s base resource

--- src/middlewared/middlewared/plugins/kubernetes_linux/services.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/services.py
@@ -1,26 +1,11 @@
-from middlewared.service import accepts, CRUDService, filterable
-from middlewared.schema import Dict, Str
-from middlewared.utils import filter_list
-
+from .k8s_base_resources import KubernetesBaseResource
 from .k8s_new import Service
 
 
-class KubernetesServicesService(CRUDService):
+class KubernetesServicesService(KubernetesBaseResource):
+
+    KUBERNETES_RESOURCE = Service
 
     class Config:
         namespace = 'k8s.service'
         private = True
-
-    @filterable
-    async def query(self, filters, options):
-        return filter_list((await Service.query())['items'], filters, options)
-
-    @accepts(
-        Str('name'),
-        Dict(
-            'service_delete_options',
-            Str('namespace', required=True),
-        )
-    )
-    async def do_delete(self, name, options):
-        await Service.delete(name, **options)
