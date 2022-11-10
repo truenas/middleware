63d8e09e3af2777d76cd3b5b64c0aa89d1936b1e
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 20:12:04 +0500

    Add class for retrieving custom objects in k8s api

---
+++ src/middlewared/middlewared/plugins/kubernetes_linux/k8s_new/custom_object_api.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from .client import K8sClientBase
+
+
+class CustomObject(K8sClientBase):
+    OBJECT_HUMAN_NAME = 'Openebs'
+
+    @classmethod
+    def uri(
+        cls, group: str, version: str, plural: Optional[str] = None, name: Optional[str] = None,
+        namespace: Optional[str] = None, parameters: Optional[dict] = None
+    ) -> str:
+        uri = f'/apis/{group}/{version}'
+        if namespace:
+            uri += f'/namespaces/{namespace}/{plural}'
+            return uri + f'/{name}' if name else uri
+
+        return uri + f'/{plural}' + cls.query_selectors(parameters if parameters else {})
+
+    @classmethod
+    async def query(cls, group: str, version: str, plural: str, **kwargs):
+        return await cls.call(
+            cls.uri(group, version, plural, namespace=kwargs.pop('namespace', None), parameters=kwargs), mode='get'
+        )
+
+    @classmethod
+    async def create(cls, group: str, version: str, plural: str, data: dict, **kwargs):
+        return await cls.call(cls.uri(
+            group, version, plural, namespace=kwargs.pop('namespace', None), parameters=kwargs), body=data, mode='post'
+        )
+
+    @classmethod
+    async def delete(cls, group: str, version: str, plural: str, name: str,  **kwargs):
+        return await cls.call(
+            cls.uri(group, version, plural, name, namespace=kwargs.pop('namespace', None), parameters=kwargs),
+            mode='delete',
+        )
