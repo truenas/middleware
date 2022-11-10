28ba4fa40bfaa8e2affe96d484c78dbfbd5d7f2c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-11-10 10:42:02 +0500

    Update job classes to use newer k8s base resource

--- src/middlewared/middlewared/plugins/kubernetes_linux/jobs.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/jobs.py
@@ -1,26 +1,11 @@
-from middlewared.schema import Dict, Str
-from middlewared.service import accepts, CRUDService, filterable
-from middlewared.utils import filter_list
-
+from .k8s_base_resources import KubernetesBaseResource
 from .k8s_new import Job
 
 
-class KubernetesJobService(CRUDService):
+class KubernetesJobService(KubernetesBaseResource):
+
+    KUBERNETES_RESOURCE = Job
 
     class Config:
         namespace = 'k8s.job'
         private = True
-
-    @filterable
-    async def query(self, filters, options):
-        return filter_list((await Job.query())['items'], filters, options)
-
-    @accepts(
-        Str('name'),
-        Dict(
-            'k8s_job_delete_options',
-            Str('namespace', required=True),
-        )
-    )
-    async def do_delete(self, name, options):
-        await Job.delete(name, **options)
