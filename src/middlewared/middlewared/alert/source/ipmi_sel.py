c21cb8868f9758186b218b5db32e48df24f28991
rapperskull <rapper.skull@hotmail.it>
2022-11-14 13:21:50 +0100

    Add ("Power Supply", "Fully Redundant") to IPMI_EVENTS_BLACKLIST
    I don't know where "Redundancy State" sensor came from (maybe OEM-specific?), but it's not a standard sensor type. Dell uses a "Power Supply" sensor to indicate the redundancy state.
    
    (cherry picked from commit 3aa2e5a2dc07e21857ceff160d66e405be99308b)

--- src/middlewared/middlewared/alert/source/ipmi_sel.py
+++ src/middlewared/middlewared/alert/source/ipmi_sel.py
@@ -107,6 +107,7 @@ class IPMISELAlertSource(AlertSource):
         ("Redundancy State", "Fully Redundant"),
         ("Processor", "Presence detected"),
         ("Power Supply", "Presence detected"),
+        ("Power Supply", "Fully Redundant"),
     )
 
     async def check(self):
