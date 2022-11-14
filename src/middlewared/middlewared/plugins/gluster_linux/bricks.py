77a02c6d97aedbe1a48af01257ddbe0658ca33e9
caleb <yocalebo@gmail.com>
2022-11-14 11:17:49 -0500

    call format_bricks in bricks plugin

--- src/middlewared/middlewared/plugins/gluster_linux/bricks.py
+++ src/middlewared/middlewared/plugins/gluster_linux/bricks.py
@@ -2,6 +2,7 @@ from glustercli.cli.bricks import add as bricks_add
 
 from middlewared.service import Service, accepts, job
 from middlewared.schema import Dict, Str, List
+from .utils import format_bricks
 
 
 class GlusterBricksService(Service):
@@ -10,9 +11,6 @@ class GlusterBricksService(Service):
         namespace = 'gluster.bricks'
         cli_namespace = 'system.gluster.brick'
         private = True
-
-    async def format_bricks(self, bricks):
-        return [f'{i["peer_name"]}:{i["peer_path"]}' for i in bricks]
 
     @accepts(Dict(
         'add_bricks',
@@ -38,7 +36,7 @@ class GlusterBricksService(Service):
             `peer_path` str: absolute path of the brick on `peer_name`
         """
         job.set_progress(50, 'Formatting brick information')
-        options = {'args': (data['name'], await self.format_bricks(data['bricks']),), 'kwargs': {'force': True}}
+        options = {'args': (data['name'], await format_bricks(data['bricks']),), 'kwargs': {'force': True}}
         job.set_progress(99, f'Adding bricks to {data["name"]!r}')
         await self.middleware.call('gluster.method.run', bricks_add, options)
         job.set_progress(100, f'Bricks successfully added to {data["name"]!r}')
