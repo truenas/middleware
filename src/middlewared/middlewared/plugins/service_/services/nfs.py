c2076a43bedce18a1dd25490dd98b3eadff8fb99
Andrew Walker <awalker@ixsystems.com>
2022-11-17 10:35:20 -0500

    Add endpoint to check service configuration prior to start
    Various services have conditions that must be satisfied prior
    to starting the service. Since webui is reporting on start
    failures now, also allow presenting clear reasons why the
    service won't be started if it's horribly misconfigured or
    state is broken.
    
    Services using `check_configuration` should raise CallError
    with text describing the nature of the misconfiguration.
    
    If service is started with `silent` option, then configuration
    check failures will be logged to the middleware log, otherwise
    exception will be re-raised to caller of service.start method.

--- src/middlewared/middlewared/plugins/service_/services/nfs.py
+++ src/middlewared/middlewared/plugins/service_/services/nfs.py
@@ -1,4 +1,8 @@
+import errno
+import os
+
 from .base import SimpleService
+from middlewared.service_exception import CallError
 
 
 class NFSService(SimpleService):
@@ -8,6 +12,15 @@ class NFSService(SimpleService):
     etc = ["nfsd"]
 
     systemd_unit = "nfs-server"
+
+    async def check_configuration(self):
+        if not await self.middleware.run_in_thread(os.path.exists, '/etc/exports'):
+            raise CallError(
+                'At least one configured and available NFS export is required '
+                'in order to start the NFS service. Check the NFS share configuration '
+                'and availability of any paths currently being exported.',
+                errno.EINVAL
+            )
 
     async def systemd_extra_units(self):
         return ["rpc-statd"]
