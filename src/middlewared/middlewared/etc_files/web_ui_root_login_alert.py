91aa122dc3d5c4a732899743ebe8d8007d4b8406
themylogin <themylogin@gmail.com>
2022-11-18 20:35:54 +0100

    Use cached users and groups list when creating root login alert on users update

--- src/middlewared/middlewared/etc_files/web_ui_root_login_alert.py
+++ src/middlewared/middlewared/etc_files/web_ui_root_login_alert.py
@@ -1,7 +1,11 @@
 
 
 async def render(service, middleware, render_ctx):
-    if await middleware.call('privilege.always_has_root_password_enabled'):
+    if await middleware.call(
+            'privilege.always_has_root_password_enabled',
+            render_ctx['user.query'],
+            render_ctx['group.query'],
+    ):
         await middleware.call('alert.oneshot_create', 'WebUiRootLogin', None)
     else:
         await middleware.call('alert.oneshot_delete', 'WebUiRootLogin', None)
