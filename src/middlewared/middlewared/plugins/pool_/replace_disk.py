5acdb5e676632ec2038fbbbe723b26b284906bb2
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2022-11-18 10:09:02 -0500

    NAS-118922 / 22.12 / fix and improve pool.replace (#10106)
    * clean-up round 1
    
    * clean-up round 2
    
    * clean-up round 3
    
    * clean-up round 4
    
    * clean-up round 5
    
    * move replace out of try: finally clause
    
    * clean-up round 6
    
    * move replace back inside try: finally

--- src/middlewared/middlewared/plugins/pool_/replace_disk.py
+++ src/middlewared/middlewared/plugins/pool_/replace_disk.py
@@ -5,7 +5,6 @@ import os
 from middlewared.schema import accepts, Bool, Dict, Int, returns, Str
 from middlewared.service import item_method, job, Service, ValidationErrors
 from middlewared.service_exception import MatchNotFound
-from middlewared.utils import osc
 
 
 class PoolService(Service):
@@ -45,78 +44,57 @@ class PoolService(Service):
             }
         """
         pool = await self.middleware.call('pool.get_instance', oid)
-
         verrors = ValidationErrors()
-
         unused_disks = await self.middleware.call('disk.get_unused')
-        disk = list(filter(lambda x: x['identifier'] == options['disk'], unused_disks))
-        if not disk:
+        if not (disk := list(filter(lambda x: x['identifier'] == options['disk'], unused_disks))):
             verrors.add('options.disk', 'Disk not found.', errno.ENOENT)
         else:
             disk = disk[0]
-
             if not options['force'] and not await self.middleware.call('disk.check_clean', disk['devname']):
                 verrors.add('options.force', 'Disk is not clean, partitions were found.')
 
-        found = await self.middleware.call('pool.find_disk_from_topology', options['label'], pool)
-
-        if not found:
+        if not (found := await self.middleware.call('pool.find_disk_from_topology', options['label'], pool)):
             verrors.add('options.label', f'Label {options["label"]} not found.', errno.ENOENT)
 
-        if verrors:
-            raise verrors
-
-        old_disk = None
-        if options['preserve_settings']:
-            try:
-                old_disk = await self.middleware.call('disk.query', [['zfs_guid', '=', options['label']]], {
-                    'extra': {'include_expired': True},
-                    'get': True
-                })
-            except MatchNotFound:
-                pass
-
-        create_swap = found[0] in ('data', 'spare')
+        verrors.check()
 
         swap_disks = [disk['devname']]
-        # If the disk we are replacing is still available, remove it from swap as well
-        if found[1] and os.path.exists(found[1]['path']):
-            from_disk = await self.middleware.call(
-                'disk.label_to_disk', found[1]['path'].replace('/dev/', '')
-            )
-            if from_disk:
+        if found[1] and await self.middleware.run_in_thread(os.path.exists, found[1]['path']):
+            if from_disk := await self.middleware.call('disk.label_to_disk', found[1]['path'].replace('/dev/', '')):
+                # If the disk we are replacing is still available, remove it from swap as well
                 swap_disks.append(from_disk)
 
         await self.middleware.call('disk.swaps_remove_disks', swap_disks)
 
         vdev = []
-        await self.middleware.call(
-            'pool.format_disks', job, {disk['devname']: {'vdev': vdev, 'create_swap': create_swap}},
-        )
+        format_opts = {disk['devname']: {'vdev': vdev, 'create_swap': found[0] in ('data', 'spare')}}
+        await self.middleware.call('pool.format_disks', job, format_opts)
 
-        new_devname = vdev[0].replace('/dev/', '')
-
-        job.set_progress(30, 'Replacing disk')
         try:
-            await self.middleware.call(
-                'zfs.pool.replace', pool['name'], options['label'], new_devname
-            )
-            # If we are replacing a faulted disk, kick it right after replace
-            # is initiated.
+            job.set_progress(30, 'Replacing disk')
+            new_devname = vdev[0].replace('/dev/', '')
+            await self.middleware.call('zfs.pool.replace', pool['name'], options['label'], new_devname)
             try:
-                vdev = await self.middleware.call(
-                    'zfs.pool.get_vdev', pool['name'], options['label'],
-                )
+                vdev = await self.middleware.call('zfs.pool.get_vdev', pool['name'], options['label'])
                 if vdev['status'] not in ('ONLINE', 'DEGRADED'):
+                    job.set_progress(80, 'Detaching old disk')
+                    # If we are replacing a faulted disk, kick it right after replace is initiated.
                     await self.middleware.call('zfs.pool.detach', pool['name'], options['label'])
             except Exception:
-                self.logger.warn('Failed to detach device', exc_info=True)
+                self.logger.warning('Failed to detach device with label %r', options['label'], exc_info=True)
         finally:
             # Needs to happen even if replace failed to put back disk that had been
             # removed from swap prior to replacement
             asyncio.ensure_future(self.middleware.call('disk.swaps_configure'))
 
-        if old_disk:
-            await self.middleware.call('disk.copy_settings', old_disk, disk)
+        if options['preserve_settings']:
+            filters = [['zfs_guid', '=', options['label']]]
+            options = {'extra': {'include_expired': True}, 'get': True}
+            try:
+                old_disk = await self.middleware.call('disk.query', filters, options)
+                job.set_progress(98, 'Copying old disk settings to new')
+                await self.middleware.call('disk.copy_settings', old_disk, disk)
+            except MatchNotFound:
+                pass
 
         return True
