fa38a7bfca803324b1027000c7afc0e3c6a4ab6c
themylogin <themylogin@gmail.com>
2022-11-20 12:37:02 +0100

    truenas-set-authentication-method.py helper script for Rootless login installer changes

---
+++ src/freenas/usr/local/bin/truenas-set-authentication-method.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import json
+import os
+import shutil
+import sys
+
+import sqlite3
+
+from middlewared.plugins.account import crypted_password
+from middlewared.utils.db import FREENAS_DATABASE
+
+if __name__ == "__main__":
+    authentication_method = json.loads(sys.stdin.read())
+    username = authentication_method["username"]
+    password = crypted_password(authentication_method["password"])
+
+    conn = sqlite3.connect(FREENAS_DATABASE)
+    conn.row_factory = sqlite3.Row
+
+    c = conn.cursor()
+    if username == "root":
+        c.execute("UPDATE account_bsdusers SET bsdusr_unixhash = ? WHERE bsdusr_username = 'root'", (password,))
+    else:
+        home = f"/home/{username}"
+
+        c.execute("""
+            INSERT INTO account_bsdgroups (bsdgrp_gid, bsdgrp_group, bsdgrp_builtin, bsdgrp_sudo, bsdgrp_smb,
+                                           bsdgrp_sudo_commands, bsdgrp_sudo_nopasswd)
+            VALUES (1000, ?, 0, 0, 0, '[]', 0)
+        """, (username,))
+
+        c.execute("SELECT last_insert_rowid()")
+        group_id = dict(c.fetchone())["last_insert_rowid()"]
+
+        c.execute("SELECT * FROM account_bsdusers WHERE bsdusr_username = 'root'")
+        user = dict(c.fetchone())
+
+        del user["id"]
+        user["bsdusr_uid"] = 1000
+        user["bsdusr_username"] = username
+        user["bsdusr_unixhash"] = password
+        user["bsdusr_home"] = home
+        user["bsdusr_full_name"] = "Local Administrator"
+        user["bsdusr_sudo"] = 1
+        user["bsdusr_group_id"] = group_id
+        c.execute(f"""
+            INSERT INTO account_bsdusers ({', '.join([k for k in user.keys()])})
+            VALUES ({', '.join(['?' for k in user.keys()])})
+        """, tuple(user.values()))
+
+        c.execute("SELECT last_insert_rowid()")
+        user_id = dict(c.fetchone())["last_insert_rowid()"]
+
+        c.execute("SELECT id FROM account_bsdgroups WHERE bsdgrp_group = 'builtin_administrators'")
+        builtin_administrators_group_id = dict(c.fetchone())["id"]
+
+        c.execute("""
+            INSERT INTO account_bsdgroupmembership (bsdgrpmember_group_id, bsdgrpmember_user_id) VALUES (?, ?)
+        """, (user_id, builtin_administrators_group_id))
+
+        shutil.copytree("/root", home)
+        for root, dirs, files in os.walk(home):
+            for d in dirs:
+                os.chown(os.path.join(root, d), 1000, 1000)
+            for f in files:
+                os.chown(os.path.join(root, f), 1000, 1000)
+
+        c.execute("UPDATE account_bsdusers SET bsdusr_password_disabled = 1 WHERE bsdusr_username = 'root'")
+
+    conn.commit()
+    conn.close()
