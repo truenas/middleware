4cde23f83f293f80099773dce8506605cd6254f1
ericbsd <ericturgeon.bsd@gmail.com>
2022-11-21 15:03:11 -0400

    Adding vm test to get cpu model

--- tests/api2/test_540_vm.py
+++ tests/api2/test_540_vm.py
@@ -38,59 +38,66 @@ def test_02_get_virtualization_details():
     assert isinstance(results.json()['supported'], bool), results.text
 
 
-def test_03_looking_vm_flags():
+def test_03_get_vm_flags():
     results = GET('/vm/flags/')
     assert results.status_code == 200, results.text
     assert isinstance(results.json(), dict), results.text
+
+
+def test_04_get_vm_cpu_model_choices():
+    results = GET('/vm/cpu_model_choices/')
+    assert results.status_code == 200, results.text
+    assert isinstance(results.json(), dict), results.text
+    assert results.json()['EPYC'] == 'EPYC', results.json()
 
 
 # Only run if the system support virtualization
 if support_virtualization:
 
     @pytest.mark.parametrize('dkey', list(bootloader.keys()))
-    def test_04_verify_vm_bootloader_options(dkey):
+    def test_05_verify_vm_bootloader_options(dkey):
         results = GET('/vm/bootloader_options/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert bootloader[dkey] == results.json()[dkey]
 
-    def test_05_get_available_memory_for_vms(data):
+    def test_06_get_available_memory_for_vms(data):
         results = POST('/vm/get_available_memory/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), int), results.text
 
     # /vm/guest_architecture_and_machine_choices
     @pytest.mark.parametrize('dkey', ['i686', 'x86_64'])
-    def test_06_verify_vm_guest_architecture_and_machine_choices(dkey):
+    def test_07_verify_vm_guest_architecture_and_machine_choices(dkey):
         results = GET('/vm/guest_architecture_and_machine_choices/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert isinstance(results.json()[dkey], list), results.text
 
-    def test_07_verify_maximum_supported_vcpus():
+    def test_08_verify_maximum_supported_vcpus():
         results = GET('/vm/maximum_supported_vcpus/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), int), results.text
 
     @pytest.mark.parametrize('dkey', ['port', 'web'])
-    def test_08_get_vm_port_wizard(dkey):
+    def test_09_get_vm_port_wizard(dkey):
         results = GET('/vm/port_wizard/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert isinstance(results.json()[dkey], int), results.text
 
-    def test_09_get_random_mac():
+    def test_10_get_random_mac():
         results = GET('/vm/random_mac/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), str), results.text
 
-    def test_10_get_resolution_choices():
+    def test_12_get_resolution_choices():
         results = GET('/vm/resolution_choices/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert results.json()['1920x1080'] == '1920x1080', results.text
 
-    def test_11_creating_vm(data):
+    def test_13_creating_vm(data):
         global payload
         payload = {
             'name': 'vmtest',
@@ -104,7 +111,7 @@ if support_virtualization:
         assert results.status_code == 200, results.text
         data['vmid'] = results.json()['id']
 
-    def test_12_get_vm_query(data):
+    def test_13_get_vm_query(data):
         results = GET(f'/vm/?id={data["vmid"]}')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), list), results.text
@@ -113,15 +120,15 @@ if support_virtualization:
 
     @pytest.mark.parametrize('dkey', ['name', 'description', 'vcpus', 'memory',
                                       'bootloader', 'autostart'])
-    def test_13_look_vm_query_(dkey):
+    def test_14_look_vm_query_(dkey):
         assert vmware_query.json()[0][dkey] == payload[dkey], vmware_query.text
 
-    def test_14_start_vm(data):
+    def test_15_start_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/start/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), type(None)), results.text
 
-    def test_15_verify_vm_status_is_running(data):
+    def test_16_verify_vm_status_is_running(data):
         results = POST(f'/vm/id/{data["vmid"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -129,42 +136,42 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], int), results.text
         assert results.json()['domain_state'] == 'RUNNING', results.text
 
-    def test_16_get_vm_console_name(data):
+    def test_17_get_vm_console_name(data):
         results = POST('/vm/get_console/', data['vmid'])
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), str), results.text
         assert results.json() == f'{data["vmid"]}_vmtest'
 
-    def test_17_get_vm_memory_usage(data):
+    def test_18_get_vm_memory_usage(data):
         results = POST('/vm/get_memory_usage/', data["vmid"])
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), int), results.text
 
-    def test_18_get_vm_memory_info_on_started_vm(data):
+    def test_19_get_vm_memory_info_on_started_vm(data):
         results = POST('/vm/get_memory_usage/', data["vmid"])
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), int), results.text
 
-    def test_19_get_vm_log_file_path(data):
+    def test_20_get_vm_log_file_path(data):
         results = POST('/vm/log_file_path/', data["vmid"])
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), str), results.text
         assert f'{data["vmid"]}_vmtest' in results.json()
 
     @pytest.mark.parametrize('dkey', ['RNP', 'PRD', 'RPRD'])
-    def test_20_verify_vm_bootloader_options(dkey):
+    def test_21_verify_vm_bootloader_options(dkey):
         results = GET('/vm/get_vmemory_in_use/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert isinstance(results.json()[dkey], int), results.text
 
-    def test_21_suspend_vm(data):
+    def test_22_suspend_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/suspend')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), type(None)), results.text
         sleep(1)
 
-    def test_22_verify_vm_status_is_suspended(data):
+    def test_23_verify_vm_status_is_suspended(data):
         results = POST(f'/vm/id/{data["vmid"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -172,13 +179,13 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], int), results.text
         assert results.json()['domain_state'] == 'PAUSED', results.text
 
-    def test_23_resume_vm(data):
+    def test_24_resume_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/resume')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), type(None)), results.text
         sleep(1)
 
-    def test_24_verify_vm_status_is_running_after_resuming(data):
+    def test_25_verify_vm_status_is_running_after_resuming(data):
         results = POST(f'/vm/id/{data["vmid"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -186,14 +193,14 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], int), results.text
         assert results.json()['domain_state'] == 'RUNNING', results.text
 
-    def test_25_restart_vm(data):
+    def test_26_restart_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/restart')
         assert results.status_code == 200, results.text
         job_status = wait_on_job(results.json(), 180)
         assert job_status['state'] == 'SUCCESS', str(job_status['results'])
         sleep(1)
 
-    def test_26_verify_vm_status_is_running_after_restarting(data):
+    def test_27_verify_vm_status_is_running_after_restarting(data):
         results = POST(f'/vm/id/{data["vmid"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -201,20 +208,20 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], int), results.text
         assert results.json()['domain_state'] == 'RUNNING', results.text
 
-    def test_26_stop_vm(data):
+    def test_28_stop_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/stop/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), int), results.text
         job_status = wait_on_job(results.json(), 180)
         assert job_status['state'] == 'SUCCESS', str(job_status['results'])
 
-    def test_27_poweroff_vm(data):
+    def test_29_poweroff_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/poweroff/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), type(None)), results.text
         sleep(1)
 
-    def test_28_verify_vm_status_is_stopped_and_shutoff(data):
+    def test_30_verify_vm_status_is_stopped_and_shutoff(data):
         results = POST(f'/vm/id/{data["vmid"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -222,7 +229,7 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], type(None)), results.text
         assert results.json()['domain_state'] == 'SHUTOFF', results.text
 
-    def test_29_update_vm(data):
+    def test_31_update_vm(data):
         global payload
         payload = {
             'memory': 768,
@@ -232,19 +239,19 @@ if support_virtualization:
         assert GET(f'/vm?id={data["vmid"]}').json()[0]['memory'] == 768
 
     @pytest.mark.parametrize('dkey', ['memory'])
-    def test_30_get_vm_query(data, dkey):
+    def test_32_get_vm_query(data, dkey):
         results = GET(f'/vm/id/{data["vmid"]}')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert results.json()[dkey] == payload[dkey], results.text
 
-    def test_31_clone_a_vm(data):
+    def test_33_clone_a_vm(data):
         results = POST(f'/vm/id/{data["vmid"]}/clone/', 'vmtest2')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), bool), results.text
         data['vmid2'] = GET('/vm/?name=vmtest2').json()[0]['id']
 
-    def test_32_verify_cloned_vm_status_is_stopped(data):
+    def test_34_verify_cloned_vm_status_is_stopped(data):
         results = POST(f'/vm/id/{data["vmid2"]}/status/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
@@ -252,18 +259,18 @@ if support_virtualization:
         assert isinstance(results.json()['pid'], type(None)), results.text
         assert results.json()['domain_state'] == 'SHUTOFF', results.text
 
-    def test_33_get_the_clone_vm_console_name(data):
+    def test_35_get_the_clone_vm_console_name(data):
         results = POST('/vm/get_console/', data['vmid2'])
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), str), results.text
         assert results.json() == f'{data["vmid2"]}_vmtest2'
 
-    def test_34_get_vm_memory_info_on_stopped_vm(data):
+    def test_36_get_vm_memory_info_on_stopped_vm(data):
         results = POST('/vm/get_memory_usage/', data["vmid2"])
         assert results.status_code == 422, results.text
         assert isinstance(results.json(), dict), results.text
 
     @pytest.mark.parametrize('vmid', ['vmid', 'vmid2'])
-    def test_35_delete_vms(data, vmid):
+    def test_37_delete_vms(data, vmid):
         results = DELETE(f'/vm/id/{data[vmid]}/')
         assert results.status_code == 200, results.text
