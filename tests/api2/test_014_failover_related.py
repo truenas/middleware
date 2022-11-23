9a325b7a770ed34e71ba5c4f6adf3cf82aeff55c
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2022-11-23 13:48:35 -0500

    NAS-119126 / 22.12 / Revert the vrrp.thread changes (#10122)
    * Revert "fix vrrp_thread integration test"
    
    This reverts commit 2b7eafdc5b5c91a5135b776e3b2e9a646af172eb.
    
    * Revert "NAS-119008 / 22.12 / properly send HA events on SCALE (#10115)"
    
    This reverts commit 98ec9b5f8b7826ecc9083866510aa3617410e3eb.

--- tests/api2/test_014_failover_related.py
+++ tests/api2/test_014_failover_related.py
@@ -109,20 +109,3 @@ if ha:
             remote = call('failover.call_remote', 'network.configuration.config')
             assert remote['nameserver3'] == old_ns
             assert remote['state']['nameserver3'] == old_ns
-
-    def test_08_vrrp_thread_is_running():
-        rv = make_ws_request(ip, {'msg': 'method', 'method': 'vrrp.thread.is_running'})
-        assert isinstance(rv, dict)
-        assert rv['result'], 'VRRP FIFO Thread should always be running on HA systems'
-
-        # we stop and start this thread on failover backup events
-
-        # stop it
-        make_ws_request(ip, {'msg': 'method', 'method': 'vrrp.thread.stop'})
-        rv = make_ws_request(ip, {'msg': 'method', 'method': 'vrrp.thread.is_running'})
-        assert not rv['result'], 'VRRP FIFO Thread should stop on HA systems'
-
-        # start it
-        make_ws_request(ip, {'msg': 'method', 'method': 'vrrp.thread.start'})
-        rv = make_ws_request(ip, {'msg': 'method', 'method': 'vrrp.thread.is_running'})
-        assert rv['result'], 'VRRP FIFO Thread should start on HA systems'
