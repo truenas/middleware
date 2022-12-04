e27988d518c6a0f96f0bc0b0d00bf0410cf2b549
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-12-04 13:52:22 +0500

    Move pool resilver logic to separate service part

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -7,7 +7,7 @@ import errno
 import itertools
 import json
 import logging
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 from pathlib import Path
 import os
 import re
@@ -27,7 +27,7 @@ from middlewared.schema import (
     Ref, returns, OROperator, NOT_PROVIDED,
 )
 from middlewared.service import (
-    ConfigService, filterable, item_method, job, pass_app, private, CallError, CRUDService, ValidationErrors, periodic
+    filterable, item_method, job, pass_app, private, CallError, CRUDService, ValidationErrors, periodic
 )
 from middlewared.service_exception import InstanceNotFound, ValidationError
 import middlewared.sqlalchemy as sa
@@ -35,7 +35,7 @@ from middlewared.utils import filter_list
 from middlewared.utils.asyncio_ import asyncio_map
 from middlewared.utils.path import is_child
 from middlewared.utils.size import MB
-from middlewared.validators import Exact, Match, Or, Range, Time
+from middlewared.validators import Exact, Match, Or, Range
 
 logger = logging.getLogger(__name__)
 
@@ -117,116 +117,6 @@ def _null(x):
     if x == 'none':
         return None
     return x
-
-
-class PoolResilverModel(sa.Model):
-    __tablename__ = 'storage_resilver'
-
-    id = sa.Column(sa.Integer(), primary_key=True)
-    enabled = sa.Column(sa.Boolean(), default=True)
-    begin = sa.Column(sa.Time(), default=time(hour=18))
-    end = sa.Column(sa.Time(), default=time(hour=9))
-    weekday = sa.Column(sa.String(120), default='1,2,3,4,5,6,7')
-
-
-class PoolResilverService(ConfigService):
-
-    class Config:
-        namespace = 'pool.resilver'
-        datastore = 'storage.resilver'
-        datastore_extend = 'pool.resilver.resilver_extend'
-        cli_namespace = 'storage.resilver'
-
-    ENTRY = Dict(
-        'pool_resilver_entry',
-        Int('id', required=True),
-        Str('begin', validators=[Time()], required=True),
-        Str('end', validators=[Time()], required=True),
-        Bool('enabled', required=True),
-        List('weekday', required=True, items=[Int('weekday', validators=[Range(min=1, max=7)])])
-    )
-
-    @private
-    async def resilver_extend(self, data):
-        data['begin'] = data['begin'].strftime('%H:%M')
-        data['end'] = data['end'].strftime('%H:%M')
-        data['weekday'] = [int(v) for v in data['weekday'].split(',') if v]
-        return data
-
-    @private
-    async def validate_fields_and_update(self, data, schema):
-        verrors = ValidationErrors()
-
-        begin = data.get('begin')
-        if begin:
-            data['begin'] = time(int(begin.split(':')[0]), int(begin.split(':')[1]))
-
-        end = data.get('end')
-        if end:
-            data['end'] = time(int(end.split(':')[0]), int(end.split(':')[1]))
-
-        weekdays = data.get('weekday')
-        if not weekdays:
-            verrors.add(
-                f'{schema}.weekday',
-                'At least one weekday should be selected'
-            )
-        else:
-            data['weekday'] = ','.join([str(day) for day in weekdays])
-
-        return verrors, data
-
-    async def do_update(self, data):
-        """
-        Configure Pool Resilver Priority.
-
-        If `begin` time is greater than `end` time it means it will rollover the day, e.g.
-        begin = "19:00", end = "05:00" will increase pool resilver priority from 19:00 of one day
-        until 05:00 of the next day.
-
-        `weekday` follows crontab(5) values 0-7 (0 or 7 is Sun).
-
-        .. examples(websocket)::
-
-          Enable pool resilver priority all business days from 7PM to 5AM.
-
-            :::javascript
-            {
-                "id": "6841f242-840a-11e6-a437-00e04d680384",
-                "msg": "method",
-                "method": "pool.resilver.update",
-                "params": [{
-                    "enabled": true,
-                    "begin": "19:00",
-                    "end": "05:00",
-                    "weekday": [1, 2, 3, 4, 5]
-                }]
-            }
-        """
-        config = await self.config()
-        original_config = config.copy()
-        config.update(data)
-
-        verrors, new_config = await self.validate_fields_and_update(config, 'pool_resilver_update')
-        verrors.check()
-
-        # before checking if any changes have been made, original_config needs to be mapped to new_config
-        original_config['weekday'] = ','.join([str(day) for day in original_config['weekday']])
-        original_config['begin'] = time(*(int(value) for value in original_config['begin'].split(':')))
-        original_config['end'] = time(*(int(value) for value in original_config['end'].split(':')))
-        if len(set(original_config.items()) ^ set(new_config.items())) > 0:
-            # data has changed
-            await self.middleware.call(
-                'datastore.update',
-                self._config.datastore,
-                new_config['id'],
-                new_config
-            )
-
-            await self.middleware.call('service.restart', 'cron')
-            await self.middleware.call('pool.configure_resilver_priority')
-
-        return await self.config()
 
 
 class PoolModel(sa.Model):
@@ -4306,7 +4196,3 @@ class PoolDatasetService(CRUDService):
                 return await delegate.query(path, enabled)
 
         raise RuntimeError(f'Unknown attachment delegate {name!r}')
-
-
-def setup(middleware):
-    middleware.create_task(middleware.call('pool.configure_resilver_priority'))
