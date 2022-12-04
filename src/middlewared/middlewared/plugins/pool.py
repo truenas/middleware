168d208e5c0b0d497c7bfd5b6a90678be5ac015c
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-12-04 16:20:26 +0500

    Move pool dataset information logic to separate service part

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -63,64 +63,12 @@ class PoolDatasetService(CRUDService):
 
     attachment_delegates = []
     dataset_store = 'storage.encrypteddataset'
-    ENTRY = Dict(
-        'pool_dataset_entry',
-        Str('id', required=True),
-        Str('type', required=True),
-        Str('name', required=True),
-        Str('pool', required=True),
-        Bool('encrypted'),
-        Str('encryption_root', null=True),
-        Bool('key_loaded', null=True),
-        List('children', required=True),
-        Dict('user_properties', additional_attrs=True, required=True),
-        Bool('locked'),
-        *[Dict(
-            p[1] or p[0],
-            Any('parsed', null=True),
-            Str('rawvalue', null=True),
-            Str('value', null=True),
-            Str('source', null=True),
-            Any('source_info', null=True),
-        ) for p in get_props_of_interest_mapping() if (p[1] or p[0]) != 'mountpoint'],
-        Str('mountpoint', null=True),
-    )
 
     class Config:
         datastore_primary_key_type = 'string'
         namespace = 'pool.dataset'
         event_send = False
         cli_namespace = 'storage.dataset'
-
-    @accepts()
-    @returns(Dict(
-        *[Str(k, enum=[k]) for k in ZFS_CHECKSUM_CHOICES if k != 'OFF'],
-    ))
-    async def checksum_choices(self):
-        """
-        Retrieve checksums supported for ZFS dataset.
-        """
-        return {v: v for v in ZFS_CHECKSUM_CHOICES if v != 'OFF'}
-
-    @accepts()
-    @returns(Dict(
-        *[Str(k, enum=[k]) for k in ZFS_COMPRESSION_ALGORITHM_CHOICES],
-    ))
-    async def compression_choices(self):
-        """
-        Retrieve compression algorithm supported by ZFS.
-        """
-        return {v: v for v in ZFS_COMPRESSION_ALGORITHM_CHOICES}
-
-    @accepts()
-    @returns(Dict(
-        *[Str(k, enum=[k]) for k in ZFS_ENCRYPTION_ALGORITHM_CHOICES],
-    ))
-    async def encryption_algorithm_choices(self):
-        """
-        Retrieve encryption algorithms supported for ZFS dataset encryption.
-        """
-        return {v: v for v in ZFS_ENCRYPTION_ALGORITHM_CHOICES}
 
     @private
     @accepts(
@@ -1409,58 +1357,6 @@ class PoolDatasetService(CRUDService):
         verrors.check()
         if quotas:
             await self.middleware.call('zfs.dataset.set_quota', ds, quotas)
-
-    @accepts(Str('pool'))
-    @returns(Str())
-    async def recommended_zvol_blocksize(self, pool):
-        """
-        Helper method to get recommended size for a new zvol (dataset of type VOLUME).
-
-        .. examples(websocket)::
-
-          Get blocksize for pool "tank".
-
-            :::javascript
-            {
-                "id": "6841f242-840a-11e6-a437-00e04d680384",
-                "msg": "method",
-                "method": "pool.dataset.recommended_zvol_blocksize",
-                "params": ["tank"]
-            }
-        """
-        pool = await self.middleware.call('pool.query', [['name', '=', pool]])
-        if not pool:
-            raise CallError(f'"{pool}" not found.', errno.ENOENT)
-
-        """
-        Cheatsheat for blocksizes is as follows:
-        2w/3w mirror = 16K
-        3wZ1, 4wZ2, 5wZ3 = 16K
-        4w/5wZ1, 5w/6wZ2, 6w/7wZ3 = 32K
-        6w/7w/8w/9wZ1, 7w/8w/9w/10wZ2, 8w/9w/10w/11wZ3 = 64K
-        10w+Z1, 11w+Z2, 12w+Z3 = 128K
-
-        If the zpool was forcefully created with mismatched
-        vdev geometry (i.e. 3wZ1 and a 5wZ1) then we calculate
-        the blocksize based on the largest vdev of the zpool.
-        """
-        maxdisks = 1
-        for vdev in pool[0]['topology']['data']:
-            if vdev['type'] == 'RAIDZ1':
-                disks = len(vdev['children']) - 1
-            elif vdev['type'] == 'RAIDZ2':
-                disks = len(vdev['children']) - 2
-            elif vdev['type'] == 'RAIDZ3':
-                disks = len(vdev['children']) - 3
-            elif vdev['type'] == 'MIRROR':
-                disks = maxdisks
-            else:
-                disks = len(vdev['children'])
-
-            if disks > maxdisks:
-                maxdisks = disks
-
-        return f'{max(16, min(128, 2 ** ((maxdisks * 8) - 1).bit_length()))}K'
 
     @item_method
     @accepts(Str('id', required=True))
---
+++ src/middlewared/middlewared/plugins/pool_/dataset_info.py
@@ -0,0 +1,94 @@
+import errno
+
+from middlewared.schema import accepts, Dict, returns, Str
+from middlewared.service import CallError, Service
+
+from .utils import ZFS_CHECKSUM_CHOICES, ZFS_COMPRESSION_ALGORITHM_CHOICES, ZFS_ENCRYPTION_ALGORITHM_CHOICES
+
+
+class PoolDatasetService(Service):
+
+    class Config:
+        namespace = 'pool.dataset'
+
+    @accepts()
+    @returns(Dict(
+        *[Str(k, enum=[k]) for k in ZFS_CHECKSUM_CHOICES if k != 'OFF'],
+    ))
+    async def checksum_choices(self):
+        """
+        Retrieve checksums supported for ZFS dataset.
+        """
+        return {v: v for v in ZFS_CHECKSUM_CHOICES if v != 'OFF'}
+
+    @accepts()
+    @returns(Dict(
+        *[Str(k, enum=[k]) for k in ZFS_COMPRESSION_ALGORITHM_CHOICES],
+    ))
+    async def compression_choices(self):
+        """
+        Retrieve compression algorithm supported by ZFS.
+        """
+        return {v: v for v in ZFS_COMPRESSION_ALGORITHM_CHOICES}
+
+    @accepts()
+    @returns(Dict(
+        *[Str(k, enum=[k]) for k in ZFS_ENCRYPTION_ALGORITHM_CHOICES],
+    ))
+    async def encryption_algorithm_choices(self):
+        """
+        Retrieve encryption algorithms supported for ZFS dataset encryption.
+        """
+        return {v: v for v in ZFS_ENCRYPTION_ALGORITHM_CHOICES}
+
+    @accepts(Str('pool'))
+    @returns(Str())
+    async def recommended_zvol_blocksize(self, pool):
+        """
+        Helper method to get recommended size for a new zvol (dataset of type VOLUME).
+
+        .. examples(websocket)::
+
+          Get blocksize for pool "tank".
+
+            :::javascript
+            {
+                "id": "6841f242-840a-11e6-a437-00e04d680384",
+                "msg": "method",
+                "method": "pool.dataset.recommended_zvol_blocksize",
+                "params": ["tank"]
+            }
+        """
+        pool = await self.middleware.call('pool.query', [['name', '=', pool]])
+        if not pool:
+            raise CallError(f'"{pool}" not found.', errno.ENOENT)
+
+        """
+        Cheatsheat for blocksizes is as follows:
+        2w/3w mirror = 16K
+        3wZ1, 4wZ2, 5wZ3 = 16K
+        4w/5wZ1, 5w/6wZ2, 6w/7wZ3 = 32K
+        6w/7w/8w/9wZ1, 7w/8w/9w/10wZ2, 8w/9w/10w/11wZ3 = 64K
+        10w+Z1, 11w+Z2, 12w+Z3 = 128K
+
+        If the zpool was forcefully created with mismatched
+        vdev geometry (i.e. 3wZ1 and a 5wZ1) then we calculate
+        the blocksize based on the largest vdev of the zpool.
+        """
+        maxdisks = 1
+        for vdev in pool[0]['topology']['data']:
+            if vdev['type'] == 'RAIDZ1':
+                disks = len(vdev['children']) - 1
+            elif vdev['type'] == 'RAIDZ2':
+                disks = len(vdev['children']) - 2
+            elif vdev['type'] == 'RAIDZ3':
+                disks = len(vdev['children']) - 3
+            elif vdev['type'] == 'MIRROR':
+                disks = maxdisks
+            else:
+                disks = len(vdev['children'])
+
+            if disks > maxdisks:
+                maxdisks = disks
+
+        return f'{max(16, min(128, 2 ** ((maxdisks * 8) - 1).bit_length()))}K'
