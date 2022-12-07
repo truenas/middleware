e3ff16eeaf0720ca71b12329b9493fb7fb066ea4
themylogin <themylogin@gmail.com>
2022-12-07 20:27:21 +0100

    Use html2text in `format_alert` (#10195)

--- src/middlewared/middlewared/alert/base.py
+++ src/middlewared/middlewared/alert/base.py
@@ -5,6 +5,8 @@ import json
 import logging
 import os
 
+import html2text
+
 from middlewared.alert.schedule import IntervalSchedule
 from middlewared.utils.lang import undefined
 
@@ -323,7 +325,7 @@ def format_alerts(product_name, hostname, node_map, alerts, gone_alerts, new_ale
 
 
 def format_alert(alert, node_map):
-    return (f"{node_map[alert.node]} - " if node_map else "") + alert.formatted
+    return (f"{node_map[alert.node]} - " if node_map else "") + html2text.html2text(alert.formatted)
 
 
 def ellipsis(s, l):
