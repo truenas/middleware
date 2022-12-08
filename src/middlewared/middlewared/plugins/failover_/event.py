8ba0f0cdd46e30a9f5d349909f91ac8052358186
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-12-08 21:25:32 +0100

    NAS-119298 / 22.12 / retry pool import on invalid cachefile (by yocalebo) (#10208)
    * catch ZFSInvalidCachefileException on pool import
    
    (cherry picked from commit f58227859e644ea8020a596769bec399dbcef229)
    
    * retry import on invalid cachefile
    
    (cherry picked from commit f45f7084fb0a84c6d8716b30fd36e4b3a7ffc0af)
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/failover_/event.py
+++ src/middlewared/middlewared/plugins/failover_/event.py
@@ -13,8 +13,6 @@ from middlewared.service import Service, job, accepts
 from middlewared.schema import Dict, Bool, Int
 from middlewared.plugins.failover_.zpool_cachefile import ZPOOL_CACHE_FILE
 from middlewared.plugins.failover_.event_exceptions import AllZpoolsFailedToImport, IgnoreFailoverEvent, FencedError
-from libzfs import Error as libzfs_errnos
-
 
 logger = logging.getLogger('failover')
 
@@ -385,12 +383,9 @@ class FailoverEventsService(Service):
                     'zfs.pool.import_pool', vol['guid'], options, any_host, cachefile, new_name, import_options
                 )
             except Exception as e:
-                error = next((i.name for i in libzfs_errnos if i.value == e.errno), '')
-                if error == 'NOENT' or e.errno == errno.ENOENT:
-                    # NOENT when cachefile exists and zpool isn't found from contents in cachefile
-                    # ENONENT when the cachefile doesn't exist on disk
-                    logger.warning('Failed importing %r using cachefile so trying without it.', vol['name'])
+                if e.errno == errno.ENOENT:
                     try_again = True
+                    logger.warning('Failed importing %r using cachefile so trying without it.', vol['name'])
                 else:
                     vol['error'] = str(e)
                     failed.append(vol)
