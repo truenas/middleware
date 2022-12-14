fed16d06971bbf492dc0ff342e701c7a1c529b97
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-14 19:58:30 +0100

    Update helm.py

--- src/middlewared/middlewared/plugins/chart_releases_linux/helm.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/helm.py
@@ -9,6 +9,8 @@ from middlewared.service import CallError, private, Service
 
 from .utils import get_namespace
 
+RE_ERRCLEANUP = re.compile(r'^Error: .+[)]: ')
+
 
 class ChartReleaseService(Service):
 
@@ -18,7 +20,6 @@ class ChartReleaseService(Service):
     @private
     def helm_action(self, chart_release, chart_path, config, tn_action):
         args = ['-f']
-        CLEANUPREGEX = re.compile(r'^Error: .+[)]: ')
 
         if os.path.exists(os.path.join(chart_path, 'ix_values.yaml')):
             args.extend([os.path.join(chart_path, 'ix_values.yaml'), '-f'])
@@ -46,7 +47,7 @@ class ChartReleaseService(Service):
             if cp.returncode:
                 errmsg = stderr.decode()
                 if tn_action == 'upgrade' or tn_action == 'install':
-                    errmsg = re.sub(CLEANUPREGEX, '', errmsg, 1)
+                    errmsg = re.sub(RE_ERRCLEANUP, '', errmsg, 1)
                 raise CallError(f'Failed to {tn_action} App: {errmsg}')
 
         self.middleware.call_sync('chart.release.clear_chart_release_portal_cache', chart_release)
