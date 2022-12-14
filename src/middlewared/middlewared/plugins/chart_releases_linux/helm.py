9e68ba6e6c5a3033d08c95919184617a3782286b
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-14 12:12:23 +0100

    Update helm.py

--- src/middlewared/middlewared/plugins/chart_releases_linux/helm.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/helm.py
@@ -19,7 +19,7 @@ class ChartReleaseService(Service):
     def helm_action(self, chart_release, chart_path, config, tn_action):
         args = ['-f']
         CLEANUPREGEX = re.compile(r'^Error: .+[)]: ')
-        
+
         if os.path.exists(os.path.join(chart_path, 'ix_values.yaml')):
             args.extend([os.path.join(chart_path, 'ix_values.yaml'), '-f'])
 
