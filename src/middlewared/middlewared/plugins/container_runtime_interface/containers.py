11ee75daa210792afc96de0a494f6994d1e889e5
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-12-16 02:19:16 +0500

    Use containerd client to query/delete containers

--- src/middlewared/middlewared/plugins/container_runtime_interface/containers.py
+++ src/middlewared/middlewared/plugins/container_runtime_interface/containers.py
@@ -1,8 +1,8 @@
-import aiodocker
-
 from middlewared.schema import accepts, Str
 from middlewared.service import CallError, CRUDService, filterable
 from middlewared.utils import filter_list
+
+from .client import ContainerdClient
 
 
 class ContainerService(CRUDService):
@@ -12,12 +12,12 @@ class ContainerService(CRUDService):
         private = True
 
     @filterable
-    async def query(self, filters, options):
+    def query(self, filters, options):
         containers = []
-        async with aiodocker.Docker() as docker:
-            for container in (await docker.containers.list(all=True)):
+        with ContainerdClient('container') as client:
+            for container in client.list_containers():
                 containers.append({
-                    'id': container.id
+                    'id': container['id'],
                 })
 
         return filter_list(containers, filters, options)
@@ -25,11 +25,10 @@ class ContainerService(CRUDService):
     @accepts(
         Str('container_id'),
     )
-    async def do_delete(self, container_id):
+    def do_delete(self, container_id):
         try:
-            async with aiodocker.Docker() as docker:
-                container = docker.containers.container(container_id)
-                await container.delete(force=True)
+            with ContainerdClient('container') as client:
+                client.remove_container(container_id)
         except Exception as e:
             raise CallError(f'Unable to delete {container_id!r} container: {e}')
         else:
