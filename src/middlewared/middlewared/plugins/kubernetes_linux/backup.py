45b5b09c65ca798c399a4dd5cacdbe1b60064f29
M. Rehan <mrehanlm93@gmail.com>
2022-12-18 13:48:04 +0500

    Fix restore of apps

--- src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/backup.py
@@ -89,7 +89,14 @@ class KubernetesService(Service):
 
     @private
     def to_ignore_datasets_on_backup(self, k8s_dataset):
-        return [os.path.join(k8s_dataset, ds_name) for ds_name in ('catalogs', 'docker', 'k3s/kubelet')]
+        return {
+            os.path.join(k8s_dataset, ds_name): ds_props
+            for ds_name, ds_props in {
+                'catalogs': {'mount': True},
+                'docker': {'mount': True},
+                'k3s/kubelet': {'mount': False, 'creation_props': {'mountpoint': 'legacy'}},
+            }.items()
+        }
 
     @accepts()
     @returns(Dict('backups', additional_attrs=True))
