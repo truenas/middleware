7e90b68a30d38045db649d399f6fe9e6e1e82c2d
themylogin <themylogin@gmail.com>
2022-12-19 13:28:34 +0100

    Address review

--- src/middlewared/middlewared/plugins/pool_/format_disks.py
+++ src/middlewared/middlewared/plugins/pool_/format_disks.py
@@ -7,24 +7,25 @@ class PoolService(Service):
     @private
     async def format_disks(self, job, disks):
         """
-        Format all disks, putting all freebsd-zfs partitions created
-        into their respective vdevs and encrypting disks if specified for FreeBSD.
+        Format all disks, putting all ZFS partitions created  into their respective vdevs.
         """
         # Make sure all SED disks are unlocked
         await self.middleware.call('disk.sed_unlock_all')
         swapgb = (await self.middleware.call('system.advanced.config'))['swapondrive']
         formatted = 0
         await self.middleware.call('pool.remove_unsupported_md_devices_from_disks', disks)
+        is_ha_capable = await self.middleware.call('system.is_ha_capable')
+        len_disks = len(disks)
 
         async def format_disk(arg):
             nonlocal formatted
             disk, config = arg
             swap_size = 0
-            if config['create_swap'] and not await self.middleware.call('system.is_ha_capable'):
+            if config['create_swap'] and not is_ha_capable:
                 swap_size = swapgb
             await self.middleware.call('disk.format', disk, config.get('min_size'), swap_size, False)
             formatted += 1
-            job.set_progress(15, f'Formatting disks ({formatted}/{len(disks)})')
+            job.set_progress(15, f'Formatting disks ({formatted}/{len_disks})')
 
         await asyncio_map(format_disk, disks.items(), limit=16)
 
