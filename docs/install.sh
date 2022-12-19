ae929fe538ea7cc7c3963bb7ef0f853c7340e2c1
themylogin <themylogin@gmail.com>
2022-12-19 14:06:06 +0100

    Fix docs build

--- docs/install.sh
+++ docs/install.sh
@@ -2,4 +2,4 @@
 chmod +x /usr/bin/apt*
 apt update
 apt install -y python3-pip python3-sphinx-prompt python3-sphinx-rtd-theme
-pip3 install Sphinx-Substitution-Extensions
+pip3 install Sphinx-Substitution-Extensions==2020.9.30.0
