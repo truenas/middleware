73bc2c4c8e247d0c032939dfbd6fa163b1ce866b
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-12-20 23:55:32 +0500

    Add/remove nvidia runtime class based on nvidia gpu detection

--- src/middlewared/middlewared/plugins/kubernetes_linux/utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/utils.py
@@ -9,6 +9,7 @@ KUBEROUTER_TABLE_ID = 77
 KUBEROUTER_TABLE_NAME = 'kube-router'
 MIGRATION_NAMING_SCHEMA = 'ix-app-migrate-%Y-%m-%d_%H-%M'
 NODE_NAME = 'ix-truenas'
+NVIDIA_RUNTIME_CLASS_NAME = 'nvidia'
 OPENEBS_ZFS_GROUP_NAME = 'zfs.openebs.io'
 UPDATE_BACKUP_PREFIX = 'system-update-'
 
