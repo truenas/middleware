836754cd4eade7716bc67ea6303f4421a07cdc53
Waqar Ahmed <waqarahmedjoyia@live.com>
2022-12-22 21:16:02 +0500

    Update nvidia devie plugin tag

--- src/middlewared/middlewared/pytest/unit/plugins/container_images/test_image_update_available.py
+++ src/middlewared/middlewared/pytest/unit/plugins/container_images/test_image_update_available.py
@@ -7,11 +7,6 @@ from middlewared.pytest.unit.middleware import Middleware
 
 image_detail = {
     'id': 'sha256:f2a70e6c04c76c0e7bdb6f9fa95f62fd46f8d058d38b1cf0a0590264f7c9a4d0',
-    'labels': {
-        'architecture': 'x86_64',
-        'vendor': 'MinIO Inc <dev@min.io>',
-        'version': 'RELEASE.2022-08-25T07-17-05Z'
-    },
     'repo_tags': [
         'minio/minio:RELEASE.2022-08-25T07-17-05Z'
     ],
@@ -19,9 +14,6 @@ image_detail = {
         'minio/minio@sha256:1811ba43461b1c38a4f5db1fdab826cb3d6eecb1d7d53ff6da8902bb0ee37695'
     ],
     'size': 224978361,
-    'created': {
-        '$date': 1661389907000
-    },
     'dangling': False,
     'update_available': False,
     'system_image': False
