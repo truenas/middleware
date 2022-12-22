e4dd193c832feddbcede5f3778bfc7f47d40a05c
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-12-22 21:31:42 +0100

    Add host path validations for applications (#10276)
    (cherry picked from commit 4f4bf9b97a06e04328e8e4e90c0c598202a5a388)
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

--- src/middlewared/middlewared/pytest/unit/plugins/chart_releases/test_chart_release_validation_schema.py
+++ src/middlewared/middlewared/pytest/unit/plugins/chart_releases/test_chart_release_validation_schema.py
@@ -45,6 +45,23 @@ async def test_create_schema_formation():
     chart_release_svc = ChartReleaseService(m)
 
     m['chart.release.validate_locked_host_path'] = chart_release_svc.validate_locked_host_path
+    m['kubernetes.config'] = lambda *args: {
+        'id': 1,
+        'pool': 'pool',
+        'cluster_cidr': '172.16.0.0/16',
+        'service_cidr': '172.17.0.0/16',
+        'cluster_dns_ip': '172.17.0.10',
+        'route_v4_interface': 'ens1',
+        'route_v4_gateway': '192.168.0.1',
+        'route_v6_interface': None,
+        'route_v6_gateway': None,
+        'node_ip': '192.168.0.10',
+        'configure_gpus': True,
+        'servicelb': True,
+        'validate_host_path': False,
+        'passthrough_mode': False,
+        'dataset': 'pool/ix-applications'
+    }
     m['pool.dataset.path_in_locked_datasets'] = lambda *args: True
     with patch('middlewared.schema.HostPath.validate') as validation:
         validation.return_value = ValidationErrors()
