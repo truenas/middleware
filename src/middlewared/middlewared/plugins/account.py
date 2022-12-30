2183b88c89be98a0bc28352a0622baf13ff854f5
bugclerk <40872210+bugclerk@users.noreply.github.com>
2022-12-30 20:17:13 +0100

    add cli as a shell to user.shell_choices (#10344)
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/account.py
+++ src/middlewared/middlewared/plugins/account.py
@@ -737,7 +737,11 @@ class UserService(CRUDService):
 
         `user_id`: deprecated and ignored
         """
-        shells = {'/usr/sbin/nologin': 'nologin'}
+        shells = {
+            '/usr/sbin/nologin': 'nologin',
+            '/usr/bin/cli': 'TrueNAS CLI',  # installed via midcli
+            '/usr/bin/cli_console': 'TrueNAS Console',  # installed via midcli
+        }
         with open('/etc/shells') as f:
             for shell in filter(lambda x: x.startswith('/usr/bin'), f):
                 # on scale /etc/shells has duplicate entries like (/bin/sh, /usr/bin/sh) (/bin/bash, /usr/bin/bash) etc.
