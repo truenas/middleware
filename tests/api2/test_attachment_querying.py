742a52d65c296dcf543041d9ec248bbfd2f89e76
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-01-03 22:59:47 +0500

    Update querying attachment tests

--- tests/api2/test_attachment_querying.py
+++ tests/api2/test_attachment_querying.py
@@ -29,5 +29,8 @@ def test_attachment_with_child_path(request):
             with dataset(CHILD_DATASET) as child_dataset:
                 child_path = f'/mnt/{child_dataset}'
                 attachments = call('pool.dataset.attachments_with_path', child_path)
-                assert len(attachments) > 0, attachments
+                assert len(attachments) == 0, attachments
+
+                attachments = call('pool.dataset.attachments_with_path', child_path, True)
+                assert len(attachments) == 1, attachments
                 assert attachments[0]['type'] == 'NFS Share', attachments
