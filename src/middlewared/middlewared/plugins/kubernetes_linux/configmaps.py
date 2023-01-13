2e8036452d4151e355edb2d95b4a675069225fc0
M. Rehan <mrehanlm93@gmail.com>
2023-01-14 00:03:16 +0500

    Allow apps to use single gpu more than once

--- src/middlewared/middlewared/plugins/kubernetes_linux/configmaps.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/configmaps.py
@@ -1,18 +1,13 @@
-from middlewared.service import CRUDService, filterable
-from middlewared.utils import filter_list
+from middlewared.service import CRUDService
 
+from .k8s_base_resources import KubernetesBaseResource
 from .k8s import Configmap
 
 
-class KubernetesSecretService(CRUDService):
+class KubernetesSecretService(KubernetesBaseResource, CRUDService):
+
+    KUBERNETES_RESOURCE = Configmap
 
     class Config:
         namespace = 'k8s.configmap'
         private = True
-
-    @filterable
-    async def query(self, filters, options):
-        options = options or {}
-        label_selector = options.get('extra', {}).get('labelSelector')
-        kwargs = {k: v for k, v in [('labelSelector', label_selector)] if v}
-        return filter_list((await Configmap.query(**kwargs))['items'], filters, options)
