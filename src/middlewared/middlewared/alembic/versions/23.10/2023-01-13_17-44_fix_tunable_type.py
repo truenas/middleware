f7c70b608ebcaa97dc6a1af564186d38b7f4f339
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-01-13 13:28:41 -0500

    fix applying sysctl tunables on upgrade from 22.02 (#10432)

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-01-13_17-44_fix_tunable_type.py
@@ -0,0 +1,25 @@
+"""upper-case tun_type column in system_tunable table
+
+Revision ID: d81ede53eb14
+Revises: 82ad1e72a7f0
+Create Date: 2023-01-13 17:44:37.982722+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd81ede53eb14'
+down_revision = '82ad1e72a7f0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute('UPDATE system_tunable SET tun_type = "SYSCTL" where tun_type = "sysctl"')
+
+
+def downgrade():
+    pass
