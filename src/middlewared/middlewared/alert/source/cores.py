931464433bd126655c59bd3fde2f84981b77c46b
caleb <yocalebo@gmail.com>
2023-01-13 12:14:24 -0500

    fix old jira link

--- src/middlewared/middlewared/alert/source/cores.py
+++ src/middlewared/middlewared/alert/source/cores.py
@@ -10,7 +10,7 @@ class CoreFilesArePresentAlertClass(AlertClass):
     level = AlertLevel.WARNING
     title = "Core Files Found in System Database"
     text = ("The following system core files were found: %(corefiles)s. Please create a ticket at "
-            "https://jira.ixsystems.com/ and attach the relevant core files along with a system debug. "
+            "https://ixsystems.atlassian.net/ and attach the relevant core files along with a system debug. "
             "Once the core files have been archived and attached to the ticket, they may be removed "
             "by running the following command in shell: 'rm /var/db/system/cores/*'.")
 
