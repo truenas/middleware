2aade45a3470f39a1936829c371382f3aa09253e
themylogin <themylogin@gmail.com>
2023-01-16 15:33:21 +0100

    Do not show WebUiRootLogin alert if root password is not disabled
    (cherry picked from commit 0ea4d82b34971f93cc05857c217411d28acd3466)

--- src/middlewared/middlewared/etc_files/web_ui_root_login_alert.py
+++ src/middlewared/middlewared/etc_files/web_ui_root_login_alert.py
@@ -1,10 +1,13 @@
+from middlewared.utils import filter_list
 
 
 async def render(service, middleware, render_ctx):
-    if await middleware.call(
-            'privilege.always_has_root_password_enabled',
-            render_ctx['user.query'],
-            render_ctx['group.query'],
+    root_user = filter_list(render_ctx['user.query'], [('username', '=', 'root')], {'get': True})
+
+    if root_user['password_disabled'] and await middleware.call(
+        'privilege.always_has_root_password_enabled',
+        render_ctx['user.query'],
+        render_ctx['group.query'],
     ):
         await middleware.call('alert.oneshot_create', 'WebUiRootLogin', None)
     else:
