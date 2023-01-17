39af669e592490a1410e414eb33585e8611a8ffe
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-01-17 13:35:50 +0100

    NAS-119485 / 23.10 / Allow specifying custom ovmf for VMs (by Qubad786) (#10445)
    * Allow specifying custom ovmf
    
    (cherry picked from commit 28d823f847e6749417367c3b1a2e42f411267a48)
    
    * Add merge migration
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-01-16_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 519c9d598091
+Revises: a5584351bdb8, 67ee25d22253
+Create Date: 2023-01-16 10:51:51.496712+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '519c9d598091'
+down_revision = ('a5584351bdb8', '67ee25d22253')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
