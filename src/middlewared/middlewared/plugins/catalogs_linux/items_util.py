42b41ea1d5a225e43890e3ab232d2cb3578ca0f8
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-01-22 18:40:08 +0500

    Enhance app item version support checks

--- src/middlewared/middlewared/plugins/catalogs_linux/items_util.py
+++ src/middlewared/middlewared/plugins/catalogs_linux/items_util.py
@@ -1,3 +1,5 @@
+import typing
+
 from catalog_validation.items.items_util import (
     get_item_details as get_catalog_item_details,
     get_item_version_details as get_catalog_item_version_details,
@@ -12,7 +14,12 @@ def get_item_default_values(version_details: dict) -> dict:
     return construct_schema(version_details, {}, False)['new_values']
 
 
-def minimum_scale_version_check_update(version_details):
+def minimum_scale_version_check_update(version_details: dict) -> dict:
+    version_details['supported'] = minimum_scale_version_check_update_impl(version_details)[0]
+    return version_details
+
+
+def minimum_scale_version_check_update_impl(version_details: dict) -> typing.Tuple[bool, bool]:
     if (
         version_details['healthy'] and version_details['supported'] and version_details['chart_metadata'].get(
             'minimum_scale_version'
@@ -20,15 +27,15 @@ def minimum_scale_version_check_update(version_details):
     ):
         try:
             if manifest_version() != version_details['chart_metadata']['minimum_scale_version'] and not can_update(
-                    version_details['chart_metadata']['minimum_scale_version'], manifest_version()
+                version_details['chart_metadata']['minimum_scale_version'], manifest_version()
             ):
-                version_details['supported'] = False
+                return False, False
         except Exception:
             # In case invalid version string is specified we don't want a traceback here
             # let's just explicitly not support the app version in question
-            version_details['supported'] = False
+            return False, True
 
-    return version_details
+    return True, False
 
 
 def get_item_details(item_location: str, questions_context: dict, options: dict) -> dict:
