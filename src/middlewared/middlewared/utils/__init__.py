f623059140b6453452fe7ee354e31b4f3bb034b5
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-01-24 22:40:43 +0500

    Use sw_version for retrieving version of manifest

--- src/middlewared/middlewared/utils/__init__.py
+++ src/middlewared/middlewared/utils/__init__.py
@@ -1,5 +1,4 @@
 import asyncio
-import functools
 import logging
 import re
 import signal
@@ -266,11 +265,6 @@ def sw_version(fullname=True):
         (i.e. 22.12.0 instead of TrueNAS-SCALE-22.12.0)
     """
     return sw_info()['fullname' if fullname else 'version']
-
-
-@functools.cache
-def manifest_version():
-    return osc.get_app_version()['version']
 
 
 def sw_version_is_stable():
