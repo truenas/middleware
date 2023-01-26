9f1d399de381aee11d76eb3568d46e3f7bfe266b
themylogin <themylogin@gmail.com>
2023-01-26 23:18:01 +0100

    Merge migration
    (cherry picked from commit 06614ab06b3870e5323d69a3a10f99a2cdd2b1db)

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-01-26_22-31_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: e1c7ee02b545
+Revises: 9c44b7e06dff, 004f0934ff0f
+Create Date: 2023-01-26 22:31:27.803074+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1c7ee02b545'
+down_revision = ('9c44b7e06dff', '004f0934ff0f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
