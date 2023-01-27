ff5bdb0e23340abf5bad2bf5b4ee41976df4001d
themylogin <themylogin@gmail.com>
2023-01-27 16:32:21 +0100

    Oneshot alerts are not created/deleted synchronously, give them some time to appear/disappear

--- tests/api2/test_pool_is_upgraded_alert_removal.py
+++ tests/api2/test_pool_is_upgraded_alert_removal.py
@@ -1,7 +1,19 @@
 import contextlib
+import time
 
 from middlewared.test.integration.assets.pool import another_pool
 from middlewared.test.integration.utils import call, ssh
+
+
+def assert_has_outdated_pool_alert(pool_name, has):
+    for i in range(60):
+        alerts = call("alert.list")
+        if any((i["klass"] == "PoolUpgraded" and i["args"] == pool_name for i in alerts)) == has:
+            break
+
+        time.sleep(1)
+    else:
+        assert False, alerts
 
 
 @contextlib.contextmanager
@@ -10,6 +22,7 @@ def outdated_pool():
         device = pool["topology"]["data"][0]["path"]
         ssh(f"zpool export {pool['name']}")
         ssh(f"zpool create test -o altroot=/mnt -o feature@sha512=disabled -f {device}")
+        assert_has_outdated_pool_alert(pool["name"], True)
         yield pool
 
 
@@ -17,13 +30,11 @@ def test_outdated_pool_alert_removed_on_pool_upgrade():
     with outdated_pool() as pool:
         call("pool.upgrade", pool["id"])
 
-        alerts = call("alert.list")
-        assert not any((i["klass"] == "PoolUpgraded" and i["args"] == pool["name"] for i in alerts)), alerts
+        assert_has_outdated_pool_alert(pool["name"], False)
 
 
 def test_outdated_pool_alert_removed_on_pool_delete():
     with outdated_pool() as pool:
         pass
 
-    alerts = call("alert.list")
-    assert not any((i["klass"] == "PoolUpgraded" and i["args"] == pool["name"] for i in alerts)), alerts
+    assert_has_outdated_pool_alert(pool["name"], False)
