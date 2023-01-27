7883fac615d5bb59b792cd1516d5b3be607d9944
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-01-27 18:12:05 +0100

    NAS-119918 / 23.10 / `ssh_adminlogin` (by themylogin) (#10573)
    * `ssh_adminlogin`
    
    (cherry picked from commit 16613f05bf14e7e88b342cb75e274e1670a69d0a)
    
    * Merge migration
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-01-27_16-53_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: eaadca673130
+Revises: e1c7ee02b545, af5efb72c74f
+Create Date: 2023-01-27 16:53:39.823376+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eaadca673130'
+down_revision = ('e1c7ee02b545', 'af5efb72c74f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
