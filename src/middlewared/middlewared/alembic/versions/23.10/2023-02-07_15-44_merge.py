51073eba536797cc41a69076c18d4cf6c4057bc2
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-02-07 17:25:25 +0100

    NAS-120147 / 23.10 / Restore AUTOINCREMENT property on primary keys of the tables that wer… (by themylogin) (#10655)
    * Restore AUTOINCREMENT property on primary keys of the tables that were altered with `op.batch_alter_table`
    
    (cherry picked from commit ecdfdf1d6fe0beac25f6a15b00209a5c87816387)
    
    * Remove obsolete autoincrement checks
    
    (cherry picked from commit bcebfc4dcf512249439b3a3ae478b828e2759c99)
    
    * fix syntax error
    
    (cherry picked from commit 43cc1f34793f65b86439496bcb2b03c478487402)
    
    * Merge
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>
    Co-authored-by: Caleb St. John <30729806+yocalebo@users.noreply.github.com>

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-02-07_15-44_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 653ea1a2ba57
+Revises: eaadca673130, 1f39ac35aaeb
+Create Date: 2023-02-07 15:44:00.629175+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '653ea1a2ba57'
+down_revision = ('eaadca673130', '1f39ac35aaeb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
