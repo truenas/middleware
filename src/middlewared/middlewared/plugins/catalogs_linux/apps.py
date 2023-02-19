502ca7c6aee653ff4fa8296b9a3d5a9b8abc10c7
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-02-19 17:27:02 +0500

    Document return type of available apps endpoint

--- src/middlewared/middlewared/plugins/catalogs_linux/apps.py
+++ src/middlewared/middlewared/plugins/catalogs_linux/apps.py
@@ -1,4 +1,4 @@
-from middlewared.schema import Dict
+from middlewared.schema import Bool, Dict, List, Str
 from middlewared.service import filterable, filterable_returns, Service
 from middlewared.utils import filter_list
 
@@ -7,7 +7,20 @@ class AppService(Service):
 
     @filterable
     @filterable_returns(Dict(
-
+        'available_apps',
+        Bool('healthy', required=True),
+        List('categories', required=True),
+        Str('name', required=True),
+        Str('title', required=True),
+        Str('description', required=True),
+        Str('app_readme', required=True),
+        Str('location', required=True),
+        Str('healthy_error', required=True, null=True),
+        Str('latest_version', required=True),
+        Str('latest_app_version', required=True),
+        Str('icon_url', required=True),
+        Str('train', required=True),
+        Str('catalog', required=True),
     ))
     def available(self, filters, options):
         results = []
