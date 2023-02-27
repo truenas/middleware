139c966c775b564bb15e5bbc0402e76669b656cb
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-02-27 12:06:31 +0100

    NAS-120424 / 23.10 / Add community train to preferred trains (by Qubad786) (#10757)
    * Add community train to preferred trains
    
    (cherry picked from commit 05a925528fcae558be75ece147b238a3ce1637a1)
    
    * Add merge migration
    
    ---------
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-02-27_16-02_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 54beb9f4bdf5
+Revises: 60a953d6da3a, 9694ff0f4de6
+Create Date: 2023-02-27 16:02:00.629175+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '54beb9f4bdf5'
+down_revision = ('60a953d6da3a', '9694ff0f4de6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
