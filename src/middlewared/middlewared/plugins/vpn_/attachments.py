e3cf083fceec0a512b11d88a55afdf45fe1aa8f5
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-02-27 11:13:56 +0500

    When nobind is set, do not report openvpn client port as used

--- src/middlewared/middlewared/plugins/vpn_/attachments.py
+++ src/middlewared/middlewared/plugins/vpn_/attachments.py
@@ -16,6 +16,14 @@ class OpenVPNClientServicePortDelegate(ServicePortDelegate):
     port_fields = ['port']
     title = 'Openvpn Client Service'
 
+    async def get_ports_internal(self):
+        await self.basic_checks()
+        config = await self.middleware.call(f'{self.namespace}.config')
+        if config['nobind']:
+            return []
+        else:
+            return [config[k] for k in filter(lambda k: config.get(k), self.port_fields)]
+
 
 async def setup(middleware):
     await middleware.call('port.register_attachment_delegate', OpenVPNServerServicePortDelegate(middleware))
