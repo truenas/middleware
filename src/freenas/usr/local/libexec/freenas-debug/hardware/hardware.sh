b513aab38f06f63273921eb1dad7ff9af4f38bb1
caleb <yocalebo@gmail.com>
2023-02-28 07:27:20 -0500

    cleanup and improve hardware.sh

--- src/freenas/usr/local/libexec/freenas-debug/hardware/hardware.sh
+++ src/freenas/usr/local/libexec/freenas-debug/hardware/hardware.sh
@@ -1,73 +1,15 @@
 #!/bin/sh
 get_physical_disks_list()
 {
-	if is_linux; then
-		lsblk -ndo path | grep -v '^/dev/sr'
-	else
-		sysctl -n kern.disks | tr ' ' '\n'| grep -v '^cd' \
-			| sed 's/\([^0-9]*\)/\1 /' | sort +0 -1 +1n | tr -d ' '
-	fi
+	sysctl -n kern.disks | tr ' ' '\n'| grep -v '^cd' \
+		| sed 's/\([^0-9]*\)/\1 /' | sort +0 -1 +1n | tr -d ' '
 }
 
 
 hardware_opt() { echo h; }
 hardware_help() { echo "Dump Hardware Configuration"; }
 hardware_directory() { echo "Hardware"; }
-
-hardware_linux()
-{
-	section_header "Hardware"
-
-	echo "Machine class: $(uname -m)"
-
-	echo "Machine model: $(lscpu | grep 'Model name' | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//')"
-
-	echo "Number of active CPUs: $(grep -c 'model name' /proc/cpuinfo)"
-
-	echo "Number of CPUs online: $(lscpu -p=online | grep -v "^#" | grep -c "Y")"
-
-	echo "Current CPU frequency: $(lscpu | grep 'CPU MHz' | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//')"
-
-	echo "Physical Memory: $(getconf -a | grep PAGES | awk 'BEGIN {total = 1} {if (NR == 1 || NR == 3) total *=$NF} END {print total / 1024 / 1024 / 1024" GiB"}')"
-
-	section_footer
-
-	section_header "lspci -vvvD"
-	lspci -vvvD
-	section_footer
-
-	section_header "lshw -businfo"
-	lshw -businfo
-	section_footer
-
-	section_header "lshw"
-	lshw
-	section_footer
-
-	section_header "usb-devices"
-	usb-devices
-	section_footer
-
-	section_header "dmidecode"
-	dmidecode
-	section_footer
-
-	section_header "lsblk -o NAME,ALIGNMENT,MIN-IO,OPT-IO,PHY-SEC,LOG-SEC,ROTA,SCHED,RQ-SIZE,RA,WSAME,HCTL"
-	lsblk -o NAME,ALIGNMENT,MIN-IO,OPT-IO,PHY-SEC,LOG-SEC,ROTA,SCHED,RQ-SIZE,RA,WSAME,HCTL
-	section_footer
-
-	for disk in $(get_physical_disks_list)
-	do
-		output=$(sg_vpd --page=di "$disk" 2> /dev/null)
-		if [ $? -eq 0 ]; then
-			section_header "sg_vpd --page=di $disk"
-			echo "$output"
-			section_footer
-		fi
-	done
-}
-
-hardware_freebsd()
+hardware_func()
 {
 	section_header "Hardware"
 
@@ -138,32 +80,13 @@ hardware_freebsd()
 			section_header "camcontrol inquiry ${disk}"
 			camcontrol inquiry "${disk}"
 			section_footer
-		fi
-	done
-
-	for disk in $(get_physical_disks_list)
-	do
-		if echo "${disk}" | egrep -q '^ada[0-9]+'
+		elif echo "${disk}" | egrep -q '^ada[0-9]+'
 		then
 			section_header "camcontrol identify ${disk}"
 			camcontrol identify "${disk}"
 			section_footer
 		fi
 	done
-
-	#
-	#	This logic is being moved to the IPMI module
-	#	because we are running duplicate ipmitool commands
-	#
-	#if [ -c /dev/ipmi0 ]
-	#then
-	#	for list_type in sel sdr
-	#	do
-	#		section_header "ipmitool $list_type list"
-	#		ipmitool $list_type list
-	#		section_footer
-	#	done
-	#fi
 
 	if which getencstat > /dev/null
 	then
@@ -195,13 +118,4 @@ hardware_freebsd()
 	section_header "Enclosures (midclt call enclosure.query)"
 	midclt call enclosure.query |jq .
 	section_footer
-}
-
-hardware_func()
-{
-	if is_linux; then
-		hardware_linux
-	else
-		hardware_freebsd
-	fi
 }
