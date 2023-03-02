2a632d0eacdcf9c2a8df3a0b1e01371c61e5bdf4
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-03 02:20:38 +0500

    Move logic of re-initializing cri to separate internal service

---
+++ src/middlewared/middlewared/plugins/kubernetes_linux/cri.py
@@ -0,0 +1,35 @@
+import os
+
+from middlewared.service import Service
+
+
+class K8sCRIService(Service):
+
+    class Config:
+        namespace = 'k8s.cri'
+        private = True
+
+    async def re_initialize(self):
+        # We would be doing the following here:
+        # 1) Remove old docker dataset
+        # 2) Create new docker dataset
+        # 3) Start docker
+        # 4) Load bundled docker images
+        # 5) Call it a day
+        k8s_config = await self.middleware.call('kubernetes.config')
+        docker_ds = os.path.join(k8s_config['dataset'], 'docker')
+        if await self.middleware.call(
+            'zfs.dataset.query', [['id', '=', docker_ds]], {
+                'extra': {'retrieve_children': False, 'retrieve_properties': False}
+            }
+        ):
+            await self.middleware.call('zfs.dataset.delete', docker_ds, {'recursive': True, 'force': True})
+
+        await self.middleware.call('zfs.dataset.create', {'name': docker_ds, 'type': 'FILESYSTEM'})
+        await self.middleware.call('zfs.dataset.mount', docker_ds)
+
+        # start docker and load default images
+        await self.middleware.call('service.start', 'docker')
+        await self.middleware.call('container.image.load_default_images')
+
+        await self.middleware.call('service.stop', 'docker')
