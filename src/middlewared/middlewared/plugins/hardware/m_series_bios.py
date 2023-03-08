12fb88d3ce21f75d0762284d2a237b70493aea87
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-03-08 19:23:46 +0100

    NAS-120607 / 22.12.2 / add nvdimm and bios alerts for m-series on SCALE (by yocalebo) (#10817)
    * add mseries.nvdimm.info
    
    (cherry picked from commit bf8dd73ba6366c06937c51cdea4323cc7b2c958e)
    
    * add mseries.bios.is_old_version
    
    (cherry picked from commit 0f8fc4fb6c508aae2477adc82451d8ead821617d)
    
    * fix and improve m_series_nvdimm.py
    
    (cherry picked from commit 4969e3ac2db71d43b2bef990090f92c3e06f3d68)
    
    * cant cache it anymore
    
    (cherry picked from commit f883df1b92946811eb6747beb7d53e8d35b69a1f)
    
    * feature parity nvdimm/bios alerts with CORE
    
    (cherry picked from commit 3f6bded3925cc8b657eef94f290da4ffc993ea55)
    
    * rename to reflect reality
    
    (cherry picked from commit 013d700642487f3f193460898d7ab5e5f8b2fc9c)
    
    * fix the bugs
    
    (cherry picked from commit d0f3bb33256c5b1ae936907c8923cfdf34789b1f)
    
    ---------
    
    Co-authored-by: caleb <yocalebo@gmail.com>

---
+++ src/middlewared/middlewared/plugins/hardware/m_series_bios.py
@@ -0,0 +1,23 @@
+import datetime
+
+from middlewared.service import Service
+from middlewared.utils.functools import cache
+
+
+class MseriesBiosService(Service):
+
+    class Config:
+        private = True
+        namespace = 'mseries.bios'
+
+    @cache
+    def is_old_version(self):
+        chassis = self.middleware.call_sync("truenas.get_chassis_hardware")
+        bios_dates = {
+            "TRUENAS-M40": datetime.date(2020, 2, 20),
+            "TRUENAS-M50": datetime.date(2020, 12, 3),
+            "TRUENAS-M60": datetime.date(2020, 12, 3),
+        }
+        min_bios_date = next((v for k, v in bios_dates.items() if chassis.startswith(k)), None)
+        if min_bios_date and (bios := self.middleware.call_sync("system.dmidecode_info")["bios-release-date"]):
+            return bios < min_bios_date
