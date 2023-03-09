caa9f71a500138c23406f946e75faeafe83ef088
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-03-09 14:09:18 -0500

    NAS-120685 / 23.10 / Fix system reboot alerts for SCALE HA (#10834)
    * fix unscheduled_reboot_alert.py
    
    * fix, improve, move unscheduled_reboot_alert.py
    
    * dont redefine global constants
    
    * remove unused import
    
    * separate the alerts into alert directory
    
    * alerts to their own file to match current paradigm

---
+++ src/middlewared/middlewared/plugins/failover_/scheduled_reboot_alert.py
@@ -0,0 +1,58 @@
+import datetime
+import os
+
+WATCHDOG_ALERT_FILE = "/data/sentinels/.watchdog-alert"
+FENCED_ALERT_FILE = "/data/sentinels/.fenced-alert"
+
+
+def get_fqdn(middleware):
+    gc = middleware.call_sync("datastore.config", "network.globalconfiguration")
+    key = "gc_hostname_a"
+    if middleware.call_sync("failover.node") == "B":
+        key = "gc_hostname_b"
+
+    return f"{gc[key]}.{gc['gc_domain']}" if gc["gc_domain"] else gc[key]
+
+
+def get_sentinel_files_time_and_clean_them_up(middleware):
+    watchdog_time = fenced_time = None
+    try:
+        os.makedirs(os.path.dirname(FENCED_ALERT_FILE), exist_ok=True)
+    except Exception:
+        middleware.logger.error('Unhandled exceptin creating sentinels directory', exc_info=True)
+    else:
+        for idx, i in enumerate((WATCHDOG_ALERT_FILE, FENCED_ALERT_FILE)):
+            try:
+                with open(i) as f:
+                    time = float(f.read().strip())
+                    if idx == 0:
+                        watchdog_time = time
+                    else:
+                        fenced_time = time
+
+                # if file exists, we've gotten the time from it so remove it
+                os.unlink(i)
+            except (FileNotFoundError, ValueError):
+                pass
+
+    return watchdog_time, fenced_time
+
+
+def setup_impl(middleware):
+    if not middleware.call_sync("core.is_starting_during_boot") or not middleware.call_sync("failover.licensed"):
+        return
+
+    now = datetime.datetime.now().strftime("%c")
+    fqdn = get_fqdn(middleware)
+    watchdog_time, fenced_time = get_sentinel_files_time_and_clean_them_up(middleware)
+    if watchdog_time and (not fenced_time or watchdog_time > fenced_time):
+        middleware.call_sync("alert.oneshot_create", "FailoverReboot", {'fqdn': fqdn, 'now': now})
+    elif fenced_time:
+        middleware.call_sync("alert.oneshot_create", "FencedReboot", {'fqdn': fqdn, 'now': now})
+    else:
+        middleware.call_sync("alert.oneshot_delete", "FencedReboot")
+        middleware.call_sync("alert.oneshot_delete", "FailoverReboot")
+
+
+async def setup(middleware):
+    await middleware.run_in_thread(setup_impl, middleware)
