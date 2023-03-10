171ba1e1b307908426d8476e0de68c706069d1d4
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-03-10 17:09:23 +0100

    NAS-120955 / 22.12.2 / Ensure required cgroup controllers are always present before starting apps (by sonicaj) (#10844)
    * Add changes to ensure that required cgroups are always present before attempting to start apps
    
    * Document source of changes
    
    * Dismiss alert if validation of k8s setup works
    
    ---------
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

--- src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
@@ -2,6 +2,7 @@ import asyncio
 import errno
 import json
 import os
+import re
 import shutil
 import uuid
 
@@ -14,6 +15,7 @@ from middlewared.utils import run
 from .k8s.config import reinitialize_config
 
 
+RE_CGROUP_CONTROLLERS = re.compile(r'(\w+)\s+')
 START_LOCK = asyncio.Lock()
 
 
@@ -195,6 +197,41 @@ class KubernetesService(Service):
         }
 
     @private
+    def ensure_cgroups_are_setup(self):
+        # Logic copied over from kubernetes
+        # https://github.com/kubernetes/kubernetes/blob/08fbe92fa76d35048b4b4891b41fc6912e689cc7/
+        # pkg/kubelet/cm/cgroup_manager_linux.go#L238
+        supported_controllers = {'cpu', 'cpuset', 'memory', 'hugetlb', 'pids'}
+        cgroup_root_path = '/sys/fs/cgroup'
+        system_supported_controllers_path = os.path.join(cgroup_root_path, 'cgroup.controllers')
+        try:
+            with open(system_supported_controllers_path, 'r') as f:
+                available_controllers = set(RE_CGROUP_CONTROLLERS.findall(f.read()))
+        except FileNotFoundError:
+            raise CallError(
+                'Unable to determine available cgroup controllers as '
+                f'{system_supported_controllers_path!r} does not exist'
+            )
+
+        needed_controllers = supported_controllers & available_controllers
+        system_available_controllers_path = os.path.join(cgroup_root_path, 'cgroup.subtree_control')
+        try:
+            with open(system_available_controllers_path, 'r') as f:
+                available_controllers_for_consumption = set(RE_CGROUP_CONTROLLERS.findall(f.read()))
+        except FileNotFoundError:
+            raise CallError(
+                'Unable to determine cgroup controllers which are available for consumption as '
+                f'{system_available_controllers_path!r} does not exist'
+            )
+
+        missing_controllers = needed_controllers - available_controllers_for_consumption
+        if missing_controllers:
+            raise CallError(
+                f'Missing {", ".join(missing_controllers)!r} cgroup controller(s) '
+                'which are required for apps to function'
+            )
+
+    @private
     async def validate_k8s_fs_setup(self):
         config = await self.middleware.call('kubernetes.config')
         if not await self.middleware.call('pool.query', [['name', '=', config['pool']]]):
@@ -240,6 +277,7 @@ class KubernetesService(Service):
         if errors:
             raise CallError(str(errors))
 
+        await self.middleware.call('kubernetes.ensure_cgroups_are_setup')
         await self.middleware.call('k8s.migration.scale_version_check')
 
     @private
@@ -405,10 +443,10 @@ class KubernetesService(Service):
                     'ApplicationsConfigurationFailed',
                     {'error': e.errmsg},
                 )
-            else:
-                await self.middleware.call('alert.oneshot_delete', 'ApplicationsConfigurationFailed', None)
 
             raise
+
+        await self.middleware.call('alert.oneshot_delete', 'ApplicationsConfigurationFailed', None)
 
 
 async def _event_system(middleware, event_type, args):
