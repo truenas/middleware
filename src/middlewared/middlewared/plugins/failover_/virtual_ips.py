1548644593f7dd8d740a0a313ca6774f990eeef0
caleb <yocalebo@gmail.com>
2023-03-17 19:30:34 -0400

    cleanup while im here
    (cherry picked from commit 47e0af5ab7f9f0e571940dd72563c248ba689574)

--- src/middlewared/middlewared/plugins/failover_/virtual_ips.py
+++ src/middlewared/middlewared/plugins/failover_/virtual_ips.py
@@ -17,31 +17,23 @@ class DetectVirtualIpStates(Service):
         masters, backups = [], []
 
         # get failover group id for `iface`
-        group_id = [
-            group for group, names in groups.items() if ifname in names
-        ][0]
+        group_id = [group for group, names in groups.items() if ifname in names][0]
 
         # get all interfaces in `group_id`
-        ids = [
-            names for group, names in groups.items() if group == group_id
-        ][0]
+        ids = [names for group, names in groups.items() if group == group_id][0]
 
         # need to remove the passed in `ifname` from the list
         ids.remove(ifname)
 
-        # if the user provided VIP(s) is/are missing from the interface
-        # then it's considered "BACKUP" if the interface has the VIP(s)
-        # then it's considered "MASTER"
-        if len(ids):
-            # we can have more than one interface in the failover
-            # group so check the state of the interface
-            for i in ids:
-                iface = netif.get_interface(i)
-                for j in iface.vrrp_config:
-                    if j['state'] == 'MASTER':
-                        masters.append(i)
-                    else:
-                        backups.append(i)
+        # we can have more than one interface in the failover
+        # group so check the state of the interface
+        for i in ids:
+            iface = netif.get_interface(i)
+            for j in iface.vrrp_config:
+                if j['state'] == 'MASTER':
+                    masters.append(i)
+                else:
+                    backups.append(i)
 
         return masters, backups
 
@@ -63,9 +55,7 @@ class DetectVirtualIpStates(Service):
         return masters, backups, inits
 
     async def check_states(self, local, remote):
-
         errors = []
-
         interfaces = set(local[0] + local[1] + remote[0] + remote[1])
         if not interfaces:
             errors.append('There are no failover interfaces')
