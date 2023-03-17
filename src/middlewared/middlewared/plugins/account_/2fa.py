ddcac2d32509d21f16424e2b2c52e1b75a620701
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-17 20:21:42 +0500

    Add endpoint to verify user otp

--- src/middlewared/middlewared/plugins/account_/2fa.py
+++ src/middlewared/middlewared/plugins/account_/2fa.py
@@ -1,6 +1,6 @@
 import pyotp
 
-from middlewared.schema import accepts, Int, returns, Str
+from middlewared.schema import accepts, Bool, Int, returns, Str
 from middlewared.service import CallError, Service
 
 
@@ -26,3 +26,20 @@ class UserService(Service):
             f'{await self.middleware.call("system.hostname")}@{await self.middleware.call("system.product_name")}',
             'iXsystems'
         )
+
+    @accepts(Int('user_id'), Str('token'))
+    @returns(Bool('token_verified'))
+    def verify_twofactor_token(self, user_id, token):
+        """
+        Returns boolean true if provided `token` is successfully authenticated for `user_id`.
+        """
+        twofactor_config = await self.middleware.call('auth.twofactor.config')
+        if not twofactor_config['enabled']:
+            raise CallError('Please enable Two Factor Authentication first')
+
+        user_twofactor_config = await self.middleware.call('auth.twofactor.get_user_twofactor_config', user_id)
+        totp = pyotp.totp.TOTP(
+            user_twofactor_config['secret'], interval=twofactor_config['interval'],
+            digits=twofactor_config['otp_digits'],
+        )
+        return totp.verify(token, valid_window=twofactor_config['window'])
