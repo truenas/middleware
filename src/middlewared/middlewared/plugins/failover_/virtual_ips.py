e284d492b33a2046f22ca40df6e773234f5c5147
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-03-18 00:37:18 +0100

    NAS-121074 / 22.12.2 / fix failover.vip.get_states (by yocalebo) (#10900)
    * fix failover.vip.get_states
    
    (cherry picked from commit 4f75afa9886117f464f8d1a8d7558f401268abe8)
    
    * cleanup while im here
    
    (cherry picked from commit 47e0af5ab7f9f0e571940dd72563c248ba689574)
    
    ---------
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/failover_/virtual_ips.py
+++ src/middlewared/middlewared/plugins/failover_/virtual_ips.py
@@ -17,32 +17,23 @@ class DetectVirtualIpStates(Service):
         masters, backups = [], []
 
         # get failover group id for `iface`
-        group_id = [
-            group for group, names in groups.items() if ifname in names
-        ][0]
+        group_id = [group for group, names in groups.items() if ifname in names][0]
 
         # get all interfaces in `group_id`
-        ids = [
-            names for group, names in groups.items() if group == group_id
-        ][0]
+        ids = [names for group, names in groups.items() if group == group_id][0]
 
         # need to remove the passed in `ifname` from the list
         ids.remove(ifname)
 
-        # if the user provided VIP(s) is/are missing from the interface
-        # then it's considered "BACKUP" if the interface has the VIP(s)
-        # then it's considered "MASTER"
-        if len(ids):
-
-            # we can have more than one interface in the failover
-            # group so check the state of the interface
-            for i in ids:
-                iface = netif.get_interface(i)
-                for j in iface.vrrp_config:
-                    if j['state'] == 'MASTER':
-                        masters.append(i)
-                    else:
-                        backups.append(i)
+        # we can have more than one interface in the failover
+        # group so check the state of the interface
+        for i in ids:
+            iface = netif.get_interface(i)
+            for j in iface.vrrp_config:
+                if j['state'] == 'MASTER':
+                    masters.append(i)
+                else:
+                    backups.append(i)
 
         return masters, backups
 
@@ -53,23 +44,18 @@ class DetectVirtualIpStates(Service):
             interfaces = await self.middleware.call('interface.query')
 
         int_ifaces = await self.middleware.call('interface.internal_interfaces')
-
-        for i in filter(lambda x: x['name'] not in int_ifaces, interfaces):
-            if not i.get('failover_critical', False) or not i['state']['vrrp_config']:
-                continue
-
-            vrrp_state = i['state']['vrrp_config'][0]['state']
-            if vrrp_state == 'MASTER':
-                masters.append(i['name'])
-            elif vrrp_state == 'BACKUP':
-                backups.append(i['name'])
+        for i in filter(lambda x: x['name'] not in int_ifaces and x['state']['vrrp_config'], interfaces):
+            if i['state']['link_state'] == 'LINK_STATE_UP':
+                vrrp_state = i['state']['vrrp_config'][0]['state']
+                if vrrp_state == 'MASTER':
+                    masters.append(i['name'])
+                elif vrrp_state == 'BACKUP':
+                    backups.append(i['name'])
 
         return masters, backups, inits
 
     async def check_states(self, local, remote):
-
         errors = []
-
         interfaces = set(local[0] + local[1] + remote[0] + remote[1])
         if not interfaces:
             errors.append('There are no failover interfaces')
