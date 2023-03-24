5ffa031b177d4a8592275b0d5a6dd814c229d8d8
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-24 15:18:48 +0500

    Add unit test to validate white listing namespaces with port validate
    (cherry picked from commit 4fafa478b7e30cf1fc3341ccac1e1a717d83a892)

--- src/middlewared/middlewared/pytest/unit/plugins/test_port_attachments.py
+++ src/middlewared/middlewared/pytest/unit/plugins/test_port_attachments.py
@@ -1,73 +1,583 @@
+import contextlib
 import pytest
 
 from unittest.mock import patch
 
-from middlewared.plugins.ports.ports import PortService
+from middlewared.plugins.ports.ports import PortService, ValidationErrors
 from middlewared.pytest.unit.middleware import Middleware
 
 
-PORTS_IN_USED = [
+PORTS_IN_USE = [
+    {
+        'namespace': 'snmp',
+        'title': 'SNMP Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                160
+            ],
+            [
+                '0.0.0.0',
+                161
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        160
+                    ],
+                    [
+                        '0.0.0.0',
+                        161
+                    ]
+                ]
+            }
+        ]
+    },
     {
         'namespace': 'ssh',
         'title': 'SSH Service',
-        'ports': [[
-            '0.0.0.0',
-            22100
-        ]],
-        'port_details': [{
-            'description': None,
-            'ports': [[
+        'ports': [
+            [
                 '0.0.0.0',
-                22100
-            ]]
-        }]
+                22
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        22
+                    ]
+                ]
+            }
+        ]
     },
     {
         'namespace': 'tftp',
         'title': 'TFTP Service',
-        'ports': [[
-            '192.168.0.23',
-            6900
-        ]],
-        'port_details': [{
-            'description': None,
-            'ports': [[
-                '192.168.0.23',
-                6900
-            ]]
-        }]
+        'ports': [
+            [
+                '0.0.0.0',
+                69
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        69
+                    ]
+                ]
+            }
+        ]
     },
+    {
+        'namespace': 'chart.release',
+        'title': 'Applications',
+        'ports': [
+            [
+                '0.0.0.0',
+                21027
+            ],
+            [
+                '0.0.0.0',
+                22000
+            ],
+            [
+                '0.0.0.0',
+                22000
+            ],
+            [
+                '0.0.0.0',
+                8384
+            ],
+            [
+                '0.0.0.0',
+                32400
+            ],
+            [
+                '0.0.0.0',
+                22601
+            ],
+            [
+                '0.0.0.0',
+                59372
+            ],
+            [
+                '0.0.0.0',
+                14831
+            ],
+            [
+                '0.0.0.0',
+                20489
+            ]
+        ],
+        'port_details': [
+            {
+                'description': '\'syncthing\' application',
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        21027
+                    ],
+                    [
+                        '0.0.0.0',
+                        22000
+                    ],
+                    [
+                        '0.0.0.0',
+                        22000
+                    ],
+                    [
+                        '0.0.0.0',
+                        8384
+                    ]
+                ]
+            },
+            {
+                'description': '\'plex\' application',
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        32400
+                    ],
+                    [
+                        '0.0.0.0',
+                        22601
+                    ],
+                    [
+                        '0.0.0.0',
+                        59372
+                    ],
+                    [
+                        '0.0.0.0',
+                        14831
+                    ]
+                ]
+            },
+            {
+                'description': '\'netdata\' application',
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        20489
+                    ]
+                ]
+            },
+        ]
+    },
+    {
+        'namespace': 'kmip',
+        'title': 'KMIP Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                5696
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        5696
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'rsyncd',
+        'title': 'Rsyncd Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                11000
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        11000
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'webdav',
+        'title': 'Webdav Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                10258
+            ],
+            [
+                '0.0.0.0',
+                14658
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        10258
+                    ],
+                    [
+                        '0.0.0.0',
+                        14658
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'smb',
+        'title': 'SMB Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                137
+            ],
+            [
+                '0.0.0.0',
+                138
+            ],
+            [
+                '0.0.0.0',
+                139
+            ],
+            [
+                '0.0.0.0',
+                445
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        137
+                    ],
+                    [
+                        '0.0.0.0',
+                        138
+                    ],
+                    [
+                        '0.0.0.0',
+                        139
+                    ],
+                    [
+                        '0.0.0.0',
+                        445
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 's3',
+        'title': 'S3 Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                8703
+            ],
+            [
+                '0.0.0.0',
+                9010
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        8703
+                    ],
+                    [
+                        '0.0.0.0',
+                        9010
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'kubernetes',
+        'title': 'Kubernetes Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                6443
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        6443
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'ftp',
+        'title': 'FTP Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                3730
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        3730
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'openvpn.server',
+        'title': 'Openvpn Server Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                1194
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        1194
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'system.general',
+        'title': 'WebUI Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                80
+            ],
+            [
+                '0.0.0.0',
+                443
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        80
+                    ],
+                    [
+                        '0.0.0.0',
+                        443
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'reporting',
+        'title': 'Reporting Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                2003
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        2003
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'iscsi.global',
+        'title': 'iSCSI Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                3260
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        3260
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'nfs',
+        'title': 'NFS Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                2049
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        2049
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'namespace': 'gluster.fuse',
+        'title': 'Gluster Service',
+        'ports': [
+            [
+                '0.0.0.0',
+                24007
+            ],
+            [
+                '0.0.0.0',
+                24008
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        24007
+                    ],
+                    [
+                        '0.0.0.0',
+                        24008
+                    ]
+                ]
+            }
+        ]
+    },
+    {
+        'title': 'System',
+        'ports': [
+            [
+                '0.0.0.0',
+                67
+            ],
+            [
+                '0.0.0.0',
+                123
+            ],
+            [
+                '0.0.0.0',
+                3702
+            ],
+            [
+                '0.0.0.0',
+                5353
+            ],
+            [
+                '0.0.0.0',
+                6000
+            ]
+        ],
+        'port_details': [
+            {
+                'description': None,
+                'ports': [
+                    [
+                        '0.0.0.0',
+                        67
+                    ],
+                    [
+                        '0.0.0.0',
+                        123
+                    ],
+                    [
+                        '0.0.0.0',
+                        3702
+                    ],
+                    [
+                        '0.0.0.0',
+                        5353
+                    ],
+                    [
+                        '0.0.0.0',
+                        6000
+                    ]
+                ]
+            }
+        ],
+        'namespace': 'system'
+    }
 ]
 
 
-@pytest.mark.parametrize('port,bindip', [
-    (6900, '192.168.0.23'),
-    (6900, '0.0.0.0'),
-    (22100, '192.168.0.23'),
-    (22100, '0.0.0.0'),
-    (22100, '192.168.0.24')
-])
-@pytest.mark.asyncio
-async def test_port_delegate_validation_with_invalid_port_binding(port, bindip):
-    m = Middleware()
+@contextlib.contextmanager
+def get_port_service():
     with patch('middlewared.plugins.ports.ports.PortService.get_in_use') as get_in_use_port:
-        get_in_use_port.return_value = PORTS_IN_USED
-        verrors = await PortService(m).validate_port('test', port, bindip)
-        assert [e.errmsg for e in verrors.errors] != []
+        get_in_use_port.return_value = PORTS_IN_USE
+        yield PortService(Middleware())
 
 
 @pytest.mark.parametrize('port,bindip,whitelist_namespace', [
-    (6900, '192.168.0.24', None),
-    (6000, '0.0.0.0', None),
-    (6400, '192.168.0.23', None),
-    (22100, '192.168.0.23', 'ssh'),
-    (6900, '0.0.0.0', 'tftp'),
-
+    (67, '0.0.0.0', 'system'),
+    (67, '192.168.0.12', 'system'),
+    (24007, '0.0.0.0', 'gluster.fuse'),
+    (24007, '192.168.0.12', 'gluster.fuse'),
+    (21027, '0.0.0.0', 'chart.release'),
+    (21027, '192.168.0.12', 'chart.release'),
 ])
 @pytest.mark.asyncio
-async def test_port_delegate_validation_with_valid_port_binding(port, bindip, whitelist_namespace):
-    m = Middleware()
-    with patch('middlewared.plugins.ports.ports.PortService.get_in_use') as get_in_use_port:
-        get_in_use_port.return_value = PORTS_IN_USED
-        verrors = await PortService(m).validate_port('test', port, bindip, whitelist_namespace)
-        assert [e.errmsg for e in verrors.errors] == []
+async def test_port_validate_whitelist_namespace_logic(port, bindip, whitelist_namespace):
+    with get_port_service() as port_service:
+        with pytest.raises(ValidationErrors):
+            await port_service.validate_port('test', port, bindip, raise_error=True)
+
+        assert (await port_service.validate_port('test', port, bindip, whitelist_namespace)).errors == []
