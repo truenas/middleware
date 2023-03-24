dadc8c9965a29c5d0f42e3a84d351765d543e8c6
caleb <yocalebo@gmail.com>
2023-03-24 14:54:50 -0400

    fix nvdimm alerts
    (cherry picked from commit d7b59adc9d2bf580252128c96f642cfee72e7c7e)

--- src/middlewared/middlewared/alert/source/mseries_nvdimm_and_bios.py
+++ src/middlewared/middlewared/alert/source/mseries_nvdimm_and_bios.py
@@ -1,5 +1,3 @@
-import subprocess
-import json
 import datetime
 
 from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
@@ -42,22 +40,6 @@ class NVDIMMFirmwareVersionAlertClass(AlertClass):
     proactive_support = True
 
 
-class NVDIMMUnknownModelAlertClass(AlertClass):
-    category = AlertCategory.HARDWARE
-    level = AlertLevel.WARNING
-    title = 'Unknown NVDIMM Model'
-    text = 'Unknown NVDIMM Model: dev: "%(dev)s" %(size)dGB %(clock_speed)dMHz'
-    products = ('SCALE_ENTERPRISE',)
-
-
-class NVDIMMUnknownFirmwareAlertClass(AlertClass):
-    category = AlertCategory.HARDWARE
-    level = AlertLevel.WARNING
-    title = 'Unknown NVDIMM Firmware Version'
-    text = 'Unknown NVDIMM Firmware Version: dev: "%(dev)s" firmware version: "%(fwver)s"'
-    products = ('SCALE_ENTERPRISE',)
-
-
 class OldBiosVersionAlertClass(AlertClass):
     category = AlertCategory.HARDWARE
     level = AlertLevel.WARNING
@@ -74,73 +56,56 @@ class NVDIMMAndBIOSAlertSource(ThreadedAlertSource):
     schedule = IntervalSchedule(datetime.timedelta(minutes=5))
     products = ('SCALE_ENTERPRISE',)
 
-    def get_ndctl_output(self):
-        output = []
-        try:
-            rv = subprocess.run(['ndctl', 'list', '-D', '--health'], stdout=subprocess.PIPE)
-            output = json.loads(rv.stdout.decode())
-        except (subprocess.CalledProcessError, json.JSONDecodeError):
-            self.middleware.logger.warning('Failed to query health information for nvdimm(s).', exc_info=True)
-
-        return output
-
-    def generate_lifetime_alerts_and_get_health_states(self, alerts, health_states):
-        for nvdimm in self.get_ndctl_output():
-            lifetime = 100 - nvdimm['health']['life_used_percentage']
-            alert = None
-            if lifetime < 10:
-                alert = NVDIMMLifetimeCriticalAlertClass
-            elif lifetime < 20:
-                alert = NVDIMMLifetimeWarningAlertClass
-
-            if alert is not None:
-                alerts.append(Alert(alert, {'dev': nvdimm['dev'], 'value': lifetime}))
-
-            health_states[nvdimm['dev']] = nvdimm['health']['health_state']
-
-    def generate_firmware_and_health_state_and_bios_alerts(self, alerts, health_states, old_bios):
-        model_to_fw = {
-            (16, 2666): ['2.1', '2.2', '2.4'],
-            (16, 2933): ['2.2'],
-            (32, 2933): ['2.4'],
-        }
+    def produce_alerts(nvdimm, alerts, old_bios):
+        persistency_restored = 0x4
+        arm_info = 0x40
+        dev = nvdimm['dev']
         old_bios_alert_already_generated = old_bios
-        for i in self.middleware.call_sync('mseries.nvdimm.info'):
-            model = (i['size'], i['clock_speed'])
-            if model not in model_to_fw:
+        for _hex, vals in nvdimm['critical_health_info'].items():
+            hex_int = int(_hex, 16)
+            if hex_int & ~(persistency_restored | arm_info):
                 alerts.append(Alert(
-                    NVDIMMUnknownModelAlertClass,
-                    {'dev': i['dev'], 'size': i['size'], 'clock_speed': i['clock_speed']}
+                    NVDIMMAlertClass,
+                    {'dev': dev, 'value': _hex, 'status': ','.join(vals)}
                 ))
-            elif i['firmware_version'] is None:
-                alerts.append(Alert(
-                    NVDIMMUnknownFirmwareAlertClass,
-                    {'dev': i['dev'], 'fwver': 'UNKNOWN'}
-                ))
-            else:
-                if i['firmware_version'] not in model_to_fw[model]:
-                    alerts.append(Alert(NVDIMMFirmwareVersionAlertClass, {'dev': i['dev']}))
 
-                if (value := health_states.get(i['dev'])) is not None and value != 'ok':
+            if nvdimm['specrev'] >= 22 and not (hex_int & arm_info):
+                alerts.append(Alert(
+                    NVDIMMAlertClass,
+                    {'dev': dev, 'value': 'ARM_INFO', 'status': 'not set'}
+                ))
+
+        for i in ('nvm_health_info', 'nvm_error_threshold_status', 'nvm_warning_threshold_status'):
+            for _hex, vals in nvdimm[i].items():
+                if int(_hex, 16) != 0:
                     alerts.append(Alert(
                         NVDIMMAlertClass,
-                        {'dev': i['dev'], 'value': value, 'status': i['module_health']}
+                        {'dev': dev, 'value': _hex, 'status': ','.join(vals)}
                     ))
 
-                if not old_bios_alert_already_generated and i['old_bios']:
-                    alerts.append(Alert(OldBiosVersionAlertClass))
-                    old_bios_alert_already_generated = True
+        for i in ('nvm_lifetime', 'es_lifetime'):
+            val = int(nvdimm[i].rstrip('%'))
+            if val < 20:
+                alert = NVDIMMLifetimeWarningAlertClass if val > 10 else NVDIMMLifetimeCriticalAlertClass
+                name = dev if i == 'nvm_lifetime' else 'nvm energy source'
+                alerts.append(Alert(alert, {'dev': name, 'value': val}))
+
+        if nvdimm['running_firmware'] not in nvdimm['qualified_firmware']:
+            alerts.append(Alert(NVDIMMFirmwareVersionAlertClass, {'dev': dev}))
+
+        if not old_bios_alert_already_generated and nvdimm['old_bios']:
+            alerts.append(Alert(OldBiosVersionAlertClass))
+            old_bios_alert_already_generated = True
 
     def check_sync(self):
         alerts = []
-        health_states = {}
         sys = ('TRUENAS-M40', 'TRUENAS-M50', 'TRUENAS-M60')
         if self.middleware.call_sync('truenas.get_chassis_hardware').startswith(sys):
             old_bios = self.middleware.call_sync('mseries.bios.is_old_version')
             if old_bios:
                 alerts.append(Alert(OldBiosVersionAlertClass))
 
-            self.generate_lifetime_alerts_and_get_health_states(alerts, health_states)
-            self.generate_firmware_and_health_state_and_bios_alerts(alerts, health_states, old_bios)
+            for nvdimm in self.middleware.call_sync('mserires.nvdimm.info'):
+                self.produce_lifetime_alerts(nvdimm, alerts, old_bios)
 
         return alerts
