692017aa70c0c1460483661de10a07ec4942fe57
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-24 22:24:51 +0500

    Only include VIP for HA capable machine in k8s settings
    (cherry picked from commit 3ab7e08647a923f315f5f1269ed190313330688f)

--- src/middlewared/middlewared/plugins/kubernetes_linux/update.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/update.py
@@ -492,7 +492,11 @@ class KubernetesService(ConfigService):
         """
         return {
             d['address']: d['address'] for d in await self.middleware.call(
-                'interface.ip_in_use', {'static': True, 'any': True, 'ipv6': False}
+                'interface.ip_in_use', {
+                    'static': True,
+                    'any': not (await self.middleware.call('system.is_ha_capable')),
+                    'ipv6': False
+                }
             )
         }
 
