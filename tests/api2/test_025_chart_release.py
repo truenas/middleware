2dcf51bca863f5e89dd392711966c06c150ed094
ericbsd <ericturgeon.bsd@gmail.com>
2023-03-27 13:52:22 -0300

    Changed ipfs train to community in chart release api test
    and kubernetes backup chart release api test

--- tests/api2/test_025_chart_release.py
+++ tests/api2/test_025_chart_release.py
@@ -52,7 +52,7 @@ if not ha:
             "item_name": "ipfs",
             "item_version_details": {
                 "catalog": "OFFICIAL",
-                "train": 'charts'
+                "train": 'community'
             }
         }
         results = POST('/catalog/get_item_details/', payload)
@@ -69,7 +69,7 @@ if not ha:
             'catalog': 'OFFICIAL',
             'item': 'ipfs',
             'release_name': 'ipfs',
-            'train': 'charts',
+            'train': 'community',
             'version': ipfs_version
         }
         results = POST('/chart/release/', payload)
@@ -91,7 +91,7 @@ if not ha:
         results = GET(f'/chart/release/id/{release_id}/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
-        assert results.json()['catalog_train'] == 'charts', results.text
+        assert results.json()['catalog_train'] == 'community', results.text
 
     def test_09_get_ipfs_chart_release_name(request):
         depends(request, ['release_ipfs'])
