9183608efc3370bed4574c1f1b7ee9f5bbd4ac53
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-29 01:37:11 +0500

    Use usernames for provisioning uris and verifying tokens

--- src/middlewared/middlewared/plugins/account_/2fa.py
+++ src/middlewared/middlewared/plugins/account_/2fa.py
@@ -1,21 +1,21 @@
 import pyotp
 
-from middlewared.schema import accepts, Bool, Int, returns, Str
+from middlewared.schema import accepts, Bool, returns, Str
 from middlewared.service import CallError, Service
 
 
 class UserService(Service):
 
-    @accepts(Int('user_id'))
+    @accepts(Str('username'))
     @returns(Str(title='Provisioning URI'))
-    async def provisioning_uri(self, user_id):
+    async def provisioning_uri(self, username):
         """
-        Returns the provisioning URI for the OTP for `user_id`. This can then be encoded in a QR code and used
+        Returns the provisioning URI for the OTP for `username`. This can then be encoded in a QR code and used
         to provision an OTP app like Google Authenticator.
         """
-        user = await self.middleware.call('user.get_instance', user_id)
+        user = await self.middleware.call('user.query', [['username', '=', username]], {'get': True})
         twofactor_config = await self.middleware.call('auth.twofactor.config')
-        user_twofactor_config = await self.middleware.call('auth.twofactor.get_user_config', user_id)
+        user_twofactor_config = await self.middleware.call('auth.twofactor.get_user_config', user['id'])
         if not user_twofactor_config['secret']:
             raise CallError(f'{user["username"]!r} user does not has two factor authentication configured')
 
@@ -27,17 +27,18 @@ class UserService(Service):
             'iXsystems'
         )
 
-    @accepts(Int('user_id'), Str('token', null=True))
+    @accepts(Str('username'), Str('token', null=True))
     @returns(Bool('token_verified'))
-    def verify_twofactor_token(self, user_id, token):
+    def verify_twofactor_token(self, username, token):
         """
-        Returns boolean true if provided `token` is successfully authenticated for `user_id`.
+        Returns boolean true if provided `token` is successfully authenticated for `username`.
         """
         twofactor_config = self.middleware.call_sync('auth.twofactor.config')
         if not twofactor_config['enabled']:
             raise CallError('Please enable Two Factor Authentication first')
 
-        user_twofactor_config = self.middleware.call_sync('auth.twofactor.get_user_config', user_id)
+        user = await self.middleware.call('user.query', [['id', '=', username]], {'get': True})
+        user_twofactor_config = self.middleware.call_sync('auth.twofactor.get_user_config', user['id'])
         totp = pyotp.totp.TOTP(
             user_twofactor_config['secret'], interval=twofactor_config['interval'],
             digits=twofactor_config['otp_digits'],
