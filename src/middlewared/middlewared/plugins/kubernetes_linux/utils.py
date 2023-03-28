5141c83d1db60b7450a30be3efbe072923a19022
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-03-28 23:13:17 +0500

    Add ix-cgroups systemd unit file
    (cherry picked from commit 898dc3bb3b84d914199d40e1846b87418b60ee2a)
    
    Move cgroups changes to system namespace
    
    (cherry picked from commit ed003e013dfdd84b1d84b016e9d7ce39d0262531)
    
    Change cgroup endpoint name
    
    (cherry picked from commit 7ea8c192c87f99bdff5fa9ae05f648f53c301f93)
    
    Update cgrups systemd unit
    
    (cherry picked from commit b82ecfe601b6655700a377c79888beb2fc7df8b4)
    
    Remove cgroups logic from k8s plugin
    
    (cherry picked from commit 6efdbeee4dc240b6b9578d1fa1ae2d27397ac869)
    
    Update description of cgroups systemd service
    
    (cherry picked from commit 9ae989307f274e9262d43c7beca35673ae255f06)
    
    Install ix-cgroups service when installing truenas deb package
    
    (cherry picked from commit beef4c5a33fbc7967afbec778a41673607881a38)
    
    Add setup cgroups helper script
    
    (cherry picked from commit 26d1ec707b705e875eeb9b8be35d753e0473cb8b)
    
    Update ix-cgroups systemd unit file
    
    (cherry picked from commit 5d8f97e063de361fe353cfd1f3073f8891fa2a0b)
    
    Remove cgroups logic from system plugin
    
    (cherry picked from commit 72e7f7fae630bb2d5c92ab6c33f2e5efd9798048)
    
    Setup cgroups before middleware unit starts
    
    (cherry picked from commit 9a27a975b9354f68a2db025ce8321a5f86acf248)
    
    Add a comment as to why cgroup controllers need to be setup before middleware
    
    (cherry picked from commit 2b99541d9430073f10a4951c94fcca7387ca8549)
    
    Fix ix-cgroups dependencies
    
    (cherry picked from commit fd91a8eb96c87e5b904304d89463cb7b2f459373)
    
    Remove regex usage from cgroups script
    
    (cherry picked from commit 0d7cae382739c5ff6f9231a0cf44d614c4f35e14)
    
    Add comment outlining cgroups workflow
    
    (cherry picked from commit cbeb8e3d673154bd4653ee91132636489bca7b2c)
    
    Fix flake8

--- src/middlewared/middlewared/plugins/kubernetes_linux/utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/utils.py
@@ -1,13 +1,7 @@
-import contextlib
 import os
-import re
-
-from middlewared.service import CallError
 
 
 BACKUP_NAME_PREFIX = 'ix-applications-backup-'
-CGROUP_ROOT_PATH = '/sys/fs/cgroup'
-CGROUP_AVAILABLE_CONTROLLERS_PATH = os.path.join(CGROUP_ROOT_PATH, 'cgroup.subtree_control')
 KUBECONFIG_FILE = '/etc/rancher/k3s/k3s.yaml'
 KUBERNETES_WORKER_NODE_PASSWORD = 'e3d26cefbdf2f81eff5181e68a02372f'
 KUBEROUTER_RULE_PRIORITY = 32764
@@ -16,30 +10,8 @@ KUBEROUTER_TABLE_NAME = 'kube-router'
 MIGRATION_NAMING_SCHEMA = 'ix-app-migrate-%Y-%m-%d_%H-%M'
 NODE_NAME = 'ix-truenas'
 OPENEBS_ZFS_GROUP_NAME = 'zfs.openebs.io'
-RE_CGROUP_CONTROLLERS = re.compile(r'(\w+)\s+')
 UPDATE_BACKUP_PREFIX = 'system-update-'
 
 
 def applications_ds_name(pool):
     return os.path.join(pool, 'ix-applications')
-
-
-def get_available_controllers_for_consumption() -> set:
-    try:
-        with open(CGROUP_AVAILABLE_CONTROLLERS_PATH, 'r') as f:
-            return set(RE_CGROUP_CONTROLLERS.findall(f.read()))
-    except FileNotFoundError:
-        raise CallError(
-            'Unable to determine cgroup controllers which are available for consumption as '
-            f'{CGROUP_AVAILABLE_CONTROLLERS_PATH!r} does not exist'
-        )
-
-
-def update_available_controllers_for_consumption(to_add_controllers: set) -> set:
-    # This will try to update available controllers for consumption and return the current state
-    # regardless of the update failing
-    with contextlib.suppress(FileNotFoundError, OSError):
-        with open(CGROUP_AVAILABLE_CONTROLLERS_PATH, 'w') as f:
-            f.write(f'{" ".join(map(lambda s: f"+{s}", to_add_controllers))}')
-
-    return get_available_controllers_for_consumption()
