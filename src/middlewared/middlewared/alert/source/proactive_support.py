738a2b610b59a0a6fe589d4602f4d6c99c8f74f8
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-04-05 09:54:27 -0400

    NAS-121348 / 23.10 / fix proactive support alert verbiage (#11055)
    * fix proactive support alert
    
    * fix punctuation

--- src/middlewared/middlewared/alert/source/proactive_support.py
+++ src/middlewared/middlewared/alert/source/proactive_support.py
@@ -6,7 +6,6 @@ class ProactiveSupportAlertClass(AlertClass):
     level = AlertLevel.WARNING
     title = "Proactive Support Is Not Configured"
     text = "%s"
-
     products = ("SCALE_ENTERPRISE",)
 
 
@@ -15,13 +14,11 @@ class ProactiveSupportAlertSource(AlertSource):
     run_on_backup_node = False
 
     async def check(self):
+        webui_page = 'System Settings->General->Support page'
         support = await self.middleware.call('support.config')
         available = await self.middleware.call('support.is_available')
         if available and support['enabled'] is None:
-            return Alert(
-                ProactiveSupportAlertClass,
-                'Proactive Support is not configured. Please see the System/Proactive Support page.'
-            )
+            return Alert(ProactiveSupportAlertClass, f'Proactive support is not configured. Review the {webui_page}.')
 
         if support['enabled']:
             # This is for people who had ix alert enabled before Proactive Support
@@ -34,5 +31,5 @@ class ProactiveSupportAlertSource(AlertSource):
             if unfilled:
                 return Alert(
                     ProactiveSupportAlertClass,
-                    'Please complete these fields on the System/Proactive Support page: %s.' % ', '.join(unfilled)
+                    f'Please complete these fields on the {webui_page}: {", ".join(unfilled)}'
                 )
