a8e4bb5261e5101adf74418ae1aca690e611f117
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-04-27 06:23:49 +0400

    Add migration to remove geli related attrs in db

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-04-27_06-20_remove_geli_keys.py
@@ -0,0 +1,25 @@
+"""
+Remove geli related keys
+
+Revision ID: d2a26c29efca
+Revises: 55836e7dac39
+Create Date: 2023-04-27 06:20:01.757983+00:00
+
+"""
+from alembic import op
+
+
+revision = 'd2a26c29efca'
+down_revision = '55836e7dac39'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('storage_volume', schema=None) as batch_op:
+        batch_op.drop_column('vol_encrypt')
+        batch_op.drop_column('vol_encryptkey')
+
+
+def downgrade():
+    pass
