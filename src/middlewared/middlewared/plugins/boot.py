3a2983f44a3409722f6810486e0bec3cc9f3e67f
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-05-09 15:48:37 -0300

    NAS-112093 / 13.0 / Set ashift to 12 for pools which do not have it (by sonicaj) (#11267)
    * Add endpoint check/update ashift property on boot-pool
    
    * Update ashift property on boot-pool when replacing/extending/detaching
    
    * Set ashift to 12 for pools which do not have it
    
    ---------
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

--- src/middlewared/middlewared/plugins/boot.py
+++ src/middlewared/middlewared/plugins/boot.py
@@ -80,7 +80,7 @@ class BootService(Service):
         `expand` option will determine whether the new disk partition will be
                  the maximum available or the same size as the current disk.
         """
-
+        await self.check_update_ashift_property()
         disks = list(await self.get_disks())
         if len(disks) > 1:
             raise CallError('3-way mirror not supported')
@@ -123,6 +123,7 @@ class BootService(Service):
         """
         Detach given `dev` from boot pool.
         """
+        await self.check_update_ashift_property()
         await self.middleware.call('zfs.pool.detach', BOOT_POOL_NAME, dev)
 
     @accepts(Str('label'), Str('dev'))
@@ -130,6 +131,7 @@ class BootService(Service):
         """
         Replace device `label` on boot pool with `dev`.
         """
+        await self.check_update_ashift_property()
         format_opts = {}
         disks = list(await self.get_disks())
         swap_part = await self.middleware.call('disk.get_partition', disks[0], 'SWAP')
@@ -171,6 +173,17 @@ class BootService(Service):
         Get Automatic Scrub Interval value in days.
         """
         return (await self.middleware.call('system.advanced.config'))['boot_scrub']
+
+    @private
+    async def check_update_ashift_property(self):
+        properties = {}
+        if (
+            zfs_pool := await self.middleware.call('zfs.pool.query', [('name', '=', BOOT_POOL_NAME)])
+        ) and zfs_pool[0]['properties']['ashift']['source'] == 'DEFAULT':
+            properties['ashift'] = {'value': '12'}
+
+        if properties:
+            await self.middleware.call('zfs.pool.update', BOOT_POOL_NAME, {'properties': properties})
 
 
 async def setup(middleware):
