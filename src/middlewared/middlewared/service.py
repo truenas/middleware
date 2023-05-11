2c77d3d68db71bf730050430544ad3d0878300f5
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-05-11 04:37:28 +0400

    Move service change mixin to new service package

--- src/middlewared/middlewared/service.py
+++ src/middlewared/middlewared/service.py
@@ -48,31 +48,6 @@ def get_datastore_primary_key_schema(klass):
     })
 
 
-class ServiceChangeMixin:
-    async def _service_change(self, service, verb):
-
-        svc_state = (await self.middleware.call(
-            'service.query',
-            [('service', '=', service)],
-            {'get': True}
-        ))['state'].lower()
-
-        # For now its hard to keep track of which services change rc.conf.
-        # To be safe run this every time any service is updated.
-        # This adds up ~180ms so its seems a reasonable workaround for the time being.
-        await self.middleware.call('etc.generate', 'rc')
-
-        if svc_state == 'running':
-            started = await self.middleware.call(f'service.{verb}', service)
-
-            if not started:
-                raise CallError(
-                    f'The {service} service failed to start',
-                    CallError.ESERVICESTARTFAILURE,
-                    [service],
-                )
-
-
 class CompoundService(Service):
     def __init__(self, middleware, parts):
         super().__init__(middleware)
