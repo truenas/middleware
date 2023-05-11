fb8b5331d1a02038f25d4a870fedafa0d778967d
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-05-11 04:40:39 +0400

    Move compound service to new service package

--- src/middlewared/middlewared/servicenew/__init__.py
+++ src/middlewared/middlewared/servicenew/__init__.py
@@ -2,6 +2,7 @@ from middlewared.service_exception import ( # noqa
     CallException, CallError, InstanceNotFound, ValidationError, ValidationErrors
 )
 
+from .compound_service import CompoundService # noqa
 from .decorators import (
     cli_private, filterable, filterable_returns, item_method, job, lock, no_auth_required, pass_app,
     periodic, private, rest_api_metadata, skip_arg, threaded,
@@ -9,3 +10,6 @@ from .decorators import (
 from .service import Service # noqa
 from .service_mixin import ServiceChangeMixin # noqa
 from .throttle import throttle # noqa
+
+
+ABSTRACT_SERVICES = (CompoundService, ) # noqa
---
+++ src/middlewared/middlewared/servicenew/compound_service.py
@@ -0,0 +1,48 @@
+import itertools
+
+from .base import service_config
+from .service import Service
+
+
+class CompoundService(Service):
+    def __init__(self, middleware, parts):
+        super().__init__(middleware)
+
+        config_specified = {}
+        for part1, part2 in itertools.combinations(parts, 2):
+            for key in set(part1._config_specified.keys()) & set(part2._config_specified.keys()):
+                if part1._config_specified[key] != part2._config_specified[key]:
+                    raise RuntimeError(f'{part1} has {key}={part1._config_specified[key]!r}, but '
+                                       f'{part2} has {key}={part2._config_specified[key]!r}')
+            config_specified.update(part1._config_specified)
+            config_specified.update(part2._config_specified)
+
+        self._config = service_config(parts[0].__class__, config_specified)
+
+        self.parts = parts
+
+        methods_parts = {}
+        for part in self.parts:
+            for name in dir(part):
+                if name.startswith('_'):
+                    continue
+
+                meth = getattr(part, name)
+                if not callable(meth):
+                    continue
+
+                if hasattr(self, name):
+                    raise RuntimeError(
+                        f'Duplicate method name {name} for service parts {methods_parts[name]} and {part}',
+                    )
+
+                setattr(self, name, meth)
+                methods_parts[name] = part
+
+        for part in self.parts:
+            if part.__doc__:
+                self.__doc__ = part.__doc__
+                break
+
+    def __repr__(self):
+        return f'<CompoundService: {", ".join([repr(part) for part in self.parts])}>'
