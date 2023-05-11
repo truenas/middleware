bf2633827c86de8983fece6b401618f83fd9b664
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-05-11 04:37:28 +0400

    Move service change mixin to new service package

--- src/middlewared/middlewared/servicenew/__init__.py
+++ src/middlewared/middlewared/servicenew/__init__.py
@@ -1,6 +1,11 @@
+from middlewared.service_exception import ( # noqa
+    CallException, CallError, InstanceNotFound, ValidationError, ValidationErrors
+)
+
 from .decorators import (
     cli_private, filterable, filterable_returns, item_method, job, lock, no_auth_required, pass_app,
     periodic, private, rest_api_metadata, skip_arg, threaded,
 ) # noqa
 from .service import Service # noqa
+from .service_mixin import ServiceChangeMixin # noqa
 from .throttle import throttle # noqa
