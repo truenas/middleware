fbde87f9ab7652ea7442f0851dda8a14a5a97846
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-05-11 04:33:49 +0400

    Move service to new service package

--- src/middlewared/middlewared/servicenew/__init__.py
+++ src/middlewared/middlewared/servicenew/__init__.py
@@ -2,4 +2,5 @@ from .decorators import (
     cli_private, filterable, filterable_returns, item_method, job, lock, no_auth_required, pass_app,
     periodic, private, rest_api_metadata, skip_arg, threaded,
 ) # noqa
+from .service import Service # noqa
 from .throttle import throttle # noqa
