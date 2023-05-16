be458005e9142d6f88f5410f71553a80ca1c25fa
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2023-05-16 12:06:28 -0700

    NAS-121743 / 23.10 / NAS-121743 (#11314)
    * Add initiator_name parameter to iscsi_scsi_connection
    
    * Add test_15_invalid_initiator_name
    
    (cherry picked from commit 4bad047b6d5e0de080403428c4c8172131c1d030)

--- tests/protocols/iscsi_proto.py
+++ tests/protocols/iscsi_proto.py
@@ -1,9 +1,20 @@
-import contextlib                                                                                                                                               
-import pyscsi
-from pyscsi.utils import init_device
-from pyscsi.pyscsi.scsi import SCSI
+import contextlib
+import inspect
 
-def iscsi_scsi_connect(host, iqn, lun=0, user=None, secret=None, target_user=None, target_secret=None):
+from pyscsi.pyscsi.scsi import SCSI
+from pyscsi.utils import init_device
+
+
+def initiator_name_supported():
+    """
+    Returns whether a non-None initiator name may be supplied.
+
+    This can have an impact on what kinds of tests can be run.
+    """
+    return 'initiator_name' in inspect.signature(init_device).parameters
+
+
+def iscsi_scsi_connect(host, iqn, lun=0, user=None, secret=None, target_user=None, target_secret=None, initiator_name=None):
     """
     Connect to the specified target, returning a SCSI object from python-scsi.
 
@@ -17,24 +28,32 @@ def iscsi_scsi_connect(host, iqn, lun=0, user=None, secret=None, target_user=Non
     pprint.pprint(s.inquiry().result)
     s.device.close()
     """
+    hil = f"{host}/{iqn}/{lun}"
     if user is None and secret is None:
-        device = init_device("iscsi://{}/{}/{}".format(host, iqn, lun))
+        device_str = f"iscsi://{hil}"
     elif user and secret:
         if target_user is None and target_secret is None:
             # CHAP
-            device = init_device("iscsi://{}%{}@{}/{}/{}".format(user, secret, host, iqn, lun))
+            device_str = f"iscsi://{user}%{secret}@{hil}"
         elif target_user and target_secret:
             # Mutual CHAP
-            device = init_device("iscsi://{}%{}@{}/{}/{}?target_user={}&target_password={}".format(user, secret, host, iqn, lun, target_user, target_secret))
+            device_str = f"iscsi://{user}%{secret}@{hil}?target_user={target_user}&target_password={target_secret}"
         else:
             raise ValueError("If either of target_user and target_secret is set, then both should be set.")
     else:
         raise ValueError("If either of user and secret is set, then both should be set.")
+    if initiator_name:
+        if not initiator_name_supported():
+            raise ValueError("Initiator name supplied, but not supported.")
+        device = init_device(device_str, initiator_name=initiator_name)
+    else:
+        device = init_device(device_str)
     s = SCSI(device)
     return s
 
+
 @contextlib.contextmanager
-def iscsi_scsi_connection(host, iqn, lun=0, user=None, secret=None, target_user=None, target_secret=None):
+def iscsi_scsi_connection(host, iqn, lun=0, user=None, secret=None, target_user=None, target_secret=None, initiator_name=None):
     """
     Factory function to connect to the specified target, returning a SCSI
     object from python-scsi.
@@ -49,10 +68,9 @@ def iscsi_scsi_connection(host, iqn, lun=0, user=None, secret=None, target_user=
         inqdata = s.inquiry().result
     """
 
-    s = iscsi_scsi_connect(host, iqn, lun, user, secret, target_user, target_secret)
+    s = iscsi_scsi_connect(host, iqn, lun, user, secret, target_user, target_secret, initiator_name)
 
     try:
         yield s
     finally:
         s.device.close()
-
