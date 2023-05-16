1711210ea1fc34b7654b8ca01a9500231a63b91b
caleb <yocalebo@gmail.com>
2023-05-16 10:06:39 -0400

    add back the PMBus HA hack

--- src/middlewared/middlewared/plugins/ipmi_/sensors.py
+++ src/middlewared/middlewared/plugins/ipmi_/sensors.py
@@ -1,6 +1,8 @@
+from random import uniform
 from subprocess import run
+from time import sleep
 
-from middlewared.service import Service, filterable, filterable_returns
+from middlewared.service import Service, filterable, filterable_returns, private
 from middlewared.utils import filter_list
 from middlewared.schema import List, Dict
 
@@ -28,16 +30,17 @@ class IpmiSensorsService(Service):
         namespace = 'ipmi.sensors'
         cli_namespace = 'service.ipmi.sensors'
 
-    @filterable
-    @filterable_returns(List('sensors', items=[Dict('sensor', additional_attrs=True)]))
-    def query(self, filters, options):
+    @private
+    def query_impl(self):
         rv = []
         if not self.middleware.call_sync('system.dmidecode_info')['has-ipmi']:
             return rv
 
+        mseries = self.middleware.call_sync('failover.hardware') == 'ECHOWARP'
+        reread = None
         for line in filter(lambda x: x, get_sensors_data()):
             if (values := line.split(',')) and len(values) == 13:
-                rv.append({
+                sensor = {
                     'id': values[0],
                     'name': values[1],
                     'type': values[2],
@@ -51,6 +54,34 @@ class IpmiSensorsService(Service):
                     'upper-critical': values[10],
                     'upper-non-recoverable': values[11],
                     'event': [i.replace("'", '').strip().lower() for i in values[12].split("' '")]
-                })
+                }
+                if sensor['type'] == 'Power Supply' and mseries and 'no presence detected' in sensor['event']:
+                    # PMBus (which controls the PSU's status) can not be probed at the same time because
+                    # it's not a shared bus. HA systems show false positive "no presence detected" more
+                    # often because both controllers are randomly probing the status of the PSU's at the
+                    # same time because this method is called in an alert. The alert, by default, gets
+                    # called on both controllers.
+                    reread = f'"{sensor["name"]}" reporting "no presence detected"'
 
-        return filter_list(rv, filters, options)
+                rv.append(sensor)
+
+        return rv, reread
+
+    @filterable
+    @filterable_returns(List('sensors', items=[Dict('sensor', additional_attrs=True)]))
+    def query(self, filters, options):
+        sensors, reread = self.query_impl()
+        if reread is not None:
+            max_retries = 3
+            while max_retries != 0:
+                self.logger.info('%s re-reading', reread)
+                sleep(round(uniform(0.4, 1.2), 2))
+                sensors, reread = self.query_impl()
+                if reread is None:
+                    # re-read the sensors list and PSU status came back
+                    # healthy so exit early
+                    break
+                else:
+                    max_retries -= 1
+
+        return filter_list(sensors, filters, options)
