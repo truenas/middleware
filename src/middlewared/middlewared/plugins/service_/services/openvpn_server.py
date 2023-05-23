7f150bd89e2d9aeb7a2197922bdbd8567eff9752
caleb <yocalebo@gmail.com>
2023-05-23 11:46:17 -0400

    mark opnvpn{client/server} and rsync as deprecated

--- src/middlewared/middlewared/plugins/service_/services/openvpn_server.py
+++ src/middlewared/middlewared/plugins/service_/services/openvpn_server.py
@@ -3,7 +3,6 @@ from .base import SimpleService
 
 class OpenVPNServerService(SimpleService):
     name = "openvpn_server"
-
+    deprecated = True
     etc = ["ssl", "openvpn_server"]
-
     systemd_unit = "openvpn-server@server"
--- src/middlewared/middlewared/plugins/service_/services/rsync.py
+++ src/middlewared/middlewared/plugins/service_/services/rsync.py
@@ -3,7 +3,6 @@ from .base import SimpleService
 
 class RsyncService(SimpleService):
     name = "rsync"
-
+    deprecated = True
     etc = ["rsync"]
-
     systemd_unit = "rsync"
