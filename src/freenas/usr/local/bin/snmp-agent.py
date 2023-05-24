2cbca7c16c4ec98c5312cb651b3f869b1ffa72b2
Caleb <yocalebo@gmail.com>
2023-05-24 11:37:50 -0400

    remove usage of // (cast to int explicitly)

--- src/freenas/usr/local/bin/snmp-agent.py
+++ src/freenas/usr/local/bin/snmp-agent.py
@@ -461,13 +461,13 @@ if __name__ == "__main__":
             arc_efficiency = get_arc_efficiency(kstat)
 
             prefix = "kstat.zfs.misc.arcstats"
-            zfs_arc_size.update(kstat[f"{prefix}.size"] // 1024)
-            zfs_arc_meta.update(kstat[f"{prefix}.arc_meta_used"] // 1024)
-            zfs_arc_data.update(kstat[f"{prefix}.data_size"] // 1024)
+            zfs_arc_size.update(int(kstat[f"{prefix}.size"] / 1024))
+            zfs_arc_meta.update(int(kstat[f"{prefix}.arc_meta_used"] / 1024))
+            zfs_arc_data.update(int(kstat[f"{prefix}.data_size"] / 1024))
             zfs_arc_hits.update(int(kstat[f"{prefix}.hits"] % 2 ** 32))
             zfs_arc_misses.update(int(kstat[f"{prefix}.misses"] % 2 ** 32))
-            zfs_arc_c.update(kstat[f"{prefix}.c"] // 1024)
-            zfs_arc_p.update(kstat[f"{prefix}.p"] // 1024)
+            zfs_arc_c.update(int(kstat[f"{prefix}.c"] / 1024))
+            zfs_arc_p.update(int(kstat[f"{prefix}.p"] / 1024))
             zfs_arc_miss_percent.update(str(get_zfs_arc_miss_percent(kstat)).encode("ascii"))
             zfs_arc_cache_hit_ratio.update(str(arc_efficiency["cache_hit_ratio"]["per"][:-1]).encode("ascii"))
             zfs_arc_cache_miss_ratio.update(str(arc_efficiency["cache_miss_ratio"]["per"][:-1]).encode("ascii"))
