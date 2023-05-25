a1c54444bb7d55fff75f1a42c62a3d9437abba90
Caleb <yocalebo@gmail.com>
2023-05-25 08:32:15 -0400

    improve verbiage for deprecated service alert
    (cherry picked from commit 7a33a0fb6ef67598c01930ae65c6046ebcd8d681)

--- src/middlewared/middlewared/alert/source/deprecated_service.py
+++ src/middlewared/middlewared/alert/source/deprecated_service.py
@@ -1,11 +1,19 @@
 from middlewared.alert.base import Alert, AlertClass, SimpleOneShotAlertClass, AlertCategory, AlertLevel
 
+URL = "https://www.truenas.com/docs/scale/scaledeprecatedfeatures/"
+
 
 class DeprecatedServiceAlertClass(AlertClass, SimpleOneShotAlertClass):
     category = AlertCategory.SHARING
     level = AlertLevel.WARNING
     title = "Deprecated Service is Running"
-    text = "The following running service is deprecated and will be removed in a future release: %(service)s"
+    text = (
+        "The following active service is deprecated in SCALE Bluefin %(service)s"
+        " This service is scheduled for removal in next SCALE major version (Cobia)."
+        " Please plan to migrate these services to the equivalent SCALE application"
+        " before upgrading to the next SCALE major version. For additional details"
+        f" and migration tutorials, see {URL}."
+    )
 
     async def create(self, args):
         return Alert(DeprecatedServiceAlertClass, args, key=args['service'])
