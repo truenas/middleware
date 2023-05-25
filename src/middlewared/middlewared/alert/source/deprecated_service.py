721fe9600d4fab59ffce5699ddbcf7878445febf
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-05-25 11:17:55 -0400

    NAS-122140 / 22.12.3 / improve verbiage for deprecated service alert (#11393)
    * improve verbiage for deprecated service alert
    
    * missing "the"
    
    * remove specific version names

--- src/middlewared/middlewared/alert/source/deprecated_service.py
+++ src/middlewared/middlewared/alert/source/deprecated_service.py
@@ -2,12 +2,18 @@ import json
 
 from middlewared.alert.base import Alert, AlertClass, SimpleOneShotAlertClass, AlertCategory, AlertLevel
 
+URL = "https://www.truenas.com/docs/scale/scaledeprecatedfeatures/"
+
 
 class DeprecatedServiceAlertClass(AlertClass, SimpleOneShotAlertClass):
     category = AlertCategory.SHARING
     level = AlertLevel.WARNING
     title = "Deprecated Service is Running"
-    text = "The following running service is deprecated and will be removed in a future release: %(service)s"
+    text = (
+        "The following active service is deprecated %(service)s. "
+        "This service is scheduled for removal in a future version of SCALE. "
+        f"Before upgrading, please check {URL} to confirm whether or not "
+        "the service has been removed in the next version of SCALE."
 
     async def create(self, args):
         return Alert(DeprecatedServiceAlertClass, args, key=args['service'])
