9b661a422897a774274d52d46e6a7f1caf81d5be
ericbsd <ericturgeon.bsd@gmail.com>
2023-05-30 12:31:53 -0300

    fixed TrueNAS catalog catalog and replace TRUECHARTS by CUSTOMCHART
    TRUECHARTS is wrong since it is not TrueChart.
    
    (cherry picked from commit 2b08676ea9379fcdc511c63d5eb8730d17888ec2)

--- tests/api2/test_022_catalog.py
+++ tests/api2/test_022_catalog.py
@@ -14,27 +14,27 @@ pytestmark = pytest.mark.skipif(dev_test, reason=reason)
 # TODO: why does this not run on HA?
 if not ha:
     official_repository = 'https://github.com/truenas/charts.git'
-    truechart_repository = 'https://github.com/ericbsd/charts.git'
+    custom_repository = 'https://github.com/ericbsd/charts.git'
     github_official_charts = 'https://api.github.com/repos/truenas/charts/contents/charts/'
-    github_truechart_charts = 'https://api.github.com/repos/ericbsd/charts/contents/charts/'
+    github_custom_charts = 'https://api.github.com/repos/ericbsd/charts/contents/charts/'
     official_charts = []
     for chart_dict in GET(github_official_charts).json():
         if chart_dict['type'] == 'dir':
             official_charts.append(chart_dict['name'])
 
-    truechart_charts = []
-    for chart_dict in GET(github_truechart_charts).json():
+    custom_charts = []
+    for chart_dict in GET(github_custom_charts).json():
         if chart_dict['type'] == 'dir':
-            truechart_charts.append(chart_dict['name'])
+            custom_charts.append(chart_dict['name'])
 
-    truechart_catalog = {
-        'label': 'TRUECHARTS',
-        'repository': truechart_repository,
+    custom_catalog = {
+        'label': 'CUSTOMCHART',
+        'repository': custom_repository,
         'branch': 'master',
         'builtin': False,
         'preferred_trains': ['charts'],
         'location': '/tmp/ix-applications/catalogs/github_com_ericbsd_charts_git_master',
-        'id': 'TRUECHARTS'
+        'id': 'CUSTOMCHART'
     }
 
     def test_01_get_catalog_list():
@@ -99,13 +99,13 @@ if not ha:
         assert isinstance(results, dict), str(job_status['results'])
         assert chart in results['charts'], str(job_status['results'])
 
-    def test_09_set_truechart_catalog():
+    def test_09_set_custom_catalog():
         global payload, results
         payload = {
             'force': False,
             'preferred_trains': ['charts'],
-            'label': 'TRUECHARTS',
-            'repository': truechart_repository,
+            'label': 'CUSTOMCHART',
+            'repository': custom_repository,
             'branch': 'master'
         }
         results = POST('/catalog/', payload)
@@ -116,26 +116,26 @@ if not ha:
         results = job_status['results']['result']
         assert isinstance(results, dict), results.text
 
-    @pytest.mark.parametrize('key', list(truechart_catalog.keys()))
-    def test_10_verify_truechart_catalog_object(key):
-        assert results[key] == truechart_catalog[key], results.text
+    @pytest.mark.parametrize('key', list(custom_catalog.keys()))
+    def test_10_verify_custom_catalog_object(key):
+        assert results[key] == custom_catalog[key], str(results)
 
-    @pytest.mark.parametrize('key', list(truechart_catalog.keys()))
-    def test_11_verify_truechart_catalog_object(key):
-        results = GET('/catalog/id/TRUECHARTS/')
+    @pytest.mark.parametrize('key', list(custom_catalog.keys()))
+    def test_11_verify_custom_catalog_object(key):
+        results = GET('/catalog/id/CUSTOMCHART/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
-        assert results.json()[key] == truechart_catalog[key], results.text
+        assert results.json()[key] == custom_catalog[key], results.text
 
-    def test_12_validate_truechart_catalog():
-        results = POST('/catalog/validate/', 'TRUECHARTS')
+    def test_12_validate_custom_catalog():
+        results = POST('/catalog/validate/', 'CUSTOMCHART')
         assert results.status_code == 200, results.text
         job_id = results.json()
         job_status = wait_on_job(job_id, 300)
         assert job_status['state'] == 'SUCCESS', str(job_status['results'])
 
-    def test_13_sync_truechart_catalog():
-        results = POST('/catalog/sync/', 'TRUECHARTS')
+    def test_13_sync_custom_catalog():
+        results = POST('/catalog/sync/', 'CUSTOMCHART')
         assert results.status_code == 200, results.text
         job_id = results.json()
         job_status = wait_on_job(job_id, 300)
@@ -143,10 +143,10 @@ if not ha:
         results = job_status['results']['result']
         assert results is None, results.text
 
-    @pytest.mark.parametrize('chart', truechart_charts)
-    def test_14_get_truechart_catalog_item(chart):
+    @pytest.mark.parametrize('chart', custom_charts)
+    def test_14_get_custom_catalog_item(chart):
         payload = {
-            'label': 'TRUECHARTS',
+            'label': 'CUSTOMCHART',
             'options': {
                 'cache': True,
                 'retrieve_all_trains': True,
@@ -161,23 +161,23 @@ if not ha:
         assert isinstance(results, dict), str(job_status['results'])
         assert chart in results['charts'], str(job_status['results'])
 
-    def test_15_change_truechart_preferred_trains():
+    def test_15_change_custom_preferred_trains():
         payload = {
             'preferred_trains': ['test']
         }
-        results = PUT('/catalog/id/TRUECHARTS/', payload)
+        results = PUT('/catalog/id/CUSTOMCHART/', payload)
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
 
-    def test_16_verify_truechart_catalog_preferred_trains():
-        results = GET('/catalog/id/TRUECHARTS/')
+    def test_16_verify_custom_catalog_preferred_trains():
+        results = GET('/catalog/id/CUSTOMCHART/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), dict), results.text
         assert 'test' in results.json()['preferred_trains'], results.text
 
-    def test_17_get_truechart_catalog_item_test_trains():
+    def test_17_get_custom_catalog_item_test_trains():
         payload = {
-            'label': 'TRUECHARTS',
+            'label': 'CUSTOMCHART',
             'options': {
                 'cache': True,
                 'retrieve_all_trains': True,
@@ -198,8 +198,8 @@ if not ha:
         job_status = wait_on_job(results.json(), 180)
         assert job_status['state'] == 'SUCCESS', str(job_status['results'])
 
-    def test_19_delete_truechart_catalog():
-        results = DELETE('/catalog/id/TRUECHARTS/')
+    def test_19_delete_custom_catalog():
+        results = DELETE('/catalog/id/CUSTOMCHART/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), bool), results.text
         assert results.json() is True, results.text
