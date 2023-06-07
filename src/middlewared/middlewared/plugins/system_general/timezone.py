998edc496cf00a81db337ccf36c8227f64ebee9f
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-06-07 12:46:11 -0300

    fix system.general.timezone_choices and test (#11472)
    (cherry picked from commit d962072af749397b8b5a95a3d45096bba02e4f6d)
    
    Co-authored-by: Caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/system_general/timezone.py
+++ src/middlewared/middlewared/plugins/system_general/timezone.py
@@ -1,12 +1,11 @@
-import os
+from subprocess import run
 
 from middlewared.schema import accepts, Dict, returns
-from middlewared.service import private, Service
+from middlewared.service import Service
+from middlewared.utils.functools import cache
 
 
 class SystemGeneralService(Service):
-
-    TIMEZONE_CHOICES = None
 
     class Config:
         namespace = 'system.general'
@@ -14,26 +13,11 @@ class SystemGeneralService(Service):
 
     @accepts()
     @returns(Dict('system_timezone_choices', additional_attrs=True, title='System Timezone Choices'))
-    async def timezone_choices(self):
-        """
-        Returns time zone choices.
-        """
-        if not self.TIMEZONE_CHOICES:
-            self.TIMEZONE_CHOICES = await self.get_timezone_choices()
-
-        return self.TIMEZONE_CHOICES
-
-    @private
-    async def get_timezone_choices(self):
-        timezones = {}
-        basepath = '/usr/share/zoneinfo/'
-        for root, dirs, files in os.walk(basepath):
-            relpath = os.path.normpath(os.path.relpath(root, basepath))
-            for timezone in (files if 'right' not in relpath and 'posix' not in relpath else []):
-                if relpath != '.':
-                    zone_name = f'{relpath}/{timezone}'
-                else:
-                    zone_name = timezone
-                if 'Etc/GMT' not in zone_name:
-                    timezones[zone_name] = zone_name
-        return timezones
+    @cache
+    def timezone_choices(self):
+        """Returns available timezones"""
+        choices = dict()
+        for i in run(['timedatectl', 'list-timezones'], capture_output=True).stdout.decode().split('\n'):
+            if (choice := i.strip()):
+                choices[choice] = choice
+        return choices
