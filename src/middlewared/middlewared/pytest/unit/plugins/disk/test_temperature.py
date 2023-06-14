1f50ab170a11bd9a7ebf4e19bac92a892041c663
themylogin <themylogin@gmail.com>
2023-06-14 15:41:36 +0200

    Fix `disk.read_sata_or_sas_disk_temps` only working properly with SCSI disks
    (cherry picked from commit 0b9369d35b5ff5af312e614efd242bd225497b37)

--- src/middlewared/middlewared/pytest/unit/plugins/disk/test_temperature.py
+++ src/middlewared/middlewared/pytest/unit/plugins/disk/test_temperature.py
@@ -1,6 +1,6 @@
 import pytest
 
-from middlewared.plugins.disk_.temperature import get_temperature
+from middlewared.plugins.disk_.temperature import get_temperature, DiskService
 
 
 @pytest.mark.parametrize("stdout,temperature", [
@@ -21,3 +21,19 @@ from middlewared.plugins.disk_.temperature import get_temperature
 ])
 def test__get_temperature(stdout, temperature):
     assert get_temperature(stdout) == temperature
+
+
+@pytest.mark.parametrize("filename,line,result", [
+    (
+        "attrlog.WDC_WD30EFRX_68EUZN0-WD_WCC4N2PNTD34.ata.csv",
+        "2023-06-14 06:16:14;\t1;200;0;\t3;187;5616;\t4;100;84;\t5;200;0;\t7;200;0;\t9;70;22359;\t10;100;0;\t11;100;0;\t12;100;84;\t192;200;60;193;199;5043;\t194;91;59;\t196;200;0;\t197;200;0;\t198;100;0;\t199;200;0;\t200;100;0;",
+        59,
+    ),
+    (
+        "attrlog.HGST-HUS726020AL4210-N4G21DPK.scsi.csv",
+        "2023-06-14\t06:02:03;\tread-corr-by-ecc-fast;0;\tread-corr-by-ecc-delayed;9;\tread-corr-by-retry;0;\tread-total-err-corrected;9;\tread-corr-algorithm-invocations;2278941;\tread-gb-processed;77192.668;\tread-total-unc-errors;0;\twrite-corr-by-ecc-fast;0;\twrite-corr-by-ecc-delayed;41;\twrite-corr-by-retry;0;\twrite-total-err-corrected;41;\twrite-corr-algorithm-invocations;1546627;\twrite-gb-processed;229818.534;\twrite-total-unc-errors;0;\tverify-corr-by-ecc-fast;0;\tverify-corr-by-ecc-delayed;0;\tverify-corr-by-retry;0;\tverify-total-err-corrected;0;\tverify-corr-algorithm-invocations;56103;\tverify-gb-processed;0.000;\tverify-total-unc-errors;0;\tnon-medium-errors;0;\ttemperature;27;",
+        27,
+    ),
+])
+def test_parse_sata_or_sas_disk_temp(filename, line, result):
+    assert DiskService(None).parse_sata_or_sas_disk_temp(filename, line) == result
