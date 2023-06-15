b263daea96817aaba4e72a5bdb0b31df757036ea
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-06-16 01:12:29 +0400

    Add a config service to allow enabling/disabling fips service

--- src/middlewared/middlewared/plugins/security/update.py
+++ src/middlewared/middlewared/plugins/security/update.py
@@ -1,8 +1,49 @@
 import middlewared.sqlalchemy as sa
 
+from middlewared.schema import accepts, Bool, Dict, Int, Patch
+from middlewared.service import ConfigService
+
 
 class SystemGeneralModel(sa.Model):
     __tablename__ = 'system_security'
 
     id = sa.Column(sa.Integer(), primary_key=True)
     enable_fips = sa.Column(sa.Boolean(), default=True)
+
+
+class SystemSecurityService(ConfigService):
+
+    class Config:
+        datastore = 'system.security'
+
+    ENTRY = Dict(
+        'system_security_entry',
+        Bool('enable_fips', required=True),
+        Int('id', required=True),
+    )
+
+    @accepts(
+        Patch(
+            'system_security_entry', 'system_security_update',
+            ('rm', {'name': 'id'}),
+            ('attr', {'update': True}),
+        )
+    )
+    async def do_update(self, data):
+        """
+        Update System Security Service Configuration.
+
+        `enable_fips` when set, enables FIPS mode.
+        """
+        old = await self.config()
+        new = old.copy()
+        new.update(data)
+
+        await self.middleware.call(
+            'datastore.update',
+            self._config.datastore,
+            old['id'],
+            new,
+        )
+
+        return await self.config()
