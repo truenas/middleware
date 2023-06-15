d2ba17d307fcb203e6d7d2f3e3a227a88501dbb9
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-06-16 01:04:23 +0400

    Add a migration adding system security table enabling fips module for everyone

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2023-06-16_fips.py
@@ -0,0 +1,29 @@
+"""
+FIPS Support
+
+Revision ID: c0b117cde6b8
+Revises: 0893833a57be
+Create Date: 2023-06-16 22:57:01.757983+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'c0b117cde6b8'
+down_revision = '0893833a57be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'system_security',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('enable_fips', sa.Boolean(), nullable=False, server_default='1'),
+        sa.PrimaryKeyConstraint('id'),
+    )
+
+
+def downgrade():
+    pass
