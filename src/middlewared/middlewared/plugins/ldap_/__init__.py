23ab064ef63fdbeb49f4cd465ec79b2692e58b8c
Andrew Walker <awalker@ixsystems.com>
2023-06-16 08:50:39 -0700

    Add comment

--- src/middlewared/middlewared/plugins/ldap_/__init__.py
+++ src/middlewared/middlewared/plugins/ldap_/__init__.py
@@ -1,2 +1,5 @@
 import sys
+# nss-pam-ldapd generates constants at compile time that are stored in python
+# nslcd client files in /usr/share/nslcd-utils. Hence, path is expanded to include
+# this for the middleware nslcd client
 sys.path.append('/usr/share/nslcd-utils')
