aa454575a7479fb28ed838d9e336deb4f8d83999
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-06-21 18:34:59 +0400

    Use synchronous render implementation for fips

--- src/middlewared/middlewared/etc_files/fips.py
+++ src/middlewared/middlewared/etc_files/fips.py
@@ -1,11 +1,9 @@
 import subprocess
 
-from middlewared.utils import run
 
-
-async def render(service, middleware):
+def render(service, middleware):
     try:
-        await run(['configure_fips'], encoding='utf-8', errors='ignore')
+        subprocess.run(['configure_fips'], capture_output=True, check=True)
     except subprocess.CalledProcessError as e:
         middleware.logger.error('configure_fips error:\n%s', e.stderr)
         raise
