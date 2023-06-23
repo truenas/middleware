9a41893f0dadff2f43913109fe619bb196fcb894
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-06-23 21:09:09 +0500

    Add util methods to update/checkout repo

--- src/middlewared/middlewared/utils/git.py
+++ src/middlewared/middlewared/utils/git.py
@@ -23,3 +23,24 @@ def clone_repository(
         raise CallError(
             f'Failed to clone {repository_uri!r} repository at {destination!r} destination: {cp.stderr.decode()}'
         )
+
+
+def checkout_repository(destination: str, branch: str) -> None:
+    cp = subprocess.run(['git', '-C', destination, 'checkout', branch], capture_output=True)
+    if cp.returncode:
+        raise CallError(
+            f'Failed to checkout {branch!r} branch for {destination!r} repository: {cp.stderr.decode()}'
+        )
+
+
+def update_repo(destination: str, branch: str) -> None:
+    cp = subprocess.run(['git', '-C', destination, 'pull', 'origin', branch], capture_output=True)
+    if cp.returncode:
+        raise CallError(
+            f'Failed to update {destination!r} repository: {cp.stderr.decode()}'
+        )
+
+
+def validate_git_repo(destination: str) -> bool:
+    cp = subprocess.run(['git', '-C', destination, 'status'], capture_output=False)
+    return cp.returncode == 0
