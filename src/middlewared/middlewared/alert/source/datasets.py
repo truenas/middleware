a980d29a6a431e295213ce55a373d68b1b86761e
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-06-27 10:28:57 -0300

    NAS-122617 / 22.12.4 / Do not raise alert for unencrypted apps dataset (by sonicaj) (#11594)
    * Do not raise alert for unencrypted apps dataset
    
    (cherry picked from commit 4df9ce01b05ce5706147fe6be644c80b07ccd478)
    
    * Only run the alert every 12 hours
    
    (cherry picked from commit c32fadd5225692f9e1f6250be986a4415c7961dc)
    
    ---------
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

--- src/middlewared/middlewared/alert/source/datasets.py
+++ src/middlewared/middlewared/alert/source/datasets.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, AlertSource, Alert
+from middlewared.alert.schedule import IntervalSchedule
 
 
 class EncryptedDatasetAlertClass(AlertClass):
@@ -13,10 +16,17 @@ class EncryptedDatasetAlertClass(AlertClass):
 
 class UnencryptedDatasetsAlertSource(AlertSource):
 
+    schedule = IntervalSchedule(timedelta(hours=12))
+
     async def check(self):
         unencrypted_datasets = []
         for dataset in await self.middleware.call('pool.dataset.query', [['encrypted', '=', True]]):
             for child in dataset['children']:
+                if child['name'] == f'{child["pool"]}/ix-applications' or child['name'].startswith(
+                    f'{child["pool"]}/ix-applications/'
+                ):
+                    continue
+
                 if not child['encrypted']:
                     unencrypted_datasets.append(child['name'])
 
