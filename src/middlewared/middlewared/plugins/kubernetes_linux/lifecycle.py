3468c9b8588a5faee5da30a9ea2e052e78d97742
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-08 05:28:20 +0500

    Add events for k8s state

--- src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/lifecycle.py
@@ -409,7 +409,7 @@ class KubernetesService(Service):
     async def set_status(self, new_status):
         assert new_status in Status.__members__
         self.STATUS = Status(new_status)
-        # TODO: Let's perhaps send an event here ?
+        self.middleware.send_event('kubernetes.state', 'CHANGED', fields={'status': new_status})
 
     @accepts()
     @returns(Str())
@@ -452,6 +452,7 @@ async def _event_system_shutdown(middleware, event_type, args):
 
 
 async def setup(middleware):
+    middleware.event_register('kubernetes.state', 'Kubernetes state events')
     middleware.event_subscribe('system.ready', _event_system_ready)
     middleware.event_subscribe('system.shutdown', _event_system_shutdown)
     await middleware.call('kubernetes.initialize_k8s_status')
