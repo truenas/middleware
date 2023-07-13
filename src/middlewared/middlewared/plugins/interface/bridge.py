truenas/middleware

commit 97ff05a74845dacbd2974ef8c03a846e060986aa

fix bridge MTU logic on SCALE (#7534)

src/middlewared/middlewared/plugins/interface/bridge.py
@@ -1,5 +1,4 @@
 from middlewared.service import private, Service
-
 from .netif import netif
 
 
@@ -9,59 +8,47 @@ class InterfaceService(Service):
         namespace_alias = 'interfaces'
 
     @private
-    def pre_bridge_setup(self, bridge, sync_interface_opts):
-        name = bridge['interface']['int_interface']
-        members_database = set(bridge['members'])
-        for member in members_database:
-            sync_interface_opts[member]['skip_mtu'] = True
-
-        try:
-            iface = netif.get_interface(name)
-        except KeyError:
-            return
-
-        members = set(iface.members)
-        for member in members - members_database:
-            # These interfaces may be added dynamically for VMs
-            if member.startswith(('vnet', 'epair', 'tap')):
-                continue
-            iface.delete_member(member)
-
-    @private
     def bridge_setup(self, bridge, parent_interfaces):
         name = bridge['interface']['int_interface']
-        self.logger.info(f'Setting up {name}')
+        bridge_mtu = bridge['interface']['int_mtu'] or 1500
         try:
             iface = netif.get_interface(name)
         except KeyError:
             netif.create_interface(name)
             iface = netif.get_interface(name)
 
-        mtu = bridge['interface']['int_mtu'] or 1500
+        self.logger.info('Setting up %r', name)
 
-        members = set(iface.members)
-        members_database = set(bridge['members'])
+        if iface.mtu != bridge_mtu:
+            self.logger.info('Setting %r MTU to %d', name, bridge_mtu)
+            iface.mtu = bridge_mtu
 
-        for member in members_database:
+        db_members = set(bridge['members'])
+        os_members = set(iface.members)
+        for member in os_members - db_members:
+            # remove members from the bridge that aren't in the db
+            self.logger.info('Removing member interface %r from %r', member, name)
+            iface.delete_member(member)
+
+        for member in db_members - os_members:
+            # now add members that are written in db but do not exist in
+            # the bridge on OS side
             try:
-                member_iface = netif.get_interface(member)
-            except KeyError:
-                self.logger.error('Bridge member %s not found', member)
+                self.logger.info('Adding member interface %r to %r', member, name)
+                iface.add_member(member)
+            except FileNotFoundError:
+                self.logger.error('Bridge member %r not found', member)
                 continue
 
-            if member_iface.mtu != mtu:
-                member_iface.mtu = mtu
-
+            # now make sure the bridge member is up
+            member_iface = netif.get_interface(member)
             if netif.InterfaceFlags.UP not in member_iface.flags:
+                self.logger.info('Bringing up member interface %r in %r', member_iface.name, name)
                 member_iface.up()
 
             parent_interfaces.append(member)
 
-        for member in members_database - members:
-            try:
-                iface.add_member(member)
-            except FileNotFoundError:
-                self.logger.error('Bridge member %s not found', member)
-
-        if iface.mtu != mtu:
-            iface.mtu = mtu
+        # finally we need to up the main bridge if it's not already up
+        if netif.InterfaceFlags.UP not in iface.flags:
+            self.logger.info('Bringing up %r', name)
+            iface.up()

src/middlewared/middlewared/plugins/network.py
@@ -2045,7 +2045,6 @@ class InterfaceService(CRUDService):
         """
         Sync interfaces configured in database to the OS.
         """
-
         await self.middleware.call_hook('interface.pre_sync')
 
         disable_capabilities_ifaces = {
@@ -2090,20 +2089,6 @@ class InterfaceService(CRUDService):
             except Exception:
                 self.middleware.logger.error('Error setting up VLAN %s', vlan['vlan_vint'], exc_info=True)
 
-        bridges = await self.middleware.call('datastore.query', 'network.bridge')
-        # Considering a scenario where we have the network configuration
-        # physical iface -> vlan -> bridge
-        # If all these interfaces are set to have 9000 MTU, we won't be able to set that up on boot
-        # unless physical iface has a 9000 MTU when the bridge is being setup.
-        # To address such scenarios, we divide the approach in 4 steps:
-        # 1) Remove orphaned members from bridge
-        # 2) Sync all non-bridge interfaces ( in this order physical ifaces -> laggs -> vlans )
-        # 3) Setup bridge with MTU
-        # 4) Sync bridge interfaces
-
-        for bridge in bridges:
-            await self.middleware.call('interface.pre_bridge_setup', bridge, sync_interface_opts)
-
         run_dhcp = []
         # Set VLAN interfaces MTU last as they are restricted by underlying interfaces MTU
         for interface in sorted(
@@ -2115,6 +2100,7 @@ class InterfaceService(CRUDService):
             except Exception:
                 self.logger.error('Failed to configure {}'.format(interface), exc_info=True)
 
+        bridges = await self.middleware.call('datastore.query', 'network.bridge')
         for bridge in bridges:
             name = bridge['interface']['int_interface']
 
@@ -2144,10 +2130,6 @@ class InterfaceService(CRUDService):
         for name, iface in await self.middleware.run_in_thread(lambda: list(netif.list_interfaces().items())):
             # Skip internal interfaces
             if name.startswith(internal_interfaces):
-                continue
-
-            # bridge0/bridge1 are special, may be used by VM
-            if name in ('bridge0', 'bridge1'):
                 continue
 
             # If there are no interfaces configured we start DHCP on all

commit cf85be07f57b54170c9b3d08cbe78bd0bc58e415

On bridge update, do not put its member down if it already was a member of the bridge before update.
This bug was introduced by 97ff05a74845dacbd2974ef8c03a846e060986aa

src/middlewared/middlewared/plugins/interface/bridge.py
@@ -50,6 +50,7 @@ class InterfaceService(Service):
                 self.logger.info('Bringing up member interface %r in %r', member_iface.name, name)
                 member_iface.up()
 
+        for member in db_members:
             parent_interfaces.append(member)
 
         if iface.stp != bridge['stp']: