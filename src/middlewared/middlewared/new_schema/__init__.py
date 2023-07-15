4a9e773abb581b6364a6bcb003621a2a624e1738
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 05:32:33 +0400

    Move localusername schema to separate module

--- src/middlewared/middlewared/new_schema/__init__.py
+++ src/middlewared/middlewared/new_schema/__init__.py
@@ -2,4 +2,5 @@ from .attribute import Attribute  # noqa
 from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
 from .plugin_schema import Schemas  # noqa
+from .username import LocalUsername  # noqa
 from .utils import NOT_PROVIDED, REDACTED_VALUE  # noqa
