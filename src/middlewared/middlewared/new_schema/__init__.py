9bfa72581821d1505fe6614a858321862970f0c4
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 05:30:01 +0400

    Move enummixin to separate module

--- src/middlewared/middlewared/new_schema/__init__.py
+++ src/middlewared/middlewared/new_schema/__init__.py
@@ -1,4 +1,5 @@
 from .attribute import Attribute  # noqa
+from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
 from .plugin_schema import Schemas  # noqa
 from .utils import NOT_PROVIDED, REDACTED_VALUE  # noqa
