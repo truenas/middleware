fb0336e4b63915f0b5cc818c75d8f94a3c80bec0
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 06:37:39 +0400

    Fix convert schema import

--- src/middlewared/middlewared/schema/__init__.py
+++ src/middlewared/middlewared/schema/__init__.py
@@ -1,13 +1,17 @@
-from .adaptable_schemas import Any, Bool, OROperator, Patch, Ref  # noqa
+from middlewared.service_exception import ValidationErrors  # noqa
+
+from .adaptable_schemas import Any, Bool, OROperator, Ref  # noqa
 from .attribute import Attribute  # noqa
+from .convert_schema import convert_schema  # noqa
 from .dict_schema import Cron, Dict  # noqa
 from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
 from .integer_schema import Float, Int, Timestamp  # noqa
 from .list_schema import List  # noqa
+from .patch import Patch  # noqa
 from .plugin_schema import Schemas  # noqa
 from .processor import accepts, clean_and_validate_arg, returns  # noqa
-from .resolvers import convert_schema, resolve_methods  # noqa
+from .resolvers import resolve_methods  # noqa
 from .string_schema import ( # noqa
     Dataset, Datetime, Dir, File, HostPath, IPAddr, LDAP_DN, Path, Password, SID, Str, Time, UnixPerm, URI
 )
--- src/middlewared/middlewared/schema/resolvers.py
+++ src/middlewared/middlewared/schema/resolvers.py
@@ -1,25 +1,9 @@
 import pprint
 
-from .adaptable_schemas import Bool, OROperator, Patch, Ref
+from .adaptable_schemas import OROperator, Ref
 from .attribute import Attribute
-from .dict_schema import Dict
 from .exceptions import ResolverError
-from .integer_schema import Int
-from .string_schema import Str
-
-
-def convert_schema(spec):
-    t = spec.pop('type')
-    name = spec.pop('name')
-    if t in ('int', 'integer'):
-        return Int(name, **spec)
-    elif t in ('str', 'string'):
-        return Str(name, **spec)
-    elif t in ('bool', 'boolean'):
-        return Bool(name, **spec)
-    elif t == 'dict':
-        return Dict(name, *spec.get('args', []), **spec.get('kwargs', {}))
-    raise ValueError(f'Unknown type: {t}')
+from .patch import Patch
 
 
 def resolver(schemas, obj):
