01311324920757c186c5c5fab21ede29bcb62b04
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 05:48:20 +0400

    Move integer based schemas to separate module

--- src/middlewared/middlewared/new_schema/__init__.py
+++ src/middlewared/middlewared/new_schema/__init__.py
@@ -1,6 +1,7 @@
 from .attribute import Attribute  # noqa
 from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
+from .integer_schema import Float, Int, Timestamp  # noqa
 from .plugin_schema import Schemas  # noqa
 from .string_schema import ( # noqa
     Dataset, Datetime, Dir, File, HostPath, IPAddr, LDAP_DN, Path, Password, SID, Str, Time, UnixPerm, URI
