dbf8468fe59808fa109dc42265a9aa6d10ce29d7
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 05:25:47 +0400

    Move attribute schema to separate module

---
+++ src/middlewared/middlewared/new_schema/__init__.py
@@ -0,0 +1,3 @@
+from .attribute import Attribute  # noqa
+from .exceptions import Error  # noqa
+from .utils import NOT_PROVIDED, REDACTED_VALUE  # noqa
---
+++ src/middlewared/middlewared/new_schema/exceptions.py
@@ -0,0 +1,13 @@
+import errno
+
+
+class Error(Exception):
+
+    def __init__(self, attribute, errmsg, errno=errno.EINVAL):
+        self.attribute = attribute
+        self.errmsg = errmsg
+        self.errno = errno
+        self.extra = None
+
+    def __str__(self):
+        return '[{0}] {1}'.format(self.attribute, self.errmsg)
