b4b4fd0ec9854bcf8468cc017ade1d20d907379b
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-07-15 05:51:00 +0400

    Move list schema to separate module

--- src/middlewared/middlewared/new_schema/__init__.py
+++ src/middlewared/middlewared/new_schema/__init__.py
@@ -2,6 +2,7 @@ from .attribute import Attribute  # noqa
 from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
 from .integer_schema import Float, Int, Timestamp  # noqa
+from .list_schema import List  # noqa
 from .plugin_schema import Schemas  # noqa
 from .string_schema import ( # noqa
     Dataset, Datetime, Dir, File, HostPath, IPAddr, LDAP_DN, Path, Password, SID, Str, Time, UnixPerm, URI
