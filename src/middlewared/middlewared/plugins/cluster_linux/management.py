370f3e892e6d88ef10e169da62756deddb72ae0c
Andrew Walker <awalker@ixsystems.com>
2023-07-26 12:09:48 -0700

    Add docstring

--- src/middlewared/middlewared/plugins/cluster_linux/management.py
+++ src/middlewared/middlewared/plugins/cluster_linux/management.py
@@ -266,6 +266,42 @@ class ClusterManagement(Service):
         List('cluster_volumes')
     ))
     def summary(self, data):
+        """
+        WARNING: clustering APIs are not intended for 3rd-party consumption and may result
+        in a misconfigured SCALE cluster, production outage, or data loss.
+
+        This endpoint aggregates relevant cluster-related state into a single
+        large return. Generally, it is a good idea to subscribe to cluster events
+        which will send events from cluster leader on takeover (and fresh summary).
+
+        `include_volumes` - include clustered volume information (default) - may be
+        skipped as performance optimization.
+
+        Return contains the following:
+        `healthy` - whether cluster state is healthy (ctdb.general.healthy)
+        `leader` - object containing information about the current cluster leader
+        (meaning of items will be described below under `cluster_node`).
+        `cluster_nodes` an array of `cluster_node` objects. Note: these will _always_
+        be listed in ascending order based on `pnn` value. Cluster leader will be included.
+        `cluster_volumes` - an array of clustered (currently glusterfs) volumes
+
+        `cluster_node` object representing a single node in the cluster containing
+        the following keys:
+
+        `pnn` - internal node number.
+        `this_node` - whether the node in question is the current node.
+        `enabled` - whether the node is enabled or disabled. Node will be disabled prior
+        to replacement in the cluster. The final stages of node removal are disruptive
+        operations cluster-wide and so a disabled node may be present in summary until
+        administrator has maintenance window to finalize removal.
+        `uuid` - UUID uniquely identifying the node (glusterfs peer UUID)
+        `status` - object containing both ctdb and gluster peer status for node.
+        `status.node.flags` - ctdb status flags for node.
+        `status.node.partially_online` - ctdb status indicating that node is still coming online.
+        `status.peering.connected` - gluster peer status indicating whether peer is connected
+        `status.peering.state` - peering state of node
+        `status.peering.status` - verbose peer status
+        """
         healthy = self.middleware.call_sync('ctdb.general.healthy')
         leader = self.middleware.call_sync('ctdb.general.recovery_master')
         nodes_file = self.middleware.call_sync('ctdb.private.ips.query')
