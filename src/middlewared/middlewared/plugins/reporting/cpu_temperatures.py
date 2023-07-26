d1496743a867a884455b3074ee69abda2d388f92
M. Rehan <mrehanlm93@gmail.com>
2023-07-26 18:34:52 +0500

    Safely retrieve cpu temp stats

--- src/middlewared/middlewared/plugins/reporting/cpu_temperatures.py
+++ src/middlewared/middlewared/plugins/reporting/cpu_temperatures.py
@@ -7,6 +7,6 @@ class ReportingService(Service):
     async def cpu_temperatures(self):
         netdata_metrics = await self.middleware.call('netdata.get_all_metrics')
         data = {}
-        for core, cpu_temp in netdata_metrics['cputemp.temperatures']['dimensions'].items():
+        for core, cpu_temp in netdata_metrics.get('cputemp.temperatures', {'dimensions': {}})['dimensions'].items():
             data[core] = cpu_temp['value']
         return data
