c82c8196dce3d6632b39209490642327e4f9af70
caleb <yocalebo@gmail.com>
2023-07-31 08:30:36 -0400

    remove dead code

--- src/middlewared/middlewared/plugins/reporting/update.py
+++ src/middlewared/middlewared/plugins/reporting/update.py
@@ -1,14 +1,7 @@
-import copy
-import errno
-
 import middlewared.sqlalchemy as sa
-
-from middlewared.schema import accepts, Bool, Dict, Int, List, Patch, Ref, returns, Str
-from middlewared.service import CallError, ConfigService, filterable, filterable_returns, private, ValidationErrors
-from middlewared.utils import filter_list, run
+from middlewared.schema import accepts, Bool, Dict, Int, Patch, Str
+from middlewared.service import ConfigService
 from middlewared.validators import Range
-
-from .rrd_utils import RRD_PLUGINS
 
 
 class ReportingModel(sa.Model):
@@ -35,12 +28,6 @@ class ReportingService(ConfigService):
         Int('graph_points', validators=[Range(min=1, max=4096)], required=True),
         Int('id', required=True),
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__rrds = {}
-        for name, klass in RRD_PLUGINS.items():
-            self.__rrds[name] = klass(self.middleware)
 
     @accepts(
         Patch(
@@ -90,7 +77,8 @@ class ReportingService(ConfigService):
                 }]
             }
         """
-
+        # FIXME: The below code is a NO-OP (expected) and should be fixed
+        """
         confirm_rrd_destroy = data.pop('confirm_rrd_destroy', False)
 
         old = await self.config()
@@ -123,139 +111,8 @@ class ReportingService(ConfigService):
         )
 
         if destroy_database:
-            await self.clear(False)
+            # This was a NO-OP method after netdata integration
+            # await self.clear(False)
 
         return await self.config()
-
-    @accepts(Bool('start_collectd', default=True, hidden=True))
-    @returns()
-    async def clear(self, start_collectd):
         """
-        Clear reporting database.
-        """
-
-    @filterable
-    @filterable_returns(Dict(
-        'graph',
-        Str('name'),
-        Str('title'),
-        Str('vertical_label'),
-        List('identifiers', items=[Str('identifier')], null=True),
-        Bool('stacked'),
-        Bool('stacked_show_total'),
-    ))
-    def graphs(self, filters, options):
-        return filter_list([
-            i.asdict() for i in self.__rrds.values() if i.has_data()
-        ], filters, options)
-
-    def __rquery_to_start_end(self, query):
-        unit = query.get('unit')
-        if unit:
-            verrors = ValidationErrors()
-            for i in ('start', 'end'):
-                if i in query:
-                    verrors.add(
-                        f'reporting_query.{i}',
-                        f'{i!r} should only be used if "unit" attribute is not provided.',
-                    )
-            verrors.check()
-        else:
-            if 'start' not in query:
-                unit = 'HOURLY'
-            else:
-                starttime = query['start']
-                endtime = query.get('end') or 'now'
-
-        if unit:
-            unit = unit[0].lower()
-            page = query['page']
-            starttime = f'end-{page + 1}{unit}'
-            if not page:
-                endtime = 'now'
-            else:
-                endtime = f'now-{page}{unit}'
-        return starttime, endtime
-
-    @accepts(
-        List('graphs', items=[
-            Dict(
-                'graph',
-                Str('name', required=True),
-                Str('identifier', default=None, null=True),
-            ),
-        ], empty=False),
-        Dict(
-            'reporting_query',
-            Str('unit', enum=['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR']),
-            Int('page', default=0),
-            Str('start', empty=False),
-            Str('end', empty=False),
-            Bool('aggregate', default=True),
-            register=True,
-        )
-    )
-    @returns(List('reporting_data', items=[Dict(
-        'graph_reporting_data',
-        Str('name', required=True),
-        Str('identifier', required=True, null=True),
-        List('data'),
-        Dict(
-            'aggregations',
-            List('min'),
-            List('max'),
-            List('mean'),
-        ),
-        additional_attrs=True
-    )]))
-    def get_data(self, graphs, query):
-        """
-        Get reporting data for given graphs.
-
-        List of possible graphs can be retrieved using `reporting.graphs` call.
-
-        For the time period of the graph either `unit` and `page` OR `start` and `end` should be
-        used, not both.
-
-        `aggregate` will return aggregate available data for each graph (e.g. min, max, mean).
-
-        .. examples(websocket)::
-
-          Get graph data of "nfsstat" from the last hour.
-
-            :::javascript
-            {
-                "id": "6841f242-840a-11e6-a437-00e04d680384",
-                "msg": "method",
-                "method": "reporting.get_data",
-                "params": [
-                    [{"name": "nfsstat"}],
-                    {"unit": "HOURLY"},
-                ]
-            }
-
-        """
-        starttime, endtime = self.__rquery_to_start_end(query)
-        rv = []
-        for i in graphs:
-            try:
-                rrd = self.__rrds[i['name']]
-            except KeyError:
-                raise CallError(f'Graph {i["name"]!r} not found.', errno.ENOENT)
-            rv.append(
-                rrd.export(i['identifier'], starttime, endtime, aggregate=query['aggregate'])
-            )
-        return rv
-
-    @private
-    @accepts(Ref('reporting_query'))
-    def get_all(self, query):
-        starttime, endtime = self.__rquery_to_start_end(query)
-        rv = []
-        for rrd in self.__rrds.values():
-            idents = rrd.get_identifiers()
-            if idents is None:
-                idents = [None]
-            for ident in idents:
-                rv.append(rrd.export(ident, starttime, endtime, aggregate=query['aggregate']))
-        return rv
