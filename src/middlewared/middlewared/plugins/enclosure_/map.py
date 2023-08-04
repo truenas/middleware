1c6767bd101a522e89c3737115c9d94160f223d6
Caleb <yocalebo@gmail.com>
2023-08-04 08:03:29 -0400

    fix KeyError crash in enclosure.query
    (cherry picked from commit a92e2049ad7c40be7b44505eb73cf20472e1236d)

--- src/middlewared/middlewared/plugins/enclosure_/map.py
+++ src/middlewared/middlewared/plugins/enclosure_/map.py
@@ -438,7 +438,7 @@ class EnclosureService(Service):
         mapped = [
             {
                 "id": "mapped_enclosure_0",
-                "bsg": original_enclosure["bsg"],
+                "bsg": controller_enclosures[0]["bsg"],
                 "name": "Drive Bays",
                 "model": controller_enclosures[0]["model"],
                 "controller": True,
