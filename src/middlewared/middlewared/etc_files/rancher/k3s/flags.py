3bcb1bf444e4ac792d0f5142a572f97958de9dcd
Stavros kois <s.kois@outlook.com>
2023-08-15 12:30:33 +0300

    switch to KubeletConfiguration as flags are deprecated

--- src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
+++ src/middlewared/middlewared/etc_files/rancher/k3s/flags.py
@@ -7,6 +7,12 @@ import yaml
 
 
 FLAGS_PATH = '/etc/rancher/k3s/config.yaml'
+KUBELET_CONFIG_PATH = '/etc/rancher/k3s/kubelet_config.yaml'
+KUBELET_CONFIG = {
+    'apiVersion': 'kubelet.config.k8s.io/v1beta1',
+    'kind': 'KubeletConfiguration',
+    'maxPods': 250,
+}
 
 
 def render(service, middleware):
@@ -32,11 +38,14 @@ def render(service, middleware):
         'feature-gates=MixedProtocolLBService=true',
     ]
     kubelet_args = [
-        'max-pods=250',
+        f'config={KUBELET_CONFIG_PATH}',
     ]
     os.makedirs('/etc/rancher/k3s', exist_ok=True)
 
     features_mapping = {'servicelb': 'servicelb', 'metrics_server': 'metrics-server'}
+
+    with open(KUBELET_CONFIG_PATH, 'w') as f:
+        f.write(yaml.dump(KUBELET_CONFIG))
 
     with open(FLAGS_PATH, 'w') as f:
         f.write(yaml.dump({
