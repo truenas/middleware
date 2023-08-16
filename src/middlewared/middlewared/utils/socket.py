a4e7b12e8362bf5893c2a85f7c38427fcb7c6c09
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-08-16 14:32:01 +0500

    Catch FileNotFoundError to avoid race condition

--- src/middlewared/middlewared/utils/socket.py
+++ src/middlewared/middlewared/utils/socket.py
@@ -1,16 +1,13 @@
-import os
 import socket
 
 
 def is_socket_available(socket_path: str) -> bool:
     """Check if a Unix socket is available."""
-    if not os.path.exists(socket_path):
-        return False
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
         s.connect(socket_path)
         return True
-    except socket.error:
+    except (socket.error, FileNotFoundError):
         return False
     finally:
         s.close()
