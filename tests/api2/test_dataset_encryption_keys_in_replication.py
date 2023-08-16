9c80c787e17edc62cb66e7278ac18a8c1b8a75eb
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-08-16 17:46:55 +0500

    Add basic test to create datasets and run replication task
    (cherry picked from commit 4e37d451b17196031f84c6eacf7881be4e77ac0d)

---
+++ tests/api2/test_dataset_encryption_keys_in_replication.py
@@ -0,0 +1,39 @@
+from middlewared.test.integration.assets.pool import dataset
+from middlewared.test.integration.assets.replication import replication_task
+from middlewared.test.integration.utils import call
+
+
+BASE_REPLICATION = {
+    'direction': 'PUSH',
+    'transport': 'LOCAL',
+    'source_datasets': [],
+    'target_dataset': None,
+    'recursive': False,
+    'auto': False,
+    'retention_policy': 'NONE',
+}
+
+
+def encryption_props():
+    return {
+        'encryption_options': {'generate_key': True},
+        'encryption': True,
+        'inherit_encryption': False
+    }
+
+
+def test_single_source_replication():
+    with dataset('source_test', encryption_props(), pool='tank') as src:
+        with dataset('parent_destination', encryption_props(), pool='tank') as parent_ds:
+            with dataset(f'{parent_ds.rsplit("/", 1)[-1]}/destination_test', pool='tank') as dst:
+                with replication_task({
+                    **BASE_REPLICATION,
+                    'name': 'encryption_replication_test',
+                    'source_datasets': [src],
+                    'target_dataset': dst,
+                    'name_regex': '.+',
+                    'auto': False,
+                }) as task:
+
+                    call('zfs.snapshot.create', {'dataset': src, 'name': 'snap-1', 'recursive': True})
+                    call('replication.run', task['id'], job=True)
