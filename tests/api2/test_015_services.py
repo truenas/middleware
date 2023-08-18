60b9d6d946a9bd9544a63b79e05bf1b5100fedef
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-08-18 14:54:16 -0300

    Mark `test_non_silent_service_start_failure` as flaky (#11890)

--- tests/api2/test_015_services.py
+++ tests/api2/test_015_services.py
@@ -14,7 +14,8 @@ from middlewared.test.integration.utils import call, ssh
 pytestmark = pytest.mark.skipif(dev_test, reason='Skip for testing')
 
 
-def test_01_non_silent_service_start_failure():
+@pytest.mark.flaky(reruns=5, reruns_delay=5)  # Sometimes systemd unit state is erroneously reported as active
+def test_non_silent_service_start_failure():
     """
     This test for 2 conditions:
         1. middleware raises CallError that isn't empty
@@ -28,18 +29,18 @@ def test_01_non_silent_service_start_failure():
     # Error looks like
     """
     middlewared.service_exception.CallError: [EFAULT] Jan 10 08:49:14 systemd[1]: Starting Network UPS Tools - power device monitor and shutdown controller...
-    Jan 10 08:49:14 upsmon[3032658]: fopen /run/nut/upsmon.pid: No such file or directory
-    Jan 10 08:49:14 upsmon[3032658]: Unable to use old-style MONITOR line without a username
-    Jan 10 08:49:14 upsmon[3032658]: Convert it and add a username to upsd.users - see the documentation
-    Jan 10 08:49:14 upsmon[3032658]: Fatal error: unusable configuration
-    Jan 10 08:49:14 upsmon[3032658]: Network UPS Tools upsmon 2.7.4
+    Jan 10 08:49:14 nut-monitor[3032658]: fopen /run/nut/upsmon.pid: No such file or directory
+    Jan 10 08:49:14 nut-monitor[3032658]: Unable to use old-style MONITOR line without a username
+    Jan 10 08:49:14 nut-monitor[3032658]: Convert it and add a username to upsd.users - see the documentation
+    Jan 10 08:49:14 nut-monitor[3032658]: Fatal error: unusable configuration
+    Jan 10 08:49:14 nut-monitor[3032658]: Network UPS Tools upsmon 2.7.4
     Jan 10 08:49:14 systemd[1]: nut-monitor.service: Control process exited, code=exited, status=1/FAILURE
     Jan 10 08:49:14 systemd[1]: nut-monitor.service: Failed with result 'exit-code'.
     Jan 10 08:49:14 systemd[1]: Failed to start Network UPS Tools - power device monitor and shutdown controller.
     """
     lines1 = e.value.errmsg.splitlines()
     first_ts, len_lines1 = ' '.join(lines1.pop(0).split()[:3]), len(lines1)
-    assert any('upsmon[' in line for line in lines1), lines1
+    assert any('nut-monitor[' in line for line in lines1), lines1
     assert any('systemd[' in line for line in lines1), lines1
 
     # make sure we don't trigger system StartLimitBurst threshold
@@ -58,18 +59,18 @@ def test_01_non_silent_service_start_failure():
     # Error looks like: (Notice timestamp change, which is what we verify
     """
     middlewared.service_exception.CallError: [EFAULT] Jan 10 08:49:15 systemd[1]: Starting Network UPS Tools - power device monitor and shutdown controller...
-    Jan 10 08:49:15 upsmon[3032739]: fopen /run/nut/upsmon.pid: No such file or directory
-    Jan 10 08:49:15 upsmon[3032739]: Unable to use old-style MONITOR line without a username
-    Jan 10 08:49:15 upsmon[3032739]: Convert it and add a username to upsd.users - see the documentation
-    Jan 10 08:49:15 upsmon[3032739]: Fatal error: unusable configuration
-    Jan 10 08:49:15 upsmon[3032739]: Network UPS Tools upsmon 2.7.4
+    Jan 10 08:49:15 nut-monitor[3032739]: fopen /run/nut/upsmon.pid: No such file or directory
+    Jan 10 08:49:15 nut-monitor[3032739]: Unable to use old-style MONITOR line without a username
+    Jan 10 08:49:15 nut-monitor[3032739]: Convert it and add a username to upsd.users - see the documentation
+    Jan 10 08:49:15 nut-monitor[3032739]: Fatal error: unusable configuration
+    Jan 10 08:49:15 nut-monitor[3032739]: Network UPS Tools upsmon 2.7.4
     Jan 10 08:49:15 systemd[1]: nut-monitor.service: Control process exited, code=exited, status=1/FAILURE
     Jan 10 08:49:15 systemd[1]: nut-monitor.service: Failed with result 'exit-code'.
     Jan 10 08:49:15 systemd[1]: Failed to start Network UPS Tools - power device monitor and shutdown controller.
     """
     lines2 = e.value.errmsg.splitlines()
     second_ts, len_lines2 = ' '.join(lines2.pop(0).split()[:3]), len(lines2)
-    assert any('upsmon[' in line for line in lines2), lines2
+    assert any('nut-monitor[' in line for line in lines2), lines2
     assert any('systemd[' in line for line in lines2), lines2
 
     # timestamp should change since we sleep(1)
