00ef74bd9c6037958efc5572be29b7e28ab38eb0
mgrimesix <126630154+mgrimesix@users.noreply.github.com>
2023-08-24 07:34:04 -0700

    NAS-123599 / 24.04 / Remove unused 'quiet' parameter. (#11904) (#11913)
    * NAS-123599 / 23.10 / Remove unused 'quiet' parameter. (#11904)
    
    The `quiet` parameter for NFS shares is a legacy option from FreeBSD. We can safely remove to avoid confusion for CLI users.
    
    * Add initial merge migration to 24.04

---
+++ src/middlewared/middlewared/alembic/versions/24.04/2023-08-23_18-35_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 80c01d290a1d
+Revises: 1edf1b6b04db
+Create Date: 2023-08-23 18:35:29.133731+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '80c01d290a1d'
+down_revision = '1edf1b6b04db'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
