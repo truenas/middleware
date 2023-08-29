16af2fd680e52055b3b56e18faba6cb3ac59e531
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-08-29 10:48:54 -0300

    NAS-123654 / 23.10 / Use only one encrypted pool for system dataset on encrypted pool tests (by themylogin) (#11976)
    * Use only one encrypted pool for system dataset on encrypted pool tests
    
    (cherry picked from commit ef06e5015303bd3e53dccbe81d6a8c6ef77a9cfd)
    
    * Remove `/var/log` migration tests
    
    (cherry picked from commit d16b29cd3fd5cb39053f10a010a26c990989fa2c)
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>

--- tests/api2/test_system_dataset.py
+++ tests/api2/test_system_dataset.py
@@ -1,76 +1,53 @@
+import errno
+
 import pytest
-from pytest_dependency import depends
 
+from middlewared.service_exception import CallError
 from middlewared.test.integration.assets.pool import another_pool
-from middlewared.test.integration.utils import call, mock, pool, ssh
-
-from auto_config import dev_test
-pytestmark = pytest.mark.skipif(dev_test, reason='Skipping for test development testing')
+from middlewared.test.integration.utils import call, pool
 
 
-def read_log():
-    return ssh("cat /var/log/middlewared.log")
+@pytest.fixture(scope="session")
+def passphrase_encrypted_pool_session():
+    with another_pool({"encryption": True, "encryption_options": {"passphrase": "passphrase"}}) as p:
+        yield p["name"]
 
 
-def write_to_log(string):
-    assert string not in read_log()
-
-    with mock("test.test1", f"""
-        from middlewared.service import lock
-
-        async def mock(self, *args):
-            self.logger.debug({string!r})
-    """):
-        call("test.test1")
-
-    assert string in read_log()
-
-
-def test_system_dataset_migrate(request):
-
+@pytest.fixture(scope="function")
+def passphrase_encrypted_pool(passphrase_encrypted_pool_session):
     config = call("systemdataset.config")
     assert config["pool"] == pool
-    assert config["syslog"]
 
-    # Make sure that log files are synced to the new location
-    write_to_log("test_system_dataset_migrate step 1")
+    try:
+        call("pool.dataset.delete", passphrase_encrypted_pool_session, {"recursive": True})
+    except CallError as e:
+        if e.errno != errno.ENOENT:
+            raise
 
-    call("systemdataset.update", {"pool": "boot-pool"}, job=True)
-    assert "test_system_dataset_migrate step 1" in read_log()
-
-    write_to_log("test_system_dataset_migrate step 2")
-
-    call("systemdataset.update", {"pool": pool}, job=True)
-    assert "test_system_dataset_migrate step 1" in read_log()
-    assert "test_system_dataset_migrate step 2" in read_log()
+    yield passphrase_encrypted_pool_session
 
 
 @pytest.mark.parametrize("lock", [False, True])
-def test_migrate_to_a_pool_with_passphrase_encrypted_root_dataset(request, lock):
+def test_migrate_to_a_pool_with_passphrase_encrypted_root_dataset(passphrase_encrypted_pool, lock):
+    if lock:
+        call("pool.dataset.lock", passphrase_encrypted_pool, job=True)
 
-    config = call("systemdataset.config")
-    assert config["pool"] == pool
+    assert passphrase_encrypted_pool in call("systemdataset.pool_choices")
 
-    with another_pool({"encryption": True, "encryption_options": {"passphrase": "passphrase"}}):
-        if lock:
-            call("pool.dataset.lock", "test", job=True)
+    call("systemdataset.update", {"pool": passphrase_encrypted_pool}, job=True)
 
-        assert "test" in call("systemdataset.pool_choices")
+    ds = call("zfs.dataset.get_instance", f"{passphrase_encrypted_pool}/.system")
+    assert ds["properties"]["encryption"]["value"] == "off"
 
-        call("systemdataset.update", {"pool": "test"}, job=True)
-
-        ds = call("zfs.dataset.get_instance", "test/.system")
-        assert ds["properties"]["encryption"]["value"] == "off"
-
-        call("systemdataset.update", {"pool": pool}, job=True)
+    call("systemdataset.update", {"pool": pool}, job=True)
 
 
-def test_lock_passphrase_encrypted_pool_with_system_dataset(request):
+def test_lock_passphrase_encrypted_pool_with_system_dataset(passphrase_encrypted_pool):
+    call("systemdataset.update", {"pool": passphrase_encrypted_pool}, job=True)
 
-    with another_pool({"encryption": True, "encryption_options": {"passphrase": "passphrase"}}):
-        call("systemdataset.update", {"pool": "test"}, job=True)
+    call("pool.dataset.lock", passphrase_encrypted_pool, job=True)
 
-        call("pool.dataset.lock", "test", job=True)
+    ds = call("zfs.dataset.get_instance", f"{passphrase_encrypted_pool}/.system")
+    assert ds["properties"]["mounted"]["value"] == "yes"
 
-        ds = call("zfs.dataset.get_instance", "test/.system")
-        assert ds["properties"]["mounted"]["value"] == "yes"
+    call("systemdataset.update", {"pool": pool}, job=True)
