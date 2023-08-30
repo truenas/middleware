10364f4bc248d6bd44294a4f75b8515ac2650767
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-08-30 12:46:09 -0300

    NAS-123821 / 23.10 / Fix zpool status alerts (by yocalebo) (#11997)
    * fix volume status alerts
    
    (cherry picked from commit c884ecf6eafbe28a81df6de62acd808a9943a5af)
    
    * minor fixes
    
    (cherry picked from commit ec27aee82bb3792b65ec046bfc6a78fef085b72a)
    
    ---------
    
    Co-authored-by: Caleb <yocalebo@gmail.com>

---
+++ src/middlewared/middlewared/utils/zfs.py
@@ -0,0 +1,36 @@
+import os
+
+
+def query_imported_fast_impl(name_filters=None):
+    # the equivalent of running `zpool list -H -o guid,name` from cli
+    # name_filters will be a list of pool names
+    out = dict()
+    name_filters = name_filters or []
+    with os.scandir('/proc/spl/kstat/zfs') as it:
+        for entry in filter(lambda entry: not name_filters or entry.name in name_filters, it):
+            if not entry.is_dir() or entry.name == '$import':
+                continue
+
+            guid = guid_fast_impl(entry.name)
+            state = state_fast_impl(entry.name)
+            out.update({guid: {'name': entry.name, 'state': state}})
+
+    return out
+
+
+def guid_fast_impl(pool):
+    """
+    Lockless read of zpool guid. Raises FileNotFoundError
+    if pool not imported.
+    """
+    with open(f'/proc/spl/kstat/zfs/{pool}/guid') as f:
+        return f.read().strip()
+
+
+def state_fast_impl(pool):
+    """
+    Lockless read of zpool state. Raises FileNotFoundError
+    if pool not imported.
+    """
+    with open(f'/proc/spl/kstat/zfs/{pool}/state') as f:
+        return f.read().strip()
