a348c1bbe91899d42e65633bd1c94acbb83fb9ba
Caleb <yocalebo@gmail.com>
2023-09-01 08:14:41 -0400

    fix init_shutdown_script.py

--- src/middlewared/middlewared/plugins/init_shutdown_script.py
+++ src/middlewared/middlewared/plugins/init_shutdown_script.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import stat
 import subprocess
 import tempfile
 
@@ -58,8 +57,8 @@ class InitShutdownScriptService(CRUDService):
 
         There are three choices for `when`:
 
-        1) PREINIT - This is early in the boot process before all the services / rc scripts have started
-        2) POSTINIT - This is late in the boot process when most of the services / rc scripts have started
+        1) PREINIT - This is early in the boot process before all the services have started
+        2) POSTINIT - This is late in the boot process when most of the services have started
         3) SHUTDOWN - This is on shutdown
 
         `timeout` is an integer value which indicates time in seconds which the system should wait for the execution
@@ -69,16 +68,13 @@ class InitShutdownScriptService(CRUDService):
         by the base OS's limit.
         """
         await self.validate(data, 'init_shutdown_script_create')
-
         await self.init_shutdown_script_compress(data)
-
         data['id'] = await self.middleware.call(
             'datastore.insert',
             self._config.datastore,
             data,
             {'prefix': self._config.datastore_prefix}
         )
-
         return await self.get_instance(data['id'])
 
     async def do_update(self, id_, data):
@@ -88,11 +84,8 @@ class InitShutdownScriptService(CRUDService):
         old = await self.get_instance(id_)
         new = old.copy()
         new.update(data)
-
         await self.validate(new, 'init_shutdown_script_update')
-
         await self.init_shutdown_script_compress(new)
-
         await self.middleware.call(
             'datastore.update',
             self._config.datastore,
@@ -100,31 +93,24 @@ class InitShutdownScriptService(CRUDService):
             new,
             {'prefix': self._config.datastore_prefix}
         )
-
         return await self.get_instance(new['id'])
 
     async def do_delete(self, id_):
         """
         Delete init/shutdown task of `id`.
         """
-        return await self.middleware.call(
-            'datastore.delete',
-            self._config.datastore,
-            id_
-        )
+        return await self.middleware.call('datastore.delete', self._config.datastore, id_)
 
     @private
     async def init_shutdown_script_extend(self, data):
         data['type'] = data['type'].upper()
         data['when'] = data['when'].upper()
-
         return data
 
     @private
     async def init_shutdown_script_compress(self, data):
         data['type'] = data['type'].lower()
         data['when'] = data['when'].lower()
-
         return data
 
     @private
@@ -157,56 +143,55 @@ class InitShutdownScriptService(CRUDService):
         verrors.check()
 
     @private
+    def write_script_text(self, script_text):
+        with tempfile.NamedTemporaryFile(delete=False, mode='w') as f:
+            f.write(script_text)
+            return f.name
+
+    @private
+    def script_already_exists(self, script):
+        return os.path.exists(script) and os.access(script, os.X_OK)
+
+    @private
+    def rem_tmp_script(self, script):
+        try:
+            os.unlink(script)
+        except FileNotFoundError:
+            pass
+        except Exception:
+            self.logger.debug('Unexpected failure removing %r', script, exc_info=True)
+
+    @private
     async def execute_task(self, task):
         task_type = task['type']
-        cmd = None
-        tmp_script = None
+        cmd = tmp_script = None
 
         if task_type == 'COMMAND':
             cmd = task['command']
         elif task_type == 'SCRIPT' and task['script_text']:
-            fd, tmp_script = tempfile.mkstemp(text=True)
-            os.close(fd)
-            os.chmod(tmp_script, stat.S_IRWXU)
-            with open(tmp_script, 'w') as f:
-                f.write(task['script_text'])
+            tmp_script = await self.middleware.run_in_thread(self.write_script_text, task['script_text'])
             cmd = f'exec {tmp_script}'
-        elif os.path.exists(task['script'] or '') and os.access(task['script'], os.X_OK):
+        elif await self.middleware.run_in_thread(self.script_already_exists, task['script']):
             cmd = f'exec {task["script"]}'
+        else:
+            return
 
         try:
-            if cmd:
-                proc = await Popen(
-                    cmd,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT,
-                    shell=True,
-                    close_fds=True
-                )
-                stdout, stderr = await proc.communicate()
-
-                if proc.returncode:
-                    if task_type == 'COMMAND':
-                        cmd = task['command']
-                    elif task_type == 'SCRIPT' and task['script_text']:
-                        cmd = task['comment']
-                    elif task_type == 'SCRIPT' and task['script']:
-                        cmd = task['script']
-                    else:
-                        cmd = ''
-                    self.middleware.logger.debug(
-                        'Execution failed for '
-                        f'{task_type} {cmd}: {stdout.decode()}'
-                    )
-        except Exception as error:
-            if task_type == 'SCRIPT' and task['script_text']:
-                cmd = task['comment']
-            self.middleware.logger.debug(
-                f'{task["type"]} {cmd}: {error!r}'
+            proc = await Popen(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                shell=True,
+                close_fds=True
             )
+            stdout, stderr = await proc.communicate()
+            if proc.returncode:
+                self.logger.debug('Failed to execute %r with error %r', cmd, stdout.decode())
+        except Exception:
+            self.logger.debug('Unexpected failure executing %r', cmd, exc_info=True)
         finally:
-            if tmp_script and os.path.exists(tmp_script):
-                os.unlink(tmp_script)
+            if tmp_script is not None:
+                await self.middleware.run_in_thread(self.rem_tmp_script, tmp_script)
 
     @private
     @accepts(
@@ -214,14 +199,13 @@ class InitShutdownScriptService(CRUDService):
     )
     @job()
     async def execute_init_tasks(self, job, when):
-
-        tasks = await self.middleware.call(
-            'initshutdownscript.query', [
+        tasks = await self.middleware.call('initshutdownscript.query', [
                 ['enabled', '=', True],
                 ['when', '=', when]
-            ])
+        ])
+        tasks_len = len(tasks)
 
-        for i, task in enumerate(tasks):
+        for idx, task in enumerate(tasks):
             try:
                 await asyncio.wait_for(self.middleware.create_task(self.execute_task(task)), timeout=task['timeout'])
             except asyncio.TimeoutError:
@@ -233,8 +217,8 @@ class InitShutdownScriptService(CRUDService):
                     cmd = task['script']
                 else:
                     cmd = ''
-                self.middleware.logger.debug(f'{task["type"]} {cmd} timed out')
+                self.logger.debug('Timed out running %s: %r', task['type'], cmd)
             finally:
-                job.set_progress((100 / len(tasks)) * (i + 1))
+                job.set_progress((100 / tasks_len) * (idx + 1))
 
         job.set_progress(100, f'Completed tasks for {when}')
