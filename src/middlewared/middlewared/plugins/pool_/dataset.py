4717cbc4560a0a1e47acff5faf4bbff327f7a55a
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-09-02 05:21:15 +0500

    Fix event name when creating/updating dataset

--- src/middlewared/middlewared/plugins/pool_/dataset.py
+++ src/middlewared/middlewared/plugins/pool_/dataset.py
@@ -718,7 +718,7 @@ class PoolDatasetService(CRUDService):
             })
             await acl_job.wait(raise_error=True)
 
-        self.middleware.send_event('pool.query', 'ADDED', id=data['id'], fields=created_ds)
+        self.middleware.send_event('pool.dataset.query', 'ADDED', id=data['id'], fields=created_ds)
         return created_ds
 
     @accepts(Str('id', required=True), Patch(
@@ -866,7 +866,7 @@ class PoolDatasetService(CRUDService):
             await self.middleware.call('iscsi.global.resync_lun_size_for_zvol', id_)
 
         updated_ds = await self.get_instance(id_)
-        self.middleware.send_event('pool.query', 'CHANGED', id=id_, fields=updated_ds)
+        self.middleware.send_event('pool.dataset.query', 'CHANGED', id=id_, fields=updated_ds)
         return updated_ds
 
     @accepts(Str('id'), Dict(
