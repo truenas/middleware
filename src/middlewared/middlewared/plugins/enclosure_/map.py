9cdff0fd32f7031566fb8ed461d89b1fcaea7bb3
caleb <yocalebo@gmail.com>
2023-09-25 07:11:21 -0400

    fix enclosur emapping for platforms with nvme

--- src/middlewared/middlewared/plugins/enclosure_/map.py
+++ src/middlewared/middlewared/plugins/enclosure_/map.py
@@ -439,9 +439,9 @@ class EnclosureService(Service):
         mapped = [
             {
                 "id": "mapped_enclosure_0",
-                "bsg": controller_enclosures[0]["bsg"],
+                "bsg": original_enclosure["bsg"],
                 "name": "Drive Bays",
-                "model": controller_enclosures[0]["model"],
+                "model": original_enclosure["model"],
                 "controller": True,
                 "elements": [
                     {
