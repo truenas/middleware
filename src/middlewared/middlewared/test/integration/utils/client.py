53d216425898236e56ebd558c3cb6db5571b85a4
Andrew Walker <awalker@ixsystems.com>
2023-09-29 05:07:14 -0700

    When possible reuse websocket sessions in tests
    This is a minor optimization to open and use a single websocket
    client connection for the duration of the test. API consumers
    (for instance for HA tests) can import the PersistentCtx and
    run setup method with relevant arguments.

--- src/middlewared/middlewared/test/integration/utils/client.py
+++ src/middlewared/middlewared/test/integration/utils/client.py
@@ -7,11 +7,60 @@ import requests
 from middlewared.client import Client
 from middlewared.client.utils import undefined
 
-__all__ = ["client", "host", "host_websocket_uri", "password", "session", "url", "websocket_url"]
+__all__ = ["client", "host", "host_websocket_uri", "password", "session", "url", "websocket_url", "PersistentCtx"]
+
+
+class ClientCtx:
+    conn = None
+
+    def setup(self, *, auth=undefined, auth_required=True, py_exceptions=True, log_py_exceptions=True, host_ip=None):
+        if auth is None:
+            raise ValueError('Authentication is required for client context wrapper')
+
+        elif auth is undefined:
+            auth = ("root", password())
+
+        if self.conn is not None:
+            self.conn.close()
+            self.conn = None
+
+        self.conn = Client(
+            host_websocket_uri(host_ip),
+            py_exceptions=py_exceptions,
+            log_py_exceptions=log_py_exceptions
+        )
+
+        try:
+            logged_in = self.conn.call("auth.login", *auth)
+            if auth_required:
+                assert logged_in
+        except Exception:
+            self.conn.close()
+            self.conn = None
+            raise
+
+        return self.conn
+
+    def get_or_setup(self, *args, **kwargs):
+        if self.conn:
+            try:
+                self.conn.ping()
+                return self.conn
+            except Exception:
+                pass
+
+        return self.setup(*args, **kwargs)
+
+
+PersistentCtx = ClientCtx()
 
 
 @contextlib.contextmanager
 def client(*, auth=undefined, auth_required=True, py_exceptions=True, log_py_exceptions=True, host_ip=None):
+    if auth is undefined and host_ip is None and auth_required:
+        yield PersistentCtx.get_or_setup()
+        return
+
     if auth is undefined:
         auth = ("root", password())
 
