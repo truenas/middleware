46f0df87a4411d7d88515be93f1e4eaefdeeb1c6
M. Rehan <mrehanlm93@gmail.com>
2023-10-05 16:30:59 +0500

    NAS-124182 / 24.04 / Fix disk temperature graph (#12245)
    * refector smartd plugin for 30 sec time interval
    
    * test fix

--- src/middlewared/middlewared/pytest/unit/plugins/reporting/test_netdata_approximation.py
+++ src/middlewared/middlewared/pytest/unit/plugins/reporting/test_netdata_approximation.py
@@ -4,9 +4,9 @@ from middlewared.plugins.reporting.utils import get_metrics_approximation, calcu
 
 
 @pytest.mark.parametrize('disk_count,core_count,interface_count,pool_count,expected_output', [
-    (4, 2, 1, 2, {1: 354, 1800: 4}),
-    (1600, 32, 4, 4, {1: 44001, 1800: 1600}),
-    (10, 16, 2, 2, {1: 761, 1800: 10}),
+    (4, 2, 1, 2, {1: 354, 60: 4}),
+    (1600, 32, 4, 4, {1: 44001, 60: 1600}),
+    (10, 16, 2, 2, {1: 761, 60: 10}),
 ])
 def test_netdata_metrics_count_approximation(disk_count, core_count, interface_count, pool_count, expected_output):
     assert get_metrics_approximation(disk_count, core_count, interface_count, pool_count) == expected_output
@@ -14,9 +14,9 @@ def test_netdata_metrics_count_approximation(disk_count, core_count, interface_c
 
 @pytest.mark.parametrize('disk_count,core_count,interface_count,pool_count,days,expected_output', [
     (4, 2, 1, 2, 7, 204),
-    (1600, 32, 4, 4, 4, 14502),
+    (1600, 32, 4, 4, 4, 14511),
     (10, 16, 2, 2, 3, 188),
-    (1600, 32, 4, 4, 18, 65261),
+    (1600, 32, 4, 4, 18, 65299),
 ])
 def test_netdata_disk_space_approximation(disk_count, core_count, interface_count, pool_count, days, expected_output):
     assert calculate_disk_space_for_netdata(get_metrics_approximation(
