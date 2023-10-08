bf83ac0e5589e201ca4e45ed664bfdc1ab123cd1
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-08 16:26:11 +0500

    Add 2fa migration file

---
+++ src/middlewared/middlewared/alembic/versions/24.04/2023-10-08_16-22_2fa_per_user_settings.py
@@ -0,0 +1,22 @@
+"""
+2FA Per user settings
+
+Revision ID: 0a95d753f6d3
+Revises: 3df553b07a99
+Create Date: 2023-10-08 16:22:17.935672+00:00
+"""
+from alembic import op
+
+
+revision = '0a95d753f6d3'
+down_revision = '3df553b07a99'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
