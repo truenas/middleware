f0da2e590c87ef1bd1f46b9d4f11dd3557db3295
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-08 16:34:45 +0500

    Add otp/window/interval columns to the user 2fa table

--- src/middlewared/middlewared/alembic/versions/24.04/2023-10-08_16-22_2fa_per_user_settings.py
+++ src/middlewared/middlewared/alembic/versions/24.04/2023-10-08_16-22_2fa_per_user_settings.py
@@ -5,6 +5,8 @@ Revision ID: 0a95d753f6d3
 Revises: 3df553b07a99
 Create Date: 2023-10-08 16:22:17.935672+00:00
 """
+import sqlalchemy as sa
+
 from alembic import op
 
 
@@ -15,7 +17,10 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    with op.batch_alter_table('account_twofactor_user_auth', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('interval', sa.INTEGER(), nullable=False, server_default='30'))
+        batch_op.add_column(sa.Column('otp_digits', sa.INTEGER(), nullable=False, server_default='6'))
+        batch_op.add_column(sa.Column('window', sa.INTEGER(), nullable=False, server_default='0'))
 
 
 def downgrade():
