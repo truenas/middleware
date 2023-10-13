2eb26bc161f154eebc25a2f9c9b7d7d88a21bc1f
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-14 00:52:07 +0500

    Update existing 2fa integration tests to reflect newer reality of 2fa attrs present for each user

--- tests/api2/test_twofactor_auth.py
+++ tests/api2/test_twofactor_auth.py
@@ -16,6 +16,7 @@ TEST_USERNAME_2 = 'test2fauser2'
 TEST_PASSWORD = 'testpassword'
 TEST_PASSWORD_2 = 'testpassword2'
 TEST_GID = 544
+TEST_TWOFACTOR_INTERVAL = {'interval': 60}
 
 
 @contextlib.contextmanager
@@ -43,8 +44,12 @@ def test_secret_generation_for_user():
         assert get_user_secret(user_obj['id'], False) != []
         assert get_user_secret(user_obj['id'])['secret'] is None
 
-        call('user.renew_2fa_secret', user_obj['username'])
-        assert get_user_secret(user_obj['id'])['secret'] is not None
+        call('user.renew_2fa_secret', user_obj['username'], {'interval': 70, 'otp_digits': 8})
+
+        user_secret_obj = get_user_secret(user_obj['id'])
+        assert user_secret_obj['secret'] is not None
+        for k, v in (('interval', 70), ('otp_digits', 8)):
+            assert user_secret_obj[k] == v
 
 
 def test_login_without_otp_for_user_without_2fa():
@@ -64,10 +69,10 @@ def test_login_with_otp_for_user_with_2fa():
         'full_name': TEST_USERNAME_2,
     }) as user_obj:
         with enabled_twofactor_auth():
-            call('user.renew_2fa_secret', user_obj['username'])
+            call('user.renew_2fa_secret', user_obj['username'], TEST_TWOFACTOR_INTERVAL)
             assert call(
                 'auth.get_login_user', TEST_USERNAME_2, TEST_PASSWORD_2,
-                get_2fa_totp_token(get_user_secret(user_obj['id'])['secret'])
+                get_2fa_totp_token(get_user_secret(user_obj['id']))
             ) is not None
 
 
@@ -78,20 +83,20 @@ def test_user_2fa_secret_renewal():
         'full_name': TEST_USERNAME_2,
     }) as user_obj:
         with enabled_twofactor_auth():
-            call('user.renew_2fa_secret', user_obj['username'])
+            call('user.renew_2fa_secret', user_obj['username'], TEST_TWOFACTOR_INTERVAL)
             assert call(
                 'auth.get_login_user', TEST_USERNAME_2, TEST_PASSWORD_2,
-                get_2fa_totp_token(get_user_secret(user_obj['id'])['secret'])
+                get_2fa_totp_token(get_user_secret(user_obj['id']))
             ) is not None
-            secret = get_user_secret(user_obj['id'])['secret']
+            secret = get_user_secret(user_obj['id'])
 
-            call('user.renew_2fa_secret', user_obj['username'])
+            call('user.renew_2fa_secret', user_obj['username'], TEST_TWOFACTOR_INTERVAL)
             call('user.get_instance', user_obj['id'])
             assert get_user_secret(user_obj['id'])['secret'] != secret
 
             assert call(
                 'auth.get_login_user', TEST_USERNAME_2, TEST_PASSWORD_2,
-                get_2fa_totp_token(get_user_secret(user_obj['id'])['secret'])
+                get_2fa_totp_token(get_user_secret(user_obj['id']))
             ) is not None
 
 
@@ -109,17 +114,17 @@ def test_multiple_users_login_with_otp():
                 'password': TEST_PASSWORD_2,
                 'full_name': TEST_USERNAME_2,
             }) as second_user:
-                call('user.renew_2fa_secret', second_user['username'])
+                call('user.renew_2fa_secret', second_user['username'], TEST_TWOFACTOR_INTERVAL)
                 assert call(
                     'auth.get_login_user', TEST_USERNAME_2, TEST_PASSWORD_2,
-                    get_2fa_totp_token(get_user_secret(second_user['id'])['secret'])
+                    get_2fa_totp_token(get_user_secret(second_user['id']))
                 ) is not None
 
                 assert call('auth.get_login_user', TEST_USERNAME_2, TEST_PASSWORD_2) is None
 
-                call('user.renew_2fa_secret', first_user['username'])
+                call('user.renew_2fa_secret', first_user['username'], TEST_TWOFACTOR_INTERVAL)
 
                 assert call(
                     'auth.get_login_user', TEST_USERNAME, TEST_PASSWORD,
-                    get_2fa_totp_token(get_user_secret(first_user['id'])['secret'])
+                    get_2fa_totp_token(get_user_secret(first_user['id']))
                 ) is not None
