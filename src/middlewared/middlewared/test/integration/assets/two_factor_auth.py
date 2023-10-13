c3a1e001fc3f7699b35df7d502f8443766239761
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-14 00:45:43 +0500

    Update 2fa test assets to correctly reflect newer changes

--- src/middlewared/middlewared/test/integration/assets/two_factor_auth.py
+++ src/middlewared/middlewared/test/integration/assets/two_factor_auth.py
@@ -8,19 +8,18 @@ from middlewared.test.integration.utils import call
 @contextlib.contextmanager
 def enabled_twofactor_auth():
     try:
-        yield call('auth.twofactor.update', {'enabled': True, 'window': 3, 'interval': 60})
+        yield call('auth.twofactor.update', {'enabled': True, 'window': 3})
     finally:
-        call('auth.twofactor.update', {'enabled': False, 'window': 0, 'interval': 30})
+        call('auth.twofactor.update', {'enabled': False, 'window': 0})
 
 
 def get_user_secret(user_id: int, get: typing.Optional[bool] = True) -> typing.Union[dict, list]:
     return call('datastore.query', 'account.twofactor_user_auth', [['user_id', '=', user_id]], {'get': get})
 
 
-def get_2fa_totp_token(secret: str) -> str:
-    twofactor_config = call('auth.twofactor.config')
+def get_2fa_totp_token(users_config: dict) -> str:
     return pyotp.TOTP(
-        secret,
-        interval=twofactor_config['interval'],
-        digits=twofactor_config['otp_digits'],
+        users_config['secret'],
+        interval=users_config['interval'],
+        digits=users_config['otp_digits'],
     ).now()
