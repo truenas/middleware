ad9973e6dee801319343f088d03bc9bfc838b136
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-15 03:52:29 +0500

    Reduce the time to check if truecommand connection is active
    This commit adds changes to reduce the time to check if truecommand connection is active to 30 seconds instead of 30 minutes after setting up the interfaces and everything because the latter was way too long and system only updated the status before if truecommand.config was explicitly called. Setting it to 30 seconds works nicely and is enough to ensure the relevant wireguard interface is up and everything.

--- src/middlewared/middlewared/plugins/truecommand/wireguard.py
+++ src/middlewared/middlewared/plugins/truecommand/wireguard.py
@@ -131,8 +131,8 @@ class TruecommandService(Service):
                 await self.middleware.call('service.start', 'truecommand')
                 await self.middleware.call('service.reload', 'http')
                 asyncio.get_event_loop().call_later(
-                    HEALTH_CHECK_SECONDS,
-                    lambda: asyncio.ensure_future(self.middleware.call('truecommand.health_check')),
+                    30,  # 30 seconds is enough time to initiate a health check to see if the connection is alive
+                    lambda: self.middleware.create_task(self.middleware.call('truecommand.health_check')),
                 )
             else:
                 # start polling iX Portal to see what's up and why we don't have these values set
