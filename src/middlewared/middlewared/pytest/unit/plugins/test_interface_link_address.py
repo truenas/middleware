f9f2fd4249b04d626f9c84ba0cd31508c6be15c2
themylogin <themylogin@gmail.com>
2023-10-25 00:35:50 +0200

    Persist all network interface hardware addresses regardless of if they are configured (#12381)

--- src/middlewared/middlewared/pytest/unit/plugins/test_interface_link_address.py
+++ src/middlewared/middlewared/pytest/unit/plugins/test_interface_link_address.py
@@ -7,71 +7,187 @@ import sqlalchemy as sa
 
 from middlewared.plugins.interface.link_address import InterfaceService, setup as link_address_setup
 from middlewared.pytest.unit.plugins.test_datastore import Model, datastore_test
+from middlewared.sqlalchemy import JSON
 
 logger = logging.getLogger(__name__)
 
 
-class InterfaceModel(Model):
+class NetworkBridgeModel(Model):
+    __tablename__ = 'network_bridge'
+
+    id = sa.Column(sa.Integer(), primary_key=True)  # noqa
+    interface_id = sa.Column(sa.ForeignKey('network_interfaces.id', ondelete='CASCADE'))
+    members = sa.Column(JSON(list), default=[])
+
+
+class NetworkInterfaceModel(Model):
     __tablename__ = "network_interfaces"
 
-    id = sa.Column(sa.Integer(), primary_key=True)
+    id = sa.Column(sa.Integer(), primary_key=True)  # noqa
     int_interface = sa.Column(sa.Integer(), nullable=False)
-    int_link_address = sa.Column(sa.String(17), nullable=True)
     int_settings = sa.Column(sa.Integer())
 
 
-@pytest.mark.parametrize("test", [
-    # (
-    #     Hardware interfaces
-    #     Interfaces in database BEFORE (marked by settings ID)
-    #     Interfaces in database AFTER (marked by settings ID)
-    # )
+class NetworkInterfaceLinkAddressModel(Model):
+    __tablename__ = 'network_interface_link_address'
+
+    id = sa.Column(sa.Integer, primary_key=True)  # noqa
+    interface = sa.Column(sa.String(300))
+    link_address = sa.Column(sa.String(17), nullable=True)
+    link_address_b = sa.Column(sa.String(17), nullable=True)
+
+
+class NetworkLaggInterfaceModel(Model):
+    __tablename__ = 'network_lagginterface'
+
+    id = sa.Column(sa.Integer, primary_key=True)  # noqa
+    lagg_interface_id = sa.Column(sa.Integer(), sa.ForeignKey('network_interfaces.id'))
+
+
+class NetworkLaggInterfaceMemberModel(Model):
+    __tablename__ = 'network_lagginterfacemembers'
+
+    id = sa.Column(sa.Integer, primary_key=True)  # noqa
+    lagg_interfacegroup_id = sa.Column(sa.ForeignKey('network_lagginterface.id', ondelete='CASCADE'), index=True)
+    lagg_physnic = sa.Column(sa.String(120), unique=True)
+
+
+class NetworkVlanModel(Model):
+    __tablename__ = 'network_vlan'
+
+    id = sa.Column(sa.Integer(), primary_key=True)  # noqa
+    vlan_vint = sa.Column(sa.String(120))
+    vlan_pint = sa.Column(sa.String(300))
+
+
+class VMDeviceModel(Model):
+    __tablename__ = 'vm_device'
+
+    id = sa.Column(sa.Integer(), primary_key=True)  # noqa
+    dtype = sa.Column(sa.String(50))
+    attributes = sa.Column(sa.JSON())
+
+
+@pytest.mark.parametrize("before,after", [
+    # BSD -> Linux interface rename
     (
-        [("eth0", "08:00:27:1e:9f:d3"), ("eth1", "08:00:27:1e:9f:d4")],
-        [("em0", "08:00:27:1e:9f:d4", 0), ("em1", "08:00:27:1e:9f:d3", 1)],
-        [("eth0", "08:00:27:1e:9f:d3", 1), ("eth1", "08:00:27:1e:9f:d4", 0)],
+        {
+            "hw": {"em0": "08:00:27:1e:9f:d3", "em1": "08:00:27:1e:9f:d4"},
+            "interface": {"em0": 0, "em1": 1, "bridge6": 66, "lagg7": 77, "vlan8": 88},
+            "bridge": {"bridge6": ["em0", "em1"]},
+            "lagg": {"lagg7": ["em1", "em0"]},
+            "vlan": {"vlan8": "em1"},
+            "vm": ["em1"],
+        },
+        {
+            "hw": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "interface": {"eth0": 0, "eth1": 1, "br6": 66, "bond7": 77, "vlan8": 88},
+            "bridge": {"br6": ["eth0", "eth1"]},
+            "lagg": {"bond7": ["eth1", "eth0"]},
+            "vlan": {"vlan8": "eth1"},
+            "vm": ["eth1"],
+        },
     ),
+    # Interfaces swapped names
     (
-        [("eth0", "08:00:27:1e:9f:d3"), ("eth1", "08:00:27:1e:9f:d4")],
-        [("eth0", None, 0), ("eth1", "08:00:27:1e:9f:d4", 1)],
-        [("eth0", "08:00:27:1e:9f:d3", 0), ("eth1", "08:00:27:1e:9f:d4", 1)],
+        {
+            "hw": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "interface": {"eth0": 0, "eth1": 1},
+        },
+        {
+            "hw": {"eth1": "08:00:27:1e:9f:d3", "eth0": "08:00:27:1e:9f:d4"},
+            "interface": {"eth1": 0, "eth0": 1},
+        },
     ),
+    # Interface gone
     (
-        [("eth0", "08:00:27:1e:9f:d4")],
-        [("em0", "08:00:27:1e:9f:d4", 0), ("eth0", "08:00:27:1e:9f:d4", 0)],
-        [("em0", None, 0), ("eth0", "08:00:27:1e:9f:d4", 0)],
+        {
+            "hw": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "interface": {"eth0": 0, "eth1": 1, "br0": 66, "bond0": 77, "vlan0": 88},
+            "bridge": {"br0": ["eth0", "eth1"]},
+            "lagg": {"bond0": ["eth0", "eth1"]},
+            "vlan": {"vlan0": "eth1"},
+            "vm": ["eth1"],
+        },
+        {
+            "interface.query": {"eth0": "08:00:27:1e:9f:d3"},
+            "hw": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "interface": {"eth0": 0, "eth1": 1, "br0": 66, "bond0": 77, "vlan0": 88},
+            "bridge": {"br0": ["eth0", "eth1"]},
+            "lagg": {"bond0": ["eth0", "eth1"]},
+            "vlan": {"vlan0": "eth1"},
+            "vm": ["eth1"],
+        },
     ),
+    # New interface
     (
-        [("eth0", "08:00:27:1e:9f:d4")],
-        [("em0", "08:00:27:1e:9f:d5", 0)],
-        [("em0", None, 0)],
+        {
+            "hw": {"eth0": "08:00:27:1e:9f:d3"},
+            "interface": {"eth0": 0, "eth1": 1, "br0": 66, "bond0": 77, "vlan0": 88},
+            "bridge": {"br0": ["eth0", "eth1"]},
+            "lagg": {"bond0": ["eth0", "eth1"]},
+            "vlan": {"vlan0": "eth1"},
+            "vm": ["eth1"],
+        },
+        {
+            "interface.query": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "hw": {"eth0": "08:00:27:1e:9f:d3", "eth1": "08:00:27:1e:9f:d4"},
+            "interface": {"eth0": 0, "eth1": 1, "br0": 66, "bond0": 77, "vlan0": 88},
+            "bridge": {"br0": ["eth0", "eth1"]},
+            "lagg": {"bond0": ["eth0", "eth1"]},
+            "vlan": {"vlan0": "eth1"},
+            "vm": ["eth1"],
+        },
     ),
 ])
 @pytest.mark.asyncio
-async def test__interface_link_address_setup(test):
-    real_interfaces, database, result = test
-
+async def test__interface_link_address_setup(before, after):
     async with datastore_test() as ds:
         ds.middleware["failover.node"] = AsyncMock(return_value="MANUAL")
         ds.middleware["failover.status"] = AsyncMock(return_value="SINGLE")
         ds.middleware["interface.persist_link_addresses"] = InterfaceService(ds.middleware).persist_link_addresses
 
-        query = ds.middleware["datastore.query"]
-
-        async def mock_query(table, *args):
-            if table in ["network.bridge", "network.lagginterfacemembers", "network.vlan", "vm.device"]:
-                return []
-
-            return await query(table, *args)
-
-        ds.middleware["datastore.query"] = mock_query
-
-        for interface, link_address, settings in database:
-            await ds.insert("network.interfaces", {
+        for interface, link_address in before.get("hw", {}).items():
+            await ds.insert("network.interface_link_address", {
                 "interface": interface,
                 "link_address": link_address,
+            })
+
+        interface_id = {}
+        for interface, settings in before.get("interface", {}).items():
+            interface_id[interface] = await ds.insert("network.interfaces", {
+                "interface": interface,
                 "settings": settings,
             }, {"prefix": "int_"})
+
+        for interface, members in before.get("bridge", {}).items():
+            await ds.insert("network.bridge", {
+                "interface": interface_id[interface],
+                "members": members,
+            })
+
+        for interface, members in before.get("lagg", {}).items():
+            lagg_id = await ds.insert("network.lagginterface", {
+                "interface": interface_id[interface],
+            }, {"prefix": "lagg_"})
+
+            for member in members:
+                await ds.insert("network.lagginterfacemembers", {
+                    "interfacegroup": lagg_id,
+                    "physnic": member,
+                }, {"prefix": "lagg_"})
+
+        for vint, pint in before.get("vlan", {}).items():
+            await ds.insert("network.vlan", {
+                "vint": vint,
+                "pint": pint,
+            }, {"prefix": "vlan_"})
+
+        for interface in before.get("vm", []):
+            await ds.insert("vm.device", {
+                "dtype": "NIC",
+                "attributes": {"nic_attach": interface},
+            })
 
         ds.middleware["interface.query"] = AsyncMock(return_value=[
             {
@@ -80,19 +196,77 @@ async def test__interface_link_address_setup(test):
                     "link_address": link_address,
                 }
             }
-            for name, link_address in real_interfaces
+            for name, link_address in (
+                after["interface.query"] if "interface.query" in after else after.get("hw", {})
+            ).items()
         ])
 
         await link_address_setup(ds.middleware)
 
-        def key(v):
-            return v["interface"]
-        assert sorted(await ds.query("network.interfaces", [], {"prefix": "int_"}), key=key) == sorted([
+        assert await ds.query("network.interface_link_address", [], {"prefix": "int_"}) == [
             {
                 "id": ANY,
                 "interface": interface,
                 "link_address": link_address,
+                "link_address_b": None,
+            }
+            for interface, link_address in after.get("hw", {}).items()
+        ]
+
+        assert await ds.query("network.interfaces", [], {"prefix": "int_"}) == [
+            {
+                "id": ANY,
+                "interface": interface,
                 "settings": settings,
             }
-            for interface, link_address, settings in result
-        ], key=key)
+            for interface, settings in after.get("interface", {}).items()
+        ]
+
+        assert await ds.query("network.bridge") == [
+            {
+                "id": ANY,
+                "interface": {"id": ANY, "int_interface": interface, "int_settings": ANY},
+                "members": members,
+            }
+            for interface, members in after.get("bridge", {}).items()
+        ]
+
+        assert await ds.query("network.lagginterface", [], {"prefix": "lagg_"}) == [
+            {
+                "id": ANY,
+                "interface": {"id": ANY, "int_interface": interface, "int_settings": ANY},
+            }
+            for interface, members in after.get("lagg", {}).items()
+        ]
+
+        for lagg in await ds.query("network.lagginterface", [], {"prefix": "lagg_"}):
+            members = after["lagg"].get(lagg["interface"]["int_interface"])
+
+            assert await ds.query(
+                "network.lagginterfacemembers", [["interfacegroup", "=", lagg["id"]]], {"prefix": "lagg_"}
+            ) == [
+                {
+                    "id": ANY,
+                    "interfacegroup": ANY,
+                    "physnic": member,
+                }
+                for member in members
+            ]
+
+        assert await ds.query("network.vlan", [], {"prefix": "vlan_"}) == [
+            {
+                "id": ANY,
+                "vint": vint,
+                "pint": pint,
+            }
+            for vint, pint in after.get("vlan", {}).items()
+        ]
+
+        assert await ds.query("vm.device") == [
+            {
+                "id": ANY,
+                "dtype": "NIC",
+                "attributes": {"nic_attach": interface},
+            }
+            for interface in after.get("vm", [])
+        ]
