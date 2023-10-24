8a79f1737c362551491131b77489c405e309453e
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-24 18:53:11 +0500

    Remove updatenotapplied alert class

--- src/middlewared/middlewared/alert/source/update.py
+++ src/middlewared/middlewared/alert/source/update.py
@@ -1,6 +1,3 @@
-from datetime import timedelta
-import os
-import json
 import logging
 
 try:
@@ -9,10 +6,8 @@ try:
 except ImportError:
     Update = PendingUpdates = None
 
-from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, FilePresenceAlertSource, ThreadedAlertSource
-from middlewared.alert.schedule import IntervalSchedule
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, FilePresenceAlertSource
 
-UPDATE_APPLIED_SENTINEL = "/tmp/.updateapplied"
 
 log = logging.getLogger("update_check_alertmod")
 
@@ -29,29 +24,6 @@ class UpdateNotAppliedAlertClass(AlertClass):
     level = AlertLevel.WARNING
     title = "Update Not Applied"
     text = "%s"
-
-
-class UpdateNotAppliedAlertSource(ThreadedAlertSource):
-    schedule = IntervalSchedule(timedelta(minutes=10))
-
-    def check_sync(self):
-        if os.path.exists(UPDATE_APPLIED_SENTINEL):
-            try:
-                with open(UPDATE_APPLIED_SENTINEL, "rb") as f:
-                    data = json.loads(f.read().decode("utf8"))
-            except Exception:
-                log.error(
-                    "Could not load UPDATE APPLIED SENTINEL located at {0}".format(
-                        UPDATE_APPLIED_SENTINEL
-                    ),
-                    exc_info=True
-                )
-                return
-
-            if is_update_applied:
-                update_applied, msg = is_update_applied(data["update_version"], create_alert=False)
-                if update_applied:
-                    return Alert(UpdateNotAppliedAlertClass, msg)
 
 
 class UpdateFailedAlertClass(AlertClass):
