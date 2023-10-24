f4ca95a98838c3f39444792d3590e94190d5aa52
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-10-24 16:24:01 +0300

    NAS-124752 / 24.04 / Add additional metadata to chart context (#12373)
    * Add additional metadata to chart context
    
    * one moreplace
    
    * switch to boolean flags
    
    * restore comment
    
    * revert change

--- src/middlewared/middlewared/plugins/chart_releases_linux/utils.py
+++ src/middlewared/middlewared/plugins/chart_releases_linux/utils.py
@@ -37,6 +37,7 @@ def get_action_context(release_name):
         'isInstall': False,
         'isUpdate': False,
         'isUpgrade': False,
+        'isStopped': False,
         'storageClassName': get_storage_class_name(release_name),
         'upgradeMetadata': {},
         'hasSMBCSI': True,
