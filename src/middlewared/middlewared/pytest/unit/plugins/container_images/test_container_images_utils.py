4b031d2fd157286716a386ee64f340f153618915
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-10-24 19:27:30 +0500

    Add unit tests to validate normalization logic for getting dockerhub limits

--- src/middlewared/middlewared/pytest/unit/plugins/container_images/test_container_images_utils.py
+++ src/middlewared/middlewared/pytest/unit/plugins/container_images/test_container_images_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from middlewared.plugins.container_runtime_interface.utils import normalize_reference
+from middlewared.plugins.container_runtime_interface.utils import normalize_reference, normalize_docker_limits_header
 
 
 @pytest.mark.parametrize("reference,expected_results", [
@@ -72,3 +72,69 @@ from middlewared.plugins.container_runtime_interface.utils import normalize_refe
 def test_normalize_reference(reference, expected_results):
     actual_results = normalize_reference(reference)
     assert actual_results == expected_results
+
+
+@pytest.mark.parametrize('header,expected_response', [
+    (
+        {
+            'ratelimit-limit': '100;w=21600',
+            'ratelimit-remaining': '100;w=21600'},
+        {
+          'total_pull_limit': 100,
+          'total_time_limit_in_secs': 21600,
+          'remaining_pull_limit': 100,
+          'remaining_time_limit_in_secs': 21600,
+          'error': None
+        }
+    ),
+    (
+        {
+            'ratelimit-limit': '100;w=21600',
+            'ratelimit-remaining': '97;w=21600'
+        },
+        {
+             'total_pull_limit': 100,
+             'total_time_limit_in_secs': 21600,
+             'remaining_pull_limit': 97,
+             'remaining_time_limit_in_secs': 21600,
+             'error': None
+        }
+    ),
+    (
+        {
+            'ratelimit-limit': '103;w=21',
+            'ratelimit-remaining': '68;w=600'
+        },
+        {
+             'total_pull_limit': 103,
+             'total_time_limit_in_secs': 21,
+             'remaining_pull_limit': 68,
+             'remaining_time_limit_in_secs': 600,
+             'error': None
+        }
+    ),
+    (
+        {
+            'ratelimit-limit': '100;w=21600'
+        },
+        {
+            'error': 'Unable to retrieve rate limit information from registry'
+        }
+    ),
+    (
+        {
+            'ratelimit-remaining': '100;w=21600'
+        },
+        {
+            'error': 'Unable to retrieve rate limit information from registry'
+        }
+    ),
+    (
+        {},
+        {
+            'error': 'Unable to retrieve rate limit information from registry'
+        }
+    ),
+])
+def test_normalize_docker_limits_header(header, expected_response):
+    assert normalize_docker_limits_header(header) == expected_response
