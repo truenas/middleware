f6cfe85fabbb15107879e2b5f21ff4ce6e64bdfe
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-11-06 13:07:45 -0500

    fix failover.upgrade_pending and utils.can_update (#12457)

--- src/middlewared/middlewared/plugins/update_/utils.py
+++ src/middlewared/middlewared/plugins/update_/utils.py
@@ -13,9 +13,35 @@ SEP = re.compile(r"[-.]")
 
 def can_update(old_version, new_version):
     for x, y in itertools.zip_longest(SEP.split(old_version), SEP.split(new_version), fillvalue=''):
-        if x < y:
+        if x.startswith('U') and x[1:].isdigit():
+            x = x[1:]
+        if y.startswith('U') and y[1:].isdigit():
+            y = y[1:]
+
+        for special in ['CUSTOM']:
+            if x == special and y != special:
+                return False
+            elif x != special and y == special:
+                return True
+
+        if not x.isdigit() and (y.isdigit() or y == ''):
             return True
-        if 'MASTER' in x and 'INTERNAL' in y:
+        if (x.isdigit() or x == '') and not y.isdigit():
+            return False
+
+        if x == 'MASTER' and y != 'MASTER':
+            return False
+        elif x != 'MASTER' and y == 'MASTER':
+            return True
+
+        if (x == 'INTERNAL') != (y == 'INTERNAL'):
+            return True
+
+        if x.isdigit() and y.isdigit():
+            x = int(x)
+            y = int(y)
+
+        if x < y:
             return True
         if x > y:
             return False
