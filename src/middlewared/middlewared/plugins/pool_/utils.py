643d2c0b15c8ee9055d89568a5d2de5d44400250
M. Rehan <mrehanlm93@gmail.com>
2023-11-11 17:36:12 +0500

    Add BLAKE3 checksum option

--- src/middlewared/middlewared/plugins/pool_/utils.py
+++ src/middlewared/middlewared/plugins/pool_/utils.py
@@ -15,7 +15,7 @@ DATASET_DATABASE_MODEL_NAME = 'storage.encrypteddataset'
 RE_DRAID_DATA_DISKS = re.compile(r':\d*d')
 RE_DRAID_SPARE_DISKS = re.compile(r':\d*s')
 RE_DRAID_NAME = re.compile(r'draid\d:\d+d:\d+c:\d+s-\d+')
-ZFS_CHECKSUM_CHOICES = ['ON', 'OFF', 'FLETCHER2', 'FLETCHER4', 'SHA256', 'SHA512', 'SKEIN', 'EDONR']
+ZFS_CHECKSUM_CHOICES = ['ON', 'OFF', 'FLETCHER2', 'FLETCHER4', 'SHA256', 'SHA512', 'SKEIN', 'EDONR', 'BLAKE3']
 ZFS_COMPRESSION_ALGORITHM_CHOICES = [
     'ON', 'OFF', 'LZ4', 'GZIP', 'GZIP-1', 'GZIP-9', 'ZSTD', 'ZSTD-FAST', 'ZLE', 'LZJB',
 ] + [f'ZSTD-{i}' for i in range(1, 20)] + [
