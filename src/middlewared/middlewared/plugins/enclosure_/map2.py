eda69c78a808d8948d3d44de2bbda8d42d2a5532
Caleb <yocalebo@gmail.com>
2023-11-13 14:56:43 -0500

    simplify map2.py

--- src/middlewared/middlewared/plugins/enclosure_/map2.py
+++ src/middlewared/middlewared/plugins/enclosure_/map2.py
@@ -1,71 +1,52 @@
-from .slot_mappings import get_mapping_info
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def map_enclosures(enclosures):
-    mapped = [{'id': 'mapped_enclosure_0', 'elements': {'Array Device Slot': {}}}]
-    jbods = []
-    for enclosure in enclosures:
-        if not enclosure['controller']:
-            # we're on a platform that has a JBOD attached so we
-            # want to place these in another list so we don't lose
-            # them during the mapping process
-            jbods.append(enclosure)
+def to_ignore(enclosure, model):
+    if not enclosure['controller']:
+        # this is a JBOD and doesn't need to
+        # be "combined" into any other object
+        return True
+
+    all_nvme_flash = ('F60', 'F100', 'F130', 'R30')
+    if model.startswith(all_nvme_flash):
+        # the head-unit is all nvme flash so
+        # these are treated as-is and don't
+        # need to be combined
+        return True
+
+
+def combine_enclosures(enclosures):
+    """Purpose of this function is to combine certain enclosures
+    Array Device Slot elements into 1. For example, the MINIs/R20s
+    have their disk drives spread across multiple enclosures. We
+    need to map them all into 1 unit. Another example is that we
+    have platforms (M50/60, R50B) that have rear nvme drive bays.
+    NVMe doesn't get exposed via a traditional SES device because,
+    well, it's nvme. So we create a "fake" nvme "enclosure" that
+    mimics the drive slot information that a traditional enclosure
+    would do. We take these enclosure devices and simply add them
+    to the head-unit enclosure object.
+
+    NOTE: The array device slots have already been mapped to their
+    human-readable slot numbers. That logic is in the `Enclosure`
+    class in "enclosure_/enclosure_class.py"
+    """
+    head_unit_idx, to_combine = None, dict()
+    for idx, enclosure in enumerate(enclosures):
+        model = enclosure.get('model', '')
+        if to_ignore(enclosure, model):
             continue
+        elif enclosure['elements']['Array Device Slot'].get(1):
+            # the enclosure object whose disk slot has number 1
+            # will always be the head-unit
+            head_unit_idx = idx
+        else:
+            to_combine.update(enclosure['elements'].pop('Array Device Slot', dict()))
 
-        mapped_info = get_mapping_info(enclosure['dmi'], enclosures)
-        if mapped_info is None:
-            # X/M/F series don't need to be mapped
-            return enclosures
-
-        vers_key = 'DEFAULT'
-        if not mapped_info['any_version']:
-            # platforms can have different "versions" which
-            # means that they can be ever so slightly cabled
-            # differently which leads to us having to map the
-            # drives based on how they're cabled
-            # (hence the "version")
-            for key, vers in mapped_info['versions'].items():
-                if enclosure['revision'] == key:
-                    vers_key = vers
-                    break
-
-        disk_slots_mapping = None
-        for key, _dsm in mapped_info['versions'][vers_key].items():
-            # now that we know what product "version" we're on, we need to be
-            # sure and pull the drive mappings based on the enclosures unique
-            # (non-changing) id. The non-changing id that uniquely identifies
-            # the enclosure is different between each platform
-            if (uniq_id := enclosure.get(key)) and (disk_slots_mapping := _dsm.get(uniq_id)):
-                break
-
-        if disk_slots_mapping is None:
-            raise LookupError(f'Failed to find unique ID for enclosure {enclosure["name"]!r}')
-
-        for orig_slot, orig_info in enclosure['elements']['Array Device Slot'].items():
-            mapped_slot = disk_slots_mapping[orig_slot]['mapped_slot']
-            mapped[0]['elements']['Array Device Slot'].update({
-                mapped_slot: {
-                    'descriptor': f'Disk #{mapped_slot}',
-                    'status': orig_info['status'],
-                    'value': orig_info['value'],
-                    'value_raw': orig_info['value_raw'],
-                    'dev': orig_info['dev'],
-                    'original': {
-                        # the main purpose of the `original` key is so that
-                        # we have quick access when a user requests to identify
-                        # or fault a drive slot led. Since platforms can have
-                        # 100's (or 1k+) disk slots, the only components we
-                        # care about when lighting up a disk is to get the
-                        # 1. enclosure scsi generice device (i.e. /dev/sg0)
-                        # 2. the original slot of the disk (if it was mapped)
-                        'enclosure_id': enclosure['id'],
-                        'enclosure_sg': enclosure['sg'],
-                        'enclosure_bsg': enclosure['bsg'],
-                        'descriptor': orig_info['descriptor'],
-                        'slot': orig_slot
-                    }
-                }
-            })
-
-    # we've mapped the head-unit but we don't want to lose the JBOD information
-    return mapped + jbods
+    if head_unit_idx is not None:
+        enclosures[head_unit_idx]['elements']['Array Device Slot'].update(to_combine)
+        enclosures[head_unit_idx]['elements']['Array Device Slot'] = {
+            k: v for k, v in sorted(enclosures[head_unit_idx]['elements']['Array Device Slot'].items())
+        }
