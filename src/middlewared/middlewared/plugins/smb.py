e52d6ae75d0776b80e26e9c8562af2c64530666a
mgrimesix <126630154+mgrimesix@users.noreply.github.com>
2023-11-14 09:37:06 -0800

    NAS-125212 / 24.04 / fix smb.update stall waiting for sentinel file (#12490)
    The /var/run/middleware directory is tmpfs and starts with zero files on every boot.
    We do not need to actively manage the removal of the ix-netif-sentinel.
    This change eliminates the stall on calling smb.update.

--- src/middlewared/middlewared/plugins/smb.py
+++ src/middlewared/middlewared/plugins/smb.py
@@ -504,11 +504,8 @@ class SMBService(TDBWrapConfigService):
         """
         found_sentinel = False
         while timeout >= 0 and not found_sentinel:
-            try:
-                os.remove(NETIF_COMPLETE_SENTINEL)
+            if await self.middleware.run_in_thread(os.path.exists, NETIF_COMPLETE_SENTINEL):
                 found_sentinel = True
-            except FileNotFoundError:
-                pass
 
             timeout -= 1
             if timeout <= 0:
