2b58e37aa9ddc038fb9137e646581620d65cde80
Caleb <yocalebo@gmail.com>
2023-11-15 13:00:50 -0500

    juggling too many chainsaws

--- tests/api2/test_007_early_settings.py
+++ tests/api2/test_007_early_settings.py
@@ -1,7 +1,9 @@
 from middlewared.test.integration.utils import call
+from middlewared.plugins.sysctl.sysctl_info import DEFAULT_ARC_MAX_FILE
 
 
 def test_sysctl_arc_max_is_set():
-    """Middleware should have set this value early in boot phase
-    and this should return a number"""
-    assert call('sysctl.get_default_arc_max')
+    """Middleware should have created this file and written a number
+    to it early in the boot process. That's why we check it here in
+    this test so early"""
+    assert call('filesystem.stat', DEFAULT_ARC_MAX_FILE)['size']
