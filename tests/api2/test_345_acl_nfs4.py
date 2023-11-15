286f17e43519b90c7d54e0bcd9d49d312dbdd465
Andrew Walker <awalker@ixsystems.com>
2023-11-15 11:59:04 -0800

    Fix

--- tests/api2/test_345_acl_nfs4.py
+++ tests/api2/test_345_acl_nfs4.py
@@ -285,25 +285,13 @@ We first create a child dataset to verify that ACLs do not change unless
 
 def test_12_prepare_recursive_tests(request):
     depends(request, ["HAS_NFS4_ACLS"], scope="session")
-    result = POST(
-        '/pool/dataset/', {
-            'name': ACLTEST_SUBDATASET,
-            'acltype': 'NFSV4',
-        }
-    )
-    assert result.status_code == 200, result.text
+    call('pool.dataset.create', {'name': ACLTEST_SUBDATASET, 'acltype': 'NFSV4'})
 
-    cmd = f'mkdir -p /mnt/{ACLTEST_DATASET}/dir1/dir2'
-    results = SSH_TEST(cmd, user, password, ip)
-    assert results['result'] is True, results['output']
-
-    cmd = f'touch /mnt/{ACLTEST_DATASET}/dir1/testfile'
-    results = SSH_TEST(cmd, user, password, ip)
-    assert results['result'] is True, results['output']
-
-    cmd = f'touch /mnt/{ACLTEST_DATASET}/dir1/dir2/testfile'
-    results = SSH_TEST(cmd, user, password, ip)
-    assert results['result'] is True, results['output']
+    ssh(';'.join([
+        f'mkdir -p /mnt/{ACLTEST_DATASET}/dir1/dir2',
+        f'touch /mnt/{ACLTEST_DATASET}/dir1/testfile',
+        f'touch /mnt/{ACLTEST_DATASET}/dir1/dir2/testfile'
+    ]))
 
 
 def test_13_recursive_no_traverse(request):
@@ -319,62 +307,41 @@ def test_13_recursive_no_traverse(request):
     expected_flags_1['INHERITED'] = True
 
     # get acl of child dataset. This should not  change in this test
-    results = POST('/filesystem/getacl/',
-                   {'path': f'/mnt/{ACLTEST_SUBDATASET}', 'simplified': True})
-    assert results.status_code == 200, results.text
-    init_acl = results.json()['acl'][0]['perms']
+    acl_result = call('filesystem.getacl', f'/mnt/{ACLTEST_SUBDATASET}', True)
+    init_acl = acl_result['acl'][0]['perms']
 
-    result = POST(
-        f'/pool/dataset/id/{dataset_url}/permission/', {
-            'acl': default_acl,
-            'group': group,
-            'user': 'nobody',
-            'options': {'recursive': True}
-        }
-    )
-    assert result.status_code == 200, result.text
-    JOB_ID = result.json()
-    job_status = wait_on_job(JOB_ID, 180)
-    assert job_status['state'] == 'SUCCESS', str(job_status['results'])
+    call('filesystem.setacl', {
+        'path': TEST_INFO['dataset_path'],
+        'dacl': default_acl,
+        'group': group,
+        'user': 'nobody',
+        'options': {'recursive': True}
+    }, job=True)
 
     # Verify that it hasn't changed
-    results = POST('/filesystem/getacl/',
-                   {'path': f'/mnt/{ACLTEST_SUBDATASET}', 'simplified': True})
-    assert results.status_code == 200, results.text
-    fin_acl = results.json()['acl'][0]['perms']
-    assert init_acl == fin_acl, results.text
+    acl_result = call('filesystem.getacl', f'/mnt/{ACLTEST_SUBDATASET}', True)
+    fin_acl = acl_result['acl'][0]['perms']
+    assert init_acl == fin_acl, str(acl_result)
 
     # check on dir 1. Entry 1 should have INHERIT flag added, and
     # INHERIT_ONLY should be set to False at this depth.
-    results = POST(
-        '/filesystem/getacl/', {
-            'path': f'/mnt/{ACLTEST_DATASET}/dir1',
-            'simplified': False
-        }
-    )
-    assert results.status_code == 200, results.text
-    theacl = results.json()['acl']
+    acl_result = call('filesystem.getacl', f'/mnt/{ACLTEST_DATASET}/dir1', False)
+    theacl = acl_result['acl']
     assert theacl[0]['flags'] == expected_flags_0, results.text
     assert theacl[1]['flags'] == expected_flags_1, results.text
 
     # Verify that user was changed on subdirectory
-    assert results.json()['uid'] == 65534, results.text
+    assert acl_result['uid'] == 65534, results.text
 
     # check on dir 2 - the no propogate inherit flag should have taken
     # effect and ACL length should be 1
-    results = POST(
-        '/filesystem/getacl/', {
-            'path': f'/mnt/{ACLTEST_DATASET}/dir1/dir2',
-            'simplified': False
-        }
-    )
-    assert results.status_code == 200, results.text
-    theacl = results.json()['acl']
+    acl_result = call('filesystem.getacl', f'/mnt/{ACLTEST_DATASET}/dir1/dir2', False)
+    theacl = acl_result['acl']
     assert theacl[0]['flags'] == expected_flags_0, results.text
     assert len(theacl) == 1, results.text
 
     # Verify that user was changed two deep
-    assert results.json()['uid'] == 65534, results.text
+    assert acl_result['uid'] == 65534, results.text
 
 
 def test_14_recursive_with_traverse(request):
