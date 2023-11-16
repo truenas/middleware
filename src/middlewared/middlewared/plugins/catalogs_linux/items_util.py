7f9498540a8843b97b0b2ba168a0f6f5aabfb4ec
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-16 11:20:56 +0500

    Modify scale version check implementation to account for min/max scale range in app versions

--- src/middlewared/middlewared/plugins/catalogs_linux/items_util.py
+++ src/middlewared/middlewared/plugins/catalogs_linux/items_util.py
@@ -5,10 +5,9 @@ import os
 import typing
 
 from catalog_validation.items.items_util import (
-    get_item_version_details as get_catalog_item_version_details,
-    normalise_questions,
+    get_item_version_details as get_catalog_item_version_details, normalise_questions,
 )
-from catalog_validation.utils import CACHED_VERSION_FILE_NAME, VERSION_VALIDATION_SCHEMA
+from catalog_validation.utils import CACHED_VERSION_FILE_NAME, RE_VERSION_PATTERN, VERSION_VALIDATION_SCHEMA
 
 from middlewared.plugins.chart_releases_linux.schema import construct_schema
 from middlewared.plugins.update_.utils import can_update
@@ -21,31 +20,68 @@ def get_item_default_values(version_details: dict) -> dict:
 
 
 def minimum_scale_version_check_update(version_details: dict) -> dict:
-    version_details['supported'] = minimum_scale_version_check_update_impl(version_details)[0]
+    version_details['supported'] = min_max_scale_version_check_update_impl(version_details)[0]
     return version_details
 
 
-def minimum_scale_version_check_update_impl(
+def custom_scale_version_checks(
+    min_scale_version: str, max_scale_version: str, system_scale_version: str
+) -> typing.Tuple[bool, bool, str]:
+    if not (normalized_system_version := RE_VERSION_PATTERN.findall(system_scale_version)):
+        return False, False, 'Unable to determine system scale version'
+
+    normalized_system_version = normalized_system_version[0]
+
+    if min_scale_version and min_scale_version != normalized_system_version and not can_update(
+        min_scale_version, normalized_system_version
+    ):
+        return False, False, 'Your system version is less then specified minimum scale version for the app version'
+
+    if max_scale_version and max_scale_version != normalized_system_version and not can_update(
+        normalized_system_version, max_scale_version
+    ):
+        return False, False, 'Your system version is greater then specified maximum scale version for the app version'
+
+    return True, False, ''
+
+
+def min_max_scale_version_check_update_impl(
     version_details: dict, check_supported_key: bool = True
-) -> typing.Tuple[bool, bool]:
+) -> typing.Tuple[bool, bool, str]:
     # `check_supported_key` is used because when catalog validation returns the data it only checks the
     # missing features and based on that makes the decision. So if something is not already supported
     # we do not want to validate minimum scale version in that case. However, when we want to report to
     # the user as to why exactly the app version is not supported, we need to be able to make that distinction
-    if version_details.get('healthy', True) and version_details['chart_metadata'].get('minimum_scale_version') and (
-        not check_supported_key or version_details['supported']
+    system_scale_version = sw_info()['version']
+    min_scale_version = version_details.get('chart_metadata', {}).get('annotations', {}).get('min_scale_version')
+    max_scale_version = version_details.get('chart_metadata', {}).get('annotations', {}).get('max_scale_version')
+    if (
+        version_details.get('healthy', True) and (not check_supported_key or version_details['supported'])
+        and (min_scale_version or max_scale_version)
     ):
         try:
-            if sw_info()['version'] != version_details['chart_metadata']['minimum_scale_version'] and not can_update(
-                version_details['chart_metadata']['minimum_scale_version'], sw_info()['version']
-            ):
-                return False, False
+            if any(k in system_scale_version for k in ('MASTER', 'INTERNAL', 'CUSTOM')):
+                return custom_scale_version_checks(min_scale_version, max_scale_version, system_scale_version)
+            else:
+                if (
+                    min_scale_version and min_scale_version != system_scale_version and
+                    not can_update(min_scale_version, system_scale_version)
+                ):
+                    return False, False, ('Your system version is less then specified minimum scale '
+                                          'version for the app version. ')
+
+                if (
+                    max_scale_version and system_scale_version != max_scale_version and
+                    not can_update(system_scale_version, max_scale_version)
+                ):
+                    return False, False, ('Your system version is greater then specified maximum '
+                                          'scale version for the app version. ')
         except Exception:
             # In case invalid version string is specified we don't want a traceback here
             # let's just explicitly not support the app version in question
-            return False, True
+            return False, True, 'Unable to complete scale version compatibility checks'
 
-    return True, False
+    return True, False, ''
 
 
 def get_item_details(item_location: str, item_data: dict, questions_context: dict) -> dict:
