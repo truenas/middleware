92756b3cdfb2ede56051d46f9ab36298e82fd30e
Caleb <yocalebo@gmail.com>
2023-11-20 09:27:20 -0500

    fix building developer documentation

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
-chmod +x /usr/bin/apt* /usr/bin/dpkg
+/usr/bin/python3 /usr/local/libexec/disable-rootfs-protection
 apt update
 apt install -y python3-pip
 pip3 install --break-system-packages sphinx-rtd-theme Sphinx-Substitution-Extensions==2020.9.30.0
