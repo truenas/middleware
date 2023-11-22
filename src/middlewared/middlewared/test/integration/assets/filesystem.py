cbbdfb717a6f01f4e32da3f6dbb198de23a1583f
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2023-11-22 07:45:01 -0800

    Fix ORing in options to dict (#12563)

--- src/middlewared/middlewared/test/integration/assets/filesystem.py
+++ src/middlewared/middlewared/test/integration/assets/filesystem.py
@@ -5,7 +5,7 @@ from middlewared.test.integration.utils import call, ssh
 
 @contextlib.contextmanager
 def directory(path, options=None):
-    call('filesystem.mkdir', {'path': path} | options or {})
+    call('filesystem.mkdir', {'path': path} | (options or {}))
 
     try:
         yield path
