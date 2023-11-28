18014108658b5b0aa20d4f896a2b5ee09fee885f
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-29 01:42:55 +0500

    Remove create/update methods for storage class service

--- src/middlewared/middlewared/plugins/kubernetes_linux/storage_classes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/storage_classes.py
@@ -15,33 +15,8 @@ class KubernetesStorageClassService(CRUDService):
     async def query(self, filters, options):
         return filter_list((await StorageClass.query())['items'], filters, options)
 
-    async def do_create(self, data):
-        try:
-            await StorageClass.create(data)
-        except ApiException as e:
-            raise CallError(f'Unable to create storage class: {e}')
-
-    async def do_update(self, name, data):
-        try:
-            await StorageClass.update(name, data)
-        except ApiException as e:
-            raise CallError(f'Unable to update storage class: {e}')
-
     async def do_delete(self, name):
         try:
             await StorageClass.delete(name)
         except ApiException as e:
             raise CallError(f'Failed to delete storage class: {e}')
-
-    async def retrieve_storage_class_manifest(self):
-        return {
-            'apiVersion': 'storage.k8s.io/v1',
-            'kind': 'StorageClass',
-            'metadata': {
-                'name': None,
-            },
-            'parameters': {'fstype': 'zfs', 'poolname': None, 'shared': 'yes'},
-            'provisioner': 'zfs.csi.openebs.io',
-            'allowVolumeExpansion': True,
-            'reclaimPolicy': 'Retain',
-        }
