60d6a00c50a6e33e0a1b7d8625e42b88f0468280
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-29 01:54:30 +0500

    Remove k8s.pv service

--- src/middlewared/middlewared/plugins/kubernetes_linux/persistent_volumes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/persistent_volumes.py
@@ -1,4 +1,3 @@
-from middlewared.schema import accepts, Dict, List, Str
 from middlewared.service import CRUDService, filterable
 from middlewared.utils import filter_list
 
@@ -15,51 +14,6 @@ class KubernetesPersistentVolumesService(CRUDService):
     @filterable
     async def query(self, filters, options):
         return filter_list((await PersistentVolume.query())['items'], filters, options)
-
-    @accepts(
-        Dict(
-            'pv_create',
-            Dict(
-                'metadata',
-                Str('name', required=True),
-                Dict(
-                    'annotations',
-                    Str('pv.kubernetes.io/provisioned-by', default='zfs.csi.openebs.io'),
-                ),
-                additional_attrs=True,
-            ),
-            Dict(
-                'spec',
-                List('accessModes', enum=['ReadWriteOnce'], default=['ReadWriteOnce']),
-                Dict(
-                    'capacity',
-                    Str('storage', required=True),
-                ),
-                Dict(
-                    'claimRef',
-                    Str('kind', default='PersistentVolumeClaim'),
-                    Str('name', required=True),
-                    Str('namespace', required=True),
-                ),
-                Dict(
-                    'csi',
-                    Str('driver', default='zfs.csi.openebs.io'),
-                    Str('fsType', default='zfs'),
-                    Dict(
-                        'volumeAttributes',
-                        Str('openebs.io/poolname', required=True),
-                        additional_attrs=True
-                    ),
-                    Str('volumeHandle', required=True),
-                ),
-                Str('persistentVolumeReclaimPolicy', default='Retain', enum=['Retain', 'Recycle', 'Delete']),
-                Str('storageClassName', required=True),
-                Str('volumeMode', default='Filesystem'),
-            ),
-        )
-    )
-    async def do_create(self, data):
-        await PersistentVolume.create(data)
 
     async def do_delete(self, pv_name):
         await PersistentVolume.delete(pv_name)
