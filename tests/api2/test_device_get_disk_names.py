7015e8add5ec9e9f27cd1bfe3197ab5d957cc551
Andrew Walker <awalker@ixsystems.com>
2023-11-28 09:27:47 -0800

    Remove tests

--- tests/api2/test_device_get_disk_names.py
+++
@@ -1,5 +0,0 @@
-from middlewared.test.integration.utils import call
-
-
-def test_device_get_disk_names():
-    assert set(list(call('device.get_disks', False, True))) == set(call('device.get_disk_names'))
