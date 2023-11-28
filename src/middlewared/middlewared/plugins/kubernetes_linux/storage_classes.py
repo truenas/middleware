b00067c85f27f0692339f707abcb5af9bf1bff97
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-11-29 01:28:44 +0500

    Remove logic responsible for creating default storage class

--- src/middlewared/middlewared/plugins/kubernetes_linux/storage_classes.py
+++ src/middlewared/middlewared/plugins/kubernetes_linux/storage_classes.py
@@ -1,12 +1,7 @@
-import os
-
 from middlewared.service import CallError, CRUDService, filterable
 from middlewared.utils import filter_list
 
 from .k8s import ApiException, StorageClass
-
-
-DEFAULT_STORAGE_CLASS = 'openebs-zfspv-default'
 
 
 class KubernetesStorageClassService(CRUDService):
@@ -37,34 +32,6 @@ class KubernetesStorageClassService(CRUDService):
             await StorageClass.delete(name)
         except ApiException as e:
             raise CallError(f'Failed to delete storage class: {e}')
-
-    async def setup_default_storage_class(self):
-        try:
-            await self.setup_default_storage_class_internal()
-        except Exception as e:
-            # Let's not make this fatal as workloads managed by us will still be functional
-            self.logger.error('Failed to setup default storage class: %s', e)
-
-    async def setup_default_storage_class_internal(self):
-        storage_ds = os.path.join((await self.middleware.call('kubernetes.config'))['dataset'], 'default_volumes')
-        config = await self.retrieve_storage_class_manifest()
-        config['metadata'].update({
-            'name': DEFAULT_STORAGE_CLASS,
-            'annotations': {'storageclass.kubernetes.io/is-default-class': 'true'},
-        })
-        config['parameters']['poolname'] = storage_ds
-        # Default storage class is going to have delete retain policy
-        # We do not want user deployed charts to have leftover hundreds of
-        # datasets potentially with time after their relevant charts have been deleted
-        config['reclaimPolicy'] = 'Delete'
-
-        if await self.query([
-            ['metadata.annotations.storageclass\\.kubernetes\\.io/is-default-class', '=', 'true'],
-            ['metadata.name', '=', DEFAULT_STORAGE_CLASS],
-        ]):
-            await self.middleware.call('k8s.storage_class.delete', DEFAULT_STORAGE_CLASS)
-
-        await self.middleware.call('k8s.storage_class.create', config)
 
     async def retrieve_storage_class_manifest(self):
         return {
