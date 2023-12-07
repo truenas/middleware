8cdc7de1fef6897d40615a6cf5d3b56841ccdab1
themylogin <themylogin@gmail.com>
2023-12-07 11:27:18 +0100

    Forbid using `0/10` is crontab as our cron implementation does not support it

--- src/middlewared/middlewared/utils/cron.py
+++ src/middlewared/middlewared/utils/cron.py
@@ -1,4 +1,6 @@
 # -*- coding=utf-8 -*-
+import re
+
 from croniter import croniter
 
 CRON_FIELDS = ["minute", "hour", "dom", "month", "dow"]
@@ -7,6 +9,10 @@ CRON_FIELDS = ["minute", "hour", "dom", "month", "dow"]
 def croniter_for_schedule(schedule, *args, **kwargs):
     cron_expression = ''
     for field in CRON_FIELDS:
-        cron_expression += schedule.get(field) + ' ' if schedule.get(field) else '* '
+        value = schedule.get(field) or '*'
+        if '/' in value and not re.match(r'^(\*|[0-9]+-[0-9]+)/([0-9]+)$', value):
+            raise ValueError("Only range or `*` are allowed before `/`")
+
+        cron_expression += f'{value} '
 
     return croniter(cron_expression, *args, **kwargs)
