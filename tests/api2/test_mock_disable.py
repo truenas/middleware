2e4a1786bbccdb0f39b30e4a717e9a355b915f25
Andrew Walker <awalker@ixsystems.com>
2023-12-08 06:20:17 -0800

    Disable mocking support in stable releases
    Development and testing tools should be disabled for published
    releases.

---
+++ tests/api2/test_mock_disable.py
@@ -0,0 +1,14 @@
+import errno
+import pytest
+
+from middlewared.service_exception import CallError
+from middlewared.test.integration.utils import call, mock
+
+
+def test_disable_mock_on_stable_release():
+    with mock("system.is_stable", return_value=True):
+        with pytest.raises(CallError) as ve:
+            with mock("vmware.connect", return_value=None):
+                pass
+
+        assert ve.value.errno == errno.EPERM
