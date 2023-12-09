ca7ce01a1bdcf3814e28ff35c2823946314dae4f
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-12-09 18:24:38 +0500

    Add integration test to make sure group ownership is good and netdata is retrieving stats

--- tests/api2/test_023_kubernetes.py
+++ tests/api2/test_023_kubernetes.py
@@ -1,11 +1,10 @@
-import os
 import pytest
-import sys
+import time
+
 from pytest_dependency import depends
-apifolder = os.getcwd()
-sys.path.append(apifolder)
 from functions import GET, PUT, wait_on_job
 from auto_config import ha, pool_name, interface, ip
+from middlewared.test.integration.utils import call
 
 
 # Read all the test below only on non-HA
@@ -53,3 +52,29 @@ if not ha:
         results = GET('/kubernetes/events/')
         assert results.status_code == 200, results.text
         assert isinstance(results.json(), list), results.text
+
+    def test_06_kubernetes_config_acl(request):
+        depends(request, ['setup_kubernetes'])
+        acl_mode = call('filesystem.stat', '/etc/rancher/k3s/k3s.yaml')
+        netdata_group = call('group.query', [['group', '=', 'netdata']], {'get': True})
+        assert acl_mode['gid'] == netdata_group['gid']
+        assert (acl_mode['mode'] & 0o640) == 0o640
+
+    def test_07_kubernetes_pods_stats(request):
+        depends(request, ['setup_kubernetes'])
+        while True:
+            time.sleep(5)
+            kube_system_pods = call(
+                'k8s.pod.query', [
+                    ['metadata.namespace', '=', 'kube-system']
+                ], {'select': ['metadata.name', 'status.phase']}
+            )
+            if any(pod['status']['phase'] == 'Running' for pod in kube_system_pods):
+                break
+
+        stats = call('chart.release.stats_internal', kube_system_pods)
+        assert any(
+            d[k] > 0 for d, k in (
+                (stats, 'memory'), (stats, 'cpu'), (stats['network'], 'incoming'), (stats['network'], 'outgoing')
+            )
+        ), stats
