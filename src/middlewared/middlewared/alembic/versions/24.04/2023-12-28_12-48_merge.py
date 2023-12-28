6ec30e886b78e9bb3f7a4582e1083798af9c6c28
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-12-28 09:49:15 -0300

    NAS-125932 / 24.04 / Fix non-physical interface link address getting into database when it is updated (by themylogin) (#12814)
    * Fix non-physical interface link address getting into database when it is updated
    
    (cherry picked from commit 8fbbe5bca320aa3739f816573727e521c8785637)
    
    * Merge migration
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/24.04/2023-12-28_12-48_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: a598a2c81461
+Revises: e2e0b53cb627, 7a5ebab17483
+Create Date: 2023-12-28 12:48:12.099465+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a598a2c81461'
+down_revision = ('e2e0b53cb627', '7a5ebab17483')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
