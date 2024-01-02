5e809c84f438def731e9ea8e7f3265cf02d6d760
themylogin <themylogin@gmail.com>
2024-01-02 16:16:23 +0100

    Merme migration
    (cherry picked from commit eb03c03cb4c6b624b9e3beb01da3c695e0a4e801)

---
+++ src/middlewared/middlewared/alembic/versions/23.10/2024-01-02_15-15_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: bc770461df0d
+Revises: 61095406c3a0, 7a5ebab17483
+Create Date: 2024-01-02 15:15:55.324871+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc770461df0d'
+down_revision = ('61095406c3a0', '7a5ebab17483')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
