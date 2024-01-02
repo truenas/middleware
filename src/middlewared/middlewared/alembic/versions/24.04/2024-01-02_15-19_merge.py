7b18b0fd79177e53aa657b7e1cba839bf11298d5
themylogin <themylogin@gmail.com>
2024-01-02 16:20:41 +0100

    Merge migration

---
+++ src/middlewared/middlewared/alembic/versions/24.04/2024-01-02_15-19_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: a33672445bed
+Revises: 69789458866a, bc770461df0d
+Create Date: 2024-01-02 15:19:53.113060+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a33672445bed'
+down_revision = ('69789458866a', 'bc770461df0d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
