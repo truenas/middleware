00ce61df205cb7c2ef8f7c7841e68921df9fe8ca
Brian Meagher <brian.meagher@ixsystems.com>
2024-01-29 12:58:25 -0800

    Add iscsi.alua.* API commands
    Improve ALUA stability when many targets are present:
    - Replace recent systemd_unit_timeout=30 with systemd_async_start=True
    - Add iscsi.alua.* APIs including jobs standby_after_start and active_elected.
    - Update call sites to call these jobs and APIs.
    - Update scst.conf.mako
    
    In an effort to reduce startup time, ALUA targets will now NOT start with
    cluster_mode switched on.  Instead, when iSCSI starts on the BACKUP node it
    will initiate a job that resets and existing dlm state (on both nodes), and
    then incrementally enables cluster_mode on all extents (on both nodes).
    
    (cherry picked from commit b3663f912f82b6b24023d55b8113687ea1a12d6f)

--- src/middlewared/middlewared/plugins/service_/services/iscsitarget.py
+++ src/middlewared/middlewared/plugins/service_/services/iscsitarget.py
@@ -6,16 +6,21 @@ from .base import SimpleService
 class ISCSITargetService(SimpleService):
     name = "iscsitarget"
     reloadable = True
-    systemd_unit_timeout = 30
+    systemd_async_start = True
 
     etc = ["scst", "scst_targets"]
 
     systemd_unit = "scst"
 
+    async def before_start(self):
+        await self.middleware.call("iscsi.alua.before_start")
+
     async def after_start(self):
         await self.middleware.call("iscsi.host.injection.start")
+        await self.middleware.call("iscsi.alua.after_start")
 
     async def before_stop(self):
+        await self.middleware.call("iscsi.alua.before_stop")
         await self.middleware.call("iscsi.host.injection.stop")
 
     async def reload(self):
