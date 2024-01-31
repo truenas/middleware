d02076640f1c285e3cfa1b474c03e5da76b11a9d
M. Rehan <mrehanlm93@gmail.com>
2024-01-31 20:51:56 +0500

    Add merge migration

---
+++ src/middlewared/middlewared/alembic/versions/24.10/2024-01-31_10-05_merge.py
@@ -0,0 +1,24 @@
+"""merge migration
+
+Revision ID: 968d515e63e7
+Revises: 6a7c2281f48e
+Create Date: 2024-01-31 11:30:59.733983+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '968d515e63e7'
+down_revision = '6a7c2281f48e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
