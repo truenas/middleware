system-detox/truenas-middleware

Add pytest markers for more intelligent runs
This commit adds basic framework for setting custom markers
in our API tests. They can either be set globally for the
module via
`pytestmark = pytest.mark.smb`

and multiple for module
`pytestmark = [pytest.mark.fs, pytest.mark.slow]`

or via test-specific decorator
`@pytest.mark.smb`

Long-running tests (more than a couple of minutes) should be
marked as `slow` so that they can be skipped in incremental
builds.

tests/api2/test_230_idmap.py
@@ -15,6 +15,7 @@ try:
         LDAPBINDPASSWORD,
         LDAPHOSTNAME,
     )
+    pytestmark = pytest.mark.cert
 except ImportError:
     Reason = 'LDAP* variable are not setup in config.py'
     # comment pytestmark for development testing with --dev-test

truenas/middleware

tests/api2/test_230_idmap.py
@@ -15,6 +15,7 @@ try:
         LDAPBINDPASSWORD,
         LDAPHOSTNAME,
     )
+    pytestmark = pytest.mark.certs
 except ImportError:
     Reason = 'LDAP* variable are not setup in config.py'
     # comment pytestmark for development testing with --dev-test