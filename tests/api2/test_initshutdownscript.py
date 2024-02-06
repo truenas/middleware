33be7f27741b8727588c75ee4f53a052d07ab11a
themylogin <themylogin@gmail.com>
2024-02-06 14:00:01 +0100

    initshutdownscript tests (#13095)

--- tests/api2/test_initshutdownscript.py
+++ tests/api2/test_initshutdownscript.py
@@ -1,11 +1,12 @@
 import base64
+import contextlib
 import errno
 import stat
-import uuid
+import time
 
 import pytest
 
-from middlewared.test.integration.utils.client import client
+from middlewared.test.integration.utils import client, ssh
 from middlewared.service_exception import ValidationErrors, ValidationError
 
 TEST_SCRIPT_FILE = '/root/.TEST_SCRIPT_FILE'
@@ -18,43 +19,53 @@ def ws_client():
         yield c
 
 
-def test_initshutudown_script(ws_client):
-    # create the test script file first
+@contextlib.contextmanager
+def initshutudown_script(ws_client, contents, extra=None):
+    extra = extra or {}
+
     ws_client.call(
         'filesystem.file_receive',
         TEST_SCRIPT_FILE,
-        base64.b64encode(b'echo "testing"').decode(),
+        base64.b64encode(contents.encode('utf-8')).decode(),
         {'mode': _775},
     )
-    _id = ws_client.call(
+    script = ws_client.call(
         'initshutdownscript.create',
         {
             'type': 'SCRIPT',
             'script': TEST_SCRIPT_FILE,
             'when': 'PREINIT',
+            **extra,
         }
-    )['id']
-    filters = [['id', '=', _id]]
-    opts = {'get': True}
+    )
+    try:
+        yield script
+    finally:
+        ws_client.call('initshutdownscript.delete', script['id'])
 
-    # verify
-    assert ws_client.call('initshutdownscript.query', filters, opts)['script'] == TEST_SCRIPT_FILE
 
-    # add a comment
-    ws_client.call('initshutdownscript.update', _id, {'comment': 'test_comment'})
-    assert ws_client.call('initshutdownscript.query', filters, opts)['comment'] == 'test_comment'
+def test_initshutudown_script(ws_client):
+    with initshutudown_script(ws_client, 'echo "testing"') as script:
+        _id = script['id']
+        filters = [['id', '=', _id]]
+        opts = {'get': True}
 
-    # disable it
-    ws_client.call('initshutdownscript.update', _id, {'enabled': False})
-    assert ws_client.call('initshutdownscript.query', filters, opts)['enabled'] is False
+        # verify
+        assert ws_client.call('initshutdownscript.query', filters, opts)['script'] == TEST_SCRIPT_FILE
 
-    # delete it
-    ws_client.call('initshutdownscript.delete', _id)
+        # add a comment
+        ws_client.call('initshutdownscript.update', _id, {'comment': 'test_comment'})
+        assert ws_client.call('initshutdownscript.query', filters, opts)['comment'] == 'test_comment'
+
+        # disable it
+        ws_client.call('initshutdownscript.update', _id, {'enabled': False})
+        assert ws_client.call('initshutdownscript.query', filters, opts)['enabled'] is False
+
     assert not ws_client.call('initshutdownscript.query', filters)
 
 
 def test_initshutdown_script_bad(ws_client):
-    bad_script = f'/root/{uuid.uuid4()}'
+    bad_script = f'/root/nonexistent-script'
     with pytest.raises(ValidationErrors) as e:
         ws_client.call(
             'initshutdownscript.create',
@@ -72,3 +83,36 @@ def test_initshutdown_script_bad(ws_client):
             errno.ENOENT
         )
     ]
+
+
+def test_initshutdownscript_success(ws_client):
+    ssh("rm /tmp/flag", check=False)
+
+    with initshutudown_script(ws_client, 'echo ok > /tmp/flag'):
+        ws_client.call('initshutdownscript.execute_init_tasks', 'PREINIT', job=True)
+
+    assert ssh("cat /tmp/flag") == "ok\n"
+
+
+def test_initshutdownscript_timeout(ws_client):
+    ssh("rm /tmp/flag", check=False)
+
+    with initshutudown_script(ws_client, 'sleep 10', {"timeout": 2}):
+        start = time.monotonic()
+        ws_client.call('initshutdownscript.execute_init_tasks', 'PREINIT', job=True)
+
+        assert time.monotonic() - start < 5
+
+    assert f"Timed out running SCRIPT: {TEST_SCRIPT_FILE!r}" in ssh("cat /var/log/middlewared.log")
+
+
+def test_initshutdownscript_failure(ws_client):
+    ssh("rm /tmp/flag", check=False)
+
+    with initshutudown_script(ws_client, 'echo everything went wrong > /dev/stderr; exit 1'):
+        ws_client.call('initshutdownscript.execute_init_tasks', 'PREINIT', job=True)
+
+    assert (
+        f"Failed to execute 'exec {TEST_SCRIPT_FILE}' with error 'everything went wrong\\n'" in
+        ssh("cat /var/log/middlewared.log")
+    )
