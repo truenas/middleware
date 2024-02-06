c388ba098b739ccce031686db80f25f1fcaf7f57
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-02-06 19:20:27 +0500

    Add a comment outlining socket check behaviour
    (cherry picked from commit 28e93b5a1d4e556148030f209d53949a01e5379b)

--- src/middlewared/middlewared/plugins/container_runtime_interface/containerd_utils.py
+++ src/middlewared/middlewared/plugins/container_runtime_interface/containerd_utils.py
@@ -4,6 +4,8 @@ from .client import ContainerdClient
 
 
 def check_containerd_connection() -> bool:
+    # Just to clarify that this does not make any internet requests but rather just checks if the socket is available
+    # and if we can connect to it. The image check returns nothing if the image is not present.
     with contextlib.suppress(Exception):
         with ContainerdClient('image') as client:
             client.get_image('alpine')
