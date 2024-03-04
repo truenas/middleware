5b789f23f5f6fd903b0a6678c3376299b9efc2dd
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-03-04 11:59:31 -0500

    Fix disk temperature logs not being retrieved when smartd interval is changed (#13281)
    (cherry picked from commit 848463f1e80961f53acdb78060cf2d383049a2ea)
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

--- src/freenas/usr/lib/netdata/python.d/smart_log.chart.py
+++ src/freenas/usr/lib/netdata/python.d/smart_log.chart.py
@@ -22,7 +22,6 @@ CSV = '.csv'
 DEF_AGE = 30
 DEF_PATH = '/var/log/smartd'
 DEF_RESCAN_INTERVAL = 60
-UPDATE_INTERVAL = 30
 INCREMENTAL = 'incremental'
 RE_ATA = re.compile(
     r'(\d+);'  # attribute
@@ -258,7 +257,7 @@ class Service(SimpleService):
         self.exclude = configuration.get('exclude_disks', str()).split()
         self.disks = list()
         self.runs = 0
-        self.update_data = UPDATE_INTERVAL
+        self.update_data = self.age
         self.do_force_rescan = False
         # smartd daemon only queries drive temps every 30mins so the files won't be updated
         # but once every 30ish minutes - we should change this if at any point we change smartd interval
@@ -295,14 +294,14 @@ class Service(SimpleService):
                 self.do_force_rescan = True
                 continue
 
-            if self.update_data >= UPDATE_INTERVAL and not disk.populate_attrs():
+            if self.update_data >= self.age and not disk.populate_attrs():
                 disk.alive = False
                 self.do_force_rescan = True
                 continue
 
             data.update(disk.data())
 
-        if not self.do_force_rescan and self.update_data >= UPDATE_INTERVAL:
+        if not self.do_force_rescan and self.update_data >= self.age:
             self.update_data = 0
 
         return data
