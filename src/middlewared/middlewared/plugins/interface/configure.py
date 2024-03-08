a1a633c92d80885b42a0397ac4a6666ea4ed1d90
Brian Meagher <brian.meagher@ixsystems.com>
2024-03-08 08:49:16 -0800

    Address review
    Move rdma.get_configured_interfaces to interface.get_configured_interfaces and
    reimplement
    
    (cherry picked from commit f9a330c24007e9e28e487c174591004adbdb36fd)

--- src/middlewared/middlewared/plugins/interface/configure.py
+++ src/middlewared/middlewared/plugins/interface/configure.py
@@ -173,3 +173,24 @@ class InterfaceService(Service):
         if 'vhid' in alias:
             addr.vhid = alias['vhid']
         return addr
+
+    @private
+    async def get_configured_interfaces(self):
+        """
+        Return a list of configured interfaces.
+
+        This will include names of regular interfaces that have been configured,
+        plus any higher-order interfaces and their constituents."""
+        ds = await self.middleware.call('interface.get_datastores')
+        # Interfaces
+        result = set([i['int_interface'] for i in ds['interfaces']])
+        # Bridges
+        for bridge in ds['bridge']:
+            result.update(bridge['members'])
+        # VLAN
+        for vlan in ds['vlan']:
+            result.add(vlan['vlan_pint'])
+        # Link Aggregation
+        for lag in ds['laggmembers']:
+            result.add(lag['lagg_physnic'])
+        return list(result)
