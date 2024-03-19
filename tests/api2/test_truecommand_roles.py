35e9ba8d0b8a9a20b0662b53e1865eeb1b7e3df4
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-03-20 01:48:11 +0500

    Update integration tests

--- tests/api2/test_truecommand_roles.py
+++ tests/api2/test_truecommand_roles.py
@@ -5,15 +5,15 @@ from middlewared.test.integration.assets.roles import common_checks
 
 def test_truecommand_readonly_role():
     common_checks(
-        'truecommand.connected', 'READONLY_ADMIN', True, valid_role_exception=False
+        'truenas.managed_by_truecommand', 'READONLY_ADMIN', True, valid_role_exception=False
     )
 
 
 @pytest.mark.parametrize('endpoint,role,should_work,valid_role_exception', [
     ('truecommand.config', 'TRUECOMMAND_READ', True, False),
     ('truecommand.config', 'TRUECOMMAND_WRITE', True, False),
-    ('truecommand.connected', 'TRUECOMMAND_READ', True, False),
-    ('truecommand.connected', 'TRUECOMMAND_WRITE', True, False),
+    ('truecommand.info', 'TRUECOMMAND_READ', True, False),
+    ('truecommand.info', 'TRUECOMMAND_WRITE', True, False),
     ('truecommand.update', 'TRUECOMMAND_READ', False, True),
     ('truecommand.update', 'TRUECOMMAND_WRITE', True, True),
 ])
