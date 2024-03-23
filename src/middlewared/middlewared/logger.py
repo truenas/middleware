a947d93adfb35958bba91be16324b0e4ba461b97
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-03-23 07:27:10 -0400

    Silence some syslog DEBUG spam from passlib.utils.compat (#13397)
    (cherry picked from commit 2891ff88183367a5a8e6b04681ec734e37466575)
    
    Co-authored-by: Brian Meagher <brian.meagher@ixsystems.com>

--- src/middlewared/middlewared/logger.py
+++ src/middlewared/middlewared/logger.py
@@ -20,6 +20,7 @@ logging.getLogger('googleapiclient').setLevel(logging.ERROR)
 # registered 'pbkdf2_sha256' handler: <class 'passlib.handlers.pbkdf2.pbkdf2_sha256'>
 logging.getLogger('passlib.registry').setLevel(logging.INFO)
 logging.getLogger('passlib.handlers').setLevel(logging.INFO)
+logging.getLogger('passlib.utils.compat').setLevel(logging.INFO)
 # pyroute2.ndb is chatty....only log errors
 logging.getLogger('pyroute2.ndb').setLevel(logging.CRITICAL)
 logging.getLogger('pyroute2.netlink').setLevel(logging.CRITICAL)
