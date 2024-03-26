13c4f68330ea85abbc483343004d277a1bdd6aaf
themylogin <themylogin@gmail.com>
2024-03-26 13:55:48 +0100

    Add LICENSE.IX to HA-related alerts and unit tests

--- src/middlewared/middlewared/pytest/unit/plugins/enclosure/test_element_types.py
+++ src/middlewared/middlewared/pytest/unit/plugins/enclosure/test_element_types.py
@@ -1,3 +1,8 @@
+# Copyright (c) - iXsystems Inc.
+#
+# Licensed under the terms of the TrueNAS Enterprise License Agreement
+# See the file LICENSE.IX for complete terms and conditions
+
 import pytest
 
 from middlewared.plugins.enclosure_ import element_types
