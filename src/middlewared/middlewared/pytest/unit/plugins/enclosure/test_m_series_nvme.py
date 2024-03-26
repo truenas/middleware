26bee12b6b8e036178711469abcc46382d443d0f
themylogin <themylogin@gmail.com>
2024-03-26 13:55:48 +0100

    Add LICENSE.IX to HA-related alerts and unit tests
    (cherry picked from commit 13c4f68330ea85abbc483343004d277a1bdd6aaf)

--- src/middlewared/middlewared/pytest/unit/plugins/enclosure/test_m_series_nvme.py
+++ src/middlewared/middlewared/pytest/unit/plugins/enclosure/test_m_series_nvme.py
@@ -1,3 +1,8 @@
+# Copyright (c) - iXsystems Inc.
+#
+# Licensed under the terms of the TrueNAS Enterprise License Agreement
+# See the file LICENSE.IX for complete terms and conditions
+
 from unittest.mock import Mock, patch
 
 from middlewared.plugins.enclosure_.nvme import EnclosureService
