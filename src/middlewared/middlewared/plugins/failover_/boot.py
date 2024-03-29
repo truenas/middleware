63bbe5b34bc65c4272f3abc4aaccbf61ba79bd04
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-03-30 02:05:56 +0500

    Add a method which determines if reboot is required for HA
    (cherry picked from commit de4657a740be4e5a34668902d4fe541405393d6e)
    (cherry picked from commit 525d8d07e41839518b3fb1e15beb9cbcabe0915c)

--- src/middlewared/middlewared/plugins/failover_/boot.py
+++ src/middlewared/middlewared/plugins/failover_/boot.py
@@ -1,3 +1,4 @@
+from middlewared.schema import accepts, Bool, Dict, returns, Str
 from middlewared.service import private, Service
 
 
@@ -11,3 +12,37 @@ class FailoverService(Service):
                 'failover.call_remote', 'system.boot_id',
             ),
         }
+
+    @accepts()
+    @returns(Dict(
+        Bool('reboot_required'),
+        Bool('node_a_reboot_required'),
+        Bool('node_b_reboot_required'),
+        Str('reason'),
+    ))
+    async def reboot_required(self):
+        """
+        Returns whether a reboot is required for failover/security system configuration changes to take effect.
+        """
+        fips_change_info = await self.middleware.call('keyvalue.get', 'fips_toggled', False)
+        if not fips_change_info:
+            return {
+                'reboot_required': False,
+                'node_a_reboot_required': False,
+                'node_b_reboot_required': False,
+                'reason': 'No reboot required',
+            }
+
+        existing_boot_ids = await self.retrieve_boot_ids()
+        info = {
+            'reason': 'No reboot required',
+            'node_a_reboot_required': existing_boot_ids['A'] == fips_change_info['A'],
+            'node_b_reboot_required': existing_boot_ids['B'] == fips_change_info['B'],
+        }
+        if info['node_a_reboot_required'] or info['node_b_reboot_required']:
+            info.update({
+                'reboot_required': True,
+                'reason': 'Reboot required for FIPS configuration change to take effect',
+            })
+
+        return info
