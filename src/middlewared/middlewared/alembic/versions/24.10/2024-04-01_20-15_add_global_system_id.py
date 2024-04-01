7215969f780c9af8b0751810f99db38c22e72d76
M. Rehan <mrehanlm93@gmail.com>
2024-04-01 21:42:37 +0500

    Update migration

--- src/middlewared/middlewared/alembic/versions/24.10/2024-04-01_20-15_add_global_system_id.py
+++ src/middlewared/middlewared/alembic/versions/24.10/2024-04-01_20-15_add_global_system_id.py
@@ -1,8 +1,8 @@
 """Add global system ID
 
 Revision ID: 14974a858948
-Revises: 7836261b2f64
-Create Date: 2024-03-21 12:15:50.886820+00:00
+Revises: 1a6fc6735dc2
+Create Date: 2024-04-01 20:15:50.886820+00:00
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '14974a858948'
-down_revision = '7836261b2f64'
+down_revision = '1a6fc6735dc2'
 branch_labels = None
 depends_on = None
 
