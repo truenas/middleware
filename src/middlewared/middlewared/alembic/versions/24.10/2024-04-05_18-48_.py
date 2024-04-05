cd828db84f3ed8849357f34a15c765b042f37c4b
Brian Meagher <brian.meagher@ixsystems.com>
2024-04-05 11:51:23 -0700

    Merge migrations

---
+++ src/middlewared/middlewared/alembic/versions/24.10/2024-04-05_18-48_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3cb1ccef4ade
+Revises: d7243bc7bab9, 423e4c21c28d
+Create Date: 2024-04-05 18:48:47.198813+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3cb1ccef4ade'
+down_revision = ('d7243bc7bab9', '423e4c21c28d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
