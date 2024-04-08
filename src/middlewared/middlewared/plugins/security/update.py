41e974f537f82ca1ba23a75f33a016c424dcd906
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-04-08 08:41:36 -0400

    NAS-128246 / 24.04.1 / Fix reboot info (by yocalebo) (#13490)
    * add connect_{timeout} in mismatch_disks
    
    (cherry picked from commit de26bce40b0291fa58024190ad0c5f93ae27708f)
    
    * fix and improve failover_/reboot.py
    
    (cherry picked from commit 4cf51a75ec61cba40aea94eb68b4a07f87cf801c)
    
    * fix and improve security/update.py
    
    (cherry picked from commit f33bb1f723e43b03e4caa9a3c722d41983ef3976)
    
    * fix failover_/disabled_reasons.py
    
    (cherry picked from commit b7c21cfed9469c3c3ebd1bb9128e6b131e117fbe)
    
    * fix issues found during testing
    
    (cherry picked from commit ae7eafea837a36b7451149c12493122f752080a4)
    
    * update fips key properly
    
    (cherry picked from commit e84d7312fdf5054f3e972adda81aa259df5fc422)
    
    * remove stray file
    
    (cherry picked from commit d9ae89cbf67be0643930779eae207d943124a905)
    
    ---------
    
    Co-authored-by: Caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/security/update.py
+++ src/middlewared/middlewared/plugins/security/update.py
@@ -1,8 +1,9 @@
 import middlewared.sqlalchemy as sa
 
 from middlewared.plugins.failover_.disabled_reasons import DisabledReasonsEnum
+from middlewared.plugins.failover_.reboot import FIPS_KEY
 from middlewared.schema import accepts, Bool, Dict, Int, Patch
-from middlewared.service import CallError, ConfigService, ValidationError, job
+from middlewared.service import ConfigService, ValidationError, job, private
 
 
 class SystemSecurityModel(sa.Model):
@@ -25,6 +26,74 @@ class SystemSecurityService(ConfigService):
         Int('id', required=True),
     )
 
+    @private
+    async def configure_fips_on_ha(self, is_ha, job):
+        if not is_ha:
+            return
+
+        boot_info = await self.middleware.call('failover.reboot.info')
+        if boot_info['this_node']['reboot_required']:
+            # means FIPS is being toggled but this node is already pending a reboot
+            # so it means the user toggled FIPS twice without a reboot in between
+            boot_info['this_node']['reboot_required'] = False
+            await self.middleware.call('keyvalue.set', FIPS_KEY, boot_info)
+        else:
+            # means FIPS is toggled and this node isn't pending a reboot, so mark it
+            # as such
+            boot_info['this_node']['reboot_required'] = True
+            await self.middleware.call('keyvalue.set', FIPS_KEY, boot_info)
+
+        if boot_info['other_node']['reboot_required']:
+            # means FIPS is being toggled but other node is already pending a reboot
+            # so it means the user toggled FIPS twice and somehow the other node
+            # didn't reboot (even though we do this automatically). This is an edge
+            # case and means someone or something is doing things behind our backs
+            boot_info['other_node']['reboot_required'] = False
+            await self.middleware.call('keyvalue.set', FIPS_KEY, boot_info)
+        else:
+            try:
+                # we automatically reboot (and wait for) the other controller
+                reboot_job = await self.middleware.call('failover.reboot.other_node')
+                await job.wrap(reboot_job)
+            except Exception:
+                self.logger.error('Unexpected failure rebooting the other node', exc_info=True)
+                # something extravagant happened so we'll just play it safe and say that
+                # another reboot is required
+                boot_info['other_node']['reboot_required'] = True
+            else:
+                new_info = await self.middleware.call('failover.reboot.info')
+                if boot_info['other_node']['id'] == new_info['other_node']['id']:
+                    # standby "rebooted" but the boot id is the same....not good
+                    self.logger.warning('Other node claims it rebooted but boot id is the same')
+                    boot_info['other_node']['reboot_required'] = True
+                else:
+                    boot_info['other_node']['id'] = new_info['other_node']['id']
+                    boot_info['other_node']['reboot_required'] = False
+
+            await self.middleware.call('keyvalue.set', FIPS_KEY, boot_info)
+
+    @private
+    async def validate(self, is_ha, ha_disabled_reasons):
+        schema = 'system_security_update.enable_fips'
+        if not await self.middleware.call('system.security.info.fips_available'):
+            raise ValidationError(
+                schema,
+                'This feature can only be enabled on licensed iX enterprise systems. '
+                'Please contact iX sales for more information.'
+            )
+
+        if is_ha and ha_disabled_reasons:
+            bad_reasons = set(ha_disabled_reasons) - {
+                DisabledReasonsEnum.LOC_FIPS_REBOOT_REQ.name,
+                DisabledReasonsEnum.REM_FIPS_REBOOT_REQ.name,
+            }
+            if bad_reasons:
+                formatted = '\n'.join([DisabledReasonsEnum[i].value for i in bad_reasons])
+                raise ValidationError(
+                    schema,
+                    f'Security settings cannot be updated while HA is in an unhealthy state: ({formatted})'
+                )
+
     @accepts(
         Patch(
             'system_security_entry', 'system_security_update',
@@ -39,20 +108,9 @@ class SystemSecurityService(ConfigService):
 
         `enable_fips` when set, enables FIPS mode.
         """
-        if not await self.middleware.call('system.security.info.fips_available'):
-            raise ValidationError(
-                'system_security_update.enable_fips',
-                'This feature can only be enabled on licensed iX enterprise systems. '
-                'Please contact iX sales for more information.'
-            )
-
         is_ha = await self.middleware.call('failover.licensed')
-        if is_ha and (reasons := await self.middleware.call('failover.disabled.reasons')):
-            if set(reasons) - {
-                DisabledReasonsEnum.LOC_FIPS_REBOOT_REQ,
-                DisabledReasonsEnum.REM_FIPS_REBOOT_REQ,
-            }:
-                raise CallError('Security settings cannot be updated while HA is in an unhealthy state')
+        reasons = await self.middleware.call('failover.disabled.reasons')
+        await self.validate(is_ha, reasons)
 
         old = await self.config()
         new = old.copy()
@@ -66,10 +124,7 @@ class SystemSecurityService(ConfigService):
             # TODO: We likely need to do some SSH magic as well
             #  let's investigate the exact configuration there
             await self.middleware.call('etc.generate', 'fips')
-            if is_ha:
-                boot_id_info = await self.middleware.call('failover.reboot.retrieve_boot_ids')
-                await self.middleware.call('keyvalue.set', 'fips_toggled', boot_id_info)
-                reboot_job = await self.middleware.call('failover.reboot.other_node')
-                await job.wrap(reboot_job)
+            # TODO: We need to fix this for non-HA iX hardware...
+            await self.configure_fips_on_ha(is_ha, job)
 
         return await self.config()
