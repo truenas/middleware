546792110cef91477fe54357136c9f77b376838c
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-04-08 15:23:15 -0400

    NAS-128261 / 24.04.1 / replace test-400 with proper test (by yocalebo) (#13499)
    * replace test-400 with proper test
    
    (cherry picked from commit b559ecb156fc28069700a198146e031dc4c35adf)
    
    * address review
    
    (cherry picked from commit f9f444a6b91b6a03b2bc2664f833fcfe0179c9d8)
    
    ---------
    
    Co-authored-by: Caleb <yocalebo@gmail.com>

---
+++ tests/api2/test_enable_disable_services.py
@@ -0,0 +1,10 @@
+from middlewared.test.integration.utils import call
+
+def enable_services():
+    for svc in filter(lambda x: not x['enable'], call('service.query'):
+        call('service.update', svc['id'], {'enable': True})
+
+
+def disable_services():
+    for svc in filter(lambda x: x['enable'], call('service.query'):
+        call('service.update', svc['id'], {'enable': False})
