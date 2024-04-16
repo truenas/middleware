120a00c797680c5c5a592e08cf95c0b5615cb3c3
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:45:05 +0500

    Update crypto plugin roles tests

--- tests/api2/test_certificate_roles.py
+++ tests/api2/test_certificate_roles.py
@@ -9,32 +9,32 @@ from middlewared.test.integration.assets.roles import common_checks
     ('certificate.profiles', 'CERTIFICATE_AUTHORITY_READ', False),
     ('certificateauthority.profiles', 'CERTIFICATE_READ', False),
 ))
-def test_profiles_read_roles(method, role, valid_role):
-    common_checks(method, role, valid_role, valid_role_exception=False)
+def test_profiles_read_roles(unprivileged_user_fixture, method, role, valid_role):
+    common_checks(unprivileged_user_fixture, method, role, valid_role, valid_role_exception=False)
 
 
 @pytest.mark.parametrize('role, valid_role', (
     ('CERTIFICATE_AUTHORITY_WRITE', True),
     ('CERTIFICATE_AUTHORITY_READ', False),
 ))
-def test_certificate_authority_create_role(role, valid_role):
-    common_checks('certificateauthority.create', role, valid_role, method_args=[{}])
+def test_certificate_authority_create_role(unprivileged_user_fixture, role, valid_role):
+    common_checks(unprivileged_user_fixture, 'certificateauthority.create', role, valid_role, method_args=[{}])
 
 
 @pytest.mark.parametrize('role, valid_role', (
     ('CERTIFICATE_WRITE', True),
     ('CERTIFICATE_READ', False),
 ))
-def test_certificate_create_role(role, valid_role):
-    common_checks('certificate.create', role, valid_role, method_args=[], method_kwargs={'job': True})
+def test_certificate_create_role(unprivileged_user_fixture, role, valid_role):
+    common_checks(unprivileged_user_fixture, 'certificate.create', role, valid_role, method_args=[], method_kwargs={'job': True})
 
 
 @pytest.mark.parametrize('role, valid_role', (
     ('CERTIFICATE_AUTHORITY_WRITE', True),
     ('CERTIFICATE_AUTHORITY_READ', False),
 ))
-def test_signing_csr_role(role, valid_role):
-    common_checks('certificateauthority.ca_sign_csr', role, valid_role, method_args=[{
+def test_signing_csr_role(unprivileged_user_fixture, role, valid_role):
+    common_checks(unprivileged_user_fixture, 'certificateauthority.ca_sign_csr', role, valid_role, method_args=[{
         'ca_id': 1,
         'csr_cert_id': 1,
         'name': 'test_csr_signing_role',
