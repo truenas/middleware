ec4868e083c1cafe3c99e42ef043fcf7a10ea3db
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-16 22:45:50 +0500

    Add a fixture for unprivileged user

--- src/middlewared/middlewared/test/integration/assets/roles.py
+++ src/middlewared/middlewared/test/integration/assets/roles.py
@@ -1,8 +1,30 @@
+import contextlib
 import errno
+import random
 import pytest
+import string
 
 from middlewared.client.client import ClientException
-from middlewared.test.integration.assets.account import unprivileged_user_client
+from middlewared.test.integration.assets.account import (
+    unprivileged_user,
+    unprivileged_user_client as unprivileged_user_client_main,
+)
+from middlewared.test.integration.utils import call
+
+
+@pytest.fixture(scope='session')
+def unprivileged_user_fixture(request):
+    suffix = ''.join([random.choice(string.ascii_lowercase + string.digits) for _ in range(8)])
+    group_name = f'unprivileged_users_fixture_{suffix}'
+    with unprivileged_user(
+        username=f'unprivileged_fixture_{suffix}',
+        group_name=group_name,
+        privilege_name=f'Unprivileged users fixture ({suffix})',
+        allowlist=[],
+        roles=[],
+        web_shell=False,
+    ) as t:
+        yield t, group_name
 
 
 def common_checks(
@@ -10,7 +32,7 @@ def common_checks(
 ):
     method_args = method_args or []
     method_kwargs = method_kwargs or {}
-    with unprivileged_user_client(roles=[role]) as client:
+    with unprivileged_user_client_main(roles=[role]) as client:
         if valid_role:
             if valid_role_exception:
                 with pytest.raises(Exception) as exc_info:
