467785f8b9802795e9f0378cae5d8ddb3ed8ddad
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:46:55 +0500

    Update iscsi global plugin roles tests

--- tests/api2/test_iscsi_global_crud_roles.py
+++ tests/api2/test_iscsi_global_crud_roles.py
@@ -4,18 +4,18 @@ from middlewared.test.integration.assets.roles import common_checks
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_GLOBAL_READ"])
-def test_read_role_can_read(role):
-    common_checks("iscsi.global.config", role, True, valid_role_exception=False)
-    common_checks("iscsi.global.sessions", role, True, valid_role_exception=False)
-    common_checks("iscsi.global.client_count", role, True, valid_role_exception=False)
-    common_checks("iscsi.global.alua_enabled", role, True, valid_role_exception=False)
+def test_read_role_can_read(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.global.config", role, True, valid_role_exception=False)
+    common_checks(unprivileged_user_fixture, "iscsi.global.sessions", role, True, valid_role_exception=False)
+    common_checks(unprivileged_user_fixture, "iscsi.global.client_count", role, True, valid_role_exception=False)
+    common_checks(unprivileged_user_fixture, "iscsi.global.alua_enabled", role, True, valid_role_exception=False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_GLOBAL_READ"])
-def test_read_role_cant_write(role):
-    common_checks("iscsi.global.update", role, False)
+def test_read_role_cant_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.global.update", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_ISCSI_WRITE", "SHARING_ISCSI_GLOBAL_WRITE"])
-def test_write_role_can_write(role):
-    common_checks("iscsi.global.update", role, True)
+def test_write_role_can_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.global.update", role, True)
