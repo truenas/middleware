83a91324e9015e5a97461b93172b7ce8f0ffd378
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:56:31 +0500

    Update iscsi host plugin roles tests

--- tests/api2/test_iscsi_host_crud_roles.py
+++ tests/api2/test_iscsi_host_crud_roles.py
@@ -4,23 +4,23 @@ from middlewared.test.integration.assets.roles import common_checks
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_HOST_READ"])
-def test_read_role_can_read(role):
-    common_checks("iscsi.host.query", role, True, valid_role_exception=False)
-    common_checks("iscsi.host.get_initiators", role, True)
-    common_checks("iscsi.host.get_targets", role, True)
+def test_read_role_can_read(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.host.query", role, True, valid_role_exception=False)
+    common_checks(unprivileged_user_fixture, "iscsi.host.get_initiators", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.host.get_targets", role, True)
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_HOST_READ"])
-def test_read_role_cant_write(role):
-    common_checks("iscsi.host.create", role, False)
-    common_checks("iscsi.host.update", role, False)
-    common_checks("iscsi.host.delete", role, False)
-    common_checks("iscsi.host.set_initiators", role, False)
+def test_read_role_cant_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.host.create", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.host.update", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.host.delete", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.host.set_initiators", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_ISCSI_WRITE", "SHARING_ISCSI_HOST_WRITE"])
-def test_write_role_can_write(role):
-    common_checks("iscsi.host.create", role, True)
-    common_checks("iscsi.host.update", role, True)
-    common_checks("iscsi.host.delete", role, True)
-    common_checks("iscsi.host.set_initiators", role, True)
+def test_write_role_can_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.host.create", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.host.update", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.host.delete", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.host.set_initiators", role, True)
