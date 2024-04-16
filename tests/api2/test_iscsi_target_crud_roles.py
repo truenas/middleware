4a254027bb62dce0e6b459f3b8c8d8f3d7e0297e
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:57:11 +0500

    Update iscsi target plugin roles tests

--- tests/api2/test_iscsi_target_crud_roles.py
+++ tests/api2/test_iscsi_target_crud_roles.py
@@ -4,19 +4,19 @@ from middlewared.test.integration.assets.roles import common_checks
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_TARGET_READ"])
-def test_read_role_can_read(role):
-    common_checks("iscsi.target.query", role, True, valid_role_exception=False)
+def test_read_role_can_read(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.target.query", role, True, valid_role_exception=False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_TARGET_READ"])
-def test_read_role_cant_write(role):
-    common_checks("iscsi.target.create", role, False)
-    common_checks("iscsi.target.update", role, False)
-    common_checks("iscsi.target.delete", role, False)
+def test_read_role_cant_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.target.create", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.target.update", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.target.delete", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_ISCSI_WRITE", "SHARING_ISCSI_TARGET_WRITE"])
-def test_write_role_can_write(role):
-    common_checks("iscsi.target.create", role, True)
-    common_checks("iscsi.target.update", role, True)
-    common_checks("iscsi.target.delete", role, True)
+def test_write_role_can_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.target.create", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.target.update", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.target.delete", role, True)
