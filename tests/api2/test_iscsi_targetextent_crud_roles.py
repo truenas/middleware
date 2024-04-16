dc384a70a4e2199d183b46d272e520456992ee8f
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:50:32 +0500

    Update iscsi targetextent plugin roles tests

--- tests/api2/test_iscsi_targetextent_crud_roles.py
+++ tests/api2/test_iscsi_targetextent_crud_roles.py
@@ -4,19 +4,19 @@ from middlewared.test.integration.assets.roles import common_checks
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_TARGETEXTENT_READ"])
-def test_read_role_can_read(role):
-    common_checks("iscsi.targetextent.query", role, True, valid_role_exception=False)
+def test_read_role_can_read(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.query", role, True, valid_role_exception=False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_TARGETEXTENT_READ"])
-def test_read_role_cant_write(role):
-    common_checks("iscsi.targetextent.create", role, False)
-    common_checks("iscsi.targetextent.update", role, False)
-    common_checks("iscsi.targetextent.delete", role, False)
+def test_read_role_cant_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.create", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.update", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.delete", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_ISCSI_WRITE", "SHARING_ISCSI_TARGETEXTENT_WRITE"])
-def test_write_role_can_write(role):
-    common_checks("iscsi.targetextent.create", role, True)
-    common_checks("iscsi.targetextent.update", role, True)
-    common_checks("iscsi.targetextent.delete", role, True)
+def test_write_role_can_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.create", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.update", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.targetextent.delete", role, True)
