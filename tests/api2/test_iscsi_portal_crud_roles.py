89c98dc9f07ee7e691b1eb16d4f68bdca297b53b
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-04-17 00:47:43 +0500

    Update iscsi portal plugin roles tests

--- tests/api2/test_iscsi_portal_crud_roles.py
+++ tests/api2/test_iscsi_portal_crud_roles.py
@@ -4,19 +4,19 @@ from middlewared.test.integration.assets.roles import common_checks
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_PORTAL_READ"])
-def test_read_role_can_read(role):
-    common_checks("iscsi.portal.query", role, True, valid_role_exception=False)
+def test_read_role_can_read(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.portal.query", role, True, valid_role_exception=False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_READ", "SHARING_ISCSI_READ", "SHARING_ISCSI_PORTAL_READ"])
-def test_read_role_cant_write(role):
-    common_checks("iscsi.portal.create", role, False)
-    common_checks("iscsi.portal.update", role, False)
-    common_checks("iscsi.portal.delete", role, False)
+def test_read_role_cant_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.portal.create", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.portal.update", role, False)
+    common_checks(unprivileged_user_fixture, "iscsi.portal.delete", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_ISCSI_WRITE", "SHARING_ISCSI_PORTAL_WRITE"])
-def test_write_role_can_write(role):
-    common_checks("iscsi.portal.create", role, True)
-    common_checks("iscsi.portal.update", role, True)
-    common_checks("iscsi.portal.delete", role, True)
+def test_write_role_can_write(unprivileged_user_fixture, role):
+    common_checks(unprivileged_user_fixture, "iscsi.portal.create", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.portal.update", role, True)
+    common_checks(unprivileged_user_fixture, "iscsi.portal.delete", role, True)
