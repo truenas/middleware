b5006c3ae994bb1148dffab73a81cd9faf910f5e
Caleb <yocalebo@gmail.com>
2024-04-18 11:50:52 -0400

    modernize test_009_fenced

--- tests/api2/test_009_fenced.py
+++ tests/api2/test_009_fenced.py
@@ -1,17 +1,9 @@
-import sys
-from os import getcwd, environ
-apifolder = getcwd()
-sys.path.append(apifolder)
+import pytest
 
-from functions import make_ws_request
 from auto_config import ha
+from middlewared.test.integration.utils import call
 
-# Do not run the code code below on non-HA
-if ha:
-    IP = environ.get('controller1_ip')
-    assert IP, 'Need controller 1 IP before this will work'
 
-    def test_01_verify_fenced_is_running(request):
-        payload = {'msg': 'method', 'method': 'failover.fenced.run_info', 'params': []}
-        res = make_ws_request(IP, payload)
-        assert res['result']['running'], res
+@pytest.mark.skipif(not ha, reason='HA only test')
+def test_01_verify_fenced_is_running():
+    assert call('failover.fenced.run_info')['running']
