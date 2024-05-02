d8da2cdc3c5283e5454343dd3979ca364e50e8b2
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-05-02 05:47:32 -0700

    NAS-128710 / 24.10 / Add migrations for SMB-related fields (by anodos325) (#13644)
    In the DragonFish release we removed the clustering feature from
    TrueNAS. In prior versions we had some SMB share presets that were
    exposed only to clustering users via the webui, but were still
    accessible via the API. These presets existed for TrueCommand purposes
    to simplify cluster share creation. Unfortunately, some users may have
    set these via our APIs on non-clustered shares, and so this commit adds
    a migration to convert them to an appropriate SMB share configuration
    post-Cobia.
    
    Due to a longstanding bug in SCALE, there were circumstances in which
    pam_mkhomedir would create the directory `/nonexistent` when users had
    SMB homes shares. Starting in dragonfish / is readonly with the result
    that the pam_session for users with a home directory of `/nonexistent`
    to fail, resulting in SMB access being rejected in "obey pam
    restrictions" is set. This commit adds a migration to force home
    directories of SMB users to change to /var/empty if it was set to
    /nonexistent.
    
    (cherry picked from commit c26810011cb41416fd79076b4c8d334c64ae928a)
    
    Co-authored-by: Andrew Walker <awalker@ixsystems.com>

---
+++ src/middlewared/middlewared/alembic/versions/24.10/2024-05-02_12-00_merge_migration.py
@@ -0,0 +1,24 @@
+""" Merge migration for changes in SMB-related fields from 24.04
+
+Revision ID: 135a7e02cbec
+Revises: 4f11cc19bb9c, f38c2bbe776a
+Create Date: 2024-05-02 12:00:35.086514+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '135a7e02cbec'
+down_revision = ('4f11cc19bb9c', 'f38c2bbe776a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
