2b57fe057522960d60ff6b786713dee699947c8f
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-19 21:21:49 +0500

    Add catalog.create endpoint

--- src/middlewared/middlewared/plugins/catalog/update.py
+++ src/middlewared/middlewared/plugins/catalog/update.py
@@ -1,7 +1,9 @@
+import errno
+
 import middlewared.sqlalchemy as sa
 
-from middlewared.schema import Bool, Dict, List, Str
-from middlewared.service import CRUDService
+from middlewared.schema import accepts, Bool, Dict, List, Patch, Str
+from middlewared.service import CRUDService, job, private, ValidationErrors
 from middlewared.validators import Match
 
 
@@ -19,24 +21,66 @@ class CatalogService(CRUDService):
 
     class Config:
         datastore = 'services.catalog'
+        datastore_extend = 'catalog.extend'
         datastore_primary_key = 'label'
         datastore_primary_key_type = 'string'
         cli_namespace = 'app.catalog'
         namespace = 'catalog'
 
-    ENTRY = Dict(
-        'catalog_entry',
-        Bool('builtin'),
-        List('preferred_trains'),
-        Str(
-            'label', required=True, validators=[Match(
-                r'^\w+[\w.-]*$',
-                explanation='Label must start with an alphanumeric character and can include dots and dashes.'
-            )],
-            max_length=60,
-        ),
-        Str('repository', required=True, empty=False),
-        Str('branch', required=True, empty=False),
-        Str('location', required=True),
-        Str('id', required=True),
+    ENTRY = Patch(
+        'catalog_create', 'catalog_entry',
+        ('add', Bool('builtin')),
+        ('add', Str('id')),
+        ('rm', {'name': 'force'}),
     )
+
+    @private
+    def extend(self, data):
+        data['id'] = data['label']
+        return data
+
+    @accepts(
+        Dict(
+            'catalog_create',
+            Bool('force', default=False),
+            List('preferred_trains'),
+            Str(
+                'label', required=True, validators=[Match(
+                    r'^\w+[\w.-]*$',
+                    explanation='Label must start with an alphanumeric character and can include dots and dashes.'
+                )],
+                max_length=60,
+            ),
+            Str('repository', required=True, empty=False),
+            Str('branch', required=True, empty=False),
+            register=True,
+        )
+    )
+    @job(lock='catalog_create')
+    async def do_create(self, job, data):
+        """
+        Create a new catalog entry.
+        """
+        verrors = ValidationErrors()
+        # We normalize the label
+        data['label'] = data['label'].upper()
+
+        if await self.query([['id', '=', data['label']]]):
+            verrors.add('catalog_create.label', 'A catalog with specified label already exists', errno=errno.EEXIST)
+
+        if await self.query([['repository', '=', data['repository']], ['branch', '=', data['branch']]]):
+            for k in ('repository', 'branch'):
+                verrors.add(
+                    f'catalog_create.{k}', 'A catalog with same repository/branch already exists', errno=errno.EEXIST
+                )
+
+        verrors.check()
+
+        job.set_progress(60, 'Completed catalog validation')
+
+        if not data['preferred_trains']:
+            data['preferred_trains'] = ['stable']
+
+        await self.middleware.call('datastore.insert', self._config.datastore, data)
+
+        return await self.get_instance(data['label'])
