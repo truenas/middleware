894dca75602068bd556a3b8f0f2b532c34c13a6a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-19 20:11:00 +0500

    Add basic method for handling state change of docker

---
+++ src/middlewared/middlewared/plugins/docker/state_setup.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+import uuid
+
+from datetime import datetime
+
+from middlewared.service import private, Service
+
+from .state_utils import DATASET_DEFAULTS, docker_datasets, docker_dataset_custom_props, docker_dataset_update_props
+
+
+class DockerSetupService(Service):
+
+    namespace = 'docker.setup'
+
+    @private
+    async def status_change(self):
+        config = await self.middleware.call('docker.config')
+        if not config['pool']:
+            return
+
+    @private
+    async def pools(self, config):
+        return await self.middleware.call('pool.query', [('status', '=', 'ONLINE')])
+
+    @private
+    async def create_update_docker_datasets(self, docker_ds):
+        create_props_default = DATASET_DEFAULTS.copy()
+        for dataset_name in docker_datasets(docker_ds):
+            custom_props = docker_dataset_custom_props(dataset_name.split('/', 1)[-1])
+            # got custom properties, need to re-calculate
+            # the update and create props.
+            create_props = dict(create_props_default, **custom_props) if custom_props else create_props_default
+            update_props = docker_dataset_update_props(create_props)
+
+            dataset = await self.middleware.call(
+                'zfs.dataset.query', [['id', '=', dataset_name]], {
+                    'extra': {
+                        'properties': list(update_props),
+                        'retrieve_children': False,
+                        'user_properties': False,
+                    }
+                }
+            )
+            if not dataset:
+                test_path = os.path.join('/mnt', dataset_name)
+                if self.middleware.run_in_thread(os.path.exists, test_path):
+                    await self.middleware.run_in_thread(
+                        shutil.move, test_path, f'{test_path}-{str(uuid.uuid4())[:4]}-{datetime.now().isoformat()}',
+                    )
+                await self.middleware.call(
+                    'zfs.dataset.create', {
+                        'name': dataset_name, 'type': 'FILESYSTEM', 'properties': create_props,
+                    }
+                )
+                if create_props.get('mountpoint') != 'legacy':
+                    # since, legacy mountpoints should not be zfs mounted.
+                    await self.middleware.call('zfs.dataset.mount', dataset_name)
+            elif any(val['value'] != update_props[name] for name, val in dataset[0]['properties'].items()):
+                await self.middleware.call(
+                    'zfs.dataset.update', dataset_name, {
+                        'properties': {k: {'value': v} for k, v in update_props.items()}
+                    }
+                )
