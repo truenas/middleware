9db71a84dfc6f88d2b6cd22277eb2f7681a4484f
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-20 21:42:37 +0500

    Add items api integration

--- src/middlewared/middlewared/plugins/catalog/utils.py
+++ src/middlewared/middlewared/plugins/catalog/utils.py
@@ -6,3 +6,7 @@ from middlewared.utils import MIDDLEWARE_RUN_DIR
 OFFICIAL_ENTERPRISE_TRAIN = 'enterprise'
 OFFICIAL_LABEL = 'TRUENAS'
 TMP_IX_APPS_CATALOGS = os.path.join(MIDDLEWARE_RUN_DIR, 'ix-apps/catalogs')
+
+
+def get_cache_key(label: str) -> str:
+    return f'catalog_{label}_train_details'
