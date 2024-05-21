120d05fd9f7c56f29aa9ba4dd27845a9fab993b4
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-21 05:54:23 +0500

    Add app version utils

--- src/middlewared/middlewared/plugins/catalog/apps_util.py
+++ src/middlewared/middlewared/plugins/catalog/apps_util.py
@@ -1,15 +1,23 @@
+import errno
+import json
+import jsonschema
+import os
 import re
 
+from apps_ci.names import CACHED_VERSION_FILE_NAME
+from apps_validation.json_schema_utils import VERSION_VALIDATION_SCHEMA
 from catalog_reader.app import get_app_version_details as get_catalog_app_version_details
+from catalog_reader.questions import normalize_questions
 
 from middlewared.plugins.update_.utils import can_update
+from middlewared.service import CallError
 from middlewared.utils import sw_info
 
 
 RE_VERSION_PATTERN = re.compile(r'(\d{2}\.\d{2}(?:\.\d)*)')  # We are only interested in XX.XX here
 
 
-def get_item_default_values(version_details: dict) -> dict:
+def get_app_default_values(version_details: dict) -> dict:
     # FIXME: This needs to be done obviously
     return {}
     return construct_schema(version_details, {}, False)['new_values']
@@ -80,5 +88,41 @@ def minimum_scale_version_check_update(version_details: dict) -> dict:
 
 def get_app_version_details(version_path: str, questions_context: dict) -> dict:
     return minimum_scale_version_check_update(get_catalog_app_version_details(version_path, questions_context, {
-        'default_values_callable': get_item_default_values,
+        'default_values_callable': get_app_default_values,
     }))
+
+
+def get_app_details(app_location: str, app_data: dict, questions_context: dict) -> dict:
+    app_name = os.path.basename(app_location)
+    app_data['versions'] = retrieve_cached_versions_data(os.path.join(app_location, CACHED_VERSION_FILE_NAME), app_name)
+
+    # At this point, we have cached versions and apps data - now we want to do the following:
+    # 1) Update location in each version entry
+    # 2) Make sure default values have been normalised
+    # 3) Normalise questions
+    for version_name, version_data in app_data['versions'].items():
+        minimum_scale_version_check_update(version_data)
+        version_data.update({
+            'location': os.path.join(app_location, version_name),
+            'values': get_app_default_values(version_data),
+        })
+        normalize_questions(version_data, questions_context)
+
+    return app_data
+
+
+def retrieve_cached_versions_data(version_path: str, app_name: str) -> dict:
+    try:
+        with open(version_path, 'r') as f:
+            data = json.loads(f.read())
+            jsonschema.validate(data, VERSION_VALIDATION_SCHEMA)
+    except FileNotFoundError:
+        raise CallError(f'Unable to locate {app_name!r} versions', errno=errno.ENOENT)
+    except IsADirectoryError:
+        raise CallError(f'{version_path!r} must be a file')
+    except json.JSONDecodeError:
+        raise CallError(f'Unable to parse {version_path!r} file')
+    except jsonschema.ValidationError as e:
+        raise CallError(f'Unable to validate {version_path!r} file: {e}')
+    else:
+        return data
