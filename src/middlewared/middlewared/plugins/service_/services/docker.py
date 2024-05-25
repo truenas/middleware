a1585e8c21fb0d45594a1fc5a30ac40999498aac
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-25 22:29:55 +0500

    Manage state of docker in different lifecycle based hooks

--- src/middlewared/middlewared/plugins/service_/services/docker.py
+++ src/middlewared/middlewared/plugins/service_/services/docker.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from middlewared.plugins.docker.state_utils import Status
+
 from .base import SimpleService
 
 
@@ -9,9 +11,8 @@ class DockerService(SimpleService):
     systemd_unit = 'docker'
 
     async def before_start(self):
-        # TODO: Add status updates here and start checks
-        # FIXME: We should have alerts as well in case of failure to validate
-        await self.middleware.call('docker.setup.validate_fs')
+        await self.middleware.call('docker.state.set_status', Status.INITIALIZING.value)
+        await self.middleware.call('docker.state.before_start_check')
         for key, value in (
             ('vm.panic_on_oom', 0),
             ('vm.overcommit_memory', 1),
@@ -19,17 +20,28 @@ class DockerService(SimpleService):
             await self.middleware.call('sysctl.set_value', key, value)
 
     async def start(self):
-        await super().start()
-        timeout = 40
-        # First time when docker is started, it takes a bit more time to initialise itself properly
-        # and we need to have sleep here so that after start is called post_start is not dismissed
-        while timeout > 0:
-            if not await self.middleware.call('service.started', 'docker'):
-                await asyncio.sleep(2)
-                timeout -= 2
-            else:
-                break
+        try:
+            await super().start()
+            timeout = 40
+            # First time when docker is started, it takes a bit more time to initialise itself properly
+            # and we need to have sleep here so that after start is called post_start is not dismissed
+            while timeout > 0:
+                if not await self.middleware.call('service.started', 'docker'):
+                    await asyncio.sleep(2)
+                    timeout -= 2
+                else:
+                    break
+        finally:
+            asyncio.get_event_loop().call_later(
+                2,
+                lambda: self.middleware.create_task(self.middleware.call('docker.state.after_start_check')),
+            )
+
+    async def after_start(self):
+        await self.middleware.call('docker.state.set_status', Status.RUNNING.value)
 
     async def before_stop(self):
-        # TODO: Add status updates here and any other custom stuff which might need to be done
-        pass
+        await self.middleware.call('docker.state.set_status', Status.STOPPING.value)
+
+    async def after_stop(self):
+        await self.middleware.call('docker.state.set_status', Status.STOPPED.value)
