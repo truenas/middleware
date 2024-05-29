573846da20f296ee1b36f2914a4c800f7140f692
caleb <yocalebo@gmail.com>
2024-05-29 17:01:53 -0400

    update unit tests

--- src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
+++ src/middlewared/middlewared/pytest/unit/common/smart/test_smartctl.py
@@ -29,6 +29,7 @@ async def test_get_disk__unknown_usb_bridge():
                 "hctl": "17:0:0:0",
                 "size": 10000831348736,
                 "mediasize": 10000831348736,
+                "vendor": "HGST",
                 "ident": "ZZBBBAAA",
                 "serial": "ZZBBBAAA",
                 "model": "USB MODEL",
@@ -61,6 +62,7 @@ async def test_get_disk__generic():
                 "driver": "sd",
                 "hctl": "17:0:0:0",
                 "size": 10000831348736,
+                "vendor": "HGST",
                 "mediasize": 10000831348736,
                 "ident": "ZZBBBAAA",
                 "serial": "ZZBBBAAA",
@@ -83,3 +85,40 @@ async def test_get_disk__generic():
         run.return_value = Mock(stdout="Everything is OK")
 
         assert await get_smartctl_args(context, "sda", "") == ["/dev/sda"]
+
+
+@pytest.mark.asyncio
+async def test_get_disk__nvme_behind_sd():
+    context = SMARTCTX(
+        devices={
+            "sda": {
+                "name": "sda",
+                "sectorsize": 4096,
+                "number": 2048,
+                "subsystem": "scsi",
+                "driver": "sd",
+                "hctl": "17:0:0:0",
+                "size": 10000831348736,
+                "vendor": "NVMe",
+                "mediasize": 10000831348736,
+                "ident": "ZZBBBAAA",
+                "serial": "ZZBBBAAA",
+                "model": "USB MODEL",
+                "descr": "USB MODEL",
+                "lunid": "5000cca251214158",
+                "bus": "scsi",
+                "type": "HDD",
+                "blocks": 19532873728,
+                "serial_lunid": "ZZBBBAAA_5000cca251214158",
+                "rotationrate": "7200",
+                "stripesize": None,
+                "parts": [],
+                "dif": False
+            },
+        },
+        enterprise_hardware=False,
+    )
+    with patch("middlewared.common.smart.smartctl.run") as run:
+        run.return_value = Mock(stdout="Everything is OK")
+
+        assert await get_smartctl_args(context, "sda", "") == ["/dev/sda", "-d", "nvme"]
