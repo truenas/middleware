1312184c0480b3bfe04c62e0210ee4c8d62f3cb6
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-09 00:12:43 +0500

    Set apps state to failed if mount/umount fails

--- src/middlewared/middlewared/plugins/service_/services/docker.py
+++ src/middlewared/middlewared/plugins/service_/services/docker.py
@@ -12,11 +12,20 @@ class DockerService(SimpleService):
 
     async def mount_umount_ix_apps(self, mount):
         if docker_ds := (await self.middleware.call('docker.config'))['dataset']:
-            if mount:
-                await self.middleware.call('zfs.dataset.mount', docker_ds, {'recursive': True, 'force_mount': True})
-            else:
-                await self.middleware.call('zfs.dataset.umount', docker_ds, {'force': True})
-            await self.middleware.call('catalog.sync')
+            try:
+                if mount:
+                    await self.middleware.call('zfs.dataset.mount', docker_ds, {'recursive': True, 'force_mount': True})
+                else:
+                    await self.middleware.call('zfs.dataset.umount', docker_ds, {'force': True})
+                await self.middleware.call('catalog.sync')
+            except Exception as e:
+                await self.middleware.call(
+                    'docker.state.set_status', Status.FAILED.value,
+                    f'Failed to {"mount" if mount else "umount"} {docker_ds!r}: {e}',
+                )
+                self.middleware.logger.error(
+                    'Failed to %s %r', 'mount' if mount else 'umount', docker_ds, exc_info=True,
+                )
 
     async def before_start(self):
         await self.middleware.call('docker.state.set_status', Status.INITIALIZING.value)
