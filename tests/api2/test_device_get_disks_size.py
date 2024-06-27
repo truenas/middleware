a28cc74b7aee246f0ad4f5e51c6c4c761c8146bc
Andrew Walker <awalker@ixsystems.com>
2024-06-27 11:55:50 -0700

    Remove tests

--- tests/api2/test_device_get_disks_size.py
+++
@@ -1,7 +0,0 @@
-from middlewared.test.integration.utils import call, ssh
-
-
-def test_device_get_disks_size():
-    boot_disk = call('boot.get_disks')[0]
-    fdisk_size = int(ssh(f'fdisk -s /dev/{boot_disk}').strip()) * 1024
-    assert call('device.get_disks')[boot_disk]['size'] == fdisk_size
