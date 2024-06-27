aeef47d5517cfd3f56a7b204893b901f3553cf68
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-27 23:25:54 +0500

    Add app.update endpoint

--- src/middlewared/middlewared/plugins/apps/crud.py
+++ src/middlewared/middlewared/plugins/apps/crud.py
@@ -1,6 +1,5 @@
 import contextlib
 import errno
-import os
 import shutil
 import textwrap
 
@@ -9,13 +8,12 @@ from middlewared.service import CallError, CRUDService, filterable, job
 from middlewared.utils import filter_list
 from middlewared.validators import Match, Range
 
-from .app_lifecycle_utils import add_context_to_values, update_app_config
-from .app_path_utils import get_installed_app_path
+from .app_lifecycle_utils import add_context_to_values, get_current_app_config, update_app_config
+from .app_path_utils import get_installed_app_path, get_installed_app_version_path
 from .app_query_utils import list_apps
 from .app_setup_utils import setup_install_app_dir
 from .app_utils import get_version_in_use_of_app
 from .compose_utils import compose_action
-from .utils import IX_APPS_MOUNT_PATH
 from .version_utils import get_latest_version_from_app_versions
 
 
@@ -77,11 +75,11 @@ class AppService(CRUDService):
         app_version_details = complete_app_details['versions'][version]
         self.middleware.call_sync('catalog.version_supported_error_check', app_version_details)
 
-        app_dir = os.path.join(IX_APPS_MOUNT_PATH, 'app_configs', app_name)
         # The idea is to validate the values provided first and if it passes our validation test, we
         # can move forward with setting up the datasets and installing the catalog item
         new_values, context = self.middleware.call_sync(
-            'app.schema.normalise_and_validate_values', app_version_details, data['values'], False, app_dir
+            'app.schema.normalise_and_validate_values', app_version_details, data['values'], False,
+            get_installed_app_path(app_name)
         )
 
         job.set_progress(25, 'Initial Validation completed')
@@ -105,6 +103,40 @@ class AppService(CRUDService):
         else:
             job.set_progress(100, f'{data["app_name"]!r} installed successfully')
             return self.get_instance__sync(app_name)
+
+    @accepts(
+        Str('app_name'),
+        Dict(
+            'app_update',
+            Dict('values', additional_attrs=True, private=True),
+        )
+    )
+    @job(lock=lambda args: f'app_update_{args[0]}')
+    def do_update(self, job, app_name, data):
+        app = self.get_instance__sync(app_name)
+        config = get_current_app_config(app_name, app['version'])
+        config.update(data['values'])
+        # We use update=False because we want defaults to be populated again if they are not present in the payload
+        # Why this is not dangerous is because the defaults will be added only if they are not present/configured for
+        # the app in question
+        app_version_details = self.middleware.call_sync(
+            'catalog.app_version_details', get_installed_app_version_path(app_name, app['version'])
+        ) | {'catalog_app_last_updated': app['catalog_app_last_updated']}  # FIXME: We should already have this
+
+        new_values, context = self.middleware.call_sync(
+            'app.schema.normalise_and_validate_values', app_version_details, data['values'], False,
+            get_installed_app_path(app_name),
+        )
+
+        job.set_progress(25, 'Initial Validation completed')
+
+        new_values = add_context_to_values(app_name, new_values, update=True)
+        update_app_config(app_name, app['version'], new_values)
+        job.set_progress(60, 'Configuration updated, updating docker resources')
+        compose_action(app_name, app['version'], 'up', force_recreate=True, remove_orphans=True)
+
+        job.set_progress(100, f'Update completed for {app_name!r}')
+        return self.get_instance__sync(app_name)
 
     def delete_internal(self, app_name, version):
         compose_action(app_name, version, 'down', remove_orphans=True)
