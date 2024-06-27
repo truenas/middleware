113d70cd0fd1aa1c552f11308ee57eb88cca0f5a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-27 13:30:59 +0500

    Add basic delete endpoint in place

--- src/middlewared/middlewared/plugins/apps/app_utils.py
+++ src/middlewared/middlewared/plugins/apps/app_utils.py
@@ -18,4 +18,4 @@ def get_version_in_use_of_app(app_name: str) -> str:
     # Raises an Index error if no versions are found
     # This will treat the latest version available as the current one which is being used
     versions_path = Path(get_installed_app_versions_dir_path(app_name))
-    return sorted([Version(v) for v in versions_path.iterdir() if v.is_dir()])[-1].__str__()
+    return sorted([Version(v.name) for v in versions_path.iterdir() if v.is_dir()])[-1].__str__()
