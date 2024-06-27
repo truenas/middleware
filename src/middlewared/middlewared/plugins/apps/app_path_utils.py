568dbe11e2bbb6f51b99034ff9f55bcb58f94c62
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-27 11:36:23 +0500

    Add util to get current active version of an app

--- src/middlewared/middlewared/plugins/apps/app_path_utils.py
+++ src/middlewared/middlewared/plugins/apps/app_path_utils.py
@@ -7,8 +7,16 @@ def get_app_parent_config_path() -> str:
     return os.path.join(IX_APPS_MOUNT_PATH, 'app_configs')
 
 
+def get_installed_app_path(app_name: str) -> str:
+    return os.path.join(get_app_parent_config_path(), app_name)
+
+
+def get_installed_app_versions_dir_path(app_name: str) -> str:
+    return os.path.join(get_installed_app_path(app_name), 'versions')
+
+
 def get_installed_app_version_path(app_name: str, version: str) -> str:
-    return os.path.join(get_app_parent_config_path(), app_name, 'versions', version)
+    return os.path.join(get_installed_app_versions_dir_path(app_name), version)
 
 
 def get_installed_app_config_path(app_name: str, version: str) -> str:
