10fb897fe3e80016a7e51895382283cda3c8f62a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-27 12:18:05 +0500

    Switch to master for catalog

--- src/middlewared/middlewared/plugins/catalog/utils.py
+++ src/middlewared/middlewared/plugins/catalog/utils.py
@@ -6,7 +6,7 @@ from middlewared.utils import MIDDLEWARE_RUN_DIR
 OFFICIAL_ENTERPRISE_TRAIN = 'enterprise'
 OFFICIAL_LABEL = 'TRUENAS'
 OFFICIAL_CATALOG_REPO = 'https://github.com/truenas/apps'
-OFFICIAL_CATALOG_BRANCH = 'main'  # FIXME: Change this when perm issues for master have been resolved
+OFFICIAL_CATALOG_BRANCH = 'master'
 TMP_IX_APPS_CATALOGS = os.path.join(MIDDLEWARE_RUN_DIR, 'ix-apps/catalogs')
 
 
