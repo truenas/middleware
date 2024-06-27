81e7fb3fa07cdf2f61b4ab6adf4ad3b5327af554
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-06-27 21:05:42 +0500

    Save app's metadata when installing app

--- src/middlewared/middlewared/plugins/apps/app_setup_utils.py
+++ src/middlewared/middlewared/plugins/apps/app_setup_utils.py
@@ -1,10 +1,20 @@
 import os
 import shutil
+import yaml
 
-from .app_path_utils import get_app_parent_config_path, get_installed_app_version_path
+from .app_path_utils import get_app_parent_config_path, get_installed_app_metadata_path, get_installed_app_version_path
 
 
-def setup_install_app_dir(app_name: str, to_install_app_location: str):
+def setup_install_app_dir(
+    app_name: str, to_install_app_location: str, to_install_app_name: str, to_install_app_train: str,
+):
     os.makedirs(os.path.join(get_app_parent_config_path(), app_name, 'versions'), exist_ok=True)
     to_install_app_version = os.path.basename(to_install_app_location)
     shutil.copytree(to_install_app_location, get_installed_app_version_path(app_name, to_install_app_version))
+
+    # Now that we have copied the app version to the app dir, we will now update the app config
+    with open(get_installed_app_metadata_path(app_name), 'w') as f:
+        f.write(yaml.safe_dump({
+            'app_name': to_install_app_name,
+            'app_train': to_install_app_train,
+        }))
