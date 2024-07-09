7ccdaa9f63beb676d3adfaad0d4ab38d0937a7d3
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-09 12:58:24 +0500

    Add a context manager to manage upgrade process

--- src/middlewared/middlewared/plugins/apps/ix_apps/upgrade.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/upgrade.py
@@ -1,12 +1,19 @@
+import contextlib
 import shutil
 
 from .metadata import update_app_metadata
 from .path import get_installed_app_version_path
 
 
+@contextlib.contextmanager
 def upgrade_config(app_name: str, upgrade_version: dict):
     version_path = get_installed_app_version_path(app_name, upgrade_version['version'])
     shutil.rmtree(version_path, ignore_errors=True)
     shutil.copytree(upgrade_version['location'], version_path)
-    # Now that we have copied over version config, we need to update metadata to reflect the app has upgraded
-    update_app_metadata(app_name, upgrade_version)
+    try:
+        yield
+    except Exception:
+        shutil.rmtree(version_path, ignore_errors=True)
+        raise
+    else:
+        update_app_metadata(app_name, upgrade_version)
