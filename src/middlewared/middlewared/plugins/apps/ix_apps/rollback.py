922b83a90038284182cba4a72e18b05ccd7120e7
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-09 13:47:29 +0500

    Add util to remove newer versions of an app then it is currently on

--- src/middlewared/middlewared/plugins/apps/ix_apps/rollback.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/rollback.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from pkg_resources import parse_version
 
@@ -15,3 +16,16 @@ def get_rollback_versions(app_name: str, current_version: str) -> list[str]:
             rollback_versions.append(entry.name)
 
     return sorted(rollback_versions, key=parse_version)
+
+
+def clean_newer_versions(app_name: str, current_version: str):
+    """
+    Any versions above current_version will be removed from app's config
+    """
+    with os.scandir(get_installed_app_versions_dir_path(app_name)) as scan:
+        for entry in filter(
+            lambda e: e.name != current_version and e.is_dir() and RE_VERSION.findall(e.name) and (
+                parse_version(e.name) > parse_version(current_version)
+            ), scan
+        ):
+            shutil.rmtree(entry.path, ignore_errors=True)
