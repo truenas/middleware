12949f38700b36c6b018725cb4eae44aba075d75
Logan Cary <logan.cary@ixsystems.com>
2024-07-12 09:07:17 -0400

    remove redundant `&& echo`

--- tests/api2/test_system_vendor.py
+++ tests/api2/test_system_vendor.py
@@ -6,8 +6,8 @@ SENTINEL_FILE_PATH = "/data/.vendor"
 
 def test_name_is_none():
     # /data/.vendor should not exist
-    output = ssh(f"test -e {SENTINEL_FILE_PATH} && echo {SENTINEL_FILE_PATH}", check=False)
-    assert SENTINEL_FILE_PATH not in output
+    file_exists = ssh(f"test -e {SENTINEL_FILE_PATH}", check=False, complete_response=True)["result"]
+    assert not file_exists
 
     # system.vendor.name should successfully return None
     vendor_name = call("system.vendor.name")
