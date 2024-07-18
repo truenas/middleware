29acaac13e33a68dd9a3ef85e8a3dd855e31b629
caleb <yocalebo@gmail.com>
2024-07-18 15:01:50 -0400

    fix failing test

--- tests/api2/test_nfs_share_crud_roles.py
+++ tests/api2/test_nfs_share_crud_roles.py
@@ -14,10 +14,8 @@ def test_read_role_cant_write(unprivileged_user_fixture, role):
     common_checks(unprivileged_user_fixture, "sharing.nfs.create", role, False)
     common_checks(unprivileged_user_fixture, "sharing.nfs.update", role, False)
     common_checks(unprivileged_user_fixture, "sharing.nfs.delete", role, False)
-
     common_checks(unprivileged_user_fixture, "nfs.get_nfs3_clients", role, False)
     common_checks(unprivileged_user_fixture, "nfs.get_nfs4_clients", role, False)
-    common_checks(unprivileged_user_fixture, "nfs.add_principal", role, False)
 
 
 @pytest.mark.parametrize("role", ["SHARING_WRITE", "SHARING_NFS_WRITE"])
@@ -25,11 +23,8 @@ def test_write_role_can_write(unprivileged_user_fixture, role):
     common_checks(unprivileged_user_fixture, "sharing.nfs.create", role, True)
     common_checks(unprivileged_user_fixture, "sharing.nfs.update", role, True)
     common_checks(unprivileged_user_fixture, "sharing.nfs.delete", role, True)
-
     common_checks(unprivileged_user_fixture, "nfs.get_nfs3_clients", role, True, valid_role_exception=False)
     common_checks(unprivileged_user_fixture, "nfs.get_nfs4_clients", role, True, valid_role_exception=False)
-    common_checks(unprivileged_user_fixture, "nfs.add_principal", role, True)
-
     common_checks(
         unprivileged_user_fixture, "service.start", role, True, method_args=["nfs"], valid_role_exception=False
     )
