24f2fe7cbe03a61817f89f92eeb4a97646b2ec4a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-19 14:59:36 +0500

    Complete implementation for listing k8s backups

--- src/middlewared/middlewared/plugins/kubernetes_to_docker/list_k8s_backups.py
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/list_k8s_backups.py
@@ -3,7 +3,7 @@ import os
 from middlewared.schema import accepts, Dict, returns, Str
 from middlewared.service import Service
 
-from .list_utils import get_backup_dir
+from .list_utils import get_backup_dir, K8s_BACKUP_NAME_PREFIX
 from .utils import get_k8s_ds
 
 
@@ -11,9 +11,14 @@ class K8stoDockerMigrationService(Service):
 
     class Config:
         namespace = 'k8s_to_docker'
+        cli_namespace = 'k8s_to_docker'
 
     @accepts(Str('kubernetes_pool'))
-    @returns(Dict('backups', additional_attrs=True))
+    @returns(Dict(
+        'backups',
+        Str('error', null=True),
+        Dict('backups', additional_attrs=True),
+    ))
     def list_backups(self, kubernetes_pool):
         """
         List existing kubernetes backups
@@ -29,5 +34,39 @@ class K8stoDockerMigrationService(Service):
         backup_base_dir = get_backup_dir(k8s_ds)
         if not os.path.exists(backup_base_dir):
             return backup_config | {'error': f'Unable to locate {backup_base_dir!r} backups directory'}
+
+        backups = backup_config['backups']
+        snapshots = self.middleware.call_sync(
+            'zfs.snapshot.query', [['name', '^', f'{k8s_ds}@{K8s_BACKUP_NAME_PREFIX}']], {'select': ['name']}
+        )
+        releases_datasets = set(
+            ds['id'].split('/', 3)[-1].split('/', 1)[0]
+            for ds in self.middleware.call_sync('zfs.dataset.get_instance', f'{k8s_ds}/releases')['children']
+        )
+
+        for snapshot in snapshots:
+            backup_name = snapshot['name'].split('@', 1)[-1].split(K8s_BACKUP_NAME_PREFIX, 1)[-1]
+            backup_path = os.path.join(backup_base_dir, backup_name)
+            if not os.path.exists(backup_path):
+                continue
+
+            backup_data = {
+                'name': backup_name,
+                'releases': [],
+                'snapshot_name': snapshot['name'],
+                'created_on': self.middleware.call_sync(
+                    'zfs.snapshot.get_instance', snapshot['name']
+                )['properties']['creation']['parsed'],
+                'backup_path': backup_path,
+            }
+
+            with os.scandir(backup_path) as entries:
+                for release in filter(lambda r: r.name in releases_datasets, entries):
+                    backup_data['releases'].append(release.name)
+
+            for release in filter(lambda r: r in releases_datasets, os.listdir(backup_path)):
+                backup_data['releases'].append(release)
+
+            backups[backup_name] = backup_data
 
         return backup_config
--- src/middlewared/middlewared/plugins/kubernetes_to_docker/list_utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/list_utils.py
@@ -1,5 +1,8 @@
 import os
 
 
+K8s_BACKUP_NAME_PREFIX = 'ix-applications-backup-'
+
+
 def get_backup_dir(k8s_ds: str) -> str:
     return os.path.join('/mnt', k8s_ds, 'backups')
