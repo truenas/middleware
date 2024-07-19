b9424efc3179f425f8a9e6a61c50e700bd69558a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-19 14:28:11 +0500

    Add basic method to list k8s backups

--- src/middlewared/middlewared/plugins/kubernetes_to_docker/list_k8s_backups.py
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/list_k8s_backups.py
@@ -1,7 +1,26 @@
+from middlewared.schema import accepts, Dict, returns, Str
 from middlewared.service import Service
+
+from .utils import get_k8s_ds
 
 
 class K8stoDockerMigrationService(Service):
 
     class Config:
         namespace = 'k8s_to_docker'
+
+    @accepts(Str('kubernetes_pool'))
+    @returns(Dict('backups', additional_attrs=True))
+    def list_backups(self, kubernetes_pool):
+        """
+        List existing kubernetes backups
+        """
+        backup_config = {
+            'error': None,
+            'backups': {},
+        }
+        k8s_ds = get_k8s_ds(kubernetes_pool)
+        if not self.middleware.call_sync('pool.dataset.query', [['id', '=', k8s_ds]]):
+            return backup_config | {'error': f'Unable to locate {k8s_ds!r} dataset'}
+
+        return backup_config
