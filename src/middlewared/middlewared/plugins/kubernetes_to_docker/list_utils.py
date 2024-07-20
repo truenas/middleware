4457249491496f2df279943c97288a95a2f14dd2
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-20 12:29:47 +0500

    Add logic to read namespace of chart release and see if it is valid

--- src/middlewared/middlewared/plugins/kubernetes_to_docker/list_utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/list_utils.py
@@ -1,6 +1,13 @@
 import os
 
+import yaml
 
+from catalog_reader.train_utils import get_train_path
+
+from .yaml import SerializedDatesFullLoader
+
+
+HELM_SECRET_PREFIX = 'sh.helm.release'
 K8s_BACKUP_NAME_PREFIX = 'ix-applications-backup-'
 
 
@@ -8,5 +15,24 @@ def get_backup_dir(k8s_ds: str) -> str:
     return os.path.join('/mnt', k8s_ds, 'backups')
 
 
+def get_release_metadata(release_path: str) -> dict:
+    try:
+        with open(os.path.join(release_path, 'namespace.yaml')) as f:
+            return yaml.load(f.read(), Loader=SerializedDatesFullLoader)
+    except (FileNotFoundError, yaml.YAMLError):
+        return {}
+
+
 def chart_release_can_be_migrated(release_name: str, release_path: str, catalog_path: str, apps_mapping: dict) -> bool:
+    if not (release_metadata := get_release_metadata(release_path)) or not all(
+        k in release_metadata.get('metadata', {}).get('labels', {})
+        for k in ('catalog', 'catalog_branch', 'catalog_train')
+    ):
+        return False
+
+    metadata_labels = release_metadata['metadata']['labels']
+    if metadata_labels['catalog'] != 'TRUENAS' or metadata_labels['catalog_branch'] != 'master':
+        return False
+
+    train_path = get_train_path(catalog_path)
     return False
