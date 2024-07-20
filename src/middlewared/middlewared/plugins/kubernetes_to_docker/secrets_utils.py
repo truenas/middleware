888bda2d78bf0ec15eb68eba448d213b219adca4
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-20 15:16:22 +0500

    Add changes to read helm and app secrets and retrieve them

--- src/middlewared/middlewared/plugins/kubernetes_to_docker/secrets_utils.py
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/secrets_utils.py
@@ -1,13 +1,25 @@
+import binascii
+import contextlib
+import gzip
+import json
 import os
+from base64 import b64decode
+
+import yaml
+
+from .yaml import SerializedDatesFullLoader
 
 
 HELM_SECRET_PREFIX = 'sh.helm.release'
 
 
-def list_secrets(secrets_dir: str) -> dict[str, list[str]]:
+def list_secrets(secrets_dir: str) -> dict[str, dict[str, dict]]:
     secrets = {
-        'helm_secrets': [],
-        'release_secrets': [],
+        'helm_secret': {
+            'secret_name': None,
+            'name': None,
+        },
+        'release_secrets': {},
     }
     with os.scandir(secrets_dir) as it:
         for entry in it:
@@ -15,9 +27,32 @@ def list_secrets(secrets_dir: str) -> dict[str, list[str]]:
                 continue
 
             if entry.name.startswith(HELM_SECRET_PREFIX):
-                secrets['helm_secrets'].append(entry.name)
+                if secrets['helm_secret']['secret_name'] is None or entry.name > secrets['helm_secret']['secret_name']:
+                    secrets['helm_secret'] = {
+                        'secret_name': entry.name,
+                        **get_secret_contents(entry.path, True).get('release', {}),
+                    }
             else:
-                secrets['release_secrets'].append(entry.name)
+                secrets['release_secrets'][entry.name] = get_secret_contents(entry.path)
 
-    secrets['helm_secrets'].sort()
     return secrets
+
+
+def get_secret_contents(secret_path: str, helm_secret: bool = False) -> dict:
+    with open(secret_path, 'r') as f:
+        secret = yaml.load(f.read(), Loader=SerializedDatesFullLoader)
+
+    if isinstance(secret.get('data'), dict) is False:
+        return {}
+
+    contents = {}
+    for k, v in secret['data'].items():
+        with contextlib.suppress(binascii.Error, gzip.BadGzipFile, KeyError):
+            if helm_secret:
+                v = json.loads(gzip.decompress(b64decode(b64decode(v))).decode())
+            else:
+                v = b64decode(v).decode()
+
+            contents[k] = v
+
+    return contents
