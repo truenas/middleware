5316898f82e00c1ae1b99410fa146afaf9889418
Logan Cary <logan.cary@ixsystems.com>
2024-07-22 15:01:09 -0400

    NAS-129799 / 24.10 / Add service to start websocat on HexOS builds (#14009)
    * script to start vendor service
    
    * define `system.vendor` namespace, remove unneeded logic
    
    * Move vendor plugin to valid location
    
    * set `cli_private` for vendor plugin
    
    * testing
    
    * done testing
    
    * optimize by replacing the hash subprocess
    
    * remove redundant `&& echo`
    
    * add `Restart` params, vendor-specified envvars file
    
    * cut down the `TimeoutStartSec` property
    
    * more specific `except` cases
    
    * use `dotenv`
    
    * must pass the envvars to the service
    
    * log unexpected errors, simplify `main`
    
    * remove `sys.exit()`
    
    * Revert "remove `sys.exit()`" at request of @yocalebo
    
    This reverts commit 16993e45fe8a4ed48b52e1d9790e19b397d7bac4.
    
    * make sure system is ready
    
    * address review
    
    * address @yocalebo

---
+++ src/middlewared/middlewared/plugins/system_vendor/vendor.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+from middlewared.api import api_method
+from middlewared.api.current import VendorNameArgs, VendorNameResult, UnvendorArgs, UnvendorResult
+from middlewared.service import Service
+
+
+SENTINEL_FILE_PATH = '/data/.vendor'
+
+
+class VendorService(Service):
+
+    class Config:
+        namespace = 'system.vendor'
+        cli_private = True
+
+    @api_method(VendorNameArgs, VendorNameResult, private=True)
+    def name(self) -> str | None:
+        try:
+            with open(SENTINEL_FILE_PATH, 'r') as file:
+                return json.load(file).get('name') or None  # Don't return an empty string.
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            self.logger.exception('Can\'t retrieve vendor name: %r is not proper JSON format', SENTINEL_FILE_PATH)
+        except Exception:
+            self.logger.exception('Unexpected error while reading %r', SENTINEL_FILE_PATH)
+
+    @api_method(UnvendorArgs, UnvendorResult, private=True)
+    def unvendor(self):
+        try:
+            os.remove(SENTINEL_FILE_PATH)
+        except FileNotFoundError:
+            pass
+        except Exception:
+            self.logger.exception('Unexpected error attempting to remove %r', SENTINEL_FILE_PATH)
