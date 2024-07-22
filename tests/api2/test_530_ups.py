4482243f4635144ae321ccf23458e76255045e68
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2024-07-22 16:56:11 +0100

    NAS-129207 / 24.10 / Utilize NUT dummy-ups to facilitate UPS event CI testing (#14049)
    * Convert UPS tests that use the REST interface to WS
    
    * Add tests that use the dummy-ups driver to generate alerts

--- tests/api2/test_530_ups.py
+++ tests/api2/test_530_ups.py
@@ -1,225 +1,236 @@
-#!/usr/bin/env python3
-# License: BSD
+import os
+from tempfile import NamedTemporaryFile
+from time import sleep
 
 import pytest
-import os
-import sys
-apifolder = os.getcwd()
-sys.path.append(apifolder)
-from functions import GET, PUT, POST
 
-first_ups_list = [
-    'rmonitor',
-    'mode',
-    'shutdown',
-    'port',
-    'remotehost',
-    'identifier',
-    'driver',
-    'monpwd'
-]
+from assets.websocket.service import ensure_service_enabled, ensure_service_started
+from auto_config import password, user
+from functions import send_file
 
-second_ups_list = [
-    'rmonitor',
-    'mode',
-    'shutdown',
-    'port',
-    'identifier',
-    'monpwd'
-]
+from middlewared.test.integration.utils import call, mock, ssh
+from middlewared.test.integration.utils.client import truenas_server
 
+DUMMY_FAKEDATA_DEV = '/tmp/fakedata.dev'
+SHUTDOWN_MARKER_FILE = '/tmp/is_shutdown'
 
-def test_01_get_ups_service_id():
-    global ups_id
-    results = GET('/service/?service=ups')
-    assert results.status_code == 200, results.text
-    assert results.json()[0]['state'] == 'STOPPED', results.text
-    assert results.json()[0]['enable'] is False, results.text
-    ups_id = results.json()[0]['id']
+first_ups_payload = {
+    'rmonitor': True,
+    'mode': 'MASTER',
+    'shutdown': 'BATT',
+    'port': '655',
+    'remotehost': '127.0.0.1',
+    'identifier': 'ups',
+    'driver': 'usbhid-ups$PROTECT NAS',
+    'monpwd': 'mypassword'
+}
 
+second_ups_payload = {
+    'rmonitor': False,
+    'mode': 'SLAVE',
+    'shutdown': 'LOWBATT',
+    'port': '65535',
+    'identifier': 'foo',
+    'monpwd': 'secondpassword'
+}
 
-def test_02_Enabling_UPS_Service_at_boot():
-    results = PUT('/service/id/ups/', {'enable': True})
-    assert results.status_code == 200, results.text
-
-
-def test_03_look_if_UPS_service_is_enable():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['enable'] is True, results.text
+default_dummy_data = {
+    'battery.charge': 100,
+    'driver.parameter.pollinterval': 2,
+    'input.frequency': 49.9,
+    'input.frequency.nominal': 50.0,
+    'input.voltage': 230,
+    'input.voltage.nominal': 240,
+    'ups.status': 'OL',
+    'ups.timer.shutdown': -1,
+    'ups.timer.start': -1,
+}
 
 
-def test_04_Set_UPS_options():
-    global payload, results
+def get_service_state():
+    return call('service.query', [['service', '=', 'ups']], {'get': True})
+
+
+def remove_file(filepath):
+    ssh(f'rm {filepath}', check=False)
+
+
+def did_shutdown():
+    return ssh(f'cat {SHUTDOWN_MARKER_FILE}', check=False) == "done\n"
+
+
+def write_fake_data(data=None):
+    data = data or {}
+    all_data = default_dummy_data | data
+    with NamedTemporaryFile() as f:
+        for k, v in all_data.items():
+            f.write(f'{k}: {v}\n'.encode('utf-8'))
+        f.flush()
+        os.fchmod(f.fileno(), 0o644)
+        results = send_file(f.name, DUMMY_FAKEDATA_DEV, user, password, truenas_server.ip)
+        assert results['result'], str(results['output'])
+
+
+def wait_for_alert(klass, retries=10):
+    assert retries > 0
+    while retries:
+        alerts = call('alert.list')
+        for alert in alerts:
+            if alert['klass'] == klass:
+                return alert
+        sleep(1)
+        retries -= 1
+
+
+@pytest.fixture(scope='module')
+def ups_running():
+    with ensure_service_enabled('ups'):
+        with ensure_service_started('ups'):
+            yield
+
+
+@pytest.fixture(scope='module')
+def dummy_ups_driver_configured():
+    write_fake_data()
+    remove_file(SHUTDOWN_MARKER_FILE)
+    old_config = call('ups.config')
+    del old_config['complete_identifier']
+    del old_config['id']
     payload = {
-        'rmonitor': True,
         'mode': 'MASTER',
-        'shutdown': 'BATT',
-        'port': '655',
-        'remotehost': '127.0.0.1',
-        'identifier': 'ups',
-        'driver': 'usbhid-ups$PROTECT NAS',
-        'monpwd': 'mypassword'
+        'driver': 'dummy-ups',
+        'port': DUMMY_FAKEDATA_DEV,
+        'description': 'dummy-ups in dummy-once mode',
+        'shutdowncmd': f'echo done > {SHUTDOWN_MARKER_FILE}'
     }
-    results = PUT('/ups/', payload)
-    assert results.status_code == 200, results.text
+    with mock('ups.driver_choices', return_value={'dummy-ups': 'Driver for multi-purpose UPS emulation',
+                                                  'usbhid-ups$PROTECT NAS': 'AEG Power Solutions ups 3 PROTECT NAS (usbhid-ups)'}):
+        call('ups.update', payload)
+        try:
+            yield
+        finally:
+            call('ups.update', old_config)
+            remove_file(SHUTDOWN_MARKER_FILE)
+            remove_file(DUMMY_FAKEDATA_DEV)
 
 
-@pytest.mark.parametrize('data', first_ups_list)
-def test_05_look_at_UPS_options_output_of_(data):
-    assert payload[data] == results.json()[data], results.text
+def test__enable_ups_service():
+    results = get_service_state()
+    assert results['state'] == 'STOPPED', results
+    assert results['enable'] is False, results
+    call('service.update', 'ups', {'enable': True})
+    results = get_service_state()
+    assert results['enable'] is True, results
 
 
-def test_06_starting_ups_service():
-    payload = {
-        "service": "ups",
-    }
-    results = POST('/service/start/', payload)
-    assert results.status_code == 200, results.text
+def test__set_ups_options():
+    results = call('ups.update', first_ups_payload)
+    for data in first_ups_payload.keys():
+        assert first_ups_payload[data] == results[data], results
 
 
-def test_07_look_UPS_service_status_is_running():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['state'] == 'RUNNING', results.text
+def test__start_ups_service():
+    call('service.start', 'ups')
+    results = get_service_state()
+    assert results['state'] == 'RUNNING', results
 
 
-def test_08_get_API_reports_UPS_configuration_as_saved():
-    global results
-    results = GET('/ups/')
-    assert results.status_code == 200, results.text
+def test__get_reports_configuration_as_saved():
+    results = call('ups.config')
+    for data in first_ups_payload.keys():
+        assert first_ups_payload[data] == results[data], results
 
 
-@pytest.mark.parametrize('data', first_ups_list)
-def test_09_look_API_reports_UPS_configuration_of_(data):
-    assert payload[data] == results.json()[data], results.text
-
-
-def test_10_change_UPS_options_while_service_is_running():
-    global payload, results
+def test__change_ups_options_while_service_is_running():
     payload = {
         'port': '65545',
         'identifier': 'boo'
     }
-    results = PUT('/ups/', payload)
-    assert results.status_code == 200, results.text
+    results = call('ups.update', payload)
+    for data in ['port', 'identifier']:
+        assert payload[data] == results[data], results
+    results = call('ups.config')
+    for data in ['port', 'identifier']:
+        assert payload[data] == results[data], results
 
 
-@pytest.mark.parametrize('data', ['port', 'identifier'])
-def test_11_look_at_UPS_options_output_of_(data):
-    assert payload[data] == results.json()[data], results.text
+def test__stop_ups_service():
+    results = get_service_state()
+    assert results['state'] == 'RUNNING', results
+    call('service.stop', 'ups')
+    results = get_service_state()
+    assert results['state'] == 'STOPPED', results
 
 
-def test_12_get_API_reports_UPS_configuration_as_saved():
-    global results
-    results = GET('/ups/')
-    assert results.status_code == 200, results.text
+def test__change_ups_options():
+    results = call('ups.update', second_ups_payload)
+    for data in second_ups_payload.keys():
+        assert second_ups_payload[data] == results[data], results
+    call('service.start', 'ups')
+    results = get_service_state()
+    assert results['state'] == 'RUNNING', results
+    results = call('ups.config')
+    for data in second_ups_payload.keys():
+        assert second_ups_payload[data] == results[data], results
 
 
-@pytest.mark.parametrize('data', ['port', 'identifier'])
-def test_13_look_API_reports_UPS_configuration_of_(data):
-    assert payload[data] == results.json()[data], results.text
+def test__get_ups_driver_choice():
+    results = call('ups.driver_choices')
+    assert isinstance(results, dict) is True, results
 
 
-def test_14_look_if_UPS_service_status_is_still_running():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['state'] == 'RUNNING', results.text
+def test__get_ups_port_choice():
+    results = call('ups.port_choices')
+    assert isinstance(results, list) is True, results
+    assert isinstance(results[0], str) is True, results
 
 
-def test_15_stop_ups_service():
-    payload = {
-        "service": "ups",
-    }
-    results = POST('/service/stop/', payload)
-    assert results.status_code == 200, results.text
+def test__disable_and_stop_ups_service():
+    call('service.update', 'ups', {'enable': False})
+    results = get_service_state()
+    assert results['enable'] is False, results
+    call('service.stop', 'ups')
+    results = get_service_state()
+    assert results['state'] == 'STOPPED', results
 
 
-def test_16_look_UPS_service_status_is_stopped():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['state'] == 'STOPPED', results.text
+def test__ups_online_to_online_lowbattery(ups_running, dummy_ups_driver_configured):
+    results = get_service_state()
+    assert results['state'] == 'RUNNING', results
+    sleep(2)
+    assert 'UPSBatteryLow' not in [alert['klass'] for alert in call('alert.list')]
+    write_fake_data({'battery.charge': 20, 'ups.status': 'OL LB'})
+    alert = wait_for_alert('UPSBatteryLow')
+    assert alert
+    assert 'battery.charge: 20' in alert['formatted'], alert
+    assert not did_shutdown()
 
 
-def test_17_Change_UPS_options():
-    global payload, results
-    payload = {
-        'rmonitor': False,
-        'mode': 'SLAVE',
-        'shutdown': 'LOWBATT',
-        'port': '65535',
-        'identifier': 'foo',
-        'monpwd': 'secondpassword'
-    }
-    results = PUT('/ups/', payload)
-    assert results.status_code == 200, results.text
+def test__ups_online_to_onbatt(ups_running, dummy_ups_driver_configured):
+    assert 'UPSOnBattery' not in [alert['klass'] for alert in call('alert.list')]
+    write_fake_data({'battery.charge': 40, 'ups.status': 'OB'})
+    alert = wait_for_alert('UPSOnBattery')
+    assert alert
+    assert 'battery.charge: 40' in alert['formatted'], alert
+    assert not did_shutdown()
 
 
-@pytest.mark.parametrize('data', second_ups_list)
-def test_18_look_at_change_UPS_options_output_of_(data):
-    assert payload[data] == results.json()[data], results.text
+def test__ups_onbatt_to_online(ups_running, dummy_ups_driver_configured):
+    assert 'UPSOnline' not in [alert['klass'] for alert in call('alert.list')]
+    write_fake_data({'battery.charge': 100, 'ups.status': 'OL'})
+    alert = wait_for_alert('UPSOnline')
+    assert alert
+    assert 'battery.charge: 100' in alert['formatted'], alert
+    assert not did_shutdown()
 
 
-def test_19_starting_ups_service():
-    payload = {
-        "service": "ups",
-    }
-    results = POST('/service/start/', payload)
-    assert results.status_code == 200, results.text
-
-
-def test_20_look_UPS_service_status_is_running():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['state'] == 'RUNNING', results.text
-
-
-def test_21_get_API_reports_UPS_configuration_as_changed():
-    global results
-    results = GET('/ups/')
-    assert results.status_code == 200, results.text
-
-
-@pytest.mark.parametrize('data', second_ups_list)
-def test_22_look_API_reports_UPS_configuration_of_(data):
-    assert payload[data] == results.json()[data], results.text
-
-
-def test_23_get_ups_driver_choice():
-    results = GET('/ups/driver_choices/')
-    assert results.status_code == 200, results.text
-    assert isinstance(results.json(), dict) is True, results.text
-    global ups_dc
-    ups_dc = results
-
-
-def test_24_get_ups_driver_choice():
-    results = GET('/ups/port_choices/')
-    assert results.status_code == 200, results.text
-    assert isinstance(results.json(), list) is True, results.text
-    assert isinstance(results.json()[0], str) is True, results.text
-
-
-def test_25_Disabling_UPS_Service():
-    results = PUT('/service/id/ups/', {'enable': False})
-    assert results.status_code == 200, results.text
-
-
-def test_26_Disabling_UPS_Service_at_boot():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['enable'] is False, results.text
-
-
-def test_27_stop_ups_service():
-    payload = {
-        "service": "ups",
-    }
-    results = POST('/service/stop/', payload)
-    assert results.status_code == 200, results.text
-
-
-def test_28_look_UPS_service_status_is_stopped():
-    results = GET(f'/service/id/{ups_id}/')
-    assert results.status_code == 200, results.text
-    assert results.json()['state'] == 'STOPPED', results.text
+def test__ups_online_to_onbatt_lowbattery(ups_running, dummy_ups_driver_configured):
+    assert 'UPSOnBattery' not in [alert['klass'] for alert in call('alert.list')]
+    write_fake_data({'battery.charge': 10, 'ups.status': 'OB LB'})
+    alert = wait_for_alert('UPSOnBattery')
+    assert alert
+    assert 'battery.charge: 10' in alert['formatted'], alert
+    alert = wait_for_alert('UPSBatteryLow')
+    assert alert
+    assert 'battery.charge: 10' in alert['formatted'], alert
+    assert did_shutdown()
