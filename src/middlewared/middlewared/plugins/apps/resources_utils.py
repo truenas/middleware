3fb5fcdd8a9d5bda36de488fa9ca0e4d19f9e056
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-23 12:03:58 +0500

    Add util function which normalizes gpu details and gives us the data we need to expose gpu resources

---
+++ src/middlewared/middlewared/plugins/apps/resources_utils.py
@@ -0,0 +1,52 @@
+def get_gpu_base_dict() -> dict:
+    return {
+        'vendor': '',
+        'description': '',
+        'error': None,
+        'vendor_specific_config': {},
+        'gpu_details': {},
+    }
+
+
+def get_normalized_gpu_choices(all_gpus_info: list[dict], nvidia_gpus: dict) -> list[dict]:
+    all_gpus_info = {gpu['addr']['pci_slot']: gpu for gpu in all_gpus_info}
+    gpus = []
+    for pci_slot, gpu_info in all_gpus_info.items():
+        gpu_config = get_gpu_base_dict() | {
+            'vendor': gpu_info['vendor'],
+            'description': gpu_info['description'],
+            'gpu_details': gpu_info,
+        }
+        if gpu_info['vendor'] == 'NVIDIA':
+            if pci_slot not in nvidia_gpus:
+                gpus.append(gpu_config | {
+                    'error': 'Unable to locate GPU details from procfs',
+                })
+                continue
+
+            nvidia_gpu = nvidia_gpus[pci_slot]
+            error = None
+            if not nvidia_gpu.get('gpu_uuid'):
+                error = 'GPU UUID not found'
+            elif '?' in nvidia_gpu['gpu_uuid']:
+                error = 'Malformed GPU UUID found'
+            if error:
+                gpus.append(gpu_config | {
+                    'error': error,
+                    'nvidia_gpu_details': nvidia_gpu,
+                })
+                continue
+
+            gpu_config.update({
+                'vendor_specific_config': {
+                    'uuid': nvidia_gpu['gpu_uuid'],
+                },
+                'description': nvidia_gpu.get('model') or gpu_config['description'],
+            })
+
+        if not gpu_info['available_to_host']:
+            gpus.append(gpu_config | {
+                'error': 'GPU not available to host',
+            })
+
+    return gpus
