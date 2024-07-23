109c2f6841fca5356a2837b5ab92f6a9c73bd56d
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-07-23 12:08:44 +0500

    Add public endpoint which exposes gpus which can be consumed by apps

--- src/middlewared/middlewared/plugins/apps/resources.py
+++ src/middlewared/middlewared/plugins/apps/resources.py
@@ -1,5 +1,9 @@
 from middlewared.schema import accepts, Dict, Int, List, Ref, returns, Str
-from middlewared.service import Service
+from middlewared.service import private, Service
+
+from middlewared.utils.gpu import get_nvidia_gpus
+
+from .resources_utils import get_normalized_gpu_choices
 
 
 class AppService(Service):
@@ -52,3 +56,24 @@ class AppService(Service):
             ip['address']: ip['address']
             for ip in await self.middleware.call('interface.ip_in_use', {'static': True, 'any': True})
         }
+
+    @accepts()
+    @returns(Dict('gpu_choices', additional_attrs=True))
+    async def gpu_choices(self):
+        """
+        Returns GPU choices which can be used by applications.
+        """
+        return {
+            gpu['description']: {
+                k: gpu[k] for k in ('vendor', 'description', 'vendor_specific_config')
+            }
+            for gpu in await self.gpu_choices_internal()
+            if not gpu['error']
+        }
+
+    @private
+    async def gpu_choices_internal(self):
+        return get_normalized_gpu_choices(
+            await self.middleware.call('device.get_gpus'),
+            await self.middleware.run_in_thread(get_nvidia_gpus),
+        )
