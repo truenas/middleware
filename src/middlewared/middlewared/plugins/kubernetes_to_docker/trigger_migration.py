07be176d06b609281e1501423d720472152d915a
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-08-06 08:56:54 +0500

    Bring back k8s db model

---
+++ src/middlewared/middlewared/plugins/kubernetes_to_docker/trigger_migration.py
@@ -0,0 +1,10 @@
+import middlewared.sqlalchemy as sa
+
+from middlewared.service import Service
+
+
+class KubernetesModel(sa.Model):
+    __tablename__ = 'services_kubernetes'
+
+    id = sa.Column(sa.Integer(), primary_key=True)
+    pool = sa.Column(sa.String(255), default=None, nullable=True)
