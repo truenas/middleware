b329c1e63b4b64ac9b3b924e8f0a25ce0ffad011
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-08-08 10:01:12 +0500

    Add basic validation for app.custom.create

--- src/middlewared/middlewared/plugins/apps/custom_app.py
+++ src/middlewared/middlewared/plugins/apps/custom_app.py
@@ -1,4 +1,4 @@
-from middlewared.service import Service
+from middlewared.service import Service, ValidationErrors
 
 
 class AppCustomService(Service):
@@ -6,3 +6,19 @@ class AppCustomService(Service):
     class Config:
         namespace = 'app.custom'
         private = True
+
+    def create(self, data, job=None):
+        """
+        Create a custom app.
+        """
+        verrors = ValidationErrors()
+        if not data.get('custom_compose'):
+            verrors.add('app_create.custom_compose', 'This field is required')
+
+        verrors.check()
+
+        # For debug purposes
+        job = job or type('dummy_job', (object,), {'set_progress': lambda *args: None})()
+
+        app_name = data['app_name']
+        compose_contents = data['custom_compose']
