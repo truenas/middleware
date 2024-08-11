00531461aff2f4c2668f081853f84773e9f5a3b9
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-08-11 20:11:00 +0500

    Add logic to retrieve docker exec command

--- src/middlewared/middlewared/main.py
+++ src/middlewared/middlewared/main.py
@@ -622,7 +622,7 @@ class ShellWorkerThread(threading.Thread):
         super(ShellWorkerThread, self).__init__(daemon=True)
 
     def get_command(self, username, as_root, options):
-        allowed_options = ('vm_id',)
+        allowed_options = ('vm_id', 'app')
         if all(options.get(k) for k in allowed_options):
             raise CallError(f'Only one option is supported from {", ".join(allowed_options)}')
 
@@ -630,6 +630,16 @@ class ShellWorkerThread(threading.Thread):
             command = [
                 '/usr/bin/virsh', '-c', 'qemu+unix:///system?socket=/run/truenas_libvirt/libvirt-sock',
                 'console', f'{options["vm_data"]["id"]}_{options["vm_data"]["name"]}'
+            ]
+
+            if not as_root:
+                command = ['/usr/bin/sudo', '-H', '-u', username] + command
+
+            return command, not as_root
+        elif options.get('app'):
+            command = [
+                '/usr/bin/docker', 'docker', 'exec', '-n', options['container_name'],
+                '-it', options.get('command', '/bin/bash'),
             ]
 
             if not as_root:
