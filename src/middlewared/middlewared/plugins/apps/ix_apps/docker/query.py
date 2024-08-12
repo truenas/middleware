e84577c0823451d122fd6a6f604d43d8877fef8f
Qubad786 <mrehanlm93@gmail.com>
2024-08-12 15:27:08 +0500

    Fix edge case when listing docker resources (#14188)

--- src/middlewared/middlewared/plugins/apps/ix_apps/docker/query.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/docker/query.py
@@ -1,3 +1,5 @@
+import docker.errors
+
 from collections import defaultdict
 from itertools import chain, repeat
 
@@ -5,6 +7,17 @@ from .utils import get_docker_client, PROJECT_KEY
 
 
 def list_resources_by_project(project_name: str | None = None) -> dict[str, dict[str, list]]:
+    retries = 2
+    while retries > 0:
+        try:
+            return list_resources_by_project_internal(project_name)
+        except docker.errors.NotFound:
+            retries -= 1
+            if retries == 0:
+                raise
+
+
+def list_resources_by_project_internal(project_name: str | None = None) -> dict[str, dict[str, list]]:
     with get_docker_client() as client:
         label_filter = {'label': f'{PROJECT_KEY}={project_name}' if project_name else PROJECT_KEY}
         containers = client.containers.list(all=True, filters=label_filter, sparse=False)
