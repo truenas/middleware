6be195f2523e2ec853e2ad5dedd5a9580766de2c
Aiden Baker <baker.aiden@protonmail.com>
2024-08-13 09:25:11 -0400

    Fixed newline

--- tests/api2/test_system_general.py
+++ tests/api2/test_system_general.py
@@ -2,6 +2,7 @@ from middlewared.test.integration.utils import call
 
 TIMEZONE = "America/New_York"
 
+
 def test_setting_timezone():
     assert TIMEZONE in call("system.general.timezone_choices")
     call("system.general.update", {"timezone": TIMEZONE})
