56535d4a1a49794a01a4319ff88182d99dd62d86
Aiden <53726630+aiden3c@users.noreply.github.com>
2024-08-14 09:19:48 -0400

    NAS-130573 / 25.04 / Update REST tests to use Websocket (#14199)

--- tests/api2/test_110_certificate.py
+++ tests/api2/test_110_certificate.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python3
-
-# Author: Eric Turgeon
-# License: BSD
-
 import pytest
 import re
-import sys
-import os
 
 from time import sleep
-apifolder = os.getcwd()
-sys.path.append(apifolder)
-from functions import GET, DELETE, POST
+from middlewared.test.integration.utils import call
 
 try:
     from config import (
@@ -21,26 +12,20 @@ try:
         LDAPHOSTNAME,
     )
 except ImportError:
-    Reason = 'LDAP* variable are not setup in config.py'
+    Reason = "LDAP* variable are not setup in config.py"
     # comment pytestmark for development testing with --dev-test
     pytestmark = pytest.mark.skipif(True, reason=Reason)
 
 
-def test_01_get_certificate_query():
-    results = GET('/certificate/')
-    assert results.status_code == 200, results.text
-    assert isinstance(results.json(), list), results.text
-
-
-def test_create_idmap_certificate():
-    global certificate_id, idmap_id
+def test_certificate():
+    # create certificate
     payload = {
-        'name': 'BOB',
-        'range_low': 1000,
-        'range_high': 2000,
-        'certificate': 1,
+        "name": "BOB",
+        "range_low": 1000,
+        "range_high": 2000,
+        "certificate": 1,
         "idmap_backend": "RFC2307",
-        'options': {
+        "options": {
             "ldap_server": "STANDALONE",
             "bind_path_user": LDAPBASEDN,
             "bind_path_group": LDAPBASEDN,
@@ -51,34 +36,26 @@ def test_create_idmap_certificate():
             "ldap_realm": False,
         }
     }
-    results = POST('/idmap/', payload)
-    assert results.status_code == 200, results.text
-    idmap_id = results.json()['id']
-    certificate_id = results.json()['certificate']['id']
+    results = call("idmap.create", payload)
+    idmap_id = int(results["id"])
+    certificate_id = results["certificate"]["id"]
 
+    # successful delete
+    results = call("certificate.delete", certificate_id, True)
+    job_id = int(results)
 
-def test_02_delete_used_certificate():
-    global job_id
-    results = DELETE(f'/certificate/id/{certificate_id}/', True)
-    assert results.status_code == 200, results.text
-    job_id = int(results.text)
-
-
-def test_03_verify_certificate_delete_failed():
+    # failed delete
     while True:
-        get_job = GET(f'/core/get_jobs/?id={job_id}')
-        assert get_job.status_code == 200, get_job.text
-        job_status = get_job.json()[0]
-        if job_status['state'] in ('RUNNING', 'WAITING'):
-            sleep(5)
+        get_job = call("core.get_jobs", [["id", "=", job_id]])
+        job_status = get_job[0]
+        if job_status["state"] in ("RUNNING", "WAITING"):
+            sleep(1)
         else:
-            assert job_status['state'] == 'FAILED', get_job.text
+            assert job_status["state"] == "FAILED", get_job
             assert bool(re.search(
-                r'Certificate is being used by following service.*IDMAP', job_status['error'], flags=re.DOTALL
-            )) is True, job_status['error']
+                r"Certificate is being used by following service.*IDMAP", job_status["error"], flags=re.DOTALL
+            )) is True, job_status["error"]
             break
 
-
-def test_04_delete_idmap():
-    results = DELETE(f'/idmap/id/{idmap_id}/')
-    assert results.status_code == 200, results.text
+    # delete idmap
+    call("idmap.delete", idmap_id)
