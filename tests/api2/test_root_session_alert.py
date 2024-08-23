b69b18fcdab8feab846e9a947b5f36e944e649d2
Andrew Walker <awalker@ixsystems.com>
2024-08-23 06:14:11 -0600

    Redesign root login alerts (#14324)
    In lieu of generating one-shot alerts when root-level account
    authenticates, generate a daily summary report of session IDs and
    addresses for clients that authenticated to the UI / API as a
    root account and limit such behavior to SCALE_ENTERPRISE.
    
    Also add new daily alert for failed login attempts to the UI / API.

--- tests/api2/test_root_session_alert.py
+++ tests/api2/test_root_session_alert.py
@@ -1,48 +1,37 @@
 import pytest
 
+from middlewared.test.integration.assets.product import product_type
 from middlewared.test.integration.utils.client import client, truenas_server
 from middlewared.test.integration.utils import call
+from time import sleep
+
+
+@pytest.fixture(scope="function")
+def set_product_type(request):
+    # force SCALE_ENTERPRISE product type
+    with product_type():
+        yield
 
 
 def get_session_alert(call_fn, session_id):
-    alerts = call_fn('alert.list')
-    alert_msg = None
+    # sleep a little while to let auth event get logged
+    sleep(5)
 
-    for alert in alerts:
-        if alert['klass'] == 'AdminSessionActive':
-            if session_id and session_id not in alert['formatted']:
-                continue
+    alert = call_fn('alert.run_source', 'AdminSession')
+    assert alert
 
-            alert_msg = alert['formatted']
-            break
-
-    assert alert_msg is not None, str(alerts)
-    return alert_msg
+    assert session_id in alert[0]['args']['sessions'], str(alert[0]['args'])
 
 
 def check_session_alert(call_fn):
     session_id = call_fn('auth.sessions', [['current', '=', True]], {'get': True})['id']
-    session_alert = get_session_alert(call_fn, session_id)
-
-    return session_id
+    get_session_alert(call_fn, session_id)
 
 
-def test_root_session_alert():
-    # We have a persistent root session so we expect alert to be present
+def test_root_session(set_product_type):
+    # first check with our regular persistent session
     check_session_alert(call)
 
-
-def test_root_session_logout():
     with client(host_ip=truenas_server.ip) as c:
-        # ensure that client generates alert
-        session_id = check_session_alert(c.call)
-        c.call('auth.logout')
-
-    # Make sure our session properly closed
-    closed_session = call('auth.sessions', [['id', '=', session_id]])
-    assert not closed_session
-
-    # Make sure old session ID no longer in alert
-    session_alert = get_session_alert(call, None)
-
-    assert session_id not in session_alert
+        # check that we also pick up second alert
+        check_session_alert(c.call)
