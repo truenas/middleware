b682e87a99b37c33988593c45e7d9f55ac5e45db
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2024-08-23 22:02:43 +0100

    NAS-130796 / 25.04 / Do not allow iSCSI extents to use zvols on boot-pool (#14337)
    * Do not allow iSCSI extents to use zvols on boot-pool

--- src/middlewared/middlewared/plugins/iscsi_/extents.py
+++ src/middlewared/middlewared/plugins/iscsi_/extents.py
@@ -335,6 +335,9 @@ class iSCSITargetExtentService(SharingService):
             if not os.path.exists(device):
                 verrors.add(f'{schema_name}.disk', f'Device {device!r} for volume {zvol_name!r} does not exist')
 
+            if self.middleware.call_sync('boot.is_boot_pool_path', device):
+                verrors.add(f'{schema_name}.disk', 'Disk residing in boot pool cannot be consumed and is not supported')
+
             if '@' in zvol_name and not data['ro']:
                 verrors.add(f'{schema_name}.ro', 'Must be set when disk is a ZFS Snapshot')
 
