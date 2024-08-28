a02112b16d600c58a2c6c99b938b4a65ce0d72ef
themylogin <themylogin@gmail.com>
2024-08-28 11:56:03 +0200

    `Other TrueNAS controller is inaccessible` proactive support alert
    (cherry picked from commit e796aed4f4503e340f3c3022467e2cefd478c95f)
    (cherry picked from commit 1a734074cc58d639d2a421f1767c10bfd9dca7ad)

---
+++ src/middlewared/middlewared/alert/source/failover_remote_inaccessible.py
@@ -0,0 +1,39 @@
+# Copyright (c) - iXsystems Inc.
+#
+# Licensed under the terms of the TrueNAS Enterprise License Agreement
+# See the file LICENSE.IX for complete terms and conditions
+
+import time
+
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, AlertSource, UnavailableException
+
+
+class FailoverRemoteSystemInaccessibleAlertClass(AlertClass):
+    category = AlertCategory.HA
+    level = AlertLevel.CRITICAL
+    title = 'Other Controller is Inaccessible'
+    text = 'Other TrueNAS controller is inaccessible. Contact support.'
+    products = ('SCALE_ENTERPRISE',)
+    proactive_support = True
+
+
+class FailoverRemoteSystemInaccessibleAlertSource(AlertSource):
+    products = ('SCALE_ENTERPRISE',)
+    failover_related = True
+    run_on_backup_node = False
+
+    def __init__(self, middleware):
+        super().__init__(middleware)
+        self.last_available = time.monotonic()
+
+    async def check(self):
+        try:
+            await self.middleware.call('failover.call_remote', 'core.ping', {'timeout': 2})
+        except Exception:
+            if time.monotonic() - self.last_available > 4 * 3600:
+                return [Alert(FailoverRemoteSystemInaccessibleAlertClass)]
+            else:
+                raise UnavailableException()
+
+        self.last_available = time.monotonic()
+        return []
