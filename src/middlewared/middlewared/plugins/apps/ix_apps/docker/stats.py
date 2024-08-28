7ce23be5b30e50075e01deda1dae12a486015843
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-08-28 11:42:21 -0700

    NAS-130471 / 24.10-RC.1 / Add an event source for app statistics (by sonicaj) (#14363)
    * Add initial logic to retrieve stats of active projects
    
    (cherry picked from commit d1a5cbcaecdb94ae3a4eef656741baf5e7485f7d)
    
    * Fix stats docker func
    
    (cherry picked from commit a4d3781613acec0095b7c7ee03721c1261521cd7)
    
    * Add event source for stats
    
    (cherry picked from commit e9481f60e771868010125cc37dff1c6f320a1718)
    
    * Report app names
    
    (cherry picked from commit 79b324549d949019e450497bf0128e1853c4a8d3)
    
    * Properly normalize app stats
    
    (cherry picked from commit 2979fa8fee0e046a75b3e83368cf0f70151a3425)
    
    * Add try/catch for the case when docker is unconfigured for stats
    
    (cherry picked from commit 41304c3d291184d8cdb8c973b98166f9998192b3)
    
    * Update description as network traffic is not bytes/s but bytes per interval
    
    (cherry picked from commit 245ba6c0c8acc3b948c02f5ecffe771bcb1f200b)
    
    * Do not do a deepcopy
    
    (cherry picked from commit 9715c854a893d625437ccb1c8cfd5e346e303767)
    
    * Stop on docker status instead of checking db pool or service.started as there is a race condition there
    
    (cherry picked from commit d9057f39797b291c21fa89f97ba26394704d5842)
    
    * Update description of return type
    
    (cherry picked from commit 21fe3bbe1a8b8057d1e20b688314224aedcf3487)
    
    * Report stopped apps as well
    
    (cherry picked from commit 689d00d1d54d7971590d0a725a48c89d5b3b19ff)
    
    * Have retry logic in place for handling edge case of stopping an app
    
    (cherry picked from commit 85c5ce66e0a3f9d5ca558fea6690b0241acecbd7)
    
    ---------
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

---
+++ src/middlewared/middlewared/plugins/apps/ix_apps/docker/stats.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+import requests
+
+from .utils import get_docker_client, PROJECT_KEY
+
+
+def get_default_stats():
+    return defaultdict(lambda: {
+        'cpu_usage': 0,
+        'memory': 0,
+        'networks': defaultdict(lambda: {'rx_bytes': 0, 'tx_bytes': 0}),
+        'blkio': {'read': 0, 'write': 0},
+    })
+
+
+def list_resources_stats_by_project(project_name: str | None = None) -> dict:
+    retries = 2
+    while retries > 0:
+        # We do this because when an app is being stopped, we can run into a race condition
+        # where the container got listed but when we queried it's stats we were not able
+        # to get them as the container by that time had been nuked (this is similar to what we
+        # do when we list resources by project)
+        try:
+            return list_resources_stats_by_project_internal(project_name)
+        except requests.exceptions.HTTPError:
+            retries -= 1
+            if retries == 0:
+                raise
+
+
+def list_resources_stats_by_project_internal(project_name: str | None = None) -> dict:
+    projects = get_default_stats()
+    with get_docker_client() as client:
+        label_filter = {'label': f'{PROJECT_KEY}={project_name}' if project_name else PROJECT_KEY}
+        for container in client.containers.list(all=True, filters=label_filter, sparse=False):
+            stats = container.stats(stream=False, decode=None, one_shot=True)
+            project = container.labels.get(PROJECT_KEY)
+            if not project:
+                continue
+
+            blkio_container_stats = stats.get('blkio_stats', {}).get('io_service_bytes_recursive') or {}
+            project_stats = projects[project]
+            project_stats['cpu_usage'] += stats.get('cpu_stats', {}).get('cpu_usage', {}).get('total_usage', 0)
+            project_stats['memory'] += stats.get('memory_stats', {}).get('usage', 0)
+            for entry in filter(lambda x: x['op'] in ('read', 'write'), blkio_container_stats):
+                project_stats['blkio'][entry['op']] += entry['value']
+            for net_name, net_values in stats.get('networks', {}).items():
+                project_stats['networks'][net_name]['rx_bytes'] += net_values.get('rx_bytes', 0)
+                project_stats['networks'][net_name]['tx_bytes'] += net_values.get('tx_bytes', 0)
+
+    return projects
