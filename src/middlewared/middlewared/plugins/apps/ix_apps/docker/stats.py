e54c4ded532c62d5ff714fe4458f7615c9e396e7
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-08-28 11:54:53 +0500

    Properly normalize app stats
    (cherry picked from commit 2979fa8fee0e046a75b3e83368cf0f70151a3425)

--- src/middlewared/middlewared/plugins/apps/ix_apps/docker/stats.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/docker/stats.py
@@ -3,13 +3,17 @@ from collections import defaultdict
 from .utils import get_docker_client, PROJECT_KEY
 
 
-def list_resources_stats_by_project(project_name: str | None = None) -> dict:
-    projects = defaultdict(lambda: {
+def get_default_stats():
+    return defaultdict(lambda: {
         'cpu_usage': 0,
-        'memory_stats': 0,
+        'memory': 0,
         'networks': defaultdict(lambda: {'rx_bytes': 0, 'tx_bytes': 0}),
-        'blkio_stats': {'read': 0, 'write': 0},
+        'blkio': {'read': 0, 'write': 0},
     })
+
+
+def list_resources_stats_by_project(project_name: str | None = None) -> dict:
+    projects = get_default_stats()
     with get_docker_client() as client:
         label_filter = {'label': f'{PROJECT_KEY}={project_name}' if project_name else PROJECT_KEY}
         for container in client.containers.list(all=True, filters=label_filter, sparse=False):
@@ -18,12 +22,12 @@ def list_resources_stats_by_project(project_name: str | None = None) -> dict:
             if not project:
                 continue
 
-            blkio_container_stats = stats.get('blkio_stats', {}).get('io_service_bytes_recursive') or {}
+            blkio_container_stats = stats.get('blkio', {}).get('io_service_bytes_recursive') or {}
             project_stats = projects[project]
             project_stats['cpu_usage'] += stats.get('cpu_stats', {}).get('cpu_usage', {}).get('total_usage', 0)
-            project_stats['memory_stats'] += stats.get('memory_stats', {}).get('usage', 0)
+            project_stats['memory'] += stats.get('memory', {}).get('usage', 0)
             for entry in filter(lambda x: x['op'] in ('read', 'write'), blkio_container_stats):
-                project_stats['blkio_stats'][entry['op']] += entry['value']
+                project_stats['blkio'][entry['op']] += entry['value']
             for net_name, net_values in stats.get('networks', {}).items():
                 project_stats['networks'][net_name]['rx_bytes'] += net_values.get('rx_bytes', 0)
                 project_stats['networks'][net_name]['tx_bytes'] += net_values.get('tx_bytes', 0)
