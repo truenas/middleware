09e3883a94ec141a9c4c13499c7b05d1773855fc
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-09-03 04:48:55 -0700

    NAS-130591 / 24.10-RC.1 / `Other TrueNAS controller is inaccessible` proactive support alert (by themylogin) (#14375)
    * `Other TrueNAS controller is inaccessible` proactive support alert
    
    (cherry picked from commit e796aed4f4503e340f3c3022467e2cefd478c95f)
    
    * Proactive support: also notify gone alerts when requested
    
    (cherry picked from commit f188d7a45cb374f6c1157e8acb571905084647a0)
    
    * Add incident ID
    
    (cherry picked from commit 62b36c1fcc0cce4a239c47299ec3b82614eea2a2)
    
    * Fix
    
    (cherry picked from commit 4cb1e74fba7fc988ae3e93e49b6989b9cc04db3f)
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ src/middlewared/middlewared/alert/source/failover_remote_inaccessible.py
@@ -0,0 +1,45 @@
+# Copyright (c) - iXsystems Inc.
+#
+# Licensed under the terms of the TrueNAS Enterprise License Agreement
+# See the file LICENSE.IX for complete terms and conditions
+
+import time
+
+from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, AlertSource, UnavailableException
+from middlewared.utils.crypto import generate_token
+
+
+class FailoverRemoteSystemInaccessibleAlertClass(AlertClass):
+    category = AlertCategory.HA
+    level = AlertLevel.CRITICAL
+    title = 'Other Controller is Inaccessible'
+    text = 'Other TrueNAS controller is inaccessible. Contact support. Incident ID: %s.'
+    products = ('SCALE_ENTERPRISE',)
+    proactive_support = True
+    proactive_support_notify_gone = True
+
+
+class FailoverRemoteSystemInaccessibleAlertSource(AlertSource):
+    products = ('SCALE_ENTERPRISE',)
+    failover_related = True
+    run_on_backup_node = False
+
+    def __init__(self, middleware):
+        super().__init__(middleware)
+        self.last_available = time.monotonic()
+        self.incident_id = None
+
+    async def check(self):
+        try:
+            await self.middleware.call('failover.call_remote', 'core.ping', [], {'timeout': 2})
+        except Exception:
+            if time.monotonic() - self.last_available > 4 * 3600:
+                if self.incident_id is None:
+                    self.incident_id = generate_token(16, url_safe=True)
+                return [Alert(FailoverRemoteSystemInaccessibleAlertClass, args=[self.incident_id])]
+            else:
+                raise UnavailableException()
+
+        self.last_available = time.monotonic()
+        self.incident_id = None
+        return []
