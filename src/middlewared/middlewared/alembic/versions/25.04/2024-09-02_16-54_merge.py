b48b61ddca31b208192b1d074dc9b6862366d4e2
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-09-03 04:59:28 -0700

    NAS-130967 / 25.04 / Provide cpu topology extensions to VM if requested (by Qubad786) (#14396)
    * Provide cpu topology extensions to VM if requested
    
    (cherry picked from commit f50e1d8f2c87aaf8a0c80a1d2df684377ac1b9bf)
    
    * Add merge migration
    
    ---------
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/25.04/2024-09-02_16-54_merge.py
@@ -0,0 +1,19 @@
+"""Merge
+
+Revision ID: 991d17a5b3a2
+Revises: d4d5d52be239, 7b13df980355
+Create Date: 2024-09-02 16:54:59.547731+00:00
+"""
+
+revision = '991d17a5b3a2'
+down_revision = ('d4d5d52be239', 'd24d6760fda4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
