27a325d105faef04f1b2e5371d3520b91951e1bd
Logan Cary <logan.cary@ixsystems.com>
2024-09-03 13:56:28 -0400

    NAS-130898 / 25.04 / Remove REST from test_auth_token.py (#14400)
    * remove rest test
    
    * address @yocalebo

--- tests/api2/test_auth_token.py
+++ tests/api2/test_auth_token.py
@@ -4,11 +4,6 @@ import json
 import pytest
 import requests
 
-import os
-import sys
-sys.path.append(os.getcwd())
-from functions import GET
-
 from middlewared.test.integration.assets.account import unprivileged_user as unprivileged_user_template
 from middlewared.test.integration.utils import call, client, ssh
 from middlewared.test.integration.utils.client import truenas_server
@@ -18,11 +13,6 @@ from middlewared.test.integration.utils.shell import assert_shell_works
 @pytest.fixture(scope="module")
 def download_token():
     return call("auth.generate_token", 300, {"filename": "debug.txz", "job": 1020}, True)
-
-
-def test_download_auth_token_cannot_be_used_for_restful_api_call(download_token):
-    results = GET("/user/id/1/", anonymous=True, headers={"Authorization": f"Token {download_token}"})
-    assert results.status_code == 403, results.text
 
 
 def test_download_auth_token_cannot_be_used_for_upload(download_token):
