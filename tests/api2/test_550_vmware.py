b374d55cec20a1379e3f391003ccc8602b76e6d6
Aiden Baker <baker.aiden@protonmail.com>
2024-09-04 09:23:44 -0400

    Initial cleanup test

--- tests/api2/test_550_vmware.py
+++ tests/api2/test_550_vmware.py
@@ -1,44 +1,29 @@
-#!/usr/bin/env python3
-
-# Author: Eric Turgeon
-# License: BSD
-
 import pytest
-import sys
-import os
-from pytest_dependency import depends
-apifolder = os.getcwd()
-sys.path.append(apifolder)
-from functions import GET, POST, SSH_TEST
-from auto_config import user, password
+
+from middlewared.test.integration.utils import call, ssh
 
 try:
-    Reason = 'VMWARE credentials credential is missing'
     from config import VMWARE_HOST, VMWARE_USERNAME, VMWARE_PASSWORD
     vmw_credentials = True
 except ImportError:
     vmw_credentials = False
 
 
-def test_01_get_vmware_query():
-    results = GET('/vmware/')
-    assert results.status_code == 200
-    assert isinstance(results.json(), list) is True
+@pytest.mark.skipif(not vmw_credentials, reason='VMWARE credentials are missing')
+def test_create_vmware():
 
+    call('vmware.query')
 
-if vmw_credentials:
-    def test_02_create_vmware():
-        payload = {
-            'hostname': VMWARE_HOST,
-            'username': VMWARE_USERNAME,
-            'password': VMWARE_PASSWORD
-        }
-        results = POST('/vmware/get_datastores/', payload)
-        assert results.status_code == 200, results.text
-        assert isinstance(results.json(), list) is True, results.text
+    payload = {
+        'hostname': VMWARE_HOST,
+        'username': VMWARE_USERNAME,
+        'password': VMWARE_PASSWORD
+    }
+    results = call('vmware.get_datastores', payload)
 
-    def test_03_verify_vmware_get_datastore_do_not_leak_password(request):
-        cmd = f"grep -R \"{os.environ['VMWARE_PASSWORD']}\" " \
-            "/var/log/middlewared.log"
-        results = SSH_TEST(cmd, user, password)
-        assert results['result'] is False, str(results['output'])
+@pytest.mark.skipif(not vmw_credentials, reason='VMWARE credentials are missing')
+def test_verify_vmware_get_datastore_do_not_leak_password(request):
+    cmd = f'grep -R "{VMWARE_PASSWORD}" ' \
+        '/var/log/middlewared.log'
+    results = ssh(cmd, complete_response=True)
+    assert results['result'] is False
