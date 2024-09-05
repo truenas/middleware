8e640312a20b4c77b44dd17c4884caabdc7b615f
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-09-05 08:08:08 -0700

    Allow retrieving logs of apps in crashed state (#14426)
    (cherry picked from commit 4f2b59a5e9289c1403f44cb35992f5979a0c8f9b)
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

--- src/middlewared/middlewared/plugins/apps/ix_apps/utils.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/utils.py
@@ -1,7 +1,22 @@
+import enum
+
 from catalog_reader.library import RE_VERSION  # noqa
 from middlewared.plugins.apps_images.utils import normalize_reference  # noqa
 from middlewared.plugins.apps.schema_utils import CONTEXT_KEY_NAME  # noqa
 from middlewared.plugins.apps.utils import IX_APPS_MOUNT_PATH, PROJECT_PREFIX, run  # noqa
+
+
+class AppState(enum.Enum):
+    CRASHED = 'CRASHED'
+    DEPLOYING = 'DEPLOYING'
+    RUNNING = 'RUNNING'
+    STOPPED = 'STOPPED'
+
+
+class ContainerState(enum.Enum):
+    EXITED = 'exited'
+    RUNNING = 'running'
+    STARTING = 'starting'
 
 
 def get_app_name_from_project_name(project_name: str) -> str:
