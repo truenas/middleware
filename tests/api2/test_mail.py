a7c5455159804e578a19a7d5ddc2576d84f933e8
Logan Cary <logan.cary@ixsystems.com>
2024-09-06 12:10:19 -0400

    NAS-131033 / 25.04 / API test_mail.test_config_settings regression (#14440)
    * dont restore state
    
    * don't restore state

--- tests/api2/test_mail.py
+++ tests/api2/test_mail.py
@@ -2,7 +2,6 @@ from middlewared.test.integration.utils import call
 
 
 def test_config_settings():
-    orig_config = call("mail.config")
     payload = {
         "fromemail": "william.spam@ixsystems.com",
         "outgoingserver": "mail.ixsystems.com",
@@ -12,11 +11,7 @@ def test_config_settings():
         "smtp": True,
         "user": "william.spam@ixsystems.com"
     }
-    try:
-        call("mail.update", payload)
-        config = call("mail.config")
-        # test that payload is a subset of config
-        assert payload.items() <= config.items()
-    finally:
-        call("mail.update", {key: orig_config[key] for key in payload})
-        assert call("mail.config") == orig_config
+    call("mail.update", payload)
+    config = call("mail.config")
+    # test that payload is a subset of config
+    assert payload.items() <= config.items()
