28f267ab0fc6284aef665c22c0d224ceb9b118fa
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-09-07 05:03:18 +0500

    Add basic docker network service

---
+++ src/middlewared/middlewared/plugins/docker/network.py
@@ -0,0 +1,28 @@
+from middlewared.schema import Dict, Str
+from middlewared.service import CRUDService, filterable
+from middlewared.utils import filter_list
+
+
+class DockerNetworkService(CRUDService):
+
+    class Config:
+        namespace = 'docker.network'
+        datastore_primary_key_type = 'string'
+        cli_namespace = 'docker.network'
+        role_prefix = 'DOCKER'
+
+    ENTRY = Dict(
+        'docker_network_entry',
+        Str('id', required=True),
+        Str('name', required=True),
+        Str('short_id', required=True),
+        additional_attrs=True,
+    )
+
+    @filterable
+    def query(self, app, filters, options):
+        """
+        Query all docker networks
+        """
+        if not self.middleware.call_sync('docker.state.validate', False):
+            return filter_list([], filters, options)
