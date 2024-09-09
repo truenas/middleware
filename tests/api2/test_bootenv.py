e5c84e5de853bcb2e614406862cec3e169479c25
Aiden Baker <baker.aiden@protonmail.com>
2024-09-09 09:31:14 -0400

    API tests

--- tests/api2/test_bootenv.py
+++ tests/api2/test_bootenv.py
@@ -1,4 +1,32 @@
+import errno
+
+import pytest
+
+from middlewared.service_exception import ValidationErrors, ValidationError
 from middlewared.test.integration.utils import call, ssh
+
+
+def test_get_default_environment_and_make_new_one():
+    active_be_id = call('bootenv.query', [['activated', '=', True]], {'get': True})['id']
+
+    # create duplicate name to test failure
+    with pytest.raises(ValidationErrors) as ve:
+        call('bootenv.create', {'name': active_be_id, 'source': active_be_id})
+    assert ve.value.errors == [
+        ValidationError('bootenv_create.name', f'The name "{active_be_id}" already exists', errno.EEXIST)
+    ]
+
+    # create new bootenv and activate it
+    call('bootenv.create', {'name': 'bootenv01', 'source': active_be_id})
+    call('bootenv.query', [['name', '=', 'bootenv01']], {'get': True})
+
+
+def test_change_boot_environment_name_and_attributes_then_delete():
+    call('bootenv.update', 'bootenv01', {'name': 'bootenv03'})
+    call('bootenv.set_attribute', 'bootenv03', {'keep': True})
+    assert call('bootenv.query', [['name', '=', 'bootenv03']], {'get': True})['keep'] is True
+    call('bootenv.set_attribute', 'bootenv03', {'keep': False})
+    call('bootenv.delete', 'bootenv03')
 
 
 def test_promote_current_be_datasets():
