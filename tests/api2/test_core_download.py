140f4d1252f1d8f6ab12fedcef85639ceaabe20b
Aiden Baker <baker.aiden@protonmail.com>
2024-09-09 11:45:40 -0400

    Rewrote

--- tests/api2/test_core_download.py
+++ tests/api2/test_core_download.py
@@ -6,24 +6,12 @@ from middlewared.test.integration.utils import call
 
 
 def test_get_download_for_config_dot_save():
-    # ping core for sanity
-    assert call('core.ping') == 'pong'
-
-
     # set up core download
-    job_id, url = call('core.download', 'config.save', [], 'freenas.db')
-
+    job_id, url = call('core.download', 'config.save', [], 'freenas.db', job=True)
 
     # is the job running?
-    results = call('core.get_jobs', [['id', '=', job_id]], {"get": True})
-    assert results['state'] == 'RUNNING'
-
+    assert call('core.get_jobs', [['id', '=', job_id]], {'get': True})['state'] == 'SUCCESS'
 
     # download from URL
     rv = urlretrieve(f'http://{truenas_server.ip}{url}')
     assert stat(rv[0]).st_size > 0
-
-
-    # verify success
-    results = call('core.get_jobs', [['id', '=', job_id]], {"get": True})
-    assert results['state'] == 'SUCCESS'
