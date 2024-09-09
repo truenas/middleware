e358908b99a2e8f294d73eafaaf0939e5315cf1f
Aiden Baker <baker.aiden@protonmail.com>
2024-09-09 15:11:58 -0400

    Updated test

--- tests/api2/test_230_idmap.py
+++ tests/api2/test_230_idmap.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python3
-
 import pytest
-import sys
-import os
 
-apifolder = os.getcwd()
-sys.path.append(apifolder)
-from functions import DELETE, GET, POST
+from middlewared.test.integration.utils import call
 
 try:
     from config import (
@@ -21,8 +15,7 @@ except ImportError:
     pytestmark = pytest.mark.skipif(True, reason=Reason)
 
 
-def test_create_idmap_certificate():
-    global idmap_id
+def test_create_and_delete_idmap_certificate():
     payload = {
         'name': 'BOB.NB',
         'range_low': 1000,
@@ -40,16 +33,6 @@ def test_create_idmap_certificate():
             "ldap_realm": False,
         }
     }
-    results = POST('/idmap/', payload)
-    assert results.status_code == 200, results.text
-    idmap_id = results.json()['id']
+    idmap_id = call('idmap.create', payload)['id']
 
-
-def test_02_delete_used_certificate():
-    results = DELETE(f'/idmap/id/{idmap_id}/')
-    assert results.status_code == 200, results.text
-
-
-def test_03_verify_delete_job():
-    results = GET(f'/idmap/id/{idmap_id}/')
-    assert results.status_code == 404, results.text
+    results = call('idmap.delete', idmap_id)
