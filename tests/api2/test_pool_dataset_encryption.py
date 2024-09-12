cf0584ca933cf7bb7e35165f4a97199ba9c8d847
Logan Cary <logan.cary@ixsystems.com>
2024-09-12 17:15:57 -0400

    decided on an updated structure

--- tests/api2/test_pool_dataset_encryption.py
+++ tests/api2/test_pool_dataset_encryption.py
@@ -1,9 +1,11 @@
+import contextlib
 import secrets
 
 import pytest
 from pytest_dependency import depends
 
 from middlewared.service_exception import ValidationErrors
+from middlewared.test.integration.assets.pool import another_pool
 from middlewared.test.integration.utils import call, ssh
 from truenas_api_client.exc import ClientException
 
@@ -13,320 +15,217 @@ pool_token_hex = secrets.token_hex(32)
 pool_token_hex2 = secrets.token_hex(32)
 dataset_token_hex = secrets.token_hex(32)
 dataset_token_hex2 = secrets.token_hex(32)
+
 encrypted_pool_name = 'test_encrypted'
 dataset = f'{encrypted_pool_name}/encrypted'
 child_dataset = f'{dataset}/child'
 
 
-@pytest.mark.dependency(name='CREATED_POOL')
-def test_create_a_normal_pool(request):
-    global pool_id, pool_disks
-    # Get one disk for encryption testing
-    pool_disks = [call('disk.get_unused')[0]['name']]
-    payload = {
-        'name': encrypted_pool_name,
-        'encryption': False,
-        'topology': {
-            'data': [
-                {'type': 'STRIPE', 'disks': pool_disks}
-            ],
-        },
-        'allow_duplicate_serials': True,
-    }
-    pool_id = call('pool.create', payload, job=True)['id']
+@pytest.fixture(scope='class')
+def create_pool():
+    with another_pool({'name': encrypted_pool_name}):
+        yield
 
 
-def test_create_a_passphrase_encrypted_root_on_normal_pool(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'generate_key': False,
-            'pbkdf2iters': 100000,
-            'algorithm': 'AES-128-CCM',
-            'passphrase': 'my_passphrase',
-        },
-        'encryption': True,
-        'inherit_encryption': False
-    }
-    results = call('pool.dataset.create', payload)
-    assert results['key_format']['value'] == 'PASSPHRASE'
+@contextlib.contextmanager
+def create_dataset(payload):
+    yield call('pool.dataset.create', payload)
+    assert call('pool.dataset.delete', payload['name'])
 
 
-def test_verify_pool_dataset_does_not_leak_passphrase_into_middleware_log(request):
-    cmd = 'grep -R "my_passphrase" /var/log/middlewared.log'
+def check_log_for(phrase):
+    cmd = f'grep -R "{phrase}" /var/log/middlewared.log'
     results = ssh(cmd, check=False, complete_response=True)
     assert results['result'] is False, str(results['output'])
 
 
-def test_add_the_comment_on_the_passphrase_encrypted_root(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'comments': 'testing encrypted dataset'
-    }
-    call('pool.dataset.update', dataset, payload)
+@pytest.mark.usefixtures('create_pool')
+class TestNormalPool:
 
+    def test_passphrase_encrypted_root(self):
+        payload = {
+            'name': dataset,
+            'encryption_options': {
+                'generate_key': False,
+                'pbkdf2iters': 100000,
+                'algorithm': 'AES-128-CCM',
+                'passphrase': 'my_passphrase',
+            },
+            'encryption': True,
+            'inherit_encryption': False
+        }
+        with create_dataset(payload) as ds:
+            assert ds['key_format']['value'] == 'PASSPHRASE'
+            check_log_for('my_passphrase')
 
-def test_change_a_passphrase_encrypted_root_key_encryption(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'key': dataset_token_hex,
-    }
-    call('pool.dataset.change_key', dataset, payload, job=True)
+            call('pool.dataset.update', dataset, {'comments': 'testing encrypted dataset'})
+            call('pool.dataset.change_key', dataset, {'key': dataset_token_hex}, job=True)
 
+            ds = call('pool.dataset.get_instance', dataset)
+            assert ds['key_format']['value'] == 'HEX'
 
-def test_verify_that_the_dataset_encrypted_root_changed_to_key_encryption(request):
-    depends(request, ['CREATED_POOL'])
-    results = call('pool.dataset.get_instance', dataset)
-    assert results['key_format']['value'] == 'HEX'
-
-
-def test_delete_passphrase_encrypted_root(request):
-    depends(request, ['CREATED_POOL'])
-    assert call('pool.dataset.delete', dataset)
-
-
-def test_create_not_encrypted_dataset_on_a_normal_pool(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption': False,
-    }
-    results = call('pool.dataset.create', payload)
-    assert results['key_format']['value'] is None
-
-
-def test_delete_not_encrypted_dataset(request):
-    depends(request, ['CREATED_POOL'])
-    assert call('pool.dataset.delete', dataset)
-
-
-def test_create_a_dataset_with_inherit_encryption_true_on_a_normal_pool(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'inherit_encryption': True
-    }
-    call('pool.dataset.create', payload)
-
-
-def test_verify_that_the_dataset_created_is_not_encrypted_like_the_parrent(request):
-    depends(request, ['CREATED_POOL'])
-    results = call('pool.dataset.get_instance', dataset)
-    assert results['key_format']['value'] is None
-
-
-def test_delete_dataset(request):
-    depends(request, ['CREATED_POOL'])
-    assert call('pool.dataset.delete', dataset)
-
-
-def test_try_to_create_an_encrypted_dataset_with_pbkdf2itersl_zero(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'pbkdf2iters': 0,
+    @pytest.mark.parametrize('payload', [
+        {
+            'encryption': False,
         },
-        'encryption': True,
-        'inherit_encryption': False
-    }
-    with pytest.raises(ValidationErrors, match='Should be greater or equal than 100000'):
-        call('pool.dataset.create', payload)
+        {
+            'inherit_encryption': True
+        }
+    ])
+    def test_dataset_not_encrypted(self, payload: dict):
+        payload['name'] = dataset
+        with create_dataset(payload) as ds:
+            assert ds['key_format']['value'] is None
 
-
-def test_try_to_create_an_encrypted_dataset_with_inherit_encryption_true(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'passphrase': 'my_passphrase',
-        },
-        'encryption': True,
-        'inherit_encryption': True
-    }
-    with pytest.raises(ValidationErrors, match='Must be disabled when encryption is enabled'):
-        call('pool.dataset.create', payload)
-
-
-def test_verify_pool_encrypted_dataset_does_not_leak_passphrase_into_middleware_log(request):
-    cmd = 'grep -R "my_passphrase" /var/log/middlewared.log'
-    results = ssh(cmd, check=False, complete_response=True)
-    assert results['result'] is False, str(results['output'])
-
-
-def test_try_to_create_an_encrypted_dataset_with_passphrase_and_generate_key(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'generate_key': True,
-            'passphrase': 'my_passphrase',
-        },
-        'encryption': True,
-        'inherit_encryption': False
-    }
-    with pytest.raises(ValidationErrors, match='Must be disabled when dataset is to be encrypted with passphrase'):
-        call('pool.dataset.create', payload)
-
-
-def test_create_an_encrypted_root_with_generate_key(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'generate_key': True,
-        },
-        'encryption': True,
-        'inherit_encryption': False
-    }
-    results = call('pool.dataset.create', payload)
-    assert results['key_format']['value'] == 'HEX'
-
-
-def test_delete_generate_key_encrypted_root(request):
-    depends(request, ['CREATED_POOL'])
-    assert call('pool.dataset.delete', dataset)
-
-
-def test_create_an_encrypted_root_with_a_key(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'name': dataset,
-        'encryption_options': {
-            'key': dataset_token_hex,
-        },
-        'encryption': True,
-        'inherit_encryption': False
-    }
-    results = call('pool.dataset.create', payload)
-    assert results['key_format']['value'] == 'HEX'
-
-
-def test_verify_pool_encrypted_root_dataset_does_not_leak_encryption_key_into_middleware_log(request):
-    cmd = f'grep -R "{dataset_token_hex}" /var/log/middlewared.log'
-    results = ssh(cmd, check=False, complete_response=True)
-    assert results['result'] is False, str(results['output'])
-
-
-def test_make_sure_we_are_not_able_to_lock_key_encrypted_dataset(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'force_umount': True
-    }
-    with pytest.raises(ClientException, match='Only datasets which are encrypted with passphrase can be locked'):
-        call('pool.dataset.lock', dataset, payload, job=True)
-
-def test_change_a_key_encrypted_dataset_to_passphrase(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'passphrase': 'my_passphrase'
-    }
-    call('pool.dataset.change_key', dataset, payload, job=True)
-
-
-def test_verify_that_the_dataset_changed_to_passphrase(request):
-    depends(request, ['CREATED_POOL'])
-    results = call('pool.dataset.get_instance', dataset)
-    assert results['key_format']['value'] == 'PASSPHRASE'
-
-
-def test_verify_pool_dataset_change_key_does_not_leak_passphrase_into_middleware_log(request):
-    cmd = 'grep -R "my_passphrase" /var/log/middlewared.log'
-    results = ssh(cmd, check=False, complete_response=True)
-    assert results['result'] is False, str(results['output'])
-
-
-def test_lock_passphrase_encrypted_datasets_and_ensure_they_get_locked(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'force_umount': True
-    }
-    assert call('pool.dataset.lock', dataset, payload, job=True)
-
-
-def test_verify_passphrase_encrypted_root_is_locked(request):
-    depends(request, ['CREATED_POOL'])
-    job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
-    for dictionary in job_status_result:
-        if dictionary['name'] == dataset:
-            assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
-            assert dictionary['unlock_successful'] is False, str(job_status_result)
-            assert dictionary['locked'] is True, str(job_status_result)
-            break
-    else:
-        pytest.fail(str(job_status_result))
-
-
-def test_unlock_passphrase_encrypted_datasets_with_wrong_passphrase(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'recursive': True,
-        'datasets': [
+    @pytest.mark.parametrize('payload, message', [
+        (
             {
-                'name': dataset,
-                'passphrase': 'bad_passphrase'
-            }
-        ]
-    }
-    job_status = call('pool.dataset.unlock', dataset, payload, job=True)
-    assert job_status['failed'][dataset]['error'] == 'Invalid Key', str(job_status['results'])
-
-
-def test_verify_passphrase_encrypted_root_still_locked(request):
-    depends(request, ['CREATED_POOL'])
-    job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
-    for dictionary in job_status_result:
-        if dictionary['name'] == dataset:
-            assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
-            assert dictionary['unlock_successful'] is False, str(job_status_result)
-            assert dictionary['locked'] is True, str(job_status_result)
-            break
-    else:
-        pytest.fail(str(job_status_result))
-
-
-def test_unlock_passphrase_encrypted_datasets(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'recursive': True,
-        'datasets': [
+                'encryption_options': {
+                    'pbkdf2iters': 0
+                },
+                'inherit_encryption': False
+            },
+            'Should be greater or equal than 100000'
+        ),
+        (
             {
-                'name': dataset,
-                'passphrase': 'my_passphrase'
-            }
-        ]
-    }
-    job_status = call('pool.dataset.unlock', dataset, payload, job=True)
-    assert job_status['unlocked'] == [dataset], str(job_status)
+                'encryption_options': {
+                    'passphrase': 'my_passphrase',
+                },
+                'inherit_encryption': True
+            },
+            'Must be disabled when encryption is enabled'
+        ),
+        (
+            {
+                'encryption_options': {
+                    'generate_key': True,
+                    'passphrase': 'my_passphrase',
+                },
+                'inherit_encryption': False
+            },
+            'Must be disabled when dataset is to be encrypted with passphrase'
+        )
+    ])
+    def test_try_to_create_invalid_encrypted_dataset(self, payload: dict, message: str):
+        payload.update({
+            'name': dataset,
+            'encryption': True,
+        })
+        with pytest.raises(ValidationErrors, match=message):
+            with create_dataset(payload): pass
+
+    def test_invalid_encrypted_dataset_does_not_leak_passphrase_into_middleware_log(self):
+        check_log_for('my_passphrase')
+
+    @pytest.mark.parametrize('payload', [
+        {
+            'encryption_options': {
+                'generate_key': True,
+            },
+        },
+        {
+            'encryption_options': {
+                'key': dataset_token_hex,
+            },
+        }
+    ])
+    def test_encrypted_root_with_key(self, payload: dict):
+        payload.update({
+            'name': dataset,
+            'encryption': True,
+            'inherit_encryption': False,
+        })
+        with create_dataset(payload) as ds:
+            assert ds['key_format']['value'] == 'HEX'
+            check_log_for(dataset_token_hex)
+
+            with pytest.raises(ClientException, match='Only datasets which are encrypted with passphrase can be locked'):
+                call('pool.dataset.lock', dataset, {'force_umount': True}, job=True)
+            
+            # Change to a passphrase-encrypted dataset
+            call('pool.dataset.change_key', dataset, {'passphrase': 'my_passphrase'}, job=True)
+            ds = call('pool.dataset.get_instance', dataset)
+            assert ds['key_format']['value'] == 'PASSPHRASE'
+            check_log_for('my_passphrase')
+
+            assert call('pool.dataset.lock', dataset, {'force_umount': True}, job=True)
 
 
-def test_verify_passphrase_encrypted_root_is_unlocked(request):
-    depends(request, ['CREATED_POOL'])
-    job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
-    for dictionary in job_status_result:
-        if dictionary['name'] == dataset:
-            assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
-            assert dictionary['unlock_successful'] is True, str(job_status_result)
-            assert dictionary['locked'] is False, str(job_status_result)
-            break
-    else:
-        pytest.fail(str(job_status_result))
+    def test_verify_passphrase_encrypted_root_is_locked(self):
+        job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
+        for dictionary in job_status_result:
+            if dictionary['name'] == dataset:
+                assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
+                assert dictionary['unlock_successful'] is False, str(job_status_result)
+                assert dictionary['locked'] is True, str(job_status_result)
+                break
+        else:
+            pytest.fail(str(job_status_result))
 
 
-def test_delete_encrypted_dataset(request):
-    depends(request, ['CREATED_POOL'])
-    assert call('pool.dataset.delete', dataset)
+    def test_unlock_passphrase_encrypted_datasets_with_wrong_passphrase(self, request):
+        depends(request, ['CREATED_POOL'])
+        payload = {
+            'recursive': True,
+            'datasets': [
+                {
+                    'name': dataset,
+                    'passphrase': 'bad_passphrase'
+                }
+            ]
+        }
+        job_status = call('pool.dataset.unlock', dataset, payload, job=True)
+        assert job_status['failed'][dataset]['error'] == 'Invalid Key', str(job_status['results'])
 
 
-def test_delete_pool(request):
-    depends(request, ['CREATED_POOL'])
-    payload = {
-        'cascade': True,
-        'restart_services': True,
-        'destroy': True
-    }
-    call('pool.export', pool_id, payload, job=True)
+    def test_verify_passphrase_encrypted_root_still_locked(self, request):
+        depends(request, ['CREATED_POOL'])
+        job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
+        for dictionary in job_status_result:
+            if dictionary['name'] == dataset:
+                assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
+                assert dictionary['unlock_successful'] is False, str(job_status_result)
+                assert dictionary['locked'] is True, str(job_status_result)
+                break
+        else:
+            pytest.fail(str(job_status_result))
+
+
+    def test_unlock_passphrase_encrypted_datasets(self, request):
+        depends(request, ['CREATED_POOL'])
+        payload = {
+            'recursive': True,
+            'datasets': [
+                {
+                    'name': dataset,
+                    'passphrase': 'my_passphrase'
+                }
+            ]
+        }
+        job_status = call('pool.dataset.unlock', dataset, payload, job=True)
+        assert job_status['unlocked'] == [dataset], str(job_status)
+
+
+    def test_verify_passphrase_encrypted_root_is_unlocked(self, request):
+        depends(request, ['CREATED_POOL'])
+        job_status_result = call('pool.dataset.encryption_summary', dataset, job=True)
+        for dictionary in job_status_result:
+            if dictionary['name'] == dataset:
+                assert dictionary['key_format'] == 'PASSPHRASE', str(job_status_result)
+                assert dictionary['unlock_successful'] is True, str(job_status_result)
+                assert dictionary['locked'] is False, str(job_status_result)
+                break
+        else:
+            pytest.fail(str(job_status_result))
+
+
+    def test_delete_encrypted_dataset(self, request):
+        depends(request, ['CREATED_POOL'])
+        assert call('pool.dataset.delete', dataset)
+
+
+#########################################################
 
 
 def test_create_a_passphrase_encrypted_pool(request):
@@ -389,7 +288,7 @@ def test_try_to_change_a_passphrase_encrypted_root_to_key_on_passphrase_encrypte
     payload = {
         'key': dataset_token_hex
     }
-    with pytest.raises(ValidationErrors, match=f'{dataset} has parent\\(s\\) which are encrypted with a passphrase'):
+    with pytest.raises(Exception, match=f'{dataset} has parent\\(s\\) which are encrypted with a passphrase'):
         call('pool.dataset.change_key', dataset, payload, job=True)
 
 
@@ -461,6 +360,9 @@ def test_delete_the_passphrase_encrypted_pool_with_is_datasets(request):
         'destroy': True
     }
     call('pool.export', pool_id, payload, job=True)
+
+
+############################################################################
 
 
 def test_creating_a_key_encrypted_pool(request):
