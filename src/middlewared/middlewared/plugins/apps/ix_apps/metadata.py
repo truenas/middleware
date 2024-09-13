f3002dcd1f643cde18afbf5828b2541cfe529498
M. Rehan <mrehanlm93@gmail.com>
2024-09-13 20:10:51 +0500

    Small bug fix
    (cherry picked from commit 3ee38c6e175068aeda6fdf5744e5bd988a489da7)

--- src/middlewared/middlewared/plugins/apps/ix_apps/metadata.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/metadata.py
@@ -12,7 +12,7 @@ def _load_app_yaml(yaml_path: str) -> dict[str, typing.Any]:
     """ wrapper around yaml.safe_load that ensure dict always returned """
     try:
         with open(yaml_path, 'r') as f:
-            if (data := yaml.safe_load(f.read())) is None:
+            if (data := yaml.safe_load(f)) is None:
                 # yaml.safe_load may return None if file empty
                 return {}
 
@@ -29,15 +29,14 @@ def update_app_metadata(
     app_name: str, app_version_details: dict, migrated: bool | None = None, custom_app: bool = False,
 ):
     migrated = get_app_metadata(app_name).get('migrated', False) if migrated is None else migrated
-    with open(get_installed_app_metadata_path(app_name), 'w') as f:
-        f.write(yaml.safe_dump({
+    write_if_changed(get_installed_app_metadata_path(app_name), yaml.safe_dump({
             'metadata': app_version_details['app_metadata'],
             'migrated': migrated,
             'custom_app': custom_app,
             **{k: app_version_details[k] for k in ('version', 'human_version')},
             **get_portals_and_app_notes(app_name, app_version_details['version']),
             # TODO: We should not try to get portals for custom apps for now
-        }))
+        }), perms=0o600, raise_error=False)
 
 
 def update_app_metadata_for_portals(app_name: str, version: str):
@@ -49,7 +48,7 @@ def update_app_metadata_for_portals(app_name: str, version: str):
     write_if_changed(get_installed_app_metadata_path(app_name), yaml.safe_dump({
         **app_metadata,
         **get_portals_and_app_notes(app_name, version),
-    }), perms=0o600, raise_error=True)
+    }), perms=0o600, raise_error=False)
 
 
 def get_collective_config() -> dict[str, dict]:
@@ -66,4 +65,4 @@ def update_app_yaml_for_last_update(version_path: str, last_update: str):
     app_config = _load_app_yaml(app_yaml_path)
     app_config['last_update'] = last_update
 
-    write_if_changed(app_yaml_path, yaml.safe_dump(app_config), perms=0o600, raise_error=True)
+    write_if_changed(app_yaml_path, yaml.safe_dump(app_config), perms=0o600, raise_error=False)
