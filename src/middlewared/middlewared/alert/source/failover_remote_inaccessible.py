b6a879027a9704dabde7b033c7dc0b761a878263
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-09-14 04:48:10 -0700

    fix HA inaccessible alert (#14524)
    (cherry picked from commit 275cc23224baaf8bc613fce541a1bf6219af7339)
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/alert/source/failover_remote_inaccessible.py
+++ src/middlewared/middlewared/alert/source/failover_remote_inaccessible.py
@@ -30,6 +30,9 @@ class FailoverRemoteSystemInaccessibleAlertSource(AlertSource):
         self.incident_id = None
 
     async def check(self):
+        if not await self.middleware.call('failover.licensed'):
+            return []
+
         try:
             await self.middleware.call('failover.call_remote', 'core.ping', [], {'timeout': 2})
         except Exception:
