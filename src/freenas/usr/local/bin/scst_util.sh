85ddd27f8a74b6a244e24c43e9c8170d6174f189
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2024-09-18 07:30:38 -0700

    NAS-131186 / 25.04 / Add stop-alua to scst_util.sh and call before stopping middlewared (#14520)
    * Add stop-alua to scst_util.sh and call before stopping middlewared
    
    * Increase fenced_start_loop max_retries to 4
    
    This will help avoid fenced startup issues when a reboot is slow,
    for example if a large number of iSCSI targets are present.

--- src/freenas/usr/local/bin/scst_util.sh
+++ src/freenas/usr/local/bin/scst_util.sh
@@ -8,13 +8,32 @@ force_close() {
 	wait
 }
 
+stop_alua() {
+	shopt -s nullglob
+
+	# Disable iSCSI
+	if [ -f /sys/kernel/scst_tgt/targets/iscsi/enabled ]; then
+		echo 0 > /sys/kernel/scst_tgt/targets/iscsi/enabled
+	fi
+
+	# Turn off any cluster_mode in parallel
+	for cm in /sys/kernel/scst_tgt/devices/*/cluster_mode ; do
+		echo 0 > "$cm" &
+	done
+	wait
+}
+
 case "$1" in
     force-close)
         force_close
         rc=$?
         ;;
+    stop-alua)
+        stop_alua
+        rc=$?
+        ;;
     *)
-        echo "Usage: $0 {force-close}"
+        echo "Usage: $0 {force-close|stop-alua}"
         exit 2
         ;;
 esac
