9ca2086812bc443066531a0fdafb9de3e17e0dca
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-09-22 03:44:48 +0500

    Add tests to make sure after docker is configured, catalog functions properly

--- tests/api2/test_docker_setup.py
+++ tests/api2/test_docker_setup.py
@@ -32,6 +32,21 @@ def test_docker_datasets_properties():
         assert invalid_props == {}, f'{ds_name} has invalid properties: {invalid_props}'
 
 
+@pytest.mark.dependency(depends=['docker_setup'])
+def test_catalog_synced_properly():
+    assert call('catalog.synced') is True
+
+
+@pytest.mark.dependency(depends=['docker_setup'])
+def test_catalog_sync_location():
+    assert call('catalog.config')['location'] == '/mnt/.ix-apps/truenas_catalog'
+
+
+@pytest.mark.dependency(depends=['docker_setup'])
+def test_apps_being_reported():
+    assert call('app.available', [], {'count': True}) != 0
+
+
 def test_unset_docker_pool(docker_pool):
     docker_config = call('docker.update', {'pool': None}, job=True)
     assert docker_config['pool'] is None, docker_config
