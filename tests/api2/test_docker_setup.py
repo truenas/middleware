a50e54dfac83b017aefb8df62976fbbd29d54d51
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-09-22 03:38:43 +0500

    Add a test to verify various apps datasets props

--- tests/api2/test_docker_setup.py
+++ tests/api2/test_docker_setup.py
@@ -1,7 +1,8 @@
 import pytest
 
-from middlewared.test.integration.utils import call
 from middlewared.test.integration.assets.pool import another_pool
+from middlewared.test.integration.utils import call
+from middlewared.test.integration.utils.docker import dataset_props
 
 
 @pytest.fixture(scope='module')
@@ -10,9 +11,25 @@ def docker_pool():
         yield pool['name']
 
 
+@pytest.mark.dependency(name='docker_setup')
 def test_docker_setup(docker_pool):
     docker_config = call('docker.update', {'pool': docker_pool}, job=True)
     assert docker_config['pool'] == docker_pool, docker_config
+
+
+@pytest.mark.dependency(depends=['docker_setup'])
+def test_docker_datasets_properties():
+    docker_config = call('docker.config')
+    datasets = {
+        ds['name']: ds['properties'] for ds in call('zfs.dataset.query', [['id', '^', docker_config['dataset']]])
+    }
+    for ds_name, current_props in datasets.items():
+        invalid_props = {}
+        for to_check_prop, to_check_prop_value in dataset_props(ds_name).items():
+            if current_props[to_check_prop]['value'] != to_check_prop_value:
+                invalid_props[to_check_prop] = current_props[to_check_prop]['value']
+
+        assert invalid_props == {}, f'{ds_name} has invalid properties: {invalid_props}'
 
 
 def test_unset_docker_pool(docker_pool):
