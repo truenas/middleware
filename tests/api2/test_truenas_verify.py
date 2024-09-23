a47be289a8e1306ced216dad412fd2ed697971c1
Andrew Walker <awalker@ixsystems.com>
2024-09-23 10:08:20 -0600

    Remove

--- tests/api2/test_truenas_verify.py
+++
@@ -1,9 +0,0 @@
-from middlewared.test.integration.utils import ssh
-
-
-def test_truenas_verify():
-    response = ssh('truenas_verify', check=False, complete_response=True)
-
-    # Jenkins vms alter the system files for setup, so truenas_verify should generate errors.
-    assert not response['result']
-    assert ssh('head /var/log/truenas_verify.log'), 'Test environment should log file verification errors.'
