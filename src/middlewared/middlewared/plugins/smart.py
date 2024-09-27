truenas/middleware

commit b082771ce8d1db6ff2f41498c57fc0a6a1bf50e0

UI uses `smart.test.query_for_disk` with disk names, let it be that way (#14547)

src/middlewared/middlewared/plugins/smart.py
@@ -310,14 +310,16 @@ class SMARTTestService(CRUDService):
         return verrors
 
     @accepts(Str('disk'), roles=['REPORTING_READ'])
-    async def query_for_disk(self, disk):
+    async def query_for_disk(self, disk_name):
         """
-        Query S.M.A.R.T. tests for the specified disk.
+        Query S.M.A.R.T. tests for the specified disk name.
         """
+        disk = await self.middleware.call_sync('disk.query', [['name', '=', disk_name]], {'get': True})
+
         return [
             test
             for test in await self.query()
-            if test['all_disks'] or disk in test['disks']
+            if test['all_disks'] or disk['identifier'] in test['disks']
         ]
 
     @accepts(Bool('full_disk', default=False))

commit eff901d9ac9cc9bed5ad418f593ddce0ecac9b92

Fix bug introduced in https://github.com/truenas/middleware/pull/14547

src/middlewared/middlewared/plugins/smart.py
@@ -315,7 +315,7 @@ class SMARTTestService(CRUDService):
         """
         Query S.M.A.R.T. tests for the specified disk name.
         """
-        disk = await self.middleware.call_sync('disk.query', [['name', '=', disk_name]], {'get': True})
+        disk = await self.middleware.call('disk.query', [['name', '=', disk_name]], {'get': True})
 
         return [
             test