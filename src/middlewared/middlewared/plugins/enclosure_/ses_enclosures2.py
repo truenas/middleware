cd8a6b78b4b81bf2ac9e3d0161d37df274611bad
Caleb <yocalebo@gmail.com>
2024-10-02 07:47:47 -0400

    remove call to enclosure.list_ses_enclosures

--- src/middlewared/middlewared/plugins/enclosure_/ses_enclosures2.py
+++ src/middlewared/middlewared/plugins/enclosure_/ses_enclosures2.py
@@ -15,13 +15,17 @@ def get_ses_enclosure_status(bsg_path):
         logger.error('Error querying enclosure status for %r', bsg_path, exc_info=True)
 
 
-def get_ses_enclosures(dmi):
+def get_ses_enclosures(dmi, asdict=True):
     rv = list()
     with suppress(FileNotFoundError):
         for i in Path('/sys/class/enclosure').iterdir():
             bsg = f'/dev/bsg/{i.name}'
             if (status := get_ses_enclosure_status(bsg)):
                 sg = next((i / 'device/scsi_generic').iterdir())
-                rv.append(Enclosure(bsg, f'/dev/{sg.name}', dmi, status).asdict())
+                enc = Enclosure(bsg, f'/dev/{sg.name}', dmi, status)
+                if asdict:
+                    rv.append(enc.asdict())
+                else:
+                    rv.append(enc)
 
     return rv
