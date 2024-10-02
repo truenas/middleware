4ab48979d82ab84e46df4073540c0681d23fc363
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-10-02 05:01:51 -0700

    Add CSR profiles to webui crypto plugin (#14607)
    (cherry picked from commit 386841a5325db4aa516200c5d33a76868b896a0c)
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

--- src/middlewared/middlewared/plugins/webui/crypto.py
+++ src/middlewared/middlewared/plugins/webui/crypto.py
@@ -20,3 +20,7 @@ class WebUICryptoService(Service):
     @accepts(Int('cert_id'), roles=['READONLY_ADMIN'])
     async def get_certificate_domain_names(self, cert_id):
         return await self.middleware.call('certificate.get_domain_names', cert_id)
+
+    @accepts(roles=['READONLY_ADMIN'])
+    async def csr_profiles(self):
+        return await self.middleware.call('certificate.certificate_signing_requests_profiles')
