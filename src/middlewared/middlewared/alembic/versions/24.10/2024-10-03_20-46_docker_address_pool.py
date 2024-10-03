6b7c217ae53bb3c84d33b8a2f9eabbb2761d7249
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-10-04 00:07:45 +0500

    Add migration to update default address pool
    (cherry picked from commit 5cc3fbec32861d4d364176b4473979e5d614b527)

---
+++ src/middlewared/middlewared/alembic/versions/24.10/2024-10-03_20-46_docker_address_pool.py
@@ -0,0 +1,35 @@
+"""
+Docker address pool default updated
+
+Revision ID: 92b98613c498
+Revises: c31881e67797
+Create Date: 2024-10-03 20:46:17.935672+00:00
+"""
+import json
+
+from alembic import op
+
+
+revision = '92b98613c498'
+down_revision = 'c31881e67797'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+
+    if docker_config := list(map(
+        dict, conn.execute('SELECT * FROM services_docker').fetchall()
+    )):
+        docker_config = docker_config[0]
+        address_pool_config = json.loads(docker_config['address_pools'])
+
+        if address_pool_config == [{'base': '172.30.0.0/16', 'size': 27}, {'base': '172.31.0.0/16', 'size': 27}]:
+            conn.execute("UPDATE services_docker SET address_pools = ? WHERE id = 1", [json.dumps(
+                [{"base": "172.17.0.0/12", "size": 24}]
+            )])
+
+
+def downgrade():
+    pass
