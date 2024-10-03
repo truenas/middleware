fbd2e4569c6b65bac5f243c046326141ccc43d94
Aiden Baker <baker.aiden@protonmail.com>
2024-10-03 13:55:45 -0400

    Closer than I thought

--- tests/api2/test_openssl.py
+++ tests/api2/test_openssl.py
@@ -1,25 +1,24 @@
 import pytest
 
+import subprocess
+
 from middlewared.test.integration.utils import call, ssh
 from auto_config import ha
 
 
 @pytest.mark.skipif(not ha, reason='Test only valid for HA')
 def test_fips_context():
-    print('AIDEN debug info')
-    print(call('failover.disabled.reasons'))
-    print(call('failover.call_remote', 'failover.disabled.reasons'))
-    print(call('failover.call_remote', 'core.ping', [], {'timeout': 2}))
     call('system.security.update', {'enable_fips': True}, job=True)
-    print("AIDEN enabled fips")
+    print("AIDEN - enabled fips")
     reason_1 = call('failover.call_remote', 'system.reboot.list_reasons')
     reason_2 = call('system.reboot.list_reasons')
-    fips_test = ssh('openssl list -providers')
+    fips_test = subprocess.run(["openssl", "list", "-providers"], capture_output=True).stdout
     print(fips_test, reason_1, reason_2)
-    assert '3.0.9' in fips_test
+    assert '3.0.9' in str(fips_test)
     call('system.security.update', {'enable_fips': False}, job=True)
-    print("AIDEN disabled fips")
+    print("AIDEN - disabled fips")
     reason_1 = call('failover.call_remote', 'system.reboot.list_reasons')
     reason_2 = call('system.reboot.list_reasons')
-    fips_test = ssh('openssl list -providers')
+    fips_test = subprocess.run(["openssl", "list", "-providers"],capture_output=True).stdout
+
     print(fips_test, reason_1, reason_2)
