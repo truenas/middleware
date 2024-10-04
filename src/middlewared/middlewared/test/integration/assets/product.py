79585519c57d7ae9070dd65a61919cb13fcf6987
Mark Grimes <mark.grimes@ixsystems.com>
2024-10-04 07:26:52 -0700

    Exclude test/integration.

--- src/middlewared/middlewared/test/integration/assets/product.py
+++ src/middlewared/middlewared/test/integration/assets/product.py
@@ -1,10 +1,9 @@
 import contextlib
 
-from middlewared.plugins.system.product import ProductType
 from middlewared.test.integration.utils import mock
 
 
 @contextlib.contextmanager
-def product_type(product_type=ProductType.SCALE_ENTERPRISE):
+def product_type(product_type='SCALE_ENTERPRISE'):
     with mock('system.product_type', return_value=product_type):
         yield
