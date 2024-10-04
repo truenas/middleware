68753e30f6d26f7725bee92a287ec2f15e5822df
William Grzybowski <william@grzy.org>
2024-10-04 15:04:28 -0300

    Adjust migration

--- src/middlewared/middlewared/alembic/versions/25.04/2024-10-04_11-00_virt_global.py
+++ src/middlewared/middlewared/alembic/versions/25.04/2024-10-04_11-00_virt_global.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'dd6e581235b2'
-down_revision = '6dedf12c1035'
+down_revision = '5fe28eada969'
 branch_labels = None
 depends_on = None
 
