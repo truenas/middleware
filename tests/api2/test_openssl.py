8254f217aed95e81891ddf83a8be64b531c28806
Aiden Baker <baker.aiden@protonmail.com>
2024-10-07 13:01:21 -0400

    (maybe) final test

--- tests/api2/test_openssl.py
+++ tests/api2/test_openssl.py
@@ -8,8 +8,8 @@ from auto_config import ha
 
 @pytest.mark.skipif(not ha, reason='Test only valid for HA')
 def test_fips_context():
-    payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && cat /root/osslproviders && midclt call --job system.security.update '{"enable_fips": false}'"""
+    payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && midclt call --job system.security.update '{"enable_fips": false}'"""
     print("FIPS payload")
-    print(ssh(payload, complete_response=True, check=False, timeout=300))
+    print(ssh(payload, complete_response=True, timeout=300))
     print("Were back in!")
     assert "3.0.9" in ssh("cat /root/osslproviders")
