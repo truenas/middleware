1807cb2aef8a58d033d055367f11b452175cc9f3
Logan Cary <logan.cary@ixsystems.com>
2024-10-07 16:22:24 -0400

    NAS-131462 / 25.04 / Convert alert plugin to new api style (#14615)
    * convert to new style
    
    * validate alert level
    
    * remove unused models
    
    * eliminate Ref to old alert schema
    
    * condense the added `disk.temperature_alerts` models into `alerts.py`
    
    * forgot one thing
    
    * separate disk models
    
    * import .disk

--- src/middlewared/middlewared/api/v25_04_0/__init__.py
+++ src/middlewared/middlewared/api/v25_04_0/__init__.py
@@ -1,9 +1,12 @@
 from .acme_protocol import *  # noqa
+from .alert import *  # noqa
+from .alertservice import *  # noqa
 from .api_key import *  # noqa
 from .auth import *  # noqa
 from .cloud_sync import *  # noqa
 from .common import *  # noqa
 from .core import *  # noqa
+from .disk import *  # noqa
 from .failover_reboot import *  # noqa
 from .group import *  # noqa
 from .keychain import *  # noqa
