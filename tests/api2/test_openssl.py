11e41964f4c887487d5f72ebfc6a1754ad727009
Aiden Baker <baker.aiden@protonmail.com>
2024-10-07 09:33:27 -0400

    New test method

--- tests/api2/test_openssl.py
+++ tests/api2/test_openssl.py
@@ -8,17 +8,8 @@ from auto_config import ha
 
 @pytest.mark.skipif(not ha, reason='Test only valid for HA')
 def test_fips_context():
-    call('system.security.update', {'enable_fips': True}, job=True)
-    print("AIDEN - enabled fips")
-    reason_1 = call('failover.call_remote', 'system.reboot.list_reasons')
-    reason_2 = call('system.reboot.list_reasons')
-    fips_test = subprocess.run(["openssl", "list", "-providers"], capture_output=True).stdout
-    print(fips_test, reason_1, reason_2)
-    assert '3.0.9' in str(fips_test)
-    call('system.security.update', {'enable_fips': False}, job=True)
-    print("AIDEN - disabled fips")
-    reason_1 = call('failover.call_remote', 'system.reboot.list_reasons')
-    reason_2 = call('system.reboot.list_reasons')
-    fips_test = subprocess.run(["openssl", "list", "-providers"],capture_output=True).stdout
-
-    print(fips_test, reason_1, reason_2)
+    payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && midclt call --job system.security.update '{"enable_fips": false}'"""
+    print("FIPS payload")
+    ssh(payload)
+    print("Were back in!")
+    print(ssh("cat /root/opensslproviders"))
