c6307947f67394291b297f62e740ec5957798605
Aiden Baker <baker.aiden@protonmail.com>
2024-10-08 14:58:28 -0400

    Used pytest flaky

--- tests/api2/test_openssl.py
+++ tests/api2/test_openssl.py
@@ -8,22 +8,18 @@ fips_version = "3.0.9"
 
 
 # Sometimes this test fails because the testing environment has broken failover (randomly. Fun transient error. Reports a failed heartbeat).
+@pytest.mark.flaky(reruns=retry, reruns_delay=5)
 @pytest.mark.skipif(not ha, reason='Test only valid for HA')
 def test_fips_version():
     # The reason we have a set of commands in a payload is because of some annoying FIPS technicalities.
     # Basically, when FIPS is enabled, we can't use SSH because the SSH key used by root isn't using a FIPS provided algorithm. (this might need to be investigated further)
     # To allow testing, we write our FIPS information to a file during this phase, and then go disable FIPS to get SSH back all in one joint command.
     payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && midclt call system.reboot.info >> /root/osslproviders && midclt call --job system.security.update '{"enable_fips": false}'"""
-    for i in range(retry):
-        request = ssh(payload, complete_response=True, check=False, timeout=300)
-        if request["returncode"] != 0:
-            continue
-        break
-    else:
-        request = ssh(payload, complete_response=True, timeout=300) # fail the test
 
-    enabled_info = ssh("cat /root/osslproviders")
+    ssh(payload, complete_response=True, timeout=300)
+
     # Check that things are what we expect when FIPS was enabled
+    enabled_info = ssh("cat /root/osslproviders")
     assert fips_version in enabled_info
     assert "FIPS configuration was changed." in enabled_info
 
