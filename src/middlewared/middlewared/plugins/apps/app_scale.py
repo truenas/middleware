326a56931f19b0ed130bccefc56a0830849d5b13
M. Rehan <mrehanlm93@gmail.com>
2024-10-08 05:32:10 +0500

    Don't report crashed state when stopping an app

--- src/middlewared/middlewared/plugins/apps/app_scale.py
+++ src/middlewared/middlewared/plugins/apps/app_scale.py
@@ -2,6 +2,8 @@ from middlewared.schema import accepts, Str, returns
 from middlewared.service import job, Service
 
 from .compose_utils import compose_action
+from .ix_apps.query import get_default_workload_values
+from .utils import get_app_stop_cache_key
 
 
 class AppService(Service):
@@ -18,11 +20,24 @@ class AppService(Service):
         Stop `app_name` app.
         """
         app_config = self.middleware.call_sync('app.get_instance', app_name)
-        job.set_progress(20, f'Stopping {app_name!r} app')
-        compose_action(
-            app_name, app_config['version'], 'down', remove_orphans=True, remove_images=False, remove_volumes=False,
-        )
-        job.set_progress(100, f'Stopped {app_name!r} app')
+        cache_key = get_app_stop_cache_key(app_name)
+        try:
+            self.middleware.call_sync('cache.put', cache_key, True)
+            self.middleware.send_event(
+                'app.query', 'CHANGED', id=app_name,
+                fields=app_config | {'state': 'STOPPING', 'active_workloads': get_default_workload_values()},
+            )
+            job.set_progress(20, f'Stopping {app_name!r} app')
+            compose_action(
+                app_name, app_config['version'], 'down', remove_orphans=True, remove_images=False, remove_volumes=False,
+            )
+            job.set_progress(100, f'Stopped {app_name!r} app')
+        finally:
+            self.middleware.send_event(
+                'app.query', 'CHANGED', id=app_name,
+                fields=app_config | {'state': 'STOPPED', 'active_workloads': get_default_workload_values()},
+            )
+            self.middleware.call_sync('cache.pop', cache_key)
 
     @accepts(Str('app_name'), roles=['APPS_WRITE'])
     @returns()
