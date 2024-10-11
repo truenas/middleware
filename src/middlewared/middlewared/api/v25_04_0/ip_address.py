cc777c74f4c151f6e7a24eb8b039ba5422d0196b
Aiden Baker <baker.aiden@protonmail.com>
2024-10-11 12:00:59 -0400

    Initial schema

---
+++ src/middlewared/middlewared/api/v25_04_0/ip_address.py
@@ -0,0 +1,50 @@
+from _pytest.mark import KeywordMatcher
+from libvirt import Callable
+from middlewared.api.base import BaseModel
+from typing import Literal, TypeAlias
+from ipaddress import ip_network, ip_interface, ip_address, IPv4Network, IPv4Interface, IPv4Address, IPv6Network, IPv6Interface, IPv6Address
+
+ExcludedAddrTypes: TypeAlias = Literal[
+    'MULTICAST',
+    'PRIVATE',
+    'GLOBAL',
+    'UNSPECIFIED',
+    'RESERVED',
+    'LOOPBACK',
+    'LINK_LOCAL'
+]
+
+class IpAddr(BaseModel):
+    cidr: bool = False
+    network: bool = False
+    network_strict: bool = False
+    address_types: list[ExcludedAddrTypes] = []
+    v4: bool = True
+    v6: bool = True
+    factory: Callable
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.v4 and self.v6:
+            if self.network:
+                self.factory = ip_network
+            elif self.cidr:
+                self.factory = ip_interface
+            else:
+                self.factory = ip_address
+        elif self.v4:
+            if self.network:
+                    self.factory = IPv4Network
+            elif self.cidr:
+                self.factory = IPv4Interface
+            else:
+                self.factory = IPv4Address
+        elif self.v6:
+            if self.network:
+                self.factory = IPv6Network
+            elif self.cidr:
+                self.factory = IPv6Interface
+            else:
+                self.factory = IPv6Address
+        else:
+            raise ValueError('Either IPv4 or IPv6 should be allowed')
