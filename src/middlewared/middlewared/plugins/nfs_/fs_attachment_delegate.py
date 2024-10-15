4f83dd1afea4aca7da02a3b19c1522166bdbafa7
Mark Grimes <mark.grimes@ixsystems.com>
2024-10-15 12:01:29 -0700

    tweak the doc string.

--- src/middlewared/middlewared/plugins/nfs_/fs_attachment_delegate.py
+++ src/middlewared/middlewared/plugins/nfs_/fs_attachment_delegate.py
@@ -15,7 +15,8 @@ class NFSFSAttachmentDelegate(LockableFSAttachmentDelegate):
 
     async def delete(self, attachments):
         ''' Delete NFS shares
-        The attachments is a list of shares exporting a pool that is being deleted'''
+        The attachments is a list of shares exporting a pool that is being deleted
+        '''
         for attachment in attachments:
             self.logger.debug(f"[MCG DEBUG] delete share: {attachment}")
             await self.middleware.call('sharing.nfs.delete', attachment['id'])
