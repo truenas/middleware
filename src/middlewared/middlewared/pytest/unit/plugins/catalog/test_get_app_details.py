2cad8e495cb678f193585f572e253be7c8230d3e
sonicaj <waqarahmedjoyia@live.com>
2024-10-16 22:51:59 +0500

    NAS-131767 / 25.04 / Add unit tests for catalog plugin (#14670)
    * Add unit tests for catalog plugin
    
    * Replace mocker with unittest.mock

---
+++ src/middlewared/middlewared/pytest/unit/plugins/catalog/test_get_app_details.py
@@ -0,0 +1,155 @@
+import unittest
+
+import pytest
+
+from middlewared.plugins.catalog.apps_util import get_app_details
+
+
+@pytest.mark.parametrize('app_data, versions, expected', [
+    (
+        {
+            'app_readme': '',
+            'categories': [
+                'media'
+            ],
+            'description': '',
+            'healthy': True,
+            'healthy_error': None,
+            'home': 'https://actualbudget.org',
+            'location': '/mnt/.ix-apps/truenas_catalog/trains/community/actual-budget',
+            'latest_version': '1.1.11',
+            'latest_app_version': '24.10.1',
+            'latest_human_version': '24.10.1_1.1.11',
+            'last_update': '12-02-21 00:00:00',
+            'name': 'actual-budget',
+            'recommended': False,
+            'title': 'Actual Budget',
+        },
+        {
+            '1.0.1': {
+                'name': 'chia',
+                'categories': [],
+                'app_readme': None,
+                'location': '/mnt/mypool/ix-applications/catalogs/'
+                            'github_com_truenas_charts_git_master/charts/chia',
+                'healthy': True,
+                'supported': True,
+                'healthy_error': None,
+                'required_features': [],
+                'version': '1.0.1',
+                'human_version': '1.15.12',
+                'home': None,
+                'readme': None,
+                'changelog': None,
+                'last_update': '1200-20-00 00:00:00',
+                'app_metadata': {
+                    'name': 'chia',
+                    'train': 'stable',
+                    'version': '1.0.1',
+                    'app_version': '1.0.1',
+                    'title': 'chia',
+                    'description': 'desc',
+                    'home': 'None',
+                },
+                'schema': {
+                    "groups": [],
+                    "questions": []
+                }
+            }
+        },
+        {
+            'app_readme': '',
+            'categories': ['media'],
+            'description': '',
+            'healthy': True,
+            'healthy_error': None,
+            'home': 'https://actualbudget.org',
+            'location': '/mnt/.ix-apps/truenas_catalog/trains/community/actual-budget',
+            'latest_version': '1.1.11',
+            'latest_app_version': '24.10.1',
+            'latest_human_version': '24.10.1_1.1.11',
+            'last_update': '12-02-21 00:00:00',
+            'name': 'actual-budget',
+            'recommended': False,
+            'title': 'Actual Budget',
+            'versions': {
+                '1.0.1': {
+                    'name': 'chia',
+                    'categories': [],
+                    'app_readme': None,
+                    'location': '/path/to/app/1.0.1',
+                    'healthy': True,
+                    'supported': True,
+                    'healthy_error': None,
+                    'required_features': [],
+                    'version': '1.0.1',
+                    'human_version': '1.15.12',
+                    'home': None,
+                    'readme': None,
+                    'changelog': None,
+                    'last_update': '1200-20-00 00:00:00',
+                    'app_metadata': {
+                        'name': 'chia',
+                        'train': 'stable',
+                        'version': '1.0.1',
+                        'app_version': '1.0.1',
+                        'title': 'chia',
+                        'description': 'desc',
+                        'home': 'None',
+                    },
+                    'schema': {
+                        'groups': [],
+                        'questions': []
+                    },
+                    'values': {}
+                }
+            }
+        }
+    ),
+    (
+        {
+            'app_readme': '',
+            'categories': [
+                'media'
+            ],
+            'description': '',
+            'healthy': True,
+            'healthy_error': None,
+            'home': 'https://actualbudget.org',
+            'location': '/mnt/.ix-apps/truenas_catalog/trains/community/actual-budget',
+            'latest_version': '1.1.11',
+            'latest_app_version': '24.10.1',
+            'latest_human_version': '24.10.1_1.1.11',
+            'last_update': '12-02-21 00:00:00',
+            'name': 'actual-budget',
+            'recommended': False,
+            'title': 'Actual Budget',
+        },
+        {},
+        {
+            'app_readme': '',
+            'categories': ['media'],
+            'description': '',
+            'healthy': True,
+            'healthy_error': None,
+            'home': 'https://actualbudget.org',
+            'location': '/mnt/.ix-apps/truenas_catalog/trains/community/actual-budget',
+            'latest_version': '1.1.11',
+            'latest_app_version': '24.10.1',
+            'latest_human_version': '24.10.1_1.1.11',
+            'last_update': '12-02-21 00:00:00',
+            'name': 'actual-budget',
+            'recommended': False,
+            'title': 'Actual Budget',
+            'versions': {}
+        }
+    ),
+])
+@unittest.mock.patch('middlewared.plugins.catalog.apps_util.normalize_questions')
+@unittest.mock.patch('middlewared.plugins.catalog.apps_util.retrieve_cached_versions_data')
+def test_get_app_details(mock_retrieve_cached_versions_data, mock_normalize_questions, app_data, versions, expected):
+    mock_retrieve_cached_versions_data.return_value = versions
+
+    if isinstance(expected, dict):
+        result = get_app_details('/path/to/app', app_data, {})
+        assert expected == result
---
+++ src/middlewared/middlewared/pytest/unit/plugins/catalog/test_retrieve_cached_versions_data.py
@@ -0,0 +1,82 @@
+import textwrap
+import unittest
+
+import pytest
+
+from middlewared.plugins.catalog.apps_util import retrieve_cached_versions_data
+from middlewared.service import CallError
+
+
+@pytest.mark.parametrize('file, should_work', [
+    (
+        '''
+        version: 1.0.1
+        ''',
+        False
+    ),
+    (
+        '''
+        {
+            'versions': '1.0.1'
+        }
+        ''',
+        False
+    ),
+    (
+        None,
+        False
+    ),
+    (
+        textwrap.dedent(
+            '''
+            {
+                "1.0.1": {
+                    "name": "chia",
+                    "categories": [],
+                    "app_readme": null,
+                    "location": "/mnt/mypool/ix-applications/catalogs/github_com_truenas_charts_git_master/charts/chia",
+                    "healthy": true,
+                    "supported": true,
+                    "healthy_error": null,
+                    "required_features": [],
+                    "version": "1.0.1",
+                    "human_version": "1.15.12",
+                    "home": null,
+                    "readme": null,
+                    "changelog": null,
+                    "last_update": "2024-10-09 00:00:00",
+                    "app_metadata": {
+                        "name": "chia",
+                        "train": "stable",
+                        "version": "1.0.1",
+                        "app_version": "1.0.1",
+                        "title": "chia",
+                        "description": "desc",
+                        "home": "",
+                        "sources": [],
+                        "maintainers": [],
+                        "run_as_context": [],
+                        "capabilities": [],
+                        "host_mounts": []
+                    },
+                    "schema": {
+                        "groups": [],
+                        "questions": []
+                    }
+                }
+            }
+            '''
+        ),
+        True
+    ),
+])
+@unittest.mock.patch('builtins.open', new_callable=unittest.mock.mock_open)
+def test_retrieve_cached_versions_data(mock_file, file, should_work):
+    mock_file.return_value.read.return_value = file
+
+    if should_work:
+        result = retrieve_cached_versions_data('/path/to/app', 'actual-budget')
+        assert isinstance(result, dict)
+    else:
+        with pytest.raises(CallError):
+            retrieve_cached_versions_data('/path/to/app', 'actual-budget')
