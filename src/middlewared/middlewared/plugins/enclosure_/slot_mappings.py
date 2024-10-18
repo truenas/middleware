8644e5e8b74af13312d67fd920861e05998a858f
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2024-10-18 13:26:01 -0400

    fix typo in R10 drive mapping (#14713)

--- src/middlewared/middlewared/plugins/enclosure_/slot_mappings.py
+++ src/middlewared/middlewared/plugins/enclosure_/slot_mappings.py
@@ -420,7 +420,7 @@ def get_slot_info(enc):
                         enc.model: {
                             1: {SYSFS_SLOT_KEY: 0, MAPPED_SLOT_KEY: 1, SUPPORTS_IDENTIFY_KEY: False},
                             5: {SYSFS_SLOT_KEY: 4, MAPPED_SLOT_KEY: 2, SUPPORTS_IDENTIFY_KEY: False},
-                            9: {SYSFS_SLOT_KEY: 5, MAPPED_SLOT_KEY: 3, SUPPORTS_IDENTIFY_KEY: False},
+                            9: {SYSFS_SLOT_KEY: 8, MAPPED_SLOT_KEY: 3, SUPPORTS_IDENTIFY_KEY: False},
                             13: {SYSFS_SLOT_KEY: 12, MAPPED_SLOT_KEY: 4, SUPPORTS_IDENTIFY_KEY: False},
                             2: {SYSFS_SLOT_KEY: 1, MAPPED_SLOT_KEY: 5, SUPPORTS_IDENTIFY_KEY: False},
                             6: {SYSFS_SLOT_KEY: 5, MAPPED_SLOT_KEY: 6, SUPPORTS_IDENTIFY_KEY: False},
