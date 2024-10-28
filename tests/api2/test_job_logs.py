03bc8f82bbffd18c0ab405c05da01413097734b7
themylogin <themylogin@gmail.com>
2024-10-28 07:47:50 +0100

    Fix `__job_by_credential_and_id` crashing for non-full-admins and internally ran jobs

--- tests/api2/test_job_logs.py
+++ tests/api2/test_job_logs.py
@@ -1,10 +1,20 @@
+import errno
+
+import pytest
 import requests
 
+from middlewared.service_exception import CallError
 from middlewared.test.integration.assets.account import unprivileged_user_client
-from middlewared.test.integration.utils import mock, url
+from middlewared.test.integration.utils import call, mock, url
 
 
-def test_job_download_logs():
+@pytest.fixture(scope="module")
+def c():
+    with unprivileged_user_client(allowlist=[{"method": "CALL", "resource": "test.test1"}]) as c:
+        yield c
+
+
+def test_job_download_logs(c):
     with mock("test.test1", """    
         from middlewared.service import job
 
@@ -12,16 +22,37 @@ def test_job_download_logs():
         def mock(self, job, *args):
             job.logs_fd.write(b'Job logs')
     """):
-        with unprivileged_user_client(allowlist=[{"method": "CALL", "resource": "test.test1"}]) as c:
-            jid = c.call("test.test1")
+        jid = c.call("test.test1")
 
-            c.call("core.job_wait", jid, job=True)
+        c.call("core.job_wait", jid, job=True)
 
-            path = c.call("core.job_download_logs", jid, 'logs.txt')
+        path = c.call("core.job_download_logs", jid, 'logs.txt')
 
-            r = requests.get(f"{url()}{path}")
-            r.raise_for_status()
+        r = requests.get(f"{url()}{path}")
+        r.raise_for_status()
 
-            assert r.headers["Content-Disposition"] == "attachment; filename=\"logs.txt\""
-            assert r.headers["Content-Type"] == "application/octet-stream"
-            assert r.text == "Job logs"
+        assert r.headers["Content-Disposition"] == "attachment; filename=\"logs.txt\""
+        assert r.headers["Content-Type"] == "application/octet-stream"
+        assert r.text == "Job logs"
+
+
+def test_job_download_logs_unprivileged_downloads_internal_logs(c):
+    with mock("test.test1", """
+        def mock(self, *args):
+            job = self.middleware.call_sync("test.test2")
+            job.wait_sync(raise_error=True)
+            return job.id
+    """):
+        with mock("test.test2", """
+            from middlewared.service import job
+
+            @job(logs=True)
+            def mock(self, job, *args):
+                job.logs_fd.write(b'Job logs')
+        """):
+            jid = call("test.test1")
+
+            with pytest.raises(CallError) as ve:
+                c.call("core.job_download_logs", jid, 'logs.txt')
+
+            assert ve.value.errno == errno.EPERM
