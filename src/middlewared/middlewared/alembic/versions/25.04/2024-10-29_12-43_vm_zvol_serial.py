7392f25ea7a4b49be256bd9682e593472f15d9bc
M. Rehan <mrehanlm93@gmail.com>
2024-10-29 13:58:50 +0500

    Make sure serial is specified for all VM disk devices

---
+++ src/middlewared/middlewared/alembic/versions/25.04/2024-10-29_12-43_vm_zvol_serial.py
@@ -0,0 +1,48 @@
+"""
+Add serial to VM zvol disk devices
+
+Revision ID: a4ce4939b908
+Revises: dd6e581235b2
+Create Date: 2024-10-29 12:43:45.960915+00:00
+
+"""
+import json
+from secrets import choice
+from string import ascii_letters, digits, punctuation
+
+from alembic import op
+
+
+revision = 'a4ce4939b908'
+down_revision = 'dd6e581235b2'
+branch_labels = None
+depends_on = None
+
+
+def generate_string(string_size=8, punctuation_chars=False, extra_chars=None):
+    """
+    Generate a cryptographically secure random string of size `string_size`.
+    If `punctuation_chars` is True, then punctuation characters will be added to the string.
+    Otherwise, only ASCII (upper and lower) and digits (0-9) are used to generate the string.
+    """
+    initial_string = ascii_letters + digits
+    if punctuation_chars:
+        initial_string += punctuation
+    if extra_chars is not None and isinstance(extra_chars, str):
+        initial_string += extra_chars
+
+    # remove any duplicates since extra_chars is user-provided
+    initial_string = ''.join(set(initial_string))
+    return ''.join(choice(initial_string) for i in range(string_size))
+
+
+def upgrade():
+    conn = op.get_bind()
+    for device in conn.execute("SELECT * FROM vm_device WHERE dtype IN ('DISK', 'RAW')").fetchall():
+        attributes = json.loads(device['attributes'])
+        attributes['serial'] = generate_string(string_size=8)
+        conn.execute("UPDATE vm_device SET attributes = ? WHERE id = ?", (json.dumps(attributes), device['id']))
+
+
+def downgrade():
+    pass
