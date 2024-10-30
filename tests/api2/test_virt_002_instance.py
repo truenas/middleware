f2743e7316d0b8d09ca87abcb86c3eef63dbc3bb
William Grzybowski <56250+william-gr@users.noreply.github.com>
2024-10-30 09:29:47 -0300

    NAS-130523 / 25.04 / Archlinux has been failing to install netcat with 404 error (#14811)

--- tests/api2/test_virt_002_instance.py
+++ tests/api2/test_virt_002_instance.py
@@ -8,6 +8,18 @@ from middlewared.test.integration.utils.ssh import ssh
 
 from auto_config import pool_name
 
+INS1_NAME = 'debian'
+INS1_OS = 'Debian'
+INS1_IMAGE = 'debian/trixie'
+
+INS2_NAME = 'void'
+INS2_OS = 'Void Linux'
+INS2_IMAGE = 'voidlinux/musl'
+
+INS3_NAME = 'centos'
+INS3_OS = 'CentOS'
+INS3_IMAGE = 'centos/9-Stream'
+
 
 def clean():
     call('virt.global.update', {'pool': None}, job=True)
@@ -27,58 +39,58 @@ def test_virt_instance_create():
         c.subscribe('virt.instance.agent_running', wait_debian, sync=True)
 
         # Create first so there is time for the agent to start
-        call('virt.instance.create', {'name': 'debian', 'image': 'debian/trixie', 'instance_type': 'VM'}, job=True)
+        call('virt.instance.create', {'name': INS1_NAME, 'image': INS1_IMAGE, 'instance_type': 'VM'}, job=True)
 
-        call('virt.instance.create', {'name': 'void', 'image': 'voidlinux/musl'}, job=True)
-        ssh('incus exec void cat /etc/os-release | grep "Void Linux"')
+        call('virt.instance.create', {'name': INS2_NAME, 'image': INS2_IMAGE}, job=True)
+        ssh(f'incus exec {INS2_NAME} cat /etc/os-release | grep "{INS2_OS}"')
 
         call('virt.instance.create', {
-            'name': 'arch',
-            'image': 'archlinux/current/default',
+            'name': INS3_NAME,
+            'image': INS3_IMAGE,
             'devices': [
                 {'name': 'tpm', 'dev_type': 'TPM', 'path': '/dev/tpm0', 'pathrm': '/dev/tmprm0'},
             ],
         }, job=True)
-        ssh('incus exec arch cat /etc/os-release | grep "Arch Linux"')
+        ssh(f'incus exec {INS3_NAME} cat /etc/os-release | grep "{INS3_OS}"')
 
-        devices = call('virt.instance.device_list', 'arch')
+        devices = call('virt.instance.device_list', INS3_NAME)
         assert any(i for i in devices if i['name'] == 'tpm'), devices
 
         assert wait_agent.wait(timeout=60)
-        ssh('incus exec debian cat /etc/os-release | grep "Debian"')
+        ssh(f'incus exec {INS1_NAME} cat /etc/os-release | grep "{INS1_OS}"')
 
 
 def test_virt_instance_update():
-    call('virt.instance.update', 'void', {'cpu': '1', 'memory': 500 * 1024 * 1024, 'environment': {'FOO': 'BAR'}}, job=True)
-    ssh('incus exec void grep MemTotal: /proc/meminfo|grep 512000')
+    call('virt.instance.update', INS2_NAME, {'cpu': '1', 'memory': 500 * 1024 * 1024, 'environment': {'FOO': 'BAR'}}, job=True)
+    ssh(f'incus exec {INS2_NAME} grep MemTotal: /proc/meminfo|grep 512000')
     # Checking CPUs seems to cause a racing condition (perhaps CPU currently in use in the container?)
     # rv = ssh('incus exec void cat /proc/cpuinfo |grep processor|wc -l')
     # assert rv.strip() == '1'
-    rv = ssh('incus exec void env | grep ^FOO=')
+    rv = ssh(f'incus exec {INS2_NAME} env | grep ^FOO=')
     assert rv.strip() == 'FOO=BAR'
 
 
 def test_virt_instance_stop():
     # Stop only one of them so the others are stopped during delete
-    assert ssh('incus list void -f json| jq ".[].status"').strip() == '"Running"'
-    instance = call('virt.instance.query', [['id', '=', 'void']], {'get': True})
+    assert ssh(f'incus list {INS2_NAME} -f json| jq ".[].status"').strip() == '"Running"'
+    instance = call('virt.instance.query', [['id', '=', INS2_NAME]], {'get': True})
     assert instance['status'] == 'RUNNING'
-    call('virt.instance.stop', 'void', {'force': True}, job=True)
-    instance = call('virt.instance.query', [['id', '=', 'void']], {'get': True})
+    call('virt.instance.stop', INS2_NAME, {'force': True}, job=True)
+    instance = call('virt.instance.query', [['id', '=', INS2_NAME]], {'get': True})
     assert instance['status'] == 'STOPPED'
-    assert ssh('incus list void -f json| jq ".[].status"').strip() == '"Stopped"'
+    assert ssh(f'incus list {INS2_NAME} -f json| jq ".[].status"').strip() == '"Stopped"'
 
 
 def test_virt_instance_device_add():
-    assert ssh('incus list debian -f json| jq ".[].status"').strip() == '"Running"'
-    call('virt.instance.stop', 'debian', {'force': True}, job=True)
+    assert ssh(f'incus list {INS1_NAME} -f json| jq ".[].status"').strip() == '"Running"'
+    call('virt.instance.stop', INS1_NAME, {'force': True}, job=True)
 
-    call('virt.instance.device_add', 'debian', {
+    call('virt.instance.device_add', INS1_NAME, {
         'name': 'tpm',
         'dev_type': 'TPM',
     })
 
-    call('virt.instance.device_add', 'arch', {
+    call('virt.instance.device_add', INS3_NAME, {
         'name': 'proxy',
         'dev_type': 'PROXY',
         'source_proto': 'TCP',
@@ -95,9 +107,9 @@ def test_virt_instance_device_add():
     #     'destination': '/host',
     # })
 
-    devices = call('virt.instance.device_list', 'debian')
+    devices = call('virt.instance.device_list', INS1_NAME)
     assert any(i for i in devices if i['name'] == 'tpm'), devices
-    devices = call('virt.instance.device_list', 'arch')
+    devices = call('virt.instance.device_list', INS3_NAME)
     assert any(i for i in devices if i['name'] == 'proxy'), devices
     # assert 'disk1' in devices, devices
 
@@ -108,50 +120,50 @@ def test_virt_instance_device_add():
 
     with client() as c:
         c.subscribe('virt.instance.agent_running', wait_debian, sync=True)
-        call('virt.instance.start', 'debian', job=True)
+        call('virt.instance.start', INS1_NAME, job=True)
         assert wait_agent.wait(timeout=30)
 
     ssh('incus exec debian ls /dev/tpm0')
     # ssh('incus exec debian ls /host')
 
     with dataset('virtshare') as ds:
-        call('virt.instance.device_add', 'arch', {
+        call('virt.instance.device_add', INS3_NAME, {
             'name': 'disk1',
             'dev_type': 'DISK',
             'source': f'/mnt/{ds}',
             'destination': '/host',
         })
-        devices = call('virt.instance.device_list', 'arch')
+        devices = call('virt.instance.device_list', INS3_NAME)
         assert any(i for i in devices if i['name'] == 'disk1'), devices
         with mkfile(f'/mnt/{ds}/testfile'):
-            ssh('incus exec arch ls /host/testfile')
-        call('virt.instance.device_delete', 'arch', 'disk1')
+            ssh(f'incus exec {INS3_NAME} ls /host/testfile')
+        call('virt.instance.device_delete', INS3_NAME, 'disk1')
 
 
 def test_virt_instance_proxy():
-    ssh('incus exec arch -- pacman -S --noconfirm openbsd-netcat')
-    ssh('incus exec -T arch -- bash -c "nohup nc -l 0.0.0.0 80 > /tmp/nc 2>&1 &"')
+    ssh(f'incus exec {INS3_NAME} -- yum install -y nmap-ncat')
+    ssh(f'incus exec -T {INS3_NAME} -- bash -c "nohup nc -l 0.0.0.0 80 > /tmp/nc 2>&1 &"')
     ssh('echo "foo" | nc -w 1 localhost 8005 || true')
-    rv = ssh('incus exec arch -- cat /tmp/nc')
+    rv = ssh(f'incus exec {INS3_NAME} -- cat /tmp/nc')
 
     assert rv.strip() == 'foo'
 
 
 def test_virt_instance_device_delete():
-    call('virt.instance.stop', 'debian', {'force': True}, job=True)
-    call('virt.instance.device_delete', 'debian', 'tpm')
-    devices = call('virt.instance.device_list', 'debian')
+    call('virt.instance.stop', INS1_NAME, {'force': True}, job=True)
+    call('virt.instance.device_delete', INS1_NAME, 'tpm')
+    devices = call('virt.instance.device_list', INS1_NAME)
     assert not any(i for i in devices if i['name'] == 'tpm'), devices
 
 
 def test_virt_instance_delete():
-    call('virt.instance.delete', 'void', job=True)
-    ssh('incus config show void 2>&1 | grep "not found"')
+    call('virt.instance.delete', INS2_NAME, job=True)
+    ssh(f'incus config show {INS2_NAME} 2>&1 | grep "not found"')
 
-    call('virt.instance.delete', 'arch', job=True)
-    ssh('incus config show arch 2>&1 | grep "not found"')
+    call('virt.instance.delete', INS3_NAME, job=True)
+    ssh(f'incus config show {INS3_NAME} 2>&1 | grep "not found"')
 
-    call('virt.instance.delete', 'debian', job=True)
-    ssh('incus config show debian 2>&1 | grep "not found"')
+    call('virt.instance.delete', INS1_NAME, job=True)
+    ssh(f'incus config show {INS1_NAME} 2>&1 | grep "not found"')
 
     assert len(call('virt.instance.query')) == 0
