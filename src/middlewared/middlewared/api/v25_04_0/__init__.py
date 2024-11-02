878c92bd41cce92824a2ca21ecb6a5432d9cccda
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-02 16:45:55 +0500

    Refactor acme_protocol filename to reflect actual namespace

--- src/middlewared/middlewared/api/v25_04_0/__init__.py
+++ src/middlewared/middlewared/api/v25_04_0/__init__.py
@@ -1,5 +1,5 @@
-from .acme_protocol import *  # noqa
 from .acl import *  # noqa
+from .acme_registration import *  # noqa
 from .alert import *  # noqa
 from .alertservice import *  # noqa
 from .api_key import *  # noqa
