cf3379dd7bbd5c1b7528f862d62a1157d80c1ed4
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-02 19:55:01 +0500

    Specify pydantic core schema for host path field

--- src/middlewared/middlewared/api/base/types/base/path.py
+++ src/middlewared/middlewared/api/base/types/base/path.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from typing import Any
+
+from pydantic_core import core_schema, CoreSchema
 
 
 __all__ = ['FilePath', 'HostPath']
@@ -16,6 +19,17 @@ class HostPath(str):
         if not path.exists():
             raise ValueError(f'Path does not exist (underlying dataset may be locked or the path is just missing).')
         return str(value)
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type: Any, handler: Any) -> CoreSchema:
+        # Define a core schema that treats this as a string in JSON while applying validation
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.no_info_after_validator_function(
+                cls.validate_path,
+                core_schema.str_schema(),
+            ),
+        )
 
 
 class FilePath(HostPath):
