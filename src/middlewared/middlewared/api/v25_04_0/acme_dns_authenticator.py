544f9657fd756707564b3a2c9167d4f78a43fcc9
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-02 19:42:09 +0500

    Specify schemas for different authenticators we have

--- src/middlewared/middlewared/api/v25_04_0/acme_dns_authenticator.py
+++ src/middlewared/middlewared/api/v25_04_0/acme_dns_authenticator.py
@@ -1,14 +1,20 @@
-from pydantic import ConfigDict, Field, Secret
+from typing import Literal
+
+from lexicon.providers.ovh import ENDPOINTS
+from pydantic import ConfigDict, conint, Field, Secret
 
 from middlewared.api.base import (
-    BaseModel, Excluded, excluded_field, single_argument_args, ForUpdateMetaclass, LongString,
+    BaseModel, Excluded, excluded_field, single_argument_args, ForUpdateMetaclass, LongString, NonEmptyString,
 )
+from middlewared.api.base.types import FilePath
+
 
 __all__ = [
     'ACMEDNSAuthenticatorEntry', 'ACMEDNSAuthenticatorCreateArgs', 'ACMEDNSAuthenticatorCreateResult',
     'ACMEDNSAuthenticatorUpdateArgs', 'ACMEDNSAuthenticatorUpdateResult', 'ACMEDNSAuthenticatorDeleteArgs',
     'ACMEDNSAuthenticatorDeleteResult', 'ACMEDNSAuthenticatorSchemasArgs', 'ACMEDNSAuthenticatorSchemasResult',
-    'ACMEDNSAuthenticatorPerformChallengeArgs', 'ACMEDNSAuthenticatorPerformChallengeResult',
+    'ACMEDNSAuthenticatorPerformChallengeArgs', 'ACMEDNSAuthenticatorPerformChallengeResult', 'Route53SchemaArgs',
+    'ACMECustomDNSAuthenticatorReturns', 'CloudFlareSchemaArgs', 'OVHSchemaArgs', 'ShellSchemaArgs',
 ]
 
 
@@ -63,6 +69,39 @@ class ACMEDNSAuthenticatorPerformChallengeResult(BaseModel):
 
 
 ### Custom ACME DNS Authenticator Schemas
+
+
+class ACMECustomDNSAuthenticatorReturns(BaseModel):
+    result: dict
+
+
+@single_argument_args('cloudflare')
+class CloudFlareSchemaArgs(BaseModel):
+    cloudflare_email: NonEmptyString = Field(..., description='Cloudflare Email')
+    api_key: Secret[NonEmptyString] = Field(..., description='API Key')
+    api_token: Secret[NonEmptyString] = Field(..., description='API Token')
+
+
+@single_argument_args('ovh')
+class OVHSchemaArgs(BaseModel):
+    application_key: NonEmptyString = Field(..., description='OVH Application Key')
+    application_secret: NonEmptyString = Field(..., description='OVH Application Secret')
+    consumer_key: NonEmptyString = Field(..., description='OVH Consumer Key')
+    endpoint: Literal[tuple(ENDPOINTS.keys())] = Field(..., description='OVH Endpoint')
+
+
+@single_argument_args('route53')
+class Route53SchemaArgs(BaseModel):
+    access_key_id: NonEmptyString = Field(..., description='AWS Access Key ID')
+    secret_access_key: NonEmptyString = Field(..., description='AWS Secret Access Key')
+
+
+@single_argument_args('shell')
+class ShellSchemaArgs(BaseModel):
+    script: FilePath = Field(..., description='Authentication Script')
+    user: NonEmptyString = Field(description='Running user', default='nobody')
+    timeout: conint(ge=5) = Field(description='Script Timeout', default=60)
+    delay: conint(ge=10) = Field(description='Propagation delay', default=60)
 
 
 class ACMEDNSAuthenticatorAttributeSchema(BaseModel):
