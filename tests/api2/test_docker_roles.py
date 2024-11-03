85b17fb53a0ab19284cf03089145a6e5b4115cf5
themylogin <themylogin@gmail.com>
2024-11-03 14:17:47 +0100

    Fix docker roles tests (#14854)

--- tests/api2/test_docker_roles.py
+++ tests/api2/test_docker_roles.py
@@ -10,9 +10,9 @@ from middlewared.test.integration.assets.roles import common_checks
     ('docker.config', 'DOCKER_READ', True, False),
     ('docker.config', 'DOCKER_WRITE', True, False),
     ('docker.config', 'CATALOG_READ', False, False),
-    ('docker.lacks_nvidia_drivers', 'DOCKER_READ', True, False),
-    ('docker.lacks_nvidia_drivers', 'DOCKER_WRITE', True, False),
-    ('docker.lacks_nvidia_drivers', 'CATALOG_READ', False, False),
+    ('docker.nvidia_status', 'DOCKER_READ', True, False),
+    ('docker.nvidia_status', 'DOCKER_WRITE', True, False),
+    ('docker.nvidia_status', 'CATALOG_READ', False, False),
     ('docker.update', 'DOCKER_READ', False, False),
     ('docker.update', 'DOCKER_WRITE', True, True),
 ))
