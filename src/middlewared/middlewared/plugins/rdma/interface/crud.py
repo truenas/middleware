59bf05dc291578453c560c7d108842c1b2eb1364
Brian M <brian.meagher@ixsystems.com>
2024-11-04 16:02:57 -0800

    Robustize rdma.interface.delete

--- src/middlewared/middlewared/plugins/rdma/interface/crud.py
+++ src/middlewared/middlewared/plugins/rdma/interface/crud.py
@@ -97,9 +97,12 @@ class RDMAInterfaceService(CRUDService):
         data = await self.get_instance(id_)
 
         # Attempt to remove the live configuration
-        result = await self.middleware.call('rdma.interface.configure_interface', data['node'], data['ifname'], None)
-        if not result:
-            self.logger.warn("Failed to delete active RDMA interface configuration")
+        try:
+            result = await self.middleware.call('rdma.interface.configure_interface', data['node'], data['ifname'], None)
+            if not result:
+                self.logger.warn("Failed to delete active RDMA interface configuration")
+        except Exception:
+            self.logger.error('Failed to remove live RDMA configuration', exc_info=True)
 
         # Now delete the entry
         return await self.middleware.call('datastore.delete', self._config.datastore, id_)
