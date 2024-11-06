dffc54f9894e092407edd3e7a36c57ee371bb971
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2024-11-06 13:28:02 -0500

    NAS-132307 / 25.04 / remove accepts from CacheService plugin (#14881)
    * remove accepts from CacheService plugin
    
    * address review
    
    * moar reviewwww

--- src/middlewared/middlewared/plugins/cache.py
+++ src/middlewared/middlewared/plugins/cache.py
@@ -1,81 +1,58 @@
-from middlewared.schema import Any, Str, Int, accepts
-from middlewared.service import Service, private
-
 from collections import namedtuple
-import time
+from collections.abc import Callable
+from time import monotonic
+from typing import Any
+
+from middlewared.service import Service
 
 
 class CacheService(Service):
-
     class Config:
         private = True
 
     def __init__(self, *args, **kwargs):
         super(CacheService, self).__init__(*args, **kwargs)
         self.__cache = {}
-        self.kv_tuple = namedtuple('Cache', ['value', 'timeout'])
+        self.kv_tuple = namedtuple("Cache", ["value", "timeout"])
 
-    @accepts(Str('key'))
-    def has_key(self, key):
-        """
-        Check if given `key` is in cache.
-        """
+    def has_key(self, key: str):
+        """Check if given `key` is in cache."""
         return key in self.__cache
 
-    @accepts(Str('key'))
-    def get(self, key):
+    def get(self, key: str):
         """
         Get `key` from cache.
 
         Raises:
             KeyError: not found in the cache
         """
-
         if self.__cache[key].timeout > 0:
             self.get_timeout(key)
-
         return self.__cache[key].value
 
-    @accepts(Str('key'), Any('value'), Int('timeout', default=0))
-    def put(self, key, value, timeout):
-        """
-        Put `key` of `value` in the cache.
-        """
-
+    def put(self, key: str, value: Any, timeout: int = 0):
+        """Put `key` of `value` in the cache."""
         if timeout != 0:
-            timeout = time.monotonic() + timeout
+            timeout = monotonic() + timeout
+        self.__cache[key] = self.kv_tuple(value=value, timeout=timeout)
 
-        v = self.kv_tuple(value=value, timeout=timeout)
-        self.__cache[key] = v
-
-    @accepts(Str('key'))
-    def pop(self, key):
-        """
-        Removes and returns `key` from cache.
-        """
+    def pop(self, key: str):
+        """Removes and returns `key` from cache."""
         cache = self.__cache.pop(key, None)
-
         if cache is not None:
             cache = cache.value
-
         return cache
 
-    @private
-    def get_timeout(self, key):
-        """
-        Check if 'key' has expired
-        """
-        now = time.monotonic()
+    def get_timeout(self, key: str):
+        """Check if 'key' has expired"""
+        now = monotonic()
         value, timeout = self.__cache[key]
-
         if now >= timeout:
             # Bust the cache
             del self.__cache[key]
+            raise KeyError(f"{key} has expired")
 
-            raise KeyError(f'{key} has expired')
-
-    @private
-    def get_or_put(self, key, timeout, method):
+    def get_or_put(self, key: str, timeout: int, method: Callable):
         try:
             return self.get(key)
         except KeyError:
