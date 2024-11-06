577f2d69a3255be870814992980e2da33a915075
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2024-11-06 14:15:35 -0500

    add hardware.virtualization plugin (#14886)

---
+++ src/middlewared/middlewared/plugins/hardware/virt_detection.py
@@ -0,0 +1,19 @@
+import subprocess
+
+from middlewared.service import Service
+from middlewared.utils.functools_ import cache
+
+
+class HardwareVirtualization(Service):
+    class Config:
+        namespace = "hardware.virtualization"
+        private = True
+
+    @cache
+    def variant(self) -> str:
+        rv = subprocess.run(["systemd-detect-virt"], capture_output=True)
+        return rv.stdout.decode().strip()
+
+    def is_virtualized(self) -> bool:
+        """Detect if the TrueNAS system is virtualized"""
+        return self.variant() != 'none'
