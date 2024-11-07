e48d07afef963be7f31d20260aba204760e249c0
M. Rehan <mrehanlm93@gmail.com>
2024-11-07 20:26:19 +0500

    Merge migration

---
+++ src/middlewared/middlewared/alembic/versions/25.04/2024-11-07_20-25_merge.py
@@ -0,0 +1,20 @@
+"""
+Merge
+
+Revision ID: f077d0ae123d
+Revises: 0ec74f350c66, b5829f78cec3
+Create Date: 2024-11-07 20:25:59.547731+00:00
+"""
+
+revision = 'f077d0ae123d'
+down_revision = ('0ec74f350c66', 'b5829f78cec3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
