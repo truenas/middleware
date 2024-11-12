a72860d5723ef348078d4ddab181f7fe9e05d5b4
caleb <yocalebo@gmail.com>
2024-11-12 09:19:56 -0500

    update test

--- tests/api2/test_boot_environments.py
+++ tests/api2/test_boot_environments.py
@@ -36,6 +36,8 @@ def simulate_can_activate_is_false(be_ds):
 
 
 def validate_activated_be(be_name, activate_string="R"):
+    """Validate the boot environment shows that it is activated
+    according to OS perspective."""
     for line in ssh("zectl list -H").splitlines():
         values = line.split()
         be, activated = values[0], values[1]
@@ -73,91 +75,50 @@ def test_failure_conditions_for_activate(orig_be):
 
 
 def test_clone_activate_keep_and_destroy(orig_be):
+    """Perform various boot environment operations via the API
+    and verify functionality works as expected.
     """
-    1. clone the active BE via api
-    2. verify the cloned BE shows up in API
-    3. verify the cloned BE exists on OS
-    4. test activating non-functional BE fails
-    5. activate the cloned BE via api
-    6. verify the cloned BE shows activated in the API
-    7. verify the cloned BE is activated on OS
-    8. activate original BE
-    9. verify the original BE is activated on OS
-    10. mark a BE to be kept
-    11. verify the BE is marked kept via the api
-    12. verify the BE is marked kept on OS
-    13. mark BE as to be NOT kept
-    14. verify the BE is marked as not kept via the api
-    15. verify the BE is marked not kept on OS
-    16. destroy the cloned BE via api
-    17. destroy the cloned BE is destroyed via the api
-    18. verify the cloned BE is destroyed on OS
-    """
-    # step 1
-    call("boot.environment.clone", {"id": orig_be["id"], "target": TEMP_BE_NAME})
+    tmp = call("boot.environment.clone", {"id": orig_be["id"], "target": TEMP_BE_NAME})
+    assert tmp["id"] == TEMP_BE_NAME
 
-    # step 2
-    tmp = be_query(TEMP_BE_NAME)
-
-    # step 3
     rv = ssh("zectl list -H").strip()
     assert TEMP_BE_NAME in rv, rv
 
-    # step 4
     with simulate_can_activate_is_false(tmp["dataset"]):
         with pytest.raises(ValidationError) as ve:
             call("boot.environment.activate", {"id": tmp["id"]})
     assert ve.value.attribute == "boot.environment.activate"
     assert ve.value.errmsg == f"{tmp['id']!r} can not be activated"
 
-    # step 5
-    call("boot.environment.activate", {"id": TEMP_BE_NAME})
+    rv = call("boot.environment.activate", {"id": TEMP_BE_NAME})
+    assert rv["id"] == TEMP_BE_NAME
+    assert rv["activated"] is True
 
-    # step 6
-    rv = be_query(TEMP_BE_NAME)
-    assert rv["activated"], rv
-
-    # step 7
     validate_activated_be(TEMP_BE_NAME)
 
-    # step 8
-    call("boot.environment.activate", {"id": orig_be["id"]})
-    rv = be_query(orig_be["id"])
-    assert rv["activated"], rv
+    rv = call("boot.environment.activate", {"id": orig_be["id"]})
+    assert rv["activated"] is True
 
-    # step 9
     validate_activated_be(orig_be["id"], activate_string="NR")
 
-    # step 10
-    call("boot.environment.keep", {"id": orig_be["id"], "value": True})
+    rv = call("boot.environment.keep", {"id": orig_be["id"], "value": True})
+    assert rv["keep"] is True
 
-    # step 11
-    rv = be_query(orig_be["id"])
-    assert rv["keep"] is True, rv
-
-    # step 12
     values = get_zfs_property(orig_be["dataset"], "zectl:keep")
     assert values[2] == "True"
 
-    # step 13
-    call("boot.environment.keep", {"id": orig_be["id"], "value": False})
+    rv = call("boot.environment.keep", {"id": orig_be["id"], "value": False})
+    assert rv["keep"] is False
 
-    # step 14
-    rv = be_query(orig_be["id"])
-    assert rv["keep"] is False, rv
-
-    # step 15
     values = get_zfs_property(orig_be["dataset"], "zectl:keep")
     assert values[2] == "False"
 
-    # step 16
-    call("boot.environment.destroy", {"id": TEMP_BE_NAME})
+    rv = call("boot.environment.destroy", {"id": TEMP_BE_NAME})
+    assert rv is None
 
-    # step 17
-    rv = be_query(TEMP_BE_NAME, get=False)
-    assert not rv, rv
+    rv = call("boot.environment.query", [["id", "=", TEMP_BE_NAME]])
+    assert rv == [], rv
 
-    # step 18
     rv = ssh("zectl list -H").strip()
     assert TEMP_BE_NAME not in rv, rv
 
