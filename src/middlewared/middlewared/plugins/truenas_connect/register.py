722f5893b1d4a25208c8bdabbfd21c4ba99846ff
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-13 12:49:10 +0500

    Add docstring

--- src/middlewared/middlewared/plugins/truenas_connect/register.py
+++ src/middlewared/middlewared/plugins/truenas_connect/register.py
@@ -16,6 +16,13 @@ class TrueNASConnectService(Service):
 
     @api_method(TNCGetRegistrationURIArgs, TNCGetRegistrationURIResult)
     async def get_registration_uri(self):
+        """
+        Return the registration URI for TrueNAS Connect.
+
+        When this endpoint will be called, a token will be generated which will be used to assist with
+        initial setup with truenas connect but if during the initial setup with truenas connect, middleware
+        is restarted, the process will have to be initiated again.
+        """
         query_params = {
             'version': await self.middleware.call('system.version_short'),
             'model': (await self.middleware.call('truenas.get_chassis_hardware')).removeprefix('TRUENAS-'),
