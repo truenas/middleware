5eab068648ff403a9762fa44a354d4086e41ad24
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-17 15:11:18 +0500

    Add logic to finalize system registration

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -3,4 +3,4 @@ import urllib.parse
 
 BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
 REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
-REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(BASE_URL, 'v1/systems/finalize')
+REGISTRATION_FINALIZATION_URI = 'https://account-service.dev.ixsystems.net/v1/systems/finalize'
