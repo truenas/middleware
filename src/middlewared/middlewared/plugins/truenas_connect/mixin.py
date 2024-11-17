911353acdc815c69f7c03c69d4d53574fed7ace8
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-17 14:34:03 +0500

    Add tnc mixin

---
+++ src/middlewared/middlewared/plugins/truenas_connect/mixin.py
@@ -0,0 +1,32 @@
+import asyncio
+import json
+
+import aiohttp
+import async_timeout
+
+
+class TNCAPIMixin:
+
+    async def _call(
+        self, endpoint: str, mode: str, options: dict | None = None, payload: dict | None = None,
+        headers: dict | None = None,
+    ):
+        # FIXME: Add network activity check for TNC
+        options = options or {}
+        timeout = options.get('timeout', 15)
+        response = {'error': None, 'response': {}}
+        try:
+            async with async_timeout.timeout(timeout):
+                async with aiohttp.ClientSession(raise_for_status=True, trust_env=True) as session:
+                    req = await getattr(session, mode)(
+                        endpoint,
+                        data=json.dumps(payload or {}),
+                        headers=headers,
+                    )
+        except asyncio.TimeoutError:
+            response['error'] = f'Unable to connect with TNC in {timeout} seconds.'
+        except aiohttp.ClientResponseError as e:
+            response['error'] = f'Error Code ({req.status}): {e}'
+        else:
+            response['response'] = await req.json()
+        return response
--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -1,1 +1,6 @@
-REGISTRATION_URI = 'https://truenas.connect.dev.ixsystems.net/system/register'
+import urllib.parse
+
+
+BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
+REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
+REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(BASE_URL, 'v1/systems/finalize')
