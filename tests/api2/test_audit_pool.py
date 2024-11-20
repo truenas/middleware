5762c798e0a029f5b7b69ea5ea323b82220a3ef3
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2024-11-20 16:23:07 -0500

    NAS-132498 / 25.04 / audit when pool is exported (#14967)
    * audit when pool is exported
    
    * fix order of parameters
    
    * add audit test
    
    * destroy pool
    
    ---------
    
    Co-authored-by: Logan Cary <logan.cary@ixsystems.com>

--- tests/api2/test_audit_pool.py
+++ tests/api2/test_audit_pool.py
@@ -2,7 +2,7 @@ import pytest
 
 from middlewared.test.integration.assets.pool import another_pool
 from middlewared.test.integration.utils import call
-from middlewared.test.integration.utils.audit import expect_audit_log
+from middlewared.test.integration.utils.audit import expect_audit_log, expect_audit_method_calls
 
 
 def test_pool_update_audit_success():
@@ -37,3 +37,30 @@ def test_pool_update_audit_error():
         }]):
             with pytest.raises(Exception):
                 call('pool.update', *params, job=True)
+
+
+def test_pool_create_and_export_audit():
+    unused_disks = call('disk.get_unused')
+    assert len(unused_disks) >= 1
+
+    pool_name = 'TempPool'
+    create_payload =  {
+        'name': pool_name,
+        'topology': {'data': [{'type': 'STRIPE', 'disks': [unused_disks[0]['name']]}]}
+    }
+
+    with expect_audit_method_calls([{
+        'method': 'pool.create',
+        'params': [create_payload],
+        'description': f'Pool create {pool_name}',
+    }]):
+        pool_id = call('pool.create', create_payload, job=True)['id']
+
+    export_payload = {'destroy': True}
+
+    with expect_audit_method_calls([{
+        'method': 'pool.export',
+        'params': [pool_id, export_payload],
+        'description': f'Pool Export {pool_name}',
+    }]):
+        call('pool.export', pool_id, export_payload, job=True)
