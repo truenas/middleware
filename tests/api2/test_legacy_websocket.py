1b1b658d147b751bdec0cbf912d757a65e8d22d1
themylogin <themylogin@gmail.com>
2024-11-26 12:57:01 +0100

    Only redact secrets when the method return value is passed to the user

--- tests/api2/test_legacy_websocket.py
+++ tests/api2/test_legacy_websocket.py
@@ -1,7 +1,11 @@
+import random
+import string
+
 import pytest
 
 from truenas_api_client import Client
 
+from middlewared.test.integration.assets.account import unprivileged_user
 from middlewared.test.integration.assets.cloud_sync import credential
 from middlewared.test.integration.utils import password, websocket_url
 
@@ -17,7 +21,28 @@ def c():
         yield c
 
 
-def test_adapts_cloud_credentials(c):
+@pytest.fixture(scope="module")
+def unprivileged_client():
+    suffix = "".join([random.choice(string.ascii_lowercase + string.digits) for _ in range(8)])
+    with unprivileged_user(
+        username=f"unprivileged_{suffix}",
+        group_name=f"unprivileged_users_{suffix}",
+        privilege_name=f"Unprivileged users ({suffix})",
+        allowlist=[],
+        roles=["READONLY_ADMIN"],
+        web_shell=False,
+    ) as t:
+        with Client(websocket_url() + "/websocket") as c:
+            c.call("auth.login_ex", {
+                "mechanism": "PASSWORD_PLAIN",
+                "username": t.username,
+                "password": t.password,
+            })
+            yield c
+
+
+@pytest.fixture(scope="module")
+def ftp_credential():
     with credential({
         "provider": {
             "type": "FTP",
@@ -27,5 +52,14 @@ def test_adapts_cloud_credentials(c):
             "pass": "",
         },
     }) as cred:
-        result = c.call("cloudsync.credentials.get_instance", cred["id"])
-        assert result["provider"] == "FTP"
+        yield cred
+
+
+def test_adapts_cloud_credentials(c, ftp_credential):
+    result = c.call("cloudsync.credentials.get_instance", ftp_credential["id"])
+    assert result["provider"] == "FTP"
+
+
+def test_adapts_cloud_credentials_for_unprivileged(unprivileged_client, ftp_credential):
+    result = unprivileged_client.call("cloudsync.credentials.get_instance", ftp_credential["id"])
+    assert result["attributes"] == "********"
