6ca3927ae7881033bbd790532c440fbcffb3b5e9
Caleb <yocalebo@gmail.com>
2024-11-26 11:36:15 -0500

    Revert "clean up utils/syslog.py"
    This reverts commit 2433f0649047419d2b9fb5e8a9ad4acfa0f98cf1.

--- src/middlewared/middlewared/utils/syslog.py
+++ src/middlewared/middlewared/utils/syslog.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+import os
 import syslog
-
-from middlewared.utils.time_utils import utc_now
+import uuid
 
 
 def syslog_message(message):
     data = f'<{syslog.LOG_USER | syslog.LOG_INFO}>'
-    data += f'{utc_now().strftime("%b %d %H:%M:%S")} '
+
+    data += f'{datetime.utcnow().strftime("%b %d %H:%M:%S")} '
+
     data += 'TNAUDIT_MIDDLEWARE: '
+
     data += message
+
     data = data.encode('ascii', 'ignore')
+
     return data
