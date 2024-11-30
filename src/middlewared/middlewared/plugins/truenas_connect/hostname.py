853ae8a771954cb16cb6ea1f8b5e5bdfb7b89929
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-30 19:40:58 +0500

    Add bits to enable registration/update of hostname

--- src/middlewared/middlewared/plugins/truenas_connect/hostname.py
+++ src/middlewared/middlewared/plugins/truenas_connect/hostname.py
@@ -1,7 +1,7 @@
-from middlewared.service import Service
+from middlewared.service import CallError, Service
 
 from .mixin import TNCAPIMixin
-from .urls import HOSTNAME_URL
+from .urls import HOSTNAME_URL, LECA_HOSTNAME_URL
 from .utils import get_account_id_and_system_id
 
 
@@ -11,12 +11,9 @@ class TNCHostnameService(Service, TNCAPIMixin):
         namespace = 'tn_connect.hostname'
         private = True
 
-    async def call(self, mode, payload=None):
+    async def call(self, url, mode, payload=None):
         config = await self.middleware.call('tn_connect.config')
-        creds = get_account_id_and_system_id(config)
-        return await self._call(
-            HOSTNAME_URL.format(**creds), mode, payload=payload, headers=await self.auth_headers(config)
-        )
+        return await self._call(url, mode, payload=payload, headers=await self.auth_headers(config))
 
     async def config(self):
         config = await self.middleware.call('tn_connect.config')
@@ -30,7 +27,7 @@ class TNCHostnameService(Service, TNCAPIMixin):
                 'hostname_configured': False,
             }
 
-        resp = (await self.call('get')) | {'base_domain': None}
+        resp = (await self.call(HOSTNAME_URL.format(**creds), 'get')) | {'base_domain': None}
         resp['hostname_details'] = resp.pop('response')
         for domain in resp['hostname_details']:
             if len(domain.rsplit('.', maxsplit=4)) == 4:
@@ -42,5 +39,23 @@ class TNCHostnameService(Service, TNCAPIMixin):
             'hostname_configured': bool(resp['hostname_details']),
         }
 
-    async def register_update_ips(self, payload):
-        return await self.call('post', payload=payload)
+    async def register_update_ips(self):
+        tnc_config = await self.middleware.call('tn_connect.config')
+        config = await self.config()
+        if config['error']:
+            raise CallError(f'Failed to fetch TNC hostname configuration: {config["error"]}')
+
+        register = config['hostname_configured'] is False
+        if register:
+            payload = {
+                'ips': [tnc_config['ip']],
+                'system_id': tnc_config['jwt_details']['system_id'],
+                'create_wildcard': True,
+            }
+        else:
+            payload = {config['base_domain']: tnc_config['ip']}
+
+        # FIXME: Put does not give json in response, handle that and is broken upstream atm as well
+        return await self.call(
+            LECA_HOSTNAME_URL, 'post' if register else 'put', payload=payload,
+        )
