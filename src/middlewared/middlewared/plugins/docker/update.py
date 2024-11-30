262ba1bd4f19f4b2ae244841df01a5f44421de42
themylogin <themylogin@gmail.com>
2024-11-30 04:11:19 +0100

    `docker.nvidia_present` (#15055)

--- src/middlewared/middlewared/plugins/docker/update.py
+++ src/middlewared/middlewared/plugins/docker/update.py
@@ -6,10 +6,12 @@ import middlewared.sqlalchemy as sa
 
 from middlewared.api import api_method
 from middlewared.api.current import (
-    DockerEntry, DockerStatusArgs, DockerStatusResult, DockerUpdateArgs, DockerUpdateResult,
+    DockerEntry, DockerStatusArgs, DockerStatusResult, DockerUpdateArgs, DockerUpdateResult, DockerNvidiaPresentArgs,
+    DockerNvidiaPresentResult,
 )
 from middlewared.schema import ValidationErrors
 from middlewared.service import CallError, ConfigService, job, private
+from middlewared.utils.gpu import get_gpus
 from middlewared.utils.zfs import query_imported_fast_impl
 
 from .state_utils import Status
@@ -131,6 +133,19 @@ class DockerService(ConfigService):
         Returns the status of the docker service.
         """
         return await self.middleware.call('docker.state.get_status_dict')
+
+    @api_method(DockerNvidiaPresentArgs, DockerNvidiaPresentResult)
+    def nvidia_present(self):
+        adv_config = self.middleware.call_sync("system.advanced.config")
+
+        for gpu in get_gpus():
+            if gpu["addr"]["pci_slot"] in adv_config["isolated_gpu_pci_ids"]:
+                continue
+
+            if gpu["vendor"] == "NVIDIA":
+                return True
+
+        return False
 
     @private
     def configure_nvidia(self):
