cee85ffb3454c2c23c66460ef35d81a5086cd590
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-30 19:06:58 +0500

    Add some metadata to tnc hostname service

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -6,5 +6,7 @@ BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
 HOSTNAME_URL = urllib.parse.urljoin(
     ACCOUNT_SERVICE_BASE_URL, '/v1/accounts/{account_id}/systems/{system_id}/hostnames/'
 )
+LECA_BASE_URL = 'https://leca-server.dev.ixsystems.net'
+LECA_HOSTNAME_URL = urllib.parse.urljoin(LECA_BASE_URL, '/v1/hostnames/')
 REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
 REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, '/v1/systems/finalize')
