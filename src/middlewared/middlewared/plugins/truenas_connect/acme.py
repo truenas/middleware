682a6ba7910d2f5931d84bd576a2af49ce10705c
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-11-30 20:21:40 +0500

    Add basic acme service to retrieve acme config

---
+++ src/middlewared/middlewared/plugins/truenas_connect/acme.py
@@ -0,0 +1,32 @@
+from middlewared.service import Service
+
+from .mixin import TNCAPIMixin
+from .urls import ACME_CONFIG_URL
+from .utils import get_account_id_and_system_id
+
+
+class TNCACMEService(Service, TNCAPIMixin):
+
+    class Config:
+        private = True
+        namespace = 'tn_connect.acme'
+
+    async def call(self, url, mode, payload=None):
+        config = await self.middleware.call('tn_connect.config')
+        return await self._call(url, mode, payload=payload, headers=await self.auth_headers(config))
+
+    async def config(self):
+        config = await self.middleware.call('tn_connect.config')
+        creds = get_account_id_and_system_id(config)
+        if not config['enabled'] or creds is None:
+            return {
+                'error': 'TrueNAS Connect is not enabled or not configured properly',
+                'tnc_configured': False,
+                'acme_details': {},
+            }
+
+        resp = await self.call(ACME_CONFIG_URL.format(account_id=creds['account_id']), 'get')
+        resp['acme_details'] = resp.pop('response')
+        return resp | {
+            'tnc_configured': True,
+        }
