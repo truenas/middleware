36d34b14f29fb1fdf21e91740a592bc0f21f746c
caleb <yocalebo@gmail.com>
2024-12-03 08:07:57 -0500

    expand comment
    (cherry picked from commit 809394246d165c302389fbbfbcc29b5d06ca2784)

--- tests/api2/test_default_locale.py
+++ tests/api2/test_default_locale.py
@@ -3,7 +3,7 @@ from middlewared.test.integration.utils import call
 
 def test_default_locale_exists():
     # it's important we keep this empty file
-    # since it causes error like these
+    # since it causes error like these if its missing
     # root@truenas[~]# tail -1 /var/log/error
     #   Nov 20 21:17:01 truenas CRON[399613]: pam_env(cron:session): \
     #   Unable to open env file: /etc/default/locale: No such file or directory
