ef37be7a6f615d426edb91df2e9c2cea1819f2cc
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-12-08 18:37:11 +0500

    Add validation for tn_connect.update

--- src/middlewared/middlewared/plugins/truenas_connect/update.py
+++ src/middlewared/middlewared/plugins/truenas_connect/update.py
@@ -1,7 +1,7 @@
 import middlewared.sqlalchemy as sa
 from middlewared.api import api_method
 from middlewared.api.current import TNCEntry, TNCUpdateArgs, TNCUpdateResult, TNCIPChoicesArgs, TNCIPChoicesResult
-from middlewared.service import ConfigService
+from middlewared.service import ConfigService, private, ValidationErrors
 
 from .status_utils import Status
 
@@ -26,17 +26,47 @@ class TrueNASConnectService(ConfigService):
         namespace = 'tn_connect'
         entry = TNCEntry
 
+    @private
+    async def validate_data(self, old_config, data):
+        verrors = ValidationErrors()
+        if data['enabled'] and not data['ips']:
+            verrors.add('tn_connect_update.ips', 'This field is required when TrueNAS Connect is enabled')
+
+        ip_choices = await self.ip_choices()
+        for index, ip in enumerate(data['ips']):
+            if ip not in ip_choices:
+                verrors.add(f'tn_connect_update.ips.{index}', 'Provided IP is not valid')
+
+        ips_changed = set(old_config['ips']) != set(data['ips'])
+        if ips_changed and (
+            data['enabled'] is True and old_config['status'] not in (Status.DISABLED.name, Status.CONFIGURED.name)
+        ):
+            verrors.add(
+                'tn_connect_update.ips',
+                'IPs cannot be changed when TrueNAS Connect is in a state other than disabled or completely configured'
+            )
+
+        verrors.check()
+
     @api_method(TNCUpdateArgs, TNCUpdateResult)
     async def do_update(self, data):
         """
         Update TrueNAS Connect configuration.
         """
         config = await self.config()
-        config.update(data)
-        # TODO: Handle the case where user imports same db in a different system
+        data = config | data
+        await self.validate_data(config, data)
+
         await self.middleware.call(
-            'datastore.update', self._config.datastore, config['id'], {'enabled': config['enabled']}
+            'datastore.update', self._config.datastore, config['id'], {
+                'enabled': data['enabled'],
+                'ips': data['ips'],
+            }
         )
+
+        # TODO: Handle the case where user imports same db in a different system
+        # TODO: Trigger a job to update the registration details if ips are changed
+
         return await self.config()
 
     @api_method(TNCIPChoicesArgs, TNCIPChoicesResult)
