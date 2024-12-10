edda7e8a8325ab80e8722e332429ee5a64aca1c3
Andrew Walker <awalker@ixsystems.com>
2024-12-10 15:25:39 -0600

    Pass app for update method for ConfigService (#15173)
    When we have pair of update/do_update methods for a ConfigService
    we need to pass the authenticated credentials between them so that
    the latter method is called with appropriate privileges. If this
    doesn't happen, then in addition to the obvious problem of getting
    a privileged credential passed to the do_update method we also
    lose credential information for a job log if the method has a job
    decorator.
    
    (cherry picked from commit b14e7d9c15bdf5d5ee09d062f1c86f53fac4a5d3)

--- src/middlewared/middlewared/service/config_service.py
+++ src/middlewared/middlewared/service/config_service.py
@@ -4,7 +4,7 @@ import copy
 from middlewared.schema import accepts, Dict, Patch, returns
 
 from .base import ServiceBase
-from .decorators import private
+from .decorators import pass_app, private
 from .service import Service
 from .service_mixin import ServiceChangeMixin
 
@@ -78,9 +78,10 @@ class ConfigService(ServiceChangeMixin, Service, metaclass=ConfigServiceMetabase
         options['prefix'] = self._config.datastore_prefix
         return await self._get_or_insert(self._config.datastore, options)
 
-    async def update(self, data):
+    @pass_app(rest=True)
+    async def update(self, app, data):
         rv = await self.middleware._call(
-            f'{self._config.namespace}.update', self, self.do_update, [data]
+            f'{self._config.namespace}.update', self, self.do_update, [data], app=app
         )
         await self.middleware.call_hook(f'{self._config.namespace}.post_update', rv)
         return rv
