7ea8520df2c63ff1511c5723ad24928af0ce541c
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2024-12-12 19:45:32 +0000

    NAS-132959 / 25.04 / Mark extent read-only if underlying zvol is R/O (#15195)
    * Update extent ro prop if zvol readonly changes.
    
    * Update zvol readonly prop if extent is ro changed
    
    * Add unit test test_36_target_readonly_extent
    
    ---------
    
    Co-authored-by: Caleb <yocalebo@gmail.com>
    (cherry picked from commit bca13acb365ef5e95de7a111af2f78fb061ee4d1)

--- src/middlewared/middlewared/plugins/iscsi_/extents.py
+++ src/middlewared/middlewared/plugins/iscsi_/extents.py
@@ -104,8 +104,8 @@ class iSCSITargetExtentService(SharingService):
 
         `ro` when set to true prevents the initiator from writing to this LUN.
         """
+        await self.validate(data)
         verrors = ValidationErrors()
-        await self.middleware.call('iscsi.extent.validate', data)
         await self.clean(data, 'iscsi_extent_create', verrors)
         verrors.check()
 
@@ -114,7 +114,16 @@ class iSCSITargetExtentService(SharingService):
         # This change is being made in conjunction with threads_num being specified in scst.conf
         if data['type'] == 'DISK' and data['path'].startswith('zvol/'):
             zvolname = zvol_path_to_name(os.path.join('/dev', data['path']))
-            await self.middleware.call('zfs.dataset.update', zvolname, {'properties': {'volthreading': {'value': 'off'}}})
+            await self.middleware.call(
+                'zfs.dataset.update',
+                zvolname,
+                {
+                    'properties': {
+                        'volthreading': {'value': 'off'},
+                        'readonly': {'value': 'on' if data['ro'] else 'off'}
+                    }
+                }
+            )
 
         data['id'] = await self.middleware.call(
             'datastore.insert', self._config.datastore, {**data, 'vendor': 'TrueNAS'},
@@ -145,14 +154,25 @@ class iSCSITargetExtentService(SharingService):
         new.update(data)
 
         await self.middleware.call('iscsi.extent.validate', new)
-        await self.clean(
-            new, 'iscsi_extent_update', verrors, old=old
-        )
+        await self.clean(new, 'iscsi_extent_update', verrors, old=old)
         verrors.check()
 
         await self.middleware.call('iscsi.extent.save', new, 'iscsi_extent_create', verrors, old)
         verrors.check()
         new.pop(self.locked_field)
+
+        zvolpath = new.get('path')
+        if zvolpath is not None and zvolpath.startswith('zvol/'):
+            zvolname = zvol_path_to_name(os.path.join('/dev', zvolpath))
+            await self.middleware.call(
+                'zfs.dataset.update',
+                zvolname,
+                {
+                    'properties': {
+                        'readonly': {'value': 'on' if new['ro'] else 'off'}
+                    }
+                }
+            )
 
         await self.middleware.call(
             'datastore.update',
@@ -162,6 +182,9 @@ class iSCSITargetExtentService(SharingService):
             {'prefix': self._config.datastore_prefix}
         )
 
+        await self._service_change('iscsitarget', 'reload')
+
+        # scstadmin can have issues when modifying an existing extent, re-run
         await self._service_change('iscsitarget', 'reload')
 
         return await self.get_instance(id_)
@@ -219,8 +242,8 @@ class iSCSITargetExtentService(SharingService):
                 await self.middleware.call('iscsi.alua.wait_for_alua_settled')
 
     @private
-    def validate(self, data):
-        data['serial'] = self.extent_serial(data['serial'])
+    async def validate(self, data):
+        data['serial'] = await self.extent_serial(data['serial'])
         data['naa'] = self.extent_naa(data.get('naa'))
 
     @private
@@ -386,10 +409,10 @@ class iSCSITargetExtentService(SharingService):
         return data
 
     @private
-    def extent_serial(self, serial):
+    async def extent_serial(self, serial):
         if serial in [None, '']:
             used_serials = [i['serial'] for i in (
-                self.middleware.call_sync('iscsi.extent.query', [], {'select': ['serial']})
+                await self.middleware.call('iscsi.extent.query', [], {'select': ['serial']})
             )]
             tries = 5
             for i in range(tries):
