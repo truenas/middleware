cbb4505a594ff69aab0f3dd7978ac58d87548af2
Caleb <yocalebo@gmail.com>
2024-12-14 11:51:29 -0500

    add to apps/__init__.py

--- src/middlewared/middlewared/apps/__init__.py
+++ src/middlewared/middlewared/apps/__init__.py
@@ -1,2 +1,5 @@
 from .file_app import FileApplication  # noqa
 from .webshell_app import ShellApplication  # noqa
+from .websocket_app import WebSocketApplication  # noqa
+
+__all__ = ("FileApplication", "ShellApplication", "WebSocketApplication")
