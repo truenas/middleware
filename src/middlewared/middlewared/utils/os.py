cc231b08dd3a2021614a6d2f1a9e546682d308f9
caleb <yocalebo@gmail.com>
2024-12-16 11:20:55 -0500

    add git_pids() helper function

--- src/middlewared/middlewared/utils/os.py
+++ src/middlewared/middlewared/utils/os.py
@@ -1,11 +1,19 @@
-from os import closerange, kill
+from collections.abc import Generator
+from dataclasses import dataclass
+from os import closerange, kill, scandir
 from resource import getrlimit, RLIMIT_NOFILE, RLIM_INFINITY
 from signal import SIGKILL, SIGTERM
 from time import sleep, time
 
-__all__ = ['close_fds', 'terminate_pid']
+__all__ = ['close_fds', 'get_pids', 'PidEntry', 'terminate_pid']
 
 ALIVE_SIGNAL = 0
+
+
+@dataclass(slots=True, frozen=True, kw_only=True)
+class PidEntry:
+    cmdline: bytes
+    pid: int
 
 
 def close_fds(low_fd, max_fd=None):
@@ -51,3 +59,20 @@ def terminate_pid(pid: int, timeout: int = 10) -> bool:
     except ProcessLookupError:
         # Process may have terminated between checks
         return True
+
+
+def get_pids(pid: int | None = None) -> Generator[PidEntry] | PidEntry | None:
+    spid = str(pid) if pid is not None else None
+    with scandir("/proc/") as sdir:
+        for i in filter(lambda x: x.name.isdigit(), sdir):
+            try:
+                with open(f'{i.path}/cmdline', 'rb') as f:
+                    cmdline = f.read().replace(b'\x00', b' ')
+            except FileNotFoundError:
+                # process could have gone away
+                pass
+            else:
+                if spid == i.name:
+                    return PidEntry(cmdline=cmdline, pid=pid)
+                else:
+                    yield PidEntry(cmdline=cmdline, pid=int(i.name))
