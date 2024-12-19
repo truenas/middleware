80cff39167d98692a3872f426b6c37a65f1c555e
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-12-20 01:07:14 +0500

    Allow rolling back host path snapshots

--- src/middlewared/middlewared/plugins/apps/upgrade.py
+++ src/middlewared/middlewared/plugins/apps/upgrade.py
@@ -23,7 +23,7 @@ class AppService(Service):
     @private
     def take_snapshot_of_hostpath(self, app, snapshot_hostpath):
         app_info = self.middleware.call_sync('app.get_instance', app) if isinstance(app, str) else app
-        host_path_mapping = self.middleware.call_sync('app.get_host_path_mapping', app_info['name'])
+        host_path_mapping = self.middleware.call_sync('app.get_hostpaths_datasets', app_info['name'])
         # Stop the app itself before we attempt to take snapshots
         self.middleware.call_sync('app.stop', app_info['name']).wait_sync()
         if not snapshot_hostpath:
@@ -52,7 +52,7 @@ class AppService(Service):
                 continue
 
             self.middleware.call_sync('zfs.snapshot.create', {'dataset': dataset, 'name': app_info['version']})
-
+            logger.debug('Created snapshot %r for %r app', snap_name, app_info['name'])
 
     @accepts(
         Str('app_name'),
