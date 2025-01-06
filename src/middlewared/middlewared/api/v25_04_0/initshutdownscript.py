9dd1b33406ba40c9e9f35f0438467510aedc42d8
Logan Cary <logan.cary@ixsystems.com>
2025-01-06 15:57:43 -0500

    doc

--- src/middlewared/middlewared/api/v25_04_0/initshutdownscript.py
+++ src/middlewared/middlewared/api/v25_04_0/initshutdownscript.py
@@ -16,10 +16,23 @@ __all__ = [
 class InitShutdownScriptCreate(BaseModel):
     type: Literal["COMMAND", "SCRIPT"]
     command: str | None = ""
+    """Must be set if `type="COMMAND"`."""
     script: str | None = ""
+    """Must be set if `type="SCRIPT"`."""
     when: Literal["PREINIT", "POSTINIT", "SHUTDOWN"]
+    """
+    "PREINIT": Early in the boot process before all services have started.
+    "POSTINIT": Late in the boot process when most services have started.
+    "SHUTDOWN": On shutdown.
+    """
     enabled: bool = True
     timeout: int = 10
+    """An integer time in seconds that the system should wait for the execution of the script/command.
+    
+    A hard limit for a timeout is configured by the base OS, so when a script/command is set to execute on SHUTDOWN,
+    the hard limit configured by the base OS is changed adding the timeout specified by script/command so it can be
+    ensured that it executes as desired and is not interrupted by the base OS's limit.
+    """
     comment: Annotated[str, Field(max_length=255)] = ""
 
 
@@ -54,6 +67,7 @@ class InitShutdownScriptDeleteArgs(BaseModel):
 
 class InitShutdownScriptDeleteResult(BaseModel):
     result: Literal[True]
+    """Always return `True`."""  # FIXME: Should return False if no record was deleted.
 
 
 class InitShutdownScriptExecuteInitTasksArgs(BaseModel):
