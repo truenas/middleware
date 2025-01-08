d75f7d1d79f8e6119aad8351597785a49f26cf2b
Logan Cary <logan.cary@ixsystems.com>
2025-01-08 09:34:20 -0500

    remove duplicate docs

--- src/middlewared/middlewared/plugins/device.py
+++ src/middlewared/middlewared/plugins/device.py
@@ -10,16 +10,7 @@ class DeviceService(Service):
 
     @api_method(DeviceGetInfoArgs, DeviceGetInfoResult, roles=['READONLY_ADMIN'])
     async def get_info(self, data):
-        """
-        Get info for `type` device.
-
-        If `type` is "DISK":
-            `get_partitions`: when set to `True` will query partition
-                information for the disks. NOTE: this can be expensive on
-                systems with a large number of disks present.
-            `serials_only`: when set to `True` will query serial information
-                _ONLY_ for the disks.
-        """
+        """Get info for `type` device."""
         method = f'device.get_{data["type"].lower()}s'
         if method == 'device.get_disks':
             return await self.middleware.call(method, data['get_partitions'], data['serials_only'])
