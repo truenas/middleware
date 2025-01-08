truenas/middleware

commit 9ef345f5db6572204a368145066e3704e6ef0ca7

Remove accepts style decorator from apps plugin (#15225)

src/middlewared/middlewared/service/crud_service.py
@@ -68,9 +68,11 @@ class CRUDServiceMetabase(ServiceBase):
             klass.ENTRY = None
             # FIXME: Remove `wraps` handling when we get rid of `@filterable` in `CRUDService.query` definition
             query_result_model = query_result(klass._config.entry)
-            klass.query = api_method(QueryArgs, query_result_model)(
-                klass.query.wraps if hasattr(klass.query, "wraps") else klass.query
-            )
+            if not hasattr(klass.query, '_filterable') or klass.query._filterable is False:
+                # No need to inject api method if filterable has been explicitly specified
+                klass.query = api_method(QueryArgs, query_result_model)(
+                    klass.query.wraps if hasattr(klass.query, "wraps") else klass.query
+                )
             # FIXME: Remove `wraps` handling when we get rid of `@accepts` in `CRUDService.get_instance` definition
             get_instance_args_model = get_instance_args(klass._config.entry)
             get_instance_result_model = get_instance_result(klass._config.entry)

src/middlewared/middlewared/api/base/decorator.py
@@ -22,6 +22,9 @@ def api_method(
     cli_private: bool = False,
     authentication_required: bool = True,
     authorization_required: bool = True,
+    pass_app: bool = False,
+    pass_app_require: bool = False,
+    pass_app_rest: bool = False,
 ):
     """
     Mark a `Service` class method as an API method.
@@ -58,6 +61,13 @@ def api_method(
         raise TypeError("`returns` model must only have one field called `result`")
 
     def wrapper(func):
+        if pass_app:
+            # Pass the application instance as parameter to the method
+            func._pass_app = {
+                'require': pass_app_require,
+                'rest': pass_app_rest,
+            }
+
         args_index = calculate_args_index(func, audit_callback)
 
         if asyncio.iscoroutinefunction(func):

src/middlewared/middlewared/api/v25_04_0/__init__.py
@@ -6,6 +6,7 @@ from .alertservice import *  # noqa
 from .api_key import *  # noqa
 from .app import *  # noqa
 from .app_image import *  # noqa
+from .app_ix_volume import *  # noqa
 from .auth import *  # noqa
 from .boot_environments import *  # noqa
 from .catalog import *  # noqa

src/middlewared/middlewared/api/v25_04_0/app.py
@@ -1,11 +1,371 @@
-from middlewared.api.base import BaseModel, NonEmptyString
+from typing import Literal, TypeAlias
+
+from pydantic import ConfigDict, Field, RootModel, Secret
+
+from middlewared.api.base import BaseModel, LongString, NonEmptyString, single_argument_args, single_argument_result
 
 from .catalog import CatalogAppInfo
 
 
 __all__ = [
     'AppCategoriesArgs', 'AppCategoriesResult', 'AppSimilarArgs', 'AppSimilarResult', 'AppAvailableResponse',
+    'AppEntry', 'AppCreateArgs', 'AppCreateResult', 'AppUpdateArgs', 'AppUpdateResult', 'AppDeleteArgs',
+    'AppDeleteResult', 'AppConfigArgs', 'AppConfigResult', 'AppConvertToCustomArgs', 'AppConvertToCustomResult',
+    'AppStopArgs', 'AppStopResult', 'AppStartArgs', 'AppStartResult', 'AppRedeployArgs', 'AppRedeployResult',
+    'AppOutdatedDockerImagesArgs', 'AppOutdatedDockerImagesResult', 'AppPullImagesArgs', 'AppPullImagesResult',
+    'AppContainerIDArgs', 'AppContainerIDResult', 'AppContainerConsoleChoiceArgs', 'AppContainerConsoleChoiceResult',
+    'AppCertificateChoicesArgs', 'AppCertificateChoicesResult', 'AppCertificateAuthorityArgs',
+    'AppCertificateAuthorityResult', 'AppUsedPortsArgs', 'AppUsedPortsResult', 'AppIPChoicesArgs', 'AppIPChoicesResult',
+    'AppAvailableSpaceArgs', 'AppAvailableSpaceResult', 'AppGpuChoicesArgs', 'AppGpuChoicesResult', 'AppRollbackArgs',
+    'AppRollbackResult', 'AppRollbackVersionsArgs', 'AppRollbackVersionsResult', 'AppUpgradeArgs', 'AppUpgradeResult',
+    'AppUpgradeSummaryArgs', 'AppUpgradeSummaryResult',
 ]
+
+
+CONTAINER_STATES: TypeAlias = Literal['crashed', 'created', 'exited', 'running', 'starting']
+
+
+class HostPorts(BaseModel):
+    host_port: int
+    host_ip: str
+
+
+class UsedPorts(BaseModel):
+    container_port: int
+    protocol: str
+    host_ports: list[HostPorts]
+
+
+class AppVolumes(BaseModel):
+    source: str
+    destination: str
+    mode: str
+    type_: str = Field(alias='type')
+
+
+class AppContainerDetails(BaseModel):
+    id: str
+    service_name: str
+    image: str
+    port_config: list[UsedPorts]
+    state: CONTAINER_STATES
+    volume_mounts: list[AppVolumes]
+
+
+class AppNetworks(BaseModel):
+    Name: str
+    Id: str
+    Labels: dict
+
+    model_config = ConfigDict(extra='allow')
+
+
+class AppActiveWorkloads(BaseModel):
+    containers: int
+    used_ports: list[UsedPorts]
+    container_details: list[AppContainerDetails]
+    volumes: list[AppVolumes]
+    images: list[NonEmptyString]
+    networks: list[AppNetworks]
+
+
+class AppEntry(BaseModel):
+    name: NonEmptyString
+    id: NonEmptyString
+    state: Literal['CRASHED', 'DEPLOYING', 'RUNNING', 'STOPPED', 'STOPPING']
+    upgrade_available: bool
+    latest_version: NonEmptyString | None
+    image_updates_available: bool
+    custom_app: bool
+    migrated: bool
+    human_version: NonEmptyString
+    version: NonEmptyString
+    metadata: dict
+    active_workloads: AppActiveWorkloads
+    notes: LongString | None
+    portals: dict
+    version_details: dict | None = None
+    config: dict | None = None
+
+
+@single_argument_args('app_create')
+class AppCreateArgs(BaseModel):
+    custom_app: bool = False
+    values: Secret[dict] = Field(default_factory=dict)
+    custom_compose_config: Secret[dict] = Field(default_factory=dict)
+    custom_compose_config_string: Secret[LongString] = ''
+    catalog_app: str | None = None
+    app_name: str = Field(pattern=r'^[a-z]([-a-z0-9]*[a-z0-9])?$', min_length=1, max_length=40)
+    '''
+    Application name must have the following:
+    1) Lowercase alphanumeric characters can be specified
+    2) Name must start with an alphabetic character and can end with alphanumeric character
+    3) Hyphen '-' is allowed but not as the first or last character
+    e.g abc123, abc, abcd-1232
+    '''
+    train: NonEmptyString = 'stable'
+    version: NonEmptyString = 'latest'
+
+
+class AppCreateResult(BaseModel):
+    result: AppEntry
+
+
+class AppUpdate(BaseModel):
+    values: Secret[dict] = Field(default_factory=dict)
+    custom_compose_config: Secret[dict] = Field(default_factory=dict)
+    custom_compose_config_string: Secret[LongString] = ''
+
+
+class AppUpdateArgs(BaseModel):
+    app_name: NonEmptyString
+    update: AppUpdate = AppUpdate()
+
+
+class AppUpdateResult(BaseModel):
+    result: AppEntry
+
+
+class AppDelete(BaseModel):
+    remove_images: bool = True
+    remove_ix_volumes: bool = False
+    force_remove_ix_volumes: bool = False
+    force_remove_custom_app: bool = False
+
+
+class AppDeleteArgs(BaseModel):
+    app_name: NonEmptyString
+    options: AppDelete = AppDelete()
+
+
+class AppDeleteResult(BaseModel):
+    result: Literal[True]
+
+
+class AppConfigArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppConfigResult(BaseModel):
+    result: dict
+
+
+class AppConvertToCustomArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppConvertToCustomResult(BaseModel):
+    result: AppEntry
+
+
+class AppStopArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppStopResult(BaseModel):
+    result: None
+
+
+class AppStartArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppStartResult(BaseModel):
+    result: None
+
+
+class AppRedeployArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppRedeployResult(BaseModel):
+    result: AppEntry
+
+
+class AppOutdatedDockerImagesArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppOutdatedDockerImagesResult(BaseModel):
+    result: list[NonEmptyString]
+
+
+class AppPullImages(BaseModel):
+    redeploy: bool = True
+
+
+class AppPullImagesArgs(BaseModel):
+    app_name: NonEmptyString
+    options: AppPullImages = AppPullImages()
+
+
+class AppPullImagesResult(BaseModel):
+    result: None
+
+
+class AppContainerIDOptions(BaseModel):
+    alive_only: bool = True
+
+
+class AppContainerIDArgs(BaseModel):
+    app_name: NonEmptyString
+    options: AppContainerIDOptions = AppContainerIDOptions()
+
+
+class ContainerDetails(BaseModel):
+    id: NonEmptyString
+    service_name: NonEmptyString
+    image: NonEmptyString
+    state: CONTAINER_STATES
+
+
+class AppContainerResponse(RootModel[dict[str, ContainerDetails]]):
+    pass
+
+
+class AppContainerIDResult(BaseModel):
+    result: AppContainerResponse
+
+
+class AppContainerConsoleChoiceArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppContainerConsoleChoiceResult(BaseModel):
+    result: AppContainerResponse
+
+
+class AppCertificateChoicesArgs(BaseModel):
+    pass
+
+
+class AppCertificate(BaseModel):
+    id: int
+    name: NonEmptyString
+
+
+class AppCertificateChoicesResult(BaseModel):
+    result: list[AppCertificate]
+
+
+class AppCertificateAuthorityArgs(BaseModel):
+    pass
+
+
+class AppCertificateAuthorityResult(BaseModel):
+    result: list[AppCertificate]
+
+
+class AppUsedPortsArgs(BaseModel):
+    pass
+
+
+class AppUsedPortsResult(BaseModel):
+    result: list[int]
+
+
+class AppIPChoicesArgs(BaseModel):
+    pass
+
+
+class AppIPChoicesResult(BaseModel):
+    result: dict[NonEmptyString, NonEmptyString]
+
+
+class AppAvailableSpaceArgs(BaseModel):
+    pass
+
+
+class AppAvailableSpaceResult(BaseModel):
+    result: int
+
+
+class AppGpuChoicesArgs(BaseModel):
+    pass
+
+
+class GPU(BaseModel):
+    vendor: NonEmptyString | None
+    description: LongString | None
+    error: NonEmptyString | None
+    vendor_specific_config: dict
+    gpu_details: dict
+    pci_slot: NonEmptyString | None
+
+
+class AppGPUResponse(RootModel[dict[str, GPU]]):
+    pass
+
+
+class AppGpuChoicesResult(BaseModel):
+    result: AppGPUResponse
+
+
+class AppRollbackOptions(BaseModel):
+    app_version: NonEmptyString
+    rollback_snapshot: bool = True
+
+
+class AppRollbackArgs(BaseModel):
+    app_name: NonEmptyString
+    options: AppRollbackOptions
+
+
+class AppRollbackResult(BaseModel):
+    result: AppEntry
+
+
+class AppRollbackVersionsArgs(BaseModel):
+    app_name: NonEmptyString
+
+
+class AppRollbackVersionsResult(BaseModel):
+    result: list[NonEmptyString]
+
+
+class UpgradeOptions(BaseModel):
+    app_version: NonEmptyString = 'latest'
+    values: Secret[dict] = Field(default_factory=dict)
+
+
+class AppUpgradeArgs(BaseModel):
+    app_name: NonEmptyString
+    options: UpgradeOptions = UpgradeOptions()
+
+
+class AppUpgradeResult(BaseModel):
+    result: AppEntry
+
+
+class UpgradeSummaryOptions(BaseModel):
+    app_version: NonEmptyString = 'latest'
+
+
+class AppUpgradeSummaryArgs(BaseModel):
+    app_name: NonEmptyString
+    options: UpgradeSummaryOptions = UpgradeSummaryOptions()
+
+
+class AppVersionInfo(BaseModel):
+    version: str
+    '''Version of the app'''
+    human_version: str
+    '''Human readable version of the app'''
+
+
+@single_argument_result
+class AppUpgradeSummaryResult(BaseModel):
+    latest_version: str
+    '''Latest version available for the app'''
+    latest_human_version: str
+    '''Latest human readable version available for the app'''
+    upgrade_version: str
+    '''Version user has requested to be upgraded at'''
+    upgrade_human_version: str
+    '''Human readable version user has requested to be upgraded at'''
+    available_versions_for_upgrade: list[AppVersionInfo]
+    '''List of available versions for upgrade'''
+    changelog: LongString | None
 
 
 class AppAvailableResponse(CatalogAppInfo):

src/middlewared/middlewared/api/v25_04_0/app_ix_volume.py
@@ -1,0 +1,17 @@
+from middlewared.api.base import BaseModel, NonEmptyString
+
+
+__all__ = ['AppIXVolumeEntry', 'AppIXVolumeExistsArgs', 'AppIXVolumeExistsResult']
+
+
+class AppIXVolumeEntry(BaseModel):
+    app_name: NonEmptyString
+    name: NonEmptyString
+
+
+class AppIXVolumeExistsArgs(BaseModel):
+    name: NonEmptyString
+
+
+class AppIXVolumeExistsResult(BaseModel):
+    result: bool

src/middlewared/middlewared/plugins/apps/app_scale.py
@@ -1,4 +1,7 @@
-from middlewared.schema import accepts, Str, returns
+from middlewared.api import api_method
+from middlewared.api.current import (
+    AppStartArgs, AppStartResult, AppStopArgs, AppStopResult, AppRedeployArgs, AppRedeployResult,
+)
 from middlewared.service import job, Service
 
 from .compose_utils import compose_action
@@ -12,8 +15,7 @@ class AppService(Service):
         namespace = 'app'
         cli_namespace = 'app'
 
-    @accepts(Str('app_name'), roles=['APPS_WRITE'])
-    @returns()
+    @api_method(AppStopArgs, AppStopResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_stop_{args[0]}')
     def stop(self, job, app_name):
         """
@@ -39,8 +41,7 @@ class AppService(Service):
             )
             self.middleware.call_sync('cache.pop', cache_key)
 
-    @accepts(Str('app_name'), roles=['APPS_WRITE'])
-    @returns()
+    @api_method(AppStartArgs, AppStartResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_start_{args[0]}')
     def start(self, job, app_name):
         """
@@ -51,8 +52,7 @@ class AppService(Service):
         compose_action(app_name, app_config['version'], 'up', force_recreate=True, remove_orphans=True)
         job.set_progress(100, f'Started {app_name!r} app')
 
-    @accepts(Str('app_name'), roles=['APPS_WRITE'])
-    @returns()
+    @api_method(AppRedeployArgs, AppRedeployResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_redeploy_{args[0]}')
     async def redeploy(self, job, app_name):
         """

src/middlewared/middlewared/plugins/apps/crud.py
@@ -1,17 +1,18 @@
 import contextlib
 import errno
-import os
 import shutil
-import textwrap
 
 from catalog_reader.custom_app import get_version_details
 
-from middlewared.schema import accepts, Bool, Dict, Int, List, Ref, returns, Str
+from middlewared.api import api_method
+from middlewared.api.current import (
+    AppEntry, AppCreateArgs, AppCreateResult, AppUpdateArgs, AppUpdateResult, AppDeleteArgs, AppDeleteResult,
+    AppConfigArgs, AppConfigResult, AppConvertToCustomArgs, AppConvertToCustomResult,
+)
 from middlewared.service import (
-    CallError, CRUDService, filterable, InstanceNotFound, job, pass_app, private, ValidationErrors
+    CallError, CRUDService, filterable_api_method, InstanceNotFound, job, private, ValidationErrors
 )
 from middlewared.utils import filter_list
-from middlewared.validators import Match, Range
 
 from .compose_utils import compose_action
 from .custom_app_utils import validate_payload
@@ -20,7 +21,6 @@ from .ix_apps.metadata import update_app_metadata, update_app_metadata_for_porta
 from .ix_apps.path import get_app_parent_volume_ds, get_installed_app_path, get_installed_app_version_path
 from .ix_apps.query import list_apps
 from .ix_apps.setup import setup_install_app_dir
-from .ix_apps.utils import AppState
 from .version_utils import get_latest_version_from_app_versions
 
 
@@ -31,55 +31,9 @@ class AppService(CRUDService):
         event_send = False
         cli_namespace = 'app'
         role_prefix = 'APPS'
+        entry = AppEntry
 
-    ENTRY = Dict(
-        'app_entry',
-        Str('name'),
-        Str('id'),
-        Str('state', enum=[state.value for state in AppState]),
-        Bool('upgrade_available'),
-        Str('human_version'),
-        Str('version'),
-        Dict('metadata', additional_attrs=True),
-        Dict(
-            'active_workloads',
-            Int('containers'),
-            List('used_ports', items=[Dict(
-                'used_port',
-                Str('container_port'),
-                Str('protocol'),
-                List('host_ports', items=[Dict(
-                    'host_port',
-                    Str('host_port'),
-                    Str('host_ip'),
-                )]),
-                additional_attrs=True,
-            )]),
-            List('container_details', items=[Dict(
-                'container_detail',
-                Str('id'),
-                Str('service_name'),
-                Str('image'),
-                List('port_config'),
-                Str('state', enum=['running', 'starting', 'exited']),
-                List('volume_mounts'),
-                additional_attrs=True,
-            )]),
-            List('volumes', items=[Dict(
-                'volume',
-                Str('source'),
-                Str('destination'),
-                Str('mode'),
-                Str('type'),
-                additional_attrs=True,
-            )]),
-            additional_attrs=True,
-        ),
-        additional_attrs=True,
-    )
-
-    @filterable
-    @pass_app(rest=True)
+    @filterable_api_method(item=AppEntry, pass_app=True, pass_app_rest=True)
     def query(self, app, filters, options):
         """
         Query all apps with `query-filters` and `query-options`.
@@ -133,8 +87,7 @@ class AppService(CRUDService):
 
         return filter_list(apps, filters, options)
 
-    @accepts(Str('app_name'), roles=['APPS_READ'])
-    @returns(Dict('app_config', additional_attrs=True))
+    @api_method(AppConfigArgs, AppConfigResult, roles=['APPS_READ'])
     def config(self, app_name):
         """
         Retrieve user specified configuration of `app_name`.
@@ -142,8 +95,7 @@ class AppService(CRUDService):
         app = self.get_instance__sync(app_name)
         return get_current_app_config(app_name, app['version'])
 
-    @accepts(Str('app_name'), roles=['APPS_WRITE'])
-    @returns(Ref('app_entry'))
+    @api_method(AppConvertToCustomArgs, AppConvertToCustomResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_start_{args[0]}')
     async def convert_to_custom(self, job, app_name):
         """
@@ -151,32 +103,7 @@ class AppService(CRUDService):
         """
         return await self.middleware.call('app.custom.convert', job, app_name)
 
-    @accepts(
-        Dict(
-            'app_create',
-            Bool('custom_app', default=False),
-            Dict('values', additional_attrs=True, private=True),
-            Dict('custom_compose_config', additional_attrs=True, private=True),
-            Str('custom_compose_config_string', private=True, max_length=2**31),
-            Str('catalog_app', required=False),
-            Str(
-                'app_name', required=True, validators=[Match(
-                    r'^[a-z]([-a-z0-9]*[a-z0-9])?$',
-                    explanation=textwrap.dedent(
-                        '''
-                        Application name must have the following:
-                        1) Lowercase alphanumeric characters can be specified
-                        2) Name must start with an alphabetic character and can end with alphanumeric character
-                        3) Hyphen '-' is allowed but not as the first or last character
-                        e.g abc123, abc, abcd-1232
-                        '''
-                    )
-                ), Range(min_=1, max_=40)]
-            ),
-            Str('train', default='stable'),
-            Str('version', default='latest'),
-        )
-    )
+    @api_method(AppCreateArgs, AppCreateResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_create_{args[0].get("app_name")}')
     def do_create(self, job, data):
         """
@@ -277,15 +204,7 @@ class AppService(CRUDService):
         self.middleware.call_sync('app.metadata.generate').wait_sync(raise_error=True)
         self.middleware.send_event('app.query', 'REMOVED', id=app_name)
 
-    @accepts(
-        Str('app_name'),
-        Dict(
-            'app_update',
-            Dict('values', additional_attrs=True, private=True),
-            Dict('custom_compose_config', additional_attrs=True, private=True),
-            Str('custom_compose_config_string', private=True, max_length=2**31),
-        )
-    )
+    @api_method(AppUpdateArgs, AppUpdateResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_update_{args[0]}')
     def do_update(self, job, app_name, data):
         """
@@ -335,16 +254,7 @@ class AppService(CRUDService):
         job.set_progress(100, f'{progress_keyword} completed for {app_name!r}')
         return self.get_instance__sync(app_name)
 
-    @accepts(
-        Str('app_name'),
-        Dict(
-            'options',
-            Bool('remove_images', default=True),
-            Bool('remove_ix_volumes', default=False),
-            Bool('force_remove_ix_volumes', default=False),
-            Bool('force_remove_custom_app', default=False),
-        )
-    )
+    @api_method(AppDeleteArgs, AppDeleteResult, roles=['APPS_WRITE'])
     @job(lock=lambda args: f'app_delete_{args[0]}')
     def do_delete(self, job, app_name, options):
         """

src/middlewared/middlewared/plugins/apps/custom_app.py
@@ -1,6 +1,3 @@
-import contextlib
-import shutil
-
 from catalog_reader.custom_app import get_version_details
 
 from middlewared.service import CallError, Service
@@ -9,7 +6,6 @@ from .compose_utils import compose_action
 from .custom_app_utils import validate_payload
 from .ix_apps.lifecycle import get_rendered_template_config_of_app, update_app_config
 from .ix_apps.metadata import update_app_metadata
-from .ix_apps.path import get_installed_app_path
 from .ix_apps.setup import setup_install_app_dir
 
 

commit 845a43acab5c6094e113e37b7e898fc7e8ceafa7

Fix redacting secrets for readonly admins (broken by 9ef345f5)

src/middlewared/middlewared/job.py
@@ -656,7 +656,7 @@ class Job:
                         self.method,
                         self.app,
                         self.result,
-                        must_redact_secrets=True
+                        expose_secrets=False,
                     )
                 except Exception as e:
                     result = None

src/middlewared/middlewared/main.py
@@ -732,7 +732,7 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
         result: dict | str | int | list | None | Job,
         *,
         new_style_returns_model: object | None = None,
-        must_redact_secrets:bool = False
+        expose_secrets: bool = True,
     ):
         """
         Serialize and redact `result` based on authenticated credential and schema.  This method is used when
@@ -747,7 +747,7 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
             result: result data to be normalized / redacted
         Keyword-only params:
             new_style_returns_model:
-            must_redact_secrets: when set to True, Secret/Private fields will _always_
+            expose secrets: when set to False, Secret/Private fields will _always_
             be redacted. This is used when generating the results info for core.get_jobs output when
             the raw_result option is set to False (which is how we call it when generating debugs).
 
@@ -755,8 +755,6 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
             pydantic.ValidationError: The result contains values that are not permitted according
             to the pydantic model. This means the return value or the model is wrong.
         """
-        expose_secrets = True
-
         if app and app.authenticated_credentials:
             # Authenticated session is _always_ presented unredacted results in the following cases:
             # 1. credential is a full_admin
@@ -779,10 +777,6 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
                     if hasattr(do_method, "new_style_returns"):
                         # FIXME: Get rid of `create`/`do_create` duality
                         methodobj = do_method
-
-        if must_redact_secrets:
-            # Caller has explicitly requested that the results be redacted.
-            expose_secrets = False
 
         if hasattr(methodobj, "new_style_returns"):
             # FIXME: When all models become new style, this should be passed explicitly

src/middlewared/middlewared/service/crud_service.py
@@ -68,7 +68,10 @@ class CRUDServiceMetabase(ServiceBase):
             klass.ENTRY = None
             # FIXME: Remove `wraps` handling when we get rid of `@filterable` in `CRUDService.query` definition
             query_result_model = query_result(klass._config.entry)
-            if not hasattr(klass.query, '_filterable') or klass.query._filterable is False:
+            if (
+                any(klass.query == getattr(parent, 'query', None) for parent in klass.__mro__[1:]) or
+                not hasattr(klass.query, '_filterable') or klass.query._filterable is False
+            ):
                 # No need to inject api method if filterable has been explicitly specified
                 klass.query = api_method(QueryArgs, query_result_model)(
                     klass.query.wraps if hasattr(klass.query, "wraps") else klass.query