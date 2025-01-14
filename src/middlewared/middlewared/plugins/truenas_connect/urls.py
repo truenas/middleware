964f55204ec32bc77eba0c86854acff8d835c4fa
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-01-14 15:35:39 +0500

    Cover hostname url usages

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -4,7 +4,6 @@ import urllib.parse
 ACCOUNT_SERVICE_BASE_URL = 'https://account-service.dev.ixsystems.net/'
 BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
 ACCOUNT_SERVICE_URL = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/accounts/{account_id}/systems/{system_id}/')
-HOSTNAME_URL = urllib.parse.urljoin(ACCOUNT_SERVICE_URL, 'hostnames/')
 LECA_DNS_URL = 'https://leca-server.dev.ixsystems.net/v1/dns-challenge'
 LECA_CLEANUP_URL = 'https://leca-server.dev.ixsystems.net/v1/hostnames'
 REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
@@ -13,3 +12,11 @@ REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, '
 
 def get_acme_config_url(tnc_config: dict) -> str:
     return urllib.parse.urljoin(tnc_config['account_service_base_url'], 'v1/accounts/{account_id}/acme')
+
+
+def get_account_service_url(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(tnc_config['account_service_base_url'], 'v1/accounts/{account_id}/systems/{system_id}/')
+
+
+def get_hostname_url(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(get_account_service_url(tnc_config), 'hostnames/')
