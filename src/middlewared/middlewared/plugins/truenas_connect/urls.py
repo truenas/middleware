0f9674812c3d2d09cbc5eea9decdabc5023ecaae
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-01-14 15:33:28 +0500

    Cover acme config url usages

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -2,7 +2,6 @@ import urllib.parse
 
 
 ACCOUNT_SERVICE_BASE_URL = 'https://account-service.dev.ixsystems.net/'
-ACME_CONFIG_URL = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/accounts/{account_id}/acme')
 BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
 ACCOUNT_SERVICE_URL = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/accounts/{account_id}/systems/{system_id}/')
 HOSTNAME_URL = urllib.parse.urljoin(ACCOUNT_SERVICE_URL, 'hostnames/')
@@ -10,3 +9,7 @@ LECA_DNS_URL = 'https://leca-server.dev.ixsystems.net/v1/dns-challenge'
 LECA_CLEANUP_URL = 'https://leca-server.dev.ixsystems.net/v1/hostnames'
 REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
 REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/systems/finalize')
+
+
+def get_acme_config_url(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(tnc_config['account_service_base_url'], 'v1/accounts/{account_id}/acme')
