30af509e78b84e4e77bbde6b2a101ff112403f5b
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-01-14 15:42:23 +0500

    Cover leca service url usages

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -3,8 +3,6 @@ import urllib.parse
 
 ACCOUNT_SERVICE_BASE_URL = 'https://account-service.dev.ixsystems.net/'
 BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
-LECA_DNS_URL = 'https://leca-server.dev.ixsystems.net/v1/dns-challenge'
-LECA_CLEANUP_URL = 'https://leca-server.dev.ixsystems.net/v1/hostnames'
 REGISTRATION_URI = urllib.parse.urljoin(BASE_URL, 'system/register')
 REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/systems/finalize')
 
@@ -19,3 +17,11 @@ def get_account_service_url(tnc_config: dict) -> str:
 
 def get_hostname_url(tnc_config: dict) -> str:
     return urllib.parse.urljoin(get_account_service_url(tnc_config), 'hostnames/')
+
+
+def get_leca_dns_url(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(tnc_config['leca_service_base_url'], 'v1/dns-challenge')
+
+
+def get_leca_cleanup_url(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(tnc_config['leca_service_base_url'], 'v1/hostnames')
