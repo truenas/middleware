d2439b18abd7c96f3004117b0bffaf556b257376
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-01-14 16:04:17 +0500

    Cover registration finalization url usages

--- src/middlewared/middlewared/plugins/truenas_connect/urls.py
+++ src/middlewared/middlewared/plugins/truenas_connect/urls.py
@@ -1,11 +1,6 @@
 import urllib.parse
 
 
-ACCOUNT_SERVICE_BASE_URL = 'https://account-service.dev.ixsystems.net/'
-BASE_URL = 'https://truenas.connect.dev.ixsystems.net/'
-REGISTRATION_FINALIZATION_URI = urllib.parse.urljoin(ACCOUNT_SERVICE_BASE_URL, 'v1/systems/finalize')
-
-
 def get_acme_config_url(tnc_config: dict) -> str:
     return urllib.parse.urljoin(tnc_config['account_service_base_url'], 'v1/accounts/{account_id}/acme')
 
@@ -28,3 +23,7 @@ def get_leca_cleanup_url(tnc_config: dict) -> str:
 
 def get_registration_uri(tnc_config: dict) -> str:
     return urllib.parse.urljoin(tnc_config['tnc_base_url'], f'system/register')
+
+
+def get_registration_finalization_uri(tnc_config: dict) -> str:
+    return urllib.parse.urljoin(tnc_config['account_service_base_url'], 'v1/systems/finalize')
