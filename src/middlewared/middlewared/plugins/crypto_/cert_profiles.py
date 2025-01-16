ec14beb0018c1eb3f1328e3a38ed0297a9219b6b
caleb <yocalebo@gmail.com>
2025-01-16 13:30:41 -0500

    use new definitions in crypto plugin

--- src/middlewared/middlewared/plugins/crypto_/cert_profiles.py
+++ src/middlewared/middlewared/plugins/crypto_/cert_profiles.py
@@ -1,119 +1,26 @@
-import copy
-
-from middlewared.schema import accepts, Dict, returns
+from middlewared.api import api_method
+from middlewared.api.current import (
+    CertProfilesArgs,
+    CertProfilesResult,
+    CSRProfilesArgs,
+    CSRProfilesResult,
+)
 from middlewared.service import Service
-
-from .utils import DEFAULT_LIFETIME_DAYS
-
-
-CERTIFICATE_PROFILES = {
-    # Options / EKUs reference rfc5246
-    'HTTPS RSA Certificate': {
-        'cert_extensions': {
-            'BasicConstraints': {
-                'enabled': True,
-                'ca': False,
-                'extension_critical': True
-            },
-            'AuthorityKeyIdentifier': {
-                'enabled': True,
-                'authority_cert_issuer': True,
-                'extension_critical': False
-            },
-            # These days, most TLS certs want "ClientAuth".
-            # LetsEncrypt appears to want this extension to issue.
-            # https://community.letsencrypt.org/t/extendedkeyusage-tls-client-
-            # authentication-in-tls-server-certificates/59140/7
-            'ExtendedKeyUsage': {
-                'enabled': True,
-                'extension_critical': True,
-                'usages': [
-                    'SERVER_AUTH',
-                    'CLIENT_AUTH',
-                ]
-            },
-            # RSA certs need "digitalSignature" for DHE,
-            # and "keyEncipherment" for nonDHE
-            # Include "keyAgreement" for compatibility (DH_DSS / DH_RSA)
-            # See rfc5246
-            'KeyUsage': {
-                'enabled': True,
-                'extension_critical': True,
-                'digital_signature': True,
-                'key_encipherment': True,
-                'key_agreement': True,
-            }
-        },
-        'key_length': 2048,
-        'key_type': 'RSA',
-        'lifetime': DEFAULT_LIFETIME_DAYS,
-        'digest_algorithm': 'SHA256'
-    },
-    'HTTPS ECC Certificate': {
-        'cert_extensions': {
-            'BasicConstraints': {
-                'enabled': True,
-                'ca': False,
-                'extension_critical': True
-            },
-            'AuthorityKeyIdentifier': {
-                'enabled': True,
-                'authority_cert_issuer': True,
-                'extension_critical': False
-            },
-            # These days, most TLS certs want "ClientAuth".
-            # LetsEncrypt appears to want this extension to issue.
-            # https://community.letsencrypt.org/t/extendedkeyusage-tls-client-
-            # authentication-in-tls-server-certificates/59140/7
-            'ExtendedKeyUsage': {
-                'enabled': True,
-                'extension_critical': True,
-                'usages': [
-                    'SERVER_AUTH',
-                    'CLIENT_AUTH',
-                ]
-            },
-            # keyAgreement is not generally required for EC certs.
-            # See Google, cloudflare certs
-            'KeyUsage': {
-                'enabled': True,
-                'extension_critical': True,
-                'digital_signature': True,
-            }
-        },
-        'ec_curve': 'SECP384R1',
-        'key_type': 'EC',
-        'lifetime': DEFAULT_LIFETIME_DAYS,
-        'digest_algorithm': 'SHA256'
-    },
-}
-CSR_PROFILES = copy.deepcopy(CERTIFICATE_PROFILES)
-for key, schema in filter(lambda v: 'cert_extensions' in v[1], CSR_PROFILES.items()):
-    schema['cert_extensions'].pop('AuthorityKeyIdentifier', None)
 
 
 class CertificateService(Service):
-
-    @accepts(roles=['CERTIFICATE_READ'])
-    @returns(Dict(
-        'certificate_profiles',
-        *[Dict(profile, additional_attrs=True) for profile in CERTIFICATE_PROFILES]
-    ))
+    @api_method(CertProfilesArgs, CertProfilesResult, roles=["CERTIFICATE_READ"])
     async def profiles(self):
         """
-        Returns a dictionary of predefined options for specific use cases i.e openvpn client/server
-        configurations which can be used for creating certificates.
+        Returns a dictionary of predefined configuration
+        options for creating certificates.
         """
-        return CERTIFICATE_PROFILES
+        return CertProfilesResult.model_dump()
 
-    @accepts(roles=['CERTIFICATE_READ'])
-    @returns(Dict(
-        *[Dict(profile, additional_attrs=True) for profile in CSR_PROFILES],
-        example=CSR_PROFILES,
-    ))
+    @api_method(CSRProfilesArgs, CSRProfilesResult, roles=["CERTIFICATE_READ"])
     async def certificate_signing_requests_profiles(self):
         """
-        Returns a dictionary of predefined options for specific use cases i.e openvpn client/server
-        configurations which can be used for creating certificate signing requests.
+        Returns a dictionary of predefined configuration
+        options for creating certificate signing requests.
         """
-        return CSR_PROFILES
+        return CSRProfilesResult.model_dump()
