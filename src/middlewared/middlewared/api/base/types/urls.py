f66ed77f775de4b87766d82b364a6f689936f23c
themylogin <themylogin@gmail.com>
2025-01-16 13:46:00 +0100

    Fix out `HttpUrl` being overwrittn by `import *` from files that do not define `__all__`

--- src/middlewared/middlewared/api/base/types/urls.py
+++ src/middlewared/middlewared/api/base/types/urls.py
@@ -4,5 +4,7 @@ from pydantic import AfterValidator, HttpUrl
 
 from middlewared.api.base.validators import https_only_check
 
+__all__ = ["HttpsOnlyURL"]
+
 
 HttpsOnlyURL = Annotated[HttpUrl, AfterValidator(https_only_check)]
