a313a3526516c4a3882baaac3348ae6c36d723aa
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-01-23 13:36:10 +0500

    Add changes to handle failure sleep
    (cherry picked from commit 62e40ac49644d54a8a5d593e381bc7aaa3e5cdb3)

--- src/middlewared/middlewared/plugins/truenas_connect/heartbeat.py
+++ src/middlewared/middlewared/plugins/truenas_connect/heartbeat.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import logging
 
 from middlewared.service import CallError, Service
@@ -7,7 +8,7 @@ from middlewared.utils.version import parse_version_string
 
 from .mixin import TNCAPIMixin
 from .status_utils import Status
-from .utils import get_account_id_and_system_id
+from .utils import calculate_sleep, get_account_id_and_system_id
 from .urls import get_heartbeat_url
 
 
@@ -37,7 +38,11 @@ class TNCHeartbeatService(Service, TNCAPIMixin):
             version=parse_version_string(await self.middleware.call('system.version_short')),
         )
         disk_mapping = await self.middleware.run_in_thread(get_disks_with_identifiers)
-        while True:
+        while tnc_config['status'] in (
+            Status.CONFIGURED.name,
+            Status.CERT_RENEWAL_IN_PROGRESS.name,
+            Status.CERT_RENEWAL_SUCCESS.name,
+        ):
             sleep_error = False
             resp = await self.call(heartbeat_url, 'post', await self.payload(disk_mapping), get_response=False)
             if resp['error'] is not None:
@@ -61,9 +66,33 @@ class TNCHeartbeatService(Service, TNCAPIMixin):
                         sleep_error = True
 
             if sleep_error:
-                pass
+                if tnc_config['last_heartbeat_failure_datetime'] is None:
+                    last_failure = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+                    await self.middleware.call('datastore.update', 'truenas_connect', tnc_config['id'], {
+                        'last_heartbeat_failure_datetime': last_failure,
+                    })
+                else:
+                    last_failure = tnc_config['last_heartbeat_failure_datetime']
+
+                sleep_secs = calculate_sleep(last_failure)
+                if sleep_secs is None:
+                    # This means that either we have a time mismatch or it's been 48 hours and we have
+                    # not been able to establish contact with TNC, so an alert should be raised
+                    logger.debug(
+                        'TNC Heartbeat: Unable to calculate sleep time, raising alert as it has likely been 48 hours '
+                        'since the last successful heartbeat (last failure: %s)', last_failure,
+                    )
+                    # TODO: Raise alert
+                    break
+                else:
+                    logger.debug(
+                        'TNC Heartbeat: Sleeping for %d seconds based off last failure (%s)', sleep_secs, last_failure
+                    )
+                    await asyncio.sleep(sleep_secs)
             else:
                 await asyncio.sleep(self.HEARTBEAT_INTERVAL)
+
+            tnc_config = await self.middleware.call('tn_connect.config_internal')
 
 
     async def payload(self, disk_mapping=None):
