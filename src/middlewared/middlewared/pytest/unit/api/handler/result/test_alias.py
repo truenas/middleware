134def43ee40b4b113bce10d990d22e3f72e7990
Logan Cary <logan.cary@ixsystems.com>
2025-01-27 10:48:04 -0500

    Move unit tests to github CI

---
+++ src/middlewared/middlewared/pytest/unit/api/handler/result/test_alias.py
@@ -0,0 +1,17 @@
+from pydantic import Field
+
+from middlewared.api.base import BaseModel
+from middlewared.api.base.handler.result import serialize_result
+
+
+def test_dump_by_alias():
+    class AliasModel(BaseModel):
+        field1_: int = Field(alias='field1')
+        field2: str
+        field3_: bool = Field(alias='field3', default=False)
+
+    class AliasModelResult(BaseModel):
+        result: AliasModel
+
+    result = serialize_result(AliasModelResult, {'field1': 1, 'field2': 'two'}, True)
+    assert result == {'field1': 1, 'field2': 'two', 'field3': False}
