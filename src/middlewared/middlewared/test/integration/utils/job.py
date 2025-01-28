66fa4fa7733ec9118f1208b1aa08cd0010f1069b
Andrew Walker <awalker@ixsystems.com>
2025-01-28 14:35:08 -0600

    Fix test asset

--- src/middlewared/middlewared/test/integration/utils/job.py
+++ src/middlewared/middlewared/test/integration/utils/job.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 import contextlib
 import logging
+from datetime import datetime
 from types import SimpleNamespace
 
 from .call import call
@@ -9,18 +10,20 @@ logger = logging.getLogger(__name__)
 
 __all__ = ["assert_creates_job"]
 
+UNITIALIZED_TS = datetime.fromtimestamp(0)
+
 
 @contextlib.contextmanager
 def assert_creates_job(method):
-    newest_job_id = 0
+    newest_job_ts = UNITIALIZED_TS
     if jobs := call("core.get_jobs"):
-        newest_job_id = jobs[-1]["id"]
+        newest_job_ts = jobs[-1]["time_started"]
 
     job = SimpleNamespace(id=None)
     yield job
 
     jobs = call("core.get_jobs", [["method", "=", method]])
-    if not jobs or jobs[-1]["id"] <= newest_job_id:
+    if not jobs or jobs[-1]["time_started"] <= newest_job_ts:
         raise RuntimeError(f"{method} was not started")
 
     job.id = jobs[-1]["id"]
