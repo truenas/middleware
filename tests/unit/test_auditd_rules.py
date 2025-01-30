4ff8ddff8ed610504a603416d71e065dcbaaf330
Mark Grimes <mark.grimes@ixsystems.com>
2025-01-30 09:44:32 -0800

    Adding support for product type - debugging
    (cherry picked from commit 20f6579d77619f78833661972af32c263c491c5a)

--- tests/unit/test_auditd_rules.py
+++ tests/unit/test_auditd_rules.py
@@ -3,21 +3,42 @@ import pytest
 import subprocess
 
 from middlewared.utils import auditd
+from middlewared.utils import ProductType
+from middlewared.plugins.system import product_type
+
+CURRENT_PRODUCT_TYPE = product_type()
+WITH_GPOS_STIG = True
+print(f"MCG DEBUG: CURRENT_PRODUCT_TYPE = {CURRENT_PRODUCT_TYPE}")
 
 
-@pytest.fixture(scope='function')
-def auditd_gpos_stig_enable():
-    auditd.set_audit_rules(True)
+def current_rule_set():
+    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
+    return rules.stdout.decode().strip()
+
+
+@pytest.fixture(
+    params=[ProductType.ENTERPRISE, ProductType.COMMUNITY_EDITION],
+    scope='function'
+)
+def auditd_gpos_stig_enable(request):
+    auditd.set_audit_rules(WITH_GPOS_STIG, request.param)
     try:
         yield
     finally:
-        auditd.set_audit_rules(False)
+        auditd.set_audit_rules(not WITH_GPOS_STIG, CURRENT_PRODUCT_TYPE)
 
 
-@pytest.fixture(scope='function')
-def auditd_gpos_stig_disable():
-    # make extra-sure we're disabled
-    auditd.set_audit_rules(False)
+@pytest.fixture(
+    params=[ProductType.ENTERPRISE, ProductType.COMMUNITY_EDITION],
+    scope='function'
+)
+def auditd_gpos_stig_disable(request):
+    try:
+        auditd.set_audit_rules(not WITH_GPOS_STIG, request.param)
+        yield
+    finally:
+        # make extra-sure we're disabled
+        auditd.set_audit_rules(not WITH_GPOS_STIG, CURRENT_PRODUCT_TYPE)
 
 
 @pytest.mark.parametrize('ruleset', auditd.AUDITRules)
@@ -27,14 +48,16 @@ def test__auditd_conf_rules_exist(ruleset):
 
 def test__auditd_enable_gpos_stig(auditd_gpos_stig_enable):
     assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.STIG_AUDIT_RULES
-    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    data = rules.stdout.decode().strip()
-    assert data != 'No rules'
+    # rules = subprocess.run(['auditctl', '-l'], capture_output=True)
+    # data = rules.stdout.decode().strip()
+    # assert data != 'No rules'
+    assert current_rule_set() != 'No rules'
 
 
 def test__auditd_disable_gpos_stig(auditd_gpos_stig_disable):
     assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.NOSTIG_AUDIT_RULES
 
-    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    data = rules.stdout.decode().strip()
-    assert data == 'No rules'
+    # rules = subprocess.run(['auditctl', '-l'], capture_output=True)
+    # data = rules.stdout.decode().strip()
+    # assert data == 'No rules'
+    assert current_rule_set() == 'No rules'
