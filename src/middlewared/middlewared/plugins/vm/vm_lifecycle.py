fca322573f723116b3d35aa8cb3b604263ee1b50
M. Rehan <mrehanlm93@gmail.com>
2025-01-31 14:52:39 +0500

    Prevent creating/updating/starting legacy VMs

--- src/middlewared/middlewared/plugins/vm/vm_lifecycle.py
+++ src/middlewared/middlewared/plugins/vm/vm_lifecycle.py
@@ -29,6 +29,7 @@ class VMService(Service, VMSupervisorMixin):
 
             ENOMEM(12): not enough free memory to run the VM without overcommit
         """
+        raise CallError('Starting legacy VMs is not supported on this system')
         await self.lifecycle_action_check()
         await self.middleware.run_in_thread(self._check_setup_connection)
 
@@ -105,6 +106,7 @@ class VMService(Service, VMSupervisorMixin):
         """
         Restart a VM.
         """
+        raise CallError('Restarting legacy VMs is not supported on this system')
         self._check_setup_connection()
         vm = self.middleware.call_sync('vm.get_instance', id_)
         stop_job = self.middleware.call_sync('vm.stop', id_, {'force_after_timeout': True})
@@ -120,6 +122,7 @@ class VMService(Service, VMSupervisorMixin):
         """
         Suspend `id` VM.
         """
+        raise CallError('Suspending legacy VMs is not supported on this system')
         self._check_setup_connection()
 
         vm = self.middleware.call_sync('vm.get_instance', id_)
@@ -131,6 +134,7 @@ class VMService(Service, VMSupervisorMixin):
         """
         Resume suspended `id` VM.
         """
+        raise CallError('Resuming legacy VMs is not supported on this system')
         self._check_setup_connection()
 
         vm = self.middleware.call_sync('vm.get_instance', id_)
