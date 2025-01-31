cd0a9728de0d16c5a617a3f2bc8967879b79efdb
Mark Grimes <mark.grimes@ixsystems.com>
2025-01-30 18:00:49 -0800

    Clean up unit test.

--- src/middlewared/middlewared/utils/auditd.py
+++ src/middlewared/middlewared/utils/auditd.py
@@ -20,23 +20,30 @@ class AUDITRules(enum.StrEnum):
     COMMUNITY = 'truenas-community-edition.rules'
 
 
-STIG_AUDIT_RULES = frozenset([rules for rules in AUDITRules])
+STIG_AUDIT_RULES = frozenset([rules for rules in AUDITRules if rules != AUDITRules.COMMUNITY])
 NOSTIG_AUDIT_RULES = frozenset([AUDITRules.BASE])
 
 
 def set_audit_rules(gpos_stig_enabled: bool, truenas_product_type: str) -> None:
+    """
+    The rules:
+        * STIG Rules CAN be applied to Community Edition Product
+        * Community Edition Rules CAN NOT be applied with STIG enabled
+
+    The rules will be enforced silently.
+    """
     rules_set = STIG_AUDIT_RULES if gpos_stig_enabled else NOSTIG_AUDIT_RULES
     optional_rules_set = set()
-    is_community_edition = bool(truenas_product_type == ProductType.COMMUNITY_EDITION)
+    want_community_edition = bool(truenas_product_type == ProductType.COMMUNITY_EDITION)
 
-    # Cannot use Community Edition rules with STIG
-    if is_community_edition and not gpos_stig_enabled:
+    if want_community_edition and not gpos_stig_enabled:
         optional_rules_set = set([AUDITRules.COMMUNITY])
 
     # first remove all files that shouldn't be there
     for rules_file in os.listdir(AUDIT_RULES_DIR):
         full_path = os.path.join(AUDIT_RULES_DIR, rules_file)
-        if rules_file not in rules_set:
+
+        if rules_file not in [*rules_set, *optional_rules_set]:
             os.unlink(full_path)
         elif not stat.S_ISLNK(os.lstat(full_path).st_mode):
             os.unlink(full_path)
--- tests/unit/test_auditd_rules.py
+++ tests/unit/test_auditd_rules.py
@@ -6,9 +6,10 @@ from middlewared.utils import auditd
 from middlewared.utils import ProductType
 from truenas_api_client import Client
 
+pp = pytest.param
 CURRENT_PRODUCT_TYPE = Client().call('system.product_type')
 WITH_GPOS_STIG = True
-print(f"MCG DEBUG: CURRENT_PRODUCT_TYPE = {CURRENT_PRODUCT_TYPE}")
+SAMPLE_CE_RULE = "-a always,exclude -F msgtype=USER_START"
 
 
 def current_rule_set():
@@ -29,16 +30,25 @@ def auditd_gpos_stig_enable(request):
 
 
 @pytest.fixture(
-    params=[ProductType.ENTERPRISE, ProductType.COMMUNITY_EDITION],
+    params=[
+        pp({"pt": ProductType.ENTERPRISE, "res": "No rules"}, id=ProductType.ENTERPRISE),
+        pp({"pt": ProductType.COMMUNITY_EDITION, "res": SAMPLE_CE_RULE}, id=ProductType.COMMUNITY_EDITION)
+    ],
     scope='function'
 )
 def auditd_gpos_stig_disable(request):
+    prod_type = request.param["pt"]
     try:
-        auditd.set_audit_rules(not WITH_GPOS_STIG, request.param)
-        yield
+        auditd.set_audit_rules(not WITH_GPOS_STIG, prod_type)
+        yield request.param
     finally:
         # make extra-sure we're disabled
         auditd.set_audit_rules(not WITH_GPOS_STIG, CURRENT_PRODUCT_TYPE)
+
+
+# Sanity check
+def test__auditd_current_prod_type():
+    assert CURRENT_PRODUCT_TYPE in dir(ProductType)
 
 
 @pytest.mark.parametrize('ruleset', auditd.AUDITRules)
@@ -47,17 +57,24 @@ def test__auditd_conf_rules_exist(ruleset):
 
 
 def test__auditd_enable_gpos_stig(auditd_gpos_stig_enable):
+    # If STIG enabled then we should see no Community Edition Rules
     assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.STIG_AUDIT_RULES
-    # rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    # data = rules.stdout.decode().strip()
-    # assert data != 'No rules'
     assert current_rule_set() != 'No rules'
 
 
 def test__auditd_disable_gpos_stig(auditd_gpos_stig_disable):
-    assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.NOSTIG_AUDIT_RULES
+    product_type = auditd_gpos_stig_disable["pt"]
+    expected_result = auditd_gpos_stig_disable["res"]
 
-    # rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    # data = rules.stdout.decode().strip()
-    # assert data == 'No rules'
-    assert current_rule_set() == 'No rules'
+    # If STIG disabled then we should see NOSTIG_AUDIT_RULES
+    # and, if requested, Community Edition Rules.
+    match product_type:
+        case ProductType.ENTERPRISE:
+            assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.NOSTIG_AUDIT_RULES
+            assert current_rule_set() == 'No rules'
+        case ProductType.COMMUNITY_EDITION:
+            expected_list = [rule_file for rule_file in auditd.NOSTIG_AUDIT_RULES]
+            expected_list += [rule_file for rule_file in set([auditd.AUDITRules.COMMUNITY])]
+            assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == set(expected_list)
+
+    assert expected_result in current_rule_set().splitlines()
