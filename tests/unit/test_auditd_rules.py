c00b83b2c4176b2d6102fd1d1f8d73822357fe73
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-02-03 11:31:44 -0500

    NAS-133851 / 25.04-RC.1 / Add support for Community Edition audit rules (by mgrimesix) (#15565)
    Add Community Edition audit rules.
    
    The auditing subsystem can generate a great deal traffic to the audit logs. Much of this logging is from PAM and its reporting of internal cron jobs.
    
    This PR introduces support for including a Community Edition rule set to reduce the amount of audit logging.
    
    Essential logging will continue with the Community Edition rule set.
    
    Original PR: #15541
    Jira URL: https://ixsystems.atlassian.net/browse/NAS-133851

--- tests/unit/test_auditd_rules.py
+++ tests/unit/test_auditd_rules.py
@@ -4,20 +4,41 @@ import subprocess
 
 from middlewared.utils import auditd
 
+WITH_GPOS_STIG = True
+# STIG test items
+MODULE_STIG_RULE = "-a always,exit -F arch=b64 -S init_module,finit_module -F key=module-load"
+SAMPLE_STIG_RULE = "-a always,exit -F arch=b64 -S all -F path=/etc/gshadow -F perm=wa -F key=identity"
+IMMUTABLE_STIG_RULE = "-e 2"
+# Non-STIG test items
+SAMPLE_CE_RULE = "-a always,exclude -F msgtype=USER_START"
+# Common test items
+INCUS_RULE = "-a always,exit -F arch=b64 -S all -F path=/usr/bin/incus -F perm=x -F auid!=-1 -F key=escalation"
+
+STIG_ASSERT_IN = [MODULE_STIG_RULE, SAMPLE_STIG_RULE]  # TODO:  IMMUTABLE_STIG_RULE when enabled
+STIG_ASSERT_NOT_IN = [SAMPLE_CE_RULE]
+
+NON_STIG_ASSERT_IN = [SAMPLE_CE_RULE, INCUS_RULE]
+NON_STIG_ASSERT_NOT_IN = [SAMPLE_STIG_RULE]
+
+
+def current_rule_set():
+    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
+    return rules.stdout.decode().strip()
+
 
 @pytest.fixture(scope='function')
 def auditd_gpos_stig_enable():
-    auditd.set_audit_rules(True)
+    auditd.set_audit_rules(WITH_GPOS_STIG)
     try:
         yield
     finally:
-        auditd.set_audit_rules(False)
+        auditd.set_audit_rules(not WITH_GPOS_STIG)
 
 
 @pytest.fixture(scope='function')
 def auditd_gpos_stig_disable():
     # make extra-sure we're disabled
-    auditd.set_audit_rules(False)
+    auditd.set_audit_rules(not WITH_GPOS_STIG)
 
 
 @pytest.mark.parametrize('ruleset', auditd.AUDITRules)
@@ -26,15 +47,26 @@ def test__auditd_conf_rules_exist(ruleset):
 
 
 def test__auditd_enable_gpos_stig(auditd_gpos_stig_enable):
+    # With STIG enabled we should see no Community Edition Rules
     assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.STIG_AUDIT_RULES
-    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    data = rules.stdout.decode().strip()
-    assert data != 'No rules'
+
+    stig_rule_set = current_rule_set().splitlines()
+    assert stig_rule_set != 'No rules'
+
+    for audit_rule in STIG_ASSERT_IN:
+        assert audit_rule in stig_rule_set
+    for audit_rule in STIG_ASSERT_NOT_IN:
+        assert audit_rule not in stig_rule_set
 
 
 def test__auditd_disable_gpos_stig(auditd_gpos_stig_disable):
+    # With STIG disabled we should see NOSTIG_AUDIT_RULES
     assert set(os.listdir(auditd.AUDIT_RULES_DIR)) == auditd.NOSTIG_AUDIT_RULES
 
-    rules = subprocess.run(['auditctl', '-l'], capture_output=True)
-    data = rules.stdout.decode().strip()
-    assert data == 'No rules'
+    non_stig_rule_set = current_rule_set().splitlines()
+    assert non_stig_rule_set != 'No rules'
+
+    for audit_rule in NON_STIG_ASSERT_IN:
+        assert audit_rule in non_stig_rule_set
+    for audit_rule in NON_STIG_ASSERT_NOT_IN:
+        assert audit_rule not in non_stig_rule_set
