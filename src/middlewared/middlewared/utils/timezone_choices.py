1b68c486140c2d5c4762d9bb8d370054c662e648
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2025-02-05 12:56:08 -0500

    NAS-134026 / 25.10 / change timezone_choices cache to immutable type (#15597)
    * change timezone_choices cache to immutable type
    
    * add util module just like country_codes

---
+++ src/middlewared/middlewared/utils/timezone_choices.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+__all__ = ("tz_choices",)
+
+
+@cache
+def tz_choices() -> tuple[tuple[str, str]]:
+    # Logic deduced from what timedatectl list-timezones does
+    tz = list()
+    with open("/usr/share/zoneinfo/tzdata.zi") as f:
+        for line in filter(lambda x: x[0] in ("Z", "L"), f):
+            index = 1 if line[0] == "Z" else 2
+            tz_choice = line.split()[index].strip()
+            tz.append((tz_choice, tz_choice))
+    return tuple(tz)
