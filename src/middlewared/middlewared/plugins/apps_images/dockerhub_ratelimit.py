9cc53c2b402c8f99a8f5959176e72a9fa97d38fa
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-02-07 08:23:31 -0500

    Use app registry credentials when retrieving dockerhub limit (#15629)
    (cherry picked from commit 71a9cc6d64666ccff85fd3be362c99edf5bdc7ad)
    
    Co-authored-by: M. Rehan <mrehanlm93@gmail.com>

--- src/middlewared/middlewared/plugins/apps_images/dockerhub_ratelimit.py
+++ src/middlewared/middlewared/plugins/apps_images/dockerhub_ratelimit.py
@@ -18,7 +18,11 @@ class ContainerImagesService(Service):
 
         Please refer to https://docs.docker.com/docker-hub/download-rate-limit/ for more information.
         """
-        limits_header = await ContainerRegistryClientMixin().get_docker_hub_rate_limit_preview()
+        auth = None
+        if creds := (await self.middleware.call('app.registry.query', [['uri', '=', 'https://index.docker.io/v1/']])):
+            auth = {'login': creds[0]['username'], 'password': creds[0]['password']}
+
+        limits_header = await ContainerRegistryClientMixin().get_docker_hub_rate_limit_preview(auth)
 
         if limits_header.get('response_obj') and hasattr(limits_header['response_obj'], 'headers'):
             return normalize_docker_limits_header(limits_header['response_obj'].headers)
