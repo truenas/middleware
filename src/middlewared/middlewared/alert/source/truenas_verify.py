806edde13d8048913ead8fa618e2f47c61c6d2d7
Mark Grimes <mark.grimes@ixsystems.com>
2025-02-10 13:45:33 -0800

    Add new alert source file in support of periodic truenas_verify runs. Update audit setup to generate an 'initial' version tagged truenas_verify log.
    Fix truenas_verify audit setup.
    
    Use ThreadedAlertSource
    
    Fix up import formatting.
    
    Removed setup_truenas_verify as an API endpoint.  Moved to an audit utility.
    Converted truenas_verify call to middleware async 'run'.
    Small Flake8 fix:  json was imported but not used.
    
    Convert to run_in_thread module call.
    
    Move from once per hour to once per day.
    
    Change truenas_verify to run only when STIG is enabled.

---
+++ src/middlewared/middlewared/alert/source/truenas_verify.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+import subprocess
+
+from middlewared.alert.base import AlertClass, AlertCategory, Alert, AlertLevel, ThreadedAlertSource
+from middlewared.alert.schedule import IntervalSchedule
+from middlewared.utils.security import system_security_config_to_stig_type
+
+
+# --------------- Monitored Alerts ----------------
+class TrueNASVerifyServiceChangeDetectionAlertClass(AlertClass):
+    category = AlertCategory.AUDIT
+    level = AlertLevel.ERROR
+    title = "TrueNAS Verify Service: Changes detected in root file system."
+    text = "%(verrs)s"
+
+
+class TrueNASVerifyServiceChangeDetectionAlertSource(ThreadedAlertSource):
+    '''
+    Periodic verification of root file system
+    '''
+    schedule = IntervalSchedule(timedelta(hours=24))
+    run_on_backup_node = False
+
+    def check_sync(self):
+        # Run only if in stig mode
+        if self.enabled():
+            # Capture the results in syslog
+            res = subprocess.run(['truenas_verify', 'syslog'], capture_output=True, text=True)
+            if res.returncode:
+                errmsg = f"{res.stderr}  See syslog for details."
+                return Alert(
+                    TrueNASVerifyServiceChangeDetectionAlertClass,
+                    {'verrs': errmsg},
+                    key=None
+                )
+
+    def enabled(self):
+        security_config = self.middleware.call_sync('system.security.config')
+        enabled_stig = system_security_config_to_stig_type(security_config)
+        return enabled_stig
