b744d25f23946debc78ebc8ef19bd7a4ca35f8a0
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-02-15 21:21:20 +0500

    Add pydantic models for auth.twofactor namespace

--- src/middlewared/middlewared/api/v25_10_0/__init__.py
+++ src/middlewared/middlewared/api/v25_10_0/__init__.py
@@ -9,6 +9,7 @@ from .app_image import *  # noqa
 from .app_ix_volume import *  # noqa
 from .app_registry import *  # noqa
 from .auth import *  # noqa
+from .auth_two_factor import *  # noqa
 from .boot import *  # noqa
 from .boot_environments import *  # noqa
 from .catalog import *  # noqa
