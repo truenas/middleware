841887967679c653b1cac3769f600f4ed1db9f40
themylogin <themylogin@gmail.com>
2025-02-23 15:50:01 +0100

    Make data partition slightly smaller than the disk

--- tests/api2/test_pool_attach.py
+++ tests/api2/test_pool_attach.py
@@ -1,5 +1,18 @@
-from middlewared.test.integration.assets.pool import another_pool
-from middlewared.test.integration.utils import call, ssh
+from middlewared.test.integration.assets.pool import another_pool, oversize_pool
+from middlewared.test.integration.utils import call
+
+
+def test_attach_stripe():
+    with another_pool() as pool:
+        disk = call("disk.get_unused")[0]["name"]
+
+        call("pool.attach", pool["id"], {
+            "target_vdev": pool["topology"]["data"][0]["guid"],
+            "new_disk": disk,
+        }, job=True)
+
+        pool = call("pool.get_instance", pool["id"])
+        assert pool["topology"]["data"][0]["type"] == "MIRROR"
 
 
 def test_attach_raidz1_vdev():
@@ -24,3 +37,13 @@ def test_attach_raidz1_vdev():
 
         pool = call("pool.get_instance", pool["id"])
         assert pool["expand"]["state"] == "FINISHED"
+
+
+def test_attach_oversize_pool():
+    with oversize_pool() as pool:
+        disk = call("disk.get_unused")[0]["name"]
+
+        call("pool.attach", pool["id"], {
+            "target_vdev": pool["topology"]["data"][0]["guid"],
+            "new_disk": disk,
+        }, job=True)
