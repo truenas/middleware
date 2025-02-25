d1d25ddce622c21f20e4939b4500ed2f55e9805f
Andrew Walker <awalker@ixsystems.com>
2025-02-25 10:28:45 -0600

    Remove tests

--- tests/api2/test_websocket_interface.py
+++
@@ -1,8 +0,0 @@
-from auto_config import interface
-from middlewared.test.integration.utils import call
-
-
-def test_websocket_interface():
-    """This tests to ensure we return the interface name
-    by which the websocket connection has been established."""
-    assert call("interface.websocket_interface")["id"] == interface
