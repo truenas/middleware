d6e18dc6ce27f0dc75a275603f0d0e90c831808c
caleb <yocalebo@gmail.com>
2025-02-27 13:39:43 -0500

    address review

--- src/middlewared/middlewared/utils/disks_/get_disks.py
+++ src/middlewared/middlewared/utils/disks_/get_disks.py
@@ -10,17 +10,14 @@ def get_disks(*, name_filters: list[str] | None = None) -> Generator[DiskEntry]:
     each disk detected on the system.
 
     Args:
-        get_partitions: bool if True, will enumerate
-            GPT partition information
         name_filters: list of strings, represent a list
             of disk names that will be filtered upon.
             The name of the disk may take the form
             of 'sda' or '/dev/sda'.
     """
-    for disk in __iterate_disks():
-        if (
-            name_filters is None
-            or disk.name in name_filters
-            or disk.devpath in name_filters
-        ):
-            yield disk
+    if name_filters is None:
+        return __iterate_disks()
+    else:
+        for disk in __iterate_disks():
+            if disk.name in name_filters or disk.devpath in name_filters:
+                yield disk
