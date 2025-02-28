48fa9e1db5a93c14c82f2db6f0d1e5c9952b28d5
Caleb <yocalebo@gmail.com>
2025-02-28 09:13:32 -0500

    add unit test for wipe_quick()

--- tests/unit/test_get_disks.py
+++ tests/unit/test_get_disks.py
@@ -1,10 +1,15 @@
+import base64
 import json
 import subprocess
 import uuid
 
 from middlewared.utils.disks_.get_disks import get_disks
+from truenas_api_client import Client
 
 import pytest
+
+VMFS_MAGIC_STRING_B64 = "DdABwA=="
+VMFS_MAGIC_STRING_WFS = "VMFS_volume_member"
 
 
 @pytest.mark.parametrize(
@@ -43,6 +48,39 @@ def test__read_partitions():
             assert uuid.UUID(a.unique_partition_guid) == uuid.UUID(b["uuid"])
             assert a.first_lba == b["start"]
             assert a.last_lba == (b["start"] + b["size"]) - 1
-            assert a.size_bytes == b["size"] * a.sector_info.logical_sector_size
+            assert a.size_bytes == b["size"] * disk.lbs
 
     assert at_least_one, "No disks with partitions! (or get_disks() failed)"
+
+
+def test__wipe_quick():
+    with Client() as c:
+        for disk_class in get_disks(
+            name_filters=[c.call("disk.get_unused")[0]["name"]]
+        ):
+            disk = disk_class
+            break
+        else:
+            assert False, "Failed to find an unused disk"
+
+    # Wipe any existing partitions
+    if disk.partitions:
+        disk.wipe_quick()
+        assert not disk.partitions
+
+    # Fake a VMFS volume at start of disk
+    with open(disk.devpath, "wb") as f:
+        f.seek(1048576)
+        f.write(base64.b64decode(VMFS_MAGIC_STRING_B64))
+
+    fs = json.loads(
+        subprocess.run(["wipefs", "-J", f"/dev/{disk}"], capture_output=True).stdout
+    )["signatures"][0]
+    assert fs["type"] == VMFS_MAGIC_STRING_WFS
+
+    # Clean the drive
+    disk.wipe_quick()
+    fs = json.loads(
+        subprocess.run(["wipefs", "-J", f"/dev/{disk}"], capture_output=True).stdout
+    )["signatures"]
+    assert not fs
