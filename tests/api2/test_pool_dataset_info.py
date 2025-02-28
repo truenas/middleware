310c91992047b657dd80be8d0b94d4cefe630c9d
Andrew Walker <awalker@ixsystems.com>
2025-02-28 14:08:34 -0600

    Remove tests

--- tests/api2/test_pool_dataset_info.py
+++
@@ -1,6 +0,0 @@
-from middlewared.test.integration.utils import call
-from middlewared.test.integration.assets.pool import pool
-
-
-def test_recommended_zvol_blocksize():
-    assert call("pool.dataset.recommended_zvol_blocksize", pool) == "16K"
