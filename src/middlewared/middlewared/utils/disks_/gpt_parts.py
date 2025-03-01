37a8cece4436aa235735760c77385a6626838e6c
Caleb <yocalebo@gmail.com>
2025-03-01 10:48:49 -0500

    pull out gpt guids for zol/zof

--- src/middlewared/middlewared/utils/disks_/gpt_parts.py
+++ src/middlewared/middlewared/utils/disks_/gpt_parts.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 from types import MappingProxyType
 
-__all__ = ("PART_TYPES", "GptPartEntry")
+__all__ = ("PART_TYPES", "GptPartEntry", "ZOF_PART_TYPE", "ZOL_PART_TYPE")
 
 # there are a TON more but we only care about a few
+ZOL_PART_TYPE = "6a898cc3-1dd2-11b2-99a6-080020736631"
+ZOF_PART_TYPE = "516e7cba-6ecf-11d6-8ff8-00022d09712b"
 PART_TYPES = MappingProxyType(
     {
         "21686148-6449-6e6f-744e-656564454649": "BIOS Boot Partition",  # boot drives
         "c12a7328-f81f-11d2-ba4b-00a0c93ec93b": "EFI System Parition",  # boot drives
-        "6a898cc3-1dd2-11b2-99a6-080020736631": "ZFS",  # linux
-        "516e7cba-6ecf-11d6-8ff8-00022d09712b": "ZFS",  # freebsd
+        ZOL_PART_TYPE: "ZFS",  # linux
+        ZOF_PART_TYPE: "ZFS",  # freeBSD
     }
 )
 
