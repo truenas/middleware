03ab99c3968d228ddbca82247f3bb95d61315b35
Logan Cary <logan.cary@ixsystems.com>
2025-03-05 15:41:25 -0500

    NetWorkConfigurationState has hosts

--- src/middlewared/middlewared/api/v25_10_0/network_configuration.py
+++ src/middlewared/middlewared/api/v25_10_0/network_configuration.py
@@ -37,12 +37,13 @@ class NetWorkConfigurationState(BaseModel):
     nameserver1: IPvAnyAddress
     nameserver2: IPvAnyAddress
     nameserver3: IPvAnyAddress
+    hosts: list[str]
 
 
 class NetworkConfigurationEntry(BaseModel):
     id: int
     hostname: Hostname
-    domain: Domain = NotRequired
+    domain: Domain
     ipv4gateway: IPvAnyAddress
     """Used instead of the default gateway provided by DHCP."""
     ipv6gateway: IPvAnyAddress
@@ -56,13 +57,13 @@ class NetworkConfigurationEntry(BaseModel):
     """Must be provided if a proxy is to be used for network operations."""
     hosts: list[str]
     domains: list[str]
-    service_announcement: ServiceAnnouncement = NotRequired
+    service_announcement: ServiceAnnouncement
     """Determines the broadcast protocols that will be used to advertise the server."""
-    activity: NetworkConfigurationActivity = NotRequired
+    activity: NetworkConfigurationActivity
     hostname_local: Hostname
     hostname_b: Hostname | None = NotRequired
     hostname_virtual: Hostname | None = NotRequired
-    state: NetWorkConfigurationState = NotRequired
+    state: NetWorkConfigurationState
 
 
 class NetWorkConfigurationUpdate(NetworkConfigurationEntry, metaclass=ForUpdateMetaclass):
