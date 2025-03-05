76738e58007de8acf066c7c2462ecf0c910f84f4
Caleb <yocalebo@gmail.com>
2025-03-05 08:02:46 -0500

    more typos

--- tests/unit/test_get_disks.py
+++ tests/unit/test_get_disks.py
@@ -48,7 +48,7 @@ def test__read_partitions():
             assert uuid.UUID(a.unique_partition_guid) == uuid.UUID(b["uuid"])
             assert a.first_lba == b["start"]
             assert a.last_lba == (b["start"] + b["size"]) - 1
-            assert disk.size_bytes == b["size"] * disk.lbs
+            assert disk.size_bytes == b["size"] * 512
 
     assert at_least_one, "No disks with partitions! (or get_disks() failed)"
 
