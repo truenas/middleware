8977e9f97e94603c4f790cdf6f0426d6d3078d28
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-10 06:58:35 +0400

    Update netdata plugin to store data using guid and run once every 5 min

--- src/freenas/usr/lib/netdata/python.d/truenas_pool.chart.py
+++ src/freenas/usr/lib/netdata/python.d/truenas_pool.chart.py
@@ -6,6 +6,7 @@ from middlewared.utils.metrics.pool_stats import get_pool_dataset_stats
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
+        self.update_every = 300
 
     def check(self):
         self.add_pool_stats_to_charts()
@@ -13,9 +14,10 @@ class Service(SimpleService):
 
     def get_data(self):
         data = {}
-        for pool_name, info in get_pool_dataset_stats().items():
+        for pool_guid, info in get_pool_dataset_stats().items():
             for i, value in info.items():
-                data[f'{pool_name}.{i}'] = value
+                data[f'{pool_guid}.{i}'] = value
+            data[f'{pool_guid}.total'] = info['used'] + info['available']
         return data
 
     def add_pool_stats_to_charts(self):
@@ -27,6 +29,7 @@ class Service(SimpleService):
             'line',
         ])
 
-        for pool_name in data.keys():
-            self.charts['usage'].add_dimension([f'{pool_name}.available', f'available', 'absolute'])
-            self.charts['usage'].add_dimension([f'{pool_name}.used', f'used', 'absolute'])
+        for pool_guid in data.keys():
+            self.charts['usage'].add_dimension([f'{pool_guid}.available', 'available', 'absolute'])
+            self.charts['usage'].add_dimension([f'{pool_guid}.used', 'used', 'absolute'])
+            self.charts['usage'].add_dimension([f'{pool_guid}.total', 'total', 'absolute'])
