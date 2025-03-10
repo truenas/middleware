4279ab6a524fb5e96393a50c8f7002ad9b9194cb
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-10 07:14:50 +0400

    Update pool usage plugin usage in realtime reporting

--- src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
+++ src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
@@ -1,12 +1,16 @@
 from collections import defaultdict
 
+from middlewared.utils.zfs import query_imported_fast_impl
+
 from .utils import safely_retrieve_dimension
 
 
 def get_pool_stats(netdata_metrics: dict) -> dict:
-    data = defaultdict(lambda: {'available': None, 'used': None})
+    data = defaultdict(lambda: {'available': None, 'used': None, 'total': None})
+    pool_data = query_imported_fast_impl()
 
     for dimension_name, value in safely_retrieve_dimension(netdata_metrics, 'truenas_pool.usage').items():
-        pool_name, stat_key = dimension_name.split('.')
-        data[pool_name][stat_key] = value
+        value = value or {}
+        pool_guid, stat_key = dimension_name.split('.')
+        data[pool_data[pool_guid]['name']][stat_key] = value
     return data
