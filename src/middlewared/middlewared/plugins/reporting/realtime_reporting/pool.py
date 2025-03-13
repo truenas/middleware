ad48500eef5f5ccb009870fb29aa8b230cbad8ea
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-13 06:00:12 +0500

    Minor fix

--- src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
+++ src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
@@ -9,7 +9,7 @@ def get_pool_stats(netdata_metrics: dict) -> dict:
     data = defaultdict(lambda: {'available': None, 'used': None, 'total': None})
     pool_data = query_imported_fast_impl()
 
-    for dimension_name, value in safely_retrieve_dimension(netdata_metrics, 'truenas_pool.usage').items():
+    for dimension_name, value in (safely_retrieve_dimension(netdata_metrics, 'truenas_pool.usage') or {}).items():
         value = value or {}
         pool_guid, stat_key = dimension_name.split('.')
         data[pool_data[pool_guid]['name']][stat_key] = value
