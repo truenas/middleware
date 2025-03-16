8acaa6a2a393133c3929dcf5c94c542a24db10fe
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-16 21:00:56 +0500

    Safely retrieve DNS authenticators in migration
    This commit adds changes to safely retrieve ACME DNS authenticators because if a user restored without his secret seed, we still end up having the ACME DNS authenticator rows which obviously won't work well. So we drop those rows and update its' related usages as well.

--- src/middlewared/middlewared/alembic/versions/25.04/2024-11-05_23-35_authenticator_normalization.py
+++ src/middlewared/middlewared/alembic/versions/25.04/2024-11-05_23-35_authenticator_normalization.py
@@ -7,6 +7,7 @@ Create Date: 2024-11-05 23:35:45.960915+00:00
 
 """
 import json
+from collections import defaultdict
 
 from alembic import op
 
@@ -19,16 +20,66 @@ branch_labels = None
 depends_on = None
 
 
+def remove_authenticator_ref(authenticator_id, cert_ids, conn):
+    if not cert_ids:
+        return
+
+    for cert in conn.execute(
+        f"SELECT * FROM system_certificate WHERE id IN ({','.join(['?'] * len(cert_ids))})", tuple(cert_ids)
+    ).fetchall():
+        cert = dict(cert)
+
+        try:
+            authenticators = json.loads(cert['cert_domains_authenticators'] or '{}')
+        except json.decoder.JSONDecodeError:
+            # Shouldn't happen, but just being safe
+            continue
+
+        # Filter out the authenticator reference
+        updated_authenticators = {
+            domain: aid for domain, aid in authenticators.items() if aid != authenticator_id
+        }
+
+        # Only update if a change was made
+        if updated_authenticators != authenticators:
+            conn.execute(
+                "UPDATE system_certificate SET cert_domains_authenticators = :auths WHERE id = :id",
+                {'auths': json.dumps(updated_authenticators), 'id': cert['id']}
+            )
+
+
 def upgrade():
     conn = op.get_bind()
-    for authenticator_config in conn.execute("SELECT * FROM system_acmednsauthenticator").fetchall():
+    authenticator_configs = [
+        dict(config) for config in conn.execute("SELECT * FROM system_acmednsauthenticator").fetchall()
+    ]
+    authenticator_mapping = defaultdict(list)
+    for cert in conn.execute("SELECT * FROM system_certificate").fetchall():
+        cert = dict(cert)
+        try:
+            authenticators = json.loads(cert['cert_domains_authenticators'] or '{}')
+        except json.JSONDecodeError:
+            # Shouldn't happen but just being safe
+            continue
+
+        for value in authenticators.values():
+            authenticator_mapping[value].append(cert['id'])
+
+    for authenticator_config in authenticator_configs:
         authenticator_config = dict(authenticator_config)
-        attributes = json.loads(decrypt(authenticator_config['attributes']))
-        attributes['authenticator'] = authenticator_config['authenticator']
-        conn.execute(
-            "UPDATE system_acmednsauthenticator SET attributes = ? WHERE id = ?",
-            (encrypt(json.dumps(attributes)), authenticator_config['id'])
-        )
+        try:
+            attributes = json.loads(decrypt(authenticator_config['attributes']))
+        except json.JSONDecodeError:
+            remove_authenticator_ref(
+                authenticator_config['id'], authenticator_mapping[authenticator_config['id']], conn
+            )
+            conn.execute("DELETE FROM system_acmednsauthenticator WHERE id = :id", {'id': authenticator_config['id']})
+        else:
+            attributes['authenticator'] = authenticator_config['authenticator']
+            conn.execute(
+                "UPDATE system_acmednsauthenticator SET attributes = ? WHERE id = ?",
+                (encrypt(json.dumps(attributes)), authenticator_config['id'])
+            )
 
     with op.batch_alter_table('system_acmednsauthenticator', schema=None) as batch_op:
         batch_op.drop_column('authenticator')
