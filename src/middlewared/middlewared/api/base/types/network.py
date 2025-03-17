79a8a521d1511e5f0d181ec3efb8fc4af7209405
Logan Cary <logan.cary@ixsystems.com>
2025-03-17 12:59:38 -0400

    use match_validator for clearer error messages

--- src/middlewared/middlewared/api/base/types/network.py
+++ src/middlewared/middlewared/api/base/types/network.py
@@ -1,6 +1,8 @@
 import functools
+import re
 from typing import Annotated, Literal
 from pydantic import AfterValidator, Field, IPvAnyAddress as IPvAnyAddress_
+from ..validators import match_validator
 
 __all__ = ["TcpPort", "Hostname", "Domain", "IPvAnyAddress", "exclude_tcp_ports"]
 
@@ -24,6 +26,12 @@ def _validate_ipaddr(address: str):
 
 
 TcpPort = Annotated[int, Field(ge=1, le=65535)]
-Hostname = Annotated[str, Field(pattern=r'^[a-zA-Z\.\-0-9]*[a-zA-Z0-9]$')]
-Domain = Annotated[str, Field(pattern=r'^[a-zA-Z\.\-0-9]*$')]
+Hostname = Annotated[str, AfterValidator(match_validator(
+    re.compile(r"^[a-z\.\-0-9]*[a-z0-9]$", re.IGNORECASE),
+    "Hostname can only contain letters, numbers, periods, and dashes and must end with a letter or number"
+))]
+Domain = Annotated[str, AfterValidator(match_validator(
+    re.compile(r"^[a-z\.\-0-9]*$", re.IGNORECASE),
+    "Domain can only contain letters, numbers, periods, and dashes"
+))]
 IPvAnyAddress =  Literal[''] | Annotated[str, AfterValidator(_validate_ipaddr)]
