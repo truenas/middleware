0e1505a86612170fb90ab6a10f3f4c55df9dd9b0
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-21 03:10:41 +0500

    Remove unused utils

--- src/middlewared/middlewared/plugins/crypto_/utils.py
+++ src/middlewared/middlewared/plugins/crypto_/utils.py
@@ -1,21 +1,11 @@
 import re
 
-from cryptography import x509
-
 from truenas_crypto_utils.key import export_private_key
 
 
 # Cert locations
 CERT_ROOT_PATH = '/etc/certificates'
 DEFAULT_CERT_NAME = 'truenas_default'
-EC_CURVES = [
-    'SECP256R1',
-    'SECP384R1',
-    'SECP521R1',
-    'ed25519',
-]  # FIXME: See if we can remvoe thisn ow
-EC_CURVE_DEFAULT = 'SECP384R1'
-EKU_OIDS = [i for i in dir(x509.oid.ExtendedKeyUsageOID) if not i.startswith('__')]
 RE_CERTIFICATE = re.compile(r"(-{5}BEGIN[\s\w]+-{5}[^-]+-{5}END[\s\w]+-{5})+", re.M | re.S)
 
 # Defining cert constants being used
@@ -37,9 +27,3 @@ def get_private_key(data: dict) -> str:
         private_key = export_private_key(data['privatekey'], data['passphrase'])
 
     return private_key
-
-
-def _set_required(name):
-    def set_r(attr):
-        attr.required = True
-    return {'name': name, 'method': set_r}
