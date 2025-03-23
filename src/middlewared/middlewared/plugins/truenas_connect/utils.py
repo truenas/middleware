bfdb18cbc2459983b98cff91a2933ca297a71373
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-24 01:50:13 +0500

    Remove unused account/system id util

--- src/middlewared/middlewared/plugins/truenas_connect/utils.py
+++ src/middlewared/middlewared/plugins/truenas_connect/utils.py
@@ -14,17 +14,6 @@ CONFIGURED_TNC_STATES = (
 HEARTBEAT_INTERVAL = 120
 
 
-def get_account_id_and_system_id(config: dict) -> dict | None:
-    jwt_details = config['registration_details'] or {}
-    if all(jwt_details.get(k) for k in ('account_id', 'system_id')) is False:
-        return None
-
-    return {
-        'account_id': jwt_details['account_id'],
-        'system_id': jwt_details['system_id'],
-    }
-
-
 def get_unset_payload() -> dict:
     return {
         'registration_details': {},
