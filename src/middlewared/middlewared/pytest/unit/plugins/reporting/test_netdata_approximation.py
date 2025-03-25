92a9ff6a8e638bebd4a2c723f4bc6195252691ab
M. Rehan <mrehanlm93@gmail.com>
2025-03-25 06:29:54 +0500

    Add plugin to retrieve disk temperatures

--- src/middlewared/middlewared/pytest/unit/plugins/reporting/test_netdata_approximation.py
+++ src/middlewared/middlewared/pytest/unit/plugins/reporting/test_netdata_approximation.py
@@ -4,9 +4,9 @@ from middlewared.plugins.reporting.utils import get_metrics_approximation, calcu
 
 
 @pytest.mark.parametrize('disk_count,core_count,interface_count,pool_count,services_count,vms_count,expected_output', [
-    (4, 2, 1, 2, 10, 2, {1: 705, 60: 4}),
-    (1600, 32, 4, 4, 10, 1, {1: 8766, 60: 1600}),
-    (10, 16, 2, 2, 12, 3, {1: 844, 60: 10}),
+    (4, 2, 1, 2, 10, 2, {1: 699, 300: 10}),
+    (1600, 32, 4, 4, 10, 1, {1: 8754, 300: 1612}),
+    (10, 16, 2, 2, 12, 3, {1: 838, 300: 16}),
 ])
 def test_netdata_metrics_count_approximation(
     disk_count, core_count, interface_count, pool_count, services_count, vms_count, expected_output
@@ -19,14 +19,14 @@ def test_netdata_metrics_count_approximation(
 @pytest.mark.parametrize(
     'disk_count,core_count,interface_count,pool_count,services_count,vms_count,days,'
     'bytes_per_point,tier_interval,expected_output', [
-        (4, 2, 1, 2, 10, 2, 7, 1, 1, 406),
+        (4, 2, 1, 2, 10, 2, 7, 1, 1, 403),
         (4, 2, 1, 2, 10, 1, 7, 4, 60, 25),
-        (1600, 32, 4, 12, 2, 4, 4, 1, 1, 2937),
-        (1600, 32, 4, 10, 1, 4, 4, 4, 900, 13),
-        (10, 16, 2, 2, 12, 1, 3, 1, 1, 184),
+        (1600, 32, 4, 12, 2, 4, 4, 1, 1, 2918),
+        (1600, 32, 4, 10, 1, 4, 4, 4, 900, 12),
+        (10, 16, 2, 2, 12, 1, 3, 1, 1, 183),
         (10, 16, 2, 2, 10, 3, 3, 4, 60, 13),
-        (1600, 32, 4, 4, 12, 3, 18, 1, 1, 13201),
-        (1600, 32, 4, 4, 12, 1, 18, 4, 900, 58),
+        (1600, 32, 4, 4, 12, 3, 18, 1, 1, 13151),
+        (1600, 32, 4, 4, 12, 1, 18, 4, 900, 57),
     ],
 )
 def test_netdata_disk_space_approximation(
@@ -58,5 +58,5 @@ def test_netdata_days_approximation(
         disk_count, core_count, interface_count, pool_count, vms_count, services_count
     )
     disk_size = calculate_disk_space_for_netdata(metric_intervals, days, bytes_per_point, tier_interval)
-    total_metrics = metric_intervals[1] + (metric_intervals[60] / 60)
+    total_metrics = metric_intervals[1] + (metric_intervals[300] / 300)
     assert round((disk_size * 1024 * 1024) / (bytes_per_point * total_metrics * (86400 / tier_interval))) == days
