90edfdedec6b1a0559d5472d918b09d818041cf0
Logan Cary <logan.cary@ixsystems.com>
2025-03-31 15:23:22 -0400

    NAS-135097 / 25.10 / convert system.* methods (#16146)
    http://jenkins.eng.ixsystems.net:8080/job/tests/job/api_tests/3727/
    
    ---------
    
    Co-authored-by: Caleb St. John <30729806+yocalebo@users.noreply.github.com>

---
+++ src/middlewared/middlewared/api/v25_10_0/system.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from pydantic import Field
+
+from middlewared.api.base import BaseModel, single_argument_result
+
+
+__all__ = ["SystemHostIDArgs", "SystemHostIDResult", "SystemInfoArgs", "SystemInfoResult",]
+
+
+class SystemHostIDArgs(BaseModel):
+    pass
+
+
+class SystemHostIDResult(BaseModel):
+    result: str
+    """The system host identifier."""
+
+
+class SystemInfoArgs(BaseModel):
+    pass
+
+
+@single_argument_result
+class SystemInfoResult(BaseModel):
+    version: str
+    """TrueNAS version."""
+    buildtime: datetime
+    """TrueNAS build time."""
+    hostname: str
+    """System host name."""
+    physmem: int
+    """System physical memory in bytes."""
+    model: str
+    """CPU model."""
+    cores: int
+    """Number of CPU cores."""
+    physical_cores: int
+    """Number of physical CPU cores."""
+    loadavg: list
+    uptime: str
+    uptime_seconds: float
+    system_serial: str | None
+    system_product: str | None
+    system_product_version: str | None
+    license: dict | None
+    boottime: datetime
+    datetime_: datetime = Field(alias='datetime')
+    timezone: str
+    system_manufacturer: str | None
+    ecc_memory: bool
