1df378bc35c8122095ff5ba5115294fef84a2325
Logan Cary <logan.cary@ixsystems.com>
2025-04-03 15:38:31 -0400

    NAS-135104 / 25.10 / Convert dns namespace (#16149)
    http://jenkins.eng.ixsystems.net:8080/job/tests/job/api_tests/3761/

--- src/middlewared/middlewared/api/base/types/list.py
+++ src/middlewared/middlewared/api/base/types/list.py
@@ -1,18 +1,22 @@
-from typing import Annotated, Hashable, List, TypeVar
+from typing import Annotated, TypeVar
 
 from pydantic_core import PydanticCustomError
-
 from pydantic import AfterValidator, Field
 
 __all__ = ['UniqueList']
 
-T = TypeVar('T', bound=Hashable)
+
+T = TypeVar('T')
 
 
 def _validate_unique_list(v: list[T]) -> list[T]:
-    if len(v) != len(set(v)):
-        raise PydanticCustomError('unique_list', 'List must be unique')
+    """Raise `PydanticCustomError` if any item in `v` is equal to any other item in `v`."""
+    # Use equals comparison instead of `set` to support unhashable types like `BaseModel`.
+    for i in range(len(v)):
+        for j in range(i + 1, len(v)):
+            if v[i] == v[j]:
+                raise PydanticCustomError('unique_list', 'List must be unique')
     return v
 
 
-UniqueList = Annotated[List[T], AfterValidator(_validate_unique_list), Field(json_schema_extra={'uniqueItems': True})]
+UniqueList = Annotated[list[T], AfterValidator(_validate_unique_list), Field(json_schema_extra={'uniqueItems': True})]
