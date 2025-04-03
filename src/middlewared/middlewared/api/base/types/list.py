a602d0b04541d2d2663f83a165ab2d273fe49f6a
Logan Cary <logan.cary@ixsystems.com>
2025-04-03 12:59:47 -0400

    replace nested generator

--- src/middlewared/middlewared/api/base/types/list.py
+++ src/middlewared/middlewared/api/base/types/list.py
@@ -11,8 +11,11 @@ T = TypeVar('T')
 
 def _validate_unique_list(v: list[T]) -> list[T]:
     """Raise `PydanticCustomError` if any item in `v` is equal to any other item in `v`."""
-    if any(v[i] == v[j] for i in range(len(v)) for j in range(i + 1, len(v))):
-        raise PydanticCustomError('unique_list', 'List must be unique')
+    # Use equals comparison instead of `set` to support unhashable types like `BaseModel`.
+    for i in range(len(v)):
+        for j in range(i + 1, len(v)):
+            if v[i] == v[j]:
+                raise PydanticCustomError('unique_list', 'List must be unique')
     return v
 
 
