4b92fd0ac49067af7ab917ff0ed6fa2fb9cbf9d7
Mark Grimes <mark.grimes@ixsystems.com>
2025-04-11 16:56:56 -0700

    Recast frozenset to set to allow for json serialization.
    (cherry picked from commit bae7c0493c9ed54dcd6059a098f5ac64e859c1a6)

--- src/middlewared/middlewared/plugins/security/update.py
+++ src/middlewared/middlewared/plugins/security/update.py
@@ -248,7 +248,7 @@ class SystemSecurityService(ConfigService):
             # SRG-OS-000071-GPOS-00039
             # Passwords must contain at least one lowercase character, one lowercase character, and
             # one number.
-            ruleset = combined['password_complexity_ruleset'] or GPOS_STIG_PASSWORD_COMPLEXITY
+            ruleset = combined['password_complexity_ruleset'] or set(GPOS_STIG_PASSWORD_COMPLEXITY)
             new['password_complexity_ruleset'] = ruleset
             if missing := GPOS_STIG_PASSWORD_COMPLEXITY - new['password_complexity_ruleset']:
                 raise ValidationError(
