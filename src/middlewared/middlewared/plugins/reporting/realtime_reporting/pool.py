330b280b6979a72252ad491360e5fc22a565543a
caleb <yocalebo@gmail.com>
2025-04-14 08:55:21 -0400

    dont report empty pool stats in reporting.realtime

--- src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
+++ src/middlewared/middlewared/plugins/reporting/realtime_reporting/pool.py
@@ -6,14 +6,19 @@ from .utils import safely_retrieve_dimension
 
 
 def get_pool_stats(netdata_metrics: dict) -> dict:
-    data = defaultdict(lambda: {'available': None, 'used': None, 'total': None})
+    data = defaultdict(dict)
     pool_data = query_imported_fast_impl()
-
-    for dimension_name, value in (safely_retrieve_dimension(netdata_metrics, 'truenas_pool.usage') or {}).items():
-        value = value or {}
-        pool_guid, stat_key = dimension_name.split('.')
-        if pool_guid not in pool_data:
-            continue
-
-        data[pool_data[pool_guid]['name']][stat_key] = value
+    for dimension_name, value in (
+        safely_retrieve_dimension(netdata_metrics, "truenas_pool.usage") or {}
+    ).items():
+        # this data eventually gets reported to our reporting.realtime
+        # subscription and so the UI team does not want us to send
+        # events with empty data. We only want to report on pools for
+        # which we're able to retrieve data from.
+        if value:
+            pool_guid, stat_key = dimension_name.split(".")
+            try:
+                data[pool_data[pool_guid]["name"]].update({stat_key: value})
+            except KeyError:
+                continue
     return data
