c37b31bf3f678bdd9ac110d0e80d8d6517035766
Logan Cary <logan.cary@ixsystems.com>
2025-04-14 14:31:45 -0400

    migration

---
+++ src/middlewared/middlewared/alembic/versions/25.10/2025-04-14_15-57_disallow_empty_passwords.py
@@ -0,0 +1,23 @@
+"""Disable password when empty string is used.
+
+Revision ID: 595b38b52541
+Revises: d7e3a916db65
+Create Date: 2025-04-14 15:57:08.141738+00:00
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '595b38b52541'
+down_revision = 'd7e3a916db65'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('UPDATE account_bsdusers SET bsdusr_password_disabled = TRUE WHERE bsdusr_unixhash = "*"')
+
+
+def downgrade():
+    pass
