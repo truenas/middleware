5d3dd6256b8f7b36197cea3e5353879e9cbdebbf
Logan Cary <logan.cary@ixsystems.com>
2025-04-16 16:15:38 -0400

    shorten cloud backup log excerpt

--- src/middlewared/middlewared/plugins/cloud_backup/restic.py
+++ src/middlewared/middlewared/plugins/cloud_backup/restic.py
@@ -2,6 +2,7 @@ import asyncio
 from dataclasses import dataclass
 from datetime import timedelta
 import json
+import pprint
 import subprocess
 
 from middlewared.job import JobProgressBuffer
@@ -126,6 +127,7 @@ async def restic_check_progress(job, proc, track_progress=False):
 
             case "summary":
                 await job.logs_fd_write((json.dumps(read) + "\n").encode("utf-8", "ignore"))
+                job.logs_excerpt = pprint.pformat(read)
                 continue
 
             case "error":
