6d8279ab52b309390742197c50c5111bed0988cc
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-04-28 19:13:52 -0400

    NAS-135605 / 25.04.1 / Robustize AlertMixin.clear_alert (by bmeagherix) (#16366)

--- src/middlewared/middlewared/test/integration/assets/alert.py
+++ src/middlewared/middlewared/test/integration/assets/alert.py
@@ -1,10 +1,19 @@
+from time import sleep
+
 from middlewared.test.integration.utils import call
 
 
 class AlertMixin:
-    def assert_alert_count(self, count):
-        alerts = [alert for alert in call('alert.list') if alert['klass'] == self.ALERT_CLASS_NAME]
-        assert len(alerts) == count, alerts
+    def alert_count(self):
+        return len([alert for alert in call('alert.list') if alert['klass'] == self.ALERT_CLASS_NAME])
+
+    def assert_alert_count(self, count, retries=5):
+        # Give a few seconds for the alerts to update
+        for i in range(retries):
+            if count == self.alert_count():
+                return
+            sleep(1)
+        assert self.alert_count() == count
 
     def clear_alert(self):
         call('alert.oneshot_delete', self.ALERT_CLASS_NAME)
