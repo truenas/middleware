310d69196a2f11187e14e16558868a8fd9caa114
Andrew Walker <awalker@ixsystems.com>
2025-04-27 18:37:16 -0600

    Fix

--- src/middlewared/middlewared/plugins/auth.py
+++ src/middlewared/middlewared/plugins/auth.py
@@ -468,7 +468,7 @@ class AuthService(Service):
         cred = TokenSessionManagerCredentials(self.token_manager, token, auth_ctx.pam_hdl, app.origin)
         pam_resp = cred.pam_authenticate()
         if pam_resp != pam.PAM_SUCCESS:
-            raise CallError(f'Failed to get token for action: {pam_resp.reason}')
+            raise CallError(f'Failed to get token for action: {pam_resp}')
 
         return cred
 
