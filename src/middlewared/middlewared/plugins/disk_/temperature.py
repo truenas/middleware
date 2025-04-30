truenas/middleware

commit 40cd2492f9b3fe580fbb2faf32fadc844609f9ba

NAS-135004 / 25.10 / Add plugin to retrieve disk temperatures (#16108)
## Context

We had removed SMART netdata plugin when we removed SMART. Now a new
plugin has been added to get disk temperature stats from the disk utils
we have.

src/middlewared/middlewared/plugins/reporting/netdata/graphs.py
@@ -1,7 +1,6 @@
 import typing
 
-from middlewared.utils.cpu import cpu_info
-from middlewared.utils.disk_temperatures import get_disks_for_temperature_reading
+from middlewared.utils.disks_.disk_class import iterate_disks
 
 from .graph_base import GraphBase
 from .utils import get_human_disk_name
@@ -365,17 +364,25 @@ class DiskTempPlugin(GraphBase):
         return 'Disk Temperature {identifier}'
 
     async def build_context(self):
-        self.disk_mapping = {}
         all_charts = await self.all_charts()
-        for disk in (await self.middleware.run_in_thread(get_disks_for_temperature_reading)).values():
-            identifier = disk.id if disk.id.startswith('nvme') else disk.serial
-            for k in (identifier, identifier.replace('-', '_')):
-                if f'smart_log_smart.disktemp.{k}' in all_charts:
-                    self.disk_mapping[get_human_disk_name(disk.__dict__)] = k
-                    break
+        self.disk_mapping = await self.middleware.run_in_thread(self.get_mapping, all_charts)
+
+    def get_mapping(self, all_charts):
+        return {
+            get_human_disk_name({
+                'identifier': disk.identifier,
+                'type': disk.media_type,
+                'name': disk.name,
+                'model': disk.model,
+            }): disk.identifier for disk in iterate_disks()
+            if f'truenas_disk_temp.{disk.identifier}' in all_charts
+        }
 
     async def get_identifiers(self) -> typing.Optional[list]:
         return list(self.disk_mapping.keys())
+
+    def get_chart_name(self, identifier: typing.Optional[str] = None) -> str:
+        return f'truenas_disk_temp.{self.disk_mapping[identifier]}'
 
     def normalize_metrics(self, metrics) -> dict:
         metrics = super().normalize_metrics(metrics)
@@ -387,9 +394,6 @@ class DiskTempPlugin(GraphBase):
             metrics['legend'][1] = 'temperature_value'
 
         return metrics
-
-    def get_chart_name(self, identifier: typing.Optional[str] = None) -> str:
-        return f'smart_log_smart.disktemp.{self.disk_mapping[identifier]}'
 
 
 class UPSBase(GraphBase):

src/middlewared/middlewared/plugins/reporting/utils.py
@@ -120,9 +120,6 @@ def get_metrics_approximation(
             # cpu usage, it is core count + 1 with +1 saving aggregated stats
             'cpu.usage': core_count + 1,
 
-            # pool usage
-            'truenas_pool.usage': pool_count * 3,
-
             # cputemp
             'cputemp.temperatures': core_count + 1,
 
@@ -193,8 +190,12 @@ def get_metrics_approximation(
             'cgroup_hash.io_full_pressure': 3 * containers_count,
             'cgroup_hash.io_full_pressure_stall_time': containers_count,
         },
-        60: {  # smartd_logs
-            'smart_log.temperature_celsius': disk_count}
+        300: {
+            # disk temp logs
+            'truenas_disk_temp.temp': disk_count,
+            # pool usage
+            'truenas_pool.usage': pool_count * 3,
+        }
     }
     return {
         sec: sum(d.values()) for sec, d in data.items()

src/freenas/usr/lib/netdata/conf.d/python.d.conf
@@ -4,5 +4,6 @@ cputemp: yes
 truenas_arcstats: yes
 truenas_cpu_usage: yes
 truenas_disk_stats: yes
+truenas_disk_temp: yes
 truenas_meminfo: yes
 truenas_pool: yes

src/freenas/usr/lib/netdata/python.d/truenas_disk_temp.chart.py
@@ -1,0 +1,43 @@
+from bases.FrameworkServices.SimpleService import SimpleService
+
+from middlewared.utils.disks_.disk_class import iterate_disks
+
+
+class Service(SimpleService):
+    def __init__(self, configuration=None, name=None):
+        SimpleService.__init__(self, configuration=configuration, name=name)
+        self.update_every = 300
+        self.disks = set()
+
+    def check(self):
+        self.add_disks_to_charts([d.identifier for d in iterate_disks()])
+        return True
+
+    def get_data(self):
+        disks_temps = {}
+        new_disks = []
+        for disk in iterate_disks():
+            # We do this to handle the case when a disk was replaced with another one or a new one was added
+            # Essentially what we want to ensure is that any new disk which has been added exists in the netdata
+            # database correctly
+            if disk.identifier not in self.disks:
+                new_disks.append(disk.identifier)
+
+            disks_temps[f'{disk.identifier}.temp'] = disk.temp().temp_c or 0
+
+        self.add_disks_to_charts(new_disks)
+        return disks_temps
+
+    def add_disks_to_charts(self, disk_ids):
+        for disk_id in disk_ids:
+            if disk_id in self.charts:
+                continue
+
+            self.charts.add_chart([
+                disk_id, disk_id, disk_id, 'Celsius',
+                'disk.temp',
+                f'Temperature for {disk_id} disk',
+                'line',
+            ])
+            self.charts[disk_id].add_dimension([f'{disk_id}.temp', 'temp', 'absolute'])
+            self.disks.add(disk_id)

src/middlewared/middlewared/pytest/unit/plugins/reporting/test_netdata_approximation.py
@@ -4,9 +4,9 @@ from middlewared.plugins.reporting.utils import get_metrics_approximation, calcu
 
 
 @pytest.mark.parametrize('disk_count,core_count,interface_count,pool_count,services_count,vms_count,expected_output', [
-    (4, 2, 1, 2, 10, 2, {1: 705, 60: 4}),
-    (1600, 32, 4, 4, 10, 1, {1: 8766, 60: 1600}),
-    (10, 16, 2, 2, 12, 3, {1: 844, 60: 10}),
+    (4, 2, 1, 2, 10, 2, {1: 699, 300: 10}),
+    (1600, 32, 4, 4, 10, 1, {1: 8754, 300: 1612}),
+    (10, 16, 2, 2, 12, 3, {1: 838, 300: 16}),
 ])
 def test_netdata_metrics_count_approximation(
     disk_count, core_count, interface_count, pool_count, services_count, vms_count, expected_output
@@ -19,14 +19,14 @@ def test_netdata_metrics_count_approximation(
 @pytest.mark.parametrize(
     'disk_count,core_count,interface_count,pool_count,services_count,vms_count,days,'
     'bytes_per_point,tier_interval,expected_output', [
-        (4, 2, 1, 2, 10, 2, 7, 1, 1, 406),
+        (4, 2, 1, 2, 10, 2, 7, 1, 1, 403),
         (4, 2, 1, 2, 10, 1, 7, 4, 60, 25),
-        (1600, 32, 4, 12, 2, 4, 4, 1, 1, 2937),
-        (1600, 32, 4, 10, 1, 4, 4, 4, 900, 13),
-        (10, 16, 2, 2, 12, 1, 3, 1, 1, 184),
+        (1600, 32, 4, 12, 2, 4, 4, 1, 1, 2918),
+        (1600, 32, 4, 10, 1, 4, 4, 4, 900, 12),
+        (10, 16, 2, 2, 12, 1, 3, 1, 1, 183),
         (10, 16, 2, 2, 10, 3, 3, 4, 60, 13),
-        (1600, 32, 4, 4, 12, 3, 18, 1, 1, 13201),
-        (1600, 32, 4, 4, 12, 1, 18, 4, 900, 58),
+        (1600, 32, 4, 4, 12, 3, 18, 1, 1, 13151),
+        (1600, 32, 4, 4, 12, 1, 18, 4, 900, 57),
     ],
 )
 def test_netdata_disk_space_approximation(
@@ -58,5 +58,5 @@ def test_netdata_days_approximation(
         disk_count, core_count, interface_count, pool_count, vms_count, services_count
     )
     disk_size = calculate_disk_space_for_netdata(metric_intervals, days, bytes_per_point, tier_interval)
-    total_metrics = metric_intervals[1] + (metric_intervals[60] / 60)
+    total_metrics = metric_intervals[1] + (metric_intervals[300] / 300)
     assert round((disk_size * 1024 * 1024) / (bytes_per_point * total_metrics * (86400 / tier_interval))) == days

src/middlewared/middlewared/utils/disks_/disk_class.py
@@ -133,7 +133,9 @@ class DiskEntry:
             # pmem devices have a uuid attribute that we use as serial
             serial = self.__opener(relative_path="uuid")
 
-        return serial
+        # strip is required because we see these cases otherwise
+        # >>> d.serial reported as '        3FJ1U1HT'
+        return serial.strip() if serial else None
 
     @cached_property
     def lunid(self) -> str | None:
@@ -150,7 +152,11 @@ class DiskEntry:
         if wwid is not None:
             wwid = wwid.removeprefix("naa.").removeprefix("0x").removeprefix("eui.")
 
-        return wwid
+        # Doing a replace here because we have seen cases where this value
+        # gets reported -> 't10.ATA     QEMU HARDDISK                           QM00003'
+        # It is like this in the file itself, it could just perhaps be isolated only to VMs
+        # but there is no harm to remove empty spaces
+        return wwid.replace(" ", "") if wwid else wwid
 
     @cached_property
     def model(self) -> str | None:
@@ -167,6 +173,11 @@ class DiskEntry:
         if fr is None:
             fr = self.__opener(relative_path="device/firmware_rev")
         return fr
+
+    @cached_property
+    def media_type(self) -> str:
+        fr = self.__opener(relative_path="queue/rotational")
+        return "HDD" if fr == "1" else "SSD"
 
     @cached_property
     def identifier(self) -> str:

commit ff8c816543c4d85d7169e57b0f08ff5d1a83e8a4

NAS-135572 / 25.10 / fix disk.temperature_agg (by yocalebo) (#16386)
This was subtly broken in 40cd2492f9b3fe580fbb2faf32fadc844609f9ba. The
title that we display in the historical reports was formulated in such a
way that broke `disk.temperature_agg` which broke a widget on dashboard.
This fixes all the above issues by doing a more human readable title but
by adding the disk name (sda, nvme0n1) as a component to the title.

Original PR: https://github.com/truenas/middleware/pull/16385
Jira URL: https://ixsystems.atlassian.net/browse/NAS-135572

Co-authored-by: Caleb <yocalebo@gmail.com>

src/middlewared/middlewared/plugins/disk_/temperature.py
@@ -67,8 +67,11 @@ class DiskService(Service):
         opts = {'start': round(start.timestamp()), 'end': round(end.timestamp())}
         final = dict()
         for disk in self.middleware.call_sync('reporting.netdata_graph', 'disktemp', opts):
-            if disk['identifier'] in names:
-                final[disk['identifier']] = {
+            # identifier looks like "sda | Type: HDD | Model: HUH721212AL4200 | Serial: aaa"
+            # so we need to normalize it before checking if caller has specified it
+            name = disk['identifier'].split(' | ')[0].strip()
+            if name in names:
+                final[name] = {
                     'min': disk['aggregations']['min'].get('temperature_value', None),
                     'max': disk['aggregations']['max'].get('temperature_value', None),
                     'avg': disk['aggregations']['mean'].get('temperature_value', None),

src/middlewared/middlewared/plugins/reporting/netdata/graphs.py
@@ -55,12 +55,7 @@ class DISKPlugin(GraphBase):
 
     def get_mapping(self, all_charts):
         return {
-            get_human_disk_name({
-                'identifier': disk.identifier,
-                'type': disk.media_type,
-                'name': disk.name,
-                'model': disk.model,
-            }): disk.identifier for disk in iterate_disks()
+            get_human_disk_name(disk): disk.identifier for disk in iterate_disks()
             if f'truenas_disk_stats.io.{disk.identifier}' in all_charts
         }
 
@@ -376,12 +371,7 @@ class DiskTempPlugin(GraphBase):
 
     def get_mapping(self, all_charts):
         return {
-            get_human_disk_name({
-                'identifier': disk.identifier,
-                'type': disk.media_type,
-                'name': disk.name,
-                'model': disk.model,
-            }): disk.identifier for disk in iterate_disks()
+            get_human_disk_name(disk): disk.identifier for disk in iterate_disks()
             if f'truenas_disk_temp.{disk.identifier}' in all_charts
         }
 

src/middlewared/middlewared/plugins/reporting/netdata/utils.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode
 
+from middlewared.utils.disks_.disk_class import DiskEntry
 
 NETDATA_PORT = 6999
 NETDATA_REQUEST_TIMEOUT = 30  # seconds
@@ -22,24 +23,19 @@ def get_query_parameters(query_params: dict | None, prefix: str = '&') -> str:
     return f'{prefix}{encoded_params}'
 
 
-def get_human_disk_name(disk_details: dict) -> str:
-    """
-    This will return a human-readable name for the disk which is guaranteed to be unique
-    """
-    identifier = disk_details['identifier']
-    disk_type = disk_details['type']
-    if disk_type == 'SSD' and disk_details['name'].startswith('nvme'):
-        disk_type = 'NVME'
-
-    model = disk_details['model']
-
-    human_identifier = ''
-    if disk_type:
-        human_identifier = f'{disk_type} | '
-
-    if model:
-        human_identifier += f'{model} Model | '
-
-    human_identifier += f'{identifier}'
-
-    return human_identifier
+def get_human_disk_name(disk: DiskEntry) -> str:
+    """This will return a human-readable name which
+    is used, primarily, in the title of the netdata
+    for disk related reports."""
+    # follows the form of <name> | <type> | <model> | <serial>
+    # the _ONLY_ guaranteed value is the <name>
+    # so we'll return "<attr>: Unknown" for any
+    # attributes of the disk that we can't determine
+    return ' | '.join(
+        [
+            disk.name,
+            f'Type: {disk.media_type if disk.media_type else "Unknown"}',
+            f'Model: {disk.model if disk.model else "Unknown"}',
+            f'Serial: {disk.serial if disk.serial else "Unknown"}',
+        ]
+    )