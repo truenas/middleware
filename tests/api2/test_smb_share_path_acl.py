06c8fb8eabf5d32c8fcc62b7b14866ca9d86f6d1
Andrew Walker <awalker@ixsystems.com>
2025-05-09 07:06:29 -0600

    Fix validation for SMB share paths that are dirs
    This commit fixes over-strict validation of ZFS properties for
    child datasets of SMB shares. If the dataset mountpoint isn't a
    child path of the share path then we don't need to check its
    properties.

---
+++ tests/api2/test_smb_share_path_acl.py
@@ -0,0 +1,48 @@
+import os
+import pytest
+
+from middlewared.test.integration.assets.filesystem import directory
+from middlewared.test.integration.assets.smb import smb_share
+from middlewared.test.integration.assets.pool import dataset
+from middlewared.test.integration.utils import call
+
+
+@pytest.fixture(scope='module')
+def fs_tree():
+    with dataset('nfs4ds', data={'share_type': 'SMB'}) as ds1:
+        with dataset(f'{ds1}/posix') as ds2:
+            # Make sure that we don't end up with collision on dataset name partial match
+            with dataset(f'{ds1}/posixcanary'):
+                mountpoint1 = os.path.join('/mnt', ds1)
+                mountpoint2 = os.path.join('/mnt', ds2)
+                path = os.path.join(mountpoint1, 'subdir')
+                with directory(path):
+                    yield {
+                        'mountpoint1': mountpoint1,
+                        'ds1': ds1,
+                        'mountpoint2': mountpoint2,
+                        'ds2': ds2,
+                        'subdir': path,
+                    }
+
+
+def test__verror_share_create_acltype(fs_tree):
+    with pytest.raises(Exception, match='ACL type mismatch with child mountpoint'):
+        with smb_share(fs_tree['mountpoint1'], SMB_NAME):
+            pass
+
+
+def test__share_create_subdir_allowed(fs_tree):
+    with smb_share(fs_tree['subdir'], SMB_NAME):
+        pass
+
+
+def test__share_create_child_allowed(fs_tree):
+    with smb_share(fs_tree['mountpoint2'], SMB_NAME):
+        pass
+
+
+def test__verror_share_update_acltype(fs_tree):
+    with smb_share(fs_tree['subdir'], SMB_NAME) as share:
+        with pytest.raises(Exception, match='ACL type mismatch with child mountpoint'):
+            call('sharing.smb.update', share['id'], {'path': fs_tree['mountpoint1']})
