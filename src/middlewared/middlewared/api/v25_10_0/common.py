e95d0973845878414271b668b43e927a5d8841af
Andrew Walker <awalker@ixsystems.com>
2025-05-13 06:46:11 -0600

    NAS-135846 / 25.10 / Improve query-options documentation (#16489)
    The query options should be properly documentated in API query methods.

--- src/middlewared/middlewared/api/v25_10_0/common.py
+++ src/middlewared/middlewared/api/v25_10_0/common.py
@@ -5,7 +5,7 @@ from middlewared.api.base import BaseModel, TimeString
 from middlewared.utils import filters
 from middlewared.utils.cron import croniter_for_schedule
 
-from pydantic import AfterValidator, model_validator
+from pydantic import AfterValidator, model_validator, Field
 
 __all__ = ["QueryFilters", "QueryOptions", "QueryArgs", "GenericQueryResult", "CronModel", "TimeCronModel"]
 
@@ -17,21 +17,47 @@ def validate_query_filters(qf: list) -> list:
     return qf
 
 
-QueryFilters = Annotated[list, AfterValidator(validate_query_filters)]
+QF_DOC = 'List of filters for query results. See API documentation for "Query Methods" for more guidance.'
+QF_FIELD = Field(default=[], description=QF_DOC, examples=[
+    [["name", "=", "bob"]],
+    [["OR", [[["name", "=", "bob"]], [["name", "=", "larry"]]]]],
+])
+QueryFilters = Annotated[list, QF_FIELD, AfterValidator(validate_query_filters)]
 
 
 class QueryOptions(BaseModel):
+    """ Query options customize the results returned by a query method. More complete documentation with examples
+    are covered in the "Query methods" section of the TrueNAS API documentation. """
     relationships: bool = True
     extend: str | None = None
     extend_context: str | None = None
     prefix: str | None = None
     extra: dict = {}
-    order_by: list[str] = []
-    select: list[str | list] = []
+    """ Extra options are defined on a per-endpoint basis and are described in the documentation for the associated
+    query method. """
+    order_by: list[str] = Field(default=[], examples=[['size', '-devname', 'nulls_first:-expiretime']])
+    """ An array of field names describing the manner in which query results should be ordered. The field names may
+    also have one of more of the following special prefixes: `-` (reverse sort direction), `nulls_first:` (place
+    any null values at the head of the results list), `nulls_last:` (place any null values at the tail of the
+    results list). """
+    select: list[str | list] = Field(default=[], examples=[['username', 'Authentication.status']])
+    """ An array of field names specifying the exact fields to include in the query return. The dot character `.`
+    may be used to explicitly select only subkeys of the query result. """
     count: bool = False
+    """ Return a numeric value representing the number of items that match the specified `query-filters`. """
     get: bool = False
+    """ Return the JSON object of the first result matching the specified `query-filters`. The query fails
+    if there specified `query-filters` return no results. """
     offset: int = 0
+    """ This specifies the beginning offset of the results array. When combined with the `limit` query-option
+    it may be used to implement pagination of large results arrays. WARNING: some query methods provide
+    volatile results and the onus is on the developer to understand whether pagination is appropriate
+    for a particular query API method. """
     limit: int = 0
+    """ This specifies the maximum number of results matching the specified `query-filters` to return. When
+    combined wtih the `offset` query-option it may be used to implement pagination of large results arrays.
+    WARNING: some query methods provide volatile results and the onus is on the developer to understand whether
+    pagination is appropriate for a particular query API method. """
     force_sql_filters: bool = False
 
     @model_validator(mode='after')
