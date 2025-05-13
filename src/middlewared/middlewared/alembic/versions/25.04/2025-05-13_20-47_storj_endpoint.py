22703004f2c42add43de054eccc6c11f27bb0364
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-05-13 12:22:32 -0400

    NAS-133835 / 25.04.2 / Allow setting Storj endpoint for global and select tiers (by creatorcary) (#16491)
    Existing and future Storj credentials will default to using
    https://gateway.storjshare.io/.
    
    UI follow-up ticket will be needed.
    
    Original PR: https://github.com/truenas/middleware/pull/16479
    Jira URL: https://ixsystems.atlassian.net/browse/NAS-133835
    
    ---------
    
    Co-authored-by: Logan Cary <logan.cary@ixsystems.com>

---
+++ src/middlewared/middlewared/alembic/versions/25.04/2025-05-13_20-47_storj_endpoint.py
@@ -0,0 +1,30 @@
+"""Add `endpoint` field to system_cloudcredentials.attributes for type STORJ_IX.
+
+Revision ID: 30c9619bf9e7
+Revises: 7c663f5ec8a1
+Create Date: 2025-05-07 20:47:46.751451+00:00
+
+"""
+import json
+
+from alembic import op
+
+from middlewared.plugins.pwenc import encrypt, decrypt
+
+
+# revision identifiers, used by Alembic.
+revision = '30c9619bf9e7'
+down_revision = '7c663f5ec8a1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    for entry in map(dict, conn.execute("SELECT id, attributes FROM system_cloudcredentials WHERE provider = 'STORJ_IX'").fetchall()):
+        if attributes := decrypt(entry["attributes"]):
+            attributes = json.loads(attributes)
+            attributes.setdefault("endpoint", "https://gateway.storjshare.io/")
+            conn.execute("UPDATE system_cloudcredentials SET attributes = ? WHERE id = ?", (
+                encrypt(json.dumps(attributes)), entry["id"]
+            ))
