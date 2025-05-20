d5fd32f774e62bccbc4b986bc304e824a25728a4
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-05-20 07:12:59 -0400

    NAS-135886 / 25.04.1 / Fix fragile behavior of config method (by sonicaj) (#16519)
    This PR adds changes to fix fragile behaviour of config method where
    what happens is that we try to see if a row exists in the config service
    table by selecting first index which if it does not exist will raise
    `IndexError` and in that case we used to insert a row in the database.
    
    The problem with this logic is that if any service has an extend in
    place which also raises `IndexError` and is not caught will result in
    duplicate entry being created in the config service table as the code
    assumed `IndexError` was being originated because it had tried to select
    the first row whereas that was not the case.
    
    Now we instead call `query` directly and get the first row like
    `get=True` was doing already in `datastore.config` method. This is safe
    in terms of that if the underlying service is doing anything else, that
    error gets raised as is and we don't make false assumptions that this
    exception is coming because we don't have a row available and let's
    insert now.
    
    Original PR: https://github.com/truenas/middleware/pull/16504
    Jira URL: https://ixsystems.atlassian.net/browse/NAS-135886
    
    ---------
    
    Co-authored-by: Waqar Ahmed <waqarahmedjoyia@live.com>

---
+++ src/middlewared/middlewared/pytest/unit/test_get_or_insert.py
@@ -0,0 +1,72 @@
+import json
+import unittest.mock
+
+import pytest
+import sqlalchemy as sa
+
+from middlewared.pytest.unit.middleware import Middleware
+from middlewared.pytest.unit.plugins.test_datastore import datastore_test, Model
+from middlewared.service.config_service import ConfigService
+
+
+class CatalogModel(Model):
+    __tablename__ = 'services_catalog'
+
+    label = sa.Column(sa.String(255), nullable=False, unique=True, primary_key=True)
+    preferred_trains = sa.Column(sa.JSON(list))
+
+
+@pytest.mark.parametrize('rows, should_work', [
+    (
+        [{'label': 'TRUENAS', 'preferred_trains': ['community', 'stable']}],
+        True,
+    ),
+    (
+        [],
+        True
+    ),
+    (
+        [],
+        False
+    )
+])
+@pytest.mark.asyncio
+async def test_get_or_insert(rows, should_work):
+    async with datastore_test() as ds:
+        middleware = Middleware()
+        middleware['datastore.query'] = ds.query
+        middleware['datastore.insert'] = ds.insert
+        middleware['datastore.config'] = ds.config
+        config_service_obj = ConfigService(middleware)
+
+        if should_work:
+            if rows:
+                # Testing case where the row exists already
+                ds.execute(
+                    'INSERT INTO `services_catalog` VALUES (?, ?)',
+                    (rows[0]['label'], json.dumps(rows[0]['preferred_trains']))
+                )
+                response = await config_service_obj._get_or_insert('services.catalog', {})
+                assert response == rows[0]
+            else:
+                # Testing case where the row does not exist and is added by config service
+                # get_or_insert
+                response = await config_service_obj._get_or_insert('services.catalog', {})
+                assert response == {'label': '', 'preferred_trains': None}
+        else:
+            # Testing case where the row does exist but the query method raises some exception
+            # in this case we say IndexError - so we make sure that we don't have datastore.insert
+            # called in this case
+            query_mock = unittest.mock.Mock(side_effect=IndexError)
+            middleware['datastore.query'] = query_mock
+
+            insert_mock = unittest.mock.Mock()
+            config_mock = unittest.mock.Mock()
+            middleware['datastore.insert'] = insert_mock
+            middleware['datastore.config'] = config_mock
+
+            with pytest.raises(IndexError):
+                await config_service_obj._get_or_insert('service.catalog', {})
+
+            insert_mock.assert_not_called()
+            config_mock.assert_not_called()
