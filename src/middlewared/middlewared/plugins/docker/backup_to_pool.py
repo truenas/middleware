022fff92dd3d33c33d6bb91c5c6f8822fde0b9b1
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-05-23 00:07:30 +0500

    Add logic to replicate apps pool to target pool

--- src/middlewared/middlewared/plugins/docker/backup_to_pool.py
+++ src/middlewared/middlewared/plugins/docker/backup_to_pool.py
@@ -2,7 +2,9 @@ import errno
 
 from middlewared.api import api_method
 from middlewared.api.current import DockerBackupToPoolArgs, DockerBackupToPoolResult
-from middlewared.service import CallError, job, Service
+from middlewared.service import CallError, job, private, Service
+
+from .utils import applications_ds_name
 
 
 class DockerService(Service):
@@ -17,19 +19,56 @@ class DockerService(Service):
         roles=['DOCKER_WRITE']
     )
     @job(lock='docker_backup_to_pool')
-    def backup_to_pool(self, job, target_pool):
+    async def backup_to_pool(self, job, target_pool):
         """
         Create a backup of existing apps on `target_pool`.
 
         This creates a backup of existing apps on the `target_pool` specified. If this is executed multiple times,
         in the next iteration it will incrementally backup the apps that have changed since the last backup.
         """
-        docker_config = self.middleware.call_sync('docker.config')
+        docker_config = await self.middleware.call('docker.config')
         if docker_config['pool'] is None:
             raise CallError('Docker is not configured', errno=errno.EINVAL)
 
         # TODO: See how locking plays a role here
-        if not self.middleware.call_sync('pool.query', [['name', '=', target_pool]]):
+        if not await self.middleware.call('pool.query', [['name', '=', target_pool]]):
             raise CallError(f'{target_pool!r} pool does not exist', errno=errno.ENOENT)
 
         job.set_progress(10, 'Initial validation has been completed')
+        await self.middleware.call('service.stop', 'docker')
+        job.set_progress(20, 'Docker service has been stopped')
+
+        try:
+            await self.incrementally_replicate_apps_dataset(docker_config['pool'], target_pool)
+        finally:
+            self.middleware.call_sync('service.start', 'docker')
+
+    @private
+    async def incrementally_replicate_apps_dataset(self, source_pool, target_pool):
+        schema = f'ix-apps-{source_pool}-backup-%Y-%m-%d_%H-%M'
+        await self.middleware.call(
+            'zfs.snapshot.create', {
+                'dataset': applications_ds_name(source_pool),
+                'naming_schema': schema,
+                'recursive': True,
+            }
+        )
+        old_ds = applications_ds_name(source_pool)
+        new_ds = applications_ds_name(target_pool)
+        replication_job = await self.middleware.call(
+            'replication.run_onetime', {
+                'direction': 'PUSH',
+                'transport': 'LOCAL',
+                'source_datasets': [old_ds],
+                'target_dataset': new_ds,
+                'recursive': True,
+                'also_include_naming_schema': [schema],
+                'retention_policy': 'SOURCE',
+                'replicate': True,
+                'readonly': 'IGNORE',
+                'exclude_mountpoint_property': False,
+            }
+        )
+        await replication_job.wait()
+        if replication_job.error:
+            raise CallError(f'Failed to replicate {old_ds} to {new_ds}: {replication_job.error}')
