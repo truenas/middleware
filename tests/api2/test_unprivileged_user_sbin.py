f09c6c64e459406d563593859a8b87af22967301
themylogin <themylogin@gmail.com>
2025-05-23 08:47:57 +0200

    Fix bashism in /etc/profile/path.sh

--- tests/api2/test_unprivileged_user_sbin.py
+++ tests/api2/test_unprivileged_user_sbin.py
@@ -1,7 +1,7 @@
 import pytest
 
 from middlewared.test.integration.assets.account import user
-from middlewared.test.integration.utils import call, ssh
+from middlewared.test.integration.utils import ssh
 
 
 @pytest.fixture(scope='module')
@@ -12,10 +12,11 @@ def unprivileged_user():
         'password': 'password',
         'group_create': True,
         'ssh_password_enabled': True,
+        'shell': '/usr/bin/sh',
     }) as u:
         yield u
 
 
-@pytest.mark.parametrize('shell', ['/usr/bin/bash', '/usr/bin/zsh'])
+@pytest.mark.parametrize('shell', ['/usr/bin/bash', '/usr/bin/dash', '/usr/bin/sh', '/usr/bin/zsh'])
 def test_unprivileged_user_has_access_to_sbin_zfs(unprivileged_user, shell):
-    ssh(f'{shell} -c "zpool status"', user='unprivileged', password='password')
+    ssh(f'{shell} -l -c "zpool status"', user='unprivileged', password='password')
