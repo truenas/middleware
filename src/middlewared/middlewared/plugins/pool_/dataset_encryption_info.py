truenas/middleware

commit 5a34dd57a4338c604c4058c2fa8cc800fd7cec4b

NAS-135991 / 25.10 / optimize pool.dataset.path_in_locked_datasets (#16524)
This highly optimizes the call to
`pool.dataset.path_in_locked_datasets`. This is done by utilizing the
newly minted `truenas_pylibzfs` module.

This is taking a completely different approach for how we determine if a
path is in a locked resource. Instead of enumerating all encrypted zfs
filesystems/volumes on the system and then iterating over that list and
see if the path is a child of any of the datasets, we will simply open
each component in the `path` to see if it's encrypted and locked.

Because the new `truenas_pylibzfs` module is thread-safe, we're able to
do this in our thread-pool. This is a substantive improvement in
efficiency and speed compared to what we were doing previously.

---------

Co-authored-by: Logan Cary <logan.cary@ixsystems.com>

src/middlewared/middlewared/plugins/pool_/dataset_encryption_info.py
@@ -14,8 +14,16 @@ from middlewared.api.current import (
     PoolDatasetExportKeyArgs, PoolDatasetExportKeyResult
 )
 from middlewared.service import CallError, job, periodic, private, Service, ValidationErrors
+from middlewared.service.decorators import pass_thread_local_storage
 from middlewared.utils import filter_list
-from middlewared.utils.path import is_child_realpath
+from middlewared.plugins.pool_.utils import get_dataset_parents
+try:
+    from truenas_pylibzfs import ZFSError, ZFSException
+except ImportError:
+    # github CI crashes without this because the
+    # image on github doesn't have ZFS installed
+    # so it's safe to ignore it
+    ZFSError = ZFSException = None
 
 from .utils import DATASET_DATABASE_MODEL_NAME, dataset_can_be_mounted, retrieve_keys_from_file, ZFSKeyFormat
 
@@ -24,17 +32,6 @@ class PoolDatasetService(Service):
 
     class Config:
         namespace = 'pool.dataset'
-
-    @private
-    async def locked_datasets_cached(self):
-        try:
-            return await self.middleware.call('cache.get', 'zfs_locked_datasets')
-        except KeyError:
-            locked_datasets = await self.middleware.call('zfs.dataset.locked_datasets')
-            if await self.middleware.call('system.ready'):
-                # Only cache if the system is ready
-                await self.middleware.call('cache.put', 'zfs_locked_datasets', locked_datasets, 20)
-            return locked_datasets
 
     @api_method(PoolDatasetEncryptionSummaryArgs, PoolDatasetEncryptionSummaryResult, roles=['DATASET_READ'])
     @job(lock=lambda args: f'encryption_summary_options_{args[0]}', pipes=['input'], check_pipes=False)
@@ -210,10 +207,36 @@ class PoolDatasetService(Service):
         self.middleware.call_sync('pool.dataset.delete_encrypted_datasets_from_db', [['name', 'in', to_remove]])
 
     @private
-    def path_in_locked_datasets(self, path, locked_datasets=None):
-        if locked_datasets is None:
-            locked_datasets = self.middleware.call_sync('zfs.dataset.locked_datasets')
-        return any(is_child_realpath(path, d['mountpoint']) for d in locked_datasets if d['mountpoint'])
+    @pass_thread_local_storage
+    def path_in_locked_datasets(self, tls, path):
+        """
+        This method checks whether any parent components of
+        a given path are locked. It returns True if a locked
+        component is found, otherwise False.
+
+        Parameters:
+            path (str): The filesystem path to be checked.
+
+        Returns:
+            bool: True if any parent component of the path
+                is locked, False otherwise.
+
+        Raises:
+            ZFSException/Exception: If an unexpected error occurs
+        """
+        # WARNING: _EXTREMELY_ hot code path. Do not add more
+        # things here unless you fully understand the side-effects.
+        for i in get_dataset_parents(path.removeprefix('/mnt/')):
+            try:
+                crypto = tls.lzh.open_resource(name=i).crypto()
+                if crypto and not crypto.info().key_is_loaded:
+                    return True
+            except ZFSException as e:
+                if ZFSError(e.code) == ZFSError.EZFS_NOENT:
+                    continue
+                else:
+                    raise
+        return False
 
     @private
     def query_encrypted_roots_keys(self, filters):

src/middlewared/middlewared/etc_files/scst.conf.mako
@@ -45,7 +45,7 @@
         return cml
 
     targets = render_ctx['iscsi.target.query']
-    extents = {d['id']: d for d in middleware.call_sync('iscsi.extent.query', [['enabled', '=', True]], {'extra': {'use_cached_locked_datasets': False}})}
+    extents = {d['id']: d for d in middleware.call_sync('iscsi.extent.query', [['enabled', '=', True]])}
     portals = {d['id']: d for d in middleware.call_sync('iscsi.portal.query')}
     initiators = {d['id']: d for d in middleware.call_sync('iscsi.initiator.query')}
     fcports_by_target_id = {d['target']['id']: d for d in render_ctx['fcport.query']}

src/middlewared/middlewared/plugins/docker/state_setup.py
@@ -36,19 +36,12 @@ class DockerSetupService(Service):
 
         await self.create_update_docker_datasets(config['dataset'])
 
-        locked_datasets = [
-            d['id'] for d in filter(
-                lambda d: d['mountpoint'], await self.middleware.call('zfs.dataset.locked_datasets')
-            )
-            if d['mountpoint'].startswith(f'{config["dataset"]}/') or d['mountpoint'] in (
-                f'/mnt/{k}' for k in (config['dataset'], config['pool'])
-            )
-        ]
-        if locked_datasets:
-            raise CallError(
-                f'Please unlock following dataset(s) before starting docker: {", ".join(locked_datasets)}',
-                errno=CallError.EDATASETISLOCKED,
-            )
+        for i in (config['dataset'], config['pool']):
+            if await self.middleware.call('pool.dataset.path_in_locked_datasets', i):
+                raise CallError(
+                    f'Cannot start docker because {i!r} is located in a locked dataset.',
+                    errno=CallError.EDATASETISLOCKED,
+                )
 
         # What we want to validate now is that the interface on default route is up and running
         # This is problematic for bridge interfaces which can or cannot come up in time

src/middlewared/middlewared/plugins/etc.py
@@ -167,10 +167,7 @@ class EtcService(Service):
             'ctx': [
                 {
                     'method': 'sharing.nfs.query',
-                    'args': [
-                        [('enabled', '=', True), ('locked', '=', False)],
-                        {'extra': {'use_cached_locked_datasets': False}}
-                    ],
+                    'args': [[('enabled', '=', True), ('locked', '=', False)]],
                 },
                 {'method': 'nfs.config'},
             ],

src/middlewared/middlewared/plugins/iscsi_/extents.py
@@ -66,20 +66,12 @@ class iSCSITargetExtentService(SharingService):
         entry = IscsiExtentEntry
 
     @private
-    async def sharing_task_determine_locked(self, data, locked_datasets):
-        """
-        `mountpoint` attribute of zvol will be unpopulated and so we
-        first try direct comparison between the two strings.
-
-        The parent dataset of a zvol may also be locked, which renders
-        the zvol inaccessible as well, and so we need to continue to the
-        common check for whether the path is in the locked datasets.
-        """
-        path = await self.get_path_field(data)
-        if data['type'] == 'DISK' and any(path == os.path.join('/mnt', d['id']) for d in locked_datasets):
-            return True
-
-        return await self.middleware.call('pool.dataset.path_in_locked_datasets', path, locked_datasets)
+    async def sharing_task_determine_locked(self, data):
+        """Determine if this extent is in a locked path"""
+        return await self.middleware.call(
+            'pool.dataset.path_in_locked_datasets',
+            await self.get_path_field(data)
+        )
 
     @api_method(
         IscsiExtentCreateArgs,

src/middlewared/middlewared/plugins/kmip/zfs_keys.py
@@ -135,7 +135,6 @@ class KMIPService(Service, KMIPServerMixin):
 
     @private
     def initialize_zfs_keys(self, connection_success):
-        locked_datasets = [ds['id'] for ds in self.middleware.call_sync('zfs.dataset.locked_datasets')]
         for ds in self.middleware.call_sync('datastore.query', 'storage.encrypteddataset',):
             if ds['encryption_key']:
                 self.zfs_keys[ds['name']] = ds['encryption_key']
@@ -147,8 +146,9 @@ class KMIPService(Service, KMIPServerMixin):
                     self.middleware.logger.debug(f'Failed to retrieve key for {ds["name"]}')
                 else:
                     self.zfs_keys[ds['name']] = key
-            if ds['name'] in self.zfs_keys and ds['name'] in locked_datasets:
-                self.middleware.call_sync('pool.dataset.unlock', ds['name'])
+            if ds['name'] in self.zfs_keys:
+                if self.middleware.call_sync('pool.dataset.path_in_locked_datasets', ds['name']):
+                    self.middleware.call_sync('pool.dataset.unlock', ds['name'])
 
     @private
     async def retrieve_zfs_keys(self):

src/middlewared/middlewared/plugins/nvmet/namespace.py
@@ -355,17 +355,9 @@ class NVMetNamespaceService(SharingService):
                 await self.middleware.run_in_thread(kernel_resize_namespace, data)
 
     @private
-    async def sharing_task_determine_locked(self, data, locked_datasets):
-        """
-        `mountpoint` attribute of zvol will be unpopulated and so we
-        first try direct comparison between the two strings.
-
-        The parent dataset of a zvol may also be locked, which renders
-        the zvol inaccessible as well, and so we need to continue to the
-        common check for whether the path is in the locked datasets.
-        """
-        path = await self.get_path_field(data)
-        if data['device_type'] == 'ZVOL' and any(path == os.path.join('/mnt', d['id']) for d in locked_datasets):
-            return True
-
-        return await self.middleware.call('pool.dataset.path_in_locked_datasets', path, locked_datasets)
+    async def sharing_task_determine_locked(self, data):
+        """Determine if this namespace is in a locked path"""
+        return await self.middleware.call(
+            'pool.dataset.path_in_locked_datasets',
+            await self.get_path_field(data)
+        )

src/middlewared/middlewared/plugins/pool_/dataset_encryption_lock.py
@@ -52,22 +52,13 @@ class PoolDatasetService(Service):
             if mountpoint:
                 await delegate.stop(await delegate.query(mountpoint, True))
 
-        try:
-            await self.middleware.call('cache.put', 'about_to_lock_dataset', id_)
-
-            # Invalidate locked datasets cache if something got locked
-            await self.middleware.call('cache.pop', 'zfs_locked_datasets')
-
-            coroutines = [detach(dg) for dg in await self.middleware.call('pool.dataset.get_attachment_delegates')]
-            await asyncio.gather(*coroutines)
-
-            await self.middleware.call(
-                'zfs.dataset.unload_key', id_, {
-                    'umount': True, 'force_umount': options['force_umount'], 'recursive': True
-                }
-            )
-        finally:
-            await self.middleware.call('cache.pop', 'about_to_lock_dataset')
+        coroutines = [detach(dg) for dg in await self.middleware.call('pool.dataset.get_attachment_delegates')]
+        await asyncio.gather(*coroutines)
+        await self.middleware.call(
+            'zfs.dataset.unload_key', id_, {
+                'umount': True, 'force_umount': options['force_umount'], 'recursive': True
+            }
+        )
 
         if ds['mountpoint']:
             await self.middleware.call('filesystem.set_zfs_attributes', {
@@ -256,8 +247,6 @@ class PoolDatasetService(Service):
                 )
 
         if unlocked:
-            # Invalidate locked datasets cache if something got unlocked
-            self.middleware.call_sync('cache.pop', 'zfs_locked_datasets')
             if options['toggle_attachments']:
                 job.set_progress(91, 'Handling attachments')
                 self.middleware.call_sync('pool.dataset.unlock_handle_attachments', dataset)

commit db15e77e5071c3bbe026ae9fdf6cebacb3b6ac50

NAS-135998 / 25.10 / fix pool.dataset.path_in_locked_datasets (#16551)
Regression introduced in
https://github.com/truenas/middleware/pull/16524. Need to handle with
path given to us is an absolute path to a zvol. Tests that regressed are
100% clean
[here](http://jenkins.eng.ixsystems.net:8080/job/tests/job/api_tests/4559/)

src/middlewared/middlewared/plugins/pool_/dataset_encryption_info.py
@@ -226,7 +226,13 @@ class PoolDatasetService(Service):
         """
         # WARNING: _EXTREMELY_ hot code path. Do not add more
         # things here unless you fully understand the side-effects.
-        for i in get_dataset_parents(path.removeprefix('/mnt/')):
+        if path.startswith('/dev/zvol/'):
+            # 10 comes from len("/dev/zvol/")
+            path = path[10:].replace('+', ' ')
+        else:
+            path = path.removeprefix('/mnt/')
+
+        for i in get_dataset_parents(path):
             try:
                 crypto = tls.lzh.open_resource(name=i).crypto()
                 if crypto and not crypto.info().key_is_loaded: