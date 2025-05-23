474b3726ff034e8de870293579f41d70703eac73
themylogin <themylogin@gmail.com>
2025-05-23 14:34:55 +0200

    NAS-135973 / 25.10 / Fix bashism in /etc/profile/path.sh (#16546)

--- src/freenas/etc/profile.d/path.sh
+++ src/freenas/etc/profile.d/path.sh
@@ -1,7 +1,7 @@
 # Replace default non-superuser PATH provided by `/etc/profile` with the one that includes `/sbin` in order to
 # allow non-privileged users to access `zfs` and `zpool` commands without having to type in full path.
 # Requested by NAS-133669
-if [[ "$PATH" == "/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games" ]];
+if [ x"$PATH" = x"/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games" -o x"$PATH" = x"/usr/local/bin:/usr/bin:/bin:/usr/games" ];
 then
   export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 fi
