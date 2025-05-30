cab49020917e2b5a50fd5d1d6f468bf10f284818
Andrew Walker <awalker@ixsystems.com>
2025-05-30 10:55:37 -0600

    Add comment

--- src/middlewared/middlewared/utils/directoryservices/common.py
+++ src/middlewared/middlewared/utils/directoryservices/common.py
@@ -4,4 +4,6 @@ def ds_config_to_fqdn(ds_config: dict) -> str:
     if ds_config['service_type'] not in (DSType.AD.value, DSType.IPA.value):
         raise ValueError(f'{ds_config["service_type"]}: service type unsupported.')
 
+    # WARNING: nsupdate with GSSAPI may expect the domain component to be upper case so
+    # any case normalization should be handled by consumer.
     return f'{ds_config["configuration"]["hostname"]}.{ds_config["configuration"]["domain"]}'
