887e20a5984ad43628ce3de623ee00f4d08f3fdc
Andrew Walker <awalker@ixsystems.com>
2025-05-30 10:12:10 -0600

    Fix

--- src/middlewared/middlewared/utils/directoryservices/common.py
+++ src/middlewared/middlewared/utils/directoryservices/common.py
@@ -4,4 +4,4 @@ def ds_config_to_fqdn(ds_config: dict) -> str:
     if ds_config['service_type'] not in (DSType.AD.value, DSType.IPA.value):
         raise ValueError(f'{ds_config["service_type"]}: service type unsupported.')
 
-    return f'{ds_config["configuration"]["hostname"]}.{ds_config["configuration"]["domain"]}'.lower()
+    return f'{ds_config["configuration"]["hostname"]}.{ds_config["configuration"]["domain"]}'
