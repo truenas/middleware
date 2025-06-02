8be6448f32610c7811c6841632d0303f17266a75
themylogin <themylogin@gmail.com>
2025-06-02 13:00:05 +0200

    NAS-135944 / 25.10 / Check service methods signatures for invalid parameters order (#16521)

--- src/middlewared/middlewared/schema/processor.py
+++ src/middlewared/middlewared/schema/processor.py
@@ -195,19 +195,31 @@ def accepts(*schema, audit=None, audit_callback=False, audit_extended=None, depr
 
 
 def calculate_args_index(f, audit_callback):
-    args_index = 0
-    if f.__code__.co_argcount >= 1 and f.__code__.co_varnames[0] == 'self':
-        args_index += 1
-    if hasattr(f, '_pass_app'):
-        args_index += 1
-        if f._pass_app['message_id']:
-            args_index += 1
-    if hasattr(f, '_pass_thread_local_storage'):
-        args_index += 1
-    if audit_callback:
-        args_index += 1
+    signature_args = list(f.__code__.co_varnames)[:f.__code__.co_argcount]
+    # This must match the order used in `Middleware._call_prepare`
+    expected_args = []
+    if signature_args and signature_args[0] == 'self':
+        expected_args.append('self')
+    if pass_app := hasattr(f, '_pass_app'):
+        expected_args.append('app')
+    # `app` comes before `job` as defined in `Job.__run_body`
     if hasattr(f, '_job'):
-        args_index += 1
+        expected_args.append('job')
+    if audit_callback:
+        expected_args.append('audit_callback')
+    if hasattr(f, '_pass_thread_local_storage'):
+        expected_args.append('tls')
+    if pass_app:
+        if f._pass_app['message_id']:
+            expected_args.append('message_id')
+
+    if signature_args[:len(expected_args)] != expected_args:
+        raise RuntimeError(
+            f"Invalid method signature for {f!r}. Its arguments list must start with {', '.join(expected_args)!r}. "
+            f"It is {', '.join(signature_args)!r}"
+        )
+
+    args_index = len(expected_args)
     if hasattr(f, '_skip_arg'):
         args_index += f._skip_arg
     return args_index
