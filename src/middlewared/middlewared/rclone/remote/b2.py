c344eae373c17930e9e1a0fe3940c19327ce4c08
themylogin <themylogin@gmail.com>
2025-06-03 16:29:08 +0200

    NAS-136116 / 25.10 / Get rid of the rest of clouc sync legacy schemas (#16571)

--- src/middlewared/middlewared/rclone/remote/b2.py
+++ src/middlewared/middlewared/rclone/remote/b2.py
@@ -1,8 +1,4 @@
-import textwrap
-
 from middlewared.rclone.base import BaseRcloneRemote
-from middlewared.schema import Int
-from middlewared.validators import Range
 
 
 class B2RcloneRemote(BaseRcloneRemote):
@@ -15,13 +11,7 @@ class B2RcloneRemote(BaseRcloneRemote):
 
     rclone_type = "b2"
 
-    task_schema = [
-        Int("chunk_size", title="Upload chunk size (in megabytes)", description=textwrap.dedent("""\
-            Upload chunk size. Must fit in memory. Note that these chunks are buffered in memory and there might be a
-            maximum of «--transfers» chunks in progress at once. Also, your largest file must be split in no more
-            than 10 000 chunks.
-        """), default=96, validators=[Range(min_=5)]),
-    ]
+    task_attributes = ["b2_chunk_size"]
 
     async def get_task_extra(self, task):
         chunk_size = task["attributes"].get("chunk_size", 96)
