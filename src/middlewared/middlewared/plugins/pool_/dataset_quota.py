e2af0e7014bcf4ee3f1ae2e2683d7340321cbc4b
caleb <yocalebo@gmail.com>
2025-06-03 08:04:40 -0400

    fixes

--- src/middlewared/middlewared/plugins/pool_/dataset_quota.py
+++ src/middlewared/middlewared/plugins/pool_/dataset_quota.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from middlewared.api import api_method
 from middlewared.api.current import (
     PoolDatasetGetQuotaArgs,
@@ -5,15 +7,17 @@ from middlewared.api.current import (
     PoolDatasetSetQuotaArgs,
     PoolDatasetSetQuotaResult
 )
+from middlewared.plugins.zfs_.utils import TNUserProp
 from middlewared.service import private, Service
 from middlewared.service.decorators import pass_thread_local_storage
 from middlewared.service_exception import ValidationError
 from middlewared.utils import filter_list
-
+from middlewared.utils.nss import pwd, grp
 try:
     import truenas_pylibzfs
 except ImportError:
     truenas_pylibzfs = None
+
 
 def quota_cb(quota, state):
     if quota.quota_type in (
@@ -22,26 +26,73 @@ def quota_cb(quota, state):
     ):
         value_key = 'used_bytes'
     elif quota.quota_type in (
-        truenas_pylibzfs.ZFSUserQuota.USEROBJ_USED,
-        truenas_pylibzfs.ZFSUserQuota.GROUPOBJ_USED,
-    ):
-        value_key = 'obj_used'
-    elif quota.quota_type in (
         truenas_pylibzfs.ZFSUserQuota.USER_QUOTA,
         truenas_pylibzfs.ZFSUserQuota.GROUP_QUOTA,
     ):
         value_key = 'quota'
     elif quota.quota_type in (
+        truenas_pylibzfs.ZFSUserQuota.USEROBJ_USED,
+        truenas_pylibzfs.ZFSUserQuota.GROUPOBJ_USED,
+    ):
+        value_key = 'obj_used'
+    elif quota.quota_type in (
         truenas_pylibzfs.ZFSUserQuota.USEROBJ_QUOTA,
         truenas_pylibzfs.ZFSUserQuota.GROUPOBJ_QUOTA,
     ):
         value_key = 'obj_quota'
+    else:
+        # shouldn't be reachable but return early
+        # to be safe
+        return True
 
-    state['quotas'].append({
-        'quota_type': state['qt'],
-        'id': quota.xid,
-        value_key: quota.value,
-    })
+    entry = {'quota_type': state['qt'], 'id': quota.xid, value_key: quota.value}
+    if quota.xid not in state['quotas']:
+        # only resolve the xid once
+        try:
+            if state['qt'] == 'USER':
+                entry['name'] = pwd.getpwuid(quota.xid, as_dict=True)['pw_name']
+            else:
+                entry['name'] = grp.getgrgid(quota.xid, as_dict=True)['gr_name']
+        except Exception:
+            pass
+    else:
+        entry.update({'name': state['quotas'][quota.xid]['name']})
+
+    state['quotas'][quota.xid].update(entry)
+    return True
+
+
+def quota_alert_cb(hdl, state):
+    if hdl.type == truenas_pylibzfs.ZFSType.ZFS_TYPE_VOLUME:
+        return True
+
+    info = hdl.asdict(
+        properties={
+            truenas_pylibzfs.ZFSProperty.USED,
+            truenas_pylibzfs.ZFSProperty.USEDDS,
+            truenas_pylibzfs.ZFSProperty.QUOTA,
+            truenas_pylibzfs.ZFSProperty.REFQUOTA,
+            truenas_pylibzfs.ZFSProperty.AVAILABLE,
+            truenas_pylibzfs.ZFSProperty.MOUNTED,
+            truenas_pylibzfs.ZFSProperty.MOUNTPOINT,
+        },
+        get_user_properties=True,
+    )
+    for propstring, default in TNUserProp.quotas():
+        info['user_properties'].setdefault(propstring, default)
+        try:
+            info['user_propertites'] = int(info['user_properties'][propstring])
+        except Exception:
+            # if we can't parse this (i.e. someone changed something by hand)
+            # we'll play it safe and just default to 0
+            info['user_propertites'] = 0
+
+    if hdl.name == hdl.pool_name:
+        state['pools'][hdl.name] = info['properties']['used']['value'] + info['properties']['available']['value']
+
+    state['datasets'][hdl.name] = info
+    hdl.iter_filesystems(callback=quota_alert_cb, state=state, fast=True)
+    return True
 
 
 class PoolDatasetService(Service):
@@ -49,8 +100,18 @@ class PoolDatasetService(Service):
     class Config:
         namespace = 'pool.dataset'
 
-    @pass_thread_local_storage
     @private
+    @pass_thread_local_storage
+    def query_for_quota_alert(self, tls):
+        """Called, at time of writing, exclusively
+        by our alert system to inform users of quota
+        thresholds."""
+        state = {"pools": dict(), "datasets": dict()}
+        tls.lzh.iter_root_filesystems(callback=quota_alert_cb, state=state)
+        return state
+
+    @private
+    @pass_thread_local_storage
     def get_quota_impl(self, tls, ds, quota_type):
         rsrc = tls.lzh.open_resource(name=ds)
         quota_type = quota_type.upper()
@@ -72,21 +133,15 @@ class PoolDatasetService(Service):
                     'used_bytes': info['properties']['used']['value'],
                 }]
             case 'USER' | 'GROUP':
-                state = {'qt': quota_type, 'quotas': list()}
+                state = {'qt': quota_type, 'quotas': defaultdict(dict)}
                 for qt in (
                     getattr(truenas_pylibzfs.ZFSUserQuota, f'{quota_type}_USED'),
-                    getattr(truenas_pylibzfs.ZFSUserQuota, f'{quota_type}OBJ_USED'),
                     getattr(truenas_pylibzfs.ZFSUserQuota, f'{quota_type}_QUOTA'),
+                    getattr(truenas_pylibzfs.ZFSUserQuota, f'{quota_type}OBJ_USED'),
                     getattr(truenas_pylibzfs.ZFSUserQuota, f'{quota_type}OBJ_QUOTA'),
                 ):
                     rsrc.iter_userspace(callback=quota_cb, quota_type=qt, state=state)
-
-                qtl = quota_type.lower()
-                for i in state['quotas']:
-                    # resolve uid/gid to name
-                    i['name'] = self.middleware.call_sync(
-                        f'{qtl}.get_{qtl}_obj', {f'{qtl[0]}id': i['id']}
-                    )['pw_name' if qtl == 'user' else 'gr_name']
+                return list(state['quotas'].values())
             case _:
                 raise ValidationError(
                     'pool.dataset.get_quota', f'Invalid quota type: {quota_type!r}'
@@ -110,18 +165,19 @@ class PoolDatasetService(Service):
         )
         return filter_list(quota_list, filters, options)
 
-    @pass_thread_local_storage
     @private
+    @pass_thread_local_storage
     def set_quota_impl(self, tls, ds, inquotas):
         ds_quotas, quotas = dict(), list()
         for i in inquotas:
             if i['quota_type'] == 'DATASET':
                 ds_quotas[truenas_pylibzfs.ZFSProperty[i['id']]] = i['quota_value']
             else:
+                qt = truenas_pylibzfs.ZFSUserQuota[f'{i["quota_type"]}_QUOTA']
                 quotas.append(
                     {
                         'xid': i['id'],
-                        'quota_type': truenas_pylibzfs.ZFSUserQuota[i['quota_type']],
+                        'quota_type': qt,
                         'value': i['quota_value']
                     }
                 )
@@ -130,7 +186,7 @@ class PoolDatasetService(Service):
         if ds_quotas:
             rsrc.set_properties(properties=ds_quotas)
         if quotas:
-            rsrc.set_quotas(quotas=quotas)
+            rsrc.set_userquotas(quotas=quotas)
 
     @api_method(
         PoolDatasetSetQuotaArgs,
@@ -182,8 +238,6 @@ class PoolDatasetService(Service):
                         f'Setting {quota_type} quota on {id_type[0]}id [{xid}] is not permitted'
                     )
                 q['id'] = xid
-
             quotas.append(q)
-
         if quotas:
             await self.middleware.call('pool.dataset.set_quota_impl', ds, quotas)
