0162a697df19d7684ce81e38141d6136ddb0b339
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-06-14 18:45:48 +0500

    Update tests

--- src/middlewared/middlewared/pytest/unit/plugins/apps/test_normalize_questions.py
+++ src/middlewared/middlewared/pytest/unit/plugins/apps/test_normalize_questions.py
@@ -2,71 +2,163 @@ import pytest
 
 from middlewared.plugins.apps.schema_normalization import AppSchemaService, REF_MAPPING
 from middlewared.pytest.unit.middleware import Middleware
-from middlewared.schema import Dict, List
 
 
-@pytest.mark.parametrize('question_attr, ref, value, update', [
+@pytest.mark.parametrize('attr_schema, ref, value, update', [
     (
-        Dict(),
+        {
+            'variable': 'cert_id',
+            'schema': {
+                'type': 'int',
+                '$ref': ['definitions/certificate']
+            }
+        },
         'definitions/certificate',
-        {'attr1': 'some_value'},
+        1234,
         False
     ),
     (
-        Dict(),
+        {
+            'variable': 'acl_config',
+            'schema': {
+                'type': 'dict',
+                '$ref': ['normalize/acl'],
+                'attrs': []
+            }
+        },
         'normalize/acl',
-        {'attr1': 'some_value'},
+        {'entries': [], 'path': '/mnt/data'},
         False
     ),
     (
-        Dict(),
+        {
+            'variable': 'acl_config',
+            'schema': {
+                'type': 'dict',
+                '$ref': ['normalize/acl'],
+                'attrs': []
+            }
+        },
         'normalize/acl',
-        {'attr1': 'some_value'},
+        {'entries': [], 'path': '/mnt/data'},
         True
     ),
     (
-        Dict(),
+        {
+            'variable': 'cert_id',
+            'schema': {
+                'type': 'int',
+                'null': True,
+                '$ref': ['definitions/certificate']
+            }
+        },
         'definitions/certificate',
         None,
         False
     )
 ])
 @pytest.mark.asyncio
-async def test_normalize_question(question_attr, ref, value, update):
+async def test_normalize_question(attr_schema, ref, value, update):
     middleware = Middleware()
     app_schema_obj = AppSchemaService(middleware)
     middleware[f'app.schema.normalize_{REF_MAPPING[ref]}'] = lambda *args: value
-    question_attr.ref = [ref]
-    result = await app_schema_obj.normalize_question(question_attr, value, update, '', '')
+    result = await app_schema_obj.normalize_question(attr_schema, value, update, {}, {})
     assert result == value
 
 
-@pytest.mark.parametrize('question_attr, ref, value, update', [
+@pytest.mark.parametrize('attr_schema, ref, value, update', [
     (
-        List(
-            items=[
-                Dict('question1', additional_attrs=True),
-                Dict('question2', additional_attrs=True),
-                Dict('question3', additional_attrs=True),
-            ]
-        ),
+        {
+            'variable': 'cert_list',
+            'schema': {
+                'type': 'list',
+                '$ref': ['definitions/certificate'],
+                'items': [
+                    {
+                        'variable': 'cert_item',
+                        'schema': {
+                            'type': 'int',
+                            '$ref': ['definitions/certificate']
+                        }
+                    }
+                ]
+            }
+        },
         'definitions/certificate',
-        [
-            {'question1': 'val1'},
-            {'question2': 'val2'},
-            {'question3': 'val3'}
-        ],
+        [1, 2, 3],
         False
     ),
 ])
 @pytest.mark.asyncio
-async def test_normalize_question_List(question_attr, ref, value, update):
+async def test_normalize_question_List(attr_schema, ref, value, update):
     middleware = Middleware()
     app_schema_obj = AppSchemaService(middleware)
-    middleware[f'app.schema.normalize_{REF_MAPPING[ref]}'] = lambda *args: value
-    for attr in question_attr.items:
-        attr.ref = [ref]
-    question_attr.ref = [ref]
+    # Mock the normalize functions to return the value as-is
+    middleware[f'app.schema.normalize_{REF_MAPPING[ref]}'] = lambda *args: args[1]  # Return the value parameter
 
-    result = await app_schema_obj.normalize_question(question_attr, value, update, '', '')
+    result = await app_schema_obj.normalize_question(attr_schema, value, update, {}, {})
     assert result == value
+
+
+@pytest.mark.parametrize('attr_schema, value, update', [
+    (
+        {
+            'variable': 'storage_config',
+            'schema': {
+                'type': 'dict',
+                'attrs': [
+                    {
+                        'variable': 'volume',
+                        'schema': {
+                            'type': 'dict',
+                            '$ref': ['normalize/ix_volume']
+                        }
+                    },
+                    {
+                        'variable': 'certificate',
+                        'schema': {
+                            'type': 'int',
+                            '$ref': ['definitions/certificate']
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            'volume': {'dataset_name': 'mydata', 'properties': {}},
+            'certificate': 123
+        },
+        False
+    ),
+])
+@pytest.mark.asyncio
+async def test_normalize_nested_dict_with_refs(attr_schema, value, update):
+    middleware = Middleware()
+    app_schema_obj = AppSchemaService(middleware)
+
+    # Mock the normalize functions
+    async def mock_normalize_ix_volume(attr, val, config, context):
+        # Simulate adding volume to config
+        config['ix_volumes'][val['dataset_name']] = {'path': f'/mnt/ix-apps/{val["dataset_name"]}'}
+        return val
+
+    async def mock_normalize_certificate(attr, val, config, context):
+        # Simulate adding certificate to config
+        config['ix_certificates'][val] = {'name': f'cert-{val}'}
+        return val
+
+    middleware['app.schema.normalize_ix_volume'] = mock_normalize_ix_volume
+    middleware['app.schema.normalize_certificate'] = mock_normalize_certificate
+
+    complete_config = {
+        'ix_volumes': {},
+        'ix_certificates': {}
+    }
+    context = {'actions': []}
+
+    result = await app_schema_obj.normalize_question(attr_schema, value, update, complete_config, context)
+
+    # Check that the normalization was applied
+    assert result == value
+    assert 'mydata' in complete_config['ix_volumes']
+    assert 123 in complete_config['ix_certificates']
