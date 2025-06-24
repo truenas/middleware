32fe0bb58ac5b32a79f29ca7fd0420a175b69424
themylogin <themylogin@gmail.com>
2025-06-25 01:28:25 +0200

    Fix adapting unions

--- src/middlewared/middlewared/api/base/handler/inspect.py
+++ src/middlewared/middlewared/api/base/handler/inspect.py
@@ -1,15 +1,20 @@
+import pydantic_core
 import types
 import typing
+
+from pydantic import ValidationError
 
 from middlewared.api.base import BaseModel
 
 
-def model_field_is_model(model) -> type[BaseModel] | None:
+def model_field_is_model(model, value_hint=None, name_hint=None) -> type[BaseModel] | None:
     """
     Return` model` if it is an API model.
     Returns the first union member that is an API model if `model` is a union.
     Otherwise, returns `None`.
     :param model: potentially, API model.
+    :param value_hint: value to choose the correct union member.
+    :param name_hint: model name to choose the correct union member.
     :return: `model` or `None`
     """
     if isinstance(model, type) and issubclass(model, BaseModel):
@@ -17,7 +22,18 @@ def model_field_is_model(model) -> type[BaseModel] | None:
     if typing.get_origin(model) is types.UnionType:
         for member in model.__args__:
             if result := model_field_is_model(member):
-                return result
+                if value_hint is not None:
+                    try:
+                        result(**value_hint)
+                    except ValidationError:
+                        pass
+                    else:
+                        return result
+                elif name_hint is not None:
+                    if result.__name__ == name_hint:
+                        return result
+                else:
+                    return result
 
 
 def model_field_is_list_of_models(model) -> type[BaseModel] | None:
