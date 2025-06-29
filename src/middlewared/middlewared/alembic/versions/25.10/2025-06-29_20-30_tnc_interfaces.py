92390a1ecaf0ceada0d07ec90dace5feb60f2798
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-06-29 16:41:49 +0500

    Fix migration

--- src/middlewared/middlewared/alembic/versions/25.10/2025-06-29_20-30_tnc_interfaces.py
+++ src/middlewared/middlewared/alembic/versions/25.10/2025-06-29_20-30_tnc_interfaces.py
@@ -1,8 +1,8 @@
 """ Add interfaces and interfaces_ips to truenas_connect
 
-Revision ID: tnc_interfaces_001
-Revises:
-Create Date: 2025-06-26 20:30:00.000000+00:00
+Revision ID: 2368b4b28a87
+Revises: d4e0268ca57d
+Create Date: 2025-06-29 20:30:00.000000+00:00
 
 """
 from alembic import op
@@ -10,16 +10,16 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'tnc_interfaces_001'
-down_revision = None
+revision = '2368b4b28a87'
+down_revision = 'd4e0268ca57d'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     with op.batch_alter_table('truenas_connect', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('interfaces', sa.JSON(), nullable=False, server_default='[]'))
-        batch_op.add_column(sa.Column('interfaces_ips', sa.JSON(), nullable=False, server_default='[]'))
+        batch_op.add_column(sa.Column('interfaces', sa.TEXT(), nullable=False, server_default='[]'))
+        batch_op.add_column(sa.Column('interfaces_ips', sa.TEXT(), nullable=False, server_default='[]'))
 
 
 def downgrade():
