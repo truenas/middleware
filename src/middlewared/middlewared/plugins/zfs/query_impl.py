5ff9849da58800ac16965163f9a83ef19ef9ee11
Caleb <yocalebo@gmail.com>
2025-07-04 16:08:20 -0400

    add zfs.resource.query (WIP)

---
+++ src/middlewared/middlewared/plugins/zfs/query_impl.py
@@ -0,0 +1,150 @@
+import dataclasses
+import itertools
+
+try:
+    from truenas_pylibzfs import ZFSError, ZFSException
+except ImportError:
+    ZFSError = ZFSException = None
+
+from middlewared.utils import BOOT_POOL_NAME_VALID
+from .normalization import normalize_asdict_result
+from .property_management import build_set_of_zfs_props, DeterminedProperties
+
+__all__ = ("query_impl", "ZFSPathNotFoundException")
+
+INTERNAL_FILESYSTEMS = (
+    ".system",
+    "ix-applications",
+    "ix-apps",
+    ".ix-virt",
+)
+
+
+class ZFSPathNotFoundException(Exception):
+    pass
+
+
+@dataclasses.dataclass(slots=True, kw_only=True)
+class CallbackState:
+    results: list
+    query_args: dict
+    dp: DeterminedProperties
+    eip: bool = False
+
+
+def __is_internal_path(path):
+    """
+    Check if a path is an internal filesystem.
+
+    Args:
+        path: relative path representing the zfs filesystem
+
+    Returns:
+        bool: True if the path is internal, False otherwise
+    """
+    for a, b in itertools.zip_longest(BOOT_POOL_NAME_VALID, INTERNAL_FILESYSTEMS):
+        if a and (path == a or path.startswith(f"{a}/")):
+            return True
+        if b and (f"/{b}" in path):
+            return True
+    return False
+
+
+def __query_impl_callback(hdl, state):
+    if state.eip and __is_internal_path(hdl.name):
+        # returning False here will halt the iteration
+        # entirely which is not what we want to do
+        return True
+
+    state.results.append(
+        normalize_asdict_result(
+            hdl.asdict(
+                properties=build_set_of_zfs_props(
+                    hdl.type, state.dp, state.query_args["properties"]
+                ),
+                get_user_properties=state.query_args["get_user_properties"],
+                get_source=state.query_args["get_source"],
+            )
+        )
+    )
+    if state.query_args["get_children"]:
+        hdl.iter_filesystems(callback=__query_impl_callback, state=state)
+    return True
+
+
+def __query_impl_paths(hdl, state):
+    # end-user has provided specific paths
+    # to be queried and so we can apply
+    # significant optimizations by querying
+    # these specific resources without iterating
+    # over all filesystems just to find the ones
+    # the end-user wants
+    for path in state.query_args["paths"]:
+        try:
+            rsrc = hdl.open_resource(name=path)
+            __query_impl_callback(rsrc, state)
+        except ZFSException as e:
+            if ZFSError(e.code) == ZFSError.EZFS_NOENT:
+                raise ZFSPathNotFoundException(f"{path!r}: not found")
+            raise
+
+
+def __query_impl_roots(hdl, state):
+    hdl.iter_root_filesystems(callback=__query_impl_callback, state=state)
+
+
+def __extract_filters_to_paths(state):
+    # Someone could pass us a query-filter looking
+    # like [["name", "=", "tank/test"]] or
+    # like [["name", "in", ["tank/test" "tank/foo"]]
+    # In either scenario, we'll extract these out
+    # and put them into the "paths" key in the callback
+    # state so that the iteration is done as efficiently
+    # as possible.
+    filters = state.query_args["query-filters"]
+    if filters and filters[0][0] == "name" and filters[0][1] in ("=", "in"):
+        extracted = filters.pop(0)
+        if isinstance(extracted[2], str):
+            # [["name", "=", "tank"]]
+            state.query_args["paths"].append(extracted[2])
+        else:
+            # [["name", "in", ["tank", "dozer"]]]
+            state.query_args["paths"].extend(extracted[2])
+
+
+def __should_exclude_internal_paths(state):
+    if not state.query_args["paths"]:
+        # no filters or paths given, which equates
+        # to an "empty" query. we'll ignore internal
+        # paths.
+        state.eip = True
+    else:
+        for path in state.query_args["paths"]:
+            if __is_internal_path(path):
+                # somone is explicilty querying an
+                # internal path
+                state.eip = False
+                break
+        else:
+            # someone specified path(s) and none
+            # are an internal path
+            state.eip = True
+
+
+def query_impl(hdl, data):
+    state = CallbackState(results=list(), query_args=data, dp=DeterminedProperties())
+    __extract_filters_to_paths(state)
+    __should_exclude_internal_paths(state)
+    if state.query_args["paths"]:
+        __query_impl_paths(hdl, state)
+    else:
+        # if someone queries for zfs resources without
+        # applying any type of filters/options/paths etc
+        # then we'll be nice and set get_children: True
+        # so that it returns some information. Otherwise
+        # it's a less than stellar experience
+        state.query_args["get_children"] = state.query_args["get_children"] or (
+            not state.query_args["query-filters"]
+        )
+        __query_impl_roots(hdl, state)
+    return state.results
---
+++ src/middlewared/middlewared/plugins/zfs/resource_crud.py
@@ -0,0 +1,84 @@
+import errno
+import pathlib
+
+from middlewared.api import api_method
+from middlewared.api.current import (
+    ZFSResourceEntry,
+    ZFSResourceQueryArgs,
+    ZFSResourceQueryResult,
+)
+from middlewared.service import Service, private
+from middlewared.service_exception import ValidationError
+
+from .query_impl import query_impl, ZFSPathNotFoundException
+
+
+class ZFSResourceService(Service):
+    class Config:
+        namespace = "zfs.resource"
+        cli_private = True
+        entry = ZFSResourceEntry
+
+    @private
+    def group_paths_by_parents(self, paths: list[str]) -> dict[str, list[str]]:
+        """
+        Group paths by their parent directories, mapping each parent to
+        all paths that are relative to it. For each path in the input list,
+        finds all other paths that are relative to that path and groups
+        them together.
+
+        Args:
+            paths: List of relative POSIX path strings
+
+        Returns:
+            Dict mapping parent paths to lists of their relative subpaths
+
+        Example:
+            >>> group_paths_by_parents(['dozer/test', 'dozer/test/foo', 'tank', 'dozer/abc'])
+            {'dozer/test': ['dozer/test/foo']}
+        """
+        root_dict = dict()
+        if not paths:
+            return root_dict
+
+        for path in paths:
+            subpaths = list()
+            for sp in paths:
+                if pathlib.Path(sp).is_relative_to(pathlib.Path(path)) and sp != path:
+                    # Find all paths that are relative to this path
+                    # (excluding the path itself)
+                    subpaths.append(sp)
+            if subpaths:
+                root_dict[path] = subpaths
+        return root_dict
+
+    @private
+    def validate_query_args(self, data):
+        for path in data["paths"]:
+            if "@" in path:
+                raise ValidationError(
+                    "zfs.resource.query",
+                    "Querying snapshot information is unsupported at this time.",
+                )
+
+        if data["get_children"] and self.group_paths_by_parents(data["paths"]):
+            raise ValidationError(
+                "zfs.resource.query",
+                (
+                    "Paths must be non-overlapping - no path can be relative to another "
+                    "when get_children is set to True."
+                ),
+            )
+
+    @api_method(
+        ZFSResourceQueryArgs,
+        ZFSResourceQueryResult,
+        pass_thread_local_storage=True,
+        roles=["FULL_ADMIN"],
+    )
+    def query(self, tls, data):
+        self.validate_query_args(data)
+        try:
+            return query_impl(tls.lzh, data)
+        except ZFSPathNotFoundException as e:
+            raise ValidationError("zfs.resource.query", str(e), errno.ENOENT)
