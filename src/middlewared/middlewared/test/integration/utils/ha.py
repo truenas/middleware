ced7a1d618787a14a1652cef60a491834cc5444c
bmeagherix <118192357+bmeagherix@users.noreply.github.com>
2025-07-11 19:10:26 +0100

    NAS-136661 / 25.10 / Add ha_settle  (#16722)
    Wait for HA to settle before beginning certain test runs.

---
+++ src/middlewared/middlewared/test/integration/utils/ha.py
@@ -0,0 +1,23 @@
+import time
+
+from middlewared.test.integration.utils import call
+
+__all__ = ['settle_ha']
+
+
+def settle_ha(delay=5, retries=24):
+    """
+    Wait for HA status to look settled.
+
+    Default to 24 retries, with a 5 second delay => 2 minutes
+    """
+    reasons = []
+    for i in range(retries):
+        try:
+            reasons = call('failover.disabled.reasons')
+            if not reasons:
+                return
+        except Exception:
+            pass
+        time.sleep(delay)
+    raise ValueError(f'HA failed to settle: {reasons}')
