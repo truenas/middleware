a76560a08d5de08ba17b7c748286668853ecd093
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-07-29 03:48:30 +0500

    Add a private model for updating tnc env

---
+++ src/middlewared/middlewared/plugins/truenas_connect/private_models.py
@@ -0,0 +1,9 @@
+from middlewared.api.base import BaseModel, HttpsOnlyURL, single_argument_args
+
+
+@single_argument_args('tn_connect_update_environment')
+class TrueNASConnectUpdateEnvironmentArgs(BaseModel):
+    account_service_base_url: HttpsOnlyURL
+    leca_service_base_url: HttpsOnlyURL
+    tnc_base_url: HttpsOnlyURL
+    heartbeat_url: HttpsOnlyURL
