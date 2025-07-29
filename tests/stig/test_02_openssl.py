517fe242d24d6396266b50cc2dcb3b0cfb0aa524
Brian M <brian.meagher@ixsystems.com>
2025-07-29 15:35:31 -0700

    Fix STIG tests for HA

--- tests/stig/test_02_openssl.py
+++ tests/stig/test_02_openssl.py
@@ -8,19 +8,19 @@ retry = 5
 fips_version = "3.0.9"
 
 
-# Sometimes this test fails because the testing environment has broken failover (randomly. Fun transient error. Reports a failed heartbeat).
-@pytest.mark.flaky(reruns=retry, reruns_delay=5)
+@pytest.mark.timeout(1200)
 @pytest.mark.skipif(not ha, reason='Test only valid for HA')
 def test_fips_version():
     # The reason we have a set of commands in a payload is because of some annoying FIPS technicalities.
     # Basically, when FIPS is enabled, we can't use SSH because the SSH key used by root isn't using a FIPS provided algorithm. (this might need to be investigated further)
     # To allow testing, we write our FIPS information to a file during this phase, and then go disable FIPS to get SSH back all in one joint command.
-    payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && midclt call system.reboot.info >> /root/osslproviders && midclt call --job system.security.update '{"enable_fips": false}'"""
+    payload = """midclt call --job system.security.update '{"enable_fips": true}' && openssl list -providers > /root/osslproviders && midclt call system.reboot.info >> /root/osslproviders && sleep 30 && midclt call --job system.security.update '{"enable_fips": false}'"""
 
-    ssh(payload, complete_response=True, timeout=300)
+    ssh(payload, complete_response=True, timeout=400)
 
     # Check that things are what we expect when FIPS was enabled
     enabled_info = ssh("cat /root/osslproviders")
+
     assert fips_version in enabled_info
     assert "FIPS configuration was changed." in enabled_info
 
