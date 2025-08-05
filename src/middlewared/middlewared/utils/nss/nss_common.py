de69c8b850446c8254b311b35a83f0ec6fc1bdff
Andrew Walker <awalker@ixsystems.com>
2025-08-05 11:45:16 -0500

    NAS-137010 / 26.04 / Cache function pointers to nss module functions (#16866)
    This commit reduces the amount of dlopen during NSS calls (for example
    when building directory services caches) by lazy-initializing a cache
    containing references to the various NSS functions on a per-module basis
    (files, sss, winbind).

--- src/middlewared/middlewared/utils/nss/nss_common.py
+++ src/middlewared/middlewared/utils/nss/nss_common.py
@@ -2,10 +2,41 @@ import enum
 import ctypes
 import os
 
+from collections import defaultdict
+
+
 NSS_MODULES_DIR = '/usr/lib/x86_64-linux-gnu'
 FILES_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_files.so.2')
 SSS_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_sss.so.2')
 WINBIND_NSS_PATH = os.path.join(NSS_MODULES_DIR, 'libnss_winbind.so.2')
+
+
+class NSSModuleFN(defaultdict):
+    """ Default dictionary containing references to C function pointers for a specific NSS module.
+    Example: '_nss_files_getpwnam_r' """
+    cddl = None
+    module_name = None
+
+    def __missing__(self, key):
+        fn = getattr(self.cdll, f'_nss_{self.module_name}_{key}')
+        self[key] = fn
+        return fn
+
+
+class NSSModuleCDLL(defaultdict):
+    """ A default dictionary that holds references to loaded shared libaries for the NSS modules above.
+    For example, '/usr/lib/x86_64-linux-gnu/libnss_files.so.2'. The returned value is an NSSModuleFN
+    that will hold references to lazy-initialized C function pointers."""
+    def __missing__(self, key):
+        mod, path = key.split('.', 1)
+        cdll = ctypes.CDLL(path, use_errno=True)
+        self[key] = NSSModuleFN()
+        self[key].cdll = cdll
+        self[key].module_name = mod.lower()
+        return self[key]
+
+
+NSSDICT = NSSModuleCDLL()
 
 
 class NssReturnCode(enum.IntEnum):
@@ -54,9 +85,12 @@ class NssError(Exception):
         return errmsg
 
 
-def get_nss_func(nss_op, nss_module):
+def get_nss_func(nss_op: NssOperation, nss_module: NssModule):
+    """ Get the C function pointer for the particular NSS operation for the NSS module. The cache
+    is lazy-initialized as different modules are used. Standalone servers will only ever use the
+    files module. """
     if nss_module == NssModule.ALL:
         raise ValueError('ALL module may not be explicitly used')
 
-    lib = ctypes.CDLL(nss_module.value, use_errno=True)
-    return getattr(lib, f'_nss_{nss_module.name.lower()}_{nss_op.value}')
+    mod_ptr = NSSDICT[f'{nss_module.name}.{nss_module.value}']
+    return mod_ptr[nss_op.value]
