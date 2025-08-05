310c1ce0020b3e7771b4f39676e09c17801caa2c
Andrew Walker <awalker@ixsystems.com>
2025-08-05 10:45:04 -0600

    Update src/middlewared/middlewared/utils/nss/nss_common.py
    Co-authored-by: Caleb St. John <30729806+yocalebo@users.noreply.github.com>

--- src/middlewared/middlewared/utils/nss/nss_common.py
+++ src/middlewared/middlewared/utils/nss/nss_common.py
@@ -24,7 +24,7 @@ class NSSModuleFN(defaultdict):
 
 
 class NSSModuleCDLL(defaultdict):
-    """ A default dictionary that holds references loaded shared libaries for the NSS modules above.
+    """ A default dictionary that holds references to loaded shared libaries for the NSS modules above.
     For example, '/usr/lib/x86_64-linux-gnu/libnss_files.so.2'. The returned value is an NSSModuleFN
     that will hold references to lazy-initialized C function pointers."""
     def __missing__(self, key):
