7dc4555620219f30d986e435f75c9fdab81c1fcb
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-08-09 22:55:55 +0500

    Add a quoted string dumper
    (cherry picked from commit 4045be4871e5827d4dd982e556c3b4e87fe9d566)

--- src/middlewared/middlewared/plugins/apps/ix_apps/utils.py
+++ src/middlewared/middlewared/plugins/apps/ix_apps/utils.py
@@ -1,5 +1,7 @@
 import enum
 
+import yaml
+
 from catalog_reader.library import RE_VERSION  # noqa
 from middlewared.plugins.apps_images.utils import normalize_reference  # noqa
 from middlewared.plugins.apps.schema_construction_utils import CONTEXT_KEY_NAME  # noqa
@@ -20,6 +22,19 @@ class ContainerState(enum.Enum):
     EXITED = 'exited'
     RUNNING = 'running'
     STARTING = 'starting'
+
+
+class QuotedStrDumper(yaml.SafeDumper):
+    pass
+
+
+def _repr_str(dumper, data: str):
+    if '\n' in data:
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+
+
+QuotedStrDumper.add_representer(str, _repr_str)
 
 
 def get_app_name_from_project_name(project_name: str) -> str:
