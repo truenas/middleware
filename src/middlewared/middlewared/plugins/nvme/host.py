9341536f92d9cac25a70fc8d52c035c8c7489eaf
Brian M <brian.meagher@ixsystems.com>
2025-08-18 12:55:33 -0700

    Address review

--- src/middlewared/middlewared/plugins/nvme/host.py
+++ src/middlewared/middlewared/plugins/nvme/host.py
@@ -10,24 +10,31 @@ LIVE_NVMEID_PATH = '/etc/nvme/hostid'
 
 
 def setup(middleware):
-    data_nvme = pathlib.Path(DATA_NVME_PATH)
-    if not data_nvme.exists():
-        data_nvme.mkdir()
+    try:
+        data_nvme = pathlib.Path(DATA_NVME_PATH)
+        try:
+            data_nvme.mkdir()
+        except FileExistsError:
+            pass
 
-    # /etc/nvme/hostnqn
-    data_hostnqn = data_nvme / 'hostnqn'
-    if not data_hostnqn.exists():
-        data_hostnqn.write_text(f'{NQN_UUID_PREFIX}{uuid.uuid4()}\n')
-        middleware.logger.debug("Generated hostnqn")
-    if not filecmp.cmp(data_hostnqn, LIVE_NVMENQN_PATH):
-        shutil.copy2(data_hostnqn, LIVE_NVMENQN_PATH)
-        middleware.logger.debug(f"Wrote {LIVE_NVMENQN_PATH}")
+        # /etc/nvme/hostnqn
+        data_hostnqn = data_nvme / 'hostnqn'
+        if not data_hostnqn.exists():
+            data = f'{NQN_UUID_PREFIX}{uuid.uuid4()}'
+            data_hostnqn.write_text(f'{data}\n')
+            middleware.logger.debug("Generated hostnqn: %s", data)
+        if not filecmp.cmp(data_hostnqn, LIVE_NVMENQN_PATH):
+            shutil.copy2(data_hostnqn, LIVE_NVMENQN_PATH)
+            middleware.logger.debug("Wrote %s", LIVE_NVMENQN_PATH)
 
-    # /etc/nvme/hostid
-    data_hostid = data_nvme / 'hostid'
-    if not data_hostid.exists():
-        data_hostid.write_text(f'{uuid.uuid4()}\n')
-        middleware.logger.debug("Generated hostid")
-    if not filecmp.cmp(data_hostid, LIVE_NVMEID_PATH):
-        shutil.copy2(data_hostid, LIVE_NVMEID_PATH)
-        middleware.logger.debug(f"Wrote {LIVE_NVMEID_PATH}")
+        # /etc/nvme/hostid
+        data_hostid = data_nvme / 'hostid'
+        if not data_hostid.exists():
+            data = f'{uuid.uuid4()}'
+            data_hostid.write_text(f'{data}\n')
+            middleware.logger.debug("Generated hostid: %s", data)
+        if not filecmp.cmp(data_hostid, LIVE_NVMEID_PATH):
+            shutil.copy2(data_hostid, LIVE_NVMEID_PATH)
+            middleware.logger.debug("Wrote %s", LIVE_NVMEID_PATH)
+    except Exception:
+        middleware.logger.debug("Failed to generate nvme host info", exc_info=True)
