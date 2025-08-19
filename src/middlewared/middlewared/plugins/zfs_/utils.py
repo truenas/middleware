f03e146f2cff373695e23b103be8cfb26e26ab52
M. Rehan <mrehanlm93@gmail.com>
2025-08-19 17:41:15 +0500

    Do not consider 'ix-virt' as an internal dataset

--- src/middlewared/middlewared/plugins/zfs_/utils.py
+++ src/middlewared/middlewared/plugins/zfs_/utils.py
@@ -139,13 +139,6 @@ def unlocked_zvols_fast(options=None, data=None):
             for file in files:
                 path = root + '/' + file
 
-                # zvols located within ix-virt are managed by incus and should
-                # never be presented as choices in middleware. Removing this
-                # check may introduce data corruption bugs by allowing zvols
-                # to be simultaneously used by multiple VMs.
-                if '.ix-virt' in path:
-                    continue
-
                 zvol_name = zvol_path_to_name(path)
 
                 try:
