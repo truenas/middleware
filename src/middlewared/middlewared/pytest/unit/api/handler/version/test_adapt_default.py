60cafcd7f0c0d869bf22d36cf1e72571f58bb452
Logan Cary <logan.cary@ixsystems.com>
2025-08-19 12:32:56 -0400

    test

--- src/middlewared/middlewared/pytest/unit/api/handler/version/test_adapt_default.py
+++ src/middlewared/middlewared/pytest/unit/api/handler/version/test_adapt_default.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 import pytest
 
 from middlewared.api.base import BaseModel
@@ -8,17 +9,28 @@ from .utils import TestModelProvider
 
 class SettingsV1(BaseModel):
     text1: str
+    field_with_alias: str = Field(alias="alias", default="field_with_alias")
+    field_with_factory: str = Field(alias="other-alias", default_factory=lambda: "field_with_factory")
 
 
-class SettingsV2(BaseModel):
-    text1: str
+class SettingsV2(SettingsV1):
     text2: str = "text2"
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("version1,value,version2,result", [
-    ("v1", {"text1": "text1"}, "v2", {"text1": "text1", "text2": "text2"}),
-    ("v2", {"text1": "text1", "text2": "text2"}, "v1", {"text1": "text1"}),
+    (
+        "v1",
+        {"text1": "text1"},
+        "v2",
+        {"text1": "text1", "text2": "text2", "alias": "field_with_alias", "other-alias": "field_with_factory"},
+    ),
+    (
+        "v2",
+        {"text1": "text1", "text2": "text2"},
+        "v1",
+        {"text1": "text1", "alias": "field_with_alias", "other-alias": "field_with_factory"},
+    ),
 ])
 async def test_adapt(version1, value, version2, result):
     adapter = APIVersionsAdapter([
