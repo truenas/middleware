67ee5b99059e25390c51453511008fc1c57723fa
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-08-28 07:30:59 -0400

    NAS-121593 / 25.10-RC.1 / Add logic for renewing DNS entries (by anodos325) (#17090)
    This commit adds logic for automatically renewing DNS entries when the
    administrator has configured TrueNAS to do such. This is required for
    some AD environments where the system administrator has configured AD to
    automatically scavenge records that haven't been updated for a while.
    
    This commit also introduces a threading lock NSUPDATE_LOCK that is held
    whenever middleware registers / deregisters / renews DNS entries for
    directory services.
    
    Original PR: https://github.com/truenas/middleware/pull/17088
    
    ---------
    
    Co-authored-by: Andrew Walker <andrew.walker@truenas.com>

--- src/middlewared/middlewared/alert/source/directory_services.py
+++ src/middlewared/middlewared/alert/source/directory_services.py
@@ -1,3 +1,4 @@
+import errno
 from datetime import timedelta
 from middlewared.alert.base import AlertClass, AlertCategory, Alert, AlertLevel, AlertSource
 from middlewared.alert.schedule import IntervalSchedule
@@ -5,12 +6,20 @@ from middlewared.utils.directoryservices.constants import DSStatus
 from middlewared.utils.directoryservices.health import (
     DSHealthObj, ADHealthError, IPAHealthError, KRB5HealthError, LDAPHealthError,
 )
+from middlewared.service_exception import CallError
 
 
 class DirectoryServiceBindAlertClass(AlertClass):
     category = AlertCategory.DIRECTORY_SERVICE
     level = AlertLevel.WARNING
     title = "DirectoryService Bind Is Not Healthy"
+    text = "%(err)s."
+
+
+class DirectoryServiceDnsUpdateAlertClass(AlertClass):
+    category = AlertCategory.DIRECTORY_SERVICE
+    level = AlertLevel.WARNING
+    title = "DirectoryService DNS Update Failed"
     text = "%(err)s."
 
 
@@ -39,3 +48,50 @@ class DirectoryServiceDomainBindAlertSource(AlertSource):
         except Exception:
             # We shouldn't be raising other sorts of errors
             self.logger.error("Unexpected error while performing health check.", exc_info=True)
+
+
+class DirectoryServiceDnsUpdateAlertSource(AlertSource):
+    # The DNS updates are potentially going to happen every 7 days, but we check whether it's
+    # needed more frequently. The reason for this is that users get a bit antsy if there's a
+    # long-lived alert. The actual test itself (directoryservices.connection.refresh_dns) is not
+    # expensive (a few datastore queries and reading a state file) and so once-per-hour isn't
+    # going to generate excessive load.
+    schedule = IntervalSchedule(timedelta(hours=1))
+    run_on_backup_node = False
+
+    async def check(self):
+        if DSHealthObj.dstype is None:
+            return
+
+        try:
+            # checks for enabled DS and whether DNS updates are enabled occur in this method
+            await self.middleware.call('directoryservices.connection.renew_dns')
+        except RuntimeError as exc:
+            # This most likely means somehow someone has cleared the kerberos realm without
+            # disabling directory services. Most likely scenario is playing around with datastore
+            # plugin or sqlite3 commands.
+            self.middleware.logger.warning('Periodic DNS update failed due to misconfiguration.', exc_info=True)
+            return Alert(DirectoryServiceDnsUpdateAlertClass, {'err', str(exc)}, key=None)
+        except FileNotFoundError:
+            # This happens if the system dataset is not mounted. We'll log an error but not
+            # raise an alert because there are probably many other things hollering about the
+            # broken sytsem dataset.
+            self.middleware.logger.warning('Periodic DNS update failed due to broken system dataset.', exc_info=True)
+        except CallError as exc:
+            self.middleware.logger.warning('Periodic DNS update failed.', exc_info=True)
+
+            if exc.errno == errno.ENOKEY:
+                # No kerberos ticket
+                errmsg = (
+                    'Unable to perform DNS update because no kerberos ticket is available. '
+                    'See the DirectoryServiceBindAlert for more information. This alert will clear itself during the '
+                    'next check after directory services are healthy again.'
+                )
+            else:
+                errmsg = exc.errmsg
+
+            return Alert(DirectoryServiceDnsUpdateAlertClass, {'err', errmsg}, key=None)
+        except Exception as exc:
+            # If needed we can enhance this in the future to parse CallError
+            self.middleware.logger.warning('Periodic DNS update failed.', exc_info=True)
+            return Alert(DirectoryServiceDnsUpdateAlertClass, {'err', str(exc)}, key=None)
