529dc6af6e7f8d100a018e57b8347491fd11200c
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-09-11 15:46:20 +0500

    Account for the case where UI might already be using tnc cert

--- src/middlewared/middlewared/plugins/crypto_/certificates.py
+++ src/middlewared/middlewared/plugins/crypto_/certificates.py
@@ -70,11 +70,16 @@ class CertificateService(CRUDService):
         if cert:
             cert = cert[0]
             if cert['name'].startswith(TNC_CERT_PREFIX):
-                verrors.add(
-                    schema_name,
-                    f'Certificate "{cert["name"]}" is reserved for TrueNAS Connect service '
-                    'and cannot be used by other services'
-                )
+                # We have added an explicit check here to account for users who already
+                # were using TNC and had it configured for UI already as nginx would fail to
+                # configure SSL otherwise for them if we fail it here
+                ui_cert = (await self.middleware.call('system.general.config'))['ui_certificate']
+                if not ui_cert or (ui_cert and ui_cert['id'] != id_):
+                    verrors.add(
+                        schema_name,
+                        f'Certificate "{cert["name"]}" is reserved for TrueNAS Connect service '
+                        'and cannot be used by other services'
+                    )
 
             if cert['cert_type'] != 'CERTIFICATE' or cert['cert_type_CSR'] or cert['cert_type_CA']:
                 verrors.add(
