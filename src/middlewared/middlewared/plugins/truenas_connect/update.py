925f5a9b7f8b27df8c208173b9add2bc208ab61f
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-09-12 18:32:28 +0500

    Fix params

--- src/middlewared/middlewared/plugins/truenas_connect/update.py
+++ src/middlewared/middlewared/plugins/truenas_connect/update.py
@@ -279,7 +279,7 @@ class TrueNASConnectService(ConfigService, TNCAPIMixin):
         # We will not make this fatal in case user had it configured with some other plugin
         # before we had added validation to prevent users from doing that
         logger.debug('Deleting TNC certificate with id %d', cert_id)
-        delete_job = await self.middleware.call('certificate.delete', cert_id, {'force': True})
+        delete_job = await self.middleware.call('certificate.delete', cert_id, True)
         await delete_job.wait()
         if delete_job.error:
             logger.error('Failed to delete TNC certificate: %s', delete_job.error)
