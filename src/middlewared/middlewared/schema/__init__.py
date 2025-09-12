d4723d1bfbaaaeedabf3145f7811993910ab9567
Logan Cary <carylog9@gmail.com>
2025-09-12 19:34:35 -0400

    remove `Str`

--- src/middlewared/middlewared/schema/__init__.py
+++ src/middlewared/middlewared/schema/__init__.py
@@ -2,7 +2,6 @@ from middlewared.service_exception import ValidationErrors  # noqa
 
 from .adaptable_schemas import Any, Bool, OROperator, Ref  # noqa
 from .attribute import Attribute  # noqa
-from .convert_schema import convert_schema  # noqa
 from .dict_schema import Dict  # noqa
 from .enum import EnumMixin  # noqa
 from .exceptions import Error  # noqa
@@ -10,8 +9,5 @@ from .integer_schema import Float, Int, Timestamp  # noqa
 from .list_schema import List  # noqa
 from .plugin_schema import Schemas  # noqa
 from .resolvers import resolve_methods  # noqa
-from .string_schema import ( # noqa
-    Str
-)
 from .username import LocalUsername  # noqa
 from .utils import NOT_PROVIDED, REDACTED_VALUE  # noqa
