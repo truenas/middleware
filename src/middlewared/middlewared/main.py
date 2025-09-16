truenas/middleware

commit e4f4854591d47ddb4b3c4d65796c736ed6eb18d9

NAS-137550 / 26.04 / fix middleware start-up regression (#17201)
Middleware no longer starts up because of a change introduced in
fab779f609470489135746e4b9f1f19dcc4cdcc3.

src/middlewared/middlewared/main.py
@@ -913,10 +913,11 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
             # FIXME: Get rid of `create`/`do_create` duality
             methodobj = do_method
 
-        if new_style_returns_model is None:
-            new_style_returns_model = methodobj.new_style_returns
+        if new_style_returns_model is None and hasattr(methodobj, "new_style_returns"):
+            # anything decorated with @api_method gets this attribute
+            return serialize_result(new_style_returns_model, result, expose_secrets)
 
-        return serialize_result(new_style_returns_model, result, expose_secrets)
+        return result
 
     async def authorize_method_call(self, app, method_name, methodobj, params):
         if hasattr(methodobj, '_no_auth_required'):

commit 7769342db9de8b891c807791a1343c5b9f846039

fix typo (#17202)
A typo introduced in https://github.com/truenas/middleware/pull/17201.

src/middlewared/middlewared/main.py
@@ -915,7 +915,7 @@ class Middleware(LoadPluginsMixin, ServiceCallMixin):
 
         if new_style_returns_model is None and hasattr(methodobj, "new_style_returns"):
             # anything decorated with @api_method gets this attribute
-            return serialize_result(new_style_returns_model, result, expose_secrets)
+            return serialize_result(methodobj.new_style_returns, result, expose_secrets)
 
         return result
 