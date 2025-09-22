7fb302fb571ae307188fc28c63839b71b22f8ca5
Logan Cary <logan.cary@ixsystems.com>
2025-09-22 15:47:06 -0400

    use `middlewared.api.base.types.IPv4Address`
    (cherry picked from commit 77d6190ef6f0eb2bbea2c6b09406ad0bb745e412)

--- src/middlewared/middlewared/api/v25_10_0/ipmi_lan.py
+++ src/middlewared/middlewared/api/v25_10_0/ipmi_lan.py
@@ -1,9 +1,8 @@
-from ipaddress import IPv4Address
 from typing import Annotated, Literal
 
 from pydantic import AfterValidator, Field, Secret
 
-from middlewared.api.base import BaseModel, query_result, ForUpdateMetaclass
+from middlewared.api.base import BaseModel, IPv4Address, query_result, ForUpdateMetaclass
 from middlewared.api.base.validators import passwd_complexity_validator
 from .common import QueryFilters, QueryOptions
 
