45f8c6276ea20f616ab721d789b3f6c41034e601
M. Rehan <mrehanlm93@gmail.com>
2025-09-25 23:28:11 +0500

    Get cpu temperatures from sysfs

--- src/freenas/usr/libexec/netdata/python.d/cputemp.chart.py
+++ src/freenas/usr/libexec/netdata/python.d/cputemp.chart.py
@@ -1,11 +1,8 @@
-from bases.FrameworkServices.SimpleService import SimpleService
-from collections import defaultdict
 from copy import deepcopy
-from third_party import lm_sensors as sensors
 
-from middlewared.utils.cpu import amd_cpu_temperatures, cpu_info, generic_cpu_temperatures
+from bases.FrameworkServices.SimpleService import SimpleService
 
-CPU_TEMPERATURE_FEAT_TYPE = 2
+from middlewared.utils.cpu import get_cpu_temperatures
 
 
 ORDER = [
@@ -21,12 +18,6 @@ CHARTS = {
 }
 
 
-def cpu_temperatures(cpu_metrics):
-    if amd_metrics := cpu_metrics.get('k10temp-pci-00c3'):
-        return amd_cpu_temperatures(amd_metrics)
-    return generic_cpu_temperatures(cpu_metrics)
-
-
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
@@ -34,74 +25,9 @@ class Service(SimpleService):
         self.definitions = deepcopy(CHARTS)
 
     def get_data(self):
-        seen, data, cpu_data = dict(), dict(), defaultdict(dict)
-        try:
-            for chip in sensors.ChipIterator():
-                chip_name = sensors.chip_snprintf_name(chip)
-                seen[chip_name] = defaultdict(list)
-                if not any(chip_name.startswith(cpu_chip) for cpu_chip in ('k10temp-pci-00c3', 'coretemp-isa')):
-                    continue
-
-                cpu_d = {}
-                for feat in sensors.FeatureIterator(chip):
-                    if feat.type != CPU_TEMPERATURE_FEAT_TYPE:
-                        continue
-
-                    feat_name = str(feat.name.decode())
-                    feat_label = sensors.get_label(chip, feat)
-                    sub_feat = next(sensors.SubFeatureIterator(chip, feat))  # current value
-
-                    if not sub_feat:
-                        continue
-
-                    try:
-                        v = sensors.get_value(chip, sub_feat.number)
-                    except sensors.SensorsError:
-                        continue
-
-                    if v is None:
-                        continue
-
-                    cpu_d[f'{chip_name}_{feat_name}'] = {'name': feat_label, 'value': v}
-
-                cpu_data[chip_name] = cpu_d
-        except sensors.SensorsError as error:
-            self.error(error)
-
-        try:
-            cpu_temps = cpu_temperatures(cpu_data)
-        except Exception as error:
-            self.error(error)
-            cpu_temps = {}
-
-        data = {}
-        total_temp = 0
-        cinfo = cpu_info()
-        for core, temp in cpu_temps.items():
-            data[f'cpu{core}'] = temp
-            total_temp += temp
-            try:
-                # we follow the paradigm that htop uses
-                # for filling in the hyper-threaded ids
-                # temperatures. (i.e. we just copy the
-                # temp of the parent physical core id)
-                data[cinfo['ht_map'][f'cpu{core}']] = temp
-                total_temp += temp
-            except KeyError:
-                continue
-
-        if total_temp:
-            data['cpu'] = total_temp / len(data)
-
-        return data or ({f'cpu{i}': 0 for i in range(cinfo['core_count'])} | {'cpu': 0})
+        return get_cpu_temperatures()
 
     def check(self):
-        try:
-            sensors.init()
-        except sensors.SensorsError as error:
-            self.error(error)
-            return False
-
         data = self.get_data()
         for i in data:
             self.definitions['temperatures']['lines'].append([str(i)])
