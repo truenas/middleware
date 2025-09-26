7a9a439a3e69611177a7149a906e8d3f23f36ca2
David Sison <dsison3@gmail.com>
2025-09-26 21:30:01 +1000

    Update src/middlewared/middlewared/plugins/apps/schema_normalization.py
    Co-authored-by: Stavros Kois <47820033+stavros-k@users.noreply.github.com>
    (cherry picked from commit 8f3a284ea4d066e3a8d04e75d81c42034c6dc383)

--- src/middlewared/middlewared/plugins/apps/schema_normalization.py
+++ src/middlewared/middlewared/plugins/apps/schema_normalization.py
@@ -108,7 +108,6 @@ class AppSchemaService(Service):
 
         if all(gpu['vendor'] == 'NVIDIA' for gpu in gpu_choices.values()):
             value['use_all_gpus'] = False
-            value['kfd_device_exists'] = False
 
         for nvidia_gpu_pci_slot in list(value['nvidia_gpu_selection']):
             if nvidia_gpu_pci_slot not in gpu_choices or gpu_choices[nvidia_gpu_pci_slot]['vendor'] != 'NVIDIA':
