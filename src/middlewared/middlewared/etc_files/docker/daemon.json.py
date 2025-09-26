c24f5b54625c6cca9422853f03d4f3ebaeab8532
Stavros Kois <s.kois@outlook.com>
2025-09-26 13:12:29 +0300

    fix registry mirrors
    (cherry picked from commit 853ad5345dadfa9c514e9073f8875e3804b088de)

--- src/middlewared/middlewared/etc_files/docker/daemon.json.py
+++ src/middlewared/middlewared/etc_files/docker/daemon.json.py
@@ -28,7 +28,7 @@ def render(service, middleware):
         'storage-driver': 'overlay2',
         'fixed-cidr-v6': config['cidr_v6'],
         'default-address-pools': config['address_pools'],
-        'registry-mirrors': config['secure_registry_mirrors'],
+        'registry-mirrors': config['secure_registry_mirrors'] + config['insecure_registry_mirrors'],
         'insecure-registries': [urlparse(registry_url).netloc for registry_url in config['insecure_registry_mirrors']],
         **(
             {
