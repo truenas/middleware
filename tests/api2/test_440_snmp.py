b9fa45f6c9c098695e12e2b4540fd6c84ef036fe
caleb <yocalebo@gmail.com>
2025-10-03 07:55:32 -0400

    skip SNMP tests

--- tests/api2/test_440_snmp.py
+++ tests/api2/test_440_snmp.py
@@ -14,7 +14,7 @@ from middlewared.test.integration.utils.system import reset_systemd_svcs
 from auto_config import ha, interface, password, user, pool_name
 from functions import async_SSH_done, async_SSH_start
 
-pytest.mark.skip('snmp-agent is broken after upgrade to Trixie: NAS-137789')
+pytestmark = pytest.mark.skip('snmp-agent is broken after upgrade to Trixie: NAS-137789')
 
 skip_ha_tests = pytest.mark.skipif(not (ha and "virtual_ip" in os.environ), reason="Skip HA tests")
 COMMUNITY = 'public'
