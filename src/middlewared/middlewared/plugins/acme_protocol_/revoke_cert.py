f4a0b4f03c2fa5dea3c470796f4e4c1cddc7dac9
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-10-08 12:43:10 +0500

    Fix revocation of certs in trixie

--- src/middlewared/middlewared/plugins/acme_protocol_/revoke_cert.py
+++ src/middlewared/middlewared/plugins/acme_protocol_/revoke_cert.py
@@ -1,7 +1,5 @@
-from OpenSSL import crypto
-
-import josepy as jose
 from acme import errors, messages
+from cryptography import x509
 from truenas_acme_utils.client_utils import get_acme_client_and_key
 
 from middlewared.service import CallError, Service
@@ -16,8 +14,7 @@ class ACMEService(Service):
     def revoke_certificate(self, acme_client_key_payload, certificate):
         acme_client, key = get_acme_client_and_key(acme_client_key_payload)
         try:
-            acme_client.revoke(
-                jose.ComparableX509(crypto.load_certificate(crypto.FILETYPE_PEM, certificate)), 0
-            )
+            cert_obj = x509.load_pem_x509_certificate(certificate.encode())
+            acme_client.revoke(cert_obj, 0)
         except (errors.ClientError, messages.Error) as e:
             raise CallError(f'Failed to revoke certificate: {e}')
