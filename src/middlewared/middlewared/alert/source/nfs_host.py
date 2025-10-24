30ad6a4e5dd0329fabbbf5e72359f4d25c0fca45
Mark Grimes <126630154+mgrimesix@users.noreply.github.com>
2025-10-24 07:00:53 -0700

    NAS-138075 / 26.04 / Remove limitation on the number of NFS share host and/or network entries (#17436)
    Removed the limitation on the number of host and network entries that
    can be assigned to an NFS share.
    
    With this change the only limitation is the size of the command buffer.
    I've been able to test a summation of over 6000 of host and network
    entries. At this extreme we experience command timeout (takes over 1 min
    to process). This was using only IP addresses. The setting _does_ take
    (despite timeout). Clients can mount and the NFS server on TrueNAS is
    functional.
    
    I expect using host names (instead of IP addresses) may introduce other
    areas of slowness due to the name lookups. This is still under test to
    quantify the impact.
    
    The hard limit has been replaced with an alert. If there are 100 or more
    entries in either list an alert is generated. Alerts are automatically
    cleared if the list is reduced to under 100 or if the share is deleted.
    
    CI tests have been updated/created.

--- src/middlewared/middlewared/alert/source/nfs_host.py
+++ src/middlewared/middlewared/alert/source/nfs_host.py
@@ -9,3 +9,35 @@ class NFSHostnameLookupFailAlertClass(AlertClass, SimpleOneShotAlertClass):
 
     async def delete(self, alerts, query):
         return []
+
+
+class NFSHostListExcessiveAlertClass(AlertClass, SimpleOneShotAlertClass):
+    category = AlertCategory.SHARING
+    level = AlertLevel.WARNING
+    title = "NFS host list excessively long"
+    text = (
+        "The NFS share, %(sharePath)s, has %(numEntries)d host entries. "
+        "An excessively long list of host entries may lead to "
+        "unexpected behavior, performance issues or failures. "
+        "If possible, please consider using a directory service, netgroups, "
+        "network ranges, or similar to reduce the length of the host list."
+    )
+
+    async def delete(self, alerts, query):
+        return []
+
+
+class NFSNetworkListExcessiveAlertClass(AlertClass, SimpleOneShotAlertClass):
+    category = AlertCategory.SHARING
+    level = AlertLevel.WARNING
+    title = "NFS network list excessively long"
+    text = (
+        "The NFS share, %(sharePath)s, has %(numEntries)d network entries. "
+        "An excessively long list of network entries may lead to "
+        "unexpected behavior, performance issues or failures. "
+        "If possible, please consider using a directory service, netgroups, "
+        "wider network ranges, or similar to reduce the length of the network list."
+    )
+
+    async def delete(self, alerts, query):
+        return []
