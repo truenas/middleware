truenas/middleware

commit 7512f4d4e1484a4f6df1034b8c7cc08bc0c4aefb

NAS-138164 / 26.04 / mount in pool.dataset.create_impl (#17435)
This only affects internal callers that call pool.dataset.create_impl
directly. Just need to move the mount call inside it.

src/middlewared/middlewared/plugins/pool_/dataset.py
@@ -409,6 +409,11 @@ class PoolDatasetService(CRUDService):
         except Exception as e:
             raise CallError(f"Failed to create dataset {kwargs['name']}: {e}")
 
+        self.middleware.call_sync(
+            'zfs.resource.mount',
+            MountArgs(filesystem=data['name'], recursive=args.create_ancestors)
+        )
+
     @api_method(
         PoolDatasetCreateArgs,
         PoolDatasetCreateResult,
@@ -656,8 +661,6 @@ class PoolDatasetService(CRUDService):
         await self.middleware.call_hook('dataset.post_create', {'encrypted': bool(encryption_dict), **dataset_data})
 
         data['id'] = data['name']
-
-        await self.middleware.call('zfs.resource.mount', MountArgs(filesystem=data['name']))
 
         created_ds = await self.get_instance(data['id'])
 

commit 96d1c595c0b11431353a861fe2c398700ca75728

NAS-138180 / 26.04 / fix system dataset setup (#17438)
7512f4d4e1 introduced a regression whereby filesystems with legacy
mountpoint property was being incorrectly handled. One of those
filesystems is the system dataset. This corrects the problem.

src/middlewared/middlewared/plugins/pool_/dataset.py
@@ -409,10 +409,14 @@ class PoolDatasetService(CRUDService):
         except Exception as e:
             raise CallError(f"Failed to create dataset {kwargs['name']}: {e}")
 
-        self.middleware.call_sync(
-            'zfs.resource.mount',
-            MountArgs(filesystem=data['name'], recursive=args.create_ancestors)
-        )
+        margs = MountArgs(filesystem=args.name, recursive=args.create_ancestors)
+        if 'mountpoint' in args.zprops:
+            if args.zprops['mountpoint'] == 'legacy':
+                return
+            elif os.path.isabs(args.zprops['mountpoint']):
+                margs['mountpoint'] = args.zprops['mountpoint']
+
+        self.middleware.call_sync('zfs.resource.mount', margs)
 
     @api_method(
         PoolDatasetCreateArgs,

src/middlewared/middlewared/plugins/zfs/name_utils.py
@@ -1,0 +1,36 @@
+import string
+
+ALLOWED = string.ascii_letters + string.digits + u'-_.: '
+MAXNAMELEN = 255
+
+
+def is_valid_name_component(component):
+    for _char in component:
+        if _char not in ALLOWED:
+            return False
+    return True
+
+
+def is_valid_fs_name(name):
+    for i in name.split('/'):
+        if not is_valid_name_component(i):
+            return False
+    return True
+
+
+def is_valid_snap_name(name):
+    parts = name.split('@')
+    return (
+        len(parts) == 2
+        and is_valid_fs_name(parts[0])
+        and is_valid_name_component(parts[1])
+    )
+
+
+def is_valid_bmark_name(name):
+    parts = name.split('#')
+    return (
+        len(parts) == 2
+        and is_valid_fs_name(parts[0])
+        and is_valid_name_component(parts[1])
+    )