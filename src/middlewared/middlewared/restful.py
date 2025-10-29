bb7537b9a5d9d861a9062d11176b14ad86d97ad1
Logan Cary <logan.cary@ixsystems.com>
2025-10-29 16:26:32 -0400

    remove all REST except resttest

--- src/middlewared/middlewared/restful.py
+++ src/middlewared/middlewared/restful.py
@@ -7,6 +7,7 @@ import errno
 import pam
 import traceback
 import types
+import typing
 import urllib.parse
 
 from aiohttp import web
@@ -24,6 +25,8 @@ from .service_exception import adapt_exception, CallError, MatchNotFound, Valida
 from .utils.account.authenticator import ApiKeyPamAuthenticator, UnixPamAuthenticator, UserPamAuthenticator
 from .utils.auth import AA_LEVEL1, CURRENT_AAL
 from .utils.origin import ConnectionOrigin
+if typing.TYPE_CHECKING:
+    from middlewared.main import Middleware
 
 
 def parse_credentials(request):
@@ -34,7 +37,7 @@ def parse_credentials(request):
             return {
                 'credentials': 'TOKEN',
                 'credentials_data': {
-                    'token': qs.get('auth_token')[0],
+                    'token': qs['auth_token'][0],
                 },
             }
         else:
@@ -75,7 +78,6 @@ def parse_credentials(request):
 
 
 async def authenticate(app, middleware, request, credentials, method, resource):
-
     if credentials['credentials'] == 'TOKEN':
         origin = await middleware.run_in_thread(ConnectionOrigin.create, request)
         # We are using the UnixPamAuthenticator here because we are generating a
@@ -153,356 +155,52 @@ class Application(App):
         self.rest = True
 
 
-class RESTfulAPI(object):
+class RESTfulAPI:
 
-    def __init__(self, middleware, app):
+    def __init__(self, middleware: 'Middleware', app):
         self.middleware = middleware
         self.app = app
-
         # Keep methods cached for future lookups
-        self._methods = {}
-        self._methods_by_service = defaultdict(dict)
-
-        self._openapi = OpenAPIResource(self)
-
-    def get_app(self):
-        return self.app
+        self.methods = {}
 
     async def register_resources(self):
-        for methodname, method in list((await self.middleware.call('core.get_methods', None, 'REST')).items()):
-            self._methods[methodname] = method
-            self._methods_by_service[methodname.rsplit('.', 1)[0]][methodname] = method
+        resttest_service = self.middleware.get_service('resttest')
+        service_resource = Resource(self, self.middleware, 'resttest', resttest_service._config)
 
-        for name, service in list((await self.middleware.call('core.get_services', 'REST')).items()):
-            openapi = True
-            if name == 'resttest':
-                openapi = False
+        for methodname, method in list((await self.middleware.call('core.get_methods', 'resttest', 'REST')).items()):
+            self.methods[methodname] = method
 
-            kwargs = {}
-            blacklist_methods = []
-            """
-            Hook up methods for the resource entrypoint.
-            For CRUD:
-              - GET -> $name.query
-              - POST - $name.create
-            For Config:
-              - GET -> $name.config
-              - PUT -> $name.update
-            """
-            if service['type'] == 'crud':
-                kwargs['get'] = '{}.query'.format(name)
-                post = f'{name}.create'
-                if post in self._methods:
-                    kwargs['post'] = '{}.create'.format(name)
-                blacklist_methods.extend(list(kwargs.values()))
-            elif service['type'] == 'config':
-                kwargs['get'] = '{}.config'.format(name)
-                put = '{}.update'.format(name)
-                if put in self._methods:
-                    kwargs['put'] = put
-                blacklist_methods.extend(list(kwargs.values()))
+            if method['require_websocket']:
+                continue
 
-            service_resource = Resource(self, self.middleware, name.replace('.', '/'), service['config'], openapi,
-                                        **kwargs)
+            short_methodname = methodname.rsplit('.', 1)[-1]
 
-            """
-            For CRUD services we also need a direct subresource so we can
-            operate on items in the entity, e.g. update or delete "john" of user namespace.
-            """
-            subresource = None
-            if service['type'] == 'crud':
-                kwargs = {}
-                get = f'{name}.query'
-                if get in self._methods:
-                    kwargs['get'] = get
-                delete = f'{name}.delete'
-                if delete in self._methods:
-                    kwargs['delete'] = delete
-                put = f'{name}.update'
-                if put in self._methods:
-                    kwargs['put'] = put
-                blacklist_methods.extend(list(kwargs.values()))
-                subresource = Resource(
-                    self, self.middleware, 'id/{id_}', service['config'], openapi,
-                    parent=service_resource, **kwargs,
-                )
-
-            for methodname, method in list(self._methods_by_service[name].items()):
-                if methodname in blacklist_methods:
-                    continue
-                if method['require_websocket']:
-                    continue
-                short_methodname = methodname.rsplit('.', 1)[-1]
-                if method.get('item_method') is True:
-                    parent = subresource
-                else:
-                    parent = service_resource
-
-                res_kwargs = {}
-                """
-                Methods with not empty accepts list and not filterable
-                are treated as POST HTTP methods.
-                """
-                if (method['accepts'] and not method['filterable']) or method['uploadable']:
-                    res_kwargs['post'] = methodname
-                else:
-                    res_kwargs['get'] = methodname
-
-                Resource(self, self.middleware, short_methodname, service['config'], openapi,
-                         parent=parent, **res_kwargs)
-
-            await asyncio.sleep(0)  # Force context switch
-
-
-class OpenAPIResource(object):
-
-    def __init__(self, rest):
-        self.rest = rest
-        self.rest.app.router.add_route('GET', '/api/v2.0', self.get)
-        self.rest.app.router.add_route('GET', '/api/v2.0/', self.get)
-        self.rest.app.router.add_route('GET', '/api/v2.0/openapi.json', self.get)
-        self._paths = defaultdict(dict)
-        self._schemas = dict()
-        self._components = defaultdict(dict)
-        self._components['schemas'] = self._schemas
-        self._components['responses'] = {
-            'NotFound': {
-                'description': 'Endpoint not found',
-            },
-            'Unauthorized': {
-                'description': 'No authorization for this endpoint',
-            },
-            'Success': {
-                'description': 'Operation succeeded',
-            },
-        }
-        self._components['securitySchemes'] = {
-            'basic': {
-                'type': 'http',
-                'scheme': 'basic'
-            },
-        }
-
-    def add_path(self, path, operation, methodname, service_config):
-        assert operation in ('get', 'post', 'put', 'delete')
-        opobject = {
-            'tags': [methodname.rsplit('.', 1)[0]],
-            'responses': {
-                '200': {'$ref': '#/components/responses/Success'},
-                '401': {'$ref': '#/components/responses/Unauthorized'},
-            },
-            'parameters': [],
-        }
-        method = self.rest._methods.get(methodname)
-        if method:
-            desc = method['description'] or ''
-            if method['downloadable']:
-                if method['check_pipes']:
-                    desc += '\n\nA file will be downloaded from this endpoint.'
-                else:
-                    desc += (
-                        '\n\nA file might be downloaded from this endpoint. Please specify `?download=0` to fetch a '
-                        'method call result instead.'
-                    )
-            if method['uploadable']:
-                if method['check_pipes']:
-                    desc += '\n\nA file must be uploaded to this endpoint. '
-                else:
-                    desc += (
-                        '\n\nA file might be uploaded to this endpoint. '
-                    )
-
-                desc += (
-                    'To upload a file, please send a multipart request with two parts. The first, named `data`, should '
-                    'contain a JSON-encoded payload, and the second, named `file`, should contain an uploaded file.'
-                )
-
-            opobject['description'] = desc
-
-            accepts = method.get('accepts')
-            if method['filterable']:
-                opobject['parameters'] += [
-                    {
-                        'name': 'limit',
-                        'in': 'query',
-                        'required': False,
-                        'schema': {'type': 'integer'},
-                    },
-                    {
-                        'name': 'offset',
-                        'in': 'query',
-                        'required': False,
-                        'schema': {'type': 'integer'},
-                    },
-                    {
-                        'name': 'count',
-                        'in': 'query',
-                        'required': False,
-                        'schema': {'type': 'boolean'},
-                    },
-                    {
-                        'name': 'sort',
-                        'in': 'query',
-                        'required': False,
-                        'schema': {'type': 'string'},
-                    },
-                ] if '{id_}' not in path else []
-                desc = f'{desc}\n\n' if desc else ''
-                opobject['description'] = desc + '`query-options.extra` can be specified as query parameters with ' \
-                                                 'prefixing them with `extra.` prefix. For example, ' \
-                                                 '`extra.retrieve_properties=false` will pass `retrieve_properties` ' \
-                                                 'as an extra argument to pool/dataset endpoint.'
-            elif accepts and not (operation == 'delete' and method['item_method'] and len(accepts) == 1) and (
-                not method['filterable']
-            ):
-                opobject['requestBody'] = self._accepts_to_request(methodname, method, accepts)
-
-            # For now we only accept `id_` as an url parameters
-            if '{id_}' in path:
-                opobject['parameters'].append({
-                    'name': 'id_',
-                    'in': 'path',
-                    'required': True,
-                    'schema': {'type': service_config['datastore_primary_key_type']},
-                })
-            method_returns = method.get('returns') or []
-            if method_returns:
-                opobject['responses']['200'] = self._returns_to_request(methodname, method_returns)
-
-        self._paths[f'/{path}'][operation] = opobject
-
-    def _convert_schema(self, schema):
-        """
-        Convert JSON Schema to OpenAPI Schema
-        """
-        schema = copy.deepcopy(schema)
-        _type = schema.get('type')
-        schema.pop('_required_', None)
-        if isinstance(_type, list):
-            if 'null' in _type:
-                _type.remove('null')
-                schema['nullable'] = True
-            schema['type'] = _type = _type[0]
-        if _type == 'object':
-            for key, val in schema.get('properties', {}).items():
-                schema['properties'][key] = self._convert_schema(val)
-        elif _type == 'array':
-            items = schema.get('items')
-            if isinstance(items, list):
-                for i, item in enumerate(list(items)):
-                    if item.get('type') == 'null':
-                        items.remove(item)
-                if len(items) > 1:
-                    schema['items'] = {'oneOf': items}
-                elif len(items) > 0:
-                    schema['items'] = items[0]
-                else:
-                    schema['items'] = {}
-        return schema
-
-    def _returns_to_request(self, methodname, method_returns):
-        method_name = f'return_schema_of_{methodname.replace(".", "_")}'
-
-        for schema in method_returns:
-            self._schemas[method_name] = self._convert_schema(schema)
-
-        json_request = {'schema': {'$ref': f'#/components/schemas/{method_name}'}}
-
-        return {
-            'description': 'Response schema:',
-            'content': {
-                'application/json': json_request,
-            }
-        }
-
-    def _accepts_to_request(self, methodname, method, schemas):
-        # Create an unique ID for every argument and register the schema
-        methodname = methodname.replace(".", "_")
-        if len(schemas) == 1 and not method["item_method"]:
-            self._schemas[methodname] = self._convert_schema(schemas[0])
-        elif len(schemas) == 2 and method["item_method"]:
-            # In this case, we ignore the first schema
-            self._schemas[methodname] = self._convert_schema(schemas[1])
-        else:
-            # If the method accepts multiple arguments lets emulate/create
-            # a new schema, which is a object containing every argument as an
-            # attribute.
-            props = {}
-            for i, schema in enumerate(schemas):
-                if i == 0 and method['item_method']:
-                    continue
-                unique_id = f'{methodname}_{i}'
-                self._schemas[unique_id] = self._convert_schema(schema)
-                props[schema['title']] = {'$ref': f'#/components/schemas/{unique_id}'}
-            new_schema = {
-                'type': 'object',
-                'properties': props
-            }
-            self._schemas[methodname] = new_schema
-
-        json_request = {'schema': {'$ref': f'#/components/schemas/{methodname}'}}
-        for i, example in enumerate(method['examples']['rest']):
-            try:
-                title, example = example.split('{', 1)
-                example = json.loads('{' + example.strip())
-            except ValueError:
-                pass
+            if method.get('item_method') is True:
+                parent = None
             else:
-                json_request.setdefault('examples', {})
-                json_request['examples'][f'example_{i + 1}'] = {'summary': title.strip(), 'value': example}
+                parent = service_resource
+            
+            # Methods with not empty accepts list and not filterable are treated as POST HTTP methods.
+            if (method['accepts'] and not method['filterable']) or method['uploadable']:
+                res_kwargs = {'post': methodname}
+            else:
+                res_kwargs = {'get': methodname}
 
-        return {
-            'content': {
-                'application/json': json_request,
-            }
-        }
+            Resource(
+                self, self.middleware, short_methodname, resttest_service._config, parent=parent, **res_kwargs
+            )
 
-    def get(self, req, **kwargs):
-
-        servers = []
-        host = req.headers.get('Host')
-        scheme = req.headers.get('X-Scheme') or req.scheme
-        port = int(req.headers.get('X-Server-Port') or 80)
-        if host:
-            # This condition is only cosmetic to avoid specifying 80/443 in the uri
-            if port not in [80, 443]:
-                host = f'{host}:{port}'
-            servers.append({
-                'url': f'{scheme}://{host}/api/v2.0',
-            })
-
-        result = {
-            'openapi': '3.0.0',
-            'info': {
-                'title': 'TrueNAS RESTful API',
-                'version': 'v2.0',
-            },
-            'paths': self._paths,
-            'servers': servers,
-            'components': self._components,
-            'security': [{'basic': []}],
-        }
-
-        resp = web.Response()
-        resp.headers['Content-type'] = 'application/json'
-        resp.text = json.dumps(result, indent=True)
-        return resp
+        await asyncio.sleep(0)  # Force context switch
 
 
 class Resource(object):
 
     name = None
     parent = None
-
-    delete = None
     get = None
     post = None
-    put = None
 
-    def __init__(
-        self, rest, middleware, name, service_config, openapi, parent=None,
-        delete=None, get=None, post=None, put=None,
-    ):
+    def __init__(self, rest, middleware, name, service_config, parent=None, get=None, post=None):
         self.rest = rest
         self.middleware = middleware
         self.name = name
@@ -511,23 +209,17 @@ class Resource(object):
         self.__method_params = {}
 
         path = self.get_path()
-        if delete:
-            self.delete = delete
         if get:
             self.get = get
         if post:
             self.post = post
-        if put:
-            self.put = put
 
-        for i in ('delete', 'get', 'post', 'put'):
+        for i in ('get', 'post'):
             operation = getattr(self, i)
             if operation is None:
                 continue
             self.rest.app.router.add_route(i.upper(), f'/api/v2.0/{path}', getattr(self, f'on_{i}'))
             self.rest.app.router.add_route(i.upper(), f'/api/v2.0/{path}/', getattr(self, f'on_{i}'))
-            if openapi:
-                self.rest._openapi.add_path(path, i, operation, self.service_config)
             self.__map_method_params(operation)
 
         self.middleware.logger.trace(f"add route {self.get_path()}")
@@ -538,7 +230,7 @@ class Resource(object):
         schema of object type.
         For that reason we need to keep track of each parameter and its order
         """
-        method = self.rest._methods.get(method_name)
+        method = self.rest.methods.get(method_name)
         if not method:
             return
         accepts = method.get('accepts')
@@ -574,7 +266,7 @@ class Resource(object):
                     resource = None
 
                 app = await create_application(req)
-                auth_required = not self.rest._methods[getattr(self, method)]['no_auth_required']
+                auth_required = not self.rest.methods[getattr(self, method)]['no_auth_required']
                 credentials = parse_credentials(req)
                 if credentials is None:
                     if auth_required:
@@ -696,7 +388,7 @@ class Resource(object):
         assert http_method in ('delete', 'get', 'post', 'put')
 
         methodname = getattr(self, http_method)
-        method = self.rest._methods[methodname]
+        method = self.rest.methods[methodname]
 
         method_kwargs = {}
         method_kwargs['app'] = app
