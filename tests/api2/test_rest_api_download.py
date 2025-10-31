eeb05d8ca1260ce421924a892c9867751fa53cb2
Logan Cary <logan.cary@ixsystems.com>
2025-10-31 10:32:49 -0400

    remove REST tests

--- tests/api2/test_rest_api_download.py
+++ tests/api2/test_rest_api_download.py
@@ -6,45 +6,7 @@ import requests
 
 from middlewared.service_exception import CallError
 from middlewared.test.integration.assets.account import unprivileged_user
-from middlewared.test.integration.utils import call, client, session, url
-
-
-@pytest.mark.parametrize("method", ["test_download_pipe", "test_download_unchecked_pipe"])
-def test_download(method):
-    with session() as s:
-        r = s.post(
-            f"{url()}/api/v2.0/resttest/{method}",
-            headers={"Content-type": "application/json"},
-            data="{\"key\": \"value\"}",
-        )
-        r.raise_for_status()
-        assert r.headers["Content-Type"] == "application/octet-stream"
-        assert r.text == '{"key": "value"}'
-
-
-def test_no_download_from_checked_pipe():
-    with session() as s:
-        r = s.post(
-            f"{url()}/api/v2.0/resttest/test_download_pipe?download=0",
-            headers={"Content-type": "application/json"},
-            data="{\"key\": \"value\"}",
-        )
-
-        assert r.status_code == 400
-        assert r.json()["message"] == "JSON response is not supported for this method."
-
-
-def test_no_download_from_unchecked_pipe():
-    with session() as s:
-        r = s.post(
-            f"{url()}/api/v2.0/resttest/test_download_unchecked_pipe?download=0",
-            headers={"Content-type": "application/json"},
-            data="{\"key\": \"value\"}",
-        )
-        r.raise_for_status()
-
-        assert r.headers["Content-Type"].startswith("application/json")
-        assert r.json() == {"wrapped": {"key": "value"}}
+from middlewared.test.integration.utils import call, client, url
 
 
 def test_download_from_download_endpoint():
