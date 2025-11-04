10e22eaa9edcee3a1a20965f885abef79fe3d8fb
Mark Grimes <mark.grimes@ixsystems.com>
2025-11-04 09:13:20 -0800

    Fix daemon.log marker messages.
    (cherry picked from commit 40933a05e0e1127cd9831c5720ccfb0929b94e31)

--- tests/api2/test_300_nfs.py
+++ tests/api2/test_300_nfs.py
@@ -1150,14 +1150,14 @@ class TestNFSops:
             syslog_tail = async_SSH_start("tail -n 0 -F /var/log/syslog", user, password, truenas_server.ip)
             daemon_tail = async_SSH_start("tail -n 0 -F /var/log/daemon.log", user, password, truenas_server.ip)
             ssh('logger "====== START_NFS_LOGGING_FILTER_TEST - expect mount messages ======"')
-            ssh('logger daemon "====== START_NFS_LOGGING_FILTER_TEST - expect mount messages ======"')
+            ssh('logger -p daemon.notice "====== START_NFS_LOGGING_FILTER_TEST - expect mount messages ======"')
 
             with SSH_NFS(truenas_server.ip, NFS_PATH, vers=4,
                          user=user, password=password, ip=truenas_server.ip) as n:
                 n.ls('/')
 
             ssh('logger "====== END_NFS_SYSLOG_FILTER_TEST - expect mount messages ======"')
-            ssh('logger daemon "====== END_NFS_SYSLOG_FILTER_TEST - expect mount messages ======"')
+            ssh('logger -p daemon.notice "====== END_NFS_SYSLOG_FILTER_TEST - expect mount messages ======"')
             syslog_data, errs = async_SSH_done(syslog_tail, 5)    # 5 second timeout
             daemon_data, errs = async_SSH_done(daemon_tail, 5)    # 5 second timeout
 
@@ -1173,14 +1173,14 @@ class TestNFSops:
             syslog_tail = async_SSH_start("tail -n 0 -F /var/log/syslog", user, password, truenas_server.ip)
             daemon_tail = async_SSH_start("tail -n 0 -F /var/log/daemon.log", user, password, truenas_server.ip)
             ssh('logger "====== START_NFS_LOGGING_FILTER_TEST - no mountd messages ======"')
-            ssh('logger daemon "====== START_NFS_LOGGING_FILTER_TEST - no mountd messages ======"')
+            ssh('logger -p daemon.notice "====== START_NFS_LOGGING_FILTER_TEST - no mountd messages ======"')
 
             with SSH_NFS(truenas_server.ip, NFS_PATH, vers=4,
                          user=user, password=password, ip=truenas_server.ip) as n:
                 n.ls('/')
 
             ssh('logger "====== END_NFS_SYSLOG_FILTER_TEST - no mountd messages ======"')
-            ssh('logger daemon "====== END_NFS_SYSLOG_FILTER_TEST - no mountd messages ======"')
+            ssh('logger -p daemon.notice "====== END_NFS_SYSLOG_FILTER_TEST - no mountd messages ======"')
             syslog_data, errs = async_SSH_done(syslog_tail, 5)    # 5 second timeout
             daemon_data, errs = async_SSH_done(daemon_tail, 5)    # 5 second timeout
 
