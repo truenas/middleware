9d7531d174aa993ebd4139d11d6e1e58abd753a2
Andrew Walker <awalker@ixsystems.com>
2025-11-05 11:27:52 -0600

    NAS-138389 / 26.04 / Fix tests for audit backend changes (#17526)
    This commit fixes a variety of CI tests that are now failing because the
    `limit` query-option is now mandatory for audit.query
    
    (cherry picked from commit e13ba694d473541312156ee527654cda96bc6c03)

--- tests/api2/test_audit_sudo.py
+++ tests/api2/test_audit_sudo.py
@@ -34,6 +34,8 @@ def user_sudo_events(username, count=False):
     }
     if count:
         payload['query-options'] = {'count': True}
+    else:
+        payload['query-options'] = {'limit': 10000}
     return call('audit.query', payload)
 
 
