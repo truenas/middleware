56f1cc3ce4ebf8e9e5b9236b2606a1a1b53e9aa5
Andrew Walker <awalker@ixsystems.com>
2025-11-07 14:32:11 -0600

    NAS-138447 / 26.04 / Fix more failing audit tests (#17565)
    * change ordering of audit messages for select as test to grab most
    recent. The first audit message for a new truenas install in our VMs is
    typically an unauthenticated call and so credenitals is None
    
    * add a sleep for the system audit test since it can take some time for
    an audit message to flush through syslog-ng into the audit database
    
    * fix issue with audit export formatting of query-options
    
    * fix issue with legacy API test not properly limiting audit.query
    result size
    
    (cherry picked from commit 3ad26fff44e2ee79261f9e269deccac8d44a61d9)

--- src/middlewared/middlewared/plugins/audit/utils.py
+++ src/middlewared/middlewared/plugins/audit/utils.py
@@ -146,7 +146,7 @@ def parse_query_filters(filters: list) -> list:
 def parse_query_options(options: dict) -> dict:
     out = options.copy()
     out['select'] = query_select_json_path_parse(options.get('select', []))
-    if out['get']:
+    if out.get('get'):
         # If we're only getting a single result then we can place an SQL
         # LIMIT for a single result in the statement we generate
         out['limit'] = 1
