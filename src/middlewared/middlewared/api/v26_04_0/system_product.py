535728fee3961412679c477adb5a935560d30e2a
sonicaj <waqarsonic1@gmail.com>
2025-11-10 21:47:55 +0500

    Revert "NAS-138483 / 26.04 / Expose SED feature flag in license (#17582)"
    This reverts commit f8b6faa921184c7555eb1f0edd5c965e48e1f606.

--- src/middlewared/middlewared/api/v26_04_0/system_product.py
+++ src/middlewared/middlewared/api/v26_04_0/system_product.py
@@ -20,7 +20,7 @@ __all__ = (
 
 
 class SystemFeatureEnabledArgs(BaseModel):
-    feature: Literal["DEDUP", "FIBRECHANNEL", "VM", "SED"]
+    feature: Literal["DEDUP", "FIBRECHANNEL", "VM"]
     """Feature to check for availability on this system."""
 
 
