1149eea171676a8e60d87e0b196dfcac84ca1f97
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-11-11 11:06:24 +0500

    Add logic to validate if all disks are SED based or not

--- src/middlewared/middlewared/plugins/disk_/sed.py
+++ src/middlewared/middlewared/plugins/disk_/sed.py
@@ -37,7 +37,7 @@ class DiskService(Service):
         return False, disk['name']
 
     @private
-    async def setup_sed_disks_for_pool(self, disks, schema_name):
+    async def setup_sed_disks_for_pool(self, disks, schema_name, validate_all_disks_are_sed=False):
         # This will be called during pool create/update when topology is being manipulated
         # we will be doing some extra steps for SED based disks here
         # What we would like to do at this point would be to see if we have SED based disks and if yes,
@@ -47,18 +47,31 @@ class DiskService(Service):
         # 3) If any of them are uninitialized, try to initialize them using global sed pass
         # 4) If anything fails in 2/3, let's raise an appropriate error
         verrors = ValidationErrors()
-        sed_disks = await self.middleware.call(
-            'disk.query', [['name', 'in', list(disks)], ['sed', '=', True]], {
+        filters = [['name', 'in', list(disks)]]
+        if validate_all_disks_are_sed is False:
+            filters.append(['sed', '=', True])
+
+        disks_to_check = await self.middleware.call(
+            'disk.query', filters, {
                 'extra': {'sed_status': True, 'passwords': True, 'real_names': True}, 'force_sql_filters': True
             }
         )
         to_setup_sed_disks = []
         failed_sed_status_disks = []
-        for sed_disk in sed_disks:
-            if sed_disk['sed_status'] in [SEDStatus.UNINITIALIZED, SEDStatus.LOCKED]:
-                to_setup_sed_disks.append(sed_disk)
-            elif sed_disk['sed_status'] == SEDStatus.FAILED:
-                failed_sed_status_disks.append(sed_disk['name'])
+        non_sed_disks = []
+        for disk in disks_to_check:
+            if validate_all_disks_are_sed and disk['sed'] is False:
+                non_sed_disks.append(disk['name'])
+            if disk['sed_status'] in [SEDStatus.UNINITIALIZED, SEDStatus.LOCKED]:
+                to_setup_sed_disks.append(disk)
+            elif disk['sed_status'] == SEDStatus.FAILED:
+                failed_sed_status_disks.append(disk['name'])
+
+        if non_sed_disks:
+            verrors.add(
+                schema_name,
+                f'Following disk(s) are not SED: {", ".join(non_sed_disks)!r}',
+            )
 
         if failed_sed_status_disks:
             # Ideally shouldn't happen but if it does, let's add a validation error and raise, no point in going
@@ -67,7 +80,8 @@ class DiskService(Service):
                 schema_name,
                 f'Failed to query status of {", ".join(failed_sed_status_disks)!r} SED disk(s).'
             )
-            verrors.check()
+
+        verrors.check()
 
         if to_setup_sed_disks:
             global_sed_password = await self.middleware.call('system.advanced.sed_global_password')
