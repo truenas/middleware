99aedb26a2563a9fef0b6403d6e2ccf7ddf137bb
Mark Grimes <126630154+mgrimesix@users.noreply.github.com>
2025-11-20 23:00:28 -0800

    NAS-138566 / 26.04 / Add auditing of static route CRUD. (#17673)
    The static route CRUD routines were missing auditing. This is a STIG
    requirement.
    
    Update ci test_staticroutes.py to include audit testing.
    
    Manually ran `test_staticroutes.py` to confirm passing condition.
    With #17669 this is safe to backport to 25.10.1 .

--- src/middlewared/middlewared/plugins/network_/static_routes.py
+++ src/middlewared/middlewared/plugins/network_/static_routes.py
@@ -34,7 +34,11 @@ class StaticRouteService(CRUDService):
         entry = StaticRouteEntry
         role_prefix = 'NETWORK_INTERFACE'
 
-    @api_method(StaticRouteCreateArgs, StaticRouteCreateResult)
+    @api_method(
+        StaticRouteCreateArgs,
+        StaticRouteCreateResult,
+        audit='Static route create'
+    )
     async def do_create(self, data):
         """
         Create a Static Route.
@@ -55,7 +59,11 @@ class StaticRouteService(CRUDService):
 
         return await self.get_instance(id_)
 
-    @api_method(StaticRouteUpdateArgs, StaticRouteUpdateResult)
+    @api_method(
+        StaticRouteUpdateArgs,
+        StaticRouteUpdateResult,
+        audit='Static route update'
+    )
     async def do_update(self, id_, data):
         """
         Update Static Route of `id`.
@@ -76,7 +84,11 @@ class StaticRouteService(CRUDService):
 
         return await self.get_instance(id_)
 
-    @api_method(StaticRouteDeleteArgs, StaticRouteDeleteResult)
+    @api_method(
+        StaticRouteDeleteArgs,
+        StaticRouteDeleteResult,
+        audit='Static route delete'
+    )
     def do_delete(self, id_):
         """
         Delete Static Route of `id`.
