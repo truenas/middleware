5e1fa0d7d6e23d3b2f0c13216ec3dd99fe5d5271
themylogin <themylogin@gmail.com>
2025-11-23 23:19:46 +0100

    `defer` delete snapshots

--- src/middlewared/middlewared/plugins/pool_/snapshot.py
+++ src/middlewared/middlewared/plugins/pool_/snapshot.py
@@ -112,6 +112,7 @@ class PoolSnapshotService(CRUDService):
             DestroyArgs(
                 path=id_,
                 recursive=options['recursive'],
+                defer=options['defer'],
             )
         )
 
