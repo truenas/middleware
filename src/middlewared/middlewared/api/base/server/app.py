a5d57d027f3198a7aad9429c6bf035c5c9a5db5e
Logan Cary <logan.cary@ixsystems.com>
2025-11-24 10:02:12 -0500

    make app.authenticated a property

--- src/middlewared/middlewared/api/base/server/app.py
+++ src/middlewared/middlewared/api/base/server/app.py
@@ -11,10 +11,13 @@ class App:
     def __init__(self, origin: ConnectionOrigin):
         self.origin = origin
         self.session_id = str(ssl_uuid4())
-        self.authenticated = False
         self.authentication_context = AuthenticationContext()
         self.authenticated_credentials: SessionManagerCredentials | None = None
         self.legacy_jobs = True
         self.private_methods = False
         self.py_exceptions = False
         self.websocket = False
+
+    @property
+    def authenticated(self) -> bool:
+        return self.authenticated_credentials is not None
