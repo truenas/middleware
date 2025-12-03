4adb0dfd86a67bc4210eceef91ac7d1a507e5ecc
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-12-03 12:54:55 +0500

    Call endpoint in data migration so sed attr gets reflect properly

--- src/middlewared/middlewared/migration/0015_flag_all_sed_pools.py
+++ src/middlewared/middlewared/migration/0015_flag_all_sed_pools.py
@@ -6,7 +6,8 @@ async def migrate(middleware):
     # If system is licensed for SED and is an enterprise system - we will check if any pool's
     # disks are SED based and if that is the case, we will mark it as such
     if await middleware.call('system.sed_enabled'):
-        pass
+        # No need to block postinit - so let's create a task
+        await middleware.create_task(middleware.call('pool.update_all_sed_attr'))
     else:
         for pool in await middleware.call('datastore.query', 'storage.volume', [['vol_all_sed', '=', None]]):
             await middleware.call('datastore.update', 'storage.volume', pool['id'], {'vol_all_sed': False})
