e941ce625e73faad2c931193374c170391d0d81b
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-12-03 14:20:02 +0500

    Handle import case

--- src/middlewared/middlewared/migration/0016_flag_all_sed_pools.py
+++ src/middlewared/middlewared/migration/0016_flag_all_sed_pools.py
@@ -7,7 +7,7 @@ async def migrate(middleware):
     # disks are SED based and if that is the case, we will mark it as such
     if await middleware.call('system.sed_enabled'):
         # No need to block postinit - so let's create a task
-        await middleware.create_task(middleware.call('pool.update_all_sed_attr'))
+        middleware.create_task(middleware.call('pool.update_all_sed_attr'))
     else:
         for pool in await middleware.call('datastore.query', 'storage.volume', [['vol_all_sed', '=', None]]):
             await middleware.call('datastore.update', 'storage.volume', pool['id'], {'vol_all_sed': False})
