xiphis/freenas-middleware

Remove python-pam and plumb in pam_truenas

src/middlewared/middlewared/utils/account/faillock.py
@@ -1,126 +1,165 @@
-# Utilities related to the faillock tally files generated by pam_faillock(8)
-
-import enum
-import os
-from dataclasses import dataclass
-from struct import unpack
-from middlewared.utils.time_utils import utc_now
-from typing import Iterator
-
-
-FAILLOCK_DIR = '/var/run/faillock'
-
-# Each faillock tally file is comprised of zero or more tally entries
+# Utilities related to faillock entries in the PAM_TRUENAS keyring
 #
-# struct  tally {
-#         char            source[52];     /* rhost or tty of the login failure */
-#                                         /* (not necessarily NULL terminated) */
-#         uint16_t        reserved;       /* reserved for future use */
-#         uint16_t        status;         /* record status  */
-#         uint64_t        time;           /* time of the login failure */
-# };
+# The PAM_TRUENAS keyring has the following structure
+#
+# (Unlocked user)
+# persistent-keyring:uid=0
+# └── PAM_TRUENAS
+#     └── username
+#         ├── API_KEYS
+#         │   ├── 0 (reserved for user password SCRAM auth)
+#         │   ├── 1 (API key)
+#         │   └── 2 (API key)
+#         ├── SESSIONS
+#         │   └── <uuid> (contains kr_sess_t struct)
+#         └── FAILLOG
+#             └── <timestamp> (contains ptn_tally_t struct)
+#
+# (locked user)
+# persistent-keyring:uid=0
+# └── PAM_TRUENAS
+#     └── username
+#         ├── API_KEYS
+#         │   ├── 0 (reserved for user password SCRAM auth)
+#         │   ├── 1 (API key)
+#         │   └── 2 (API key)
+#         ├── SESSIONS
+#         │   └── <uuid> (contains kr_sess_t struct)
+#         ├── FAILLOG
+#         │   ├── <timestamp> (contains ptn_tally_t struct)
+#         │   ├── <timestamp> (contains ptn_tally_t struct)
+#         │   └── ...
+#         └── tally_lock
 
-STRUCT_TALLY = '52sHHQ'
-TALLY_ENTRY_SZ = 64
-MAX_FAILURE = 3
-FAIL_INTERVAL = 15 * 60
-UNLOCK_TIME = 15 * 60
+import truenas_keyring
+import errno
 
 
-class TallyStatus(enum.IntFlag):
-    VALID = 0x1
-    RHOST = 0x2
-    TTY = 0x4
+# Since the code-path to check whether local accounts are tally_locked is
+# potentially hot we store references to a few different keyring objects here
+# to reduce the number of lookups per-user.
+PAM_TRUENAS_KEYRING_NAME = "PAM_TRUENAS"
+TALLY_LOCK = 'tally_lock'
+FAILLOG = 'FAILLOG'
+keyrings = {'persistent': None, 'truenas_pam': None}
 
 
-@dataclass(slots=True, frozen=True)
-class TallyEntry:
-    source: str
-    status: TallyStatus
-    timestamp: int  # 64bit time_t
+def __get_persistent_keyring():
+    if not keyrings['persistent']:
+        keyrings['persistent'] = truenas_keyring.get_persistent_keyring()
+
+    return keyrings['persistent']
 
 
-def has_tally(username: str) -> bool:
+def __get_truenas_pam_keyring():
+    if keyrings['truenas_pam']:
+        return keyrings['truenas_pam']
+
+    persistent = __get_persistent_keyring()
     try:
-        st = os.stat(os.path.join(FAILLOCK_DIR, username))
+        # The PAM keyring is lazy-initialized by the pam_truenas module
+        pam_keyring = persistent.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=PAM_TRUENAS_KEYRING_NAME
+        )
     except FileNotFoundError:
+        return None
+
+    keyrings['truenas_pam'] = pam_keyring
+    return pam_keyring
+
+
+def __get_user_keyring(username):
+    if (pam_keyring := __get_truenas_pam_keyring()) is None:
+        return None
+
+    try:
+        user_keyring = pam_keyring.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=username
+        )
+    except FileNotFoundError:
+        return None
+
+    return user_keyring
+
+
+def __get_lock_key(user_keyring):
+    try:
+        lock_key = user_keyring.search(
+            key_type=truenas_keyring.KeyType.USER,
+            description=TALLY_LOCK
+        )
+    except FileNotFoundError:
+        # truenas_pykeyring converts ENOKEY to FileNotFoundError
+        return None
+    except truenas_keyring.KeyringError as exc:
+        if exc.errno == errno.EKEYEXPIRED or exc.errno == errno.EKEYREVOKED:
+            # Forcing cpython extension iterate keyring contents forces deletion
+            # of expired lock
+            user_keyring.list_keyring_contents()
+            return None
+
+        raise
+
+    return lock_key
+
+
+def __get_failure_keyring(user_keyring):
+    try:
+        faillog_keyring = user_keyring.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=FAILLOG
+        )
+    except FileNotFoundError:
+        # truenas_pykeyring converts ENOKEY to FileNotFoundError
+        faillog_keyring = None
+
+    return faillog_keyring
+
+
+def is_tally_locked(username) -> bool:
+    """ This function checks for whether the pam_truenas user keyring has a tally_lock key set on it. """
+    if (user_keyring := __get_user_keyring(username)) is None:
+        # No user keyring / session info. It's not possible for it to be tally locked.
         return False
 
-    # NOTE: pam_faillock will reset tally file via ftruncate(tally_fd, 0)
-    return st.st_size > 0
+    lock_key = __get_lock_key(user_keyring)
+    return lock_key is not None
 
 
-def reset_tally(username: str) -> None:
-    """ Reset the tally file so that temporary account lock is removed. This function
-    is used in user.update in order to provide admin a mechanism to remove a temporary
-    lock. NOTE: behavior matches reset_tally() in pam_faillock.c in that it does not perform
-    any locking before truncation."""
-    try:
-        fd = os.open(os.path.join(FAILLOCK_DIR, username), os.O_RDWR)
-    except FileNotFoundError:
-        pass
-    else:
-        try:
-            os.ftruncate(fd, 0)
-        finally:
-            os.close(fd)
+def reset_tally(username) -> None:
+    """ Reset FAILLOG for the specified username and remove its tally-lock """
+    if (user_keyring := __get_user_keyring(username)) is None:
+        return
 
+    if (failure_keyring := __get_failure_keyring(username)) is not None:
+        # Wipe all failed login attempts from log
+        failure_keyring.clear()
 
-def iter_tally(username: str) -> Iterator[TallyEntry]:
-    try:
-        with open(os.path.join(FAILLOCK_DIR, username), 'rb') as f:
-            while data := f.read(TALLY_ENTRY_SZ):
-                unpacked = unpack(STRUCT_TALLY, data)
-                yield TallyEntry(unpacked[0].strip(b'\x00').decode(), TallyStatus(unpacked[2]), unpacked[3])
+    # There is a separate key thats presence indicates the user is
+    # tally locked. Remove this as well if it exists.
+    if (lock_key := __get_lock_key(user_keyring)) is None:
+        return
 
-    except FileNotFoundError:
-        # Nothing to do. No tally entries
-        pass
-
-
-def is_tally_locked(username: str, unlock_time: int = UNLOCK_TIME) -> bool:
-    """ Return boolean for whether the specified account name has been locked by
-    pam_faillock """
-    failures = 0
-    last_time = 0
-    entries = []
-
-    for entry in iter_tally(username):
-        if entry.timestamp > last_time:
-            last_time = entry.timestamp
-
-        entries.append(entry)
-
-    if not last_time:
-        # Short-circuit if no entries
-        return False
-
-    if int(utc_now(naive=False).timestamp()) > (last_time + unlock_time):
-        # Short-circuit if timeout has expired
-        return False
-
-    for entry in entries:
-        if entry.status & TallyStatus.VALID.value and (last_time - entry.timestamp) < FAIL_INTERVAL:
-            failures += 1
-            if failures >= MAX_FAILURE:
-                return True
-
-    return False
+    # Unlink the locking key from the user keyring.
+    user_keyring.unlink_key(lock_key.serial)
 
 
 def tally_locked_users() -> set[str]:
-    """ Return set of user names of users who are locked due to tally limits
-    defined by pam_faillock """
-    out = set()
+    """ Create a set of usernames of all users who are currently tally locked. The
+    TRUENAS_PAM_KEYRING key descriptions contain the affected user's name. """
+    locked_users = set()
+    if (pam_keyring := __get_truenas_pam_keyring()) is None:
+        return locked_users
 
-    if not os.path.exists(FAILLOCK_DIR):
-        # pam_faillock(8) may not have created the directory yet. If it doesn't
-        # exist then we can short-circuit. There's no need to create the directory.
-        return out
+    for user_keyring in pam_keyring.iter_keyring_contents():
+        try:
+            if __get_lock_key(user_keyring):
+                locked_users.add(user_keyring.key.description)
+        except Exception:
+            # This is called in user.query extend context. We can't allow exceptions or logs to be raised here
+            # because the code path is really hot.
+            pass
 
-    with os.scandir(FAILLOCK_DIR) as it:
-        for dirent in it:
-            if is_tally_locked(dirent.name):
-                out.add(dirent.name)
-
-    return out
+    return locked_users

truenas/middleware

src/middlewared/middlewared/utils/account/faillock.py
@@ -1,126 +1,165 @@
-# Utilities related to the faillock tally files generated by pam_faillock(8)
-
-import enum
-import os
-from dataclasses import dataclass
-from struct import unpack
-from middlewared.utils.time_utils import utc_now
-from typing import Iterator
-
-
-FAILLOCK_DIR = '/var/run/faillock'
-
-# Each faillock tally file is comprised of zero or more tally entries
+# Utilities related to faillock entries in the PAM_TRUENAS keyring
 #
-# struct  tally {
-#         char            source[52];     /* rhost or tty of the login failure */
-#                                         /* (not necessarily NULL terminated) */
-#         uint16_t        reserved;       /* reserved for future use */
-#         uint16_t        status;         /* record status  */
-#         uint64_t        time;           /* time of the login failure */
-# };
+# The PAM_TRUENAS keyring has the following structure
+#
+# (Unlocked user)
+# persistent-keyring:uid=0
+# └── PAM_TRUENAS
+#     └── username
+#         ├── API_KEYS
+#         │   ├── 0 (reserved for user password SCRAM auth)
+#         │   ├── 1 (API key)
+#         │   └── 2 (API key)
+#         ├── SESSIONS
+#         │   └── <uuid> (contains kr_sess_t struct)
+#         └── FAILLOG
+#             └── <timestamp> (contains ptn_tally_t struct)
+#
+# (locked user)
+# persistent-keyring:uid=0
+# └── PAM_TRUENAS
+#     └── username
+#         ├── API_KEYS
+#         │   ├── 0 (reserved for user password SCRAM auth)
+#         │   ├── 1 (API key)
+#         │   └── 2 (API key)
+#         ├── SESSIONS
+#         │   └── <uuid> (contains kr_sess_t struct)
+#         ├── FAILLOG
+#         │   ├── <timestamp> (contains ptn_tally_t struct)
+#         │   ├── <timestamp> (contains ptn_tally_t struct)
+#         │   └── ...
+#         └── tally_lock
 
-STRUCT_TALLY = '52sHHQ'
-TALLY_ENTRY_SZ = 64
-MAX_FAILURE = 3
-FAIL_INTERVAL = 15 * 60
-UNLOCK_TIME = 15 * 60
+import truenas_keyring
+import errno
 
 
-class TallyStatus(enum.IntFlag):
-    VALID = 0x1
-    RHOST = 0x2
-    TTY = 0x4
+# Since the code-path to check whether local accounts are tally_locked is
+# potentially hot we store references to a few different keyring objects here
+# to reduce the number of lookups per-user.
+PAM_TRUENAS_KEYRING_NAME = "PAM_TRUENAS"
+TALLY_LOCK = 'tally_lock'
+FAILLOG = 'FAILLOG'
+keyrings = {'persistent': None, 'truenas_pam': None}
 
 
-@dataclass(slots=True, frozen=True)
-class TallyEntry:
-    source: str
-    status: TallyStatus
-    timestamp: int  # 64bit time_t
+def __get_persistent_keyring():
+    if not keyrings['persistent']:
+        keyrings['persistent'] = truenas_keyring.get_persistent_keyring()
+
+    return keyrings['persistent']
 
 
-def has_tally(username: str) -> bool:
+def __get_truenas_pam_keyring():
+    if keyrings['truenas_pam']:
+        return keyrings['truenas_pam']
+
+    persistent = __get_persistent_keyring()
     try:
-        st = os.stat(os.path.join(FAILLOCK_DIR, username))
+        # The PAM keyring is lazy-initialized by the pam_truenas module
+        pam_keyring = persistent.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=PAM_TRUENAS_KEYRING_NAME
+        )
     except FileNotFoundError:
+        return None
+
+    keyrings['truenas_pam'] = pam_keyring
+    return pam_keyring
+
+
+def __get_user_keyring(username):
+    if (pam_keyring := __get_truenas_pam_keyring()) is None:
+        return None
+
+    try:
+        user_keyring = pam_keyring.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=username
+        )
+    except FileNotFoundError:
+        return None
+
+    return user_keyring
+
+
+def __get_lock_key(user_keyring):
+    try:
+        lock_key = user_keyring.search(
+            key_type=truenas_keyring.KeyType.USER,
+            description=TALLY_LOCK
+        )
+    except FileNotFoundError:
+        # truenas_pykeyring converts ENOKEY to FileNotFoundError
+        return None
+    except truenas_keyring.KeyringError as exc:
+        if exc.errno == errno.EKEYEXPIRED or exc.errno == errno.EKEYREVOKED:
+            # Forcing cpython extension iterate keyring contents forces deletion
+            # of expired lock
+            user_keyring.list_keyring_contents()
+            return None
+
+        raise
+
+    return lock_key
+
+
+def __get_failure_keyring(user_keyring):
+    try:
+        faillog_keyring = user_keyring.search(
+            key_type=truenas_keyring.KeyType.KEYRING,
+            description=FAILLOG
+        )
+    except FileNotFoundError:
+        # truenas_pykeyring converts ENOKEY to FileNotFoundError
+        faillog_keyring = None
+
+    return faillog_keyring
+
+
+def is_tally_locked(username) -> bool:
+    """ This function checks for whether the pam_truenas user keyring has a tally_lock key set on it. """
+    if (user_keyring := __get_user_keyring(username)) is None:
+        # No user keyring / session info. It's not possible for it to be tally locked.
         return False
 
-    # NOTE: pam_faillock will reset tally file via ftruncate(tally_fd, 0)
-    return st.st_size > 0
+    lock_key = __get_lock_key(user_keyring)
+    return lock_key is not None
 
 
-def reset_tally(username: str) -> None:
-    """ Reset the tally file so that temporary account lock is removed. This function
-    is used in user.update in order to provide admin a mechanism to remove a temporary
-    lock. NOTE: behavior matches reset_tally() in pam_faillock.c in that it does not perform
-    any locking before truncation."""
-    try:
-        fd = os.open(os.path.join(FAILLOCK_DIR, username), os.O_RDWR)
-    except FileNotFoundError:
-        pass
-    else:
-        try:
-            os.ftruncate(fd, 0)
-        finally:
-            os.close(fd)
+def reset_tally(username) -> None:
+    """ Reset FAILLOG for the specified username and remove its tally-lock """
+    if (user_keyring := __get_user_keyring(username)) is None:
+        return
 
+    if (failure_keyring := __get_failure_keyring(user_keyring)) is not None:
+        # Wipe all failed login attempts from log
+        failure_keyring.clear()
 
-def iter_tally(username: str) -> Iterator[TallyEntry]:
-    try:
-        with open(os.path.join(FAILLOCK_DIR, username), 'rb') as f:
-            while data := f.read(TALLY_ENTRY_SZ):
-                unpacked = unpack(STRUCT_TALLY, data)
-                yield TallyEntry(unpacked[0].strip(b'\x00').decode(), TallyStatus(unpacked[2]), unpacked[3])
+    # There is a separate key thats presence indicates the user is
+    # tally locked. Remove this as well if it exists.
+    if (lock_key := __get_lock_key(user_keyring)) is None:
+        return
 
-    except FileNotFoundError:
-        # Nothing to do. No tally entries
-        pass
-
-
-def is_tally_locked(username: str, unlock_time: int = UNLOCK_TIME) -> bool:
-    """ Return boolean for whether the specified account name has been locked by
-    pam_faillock """
-    failures = 0
-    last_time = 0
-    entries = []
-
-    for entry in iter_tally(username):
-        if entry.timestamp > last_time:
-            last_time = entry.timestamp
-
-        entries.append(entry)
-
-    if not last_time:
-        # Short-circuit if no entries
-        return False
-
-    if int(utc_now(naive=False).timestamp()) > (last_time + unlock_time):
-        # Short-circuit if timeout has expired
-        return False
-
-    for entry in entries:
-        if entry.status & TallyStatus.VALID.value and (last_time - entry.timestamp) < FAIL_INTERVAL:
-            failures += 1
-            if failures >= MAX_FAILURE:
-                return True
-
-    return False
+    # Unlink the locking key from the user keyring.
+    user_keyring.unlink_key(lock_key.serial)
 
 
 def tally_locked_users() -> set[str]:
-    """ Return set of user names of users who are locked due to tally limits
-    defined by pam_faillock """
-    out = set()
+    """ Create a set of usernames of all users who are currently tally locked. The
+    TRUENAS_PAM_KEYRING key descriptions contain the affected user's name. """
+    locked_users = set()
+    if (pam_keyring := __get_truenas_pam_keyring()) is None:
+        return locked_users
 
-    if not os.path.exists(FAILLOCK_DIR):
-        # pam_faillock(8) may not have created the directory yet. If it doesn't
-        # exist then we can short-circuit. There's no need to create the directory.
-        return out
+    for user_keyring in pam_keyring.iter_keyring_contents():
+        try:
+            if __get_lock_key(user_keyring):
+                locked_users.add(user_keyring.key.description)
+        except Exception:
+            # This is called in user.query extend context. We can't allow exceptions or logs to be raised here
+            # because the code path is really hot.
+            pass
 
-    with os.scandir(FAILLOCK_DIR) as it:
-        for dirent in it:
-            if is_tally_locked(dirent.name):
-                out.add(dirent.name)
-
-    return out
+    return locked_users