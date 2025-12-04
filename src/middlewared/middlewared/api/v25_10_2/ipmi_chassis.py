46882fdc12659619d1aa922f9886da83d2e767ed
Mark Grimes <mark.grimes@ixsystems.com>
2025-12-04 12:31:23 -0800

    Add `from_previous` and `to_previous` to manage api changes from 25_10_1.

--- src/middlewared/middlewared/api/v25_10_2/ipmi_chassis.py
+++ src/middlewared/middlewared/api/v25_10_2/ipmi_chassis.py
@@ -49,6 +49,19 @@ class IpmiChassisIdentifyArgs(BaseModel):
     data: IpmiChassisIdentifyRequest = Field(default_factory=IpmiChassisIdentifyRequest)
     """Request parameters for IPMI chassis identify operation."""
 
+    @classmethod
+    def from_previous(cls, previous_value):
+        """Convert from v25_10_1 format (flat verb field) to v25_10_2 format (wrapped in data)."""
+        # Previous version: {'verb': 'ON'}
+        # New version: {'data': {'verb': 'ON', 'apply_remote': False}}
+        return cls(data=previous_value)
+
+    @classmethod
+    def to_previous(cls, value):
+        """Convert from v25_10_2 format (wrapped in data) to v25_10_1 format (flat verb field)."""
+        # Return only the verb field, drop apply_remote since it didn't exist in previous version
+        return {'verb': value['data']['verb']}
+
 
 class IpmiChassisIdentifyResult(BaseModel):
     result: None
@@ -63,6 +76,23 @@ class IpmiChassisInfoRequest(BaseModel):
 class IpmiChassisInfoArgs(BaseModel):
     data: IpmiChassisInfoRequest = Field(default_factory=IpmiChassisInfoRequest)
     """Request parameters for IPMI chassis information."""
+
+    @classmethod
+    def from_previous(cls, previous_value):
+        """Convert from v25_10_1 format (empty) to v25_10_2 format (wrapped in data)."""
+        # Previous version: {} (no parameters)
+        # New version: {'data': {'query_remote': False}}
+        # If previous_value is empty dict or None, use defaults
+        if not previous_value:
+            return cls()
+        # Otherwise wrap it in data
+        return cls(data=previous_value)
+
+    @classmethod
+    def to_previous(cls, value):
+        """Convert from v25_10_2 format (wrapped in data) to v25_10_1 format (empty)."""
+        # Previous version had no parameters, so return empty dict
+        return {}
 
 
 class IpmiChassisInfoResult(BaseModel):
