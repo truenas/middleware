1fcd24f46c95a0a722d864c2db0c543dfe49b409
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-12-05 09:38:10 -0500

    NAS-138503 / 26.04 / Lock `pool.attach` on disk name (by creatorcary) (#17765)
    Automatic cherry-pick failed. Please resolve conflicts by running:
    
        git reset --hard HEAD~1
        git cherry-pick -x 0a1aeef7150b9e6ce1d4e9a0ae5fe45a4a36df1c
        git cherry-pick -x 85bf9b919e6b4f55e80292837ee587d992c4320c
        git cherry-pick -x fe288eca459c9670f438a0ae26f81ee46dac111a
    
    If the original PR was merged via a squash, you can just cherry-pick the
    squashed commit:
    
        git reset --hard HEAD~1
        git cherry-pick -x 974403f97fa213107d575c6e0f3ceab2a0472285
    
    Suppose a `pool.attach` job is running on pool X, disk Y:
    - If `pool.attach` is called again for disk Y, immediately fail (do not
    enter job queue).
    - If `pool.attach` is called again on pool X, wait until the first job
    is finished.
    - If `pool.attach` is called again for another disk on another pool,
    start immediately.
    
    http://jenkins.eng.ixsystems.net:8080/job/tests/job/api_tests/6828/
    
    Original PR: https://github.com/truenas/middleware/pull/17763
    
    Co-authored-by: Logan Cary <logan.cary@ixsystems.com>

--- src/middlewared/middlewared/plugins/pool_/attach_disk.py
+++ src/middlewared/middlewared/plugins/pool_/attach_disk.py
@@ -1,4 +1,5 @@
 import asyncio
+from collections import defaultdict
 
 from middlewared.api import api_method
 from middlewared.api.current import PoolAttachArgs, PoolAttachResult
@@ -7,74 +8,96 @@ from middlewared.service import job, Service, ValidationErrors
 
 class PoolService(Service):
 
+    POOL_LOCKS = defaultdict(asyncio.Lock)
+
     @api_method(PoolAttachArgs, PoolAttachResult, roles=['POOL_WRITE'])
-    @job(lock=lambda args: f'pool_attach_{args[0]}')
+    @job(lock=lambda args: f'pool_attach_disk_{args[1]["new_disk"]}', lock_queue_size=0)
     async def attach(self, job, oid, options):
         """
-        `target_vdev` is the GUID of the vdev where the disk needs to be attached. In case of STRIPED vdev, this
-        is the STRIPED disk GUID which will be converted to mirror. If `target_vdev` is mirror, it will be converted
-        into a n-way mirror.
+        Attach a disk to an existing vdev in a pool, converting a striped vdev to a mirror or extending
+        an existing mirror to an n-way mirror.
+
+        This operation will format the new disk, attach it to the target vdev, and wait for resilvering
+        to complete if the target is a RAIDZ vdev undergoing expansion.
+
+        Locking behavior:
+        - If another attach operation is already using the same disk, this call will fail immediately
+          with EBUSY rather than queueing.
+        - If another attach operation is running on the same pool (but with a different disk), this
+          call will queue and wait for the previous operation to complete.
+        - Operations on different pools with different disks can run concurrently.
         """
-        pool = await self.middleware.call('pool.get_instance', oid)
-        verrors = ValidationErrors()
-        topology = pool['topology']
-        topology_type = vdev = None
-        for i in topology:
-            for v in topology[i]:
-                if v['guid'] == options['target_vdev']:
-                    topology_type = i
-                    vdev = v
+        # Acquire pool lock to serialize operations on the same pool
+        async with self.POOL_LOCKS[oid]:
+            pool = await self.middleware.call('pool.get_instance', oid)
+            verrors = ValidationErrors()
+            topology = pool['topology']
+            topology_type = vdev = None
+            for i in topology:
+                for v in topology[i]:
+                    if v['guid'] == options['target_vdev']:
+                        topology_type = i
+                        vdev = v
+                        break
+                if topology_type:
                     break
-            if topology_type:
-                break
-        else:
-            verrors.add('pool_attach.target_vdev', 'Unable to locate VDEV')
+            else:
+                verrors.add('pool_attach.target_vdev', 'Unable to locate VDEV')
+                verrors.check()
+
+            if topology_type in ('cache', 'spares'):
+                verrors.add('pool_attach.target_vdev', f'Attaching disks to {topology_type} not allowed.')
+            elif topology_type == 'data':
+                # We would like to make sure here that we don't have inconsistent vdev types across data
+                if vdev['type'] not in ('DISK', 'MIRROR', 'RAIDZ1', 'RAIDZ2', 'RAIDZ3'):
+                    verrors.add('pool_attach.target_vdev', f'Attaching disk to {vdev["type"]} vdev is not allowed.')
+
+            # Let's validate new disk now
+            verrors.add_child(
+                'pool_attach',
+                await self.middleware.call(
+                    'disk.check_disks_availability',
+                    [options['new_disk']],
+                    options['allow_duplicate_serials']
+                ),
+            )
             verrors.check()
-        if topology_type in ('cache', 'spares'):
-            verrors.add('pool_attach.target_vdev', f'Attaching disks to {topology_type} not allowed.')
-        elif topology_type == 'data':
-            # We would like to make sure here that we don't have inconsistent vdev types across data
-            if vdev['type'] not in ('DISK', 'MIRROR', 'RAIDZ1', 'RAIDZ2', 'RAIDZ3'):
-                verrors.add('pool_attach.target_vdev', f'Attaching disk to {vdev["type"]} vdev is not allowed.')
 
-        # Let's validate new disk now
-        verrors.add_child(
-            'pool_attach',
-            await self.middleware.call('disk.check_disks_availability', [options['new_disk']],
-                                       options['allow_duplicate_serials']),
-        )
-        verrors.check()
+            job.set_progress(3, 'Completed validation')
 
-        job.set_progress(3, 'Completed validation')
+            if vdev['type'] in ('DISK', 'RAIDZ1', 'RAIDZ2', 'RAIDZ3'):
+                guid = vdev['guid']
+            else:
+                guid = vdev['children'][0]['guid']
 
-        if vdev['type'] in ['DISK', 'RAIDZ1', 'RAIDZ2', 'RAIDZ3']:
-            guid = vdev['guid']
-        else:
-            guid = vdev['children'][0]['guid']
+            if vdev['type'] == 'DISK':
+                devices_for_size = [vdev.get('device')]
+            else:
+                devices_for_size = [child.get('device') for child in vdev['children']]
 
-        if vdev['type'] == 'DISK':
-            devices_for_size = [vdev.get('device')]
-        else:
-            devices_for_size = [child.get('device') for child in vdev['children']]
+            sizes = []
+            for device in devices_for_size:
+                if device is not None:
+                    size = await self.middleware.call('disk.get_dev_size', device)
+                    if size is not None:
+                        sizes.append(size)
 
-        sizes = []
-        for device in devices_for_size:
-            if device is not None:
-                size = await self.middleware.call('disk.get_dev_size', device)
-                if size is not None:
-                    sizes.append(size)
+            disks = {options['new_disk']: {'vdev': [], 'size': min(sizes) if sizes else None}}
+            job.set_progress(5, 'Formatting disks')
+            await self.middleware.call('pool.format_disks', job, disks, 5, 20)
+            job.set_progress(22, 'Extending pool')
+            devname = disks[options['new_disk']]['vdev'][0]
+            extend_job = await self.middleware.call(
+                'zfs.pool.extend',
+                pool['name'],
+                None,
+                [{'target': guid, 'type': 'DISK', 'path': devname}]
+            )
+            await extend_job.wait(raise_error=True)
 
-        disks = {options['new_disk']: {'vdev': [], 'size': min(sizes) if sizes else None}}
-        job.set_progress(5, 'Formatting disks')
-        await self.middleware.call('pool.format_disks', job, disks, 5, 20)
-        job.set_progress(22, 'Extending pool')
-        devname = disks[options['new_disk']]['vdev'][0]
-        extend_job = await self.middleware.call('zfs.pool.extend', pool['name'], None, [
-            {'target': guid, 'type': 'DISK', 'path': devname}
-        ])
-        await extend_job.wait(raise_error=True)
+            if vdev['type'] not in ('RAIDZ1', 'RAIDZ2', 'RAIDZ3'):
+                return
 
-        if vdev['type'] in ('RAIDZ1', 'RAIDZ2', 'RAIDZ3'):
             while True:
                 expand = await self.middleware.call('zfs.pool.expand_state', pool['name'])
 
