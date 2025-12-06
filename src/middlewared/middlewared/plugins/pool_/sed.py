b5e270c854f5e3b53104bf48d17e9f0b4b6f6214
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-12-06 21:52:06 +0500

    Check all sed attr on pool event

--- src/middlewared/middlewared/plugins/pool_/sed.py
+++ src/middlewared/middlewared/plugins/pool_/sed.py
@@ -8,14 +8,32 @@ class PoolService(Service):
         event_send = False
 
     @private
-    async def update_all_sed_attr(self, check_all_pools=False):
+    async def update_all_sed_attr(self, skip_all_sed_check=False, filter_pool=None):
         # We will scan all pools here with relevant disks and make sure that if any pool has all disks which are
         # SED based, we will update that pool in the db to reflect reality
         # How we will do this is that we will get disks from pool topology and then check if all of them are SED
         # based or not using disk.query with sed filter (forcing db level filter for performance)
         #
-        # We will only query those pools which are actually healthy unless forced to query and update all pools
-        filters = [] if check_all_pools else [['all_sed', '=', None], ['healthy', '=', True]]
+        # We will only query those pools by default which have all sed flag unset
+        # When this method runs, it will update status for all pools based on whatever filters we have
+        # It does not account for healthy or unhealthy options and relies on consumer instead to be smart about it
+        # How this will be triggered is in 3 places:
+        # For non-HA systems:
+        # It will happen in a data migration where we will update status of all pools
+        # For HA systems:
+        # It will happen on master failover event
+        # For both systems:
+        # If we detect `sysevent.fs.zfs.config_sync` zfs event and the pool in question is healthy,
+        # this will be triggered to account for cases where for example we had a pool where all disks
+        # were there except one and all were SED at that point. So initially we would have marked that
+        # as all sed, but if the last disk was non-SED, that would then get accounted for whenever the
+        # pool in question becomes healthy and vice versa
+        # Why we still settle on setting this value initially is to prevent accidental mis-configuration of a pool
+        # which might perhaps very well be an all sed pool
+        filters = [] if skip_all_sed_check else [['all_sed', '=', None]]
+        if filter_pool:
+            filters.append(['name', '=', filter_pool])
+
         db_pools = await self.middleware.call('pool.query', filters)
         if not db_pools:
             # If all pools in db already have db row updated, there is nothing to be done here
