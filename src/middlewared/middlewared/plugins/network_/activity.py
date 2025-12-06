c3271639223bc16c9daab37b82d587b11fc3cfa3
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-12-06 15:57:08 +0500

    Add util for checking network connectivity

--- src/middlewared/middlewared/plugins/network_/activity.py
+++ src/middlewared/middlewared/plugins/network_/activity.py
@@ -1,18 +1,11 @@
 # -*- coding=utf-8 -*-
-import errno
 import logging
-
-from aiohttp import ClientSession, ClientTimeout, ClientError
 
 from middlewared.api import api_method
 from middlewared.api.current import NetworkConfigurationActivityChoicesArgs, NetworkConfigurationActivityChoicesResult
-from middlewared.service import CallError, NetworkActivityDisabled, private, Service
+from middlewared.service import NetworkActivityDisabled, private, Service
 
 logger = logging.getLogger(__name__)
-
-
-CONNECTIVITY_CHECK_URL = 'http://www.gstatic.com/generate_204'
-CONNECTIVITY_CHECK_TIMEOUT = 10
 
 
 class NetworkConfigurationService(Service):
@@ -60,39 +53,6 @@ class NetworkGeneralService(Service):
             return name not in config['activity']['activities']
 
     @private
-    async def check_internet_connectivity(self):
-        """
-        Check internet connectivity by making an HTTP request to a known endpoint.
-        This verifies both DNS resolution and network connectivity.
-        Returns True if connected, raises CallError otherwise.
-        """
-        try:
-            async with ClientSession(
-                timeout=ClientTimeout(total=CONNECTIVITY_CHECK_TIMEOUT),
-                trust_env=True
-            ) as session:
-                async with session.get(CONNECTIVITY_CHECK_URL) as resp:
-                    if resp.status == 204:
-                        return True
-                    raise CallError(
-                        f'Unexpected response from connectivity check: {resp.status}',
-                        errno.ENETUNREACH
-                    )
-        except ClientError as e:
-            raise CallError(
-                f'Internet connectivity check failed: {e}',
-                errno.ENETUNREACH
-            )
-        except TimeoutError:
-            raise CallError(
-                'Internet connectivity check timed out',
-                errno.ETIMEDOUT
-            )
-
-    @private
-    async def will_perform_activity(self, name, check_connectivity=False):
+    async def will_perform_activity(self, name):
         if not await self.middleware.call('network.general.can_perform_activity', name):
             raise NetworkActivityDisabled(f'Network activity "{self.activities[name]}" is disabled')
-
-        if check_connectivity:
-            await self.check_internet_connectivity()
