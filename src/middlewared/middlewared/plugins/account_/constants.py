4da18a2d269f98a7934b3f8485a6a9452c58854c
themylogin <themylogin@gmail.com>
2025-12-09 20:32:24 +0100

    Add `truenas_webshare` to `ALLOWED_BUILTIN_GIDS`

--- src/middlewared/middlewared/plugins/account_/constants.py
+++ src/middlewared/middlewared/plugins/account_/constants.py
@@ -23,6 +23,7 @@ USERNS_IDMAP_NONE = 0
 # unexpected security issues.
 ALLOWED_BUILTIN_GIDS = {
     14,  # ftp -- required for FTP access
+    445,  # truenas_webshare
     544,  # builtin_administrators
     545,  # builtin_users
     568,  # apps
