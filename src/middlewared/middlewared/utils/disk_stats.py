d702b99c936623758dda8edbaefbfcaba19d8b00
M. Rehan <mrehanlm93@gmail.com>
2025-12-15 15:32:58 +0500

    Use Linux's fixed 512-byte sector size for disk stats

--- src/middlewared/middlewared/utils/disk_stats.py
+++ src/middlewared/middlewared/utils/disk_stats.py
@@ -1,8 +1,15 @@
 import contextlib
 import logging
-import os
 
 from .disks_.disk_class import iterate_disks
+
+
+# always 512 on Linux
+# https://github.com/torvalds/linux/blob/daa121128a2d2ac6006159e2c47676e4fcd21eab/include/linux/blk_types.h#L25-L34
+# Basically /proc/diskstats reports read/write sector counts in fixed 512 byte units independent of the
+# underlying device's logical/physical sector size which is why we use the fixed constant/sector size here
+# reflecting how it is already being done in /proc/diskstats
+SECTOR_SIZE = 512
 
 
 logger = logging.getLogger(__name__)
@@ -22,11 +29,6 @@ def get_disk_stats() -> dict[str, dict]:
                 if disk_name not in available_disks:
                     continue
 
-                sector_size = 512  # default sector size if we are not able to find it keeping in line with netdata
-                with contextlib.suppress(FileNotFoundError, ValueError):
-                    with open(os.path.join('/sys/block', disk_name, 'queue/hw_sector_size'), 'r') as f:
-                        sector_size = int(f.read().strip())
-
                 try:
                     read_ops = int(parts[3])
                     read_sectors = int(parts[5])
@@ -38,8 +40,8 @@ def get_disk_stats() -> dict[str, dict]:
                     continue
 
                 stats[available_disks[disk_name].identifier] = {
-                    'reads': (read_sectors * sector_size) / 1024,  # convert to kb
-                    'writes': (write_sectors * sector_size) / 1024,  # convert to kb
+                    'reads': (read_sectors * SECTOR_SIZE) / 1024,  # convert to kb
+                    'writes': (write_sectors * SECTOR_SIZE) / 1024,  # convert to kb
                     'read_ops': read_ops,
                     'write_ops': write_ops,
                     'busy': busy_time,
