2eae2b43b003285a2de394f9b35d763e15b07f18
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-12-15 10:15:47 -0500

    NAS-138886 / 26.04 / Fix cloud sync bwlimit (by themylogin) (#17823)
    `bwlimit` didn't have `type=list` declaration for some time. Probably,
    that's how dicts entered the database.
    
    Original PR: https://github.com/truenas/middleware/pull/17812
    
    ---------
    
    Co-authored-by: themylogin <themylogin@gmail.com>

---
+++ src/middlewared/middlewared/alembic/versions/26.04/2025-12-15_15-14_merge.py
@@ -0,0 +1,24 @@
+"""Merge
+
+Revision ID: 2b73cb14b156
+Revises: d2c5ab6398b3, bcbd10d931bc
+Create Date: 2025-12-15 15:14:42.439699+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2b73cb14b156'
+down_revision = ('d2c5ab6398b3', 'bcbd10d931bc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
