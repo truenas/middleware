a1381cb303f1480c8bd44115403496958aa68e0f
Mark Grimes <mark.grimes@ixsystems.com>
2025-12-16 09:23:53 -0800

    gssproxy is required for NFS with kerberos. Add use-gss-proxy check after NFS start.  If not enabled, then make an effort to enable it.  Log if unsuccessful.

--- src/middlewared/middlewared/plugins/service_/services/nfs.py
+++ src/middlewared/middlewared/plugins/service_/services/nfs.py
@@ -2,6 +2,9 @@ import os
 
 from .base import SimpleService
 
+GSSPROXY_PROC = '/proc/net/rpc/use-gss-proxy'
+GSSPROXY_ENABLED = 1
+
 
 class NFSService(SimpleService):
     name = "nfs"
@@ -22,6 +25,29 @@ class NFSService(SimpleService):
             pass
         return exports
 
+    def _get_gssproxy_state(self) -> int | None:
+        """The kernel must be configured to use gssproxy if kerberos
+           is employed with an NFS share."""
+        rv = None
+        try:
+            with open(GSSPROXY_PROC, 'r') as pf:
+                val = pf.read().strip()
+                rv = int(val)
+        except Exception:
+            # Treat both missing file (valid possibility) and
+            # read or other failure (not really expected) the same
+            # by returning None
+            pass
+        return rv
+
+    async def check_gssproxy_state(self) -> int | None:
+        """async wrapper around gssproxy state get
+           -1 = unset
+            0 = disabled
+            1 = enabled
+            None = failure to read"""
+        return await self.middleware.run_in_thread(self._get_gssproxy_state)
+
     async def check_configuration(self):
         # Raise alert if there are entries in /etc/exports.d
         if (exportsdList := await self.middleware.run_in_thread(self.check_exportsd_dir)):
@@ -39,6 +65,20 @@ class NFSService(SimpleService):
         # This is to cover the case where the proc entry did not exist
         await self.middleware.call('nfs.update_procfs_v4recoverydir')
         await self._systemd_unit("rpc-statd", "start")
+
+        # Confirm use-gss-proxy is enabled
+        if await self.check_gssproxy_state() != GSSPROXY_ENABLED:
+            # We can most often establish use-gss-proxy=1 with the following:
+            await self._systemd_unit("nfs-server", "stop")
+            await self._systemd_unit("gssproxy", "restart")
+            await self._systemd_unit("nfs-server", "start")
+
+            # Re-check and log if not enabled.  Continue anyway as
+            # kerberos is often not required.
+            if (use_gss_proxy := self.check_gssproxy_state()) != GSSPROXY_ENABLED:
+                self.middleware.logger.warning(
+                    f"Failed to enable use-gss-proxy for NFS.  use-gss-proxy is {use_gss_proxy}"
+                )
 
     async def stop(self):
         await self._systemd_unit(self.systemd_unit, "stop")
