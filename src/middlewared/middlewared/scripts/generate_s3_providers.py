8098dca375ae963f03dc86ff0ffcc1d858c8932c
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-12-17 14:39:02 +0500

    Fix breaking build due to rclone
    This commit fixes an issue where we now have newer version of rclone which uses different indentation as opposed to the older version which the regex needs to account for.

--- src/middlewared/middlewared/scripts/generate_s3_providers.py
+++ src/middlewared/middlewared/scripts/generate_s3_providers.py
@@ -6,7 +6,7 @@ import subprocess
 def main():
     output = subprocess.check_output(["rclone", "help", "backend", "s3"], encoding="utf-8")
     providers_doc = re.search(r"#### --s3-provider([\s\S]+?)####", output).group(1)
-    providers = dict(re.findall(r" {4}- \"(.+)\"\n {8}- (.+)", providers_doc))
+    providers = dict(re.findall(r" {2}- \"(.+)\"\n {4}- (.+)", providers_doc))
     other_value = providers.pop("Other")  # Should be first as it is the default
     providers = {"Other": other_value, **providers}
     with open("middlewared/rclone/remote/s3_providers.py", "w") as f:
