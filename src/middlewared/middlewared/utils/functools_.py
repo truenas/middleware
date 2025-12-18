886069e2200a7fb7fc9a4f0627238616f3bb3590
themylogin <themylogin@gmail.com>
2025-12-18 13:37:44 +0100

    NAS-138993 / 26.04 / Do not call `@cache`d function twice (#17854)
    Sometimes in the middleware startup logs we see the following line
    repeated:
    ```
    [2025/12/18 04:20:21] (INFO) RDMAService._get_link_choices():72 - Fetching RDMA link netdev choices
    [2025/12/18 04:20:21] (INFO) RDMAService._get_link_choices():72 - Fetching RDMA link netdev choices
    ```
    
    `_get_link_choices` is a function wrapped with `@cache`, but if we call
    it twice at the same time, data race can occur and the underlying
    function will be called twice, which is exactly the thing we want to
    prevent. Add the [Double-checked
    locking](https://en.wikipedia.org/wiki/Double-checked_locking) to
    mitigate that.

--- src/middlewared/middlewared/utils/functools_.py
+++ src/middlewared/middlewared/utils/functools_.py
@@ -1,6 +1,7 @@
 import asyncio
 import copy
 import functools
+import threading
 
 from middlewared.utils.lang import undefined
 
@@ -9,21 +10,29 @@ def cache(func):
     value = undefined
 
     if asyncio.iscoroutinefunction(func):
+        lock = asyncio.Lock()
+
         @functools.wraps(func)
         async def wrapped(self):
             nonlocal value
 
             if value == undefined:
-                value = await func(self)
+                async with lock:
+                    if value == undefined:
+                        value = await func(self)
 
             return copy.deepcopy(value)
     else:
+        lock = threading.Lock()
+
         @functools.wraps(func)
         def wrapped(self):
             nonlocal value
 
             if value == undefined:
-                value = func(self)
+                with lock:
+                    if value == undefined:
+                        value = func(self)
 
             return copy.deepcopy(value)
 
