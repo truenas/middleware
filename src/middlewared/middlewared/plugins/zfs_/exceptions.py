cdd1f77a8b88bce6668c5254d6a28c26e64bdfbe
caleb <yocalebo@gmail.com>
2025-12-19 10:57:22 -0500

    remove ZFSSetPropertyError exception

--- src/middlewared/middlewared/plugins/zfs_/exceptions.py
+++
@@ -1,8 +0,0 @@
-from middlewared.service import CallError
-
-
-class ZFSSetPropertyError(CallError):
-    def __init__(self, property_, error):
-        self.property = property_
-        self.error = error
-        super().__init__(f'Failed to update dataset: failed to set property {self.property}: {self.error}')
