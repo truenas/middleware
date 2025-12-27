ce988d36676257117a820094b0bc4ee008266c7d
allfro <ndouba@gmail.com>
2025-12-26 21:50:44 -0500

    Remove file deletion logic in ups_config_perms
    Removed the logic that deletes UPS configuration files if not in master mode.

--- src/middlewared/middlewared/etc_files/local/nut/ups_perms.py
+++ src/middlewared/middlewared/etc_files/local/nut/ups_perms.py
@@ -14,13 +14,6 @@ UPS_DAEMONFILE = f'{UPS_CONFPATH}/upsd.conf'
 
 
 def ups_config_perms(middleware):
-    ups_config = middleware.call_sync('ups.config')
-    master_mode_files = (UPS_CONFIG, UPS_USERSFILE, UPS_DAEMONFILE)
-
-    for file in master_mode_files:
-        if ups_config['mode'].lower() != 'master':
-            os.remove(file)
-
     pathlib.Path(UPS_POWERDOWN_FLAG_FILE).unlink(missing_ok=True)
 
 
