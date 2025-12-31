0a04e3d4e2a14fab6944df52402a2279ba76d263
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2025-12-31 06:54:40 -0500

    NAS-139114 / 26.04 / fix memory leak in ssh login failure alert (#17923)
    Again, analyzing the memory allocation graph of middlewared, I saw
    ~13.3MB of memory allocated to the `journal.Reader()` object ALONE.
    Also, this is a resource leak because the `Reader()` object MUST BE
    CLOSED after use which this alert was not doing.
    
    This is particularly painful in this alert because we're creating a new
    instance every 60 seconds....painful. Either way, we don't need to use
    `journal.Reader()` object for this info. We can get the same information
    from our auditd integration. NOTE: this is dependent on
    https://github.com/truenas/audit_rules/pull/23.
    
    Finally, there is 0 reason to run this every 60 seconds since we only
    care about logs in past 24 hours.

--- src/middlewared/middlewared/alert/source/ssh_login_failures.py
+++ src/middlewared/middlewared/alert/source/ssh_login_failures.py
@@ -1,38 +1,44 @@
-from collections import deque
-from datetime import datetime, timedelta
+from datetime import timedelta
+from time import time
 
-from systemd import journal
-
-from middlewared.alert.base import AlertClass, AlertCategory, AlertLevel, Alert, ThreadedAlertSource
+from middlewared.alert.base import (
+    AlertClass,
+    AlertCategory,
+    AlertLevel,
+    Alert,
+    AlertSource,
+)
+from middlewared.alert.schedule import IntervalSchedule
 
 
 class SSHLoginFailuresAlertClass(AlertClass):
     category = AlertCategory.SYSTEM
     level = AlertLevel.WARNING
     title = "SSH Login Failures"
-    text = "%(count)d SSH login failures in the last 24 hours:\n%(failures)s"
+    text = "%(cnt)d SSH login failures in the last 24 hours"
 
 
-class SSHLoginFailuresAlertSource(ThreadedAlertSource):
-    def check_sync(self):
-        j = journal.Reader()
-        j.add_match("SYSLOG_IDENTIFIER=sshd")
-        j.seek_realtime(datetime.now() - timedelta(days=1))
-        count = 0
-        last_messages = deque([], 4)
-        for record in j:
-            if record["MESSAGE"].startswith("Failed password for"):
-                count += 1
-                last_messages.append(
-                    f"{record['__REALTIME_TIMESTAMP'].strftime('%d %b %H:%M:%S')}: {record['MESSAGE']}"
-                )
+class SSHLoginFailuresAlertSource(AlertSource):
+    schedule = IntervalSchedule(timedelta(hours=24))
 
-        if count > 0:
-            return Alert(SSHLoginFailuresAlertClass, {
-                "count": count,
-                "failures": "\n".join(
-                    ([f"... first {count - len(last_messages)} messages skipped ..."] if count > len(last_messages)
-                     else []) +
-                    list(last_messages)
-                )
-            }, key=list(last_messages))
+    async def check(self):
+        cnt = await self.middleware.call(
+            "audit.query",
+            {
+                "services": ["SYSTEM"],
+                "query-filters": [
+                    # current time minus 24h in seconds
+                    ["message_timestamp", ">=", int(time() - 86_400)],
+                    ["success", "=", False],
+                    ["event", "=", "CREDENTIAL"],
+                    ["event_data.auth_action", "=", "USER_AUTH"],
+                    ["event_data.terminal", "=", "ssh"],
+                    ["event_data.function", "=", "PAM:authentication"],
+                ],
+                "query-options": {
+                    "count": True,
+                },
+            },
+        )
+        if cnt > 0:
+            return Alert(SSHLoginFailuresAlertClass, {"cnt": cnt})
