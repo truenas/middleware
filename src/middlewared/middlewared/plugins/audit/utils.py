3c1ab9c3df4aeb24ba84dc95d1ed3e9e4a3eec32
Andrew Walker <awalker@ixsystems.com>
2026-01-02 16:36:47 -0600

    NAS-139151 / 26.04 / Properly handle OR filter auditing (#17942)
    This commit ensures that OR logic is preserved when parsing filters for
    audit queries.

--- src/middlewared/middlewared/plugins/audit/utils.py
+++ src/middlewared/middlewared/plugins/audit/utils.py
@@ -57,8 +57,11 @@ def parse_filter(filter_in, filters_out):
         if filter_in[0] != 'OR':
             raise ValueError(f'{filter_in}: invalid filter')
 
+        or_filters = []
         for f in filter_in[1]:
-            parse_filter(f, filters_out)
+            parse_filter(f, or_filters)
+
+        filters_out.append(['OR', or_filters])
 
         return
 
