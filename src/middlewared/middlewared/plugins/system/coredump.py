4a6c7b91ee55f0f9d6e01d1b1c00f18642b01951
caleb <yocalebo@gmail.com>
2026-01-02 11:23:04 -0500

    address review round 2

--- src/middlewared/middlewared/plugins/system/coredump.py
+++ src/middlewared/middlewared/plugins/system/coredump.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-from os.path import exists
+import datetime
+import os
 
 from middlewared.service import private, Service
 from middlewared.utils.journal import query_journal
 
 
 class SystemService(Service):
-
     @private
     def coredumps(self):
         coredumps = []
@@ -15,24 +14,31 @@ class SystemService(Service):
             for core in query_journal(["CODE_FUNC=submit_coredump"]):
                 # COREDUMP_TIMESTAMP is in microseconds since epoch
                 timestamp_us = int(core.get("COREDUMP_TIMESTAMP", 0))
-                timestamp = datetime.fromtimestamp(timestamp_us / 1_000_000)
+                timestamp = datetime.datetime.fromtimestamp(timestamp_us / 1_000_000)
 
+                # query_journal runs journalctl --output=json and
+                # all fields are always strings so we don't convert
+                # them here since no caller (at time of writing)
+                # depends on them being integers.
                 coredump = {
-                    'time': timestamp.strftime('%c'),
-                    'pid': int(core.get('COREDUMP_PID', 0)),
-                    'uid': int(core.get('COREDUMP_UID', 0)),
-                    'gid': int(core.get('COREDUMP_GID', 0)),
-                    'unit': core.get('COREDUMP_UNIT'),
-                    'sig': int(core.get('COREDUMP_SIGNAL', 0)),
-                    'exe': core.get('COREDUMP_EXE'),
+                    "time": timestamp.strftime("%c"),
+                    "pid": core.get("COREDUMP_PID", "0"),
+                    "uid": core.get("COREDUMP_UID", "0"),
+                    "gid": core.get("COREDUMP_GID", "0"),
+                    "unit": core.get("COREDUMP_UNIT"),
+                    "sig": core.get("COREDUMP_SIGNAL", "0"),
+                    "exe": core.get("COREDUMP_EXE"),
                 }
-                filename = core.get('COREDUMP_FILENAME')
+                filename = core.get("COREDUMP_FILENAME")
                 if not filename or not isinstance(filename, str):
-                    coredump['corefile'] = 'none'
+                    coredump["corefile"] = "none"
                 else:
-                    coredump['corefile'] = 'present' if exists(filename) else 'missing'
+                    if os.path.exists(filename):
+                        coredump["corefile"] = "present"
+                    else:
+                        coredump["corefile"] = "missing"
                 coredumps.append(coredump)
         except Exception:
-            self.logger.warning('Failed to obtain coredump information', exc_info=True)
+            self.logger.warning("Failed to obtain coredump information", exc_info=True)
 
         return coredumps
