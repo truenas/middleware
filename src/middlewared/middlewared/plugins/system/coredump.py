66e8c2b90d84f2f9dfdc5ee998f9d2f4df269a66
caleb <yocalebo@gmail.com>
2026-01-02 11:39:53 -0500

    go back to original logic

--- src/middlewared/middlewared/plugins/system/coredump.py
+++ src/middlewared/middlewared/plugins/system/coredump.py
@@ -15,22 +15,17 @@ class SystemService(Service):
                 # COREDUMP_TIMESTAMP is in microseconds since epoch
                 timestamp_us = int(core.get("COREDUMP_TIMESTAMP", 0))
                 timestamp = datetime.datetime.fromtimestamp(timestamp_us / 1_000_000)
-
-                # query_journal runs journalctl --output=json and
-                # all fields are always strings so we don't convert
-                # them here since no caller (at time of writing)
-                # depends on them being integers.
                 coredump = {
                     "time": timestamp.strftime("%c"),
-                    "pid": core.get("COREDUMP_PID", "0"),
-                    "uid": core.get("COREDUMP_UID", "0"),
-                    "gid": core.get("COREDUMP_GID", "0"),
+                    "pid": core["COREDUMP_PID"],
+                    "uid": core["COREDUMP_UID"],
+                    "gid": core["COREDUMP_GID"],
                     "unit": core.get("COREDUMP_UNIT"),
-                    "sig": core.get("COREDUMP_SIGNAL", "0"),
+                    "sig": core["COREDUMP_SIGNAL"],
                     "exe": core.get("COREDUMP_EXE"),
                 }
                 filename = core.get("COREDUMP_FILENAME")
-                if not filename or not isinstance(filename, str):
+                if not filename:
                     coredump["corefile"] = "none"
                 else:
                     if os.path.exists(filename):
