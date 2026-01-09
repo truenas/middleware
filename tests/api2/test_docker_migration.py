2354143ac4faece9076ff677149b4cc8c72da5c5
Waqar Ahmed <waqarahmedjoyia@live.com>
2026-01-10 01:06:26 +0500

    Update integration tests

--- tests/api2/test_docker_migration.py
+++ tests/api2/test_docker_migration.py
@@ -3,6 +3,7 @@ import pytest
 from middlewared.test.integration.assets.docker import docker
 from middlewared.test.integration.assets.pool import another_pool
 from middlewared.test.integration.utils import call
+from middlewared.test.integration.utils.docker import IX_APPS_MOUNT_PATH
 
 
 APP_NAME = 'actual-budget'
@@ -40,11 +41,26 @@ def test_install_docker_apps(docker_pool):
 
 
 def test_docker_app_migration(docker_pool, migration_pool):
+    source_pool_name = docker_pool['pool']
     try:
         call('docker.update', {'pool': migration_pool['name'], 'migrate_applications': True}, job=True)
         assert call('docker.config')['pool'] == migration_pool['name']
         for app_name in (APP2_NAME, APP_NAME):
             assert call('app.get_instance', app_name)['name'] == app_name
+
+        # Verify source pool's ix-apps has inherited mountpoint (not /.ix-apps)
+        source_ix_apps = call(
+            'zfs.resource.query',
+            {'paths': [f'{source_pool_name}/ix-apps'], 'properties': ['mountpoint']}
+        )
+        assert source_ix_apps[0]['properties']['mountpoint']['value'] != IX_APPS_MOUNT_PATH
+
+        # Verify destination pool's ix-apps has the correct /.ix-apps mountpoint
+        dest_ix_apps = call(
+            'zfs.resource.query',
+            {'paths': [f'{migration_pool["name"]}/ix-apps'], 'properties': ['mountpoint']}
+        )
+        assert dest_ix_apps[0]['properties']['mountpoint']['value'] == IX_APPS_MOUNT_PATH
     finally:
         call('docker.update', {'pool': None}, job=True)
         assert call('docker.config')['pool'] is None
