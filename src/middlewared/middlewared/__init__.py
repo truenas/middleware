ce71f67b22efd6a6f84b9b3d79338900312af34f
themylogin <themylogin@gmail.com>
2026-01-15 19:02:43 +0100

    NAS-139335 / 26.04 / Fix docs build (#18043)
    Just set `FAKE_ENV=1` env variable on the systems where truenas_pylibzfs
    is not available, and it won't be imported.

--- src/middlewared/middlewared/__init__.py
+++ src/middlewared/middlewared/__init__.py
@@ -5,3 +5,10 @@
 # when running setup_test.py
 # (it checks for presence of declare_namespace in __init__.py so the above paragraph
 # alone does the job)
+
+import os
+from .fake_env import setup_fake_middleware_env
+
+
+if os.environ.get("FAKE_ENV"):
+    setup_fake_middleware_env()
