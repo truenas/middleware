8d4662fd19abbdb0d17570ae84558088ea389fbd
themylogin <themylogin@gmail.com>
2026-01-22 22:44:54 +0100

    Rollback `defer_build=True`

--- src/middlewared/middlewared/api/base/handler/dump_params.py
+++ src/middlewared/middlewared/api/base/handler/dump_params.py
@@ -1,7 +1,6 @@
 import itertools
 
 from middlewared.api.base import BaseModel
-from middlewared.api.base.model import ensure_model_ready
 from middlewared.service_exception import ValidationErrors
 from .accept import accept_params
 from .remove_secrets import remove_secrets
@@ -18,7 +17,6 @@ def dump_params(model: type[BaseModel], args: list, expose_secrets: bool) -> lis
     :param expose_secrets: if false, will replace `Secret` parameters with a placeholder.
     :return: A list of method call arguments ready to be printed.
     """
-    ensure_model_ready(model)
     try:
         return accept_params(model, args, exclude_unset=True, expose_secrets=expose_secrets)
     except ValidationErrors:
--- src/middlewared/middlewared/utils/pydantic_.py
+++ src/middlewared/middlewared/utils/pydantic_.py
@@ -1,6 +1,2 @@
-from middlewared.api.base.model import ensure_model_ready
-
-
 def model_json_schema(model, *args, **kwargs):
-    ensure_model_ready(model)
     return model.model_json_schema(*args, **kwargs)
