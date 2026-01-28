0aa5d2b7b319a5d4db0dab69d8d7c7054fe3b57d
Andrew Walker <andrew.walker@truenas.com>
2026-01-28 15:23:24 -0600

    Fix

--- src/middlewared/middlewared/plugins/system_dataset/utils.py
+++ src/middlewared/middlewared/plugins/system_dataset/utils.py
@@ -1,3 +1,4 @@
 SYSDATASET_PATH = '/var/db/system'
 POOL_SYSDATASET_PREFIX = '/var/db/system_'
-TMP_SYSDATASET_PREFIX = '/var/run/middleware/.system_'
+# This needs to be in /var/db because move_mount requires MS_PRIVATE
+TMP_SYSDATASET_PREFIX = '/var/db/.system_'
