7c553e546a05c990160efeeedfaf6091bb0972af
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-01-30 14:32:46 -0500

    NAS-139542 / 25.10.2 / In become_active ensure all user-related config (by anodos325) (#18126)
    This commit ensures that when a controller becomes active, we regenerate
    the user and API key configuration so that there's no chance of stale
    information being preset.
    
    Original PR: https://github.com/truenas/middleware/pull/18125
    
    ---------
    
    Co-authored-by: Andrew Walker <andrew.walker@truenas.com>

--- src/middlewared/middlewared/plugins/failover_/event.py
+++ src/middlewared/middlewared/plugins/failover_/event.py
@@ -540,6 +540,14 @@ class FailoverEventsService(Service):
         self.run_call('vrrpthread.unpause_events')
         logger.info('Done unpausing failover event processing')
 
+        # Ensure that middleware pam configuration and API key material is generated
+        # as well as local account configuration and OATH information
+        try:
+            self.run_call('etc.generate', 'user')
+            self.run_call('etc.generate', 'pam_middleware')
+        except Exception:
+            logger.exception("Failed to regenerate user info")
+
         # Kick off a job to clean up any left-over ALUA state from when we were STANDBY/BACKUP.
         logger.info('Verifying iSCSI service')
         iscsi_suspended = iscsi_cleaned = False
