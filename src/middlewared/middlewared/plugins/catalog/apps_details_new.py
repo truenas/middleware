b475890f02cb614a8e2d8d295168ab44f2a58fe1
Waqar Ahmed <waqarahmedjoyia@live.com>
2026-02-08 21:03:57 +0500

    Move retrieve_recommended_apps to a separate func

--- src/middlewared/middlewared/plugins/catalog/apps_details_new.py
+++ src/middlewared/middlewared/plugins/catalog/apps_details_new.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import contextlib
 from typing import Any
 
+from catalog_reader.recommended_apps import retrieve_recommended_apps as retrieve_recommended_apps_from_catalog_reader
 from pydantic import BaseModel, ConfigDict, Field
 
 from middlewared.api.current import (
@@ -28,3 +30,14 @@ async def get_normalized_questions_context(context: ServiceContext) -> Normalize
         'ip_choices': await context.middleware.call('app.ip_choices'),
         'gpu_choices': await context.middleware.call('app.gpu_choices_internal'),
     })
+
+
+async def retrieve_recommended_apps(context: ServiceContext, cache: bool = True) -> dict[str, list[str]]:
+    cache_key = 'recommended_apps'
+    if cache:
+        with contextlib.suppress(KeyError):
+            return await context.middleware.call('cache.get', cache_key)
+
+    data = retrieve_recommended_apps_from_catalog_reader((await context.middleware.call('catalog.config'))['location'])
+    await context.middleware.call('cache.put', cache_key, data)
+    return data
