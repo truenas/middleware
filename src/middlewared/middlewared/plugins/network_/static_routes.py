b513119a1237517bcc2e68c58d10da015a61243a
caleb <yocalebo@gmail.com>
2026-02-11 13:40:48 -0500

    add better static route

--- src/middlewared/middlewared/plugins/network_/static_routes.py
+++ src/middlewared/middlewared/plugins/network_/static_routes.py
@@ -1,6 +1,4 @@
-from ipaddress import ip_interface, ip_address
-
-from truenas_pynetif.routing import Route, RoutingTable
+from ipaddress import ip_address, ip_interface, ip_network
 
 from middlewared.api import api_method
 from middlewared.api.current import (
@@ -13,12 +11,16 @@ from middlewared.api.current import (
     StaticRouteDeleteResult,
 )
 import middlewared.sqlalchemy as sa
+from middlewared.plugins.network_.static_routes_sync import (
+    sync_impl as staticroute_sync_impl,
+)
 from middlewared.service import CRUDService, private
-from middlewared.service_exception import ValidationErrors
+from middlewared.service_exception import ValidationError
+from truenas_pynetif.address.netlink import delete_route, netlink_route
 
 
 class StaticRouteModel(sa.Model):
-    __tablename__ = 'network_staticroute'
+    __tablename__ = "network_staticroute"
 
     id = sa.Column(sa.Integer(), primary_key=True)
     sr_destination = sa.Column(sa.String(120))
@@ -28,18 +30,18 @@ class StaticRouteModel(sa.Model):
 
 class StaticRouteService(CRUDService):
     class Config:
-        datastore = 'network.staticroute'
-        datastore_prefix = 'sr_'
-        cli_namespace = 'network.static_route'
+        datastore = "network.staticroute"
+        datastore_prefix = "sr_"
+        cli_namespace = "network.static_route"
         entry = StaticRouteEntry
-        role_prefix = 'NETWORK_INTERFACE'
+        role_prefix = "NETWORK_INTERFACE"
 
     @api_method(
         StaticRouteCreateArgs,
         StaticRouteCreateResult,
-        audit='Static route create'
+        audit="Static route create"
     )
-    async def do_create(self, data):
+    def do_create(self, data):
         """
         Create a Static Route.
 
@@ -47,115 +49,94 @@ class StaticRouteService(CRUDService):
 
         `description` is an optional attribute for any notes regarding the static route.
         """
-        verrors = ValidationErrors()
-        self._validate('staticroute_create', verrors, data)
-        verrors.check()
-
-        id_ = await self.middleware.call(
-            'datastore.insert', self._config.datastore, data,
-            {'prefix': self._config.datastore_prefix})
-
-        await (await self.middleware.call('service.control', 'RESTART', 'routing')).wait(raise_error=True)
-
-        return await self.get_instance(id_)
+        self._validate("staticroute_create", data)
+        id_ = self.middleware.call_sync(
+            "datastore.insert",
+            self._config.datastore,
+            data,
+            {"prefix": self._config.datastore_prefix},
+        )
+        self.sync()
+        return self.get_instance__sync(id_)
 
     @api_method(
         StaticRouteUpdateArgs,
         StaticRouteUpdateResult,
-        audit='Static route update'
+        audit="Static route update"
     )
-    async def do_update(self, id_, data):
+    def do_update(self, id_, data):
         """
         Update Static Route of `id`.
         """
-        old = await self.get_instance(id_)
+        old = self.get_instance__sync(id_)
         new = old.copy()
         new.update(data)
+        if new == old:
+            return new
 
-        verrors = ValidationErrors()
-        self._validate('staticroute_update', verrors, new)
-        verrors.check()
-
-        await self.middleware.call(
-            'datastore.update', self._config.datastore, id_, new,
-            {'prefix': self._config.datastore_prefix})
-
-        await (await self.middleware.call('service.control', 'RESTART', 'routing')).wait(raise_error=True)
-
-        return await self.get_instance(id_)
+        self._validate("staticroute_update", new)
+        self.middleware.call_sync(
+            "datastore.update",
+            self._config.datastore,
+            id_,
+            new,
+            {"prefix": self._config.datastore_prefix},
+        )
+        self.sync()
+        return self.get_instance__sync(id_)
 
     @api_method(
         StaticRouteDeleteArgs,
         StaticRouteDeleteResult,
-        audit='Static route delete'
+        audit="Static route delete"
     )
     def do_delete(self, id_):
         """
         Delete Static Route of `id`.
         """
-        st = self.middleware.call_sync('staticroute.get_instance', id_)
-        rv = self.middleware.call_sync('datastore.delete', self._config.datastore, id_)
+        st = self.get_instance__sync(id_)
+        rv = self.middleware.call_sync("datastore.delete", self._config.datastore, id_)
         try:
-            rt = RoutingTable()
-            rt.delete(self._netif_route(st))
+            network = ip_network(st["destination"], strict=False)
+            with netlink_route() as sock:
+                delete_route(
+                    sock,
+                    dst=network.network_address.exploded,
+                    dst_len=network.prefixlen,
+                    gateway=st["gateway"],
+                )
         except Exception:
-            self.logger.exception('Failed to delete static route %r', st['destination'])
+            self.logger.exception("Failed to delete static route %r", st["destination"])
 
         return rv
 
     @private
     def sync(self):
-        new_routes = list()
-        for route in self.middleware.call_sync('staticroute.query'):
-            new_routes.append(self._netif_route(route))
+        """Synchronize kernel static routes with the database configuration."""
+        staticroute_sync_impl(self)
 
-        rt = RoutingTable()
-        default_route_ipv4 = rt.default_route_ipv4
-        default_route_ipv6 = rt.default_route_ipv6
-        for route in rt.routes:
-            if route in new_routes:
-                new_routes.remove(route)
-                continue
-
-            if route not in [default_route_ipv4, default_route_ipv6] and route.gateway is not None:
-                self.logger.debug('Removing route %r', route.asdict())
-                try:
-                    rt.delete(route)
-                except Exception:
-                    self.logger.exception('Failed to remove route')
-
-        for route in new_routes:
-            self.logger.debug('Adding route %r', route.asdict())
-            try:
-                rt.add(route)
-            except Exception:
-                self.logger.exception('Failed to add route')
-
-    def _validate(self, schema_name, verrors, data):
-        dst, gw = data.pop('destination'), data.pop('gateway')
-        # Validate destination: CIDR notation
+    @private
+    def _validate(self, schema_name: str, data: dict) -> None:
+        dst, gw = data.pop("destination"), data.pop("gateway")
         try:
+            # Validate destination: CIDR notation
             dst = ip_interface(dst)
         except ValueError as ve:
-            verrors.add(f'{schema_name}.destination', str(ve))
+            raise ValidationError(f"{schema_name}.destination", str(ve))
 
-        # Validate gateway: IP address (not CIDR)
         try:
+            # Validate gateway: IP address (not CIDR)
             gw = ip_address(gw)
         except ValueError as ve:
-            verrors.add(f'{schema_name}.gateway', str(ve))
+            raise ValidationError(f"{schema_name}.gateway", str(ve))
 
         if dst.version != gw.version:
-            verrors.add(
-                f'{schema_name}.destination',
-                'Destination and gateway address families must match'
+            raise ValidationError(
+                f"{schema_name}.destination",
+                "Destination and gateway address families must match",
             )
         else:
-            data['destination'] = dst.exploded
-            data['gateway'] = gw.exploded
+            data["destination"] = dst.exploded
+            data["gateway"] = gw.exploded
 
-    def _netif_route(self, staticroute):
-        ip_info = ip_interface(staticroute['destination'])
-        return Route(
-            str(ip_info.ip), str(ip_info.netmask), gateway=staticroute['gateway']
-        )
+
