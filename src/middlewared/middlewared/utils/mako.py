5d2e0a73a540d7878a3dd66c23106c5ca5e12baa
themylogin <themylogin@gmail.com>
2026-02-12 18:12:20 +0100

    Remove mako filters, they are not needed anymore

--- src/middlewared/middlewared/utils/mako.py
+++ src/middlewared/middlewared/utils/mako.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from mako.lookup import TemplateLookup
+from mako.template import Template
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +12,8 @@ __all__ = ["get_template"]
 lookup = TemplateLookup(
     directories=[os.path.dirname(os.path.dirname(__file__))],
     module_directory="/run/mako",
-    imports=["from middlewared.utils.mako_filters import indent, json, markdown"]
 )
 
 
-def get_template(name):
+def get_template(name: str) -> Template:
     return lookup.get_template(name)
