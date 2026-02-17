07056a755ae3a75a447c26734d0ce63d90565d13
Brian M <brian.meagher@ixsystems.com>
2026-02-17 14:59:03 -0800

    Remove use of parted in tests

--- tests/api2/test_disk_format.py
+++ tests/api2/test_disk_format.py
@@ -1,10 +1,11 @@
+import copy
 import json
 import time
 
 from middlewared.test.integration.utils import call, ssh
 
 """
-We use 'parted' to partition disks.
+We now use 'sgdisk' to partition disks.
 Verification is based on 'parted' documentation (https://people.redhat.com/msnitzer/docs/io-limits.txt):
     The heuristic parted uses is:
     1)  Always use the reported 'alignment_offset' as the offset for the
@@ -27,28 +28,44 @@ ONE_MB = 1048576
 DATA_TYPE_UUID = "6a898cc3-1dd2-11b2-99a6-080020736631"
 
 
-def get_parted_info(disk_path):
+def get_partition_info(disk_path):
     # By the time this is called, the disk has been formatted
     # but the kernel might not have been made fully aware of the changes
     # so let's retry a bit before failing
     for i in range(10):
-        pbytes = json.loads(ssh(f'parted {disk_path} unit b p --json'))['disk']
-        if pbytes.get('partitions') is None:
-            time.sleep(1)
-        else:
-            break
-    else:
-        assert False, f'parted tool failed to find partitions (in bytes) on {disk_path!r} ({pbytes!r})'
-
-    for i in range(10):
-        psectors = json.loads(ssh(f'parted {disk_path} unit s p --json'))['disk']
+        psectors = json.loads(ssh(f'sfdisk --json {disk_path}'))['partitiontable']
         if psectors.get('partitions') is None:
             time.sleep(1)
         else:
             break
     else:
-        assert False, f'parted tool failed to find partitions (in sectors) on {disk_path!r} ({psectors!r})'
+        assert False, f'sfdisk tool failed to find partitions (in sectors) on {disk_path!r} ({psectors!r})'
 
+    # Now pickup the end sector (rather than calc it)
+    end_sectors = {}
+    result = ssh(f"sfdisk -l --output Device,End {disk_path} | grep -A20 '^Device' | grep -v '^Device'")
+    for line in result.splitlines():
+        if line:
+            try:
+                device, endsector = line.split()
+                end_sectors[device] = int(endsector)
+            except ValueError:
+                continue
+    for partition in psectors['partitions']:
+        try:
+            partition['end'] = end_sectors[partition['node']]
+        except KeyError:
+            continue
+
+    # Unlike parted, sfdisk only works with sectors.  Calculate bytes
+    pbytes = copy.deepcopy(psectors)
+    sectorsize = pbytes['sectorsize']
+    for partition in pbytes['partitions']:
+        partition['start'] *= sectorsize
+        # Apparently 'end' is the last byte in that last sector
+        partition['end'] = (partition['end'] * sectorsize) + (sectorsize - 1)
+        partition['size'] *= sectorsize
+    pbytes['units'] = 'bytes'
     return pbytes, psectors
 
 
@@ -66,25 +83,25 @@ def test_disk_format_and_wipe():
     partition = partitions[0]
     assert partition['partition_type'] == DATA_TYPE_UUID
 
-    # we used libparted to format a drive so let's
-    # validate our API matches parted output (NOTE:
+    # We now use sgdisk to format a drive so let's
+    # validate our API matches sfdisk output (NOTE:
     # we check both bytes and sectors)
-    parted_bytes, parted_sectors = get_parted_info(f'/dev/{unused[0]["name"]}')
+    sfdisk_bytes, sfdisk_sectors = get_partition_info(f'/dev/{unused[0]["name"]}')
 
-    # sanity check (make sure parted shows same number of partitions)
-    assert len(parted_bytes['partitions']) == len(partitions), parted_bytes['partitions']
-    assert len(parted_sectors['partitions']) == len(partitions), parted_sectors['partitions']
+    # sanity check (make sure sfdisk shows same number of partitions)
+    assert len(sfdisk_bytes['partitions']) == len(partitions), sfdisk_bytes['partitions']
+    assert len(sfdisk_sectors['partitions']) == len(partitions), sfdisk_sectors['partitions']
 
     # validate our API shows proper start/end sizes in bytes
-    pbyte = parted_bytes['partitions'][0]
-    assert int(pbyte['size'].split('B')[0]) == partition['size']
-    assert int(pbyte['start'].split('B')[0]) == partition['start']
-    assert int(pbyte['end'].split('B')[0]) == partition['end']
+    pbyte = sfdisk_bytes['partitions'][0]
+    assert pbyte['size'] == partition['size']
+    assert pbyte['start'] == partition['start']
+    assert pbyte['end'] == partition['end']
 
     # validate our API shows proper start/end sizes in sectors
-    psect = parted_sectors['partitions'][0]
-    assert int(psect['start'].split('s')[0]) == partition['start_sector']
-    assert int(psect['end'].split('s')[0]) == partition['end_sector']
+    psect = sfdisk_sectors['partitions'][0]
+    assert psect['start'] == partition['start_sector']
+    assert psect['end'] == partition['end_sector']
 
     # verify wipe disk should removes partition labels
     call('disk.wipe', partition['disk'], 'QUICK', job=True)
@@ -92,6 +109,6 @@ def test_disk_format_and_wipe():
     new_parts = call('disk.list_partitions', partition['disk'])
     assert len(new_parts) == 0, new_parts
 
-    # sanity check, make sure parted doesn't see partitions either
-    pbytes = json.loads(ssh(f'parted /dev/{unused[0]["name"]} unit b p --json'))['disk']
-    assert pbytes.get('partitions') is None, repr(pbytes)
+    # sanity check, make sure sfdisk doesn't see partitions either
+    result = ssh(f'sfdisk --json /dev/{unused[0]["name"]}', check=False, complete_response=True)
+    assert 'does not contain a recognized partition table' in result["stderr"]
