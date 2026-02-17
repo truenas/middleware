99421a44d4b76c7bc2417398dac8c5d82427d8d2
Logan Cary <logan.cary@ixsystems.com>
2026-02-17 14:49:39 -0500

    remove local imports

--- tests/api2/test_usage_reporting.py
+++ tests/api2/test_usage_reporting.py
@@ -5,9 +5,13 @@ import time
 import pytest
 
 from auto_config import password, pool_name, user
+from middlewared.test.integration.assets.ftp import ftp_server
+from middlewared.test.integration.assets.nfs import nfs_server
+from middlewared.test.integration.assets.pool import dataset as nfs_dataset
 from middlewared.test.integration.utils import call, ssh
 from middlewared.test.integration.utils.client import truenas_server, client
 from middlewared.test.integration.utils.shell import webshell_exec
+from protocols import SSH_NFS, ftp_connection, nfs_share
 
 
 class GatherTypes:
@@ -74,10 +78,6 @@ def test_nfs_reporting(get_usage_sample):
     # Initial state should have NFSv[3,4] and possibly some stale NFSv3 connections from previous tests
     assert set(get_usage_sample['NFS']['enabled_protocols']) == set(["NFSV3", "NFSV4"])
 
-    from middlewared.test.integration.assets.nfs import nfs_server
-    from middlewared.test.integration.assets.pool import dataset as nfs_dataset
-    from protocols import SSH_NFS, nfs_share
-
     nfs_path = f'/mnt/{pool_name}/test_nfs'
     with nfs_dataset("test_nfs"):
         with nfs_share(nfs_path):
@@ -97,9 +97,6 @@ def test_ftp_reporting(get_usage_sample):
     """ Confirm we are correctly reporting the number of connections """
     # Initial state should have no connections
     assert get_usage_sample['FTP']['num_connections'] == 0
-
-    from middlewared.test.integration.assets.ftp import ftp_server
-    from protocols import ftp_connection
 
     # Establish two connections
     with ftp_server():
