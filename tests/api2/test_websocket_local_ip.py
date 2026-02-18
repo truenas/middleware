31d09d2c813a5c083e1244dfaeedc5f4692ab908
Andrew Walker <andrew.walker@truenas.com>
2026-02-18 10:30:05 -0600

    Remove tests

--- tests/api2/test_websocket_local_ip.py
+++
@@ -1,8 +0,0 @@
-from middlewared.test.integration.utils import call
-from middlewared.test.integration.utils.client import truenas_server
-
-
-def test_websocket_local_ip():
-    """This tests to ensure we return the local IP address
-    of the TrueNAS system based on the websocket session."""
-    assert call("interface.websocket_local_ip") == truenas_server.ip
