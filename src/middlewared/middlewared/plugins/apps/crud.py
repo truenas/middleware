6673a42cd2fe7aef92c1dabd10e7c6b5a2e7a81e
sonicaj <waqarahmedjoyia@live.com>
2026-02-21 00:12:33 +0400

    NAS-139639 / 26.0.0-BETA.1 / Add validation to prevent multiple instances of flagged app (#18248)
    This commit adds changes to prevent installation of an app which has
    already been installed and is marked as an app which cannot have
    multiple instances installed.

--- src/middlewared/middlewared/plugins/apps/crud.py
+++ src/middlewared/middlewared/plugins/apps/crud.py
@@ -18,7 +18,7 @@ from middlewared.utils.filter_list import filter_list
 from .compose_utils import collect_logs, compose_action
 from .custom_app_utils import validate_payload
 from .ix_apps.lifecycle import add_context_to_values, get_current_app_config, update_app_config
-from .ix_apps.metadata import update_app_metadata, update_app_metadata_for_portals
+from .ix_apps.metadata import get_collective_metadata, update_app_metadata, update_app_metadata_for_portals
 from .ix_apps.path import get_app_parent_volume_ds, get_installed_app_path, get_installed_app_version_path
 from .ix_apps.query import list_apps
 from .ix_apps.setup import setup_install_app_dir
@@ -145,6 +145,22 @@ class AppService(CRUDService):
 
         if version not in complete_app_details.versions:
             raise CallError(f'Version {version} not found in {data["catalog_app"]} app', errno=errno.ENOENT)
+
+        app_metadata = complete_app_details.versions[version].get('app_metadata') or {}
+        annotations = app_metadata.get('annotations') or {}
+        if annotations.get('disallow_multiple_instances'):
+            # We will like to raise validation error if multiple instances of the app in question cannot
+            # be installed at the same time
+            catalog_app = data['catalog_app']
+            train = data['train']
+            for installed_app in get_collective_metadata().values():
+                installed_app_metadata = installed_app.get('metadata') or {}
+                if installed_app_metadata.get('name') == catalog_app and installed_app_metadata.get('train') == train:
+                    verrors.add(
+                        'app_create.catalog_app',
+                        f'{catalog_app!r} app does not allow multiple instances',
+                    )
+                    verrors.check()
 
         return self.create_internal(job, app_name, version, data['values'], complete_app_details)
 
