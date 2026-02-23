92b5827aa39fcaa75e6caf4d5da512cf48062697
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-02-23 10:03:30 -0500

    NAS-139938 / 25.10.2.1 / Fix ClientConnectorCertificateError object has no attribute _os_error (by themylogin) (#18275)
    Automatic cherry-pick failed. Please resolve conflicts by running:
    
        git reset --hard HEAD~1
        git cherry-pick -x d9a40f93a083c538cdc1dac8f26437fec6a654ac
    
    If the original PR was merged via a squash, you can just cherry-pick the
    squashed commit:
    
        git reset --hard HEAD~1
        git cherry-pick -x 5966611429782bc7b9b295664a15441406c1147c
    
    
    
    Original PR: https://github.com/truenas/middleware/pull/18274
    
    Co-authored-by: themylogin <themylogin@gmail.com>

--- src/middlewared/middlewared/plugins/update_/trains.py
+++ src/middlewared/middlewared/plugins/update_/trains.py
@@ -30,7 +30,13 @@ class UpdateService(Service):
                 async with client.get(url) as resp:
                     return await resp.json()
             except ClientError as e:
-                if isinstance(e, ClientConnectorError) and e.os_error.errno == errno.ENETUNREACH:
+                # Some `aiohttp.ClientConnectorError` subclasses (i.e. `ClientConnectorCertificateError`) do not have
+                # `os_error` attribute despite the parent class having one.
+                if (
+                    isinstance(e, ClientConnectorError) and
+                    hasattr(e, 'os_error') and
+                    e.os_error.errno == errno.ENETUNREACH
+                ):
                     error = errno.ENETUNREACH
                 else:
                     error = errno.ECONNRESET
