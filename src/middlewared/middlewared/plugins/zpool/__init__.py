fbfbd9939ec76a96ea3a2d98fe5157971c64dbb7
caleb <yocalebo@gmail.com>
2026-03-06 10:14:59 -0500

    badddd

--- src/middlewared/middlewared/plugins/zpool/__init__.py
+++ src/middlewared/middlewared/plugins/zpool/__init__.py
@@ -1,15 +1,11 @@
 from .get_zpool_disks_impl import get_zpool_disks_impl
 from .get_zpool_features_impl import get_zpool_features_impl
-from .get_zpool_status_impl import get_zpool_status_impl
-from .get_zpool_topology_impl import get_zpool_topology_impl
 from .query_impl import query_impl
 from .upgrade_zpool_impl import upgrade_zpool_impl
 
 __all__ = (
     "get_zpool_disks_impl",
     "get_zpool_features_impl",
-    "get_zpool_status_impl",
-    "get_zpool_topology_impl",
     "query_impl",
     "upgrade_zpool_impl",
 )
