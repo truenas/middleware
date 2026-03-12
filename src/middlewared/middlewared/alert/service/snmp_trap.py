7fb1a76b2ea721dca422b3e4a03275ee8837fffc
Ameer Hamza <ahamza@ixsystems.com>
2026-03-13 03:10:06 +0500

    Fix SNMP trap alerts failing to load TRUENAS-MIB
    Commit 6a01cb9b removed the pre-compiled TRUENAS-MIB.py from
    /usr/local/share/pysnmp/mibs/, but the SNMP trap alert service still
    expected to load the MIB as a pre-compiled .py file from that path.
    Replace with pySMI's addMibCompiler to compile the ASN.1 .txt MIB source
    on-the-fly instead.
    
    (cherry picked from commit fe66b2efd3de268454d5d0e7c637ef276245e3c9)

--- src/middlewared/middlewared/alert/service/snmp_trap.py
+++ src/middlewared/middlewared/alert/service/snmp_trap.py
@@ -1,5 +1,6 @@
 import pysnmp.hlapi
 import pysnmp.smi
+import pysnmp.smi.compiler
 
 from middlewared.alert.base import ThreadedAlertService
 
@@ -51,9 +52,11 @@ class SNMPTrapAlertService(ThreadedAlertService):
             self.context_data = pysnmp.hlapi.ContextData()
 
             mib_builder = pysnmp.smi.builder.MibBuilder()
-            mib_sources = mib_builder.getMibSources() + (
-                pysnmp.smi.builder.DirMibSource("/usr/local/share/pysnmp/mibs"),)
-            mib_builder.setMibSources(*mib_sources)
+            # Use pySMI compiler to compile the ASN.1 .txt MIB on-the-fly into Python format
+            pysnmp.smi.compiler.addMibCompiler(
+                mib_builder,
+                sources=['/usr/local/share/snmp/mibs/'] + pysnmp.smi.compiler.defaultSources,
+            )
             mib_builder.loadModules("TRUENAS-MIB")
             self.snmp_alert_level_type = mib_builder.importSymbols("TRUENAS-MIB", "AlertLevelType")[0]
             mib_view_controller = pysnmp.smi.view.MibViewController(mib_builder)
