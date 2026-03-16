21703f616949a472b2f3d62ea41f696ce0cb4280
themylogin <themylogin@gmail.com>
2026-03-16 15:19:13 +0100

    Also handle dicts in `get_mock_return_model`

--- src/middlewared/middlewared/utils/mock.py
+++ src/middlewared/middlewared/utils/mock.py
@@ -2,9 +2,11 @@ from typing import Any, Literal
 
 from pydantic import BaseModel
 
-from middlewared.api.base.handler.inspect import model_field_is_model, model_field_is_list_of_models
+from middlewared.api.base.handler.inspect import (
+    model_field_is_model, model_field_is_list_of_models, model_field_is_dict_of_models,
+)
 
-MockReturnModel = tuple[Literal['model', 'list'], type[BaseModel]]
+MockReturnModel = tuple[Literal['model', 'list', 'dict'], type[BaseModel]]
 
 
 def coerce_mock_result(result: Any, mock_return_model: MockReturnModel | None) -> Any:
@@ -22,6 +24,12 @@ def coerce_mock_result(result: Any, mock_return_model: MockReturnModel | None) -
             entry_type.model_construct(**item) if isinstance(item, dict) else item
             for item in result
         ]
+
+    if kind == 'dict' and isinstance(result, dict):
+        return {
+            k: entry_type.model_construct(**v) if isinstance(v, dict) else v
+            for k, v in result.items()
+        }
 
     return result
 
@@ -52,5 +60,11 @@ def get_mock_return_model(serviceobj: Any, methodobj: Any) -> MockReturnModel | 
         and isinstance(model, type) and issubclass(model, BaseModel)
     ):
         return ('list', model)
+
+    if (
+        (model := model_field_is_dict_of_models(annotation))
+        and isinstance(model, type) and issubclass(model, BaseModel)
+    ):
+        return ('dict', model)
 
     return None
