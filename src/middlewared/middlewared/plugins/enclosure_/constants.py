df6cd5048084e591a67f0c43af3ff0afdeb4d17d
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-04-03 14:24:49 -0400

    NAS-140540 / 26.0.0-BETA.2 / fix R60 enclosure mapping (by yocalebo) (#18645)
    We have another internal R60 that doesn't show any enclosures at all.
    The reason why is that its system product name (in DMI table) is flashed
    with the `-SC` suffix....
    
    This was supposed to have been accounted for in
    https://github.com/truenas/middleware/commit/5adab5d81bd8e2996828b7b32193c7a87548ff1d
    and we even tested on another internal R60 platform and IT WORKED.
    
    The issue is that the R60 that was tested against is NOT flashed with
    any suffixes.... which revealed the fact that the nvme mapping code did
    not follow same logic and the R60 is an all NVMe system. To remedy the
    situation add a utility function that unifies the model transformation
    into 1 place so, ideally, this won't be as easy to do in the future....
    
    Original PR: https://github.com/truenas/middleware/pull/18641
    
    Co-authored-by: caleb <yocalebo@gmail.com>

--- src/middlewared/middlewared/plugins/enclosure_/constants.py
+++ src/middlewared/middlewared/plugins/enclosure_/constants.py
@@ -1,12 +1,20 @@
-SYSFS_SLOT_KEY = 'sysfs_slot'
-MAPPED_SLOT_KEY = 'mapped_slot'
-SUPPORTS_IDENTIFY_KEY = 'supports_identify_light'
-SUPPORTS_IDENTIFY_STATUS_KEY = 'supports_identify_light_status'
-DRIVE_BAY_LIGHT_STATUS = 'drive_bay_light_status'
-MINI_MODEL_BASE = 'MINI'
-MINIR_MODEL_BASE = f'{MINI_MODEL_BASE}-R'
+SYSFS_SLOT_KEY = "sysfs_slot"
+MAPPED_SLOT_KEY = "mapped_slot"
+SUPPORTS_IDENTIFY_KEY = "supports_identify_light"
+SUPPORTS_IDENTIFY_STATUS_KEY = "supports_identify_light_status"
+DRIVE_BAY_LIGHT_STATUS = "drive_bay_light_status"
+MINI_MODEL_BASE = "MINI"
+MINIR_MODEL_BASE = f"{MINI_MODEL_BASE}-R"
 HEAD_UNIT_DISK_SLOT_START_NUMBER = 1
-DISK_FRONT_KEY = 'is_front'
-DISK_REAR_KEY = 'is_rear'
-DISK_TOP_KEY = 'is_top'
-DISK_INTERNAL_KEY = 'is_internal'
+DISK_FRONT_KEY = "is_front"
+DISK_REAR_KEY = "is_rear"
+DISK_TOP_KEY = "is_top"
+DISK_INTERNAL_KEY = "is_internal"
+
+# These are suffixes that get added on to the end
+# of models that we flash in DMI for the various
+# platforms we sell. We must remove them when we
+# enumerate enclosures and friends so model/platform
+# detection is done properly.
+# NOTE: "-PC", "-SC", and "-C" are used on R60 platform
+DMI_SUFFIXES_TO_REMOVE = ("-HA", "-S", "-PC", "-SC", "-C")
