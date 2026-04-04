b959ecd6a0478f040ad48ff72a7615989f223087
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-04-04 11:06:14 -0400

    NAS-140543 / 26.0.0-BETA.2 / fix KeyError in CRUDService.query() (by yocalebo) (#18650)

--- src/middlewared/middlewared/service/crud_service.py
+++ src/middlewared/middlewared/service/crud_service.py
@@ -136,6 +136,9 @@ class CRUDService[E](ServiceChangeMixin, Service, metaclass=CRUDServiceMetabase)
         options['extend_context'] = self._config.datastore_extend_context
         options['extend_fk'] = self._config.datastore_extend_fk
         options['prefix'] = self._config.datastore_prefix
+        options.setdefault('force_sql_filters', False)
+        options.setdefault('count', False)
+        options.setdefault('get', False)
         return options
 
     async def query(self, filters: list | None = None, options: dict | None = None) -> list[E] | E | int:
