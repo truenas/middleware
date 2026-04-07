effa43beb2d34c8ad2b3550ad9a282b28ae93946
Andrew Walker <andrew.walker@truenas.com>
2026-04-07 06:54:20 -0500

    Fix migration again

--- src/middlewared/middlewared/alembic/versions/27.0/2026-04-07_00-00_merge.py
+++ src/middlewared/middlewared/alembic/versions/27.0/2026-04-07_00-00_merge.py
@@ -1,8 +1,8 @@
 """Merge migration for ZFS tiering feature
 
 Revision ID: b7c8d9e0f1a2
-Revises: e854b7d2ae59, a1b2c3d4e5f6
-Create Date: 2026-03-23 00:00:00.000000+00:00
+Revises: 774c3c7cb4ad, a1b2c3d4e5f6
+Create Date: 2026-04-07 00:00:00.000000+00:00
 
 """
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "b7c8d9e0f1a2"
-down_revision = ("e854b7d2ae59", "a1b2c3d4e5f6")
+down_revision = ("774c3c7cb4ad", "a1b2c3d4e5f6")
 branch_labels = None
 depends_on = None
 
