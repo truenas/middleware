d6ad1a1d86c4823e932318a7a8e755142b5e5db0
caleb <yocalebo@gmail.com>
2026-04-08 10:45:27 -0400

    fix gathering service failure logs
    (cherry picked from commit 5a0f864279dc6102392c6536772c5f6d4fe4c73c)

--- src/middlewared/middlewared/plugins/service_/services/base.py
+++ src/middlewared/middlewared/plugins/service_/services/base.py
@@ -1,3 +1,5 @@
+import logging
+
 from middlewared.utils.journal import (
     format_journal_record,
     monotonic_to_realtime_since,
@@ -7,6 +9,8 @@ from middlewared.utils.journal import (
 from .base_interface import ServiceInterface, IdentifiableServiceInterface
 from .base_state import ServiceState
 from .dbus_router import system_dbus
+
+logger = logging.getLogger(__name__)
 
 
 class SimpleService(ServiceInterface, IdentifiableServiceInterface):
@@ -49,10 +53,25 @@ class SimpleService(ServiceInterface, IdentifiableServiceInterface):
 
     async def failure_logs(self):
         unit_name = self._get_systemd_unit_name()
-        monotonic_timestamp = await system_dbus.get_inactive_exit_timestamp(unit_name)
-        return await self.middleware.run_in_thread(
-            self._unit_failure_logs, monotonic_timestamp
-        )
+
+        # Walk the Wants dependency tree and find failed/crash-looping units.
+        # This catches sub-unit failures (e.g. nut-driver@ups crash-looping
+        # under nut-monitor) that the main unit's logs wouldn't show.
+        try:
+            wants_tree = await system_dbus.get_wants_tree(unit_name)
+            failed_units = await system_dbus.get_failed_units(wants_tree)
+        except Exception:
+            logger.debug("Failed to walk Wants tree for %s", unit_name, exc_info=True)
+            failed_units = {}
+
+        if failed_units:
+            units_info = list(failed_units.items())
+        else:
+            # Fall back to querying the main unit (original behavior)
+            monotonic_ts = await system_dbus.get_inactive_exit_timestamp(unit_name)
+            units_info = [(unit_name, ("unknown", monotonic_ts))]
+
+        return await self.middleware.run_in_thread(self._collect_failure_logs, units_info)
 
     def _get_systemd_unit_name(self):
         return f"{self.systemd_unit}.service"
@@ -70,33 +89,42 @@ class SimpleService(ServiceInterface, IdentifiableServiceInterface):
     async def _systemd_unit(self, unit, verb):
         await system_dbus.systemd_unit(unit, verb)
 
-    def _unit_failure_logs(self, monotonic_timestamp):
-        unit_name = self._get_systemd_unit_name()
-        since = monotonic_to_realtime_since(monotonic_timestamp)
+    @staticmethod
+    def _collect_failure_logs(units_info):
+        """Collect journalctl logs for one or more failed units.
 
+        Args:
+            units_info: list of ``(unit_name, (active_state, monotonic_timestamp))``
+        """
         # Match logic from systemd's add_matches_for_unit
         # (https://github.com/systemd/systemd/blob/main/src/shared/logs-show.c)
         # Using + for OR (disjunction) between match groups
-        match_args = [
-            # Match group 1: messages from the service itself
-            f"_SYSTEMD_UNIT={unit_name}",
-            # OR
-            "+",
-            # Match group 2: coredumps of the service
-            "MESSAGE_ID=fc2e22bc6ee647b6b90729ab34a250b1",
-            "_UID=0",
-            f"COREDUMP_UNIT={unit_name}",
-            # OR
-            "+",
-            # Match group 3: messages from PID 1 about this service
-            "_PID=1",
-            f"UNIT={unit_name}",
-            # OR
-            "+",
-            # Match group 4: messages from authorized daemons about this service
-            "_UID=0",
-            f"OBJECT_SYSTEMD_UNIT={unit_name}",
-        ]
-
-        records = query_journal(match_args, since=since)
-        return "\n".join(format_journal_record(r) for r in records)
+        all_sections: list[str] = []
+        for unit_name, (_state, monotonic_ts) in units_info:
+            since = monotonic_to_realtime_since(monotonic_ts)
+            match_args = [
+                # Match group 1: messages from the service itself
+                f"_SYSTEMD_UNIT={unit_name}",
+                # OR
+                "+",
+                # Match group 2: coredumps of the service
+                "MESSAGE_ID=fc2e22bc6ee647b6b90729ab34a250b1",
+                "_UID=0",
+                f"COREDUMP_UNIT={unit_name}",
+                # OR
+                "+",
+                # Match group 3: messages from PID 1 about this service
+                "_PID=1",
+                f"UNIT={unit_name}",
+                # OR
+                "+",
+                # Match group 4: messages from authorized daemons about this service
+                "_UID=0",
+                f"OBJECT_SYSTEMD_UNIT={unit_name}",
+            ]
+            records = query_journal(match_args, since=since)
+            if records:
+                all_sections.append(
+                    "\n".join(format_journal_record(r) for r in records)
+                )
+        return "\n".join(all_sections)
