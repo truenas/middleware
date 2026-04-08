f223e4bb53e559aa4ae74d7d4af4da9e5014e819
sonicaj <waqarahmedjoyia@live.com>
2026-04-08 23:36:32 +0500

    NAS-140613 / 27.0.0-BETA.1 / Remove unused parameters for validate_filters (#18690)
    This PR adds changes to remove unused parameters for `validate_filters`
    as they are not used anywhere anymore and the underlying library handles
    them nicely already.

--- src/middlewared/middlewared/api/base/validators/filters.py
+++ src/middlewared/middlewared/api/base/validators/filters.py
@@ -3,6 +3,6 @@ import truenas_pyfilter as _tf
 TIMESTAMP_DESIGNATOR = '.$date'
 
 
-def validate_filters(filters, recursion_depth=0, value_maps=None):
+def validate_filters(filters):
     _tf.compile_filters(list(filters))
     return filters
