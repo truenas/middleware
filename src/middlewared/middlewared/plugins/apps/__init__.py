e296ee28d70e627694f5d6584788f9e1f4b5447a
Waqar Ahmed <waqarahmedjoyia@live.com>
2026-04-09 21:37:09 +0500

    Add initial app barebones init.py file

---
+++ src/middlewared/middlewared/plugins/apps/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import typing
+
+from middlewared.api import api_method
+from middlewared.api.current import (
+    AppEntry,
+)
+from middlewared.service import GenericCRUDService, job, private
+
+
+if typing.TYPE_CHECKING:
+    from middlewared.job import Job
+    from middlewared.main import Middleware
+
+
+__all__ = ('AppService',)
+
+
+class AppService(GenericCRUDService[AppEntry]):
+
+    class Config:
+        namespace = 'app'
+        event_send = False
+        cli_namespace = 'app'
+        role_prefix = 'APPS'
+        entry = AppEntry
+
+    def __init__(self, middleware: Middleware) -> None:
+        super().__init__(middleware)
