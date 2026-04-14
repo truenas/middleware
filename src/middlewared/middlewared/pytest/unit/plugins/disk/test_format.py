b22c695c15b4e2a81013abfe4af59c835bd35f65
themylogin <themylogin@gmail.com>
2026-04-14 13:35:06 +0200

    NAS-140671 / 27.0.0-BETA.1 / `sgdisk_explicit_alignment` improvements (#18738)
    * Initial GPT header calculation size was imprecise, resulting in
    overestimating the GPT size and giving less disk space for data
    * Result should be multiplied by 8 for 4k disks as `sgdisk -a` accepts
    multiples of 2. This might have resulted in 4k disks having non-optimal
    alignment

--- src/middlewared/middlewared/pytest/unit/plugins/disk/test_format.py
+++ src/middlewared/middlewared/pytest/unit/plugins/disk/test_format.py
@@ -6,12 +6,14 @@ from middlewared.plugins.disk_.format import sgdisk_explicit_alignment
 @pytest.mark.parametrize("disk_size_bytes,sector_size_bytes,requested_partition_size,result", [
     # Legacy TrueNAS creates partitions that do not have any margins
     (6001175126016, 512, 6001175040000, 128),
-    # Replacing such a disk with a 4k-aligned disk requires smaller alignment
-    (6001175126016, 4096, 6001175040000, 16),
+    # 4k-aligned disk needs same alignment (as `-a` is expressed in 512 bytes logical sectors)
+    (6001175126016, 4096, 6001175040000, 128),
     # However, if we request a smaller partition, it would fit with default 1MB alignment perfectly
     (6001175126016, 4096, 6001174056960, None),
     # 1MB alignment is enough
     (6001175126016, 4096, 600117403648, None),
+    # Minimal alignment possible for a 4k disk
+    (1600321314816, 4096, 1600321273856, 8),
 ])
 def test_sgdisk_explicit_alignment(disk_size_bytes, sector_size_bytes, requested_partition_size, result):
     assert sgdisk_explicit_alignment(disk_size_bytes, sector_size_bytes, requested_partition_size) == result
