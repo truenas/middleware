17d0bd46825251aaa8b92acbaffbae97b95d8708
Andrew Walker <andrew.walker@truenas.com>
2026-04-14 08:00:58 -0500

    Address review
    * Use proper configfile field
    * Add logging to event source
    * Add logic for altering the special vdev reserved space

--- tests/api2/test_300_nfs.py
+++ tests/api2/test_300_nfs.py
@@ -525,7 +525,7 @@ def nfs_share_config(nfsid: int):
         share_config = configs[0]
         yield copy(share_config)
     finally:
-        excl = ['id', 'path', 'locked', 'dataset', 'relative_path']
+        excl = ['id', 'path', 'locked', 'dataset', 'relative_path', 'tier']
         [share_config.pop(key) for key in excl]
         call("sharing.nfs.update", nfsid, share_config)
 
