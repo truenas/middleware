06b812ee6fef4c339a529fff83c9ffcd4e25356c
Andrew Walker <andrew.walker@truenas.com>
2026-04-14 08:00:58 -0500

    Address review
    * Use proper configfile field
    * Add logging to event source
    * Add logic for altering the special vdev reserved space

--- src/middlewared/middlewared/etc_files/truenas_zfstierd.py
+++ src/middlewared/middlewared/etc_files/truenas_zfstierd.py
@@ -5,10 +5,9 @@ def render(service, middleware):
     config = middleware.call_sync("zfs.tier.config")
     cfg = ZFSTierdGlobalConfig(
         max_concurrent_jobs=config["max_concurrent_jobs"],
-        min_available_space=config["min_available_space"],
         reporting_write_interval=60,
         rewrite_chunk_size=1024,
         reporting_callback_interval=1,
-        max_used_percent=80,
+        max_used_percent=config["max_used_percentage"],
     )
     return generate_config(cfg)
