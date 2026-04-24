8a795f84e4b92f86544d8f8913421e764a537302
caleb <yocalebo@gmail.com>
2026-04-24 15:15:47 -0400

    more simplify

--- src/middlewared/middlewared/plugins/container/nsenter.py
+++ src/middlewared/middlewared/plugins/container/nsenter.py
@@ -5,12 +5,12 @@ from middlewared.service import CallError, ServiceContext
 
 async def nsenter(context: ServiceContext, container_id: int) -> list[str]:
     container = await context.call2(context.s.container.get_instance, container_id)
-    pid = container.status.pid
-    if pid is None:
+    if container.status.pid is None:
         raise CallError("Container is not running")
 
     return build_argv_for_shell(
-        pid=pid,
+        uuid=container.uuid,
+        uri=context.middleware.libvirt_domains_manager.containers_connection.uri,
         capabilities_policy=container.capabilities_policy,
         capabilities_state=container.capabilities_state,
         has_idmap=bool(container.idmap),
