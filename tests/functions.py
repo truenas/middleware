ce3a1fd808be5863fc1933a7418333b06966a1b4
themylogin <themylogin@gmail.com>
2026-04-27 15:02:44 +0200

    NAS-140786 / 27.0.0-BETA.1 / Runtest rewrite (#18804)
    Functionally equivalent, but more maintainable runtest.py. Further
    improvements will come in separate PRs.
    
    I mocked all side effects of the original runtest.py (config file writes
    and pytest commands), then created a full test suite with 100% coverage
    (measured using pycov).
    After that, I refactored the code and ensured all tests still pass.
    
    A similar approach can be used for rewrites in other upcoming projects
    if we decide to proceed that way.

--- tests/functions.py
+++ tests/functions.py
@@ -5,7 +5,6 @@
 
 import enum
 import json
-import os
 import re
 from subprocess import PIPE, Popen, TimeoutExpired, run
 from time import sleep
@@ -166,28 +165,6 @@ def get_file(file, destination, username, passwrd, host):
         return {'result': True, 'output': output}
 
 
-def get_folder(folder, destination, username, passwrd, host):
-    cmd = [] if passwrd is None else ["sshpass", "-p", passwrd]
-    cmd += [
-        "scp",
-        "-o",
-        "StrictHostKeyChecking=no",
-        "-o",
-        "UserKnownHostsFile=/dev/null",
-        "-o",
-        "VerifyHostKeyDNS=no",
-        "-r",
-        f"{username}@{host}:{folder}",
-        destination
-    ]
-    process = run(cmd, stdout=PIPE, universal_newlines=True)
-    output = process.stdout
-    if process.returncode != 0:
-        return {'result': False, 'output': output}
-    else:
-        return {'result': True, 'output': output}
-
-
 def RC_TEST(command):
     process = run(command, shell=True)
     if process.returncode != 0:
@@ -213,55 +190,6 @@ def cmd_test(command):
         return {'result': False, 'output': output, 'stderr': err}
     else:
         return {'result': True, 'output': output}
-
-
-def start_ssh_agent():
-    process = run(['ssh-agent', '-s'], stdout=PIPE, universal_newlines=True)
-    to_recompile = r'SSH_AUTH_SOCK=(?P<socket>[^;]+).*SSH_AGENT_PID=(?P<pid>\d+)'
-    OUTPUT_PATTERN = re.compile(to_recompile, re.MULTILINE | re.DOTALL)
-    match = OUTPUT_PATTERN.search(process.stdout)
-    if match is None:
-        return False
-    else:
-        agentData = match.groupdict()
-        os.environ['SSH_AUTH_SOCK'] = agentData['socket']
-        os.environ['SSH_AGENT_PID'] = agentData['pid']
-        return True
-
-
-def is_agent_setup():
-    return os.environ.get('SSH_AUTH_SOCK') is not None
-
-
-def setup_ssh_agent():
-    if is_agent_setup():
-        return True
-    else:
-        return start_ssh_agent()
-
-
-def create_key(keyPath):
-    process = run('ssh-keygen -t rsa -f %s -q -N ""' % keyPath, shell=True)
-    if process.returncode != 0:
-        return False
-    else:
-        return True
-
-
-def if_key_listed():
-    process = run('ssh-add -L', shell=True)
-    if process.returncode != 0:
-        return False
-    else:
-        return True
-
-
-def add_ssh_key(keyPath):
-    process = run(['ssh-add', keyPath])
-    if process.returncode != 0:
-        return False
-    else:
-        return True
 
 
 def vm_state(vm_name):
