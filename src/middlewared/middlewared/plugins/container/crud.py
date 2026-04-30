af9fb21bcadf807cce5839b42dc7babdb215617a
Waqar Ahmed <waqarahmedjoyia@live.com>
2026-04-30 19:34:53 +0500

    No need to explicitly set nullable=false as that's the default

--- src/middlewared/middlewared/plugins/container/crud.py
+++ src/middlewared/middlewared/plugins/container/crud.py
@@ -73,7 +73,7 @@ class ContainerModel(sa.Model):
     __tablename__ = 'container_container'
 
     id = sa.Column(sa.Integer(), primary_key=True)
-    uuid = sa.Column(sa.Text(), nullable=False)
+    uuid = sa.Column(sa.Text())
     name = sa.Column(sa.Text())
     description = sa.Column(sa.Text())
     autostart = sa.Column(sa.Boolean())
