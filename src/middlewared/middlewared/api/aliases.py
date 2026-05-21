b1b8bf5ea3dd0d1dcae56d655d520d8a27e8cbc4
themylogin <themylogin@gmail.com>
2026-05-21 14:37:11 +0200

    Address review

--- src/middlewared/middlewared/api/aliases.py
+++ src/middlewared/middlewared/api/aliases.py
@@ -1,4 +1,8 @@
-aliases = {
-    "v25.10.3": "v25.10.2",
-    "v25.10.4": "v25.10.2",
-}
+from types import MappingProxyType
+
+aliases = MappingProxyType(
+    {
+        "v25.10.3": "v25.10.2",
+        "v25.10.4": "v25.10.2",
+    }
+)
