3b1bde30c783bc3abccf77a50097c9b7fdab92a2
Andrew Walker <andrew.walker@truenas.com>
2026-05-25 08:53:30 -0500

    Switch to truenas_pyos access check function
    This commit switches from using our process-pool based permissions
    checker with on that's streamlined inside truenas_pyos and executes
    with GIL dropped. Historically this area has been troublesome to
    keep good performance and so simplifying middleware design here
    is worthwhile.

--- tests/api2/test_can_access_as_user.py
+++ tests/api2/test_can_access_as_user.py
@@ -42,40 +42,40 @@ def directory(name, user, group, permissions):
 
 def test_non_authorized_user_access():
     with file('test', 'root', 'root', '700') as file_path:
-        for perm_check in ('read', 'write', 'execute'):
-            assert call('filesystem.can_access_as_user', 'nobody', file_path, {perm_check: True}) is False
+        for perm_check in ('READ', 'WRITE', 'EXECUTE'):
+            assert call('filesystem.can_access_as_user', 'nobody', file_path, [perm_check]) is False
 
 
 def test_authorized_user_access():
     for user, group in (('apps', 'apps'), ('nobody', 'nogroup')):
         with file('test', user, group, '700') as file_path:
-            for perm_check in ('read', 'write', 'execute'):
-                assert call('filesystem.can_access_as_user', user, file_path, {perm_check: True}) is True
+            for perm_check in ('READ', 'WRITE', 'EXECUTE'):
+                assert call('filesystem.can_access_as_user', user, file_path, [perm_check]) is True
 
 
 def test_read_access():
     for user, group in (('apps', 'apps'), ('nobody', 'nogroup')):
         with file('test', user, group, '400') as file_path:
-            for perm_check, value in (('read', True), ('write', False), ('execute', False)):
-                assert call('filesystem.can_access_as_user', user, file_path, {perm_check: True}) is value
+            for perm_check, value in (('READ', True), ('WRITE', False), ('EXECUTE', False)):
+                assert call('filesystem.can_access_as_user', user, file_path, [perm_check]) is value
 
 
 def test_write_access():
     for user, group in (('apps', 'apps'), ('nobody', 'nogroup')):
         with file('test', user, group, '200') as file_path:
-            for perm_check, value in (('read', False), ('write', True), ('execute', False)):
-                assert call('filesystem.can_access_as_user', user, file_path, {perm_check: True}) is value
+            for perm_check, value in (('READ', False), ('WRITE', True), ('EXECUTE', False)):
+                assert call('filesystem.can_access_as_user', user, file_path, [perm_check]) is value
 
 
 def test_execute_access():
     for user, group in (('apps', 'apps'), ('nobody', 'nogroup')):
         with file('test', user, group, '100') as file_path:
-            for perm_check, value in (('read', False), ('write', False), ('execute', True)):
-                assert call('filesystem.can_access_as_user', user, file_path, {perm_check: True}) is value
+            for perm_check, value in (('READ', False), ('WRITE', False), ('EXECUTE', True)):
+                assert call('filesystem.can_access_as_user', user, file_path, [perm_check]) is value
 
 
 def test_nested_perm_execute_check():
     with directory('test_dir', 'root', 'root', '700') as dir_path:
         file_path = os.path.join(dir_path, 'testfile')
         with file_at_path(file_path, 'root', 'root', '777'):
-            assert call('filesystem.can_access_as_user', 'apps', file_path, {'execute': True}) is False
+            assert call('filesystem.can_access_as_user', 'apps', file_path, ['EXECUTE']) is False
