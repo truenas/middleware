truenas/middleware

commit c785e8aec7140d00c5c2ece36325e45e3a646cc0

NAS-141194 / 27.0.0-BETA.1 / `common` mypy checks (#19034)

src/middlewared/middlewared/plugins/apps/cert_attachments.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from middlewared.main import Middleware
 
 
-class AppCertificateAttachmentDelegate(CertificateCRUDServiceAttachmentDelegate):
+class AppCertificateAttachmentDelegate(CertificateCRUDServiceAttachmentDelegate[AppEntry]):
 
     HUMAN_NAME = 'Applications'
     NAMESPACE = 'app'

.github/workflows/mypy.yml
@@ -23,7 +23,7 @@ jobs:
           mypy \
             middlewared/job.py \
             middlewared/alert/ \
-            middlewared/common/ports/ \
+            middlewared/common/ \
             middlewared/plugins/acme_protocol/ \
             middlewared/plugins/acme_dns_authenticator/ \
             middlewared/plugins/acme_registration/ \

src/middlewared/middlewared/common/attachment/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Protocol
 
 from middlewared.service import ServiceChangeMixin, SharingTaskService
 
@@ -8,7 +8,11 @@ if TYPE_CHECKING:
     from middlewared.main import Middleware
 
 
-class FSAttachmentDelegate(ServiceChangeMixin):
+class Entry(Protocol):
+    id: int
+
+
+class FSAttachmentDelegate[E](ServiceChangeMixin):
     """
     Represents something (share, automatic task, etc.) that needs to be enabled or disabled when dataset
     becomes available or unavailable (due to import/export, encryption/decryption, etc.)
@@ -32,7 +36,7 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         self.middleware = middleware
         self.logger = middleware.logger
 
-    async def query(self, path, enabled, options=None):
+    async def query(self, path: str, enabled: bool, options: dict[str, Any] | None = None) -> list[E]:
         """
         Lists enabled/disabled items that depend on a dataset
         :param path: mountpoint of the dataset (e.g. "/mnt/tank/work")
@@ -42,7 +46,7 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         """
         raise NotImplementedError
 
-    async def get_attachment_name(self, attachment) -> str:
+    async def get_attachment_name(self, attachment: E) -> str:
         """
         Returns human-readable description of item (e.g. it's path). Will be combined with `cls.title`.
         I.e. if you return here `/mnt/tank/work`, user will see: `NFS Share "/mnt/tank/work"`
@@ -51,11 +55,11 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         """
         if isinstance(attachment, dict):
             # FIXME: This must be eventually removed
-            return attachment[self.resource_name]
+            return attachment[self.resource_name]  # type: ignore[no-any-return]
         else:
-            return getattr(attachment, self.resource_name)
+            return getattr(attachment, self.resource_name)  # type: ignore[no-any-return]
 
-    async def delete(self, attachments):
+    async def delete(self, attachments: list[E]) -> None:
         """
         Permanently delete said items
         :param attachments: list of the items returned by `query`
@@ -63,7 +67,7 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         """
         raise NotImplementedError
 
-    async def toggle(self, attachments, enabled):
+    async def toggle(self, attachments: list[E], enabled: bool) -> None:
         """
         Enable or disable said items
         :param attachments: list of the items returned by `query`
@@ -72,13 +76,13 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         """
         raise NotImplementedError
 
-    async def start(self, attachments):
+    async def start(self, attachments: list[E]) -> None:
         pass
 
-    async def stop(self, attachments):
+    async def stop(self, attachments: list[E]) -> None:
         pass
 
-    async def disable(self, attachments):
+    async def disable(self, attachments: list[E]) -> None:
         """
         Disable said items, this is used when we export pool but do not want to delete
         related attachments
@@ -88,7 +92,7 @@ class FSAttachmentDelegate(ServiceChangeMixin):
         await self.toggle(attachments, False)
 
 
-class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
+class LockableFSAttachmentDelegate[E: Entry](FSAttachmentDelegate[E]):
     """
     Represents a share/task/resource which is affected if the dataset underlying is locked
     """
@@ -96,25 +100,25 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
     # service object
     service_class: type[SharingTaskService[E]]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.enabled_field = self.service_class.enabled_field
         self.locked_field = self.service_class.locked_field
         self.path_field = self.service_class.path_field
-        self.datastore_model = self.service_class._config.datastore
-        self.datastore_prefix = self.service_class._config.datastore_prefix
-        self.namespace = self.service_class._config.namespace
+        self.datastore_model = self.service_class._config.datastore  # type: ignore[attr-defined]
+        self.datastore_prefix = self.service_class._config.datastore_prefix  # type: ignore[attr-defined]
+        self.namespace = self.service_class._config.namespace  # type: ignore[attr-defined]
         if not self.service:
-            self.service = self.service_class._config.service
+            self.service = self.service_class._config.service  # type: ignore[attr-defined]
 
-    async def get_query_filters(self, enabled, options=None):
+    async def get_query_filters(self, enabled: bool, options: dict[str, Any] | None = None) -> list[Any]:
         options = options or {}
         filters = [[self.enabled_field, '=', enabled]]
         if 'locked' in options:
             filters += [[self.locked_field, '=', options['locked']]]
         return filters
 
-    async def start_service(self):
+    async def start_service(self) -> None:
         if not (
             service_obj := await self.middleware.call('service.query', [['service', '=', self.service]])
         ) or not service_obj[0]['enable'] or service_obj[0]['state'] == 'RUNNING':
@@ -122,7 +126,7 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
 
         await (await self.middleware.call('service.control', 'START', self.service)).wait(raise_error=True)
 
-    async def query(self, path, enabled, options=None):
+    async def query(self, path: str, enabled: bool, options: dict[str, Any] | None = None) -> list[E]:
         results = []
         options = options or {}
         check_parent = options.get('check_parent', False)
@@ -134,7 +138,7 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
                 results.append(resource)
         return results
 
-    async def toggle(self, attachments, enabled):
+    async def toggle(self, attachments: list[E], enabled: bool) -> None:
         for attachment in attachments:
             if isinstance(attachment, dict):
                 # FIXME: This must be eventually removed
@@ -154,7 +158,7 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
         else:
             await self.stop(attachments)
 
-    async def delete(self, attachments):
+    async def delete(self, attachments: list[E]) -> None:
         for attachment in attachments:
             if isinstance(attachment, dict):
                 # FIXME: This must be eventually removed
@@ -173,7 +177,7 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
         """
         raise NotImplementedError
 
-    async def remove_alert(self, attachment):
+    async def remove_alert(self, attachment: E) -> None:
         if isinstance(attachment, dict):
             # FIXME: This must be eventually removed
             attachment_id = attachment['id']
@@ -182,7 +186,7 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
 
         await self.middleware.call(f'{self.namespace}.remove_locked_alert', attachment_id)
 
-    async def is_child_of_path(self, resource, path, check_parent, exact_match):
+    async def is_child_of_path(self, resource: E, path: str, check_parent: bool, exact_match: bool) -> bool:
         # What this is essentially doing is testing if resource in question is a child of queried path
         # and not vice versa. While this is desirable in most cases, there are cases we also want to see
         # if path is a child of the resource in question. In that case we want the following:
@@ -199,23 +203,24 @@ class LockableFSAttachmentDelegate[E](FSAttachmentDelegate):
         # `check_parent` flag when set can be used to check for the case when share path is the parent
         # of the path to check.
 
-        share_path = await self.service_class.get_path_field(self.service_class, resource)
+        share_path = await self.service_class.get_path_field(self.service_class, resource)  # type: ignore[arg-type]
+        # FIXME: Fix `type: ignore[arg-type]` above
         if exact_match or share_path == path:
             return share_path == path
 
         is_child = await self.middleware.call('filesystem.is_child', share_path, path)
         if not is_child and check_parent:
-            return await self.middleware.call('filesystem.is_child', path, share_path)
+            return await self.middleware.call('filesystem.is_child', path, share_path)  # type: ignore[no-any-return]
         else:
-            return is_child
+            return is_child  # type: ignore[no-any-return]
 
-    async def start(self, attachments):
+    async def start(self, attachments: list[E]) -> None:
         await self.start_service()
         for attachment in attachments:
             await self.remove_alert(attachment)
         if attachments:
             await self.restart_reload_services(attachments)
 
-    async def stop(self, attachments):
+    async def stop(self, attachments: list[E]) -> None:
         if attachments:
             await self.restart_reload_services(attachments)

src/middlewared/middlewared/common/attachment/certificate.py
@@ -16,13 +16,13 @@ class CertificateAttachmentDelegate:
     def __init__(self, middleware: Middleware) -> None:
         self.middleware = middleware
 
-    async def state(self, cert_id) -> bool:
+    async def state(self, cert_id: int) -> bool:
         raise NotImplementedError
 
-    async def redeploy(self, cert_id) -> None:
+    async def redeploy(self, cert_id: int) -> None:
         raise NotImplementedError
 
-    async def consuming_cert_human_output(self, cert_id) -> str | None:
+    async def consuming_cert_human_output(self, cert_id: int) -> str | None:
         return self.HUMAN_NAME if await self.state(cert_id) else None
 
 
@@ -35,32 +35,35 @@ class CertificateServiceAttachmentDelegate(CertificateAttachmentDelegate, Servic
     async def get_namespace(self) -> str:
         return getattr(self, 'NAMESPACE', self.SERVICE)
 
-    async def state(self, cert_id) -> bool:
+    async def state(self, cert_id: int) -> bool:
         # Config can be either a dict (legacy/unconverted services) or a Pydantic model
         # (typesafe services with `generic = True`, e.g. tn_connect). Dispatch field
         # access accordingly.
         config = await self.middleware.call(f'{await self.get_namespace()}.config')
         cert_value = config[self.CERT_FIELD] if isinstance(config, dict) else getattr(config, self.CERT_FIELD)
         if isinstance(cert_value, dict):
-            return cert_value['id'] == cert_id
-        return cert_value == cert_id
+            return cert_value['id'] == cert_id  # type: ignore[no-any-return]
+        return cert_value == cert_id  # type: ignore[no-any-return]
 
-    async def redeploy(self, cert_id) -> None:
+    async def redeploy(self, cert_id: int) -> None:
         if await self.middleware.call('service.started', self.SERVICE):
             await (
                 await self.middleware.call('service.control', self.SERVICE_VERB, self.SERVICE)
             ).wait(raise_error=True)
 
 
-class CertificateCRUDServiceAttachmentDelegate(CertificateAttachmentDelegate, ServiceChangeMixin):
+class CertificateCRUDServiceAttachmentDelegate[E](CertificateAttachmentDelegate, ServiceChangeMixin):
 
     CERT_FILTER_KEY = 'certificate'
 
-    async def get_filters(self, cert_id) -> list[Any]:
+    async def get_filters(self, cert_id: int) -> list[Any]:
         return [[self.CERT_FILTER_KEY, '=', cert_id]]
 
-    async def attachments(self, cert_id) -> list[Any]:
-        return await self.middleware.call(f'{self.NAMESPACE}.query', await self.get_filters(cert_id))
+    async def attachments(self, cert_id: int) -> list[E]:
+        return await self.middleware.call(  # type: ignore[no-any-return]
+            f'{self.NAMESPACE}.query',
+            await self.get_filters(cert_id),
+        )
 
-    async def state(self, cert_id) -> bool:
+    async def state(self, cert_id: int) -> bool:
         return bool(await self.attachments(cert_id))

src/middlewared/middlewared/common/environ.py
@@ -3,7 +3,7 @@ import time
 import urllib.request
 
 
-def environ_update(update):
+def environ_update(update: dict[str, str | None]) -> None:
     for k, v in update.items():
         if v is None:
             os.environ.pop(k, None)
@@ -12,7 +12,7 @@ def environ_update(update):
 
     if 'http_proxy' in update or 'https_proxy' in update:
         # Reset global opener so ProxyHandler can be recalculated
-        urllib.request.install_opener(None)
+        urllib.request.install_opener(None)  # type: ignore[arg-type]
 
     if 'TZ' in update:
         time.tzset()

src/middlewared/middlewared/common/event_source/manager.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import asyncio
 from collections import defaultdict
 import functools
-from typing import TYPE_CHECKING, Literal, NamedTuple, TypeAlias
+from typing import TYPE_CHECKING, Any, Literal, NamedTuple, TypeAlias
 from uuid import uuid4
 
 from middlewared.event import EventSource
@@ -21,10 +21,10 @@ class IdentData(NamedTuple):
 
 
 class Subscriber:
-    def send_event(self, event_type: str, **kwargs):
+    def send_event(self, event_type: str, **kwargs: Any) -> None:
         raise NotImplementedError
 
-    def terminate(self, error: Exception | None):
+    def terminate(self, error: Exception | None) -> None:
         raise NotImplementedError
 
 
@@ -33,28 +33,28 @@ class AppSubscriber(Subscriber):
         self.app = app
         self.collection = collection
 
-    def send_event(self, event_type, **kwargs):
+    def send_event(self, event_type: str, **kwargs: Any) -> None:
         self.app.send_event(self.collection, event_type, **kwargs)
 
-    def terminate(self, error):
+    def terminate(self, error: Exception | None) -> None:
         self.app.notify_unsubscribed(self.collection, error)
 
 
 class InternalSubscriber(Subscriber):
-    def __init__(self):
+    def __init__(self) -> None:
         self.iterator = InternalSubscriberIterator()
 
-    def send_event(self, event_type, **kwargs):
+    def send_event(self, event_type: str, **kwargs: Any) -> None:
         self.iterator.queue.put_nowait((False, (event_type, kwargs)))
 
-    def terminate(self, error):
+    def terminate(self, error: Exception | None) -> None:
         if error:
             self.iterator.queue.put_nowait((True, error))
         else:
             self.iterator.queue.put_nowait(None)
 
 
-_IteratorItem: TypeAlias = tuple[str, dict]
+_IteratorItem: TypeAlias = tuple[str, dict[str, Any]]
 """Event type and kwargs"""
 _InternalIteratorItem: TypeAlias = tuple[Literal[True], Exception] | tuple[Literal[False], _IteratorItem] | None
 """Queue item type for InternalSubscriberIterator:
@@ -64,10 +64,10 @@ _InternalIteratorItem: TypeAlias = tuple[Literal[True], Exception] | tuple[Liter
 
 
 class InternalSubscriberIterator:
-    def __init__(self):
+    def __init__(self) -> None:
         self.queue: asyncio.Queue[_InternalIteratorItem] = asyncio.Queue()
 
-    def __aiter__(self):
+    def __aiter__(self) -> InternalSubscriberIterator:
         return self
 
     async def __anext__(self) -> _IteratorItem:
@@ -78,9 +78,9 @@ class InternalSubscriberIterator:
 
         is_error, value = item
         if is_error:
-            raise value
+            raise value  # type: ignore[misc]
         else:
-            return value
+            return value  # type: ignore[return-value]
 
 
 _EventSourceDict: TypeAlias = dict[str | None, EventSource]
@@ -116,7 +116,7 @@ class EventSourceManager:
         else:
             return f'{name}:{arg}'
 
-    def register(self, name: str, event_source: type[EventSource]):
+    def register(self, name: str, event_source: type[EventSource]) -> None:
         if not issubclass(event_source, EventSource):
             raise RuntimeError(f"{event_source} is not EventSource subclass")
 
@@ -124,7 +124,7 @@ class EventSourceManager:
 
         self.middleware.role_manager.register_event(name, event_source.roles)
 
-    async def subscribe(self, subscriber: Subscriber, ident: str, name: str, arg: str | None):
+    async def subscribe(self, subscriber: Subscriber, ident: str, name: str, arg: str | None) -> None:
         if ident in self.idents:
             raise ValueError(f"Ident {ident} is already used")
 
@@ -148,7 +148,7 @@ class EventSourceManager:
         else:
             self.middleware.logger.trace("Re-using existing instance of event source %r:%r", name, arg)
 
-    async def unsubscribe(self, ident: str, error: Exception | None = None):
+    async def unsubscribe(self, ident: str, error: Exception | None = None) -> None:
         ident_data = self.idents.pop(ident, None)
         if ident_data is None:
             return
@@ -162,13 +162,13 @@ class EventSourceManager:
             instance = self.instances[ident_data.name].pop(ident_data.arg)
             await instance.cancel()
 
-    def terminate(self, ident: IdentData, error: Exception | None = None):
+    def terminate(self, ident: IdentData, error: Exception | None = None) -> None:
         ident.subscriber.terminate(error)
 
-    async def subscribe_app(self, app: RpcWebSocketApp, ident: str, name: str, arg: str | None):
+    async def subscribe_app(self, app: RpcWebSocketApp, ident: str, name: str, arg: str | None) -> None:
         await self.subscribe(AppSubscriber(app, self.get_full_name(name, arg)), ident, name, arg)
 
-    async def unsubscribe_app(self, app: RpcWebSocketApp):
+    async def unsubscribe_app(self, app: RpcWebSocketApp) -> None:
         for ident, ident_data in list(self.idents.items()):
             if isinstance(ident_data.subscriber, AppSubscriber) and ident_data.subscriber.app == app:
                 await self.unsubscribe(ident)
@@ -179,7 +179,7 @@ class EventSourceManager:
         await self.subscribe(subscriber, ident, name, arg)
         return subscriber.iterator
 
-    def _send_event(self, name: str, arg: str | None, event_type: str, **kwargs):
+    def _send_event(self, name: str, arg: str | None, event_type: str, **kwargs: Any) -> None:
         for ident in list(self.subscriptions[name][arg]):
             try:
                 ident_data = self.idents[ident]
@@ -189,7 +189,7 @@ class EventSourceManager:
 
             ident_data.subscriber.send_event(event_type, **kwargs)
 
-    async def _unsubscribe_all(self, name: str, arg: str | None, error: Exception | None = None):
+    async def _unsubscribe_all(self, name: str, arg: str | None, error: Exception | None = None) -> None:
         for ident in self.subscriptions[name][arg]:
             self.terminate(self.idents.pop(ident), error)
 

src/middlewared/middlewared/common/listen/__init__.py
@@ -1,103 +1,112 @@
-class ListenDelegate:
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from middlewared.main import Middleware
+
+
+class ListenDelegate[ListenState]:
     """
-    Represents something (e.g. service) that needs to be handle a deletion of a static IP address from the system.
+    Represents something (e.g. service) that needs to handle a deletion of a static IP address from the system.
     """
 
-    async def get_listen_state(self, ips):
+    async def get_listen_state(self, ips: list[str]) -> ListenState:
         """
         Returns a state object that will be passed to subsequent functions.
         """
         raise NotImplementedError
 
-    async def set_listen_state(self, state):
+    async def set_listen_state(self, state: ListenState) -> None:
         """
         Set to listen on the addresses from the state.
         """
         raise NotImplementedError
 
-    async def listens_on(self, state, ip):
+    async def listens_on(self, state: ListenState, ip: str) -> bool:
         """
         Checks if we are listening on an IP address.
         """
         raise NotImplementedError
 
-    async def reset_listens(self, state):
+    async def reset_listens(self, state: ListenState) -> None:
         """
         Listen on all IP addresses.
         """
         raise NotImplementedError
 
-    async def repr(self, state):
+    async def repr(self, state: ListenState) -> dict[str, str]:
         """
         Returns machine-readable state description.
         """
         raise NotImplementedError
 
 
-class ConfigServiceListenDelegate(ListenDelegate):
+class ConfigServiceListenDelegate[ListenState](ListenDelegate[ListenState]):
     """
     ConfigService listening on IP address.
     """
 
-    def __init__(self, middleware, plugin, field):
+    def __init__(self, middleware: Middleware, plugin: str, field: str) -> None:
         self.middleware = middleware
         self.plugin = plugin
         self.field = field
 
-    async def get_listen_state(self, ips):
+    async def get_listen_state(self, ips: list[str]) -> ListenState:
         config = await self.middleware.call(f"{self.plugin}.config")
-        return config[self.field]
+        return config[self.field]  # type: ignore[no-any-return]
 
-    async def set_listen_state(self, state):
+    async def set_listen_state(self, state: ListenState) -> None:
         await self.middleware.call(f"{self.plugin}.update", {self.field: state})
 
-    async def repr(self, state):
+    async def repr(self, state: ListenState) -> dict[str, str]:
         return {"type": "SERVICE", "service": self.plugin}
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__} {self.plugin}>"
 
 
-class ConfigServiceListenSingleDelegate(ConfigServiceListenDelegate):
+class ConfigServiceListenSingleDelegate(ConfigServiceListenDelegate[str]):
     """
     ConfigService listening on a single IP address.
     """
 
-    def __init__(self, *args, empty_value="0.0.0.0"):
+    def __init__(self, *args: Any, empty_value: str = "0.0.0.0"):
         super().__init__(*args)
         self.empty_value = empty_value
 
-    async def listens_on(self, state, ip):
+    async def listens_on(self, state: str, ip: str) -> bool:
         return state == ip
 
-    async def reset_listens(self, state):
+    async def reset_listens(self, state: str) -> None:
         await self.set_listen_state(self.empty_value)
 
 
-class ConfigServiceListenMultipleDelegate(ConfigServiceListenDelegate):
+class ConfigServiceListenMultipleDelegate(ConfigServiceListenDelegate[list[str]]):
     """
     ConfigService listening on multiple IP addresses.
     """
 
-    async def listens_on(self, state, ip):
+    async def listens_on(self, state: list[str], ip: str) -> bool:
         return ip in state
 
-    async def reset_listens(self, state):
+    async def reset_listens(self, state: list[str]) -> None:
         await self.set_listen_state([])
 
 
-class SystemServiceListenDelegateMixin:
+class SystemServiceListenDelegateMixin[ListenState]:
     @property
-    def service(self):
-        return self.middleware.get_service(self.plugin)._config.service
+    def service(self) -> str:
+        return self.middleware.get_service(self.plugin)._config.service  # type: ignore[attr-defined, no-any-return]
 
-    async def repr(self, state):
+    async def repr(self, state: ListenState) -> dict[str, str]:
         return {"type": "SYSTEM_SERVICE", "service": self.service}
 
 
-class SystemServiceListenSingleDelegate(SystemServiceListenDelegateMixin, ConfigServiceListenSingleDelegate):
+class SystemServiceListenSingleDelegate(SystemServiceListenDelegateMixin[str], ConfigServiceListenSingleDelegate):
     pass
 
 
-class SystemServiceListenMultipleDelegate(SystemServiceListenDelegateMixin, ConfigServiceListenMultipleDelegate):
+class SystemServiceListenMultipleDelegate(SystemServiceListenDelegateMixin[list[str]],
+                                          ConfigServiceListenMultipleDelegate):
     pass

src/middlewared/middlewared/plugins/apps/fs_attachments.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
     from middlewared.main import Middleware
 
 
-class AppFSAttachmentDelegate(FSAttachmentDelegate):
+class AppFSAttachmentDelegate(FSAttachmentDelegate[dict[str, str]]):
     name = 'apps'
     title = 'Apps'
     # Apps depend on Docker, so they start after and stop before Docker

commit 40d38380c92a0124157e24d26121cec8155e112e

NAS-141217 / 27.0.0-BETA.1 / Fix crash on middleware startup (#19044)
Caused by https://github.com/truenas/middleware/pull/19034

src/middlewared/middlewared/plugins/apps/cert_attachments.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
+from middlewared.api.current import AppEntry
 from middlewared.common.attachment.certificate import CertificateCRUDServiceAttachmentDelegate
 from middlewared.service import Service
 
 from .ix_apps.metadata import get_collective_config
 
 if TYPE_CHECKING:
-    from middlewared.api.current import AppEntry
     from middlewared.main import Middleware
 
 