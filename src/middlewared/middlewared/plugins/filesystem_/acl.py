93703bc7989abd8432748dfbcaef9de244ed028f
Andrew Walker <andrew.walker@truenas.com>
2026-06-04 11:01:10 -0500

    Address review

--- src/middlewared/middlewared/plugins/filesystem_/acl.py
+++ src/middlewared/middlewared/plugins/filesystem_/acl.py
@@ -178,7 +178,7 @@ class FilesystemService(Service):
         roles=['FILESYSTEM_ATTRS_WRITE'],
         audit='Filesystem change owner', audit_extended=lambda data: data['path']
     )
-    @job(lock=lambda args: f'perm_change:{args[0]["path"]}')
+    @job(lock=lambda args: f'perm_change:{os.path.normpath(args[0]["path"])}')
     def chown(self, job, data):
         """
         Change owner or group of file at `path`.
@@ -237,7 +237,7 @@ class FilesystemService(Service):
         roles=['FILESYSTEM_ATTRS_WRITE'],
         audit='Filesystem set permission', audit_extended=lambda data: data['path']
     )
-    @job(lock=lambda args: f'perm_change:{args[0]["path"]}')
+    @job(lock=lambda args: f'perm_change:{os.path.normpath(args[0]["path"])}')
     def setperm(self, job, data):
         """
         Set unix permissions on given `path`.
@@ -639,7 +639,7 @@ class FilesystemService(Service):
         audit='Filesystem set ACL',
         audit_extended=lambda data: data['path']
     )
-    @job(lock=lambda args: f'perm_change:{args[0]["path"]}')
+    @job(lock=lambda args: f'perm_change:{os.path.normpath(args[0]["path"])}')
     def setacl(self, job, data):
         """
         Set ACL of a given path. Takes the following parameters:
