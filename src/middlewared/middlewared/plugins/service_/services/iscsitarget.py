0747628f638e6e03a96e9a5b705311f5641ec119
themylogin <themylogin@gmail.com>
2026-06-27 14:06:34 +0200

    Fix usage of undefined logger
    (cherry picked from commit 465003112df4206fe98f21f5e5d5834639a37677)

--- src/middlewared/middlewared/plugins/service_/services/iscsitarget.py
+++ src/middlewared/middlewared/plugins/service_/services/iscsitarget.py
@@ -108,7 +108,7 @@ class ISCSITargetService(SwitchableSimpleService):
                     try:
                         return await self.middleware.call("iscsi.alua.become_active")
                     except Exception:
-                        self.logger.warning('Failover exception', exc_info=True)
+                        self.middleware.logger.warning('Failover exception', exc_info=True)
                         # Fall through
         # Fallback to doing a regular restart
         rjob = await self.middleware.call(
