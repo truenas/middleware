7641efee9aaf3408acdb637c5727eac8f6d36964
themylogin <themylogin@gmail.com>
2026-07-10 10:47:50 +0200

    Move `assert` from middleware startup to tests

--- src/middlewared/middlewared/alert/base.py
+++ src/middlewared/middlewared/alert/base.py
@@ -268,9 +268,6 @@ alert_category_names = {
 }
 
 
-assert all([category in alert_category_names for category in AlertCategory]), 'Alert Category Mismatch'
-
-
 class AlertLevel(enum.Enum):
     INFO = 1
     NOTICE = 2
