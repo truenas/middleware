9e748135d28699bdc1650cbcf9850dfc74bc7ecb
Andrew Walker <andrew.walker@truenas.com>
2026-07-16 09:05:10 -0500

    Remove tests

--- tests/api2/test_system_general.py
+++
@@ -1,9 +0,0 @@
-from middlewared.test.integration.utils import call
-
-TIMEZONE = "America/New_York"
-
-
-def test_setting_timezone():
-    assert TIMEZONE in call("system.general.timezone_choices")
-    call("system.general.update", {"timezone": TIMEZONE})
-    assert call("system.general.config")["timezone"] == TIMEZONE
