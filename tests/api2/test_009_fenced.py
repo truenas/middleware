bb91c949fcb73683a10153a3830a155a353a5f4e
caleb <yocalebo@gmail.com>
2026-07-23 12:00:15 -0400

    rm bulk tests

--- tests/api2/test_009_fenced.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from auto_config import ha
-from middlewared.test.integration.utils import call
-
-
-@pytest.mark.skipif(not ha, reason='HA only test')
-def test_01_verify_fenced_is_running():
-    assert call('failover.fenced.run_info')['running']
