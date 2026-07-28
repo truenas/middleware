1748ba25f070100de3ce9915bf5a4c2c2f5a45d9
themylogin <themylogin@gmail.com>
2026-07-28 08:44:31 +0200

    NAS-141948 / 27.0.0-BETA.1 / Fix `JobProgressBuffer.set_progress` not being thread-safe (#19402)
    `JobProgressBuffer.set_progress` called `self.job.loop.call_later`. When
    doing so from a thread, it might break event the loop.

--- src/middlewared/middlewared/utils/asyncio_.py
+++ src/middlewared/middlewared/utils/asyncio_.py
@@ -1,5 +1,6 @@
 import asyncio
 from collections.abc import Awaitable, Callable, Iterable
+from typing import Any
 
 
 async def asyncio_map[T, R](
@@ -25,3 +26,36 @@ async def asyncio_map[T, R](
 
     futures = [func(arg) for arg in arguments]
     return await asyncio.gather(*futures)
+
+
+class ThreadsafeTimer[*Ts]:
+    __slots__ = ("_loop", "_delay", "_callback", "_args", "_cancelled", "_handle")
+
+    def __init__(
+        self,
+        loop: asyncio.AbstractEventLoop,
+        delay: float,
+        callback: Callable[[*Ts], Any],
+        *args: *Ts,
+    ) -> None:
+        self._loop = loop
+        self._delay = delay
+        self._callback = callback
+        self._args = args
+        self._cancelled = False
+        self._handle: asyncio.Handle | None = None
+
+        loop.call_soon_threadsafe(self._schedule)
+
+    def _schedule(self) -> None:
+        if not self._cancelled:
+            self._handle = self._loop.call_later(
+                self._delay,
+                self._callback,
+                *self._args,
+            )
+
+    def cancel(self) -> None:
+        self._cancelled = True
+        if self._handle is not None:
+            self._loop.call_soon_threadsafe(self._handle.cancel)
