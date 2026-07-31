5e9b3b3d226f4048b5e4c7e5ea663a6dbfc48b39
Logan Cary <logan.cary@ixsystems.com>
2026-07-31 11:14:53 -0400

    Apply user properties when creating a dataset

--- tests/api2/test_pool_dataset_user_props.py
+++ tests/api2/test_pool_dataset_user_props.py
@@ -4,6 +4,21 @@ from middlewared.test.integration.assets.pool import dataset
 from middlewared.test.integration.utils import call
 
 
+def test_pool_dataset_create_applies_user_props():
+    user_props = [
+        {'key': 'org.truenas.test:first', 'value': 'one'},
+        {'key': 'org.truenas.test:second', 'value': 'two'},
+    ]
+    with dataset('create_user_props', data={'user_properties': user_props}) as ds:
+        result = call(
+            'pool.dataset.query',
+            [['id', '=', ds]],
+            {'get': True, 'extra': {'properties': [], 'retrieve_user_props': True}}
+        )
+        for prop in user_props:
+            assert result['user_properties'].get(prop['key'], {}).get('value') == prop['value'], result
+
+
 @pytest.mark.parametrize('user_props', [True, False])
 def test_pool_dataset_query_user_props_true_false(user_props):
     with dataset("query_test") as ds:
