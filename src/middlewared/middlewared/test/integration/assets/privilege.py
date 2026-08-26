beb7a86d2e4012b4fa1eaf7a4fcae7d619114cf9
themylogin <themylogin@gmail.com>
2026-08-26 20:15:20 +0200

    NAS-142214 / 27.0.0-BETA.1 / More account tests (#19508)
    API tests that provide near-100% coverage for `plugins/account_/*py`

--- src/middlewared/middlewared/test/integration/assets/privilege.py
+++ src/middlewared/middlewared/test/integration/assets/privilege.py
@@ -15,3 +15,23 @@ def privilege(data):
             call("privilege.delete", privilege["id"])
         except InstanceNotFound:
             pass
+
+
+
+@contextlib.contextmanager
+def raw_privilege(data):
+    """Insert a privilege row directly into the datastore, ignoring the validation.
+    """
+    id_ = call("datastore.insert", "account.privilege", {
+        "builtin_name": None,
+        "name": "Test raw",
+        "local_groups": [],
+        "ds_groups": [],
+        "roles": [],
+        "web_shell": False,
+        **data,
+    })
+    try:
+        yield id_
+    finally:
+        call("datastore.delete", "account.privilege", id_)
