ac88ff7b6b611d517520317a2573526afd299f25
themylogin <themylogin@gmail.com>
2026-09-09 20:53:10 +0200

    More tests

--- src/middlewared/middlewared/pytest/unit/plugins/test_rsync_extra_args.py
+++ src/middlewared/middlewared/pytest/unit/plugins/test_rsync_extra_args.py
@@ -1,6 +1,6 @@
 import pytest
 
-from middlewared.plugins.rsync_.utils import quote_extra_args
+from middlewared.plugins.rsync.utils import quote_extra_args
 
 
 @pytest.mark.parametrize(
@@ -49,6 +49,10 @@ def test_glob_is_quoted_against_the_local_shell():
     [
         (['"'], ["'\"'"]),
         (["--exclude", '"unclosed'], ["--exclude", "'\"unclosed'"]),
+        (
+            ['--rsync-path="sudo rsync"', "--exclude=foo\\"],
+            ["'--rsync-path=sudo rsync'", "'--exclude=foo\\'"],
+        ),
     ],
 )
 def test_unbalanced_quotes_are_passed_through(extra, expected):
